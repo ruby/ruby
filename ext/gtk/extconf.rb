a66aaeb22aceb2964c5a79420b29eca400dab532
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
1998-04-27 10:08:11 +0000

    extconf.rb
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/v1_1r@194 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/gtk/extconf.rb
+++ ext/gtk/extconf.rb
@@ -2,9 +2,11 @@ require "mkmf"
 
 # may need to be changed
 $LDFLAGS="-L/usr/X11R6/lib -L/usr/local/lib"
+$CFLAGS=`gtk-config --cflags`
 
 have_library("X11", "XOpenDisplay")
 have_library("Xext", "XShmQueryVersion")
+have_library("Xi", "XOpenDevice")
 if have_library("glib", "g_print") and
     have_library("gdk", "gdk_init") and
     have_library("gtk", "gtk_init")
