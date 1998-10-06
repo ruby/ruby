dc8da69e7cbc54f020ef1182fd28e57bbaa40fc3
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
1998-10-06 03:28:28 +0000

    join 1.1c6
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/v1_1r@310 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/profile.rb
+++ lib/profile.rb
@@ -1,17 +1,17 @@
 
 module Profiler__
-  Start = Float(Time.now)
+  Start = Float(Time.times[0])
   top = "toplevel".intern
   Stack = [[0, 0, top]]
-  MAP = {top => [1, 0, 0, "toplevel", top]}
+  MAP = {top => [1, 0, 0, "#toplevel"]}
 
   p = proc{|event, file, line, id, binding, klass|
     case event
     when "call", "c-call"
-      now = Float(Time.now)
+      now = Float(Time.times[0])
       Stack.push [now, 0.0, id]
     when "return", "c-return"
-      now = Float(Time.now)
+      now = Float(Time.times[0])
       tick = Stack.pop
       data = MAP[id]
       unless data
@@ -21,28 +21,32 @@ module Profiler__
 	else
 	  name += "."
 	end
-	data = [0, 0, 0, name+id.id2name, id]
+	data = [0, 0, 0, name+id.id2name]
 	MAP[id] = data
       end
       data[0] += 1
       cost = now - tick[0]
-      data[1] += cost unless id == Stack[-1][2]
+      data[1] += cost
       data[2] += cost - tick[1]
       Stack[-1][1] += cost
     end
   }
   END {
     set_trace_func nil
-    total = MAP[:toplevel][1] = Float(Time.now) - Start
+    total = Float(Time.times[0]) - Start
+    MAP[:toplevel][1] = total
 #    f = open("./rmon.out", "w")
     f = STDERR
     data = MAP.values.sort!{|a,b| b[2] <=> a[2]}
+    sum = 0
     f.printf "  %%   cumulative   self              self     total\n"           
     f.printf " time   seconds   seconds    calls  ms/call  ms/call  name\n"
     for d in data
-      f.printf "%6.2f %8.2f  %8.2f %8d ", d[2]/total*100, d[1], d[2], d[0]
+      sum += d[2]
+      f.printf "%6.2f %8.2f  %8.2f %8d ", d[2]/total*100, sum, d[2], d[0]
       f.printf "%8.2f %8.2f  %s\n", d[2]*1000/d[0], d[1]*1000/d[0], d[3]
     end
+    p total
     f.close
   }
   set_trace_func p
