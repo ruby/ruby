f96faa860bfaf119ed379db160c24bd815376fd1
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
1999-04-13 05:33:36 +0000

    ndbm.h check
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_3@426 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/dbm/extconf.rb
+++ ext/dbm/extconf.rb
@@ -3,7 +3,7 @@ $LDFLAGS = "-L/usr/local/lib"
 have_library("gdbm", "dbm_open") or
   have_library("db", "dbm_open") or
   have_library("dbm", "dbm_open")
-if have_func("dbm_open")
+if have_header("ndbm.h") and have_func("dbm_open")
   have_func("dbm_clearerr")
   create_makefile("dbm")
 end
