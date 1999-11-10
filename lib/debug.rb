94f54fc0fa7a0846fd588440b414bbd301593497
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
1999-11-10 06:36:01 +0000

    19991110
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_4@560 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/debug.rb
+++ lib/debug.rb
@@ -25,8 +25,9 @@ class DEBUGGER__
     @last_file = nil
     @last = [nil, nil]
     @scripts = {}
-  end
     @no_step = nil
+    @finish_pos = 0
+  end
 
   DEBUG_LAST_CMD = []
 
@@ -255,9 +256,13 @@ class DEBUGGER__
         STDOUT.printf "#%d %s\n", frame_pos, info
 
       when /^fi(?:nish)?$/
-	@finish_pos = @frames.size - frame_pos
-	frame_pos = 0
-	return
+	if  frame_pos == 0
+	  STDOUT.print "\"finish\" not meaningful in the outermost frame.\n"
+	else
+	  @finish_pos = @frames.size - frame_pos
+	  frame_pos = 0
+	  return
+	end
 
       when /^q(?:uit)?$/
 	input = readline("really quit? (y/n) ", false)
@@ -384,10 +389,10 @@ class DEBUGGER__
       @frames.unshift binding
     
     when 'return', 'end'
+      @frames.shift
       if @frames.size == @finish_pos
 	@stop_next = 1
       end
-      @frames.shift
     
     when 'raise' 
       excn_handle(file, line, id, binding)
