6b74d4ca9fdac5bfada26585ec38713bfc2428fd
eban <eban@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2001-04-06 09:07:45 +0000

    * bignum.c (bigdivrem): ny must be 0 or greater.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_6@1308 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bignum.c
+++ bignum.c
@@ -918,7 +918,7 @@ bigdivrem(x, y, divp, modp)
     if (modp) {			/* just normalize remainder */
 	*modp = rb_big_clone(z);
 	zds = BDIGITS(*modp);
-	while (!zds[ny-1]) ny--;
+	while (ny-- && !zds[ny-1]) ;
 	if (dd) {
 	    t2 = 0; i = ny;
 	    while(i--) {
