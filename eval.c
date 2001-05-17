4fc74dc0b20cb3535724156c81c2507546465fa5
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2001-05-17 08:49:44 +0000

    * eval.c (rb_call0): address of local_vars might change during eval.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_6@1417 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- eval.c
+++ eval.c
@@ -4421,10 +4421,13 @@ rb_call0(klass, recv, id, argc, argv, body, nosuper)
 			    rb_eval(recv, opt);
 			}
 			if (node->nd_rest >= 0) {
+			    VALUE v;
+
 			    if (argc > 0)
-				local_vars[node->nd_rest]=rb_ary_new4(argc,argv);
+				v = rb_ary_new4(argc,argv);
 			    else
-				local_vars[node->nd_rest]=rb_ary_new2(0);
+				v = rb_ary_new2(0);
+			    local_vars[node->nd_rest] = v;
 			}
 		    }
 		}
