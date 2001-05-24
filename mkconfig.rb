d5295382972becda6609864b9b5c6400b0b71a81
eban <eban@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2001-05-24 07:13:56 +0000

    * mkconfig.rb: autoconf 2.50 support.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_6@1445 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- mkconfig.rb
+++ mkconfig.rb
@@ -48,7 +48,7 @@ File.foreach "config.status" do |$_|
       v_others << v
     end
     has_version = true if name == "MAJOR"
-  elsif /^ac_given_srcdir=(.*)/
+  elsif /^(?:ac_given_)?srcdir=(.*)/
     v_fast << "  CONFIG[\"srcdir\"] = \"" + File.expand_path($1) + "\"\n"
     has_srcdir = true
   elsif /^ac_given_INSTALL=(.*)/
@@ -58,7 +58,7 @@ File.foreach "config.status" do |$_|
 end
 
 if not has_srcdir
-  v_fast << "  CONFIG[\"srcdir\"] = \"" + File.expand_path(srcdir) + "\"\n"
+  v_fast << "  CONFIG[\"srcdir\"] = \"" + File.expand_path(srcdir || '.') + "\"\n"
 end
 
 if not has_version
