d7c1ab0713a0505c88fe27a978288c9c36f2713a
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2001-07-03 15:02:20 +0000

    * lib/mkmf.rb: distclean should remove mkmf.log as well.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_6@1560 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -475,7 +475,7 @@ clean:;		@$(RM) *.#{$OBJEXT} *.so *.sl *.a $(DLLIB)
 		@$(RM) $(TARGET).lib $(TARGET).exp $(TARGET).ilk *.pdb
 
 distclean:	clean
-		@$(RM) Makefile extconf.h conftest.*
+		@$(RM) Makefile extconf.h conftest.* mkmf.rb
 		@$(RM) core ruby$(EXEEXT) *~
 
 realclean:	distclean
