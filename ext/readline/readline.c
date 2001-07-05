f985b689ca330d73afbd5efa214aae28606b3635
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2001-07-05 15:54:00 +0000

    * ext/readline/readline.c (readline_event): merge from 1.7: a   non-void function should return a value.
    * ext/readline/readline.c: merge from 1.7: add new methods:
      Readline::completion_append_character and
      Readline::completion_append_character=.
    
    * ext/readline/README, ext/readline/README.jp: merge from 1.7: add
      an English version of the document.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_6@1570 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/readline/readline.c
+++ ext/readline/readline.c
@@ -26,6 +26,7 @@ readline_event()
 {
     CHECK_INTS;
     rb_thread_schedule();
+    return 0;
 }
 
 static VALUE
@@ -164,6 +165,36 @@ readline_s_emacs_editing_mode(self)
 {
     rl_emacs_editing_mode(1,0);
     return Qnil;
+}
+
+static VALUE
+readline_s_set_completion_append_character(self, str)
+    VALUE self, str;
+{
+    if (NIL_P(str)) {
+	rl_completion_append_character = '\0';
+    } else {
+	Check_Type(str, T_STRING);
+
+	rl_completion_append_character = RSTRING(str)->ptr[0];
+    }
+
+    return self;
+}
+
+static VALUE
+readline_s_get_completion_append_character(self)
+    VALUE self;
+{
+    VALUE str;
+
+    if (rl_completion_append_character == '\0')
+	return Qnil;
+
+    str = rb_str_new("", 1);
+    RSTRING(str)->ptr[0] = rl_completion_append_character;
+
+    return str;
 }
 
 static VALUE
@@ -394,6 +425,10 @@ Init_readline()
 			       readline_s_vi_editing_mode, 0);
     rb_define_singleton_method(mReadline, "emacs_editing_mode",
 			       readline_s_emacs_editing_mode, 0);
+    rb_define_singleton_method(mReadline, "completion_append_character=",
+			       readline_s_set_completion_append_character, 1);
+    rb_define_singleton_method(mReadline, "completion_append_character",
+			       readline_s_get_completion_append_character, 0);
 
     histary = rb_obj_alloc(rb_cObject);
     rb_extend_object(histary, rb_mEnumerable);
