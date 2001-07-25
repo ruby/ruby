51efc221c15a958dc019087d3bb2ac0a80163dc7
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2001-07-25 11:31:59 +0000

    Fix a bogon in my last commit..
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@1648 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- intern.h
+++ intern.h
@@ -401,7 +401,7 @@ VALUE rb_cvar_defined _((VALUE, ID));
 void rb_cvar_set _((VALUE, ID, VALUE));
 VALUE rb_cvar_get _((VALUE, ID));
 VALUE rb_cvar_singleton _((VALUE));
-VALUE rb_cv_set _((VALUE, const char *, VALUE));
+void rb_cv_set _((VALUE, const char *, VALUE));
 VALUE rb_cv_get _((VALUE, const char *));
 void rb_define_class_variable _((VALUE, const char *, VALUE));
 VALUE rb_mod_class_variables _((VALUE));
