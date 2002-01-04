a7685a6b877631dbb4ea9538311c124b5822677a
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2002-01-04 09:03:11 +0000

    The author of getoptlong.rb has agreed on changing the license from GPL to Ruby's.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@1958 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/getoptlong.rb
+++ lib/getoptlong.rb
@@ -1,15 +1,8 @@
 #                                                         -*- Ruby -*-
 # Copyright (C) 1998, 1999, 2000  Motoyuki Kasahara
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
-# any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# You may redistribute it and/or modify it under the same license
+# terms as Ruby.
 #
 
 #
