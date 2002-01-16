e173962181dafbab3f8d850ab765e88f640a9d39
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2002-01-16 09:32:51 +0000

    * eval.c: bugus commit fixed (again, sorry).
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@1998 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- eval.c
+++ eval.c
@@ -1815,6 +1815,8 @@ is_defined(self, node, buf)
 		    !rb_obj_is_kind_of(self, rb_class_real(val)))
 		    break;
 	    }
+	    else if (!rb_method_boundp(val, node->nd_mid, call))
+		break;
 	    return arg_defined(self, node->nd_args, buf, "method");
 	}
 	break;
