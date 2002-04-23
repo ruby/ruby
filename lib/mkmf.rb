47ec225cf69289a505e4a50ce1bf0bcd0da87a10
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2002-04-23 16:45:09 +0000

    * lib/mkmf.rb (create_makefile): should print depend file when   make is other than nmake.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_6@2400 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -597,7 +597,8 @@ EOMF
     mfile.printf "###\n"
     while line = dfile.gets()
       line.gsub!(/\.o\b/, ".#{$OBJEXT}")
-      mfile.printf "%s", line.gsub(/(\s)([^\s\/]+\.[ch])/, '\1{$(srcdir)}\2') if /nmake/i =~ $make
+      line.gsub(/(\s)([^\s\/]+\.[ch])/, '\1{$(srcdir)}\2') if /nmake/i =~ $make
+      mfile.printf "%s", line
     end
     dfile.close
   end
