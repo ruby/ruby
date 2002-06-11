0b67be50a514386fb3f7ac9c4488f7e2c2e3f744
H_Konishi <H_Konishi@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2002-06-11 03:41:52 +0000

    * bcc32/configure.bat fix.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@2544 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bcc32/configure.bat
+++ bcc32/configure.bat
@@ -5,7 +5,7 @@
 
 echo> ~tmp~.mak ####
 echo>> ~tmp~.mak conf = %0
-echo>> ~tmp~.mak $(conf:\=/):
+echo>> ~tmp~.mak $(conf:\=/): nul
 echo>> ~tmp~.mak  @del ~tmp~.mak
 echo>> ~tmp~.mak  make -Dbcc32dir="$(@D)" -f$(@D)/setup.mak %1
 make -f ~tmp~.mak
