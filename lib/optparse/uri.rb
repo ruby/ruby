0a85c658591d8b0763423a419dd96a9b957c3918
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2002-08-07 00:47:41 +0000

    * lib/optparse/uri.rb: require standard uri module.  thanks to   Minero Aoki.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@2693 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/optparse/uri.rb
+++ lib/optparse/uri.rb
@@ -1,15 +1,6 @@
 # -*- ruby -*-
 
 require 'optparse'
-unless defined?(URI)
-  begin
-    require 'URI/uri'		# Akira Yamada version.
-  rescue LoadError
-    require 'uri/uri'		# Tomoyuki Kosimizu version.
-  end
-end
-if URI.respond_to?(:parse)
-  OptionParser.accept(URI) {|s| [URI.parse(s)] if s}
-else
-  OptionParser.accept(URI) {|s| [URI.create(s)] if s}
-end
+require 'uri'
+
+OptionParser.accept(URI) {|s| [URI.parse(s)] if s}
