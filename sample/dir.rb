913bc18761b5019492c30c0df9af7c5860a23bc3
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2002-12-19 20:30:05 +0000

    The use of $_ and ~/RE/ is discouraged.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@3182 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- sample/dir.rb
+++ sample/dir.rb
@@ -2,8 +2,10 @@
 # list all files but .*/*~/*.o
 dirp = Dir.open(".")
 for f in dirp
-  $_ = f
-  unless (~/^\./ || ~/~$/ || ~/\.o/)
+  case f
+  when /^\./, /~$/, /\.o/
+    # do not print
+  else
     print f, "\n"
   end
 end
