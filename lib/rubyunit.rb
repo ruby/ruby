04f2b8f7bf16ad37d3c28f0e06eb02d63ab6a731
ntalbott <ntalbott@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2003-02-12 03:12:14 +0000

    Initial revision
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@3477 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ lib/rubyunit.rb
@@ -0,0 +1,8 @@
+# Author:: Nathaniel Talbott.
+# Copyright:: Copyright (c) 2000-2002 Nathaniel Talbott. All rights reserved.
+# License:: Ruby license.
+
+require 'runit/testcase'
+require 'test/unit'
+
+TestCase = RUNIT::TestCase
