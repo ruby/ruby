d616f6005c9ca66db6d1aea08d7235a00f7fab58
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2003-06-14 13:41:29 +0000

    * eval.c (method_arity): should handle NODE_BMETHOD and   NODE_DMETHOD. [ruby-core:01138]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@3941 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- eval.c
+++ eval.c
@@ -7444,6 +7444,9 @@ method_arity(method)
 	return INT2FIX(1);
       case NODE_IVAR:
 	return INT2FIX(0);
+      case NODE_BMETHOD:
+      case NODE_DMETHOD:
+       return block_arity(method);
       default:
 	body = body->nd_next;	/* skip NODE_SCOPE */
 	if (nd_type(body) == NODE_BLOCK)
