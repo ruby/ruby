a644c35a99c043a7870bc3c550940052976ece56
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2003-07-28 14:23:12 +0000

    * file.c (Init_File): IO should include File::Const.   [ruby-dev:20964]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@4194 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- file.c
+++ file.c
@@ -2578,7 +2578,6 @@ rb_file_const(name, value)
     VALUE value;
 {
     rb_define_const(rb_mFConst, name, value);
-    rb_define_const(rb_cIO, name, value);
 }
 
 static int
@@ -2919,6 +2918,7 @@ Init_File()
     rb_define_method(rb_cFile, "flock", rb_file_flock, 1);
 
     rb_mFConst = rb_define_module_under(rb_cFile, "Constants");
+    rb_include_module(rb_cIO, rb_mFConst);
     rb_file_const("LOCK_SH", INT2FIX(LOCK_SH));
     rb_file_const("LOCK_EX", INT2FIX(LOCK_EX));
     rb_file_const("LOCK_UN", INT2FIX(LOCK_UN));
