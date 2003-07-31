0cdf0d99c1e5164c53676a39265ff99120c8a026
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2003-07-31 16:45:07 +0000

    * gc.c (Init_stack): wrong magic number.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@4248 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- gc.c
+++ gc.c
@@ -1322,7 +1322,7 @@ Init_stack(addr)
     /* ruby crashes on IA64 if compiled with optimizer on */
     /* when if STACK_LEVEL_MAX is greater than this magic number */
     /* I know this is a kludge.  I suspect optimizer bug */
-#define IA64_MAGIC_STACK_LIMIT 65535
+#define IA64_MAGIC_STACK_LIMIT 32768
     if (STACK_LEVEL_MAX > IA64_MAGIC_STACK_LIMIT)
 	STACK_LEVEL_MAX = IA64_MAGIC_STACK_LIMIT;
 #endif
