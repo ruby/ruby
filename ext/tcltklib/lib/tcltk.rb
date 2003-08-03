a144ffe53f0293bc087a741484489aa426ab7989
nagai <nagai@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2003-08-03 02:34:02 +0000

    *bug fix ( NOT MAINTAINED : only for running 'line2.rb' demo. )
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@4292 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/tcltklib/lib/tcltk.rb
+++ ext/tcltklib/lib/tcltk.rb
@@ -93,11 +93,9 @@ class TclTkInterpreter
     # ruby_fmt command format arguments by `format' and call `ruby' command
     # (notice ruby command receives only one argument)
     if $DEBUG
-      @ip._eval("proc ruby_fmt {fmt args} { puts \"ruby_fmt: $fmt
-$args\" ; set cmd [list ruby [format $fmt $args]] ; uplevel $cmd }")
+      @ip._eval("proc ruby_fmt {fmt args} { puts \"ruby_fmt: $fmt $args\" ; set cmd [list ruby [format $fmt $args]] ; uplevel $cmd }")
     else
-      @ip._eval("proc ruby_fmt {fmt args} { set cmd [list ruby [format
-$fmt $args]] ; uplevel $cmd }")
+      @ip._eval("proc ruby_fmt {fmt args} { set cmd [list ruby [format $fmt $args]] ; uplevel $cmd }")
     end
 
     # @ip._get_eval_string(*args): generate string to evaluate in tcl interpreter
