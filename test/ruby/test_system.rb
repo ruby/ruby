bd895e909d5591bc771bb4f8bf3c0ec3934726df
eban <eban@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2003-09-05 03:12:29 +0000

    * test/ruby/test_system.rb (test_system): check existence of ruby   interpreter.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@4507 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_system.rb
+++ test/ruby/test_system.rb
@@ -4,22 +4,27 @@ $KCODE = 'none'
 
 class TestSystem < Test::Unit::TestCase
   def test_system
+    if File.exist? "miniruby" or File.exist? "miniruby.exe"
+      ruby = "./miniruby"
+    else
+      ruby = "ruby"
+    end
     assert(`echo foobar` == "foobar\n")
-    assert(`./miniruby -e 'print "foobar"'` == 'foobar')
+    assert(`#{ruby} -e 'print "foobar"'` == 'foobar')
     
     tmp = open("script_tmp", "w")
     tmp.print "print $zzz\n";
     tmp.close
     
-    assert(`./miniruby -s script_tmp -zzz` == 'true')
-    assert(`./miniruby -s script_tmp -zzz=555` == '555')
+    assert(`#{ruby} -s script_tmp -zzz` == 'true')
+    assert(`#{ruby} -s script_tmp -zzz=555` == '555')
     
     tmp = open("script_tmp", "w")
     tmp.print "#! /usr/local/bin/ruby -s\n";
     tmp.print "print $zzz\n";
     tmp.close
     
-    assert(`./miniruby script_tmp -zzz=678` == '678')
+    assert(`#{ruby} script_tmp -zzz=678` == '678')
     
     tmp = open("script_tmp", "w")
     tmp.print "this is a leading junk\n";
@@ -29,8 +34,8 @@ class TestSystem < Test::Unit::TestCase
     tmp.print "this is a trailing junk\n";
     tmp.close
     
-    assert(`./miniruby -x script_tmp` == 'nil')
-    assert(`./miniruby -x script_tmp -zzz=555` == '555')
+    assert(`#{ruby} -x script_tmp` == 'nil')
+    assert(`#{ruby} -x script_tmp -zzz=555` == '555')
     
     tmp = open("script_tmp", "w")
     for i in 1..5
@@ -38,7 +43,7 @@ class TestSystem < Test::Unit::TestCase
     end
     tmp.close
     
-    `./miniruby -i.bak -pe 'sub(/^[0-9]+$/){$&.to_i * 5}' script_tmp`
+    `#{ruby} -i.bak -pe 'sub(/^[0-9]+$/){$&.to_i * 5}' script_tmp`
     done = true
     tmp = open("script_tmp", "r")
     while tmp.gets
