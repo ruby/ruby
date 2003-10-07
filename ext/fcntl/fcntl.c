4f6558a4eca8cdee8dba8eab202b23a68f13f64d
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2003-10-07 03:24:11 +0000

    * ext/fcntl/fcntl.c (Init_fcntl): define Fcntl::ACCMODE.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@4714 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/fcntl/fcntl.c
+++ ext/fcntl/fcntl.c
@@ -104,4 +104,9 @@ Init_fcntl()
 #ifdef O_WRONLY
     rb_define_const(mFcntl, "O_WRONLY", INT2NUM(O_WRONLY));
 #endif
+#ifdef O_ACCMODE
+    rb_define_const(mFcntl, "ACCMODE", INT2FIX(O_ACCMODE));
+#else
+    rb_define_const(mFcntl, "ACCMODE", INT2FIX(O_RDONLY | O_WRONLY | O_RDWR));
+#endif
 }
