c7d278adb9356e78465de640f9a41cdc0fd1f09a
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2003-10-17 02:18:01 +0000

    * test/ruby/test_range.rb: added.
    * MANIFEST: add test/ruby/test_range.rb.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@4792 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ test/ruby/test_range.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+
+$KCODE = 'none'
+
+class TestRange < Test::Unit::TestCase
+  def test_range_string
+    # XXX: Is this really the test of Range?
+    assert_equal([], ("a" ... "a").to_a)
+    assert_equal(["a"], ("a" .. "a").to_a)
+    assert_equal(["a"], ("a" ... "b").to_a)
+    assert_equal(["a", "b"], ("a" .. "b").to_a)
+  end
+end
