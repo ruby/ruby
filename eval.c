229c37a6dad61030bffc8d07f9c708f8a911601b
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2003-10-20 09:28:13 +0000

    * eval.c (rb_catch): backout.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@4812 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- eval.c
+++ eval.c
@@ -10217,19 +10217,7 @@ rb_catch(tag, func, data)
     VALUE (*func)();
     VALUE data;
 {
-    int state;
-    VALUE val = Qnil;		/* OK */
-
-    PUSH_TAG(PROT_NONE);
-    PUSH_SCOPE();
-    if ((state = EXEC_TAG()) == 0) {
-	val = rb_iterate((VALUE(*)_((VALUE)))catch_i, ID2SYM(rb_intern(tag)), func, data);
-    }
-    POP_SCOPE();
-    POP_TAG();
-    if (state) JUMP_TAG(state);
-
-    return val;
+    return rb_iterate((VALUE(*)_((VALUE)))catch_i, ID2SYM(rb_intern(tag)), func, data);
 }
 
 static VALUE
