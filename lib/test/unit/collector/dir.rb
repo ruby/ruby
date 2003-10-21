8af34b7b33714eeb0f0dead1ce79a98b780ddb22
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2003-10-21 06:57:14 +0000

    * lib/test/unit/collector/dir.rb (Test::Unit::Collector::Dir#collect_file):   prepend the directory of target file to the load path.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@4825 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/test/unit/collector/dir.rb
+++ lib/test/unit/collector/dir.rb
@@ -67,12 +67,16 @@ module Test
         end
 
         def collect_file(name, suites, already_gathered)
+          loadpath = $:.dup
+          $:.unshift(File.dirname(name))
           if(@req)
             @req.require(name)
           else
             require(name)
           end
           find_test_cases(already_gathered).each{|t| add_suite(suites, t.suite)}
+        ensure
+          $:.replace(loadpath)
         end
       end
     end
