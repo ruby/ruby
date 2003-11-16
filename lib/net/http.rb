d4060e0467e64a8ae0c800de4e53ea0af0772dd6
aamine <aamine@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2003-11-16 00:46:16 +0000

    * lib/net/http.rb (set_debug_output): warn if method is called after #start. [ruby-dev:38798]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@4963 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/net/http.rb
+++ lib/net/http.rb
@@ -366,6 +366,7 @@ module Net # :nodoc:
     #   http.start { .... }
     #
     def set_debug_output( output )
+      warn 'Net::HTTP#set_debug_output called after HTTP started'
       @debug_output = output
     end
 
