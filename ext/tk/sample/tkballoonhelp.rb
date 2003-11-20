f465e8a1c86883864990ea44b77b6262967ea8cb
nagai <nagai@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2003-11-20 08:53:52 +0000

    * ext/tk/sample/tkballoonhelp.rb: new sample script * ext/tk/sample/tkmultilistbox.rb: ditto * ext/tk/sample/tktextframe.rb: ditto
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@4991 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ ext/tk/sample/tkballoonhelp.rb
@@ -0,0 +1,99 @@
+#
+# tkballoonhelp.rb : simple balloon help widget
+#                       by Hidetoshi NAGAI (nagai@ai.kyutech.ac.jp)
+#
+# Add a balloon help to a widget. 
+# This widget has only poor featureas. If you need more useful features, 
+# please try to use the Tix extension of Tcl/Tk under Ruby/Tk.
+# 
+# The interval time to display a balloon help is defined 'interval' option
+# (default is 1000ms). 
+#
+require 'tk'
+
+class TkBalloonHelp<TkLabel
+  def _balloon_binding(interval)
+    @timer = TkAfter.new(interval, 1, proc{show})
+    def @timer.interval(val)
+      @sleep_time = val
+    end
+    @bindtag = TkBindTag.new
+    @bindtag.bind('Enter',  proc{@timer.start})
+    @bindtag.bind('Motion', proc{@timer.restart; erase})
+    @bindtag.bind('Any-ButtonPress', proc{@timer.restart; erase})
+    @bindtag.bind('Leave',  proc{@timer.stop; erase})
+    tags = @parent.bindtags
+    idx = tags.index(@parent)
+    tags[idx,0] = @bindtag
+    @parent.bindtags(tags)
+  end
+  private :_balloon_binding
+
+  def initialize(parent=nil, keys={})
+    @parent = parent
+
+    @frame = TkToplevel.new(@parent)
+    @frame.withdraw
+    @frame.overrideredirect(true)
+    @frame.transient(TkWinfo.toplevel(@parent))
+    @epath = @frame.path
+
+    keys = {} unless keys
+
+    @interval = keys.delete('interval'){1000}
+    _balloon_binding(@interval)
+
+    @label = TkLabel.new(@frame, 'background'=>'bisque').pack
+    @label.configure(_symbolkey2str(keys)) unless keys.empty?
+    @path = @label
+  end
+
+  def epath
+    @epath
+  end
+
+  def interval(val)
+    if val
+      @timer.interval(val)
+    else
+      @interval
+    end
+  end
+
+  def show
+    x = TkWinfo.pointerx(@parent)
+    y = TkWinfo.pointery(@parent)
+    @frame.geometry("+#{x+1}+#{y+1}")
+    @frame.deiconify
+    @frame.raise
+
+    @org_cursor = @parent['cursor']
+    @parent.cursor('crosshair') 
+  end
+
+  def erase
+    @parent.cursor(@org_cursor) 
+    @frame.withdraw
+  end
+
+  def destroy
+    @frame.destroy
+  end
+end
+
+################################################
+# test
+################################################
+if __FILE__ == $0
+  TkButton.new('text'=>'This button has a balloon help') {|b|
+    pack('fill'=>'x')
+    TkBalloonHelp.new(b, 'text'=>' Message ')
+  }
+  TkButton.new('text'=>'This button has another balloon help') {|b|
+    pack('fill'=>'x')
+    TkBalloonHelp.new(b, 'text'=>'configured message', 
+		      'interval'=>500, 'font'=>'courier', 
+		      'background'=>'gray', 'foreground'=>'red')
+  }
+  Tk.mainloop
+end
---
+++ ext/tk/sample/tktextframe.rb
@@ -0,0 +1,105 @@
+#
+#  tktextframe.rb : a sample of TkComposite
+#
+#                         by Hidetoshi NAGAI (nagai@ai.kyutech.ac.jp)
+#
+require 'tk'
+
+class TkTextFrame < TkText
+  include TkComposite
+
+  def initialize_composite(keys={})
+    keys = _symbolkey2str(keys)
+
+    # create scrollbars
+    @v_scroll = TkScrollbar.new(@frame, 'orient'=>'vertical')
+    @h_scroll = TkScrollbar.new(@frame, 'orient'=>'horizontal')
+
+    # create a text widget
+    @text = TkText.new(@frame, 'wrap'=>'none')
+
+    # set default receiver of method calls
+    @path = @text.path
+
+    # assign scrollbars
+    @text.xscrollbar(@h_scroll)
+    @text.yscrollbar(@v_scroll)
+
+    # allignment
+    TkGrid.rowconfigure(@frame, 0, 'weight'=>1, 'minsize'=>0)
+    TkGrid.columnconfigure(@frame, 0, 'weight'=>1, 'minsize'=>0)
+    @text.grid('row'=>0, 'column'=>0, 'sticky'=>'news')
+
+    # scrollbars ON
+    vscroll(keys.delete('vscroll'){true})
+    hscroll(keys.delete('hscroll'){true})
+
+    # set background of the text widget
+    color = keys.delete('textbackground')
+    textbackground(color) if color
+
+    # set receiver widgets for configure methods
+    delegate('DEFAULT', @text)
+    delegate('background', @frame, @h_scroll, @v_scroll)
+    delegate('activebackground', @h_scroll, @v_scroll)
+    delegate('troughcolor', @h_scroll, @v_scroll)
+    delegate('repeatdelay', @h_scroll, @v_scroll)
+    delegate('repeatinterval', @h_scroll, @v_scroll)
+    delegate('borderwidth', @frame)
+    delegate('relief', @frame)
+
+    # do configure
+    configure keys unless keys.empty?
+  end
+
+  # set background color of text widget
+  def textbackground(color = nil)
+    if color
+      @text.background(color)
+    else
+      @text.background
+    end
+  end
+
+  # vertical scrollbar : ON/OFF
+  def vscroll(mode)
+    st = TkGrid.info(@v_scroll)
+    if mode && st == [] then
+      @v_scroll.grid('row'=>0, 'column'=>1, 'sticky'=>'ns')
+    elsif !mode && st != [] then
+      @v_scroll.ungrid
+    end
+    self
+  end
+
+  # horizontal scrollbar : ON/OFF
+  def hscroll(mode, wrap_mode="char")
+    st = TkGrid.info(@h_scroll)
+    if mode && st == [] then
+      @h_scroll.grid('row'=>1, 'column'=>0, 'sticky'=>'ew')
+      wrap 'none'  # => self.wrap('none')
+    elsif !mode && st != [] then
+      @h_scroll.ungrid
+      wrap wrap_mode  # => self.wrap(wrap_mode)
+    end
+    self
+  end
+end
+
+
+################################################
+# test
+################################################
+if __FILE__ == $0
+  f = TkFrame.new.pack('fill'=>'x')
+  t = TkTextFrame.new.pack
+  TkButton.new(f, 'text'=>'vscr OFF', 
+	       'command'=>proc{t.vscroll(false)}).pack('side'=>'right')
+  TkButton.new(f, 'text'=>'vscr ON', 
+	       'command'=>proc{t.vscroll(true)}).pack('side'=>'right')
+  TkButton.new(f, 'text'=>'hscr ON', 
+	       'command'=>proc{t.hscroll(true)}).pack('side'=>'left')
+  TkButton.new(f, 'text'=>'hscr OFF', 
+	       'command'=>proc{t.hscroll(false)}).pack('side'=>'left')
+  Tk.mainloop
+end
