9ac4ebbf2178d48ae7f844e02572f8662c91a589
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-01-12 02:46:40 +0000

    * configure.in (LIBPATHFLAG, RPATHFLAG): enclose paths with single   quotes.  [ruby-dev:22564]
    * lib/mkmf.rb (libpathflag): do not enclose with quotes always.
    
    * {bcc32,win32,wince}/Makefile.sub (LIBPATHFLAG): quoted.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@5442 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -234,7 +234,7 @@ end
 
 def libpathflag(libpath=$LIBPATH)
   libpath.map{|x|
-    (x == "$(topdir)" ? LIBPATHFLAG : LIBPATHFLAG+RPATHFLAG) % %['#{x}']
+    (x == "$(topdir)" ? LIBPATHFLAG : LIBPATHFLAG+RPATHFLAG) % x
   }.join
 end
 
@@ -1020,7 +1020,7 @@ LINK_SO = config_string('LINK_SO') ||
     "$(LDSHARED) $(DLDFLAGS) $(LIBPATH) #{OUTFLAG}$(DLLIB) " \
     "$(OBJS) $(LOCAL_LIBS) $(LIBS)"
   end
-LIBPATHFLAG = config_string('LIBPATHFLAG') || ' -L%s'
+LIBPATHFLAG = config_string('LIBPATHFLAG') || " -L'%s'"
 RPATHFLAG = config_string('RPATHFLAG') || ''
 LIBARG = config_string('LIBARG') || '-l%s'
 
