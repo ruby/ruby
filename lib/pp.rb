8fc2fa55f975045fbba58ae79a53dc6e3b6c77c3
nahi <nahi@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-02-06 13:42:15 +0000

    * lib/pp.rb (PPInspectTest#test_to_s_with_iv): rollback the previous           commit.  [ruby-dev:22813]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@5635 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/pp.rb
+++ lib/pp.rb
@@ -541,7 +541,6 @@ if __FILE__ == $0
       a.instance_eval { @a = nil }
       result = PP.pp(a, '')
       assert_equal("#{a.inspect}\n", result)
-      a.instance_eval { remove_instance_variable("@a") }
     end
     
     def test_to_s_without_iv
