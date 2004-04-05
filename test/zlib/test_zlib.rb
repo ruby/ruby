22757fbaba39cfe9eecdd1604c6d95702dd04708
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-04-05 14:59:33 +0000

    update test_new_nil.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@6100 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/zlib/test_zlib.rb
+++ test/zlib/test_zlib.rb
@@ -5,12 +5,6 @@ require 'zlib'
 
 class TestZlibGzipWriter < Test::Unit::TestCase
   def test_new_nil # [ruby-dev:23228]
-    old = $VERBOSE
-    $VERBOSE = nil
-    begin
-      assert_raises(NoMethodError) { Zlib::GzipWriter.new(nil).close }
-    ensure
-      $VERBOSE = old
-    end
+    assert_raises(NoMethodError) { Zlib::GzipWriter.new(nil).close }
   end
 end
