dc252a75a251772f5d2bd9df442d9e6df74638cf
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-04-06 02:59:13 +0000

    check zlib existence.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@6103 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/zlib/test_zlib.rb
+++ test/zlib/test_zlib.rb
@@ -1,10 +1,15 @@
 require 'test/unit/testsuite'
 require 'test/unit/testcase'
 
-require 'zlib'
+begin
+  require 'zlib'
+rescue LoadError
+end
 
-class TestZlibGzipWriter < Test::Unit::TestCase
-  def test_new_nil # [ruby-dev:23228]
-    assert_raises(NoMethodError) { Zlib::GzipWriter.new(nil).close }
+if defined? Zlib
+  class TestZlibGzipWriter < Test::Unit::TestCase
+    def test_new_nil # [ruby-dev:23228]
+      assert_raises(NoMethodError) { Zlib::GzipWriter.new(nil).close }
+    end
   end
 end
