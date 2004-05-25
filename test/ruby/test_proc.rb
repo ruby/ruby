82572952ecf82aad6bc47a51e3d63d7b52858b2d
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-05-25 02:54:22 +0000

    * eval.c (rb_yield_0, proc_invoke, proc_arity): allow passing a block   to a Proc.  [ruby-dev:23533]
    * parse.y (block_par, block_var): ditto.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@6402 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_proc.rb
+++ test/ruby/test_proc.rb
@@ -86,4 +86,9 @@ class TestProc < Test::Unit::TestCase
     b = lambda {}
     assert_not_equal(a, b)
   end
+
+  def test_block_par
+    assert_equal(10, Proc.new{|&b| b.call(10)}.call {|x| x})
+    assert_equal(12, Proc.new{|a,&b| b.call(a)}.call(12) {|x| x})
+  end
 end
