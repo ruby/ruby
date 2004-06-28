44774c3557b74a0c02a266378a11f83c31bd0baa
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-06-28 05:58:13 +0000

    * configure.in, lib/mkmf.rb (LIBPATHFLAG): use double quotes due to   DOSISH compilers.  [ruby-core:03107]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@6538 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -1037,7 +1037,7 @@ LINK_SO = config_string('LINK_SO') ||
     "$(LDSHARED) $(DLDFLAGS) $(LIBPATH) #{OUTFLAG}$(DLLIB) " \
     "$(OBJS) $(LOCAL_LIBS) $(LIBS)"
   end
-LIBPATHFLAG = config_string('LIBPATHFLAG') || " -L'%s'"
+LIBPATHFLAG = config_string('LIBPATHFLAG') || ' -L"%s"'
 RPATHFLAG = config_string('RPATHFLAG') || ''
 LIBARG = config_string('LIBARG') || '-l%s'
 
