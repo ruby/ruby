4e11d565d3b2e3caad1e791be78e60ee76613401
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-07-13 06:54:01 +0000

    * lib/mkmf.rb (init_mkmf): Do not add $(libdir) to $LIBPATH in    extmk mode.
    * lib/mkmf.rb (dir_config): Prepend a new library path instead of
      appending so it is tried first.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@6619 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -670,7 +670,7 @@ def dir_config(target, idefault=nil, ldefault=nil)
     ldirs.concat(defaults.collect {|dir| dir + "/lib"})
     ldir = ([ldir] + ldirs).compact.join(File::PATH_SEPARATOR)
   end
-  $LIBPATH |= ldirs
+  $LIBPATH = ldirs | $LIBPATH
 
   [idir, ldir]
 end
@@ -969,8 +969,7 @@ def init_mkmf(config = CONFIG)
   $LIBRUBYARG = ""
   $LIBRUBYARG_STATIC = config['LIBRUBYARG_STATIC']
   $LIBRUBYARG_SHARED = config['LIBRUBYARG_SHARED']
-  $LIBPATH = CROSS_COMPILING ? [] : ["$(libdir)"]
-  $LIBPATH.unshift("$(topdir)") if $extmk
+  $LIBPATH = $extmk ? ["$(topdir)"] : CROSS_COMPILING ? [] : ["$(libdir)"]
   $INSTALLFILES = nil
 
   $objs = nil
