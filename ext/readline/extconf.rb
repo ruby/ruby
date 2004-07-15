17676bfc5ea18d72ad75ee243afa77b32ad04d70
shugo <shugo@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-07-15 14:11:38 +0000

    * ext/readline/extconf.rb: added dir_config for curses, ncurses, termcap. (backported from HEAD)
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@6641 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/readline/extconf.rb
+++ ext/readline/extconf.rb
@@ -1,5 +1,8 @@
 require "mkmf"
 
+dir_config('curses')
+dir_config('ncurses')
+dir_config('termcap')
 dir_config("readline")
 have_library("user32", nil) if /cygwin/ === RUBY_PLATFORM
 have_library("ncurses", "tgetnum") or
