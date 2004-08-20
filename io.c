34fff376ffbbf8e1fcff501869f043003f88edd5
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-08-20 08:30:43 +0000

    * io.c (rb_io_check_writable): no need to check read buffer if   already changed to write mode.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@6801 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- io.c
+++ io.c
@@ -254,7 +254,7 @@ rb_io_check_writable(fptr)
     if (!(fptr->mode & FMODE_WRITABLE)) {
 	rb_raise(rb_eIOError, "not opened for writing");
     }
-    if (READ_DATA_BUFFERED(fptr->f)) {
+    if ((fptr->mode & FMODE_RBUF) && READ_DATA_BUFFERED(fptr->f)) {
 	rb_warn("read buffer data lost");
     }
 #if NEED_IO_SEEK_BETWEEN_RW
