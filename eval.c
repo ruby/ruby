4441ea370bbdcc7fa5c90654f714d056f6ac8845
dave <dave@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-08-25 19:41:31 +0000

    Minor fix to error message
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@6820 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- eval.c
+++ eval.c
@@ -4553,7 +4553,7 @@ return_jump(retval)
 	    JUMP_TAG(TAG_RETURN);
 	}
 	if (tt->tag == PROT_THREAD) {
-	    rb_raise(rb_eThreadError, "return jump can't across threads");
+	    rb_raise(rb_eThreadError, "return can jump't across threads");
 	}
 	tt = tt->prev;
     }
