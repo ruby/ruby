ad7564a02a3288f807f1e6dd0ad9a42dc931bcdd
dave <dave@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-08-25 19:44:46 +0000

    Must learn how to transpose words in Emacs. Sorry...
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@6821 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- eval.c
+++ eval.c
@@ -4553,7 +4553,7 @@ return_jump(retval)
 	    JUMP_TAG(TAG_RETURN);
 	}
 	if (tt->tag == PROT_THREAD) {
-	    rb_raise(rb_eThreadError, "return can jump't across threads");
+	    rb_raise(rb_eThreadError, "return can't jump across threads");
 	}
 	tt = tt->prev;
     }
