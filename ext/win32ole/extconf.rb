9ec1f6d27eb7eced0c91cf121ca6af5682276dc4
suke <suke@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-10-21 15:10:06 +0000

    remove ext/win32ole/.document. add win32ole ext/.document. create .document dynamically by extconf.rb.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@7095 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/win32ole/extconf.rb
+++ ext/win32ole/extconf.rb
@@ -6,6 +6,17 @@
 require 'mkmf'
 
 dir_config("win32")
+
+SRCFILES=<<SRC
+win32ole.c
+SRC
+
+def create_docfile(src)
+  open(File.expand_path($srcdir) + "/.document", "w") {|ofs|
+    ofs.print src
+  }
+end
+
 def create_win32ole_makefile
   if have_library("ole32") and
      have_library("oleaut32") and
@@ -15,8 +26,12 @@ def create_win32ole_makefile
      have_library("advapi32") and
      have_header("windows.h")
     create_makefile("win32ole")
+    create_docfile(SRCFILES)
+  else
+    create_docfile("")
   end
 end
+
 
 case RUBY_PLATFORM
 when /mswin32/
