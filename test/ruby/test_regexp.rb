7dc3169658a6c89aebabd02a3244ce3e3d29ad45
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-10-29 23:22:57 +0000

    add a test for a problem of [ruby-talk:116455]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@7142 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_regexp.rb
+++ test/ruby/test_regexp.rb
@@ -1,7 +1,11 @@
 require 'test/unit'
 
 class TestRegexp < Test::Unit::TestCase
-  def test_undefined_bytecode_bug
+  def test_ruby_dev_24643
     assert_nothing_raised("[ruby-dev:24643]") { /(?:(?:[a]*[a])?b)*a*$/ =~ "aabaaca" }
+  end
+
+  def test_ruby_talk_116455
+    assert_match(/^(\w{2,}).* ([A-Za-z\xa2\xc0-\xff]{2,}?)$/, "Hallo Welt")
   end
 end
