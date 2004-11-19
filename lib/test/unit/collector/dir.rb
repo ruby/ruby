0fe17f5d812d5d1a065e7d472df9be3376a90920
eban <eban@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-11-19 13:45:46 +0000

    * lib/test/unit/collector/dir.rb: better support for -p/-x option.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@7321 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/test/unit/collector/dir.rb
+++ lib/test/unit/collector/dir.rb
@@ -55,6 +55,7 @@ module Test
                 sub_suite = recursive_collect(e_name, already_gathered)
                 sub_suites << sub_suite unless(sub_suite.empty?)
               else
+                next unless %r(/test_.*\.rb\Z) =~ e_name
                 (next unless(@pattern =~ e_name)) if(@pattern)
                 (next if(@exclude =~ e_name)) if(@exclude)
                 collect_file(e_name, sub_suites, already_gathered)
