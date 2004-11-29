9a851dc15725a8a5fdd91648814bf1bf6ce10fb6
(no author) <(no author)@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-11-29 06:22:42 +0000

    This commit was manufactured by cvs2svn to create branch 'ruby_1_8'.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@7415 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ test/io/nonblock/test_flush.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+require 'io/nonblock'
+
+class TestIONonblock < Test::Unit::TestCase
+  def test_flush                # [ruby-dev:24985]
+    r,w = IO.pipe
+    w.nonblock = true
+    w.sync = false
+    w << "b"
+    w.flush
+    w << "a" * 4096
+    Thread.new {
+      Thread.pass
+      w.close
+    }
+    result = ""
+    t = Thread.new {
+      while (Thread.pass; s = r.read(4096))
+        result << s
+      end
+    }
+    assert_raise(IOError) {w.flush}
+    t.join
+    assert_equal("b", result)
+  end
+end
