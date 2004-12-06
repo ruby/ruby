29cbbc5b38449e256107297b0c7d5da2b6935caf
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2004-12-06 23:02:17 +0000

    * test/socket/test_tcp.rb (TestTCPSocket#test_recvfrom): fix local   variable conflict.  [ruby-dev:25083]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@7484 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/socket/test_tcp.rb
+++ test/socket/test_tcp.rb
@@ -9,19 +9,18 @@ class TestTCPSocket < Test::Unit::TestCase
   def test_recvfrom # [ruby-dev:24705]
     svr = TCPServer.new("localhost", 0)
     Thread.new {
-      s = svr.accept
-      s.print("x"*0x1000)
+      svr.accept.print("x"*0x1000)
     }
     addr = svr.addr
-    s = TCPSocket.open(addr[2], addr[1])
+    sock = TCPSocket.open(addr[2], addr[1])
     Thread.new {
       Thread.pass
       ObjectSpace.each_object(String) {|s|
         s.replace "a" if s.length == 0x10000
       }
     }
-    assert_raise(RuntimeError) {
-      open("/tmp/n", "w"){|f| f.puts(data = s.recvfrom(0x10000))}
+    assert_raise(RuntimeError, SocketError) {
+      sock.recvfrom(0x10000)
     }
   end
 end if defined?(TCPSocket)
