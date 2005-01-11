b4114b4dc783fb53812334136cc4c3890715c6c3
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-01-11 15:36:52 +0000

    * object.c (rb_class_superclass): superclass of singleton class also   should be a singleton class.  fixed: [ruby-list:40519]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@7768 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- object.c
+++ object.c
@@ -1587,6 +1587,9 @@ rb_class_superclass(klass)
     if (!super) {
 	rb_raise(rb_eTypeError, "uninitialized class");
     }
+    if (FL_TEST(klass, FL_SINGLETON)) {
+	super = RBASIC(klass)->klass;
+    }
     while (TYPE(super) == T_ICLASS) {
 	super = RCLASS(super)->super;
     }
