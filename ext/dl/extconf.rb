bda37095ca990568069aaf07dc6b3fa8ceebc327
ttate <ttate@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-02-04 13:35:37 +0000

    added new files.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@7883 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ ext/dl/extconf.rb
@@ -0,0 +1,28 @@
+require 'mkmf'
+
+if( Config::CONFIG['CC'] =~ /gcc/ )
+  $CFLAGS << " -fno-defer-pop -fno-omit-frame-pointer"
+end
+
+$INSTALLFILES = [
+  ["dl.h", "$(archdir)$(target_prefix)", ""],
+]
+
+check = true
+if( have_header("dlfcn.h") )
+  have_library("dl")
+  check &&= have_func("dlopen")
+  check &&= have_func("dlclose")
+  check &&= have_func("dlsym")
+  have_func("dlerror")
+elsif( have_header("windows.h") )
+  check &&= have_func("LoadLibrary")
+  check &&= have_func("FreeLibrary")
+  check &&= have_func("GetProcAddress")
+else
+  check = false
+end
+
+if( check )
+  create_makefile("dl")
+end
