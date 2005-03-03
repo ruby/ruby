a48c63f36b48bdad1cfea978b58410fc70fada7a
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-03-03 09:38:40 +0000

    * {bcc32,win32,wince}/Makefile.sub (config.h): check if affected   when makefiles are modified.
    * {bcc32,win32,wince}/Makefile.sub (config.status): add variables
      for tests.
    
    * win32/ifchange.bat: try to update a file only if modified.
    
    * win32/resource.rb: more descriptions.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@8057 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- win32/resource.rb
+++ win32/resource.rb
@@ -46,11 +46,11 @@ icons.keys.sort.each do |i|
   dll_icons << "#{dll_icons.size + 1} ICON DISCARDABLE "+icons[i].dump+"\n"
 end
 
-[ # base name    extension         file type  icons
-  [$ruby_name,   CONFIG["EXEEXT"], 'VFT_APP', ruby_icon],
-  [$rubyw_name,  CONFIG["EXEEXT"], 'VFT_APP', rubyw_icon || ruby_icon],
-  [$so_name,     '.dll',           'VFT_DLL', dll_icons],
-].each do |base, ext, type, icons|
+[ # base name    extension         file type  desc, icons
+  [$ruby_name,   CONFIG["EXEEXT"], 'VFT_APP', 'CUI', ruby_icon],
+  [$rubyw_name,  CONFIG["EXEEXT"], 'VFT_APP', 'GUI', rubyw_icon || ruby_icon],
+  [$so_name,     '.dll',           'VFT_DLL', 'DLL', dll_icons],
+].each do |base, ext, type, desc, icons|
   open(base + '.rc', "w") { |f|
     f.binmode if /mingw/ =~ RUBY_PLATFORM
 
@@ -74,7 +74,7 @@ BEGIN
  BEGIN
   BLOCK "000004b0"
   BEGIN
-   VALUE "FileDescription",  "Ruby interpreter\\0"
+   VALUE "FileDescription",  "Ruby interpreter (#{desc}) #{RUBY_VERSION} [#{RUBY_PLATFORM}]\\0"
    VALUE "FileVersion",      "#{fversion}\\0"
    VALUE "Home Page",        "http://www.ruby-lang.org/\\0"
    VALUE "InternalName",     "#{base + ext}\\0"
