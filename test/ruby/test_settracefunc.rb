6697b0a7282b7439ade532e4a340c62c68d716cc
shugo <shugo@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-03-16 14:47:30 +0000

    * test/ruby/test_settracefunc.rb: added test for c-return.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@8166 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_settracefunc.rb
+++ test/ruby/test_settracefunc.rb
@@ -11,16 +11,18 @@ class TestSetTraceFunc < Test::Unit::TestCase
     a = 1
     foo
     a
+    b = 1 + 2
     set_trace_func nil
 
-    assert_equal(["line", 11], events.shift)    # line "a = 1"
-    assert_equal(["line", 12], events.shift)    # line "foo"
-    assert_equal(["call", 4], events.shift)     # call foo
-    event, lineno = events.shift                # return
-      assert_equal("return", event)
-      assert_equal(4, lineno) # [history] it could not be expected in 1.8
-    assert_equal(["line", 13], events.shift)    # line "a"
-    assert_equal(["line", 14], events.shift)    # line "set_trace_func nil"
-    assert_equal(["c-call", 14], events.shift)  # c-call set_trace_func
+    assert_equal(["line", 11], events.shift)     # line "a = 1"
+    assert_equal(["line", 12], events.shift)     # line "foo"
+    assert_equal(["call", 4], events.shift)      # call foo
+    assert_equal(["return", 4], events.shift)    # return foo
+    assert_equal(["line", 13], events.shift)     # line "a"
+    assert_equal(["line", 14], events.shift)     # line "b = 1 + 2"
+    assert_equal(["c-call", 14], events.shift)   # c-call Fixnum#+
+    assert_equal(["c-return", 14], events.shift) # c-return Fixnum#+
+    assert_equal(["line", 15], events.shift)     # line "set_trace_func nil"
+    assert_equal(["c-call", 15], events.shift)   # c-call set_trace_func
   end
 end
