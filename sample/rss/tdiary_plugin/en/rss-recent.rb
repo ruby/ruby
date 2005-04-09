98d168ffc7331938a2aaaf761768de28f012dc24
kou <kou@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-04-09 05:47:33 +0000

    * sample/rss/tdiary_plugin/rss-recent.rb: supported configuration   via Web browser.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@8288 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ sample/rss/tdiary_plugin/en/rss-recent.rb
@@ -0,0 +1,22 @@
+# -*- indent-tabs-mode: t -*-
+
+def label_rss_recent_title
+	'External RSS display'
+end
+
+def label_rss_recent_use_image_link_title
+	'Image treatment'
+end
+
+def label_rss_recent_use_image_link_description
+	"Select whether you use image as link instead of text or not " +
+		"if the RSS has Web site's image information."
+end
+
+def label_rss_recent_not_use_image_link
+	"don't use image as link"
+end
+
+def label_rss_recent_use_image_link
+	"use image as link"
+end
---
+++ sample/rss/tdiary_plugin/ja/rss-recent.rb
@@ -0,0 +1,22 @@
+# -*- indent-tabs-mode: t -*-
+
+def label_rss_recent_title
+	'外部RSSの表示'
+end
+
+def label_rss_recent_use_image_link_title
+	'画像の扱い'
+end
+
+def label_rss_recent_use_image_link_description
+	'もしRSSにWebサイトの画像情報が含まれていたら，' +
+		'Webサイトへのリンクにテキストではなくその画像を用います．'
+end
+
+def label_rss_recent_not_use_image_link
+	'リンクに画像を用いない'
+end
+
+def label_rss_recent_use_image_link
+	'リンクに画像を用いる'
+end
