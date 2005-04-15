385d24afa5cf8c417110235aa1d8f46326a89249
gotoyuzo <gotoyuzo@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-04-15 19:16:18 +0000

    * ext/openssl/extconf.rb: check for OPENSSL_cleanse.
    * ext/openssl/openssl_missing.h: ditto.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@8335 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/openssl/extconf.rb
+++ ext/openssl/extconf.rb
@@ -89,6 +89,7 @@ have_func("X509_CRL_set_version")
 have_func("X509_CRL_sort")
 have_func("X509_STORE_get_ex_data")
 have_func("X509_STORE_set_ex_data")
+have_func("OPENSSL_cleanse")
 if try_compile("#define FOO(a, ...) foo(a, ##__VA_ARGS__)\n int x(){FOO(1);FOO(1,2);FOO(1,2,3);}\n")
   $defs.push("-DHAVE_VA_ARGS_MACRO")
 end
