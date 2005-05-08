4c72dfd39f9f3a854863a7c79ac2f63690156c56
ocean <ocean@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-05-08 07:54:20 +0000

    * lib/profiler.rb: fixed "undefined method `[]' for nil:NilClass"   [ruby-core:4775] [ruby-talk:140401] [ruby-dev:26118]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@8408 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/profiler.rb
+++ lib/profiler.rb
@@ -1,40 +1,29 @@
 module Profiler__
-  Times = if defined? Process.times then Process else Time end
   # internal values
   @@start = @@stack = @@map = nil
   PROFILE_PROC = proc{|event, file, line, id, binding, klass|
     case event
     when "call", "c-call"
-      now = Float(Times::times[0])
-      @@stack.push [now, 0.0, id]
+      now = Process.times[0]
+      @@stack.push [now, 0.0]
     when "return", "c-return"
-      now = Float(Times::times[0])
-      tick = @@stack.pop
-      name = klass.to_s
-      if name.nil? then name = '' end
-      if klass.kind_of? Class
-	name += "#"
-      else
-	name += "."
+      now = Process.times[0]
+      key = [klass, id]
+      if tick = @@stack.pop
+        data = (@@map[key] ||= [0, 0.0, 0.0, key])
+        data[0] += 1
+        cost = now - tick[0]
+        data[1] += cost
+        data[2] += cost - tick[1]
+        @@stack[-1][1] += cost if @@stack[-1]
       end
-      name += id.id2name
-      data = @@map[name]
-      unless data
-	data = [0.0, 0.0, 0.0, name]
-	@@map[name] = data
-      end
-      data[0] += 1
-      cost = now - tick[0]
-      data[1] += cost
-      data[2] += cost - tick[1]
-      @@stack[-1][1] += cost
     end
   }
 module_function
   def start_profile
-    @@start = Float(Times::times[0])
-    @@stack = [[0, 0, :toplevel], [0, 0, :dummy]]
-    @@map = {"#toplevel" => [1, 0, 0, "#toplevel"]}
+    @@start = Process.times[0]
+    @@stack = []
+    @@map = {}
     set_trace_func PROFILE_PROC
   end
   def stop_profile
@@ -42,18 +31,29 @@ module_function
   end
   def print_profile(f)
     stop_profile
-    total = Float(Times::times[0]) - @@start
+    total = Process.times[0] - @@start
     if total == 0 then total = 0.01 end
-    @@map["#toplevel"][1] = total
     data = @@map.values
     data.sort!{|a,b| b[2] <=> a[2]}
     sum = 0
-    f.printf "  %%   cumulative   self              self     total\n"           
+    f.printf "  %%   cumulative   self              self     total\n"
     f.printf " time   seconds   seconds    calls  ms/call  ms/call  name\n"
     for d in data
       sum += d[2]
       f.printf "%6.2f %8.2f  %8.2f %8d ", d[2]/total*100, sum, d[2], d[0]
-      f.printf "%8.2f %8.2f  %s\n", d[2]*1000/d[0], d[1]*1000/d[0], d[3]
+      f.printf "%8.2f %8.2f  %s\n", d[2]*1000/d[0], d[1]*1000/d[0], get_name(*d[3])
     end
+    f.printf "%6.2f %8.2f  %8.2f %8d ", 0.0, total, 0.0, 1     # ???
+    f.printf "%8.2f %8.2f  %s\n", 0.0, total*1000, "#toplevel" # ???
   end
+  def get_name(klass, id)
+    name = klass.to_s || ""
+    if klass.kind_of? Class
+      name += "#"
+    else
+      name += "."
+    end
+    name + id.id2name
+  end
+  private :get_name
 end
