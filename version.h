b38dcb30b4284a2ce62746bf51f7a3429a7d41b0
eban <eban@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-05-23 15:41:59 +0000

    * 2005-05-24
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@8510 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,14 +1,26 @@
 #define RUBY_VERSION "1.9.0"
-#define RUBY_RELEASE_DATE "2005-05-23"
+<<<<<<< version.h
+#define RUBY_RELEASE_DATE "2005-05-24"
+=======
+#define RUBY_RELEASE_DATE "2005-05-24"
+>>>>>>> 1.1159
 #define RUBY_VERSION_CODE 190
-#define RUBY_RELEASE_CODE 20050523
+<<<<<<< version.h
+#define RUBY_RELEASE_CODE 20050524
+=======
+#define RUBY_RELEASE_CODE 20050524
+>>>>>>> 1.1159
 
 #define RUBY_VERSION_MAJOR 1
 #define RUBY_VERSION_MINOR 9
 #define RUBY_VERSION_TEENY 0
 #define RUBY_RELEASE_YEAR 2005
 #define RUBY_RELEASE_MONTH 5
-#define RUBY_RELEASE_DAY 23
+<<<<<<< version.h
+#define RUBY_RELEASE_DAY 24
+=======
+#define RUBY_RELEASE_DAY 24
+>>>>>>> 1.1159
 
 RUBY_EXTERN const char ruby_version[];
 RUBY_EXTERN const char ruby_release_date[];
