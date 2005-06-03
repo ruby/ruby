5794d80d53b3cf6e64a78c92ac3109dea8ad2c66
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-06-03 14:39:15 +0000

    add a test for [ruby-dev:26128].
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@8553 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_signal.rb
+++ test/ruby/test_signal.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require 'timeout'
 
 class TestSignal < Test::Unit::TestCase
   def test_signal
@@ -20,6 +21,28 @@ class TestSignal < Test::Unit::TestCase
       assert_match(/Interrupt/, x.message)
     ensure
       trap "SIGINT", oldtrap
+    end
+  end
+
+  def test_exit_action
+    begin
+      r, w = IO.pipe
+      pid = fork {
+        r0, w0 = IO.pipe
+        trap(:USR1, "EXIT")
+        Thread.start { Thread.pass }
+        r0.sysread(4096)
+      }
+      sleep 0.1
+      assert_nothing_raised("[ruby-dev:26128]") {
+        Process.kill(:USR1, pid)
+        Timeout.timeout(1) {
+          Process.waitpid pid
+        }
+      }
+    ensure
+      r.close
+      w.close
     end
   end
 end
