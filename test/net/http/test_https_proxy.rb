114f6ba3e6964111e1e0413f6f4b022e51953d3c
(no author) <(no author)@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-06-03 16:11:46 +0000

    This commit was manufactured by cvs2svn to create branch 'ruby_1_8'.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@8555 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ test/net/http/test_https_proxy.rb
@@ -0,0 +1,26 @@
+require 'net/https'
+require 'test/unit'
+
+class HTTPSProxyTest < Test::Unit::TestCase
+  def test_https_proxy_authentication
+    TCPServer.open(0) {|serv|
+      _, port, _, ipaddr = serv.addr
+      t = Thread.new {
+        proxy = Net::HTTP.Proxy(ipaddr, port, 'user', 'password')
+        http = proxy.new("foo.example.org", 8000)
+        http.use_ssl = true
+        http.start {|http|  }
+        p 1
+      }
+      sock = serv.accept
+      proxy_request = sock.gets("\r\n\r\n")
+      assert_equal(
+        "CONNECT foo.example.org:8000 HTTP/1.1\r\n" +
+        "Host: foo.example.org:8000\r\n" +
+        "Proxy-Authorization: Basic dXNlcjpwYXNzd29yZA==\r\n" +
+        "\r\n",
+        proxy_request)
+    }
+  end
+end
+ 
