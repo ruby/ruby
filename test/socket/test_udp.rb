6cdf50d8be3d80f8da875d3cedc256a066ea6553
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-09-13 12:39:45 +0000

    * test/socket/test_socket.rb (test_listen): missed to modify.
    * test/socket/test_udp.rb (test_bind): ditto.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@9149 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/socket/test_udp.rb
+++ test/socket/test_udp.rb
@@ -21,7 +21,7 @@ class TestUDPSocket < Test::Unit::TestCase
   def test_bind # [ruby-dev:25057]
     s = UDPSocket.new
     host = Object.new
-    class << host; self end.send(:define_method, :to_str) {
+    class << host; self end.fcall(:define_method, :to_str) {
       s.close
       "127.0.0.1"
     }
