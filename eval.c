7c44a27cb256345cf3eb9c242909fd3cb13d03aa
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-09-13 12:47:43 +0000

    * eval.c (BEGIN_CALLARGS): pop halfly pushed status.   fixed: [ruby-dev:26881]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@9150 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- eval.c
+++ eval.c
@@ -2148,8 +2148,10 @@ copy_node_scope(NODE *node, NODE *rval)
 #define BEGIN_CALLARGS do {\
     struct BLOCK *tmp_block = ruby_block;\
     int tmp_iter = ruby_iter->iter;\
-    if (tmp_iter == ITER_PRE) {\
+    switch (tmp_iter) {\
+      case ITER_PRE:\
 	ruby_block = ruby_block->outer;\
+      case ITER_PAS:\
 	tmp_iter = ITER_NOT;\
     }\
     PUSH_ITER(tmp_iter)
