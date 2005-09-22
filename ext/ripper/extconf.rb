6fb86f740b994108254982b954f8ad9df64bf5b4
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-09-22 14:40:37 +0000

    * ext/ripper/extconf.rb (have_command): replaced with find_executable.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@9272 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/ripper/extconf.rb
+++ ext/ripper/extconf.rb
@@ -4,7 +4,7 @@ require 'mkmf'
 require 'rbconfig'
 
 def main
-  unless have_command('bison')
+  unless find_executable('bison')
     unless File.exist?('ripper.c') or File.exist?("#{$srcdir}/ripper.c")
       $stderr.puts 'missing bison; abort'
       exit 1
@@ -15,14 +15,6 @@ def main
   $CPPFLAGS += ' -DRIPPER'
   $CPPFLAGS += ' -DRIPPER_DEBUG' if $debug
   create_makefile 'ripper'
-end
-
-def have_command(cmd)
-  checking_for(cmd) {
-    ENV['PATH'].split(File::PATH_SEPARATOR).any? {|path|
-      File.executable?("#{path}/#{cmd}#{Config::CONFIG['EXEEXT']}")
-    }
-  }
 end
 
 main
