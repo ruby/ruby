cd30677dab443c79750b46c29c69e5b865f9240b
nagai <nagai@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-11-18 22:37:53 +0000

    * ext/tk/lib/tk/font.rb: remove dependency on Ruby's version (1.8 or 1.9). * ext/tk/lib/tkextlib/ICONS/icons.rb: ditto. * ext/tk/sample/tkextlib/treectrl/demo.rb: ditto. * ext/tk/lib/tk.rb: update RELEASE_DATE
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@9564 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/tk/lib/tk.rb
+++ ext/tk/lib/tk.rb
@@ -4499,7 +4499,7 @@ end
 #Tk.freeze
 
 module Tk
-  RELEASE_DATE = '2005-11-18'.freeze
+  RELEASE_DATE = '2005-11-19'.freeze
 
   autoload :AUTO_PATH,        'tk/variable'
   autoload :TCL_PACKAGE_PATH, 'tk/variable'
