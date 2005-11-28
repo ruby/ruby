1059c9e00c0474d8ca38b58ab1317110e20aa1ce
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-11-28 04:26:46 +0000

    * ext/socket/socket.c (init_inetsock_internal): remove setting   SO_REUSEADDR option on server socket. fixed: [ruby-core:6765]   ([ ruby-Bugs-2872 ])
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@9626 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/socket/socket.c
+++ ext/socket/socket.c
@@ -1097,11 +1097,6 @@ init_inetsock_internal(arg)
 	}
 	arg->fd = fd;
 	if (type == INET_SERVER) {
-#ifndef _WIN32
-	    status = 1;
-	    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR,
-		       (char*)&status, sizeof(status));
-#endif
 	    status = bind(fd, res->ai_addr, res->ai_addrlen);
 	    syscall = "bind(2)";
 	}
