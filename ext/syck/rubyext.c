3b7777959a81b8f16c991518e0daf770f882bf35
why <why@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-12-20 03:56:05 +0000

    * ext/syck/rubyext.c (syck_emitter_reset): to ensure compatibility   with previous Ruby versions, documents are no longer headless.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@9721 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/syck/rubyext.c
+++ ext/syck/rubyext.c
@@ -2023,7 +2023,7 @@ syck_emitter_reset( argc, argv, self )
         rb_ivar_set(self, s_options, options);
     }
     
-    emitter->headless = 1;
+    emitter->headless = 0;
     emitter->bonus = (void *)bonus;
     rb_ivar_set(self, s_level, INT2FIX(0));
     rb_ivar_set(self, s_resolver, Qnil);
