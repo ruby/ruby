35d8b850a3eeb51a14feec0ce0e265770ad35458
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2005-12-21 09:20:15 +0000

    (rb_f_test): fix document.  [ruby-dev:28087]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@9731 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- file.c
+++ file.c
@@ -1398,6 +1398,16 @@ test_sticky(obj, fname)
  *   File.identical?(file_1, file_2)   =>  true or false
  *
  * Returns <code>true</code> if the named files are identical.
+ *
+ *     open("a", "w") {}
+ *     p File.identical?("a", "a")      #=> true
+ *     p File.identical?("a", "./a")    #=> true
+ *     File.link("a", "b")
+ *     p File.identical?("a", "b")      #=> true
+ *     File.symlink("a", "c")
+ *     p File.identical?("a", "c")      #=> true
+ *     open("d", "w") {}
+ *     p File.identical?("a", "d")      #=> false
  */
 
 static VALUE
@@ -3265,7 +3275,7 @@ test_check(n, argc, argv)
  *
  * Tests that take two files:
  *
- *     ?-  | boolean | True if file1 is a hard link to file2
+ *     ?-  | boolean | True if file1 and file2 are identical
  *     ?=  | boolean | True if the modification times of file1
  *         |         | and file2 are equal
  *     ?<  | boolean | True if the modification time of file1
