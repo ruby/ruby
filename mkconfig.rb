c270c82ebb443e72746ad97b766cc70f8d473cf5
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-02-21 10:24:18 +0000

    comment ruby version more specific.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@9979 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- mkconfig.rb
+++ mkconfig.rb
@@ -137,7 +137,7 @@ print <<EOS
     RbConfig::expand(val)
   end
 end
-Config = RbConfig # compatibility for ruby-1.8 and older.
+Config = RbConfig # compatibility for ruby-1.8.4 and older.
 CROSS_COMPILING = nil unless defined? CROSS_COMPILING
 EOS
 
