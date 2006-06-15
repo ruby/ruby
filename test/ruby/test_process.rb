72e8f4fd3f58efecccd732bf6f60490176c99732
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-06-15 08:29:15 +0000

    add RLIMIT_NOFILE test
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@10276 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_process.rb
+++ test/ruby/test_process.rb
@@ -1,13 +1,32 @@
 require 'test/unit'
 
 class TestProcess < Test::Unit::TestCase
-  def test_rlimit
+  def test_rlimit_availability
     begin
       Process.getrlimit(nil)
     rescue NotImplementedError
       assert_raise(NotImplementedError) { Process.setrlimit }
     rescue TypeError
       assert_raise(ArgumentError) { Process.setrlimit }
+    end
+  end
+
+  def rlimit_exist?
+    Process.getrlimit(nil)
+  rescue NotImplementedError
+    return false
+  rescue TypeError
+    return true
+  end
+
+  def test_rlimit_nofile
+    return unless rlimit_exist?
+    cur_nofile, max_nofile = Process.getrlimit(Process::RLIMIT_NOFILE)
+    Process.setrlimit(Process::RLIMIT_NOFILE, 0, max_nofile)
+    begin
+      assert_raise(Errno::EMFILE) { IO.pipe }
+    ensure
+      Process.setrlimit(Process::RLIMIT_NOFILE, cur_nofile, max_nofile)
     end
   end
 end
