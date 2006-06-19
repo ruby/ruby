cd15cdca77ec21a740a8dfff19062d65fd87a7f0
suke <suke@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-06-19 11:00:12 +0000

    should not test when win32ole is not available.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@10325 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/win32ole/test_propertyputref.rb
+++ test/win32ole/test_propertyputref.rb
@@ -1,19 +1,24 @@
 require 'test/unit'
-require 'win32ole'
+begin
+  require 'win32ole'
+rescue LoadError
+end
 
-class TestWIN32OLE_PROPERTYPUTREF < Test::Unit::TestCase
-  def setup
-    begin
-      @sapi = WIN32OLE.new('SAPI.SpVoice')
-    rescue WIN32OLERuntimeErro
-      @sapi = nil
+if defined?(WIN32OLE)
+  class TestWIN32OLE_PROPERTYPUTREF < Test::Unit::TestCase
+    def setup
+      begin
+        @sapi = WIN32OLE.new('SAPI.SpVoice')
+      rescue WIN32OLERuntimeErro
+        @sapi = nil
+      end
     end
-  end
-  def test_sapi
-    if @sapi
-      new_id = @sapi.getvoices.item(2).Id
-      @sapi.voice = @sapi.getvoices.item(2)
-      assert_equal(new_id, @sapi.voice.Id)
+    def test_sapi
+      if @sapi
+        new_id = @sapi.getvoices.item(2).Id
+        @sapi.voice = @sapi.getvoices.item(2)
+        assert_equal(new_id, @sapi.voice.Id)
+      end
     end
   end
 end
