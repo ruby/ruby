ad5f0fc6ccba716676a40c0a635fd41a8a782d4a
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-06-21 07:08:34 +0000

    use fork to isolate rlimit effect.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@10355 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_process.rb
+++ test/ruby/test_process.rb
@@ -21,12 +21,17 @@ class TestProcess < Test::Unit::TestCase
 
   def test_rlimit_nofile
     return unless rlimit_exist?
-    cur_nofile, max_nofile = Process.getrlimit(Process::RLIMIT_NOFILE)
-    Process.setrlimit(Process::RLIMIT_NOFILE, 0, max_nofile)
-    begin
-      assert_raise(Errno::EMFILE) { IO.pipe }
-    ensure
-      Process.setrlimit(Process::RLIMIT_NOFILE, cur_nofile, max_nofile)
-    end
+    pid = fork {
+      cur_nofile, max_nofile = Process.getrlimit(Process::RLIMIT_NOFILE)
+      Process.setrlimit(Process::RLIMIT_NOFILE, 0, max_nofile)
+      begin
+        IO.pipe
+      rescue Errno::EMFILE
+        exit 0
+      end
+      exit 1
+    }
+    Process.wait pid
+    assert_equal(0, $?.to_i)
   end
 end
