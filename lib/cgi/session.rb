d28d1a2e0a0162a3930f17e0181b0a0197291701
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-07-30 14:06:50 +0000

    * lib/cgi/session.rb, lib/cgi/session/pstore.rb: suppress warnings.   fixed: [ruby-talk:204896]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@10643 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/cgi/session.rb
+++ lib/cgi/session.rb
@@ -301,20 +301,14 @@ class CGI
 
     # Retrieve the session data for key +key+.
     def [](key)
-      unless @data
-	@data = @dbman.restore
-      end
+      @data ||= @dbman.restore
       @data[key]
     end
 
     # Set the session date for key +key+.
     def []=(key, val)
-      unless @write_lock
-	@write_lock = true
-      end
-      unless @data
-	@data = @dbman.restore
-      end
+      @write_lock ||= true
+      @data ||= @dbman.restore
       @data[key] = val
     end
 
@@ -380,7 +374,9 @@ class CGI
         require 'digest/md5'
         md5 = Digest::MD5.hexdigest(id)[0,16]
 	@path = dir+"/"+prefix+md5+suffix
-	unless File::exist? @path
+	if File::exist? @path
+	  @hash = nil
+	else
           unless session.new_session
             raise CGI::Session::NoSession, "uninitialized session"
           end
