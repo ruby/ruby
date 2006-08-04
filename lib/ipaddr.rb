e2c017a8ceb84a95da28fe54b51afb6e58a0d8f9
drbrain <drbrain@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-08-04 20:39:28 +0000

    Move Example so RDoc picks it up.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@10674 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/ipaddr.rb
+++ lib/ipaddr.rb
@@ -7,29 +7,9 @@
 # You can redistribute and/or modify it under the same terms as Ruby.
 #
 # $Id$
-#--
+#
 # TODO:
 #   - scope_id support
-#++
-#
-#== Example
-#
-#   require 'ipaddr'
-#   
-#   ipaddr1 = IPAddr.new "3ffe:505:2::1"
-#   
-#   p ipaddr1			#=> #<IPAddr: IPv6:3ffe:0505:0002:0000:0000:0000:0000:0001/ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff>
-#   
-#   p ipaddr1.to_s		#=> "3ffe:505:2::1"
-#   
-#   ipaddr2 = ipaddr1.mask(48)	#=> #<IPAddr: IPv6:3ffe:0505:0002:0000:0000:0000:0000:0000/ffff:ffff:ffff:0000:0000:0000:0000:0000>
-#   
-#   p ipaddr2.to_s		#=> "3ffe:505:2::"
-#   
-#   ipaddr3 = IPAddr.new "192.168.2.0/24"
-#   
-#   p ipaddr3			#=> #<IPAddr: IPv4:192.168.2.0/255.255.255.0>
-
 require 'socket'
 
 unless Socket.const_defined? "AF_INET6"
@@ -75,8 +55,27 @@ unless Socket.const_defined? "AF_INET6"
   end
 end
 
-# IPAddr provides a set of methods to manipulate an IP address.  Both
-# IPv4 and IPv6 are supported.
+# IPAddr provides a set of methods to manipulate an IP address.  Both IPv4 and
+# IPv6 are supported.
+#
+# == Example
+#
+#   require 'ipaddr'
+#   
+#   ipaddr1 = IPAddr.new "3ffe:505:2::1"
+#   
+#   p ipaddr1			#=> #<IPAddr: IPv6:3ffe:0505:0002:0000:0000:0000:0000:0001/ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff>
+#   
+#   p ipaddr1.to_s		#=> "3ffe:505:2::1"
+#   
+#   ipaddr2 = ipaddr1.mask(48)	#=> #<IPAddr: IPv6:3ffe:0505:0002:0000:0000:0000:0000:0000/ffff:ffff:ffff:0000:0000:0000:0000:0000>
+#   
+#   p ipaddr2.to_s		#=> "3ffe:505:2::"
+#   
+#   ipaddr3 = IPAddr.new "192.168.2.0/24"
+#   
+#   p ipaddr3			#=> #<IPAddr: IPv4:192.168.2.0/255.255.255.0>
+
 class IPAddr
 
   IN4MASK = 0xffffffff
