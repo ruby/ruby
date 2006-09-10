f16b5afcac605643011c74948a993e6fb81d65d2
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-09-10 13:59:59 +0000

    * instruby.rb (parse_args): remove splat.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@10907 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- instruby.rb
+++ instruby.rb
@@ -25,7 +25,7 @@ def parse_args()
   opt.on('--dest-dir=DIR') {|dir| $destdir = dir}
   opt.on('--make=COMMAND') {|make| $make = make}
   opt.on('--mantype=MAN') {|man| $mantype = man}
-  opt.on('--make-flags=FLAGS', '--mflags', Shellwords) do |*v|
+  opt.on('--make-flags=FLAGS', '--mflags', Shellwords) do |v|
     if arg = v.first
       arg.insert(0, '-') if /\A[^-][^=]*\Z/ =~ arg
     end
