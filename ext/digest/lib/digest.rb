3d9fc0d7d07b821c932f05bb52eb7557670576bf
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-09-13 16:03:23 +0000

    * ext/digest/lib/digest.rb: new file.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@10924 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ ext/digest/lib/digest.rb
@@ -0,0 +1,8 @@
+module Digest
+  autoload "MD5", "digest/md5"
+  autoload "RMD160", "digest/rmd160"
+  autoload "SHA1", "digest/sha1"
+  autoload "SHA256", "digest/sha2"
+  autoload "SHA384", "digest/sha2"
+  autoload "SHA512", "digest/sha2"
+end
