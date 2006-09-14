d6ff7b526a6b102940d06c0e70a21990203606cc
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-09-14 05:39:40 +0000

    * ext/digest/lib/digest.rb (Digest::Base.file): new method.   [ruby-dev:29572]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@10928 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/digest/lib/digest.rb
+++ ext/digest/lib/digest.rb
@@ -5,4 +5,17 @@ module Digest
   autoload "SHA256", "digest/sha2"
   autoload "SHA384", "digest/sha2"
   autoload "SHA512", "digest/sha2"
+
+  class Base
+    def self.file(name)
+      digest = self.new
+      File.open(name) {|f|
+        buf = ""
+        while f.read(16384, buf)
+          digest.update buf
+        end
+      }
+      digest
+    end
+  end
 end
