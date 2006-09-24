fc9099e7e317dd7daebaef4a539bdd8dac0836b8
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-09-24 14:29:57 +0000

    * gc.c (gc_mark_children): NODE_POSTEXE holds Ruby VALUE.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@11008 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- gc.c
+++ gc.c
@@ -835,6 +835,7 @@ gc_mark_children(ptr, lev)
 	  case NODE_OPT_N:
 	  case NODE_EVSTR:
 	  case NODE_UNDEF:
+	  case NODE_POSTEXE:
 	    ptr = (VALUE)obj->as.node.u2.node;
 	    goto again;
 
@@ -881,7 +882,6 @@ gc_mark_children(ptr, lev)
 	  case NODE_FALSE:
 	  case NODE_ATTRSET:
 	  case NODE_BLOCK_ARG:
-	  case NODE_POSTEXE:
 	    break;
 	  case NODE_ALLOCA:
 	    mark_locations_array((VALUE*)obj->as.node.u1.value,
