96b1bca0c15cc90a6a5942544d803ecf95507abb
(no author) <(no author)@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-10-04 09:52:55 +0000

    This commit was manufactured by cvs2svn to create branch 'ruby_1_8'.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@11082 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ ext/digest/lib/digest.rb
@@ -0,0 +1,27 @@
+require 'digest.so'
+
+module Digest
+  autoload "MD5", "digest/md5"
+  autoload "RMD160", "digest/rmd160"
+  autoload "SHA1", "digest/sha1"
+  autoload "SHA256", "digest/sha2"
+  autoload "SHA384", "digest/sha2"
+  autoload "SHA512", "digest/sha2"
+
+  class Base
+    # creates a digest object and read given file, _name_.
+    # 
+    #  p Digest::SHA256.file("X11R6.8.2-src.tar.bz2").hexdigest
+    #  # => "f02e3c85572dc9ad7cb77c2a638e3be24cc1b5bea9fdbb0b0299c9668475c534"
+    def self.file(name)
+      digest = self.new
+      File.open(name, "rb") {|f|
+        buf = ""
+        while f.read(16384, buf)
+          digest.update buf
+        end
+      }
+      digest
+    end
+  end
+end
