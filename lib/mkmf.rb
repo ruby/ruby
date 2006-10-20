03f19e27ed9dc0b0110dce0108626d414e00fca2
eban <eban@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-10-20 02:33:49 +0000

    * lib/mkmf.rb: fixed the bug of handling COMMON_MACROS.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@11194 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -1511,8 +1511,7 @@ EXPORT_PREFIX = config_string('EXPORT_PREFIX') {|s| s.strip}
 hdr = []
 config_string('COMMON_MACROS') do |s|
   Shellwords.shellwords(s).each do |w|
-    /(.*?)(?:=(.*))/ =~ w
-    hdr << "#define #$1 #$2"
+    hdr << "#define " + w.split(/=/, 2).join(" ")
   end
 end
 config_string('COMMON_HEADERS') do |s|
