7e69efba5e69527ea028dc8c7b702d13d411ebc1
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-10-30 09:37:52 +0000

    * bignum.c (rb_big2str0): wrong allocation length.   [ruby-dev:29710]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@11242 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bignum.c
+++ bignum.c
@@ -607,7 +607,7 @@ rb_big2str0(VALUE x, int base, int trim)
 	j /= 3;
 	break;
       case 10: case 11: case 12: case 13: case 14: case 15:
-	j = j * 241L / 800;
+	j = j * 241L / 800 + 1;
 	break;
       case 16: case 17: case 18: case 19: case 20: case 21:
       case 22: case 23: case 24: case 25: case 26: case 27:
