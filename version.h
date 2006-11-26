9744750be01abbb719c9fea69c4414feeb9ee51a
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-11-26 09:34:32 +0000

    Sun Nov 26 16:36:46 2006  URABE Shyouhei  <shyouhei@ruby-lang.org>
            * version.h: addition of RUBY_PATCHLEVEL.
            * version.c: ditto.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@11307 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,7 +1,8 @@
 #define RUBY_VERSION "1.8.5"
-#define RUBY_RELEASE_DATE "2006-11-24"
+#define RUBY_RELEASE_DATE "2006-11-26"
 #define RUBY_VERSION_CODE 185
 #define RUBY_RELEASE_CODE 20061124
+#define RUBY_PATCHLEVEL 0
 
 #define RUBY_VERSION_MAJOR 1
 #define RUBY_VERSION_MINOR 8
@@ -13,3 +14,4 @@
 RUBY_EXTERN const char ruby_version[];
 RUBY_EXTERN const char ruby_release_date[];
 RUBY_EXTERN const char ruby_platform[];
+RUBY_EXTERN const int ruby_patchlevel;
