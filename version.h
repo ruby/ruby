253c38a903fb2c7fde30af7fb94ea14adfefe53d
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-12-05 19:27:09 +0000

    2006-12-06
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_5@11348 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #define RUBY_VERSION "1.8.5"
-#define RUBY_RELEASE_DATE "2006-12-04"
+#define RUBY_RELEASE_DATE "2006-12-06"
 #define RUBY_VERSION_CODE 185
-#define RUBY_RELEASE_CODE 20061204
+#define RUBY_RELEASE_CODE 20061206
 #define RUBY_PATCHLEVEL 4
 
 #define RUBY_VERSION_MAJOR 1
@@ -9,7 +9,7 @@
 #define RUBY_VERSION_TEENY 5
 #define RUBY_RELEASE_YEAR 2006
 #define RUBY_RELEASE_MONTH 12
-#define RUBY_RELEASE_DAY 4
+#define RUBY_RELEASE_DAY 6
 
 RUBY_EXTERN const char ruby_version[];
 RUBY_EXTERN const char ruby_release_date[];
