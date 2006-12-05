ca87cbea59698332cf212aa185ac56a55db0557e
(no author) <(no author)@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-12-05 18:53:00 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_8_5_3'.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/tags/v1_8_5_3@11346 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,15 +1,17 @@
 #define RUBY_VERSION "1.8.5"
-#define RUBY_RELEASE_DATE "2006-08-23"
+#define RUBY_RELEASE_DATE "2006-12-04"
 #define RUBY_VERSION_CODE 185
-#define RUBY_RELEASE_CODE 20060823
+#define RUBY_RELEASE_CODE 20061204
+#define RUBY_PATCHLEVEL 3
 
 #define RUBY_VERSION_MAJOR 1
 #define RUBY_VERSION_MINOR 8
 #define RUBY_VERSION_TEENY 5
 #define RUBY_RELEASE_YEAR 2006
-#define RUBY_RELEASE_MONTH 8
-#define RUBY_RELEASE_DAY 23
+#define RUBY_RELEASE_MONTH 12
+#define RUBY_RELEASE_DAY 4
 
 RUBY_EXTERN const char ruby_version[];
 RUBY_EXTERN const char ruby_release_date[];
 RUBY_EXTERN const char ruby_platform[];
+RUBY_EXTERN const int ruby_patchlevel;
