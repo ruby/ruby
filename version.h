a17e7d92a2d7791094d76d57dd43f1305f321eca
eban <eban@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2006-12-26 06:37:14 +0000

    * 2006-12-26
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@11422 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #define RUBY_VERSION "1.9.0"
-#define RUBY_RELEASE_DATE "2006-12-22"
+#define RUBY_RELEASE_DATE "2006-12-26"
 #define RUBY_VERSION_CODE 190
-#define RUBY_RELEASE_CODE 20061222
+#define RUBY_RELEASE_CODE 20061226
 #define RUBY_PATCHLEVEL 0
 
 #define RUBY_VERSION_MAJOR 1
@@ -9,7 +9,7 @@
 #define RUBY_VERSION_TEENY 0
 #define RUBY_RELEASE_YEAR 2006
 #define RUBY_RELEASE_MONTH 12
-#define RUBY_RELEASE_DAY 22
+#define RUBY_RELEASE_DAY 26
 
 RUBY_EXTERN const char ruby_version[];
 RUBY_EXTERN const char ruby_release_date[];
