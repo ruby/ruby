678fbc306aab8f643b616cdee4f2821f28b48b5c
eban <eban@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-01-07 10:38:57 +0000

    * 2007-01-07
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@11511 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #define RUBY_VERSION "1.8.5"
-#define RUBY_RELEASE_DATE "2007-01-03"
+#define RUBY_RELEASE_DATE "2007-01-07"
 #define RUBY_VERSION_CODE 185
-#define RUBY_RELEASE_CODE 20070103
+#define RUBY_RELEASE_CODE 20070107
 #define RUBY_PATCHLEVEL 5000
 
 #define RUBY_VERSION_MAJOR 1
@@ -9,7 +9,7 @@
 #define RUBY_VERSION_TEENY 5
 #define RUBY_RELEASE_YEAR 2007
 #define RUBY_RELEASE_MONTH 1
-#define RUBY_RELEASE_DAY 3
+#define RUBY_RELEASE_DAY 7
 
 RUBY_EXTERN const char ruby_version[];
 RUBY_EXTERN const char ruby_release_date[];
