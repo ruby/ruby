86846f0ee50d4cd2a8783bc414a6e7904a6ece4f
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-02-08 13:03:54 +0000

    * test/ruby/marshaltestlib.rb: eval(sym) -> eval(sym.to_s)
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@11665 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/marshaltestlib.rb
+++ test/ruby/marshaltestlib.rb
@@ -29,8 +29,8 @@ module MarshalTestLib
     iv1 = o1.instance_variables.sort
     iv2 = o2.instance_variables.sort
     assert_equal(iv1, iv2)
-    val1 = iv1.map {|var| o1.instance_eval {eval var}}
-    val2 = iv1.map {|var| o2.instance_eval {eval var}}
+    val1 = iv1.map {|var| o1.instance_eval {eval var.to_s}}
+    val2 = iv1.map {|var| o2.instance_eval {eval var.to_s}}
     assert_equal(val1, val2, msg)
     if block_given?
       assert_equal(yield(o1), yield(o2), msg)
