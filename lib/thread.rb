97d26f32bbea750e4b3a6be8ee327f907d871166
shugo <shugo@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-02-24 06:42:04 +0000

    * lib/thread.rb: do not redefine Mutex#synchronize.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@11844 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/thread.rb
+++ lib/thread.rb
@@ -21,15 +21,6 @@ if $DEBUG
   Thread.abort_on_exception = true
 end
 
-class Mutex
-  def synchronize
-    self.lock
-    yield
-  ensure
-    self.unlock
-  end
-end
-
 # 
 # ConditionVariable objects augment class Mutex. Using condition variables,
 # it is possible to suspend while in the middle of a critical section until a
