b7111d382c16ee89dae3c19c756ce2272190d55a
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-02-24 17:52:20 +0000

    20070225
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_6@11874 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #define RUBY_VERSION "1.8.6"
-#define RUBY_RELEASE_DATE "2007-02-24"
+#define RUBY_RELEASE_DATE "2007-02-25"
 #define RUBY_VERSION_CODE 186
-#define RUBY_RELEASE_CODE 20070224
+#define RUBY_RELEASE_CODE 20070225
 #define RUBY_PATCHLEVEL 0
 
 #define RUBY_VERSION_MAJOR 1
@@ -9,7 +9,7 @@
 #define RUBY_VERSION_TEENY 6
 #define RUBY_RELEASE_YEAR 2007
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 24
+#define RUBY_RELEASE_DAY 25
 
 RUBY_EXTERN const char ruby_version[];
 RUBY_EXTERN const char ruby_release_date[];
