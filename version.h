dd19c29f867a20d06ee793d106df333ea5ef9b35
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-03-12 07:16:12 +0000

    *merge -c 11946
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_5@12044 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define RUBY_RELEASE_DATE "2007-03-12"
 #define RUBY_VERSION_CODE 185
 #define RUBY_RELEASE_CODE 20070312
-#define RUBY_PATCHLEVEL 31
+#define RUBY_PATCHLEVEL 32
 
 #define RUBY_VERSION_MAJOR 1
 #define RUBY_VERSION_MINOR 8
@@ -11,7 +11,9 @@
 #define RUBY_RELEASE_MONTH 3
 #define RUBY_RELEASE_DAY 12
 
+#ifdef RUBY_EXTERN
 RUBY_EXTERN const char ruby_version[];
 RUBY_EXTERN const char ruby_release_date[];
 RUBY_EXTERN const char ruby_platform[];
 RUBY_EXTERN const int ruby_patchlevel;
+#endif
