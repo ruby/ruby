ab6752498c1577fc6fd2db4338dafe07e64503bd
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-03-12 18:23:54 +0000

    * runruby.rb: added --pure (turned on by default) and --debugger   options.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@12052 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- runruby.rb
+++ runruby.rb
@@ -1,5 +1,6 @@
 #!./miniruby
 
+pure = true
 while arg = ARGV[0]
   break ARGV.shift if arg == '--'
   /\A--([-\w]+)(?:=(.*))?\z/ =~ arg or break
@@ -12,6 +13,10 @@ while arg = ARGV[0]
     archdir = value
   when re =~ "extout"
     extout = value
+  when re =~ "pure"
+    pure = (value != "no")
+  when re =~ "debugger"
+    debugger = value ? (value.split unless value == "no") : %w"gdb --args"
   else
     break
   end
@@ -25,23 +30,24 @@ abs_archdir = File.expand_path(archdir)
 $:.unshift(abs_archdir)
 
 require 'rbconfig'
-config = Config::CONFIG
+config = RbConfig::CONFIG
 
 ruby = File.join(archdir, config["RUBY_INSTALL_NAME"]+config['EXEEXT'])
 unless File.exist?(ruby)
   abort "#{ruby} is not found.\nTry `make' first, then `make test', please.\n"
 end
 
-libs = [abs_archdir, File.expand_path("lib", srcdir)]
+libs = [abs_archdir]
 if extout
   abs_extout = File.expand_path(extout)
   libs << File.expand_path("common", abs_extout) << File.expand_path(RUBY_PLATFORM, abs_extout)
 end
+libs << File.expand_path("lib", srcdir)
 config["bindir"] = abs_archdir
 ENV["RUBY"] = File.expand_path(ruby)
 ENV["PATH"] = [abs_archdir, ENV["PATH"]].compact.join(File::PATH_SEPARATOR)
 
-if e = ENV["RUBYLIB"]
+if !pure and e = ENV["RUBYLIB"]
   libs |= e.split(File::PATH_SEPARATOR)
 end
 ENV["RUBYLIB"] = $:.replace(libs).join(File::PATH_SEPARATOR)
@@ -55,5 +61,15 @@ if File.file?(libruby_so)
     ENV["LD_PRELOAD"] = [libruby_so, ENV["LD_PRELOAD"]].compact.join(' ')
   end
 end
+begin
+  open("puretest.rb", IO::EXCL|IO::CREAT|IO::WRONLY) do |f|
+    f.puts('$LOAD_PATH.replace(ENV["RUBYLIB"].split(File::PATH_SEPARATOR))')
+  end
+rescue Errno::EEXIST
+end
 
-exec ruby, *ARGV
+cmd = [ruby]
+cmd << "-rpuretest.rb" if pure
+cmd.concat(ARGV)
+cmd.unshift(*debugger) if debugger
+exec(*cmd)
