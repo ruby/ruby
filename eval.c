47807a7b44ba98fa87d9cee83852c0f6dc7615e9
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-04-20 03:16:21 +0000

    * eval.c (ruby_cleanup): inversed the order of errinfos.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@12201 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- eval.c
+++ eval.c
@@ -1565,15 +1565,14 @@ ruby_cleanup(ex)
     int ex;
 {
     int state;
-    VALUE err;
     volatile VALUE errs[2];
     int nerr;
 
-    errs[0] = ruby_errinfo;
-    ruby_safe_level = 0;
-    Init_stack((void*)&state);
-    ruby_finalize_0();
     errs[1] = ruby_errinfo;
+    ruby_safe_level = 0;
+    Init_stack((void *)&state);
+    ruby_finalize_0();
+    errs[0] = ruby_errinfo;
     PUSH_TAG(PROT_NONE);
     PUSH_ITER(ITER_NOT);
     if ((state = EXEC_TAG()) == 0) {
@@ -1584,13 +1583,16 @@ ruby_cleanup(ex)
 	ex = state;
     }
     POP_ITER();
-    ruby_errinfo = errs[0];
+    ruby_errinfo = errs[1];
     ex = error_handle(ex);
     ruby_finalize_1();
     POP_TAG();
 
-    for (nerr = sizeof(errs) / sizeof(errs[0]); nerr;) {
-	if (!(err = errs[--nerr])) continue;
+    for (nerr = 0; nerr < sizeof(errs) / sizeof(errs[0]); ++nerr) {
+	VALUE err = errs[nerr];
+
+	if (!RTEST(err)) continue;
+
 	if (rb_obj_is_kind_of(err, rb_eSystemExit)) {
 	    return sysexit_status(err);
 	}
@@ -1599,6 +1601,7 @@ ruby_cleanup(ex)
 	    ruby_default_signal(NUM2INT(sig));
 	}
     }
+
     return ex;
 }
 
