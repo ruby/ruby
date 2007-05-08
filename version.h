d0ec0e5c1785d82ebf4f8c68879836aaeaa335f6
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-05-08 11:41:19 +0000

    * 2007-05-08
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@12256 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #define RUBY_VERSION "1.8.6"
-#define RUBY_RELEASE_DATE "2007-05-05"
+#define RUBY_RELEASE_DATE "2007-05-08"
 #define RUBY_VERSION_CODE 186
-#define RUBY_RELEASE_CODE 20070505
+#define RUBY_RELEASE_CODE 20070508
 #define RUBY_PATCHLEVEL 5000
 
 #define RUBY_VERSION_MAJOR 1
@@ -9,7 +9,7 @@
 #define RUBY_VERSION_TEENY 6
 #define RUBY_RELEASE_YEAR 2007
 #define RUBY_RELEASE_MONTH 5
-#define RUBY_RELEASE_DAY 5
+#define RUBY_RELEASE_DAY 8
 
 RUBY_EXTERN const char ruby_version[];
 RUBY_EXTERN const char ruby_release_date[];
