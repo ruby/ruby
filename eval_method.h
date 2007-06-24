a5c2d584284e2b137b1d4d06574470a971c9087e
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-06-24 13:32:57 +0000

    * eval_method.h (rb_add_method): fix to check 0.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@12602 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- eval_method.h
+++ eval_method.h
@@ -150,7 +150,7 @@ rb_add_method(VALUE klass, ID mid, NODE * node, int noex)
 		    rb_warning("method redefined; discarding old %s", rb_id2name(mid));
 		}
 	    }
-	    if (klass == rb_cObject && node->nd_mid == init) {
+	    if (klass == rb_cObject && node && node->nd_mid == init) {
 		rb_warn("redefining Object#initialize may cause infinite loop");
 	    }
 	}
