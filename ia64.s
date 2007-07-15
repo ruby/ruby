1edeb9a6d12ab2a74efcf7db4498a4603918d7ee
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-07-15 01:20:19 +0000

    fix previous change.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@12792 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ia64.s
+++ ia64.s
@@ -7,7 +7,7 @@
 //   void *rb_ia64_bsp(void) { return __builtin_ia64_bsp(); }
 // 
 // Note that rb_ia64_flushrs and rb_ia64_bsp works in its own stack frame.
-// It's because BSP is updated by br.call/brl.call (not alloc instruction)
+// It's because BSP is updated by br.call/brl.call (not alloc instruction).
 // So rb_ia64_flushrs flushes stack frames including caller's one.
 // rb_ia64_bsp returns the address next to caller's register stack frame. 
 //
