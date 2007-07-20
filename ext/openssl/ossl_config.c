a86b7ee462ca61e8d50259a9e839f81f02de581c
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-07-20 06:22:54 +0000

    * ext/openssl/ossl_config.c (ossl_config_set_section): do not   initialize aggregations with dynamic values.  [ruby-talk:259306]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@12821 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/openssl/ossl_config.c
+++ ext/openssl/ossl_config.c
@@ -245,7 +245,10 @@ set_conf_section_i(VALUE i, VALUE *arg)
 static VALUE
 ossl_config_set_section(VALUE self, VALUE section, VALUE hash)
 {
-    VALUE arg[2] = { self, section };
+    VALUE arg[2];
+
+    arg[0] = self;
+    arg[1] = section;
     rb_block_call(hash, rb_intern("each"), 0, 0, set_conf_section_i, (VALUE)arg);
     return hash;
 }
