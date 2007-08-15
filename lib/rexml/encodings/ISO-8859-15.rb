2da27448383e519907e51555ffc173582acd831f
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-08-15 23:28:47 +0000

    * lib/rexml/encodings/{ISO-8859-15,CP-1252}.rb: fixed invalid syntax.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_5@13049 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/rexml/encodings/ISO-8859-15.rb
+++ lib/rexml/encodings/ISO-8859-15.rb
@@ -11,25 +11,25 @@ module REXML
       array_utf8.each do |num|
         case num
           # shortcut first bunch basic characters
-        when 0..0xA3: array_enc << num
+        when 0..0xA3; array_enc << num
           # characters removed compared to iso-8859-1
-        when 0xA4: array_enc << '&#164;'
-        when 0xA6: array_enc << '&#166;'
-        when 0xA8: array_enc << '&#168;'
-        when 0xB4: array_enc << '&#180;'
-        when 0xB8: array_enc << '&#184;'
-        when 0xBC: array_enc << '&#188;'
-        when 0xBD: array_enc << '&#189;'
-        when 0xBE: array_enc << '&#190;'
+        when 0xA4; array_enc << '&#164;'
+        when 0xA6; array_enc << '&#166;'
+        when 0xA8; array_enc << '&#168;'
+        when 0xB4; array_enc << '&#180;'
+        when 0xB8; array_enc << '&#184;'
+        when 0xBC; array_enc << '&#188;'
+        when 0xBD; array_enc << '&#189;'
+        when 0xBE; array_enc << '&#190;'
           # characters added compared to iso-8859-1
-        when 0x20AC: array_enc << 0xA4 # 0xe2 0x82 0xac
-        when 0x0160: array_enc << 0xA6 # 0xc5 0xa0
-        when 0x0161: array_enc << 0xA8 # 0xc5 0xa1
-        when 0x017D: array_enc << 0xB4 # 0xc5 0xbd
-        when 0x017E: array_enc << 0xB8 # 0xc5 0xbe
-        when 0x0152: array_enc << 0xBC # 0xc5 0x92
-        when 0x0153: array_enc << 0xBD # 0xc5 0x93
-        when 0x0178: array_enc << 0xBE # 0xc5 0xb8
+        when 0x20AC; array_enc << 0xA4 # 0xe2 0x82 0xac
+        when 0x0160; array_enc << 0xA6 # 0xc5 0xa0
+        when 0x0161; array_enc << 0xA8 # 0xc5 0xa1
+        when 0x017D; array_enc << 0xB4 # 0xc5 0xbd
+        when 0x017E; array_enc << 0xB8 # 0xc5 0xbe
+        when 0x0152; array_enc << 0xBC # 0xc5 0x92
+        when 0x0153; array_enc << 0xBD # 0xc5 0x93
+        when 0x0178; array_enc << 0xBE # 0xc5 0xb8
         else
           # all remaining basic characters can be used directly
           if num <= 0xFF
@@ -50,14 +50,14 @@ module REXML
       array_latin9.each do |num|
         case num
           # characters that differ compared to iso-8859-1
-        when 0xA4: array_enc << 0x20AC
-        when 0xA6: array_enc << 0x0160
-        when 0xA8: array_enc << 0x0161
-        when 0xB4: array_enc << 0x017D
-        when 0xB8: array_enc << 0x017E
-        when 0xBC: array_enc << 0x0152
-        when 0xBD: array_enc << 0x0153
-        when 0xBE: array_enc << 0x0178
+        when 0xA4; array_enc << 0x20AC
+        when 0xA6; array_enc << 0x0160
+        when 0xA8; array_enc << 0x0161
+        when 0xB4; array_enc << 0x017D
+        when 0xB8; array_enc << 0x017E
+        when 0xBC; array_enc << 0x0152
+        when 0xBD; array_enc << 0x0153
+        when 0xBE; array_enc << 0x0178
         else
           array_enc << num
         end
