2f17991bd12e45ba5e4ca4ae973ba0af3887b5d6
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-08-24 23:49:19 +0000

    * tool/compile_prelude.rb: fix to include "vm_core.h".
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@13254 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/compile_prelude.rb
+++ tool/compile_prelude.rb
@@ -2,14 +2,16 @@
 prelude, outfile = *ARGV
 lines = []
 
-File.readlines(prelude).each{|line|
-  lines << "#{line.dump}"
+lines = File.readlines(prelude).map{|line|
+  line.dump
 }
 
 open(outfile, 'w'){|f|
 f.puts <<EOS__
 
 #include "ruby/ruby.h"
+#include "vm_core.h"
+
 static const char *prelude_code = 
 #{lines.join("\n")}
 ;
@@ -18,7 +20,11 @@ Init_prelude(void)
 {
   rb_iseq_eval(rb_iseq_compile(
     rb_str_new2(prelude_code),
-    rb_str_new2("prelude.rb"), INT2FIX(1)));
+    rb_str_new2("#{File.basename(prelude)}"), INT2FIX(1)));
+
+#if 0
+    printf("%s\n", prelude_code);
+#endif
 }
 EOS__
 }
