bb32165fe9934ee3fb2bb06cb47772b77af0ce19
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-09-26 23:24:08 +0000

    refine a test.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@13532 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_knownbug.rb
+++ bootstraptest/test_knownbug.rb
@@ -5,8 +5,8 @@
 
 assert_finish 1, %q{
   r, w = IO.pipe
-  t1 = Thread.new { r.sysread(10) }
-  t2 = Thread.new { r.sysread(10) }
+  t1 = Thread.new { r.sysread(1) }
+  t2 = Thread.new { r.sysread(1) }
   sleep 0.1
   w.write "a"
   sleep 0.1
