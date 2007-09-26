55f7f3dd3c9569a33589e481cae8d727cafa60e5
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-09-26 11:12:55 +0000

    * bootstraptest/test_knownbug.rb: remove a fixed test.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@13524 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_knownbug.rb
+++ bootstraptest/test_knownbug.rb
@@ -2,21 +2,3 @@
 # This test file concludes tests which point out known bugs.
 # So all tests will cause failure.
 #
-
-assert_equal 'ok', %q{
-begin
-  r, w = IO.pipe
-  w.write "foo"
-  w.close
-  # assert_raise(IOError, "[ruby-dev:31650]") { 20000.times { r.ungetc "a" } }
-  r.getc
-  20000.times { r.ungetc "a" }
-  data = r.read
-  if data.size == 20002 && data[-5..-1] == "aaaoo"
-    :ok
-  end
-ensure
-  r.close
-end
-}, 'rename test/ruby/test_io.rb#_test_ungetc if fixed'
-
