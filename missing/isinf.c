093fe3b839b9d3c5890e0d31faaefcce5d8acf7c
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-11-13 16:54:27 +0000

    * missing/isinf.c (isinf): don't define is the macro is defined.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@13915 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- missing/isinf.c
+++ missing/isinf.c
@@ -27,11 +27,18 @@ isinf(double n)
 #include <ieeefp.h>
 #endif
 
+/* 
+ * isinf may be provided only as a macro.
+ * ex. HP-UX, Solaris 10
+ * http://www.gnu.org/software/automake/manual/autoconf/Function-Portability.html
+ */
+#ifndef isinf
 int
 isinf(double n)
 {
     return (!finite(n) && !isnan(n));
 }
+#endif
 
 #else
 
