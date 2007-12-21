5bb7c8612fc0904070aa9cfad096e3c462877903
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-12-21 07:52:49 +0000

    * encoding.c (Init_Encoding): use enc_name as to_s.   (enc_inspect): renamed from enc_to_s.  add "#" at beginning.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14418 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- encoding.c
+++ encoding.c
@@ -629,9 +629,9 @@ rb_enc_tolower(int c, rb_encoding *enc)
 }
 
 static VALUE
-enc_to_s(VALUE self)
+enc_inspect(VALUE self)
 {
-    return rb_sprintf("<%s:%s>", rb_obj_classname(self),
+    return rb_sprintf("#<%s:%s>", rb_obj_classname(self),
 		      rb_enc_name((rb_encoding*)DATA_PTR(self)));
 }
 
@@ -811,8 +811,8 @@ Init_Encoding(void)
 
     rb_cEncoding = rb_define_class("Encoding", rb_cObject);
     rb_undef_alloc_func(rb_cEncoding);
-    rb_define_method(rb_cEncoding, "to_s", enc_to_s, 0);
-    rb_define_method(rb_cEncoding, "inspect", enc_to_s, 0);
+    rb_define_method(rb_cEncoding, "to_s", enc_name, 0);
+    rb_define_method(rb_cEncoding, "inspect", enc_inspect, 0);
     rb_define_method(rb_cEncoding, "name", enc_name, 0);
     rb_define_method(rb_cEncoding, "based_encoding", enc_based_encoding, 0);
     rb_define_singleton_method(rb_cEncoding, "list", enc_list, 0);
