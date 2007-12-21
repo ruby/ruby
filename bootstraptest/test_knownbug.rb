328e6a02bf19c4b0397a6a2e066b273e7dd339de
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-12-21 06:38:12 +0000

    add test for [ruby-dev:32566].
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14409 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_knownbug.rb
+++ bootstraptest/test_knownbug.rb
@@ -17,3 +17,18 @@ assert_equal 'ok', %q{
   :ok
 }, '[ruby-dev:32525]'
 
+assert_finish 2, %q{
+  require "io/nonblock"
+  r, w = IO.pipe
+  w.nonblock = true
+  w.write_nonblock("a" * 100000)
+  w.nonblock = false
+  t1 = Thread.new { w.write("b" * 4096) }
+  t2 = Thread.new { w.write("c" * 4096) }
+  sleep 0.5
+  r.sysread(4096).length
+  sleep 0.5
+  r.sysread(4096).length
+  t1.join
+  t2.join
+}, '[ruby-dev:32566]'
