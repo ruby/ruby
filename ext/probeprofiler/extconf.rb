fc5c62bef79c1d09606ba0e012d7a66e64d14270
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-12-22 10:19:27 +0000

    * ext/probeprofiler/: removed.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14488 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/probeprofiler/extconf.rb
+++
@@ -1,4 +0,0 @@
-require 'mkmf'
-$VPATH << "$(top_srcdir)"
-$INCFLAGS << " -I$(top_srcdir)"
-create_makefile("probeprofiler")
--- ext/probeprofiler/lib/probeprofile.rb
+++
@@ -1,7 +0,0 @@
-require 'probeprofiler'
-END{
-  ProbeProfiler.stop_profile
-  ProbeProfiler.print_profile
-}
-
-ProbeProfiler.start_profile
