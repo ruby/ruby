0ef8ed93bcd7d0e073e0e2f28ada279471155100
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-12-23 06:57:52 +0000

    add a test for [ruby-dev:32048].
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14514 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_knownbug.rb
+++ bootstraptest/test_knownbug.rb
@@ -10,3 +10,19 @@ assert_equal 'ok', %q{
   }
   s == "a\xFF" ? :ok : :ng
 }, '[ruby-core:14288]'
+
+assert_equal 'ok', %q{
+  open("require-lock-test.rb", "w") {|f|
+    f.puts "sleep 0.1"
+    f.puts "module M"
+    f.puts "end"
+  }
+  $:.unshift Dir.pwd
+  vs = (1..2).map {|i|
+    Thread.start {
+      require "require-lock-test"
+      M
+    }
+  }.map {|t| t.value }
+  vs[0] == M && vs[1] == M ? :ok : :ng
+}, '[ruby-dev:32048]'
