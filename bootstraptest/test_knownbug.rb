71787f8ec2704a0559a8de8e91054f31e8444b3b
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-12-24 15:19:44 +0000

    add a test for [ruby-core:14385] reported by Frederick Cheung.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14615 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_knownbug.rb
+++ bootstraptest/test_knownbug.rb
@@ -2,3 +2,12 @@
 # This test file concludes tests which point out known bugs.
 # So all tests will cause failure.
 #
+
+assert_normal_exit %q{
+  counter = 2
+  while true
+    counter -= 1
+    next if counter != 0
+    break
+  end
+}, '[ruby-core:14385]'
