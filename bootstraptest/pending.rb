96b9744b3dab5bb9bbe5f53e2cb861a984b3e50e
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-12-25 13:15:53 +0000

    * bootstraptest/pending.rb: add pending issue.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14702 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/pending.rb
+++ bootstraptest/pending.rb
@@ -4,3 +4,22 @@ assert_equal 'ok', %q{
   }
   :ok
 }, '[ruby-dev:32525]'
+
+assert_equal "ok", %q{
+  module Foo
+  end
+
+  begin
+    def foo(&b)
+      Foo.module_eval &b
+    end
+    foo{
+      def bar
+      end
+    }
+    bar
+  rescue NoMethodError
+    :ok
+  end
+}, '[ruby-core:14378]'
+
