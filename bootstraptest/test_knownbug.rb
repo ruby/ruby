ed7b25a85436cff446779f580e859da94cd4d9d8
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-12-25 07:27:15 +0000

    add a test.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14679 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_knownbug.rb
+++ bootstraptest/test_knownbug.rb
@@ -2,3 +2,13 @@
 # This test file concludes tests which point out known bugs.
 # So all tests will cause failure.
 #
+
+assert_finish 1, %q{
+  r, w = IO.pipe
+  Thread.new {
+  w << "ab"
+  sleep 0.1
+  w << "ab"
+  }
+  p r.gets("abab")
+}
