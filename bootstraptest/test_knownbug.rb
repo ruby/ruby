305c42739f9896602400bca08b48ae1de978d99e
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-12-25 09:39:10 +0000

    add tests reported by Yusuke ENDOH.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14689 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_knownbug.rb
+++ bootstraptest/test_knownbug.rb
@@ -12,3 +12,27 @@ assert_finish 1, %q{
   }
   p r.gets("abab")
 }
+
+assert_normal_exit %q{
+  begin
+    raise
+  rescue
+    counter = 2
+    while true
+      counter -= 1
+      break if counter == 0
+      next
+      retry
+    end
+  end
+}, 'reported by Yusuke ENDOH'
+
+assert_normal_exit %q{
+  counter = 2
+  while true
+    counter -= 1
+    break if counter == 0
+    next
+    "#{ break }"
+  end
+}, 'reported by Yusuke ENDOH'
