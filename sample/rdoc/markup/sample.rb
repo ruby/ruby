ecb7eb6ba4a6f10ac9ff2e7531c9b12ac81646c5
drbrain <drbrain@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-12-28 06:39:15 +0000

    Move RDoc markup samples to sample/rdoc/markup directory
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14761 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- sample/rdoc/markup/sample.rb
+++ sample/rdoc/markup/sample.rb
@@ -1,20 +1,18 @@
 # This program illustrates the basic use of the SimpleMarkup
-# class. It extracts the first comment block from the 
+# class. It extracts the first comment block from the
 # simple_markup.rb file and converts it into HTML on
 # standard output. Run it using
 #
 #  % ruby sample.rb
 #
-# You should be in the sample/ directory when you do this,
+# You should be in the sample/rdoc/markup/ directory when you do this,
 # as it hardwires the path to the files it needs to require.
-# This isn't necessary in the code you write once you've 
+# This isn't necessary in the code you write once you've
 # installed the package.
 #
 # For a better way of formatting code comment blocks (and more)
 # see the rdoc package.
 #
-
-$:.unshift "../../.."
 
 require 'rdoc/markup/simple_markup'
 require 'rdoc/markup/simple_markup/to_html'
@@ -23,7 +21,7 @@ require 'rdoc/markup/simple_markup/to_html'
 
 input_string = ""
 
-File.foreach("../simple_markup.rb") do |line|
+File.foreach("../../../lib/rdoc/markup/simple_markup.rb") do |line|
   break unless line.gsub!(/^\# ?/, '')
   input_string << line
 end
