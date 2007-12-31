e460d4f75782a4785c3f47d778a1c9b8ccd25659
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2007-12-31 16:32:24 +0000

    * bootstraptest/test_knownbug.rb: shut up syntax error
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14823 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_knownbug.rb
+++ bootstraptest/test_knownbug.rb
@@ -4,6 +4,7 @@
 #
 
 assert_normal_exit %q{
+  STDERR.reopen("/dev/null") if File.exist? "/dev/null" and File.writable? "/dev/null"
   class Foo
      def self.add_method
        class_eval("def some-bad-name; puts 'hello' unless @some_variable.some_function(''); end")
