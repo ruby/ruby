22e7258275966b89b0bf2462179f99c7a8feb34a
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-01-04 01:24:12 +0000

    * re.c (rb_reg_search): avoid inner loop for reverse search.
    * regexec.c: unset USE_MATCH_RANGE_MUST_BE_INSIDE_OF_SPECIFIED_RANGE
      which is turned on since oniguruma 5.9.1.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14878 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- re.c
+++ re.c
@@ -1035,7 +1035,7 @@ rb_reg_search(VALUE re, VALUE str, int pos, int reverse)
     int result;
     VALUE match;
     static struct re_registers regs;
-    int range;
+    char *range = RSTRING_PTR(str);
     rb_encoding *enc = rb_enc_get(str);
 
     if (pos > RSTRING_LEN(str) || pos < 0) {
@@ -1045,33 +1045,15 @@ rb_reg_search(VALUE re, VALUE str, int pos, int reverse)
 
     rb_reg_prepare_re(re, str);
 
-    if (reverse) {
-        char *p = RSTRING_PTR(str) + pos;
-        while (1) {
-            result = onig_match(RREGEXP(re)->ptr,
-                                (UChar*)(RSTRING_PTR(str)),
-                                ((UChar*)(RSTRING_PTR(str)) + RSTRING_LEN(str)),
-                                (UChar*)p,
-                                &regs,
-                                ONIG_OPTION_NONE);
-            if (result != ONIG_MISMATCH) {
-                result = p - RSTRING_PTR(str);
-                break;
-            }
-            if (RSTRING_PTR(str) == p)
-                break;
-            p = rb_enc_prev_char(RSTRING_PTR(str), p, enc);
-        }
+    if (!reverse) {
+	range += RSTRING_LEN(str);
     }
-    else {
-	range = RSTRING_LEN(str) - pos;
-        result = onig_search(RREGEXP(re)->ptr,
-                             (UChar*)(RSTRING_PTR(str)),
-                             ((UChar*)(RSTRING_PTR(str)) + RSTRING_LEN(str)),
-                             ((UChar*)(RSTRING_PTR(str)) + pos),
-                             ((UChar*)(RSTRING_PTR(str)) + pos + range),
-                             &regs, ONIG_OPTION_NONE);
-    }
+    result = onig_search(RREGEXP(re)->ptr,
+			 (UChar*)(RSTRING_PTR(str)),
+			 ((UChar*)(RSTRING_PTR(str)) + RSTRING_LEN(str)),
+			 ((UChar*)(RSTRING_PTR(str)) + pos),
+			 ((UChar*)range),
+			 &regs, ONIG_OPTION_NONE);
 
     if (result < 0) {
 	if (result == ONIG_MISMATCH) {
