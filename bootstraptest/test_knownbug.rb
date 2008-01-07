fa21f1b7510dba084b907a6189a67f36d36a5008
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-01-07 01:56:16 +0000

    add test_knonbug
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14921 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_knownbug.rb
+++ bootstraptest/test_knownbug.rb
@@ -16,3 +16,28 @@ assert_equal 'ok', %q{
 # test is not written...
 # * [ruby-dev:31819] rb_clear_cache_by_class
 # * [ruby-dev:31820] valgrind set_trace_func
+
+
+assert_equal 'ok', %q{
+  while true
+    *, z = 1
+    break
+  end
+  :ok
+}, '[ruby-dev:32892]'
+
+
+assert_equal 'ok', %q{
+  1.times do
+    [
+      1, 2, 3, 4, 5, 6, 7, 8,
+      begin
+        false ? next : p
+        break while true
+      end
+    ]
+  end
+  :ok
+}, '[ruby-dev:32882]'
+
+
