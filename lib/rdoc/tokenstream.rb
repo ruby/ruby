ead85d123f37be2f166acf9a2c0fb5e08368813e
drbrain <drbrain@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-01-07 07:11:15 +0000

    Namespace TokenStream under RDoc.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14930 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/rdoc/tokenstream.rb
+++ lib/rdoc/tokenstream.rb
@@ -1,11 +1,14 @@
-# A TokenStream is a list of tokens, gathered during the parse
-# of some entity (say a method). Entities populate these streams
-# by being registered with the lexer. Any class can collect tokens
-# by including TokenStream. From the outside, you use such an object
-# by calling the start_collecting_tokens method, followed by calls
-# to add_token and pop_token
+module RDoc; end
 
-module TokenStream
+##
+# A TokenStream is a list of tokens, gathered during the parse of some entity
+# (say a method). Entities populate these streams by being registered with the
+# lexer. Any class can collect tokens by including TokenStream. From the
+# outside, you use such an object by calling the start_collecting_tokens
+# method, followed by calls to add_token and pop_token.
+
+module RDoc::TokenStream
+
   def token_stream
     @token_stream
   end
@@ -13,13 +16,18 @@ module TokenStream
   def start_collecting_tokens
     @token_stream = []
   end
+
   def add_token(tk)
     @token_stream << tk
   end
+
   def add_tokens(tks)
     tks.each  {|tk| add_token(tk)}
   end
+
   def pop_token
     @token_stream.pop
   end
+
 end
+
