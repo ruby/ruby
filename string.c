0ada40e2b1d85580ec14c96d18321d1a245d328f
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-01-11 06:03:51 +0000

    update rdoc: force_encoding doesn't accept nil.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@14991 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- string.c
+++ string.c
@@ -5468,8 +5468,6 @@ rb_str_setter(VALUE val, ID id, VALUE *var)
  *     str.force_encoding(encoding)   => str
  *
  *  Changes the encoding to +encoding+ and returns self.
- *
- *  If nil is given as +encoding+, "ASCII-8BIT" is used.
  */
 
 static VALUE
