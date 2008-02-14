a5c05a9ace5aad5fa9fc545be0be829cf61b16c4
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-02-14 08:23:56 +0000

    add a test for [ruby-dev:33072].
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@15470 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_knownbug.rb
+++ bootstraptest/test_knownbug.rb
@@ -45,3 +45,7 @@ assert_equal 'ok', %q{
 }, '[ruby-core:14537]'
 
 assert_valid_syntax('1.times {|i|print (42),1;}', '[ruby-list:44479]')
+
+assert_normal_exit %q{
+  File.read("empty", nil, nil, {})
+}, '[ruby-dev:33072]'
