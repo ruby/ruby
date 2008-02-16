125c0a0d93f8562c1115290d10e110e20053eebb
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-02-16 02:46:27 +0000

    * lib/require_relative.rb: check require_relative call in eval.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@15497 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/require_relative.rb
+++ lib/require_relative.rb
@@ -1,6 +1,11 @@
 def require_relative(relative_feature)
-  /:/ =~ caller.first
-  absolute_feature = File.expand_path(File.join(File.dirname($`), relative_feature))
+  c = caller.first
+  e = c.rindex(/:\d+:in /)
+  file = $`
+  if /\A\((.*)\)/ =~ file # eval, etc.
+    raise LoadError, "require_relative is called in #{$1}"
+  end
+  absolute_feature = File.expand_path(File.join(File.dirname(file), relative_feature))
   require absolute_feature
 end
 
