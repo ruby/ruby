adc639bb263ad411c0081b2d786865186c31ec2c
kazu <kazu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-02-17 14:08:27 +0000

    * lib/cgi.rb (CGI::escapeHTML): use gsub with Hash. [ruby-dev:33828]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@15526 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/cgi.rb
+++ lib/cgi.rb
@@ -355,12 +355,18 @@ class CGI
     end
   end
 
+  TABLE_FOR_ESCAPE_HTML__ = {
+    '&' => '&amp;',
+    '"' => '&quot;',
+    '<' => '&lt;',
+    '>' => '&gt;',
+  }
 
   # Escape special characters in HTML, namely &\"<>
   #   CGI::escapeHTML('Usage: foo "bar" <baz>')
   #      # => "Usage: foo &quot;bar&quot; &lt;baz&gt;"
   def CGI::escapeHTML(string)
-    string.gsub(/&/, '&amp;').gsub(/\"/, '&quot;').gsub(/>/, '&gt;').gsub(/</, '&lt;')
+    string.gsub(/[&\"<>]/, TABLE_FOR_ESCAPE_HTML__)
   end
 
 
