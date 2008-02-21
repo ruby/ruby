4701476b97f0297751578062178f388c210947af
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-02-21 01:36:50 +0000

    * include/ruby/encoding.h (rb_enc_asciicompat): dummy encoding is not   ascii compatible. [ruby-dev:33878]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@15559 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/encoding.h
+++ include/ruby/encoding.h
@@ -154,7 +154,7 @@ int rb_enc_codelen(int code, rb_encoding *enc);
 #define rb_enc_isspace(c,enc) ONIGENC_IS_CODE_SPACE(enc,c)
 #define rb_enc_isdigit(c,enc) ONIGENC_IS_CODE_DIGIT(enc,c)
 
-#define rb_enc_asciicompat(enc) (rb_enc_mbminlen(enc)==1)
+#define rb_enc_asciicompat(enc) (!rb_enc_dummy_p(enc) && rb_enc_mbminlen(enc)==1)
 
 int rb_enc_casefold(char *to, const char *p, const char *e, rb_encoding *enc);
 int rb_enc_toupper(int c, rb_encoding *enc);
