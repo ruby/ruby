2f866a3666b3e768a07d4a780f7ff20375b82d21
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-02-28 18:32:17 +0000

    * test/ruby/test_time.rb (test_readers): fix typo.   (test_strftime): "UTC" is also ok for time.gmtime.strftime("%Z").
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@15636 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_time.rb
+++ test/ruby/test_time.rb
@@ -327,6 +327,7 @@ class TestTime < Test::Unit::TestCase
   end
 
   def test_strftime
+    t = Time.at(946684800).getlocal
     assert_equal("Sat", T2000.strftime("%a"))
     assert_equal("Saturday", T2000.strftime("%A"))
     assert_equal("Jan", T2000.strftime("%b"))
@@ -347,7 +348,7 @@ class TestTime < Test::Unit::TestCase
     assert_equal("00:00:00", T2000.strftime("%X"))
     assert_equal("00", T2000.strftime("%y"))
     assert_equal("2000", T2000.strftime("%Y"))
-    assert_equal("GMT", T2000.strftime("%Z"))
+    assert(["GMT", "UTC"].include?(T2000.strftime("%Z")))
     assert_equal("%", T2000.strftime("%%"))
 
     assert_equal("", T2000.strftime(""))
