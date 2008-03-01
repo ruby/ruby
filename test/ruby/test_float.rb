655fd2dc372929a7d6250a5c8150bbc447798b6e
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-03-01 16:05:10 +0000

    add a test.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@15668 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_float.rb
+++ test/ruby/test_float.rb
@@ -83,6 +83,7 @@ class TestFloat < Test::Unit::TestCase
     assert_raise(ArgumentError){Float("1e")}
     assert_raise(ArgumentError){Float("1__1")}
     # add expected behaviour here.
+    assert_equal(10, Float("1_0"))
   end
 
   def test_divmod
