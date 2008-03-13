020d4e4d38beca71cdf9000356ec56ffb390a30d
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-03-13 16:38:56 +0000

    * test/ruby/test_numeric.rb (TestNumeric::test_num2long): bit-and   should not raise RangeError.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@15766 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_numeric.rb
+++ test/ruby/test_numeric.rb
@@ -211,10 +211,8 @@ class TestNumeric < Test::Unit::TestCase
   def test_num2long
     assert_raise(TypeError) { 1 & nil }
     assert_equal(1, 1 & 1.0)
-    assert_raise(RangeError) do
-      1 & 2147483648.0
-      1 & 9223372036854777856.0
-    end
+    assert_equal(0, 1 & 2147483648.0)
+    assert_equal(0, 1 & 9223372036854777856.0)
     o = Object.new
     def o.to_int; 1; end
     assert_equal(1, 1 & o)
