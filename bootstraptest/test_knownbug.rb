3f3fdb4869350554bffcabcac7bf59ab866a8ddf
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-04-07 03:28:23 +0000

    * bootstraptest/test_knownbug.rb: add a known-bug.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@15914 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_knownbug.rb
+++ bootstraptest/test_knownbug.rb
@@ -97,3 +97,16 @@ assert_normal_exit %q{
   g = "abc".enum_for(:scan, /./)
   loop { g.next }
 }, '[ruby-dev:34128]'
+
+assert_equal %q{[:bar, :foo]}, %q{
+  def foo
+    klass = Class.new do
+      define_method(:bar) do
+        return :bar
+      end
+    end
+    [klass.new.bar, :foo]
+  end
+  foo
+}, "[ ruby-Bugs-19304 ]"
+
