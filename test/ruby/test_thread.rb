5098d7ae0dcef98c82e64280d2aded2676738c3b
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-04-09 03:12:03 +0000

    * test/ruby/test_thread.rb: new tests from Sylvain Joyeux in   [ruby-Patches-19361].
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@15930 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_thread.rb
+++ test/ruby/test_thread.rb
@@ -1,6 +1,15 @@
 require 'test/unit'
+require 'thread'
 
 class TestThread < Test::Unit::TestCase
+  class Thread < ::Thread
+    def self.new(*)
+      th = super
+      th.abort_on_exception = true
+      th
+    end
+  end
+
   def test_mutex_synchronize
     m = Mutex.new
     r = 0
@@ -19,6 +28,74 @@ class TestThread < Test::Unit::TestCase
       e.join
     }
     assert_equal(max * max * max, r)
+  end
+
+  def test_condvar
+    mutex = Mutex.new
+    condvar = ConditionVariable.new
+    result = []
+    mutex.synchronize do
+      t = Thread.new do
+        mutex.synchronize do
+          result << 1
+          condvar.signal
+        end
+      end
+
+      result << 0
+      condvar.wait(mutex)
+      result << 2
+      t.join
+    end
+    assert_equal([0, 1, 2], result)
+  end
+
+  def test_condvar_wait_not_owner
+    mutex = Mutex.new
+    condvar = ConditionVariable.new
+
+    assert_raises(ThreadError) { condvar.wait(mutex) }
+  end
+
+  def test_condvar_wait_exception_handling
+    # Calling wait in the only thread running should raise a ThreadError of
+    # 'stopping only thread'
+    mutex = Mutex.new
+    condvar = ConditionVariable.new
+
+    locked = false
+    thread = Thread.new do
+      Thread.current.abort_on_exception = false
+      mutex.synchronize do
+        begin
+          condvar.wait(mutex)
+        rescue Exception
+          locked = mutex.locked?
+          raise
+        end
+      end
+    end
+
+    until thread.stop?
+      sleep(0.1)
+    end
+
+    thread.raise Interrupt, "interrupt a dead condition variable"
+    assert_raises(Interrupt) { thread.value }
+    assert(locked)
+  end
+
+  def test_local_barrier
+    dir = File.dirname(__FILE__)
+    lbtest = File.join(dir, "lbtest.rb")
+    $:.unshift File.join(File.dirname(dir), 'ruby')
+    require 'envutil'
+    $:.shift
+    10.times {
+      result = `#{EnvUtil.rubybin} #{lbtest}`
+      assert(!$?.coredump?, '[ruby-dev:30653]')
+      assert_equal("exit.", result[/.*\Z/], '[ruby-dev:30653]')
+    }
   end
 end
 
