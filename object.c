e103da4e613d82220bcabed60f5b86df3ff288d8
kazu <kazu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-04-22 07:47:11 +0000

    * object.c (rb_obj_tap): Correct documentation; pointed out by   okkez in [ruby-dev:34472].
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@16142 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- object.c
+++ object.c
@@ -502,10 +502,15 @@ rb_obj_is_kind_of(obj, c)
  *  The primary purpose of this method is to "tap into" a method chain,
  *  in order to perform operations on intermediate results within the chain.
  *
- *	(1..10)                .tap {|x| puts "original: #{x.inspect}"}
- *	  .to_a                .tap {|x| puts "array: #{x.inspect}"}
- *	  .select {|x| x%2==0} .tap {|x| puts "evens: #{x.inspect}"}
- *	  .map { |x| x*x }     .tap {|x| puts "squares: #{x.inspect}"}
+ *	(1..10).tap {
+ *	  |x| puts "original: #{x.inspect}"
+ *	}.to_a.tap {
+ *	  |x| puts "array: #{x.inspect}"
+ *	}.select {|x| x%2==0}.tap {
+ *	  |x| puts "evens: #{x.inspect}"
+ *	}.map {|x| x*x}.tap {
+ *	  |x| puts "squares: #{x.inspect}"
+ *	}
  *
  */
 
