279b358d00264f5bb7034142aab51753edfe36de
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-05-08 08:29:24 +0000

    * common.mk, ext/extmk.rb, lib/mkmf.rb: removed RUBYOPT.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@16333 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -1312,7 +1312,7 @@ arch_hdrdir = #{$arch_hdrdir}
 VPATH = #{vpath.join(CONFIG['PATH_SEPARATOR'])}
 }
   if $extmk
-    mk << "RUBYLIB = -\nRUBYOPT = -rpurelib.rb\n"
+    mk << "RUBYLIB = -\nRUBYOPT =\n"
   end
   if destdir = CONFIG["prefix"][$dest_prefix_pattern, 1]
     mk << "\nDESTDIR = #{destdir}\n"
