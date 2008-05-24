796d005a23c5f8acc5dc954afd1ba001809d76c7
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-05-24 01:34:37 +0000

    add a test for [ruby-dev:34808].
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@16565 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/test_delegate.rb
+++ test/test_delegate.rb
@@ -12,4 +12,9 @@ class TestDelegateClass < Test::Unit::TestCase
     obj.extend M
     assert_equal(:m, obj.m, "[ruby-dev:33116]")
   end
+
+  def test_systemcallerror_eq
+    e = SystemCallError.new(0)
+    assert((SimpleDelegator.new(e) == e) == (e == SimpleDelegator.new(e)), "[ruby-dev:34808]")
+  end
 end
