5d53d594f08e23e1efd9cd5dbbd2731bea02a1bf
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-06-07 18:04:08 +0000

    merge revision(s) 13785: 	* numeric.c (fix_pow): returns infinity for 0**-1.  [ruby-dev:32084]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_5@16917 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- numeric.c
+++ numeric.c
@@ -2204,6 +2204,7 @@ static VALUE
 fix_pow(x, y)
     VALUE x, y;
 {
+    static const double zero = 0.0;
     long a = FIX2LONG(x);
 
     if (FIXNUM_P(y)) {
@@ -2213,7 +2214,10 @@ fix_pow(x, y)
 	if (b == 0) return INT2FIX(1);
 	if (b == 1) return x;
 	a = FIX2LONG(x);
-	if (a == 0) return INT2FIX(0);
+	if (a == 0) {
+	    if (b > 0) return INT2FIX(0);
+	    return rb_float_new(1.0 / zero);
+	}
 	if (a == 1) return INT2FIX(1);
 	if (a == -1) {
 	    if (b % 2 == 0)
@@ -2237,7 +2241,9 @@ fix_pow(x, y)
 	x = rb_int2big(FIX2LONG(x));
 	return rb_big_pow(x, y);
       case T_FLOAT:
-	if (a == 0) return rb_float_new(0.0);
+	if (a == 0) {
+	    return rb_float_new(RFLOAT(y)->value < 0 ? (1.0 / zero) : 0.0);
+	}
 	if (a == 1) return rb_float_new(1.0);
 	return rb_float_new(pow((double)a, RFLOAT(y)->value));
       default:
