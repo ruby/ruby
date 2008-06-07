f77e42ad2d01d52d088eb3001af651ec24132357
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-06-07 21:00:39 +0000

    merge revision(s) 14465: 	* lib/date.rb: don't freeze nil even if 1.8 will not be aware of 	  the issue. [ruby-dev:32677]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_6@17002 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/date.rb
+++ lib/date.rb
@@ -6,7 +6,7 @@
 # Documentation: William Webber <william@williamwebber.com>
 #
 #--
-# $Id: date.rb,v 2.30 2006-12-30 21:43:41+09 tadf Exp $
+# $Id: date.rb,v 2.33 2007-12-22 14:41:34+09 tadf Exp $
 #++
 #
 # == Overview
@@ -250,7 +250,7 @@ class Date
   ABBR_DAYNAMES = %w(Sun Mon Tue Wed Thu Fri Sat)
 
   [MONTHNAMES, DAYNAMES, ABBR_MONTHNAMES, ABBR_DAYNAMES].each do |xs|
-    xs.each{|x| x.freeze}.freeze
+    xs.each{|x| x.freeze unless x.nil?}.freeze
   end
 
   class Infinity < Numeric # :nodoc:
