b3ee04c74650f4bc4eec6c31f2a25baad0ea531e
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-06-07 16:32:17 +0000

    merge revision(s) 13694: 	* win32/mkexports.rb: deal with __fastcall name decorations. 	  [ruby-list:44111]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_6@16893 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bcc32/mkexports.rb
+++ bcc32/mkexports.rb
@@ -7,7 +7,7 @@ STDIN.reopen(open("nul"))
 ARGV.each do |obj|
   IO.foreach("|tdump -q -oiPUBDEF -oiPUBD32 #{obj.tr('/', '\\')}") do |l|
     next unless /(?:PUBDEF|PUBD32)/ =~ l
-    SYM[$1] = !$2 if /'(.*?)'\s+Segment:\s+_(TEXT)?/ =~ l
+    SYM[$1] = true if /'(.*?)'/ =~ l
   end
 end
 
@@ -18,10 +18,7 @@ elsif $library
   exports << "Library " + $library
 end
 exports << "Description " + $description.dump if $description
-exports << "EXPORTS"
-SYM.sort.each do |sym, is_data|
-  exports << (is_data ? "#{sym} DATA" : sym)
-end
+exports << "EXPORTS" << SYM.keys.sort
 
 if $output
   open($output, 'w') {|f| f.puts exports.join("\n")}
