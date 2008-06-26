285066dd0bc256fc6b197ac6e55c51b39a574924
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-06-26 21:53:40 +0000

    * configure.in: need a ",". * process.c (rb_fork): check CANNOT_FORK_WITH_PTHREAD macro.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@17593 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- process.c
+++ process.c
@@ -2412,7 +2412,7 @@ rb_fork(int *status, int (*chfunc)(void*), void *charg, VALUE fds)
 static VALUE
 rb_f_fork(VALUE obj)
 {
-#if defined(HAVE_FORK) && !(defined(__NetBSD__) && __NetBSD_Version__ < 400000000)
+#if defined(HAVE_FORK) && !defined(CANNOT_FORK_WITH_PTHREAD)
     rb_pid_t pid;
 
     rb_secure(2);
