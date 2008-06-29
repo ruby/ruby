9ba0dcb904addc2cec449ceab24ccf5f9f9dcb35
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-06-29 08:16:02 +0000

    merge revision(s) 17108: 	* ext/openssl/ossl.h: include winsock.h if USE_WINSOCK2 is not defined. 	  a patch from arton <artonx at yahoo.co.jp> in [ruby-dev:35078]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_7@17656 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/openssl/ossl.h
+++ ext/openssl/ossl.h
@@ -46,7 +46,11 @@ extern "C" {
 
 #if defined(_WIN32)
 #  define OSSL_NO_CONF_API 1
-#  include <winsock2.h>
+#  ifdef USE_WINSOCK2
+#    include <winsock2.h>
+#  else
+#    include <winsock.h>
+#  endif
 #endif
 #include <errno.h>
 #include <openssl/err.h>
