c2034afbabef287d3a84bbb3834002f3312e5c49
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-07-01 06:14:29 +0000

    merge revision(s) 17756: 	* array.c (rb_ary_fill): check if beg is too big.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_7@17761 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- array.c
+++ array.c
@@ -2419,7 +2419,7 @@ rb_ary_fill(argc, argv, ary)
     if (len < 0) {
         return ary;
     }
-    if (len > ARY_MAX_SIZE - beg) {
+    if (beg >= ARY_MAX_SIZE || len > ARY_MAX_SIZE - beg) {
 	rb_raise(rb_eArgError, "argument too big");
     }
     end = beg + len;
