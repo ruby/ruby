f5e45d00eba0807048491cc7d122bd0723ebafe4
mame <mame@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-07-02 16:45:05 +0000

    * regint.h (GET_ALIGNMENT_PAD_SIZE, ALIGNMENT_RIGHT): cast pointer to   uintptr_t instead of unsigned int.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@17826 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- regint.h
+++ regint.h
@@ -262,13 +262,13 @@
 
 #define GET_ALIGNMENT_PAD_SIZE(addr,pad_size) do {\
   (pad_size) = WORD_ALIGNMENT_SIZE \
-               - ((unsigned int )(addr) % WORD_ALIGNMENT_SIZE);\
+               - ((uintptr_t )(addr) % WORD_ALIGNMENT_SIZE);\
   if ((pad_size) == WORD_ALIGNMENT_SIZE) (pad_size) = 0;\
 } while (0)
 
 #define ALIGNMENT_RIGHT(addr) do {\
   (addr) += (WORD_ALIGNMENT_SIZE - 1);\
-  (addr) -= ((unsigned int )(addr) % WORD_ALIGNMENT_SIZE);\
+  (addr) -= ((uintptr_t )(addr) % WORD_ALIGNMENT_SIZE);\
 } while (0)
 
 #endif /* PLATFORM_UNALIGNED_WORD_ACCESS */
