775b845c6de72bafb137401d9144716a7bf1ff78
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-07-07 07:21:51 +0000

    merge revision(s) 17839: 	* lib/net/smtp.rb (Net::SMTP::start): use 'localhost' instead of 	  'localhost.localdomain'.  [ruby-dev:35333] 	* lib/net/smtp.rb (Net::SMTP::SMTP.start): ditto.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_7@17934 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/net/smtp.rb
+++ lib/net/smtp.rb
@@ -437,7 +437,7 @@ module Net
     # +port+ is the port to connect to; it defaults to port 25.
     #
     # +helo+ is the _HELO_ _domain_ provided by the client to the
-    # server (see overview comments); it defaults to 'localhost.localdomain'. 
+    # server (see overview comments); it defaults to 'localhost'. 
     #
     # The remaining arguments are used for SMTP authentication, if required
     # or desired.  +user+ is the account name; +secret+ is your password
@@ -457,7 +457,7 @@ module Net
     # * IOError
     # * TimeoutError
     #
-    def SMTP.start(address, port = nil, helo = 'localhost.localdomain',
+    def SMTP.start(address, port = nil, helo = 'localhost',
                    user = nil, secret = nil, authtype = nil,
                    &block)   # :yield: smtp
       new(address, port).start(helo, user, secret, authtype, &block)
@@ -518,7 +518,7 @@ module Net
     # * IOError
     # * TimeoutError
     #
-    def start(helo = 'localhost.localdomain',
+    def start(helo = 'localhost',
               user = nil, secret = nil, authtype = nil)   # :yield: smtp
       if block_given?
         begin
