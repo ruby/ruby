dfd0d6a5ca865d589f943f57c29f2a6a6e2260d6
mame <mame@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-07-11 14:06:19 +0000

    * test/zlib/test_zlib.rb: add a test for Zlib::Deflate#params.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@18030 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/zlib/test_zlib.rb
+++ test/zlib/test_zlib.rb
@@ -120,6 +120,14 @@ if defined? Zlib
       s = z.finish
       assert_equal("foobar", Zlib::Inflate.inflate(s))
 
+      data = ('a'..'z').to_a.join
+      z = Zlib::Deflate.new(Zlib::NO_COMPRESSION, Zlib::MAX_WBITS,
+                            Zlib::DEF_MEM_LEVEL, Zlib::DEFAULT_STRATEGY)
+      z << data[0, 10]
+      z.params(Zlib::BEST_COMPRESSION, Zlib::DEFAULT_STRATEGY)
+      z << data[10 .. -1]
+      assert_equal(data, Zlib::Inflate.inflate(z.finish))
+
       z = Zlib::Deflate.new
       s = z.deflate("foo", Zlib::FULL_FLUSH)
       z.avail_out = 0
