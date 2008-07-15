25498d24c808346a90a55ca967138083402bc160
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-07-15 09:14:09 +0000

    * thread_win32.c (ubf_handle): cancel blocking IO if it can (only 	  Vista). see [ruby-dev:35446]
    	* win32/win32.c (errmap): add ERROR_OPERATION_ABORTED as EINTR.
    
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@18068 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- thread_win32.c
+++ thread_win32.c
@@ -532,8 +532,19 @@ native_thread_apply_priority(rb_thread_t *th)
 static void
 ubf_handle(void *ptr)
 {
+    static int checked = 0;
+    typedef BOOL (WINAPI *cancel_io_func_t)(HANDLE);
+    static cancel_io_func_t cancel_func = NULL;
     rb_thread_t *th = (rb_thread_t *)ptr;
     thread_debug("ubf_handle: %p\n", th);
+
+    if (!checked) {
+	cancel_func = (cancel_io_func_t)GetProcAddress(GetModuleHandle("kernel32"), "CancelSynchronousIo");
+	checked = 1;
+    }
+    if (cancel_func)
+	cancel_func((HANDLE)th->thread_id);
+
     w32_set_event(th->native_thread_data.interrupt_event);
 }
 
