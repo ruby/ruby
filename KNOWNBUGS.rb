7eb2e99e70b2ee8c46ff7c4a0be76a3afb83694f
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-07-15 12:50:24 +0000

    Add a comment.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@18078 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- KNOWNBUGS.rb
+++ KNOWNBUGS.rb
@@ -10,6 +10,8 @@ class TC_KnownBugs < Test::Unit::TestCase
   end
 
   def test_block_arg1
+    # &b wrongly captures the upper block such as the one given to
+    # this method, if no block is given on yield.
     just_yield {|&b|
       assert_equal(nil, b)
     }
