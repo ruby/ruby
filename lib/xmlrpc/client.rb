c9d0983a8107f2598648b72ba9eff1805c2f6fa5
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-07-16 17:07:44 +0000

    merge revision(s) 18036: 	* lib/xmlrpc/client.rb (XMLRPC::Client#do_rpc): requires 	  webrick/cookie.  [ ruby-Bugs-21139 ]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_7@18091 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/xmlrpc/client.rb
+++ lib/xmlrpc/client.rb
@@ -567,6 +567,7 @@ module XMLRPC
 
       set_cookies = resp.get_fields("Set-Cookie")
       if set_cookies and !set_cookies.empty?
+        require 'webrick/cookie'
         @cookie = set_cookies.collect do |set_cookie|
           cookie = WEBrick::Cookie.parse_set_cookie(set_cookie)
           WEBrick::Cookie.new(cookie.name, cookie.value).to_s
