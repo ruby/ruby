a0895f84b06b33cebac976214c807a4dbf483e92
mame <mame@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-07-23 14:25:32 +0000

    * test/socket/test_tcp.rb (test_recvfrom): replace an irrelevant test   for old behavior.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@18191 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/socket/test_tcp.rb
+++ test/socket/test_tcp.rb
@@ -8,22 +8,17 @@ end
 class TestTCPSocket < Test::Unit::TestCase
   def test_recvfrom
 assert false, "TODO: doesn't work on mswin32/64" if /mswin/ =~ RUBY_PLATFORM
-    c = s = nil
     svr = TCPServer.new("localhost", 0)
     th = Thread.new {
       c = svr.accept
-      Thread.pass
-      ObjectSpace.each_object(String) {|s|
-        s.replace "a" if s.length == 0x10000 and !s.frozen?
-      }
-      c.print("x"*0x1000)
+      c.write "foo"
+      c.close
     }
     addr = svr.addr
     sock = TCPSocket.open(addr[2], addr[1])
-    assert_raise(RuntimeError, SocketError, "[ruby-dev:24705]") {
-      sock.recvfrom(0x10000)
-    }
+    assert_equal(["foo", nil], sock.recvfrom(0x10000))
   ensure
+    th.kill
     th.join
   end
 end if defined?(TCPSocket)
