39a7b1590d789ad6120028df71bdb858581d12e7
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-08-04 05:18:09 +0000

    merge revision(s) 18220: 	* lib/webrick/httputils.rb (WEBrick::HTTPUtils#split_header_value): 	  reduce backtrack.  based on a fix by Christian Neukirchen 	  <chneukirchen AT gmail.com>.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_6@18347 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/webrick/httputils.rb
+++ lib/webrick/httputils.rb
@@ -23,16 +23,8 @@ module WEBrick
       ret = path.dup
 
       ret.gsub!(%r{/+}o, '/')                    # //      => /
-      while ret.sub!(%r{/\.(/|\Z)}o, '/'); end   # /.      => /
-      begin                                      # /foo/.. => /foo
-        match = ret.sub!(%r{/([^/]+)/\.\.(/|\Z)}o){
-          if $1 == ".."
-            raise "abnormal path `#{path}'"
-          else
-            "/"
-          end
-        }
-      end while match
+      while ret.sub!(%r'/\.(?:/|\Z)', '/'); end  # /.      => /
+      while ret.sub!(%r'/(?!\.\./)[^/]+/\.\.(?:/|\Z)', '/'); end # /foo/.. => /foo
 
       raise "abnormal path `#{path}'" if %r{/\.\.(/|\Z)} =~ ret
       ret
@@ -154,8 +146,8 @@ module WEBrick
     module_function :parse_header
 
     def split_header_value(str)
-      str.scan(/((?:"(?:\\.|[^"])+?"|[^",]+)+)
-                (?:,\s*|\Z)/xn).collect{|v| v[0] }
+      str.scan(%r'\G((?:"(?:\\.|[^"])+?"|[^",]+)+)
+                    (?:,\s*|\Z)'xn).flatten
     end
     module_function :split_header_value
 
