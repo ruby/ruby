420e61cf0ba7712026a48092fa215b5c035442a7
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-08-06 20:17:55 +0000

    * regenc.c (code_to_mbc): raise RangeError Integer#chr   when more than 255 is given with single byte encoding.   [ruby-dev:35789]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@18399 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- regenc.c
+++ regenc.c
@@ -629,6 +629,8 @@ onigenc_single_byte_code_to_mbclen(OnigCodePoint code ARG_UNUSED, OnigEncoding e
 extern int
 onigenc_single_byte_code_to_mbc(OnigCodePoint code, UChar *buf, OnigEncoding enc ARG_UNUSED)
 {
+  if (code > 0xff)
+      rb_raise(rb_eRangeError, "%"PRIdVALUE " out of char range", code);
   *buf = (UChar )(code & 0xff);
   return 1;
 }
