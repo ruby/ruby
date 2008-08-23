3721df734c9ad1be0bbd64321329c1af405b289b
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-08-23 01:49:56 +0000

    * ext/pty/pty.c (pty_getpty): follow rb_io_t's path -> pathv change.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@18785 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/pty/pty.c
+++ ext/pty/pty.c
@@ -415,11 +415,11 @@ pty_getpty(int argc, VALUE *argv, VALUE self)
 
     rfptr->mode = rb_io_mode_flags("r");
     rfptr->fd = info.fd;
-    rfptr->path = strdup(SlaveName);
+    rfptr->pathv = rb_obj_freeze(rb_str_new_cstr(SlaveName));
 
     wfptr->mode = rb_io_mode_flags("w") | FMODE_SYNC;
     wfptr->fd = dup(info.fd);
-    wfptr->path = strdup(SlaveName);
+    wfptr->pathv = rfptr->pathv;
 
     res = rb_ary_new2(3);
     rb_ary_store(res,0,(VALUE)rport);
