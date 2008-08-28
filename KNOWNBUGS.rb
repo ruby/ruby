503d852995a9d4df10e387d653a1128a3476a211
mame <mame@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-08-28 14:29:31 +0000

    * KNOWNBUGS.rb: add a test.  see [ruby-dev:36028]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@18898 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- KNOWNBUGS.rb
+++ KNOWNBUGS.rb
@@ -2,3 +2,14 @@
 # This test file concludes tests which point out known bugs.
 # So all tests will cause failure.
 #
+
+assert_equal 'ok', %q{
+  class Foo
+    define_method(:foo) do |&b|
+      b.call
+    end
+  end
+  Foo.new.foo do
+    break :ok
+  end
+}, '[ruby-dev:36028]'
