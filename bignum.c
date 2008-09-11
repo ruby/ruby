63471c7c3279ba5b152b170afcce5092ea62dda6
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-09-11 06:30:56 +0000

    * bignum.c (bigdivrem): adjust length for division and remainder.   a patch from TOYOFUKU Chikanobu <nobu_toyofuku at nifty.com> in   [ruby-dev:36231].
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@19293 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bignum.c
+++ bignum.c
@@ -1749,6 +1749,7 @@ bigdivrem(VALUE x, VALUE y, VALUE *divp, VALUE *modp)
 	zds = BDIGITS(*divp);
 	j = (nx==ny ? nx+2 : nx+1) - ny;
 	for (i = 0;i < j;i++) zds[i] = zds[i+ny];
+	if (!zds[i-1]) i--;
 	RBIGNUM_SET_LEN(*divp, i);
     }
     if (modp) {			/* normalize remainder */
@@ -1764,6 +1765,7 @@ bigdivrem(VALUE x, VALUE y, VALUE *divp, VALUE *modp)
 		t2 = BIGUP(q);
 	    }
 	}
+	if (!zds[ny-1]) ny--;
 	RBIGNUM_SET_LEN(*modp, ny);
 	RBIGNUM_SET_SIGN(*modp, RBIGNUM_SIGN(x));
     }
