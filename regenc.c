2af2eb28da63f4a3f5c0877f7bc9eb7cedf5db12
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-09-12 11:30:14 +0000

    * regenc.c (onigenc_single_byte_code_to_mbc): follow OnigEncoding   change.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@19300 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- regenc.c
+++ regenc.c
@@ -630,7 +630,7 @@ extern int
 onigenc_single_byte_code_to_mbc(OnigCodePoint code, UChar *buf, OnigEncoding enc ARG_UNUSED)
 {
   if (code > 0xff)
-      rb_raise(rb_eRangeError, "%"PRIdVALUE " out of char range", code);
+      rb_raise(rb_eRangeError, "%u out of char range", code);
   *buf = (UChar )(code & 0xff);
   return 1;
 }
