48aad600bab75a6b5866c9a46b5828351cc6878a
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-09-19 23:05:52 +0000

    * common.mk: prec.c is removed.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@19432 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- common.mk
+++ common.mk
@@ -519,7 +519,6 @@ parse.$(OBJEXT): {$(VPATH)}parse.c {$(VPATH)}parse.y $(RUBY_H_INCLUDES) \
   {$(VPATH)}oniguruma.h $(ID_H_INCLUDES) {$(VPATH)}regenc.h \
   {$(VPATH)}regex.h {$(VPATH)}util.h {$(VPATH)}lex.c {$(VPATH)}keywords \
   {$(VPATH)}debug.h {$(VPATH)}id.c
-prec.$(OBJEXT): {$(VPATH)}prec.c $(RUBY_H_INCLUDES) {$(VPATH)}st.h
 proc.$(OBJEXT): {$(VPATH)}proc.c {$(VPATH)}eval_intern.h \
   $(RUBY_H_INCLUDES) {$(VPATH)}st.h {$(VPATH)}node.h {$(VPATH)}util.h \
   {$(VPATH)}vm_core.h {$(VPATH)}debug.h \
