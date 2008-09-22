260c0e6a2fd3abd90e825034b736e4719c1e8621
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-09-22 02:47:05 +0000

    * include/ruby/encoding.h (enum): ISO C forbids comma at end of 	  enumerator list
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@19453 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/encoding.h
+++ include/ruby/encoding.h
@@ -203,7 +203,7 @@ typedef enum {
     econv_source_buffer_empty,
     econv_finished,
     econv_after_output,
-    econv_incomplete_input,
+    econv_incomplete_input
 } rb_econv_result_t;
 
 typedef struct rb_econv_t rb_econv_t;
