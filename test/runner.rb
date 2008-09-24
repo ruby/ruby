1db825a0477295ec6c8f8deba8334a58fc152abc
ryan <ryan@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-09-24 08:19:12 +0000

    Added ARGV filtering for tests. Allows for multiple values
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@19520 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/runner.rb
+++ test/runner.rb
@@ -6,15 +6,22 @@ rcsid = %w$Id$
 Version = rcsid[2].scan(/\d+/).collect!(&method(:Integer)).freeze
 Release = rcsid[3].freeze
 
-# not sure why these are needed now... but whatever
-$:.push(*Dir["test/*"].find_all { |path| File.directory? path })
-
 class Module
   def tu_deprecation_warning old, new = nil, kaller = nil
     # stfu - for now...
   end
 end
 
-(Dir["test/test_*.rb"] + Dir["test/**/test_*.rb"]).each do |test|
+test_dir = File.dirname(__FILE__)
+
+# not sure why these are needed now... but whatever
+$:.push(*Dir[File.join(test_dir, '*')].find_all { |path| File.directory? path })
+
+test_files = (Dir[File.join(test_dir, "test_*.rb")] +
+              Dir[File.join(test_dir, "**/test_*.rb")])
+
+test_files = test_files.grep(Regexp.union(*ARGV)) unless ARGV.empty?
+
+test_files.each do |test|
   require test
 end
