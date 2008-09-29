ebb3ace8259e280670a2d8415b61e819f246220f
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-09-29 11:56:43 +0000

    * string.c (rb_str_subseq): use rb_str_drop_bytes if possible.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@19624 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- string.c
+++ string.c
@@ -1283,7 +1283,15 @@ rb_str_sublen(VALUE str, long pos)
 VALUE
 rb_str_subseq(VALUE str, long beg, long len)
 {
-    VALUE str2 = rb_str_new5(str, RSTRING_PTR(str)+beg, len);
+    VALUE str2;
+
+    if (RSTRING_LEN(str) == beg + len) {
+        str2 = rb_str_new_shared(str);
+        rb_str_drop_bytes(str2, beg);
+    }
+    else {
+        str2 = rb_str_new5(str, RSTRING_PTR(str)+beg, len);
+    }
 
     rb_enc_cr_str_copy_for_substr(str2, str);
     OBJ_INFECT(str2, str);
