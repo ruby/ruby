5a7c5d34fbea7263c83f8136e409b559ca4007aa
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-10-05 02:39:29 +0000

    removed unsupported platforms from an ifdef macro. These lines were mistakenly removed at r19681 and recovered too much at r19684.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@19685 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- eval.c
+++ eval.c
@@ -31,7 +31,7 @@ VALUE rb_eSysStackError;
 
 #if defined(__APPLE__)
 #define environ (*_NSGetEnviron())
-#elif !defined(_WIN32) && !defined(__MACOS__) || defined(_WIN32_WCE)
+#elif !defined(_WIN32)
 extern char **environ;
 #endif
 char **rb_origenviron;
