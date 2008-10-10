fb93e65c760752fd2dbd6456500adb4b9a9c6f5e
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-10-10 13:07:07 +0000

    * include/ruby.h: updated macros for headers.  [ruby-core:19275]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@19748 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby.h
+++ include/ruby.h
@@ -18,14 +18,13 @@
 #define HAVE_RUBY_IO_H          1
 #define HAVE_RUBY_MISSING_H     1
 #define HAVE_RUBY_VM_H          1
-#define HAVE_RUBY_NODE_H        1
 #define HAVE_RUBY_ONIGURUMA_H   1
 #define HAVE_RUBY_RE_H          1
 #define HAVE_RUBY_REGEX_H       1
 #define HAVE_RUBY_RUBY_H        1
-#define HAVE_RUBY_SIGNAL_H      1
 #define HAVE_RUBY_ST_H          1
 #define HAVE_RUBY_UTIL_H        1
+#define HAVE_RUBY_VM_H          1
 #ifdef _WIN32
 #define HAVE_RUBY_WIN32_H       1
 #endif
