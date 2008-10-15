7370c38456957bd19197064199d622bc2dcbf877
ryan <ryan@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-10-15 04:16:28 +0000

    Disable minitest autorunner and run manually in order to get test/unit fork/signal tests to not trigger minitest multiple times
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@19789 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/runner.rb
+++ test/runner.rb
@@ -6,4 +6,14 @@ rcsid = %w$Id$
 Version = rcsid[2].scan(/\d+/).collect!(&method(:Integer)).freeze
 Release = rcsid[3].freeze
 
-exit Test::Unit::AutoRunner.run(true, File.dirname($0))
+# this allows minitest and test/unit to run side by side.  test/unit
+# tests with fork/signal were triggering minitest multiple times.
+require 'minitest/unit'
+MiniTest::Unit.disable_autorun
+
+args = ARGV.dup
+result = Test::Unit::AutoRunner.run(true, File.dirname($0))
+result &&= MiniTest::Unit.new.run(args)
+
+exit result
+
