0ebf9b176380e691d3505e6c96cb0e33c6d892e4
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-10-16 13:55:09 +0000

    * lib/test/unit: removed test/unit.
    * lib/test/unit.rb: new compatibility layer using minitest.
    
    * bin/testrb: use above.
    
    * test/runner.rb: ditto.
    
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@19800 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/runner.rb
+++ test/runner.rb
@@ -1,19 +1,5 @@
 require 'rbconfig'
 exit if CROSS_COMPILING
+
 require 'test/unit'
-
-rcsid = %w$Id$
-Version = rcsid[2].scan(/\d+/).collect!(&method(:Integer)).freeze
-Release = rcsid[3].freeze
-
-# this allows minitest and test/unit to run side by side.  test/unit
-# tests with fork/signal were triggering minitest multiple times.
-require 'minitest/unit'
-MiniTest::Unit.disable_autorun
-
-args = ARGV.dup
-result = Test::Unit::AutoRunner.run(true, File.dirname($0))
-result &&= MiniTest::Unit.new.run(args)
-
-exit result
-
+Test::Unit.setup_argv
