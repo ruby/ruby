679d83e6f2ae7ee7fb6ca2878fe8e97fd80ff183
xibbar <xibbar@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-10-18 14:30:47 +0000

    add test for encoding option.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@19839 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/test_tempfile.rb
+++ test/test_tempfile.rb
@@ -10,5 +10,26 @@ class TestTempfile < Test::Unit::TestCase
     o.extend M
     assert(M === o, "[ruby-dev:32932]")
   end
+  def test_tempfile_encoding_nooption
+    default_external=Encoding.default_external
+    t=Tempfile.new("TEST")
+    t.write("\xE6\x9D\xBE\xE6\xB1\x9F")
+    t.rewind
+    assert_equal(default_external,t.read.encoding)
+  end
+  def test_tempfile_encoding_ascii8bit
+    default_external=Encoding.default_external
+    t=Tempfile.new("TEST",:encoding=>"ascii-8bit")
+    t.write("\xE6\x9D\xBE\xE6\xB1\x9F")
+    t.rewind
+    assert_equal(Encoding::ASCII_8BIT,t.read.encoding)
+  end
+  def test_tempfile_encoding_ascii8bit2
+    default_external=Encoding.default_external
+    t=Tempfile.new("TEST",Dir::tmpdir,:encoding=>"ascii-8bit")
+    t.write("\xE6\x9D\xBE\xE6\xB1\x9F")
+    t.rewind
+    assert_equal(Encoding::ASCII_8BIT,t.read.encoding)
+  end
 end
 
