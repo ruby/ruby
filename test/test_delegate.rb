54e2cb81799f0ff7e443758b50e906ea5b1833d3
xibbar <xibbar@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-10-18 15:18:49 +0000

    add test for {SimpleDelegator, DelegateClass}#class .
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@19842 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/test_delegate.rb
+++ test/test_delegate.rb
@@ -17,4 +17,16 @@ class TestDelegateClass < Test::Unit::TestCase
     e = SystemCallError.new(0)
     assert((SimpleDelegator.new(e) == e) == (e == SimpleDelegator.new(e)), "[ruby-dev:34808]")
   end
+
+  class Myclass < DelegateClass(Array);end
+
+  def test_delegateclass_class
+    myclass=Myclass.new([])
+    assert_equal(Myclass,myclass.class)
+  end
+
+  def test_simpledelegator_class
+    simple=SimpleDelegator.new([])
+    assert_equal(SimpleDelegator,simple.class)
+  end
 end
