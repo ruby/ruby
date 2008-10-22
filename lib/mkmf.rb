41de0174a325067153802cf6318feb8918065486
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-10-22 16:24:58 +0000

    * lib/mkmf.rb (create_tmpsrc): get rid of side effects.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@19891 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -294,9 +294,11 @@ EOM
 end
 
 def create_tmpsrc(src)
+  src = "#{COMMON_HEADERS}\n#{src}"
   src = yield(src) if block_given?
-  src[0, 0] = COMMON_HEADERS + "\n"
-  src = src.gsub(/[ \t]+$/, '').gsub(/\A\n+|^\n+$/, '').sub(/[^\n]\z/, "\\&\n")
+  src.gsub!(/[ \t]+$/, '')
+  src.gsub!(/\A\n+|^\n+$/, '')
+  src.sub!(/[^\n]\z/, "\\&\n")
   open(CONFTEST_C, "wb") do |cfile|
     cfile.print src
   end
