405b5272766ae305c0e54267923c6950a739c262
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-11-25 14:29:39 +0000

    * array.c (rb_ary_times): taint (and untrust) status should be   inherited by "ary * 0".  [ruby-dev:37024]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@20356 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- array.c
+++ array.c
@@ -2551,7 +2551,10 @@ rb_ary_times(VALUE ary, VALUE times)
     }
 
     len = NUM2LONG(times);
-    if (len == 0) return ary_new(rb_obj_class(ary), 0);
+    if (len == 0) {
+	ary2 = ary_new(rb_obj_class(ary), 0);
+	goto out;
+    }
     if (len < 0) {
 	rb_raise(rb_eArgError, "negative argument");
     }
@@ -2566,6 +2569,7 @@ rb_ary_times(VALUE ary, VALUE times)
     for (i=0; i<len; i+=RARRAY_LEN(ary)) {
 	MEMCPY(RARRAY_PTR(ary2)+i, RARRAY_PTR(ary), VALUE, RARRAY_LEN(ary));
     }
+  out:
     OBJ_INFECT(ary2, ary);
 
     return ary2;
