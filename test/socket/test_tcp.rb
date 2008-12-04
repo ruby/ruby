a7f0a9a4bca42dcb4bdebef75ca1ee257cc2c044
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-12-04 05:19:15 +0000

    merges r20440 from trunk into ruby_1_9_1. * test/socket/test_tcp.rb (test_recvfrom, test_encoding): shouldn't assume   that th is not nil.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_1@20483 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/socket/test_tcp.rb
+++ test/socket/test_tcp.rb
@@ -18,8 +18,8 @@ assert false, "TODO: doesn't work on mswin32/64" if /mswin/ =~ RUBY_PLATFORM
     sock = TCPSocket.open(addr[2], addr[1])
     assert_equal(["foo", nil], sock.recvfrom(0x10000))
   ensure
-    th.kill
-    th.join
+    th.kill if th
+    th.join if th
   end
 
   def test_encoding
@@ -36,8 +36,8 @@ assert false, "TODO: doesn't work on mswin32/64" if /mswin/ =~ RUBY_PLATFORM
     assert_equal("foo\r\n", s)
     assert_equal(Encoding.find("ASCII-8BIT"), s.encoding)
   ensure
-    th.kill
-    th.join
+    th.kill if th
+    th.join if th
     sock.close if sock
   end
 end if defined?(TCPSocket)
