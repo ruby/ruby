d8d41b2380e897fc9660f0ecb2b0a0497528ff01
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-12-16 13:03:49 +0000

    * lib/test/unit.rb (Test::Unit.setup_argv): ALT_SEPARATOR support.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@20796 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/test/unit.rb
+++ lib/test/unit.rb
@@ -32,6 +32,7 @@ module Test
       end
 
       files.map! {|f|
+        f.gsub!(/#{File::ALT_SEPARATOR}/o, File::SEPARATOR) if File::ALT_SEPARATOR
         if File.directory? f
           Dir["#{f}/**/test_*.rb"]
         elsif File.file? f
