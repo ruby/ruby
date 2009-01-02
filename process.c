f408202de2f417e6cb44089fcb2f919bff2253dc
mame <mame@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-01-02 13:25:09 +0000

    * process.c: use sigaction instead of sigset.  [ruby-core:21021]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@21275 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- process.c
+++ process.c
@@ -2736,12 +2736,8 @@ rb_f_abort(int argc, VALUE *argv)
 }
 
 
-#if defined(sun)
-#define signal(a,b) sigset(a,b)
-#else
-# if defined(POSIX_SIGNAL)
-#  define signal(a,b) posix_signal(a,b)
-# endif
+#if defined(POSIX_SIGNAL)
+# define signal(a,b) posix_signal(a,b)
 #endif
 
 void
