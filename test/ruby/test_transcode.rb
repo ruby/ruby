770d89c5fa39e6dff431c0fbc44e68db91440aae
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-01-22 11:59:26 +0000

    Recover default_default_internal.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@21741 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_transcode.rb
+++ test/ruby/test_transcode.rb
@@ -36,6 +36,7 @@ class TestTranscode < Test::Unit::TestCase
     assert_equal("\u3042".encode, "\xA4\xA2".force_encoding('EUC-JP'))
     assert_equal("\xE3\x81\x82\x81".force_encoding("utf-8").encode,
                  "\xA4\xA2?".force_encoding('EUC-JP'))
+    Encoding.default_internal = default_default_internal
   end
 
   def test_length
