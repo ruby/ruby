21675bb6a70e927e54f6adb92e105773d08459e7
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-02-02 08:08:52 +0000

    reduce test time.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@21961 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_objectspace.rb
+++ bootstraptest/test_objectspace.rb
@@ -2,7 +2,8 @@ assert_normal_exit %q{
   eval("", TOPLEVEL_BINDING)
   minobj = ObjectSpace.to_enum(:each_object).min_by {|a| a.object_id }
   maxobj = ObjectSpace.to_enum(:each_object).max_by {|a| a.object_id }
-  minobj.object_id.upto(maxobj.object_id) {|id|
+  (((minobj.object_id-100)..(minobj.object_id+100))+
+   ((maxobj.object_id-100)..(maxobj.object_id+100))).each {|id|
     begin
       o = ObjectSpace._id2ref(id)
     rescue RangeError
