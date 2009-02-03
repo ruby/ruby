12a5cf8287f1d2e33f94385ca8a227d9b1cea689
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-02-03 16:40:42 +0000

    revert useless change part in the previous commit.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@22023 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/socket/extconf.rb
+++ ext/socket/extconf.rb
@@ -301,11 +301,11 @@ have_header("sys/un.h")
 have_header("sys/uio.h")
 have_type("struct in_pktinfo", headers) {|src|
   src.sub(%r'^/\*top\*/', '\1'"\n#if defined(IPPROTO_IP) && defined(IP_PKTINFO)") <<
-  "#else\n" << "#error no in_pktinfo\n" << "#endif\n"
+  "#else\n" << "#error\n" << ">>>>>> no in_pktinfo <<<<<<\n" << "#endif\n"
 }
 have_type("struct in6_pktinfo", headers) {|src|
   src.sub(%r'^/\*top\*/', '\1'"\n#if defined(IPPROTO_IPV6) && defined(IPV6_PKTINFO)") <<
-  "#else\n" << "#error no in6_pktinfo\n" << "#endif\n"
+  "#else\n" << "#error\n" << ">>>>>> no in6_pktinfo <<<<<<\n" << "#endif\n"
 }
 
 $distcleanfiles << "constants.h" << "constdefs.*"
