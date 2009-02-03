6249e5f7742f88c7716330b0856da81cd6da74d9
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-02-03 05:35:56 +0000

    merge revision(s) 19774: 	* lib/net/pop.rb: check for invalid APOP timestamp. (CVE-2007-1558) 	  [ruby-dev:36631] 	* test/net/pop/test_pop.rb: ditto.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_7@22002 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/net/pop.rb
+++ lib/net/pop.rb
@@ -871,7 +871,7 @@ module Net
       @socket = sock
       @error_occured = false
       res = check_response(critical { recv_response() })
-      @apop_stamp = res.slice(/<.+>/)
+      @apop_stamp = res.slice(/<[!-~]+@[!-~]+>/)
     end
 
     def inspect
