6e6676a2234ec190aaea02efaa31949388c02622
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-02-11 04:31:19 +0000

    add tests.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@22227 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/socket/test_sockopt.rb
+++ test/socket/test_sockopt.rb
@@ -2,6 +2,16 @@ require 'test/unit'
 require 'socket'
 
 class TestSocketOption < Test::Unit::TestCase
+  def test_new
+    data = [1].pack("i")
+    sockopt = Socket::Option.new(:INET, :SOCKET, :KEEPALIVE, data)
+    assert_equal(Socket::AF_INET, sockopt.family)
+    assert_equal(Socket::SOL_SOCKET, sockopt.level)
+    assert_equal(Socket::SO_KEEPALIVE, sockopt.optname)
+    assert_equal(Socket::SO_KEEPALIVE, sockopt.optname)
+    assert_equal(data, sockopt.data)
+  end
+
   def test_bool
     opt = Socket::Option.bool(:INET, :SOCKET, :KEEPALIVE, true)
     assert_equal(1, opt.int)
@@ -13,5 +23,11 @@ class TestSocketOption < Test::Unit::TestCase
     assert_equal(true, opt.bool)
     opt = Socket::Option.int(:INET, :SOCKET, :KEEPALIVE, 2)
     assert_equal(true, opt.bool)
+  end
+
+  def test_unpack
+    sockopt = Socket::Option.new(:INET, :SOCKET, :KEEPALIVE, [1].pack("i"))
+    assert_equal([1], sockopt.unpack("i"))
+    assert_equal([1], sockopt.data.unpack("i"))
   end
 end
