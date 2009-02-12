3abae536a6c97378e97f15b53238bf6dcd25ab2c
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-02-12 14:26:02 +0000

    * process.c (rb_fork): initialization with dynamic values is not   allowed in C89.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@22261 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- process.c
+++ process.c
@@ -2546,7 +2546,9 @@ rb_pid_t
 rb_fork(int *status, int (*chfunc)(void*), void *charg, VALUE fds)
 {
     if (chfunc) {
-        struct chfunc_wrapper_t warg = { chfunc, charg };
+        struct chfunc_wrapper_t warg;
+        warg.chfunc = chfunc;
+        warg.arg = charg;
         return rb_fork_err(status, chfunc_wrapper, &warg, fds, NULL, 0);
     }
     else {
