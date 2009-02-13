28eb7acf2c8c8b883090617599ec3439dc55e8e9
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-02-13 22:31:46 +0000

    * string.c (rb_external_str_new_with_enc): change evaluatioin order   for speed.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@22297 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- string.c
+++ string.c
@@ -538,8 +538,8 @@ rb_external_str_new_with_enc(const char *ptr, long len, rb_encoding *eenc)
     VALUE str;
 
     str = rb_tainted_str_new(ptr, len);
-    if (rb_enc_str_coderange(str) != ENC_CODERANGE_7BIT &&
-	eenc == rb_usascii_encoding()) {
+    if (eenc == rb_usascii_encoding() &&
+	rb_enc_str_coderange(str) != ENC_CODERANGE_7BIT) {
 	rb_enc_associate(str, rb_ascii8bit_encoding());
 	return str;
     }
