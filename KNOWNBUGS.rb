183582ab29b5e81fc4a090eb69102857210e2824
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-02-14 22:04:21 +0000

    add a test.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@22319 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- KNOWNBUGS.rb
+++ KNOWNBUGS.rb
@@ -2,3 +2,8 @@
 # This test file concludes tests which point out known bugs.
 # So all tests will cause failure.
 #
+
+assert_normal_exit(<<'End', '[ruby-dev:37934]')
+  Process.setrlimit(:NPROC, 1)
+  system("ls")
+End
