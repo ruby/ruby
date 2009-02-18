e5edfbf2074d2c99c7c0128688367c28fd911a26
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-02-18 01:49:17 +0000

    * bootstraptest/test_fork.rb: test for [ruby-core:22158]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@22390 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_fork.rb
+++ bootstraptest/test_fork.rb
@@ -8,3 +8,12 @@ assert_equal '0', %q{
     0
   end
 }, '[ruby-dev:32404]'
+
+assert_finish 10, %q{
+  children = (1..10).map{
+    Thread.start{fork{}}.value
+  }
+  while !children.empty? and pid = Process.wait
+    children.delete(pid)
+  end
+}, '[ruby-core:22158]'
