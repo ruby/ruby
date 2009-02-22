9a62ed5603148293d1bcbda38b49425a5a3c098f
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-02-22 09:47:05 +0000

    gets rid of ^D^D to quit script from stdin. merges r22017 from trunk into ruby_1_9_1. * ruby.c (load_file_internal): resets EOF flag after parse.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_1@22508 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ruby.c
+++ ruby.c
@@ -1552,7 +1552,6 @@ load_file_internal(VALUE arg)
 	    rb_io_ungetbyte(f, c);
 	}
 	require_libraries(opt);	/* Why here? unnatural */
-	rb_io_ungetbyte(f, Qnil);
     }
     if (opt->src.enc.index >= 0) {
 	enc = rb_enc_from_index(opt->src.enc.index);
@@ -1571,6 +1570,9 @@ load_file_internal(VALUE arg)
     }
     else if (f != rb_stdin) {
 	rb_io_close(f);
+    }
+    else {
+	rb_io_ungetbyte(f, Qnil);
     }
     return (VALUE)tree;
 }
