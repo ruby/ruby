b80b04fef1e7ea497897b9827f16ad39ba6a01ec
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-02-22 09:48:42 +0000

    merges r22130 from trunk into ruby_1_9_1. * include/ruby/backward/rubysig.h (rb_thread_blocking_region_begin),   (rb_thread_blocking_region_end): marked as deprecated.
    * include/ruby/backward/rubysig.h (TRAP_BEG): fix for C++.  a
      patch from Aman Gupta at [ruby-core:21934]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_1@22521 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/backward/rubysig.h
+++ include/ruby/backward/rubysig.h
@@ -28,9 +28,9 @@ extern "C" {
 #endif
 
 struct rb_blocking_region_buffer;
-RUBY_EXTERN struct rb_blocking_region_buffer *rb_thread_blocking_region_begin(void);
-RUBY_EXTERN void rb_thread_blocking_region_end(struct rb_blocking_region_buffer *);
-#define TRAP_BEG do {void *__region = rb_thread_blocking_region_begin(void);
+DEPRECATED(RUBY_EXTERN struct rb_blocking_region_buffer *rb_thread_blocking_region_begin(void));
+DEPRECATED(RUBY_EXTERN void rb_thread_blocking_region_end(struct rb_blocking_region_buffer *));
+#define TRAP_BEG do {struct rb_blocking_region_buffer *__region = rb_thread_blocking_region_begin();
 #define TRAP_END rb_thread_blocking_region_end(__region);} while (0)
 #define RUBY_CRITICAL(statements) do {statements;} while (0)
 #define DEFER_INTS (0)
