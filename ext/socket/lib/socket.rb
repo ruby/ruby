54cd7ea16834cac420067b7ea8bee3b619f082c0
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-02-28 06:05:44 +0000

    * ext/socket/lib/socket.rb (BasicSocket#connect_address): returns ::1   if the address is IPv4-mapped IPv6 address for 0.0.0.0 for MacOS X.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@22672 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/socket/lib/socket.rb
+++ ext/socket/lib/socket.rb
@@ -190,6 +190,8 @@ class BasicSocket
       raise SocketError, "unbound IPv6 socket" if addr.ip_port == 0
       if addr.ip_address == "::"
         addr = Addrinfo.new(["AF_INET6", addr.ip_port, nil, "::1"], addr.pfamily, addr.socktype, addr.protocol)
+      elsif addr.ip_address == "0.0.0.0" # MacOS X 10.4 returns "a.b.c.d" for IPv4-mapped IPv6 address.
+        addr = Addrinfo.new(["AF_INET6", addr.ip_port, nil, "::1"], addr.pfamily, addr.socktype, addr.protocol)
       end
     elsif defined?(Socket::AF_UNIX) && afamily == Socket::AF_UNIX
       raise SocketError, "unbound Unix socket" if addr.unix_path == ""
