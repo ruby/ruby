4104010c88a499cdcfbf91f51381a602c7f57b7d
seki <seki@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-03-01 15:57:33 +0000

    merged a patch from Alessandro Di Maria in [ruby-core:22560].
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@22699 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/drb/observer.rb
+++ lib/drb/observer.rb
@@ -6,16 +6,16 @@ module DRb
 
     def notify_observers(*arg)
       if defined? @observer_state and @observer_state
-	if defined? @observer_peers
-	  for i in @observer_peers.dup
-	    begin
-	      i.update(*arg)
-	    rescue
-	      delete_observer(i)
-	    end
-	  end
-	end
-	@observer_state = false
+        if defined? @observer_peers
+          @observer_peers.each do |observer, method|
+            begin
+              observer.send(method, *arg)
+            rescue
+              delete_observer(observer)
+            end
+          end
+        end
+        @observer_state = false
       end
     end
   end
