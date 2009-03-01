3465e37e63242c62c8a86cff16ba156fedf4a202
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-03-01 10:02:06 +0000

    * ext/dl/cfunc.c (rb_dlcfunc_call): fix for stdcall and missing   argument numbers.  [ruby-core:22601]
    * ext/dl/dl.h (DLSTACK_PROTO0_): added.
    
    * ext/dl/mkcallback.rb (gencallback, gen_callback_file),
      (rb_dl_init_callbacks): omit stdcall functions unless supported.
    
    * lib/rubygems/config_file.rb (SHGetFolderPath): stdcall.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@22690 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/rubygems/config_file.rb
+++ lib/rubygems/config_file.rb
@@ -36,8 +36,8 @@ class Gem::ConfigFile
 
       CSIDL_COMMON_APPDATA = 0x0023
       path = 0.chr * 260
-      SHGetFolderPath = Win32API.new 'shell32', 'SHGetFolderPath', 'LLLLP', 'L'
-      SHGetFolderPath.call 0, CSIDL_COMMON_APPDATA, 0, 1, path
+      SHGetFolderPath = Win32API.new 'shell32', 'SHGetFolderPath', 'PLPLP', 'L', :stdcall
+      SHGetFolderPath.call nil, CSIDL_COMMON_APPDATA, nil, 1, path
 
       path.strip
     rescue LoadError
