18c0a7eebf270bbdf1c6701bbbbb9fbdb270ed0a
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-03-03 11:38:55 +0000

    merging r22085 needs r20086. [ruby-dev:38067] merges r22086 from trunk into ruby_1_9_1. 	* ruby.c (process_options): set initial default_external before -r.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_1@22729 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ruby.c
+++ ruby.c
@@ -1289,6 +1289,13 @@ process_options(VALUE arg)
 	opt->src.enc.index = opt_enc_index(opt->src.enc.name);
 	src_encoding_index = opt->src.enc.index;
     }
+    if (opt->ext.enc.index >= 0) {
+	enc = rb_enc_from_index(opt->ext.enc.index);
+    }
+    else {
+	enc = lenc;
+    }
+    rb_enc_set_default_external(rb_enc_from_encoding(enc));
     if (opt->intern.enc.index >= 0) {
 	enc = rb_enc_from_index(opt->intern.enc.index);
 	rb_enc_set_default_internal(rb_enc_from_encoding(enc));
