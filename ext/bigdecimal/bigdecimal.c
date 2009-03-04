4481f3c5e8f245d4bf502fed85ef08d436229163
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-03-04 09:20:15 +0000

    merges r22689 from trunk into ruby_1_9_1. -- * ext/bigdecimal/bigdecimal.c (gfDebug): uncommented out.   [ruby-core:22600]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_1@22753 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/bigdecimal/bigdecimal.c
+++ ext/bigdecimal/bigdecimal.c
@@ -2035,7 +2035,7 @@ Init_bigdecimal(void)
  *
  */
 #ifdef _DEBUG
-/*static int gfDebug = 1;*/         /* Debug switch */
+static int gfDebug = 1;         /* Debug switch */
 static int gfCheckVal = 1;      /* Value checking flag in VpNmlz()  */
 #endif /* _DEBUG */
 
