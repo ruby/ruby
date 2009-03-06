c520d1fc84c338db3370fc7086f2840daa361dc9
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-03-06 04:45:38 +0000

    * lib/rake/testtask.rb (Rake::TestTask#define): passes each libs   as each arguments with expanded.  incorporated from rake 0.8.4.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@22790 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/rake/testtask.rb
+++ lib/rake/testtask.rb
@@ -93,7 +93,7 @@ module Rake
 
     # Create the tasks defined by this task lib.
     def define
-      lib_path = @libs.join(File::PATH_SEPARATOR)
+      lib_path = @libs.collect {|path| "-I#{File.expand_path(path)}"}
       desc "Run tests" + (@name==:test ? "" : " for #{@name}")
       task @name do
         run_code = ''
@@ -107,7 +107,7 @@ module Rake
             when :rake
               rake_loader
             end
-          @ruby_opts.unshift( "-I#{lib_path}" )
+          @ruby_opts.unshift( *lib_path )
           @ruby_opts.unshift( "-w" ) if @warning
           ruby @ruby_opts.join(" ") +
             " \"#{run_code}\" " +
