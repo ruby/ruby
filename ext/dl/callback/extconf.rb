d3c4a4686a8f4533380c24d17736db3f6124de34
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-03-09 16:57:17 +0000

    * ext/dl/callback/extconf.rb ($distcleanfiles): added callback.c.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@22862 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/dl/callback/extconf.rb
+++ ext/dl/callback/extconf.rb
@@ -1,14 +1,12 @@
 require 'mkmf'
 
 if compiled?("dl")
-  CALLBACKS = (0..8).map{|i| "callback-#{i}"}
-  CALLBACK_SRCS = CALLBACKS.map{|basename| "#{basename}.c"}
-  CALLBACK_OBJS = CALLBACKS.map{|basename| "#{basename}.o"}
+  callbacks = (0..8).map{|i| "callback-#{i}"}.unshift("callback")
+  callback_srcs = callbacks.map{|basename| "#{basename}.c"}
+  callback_objs = callbacks.map{|basename| "#{basename}.o"}
 
-  $distcleanfiles += [ "callback.h", *CALLBACK_SRCS ]
-
-  $objs = %w[ callback.o ] + CALLBACK_OBJS
-
+  $distcleanfiles += [ "callback.h", *callback_srcs ]
+  $objs = callback_objs
   $INCFLAGS << " -I$(srcdir)/.."
 
   create_makefile("dl/callback")
