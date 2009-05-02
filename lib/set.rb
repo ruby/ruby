07adea0ca7bb6f9e0864ab1a2c6aa842134c5023
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-05-02 13:55:42 +0000

    * lib/set.rb (SortedSet): Fix document. [Bug #1429]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@23327 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/set.rb
+++ lib/set.rb
@@ -458,7 +458,7 @@ end
 # 
 #   require "set"
 #   
-#   set = SortedSet.new(2, 1, 5, 6, 4, 5, 3, 3, 3)
+#   set = SortedSet.new([2, 1, 5, 6, 4, 5, 3, 3, 3])
 #   ary = []
 #   
 #   set.each do |obj|
@@ -467,7 +467,7 @@ end
 #   
 #   p ary # => [1, 2, 3, 4, 5, 6]
 #   
-#   set2 = SortedSet.new(1, 2, "3")
+#   set2 = SortedSet.new([1, 2, "3"])
 #   set2.each { |obj| } # => raises ArgumentError: comparison of Fixnum with String failed
 #   
 class SortedSet < Set
