f2140e9e4e2cd64a17a8db766f19d1ea1f16edfa
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-05-11 15:04:43 +0000

    * 2009-05-12
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_1@23395 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
 #define RUBY_VERSION "1.9.1"
-#define RUBY_RELEASE_DATE "2009-05-04"
+#define RUBY_RELEASE_DATE "2009-05-12"
 #define RUBY_PATCHLEVEL 0
 
 #define RUBY_VERSION_MAJOR 1
@@ -7,7 +7,7 @@
 #define RUBY_VERSION_TEENY 1
 #define RUBY_RELEASE_YEAR 2009
 #define RUBY_RELEASE_MONTH 5
-#define RUBY_RELEASE_DAY 4
+#define RUBY_RELEASE_DAY 12
 
 #ifdef RUBY_EXTERN
 RUBY_EXTERN const char ruby_version[];
