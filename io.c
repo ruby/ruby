c8eabecb9d5ee48ad4a0fc25220724f46691bfd9
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-06-22 16:31:49 +0000

    rdoc update.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@23824 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- io.c
+++ io.c
@@ -912,11 +912,13 @@ rb_io_set_sync(io, mode)
  *     ios.fsync   => 0 or nil
  *  
  *  Immediately writes all buffered data in <em>ios</em> to disk.
- *  Returns <code>nil</code> if the underlying operating system does not
- *  support <em>fsync(2)</em>. Note that <code>fsync</code> differs from
+ *  Note that <code>fsync</code> differs from
  *  using <code>IO#sync=</code>. The latter ensures that data is flushed
  *  from Ruby's buffers, but doesn't not guarantee that the underlying
  *  operating system actually writes it to disk.
+ *
+ *  <code>NotImplementedError</code> is raised
+ *  if the underlying operating system does not support <em>fsync(2)</em>. 
  */
 
 static VALUE
