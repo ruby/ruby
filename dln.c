61c0c7ab5d94640fdd1c8ad482f170d1890a6d14
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-07-12 14:44:54 +0000

    merges r23891 from trunk into ruby_1_9_1. -- * dln.c (dln_find_1): fix for files with dots.  [ruby-dev:38588]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_1@24046 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- dln.c
+++ dln.c
@@ -1520,7 +1520,12 @@ dln_find_1(const char *fname, const char *path, char *fbuf, int size,
     struct stat st;
     int i, fspace;
 #ifdef DOSISH
-    int is_abs = 0, has_path = 0, has_ext = 0;
+    static const char extension[][5] = {
+	".exe", ".com", ".cmd", ".bat",
+    };
+    size_t j;
+    int is_abs = 0, has_path = 0;
+    const char *ext = 0;
     const char *p = fname;
 #endif
 
@@ -1547,15 +1552,23 @@ dln_find_1(const char *fname, const char *path, char *fbuf, int size,
 	switch (*p) {
 	  case '/': case '\\':
 	    has_path = 1;
-	    has_ext = 0;
+	    ext = 0;
 	    p++;
 	    break;
 	  case '.':
-	    has_ext = 1;
+	    ext = p;
 	    p++;
 	    break;
 	  default:
 	    p = CharNext(p);
+	}
+    }
+    if (ext) {
+	for (j = 0; STRCASECMP(ext, extension[j]); j++) {
+	    if (j == sizeof(extension) / sizeof(extension[0])) {
+		ext = 0;
+		break;
+	    }
 	}
     }
     ep = bp = 0;
@@ -1563,7 +1576,7 @@ dln_find_1(const char *fname, const char *path, char *fbuf, int size,
 	RETURN_IF(is_abs);
     }
     else if (has_path) {
-	RETURN_IF(has_ext);
+	RETURN_IF(ext);
 	i = p - fname;
 	if (i + 1 > size) goto toolong;
 	fspace = size - i - 1;
@@ -1644,15 +1657,7 @@ dln_find_1(const char *fname, const char *path, char *fbuf, int size,
 	memcpy(bp, fname, i + 1);
 
 #if defined(DOSISH)
-	if (exe_flag && !has_ext) {
-	    static const char extension[][5] = {
-#if defined(__EMX__) || defined(_WIN32)
-		".exe", ".com", ".cmd", ".bat",
-/* end of __EMX__ or _WIN32 */
-#endif
-	    };
-	    int j;
-
+	if (exe_flag && !ext) {
 	  needs_extension:
 	    for (j = 0; j < sizeof(extension) / sizeof(extension[0]); j++) {
 		if (fspace < strlen(extension[j])) {
