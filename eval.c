699d9a15c4db8eec896c0a09939e42a5593f1a08
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-08-05 03:54:08 +0000

    * eval.c (rb_exc_raise, rb_exc_fatal): require exception object.   [ruby-core:24767]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@24403 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- eval.c
+++ eval.c
@@ -4735,6 +4735,7 @@ void
 rb_exc_raise(mesg)
     VALUE mesg;
 {
+    mesg = rb_make_exception(1, &mesg);
     rb_longjmp(TAG_RAISE, mesg);
 }
 
@@ -4742,6 +4743,7 @@ void
 rb_exc_fatal(mesg)
     VALUE mesg;
 {
+    mesg = rb_make_exception(1, &mesg);
     rb_longjmp(TAG_FATAL, mesg);
 }
 
