d04b691b96fa1c4c0c55a30e711309f7d5301042
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-08-06 03:05:16 +0000

    * lib/test/unit/testcase.rb (Test::Unit): removes silly TestCase   class.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@24417 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/test/unit/testcase.rb
+++ lib/test/unit/testcase.rb
@@ -2,6 +2,9 @@ require 'test/unit/assertions'
 
 module Test
   module Unit
+    # remove silly TestCase class
+    remove_const(:TestCase) if defined?(self::TestCase)
+
     class TestCase < MiniTest::Unit::TestCase
       include Assertions
       def self.test_order
