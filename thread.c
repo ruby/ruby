f72b854c7d232e0bcbeade4fd44a63130aa6143b
mame <mame@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-08-21 15:14:29 +0000

    * thread.c (rb_check_deadlock): decrease number of sleepers before   deadlock detection because the deadlock exception makes main thread   run.  [ruby-dev:39142]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@24616 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- thread.c
+++ thread.c
@@ -4049,6 +4049,7 @@ rb_check_deadlock(rb_vm_t *vm)
 	printf("%d %d %p %p\n", vm->living_threads->num_entries, vm->sleeper, GET_THREAD(), vm->main_thread);
 	st_foreach(vm->living_threads, debug_i, (st_data_t)0);
 #endif
+	vm->sleeper--;
 	rb_threadptr_raise(vm->main_thread, 2, argv);
     }
 }
