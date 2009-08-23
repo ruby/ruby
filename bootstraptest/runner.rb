0ebb7c1c9bd4d87599e948b8af6ed9d12409681a
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-08-23 06:15:00 +0000

    * bootstraptest/runner.rb (Dir.mktmpdir): updated to latest.   (in_temporary_working_directory): temporary directory name changed.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@24630 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/runner.rb
+++ bootstraptest/runner.rb
@@ -17,13 +17,27 @@ end
 
 if !Dir.respond_to?(:mktmpdir)
   # copied from lib/tmpdir.rb
-  def Dir.mktmpdir(prefix="d", tmpdir=nil)
+  def Dir.mktmpdir(prefix_suffix=nil, tmpdir=nil)
+    case prefix_suffix
+    when nil
+      prefix = "d"
+      suffix = ""
+    when String
+      prefix = prefix_suffix
+      suffix = ""
+    when Array
+      prefix = prefix_suffix[0]
+      suffix = prefix_suffix[1]
+    else
+      raise ArgumentError, "unexpected prefix_suffix: #{prefix_suffix.inspect}"
+    end
     tmpdir ||= Dir.tmpdir
     t = Time.now.strftime("%Y%m%d")
     n = nil
     begin
       path = "#{tmpdir}/#{prefix}#{t}-#{$$}-#{rand(0x100000000).to_s(36)}"
       path << "-#{n}" if n
+      path << suffix
       Dir.mkdir(path, 0700)
     rescue Errno::EEXIST
       n ||= 0
@@ -339,7 +353,7 @@ def in_temporary_working_directory(dir)
       yield
     }
   else
-    Dir.mktmpdir("bootstraptest.tmpwd") {|d|
+    Dir.mktmpdir(["bootstraptest", ".tmpwd"]) {|d|
       Dir.chdir(d) {
         yield
       }
