cd4fc5be39d86196d529f9c464d9c87e0b73d66d
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-09-09 02:22:08 +0000

    * include/ruby/ruby.h (rb_data_type_struct): constified dsize.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@24802 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- cont.c
+++ cont.c
@@ -128,7 +128,7 @@ cont_free(void *ptr)
 }
 
 static size_t
-cont_memsize(void *ptr)
+cont_memsize(const void *ptr)
 {
     rb_context_t *cont = ptr;
     size_t size = 0;
@@ -206,7 +206,7 @@ fiber_free(void *ptr)
 }
 
 static size_t
-fiber_memsize(void *ptr)
+fiber_memsize(const void *ptr)
 {
     rb_fiber_t *fib = ptr;
     size_t size = 0;
