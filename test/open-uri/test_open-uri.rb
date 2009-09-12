e9319d60e3ddcf9debf8f24f5df8f58c2b3d6f4c
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-09-12 16:18:03 +0000

    * lib/open-uri.rb (URI::FTP#buffer_open): fix the %2F handling.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@24875 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/open-uri/test_open-uri.rb
+++ test/open-uri/test_open-uri.rb
@@ -446,6 +446,15 @@ class TestOpenURI < Test::Unit::TestCase
     }
   end
 
+  def test_ftp_invalid_request
+    assert_raise(ArgumentError) { URI("ftp://127.0.0.1/").read }
+    assert_raise(ArgumentError) { URI("ftp://127.0.0.1/a%0Db").read }
+    assert_raise(ArgumentError) { URI("ftp://127.0.0.1/a%0Ab").read }
+    assert_raise(ArgumentError) { URI("ftp://127.0.0.1/a%0Db/f").read }
+    assert_raise(ArgumentError) { URI("ftp://127.0.0.1/a%0Ab/f").read }
+    assert_raise(URI::InvalidComponentError) { URI("ftp://127.0.0.1/d/f;type=x") }
+  end
+
   def test_ftp
     TCPServer.open("127.0.0.1", 0) {|serv|
       _, port, _, host = serv.addr
@@ -480,6 +489,56 @@ class TestOpenURI < Test::Unit::TestCase
       begin
         content = URI("ftp://#{host}:#{port}/foo/bar").read
         assert_equal("content", content)
+      ensure
+        Thread.kill(th)
+        th.join
+      end
+    }
+  end
+
+  def test_ftp_ascii
+    TCPServer.open("127.0.0.1", 0) {|serv|
+      _, port, _, host = serv.addr
+      th = Thread.new {
+        s = serv.accept
+        begin
+          content = "content"
+          s.print "220 Test FTP Server\r\n"
+          assert_equal("USER anonymous\r\n", s.gets); s.print "331 name ok\r\n"
+          assert_match(/\APASS .*\r\n/, s.gets); s.print "230 logged in\r\n"
+          assert_equal("TYPE I\r\n", s.gets); s.print "200 type set to I\r\n"
+          assert_equal("CWD /foo\r\n", s.gets); s.print "250 CWD successful\r\n"
+          assert_equal("TYPE A\r\n", s.gets); s.print "200 type set to A\r\n"
+          assert_equal("SIZE bar\r\n", s.gets); s.print "213 #{content.bytesize}\r\n"
+          assert_equal("PASV\r\n", s.gets)
+          TCPServer.open("127.0.0.1", 0) {|data_serv|
+            _, data_serv_port, _, data_serv_host = data_serv.addr
+            hi = data_serv_port >> 8
+            lo = data_serv_port & 0xff
+            s.print "227 Entering Passive Mode (127,0,0,1,#{hi},#{lo}).\r\n"
+            assert_equal("RETR bar\r\n", s.gets); s.print "150 file okay\r\n"
+            data_sock = data_serv.accept
+            begin
+              data_sock << content
+            ensure
+              data_sock.close
+            end
+            s.print "226 transfer complete\r\n"
+            assert_nil(s.gets)
+          }
+        ensure
+          s.close if s
+        end
+      }
+      begin
+        length = []
+        progress = []
+        content = URI("ftp://#{host}:#{port}/%2Ffoo/b%61r;type=a").read(
+         :content_length_proc => lambda {|n| length << n },
+         :progress_proc => lambda {|n| progress << n })
+        assert_equal("content", content)
+        assert_equal([7], length)
+        assert_equal(7, progress.inject(&:+))
       ensure
         Thread.kill(th)
         th.join
