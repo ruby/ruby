9e137196a6e82f5d63daec18f53b9e809f195ad5
marcandre <marcandre@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-09-13 22:35:12 +0000

    * lib/cgi/cookie.rb (value=): Keep CGI::Cookie#value in sync with the   cookie itself. A patch by Arthur Schreiber [ruby-core:17634]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@24913 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/cgi.rb
+++ lib/cgi.rb
@@ -822,8 +822,8 @@ class CGI
       super(@value)
     end
 
-    attr_accessor("name", "value", "path", "domain", "expires")
-    attr_reader("secure")
+    attr_accessor("name", "path", "domain", "expires")
+    attr_reader("secure", "value")
 
     # Set whether the Cookie is a secure cookie or not.
     #
@@ -833,16 +833,16 @@ class CGI
       @secure
     end
 
+    def value=(val)
+      @value.replace(Array(val))
+    end
+
     # Convert the Cookie to its string representation.
     def to_s
       buf = ""
       buf += @name + '='
 
-      if @value.kind_of?(String)
-        buf += CGI::escape(@value)
-      else
-        buf += @value.collect{|v| CGI::escape(v) }.join("&")
-      end
+      buf += @value.map { |v| CGI::escape(v) }.join("&")
 
       if @domain
         buf += '; domain=' + @domain
