80464f8f2ad91003c05a3ea84e2c1ab824ec2662
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-09-17 21:36:53 +0000

    * lib/mkmf.rb (rm_f, rm_rf): FileUtils.rm can take an array.   [ruby-dev:39345]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@24984 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -195,13 +195,13 @@ class Array
 end
 
 def rm_f(*files)
-  opt = ([files.pop] if Hash === files.last)
-  FileUtils.rm_f(Dir[*files], *opt)
+  opt = (Hash === files.last ? [files.pop] : [])
+  FileUtils.rm_f(Dir[*files.flatten], *opt)
 end
 
 def rm_rf(*files)
-  opt = ([files.pop] if Hash === files.last)
-  FileUtils.rm_rf(Dir[*files], *opt)
+  opt = (Hash === files.last ? [files.pop] : [])
+  FileUtils.rm_rf(Dir[*files.flatten], *opt)
 end
 
 # Returns time stamp of the +target+ file if it exists and is newer
