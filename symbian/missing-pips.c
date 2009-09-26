92d4a6d5166a78dcee21292ecbdd128130694612
azav <azav@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-09-26 19:34:27 +0000

    Update to work with the latest PIPS version 1.6
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@25109 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- symbian/missing-pips.c
+++ symbian/missing-pips.c
@@ -8,40 +8,11 @@ char **environ = 0;
 typedef void (*sighandler_t)(int);
 sighandler_t signal(int signum, sighandler_t handler);
 
-int sigfillset(sigset_t *set);
-int sigdelset(sigset_t *set, int signum);
-int sigprocmask(int how, const sigset_t *set, sigset_t *oldset);
-int raise(int sig);
 int kill(pid_t pid, int sig);
 int pthread_sigmask(int how, const sigset_t *set, sigset_t *oset);
 int execl(const char *path, const char *arg0, ... /*, (char *)0 */);
 int execv(const char *path, char *const argv[]);
 int pthread_kill(pthread_t thread, int sig);
-
-sighandler_t signal(int signum, sighandler_t handler)
-{
-	return (sighandler_t)0;
-}
-
-int sigfillset(sigset_t *set)
-{
-	return 0;
-}
-
-int sigdelset(sigset_t *set, int signum)
-{
-	return 0;
-}
-
-int sigprocmask(int how, const sigset_t *set, sigset_t *oldset)
-{
-	return 0;
-}
-
-int raise(int sig)
-{
-	return 0;
-}
 
 int kill(pid_t pid, int sig)
 {
@@ -68,18 +39,6 @@ int pthread_kill(pthread_t thread, int sig)
 	return -1;
 }
 
-
-int sigmask(int signum) {
-	return -1;
-}
-
-int sigblock(int mask) {
-	return -1;
-}
-
-int sigsetmask(int mask) {
-	return -1;
-}
 
 sighandler_t posix_signal(int signum, sighandler_t handler)
 {
