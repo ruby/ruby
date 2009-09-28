666a3f793982b593e9d231dee41fed9e90ff987c
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-09-28 13:33:13 +0000

    * dln.c (aix_loaderror): needs format string.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@25134 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- dln.c
+++ dln.c
@@ -1190,7 +1190,7 @@ aix_loaderror(const char *pathname)
 	ERRBUF_APPEND("\n");
     }
     errbuf[strlen(errbuf)-1] = '\0';	/* trim off last newline */
-    rb_loaderror(errbuf);
+    rb_loaderror("%s", errbuf);
     return;
 }
 #endif
