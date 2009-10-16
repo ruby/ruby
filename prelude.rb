0e781f81d80cb2ccd652bcc062b5b3bbc86f4af2
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-10-16 13:23:15 +0000

    * prelude.rb (require_relative): defined as a module function of   Kernel.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@25376 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- prelude.rb
+++ prelude.rb
@@ -23,13 +23,16 @@ class Thread
   end
 end
 
-def require_relative(relative_feature)
-  c = caller.first
-  e = c.rindex(/:\d+:in /)
-  file = $`
-  if /\A\((.*)\)/ =~ file # eval, etc.
-    raise LoadError, "require_relative is called in #{$1}"
+module Kernel
+  module_function
+  def require_relative(relative_feature)
+    c = caller.first
+    e = c.rindex(/:\d+:in /)
+    file = $`
+    if /\A\((.*)\)/ =~ file # eval, etc.
+      raise LoadError, "require_relative is called in #{$1}"
+    end
+    absolute_feature = File.expand_path(File.join(File.dirname(file), relative_feature))
+    require absolute_feature
   end
-  absolute_feature = File.expand_path(File.join(File.dirname(file), relative_feature))
-  require absolute_feature
 end
