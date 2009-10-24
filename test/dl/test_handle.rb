4a3549878aea75a97ac8c2e3255138a7d16209f7
tenderlove <tenderlove@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-10-24 21:56:50 +0000

    * ext/dl/handle.c (rb_dlhandle_close) check return value of dlclose()
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@25458 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/dl/test_handle.rb
+++ test/dl/test_handle.rb
@@ -2,6 +2,19 @@ require 'test_base'
 
 module DL
   class TestHandle < TestBase
+    def test_handle_close
+      handle = DL::Handle.new(LIBC_SO)
+      assert_equal 0, handle.close
+    end
+
+    def test_handle_close_twice
+      handle = DL::Handle.new(LIBC_SO)
+      handle.close
+      assert_raises(DL::DLError) do
+        handle.close
+      end
+    end
+
     def test_dlopen_returns_handle
       assert_instance_of DL::Handle, dlopen(LIBC_SO)
     end
