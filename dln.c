dcad53d3bc3e4e716abfd63a813cecb496723923
kanemoto <kanemoto@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-11-12 16:01:28 +0000

    merges r24482, r25061, r25124, r25126, r25552 from trunk into ruby_1_9_1.
    These patches are for AIX compilation issues. This commit is allowed by
    branch master (yugui). Thanks!
    
    * ext/socket/extconf.rb : Compilation failure on AIX.
      ss_len (a member of struct sockaddr_storage) has preceding __,
      but ss_family does not have it from AIX 5.2.
    
    * Makefile.in (miniruby): suppress duplication warning on AIX.
    
    * common.mk (ruby.imp): add text section [Bug #2064].
    
    * common.mk (ruby.imp): do not export Init_*.
    
    * dln.c (aix_loaderror): fixed typo. suppress warnings.
    
    * common.mk (ruby.imp): excluded prelude.o to get rid of circular
      dependency.  [ruby-dev:39052]
    
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_1@25740 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- dln.c
+++ dln.c
@@ -1100,6 +1100,7 @@ dln_sym(const char *name)
 #include <windows.h>
 #endif
 
+#if ! defined _AIX
 static const char *
 dln_strerror(void)
 {
@@ -1149,7 +1150,7 @@ dln_strerror(void)
     return message;
 #endif
 }
-
+#endif
 
 #if defined(_AIX) && ! defined(_IA64)
 static void
@@ -1160,7 +1161,7 @@ aix_loaderror(const char *pathname)
 
     static const struct errtab {
 	int errnum;
-	char *errstr;
+	const char * errstr;
     } load_errtab[] = {
 	{L_ERROR_TOOMANY,	"too many errors, rest skipped."},
 	{L_ERROR_NOLIB,		"can't load library:"},
@@ -1182,7 +1183,7 @@ aix_loaderror(const char *pathname)
     snprintf(errbuf, sizeof(errbuf), "load failed - %s ", pathname);
 
     message[0] = NULL;
-    if (!loadquery(L_GETMESSAGE, &message[0], sizeof(message)))
+    if (!loadquery(L_GETMESSAGES, &message[0], sizeof(message)))
 	ERRBUF_APPEND(strerror(errno));
     for(i = 0; message[i] && *message[i]; i++) {
 	int nerr = atoi(message[i]);
