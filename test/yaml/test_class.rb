8f90252c5250b086309be9c613cc231fe2475706
tenderlove <tenderlove@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-11-16 17:27:05 +0000

    Adding yaml tests [ruby-core:26732]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@25804 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ test/yaml/test_class.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+require 'yaml'
+
+module YAML
+  class TestClass < Test::Unit::TestCase
+    def test_to_yaml
+      assert_raises(::TypeError) do
+        TestClass.to_yaml
+      end
+    end
+
+    def test_dump
+      assert_raises(::TypeError) do
+        YAML.dump TestClass
+      end
+    end
+  end
+end
