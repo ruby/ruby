957ce6574c64845ec1d853e6132b0ed92e154a87
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-11-24 07:21:12 +0000

    * ext/digest/lib/md5.rb, ext/digest/lib/sha1.rb: Add an   obsoleteness warning under -w/-v.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@25903 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/digest/lib/md5.rb
+++ ext/digest/lib/md5.rb
@@ -3,6 +3,8 @@
 # $RoughId: md5.rb,v 1.4 2001/07/13 15:38:27 knu Exp $
 # $Id$
 
+warn "require 'md5' is obsolete; require 'digest' and use Digest::MD5." if $VERBOSE
+
 require 'digest/md5'
 
 class MD5 < Digest::MD5
--- ext/digest/lib/sha1.rb
+++ ext/digest/lib/sha1.rb
@@ -3,6 +3,8 @@
 # $RoughId: sha1.rb,v 1.4 2001/07/13 15:38:27 knu Exp $
 # $Id$
 
+warn "require 'sha1' is obsolete; require 'digest' and use Digest::SHA1." if $VERBOSE
+
 require 'digest/sha1'
 
 class SHA1 < Digest::SHA1
