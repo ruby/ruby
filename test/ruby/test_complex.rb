25caa12d1d456746da2e8c2ee61e398dba9c135b
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-11-26 12:18:40 +0000

    * test/ruby/test_complex.rb (@unify): fix the detection if math   loaded. This makes test_complex.rb work fine with the previous   commit.
    * test/ruby/test_rational.rb (@unify): ditto.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@25936 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_complex.rb
+++ test/ruby/test_complex.rb
@@ -9,7 +9,8 @@ class Complex_Test < Test::Unit::TestCase
     if @rational
       @keiju = Rational.instance_variable_get('@RCS_ID')
     end
-    @unify = $".grep(/mathn/).size != 0
+    seps = [File::SEPARATOR, File::ALT_SEPARATOR].compact.map{|x| Regexp.escape(x)}.join("|")
+    @unify = $".grep(/(?:^|#{seps})mathn(?:\.(?:rb|so))?/).size != 0
   end
 
   def test_compsub
