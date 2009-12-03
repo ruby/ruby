f28a0b6b98592eba4bfc6a7c80e12be6d8c1a93e
tenderlove <tenderlove@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-12-03 03:04:27 +0000

    * test/dl/test_callback.rb (**): testing return values of dl callbacks
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@25978 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/dl/test_callback.rb
+++ test/dl/test_callback.rb
@@ -1,9 +1,32 @@
 require_relative 'test_base'
 require_relative '../ruby/envutil'
+require 'dl/callback'
+require 'dl/func'
 
 module DL
   class TestCallback < TestBase
     include DL
+
+    def test_callback_return_value
+      addr = set_callback(TYPE_VOIDP, 1) do |str|
+        str
+      end
+      func = CFunc.new(addr, TYPE_VOIDP, 'test')
+      f = Function.new(func, [TYPE_VOIDP])
+      ptr = CPtr['blah']
+      assert_equal ptr, f.call(ptr)
+    end
+
+    def test_callback_return_arbitrary
+      addr = set_callback(TYPE_VOIDP, 1) do |ptr|
+        CPtr['foo'].to_i
+      end
+      func = CFunc.new(addr, TYPE_VOIDP, 'test')
+      f = Function.new(func, [TYPE_VOIDP])
+
+      ptr = CPtr['foo']
+      assert_equal 'foo', f.call(ptr).to_s
+    end
 
     def test_callback_with_string
       called_with = nil
