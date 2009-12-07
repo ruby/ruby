79ca5d7040e594d714a72f8118d325c3b246cfe7
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-12-07 18:51:42 +0000

    * file.c (rb_f_test): use string form in unknown command error   message.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@26045 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- file.c
+++ file.c
@@ -3680,51 +3680,51 @@ test_check(int n, int argc, VALUE *argv)
  *  File tests on a single file:
  *
  *    Test   Returns   Meaning
- *     ?A  | Time    | Last access time for file1
- *     ?b  | boolean | True if file1 is a block device
- *     ?c  | boolean | True if file1 is a character device
- *     ?C  | Time    | Last change time for file1
- *     ?d  | boolean | True if file1 exists and is a directory
- *     ?e  | boolean | True if file1 exists
- *     ?f  | boolean | True if file1 exists and is a regular file
- *     ?g  | boolean | True if file1 has the \CF{setgid} bit
+ *    "A"  | Time    | Last access time for file1
+ *    "b"  | boolean | True if file1 is a block device
+ *    "c"  | boolean | True if file1 is a character device
+ *    "C"  | Time    | Last change time for file1
+ *    "d"  | boolean | True if file1 exists and is a directory
+ *    "e"  | boolean | True if file1 exists
+ *    "f"  | boolean | True if file1 exists and is a regular file
+ *    "g"  | boolean | True if file1 has the \CF{setgid} bit
  *         |         | set (false under NT)
- *     ?G  | boolean | True if file1 exists and has a group
+ *    "G"  | boolean | True if file1 exists and has a group
  *         |         | ownership equal to the caller's group
- *     ?k  | boolean | True if file1 exists and has the sticky bit set
- *     ?l  | boolean | True if file1 exists and is a symbolic link
- *     ?M  | Time    | Last modification time for file1
- *     ?o  | boolean | True if file1 exists and is owned by
+ *    "k"  | boolean | True if file1 exists and has the sticky bit set
+ *    "l"  | boolean | True if file1 exists and is a symbolic link
+ *    "M"  | Time    | Last modification time for file1
+ *    "o"  | boolean | True if file1 exists and is owned by
  *         |         | the caller's effective uid
- *     ?O  | boolean | True if file1 exists and is owned by
+ *    "O"  | boolean | True if file1 exists and is owned by
  *         |         | the caller's real uid
- *     ?p  | boolean | True if file1 exists and is a fifo
- *     ?r  | boolean | True if file1 is readable by the effective
+ *    "p"  | boolean | True if file1 exists and is a fifo
+ *    "r"  | boolean | True if file1 is readable by the effective
  *         |         | uid/gid of the caller
- *     ?R  | boolean | True if file is readable by the real
+ *    "R"  | boolean | True if file is readable by the real
  *         |         | uid/gid of the caller
- *     ?s  | int/nil | If file1 has nonzero size, return the size,
+ *    "s"  | int/nil | If file1 has nonzero size, return the size,
  *         |         | otherwise return nil
- *     ?S  | boolean | True if file1 exists and is a socket
- *     ?u  | boolean | True if file1 has the setuid bit set
- *     ?w  | boolean | True if file1 exists and is writable by
+ *    "S"  | boolean | True if file1 exists and is a socket
+ *    "u"  | boolean | True if file1 has the setuid bit set
+ *    "w"  | boolean | True if file1 exists and is writable by
  *         |         | the effective uid/gid
- *     ?W  | boolean | True if file1 exists and is writable by
+ *    "W"  | boolean | True if file1 exists and is writable by
  *         |         | the real uid/gid
- *     ?x  | boolean | True if file1 exists and is executable by
+ *    "x"  | boolean | True if file1 exists and is executable by
  *         |         | the effective uid/gid
- *     ?X  | boolean | True if file1 exists and is executable by
+ *    "X"  | boolean | True if file1 exists and is executable by
  *         |         | the real uid/gid
- *     ?z  | boolean | True if file1 exists and has a zero length
+ *    "z"  | boolean | True if file1 exists and has a zero length
  *
  * Tests that take two files:
  *
- *     ?-  | boolean | True if file1 and file2 are identical
- *     ?=  | boolean | True if the modification times of file1
+ *    "-"  | boolean | True if file1 and file2 are identical
+ *    "="  | boolean | True if the modification times of file1
  *         |         | and file2 are equal
- *     ?<  | boolean | True if the modification time of file1
+ *    "<"  | boolean | True if the modification time of file1
  *         |         | is prior to that of file2
- *     ?>  | boolean | True if the modification time of file1
+ *    ">"  | boolean | True if the modification time of file1
  *         |         | is after that of file2
  */
 
@@ -3857,10 +3857,10 @@ rb_f_test(int argc, VALUE *argv)
   unknown:
     /* unknown command */
     if (ISPRINT(cmd)) {
-	rb_raise(rb_eArgError, "unknown command ?%c", cmd);
+	rb_raise(rb_eArgError, "unknown command '%s%c'", cmd == '\'' || cmd == '\\' ? "\\" : "", cmd);
     }
     else {
-	rb_raise(rb_eArgError, "unknown command ?\\x%02X", cmd);
+	rb_raise(rb_eArgError, "unknown command \"\\x%02X\"", cmd);
     }
     return Qnil;		/* not reached */
 }
