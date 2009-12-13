a998582754ca23b3112da47c86d5a2786dab1df5
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-12-13 14:48:54 +0000

    * lib/find.rb (Find.find): sort directory entries.  [ruby-dev:39847]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@26075 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/find.rb
+++ lib/find.rb
@@ -46,22 +46,19 @@ module Find
         end
 	begin
 	  if s.directory? then
-	    d = Dir.open(file)
-	    begin
-	      for f in d
-		next if f == "." or f == ".."
-		if File::ALT_SEPARATOR and file =~ /^(?:[\/\\]|[A-Za-z]:[\/\\]?)$/ then
-		  f = file + f
-		elsif file == "/" then
-		  f = "/" + f
-		else
-		  f = File.join(file, f)
-		end
-		paths.unshift f.untaint
-	      end
-	    ensure
-	      d.close
-	    end
+            fs = Dir.entries(file)
+            fs.sort!
+            fs.reverse_each {|f|
+              next if f == "." or f == ".."
+              if File::ALT_SEPARATOR and file =~ /^(?:[\/\\]|[A-Za-z]:[\/\\]?)$/ then
+                f = file + f
+              elsif file == "/" then
+                f = "/" + f
+              else
+                f = File.join(file, f)
+              end
+              paths.unshift f.untaint
+            }
 	  end
         rescue Errno::ENOENT, Errno::EACCES
 	end
