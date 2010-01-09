329e41051a184cd7f5262216e34e28b717e50b0c
tenderlove <tenderlove@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-01-09 22:11:35 +0000

    * test/yaml/test_struct.rb Test for dealing with structs that have   instance variables
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@26265 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ test/yaml/test_struct.rb
@@ -0,0 +1,33 @@
+require 'test/unit'
+require 'yaml'
+
+class StructWithIvar < Struct.new(:foo)
+  attr_reader :bar
+  def initialize *args
+    super
+    @bar = 'hello'
+  end
+end
+
+module YAML
+  class TestStruct < MiniTest::Unit::TestCase
+    def test_roundtrip
+      thing = StructWithIvar.new('bar')
+      struct = YAML.load(YAML.dump(thing))
+
+      assert_equal 'hello', struct.bar
+      assert_equal 'bar', struct.foo
+    end
+
+    def test_load
+      obj = YAML.load(<<-eoyml)
+--- !ruby/struct:StructWithIvar 
+:foo: bar
+:@bar: hello
+      eoyml
+
+      assert_equal 'hello', obj.bar
+      assert_equal 'bar', obj.foo
+    end
+  end
+end
