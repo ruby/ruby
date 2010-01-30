c8f9b4d5468e49962b94b245397f549eb9101e51
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-01-30 12:42:35 +0000

    * 2010-01-30
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_1@26473 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -6,8 +6,8 @@
 
 #define RUBY_RELEASE_YEAR 2010
 #define RUBY_RELEASE_MONTH 1
-#define RUBY_RELEASE_DAY 10
-#define RUBY_RELEASE_DATE "2010-01-10"
+#define RUBY_RELEASE_DAY 30
+#define RUBY_RELEASE_DATE "2010-01-30"
 
 #ifdef RUBY_EXTERN
 RUBY_EXTERN const char ruby_version[];
