6e9eaeacf5d630c94a8360e7dbd7f140a7b7b131
mame <mame@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-01-31 12:46:36 +0000

    * string.c (rb_string_value): make no exception for Symbol.   [ruby-dev:40274]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@26525 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- string.c
+++ string.c
@@ -1349,13 +1349,7 @@ rb_string_value(volatile VALUE *ptr)
 {
     VALUE s = *ptr;
     if (TYPE(s) != T_STRING) {
-	if (SYMBOL_P(s)) {
-	    s = rb_sym_to_s(s);
-	}
-	else {
-	    s = rb_str_to_str(s);
-	}
-	*ptr = s;
+	*ptr = rb_str_to_str(s);
     }
     return s;
 }
