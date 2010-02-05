07c670db6f2cd24ba0da03e14c390ca0676e44d1
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-02-05 15:04:38 +0000

    * random.c (fill_random_seed): don't use O_NOFOLLOW because   /dev/urandom is a symlink in OpenSolaris.
    * lib/securerandom.rb (SecureRandom.random_bytes: ditto.
    
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@26583 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- random.c
+++ random.c
@@ -280,9 +280,6 @@ random_seed()
 #ifdef O_NOCTTY
             |O_NOCTTY
 #endif
-#ifdef O_NOFOLLOW
-            |O_NOFOLLOW
-#endif
             )) >= 0) {
         if (fstat(fd, &statbuf) == 0 && S_ISCHR(statbuf.st_mode)) {
             read(fd, seed, seed_len);
