676cf9b5274d789f5e040a63115487a16c9987fe
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-02-17 00:19:29 +0000

    * test/dl/test_method.rb: sinf(3) is in libm.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@26690 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/dl/test_method.rb
+++ test/dl/test_method.rb
@@ -4,7 +4,7 @@ require 'dl/func'
 module DL
   class TestMethod < TestBase
     def test_method_call
-      f = Method.new(@libc['sinf'], [TYPE_FLOAT], TYPE_FLOAT)
+      f = Method.new(@libm['sinf'], [TYPE_FLOAT], TYPE_FLOAT)
       assert_in_delta 1.0, f.call(90 * Math::PI / 180), 0.0001
     end
   end
