2669dffeec7a2216241566b9ed8d35cc2a05f672
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-02-20 03:17:44 +0000

    indented.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@26716 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- KNOWNBUGS.rb
+++ KNOWNBUGS.rb
@@ -5,12 +5,12 @@
 
 assert_normal_exit %q{
   open("tst-remove-load.rb", "w") {|f|
-    f << <<'End'
-module Kernel
-  remove_method :load
-end
-raise
-End
+    f << <<-'End'
+      module Kernel
+        remove_method :load
+      end
+      raise
+    End
   }
   load "tst-remove-load.rb"
 }, '[ruby-dev:40234] [ruby-core:27959]', :timeout => 1
