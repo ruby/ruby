de4d86803026d280255841959065c795bcc8ca68
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-02-23 17:01:08 +0000

    Add some more tests.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@26742 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/openssl/test_hmac.rb
+++ test/openssl/test_hmac.rb
@@ -8,11 +8,12 @@ if defined?(OpenSSL)
 
 class OpenSSL::TestHMAC < Test::Unit::TestCase
   def setup
-    @digest = OpenSSL::Digest::MD5.new
+    @digest = OpenSSL::Digest::MD5
     @key = "KEY"
     @data = "DATA"
     @h1 = OpenSSL::HMAC.new(@key, @digest)
-    @h2 = OpenSSL::HMAC.new(@key, @digest)
+    @h2 = OpenSSL::HMAC.new(@key, @digest.new)
+    @h3 = OpenSSL::HMAC.new(@key, "MD5")
   end
 
   def teardown
@@ -20,8 +21,19 @@ class OpenSSL::TestHMAC < Test::Unit::TestCase
 
   def test_hmac
     @h1.update(@data)
+    @h2.update(@data)
+    @h3.update(@data)
+    assert_equal(@h1.digest, @h2.digest)
+    assert_equal(@h2.digest, @h3.digest)
+
     assert_equal(OpenSSL::HMAC.digest(@digest, @key, @data), @h1.digest, "digest")
     assert_equal(OpenSSL::HMAC.hexdigest(@digest, @key, @data), @h1.hexdigest, "hexdigest")
+
+    assert_equal(OpenSSL::HMAC.digest(@digest.new, @key, @data), @h2.digest, "digest")
+    assert_equal(OpenSSL::HMAC.hexdigest(@digest.new, @key, @data), @h2.hexdigest, "hexdigest")
+
+    assert_equal(OpenSSL::HMAC.digest("MD5", @key, @data), @h3.digest, "digest")
+    assert_equal(OpenSSL::HMAC.hexdigest("MD5", @key, @data), @h3.hexdigest, "hexdigest")
   end
 
   def test_dup
