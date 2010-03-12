0cb87efacda609e4d2c8167a0650f64c08c04914
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-03-12 09:00:48 +0000

    * lib/uri/common.rb (URI.encode_www_form): new method to   generate URL-encoded form data. [ruby-dev:39246]
    * lib/uri/common.rb (URI.encode_www_component,
      URI.decode_www_component): new method for encode/decode
      a name/value of HTML form.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@26888 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/uri/test_common.rb
+++ test/uri/test_common.rb
@@ -49,6 +49,31 @@ class TestCommon < Test::Unit::TestCase
     assert_equal(expected, Kernel::URI("http://www.ruby-lang.org/"))
     assert_raise(NoMethodError) { Object.new.URI("http://www.ruby-lang.org/") }
   end
+
+  def test_encode_www_component
+    assert_equal("+%21%22%23%24%25%26%27%28%29*%2B%2C-.%2F09%3A%3B%3C%3D%3E%3F%40" \
+                 "AZ%5B%5C%5D%5E_%60az%7B%7C%7D%7E",
+                 URI.encode_www_component(" !\"\#$%&'()*+,-./09:;<=>?@AZ[\\]^_`az{|}~"))
+  end
+
+  def test_decode_www_component
+    assert_equal(" !\"\#$%&'()*+,-./09:;<=>?@AZ[\\]^_`az{|}~",
+                 URI.decode_www_component(
+                   "+%21%22%23%24%25%26%27%28%29*%2B%2C-.%2F09%3A%3B%3C%3D%3E%3F%40" \
+                   "AZ%5B%5C%5D%5E_%60az%7B%7C%7D%7E"))
+  end
+
+  def test_encode_www_form
+    assert_equal("a=1", URI.encode_www_form("a" => "1"))
+    assert_equal("a=1", URI.encode_www_form(a: 1))
+    assert_equal("a=1", URI.encode_www_form([["a", "1"]]))
+    assert_equal("a=1", URI.encode_www_form([[:a, 1]]))
+    expected = "a=1&%E3%81%82=%E6%BC%A2"
+    assert_equal(expected, URI.encode_www_form("a" => "1", "\u3042" => "\u6F22"))
+    assert_equal(expected, URI.encode_www_form(a: 1, :"\u3042" => "\u6F22"))
+    assert_equal(expected, URI.encode_www_form([["a", "1"], ["\u3042", "\u6F22"]]))
+    assert_equal(expected, URI.encode_www_form([[:a, 1], [:"\u3042", "\u6F22"]]))
+  end
 end
 
 
