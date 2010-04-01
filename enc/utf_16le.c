f03ca47f3b7411e2e3582503df7de1313e609ea8
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-04-01 05:30:25 +0000

    * enc/utf_16{be,le}.c (utf16{be,le}_mbc_to_code): simplified.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@27143 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- enc/utf_16le.c
+++ enc/utf_16le.c
@@ -102,9 +102,8 @@ utf16le_mbc_to_code(const UChar* p, const UChar* end ARG_UNUSED,
   UChar c1 = *(p+1);
 
   if (UTF16_IS_SURROGATE_FIRST(c1)) {
-    code = ((((c1 - 0xd8) << 2) + ((c0  & 0xc0) >> 6) + 1) << 16)
-         + ((((c0 & 0x3f) << 2) + (p[3] - 0xdc)) << 8)
-         + p[2];
+    code = ((((c1 << 8) + c0) & 0x03ff) << 10)
+         + (((p[3] << 8) + p[2]) & 0x03ff) + 0x10000;
   }
   else {
     code = c1 * 256 + p[0];
