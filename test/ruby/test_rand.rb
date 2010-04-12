eb4572da11988894d32b2b7f0a6cc557d3abcc68
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-04-12 20:50:45 +0000

    add a test.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@27325 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_rand.rb
+++ test/ruby/test_rand.rb
@@ -404,5 +404,12 @@ END
     rand_1 = Random.new(-1).rand
     assert_not_equal(rand_1, Random.new((1 << 31) -1).rand, "#{bug3104} (2)")
     assert_not_equal(rand_1, Random.new((1 << 63) -1).rand, "#{bug3104} (2)")
+
+    [-1, -2**10, -2**40].each {|n|
+      b = (2**64).coerce(n)[0]
+      r1 = Random.new(n).rand
+      r2 = Random.new(b).rand
+      assert_equal(r1, r2)
+    }
   end
 end
