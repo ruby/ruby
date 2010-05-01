9d094c7a250b55f509ff185f64aaf6a02fc226e5
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-05-01 00:43:10 +0000

    * lib/test/unit/assertions.rb (Test::Unit::Assertions#assert):   filter out traces in minitest directory.  [ruby-core:29908]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@27572 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/test/unit/assertions.rb
+++ lib/test/unit/assertions.rb
@@ -13,7 +13,7 @@ module Test
       def assert(test, msg = (nomsg = true; nil))
         unless nomsg or msg.instance_of?(String) or msg.instance_of?(Proc) or
             (bt = caller).shift.rindex(MiniTest::MINI_DIR, 0)
-          bt = MiniTest.filter_backtrace(bt)
+          bt = MiniTest.delete_if {|s| rindex(MiniTest::MINI_DIR, 0)}
           raise ArgumentError, "assertion message must be String or Proc, but #{msg.class} was given.", bt
         end
         super
