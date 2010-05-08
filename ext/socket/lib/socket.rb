1e7807bdf71b5cbc80b9832d6842a12b4b51c868
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-05-08 01:13:33 +0000

    * ext/socket/lib/socket.rb (BasicSocket#connect_address): MacOS X 10.6   returns "::ffff:a.b.c.d" for IPv4-mapped IPv6 address.   [ruby-dev:41215] patch by Tomoyuki Chikanaga.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@27663 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/socket/lib/socket.rb
+++ ext/socket/lib/socket.rb
@@ -192,6 +192,8 @@ class BasicSocket < IO
         addr = Addrinfo.new(["AF_INET6", addr.ip_port, nil, "::1"], addr.pfamily, addr.socktype, addr.protocol)
       elsif addr.ip_address == "0.0.0.0" # MacOS X 10.4 returns "a.b.c.d" for IPv4-mapped IPv6 address.
         addr = Addrinfo.new(["AF_INET6", addr.ip_port, nil, "::1"], addr.pfamily, addr.socktype, addr.protocol)
+      elsif addr.ip_address == "::ffff:0.0.0.0" # MacOS X 10.6 returns "::ffff:a.b.c.d" for IPv4-mapped IPv6 address.
+        addr = Addrinfo.new(["AF_INET6", addr.ip_port, nil, "::1"], addr.pfamily, addr.socktype, addr.protocol)
       end
     elsif defined?(Socket::AF_UNIX) && afamily == Socket::AF_UNIX
       raise SocketError, "unbound Unix socket" if addr.unix_path == ""
