95ecdee16c498695461dd68737f4bd069c044209
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-05-12 14:49:13 +0000

    * include/ruby/intern.h (rb_quad_pack): deprecated.   (rb_quad_unpack): ditto.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@27763 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/intern.h
+++ include/ruby/intern.h
@@ -101,8 +101,8 @@ VALUE rb_ull2inum(unsigned LONG_LONG);
 LONG_LONG rb_big2ll(VALUE);
 unsigned LONG_LONG rb_big2ull(VALUE);
 #endif  /* HAVE_LONG_LONG */
-void rb_quad_pack(char*,VALUE);
-VALUE rb_quad_unpack(const char*,int);
+DEPRECATED(void rb_quad_pack(char*,VALUE));
+DEPRECATED(VALUE rb_quad_unpack(const char*,int));
 void rb_big_pack(VALUE val, unsigned long *buf, long num_longs);
 VALUE rb_big_unpack(unsigned long *buf, long num_longs);
 int rb_uv_to_utf8(char[6],unsigned long);
