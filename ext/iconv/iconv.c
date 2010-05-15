9d6194ab18dc064e4979c4fb84411cfb4a7efa6b
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-05-15 08:16:18 +0000

    * ext/iconv/iconv.c (rb_iconv_sys_fail): raise BrokenLibrary if   errno is not set.  [ruby-dev:41317]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@27800 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/iconv/iconv.c
+++ ext/iconv/iconv.c
@@ -146,6 +146,18 @@ map_charset
     return StringValuePtr(*code);
 }
 
+NORETURN(static void rb_iconv_sys_fail(const char *s));
+static void
+rb_iconv_sys_fail(const char *s)
+{
+    if (errno == 0) {
+	rb_raise(rb_eIconvBrokenLibrary, "%s", s);
+    }
+    rb_sys_fail(s);
+}
+
+#define rb_sys_fail(s) rb_iconv_sys_fail(s)
+
 static iconv_t
 iconv_create
 #ifdef HAVE_PROTOTYPES
