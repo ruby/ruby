37910b13fbc1d6831b5623368472497629b36ae9
tenderlove <tenderlove@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-05-16 02:33:05 +0000

    * ext/psych/lib/psych/visitors/json_tree.rb: using factory methods for   node creation * ext/psych/lib/psych/visitors/yaml_tree.rb: ditto
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@27806 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/psych/lib/psych/visitors/json_tree.rb
+++ ext/psych/lib/psych/visitors/json_tree.rb
@@ -1,15 +1,25 @@
 module Psych
   module Visitors
     class JSONTree < YAMLTree
-      def visit_Symbol o
-        append create_scalar o.to_s
-      end
-
       def visit_NilClass o
-        scalar = Nodes::Scalar.new(
+        scalar = create_scalar(
           'null', nil, nil, true, false, Nodes::Scalar::PLAIN)
         append scalar
       end
+
+      def visit_Integer o
+        append create_scalar(o.to_s, nil, nil, true, false, Nodes::Scalar::PLAIN)
+      end
+
+      def visit_Float o
+        return super if o.nan? || o.infinite?
+        visit_Integer o
+      end
+
+      def visit_String o
+        append create_scalar o.to_s
+      end
+      alias :visit_Symbol :visit_String
 
       private
       def create_document
@@ -26,7 +36,7 @@ module Psych
       end
 
       def create_scalar value, anchor = nil, tag = nil, plain = false, quoted = true, style = Nodes::Scalar::ANY
-        super(value, anchor, tag, false, true, style)
+        super
       end
 
       def create_sequence anchor = nil, tag = nil, implicit = true, style = Nodes::Sequence::FLOW
