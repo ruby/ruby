f8d97b0026284219aca766db4fa55d6c7357dd4d
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-05-17 06:28:16 +0000

    * enc/iso_2022_jp.h: add CP50220.
    * enc/trans/iso2022.trans: add converter for CP50220.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@27860 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- enc/iso_2022_jp.h
+++ enc/iso_2022_jp.h
@@ -5,16 +5,34 @@ ENC_ALIAS("ISO2022-JP", "ISO-2022-JP");
 ENC_REPLICATE("ISO-2022-JP-2", "ISO-2022-JP");
 ENC_ALIAS("ISO2022-JP2", "ISO-2022-JP-2");
 
-/* Windows Codepage 50221
+/* Windows Codepage 50220
  * a ISO-2022-JP variant.
  * This includes
  * * US-ASCII
+ * * JIS X 0201 Latin
  * * JIS X 0201 Katakana
  * * JIS X 0208
  * * NEC special characters
  * * NEC selected IBM extended characters
  * and this implementation doesn't include
+ * * User Defined Characters
+ *
+ * So this CP50220 has the same characters of CP51932.
+ *
+ * See http://legacy-encoding.sourceforge.jp/wiki/index.php?cp50220
+ */
+ENC_REPLICATE("CP50220", "ISO-2022-JP");
+
+/* Windows Codepage 50221
+ * a ISO-2022-JP variant.
+ * This includes
+ * * US-ASCII
  * * JIS X 0201 Latin
+ * * JIS X 0201 Katakana
+ * * JIS X 0208
+ * * NEC special characters
+ * * NEC selected IBM extended characters
+ * and this implementation doesn't include
  * * User Defined Characters
  *
  * So this CP50221 has the same characters of CP51932.
