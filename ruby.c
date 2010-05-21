f71de5c1d16f17f7974aaa5093885ede36f8371f
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-05-21 04:35:58 +0000

    * ruby.c (ruby_init_loadpath_safe): use real path for non-shared   build.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@27939 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ruby.c
+++ ruby.c
@@ -370,9 +370,12 @@ ruby_init_loadpath_safe(int safe_level)
 #elif defined(HAVE_DLADDR)
     Dl_info dli;
     if (dladdr(expand_include_path, &dli)) {
-	VALUE fname = rb_str_new_cstr(dli.dli_fname);
-	sopath = rb_file_absolute_path(fname, Qnil);
-	rb_str_resize(fname, 0);
+	VALUE rb_realpath_internal(VALUE basedir, VALUE path, int strict);
+	char fbuf[MAXPATHLEN];
+	char *f = dln_find_file_r(dli.dli_fname, getenv(PATH_ENV), fbuf, sizeof(fbuf));
+	VALUE fname = rb_str_new_cstr(f);
+	rb_str_freeze(fname);
+	sopath = rb_realpath_internal(Qnil, fname, 1);
     }
     else {
 	sopath = rb_str_new(0, 0);
