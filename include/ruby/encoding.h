1fd361834f3e02684e5eca88fc49e1f1aa6c6994
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-05-27 14:47:16 +0000

    * include/ruby/encoding.h (rb_filesystem_encindex): defined.
    * include/ruby/encoding.h (rb_locale_encindex): ditto.
    
    * encoding.c (rb_filesystem_encindex): remove static.
    
    * encoding.c (rb_locale_encindex): ditto.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@28037 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/encoding.h
+++ include/ruby/encoding.h
@@ -195,6 +195,8 @@ rb_encoding *rb_default_internal_encoding(void);
 int rb_ascii8bit_encindex(void);
 int rb_utf8_encindex(void);
 int rb_usascii_encindex(void);
+int rb_locale_encindex(void);
+int rb_filesystem_encindex(void);
 VALUE rb_enc_default_external(void);
 VALUE rb_enc_default_internal(void);
 void rb_enc_set_default_external(VALUE encoding);
