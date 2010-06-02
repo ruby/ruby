73d647e8bf9bc57c40583ac7dd9801b701bdf36e
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-06-02 14:06:25 +0000

    * test/ruby/test_{string,symbol}.rb (test_ascii_incomat_inspect): 	  ASCII range characters are no longer escaped.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@28128 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_symbol.rb
+++ test/ruby/test_symbol.rb
@@ -140,7 +140,8 @@ class TestSymbol < Test::Unit::TestCase
   def test_ascii_incomat_inspect
     [Encoding::UTF_16LE, Encoding::UTF_16BE,
      Encoding::UTF_32LE, Encoding::UTF_32BE].each do |e|
-      assert_equal(':"\\u0061\\u0062\\u0063"', "abc".encode(e).to_sym.inspect)
+      assert_equal(':"abc"', "abc".encode(e).to_sym.inspect)
+      assert_equal(':"\\u3042\\u3044\\u3046"', "\u3042\u3044\u3046".encode(e).to_sym.inspect)
     end
   end
 end
