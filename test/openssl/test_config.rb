43fa9ba75b78e314d914653fbc2f6065938901c9
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-06-03 01:17:58 +0000

    * test/openssl/test_config.rb (test_freeze): skip on Windows because Config.new   needs an argument on it.  When the maintainer returns, this test shall be   corrected.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_2@28140 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/openssl/test_config.rb
+++ test/openssl/test_config.rb
@@ -3,6 +3,7 @@ require "test/unit"
 
 class OpenSSL::TestConfig < Test::Unit::TestCase
   def test_freeze
+    skip "need an argument for OpenSSL::Config.new on Windows" if /mswin|mingw/ =~ RUBY_PLATFORM
     c = OpenSSL::Config.new
     c['foo'] = [['key', 'value']]
     c.freeze
