55789617ae7178b4bebf84173c974451aca8dd9a
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-06-03 09:17:49 +0000

    * test/rake/test_win32.rb (Rake::TestWin32): update tests.   [ruby-core:30309]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@28145 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/rake/test_win32.rb
+++ test/rake/test_win32.rb
@@ -9,44 +9,21 @@ class Rake::TestWin32 < Test::Unit::TestCase
   Win32 = Rake::Win32
 
   def test_win32_system_dir_uses_home_if_defined
-    in_environment('RAKE_SYSTEM' => nil, 'HOME' => 'C:\\HP') do
-      assert_equal "C:/HP/Rake", Win32.win32_system_dir
-    end
-  end
-
-  def test_win32_system_dir_uses_homedrive_homepath_when_no_home_defined
-    in_environment(
-      'RAKE_SYSTEM' => nil,
-      'HOME' => nil,
-      'HOMEDRIVE' => "C:",
-      'HOMEPATH' => "\\HP"
+    in_environment('RAKE_SYSTEM' => nil,
+      'HOME' => "C:\\HP",
+      'APPDATA' => nil
       ) do
       assert_equal "C:/HP/Rake", Win32.win32_system_dir
     end
   end
 
-  def test_win32_system_dir_uses_appdata_when_no_home_or_home_combo
+  def test_win32_system_dir_uses_appdata_if_defined
     in_environment(
       'RAKE_SYSTEM' => nil,
-      'HOME' => nil,
-      'HOMEDRIVE' => nil,
-      'HOMEPATH' => nil,
+      'HOME' => "C:\\HP",
       'APPDATA' => "C:\\Documents and Settings\\HP\\Application Data"
       ) do
       assert_equal "C:/Documents and Settings/HP/Application Data/Rake", Win32.win32_system_dir
-    end
-  end
-
-  def test_win32_system_dir_fallback_to_userprofile_otherwise
-    in_environment(
-      'RAKE_SYSTEM' => nil,
-      'HOME' => nil,
-      'HOMEDRIVE' => nil,
-      'HOMEPATH' => nil,
-      'APPDATA' => nil,
-      'USERPROFILE' => "C:\\Documents and Settings\\HP"
-      ) do
-      assert_equal "C:/Documents and Settings/HP/Rake", Win32.win32_system_dir
     end
   end
 
@@ -57,9 +34,9 @@ class Rake::TestWin32 < Test::Unit::TestCase
       'HOMEDRIVE' => nil,
       "HOMEPATH" => nil,
       'APPDATA' => nil,
-      "USERPROFILE" => nil
+      'USERPROFILE' => "C:\\Documents and Settings\\HP"
       ) do
-      assert_raise(Rake::Win32::Win32HomeError) do
+      assert_raise(ArgumentError) do
         Win32.win32_system_dir
       end
     end
