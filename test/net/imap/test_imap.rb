fd71d468a667a7fd4ebf6f131e4556e5b58095e8
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-06-08 07:05:36 +0000

    merge revision(s) 27487: 	* lib/net/imap (encode_utf7): encode & properly.  Thanks, Kengo 	  Matsuyama.  [ruby-dev:38063]  backported from trunk.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_7@28214 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/net/imap/test_imap.rb
+++ test/net/imap/test_imap.rb
@@ -8,4 +8,20 @@ class IMAPTest < Test::Unit::TestCase
     assert_equal("OK", r.name)
     assert_equal("NOMODSEQ", r.data.code.name)
   end
+
+  def test_encode_utf7
+    utf8 = "\357\274\241\357\274\242\357\274\243"
+    s = Net::IMAP.encode_utf7(utf8)
+    assert_equal("&,yH,Iv8j-", s)
+
+    utf8 = "\343\201\202&"
+    s = Net::IMAP.encode_utf7(utf8)
+    assert_equal("&MEI-&-", s)
+  end
+
+  def test_decode_utf7
+    s = Net::IMAP.decode_utf7("&,yH,Iv8j-")
+    utf8 = "\357\274\241\357\274\242\357\274\243"
+    assert_equal(utf8, s)
+  end
 end
