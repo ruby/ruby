dffac2eef3399023027db34b932f07a9a3a47980
mame <mame@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-06-11 15:40:39 +0000

    * ext/dbm/extconf.rb: accept Berkley DB version 3, 4 and 5.   [ruby-dev:41531]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@28284 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/dbm/extconf.rb
+++ ext/dbm/extconf.rb
@@ -5,13 +5,16 @@ dir_config("dbm")
 if dblib = with_config("dbm-type", nil)
   dblib = dblib.split(/[ ,]+/)
 else
-  dblib = %w(db db2 db1 dbm gdbm gdbm_compat qdbm)
+  dblib = %w(db db2 db1 db5 db4 db3 dbm gdbm gdbm_compat qdbm)
 end
 
 headers = {
   "db" => ["db.h"],
   "db1" => ["db1/ndbm.h", "db1.h", "ndbm.h"],
   "db2" => ["db2/db.h", "db2.h", "db.h"],
+  "db3" => ["db3/db.h", "db3.h", "db.h"],
+  "db4" => ["db4/db.h", "db4.h", "db.h"],
+  "db5" => ["db5/db.h", "db5.h", "db.h"],
   "dbm" => ["ndbm.h"],
   "gdbm" => ["gdbm-ndbm.h", "ndbm.h"],
   "gdbm_compat" => ["gdbm-ndbm.h", "ndbm.h"],
@@ -24,7 +27,7 @@ def headers.db_check(db)
   hsearch = nil
 
   case db
-  when /^db2?$/
+  when /^db[2-5]?$/
     db_prefix = "__db_n"
     hsearch = "-DDB_DBM_HSEARCH "
   when "gdbm"
@@ -36,7 +39,8 @@ def headers.db_check(db)
   db_prefix ||= ""
 
   if (have_library(db, db_prefix+"dbm_open") || have_func(db_prefix+"dbm_open")) and
-      hdr = self.fetch(db, ["ndbm.h"]).find {|h| have_type("DBM", h, hsearch)}
+      hdr = self.fetch(db, ["ndbm.h"]).find {|h| have_type("DBM", h, hsearch)} or
+      hdr = self.fetch(db, ["ndbm.h"]).find {|h| have_type("DBM", ["db.h", h], hsearch)}
     have_func(db_prefix+"dbm_clearerr") unless have_gdbm
     $defs << hsearch if hsearch
     $defs << '-DDBM_HDR="<'+hdr+'>"'
