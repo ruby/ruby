74d40e41961903f91894463504d4af60acc93058
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-07-08 14:39:09 +0000

    update doc.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@28583 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- time.c
+++ time.c
@@ -4299,60 +4299,84 @@ strftimev(const char *fmt, VALUE time)
  *  string. Any text not listed as a directive will be passed through
  *  to the output string.
  *
- *  Format meaning:
- *    %a - The abbreviated weekday name (``Sun'')
- *    %A - The  full  weekday  name (``Sunday'')
- *    %b - The abbreviated month name (``Jan'')
- *    %B - The  full  month  name (``January'')
- *    %c - The preferred local date and time representation
- *    %C - Century (20 in 2009)
- *    %d - Day of the month (01..31)
- *    %D - Date (%m/%d/%y)
- *    %e - Day of the month, blank-padded ( 1..31)
- *    %F - Equivalent to %Y-%m-%d (the ISO 8601 date format)
- *    %g - The last 2 digits of the week-based year according to ISO 8601
- *    %G - The week-based year according to ISO 8601
- *    %h - Equivalent to %b
- *    %H - Hour of the day, 24-hour clock (00..23)
- *    %I - Hour of the day, 12-hour clock (01..12)
- *    %j - Day of the year (001..366)
- *    %k - hour, 24-hour clock, blank-padded ( 0..23)
- *    %l - hour, 12-hour clock, blank-padded ( 0..12)
- *    %L - Millisecond of the second (000..999)
- *    %m - Month of the year (01..12)
- *    %M - Minute of the hour (00..59)
- *    %n - Newline (\n)
- *    %N - Fractional seconds digits, default is 9 digits (nanosecond)
- *            %3N  millisecond (3 digits)
- *            %6N  microsecond (6 digits)
- *            %9N  nanosecond (9 digits)
- *    %p - Meridian indicator (``AM''  or  ``PM'')
- *    %P - Meridian indicator (``am''  or  ``pm'')
- *    %r - time, 12-hour (same as %I:%M:%S %p)
- *    %R - time, 24-hour (%H:%M)
- *    %s - Number of seconds since 1970-01-01 00:00:00 UTC.
- *    %S - Second of the minute (00..60)
- *    %t - Tab character (\t)
- *    %T - time, 24-hour (%H:%M:%S)
- *    %u - Day of the week as a decimal, Monday being 1. (1..7)
- *    %U - Week  number  of the current year,
- *            starting with the first Sunday as the first
- *            day of the first week (00..53)
- *    %v - VMS date (%e-%b-%Y)
- *    %V - Week number of year according to ISO 8601 (01..53)
- *    %W - Week  number  of the current year,
- *            starting with the first Monday as the first
- *            day of the first week (00..53)
- *    %w - Day of the week (Sunday is 0, 0..6)
- *    %x - Preferred representation for the date alone, no time
- *    %X - Preferred representation for the time alone, no date
- *    %y - Year without a century (00..99)
- *    %Y - Year with century
- *    %z - Time zone as hour and minute offset from UTC (e.g. +0900)
- *            %:z - hour and minute offset from UTC with a colon (e.g. +09:00)
- *            %::z - hour, minute and second offset from UTC (e.g. +09:00:00)
- *    %Z - Time zone name
- *    %% - Literal ``%'' character
+ *  Format directives:
+ *
+ *    Date (Year, Month, Day):
+ *      %Y - Year with century
+ *      %C - Century (20 in 2009)
+ *      %y - Year without a century (00..99)
+ *      
+ *      %m - Month of the year (01..12)
+ *      %B - The full month name (``January'')
+ *      %b - The abbreviated month name (``Jan'')
+ *      %h - Equivalent to %b
+ *      
+ *      %d - Day of the month (01..31)
+ *      %e - Day of the month, blank-padded ( 1..31)
+ *
+ *      %j - Day of the year (001..366)
+ *      
+ *    Time (Hour, Minute, Second, Subsecond):
+ *      %H - Hour of the day, 24-hour clock (00..23)
+ *      %k - hour, 24-hour clock, blank-padded ( 0..23)
+ *      %I - Hour of the day, 12-hour clock (01..12)
+ *      %l - hour, 12-hour clock, blank-padded ( 0..12)
+ *      %P - Meridian indicator (``am'' or ``pm'')
+ *      %p - Meridian indicator (``AM'' or ``PM'')
+ *      
+ *      %M - Minute of the hour (00..59)
+ *      
+ *      %S - Second of the minute (00..60)
+ *      
+ *      %L - Millisecond of the second (000..999)
+ *      %N - Fractional seconds digits, default is 9 digits (nanosecond)
+ *              %3N  millisecond (3 digits)
+ *              %6N  microsecond (6 digits)
+ *              %9N  nanosecond (9 digits)
+ *      
+ *    Time zone:
+ *      %z - Time zone as hour and minute offset from UTC (e.g. +0900)
+ *              %:z - hour and minute offset from UTC with a colon (e.g. +09:00)
+ *              %::z - hour, minute and second offset from UTC (e.g. +09:00:00)
+ *      %Z - Time zone name
+ *      
+ *    Weekday:
+ *      %a - The abbreviated weekday name (``Sun'')
+ *      %A - The full weekday name (``Sunday'')
+ *      %u - Day of the week (Monday is 1, 1..7)
+ *      %w - Day of the week (Sunday is 0, 0..6)
+ *      
+ *    ISO 8601 week-based year:
+ *      %G - The week-based year according to ISO 8601
+ *      %g - The last 2 digits of the week-based year according to ISO 8601 (00..99)
+ *      %V - Week number of year according to ISO 8601 (01..53)
+ *      
+ *    Week number:
+ *      %U - Week number of the current year,
+ *              starting with the first Sunday as the first
+ *              day of the first week (00..53)
+ *      %W - Week number of the current year,
+ *              starting with the first Monday as the first
+ *              day of the first week (00..53)
+ *      
+ *    Seconds since the Epoch:
+ *      %s - Number of seconds since 1970-01-01 00:00:00 UTC.
+ *      
+ *    Literal string:
+ *      %n - Newline (\n)
+ *      %t - Tab character (\t)
+ *      %% - Literal ``%'' character
+ *      
+ *    Combination:
+ *      %c - The preferred local date and time representation (%a %b %e %H:%M:%S %Y)
+ *      %D - Date (%m/%d/%y)
+ *      %F - The ISO 8601 date format (%Y-%m-%d)
+ *      %v - VMS date (%e-%b-%Y)
+ *      %x - Preferred representation for the date alone, no time (%m/%d/%y)
+ *      %X - Preferred representation for the time alone, no date (%H:%M:%S)
+ *      %r - 12-hour time (%I:%M:%S %p)
+ *      %R - 24-hour time (%H:%M)
+ *      %T - 24-hour time (%H:%M:%S)
  *
  *     t = Time.now                        #=> 2007-11-19 08:37:48 -0600
  *     t.strftime("Printed on %m/%d/%Y")   #=> "Printed on 11/19/2007"
