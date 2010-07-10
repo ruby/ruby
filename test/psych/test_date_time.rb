12ce5781965faa81fafb7c20d47429111ba50f31
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-07-10 04:05:47 +0000

    merges r28541 from trunk into ruby_1_9_2. -- * ext/psych/lib/psych/scalar_scanner.rb (parse_string): support   timezones that are not one hour off. [ruby-core:31023] * ext/psych/lib/psych/visitors/yaml_tree.rb: ditto -- * test/psych/test_date_time.rb (TestDateTime#test_round_trip_with_offset):   test for [ruby-core:31023].
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_2@28601 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/psych/test_date_time.rb
+++ test/psych/test_date_time.rb
@@ -13,5 +13,11 @@ module Psych
       dt = DateTime.now
       assert_cycle dt
     end
+
+    def test_round_trip_with_offset
+      dt = DateTime.now
+      dt = dt.new_offset(Rational(3671, 60 * 60 * 24))
+      assert_cycle dt
+    end
   end
 end
