29b4247ca1fc4a70ca99b1f99fbc552f78780ff0
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-07-13 04:15:30 +0000

    * missing/close.c: keep original errno.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@28623 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- missing/close.c
+++ missing/close.c
@@ -8,12 +8,15 @@ int
 ruby_getpeername(int s, struct sockaddr * name,
          socklen_t * namelen)
 {
+    int err = errno;
     errno = 0;
     s = getpeername(s, name, namelen);
     if (errno == ECONNRESET) {
 	errno = 0;
 	s = 0;
     }
+    else if (errno == 0)
+	errno = err;
     return s;
 }
 
@@ -21,35 +24,44 @@ int
 ruby_getsockname(int s, struct sockaddr * name,
          socklen_t * namelen)
 {
+    int err = errno;
     errno = 0;
     s = getsockname(s, name, namelen);
     if (errno == ECONNRESET) {
 	errno = 0;
 	s = 0;
     }
+    else if (errno == 0)
+	errno = err;
     return s;
 }
 
 int
 ruby_shutdown(int s, int how)
 {
+    int err = errno;
     errno = 0;
     s = shutdown(s, how);
     if (errno == ECONNRESET) {
 	errno = 0;
 	s = 0;
     }
+    else if (errno == 0)
+	errno = err;
     return s;
 }
 
 int
 ruby_close(int s)
 {
+    int err = errno;
     errno = 0;
     s = close(s);
     if (errno == ECONNRESET) {
 	errno = 0;
 	s = 0;
     }
+    else if (errno == 0)
+	errno = err;
     return s;
 }
