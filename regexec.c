25380b0a57d5c5442fedefe6be7f025ace5bd1b5
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-07-24 05:33:32 +0000

    merges r28637 from trunk into ruby_1_9_2. -- * regexec.c (match_at): add end point to enclen's argument.   This only effect on compilinig with -DONIG_DEBUG_MATCH.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_2@28737 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- regexec.c
+++ regexec.c
@@ -1300,13 +1300,13 @@ match_at(regex_t* reg, const UChar* str, const UChar* end,
   s = (UChar* )sstart;
   while (1) {
 #ifdef ONIG_DEBUG_MATCH
-    {
+    if (s) {
       UChar *q, *bp, buf[50];
       int len;
       fprintf(stderr, "%4d> \"", (int )(s - str));
       bp = buf;
       for (i = 0, q = s; i < 7 && q < end; i++) {
-	len = enclen(encode, q);
+	len = enclen(encode, q, end);
 	while (len-- > 0) *bp++ = *q++;
       }
       if (q < end) { xmemcpy(bp, "...\"", 4); bp += 4; }
