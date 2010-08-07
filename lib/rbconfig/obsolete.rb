5f5e0b7191f0ecf00f0c1718c9562fee6112f04a
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-08-07 22:50:05 +0000

    * lib/rbconfig/obsolete.rb: show the location which use Config.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@28911 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/rbconfig/obsolete.rb
+++ lib/rbconfig/obsolete.rb
@@ -1,2 +1,5 @@
-warn "Use RbConfig instead of obsolete and deprecated Config."
+loc = caller[0]
+loc = loc[/\A.*:\d+:/] if loc
+loc = "#{loc} " if loc
+warn "#{loc}Use RbConfig instead of obsolete and deprecated Config."
 Config = RbConfig # compatibility for ruby-1.8.4 and older.
