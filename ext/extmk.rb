e4e9758aa5a096ccf7679dace5ef52a00b959a92
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-08-07 08:18:42 +0000

    * ext/extmk.rb (extract_makefile): follow the change of   install-rb-default line format at r28850, which causes second   run to create dummy makefiles.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@28899 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/extmk.rb
+++ ext/extmk.rb
@@ -50,7 +50,7 @@ def extract_makefile(makefile, keep = true)
     return keep
   end
   installrb = {}
-  m.scan(/^install-rb-default:[ \t]*(\S+)\n\1:[ \t]*(\S+)/) {installrb[$2] = $1}
+  m.scan(/^install-rb-default:.*[ \t](\S+)(?:[ \t].*)?\n\1:[ \t]*(\S+)/) {installrb[$2] = $1}
   oldrb = installrb.keys.sort
   newrb = install_rb(nil, "").collect {|d, *f| f}.flatten.sort
   if target_prefix = m[/^target_prefix[ \t]*=[ \t]*\/(.*)/, 1]
