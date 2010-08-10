f89a50b62c576b38796f9f41dac9b840945e230b
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-08-10 11:17:00 +0000

    * ext/dl/win32/lib/win32/resolv.rb (get_info): get rid of a warning.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@28951 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/dl/win32/lib/win32/resolv.rb
+++ ext/dl/win32/lib/win32/resolv.rb
@@ -73,7 +73,7 @@ if info.unpack('V5')[4] == 2  # VER_PLATFORM_WIN32_NT
               unless nvdom.empty?
                 @search = [ nvdom ]
                 if reg.read_i('UseDomainNameDevolution') != 0
-                  if /^[\w\d]+\./ =~ nvdom
+                  if /^\w+\./ =~ nvdom
                     devo = $'
                   end
                 end
