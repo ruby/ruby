05745ccfb367ab670ebbad8c992edf2cf70d8b4b
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-08-17 04:02:32 +0000

    * ext/pathname/pathname.c (path_dirname): Pathname#dirname translated   from pathname.rb.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@29020 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/pathname/pathname.c
+++ ext/pathname/pathname.c
@@ -439,6 +439,17 @@ path_basename(int argc, VALUE *argv, VALUE self)
 }
 
 /*
+ * See <tt>File.dirname</tt>.  Returns all but the last component of the path.
+ */
+static VALUE
+path_dirname(VALUE self)
+{
+    VALUE str = get_strpath(self);
+    str = rb_funcall(rb_cFile, rb_intern("dirname"), 1, str);
+    return rb_class_new_instance(1, &str, rb_obj_class(self));
+}
+
+/*
  * == Pathname
  *
  * Pathname represents a pathname which locates a file in a filesystem.
@@ -658,4 +669,5 @@ Init_pathname()
     rb_define_method(rb_cPathname, "truncate", path_truncate, 1);
     rb_define_method(rb_cPathname, "utime", path_utime, 2);
     rb_define_method(rb_cPathname, "basename", path_basename, -1);
+    rb_define_method(rb_cPathname, "dirname", path_dirname, 0);
 }
