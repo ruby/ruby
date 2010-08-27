ruby/ruby

commit 9be503f332515719475216e7788e6a7fab6c78d0

* math.c (math_atan2): change the behavior when x and y are zero. * test/ruby/test_math.rb (test_atan2): add tests for the above   changes.
git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@29115 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

math.c
@@ -55,7 +55,13 @@ math_atan2(VALUE obj, VALUE y, VALUE x)
     Need_Float2(y, x);
     dx = RFLOAT_VALUE(x);
     dy = RFLOAT_VALUE(y);
-    if (dx == 0.0 && dy == 0.0) domain_error("atan2");
+    if (dx == 0.0 && dy == 0.0) {
+	if (!signbit(dx))
+	    return DBL2NUM(dy);
+        if (!signbit(dy))
+	    return DBL2NUM(M_PI);
+	return DBL2NUM(-M_PI);
+    }
     if (isinf(dx) && isinf(dy)) domain_error("atan2");
     return DBL2NUM(atan2(dy, dx));
 }

test/ruby/test_math.rb
@@ -17,7 +17,10 @@ class TestMath < Test::Unit::TestCase
   end
 
   def test_atan2
-    assert_raise(Math::DomainError) { Math.atan2(0, 0) }
+    check(+0.0, Math.atan2(+0.0, +0.0))
+    check(-0.0, Math.atan2(-0.0, +0.0))
+    check(+Math::PI, Math.atan2(+0.0, -0.0))
+    check(-Math::PI, Math.atan2(-0.0, -0.0))
     assert_raise(Math::DomainError) { Math.atan2(Float::INFINITY, Float::INFINITY) }
     assert_raise(Math::DomainError) { Math.atan2(Float::INFINITY, -Float::INFINITY) }
     assert_raise(Math::DomainError) { Math.atan2(-Float::INFINITY, Float::INFINITY) }

commit 0cfe185f39d575f9c620bd14707a81bd5c29fe11

* math.c (math_atan2): you should know that M_PI is not the feature   of C90.   fixed build failure caused by r29115.
git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@29119 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

math.c
@@ -51,6 +51,9 @@ extern VALUE rb_to_float(VALUE val);
 static VALUE
 math_atan2(VALUE obj, VALUE y, VALUE x)
 {
+#ifndef M_PI
+# define M_PI 3.14159265358979323846
+#endif
     double dx, dy;
     Need_Float2(y, x);
     dx = RFLOAT_VALUE(x);