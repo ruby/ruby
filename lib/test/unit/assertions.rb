e1d15712565ef38f7acd605dc1aafb3884b6a008
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-10-22 05:10:40 +0000

    * lib/test/unit/assertions.rb (Test::Unit::Assertions#assert):   treat nil case. Please run test-all before commit such change.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@29554 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/test/unit/assertions.rb
+++ lib/test/unit/assertions.rb
@@ -14,7 +14,7 @@ module Test
 
       def assert(test, msg = UNASSIGNED)
         msg = nil if msg == UNASSIGNED
-        unless String === msg or Proc === msg then
+        unless String === msg or Proc === msg or msg.nil? then
           bt = caller.reject { |s| s.rindex(MiniTest::MINI_DIR, 0) }
           raise ArgumentError, "assertion message must be String or Proc, but #{msg.class} was given.", bt
         end
