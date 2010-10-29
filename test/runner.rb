32623a16276b8c99a8c142c570cb2f50dc3df737
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-10-29 14:35:53 +0000

    * test/profile_test_all.rb: added.   You can use test-all profiler with the following command:   RUBY_TEST_ALL_PROFILE=true make test-all   This command generates ./test_all_profile and you can analyse   which tests consume memories. * test/runner.rb: ditto.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@29627 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/runner.rb
+++ test/runner.rb
@@ -6,6 +6,8 @@ require 'test/unit'
 src_testdir = File.dirname(File.expand_path(__FILE__))
 srcdir = File.dirname(src_testdir)
 
+require_relative 'profile_test_all' if ENV['RUBY_TEST_ALL_PROFILE'] == 'true'
+
 tests = Test::Unit.new {|files, options|
   options[:base_directory] = src_testdir
   if files.empty?
