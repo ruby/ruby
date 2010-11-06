37e7bf7619f61b1a523652bbc8033a55daa7be0e
tenderlove <tenderlove@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-11-06 21:19:11 +0000

    removing empty teardown, cleaning up whitespace errors
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@29709 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/openssl/test_ocsp.rb
+++ test/openssl/test_ocsp.rb
@@ -26,18 +26,15 @@ class OpenSSL::TestOCSP < Test::Unit::TestCase
        subj, @key, serial, now, now+3600, [], @ca_cert, nil, dgst)
   end
 
-  def teardown
-  end
-
   def test_new_certificate_id
     cid = OpenSSL::OCSP::CertificateId.new(@cert, @ca_cert)
-    assert_kind_of OpenSSL::OCSP::CertificateId, cid 
+    assert_kind_of OpenSSL::OCSP::CertificateId, cid
     assert_equal @cert.serial, cid.serial
   end
 
   def test_new_certificate_id_with_digest
     cid = OpenSSL::OCSP::CertificateId.new(@cert, @ca_cert, OpenSSL::Digest::SHA256.new)
-    assert_kind_of OpenSSL::OCSP::CertificateId, cid 
+    assert_kind_of OpenSSL::OCSP::CertificateId, cid
     assert_equal @cert.serial, cid.serial
   end
 
