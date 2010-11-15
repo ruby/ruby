0dbe6b611339af6a95eadd56d327d27c1ae9131b
mame <mame@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-11-15 16:06:35 +0000

    * regparse.c (and_cclass, or_cclass): fix memory leak.  Coverity Scan   found this bug.  [ruby-dev:42579]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@29803 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- regparse.c
+++ regparse.c
@@ -2006,7 +2006,7 @@ and_cclass(CClassNode* dest, CClassNode* cc, ScanEnv* env)
 {
   OnigEncoding enc = env->enc;
   int r, not1, not2;
-  BBuf *buf1, *buf2, *pbuf;
+  BBuf *buf1, *buf2, *pbuf = 0;
   BitSetRef bsr1, bsr2;
   BitSet bs1, bs2;
 
@@ -2041,17 +2041,16 @@ and_cclass(CClassNode* dest, CClassNode* cc, ScanEnv* env)
     else {
       r = and_code_range_buf(buf1, not1, buf2, not2, &pbuf, env);
       if (r == 0 && not1 != 0) {
-	BBuf *tbuf;
+	BBuf *tbuf = 0;
 	r = not_code_range_buf(enc, pbuf, &tbuf, env);
-	if (r != 0) {
-	  bbuf_free(pbuf);
-	  return r;
-	}
 	bbuf_free(pbuf);
 	pbuf = tbuf;
       }
     }
-    if (r != 0) return r;
+    if (r != 0) {
+	bbuf_free(pbuf);
+	return r;
+    }
 
     dest->mbuf = pbuf;
     bbuf_free(buf1);
@@ -2065,7 +2064,7 @@ or_cclass(CClassNode* dest, CClassNode* cc, ScanEnv* env)
 {
   OnigEncoding enc = env->enc;
   int r, not1, not2;
-  BBuf *buf1, *buf2, *pbuf;
+  BBuf *buf1, *buf2, *pbuf = 0;
   BitSetRef bsr1, bsr2;
   BitSet bs1, bs2;
 
@@ -2100,17 +2099,16 @@ or_cclass(CClassNode* dest, CClassNode* cc, ScanEnv* env)
     else {
       r = or_code_range_buf(enc, buf1, not1, buf2, not2, &pbuf, env);
       if (r == 0 && not1 != 0) {
-	BBuf *tbuf;
+	BBuf *tbuf = 0;
 	r = not_code_range_buf(enc, pbuf, &tbuf, env);
-	if (r != 0) {
-	  bbuf_free(pbuf);
-	  return r;
-	}
 	bbuf_free(pbuf);
 	pbuf = tbuf;
       }
     }
-    if (r != 0) return r;
+    if (r != 0) {
+	bbuf_free(pbuf);
+	return r;
+    }
 
     dest->mbuf = pbuf;
     bbuf_free(buf1);
