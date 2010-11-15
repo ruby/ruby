856e1f17f1846a2ad077aa7b0da6a2b8399fd7b7
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-11-15 11:43:42 +0000

    merges r29237 and r29238 from trunk into ruby_1_9_2. -- * ext/openssl/ossl_bn.c (ossl_bn_is_prime): fix comparison   with rb_scan_args. Before this fix, OpenSSL::BN#prime?   is fully broken. -- Add ML ref [ruby-dev:42225] for r29237.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_2@29794 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ test/openssl/test_bn.rb
@@ -0,0 +1,17 @@
+begin
+  require "openssl"
+rescue LoadError
+end
+require "digest/md5"
+require "test/unit"
+
+if defined?(OpenSSL)
+
+class OpenSSL::TestBN < Test::Unit::TestCase
+  def test_prime_p
+    OpenSSL::BN.new((2 ** 107 - 1).to_s(16), 16).prime?
+    OpenSSL::BN.new((2 ** 127 - 1).to_s(16), 16).prime?(1)
+  end
+end
+
+end
