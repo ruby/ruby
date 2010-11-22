699a20e917bebfdee75612ba1d5c244e7a395077
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-11-22 07:21:48 +0000

    Sat, 24 Jul 2010 06:49:35 +0000  suke <suke@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>  merge revision(s) 28378:    	* ext/win32ole/win32ole.c (fev_initialize): initialize pTypeInfo.  	  [ruby-core:31304][Bug #3576].
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@28738 b2dd03c8-39d4-4d8f-98ff-823fe69b080e
    Signed-off-by: URABE, Shyouhei <shyouhei@ruby-lang.org>
    
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_7@29857 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/win32ole/win32ole.c
+++ ext/win32ole/win32ole.c
@@ -78,7 +78,7 @@
 
 #define WC2VSTR(x) ole_wc2vstr((x), TRUE)
 
-#define WIN32OLE_VERSION "0.7.8"
+#define WIN32OLE_VERSION "0.7.9"
 
 typedef HRESULT (STDAPICALLTYPE FNCOCREATEINSTANCEEX)
     (REFCLSID, IUnknown*, DWORD, COSERVERINFO*, DWORD, MULTI_QI*);
@@ -6113,7 +6113,6 @@ find_default_source(ole, piid, ppTypeInfo)
 
     OLE_RELEASE_TYPEATTR(pTypeInfo, pTypeAttr);
     OLE_RELEASE(pTypeInfo);
-
     /* Now that would be a bad surprise, if we didn't find it, wouldn't it? */
     if (!*ppTypeInfo) {
         if (SUCCEEDED(hr))
@@ -6209,6 +6208,7 @@ fev_initialize(argc, argv, self)
         rb_raise(rb_eTypeError, "1st parameter must be WIN32OLE object");
     }
 
+    pTypeInfo = NULL;
     if(TYPE(itf) != T_NIL) {
         if (ruby_safe_level > 0 && OBJ_TAINTED(itf)) {
             rb_raise(rb_eSecurityError, "Insecure Event Creation - %s",
