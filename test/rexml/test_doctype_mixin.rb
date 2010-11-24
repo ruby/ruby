911692e5684749c293bed767d0c6c37a29833383
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-11-24 09:37:27 +0000

    merges r29282,r29283,r29284,r29285,r29286,r29287,r29288 and r29302 from trunk into ruby_1_9_2. -- * test/rexml/: import REXML tests from   http://www.germane-software.com/repos/rexml/trunk/test/.   Many tests are failed temporary. I'll fix them quickly. Sorry. -- * test/rexml/test_listener.rb: remove needless codes. -- * test/rexml/: fix fixture data path. All REXML tests are worked. -- * test/rexml/: untabify. -- * test/rexml/test_core.rb: enable. -- * test/rexml/test_sax.rb: don't use thread and sleep to avoid slow test. -- * lib/rexml/xpath_parser.rb, test/rexml/test_xpath.rb:   add missing method availability check. [ruby-core:32447]   Reported by Wiebe Cazemier. Thanks!!! -- Specify external encoding.
    When external encoding is not specified, it uses default
    external
    encoding. But it depends the environment. So specify as
    UTF-8 for
    environments whose locale is not UTF-8.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_2@29915 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ test/rexml/test_doctype_mixin.rb
@@ -0,0 +1,67 @@
+#! /usr/local/bin/ruby
+
+
+require 'test/unit'
+require 'rexml/document'
+
+class TestDoctype < Test::Unit::TestCase
+
+  def setup
+    @sysid = "urn:x-test:sysid1"
+    @notid1 = "urn:x-test:notation1"
+    @notid2 = "urn:x-test:notation2"
+    document_string1 = <<-"XMLEND"
+    <!DOCTYPE r SYSTEM "#{@sysid}" [
+      <!NOTATION n1 SYSTEM "#{@notid1}">
+      <!NOTATION n2 SYSTEM "#{@notid2}">
+    ]>
+    <r/>
+    XMLEND
+    @doctype1 = REXML::Document.new(document_string1).doctype
+    
+    @pubid = "TEST_ID"
+    document_string2 = <<-"XMLEND"
+    <!DOCTYPE r PUBLIC "#{@pubid}">
+    <r/>
+    XMLEND
+    @doctype2 = REXML::Document.new(document_string2).doctype
+
+    document_string3 = <<-"XMLEND"
+    <!DOCTYPE r PUBLIC "#{@pubid}" "#{@sysid}">
+    <r/>
+    XMLEND
+    @doctype3 = REXML::Document.new(document_string3).doctype
+  
+  end
+   
+  def test_public
+    assert_equal(nil, @doctype1.public)
+    assert_equal(@pubid, @doctype2.public)
+    assert_equal(@pubid, @doctype3.public)
+  end
+  
+  def test_system
+    assert_equal(@sysid, @doctype1.system)
+    assert_equal(nil, @doctype2.system)
+    assert_equal(@sysid, @doctype3.system)
+  end
+
+  def test_notation
+    assert_equal(@notid1, @doctype1.notation("n1").system)
+    assert_equal(@notid2, @doctype1.notation("n2").system)
+  end
+  
+  def test_notations
+    notations = @doctype1.notations
+    assert_equal(2, notations.length)
+    assert_equal(@notid1, find_notation(notations, "n1").system)
+    assert_equal(@notid2, find_notation(notations, "n2").system)
+  end
+  
+  def find_notation(notations, name)
+    notations.find { |notation|
+      name == notation.name
+    }
+  end
+  
+end
