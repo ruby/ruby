6d74458037fd11612af429f95482a14cfa7d1381
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-12-02 01:12:41 +0000

    * ext/json/generator/extconf.rb: remove the lines which set -O3   when -O option is not set.   Note that -O3 doesn't always exist.
    * ext/json/parser/extconf.rb: ditto.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@30011 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/json/generator/extconf.rb
+++ ext/json/generator/extconf.rb
@@ -1,16 +1,6 @@
 require 'mkmf'
 require 'rbconfig'
 
-unless $CFLAGS.gsub!(/ -O[\dsz]?/, ' -O3')
-  $CFLAGS << ' -O3'
-end
-if CONFIG['CC'] =~ /gcc/
-  $CFLAGS << ' -Wall'
-  #unless $CFLAGS.gsub!(/ -O[\dsz]?/, ' -O0 -ggdb')
-  #  $CFLAGS << ' -O0 -ggdb'
-  #end
-end
-
 if RUBY_VERSION < "1.9"
   have_header("re.h")
 else
--- ext/json/parser/extconf.rb
+++ ext/json/parser/extconf.rb
@@ -1,15 +1,5 @@
 require 'mkmf'
 require 'rbconfig'
 
-unless $CFLAGS.gsub!(/ -O[\dsz]?/, ' -O3')
-  $CFLAGS << ' -O3'
-end
-if CONFIG['CC'] =~ /gcc/
-  $CFLAGS << ' -Wall'
-  #unless $CFLAGS.gsub!(/ -O[\dsz]?/, ' -O0 -ggdb')
-  #  $CFLAGS << ' -O0 -ggdb'
-  #end
-end
-
 have_header("re.h")
 create_makefile 'json/ext/parser'
