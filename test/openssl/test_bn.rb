0ce00dce21286b03fdc85d343a3694813b087146
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-12-02 08:05:48 +0000

    merges r29407 from trunk into ruby_1_9_2. -- * ext/openssl/lib/openssl/bn.rb (Integer#to_bn): OpenSSL::BN.new   accepts only Strings, so call Integer#to_s(16).   16 is for an optimization. [ruby-dev:42336]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_2@30028 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/openssl/test_bn.rb
+++ test/openssl/test_bn.rb
@@ -8,6 +8,11 @@ require "test/unit"
 if defined?(OpenSSL)
 
 class OpenSSL::TestBN < Test::Unit::TestCase
+  def test_integer_to_bn
+    assert_equal(999.to_bn, OpenSSL::BN.new(999.to_s(16), 16))
+    assert_equal((2 ** 107 - 1).to_bn, OpenSSL::BN.new((2 ** 107 - 1).to_s(16), 16))
+  end
+
   def test_prime_p
     OpenSSL::BN.new((2 ** 107 - 1).to_s(16), 16).prime?
     OpenSSL::BN.new((2 ** 127 - 1).to_s(16), 16).prime?(1)
