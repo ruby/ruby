36f1477f3faef4c1af37a6799d945c8499dd4217
nahi <nahi@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-12-20 16:07:06 +0000

    * Backported the fix at           Mon Sep 13 09:23:58 2010  NARUSE, Yui  <naruse@ruby-lang.org>
            * ext/openssl/ossl_bn.c (ossl_bn_is_prime): fix comparison
              with rb_scan_args. Before this fix, OpenSSL::BN#prime?
              is fully broken.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8@30279 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/openssl/test_bn.rb
+++ test/openssl/test_bn.rb
@@ -11,6 +11,11 @@ class OpenSSL::TestBN < Test::Unit::TestCase
     assert_equal(999.to_bn, OpenSSL::BN.new(999.to_s(16), 16))
     assert_equal((2 ** 107 - 1).to_bn, OpenSSL::BN.new((2 ** 107 - 1).to_s(16), 16))
   end
+
+  def test_prime_p
+    OpenSSL::BN.new((2 ** 107 - 1).to_s(16), 16).prime?
+    OpenSSL::BN.new((2 ** 127 - 1).to_s(16), 16).prime?(1)
+  end
 end
 
 end
