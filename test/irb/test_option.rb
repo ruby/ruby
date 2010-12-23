d80452a1839d89e8f1ef80fefba5d7b48fc9df16
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-12-23 12:45:32 +0000

    merges r30147 from trunk into ruby_1_9_2. -- * test/irb/test_option.rb (test_end_of_option): don't read .irbrc.  Bug#4138
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_2@30318 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/irb/test_option.rb
+++ test/irb/test_option.rb
@@ -5,7 +5,7 @@ module TestIRB
   class TestOption < Test::Unit::TestCase
     def test_end_of_option
       bug4117 = '[ruby-core:33574]'
-      status = assert_in_out_err(%w[-rirb -e IRB.start(__FILE__) -- --], "", //, [], bug4117)
+      status = assert_in_out_err(%w[-rirb -e IRB.start(__FILE__) -- -f --], "", //, [], bug4117)
       assert(status.success?, bug4117)
     end
   end
