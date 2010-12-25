514a5b3b7150dcaa78fee45676742c1be4f69804
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2010-12-25 01:51:06 +0000

    * ext/json/generator/generator.c (fbuffer_free): unused.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@30338 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/json/generator/generator.c
+++ ext/json/generator/generator.c
@@ -318,11 +318,6 @@ static void fbuffer_free(FBuffer *fb)
     ruby_xfree(fb);
 }
 
-static void fbuffer_free_only_buffer(FBuffer *fb)
-{
-    ruby_xfree(fb);
-}
-
 static void fbuffer_clear(FBuffer *fb)
 {
     fb->len = 0;
