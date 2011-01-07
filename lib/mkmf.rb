35f78ac1f80e63522453d0d9b0192afd4fd5b424
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-01-07 14:07:51 +0000

    * lib/mkmf.rb (configuration): backref needs to capture.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@30487 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -1660,9 +1660,7 @@ VPATH = #{vpath.join(CONFIG['PATH_SEPARATOR'])}
   extconf_h = $extconf_h ? "-DRUBY_EXTCONF_H=\\\"$(RUBY_EXTCONF_H)\\\" " : $defs.join(" ") << " "
   if warnflags = CONFIG['warnflags'] and CONFIG['GCC'] == 'yes' and !$extmk
     # turn warnings into errors only for bundled extensions.
-    warnflags = warnflags.split.map { |flag|
-      flag.gsub(/(?:\A|\s)-Werror=/, '\1-W')
-    }.join ' '
+    warnflags = warnflags.gsub(/(\A|\s)-Werror=/, '\1-W')
   end
   mk << %{
 CC = #{CONFIG['CC']}
