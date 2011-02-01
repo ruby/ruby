0f7fc54678963025e77752cae8b30b7d8f4a2845
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-02-01 11:47:01 +0000

    * enc/encdb.c: parenthesize macro arguments.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@30755 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- enc/encdb.c
+++ enc/encdb.c
@@ -14,11 +14,11 @@ int rb_encdb_alias(const char *alias, const char *orig);
 int rb_encdb_dummy(const char *name);
 void rb_encdb_declare(const char *name);
 void rb_enc_set_base(const char *name, const char *orig);
-#define ENC_REPLICATE(name, orig) rb_encdb_replicate(name, orig)
-#define ENC_ALIAS(name, orig) rb_encdb_alias(name, orig)
+#define ENC_REPLICATE(name, orig) rb_encdb_replicate((name), (orig))
+#define ENC_ALIAS(name, orig) rb_encdb_alias((name), (orig))
 #define ENC_DUMMY(name) rb_encdb_dummy(name)
 #define ENC_DEFINE(name) rb_encdb_declare(name)
-#define ENC_SET_BASE(name, orig) rb_enc_set_base(name, orig)
+#define ENC_SET_BASE(name, orig) rb_enc_set_base((name), (orig))
 
 void
 Init_encdb(void)
