dda0d61e684ac1662416f7fc721b452e370f9add
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-02-07 02:48:04 +0000

    * common.mk (rdoc): add --encoding=UTF-8; ruby's rdoc must be UTF-8.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@30813 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- common.mk
+++ common.mk
@@ -387,7 +387,7 @@ post-install-doc::
 
 rdoc: PHONY main
 	@echo Generating RDoc documentation
-	$(Q) $(XRUBY) "$(srcdir)/bin/rdoc" --no-force-update --all --ri --op "$(RDOCOUT)" $(RDOCFLAGS) "$(srcdir)"
+	$(Q) $(XRUBY) "$(srcdir)/bin/rdoc" --encoding=UTF-8 --no-force-update --all --ri --op "$(RDOCOUT)" $(RDOCFLAGS) "$(srcdir)"
 nodoc: PHONY
 
 what-where-doc: no-install-doc
