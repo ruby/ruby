7c3a80272a079aded3dd290512fe3e21c2efe209
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-02-13 00:56:52 +0000

    * test/openssl/test_config.rb (OpenSSL#test_freeze): fix error  message assertion.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@30861 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/openssl/test_config.rb
+++ test/openssl/test_config.rb
@@ -262,11 +262,12 @@ __EOC__
     c['foo'] = [['key', 'value']]
     c.freeze
 
-    # [ruby-core:18377]
+    bug = '[ruby-core:18377]'
     # RuntimeError for 1.9, TypeError for 1.8
-    assert_raise(TypeError, /frozen/) do
+    e = assert_raise(TypeError, bug) do
       c['foo'] = [['key', 'wrong']]
     end
+    assert_match(/can't modify/, e.message, bug)
   end
 
   def test_dup
