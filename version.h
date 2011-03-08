c74a962aab0c8af42a5e7ef37ac81d3a7e625def
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-03-08 15:15:55 +0000

    * ext/extmk.rb: workaround for nmake.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@31071 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,11 +1,11 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_RELEASE_DATE "2011-03-08"
+#define RUBY_RELEASE_DATE "2011-03-09"
 #define RUBY_PATCHLEVEL -1
 #define RUBY_BRANCH_NAME "trunk"
 
 #define RUBY_RELEASE_YEAR 2011
 #define RUBY_RELEASE_MONTH 3
-#define RUBY_RELEASE_DAY 8
+#define RUBY_RELEASE_DAY 9
 
 #include "ruby/version.h"
 
