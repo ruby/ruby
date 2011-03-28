5988d8cbd5be141ccb9e6516785dae6be80c724e
arton <arton@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-03-28 16:52:20 +0000

    * test/webrick/test_filehandler.rb (test_short_filename):  read real short filename by cmd because smb mounted files  have different naming convention.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@31205 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/webrick/test_filehandler.rb
+++ test/webrick/test_filehandler.rb
@@ -216,8 +216,12 @@ class WEBrick::TestFileHandler < Test::Unit::TestCase
     }
     TestWEBrick.start_httpserver(config) do |server, addr, port, log|
       http = Net::HTTP.new(addr, port)
-
-      req = Net::HTTP::Get.new("/webric~1.cgi/test")
+      if windows?
+        fname = IO.popen("dir /x webrick_long_filename.cgi", "r").read.match(/\s(w.+?cgi)\s/i)[1].downcase
+      else
+        fname = "webric~1.cgi"
+      end
+      req = Net::HTTP::Get.new("/#{fname}/test")
       http.request(req) do |res|
         if windows?
           assert_equal("200", res.code, log.call)
