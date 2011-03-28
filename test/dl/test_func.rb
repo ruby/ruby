b70f6464e0d5c120610dd2b107c78436b219147d
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-03-28 13:40:46 +0000

    merges r30733 from trunk into ruby_1_9_2. -- * test/dl/test_dl2.rb (TestDL#test_sin): math functions do not   work on x86_64 due to the design of DL2. * test/dl/test_func.rb (DL::TestFunc#test_{sinf,sin): ditto.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_2@31199 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/dl/test_func.rb
+++ test/dl/test_func.rb
@@ -22,6 +22,7 @@ module DL
     end
 
     def test_sinf
+      return if /x86_64/ =~ RUBY_PLATFORM
       begin
         f = Function.new(CFunc.new(@libm['sinf'], TYPE_FLOAT, 'sinf'),
                          [TYPE_FLOAT])
@@ -32,6 +33,7 @@ module DL
     end
 
     def test_sin
+      return if /x86_64/ =~ RUBY_PLATFORM
       f = Function.new(CFunc.new(@libm['sin'], TYPE_DOUBLE, 'sin'),
                        [TYPE_DOUBLE])
       assert_in_delta 1.0, f.call(90 * Math::PI / 180), 0.0001
