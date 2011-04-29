32ae0ae1c2d6b1d24a875e2846db11290907a11f
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-04-29 01:11:31 +0000

    * 2011-04-29
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@31374 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,11 +1,11 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_RELEASE_DATE "2011-04-28"
+#define RUBY_RELEASE_DATE "2011-04-29"
 #define RUBY_PATCHLEVEL -1
 #define RUBY_BRANCH_NAME "trunk"
 
 #define RUBY_RELEASE_YEAR 2011
 #define RUBY_RELEASE_MONTH 4
-#define RUBY_RELEASE_DAY 28
+#define RUBY_RELEASE_DAY 29
 
 #include "ruby/version.h"
 
