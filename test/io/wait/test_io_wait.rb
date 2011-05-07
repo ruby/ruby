70e9201610cf83408f5e408571a48b7eab8cc5fa
kosaki <kosaki@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-05-07 14:57:38 +0000

    * test/io/wait/test_io_wait.rb: skip tests if the platform is   mswin.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@31469 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/io/wait/test_io_wait.rb
+++ test/io/wait/test_io_wait.rb
@@ -17,18 +17,21 @@ class TestIOWait < Test::Unit::TestCase
   end
 
   def test_nread
+    return if /mswin/ =~ RUBY_PLATFORM
     assert_equal 0, @r.nread
     @w.syswrite "."
     assert_equal 1, @r.nread
   end
 
   def test_nread_buffered
+    return if /mswin/ =~ RUBY_PLATFORM
     @w.syswrite ".\n!"
     assert_equal ".\n", @r.read(2)
     assert_equal 1, @r.nread
   end
 
   def test_ready?
+    return if /mswin/ =~ RUBY_PLATFORM
     refute @r.ready?
     @w.syswrite "."
     assert @r.ready?
@@ -41,23 +44,27 @@ class TestIOWait < Test::Unit::TestCase
   end
 
   def test_wait
+    return if /mswin/ =~ RUBY_PLATFORM
     assert_nil @r.wait(0)
     @w.syswrite "."
     assert_equal @r, @r.wait(0)
   end
 
   def test_wait_buffered
+    return if /mswin/ =~ RUBY_PLATFORM
     @w.syswrite ".\n!"
     assert_equal ".\n", @r.gets
     assert_equal true, @r.wait(0)
   end
 
   def test_wait_forever
+    return if /mswin/ =~ RUBY_PLATFORM
     Thread.new { sleep 0.01; @w.syswrite "." }
     assert_equal @r, @r.wait
   end
 
   def test_wait_eof
+    return if /mswin/ =~ RUBY_PLATFORM
     Thread.new { sleep 0.01; @w.close }
     assert_nil @r.wait
   end
