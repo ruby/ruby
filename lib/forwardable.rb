fcd7300bf28ac8571b54dcf356e797a786946b6c
keiju <keiju@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-05-11 11:12:40 +0000

    * lib/forwardable.rb: support 'delegate :foo => :bar' for to meet    by specification of RDOC.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@31507 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/forwardable.rb
+++ lib/forwardable.rb
@@ -149,7 +149,7 @@ module Forwardable
   #
   def instance_delegate(hash)
     hash.each{ |methods, accessor|
-      methods = methods.to_s unless methods.respond_to?(:each)
+      methods = [methods] unless methods.respond_to?(:each)
       methods.each{ |method|
         def_instance_delegator(accessor, method)
       }
@@ -214,7 +214,7 @@ module SingleForwardable
   #
   def single_delegate(hash)
     hash.each{ |methods, accessor|
-      methods = methods.to_s unless methods.respond_to?(:each)
+      methods = [methods] unless methods.respond_to?(:each)
       methods.each{ |method|
         def_single_delegator(accessor, method)
       }
