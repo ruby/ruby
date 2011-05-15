cb12cc2c0f45e5a49d49c247d4423f7b23c4f088
Xavier Shay <xavier@rhnh.net>
2011-05-15 13:22:02 +1000

    Provides backwards compatibility for enumerable without hacking into the require internals

---
+++ lib/enumerator.rb
@@ -0,0 +1,3 @@
+# This class is now defined entirely in enumerator.c and is always available.
+# This file needs to remain here for backwards compatibility, so that `require
+# "enumerator"` will not raise an exception.
