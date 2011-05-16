8638efb23d21484e384dc45f99de4d9c500f2428
drbrain <drbrain@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-05-16 18:51:56 +0000

    * lib/abbrev.rb:  Hide copyright and revision information from RDoc. 	  Inspired by patch from David Copeland, bug #4703.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@31593 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/abbrev.rb
+++ lib/abbrev.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
-=begin
-#
+#--
 # Copyright (c) 2001,2003 Akinori MUSHA <knu@iDaemons.org>
 #
 # All rights reserved.  You can redistribute and/or modify it under
@@ -9,7 +8,7 @@
 # $Idaemons: /home/cvs/rb/abbrev.rb,v 1.2 2001/05/30 09:37:45 knu Exp $
 # $RoughId: abbrev.rb,v 1.4 2003/10/14 19:45:42 knu Exp $
 # $Id$
-=end
+#++
 
 # Calculate the set of unique abbreviations for a given set of strings.
 #
