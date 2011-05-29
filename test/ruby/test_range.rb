f0de89f81ae37ed8b12427c375bc5ad06bcfba0e
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-05-29 22:50:11 +0000

    merges r31286 from trunk into ruby_1_9_2. -- * numeric.c (ruby_float_step): wrong loop condition.   fixes [ruby-core:35753], reported by Joey Zhou.
    * test/ruby/test_range.rb (TestRange#test_step_ruby_core_35753):
      test above change.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_2@31803 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_range.rb
+++ test/ruby/test_range.rb
@@ -182,6 +182,15 @@ class TestRange < Test::Unit::TestCase
     assert_nothing_raised("[ruby-dev:34558]") { (0..2).step(o) {|x| } }
   end
 
+  def test_step_ruby_core_35753
+    assert_equal(6, (1...6.3).step.to_a.size)
+    assert_equal(5, (1.1...6).step.to_a.size)
+    assert_equal(5, (1...6).step(1.1).to_a.size)
+    assert_equal(3, (1.0...6.3).step(1.8).to_a.size)
+    assert_equal(3, (1.0...6.4).step(1.8).to_a.size)
+    assert_equal(4, (1.0...6.5).step(1.8).to_a.size)
+  end
+
   def test_each
     a = []
     (0..10).each {|x| a << x }
