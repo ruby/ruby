1a8ea0fc0f893c4034815c770c033d38418c1a90
nahi <nahi@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-05-30 02:08:57 +0000

    * lib/logger.rb (Logger::ProgName): do not depend on subversion   keyword ($Id$). ProgName with revision number was written in the   header line of each logfile for ease of tracking what version user   is using in troubleshooting.  Logger is already stable enough.
    I commit this on confirmation from Shyouhei.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_7@31806 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/logger.rb
+++ lib/logger.rb
@@ -170,8 +170,7 @@ require 'monitor'
 
 class Logger
   VERSION = "1.2.6"
-  id, name, rev = %w$Id$
-  ProgName = "#{name.chomp(",v")}/#{rev}"
+  ProgName = "#{File.basename(__FILE__)}/#{VERSION}"
 
   class Error < RuntimeError; end
   class ShiftingError < Error; end
