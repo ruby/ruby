ac899ff9c82f0b48c001d6e76b300264aecd4bca
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-05-30 04:43:20 +0000

    merges r31304 from trunk into ruby_1_9_2. -- * test/ruby/test_range.rb (TestRange#test_step_ruby_core_35753):   avoid float error. [ruby-core:35804]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_2@31808 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_range.rb
+++ test/ruby/test_range.rb
@@ -186,9 +186,9 @@ class TestRange < Test::Unit::TestCase
     assert_equal(6, (1...6.3).step.to_a.size)
     assert_equal(5, (1.1...6).step.to_a.size)
     assert_equal(5, (1...6).step(1.1).to_a.size)
-    assert_equal(3, (1.0...6.3).step(1.8).to_a.size)
-    assert_equal(3, (1.0...6.4).step(1.8).to_a.size)
-    assert_equal(4, (1.0...6.5).step(1.8).to_a.size)
+    assert_equal(3, (1.0...5.4).step(1.5).to_a.size)
+    assert_equal(3, (1.0...5.5).step(1.5).to_a.size)
+    assert_equal(4, (1.0...5.6).step(1.5).to_a.size)
   end
 
   def test_each
