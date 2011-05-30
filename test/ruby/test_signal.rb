36cad2f9a6564ebcde6c3375107c5688172837bc
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-05-30 11:23:36 +0000

    * test/ruby/test_signal.rb (TestSignal#test_signal_requiring): small   but critical typo of r31642.  sorry...   [Bug #4798] [ruby-core:36550]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@31824 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_signal.rb
+++ test/ruby/test_signal.rb
@@ -198,7 +198,7 @@ class TestSignal < Test::Unit::TestCase
   end
 
   def test_signal_requiring
-    skip "limitation of GenerateConsoleCtrlEvent()" if /mswin|mignw/ =~ RUBY_PLATFORM
+    skip "limitation of GenerateConsoleCtrlEvent()" if /mswin|mingw/ =~ RUBY_PLATFORM
     t = Tempfile.new(%w"require_ensure_test .rb")
     t.puts "sleep"
     t.close
