55ddfceed3bae80f1f9f0452cbcc7565ad77f679
sorah <sorah@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-06-06 03:47:04 +0000

    * lib/test/unit.rb(Test::Unit::Runner#puke):   Add overriding from MiniTest::Unit#puke. This reverts minitest's fix   that skip messages are hidden when not verbose mode (-v option).   To hide skip messages, use --hide-skip option instead.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@31934 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/test/unit.rb
+++ lib/test/unit.rb
@@ -573,6 +573,27 @@ module Test
         result
       end
 
+      # Overriding of MiniTest::Unit#puke
+      def puke klass, meth, e
+        # TODO:
+        #   this overriding is for minitest feature that skip messages are
+        #   hidden when not verbose (-v), note this is temporally.
+        e = case e
+            when MiniTest::Skip then
+              @skips += 1
+              "Skipped:\n#{meth}(#{klass}) [#{location e}]:\n#{e.message}\n"
+            when MiniTest::Assertion then
+              @failures += 1
+              "Failure:\n#{meth}(#{klass}) [#{location e}]:\n#{e.message}\n"
+            else
+              @errors += 1
+              bt = MiniTest::filter_backtrace(e.backtrace).join "\n    "
+              "Error:\n#{meth}(#{klass}):\n#{e.class}: #{e.message}\n    #{bt}\n"
+            end
+        @report << e
+        e[0, 1]
+      end
+
       def status(*args)
         result = super
         raise @interrupt if @interrupt
