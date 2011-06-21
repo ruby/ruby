cb138bbaa6fdec9e9e8f6821e23130d6397677cd
kosaki <kosaki@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-06-21 10:47:07 +0000

    * test/ruby/test_thread.rb (TestThread#test_priority): enable   this test again. Current GVL respect thread priority rather   than past.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@32182 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_thread.rb
+++ test/ruby/test_thread.rb
@@ -243,7 +243,7 @@ class TestThread < Test::Unit::TestCase
     end
     t1.kill
     t2.kill
-    # assert_operator(c1, :>, c2, "[ruby-dev:33124]") # not guaranteed
+    assert_operator(c1, :>, c2, "[ruby-dev:33124]") # not guaranteed
   end
 
   def test_new
