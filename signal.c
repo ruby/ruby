e6f4c92f6def7977e03fa6ce8c7b4f25f19fc2b7
sorah <sorah@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-06-23 04:33:13 +0000

    * signal.c(ruby_atomic_exchange): Fix definement style.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@32205 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- signal.c
+++ signal.c
@@ -19,7 +19,8 @@
 #include "atomic.h"
 
 #if !defined(_WIN32) && !defined(HAVE_GCC_ATOMIC_BUILTINS)
-rb_atomic_t ruby_atomic_exchange(rb_atomic_t *ptr, rb_atomic_t val)
+rb_atomic_t
+ruby_atomic_exchange(rb_atomic_t *ptr, rb_atomic_t val)
 {
     rb_atomic_t old = *ptr;
     *ptr = val;
