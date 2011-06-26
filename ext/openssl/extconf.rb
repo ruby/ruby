488fcde070191ca5b455ebcb6aaf51575519b617
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-06-26 08:58:06 +0000

    merge revision(s) 31346,31528: 	* ext/openssl/extconf.rb: Should check SSLv2_*method. 	  openssl compiled with "no-ssl2" the extconf don't fail 	  when running `make' having this compilation errors. 	  Patched by Laurent Arnoud. fixes #4562, #4556
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_8_7@32234 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/openssl/extconf.rb
+++ ext/openssl/extconf.rb
@@ -100,6 +100,12 @@ have_func("OPENSSL_cleanse")
 if try_compile("#define FOO(...) foo(__VA_ARGS__)\n int x(){FOO(1);FOO(1,2);FOO(1,2,3);}\n")
   $defs.push("-DHAVE_VA_ARGS_MACRO")
 end
+have_func("SSLv2_method")
+have_func("SSLv2_server_method")
+have_func("SSLv2_client_method")
+unless have_func("SSL_set_tlsext_host_name", ['openssl/ssl.h'])
+  have_macro("SSL_set_tlsext_host_name", ['openssl/ssl.h']) && $defs.push("-DHAVE_SSL_SET_TLSEXT_HOST_NAME")
+end
 if have_header("openssl/engine.h")
   have_func("ENGINE_add")
   have_func("ENGINE_load_builtin_engines")
