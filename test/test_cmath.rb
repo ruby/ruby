ae56a2ec03e09106830f897db2613b54fd03a2b2
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-06-30 03:27:08 +0000

    Refix test introduced by r32307.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@32317 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/test_cmath.rb
+++ test/test_cmath.rb
@@ -7,9 +7,7 @@ class TestCMath < Test::Unit::TestCase
   end
 
   def test_acos
-    c = CMath.acos(Complex(3, 4))
-    assert_in_delta 0.9368124611557199, c.real
-    assert_in_delta -2.305509031243477, c.imag
+    assert_in_delta (CMath.acos(Complex(3, 4)) - Complex(0.9368124611557199,-2.305509031243477)).abs2, 0
   end
 
   def test_cbrt_returns_principal_value_of_cube_root
