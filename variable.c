8fca66d7ebb50f8b3d7af8a5550dbcaf12138a3a
matz <matz@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-06-30 18:34:09 +0000

    * class.c (Init_class_hierarchy): should name BasicObject   explicitly.
    * variable.c (rb_const_defined_0): should not check for
      superclasses as const_get.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@32342 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- variable.c
+++ variable.c
@@ -1829,7 +1829,7 @@ rb_const_defined_0(VALUE klass, ID id, int exclude, int recurse, int visibility)
 		return (int)Qfalse;
 	    return (int)Qtrue;
 	}
-	if (!recurse && klass != rb_cObject) break;
+	if (!recurse) break;
 	tmp = RCLASS_SUPER(tmp);
     }
     if (!exclude && !mod_retry && BUILTIN_TYPE(klass) == T_MODULE) {
