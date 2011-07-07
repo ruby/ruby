68a0d412dd36b0caf1551089d8e6c64819854b6f
kosaki <kosaki@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-07-07 09:09:44 +0000

    add a comment why FreeBSD can't use MAP_STACK.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@32437 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- cont.c
+++ cont.c
@@ -517,6 +517,10 @@ fiber_entry(void *arg)
 }
 #else /* _WIN32 */
 
+/*
+ * FreeBSD require a first (i.e. addr) argument of mmap(2) is not NULL
+ * if MAP_STACK is passed.
+ */
 #if defined(MAP_STACK) && !defined(__FreeBSD__)
 #define FIBER_STACK_FLAGS (MAP_PRIVATE | MAP_ANON | MAP_STACK)
 #else
