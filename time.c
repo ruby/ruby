f11eb9bbbab9fd46b24a1fa01d35bb680da56ae8
kosaki <kosaki@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-07-15 16:16:26 +0000

    merge revision(s) 32554:
    	* time.c (time_dup): used rb_obj_class() instead of CLASS_OF().
    	  The patch is made by Kazuki Tsujimoto. [Bug #5012] [ruby-dev:44071]
    
    	* test/ruby/test_time.rb (TestTime#test_getlocal_dont_share_eigenclass):
    	  added a new test for eigenclass of time object.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@32556 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- time.c
+++ time.c
@@ -3430,7 +3430,7 @@ time_init_copy(VALUE copy, VALUE time)
 static VALUE
 time_dup(VALUE time)
 {
-    VALUE dup = time_s_alloc(CLASS_OF(time));
+    VALUE dup = time_s_alloc(rb_obj_class(time));
     time_init_copy(dup, time);
     return dup;
 }
