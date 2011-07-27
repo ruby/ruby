dddc9485da5e96d9406f7c4e942aa253d21a12c0
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-07-27 13:49:43 +0000

    * test/rinda/test_rinda.rb (test_remote_array_and_hash):   add local variables to protect objects from GC. [ruby-dev:44253]   [Bug #5104]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@32705 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/rinda/test_rinda.rb
+++ test/rinda/test_rinda.rb
@@ -539,9 +539,13 @@ class TupleSpaceProxyTest < Test::Unit::TestCase
   end
 
   def test_remote_array_and_hash
-    @ts.write(DRbObject.new([1, 2, 3]))
+    # Don't remove ary/hsh local variables.
+    # These are necessary to protect objects from GC.
+    ary = [1, 2, 3]
+    @ts.write(DRbObject.new(ary))
     assert_equal([1, 2, 3], @ts.take([1, 2, 3], 0))
-    @ts.write(DRbObject.new({'head' => 1, 'tail' => 2}))
+    hsh = {'head' => 1, 'tail' => 2}
+    @ts.write(DRbObject.new(hsh))
     assert_equal({'head' => 1, 'tail' => 2},
                  @ts.take({'head' => 1, 'tail' => 2}, 0))
   end
