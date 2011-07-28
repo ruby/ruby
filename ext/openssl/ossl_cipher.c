be10ad2072798a82ceedf38456047f518dfe6da7
nahi <nahi@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-07-28 13:52:57 +0000

    * backport r32723 from trunk.
    * ext/openssl/ossl_cipher.c (ossl_cipher_initialize): Avoid possible
      SEGV from AES encryption/decryption. Processing data by
      Cipher#update without initializing key (meaningless usage of Cipher
      object since we don't offer a way to export a key) could cause SEGV.
    
      In OpenSSL, the EVP which has EVP_CIPH_RAND_KEY flag (such as DES3)
      allows uninitialized key, but other EVPs (such as AES) does not
      allow it. Calling EVP_CipherUpdate() without initializing key causes
      SEGV so we set the data filled with "\0" as the key by default. See
      #2768.
    
    * test/openssl/test_cipher.rb: test it.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@32724 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/openssl/ossl_cipher.c
+++ ext/openssl/ossl_cipher.c
@@ -102,6 +102,7 @@ ossl_cipher_initialize(VALUE self, VALUE str)
     EVP_CIPHER_CTX *ctx;
     const EVP_CIPHER *cipher;
     char *name;
+    unsigned char key[EVP_MAX_KEY_LENGTH];
 
     name = StringValuePtr(str);
     GetCipherInit(self, ctx);
@@ -113,7 +114,14 @@ ossl_cipher_initialize(VALUE self, VALUE str)
     if (!(cipher = EVP_get_cipherbyname(name))) {
 	ossl_raise(rb_eRuntimeError, "unsupported cipher algorithm (%s)", name);
     }
-    if (EVP_CipherInit_ex(ctx, cipher, NULL, NULL, NULL, -1) != 1)
+    /*
+     * The EVP which has EVP_CIPH_RAND_KEY flag (such as DES3) allows
+     * uninitialized key, but other EVPs (such as AES) does not allow it.
+     * Calling EVP_CipherUpdate() without initializing key causes SEGV so we
+     * set the data filled with "\0" as the key by default.
+     */
+    memset(key, 0, EVP_MAX_KEY_LENGTH);
+    if (EVP_CipherInit_ex(ctx, cipher, NULL, key, NULL, -1) != 1)
 	ossl_raise(eCipherError, NULL);
 
     return self;
