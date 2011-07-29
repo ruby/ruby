d6ec67523643bb0e76f8bacc78116c2ab0a22c35
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-07-29 13:14:59 +0000

    * test/ruby/test_process.rb (test_rlimit_nofile): fix r32734.    move return statement again.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@32738 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_process.rb
+++ test/ruby/test_process.rb
@@ -56,10 +56,10 @@ class TestProcess < Test::Unit::TestCase
   end
 
   def test_rlimit_nofile
-    # if limit=0, this test freeze pn OpenBSD
+    return unless rlimit_exist?
     with_tmpchdir {
       write_file 's', <<-"End"
-	return unless rlimit_exist?
+	# if limit=0, this test freeze pn OpenBSD
 	limit = /openbsd/ =~ RUBY_PLATFORM ? 1 : 0
 	result = 1
 	begin
