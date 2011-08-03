deb0024af1c73dd3c40dc908b3b728d812763849
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-08-03 00:22:08 +0000

    * test/xmlrpc/webrick_testing.rb (WEBrick_Testing#start_server):   Like r32795, bind address should be specified.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@32824 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/xmlrpc/webrick_testing.rb
+++ test/xmlrpc/webrick_testing.rb
@@ -13,6 +13,7 @@ module WEBrick_Testing
     @__server_thread = Thread.new {
       @__server = WEBrick::HTTPServer.new(
         {
+          BindAddress: "localhost",
           :Logger => DummyLog.new,
           :AccessLog => [],
           :StartCallback => proc { @__started = true }
