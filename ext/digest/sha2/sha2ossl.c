937ff7bdeb84361431651fbbe9158152efc2f6c1
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-08-07 06:38:36 +0000

    * ext/digest/sha2/sha2ossl.c: use original SHA384_Final on DragonFly.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@32878 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/digest/sha2/sha2ossl.c
+++ ext/digest/sha2/sha2ossl.c
@@ -4,7 +4,9 @@
 #define SHA_Finish(bit) \
     void SHA##bit##_Finish(SHA##bit##_CTX *ctx, char *buf) \
     { SHA##bit##_Final((unsigned char *)buf, ctx);}
+#ifndef __DragonFly__
 #define SHA384_Final SHA512_Final
+#endif
 
 SHA_Finish(256)
 SHA_Finish(384)
