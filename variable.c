5f1eb218452a510cec93e05b6dbeb3307936f50d
yugui <yugui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-08-11 00:38:23 +0000

    merges r32283 from trunk into ruby_1_9_2. -- * variable.c (rb_const_get_0): should not look for superclasses if   the second optional argument is given for #const_get().   fix [Bug #3422] [Bug #3423]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_2@32917 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- variable.c
+++ variable.c
@@ -1589,7 +1589,7 @@ rb_const_get_0(VALUE klass, ID id, int exclude, int recurse)
 	    }
 	    return value;
 	}
-	if (!recurse && klass != rb_cObject) break;
+	if (!recurse) break;
 	tmp = RCLASS_SUPER(tmp);
     }
     if (!exclude && !mod_retry && BUILTIN_TYPE(klass) == T_MODULE) {
