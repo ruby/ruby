d42e5dcfcee7474f94c552e07f3063629590b816
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-08-15 23:20:52 +0000

    * 2011-08-16
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@32980 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
 #define RUBY_PATCHLEVEL -1
 
-#define RUBY_RELEASE_DATE "2011-08-15"
+#define RUBY_RELEASE_DATE "2011-08-16"
 #define RUBY_RELEASE_YEAR 2011
 #define RUBY_RELEASE_MONTH 8
-#define RUBY_RELEASE_DAY 15
+#define RUBY_RELEASE_DAY 16
 
 #include "ruby/version.h"
 
