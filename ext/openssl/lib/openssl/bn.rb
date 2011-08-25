965f51b348aa9f5906cd83361a0774e3853c7d35
drbrain <drbrain@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-08-25 00:43:45 +0000

    * ext/openssl/lib/openssl/bn.rb:  Hide copyright info from RDoc. * ext/openssl/lib/openssl/digest.rb:  ditto * ext/openssl/lib/openssl/x509.rb:  ditto * ext/openssl/lib/openssl/cipher.rb:  ditto
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@33066 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/openssl/lib/openssl/bn.rb
+++ ext/openssl/lib/openssl/bn.rb
@@ -1,18 +1,22 @@
-=begin
-= $RCSfile$ -- Ruby-space definitions that completes C-space funcs for BN
-
-= Info
-  'OpenSSL for Ruby 2' project
-  Copyright (C) 2002  Michal Rokos <m.rokos@sh.cvut.cz>
-  All rights reserved.
-
-= Licence
-  This program is licenced under the same licence as Ruby.
-  (See the file 'LICENCE'.)
-
-= Version
-  $Id$
-=end
+#--
+#
+# $RCSfile$
+#
+# = Ruby-space definitions that completes C-space funcs for BN
+# 
+# = Info
+# 'OpenSSL for Ruby 2' project
+# Copyright (C) 2002  Michal Rokos <m.rokos@sh.cvut.cz>
+# All rights reserved.
+# 
+# = Licence
+# This program is licenced under the same licence as Ruby.
+# (See the file 'LICENCE'.)
+# 
+# = Version
+# $Id$
+#
+#++
 
 module OpenSSL
   class BN
