bb4dcd5582eafa8947c1520cc187c64e302755a2
kosaki <kosaki@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-09-12 11:51:11 +0000

    * Backport r33231.
    * thread.c (rb_thread_select): fix a typo to initialize efds
      properly.  [Bug #5299] [ruby-core:39380]
    
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@33258 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/-ext-/old_thread_select/test_old_thread_select.rb
+++ test/-ext-/old_thread_select/test_old_thread_select.rb
@@ -23,6 +23,17 @@ class TestOldThreadSelect < Test::Unit::TestCase
     end
   end
 
+  def test_old_select_error_timeout
+    bug5299 = '[ruby-core:39380]'
+    with_pipe do |r, w|
+      t0 = Time.now
+      rc = IO.old_thread_select(nil, nil, [r.fileno], 0.001)
+      diff = Time.now - t0
+      assert_equal 0, rc, bug5299
+      assert_operator diff, :>=, 0.001, "returned too early"
+    end
+  end
+
   def test_old_select_false_positive
     bug5306 = '[ruby-core:39435]'
     with_pipe do |r2, w2|
@@ -77,7 +88,7 @@ class TestOldThreadSelect < Test::Unit::TestCase
     assert_equal 0, rc
     assert_equal true, thr.value
     assert received, "SIGINT not received"
-    ensure
-      trap(:INT, "DEFAULT")
+  ensure
+    trap(:INT, "DEFAULT")
   end
 end
