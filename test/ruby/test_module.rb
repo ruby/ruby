881891fb2f2762c321ba128f50d1717ce5b1fc48
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-10-07 08:57:42 +0000

    * test/-ext-/symbol/test_inadvertent_creation.rb: moved tests from   test_module.rb and test_symbol.rb.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@33425 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_module.rb
+++ test/ruby/test_module.rb
@@ -507,9 +507,6 @@ class TestModule < Test::Unit::TestCase
   def test_const_get_invalid_name
     c1 = Class.new
     assert_raise(NameError) { c1.const_defined?(:foo) }
-    name = "gadzooks"
-    assert !Symbol.all_symbols.any? {|sym| sym.to_s == name}
-    assert_raise(NameError) { c1.const_defined?(name) }
     bug5084 = '[ruby-dev:44200]'
     assert_raise(TypeError, bug5084) { c1.const_defined?(1) }
   end
