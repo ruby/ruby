9a82672ce294d85571f5ec470bef76f765bc32f4
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-10-24 06:41:57 +0000

    Follow the error format changed by FreeBSD 9.
    FreeBSD 8.2's last line is
    "./configure: Can't open ./configure: No such file or directory\n""
    but FreeBSD 9's is
    "cannot open ./configure: No such file or directory\n"".
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@33517 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/rubygems/test_gem_ext_configure_builder.rb
+++ test/rubygems/test_gem_ext_configure_builder.rb
@@ -46,13 +46,13 @@ class TestGemExtConfigureBuilder < Gem::TestCase
       end
     end
 
-    shell_error_msg = %r{(\./configure: .*)|(Can't open \./configure(?:: No such file or directory)?)}
+    shell_error_msg = %r{(\./configure: .*)|((?:Can't|cannot) open \./configure(?:: No such file or directory)?)}
     sh_prefix_configure = "sh ./configure --prefix="
 
     expected = %r(configure failed:
 
 #{Regexp.escape sh_prefix_configure}#{Regexp.escape @dest_path}
-.*?: #{shell_error_msg}
+(?:.*?: )?#{shell_error_msg}
 )
 
     assert_match expected, error.message
