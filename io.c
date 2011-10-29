cb9f3040d7020fcac74f292eba810081b73b195e
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-10-29 22:48:41 +0000

    * configure.in: check dup3.
    * io.c (rb_cloexec_dup2): use dup3 if available.
    
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@33561 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- io.c
+++ io.c
@@ -232,7 +232,22 @@ rb_cloexec_dup2(int oldfd, int newfd)
 {
     int ret;
 
+#if defined(HAVE_DUP3) && defined(O_CLOEXEC)
+    static int try_dup3 = 1;
+    if (try_dup3) {
+        ret = dup3(oldfd, newfd, O_CLOEXEC);
+        /* dup3 is available since Linux 2.6.27. */
+        if (ret == -1 && errno == ENOSYS) {
+            try_dup3 = 0;
+            ret = dup2(oldfd, newfd);
+        }
+    }
+    else {
+        ret = dup2(oldfd, newfd);
+    }
+#else
     ret = dup2(oldfd, newfd);
+#endif
     if (ret == -1) return -1;
     fd_set_cloexec(ret);
     return ret;
