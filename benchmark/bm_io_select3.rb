b37c003e2450d71dc97958e79660972701458cf3
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-11-17 11:24:07 +0000

    * benchmark/bm_io_select[23].rb: use Process::RLIMIT_NOFILE only when   it is defined.  if it is not defined, assume 64 as the max of fds.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@33778 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- benchmark/bm_io_select3.rb
+++ benchmark/bm_io_select3.rb
@@ -2,7 +2,11 @@
 
 ios = []
 nr = 100
-max = Process.getrlimit(Process::RLIMIT_NOFILE)[0]
+if defined?(Process::RLIMIT_NOFILE)
+  max = Process.getrlimit(Process::RLIMIT_NOFILE)[0]
+else
+  max = 64
+end
 puts "max fd: #{max} (results not apparent with <= 1024 max fd)"
 
 (max - 10).times do
