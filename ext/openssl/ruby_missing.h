41fa4bd7151f0a3c713d2bb3f4c08d2e8099b294
emboss <emboss@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-11-26 01:49:36 +0000

    * ext/openssl/extconf.rb: remove checks for available functions. * ext/openssl/missing.h: ditto.   Thanks, Tim Mooney for reporting this!   [Bug #5432] [ruby-core:40088]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@33843 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/openssl/ruby_missing.h
+++ ext/openssl/ruby_missing.h
@@ -25,17 +25,4 @@
 #define rb_io_t OpenFile
 #endif
 
-#ifndef HAVE_RB_STR_SET_LEN
-/* these methods should probably be backported to 1.8 */
-#define rb_str_set_len(str, length) do {	\
-	RSTRING(str)->ptr[(length)] = 0;		\
-	RSTRING(str)->len = (length);		\
-} while(0)
-#endif /* ! HAVE_RB_STR_SET_LEN */
-
-#ifndef HAVE_RB_BLOCK_CALL
-/* the openssl module doesn't use arg[3-4] and arg2 is always rb_each */
-#define rb_block_call(arg1, arg2, arg3, arg4, arg5, arg6) rb_iterate(rb_each, (arg1), (arg5), (arg6))
-#endif /* ! HAVE_RB_BLOCK_CALL */
-
 #endif /* _OSSL_RUBY_MISSING_H_ */
