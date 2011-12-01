abb40f8eb315e847e8811db1d72337a4995c2b13
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-12-01 06:19:57 +0000

    * include/ruby/subst.h: reverted r33916, since some names in vm.inc conflict.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@33917 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/subst.h
+++ include/ruby/subst.h
@@ -19,94 +19,94 @@
 
 #ifdef _WIN32
 #undef inet_ntop
-#define inet_ntop	 rb_w32_inet_ntop
+#define inet_ntop(f,a,n,l)      rb_w32_inet_ntop(f,a,n,l)
 
 #undef accept
-#define accept		 rb_w32_accept
+#define accept(s, a, l)		rb_w32_accept(s, a, l)
 
 #undef bind
-#define bind		 rb_w32_bind
+#define bind(s, a, l)		rb_w32_bind(s, a, l)
 
 #undef connect
-#define connect		 rb_w32_connect
+#define connect(s, a, l)	rb_w32_connect(s, a, l)
 
 #undef select
-#define select		 rb_w32_select
+#define select(n, r, w, e, t)	rb_w32_select(n, r, w, e, t)
 
 #undef getpeername
-#define getpeername	 rb_w32_getpeername
+#define getpeername(s, a, l)	rb_w32_getpeername(s, a, l)
 
 #undef getsockname
-#define getsockname	 rb_w32_getsockname
+#define getsockname(s, a, l)	rb_w32_getsockname(s, a, l)
 
 #undef getsockopt
-#define getsockopt	 rb_w32_getsockopt
+#define getsockopt(s, v, n, o, l) rb_w32_getsockopt(s, v, n, o, l)
 
 #undef ioctlsocket
-#define ioctlsocket	 rb_w32_ioctlsocket
+#define ioctlsocket(s, c, a)	rb_w32_ioctlsocket(s, c, a)
 
 #undef listen
-#define listen		 rb_w32_listen
+#define listen(s, b)		rb_w32_listen(s, b)
 
 #undef recv
-#define recv		 rb_w32_recv
+#define recv(s, b, l, f)	rb_w32_recv(s, b, l, f)
 
 #undef recvfrom
-#define recvfrom	 rb_w32_recvfrom
+#define recvfrom(s, b, l, f, fr, frl) rb_w32_recvfrom(s, b, l, f, fr, frl)
 
 #undef send
-#define send		 rb_w32_send
+#define send(s, b, l, f)	rb_w32_send(s, b, l, f)
 
 #undef sendto
-#define sendto		 rb_w32_sendto
+#define sendto(s, b, l, f, t, tl) rb_w32_sendto(s, b, l, f, t, tl)
 
 #undef setsockopt
-#define setsockopt	 rb_w32_setsockopt
+#define setsockopt(s, v, n, o, l) rb_w32_setsockopt(s, v, n, o, l)
 
 #undef shutdown
-#define shutdown	 rb_w32_shutdown
+#define shutdown(s, h)		rb_w32_shutdown(s, h)
 
 #undef socket
-#define socket		 rb_w32_socket
+#define socket(s, t, p)		rb_w32_socket(s, t, p)
 
 #undef gethostbyaddr
-#define gethostbyaddr	 rb_w32_gethostbyaddr
+#define gethostbyaddr(a, l, t)	rb_w32_gethostbyaddr(a, l, t)
 
 #undef gethostbyname
-#define gethostbyname	 rb_w32_gethostbyname
+#define gethostbyname(n)	rb_w32_gethostbyname(n)
 
 #undef gethostname
-#define gethostname	 rb_w32_gethostname
+#define gethostname(n, l)	rb_w32_gethostname(n, l)
 
 #undef getprotobyname
-#define getprotobyname	 rb_w32_getprotobyname
+#define getprotobyname(n)	rb_w32_getprotobyname(n)
 
 #undef getprotobynumber
-#define getprotobynumber rb_w32_getprotobynumber
+#define getprotobynumber(n)	rb_w32_getprotobynumber(n)
 
 #undef getservbyname
-#define getservbyname	 rb_w32_getservbyname
+#define getservbyname(n, p)	rb_w32_getservbyname(n, p)
 
 #undef getservbyport
-#define getservbyport	 rb_w32_getservbyport
+#define getservbyport(p, pr)	rb_w32_getservbyport(p, pr)
 
 #undef socketpair
-#define socketpair	 rb_w32_socketpair
+#define socketpair(a, t, p, s)	rb_w32_socketpair(a, t, p, s)
 
 #undef get_osfhandle
-#define get_osfhandle	 rb_w32_get_osfhandle
+#define get_osfhandle(h)	rb_w32_get_osfhandle(h)
 
 #undef getcwd
-#define getcwd		 rb_w32_getcwd
+#define getcwd(b, s)		rb_w32_getcwd(b, s)
 
 #undef getenv
-#define getenv		 rb_w32_getenv
+#define getenv(n)		rb_w32_getenv(n)
 
 #undef rename
-#define rename		 rb_w32_rename
+#define rename(o, n)		rb_w32_rename(o, n)
 
 #undef times
-#define times		 rb_w32_times
+#define times(t)		rb_w32_times(t)
 #endif
 
 #endif
