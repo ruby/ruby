c09f46301b20f7e86ff776fd8000d47c3f94c93f
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-12-05 10:08:07 +0000

    * regparse.c (PFETCH_READY): separate gcc specific trick.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@33954 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- regparse.c
+++ regparse.c
@@ -258,7 +258,12 @@ strdup_with_null(OnigEncoding enc, UChar* s, UChar* end)
 /* scan pattern methods */
 #define PEND_VALUE   0
 
+#ifderf __GNUC__
+/* get rid of Wunused-but-set-variable */
 #define PFETCH_READY  UChar* pfetch_prev = pfetch_prev
+#else
+#define PFETCH_READY  UChar* pfetch_prev
+#endif
 #define PEND         (p < end ?  0 : 1)
 #define PUNFETCH     p = pfetch_prev
 #define PINC       do { \
