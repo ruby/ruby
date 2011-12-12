f2323dee148b5ca9a186e19432ac7b4fc28491d6
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-12-12 08:30:24 +0000

    Set yamler as syck.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@34014 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/syck/test_array.rb
+++ test/syck/test_array.rb
@@ -4,7 +4,13 @@ require 'yaml'
 module Syck
   class TestArray < Test::Unit::TestCase
     def setup
+      @current_engine = YAML::ENGINE.yamler
+      YAML::ENGINE.yamler = 'syck'
       @list = [{ :a => 'b' }, 'foo']
+    end
+
+    def teardown
+      YAML::ENGINE.yamler = @current_engine
     end
 
     def test_to_yaml
