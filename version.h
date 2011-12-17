63783f2c9735c6ce9605fde4c658ac9cd6df4c8b
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-12-17 19:26:47 +0000

    * 2011-12-18
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_2@34062 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -5,9 +5,9 @@
 #define RUBY_VERSION_TEENY 1
 
 #define RUBY_RELEASE_YEAR 2011
-#define RUBY_RELEASE_MONTH 8
-#define RUBY_RELEASE_DAY 11
-#define RUBY_RELEASE_DATE "2011-08-11"
+#define RUBY_RELEASE_MONTH 12
+#define RUBY_RELEASE_DAY 18
+#define RUBY_RELEASE_DATE "2011-12-18"
 
 #include "ruby/version.h"
 
