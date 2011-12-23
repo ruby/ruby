8397134330c388e562a8f7aa255b36dbcd7efecf
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2011-12-23 21:59:54 +0000

    * test/readline/test_readline.rb (test_line_buffer__point): use   lambda not to exit entire method by "return".  or "next" for   proc.  [ruby-dev:45042] [Bug #5802]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@34115 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/readline/test_readline.rb
+++ test/readline/test_readline.rb
@@ -136,20 +136,40 @@ class TestReadline < Test::Unit::TestCase
         actual_text = nil
         actual_line_buffer = nil
         actual_point = nil
-        Readline.completion_proc = proc { |text|
+        Readline.completion_proc = ->(text) {
           actual_text = text
           actual_point = Readline.point
-          actual_buffer_line = Readline.line_buffer
+          actual_line_buffer = Readline.line_buffer
           stdin.write(" finish\n")
           stdin.close
           stdout.close
           return ["complete"]
         }
+
         stdin.write("first second\t")
         stdin.flush
+        Readline.completion_append_character = " "
         line = replace_stdio(stdin.path, stdout.path) {
           Readline.readline("> ", false)
         }
+        assert_equal("second", actual_text)
+        assert_equal("first second", actual_line_buffer)
+        assert_equal(12, actual_point)
+        assert_equal("first complete  finish", Readline.line_buffer)
+        assert_equal(Encoding.find("locale"), Readline.line_buffer.encoding)
+        assert_equal(true, Readline.line_buffer.tainted?)
+        assert_equal(22, Readline.point)
+
+        stdin.open
+        stdout.open
+
+        stdin.write("first second\t")
+        stdin.flush
+        Readline.completion_append_character = nil
+        line = replace_stdio(stdin.path, stdout.path) {
+          Readline.readline("> ", false)
+        }
+        assert_equal("second", actual_text)
         assert_equal("first second", actual_line_buffer)
         assert_equal(12, actual_point)
         assert_equal("first complete finish", Readline.line_buffer)
