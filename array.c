32e79b64eab63970b8dc9c6d254513f6e2d5e5ad
ayumin <ayumin@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-01-05 02:52:10 +0000

    Fix typo
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@34212 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- array.c
+++ array.c
@@ -3955,7 +3955,7 @@ rb_ary_shuffle(int argc, VALUE *argv, VALUE ary)
  *  If +rng+ is given, it will be used as the random number generator.
  *
  *     a = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
- *     a.sample         #=> [7]
+ *     a.sample         #=> 7
  *     a.sample(4)      #=> [6, 4, 2, 5]
  */
 
