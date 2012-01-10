931459d974390436161aa98fa5a4fc94e00c3951
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-01-10 03:50:23 +0000

    * gc.c: in fact, i686-linux doesn't need to define _XOPEN_SOURCE 600.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@34258 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- gc.c
+++ gc.c
@@ -37,9 +37,6 @@
 #if defined _WIN32 || defined __CYGWIN__
 #include <windows.h>
 #elif defined(HAVE_POSIX_MEMALIGN)
-# ifdef __linux__ /* i386 linux needs #define _XOPEN_SOURCE 600 to use posix_memalign */
-#  define _XOPEN_SOURCE 600
-# endif
 #elif defined(HAVE_MEMALIGN)
 #include <malloc.h>
 #endif
