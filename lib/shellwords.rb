8c72fc9bf09071300b6283411b4481456aac7e34
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-01-12 18:42:03 +0000

    * lib/shellwords.rb (Shellwords#shellsplit): Fix a bug where   consecutive backslashes in double quotes are all removed except   the one at the tail.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@34285 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/shellwords.rb
+++ lib/shellwords.rb
@@ -41,7 +41,7 @@ module Shellwords
     line.scan(/\G\s*(?>([^\s\\\'\"]+)|'([^\']*)'|"((?:[^\"\\]|\\.)*)"|(\\.?)|(\S))(\s|\z)?/m) do
       |word, sq, dq, esc, garbage, sep|
       raise ArgumentError, "Unmatched double quote: #{line.inspect}" if garbage
-      field << (word || sq || (dq || esc).gsub(/\\(?=.)/, ''))
+      field << (word || sq || (dq || esc).gsub(/\\(.)/, '\\1'))
       if sep
         words << field
         field = ''
