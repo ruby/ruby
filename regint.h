16c364121b8fe725087dadad8c19f45a4eddc958
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-02-12 00:30:50 +0000

    merge revision(s) 32544: [Backport #6005]
    	* regint.h (PLATFORM_UNALIGNED_WORD_ACCESS): Power PC does not
    	  allow unaligned word access.
    
    	* st.c (UNALIGNED_WORD_ACCESS): x86_64 allows unaligned word
    	  access as well as i386.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@34565 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- regint.h
+++ regint.h
@@ -48,7 +48,6 @@
 #endif
 
 #if defined(__i386) || defined(__i386__) || defined(_M_IX86) || \
-    (defined(__ppc__) && defined(__APPLE__)) || \
     defined(__x86_64) || defined(__x86_64__) || defined(_M_AMD86) || \
     defined(__mc68020__)
 #define PLATFORM_UNALIGNED_WORD_ACCESS
--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 95
+#define RUBY_PATCHLEVEL 96
 
-#define RUBY_RELEASE_DATE "2012-02-11"
+#define RUBY_RELEASE_DATE "2012-02-12"
 #define RUBY_RELEASE_YEAR 2012
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 11
+#define RUBY_RELEASE_DAY 12
 
 #include "ruby/version.h"
 
