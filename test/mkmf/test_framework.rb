8e799c3e5b66b7a0d51ba9e904eec89ad719fb1d
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-02-22 01:32:14 +0000

    * test/mkmf/test_framework.rb: try CoreFoundation framework, than   Cocoa which is dependent on QuickTime SDK which has separated   since Xcode 4.3.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@34729 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/mkmf/test_framework.rb
+++ test/mkmf/test_framework.rb
@@ -2,8 +2,8 @@ require_relative 'base'
 
 class TestMkmf
   class TestHaveFramework < TestMkmf
-    def test_cocoa_framework
-      assert(have_framework("Cocoa"), "try as Objective-C")
+    def test_core_foundation_framework
+      assert(have_framework("CoreFoundation"), "try as Objective-C")
     end
   end
 end if /darwin/ =~ RUBY_PLATFORM
