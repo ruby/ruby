ffdaca1d748804f2b5ca2de612f17cf6c78d351b
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-02-28 02:56:59 +0000

    merge revision(s) 34840:
    	* configure.in (debugflags): check if -ggdb is accepted.
    	  [ruby-core:42875][Bug #6080]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@34841 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 152
+#define RUBY_PATCHLEVEL 153
 
-#define RUBY_RELEASE_DATE "2012-02-27"
+#define RUBY_RELEASE_DATE "2012-02-28"
 #define RUBY_RELEASE_YEAR 2012
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 27
+#define RUBY_RELEASE_DAY 28
 
 #include "ruby/version.h"
 
