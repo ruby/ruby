62fe1ff7eee6c66cc813cbb8f7af24e62cd1c857
Sokolov Yura 'funny-falcon <funny.falcon@gmail.com>
2012-03-09 03:44:22 +0400

    st: test when delete_safe forces iteration of packed table to fail

--- test/-ext-/st/test_numhash.rb
+++ test/-ext-/st/test_numhash.rb
@@ -32,5 +32,18 @@ class Bug::StNumHash
         assert_equal(up - 1, tbl.size, "delete_safe doesn't change size from #{up} to #{up-1}")
       end
     end
+
+    def test_delete_safe_on_iteration
+      10.downto(1) do |up|
+        tbl = Bug::StNumHash.new
+        1.upto(up){|i| tbl[i] = i}
+        assert_nothing_raised("delete_safe forces iteration to fail with size #{up}") do
+          tbl.each do |k, v, t|
+            assert_equal k, t.delete_safe(k)
+            true
+          end
+        end
+      end
+    end
   end
 end
