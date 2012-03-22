caf627afce53cb5dfd34c435e1be030724043cbb
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-03-22 02:14:42 +0000

    * test/ruby/test_io.rb (TestIO#test_pos_with_getc): updated.   see [ruby-core:43550]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@35098 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_io.rb
+++ test/ruby/test_io.rb
@@ -1360,21 +1360,23 @@ class TestIO < Test::Unit::TestCase
   def test_pos_with_getc
     bug6179 = '[ruby-core:43497]'
     t = make_tempfile
-    open(t.path, "w") do |f|
-      f.write "0123456789"
-    end
+    ["", "t", "b"].each do |mode|
+      open(t.path, "w#{mode}") do |f|
+        f.write "0123456789\n"
+      end
 
-    open(t.path, "r") do |f|
-      assert_equal 0, f.pos
-      assert_equal '0', f.getc
-      assert_equal 1, f.pos
-      assert_equal '1', f.getc
-      assert_equal 2, f.pos
-      assert_equal '2', f.getc
-      assert_equal 3, f.pos
-      assert_equal '3', f.getc
-      assert_equal 4, f.pos
-      assert_equal '4', f.getc
+      open(t.path, "r#{mode}") do |f|
+        assert_equal 0, f.pos, "mode=r#{mode}"
+        assert_equal '0', f.getc, "mode=r#{mode}"
+        assert_equal 1, f.pos, "mode=r#{mode}"
+        assert_equal '1', f.getc, "mode=r#{mode}"
+        assert_equal 2, f.pos, "mode=r#{mode}"
+        assert_equal '2', f.getc, "mode=r#{mode}"
+        assert_equal 3, f.pos, "mode=r#{mode}"
+        assert_equal '3', f.getc, "mode=r#{mode}"
+        assert_equal 4, f.pos, "mode=r#{mode}"
+        assert_equal '4', f.getc, "mode=r#{mode}"
+      end
     end
   end
 
