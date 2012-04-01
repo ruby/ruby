f83ded92545335bf72b895170f63a8d214ef2a93
Mahmut Bulut <mahmutbulut0@gmail.com>
2012-04-01 21:02:39 +0300

    is_integer? method declaration in prelude, generally wanted from developers

--- golf_prelude.rb
+++ golf_prelude.rb
@@ -84,6 +84,10 @@ class String
     split('')
   end
 
+  def is_integer?
+    self == self.to_i.to_s
+  end
+  
   (Array.instance_methods-instance_methods-[:to_ary,:transpose,:flatten,:flatten!,:compact,:compact!,:assoc,:rassoc]).each{|meth|
     eval"
     def #{meth}(*args, &block)
