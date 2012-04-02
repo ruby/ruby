0ec539240cdac4b9645687328c5197ecabbe3d83
kazu <kazu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-04-02 15:29:12 +0000

    fix typos
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@35216 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- missing/alloca.c
+++ missing/alloca.c
@@ -30,7 +30,7 @@ static char	SCCSid[] = "@(#)alloca.c	1.1";	/* for the "what" utility */
 #endif
 
 #include "ruby/config.h"
-#define X3J11 1		   /* config.h should contain void if neede */
+#define X3J11 1		   /* config.h should contain void if needed */
 
 #ifdef C_ALLOCA
 
