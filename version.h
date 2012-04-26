85f444d407a3f4d004b949e9846cae3faaa2cd77
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-04-26 23:30:04 +0000

    merge revision(s) 35417:
    	* test/rinda/test_rinda.rb: fix sticks on some testsf problem 
    	  [Bug #6272]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@35481 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 200
+#define RUBY_PATCHLEVEL 201
 
-#define RUBY_RELEASE_DATE "2012-04-25"
+#define RUBY_RELEASE_DATE "2012-04-27"
 #define RUBY_RELEASE_YEAR 2012
 #define RUBY_RELEASE_MONTH 4
-#define RUBY_RELEASE_DAY 25
+#define RUBY_RELEASE_DAY 27
 
 #include "ruby/version.h"
 
