965121cdc5675d6d6f3ed356210eb447e87f7b4d
ayumin <ayumin@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-05-08 04:47:35 +0000

    * ext/date/date_core.c: improving introduction in Date/DateTime doc.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@35590 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/date/date_core.c
+++ ext/date/date_core.c
@@ -9234,30 +9234,41 @@ Init_date_core(void)
     negative_inf = -INFINITY;
 
     /*
-     * date and datetime class - Tadayoshi Funaba 1998-2011
+     * The Date and DateTime classes provide methods for storing, comparing, transforming, and operating on values of date and time. Date objects store only the date (e.g. 1987-06-29), while DateTime objects store both the date and time (e.g. 1984-02-09 05:32:12).
      *
-     * 'date' provides two classes Date and DateTime.
+     * 1998-2011 by Tadayoshi Funaba
+     *
+     * = Introduction
+     *
+     * Date and DateTime are included in the standard library. To use, require ‘date’ will provide both classes.
      *
      * == Terms and definitions
      *
      * Some terms and definitions are based on ISO 8601 and JIS X 0301.
      *
-     * === calendar date
+     * === Calendar date
      *
      * The calendar date is a particular day of a calendar year,
      * identified by its ordinal number within a calendar month within
      * that year.
      *
-     * In those classes, this is so-called "civil".
+     * In the Date and DateTime classes, this is called "civil."
      *
-     * === ordinal date
+     *      Date.civil(2012,03,17)
+     * This refers to March 17th, 2012.
+     *
+     * === Ordinal date
      *
      * The ordinal date is a particular day of a calendar year identified
      * by its ordinal number within the year.
      *
-     * In those classes, this is so-called "ordinal".
+     * In the Date and DateTime classes, this is called "ordinal."
      *
-     * === week date
+     *      Date.ordinal(2012,77)
+     * This refers to the 77th day of 2012.
+     *
+     *
+     * === Week date
      *
      * The week date is a date identified by calendar week and day numbers.
      *
@@ -9267,9 +9278,13 @@ Init_date_core(void)
      * includes the first Thursday of that year.  In the Gregorian
      * calendar, this is equivalent to the week which includes January 4.
      *
-     * In those classes, this so-called "commercial".
+     * In the Date and DateTime classes, this is called "commercial."
      *
-     * === julian day number
+     *      Date.commercial(2012,11,06)
+     * This refers to the 6th day of the 11th week of 2012.
+     *
+     *
+     * === Julian day number
      *
      * The Julian day number is in elapsed days since noon (Greenwich mean
      * time) on January 1, 4713 BCE (in the Julian calendar).
@@ -9283,9 +9298,12 @@ Init_date_core(void)
      * it just refers to "chronological Julian day number", not the
      * original.
      *
-     * In those classes, those are so-called "ajd" and "jd".
+     * In the Date and DateTime classes, this is called "ajd" and "jd."
      *
-     * === modified julian day number
+     *      Date.jd(2456004)
+     * This refers to 2,456,004 days since January 1, 4713 BCE.
+     *
+     * === Modified julian day number
      *
      * The modified Julian day number is in elapsed days since midnight
      * (Coordinated universal time) on November 17, 1858 CE (in the
@@ -9301,8 +9319,10 @@ Init_date_core(void)
      * appears, it just refers to "chronological modified Julian day
      * number", not the original.
      *
-     * In those classes, this is so-called "mjd".
+     * In the Date and DateTime classes, this is called "mjd."
      *
+     *      Date.new(2012,03,17).mjd #=> 56003
+     * 2012-03-17 is 56,003 days after November 17, 1858 CE.
      *
      * == Date
      *
