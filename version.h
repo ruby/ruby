0ac2587d0942d57d52df2d777caca06062454197
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-05-19 05:40:29 +0000

    merge revision(s) 33611:
    	* lib/webrick/utils.rb: fix fcntl call.
    
    	* lib/drb/unix.rb: ditto.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@35712 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 217
+#define RUBY_PATCHLEVEL 218
 
-#define RUBY_RELEASE_DATE "2012-05-18"
+#define RUBY_RELEASE_DATE "2012-05-19"
 #define RUBY_RELEASE_YEAR 2012
 #define RUBY_RELEASE_MONTH 5
-#define RUBY_RELEASE_DAY 18
+#define RUBY_RELEASE_DAY 19
 
 #include "ruby/version.h"
 
