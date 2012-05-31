711c067eb52e76bbaaf3a70a5b75e52a82a55ce1
drbrain <drbrain@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-05-31 00:37:45 +0000

    * test/dl/test_c_struct_entry.rb:  Removed duplicated test
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@35851 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/dl/test_c_struct_entry.rb
+++ test/dl/test_c_struct_entry.rb
@@ -24,11 +24,6 @@ class DL::TestCStructEntity < DL::TestBase
   def test_class_size_with_count
     size = DL::CStructEntity.size([[DL::TYPE_DOUBLE, 2], [DL::TYPE_CHAR, 20]])
 
-    assert_equal DL::TYPE_DOUBLE * 2 + DL::SIZEOF_CHAR * 20, size
-  end
-  def test_class_size_with_count
-    size = DL::CStructEntity.size([[DL::TYPE_DOUBLE, 2], [DL::TYPE_CHAR, 20]])
-
     types = [DL::TYPE_DOUBLE, DL::TYPE_CHAR]
     alignments = types.map { |type| DL::PackInfo::ALIGN_MAP[type] }
 
