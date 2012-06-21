21e1939d45d98421def8d64fafe14fd02b58a015
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-06-21 04:46:26 +0000

    merge revision(s) 35625: [Backport #6618]
    	* thread.c (rb_threadptr_execute_interrupts_common):
    	  test_signal_requiring of test/ruby/test_signal.rb fail if the sub
    	  process is killed on waiting IO in lex_io_gets in rb_load_file in
    	  rb_load_internal in require.
    	  This is because
    	  (1) the process receive the killing signal in
    	      rb_thread_io_blocking_region in rb_read_internal in lex_io_gets.
    	  (2) set th->errinfo as INT2FIX(TAG_FATAL) at
    	      rb_threadptr_execute_interrupts_common.
    	  (3) escape rb_load_file in rb_load_internal and jump to EXEC_TAG()
    	      without set loaded as TRUE.
    	  (4) call first rb_exc_raise(GET_THREAD()->errinfo); because loaded
    	      is FALSE as above. this errinfo should be an exception object
    	      but this is INT2FIX(TAG_FATAL).
    	  Don't call first rb_exc_raise if GET_THREAD()->errinfo is Fixnum.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@36159 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 236
+#define RUBY_PATCHLEVEL 237
 
-#define RUBY_RELEASE_DATE "2012-06-11"
+#define RUBY_RELEASE_DATE "2012-06-21"
 #define RUBY_RELEASE_YEAR 2012
 #define RUBY_RELEASE_MONTH 6
-#define RUBY_RELEASE_DAY 11
+#define RUBY_RELEASE_DAY 21
 
 #include "ruby/version.h"
 
