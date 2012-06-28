1d15cf4bebc379a0f70306ae165680ffe4a12d87
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-06-28 08:58:21 +0000

    merge revision(s) 32549,32557: [Backport #6661]
    	* win32/win32.c, include/ruby/win32.h (rb_w32_io_cancelable_p): renamed
      from rb_w32_has_cancel_io().  now it takes a parameter as fd to check
      the fd is console or not, because we cannot cancel console input even
      if we have cancel_io function.
    
    	* io.c (WAIT_FD_IN_WIN32): call above function instead of the old one,
      so now we can kill the thread which calls STDIN.gets.
      the problem was reported by ko1 vir IRC.
    
    	* win32/win32.c (is_socket, is_console): add prototypes to fix compile
    	  problem with gcc introduced at r32549.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@36244 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 242
+#define RUBY_PATCHLEVEL 243
 
-#define RUBY_RELEASE_DATE "2012-06-27"
+#define RUBY_RELEASE_DATE "2012-06-28"
 #define RUBY_RELEASE_YEAR 2012
 #define RUBY_RELEASE_MONTH 6
-#define RUBY_RELEASE_DAY 27
+#define RUBY_RELEASE_DAY 28
 
 #include "ruby/version.h"
 
