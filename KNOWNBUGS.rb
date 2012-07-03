1686286ef1655b7f97a054baae23f1955ce9f598
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-07-03 10:17:20 +0000

    merge revision(s) 36258:
    	* KNOWNBUGS.rb: Bug #2330 was fixed in r26718.
      [ruby-dev:40234] [ruby-core:27959]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@36285 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- KNOWNBUGS.rb
+++ KNOWNBUGS.rb
@@ -3,14 +3,3 @@
 # So all tests will cause failure.
 #
 
-assert_normal_exit %q{
-  open("tst-remove-load.rb", "w") {|f|
-    f << <<-'End'
-      module Kernel
-        remove_method :load
-      end
-      raise
-    End
-  }
-  load "tst-remove-load.rb"
-}, '[ruby-dev:40234] [ruby-core:27959]', :timeout => 1
