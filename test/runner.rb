c3521bf6313cbba26d3f407674b89180c8ba073d
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-07-12 03:32:28 +0000

    test: skip default gems
    * test/runner.rb: skip default gems to get rid of loading old versions
      before installation.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@36374 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/runner.rb
+++ test/runner.rb
@@ -10,6 +10,15 @@ class Gem::TestCase < MiniTest::Unit::TestCase
   @@project_dir = File.dirname($LOAD_PATH.last)
 end
 
+srcdir = File.dirname(src_testdir)
+default_gems = Dir.glob(srcdir + "/{lib,ext}/**/*.gemspec").map {|path| File.basename(path, ".*")}
+File.foreach(srcdir + "/defs/default_gems") do |line|
+  next if /^\s*#/ =~ line
+  default_gems << line[/^\S+/]
+end
+default_gems |= (ENV["GEM_SKIP"] || '').split(/:/)
+ENV["GEM_SKIP"] = default_gems.join(':')
+
 require_relative 'profile_test_all' if ENV['RUBY_TEST_ALL_PROFILE'] == 'true'
 
 exit Test::Unit::AutoRunner.run(true, src_testdir)
