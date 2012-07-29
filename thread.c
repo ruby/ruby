9cdf55485ac9783b43975d1fa68f92ebfa6bbdcc
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-07-29 16:00:36 +0000

    * thread.c (thread_create_core): hide th->async_errinfo_mask_stack from   ObjectSpace.each_object. refix of r36539.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@36570 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- thread.c
+++ thread.c
@@ -565,6 +565,7 @@ thread_create_core(VALUE thval, VALUE args, VALUE (*fn)(ANYARGS))
     th->async_errinfo_queue = rb_ary_tmp_new(0);
     th->async_errinfo_queue_checked = 0;
     th->async_errinfo_mask_stack = rb_ary_dup(current_th->async_errinfo_mask_stack);
+    RBASIC(th->async_errinfo_mask_stack)->klass = 0;
 
     native_mutex_initialize(&th->interrupt_lock);
     if (GET_VM()->event_hooks != NULL)
