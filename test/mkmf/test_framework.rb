cc59a581f1db0ffc6e8cac93cae50202e1ab81a8
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-08-01 22:29:58 +0000

    test_framework.rb: show mkmf.log on failure
    * test/mkmf/test_framework.rb (test_core_foundation_framework): show
      mkmf.log on failure.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@36591 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/mkmf/test_framework.rb
+++ test/mkmf/test_framework.rb
@@ -3,7 +3,7 @@ require_relative 'base'
 class TestMkmf
   class TestHaveFramework < TestMkmf
     def test_core_foundation_framework
-      assert(have_framework("CoreFoundation"), "try as Objective-C")
+      assert(have_framework("CoreFoundation"), mkmflog("try as Objective-C"))
     end
   end
 end if /darwin/ =~ RUBY_PLATFORM
