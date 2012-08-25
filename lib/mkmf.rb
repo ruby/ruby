22bb811a733c5c402ba9e06c69b71a5b0e046ddb
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-08-25 00:55:34 +0000

    mkmf.rb: clean timestamps
    * lib/mkmf.rb (MakeMakefile::CLEANINGS): clean timestamp files.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@36820 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -2408,7 +2408,7 @@ clean-rb-default::
 clean-rb::
 clean-so::
 clean: clean-so clean-static clean-rb-default clean-rb
-\t\t-$(Q)$(RM) $(CLEANLIBS#{sep}) $(CLEANOBJS#{sep}) $(CLEANFILES#{sep})
+\t\t-$(Q)$(RM) $(CLEANLIBS#{sep}) $(CLEANOBJS#{sep}) $(CLEANFILES#{sep}) .*.time
 
 distclean-rb-default::
 distclean-rb::
