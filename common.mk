5809d668bc6b8e43e117925d10a41e9e62d1c629
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-09-05 05:29:46 +0000

    common.mk: partial revert r36867
    * common.mk: revert r36867 partially, to use files in the source
      directory when out-of-place build.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@36909 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- common.mk
+++ common.mk
@@ -560,8 +560,8 @@ $(ENC_MK): $(srcdir)/enc/make_encmake.rb $(srcdir)/enc/Makefile.in $(srcdir)/enc
 
 PHONY:
 
-parse.c: {$(VPATH)}parse.y $(srcdir)/tool/ytab.sed {$(VPATH)}id.h
-parse.h: {$(VPATH)}parse.c
+{$(VPATH)}parse.c: {$(VPATH)}parse.y $(srcdir)/tool/ytab.sed {$(VPATH)}id.h
+{$(VPATH)}parse.h: {$(VPATH)}parse.c
 
 {$(srcdir)}.y.c:
 	$(ECHO) generating $@
