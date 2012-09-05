f65e683ccc3b6689ac00e7c1fe0fd2c0384824b2
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-09-05 01:44:36 +0000

    dir.c: not recurse dot files
    * dir.c (glob_helper): skip dot files early on recursive match.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@36903 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- dir.c
+++ dir.c
@@ -1379,15 +1379,24 @@ glob_helper(
 	if (dirp == NULL) return 0;
 
 	while (READDIR(dirp, enc, &STRUCT_DIRENT(entry), dp)) {
-	    char *buf = join_path(path, dirsep, dp->d_name);
+	    char *buf;
 	    enum answer new_isdir = UNKNOWN;
 
+	    if (recursive && dp->d_name[0] == '.') {
+		/* RECURSIVE never match dot files unless FNM_DOTMATCH is set */
+		if (!(flags & FNM_DOTMATCH)) continue;
+
+		/* always skip current and parent directories not to recurse infinitely */
+		if (!dp->d_name[1]) continue;
+		if (dp->d_name[1] == '.' && !dp->d_name[2]) continue;
+	    }
+
+	    buf = join_path(path, dirsep, dp->d_name);
 	    if (!buf) {
 		status = -1;
 		break;
 	    }
-	    if (recursive && strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0
-		&& fnmatch("*", rb_usascii_encoding(), dp->d_name, flags) == 0) {
+	    if (recursive) {
 #ifndef _WIN32
 		if (do_lstat(buf, &st, flags) == 0)
 		    new_isdir = S_ISDIR(st.st_mode) ? YES : S_ISLNK(st.st_mode) ? UNKNOWN : NO;
