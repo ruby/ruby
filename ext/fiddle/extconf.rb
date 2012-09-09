390bbfed581a17fac72a9e029624fd667d81a0c8
kosaki <kosaki@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-09-09 06:44:20 +0000

    merge revision(s) 35687:35691:
    	* ext/zlib/extconf.rb: Use an exception instaed of bare puts.
    
    	* ext/psych/extconf.rb: Use an exception instaed of bare abort.
    
    	* ext/fiddle/extconf.rb: Use an exception instaed of bare abort.
    
    	* ext/readline/extconf.rb: Use an exception instead of bare exit.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@36930 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/fiddle/extconf.rb
+++ ext/fiddle/extconf.rb
@@ -9,12 +9,12 @@ unless have_header('ffi.h')
   if have_header('ffi/ffi.h')
     $defs.push(format('-DUSE_HEADER_HACKS'))
   else
-    abort "ffi.h is missing. Please install libffi."
+    raise "ffi.h is missing. Please install libffi."
   end
 end
 
 unless have_library('ffi') || have_library('libffi')
-  abort "libffi is missing. Please install libffi."
+  raise "libffi is missing. Please install libffi."
 end
 
 have_header 'sys/mman.h'
