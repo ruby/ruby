6ae7a4fcf6040deb5d22238d649e115c0d47fd71
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-10-12 08:37:54 +0000

    merge revision(s) 33857,37157:
    	Run the test only if /dev/tty exists.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@37158 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_io.rb
+++ test/ruby/test_io.rb
@@ -2066,7 +2066,8 @@ End
 
   def test_ioctl_linux2
     return if /linux/ !~ RUBY_PLATFORM
-    return if /^i?86|^x86_64/ !~ RUBY_PLATFORM
+    return if /^i.?86|^x86_64/ !~ RUBY_PLATFORM
+    return unless File.exist?('/dev/tty')
 
     File.open('/dev/tty') { |f|
       tiocgwinsz=0x5413
