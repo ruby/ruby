b00362eb3e7f0c50a6cd1e46940167b7fd86f1a4
tenderlove <tenderlove@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-10-22 21:02:16 +0000

    * ext/psych/lib/psych.rb: Changed comment in psych.rb to update new   home page for libyaml.  Thanks to Carolyn Ann.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@37285 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/psych/lib/psych.rb
+++ ext/psych/lib/psych.rb
@@ -18,10 +18,12 @@ require 'psych/handlers/document_stream'
 ###
 # = Overview
 #
-# Psych is a YAML parser and emitter.  Psych leverages
-# libyaml[http://libyaml.org] for it's YAML parsing and emitting capabilities.
-# In addition to wrapping libyaml, Psych also knows how to serialize and
-# de-serialize most Ruby objects to and from the YAML format.
+# Psych is a YAML parser and emitter. 
+# Psych leverages libyaml [Home page: http://pyyaml.org/wiki/LibYAML] 
+# or [Git repo: https://github.com/zerotao/libyaml] for its YAML parsing
+# and emitting capabilities. In addition to wrapping libyaml, Psych also
+# knows how to serialize and de-serialize most Ruby objects to and from
+# the YAML format.
 #
 # = I NEED TO PARSE OR EMIT YAML RIGHT NOW!
 #
