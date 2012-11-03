bd2024165c8d9380a75b88607a9c0dfe066aae5f
kou <kou@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-11-03 03:06:50 +0000

    * test/rexml/test_xml_declaration.rb (TestXmlDeclaration#test_*):   Remove needless prefix from test names.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@37437 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/rexml/test_xml_declaration.rb
+++ test/rexml/test_xml_declaration.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
 #
 #  Created by Henrik Mårtensson on 2007-02-18.
 #  Copyright (c) 2007. All rights reserved.
@@ -18,15 +19,15 @@ class TestXmlDeclaration < Test::Unit::TestCase
     @xml_declaration = @doc.children[0]
   end
 
-  def test_xml_declaration_is_first_child
+  def test_is_first_child
     assert_kind_of(REXML::XMLDecl, @xml_declaration)
   end
 
-  def test_xml_declaration_has_document_as_parent
+  def test_has_document_as_parent
    assert_kind_of(REXML::Document, @xml_declaration.parent)
   end
 
-  def test_xml_declaration_has_sibling
+  def test_has_sibling
     assert_kind_of(REXML::XMLDecl, @root.previous_sibling.previous_sibling)
     assert_kind_of(REXML::Element, @xml_declaration.next_sibling.next_sibling)
   end
