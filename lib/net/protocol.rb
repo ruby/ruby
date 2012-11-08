6ce8c33936d59716a0676d2ec8e22b22e2a1da94
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-11-08 10:04:24 +0000

    * lib/net/protocol.rb (Net::InternetMessageIO#each_crlf_line):   treat \r as newline as mame pointed. [ruby-dev:46425] [Bug #7278]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@37563 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/net/protocol.rb
+++ lib/net/protocol.rb
@@ -322,7 +322,7 @@ module Net # :nodoc:
 
     def each_crlf_line(src)
       buffer_filling(@wbuf, src) do
-        while line = @wbuf.slice!(/\A.*(?:\n|\r\n|\r(?!\z))/)
+        while line = @wbuf.slice!(/\A[^\r\n]*(?:\n|\r(?:\n|(?!\z)))/)
           yield line.chomp("\n") + "\r\n"
         end
       end
--- test/net/protocol/test_protocol.rb
+++ test/net/protocol/test_protocol.rb
@@ -5,7 +5,15 @@ require "stringio"
 class TestProtocol < Test::Unit::TestCase
   def test_each_crlf_line
     assert_output('', '') do
-      Net::InternetMessageIO.new(StringIO.new("")).write_message("\u3042\r\n\u3044\r\n\u3046")
+      sio = StringIO.new("")
+      imio = Net::InternetMessageIO.new(sio)
+      assert_equal(23, imio.write_message("\u3042\r\u3044\n\u3046\r\n\u3048"))
+      assert_equal("\u3042\r\n\u3044\r\n\u3046\r\n\u3048\r\n.\r\n", sio.string)
+
+      sio = StringIO.new("")
+      imio = Net::InternetMessageIO.new(sio)
+      assert_equal(8, imio.write_message("\u3042\r"))
+      assert_equal("\u3042\r\n.\r\n", sio.string)
     end
   end
 end
