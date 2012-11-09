7c9a4b2f0abea9754c4a26da65f76561f81da08f
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-11-09 15:11:03 +0000

    * file.c (append_fspath): revert a part of r37562.
    * file.c (rb_file_expand_path_internal): ditto.
    
    * file.c (rb_file_expand_path_internal): ignore the encoding of the
      given path name and use filesystem encoding, except when the result
      is 8bit range and the filesystem encoding is US-ASCII.
      [ruby-dev:39393] [Bug #2154]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@37596 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 324
+#define RUBY_PATCHLEVEL 325
 
-#define RUBY_RELEASE_DATE "2012-11-09"
+#define RUBY_RELEASE_DATE "2012-11-10"
 #define RUBY_RELEASE_YEAR 2012
 #define RUBY_RELEASE_MONTH 11
-#define RUBY_RELEASE_DAY 9
+#define RUBY_RELEASE_DAY 10
 
 #include "ruby/version.h"
 
