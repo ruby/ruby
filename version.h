122b6dbcb7c7c5e3c02277d75b5f38bc6d5c3103
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-11-09 02:21:31 +0000

    merge revision(s) 37565: [Backport #7315]
    	* bignum.c (bigmul0): disable big_mul_toom3_temporalily.
    	  [ruby-core:48552] [Bug #7242]
    
    	* test/ruby/test_bignum.rb (test_mul_large_numbers):
    	  add a test for bigmul1_toom3 suggested in [Bug #7242].
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@37570 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 318
+#define RUBY_PATCHLEVEL 319
 
-#define RUBY_RELEASE_DATE "2012-11-08"
+#define RUBY_RELEASE_DATE "2012-11-09"
 #define RUBY_RELEASE_YEAR 2012
 #define RUBY_RELEASE_MONTH 11
-#define RUBY_RELEASE_DAY 8
+#define RUBY_RELEASE_DAY 9
 
 #include "ruby/version.h"
 
