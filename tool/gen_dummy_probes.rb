86541e02c18e6ccdd0c0c632da06120a04a9ef11
kosaki <kosaki@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-11-22 08:41:19 +0000

    * tool/gen_dummy_probes.rb: don't change #include, #if and #endif   lines. [Bug #7370]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@37799 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/gen_dummy_probes.rb
+++ tool/gen_dummy_probes.rb
@@ -2,10 +2,10 @@
 # -*- coding: us-ascii -*-
 
 text = ARGF.read
-text.upcase!
+text.gsub!(/^(?!#)(.*)/){$1.upcase}
 
 # remove the pragma declarations
-text.gsub!(/^#PRAGMA.*$/, '')
+text.gsub!(/^#pragma.*$/, '')
 
 # replace the provider section with the start of the header file
 text.gsub!(/PROVIDER RUBY \{/, "#ifndef\t_PROBES_H\n#define\t_PROBES_H\n#define DTRACE_PROBES_DISABLED 1\n")
@@ -23,3 +23,4 @@ text.gsub!(/\([^,)]+,[^,)]+,[^,)]+,[^,)]+,[^,)]+\)/, '(arg0, arg1, arg2, arg3, a
 
 text.gsub!(/ *PROBE ([^\(]*)(\([^\)]*\));/, "#define RUBY_DTRACE_\\1_ENABLED() 0\n#define RUBY_DTRACE_\\1\\2\ do \{ \} while\(0\)")
 print text
+
