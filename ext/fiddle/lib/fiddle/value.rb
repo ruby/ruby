e553663cbda58188d085a0e6e5a71d52d3d95c5a
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-11-30 01:35:13 +0000

    value.rb: adjust indent
    * ext/fiddle/lib/fiddle/value.rb (Fiddle::ValueUtil#wrap_arg): adjust
      indent.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@38020 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/fiddle/lib/fiddle/value.rb
+++ ext/fiddle/lib/fiddle/value.rb
@@ -46,67 +46,67 @@ module Fiddle
     end
 
     def wrap_arg(arg, ty, funcs = [], &block)
-        funcs ||= []
-        case arg
-        when nil
-          return 0
-        when Pointer
-          return arg.to_i
-        when IO
-          case ty
-          when TYPE_VOIDP
-            return Pointer[arg].to_i
-          else
-            return arg.to_i
-          end
-        when Function
-          if( block )
-            arg.bind_at_call(&block)
-            funcs.push(arg)
-          elsif !arg.bound?
-            raise(RuntimeError, "block must be given.")
-          end
-          return arg.to_i
-        when String
-          if( ty.is_a?(Array) )
-            return arg.unpack('C*')
-          else
-            case SIZEOF_VOIDP
-            when SIZEOF_LONG
-              return [arg].pack("p").unpack("l!")[0]
-            when SIZEOF_LONG_LONG
-              return [arg].pack("p").unpack("q")[0]
-            else
-              raise(RuntimeError, "sizeof(void*)?")
-            end
-          end
-        when Float, Integer
-          return arg
-        when Array
-          if( ty.is_a?(Array) ) # used only by struct
-            case ty[0]
-            when TYPE_VOIDP
-              return arg.collect{|v| Integer(v)}
-            when TYPE_CHAR
-              if( arg.is_a?(String) )
-                return val.unpack('C*')
-              end
-            end
-            return arg
-          else
-            return arg
-          end
+      funcs ||= []
+      case arg
+      when nil
+        return 0
+      when Pointer
+        return arg.to_i
+      when IO
+        case ty
+        when TYPE_VOIDP
+          return Pointer[arg].to_i
         else
-          if( arg.respond_to?(:to_ptr) )
-            return arg.to_ptr.to_i
+          return arg.to_i
+        end
+      when Function
+        if( block )
+          arg.bind_at_call(&block)
+          funcs.push(arg)
+        elsif !arg.bound?
+          raise(RuntimeError, "block must be given.")
+        end
+        return arg.to_i
+      when String
+        if( ty.is_a?(Array) )
+          return arg.unpack('C*')
+        else
+          case SIZEOF_VOIDP
+          when SIZEOF_LONG
+            return [arg].pack("p").unpack("l!")[0]
+          when SIZEOF_LONG_LONG
+            return [arg].pack("p").unpack("q")[0]
           else
-            begin
-              return Integer(arg)
-            rescue
-              raise(ArgumentError, "unknown argument type: #{arg.class}")
-            end
+            raise(RuntimeError, "sizeof(void*)?")
           end
         end
+      when Float, Integer
+        return arg
+      when Array
+        if( ty.is_a?(Array) ) # used only by struct
+          case ty[0]
+          when TYPE_VOIDP
+            return arg.collect{|v| Integer(v)}
+          when TYPE_CHAR
+            if( arg.is_a?(String) )
+              return val.unpack('C*')
+            end
+          end
+          return arg
+        else
+          return arg
+        end
+      else
+        if( arg.respond_to?(:to_ptr) )
+          return arg.to_ptr.to_i
+        else
+          begin
+            return Integer(arg)
+          rescue
+            raise(ArgumentError, "unknown argument type: #{arg.class}")
+          end
+        end
+      end
     end
   end
 end
