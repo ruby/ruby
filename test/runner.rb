18a3774438469b6da3f44635e38c8b00f36c4cd7
drbrain <drbrain@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-12-01 00:34:01 +0000

    * test/runner.rb:  Set GEM_HOME, GEM_PATH and GEM_SKIP to empty set.   With default_gem support in RubyGems GEM_SKIP prevents loading of   built-in gems.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@38096 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/runner.rb
+++ test/runner.rb
@@ -10,15 +10,7 @@ class Gem::TestCase < MiniTest::Unit::TestCase
   @@project_dir = File.dirname($LOAD_PATH.last)
 end
 
-srcdir = File.dirname(src_testdir)
-default_gems = Dir.glob(srcdir + "/{lib,ext}/**/*.gemspec").map {|path| File.basename(path, ".*")}
-File.foreach(srcdir + "/defs/default_gems") do |line|
-  next if /^\s*#/ =~ line
-  default_gems << line[/^\S+/]
-end
-default_gems |= (ENV["GEM_SKIP"] || '').split(/:/)
-ENV["GEM_SKIP"] = default_gems.join(':')
-ENV["GEM_HOME"] = ENV["GEM_PATH"] = "".freeze
+ENV["GEM_SKIP"] = ENV["GEM_HOME"] = ENV["GEM_PATH"] = "".freeze
 
 require_relative 'profile_test_all' if ENV['RUBY_TEST_ALL_PROFILE'] == 'true'
 
