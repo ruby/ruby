811efc8276e9ffd6adf95d8e954d8cac40ed49ef
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-12-07 02:55:28 +0000

    Add declaration of rb_warn
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@38244 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/refinement/refinement.c
+++ ext/refinement/refinement.c
@@ -1,4 +1,11 @@
 void ruby_Init_refinement(void);
+#ifdef __GNUC__
+#define PRINTF_ARGS(decl, string_index, first_to_check) \
+  decl __attribute__((format(printf, string_index, first_to_check)))
+#else
+#define PRINTF_ARGS(decl, string_index, first_to_check) decl
+#endif
+PRINTF_ARGS(void rb_warn(const char*, ...), 1, 2);
 
 void
 Init_refinement(void)
