f9e4651039f2095f9c9aec00edcabfcd9b3d01db
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-12-10 02:50:57 +0000

    KNOWNBUGS.rb: exceptions in event hooks
    * KNOWNBUGS.rb: add tests for exceptions in event hooks, which were
      discarded in 1.8 but passed through in 1.9.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@38287 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- KNOWNBUGS.rb
+++ KNOWNBUGS.rb
@@ -3,3 +3,5 @@
 # So all tests will cause failure.
 #
 
+assert_equal('ok', "TracePoint.new(:line) {raise}.enable {\n  1\n}\n'ok'")
+assert_finish(3, 'def m; end; TracePoint.new(:return) {raise}.enable {m}')
