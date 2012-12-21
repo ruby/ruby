e31b67700b8c71061bd0ee33a7235d423ca6f6f0
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-12-21 15:30:44 +0000

    * object.c (rb_obj_hash): shouldn't assume object_id can be long.   based on a patch by Heesob Park at [ruby-core:51060].   cf. [Backport #7454]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@38539 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- object.c
+++ object.c
@@ -125,7 +125,14 @@ VALUE
 rb_obj_hash(VALUE obj)
 {
     VALUE oid = rb_obj_id(obj);
-    st_index_t h = rb_hash_end(rb_hash_start(NUM2LONG(oid)));
+#if SIZEOF_LONG == SIZEOF_VOIDP
+    st_index_t index = NUM2LONG(oid);
+#elif SIZEOF_LONG_LONG == SIZEOF_VOIDP
+    st_index_t index = NUM2LL(oid);
+#else
+# error not supported
+#endif
+    st_index_t h = rb_hash_end(rb_hash_start(index));
     return LONG2FIX(h);
 }
 
