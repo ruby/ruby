6da33430de3d4ff36799925c7425921a21bb56ff
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2012-12-25 22:31:26 +0000

    string.c: empty string is not invalid
    * string.c (rb_enc_cr_str_copy_for_substr): empty string is always
      valid or 7bit.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@38617 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- string.c
+++ string.c
@@ -293,6 +293,13 @@ rb_enc_cr_str_copy_for_substr(VALUE dest, VALUE src)
      * from src to new string "dest" which is made from the part of src.
      */
     str_enc_copy(dest, src);
+    if (RSTRING_LEN(dest) == 0) {
+	if (!rb_enc_asciicompat(STR_ENC_GET(src)))
+	    ENC_CODERANGE_SET(dest, ENC_CODERANGE_VALID);
+	else
+	    ENC_CODERANGE_SET(dest, ENC_CODERANGE_7BIT);
+	return;
+    }
     switch (ENC_CODERANGE(src)) {
       case ENC_CODERANGE_7BIT:
 	ENC_CODERANGE_SET(dest, ENC_CODERANGE_7BIT);
@@ -305,12 +312,6 @@ rb_enc_cr_str_copy_for_substr(VALUE dest, VALUE src)
 	    ENC_CODERANGE_SET(dest, ENC_CODERANGE_7BIT);
 	break;
       default:
-	if (RSTRING_LEN(dest) == 0) {
-	    if (!rb_enc_asciicompat(STR_ENC_GET(src)))
-		ENC_CODERANGE_SET(dest, ENC_CODERANGE_VALID);
-	    else
-		ENC_CODERANGE_SET(dest, ENC_CODERANGE_7BIT);
-	}
 	break;
     }
 }
