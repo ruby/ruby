4136f8ce890350c8899fb853397de15b68453445
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-01-01 14:42:50 +0000

    * bootstraptest/test_thread.rb: get rid of "invalid nest" error in   ruby-mode.el.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@38670 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_thread.rb
+++ bootstraptest/test_thread.rb
@@ -189,18 +189,18 @@ assert_equal %q{11}, %q{
   }.value + Thread.current[:a]
 }
 assert_normal_exit %q{
-begin
-  100.times do |i|
-    begin
-      th = Thread.start(Thread.current) {|u| u.raise }
-      raise
-    rescue
-    ensure
-      th.join
+  begin
+    100.times do |i|
+      begin
+        th = Thread.start(Thread.current) {|u| u.raise }
+        raise
+      rescue
+      ensure
+        th.join
+      end
     end
+  rescue
   end
-rescue
-end
 }, '[ruby-dev:31371]'
 
 assert_equal 'true', %{
