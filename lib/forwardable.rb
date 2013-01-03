e25d55ae08230ba7ed597f00ecac20a546aae719
zzak <zzak@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-01-03 05:59:27 +0000

    * lib/forwardable.rb (SingleForwardable): Fix example in overview   Patch by Vladimir Andrijevik [Github Fixes #231]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@38679 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/forwardable.rb
+++ lib/forwardable.rb
@@ -196,17 +196,21 @@ end
 #    printer.def_delegator "STDOUT", "puts"  # add delegation for STDOUT.puts()
 #    printer.puts "Howdy!"
 #
-# Also, SingleForwardable can be use to Class or Module.
+# Also, SingleForwardable can be used to set up delegation for a Class or Module.
 #
-#    module Facade
-#      extend SingleForwardable
-#      def_delegator :Implementation, :service
+#   class Implementation
+#     def self.service
+#       puts "serviced!"
+#     end
+#   end
+#   
+#   module Facade
+#     extend SingleForwardable
+#     def_delegator :Implementation, :service
+#   end
 #
-#      class Implementation
-#         def service...
-#      end
-#    end
-#
+#   Facade.service #=> serviced!
+#   
 # If you want to use both Forwardable and SingleForwardable, you can
 # use methods def_instance_delegator and def_single_delegator, etc.
 module SingleForwardable
