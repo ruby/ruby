e2b7e5323e225e40533dc4cec7bf2a8f389d3d91
Vladimir Andrijevik <vladimir.andrijevik@gmail.com>
2013-01-03 12:32:42 +0100

    Use call-seq in doc for #def_single_delegator

--- lib/forwardable.rb
+++ lib/forwardable.rb
@@ -246,10 +246,12 @@ module SingleForwardable
     end
   end
 
-  #
   # Defines a method _method_ which delegates to _accessor_ (i.e. it calls
   # the method of the same name in _accessor_).  If _new_name_ is
   # provided, it is used as the name for the delegate method.
+  #
+  # :call-seq:
+  #   def_single_delegator(accessor, method, new_name=method)
   #
   def def_single_delegator(accessor, method, ali = method)
     str = %{
