e4ebf6d946ad4dcf21a4db1c2dc75dde28f58b92
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-01-15 14:07:22 +0000

    object.c: two literals
    * object.c (rb_mod_to_s): concatenate two literals.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@38837 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- object.c
+++ object.c
@@ -1349,10 +1349,9 @@ rb_mod_to_s(VALUE klass)
     VALUE refined_class, defined_at;
 
     if (FL_TEST(klass, FL_SINGLETON)) {
-	VALUE s = rb_usascii_str_new2("#<");
+	VALUE s = rb_usascii_str_new2("#<Class:");
 	VALUE v = rb_iv_get(klass, "__attached__");
 
-	rb_str_cat2(s, "Class:");
 	switch (TYPE(v)) {
 	  case T_CLASS: case T_MODULE:
 	    rb_str_append(s, rb_inspect(v));
