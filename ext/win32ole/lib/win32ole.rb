090d20fc16dbb91a816ee9a94c426da56dd1fbc2
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-01-18 02:29:59 +0000

    win32ole: remove win32ole.rb
    * ext/win32ole/lib/win32ole.rb: no longer used.  [Bug #2618] [ruby-core:27634]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@38869 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/win32ole/lib/win32ole.rb
+++
@@ -1,7 +0,0 @@
-require 'win32ole.so'
-
-# re-define Thread#initialize
-# bug #2618(ruby-core:27634)
-
-TracePoint.trace(:thread_begin) {WIN32OLE.ole_initialize}
-TracePoint.trace(:thread_end) {WIN32OLE.ole_uninitialize}
