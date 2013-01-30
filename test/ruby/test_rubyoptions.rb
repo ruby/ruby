71eddb1dea82226a31540de2982ddfc0288cbbf4
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-01-30 04:22:12 +0000

    parse.y: warn assigned but unused in toplevel
    * parse.y (local_push_gen): warn assigned but unused variables also in
      toplevel, except for -e option.  [Feature #7730] [ruby-core:51580]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@38982 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_rubyoptions.rb
+++ test/ruby/test_rubyoptions.rb
@@ -551,6 +551,8 @@ class TestRubyOptions < Test::Unit::TestCase
     assert_in_out_err(["-we", "def foo\n  _a=1\nend"], "", [], [], feature6693)
     bug7408 = '[ruby-core:49659]'
     assert_in_out_err(["-we", "def foo\n  a=1\n :a\nend"], "", [], ["-e:2: warning: assigned but unused variable - a"], bug7408)
+    feature7730 = '[ruby-core:51580]'
+    assert_in_out_err(["-w", "-"], "a=1", [], ["-:1: warning: assigned but unused variable - a"], feature7730)
   end
 
   def test_shadowing_variable
