6d32920e418db2016cb0870d171271e36168aece
zzak <zzak@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-02-03 19:21:37 +0000

    * lib/English.rb: Add English module for RDoc to parse, then   remove_const to avoid confusion. Include full list of aliases and   their associated global variable.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39035 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/English.rb
+++ lib/English.rb
@@ -15,7 +15,38 @@
 #      $OUTPUT_FIELD_SEPARATOR = ' -- '
 #      "waterbuffalo" =~ /buff/
 #      print $LOADED_FEATURES, $POSTMATCH, $PID, "\n"
-
+#
+#  Below is a full list of descriptive aliases and their associated global
+#  variable:
+#
+#  $ERROR_INFO::              $!
+#  $ERROR_POSITION::          $@
+#  $FS::                      $;
+#  $FIELD_SEPARATOR::         $;
+#  $OFS::                     $,
+#  $OUTPUT_FIELD_SEPARATOR::  $,
+#  $RS::                      $/
+#  $INPUT_RECORD_SEPARATOR::  $/
+#  $ORS::                     $\
+#  $OUTPUT_RECORD_SEPARATOR:: $\
+#  $INPUT_LINE_NUMBER::       $.
+#  $NR::                      $.
+#  $LAST_READ_LINE::          $_
+#  $DEFAULT_OUTPUT::          $>
+#  $DEFAULT_INPUT::           $<
+#  $PID::                     $$
+#  $PROCESS_ID::              $$
+#  $CHILD_STATUS::            $?
+#  $LAST_MATCH_INFO::         $~
+#  $IGNORECASE::              $=
+#  $ARGV::                    $*
+#  $MATCH::                   $&
+#  $PREMATCH::                $`
+#  $POSTMATCH::               $'
+#  $LAST_PAREN_MATCH::        $+
+#
+module English; end
+Object.send(:remove_const, :English)
 
 # The exception object passed to +raise+.
 alias $ERROR_INFO              $!
