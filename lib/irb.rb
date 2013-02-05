5b15e5a38fb3963cffb3c795883d6fc141031251
zzak <zzak@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-02-05 15:57:19 +0000

    * lib/irb.rb: Remove example from restrictions, it works [Github #246]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39075 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/irb.rb
+++ lib/irb.rb
@@ -223,16 +223,6 @@ STDOUT.sync = true
 # Because irb evaluates input immediately after it is syntactically complete,
 # the results may be slightly different than directly using ruby.
 #
-# One of the obvious differences is how irb handles symbols as continuated
-# statements:
-#
-#   ruby -e 'p :+' #=> :+
-#   irb
-#   irb(main):001:0> p :+
-#   irb(main):002:0*
-#
-# irb tries to contiue the statement 'p :+' on the next line.
-#
 # == IRB Sessions
 #
 # IRB has a special feature, that allows you to manage many sessions at once.
