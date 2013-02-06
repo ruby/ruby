90eb265f8686a5ea14cf6c745600361300b84e4f
zzak <zzak@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-02-06 20:51:56 +0000

    * doc/pty/README: Remove static documentation file * ext/pty/pty.c: Add License to PTY module overview
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39122 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/pty/pty.c
+++ ext/pty/pty.c
@@ -708,6 +708,19 @@ static VALUE cPTY;
  *
  * Creates and managed pseudo terminals (PTYs).  See also
  * http://en.wikipedia.org/wiki/Pseudo_terminal
+ *
+ * == License
+ *
+ *  C) Copyright 1998 by Akinori Ito.
+ *
+ *  This software may be redistributed freely for this purpose, in full
+ *  or in part, provided that this entire copyright notice is included
+ *  on any copies of this software and applications and derivations thereof.
+ *
+ *  This software is provided on an "as is" basis, without warranty of any
+ *  kind, either expressed or implied, as to any matter including, but not
+ *  limited to warranty of fitness of purpose, or merchantability, or
+ *  results obtained from use of this software.
  */
 
 void
