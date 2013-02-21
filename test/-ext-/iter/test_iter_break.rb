e525bcd1b3723e91311d67262b1aa726cc9b0104
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-02-21 08:31:47 +0000

    Add test for [Backport #7896] [ruby-core:52607]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39359 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/-ext-/iter/test_iter_break.rb
+++ test/-ext-/iter/test_iter_break.rb
@@ -3,7 +3,10 @@ require '-test-/iter/break'
 
 class TestIterBreak < Test::Unit::TestCase
   def test_iter_break
+    backport7896 = '[ruby-core:52607]'
+    assert_equal(nil, 1.times{Bug::Breakable.iter_break}, backport7896)
+
     feature5895 = '[ruby-dev:45132]'
-    assert_equal(42, 1.times{Bug::Breakable.iter_break(42)}, feature5895)
+    assert_equal(42, 1.times{Bug::Breakable.iter_break_value(42)}, feature5895)
   end
 end
