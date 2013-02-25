1580dd29dddc02c210f96e6250d2f20df3241ab1
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-02-25 03:40:16 +0000

    * ext/socket/extconf.rb: don't test ss_family and ss_len member of   struct sockaddr_storage.  They are not used now except SunOS   specific code.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39483 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/socket/extconf.rb
+++ ext/socket/extconf.rb
@@ -80,15 +80,6 @@ have_type("struct ip_mreq", headers) # 4.4BSD
 have_type("struct ip_mreqn", headers) # Linux 2.4
 have_type("struct ipv6_mreq", headers) # RFC 3493
 
-#   doug's fix, NOW add -Dss_family... only if required!
-doug = proc {have_struct_member("struct sockaddr_storage", "ss_family", headers)}
-if (doug[] or
-    with_cppflags($CPPFLAGS + " -Dss_family=__ss_family", &doug))
-  $defs[-1] = "-DHAVE_SOCKADDR_STORAGE" # change from -DHAVE_ST_SS_FAMILY.
-  doug = proc {have_struct_member("struct sockaddr_storage", "ss_len", headers)}
-  doug[] or with_cppflags($CPPFLAGS + " -Dss_len=__ss_len", &doug)
-end
-
 have_struct_member('struct msghdr', 'msg_control', headers) unless $mswin or $mingw
 have_struct_member('struct msghdr', 'msg_accrights', headers)
 
