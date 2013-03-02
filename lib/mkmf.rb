773248551b8cb46f281d5b6bb4f61477ce17d55d
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-03-02 15:04:23 +0000

    mswin: static-linked-ext
    * enc/depend (CPPFLAGS), lib/mkmf.rb (MakeMakefile#create_makefile):
      define RUBY_EXPORT for static-linked-ext mswin.  [Bug #7960]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39556 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -2106,6 +2106,10 @@ RULES
       $distcleandirs.concat($INSTALLFILES.collect {|files, dir| dir})
     end
 
+    if $extmk and $static
+      $defs << "-DRUBY_EXPORT=1"
+    end
+
     if $extmk and not $extconf_h
       create_header
     end
