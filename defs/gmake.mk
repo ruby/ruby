5c1af05ef521eeb7a718aefbacf9b78a0a48e5df
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-03-06 11:59:23 +0000

    defs/gmake.mk: serialize test targets.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39615 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ defs/gmake.mk
@@ -0,0 +1,17 @@
+TEST_TARGETS := $(filter check test check% test% btest%,$(MAKECMDGOALS))
+TEST_TARGETS += $(subst check,test-all,$(patsubst check-%,test-%,$(TEST_TARGETS)))
+TEST_TARGETS := $(patsubst test-%,yes-test-%,$(patsubst btest-%,yes-btest-%,$(TEST_TARGETS)))
+TEST_DEPENDS := $(if $(TEST_TARGETS),$(filter all main exts,$(MAKECMDGOALS)))
+TEST_DEPENDS += $(TEST_DEPENDS) $(if $(filter check%,$(MAKECMDGOALS)),main)
+
+ifneq ($(filter check% test,$(MAKECMDGOALS)),)
+yes-test-knownbug: $(TEST_DEPENDS) yes-btest-ruby
+yes-btest-ruby: $(TEST_DEPENDS) yes-test-sample
+yes-test-sample: $(TEST_DEPENDS)
+endif
+ifneq ($(filter check%,$(MAKECMDGOALS)) $(filter test-all,$(TEST_TARGETS)),)
+yes-test-all yes-test-ruby: $(filter-out %test-all %test-ruby check%,$(TEST_TARGETS)) \
+	yes-test-knownbug
+endif
+
+$(TEST_TARGETS): $(TEST_DEPENDS)
