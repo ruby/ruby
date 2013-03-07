1a71b8718adc0d46f307f4e14d4e4baec89b645e
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-03-07 01:39:13 +0000

    rbinstall.rb: gem subdirectories
    * tool/rbinstall.rb (gem): Gem.ensure_gem_subdirectories now has mode
      option since r39607.  refix of r38870.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39619 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/rbinstall.rb
+++ tool/rbinstall.rb
@@ -688,8 +688,7 @@ install?(:ext, :comm, :gem) do
   $:.unshift(File.join(srcdir, "lib"))
   require("rubygems.rb")
   gem_dir = Gem.default_dir
-  # Gem.ensure_gem_subdirectories makes subdirectories group-writable.
-  directories = Gem::REPOSITORY_SUBDIRECTORIES
+  directories = Gem.ensure_gem_subdirectories(gem_dir, :mode => $dir_mode)
   prepare "default gems", gem_dir, directories
 
   spec_dir = File.join(gem_dir, directories.grep(/^spec/)[0])
