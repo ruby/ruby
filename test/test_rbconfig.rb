5a5df65d9d2e17868d4bbe306a1cc543cdc95232
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-03-09 13:19:15 +0000

    merge revision(s) 39372,39383: [Backport #7912]
    	* test/test_rbconfig.rb (TestRbConfig): skip user defined values by
    	  configuration options.  [Bug #7902]
    
    	* test/test_rbconfig.rb (TestRbConfig): fix r39372.
    	  It must see RbConfig::CONFIG instead of CONFIG.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@39661 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/test_rbconfig.rb
+++ test/test_rbconfig.rb
@@ -1,10 +1,18 @@
 require 'test/unit'
 require 'rbconfig'
+require 'shellwords'
 
 class TestRbConfig < Test::Unit::TestCase
+  @@with_config = {}
+
+  Shellwords::shellwords(RbConfig::CONFIG["configure_args"]).grep(/\A--with-([^=]*)=(.*)/) do
+    @@with_config[$1.tr('_', '-')] = $2
+  end
+
   def test_sitedirs
     RbConfig::MAKEFILE_CONFIG.each do |key, val|
       next unless /\Asite(?!arch)/ =~ key
+      next if @@with_config[key]
       assert_match(/(?:\$\(|\/)site/, val, key)
     end
   end
@@ -12,6 +20,7 @@ class TestRbConfig < Test::Unit::TestCase
   def test_vendordirs
     RbConfig::MAKEFILE_CONFIG.each do |key, val|
       next unless /\Avendor(?!arch)/ =~ key
+      next if @@with_config[key]
       assert_match(/(?:\$\(|\/)vendor/, val, key)
     end
   end
@@ -19,6 +28,7 @@ class TestRbConfig < Test::Unit::TestCase
   def test_archdirs
     RbConfig::MAKEFILE_CONFIG.each do |key, val|
       next unless /\A(?!site|vendor|archdir\z).*arch.*dir\z/ =~ key
+      next if @@with_config[key]
       assert_match(/\$\(arch|\$\(rubyarchprefix\)/, val, key)
     end
   end
@@ -27,6 +37,7 @@ class TestRbConfig < Test::Unit::TestCase
     bug7823 = '[ruby-dev:46964] [Bug #7823]'
     RbConfig::MAKEFILE_CONFIG.each do |key, val|
       next unless /\Asite.*arch.*dir\z/ =~ key
+      next if @@with_config[key]
       assert_match(/\$\(sitearch|\$\(rubysitearchprefix\)/, val, "#{key} #{bug7823}")
     end
   end
@@ -35,6 +46,7 @@ class TestRbConfig < Test::Unit::TestCase
     bug7823 = '[ruby-dev:46964] [Bug #7823]'
     RbConfig::MAKEFILE_CONFIG.each do |key, val|
       next unless /\Avendor.*arch.*dir\z/ =~ key
+      next if @@with_config[key]
       assert_match(/\$\(sitearch|\$\(rubysitearchprefix\)/, val, "#{key} #{bug7823}")
     end
   end
