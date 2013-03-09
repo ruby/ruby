e2e5d3fd1e3e4e2b5f430d066280d4118254a62c
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-03-09 12:39:11 +0000

    merge revision(s) 39359:
    	Add test for [Backport #7896] [ruby-core:52607]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@39655 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/-test-/iter/break.c
+++ ext/-test-/iter/break.c
@@ -1,6 +1,14 @@
 #include <ruby.h>
 
 static VALUE
+iter_break(VALUE self)
+{
+    rb_iter_break();
+
+    UNREACHABLE;
+}
+
+static VALUE
 iter_break_value(VALUE self, VALUE val)
 {
     rb_iter_break_value(val);
@@ -12,5 +20,6 @@ void
 Init_break(void)
 {
     VALUE breakable = rb_define_module_under(rb_define_module("Bug"), "Breakable");
-    rb_define_module_function(breakable, "iter_break", iter_break_value, 1);
+    rb_define_module_function(breakable, "iter_break", iter_break, 0);
+    rb_define_module_function(breakable, "iter_break_value", iter_break_value, 1);
 }
