976a3041ef38711b84c6a35db25f189d31903e38
takano32 <takano32@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-03-12 08:05:16 +0000

    * date_core.c: clearly specify operator precedence.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39734 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/date/date_core.c
+++ ext/date/date_core.c
@@ -679,7 +679,7 @@ c_julian_leap_p(int y)
 inline static int
 c_gregorian_leap_p(int y)
 {
-    return MOD(y, 4) == 0 && y % 100 != 0 || MOD(y, 400) == 0;
+    return (MOD(y, 4) == 0 && y % 100 != 0) || MOD(y, 400) == 0;
 }
 
 static int
