68a2dd548de277be2595d68f8bd0b6fae8ee1023
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-03-20 14:06:39 +0000

    merge revision(s) 39777: [Backport #8080]
    	mingw build fix
    
    	* configure.in: struct timeval is defined in winsock2.h on mingw.
    
    	* include/ruby/missing.h: include time.h for time_t, and sys/time.h
      for timeval and timespec.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@39848 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/missing.h
+++ include/ruby/missing.h
@@ -25,22 +25,21 @@ extern "C" {
 #include RUBY_EXTCONF_H
 #endif
 
+#if defined(HAVE_TIME_H)
+# include <time.h>
+#endif
+#if defined(HAVE_SYS_TIME_H)
+# include <sys/time.h>
+#endif
+
 #if !defined(HAVE_STRUCT_TIMEVAL)
-# if defined(HAVE_SYS_TIME_H)
-#  include <sys/time.h>
-# elif !defined(_WIN32)
-#  define time_t long
 struct timeval {
     time_t tv_sec;	/* seconds */
     long tv_usec;	/* microseconds */
 };
-# endif
 #endif /* HAVE_STRUCT_TIMEVAL */
 
 #if !defined(HAVE_STRUCT_TIMESPEC)
-# if defined(HAVE_SYS_TIME_H)
-#  include <sys/time.h>
-# endif
 struct timespec {
     time_t tv_sec;	/* seconds */
     long tv_nsec;	/* nanoseconds */
