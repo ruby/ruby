042594bb502f4199a3e4e676c01c8375f6cdcb48
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-03-21 14:14:19 +0000

    ruby.h: RB_UNUSED_VAR from UNINITIALIZED_VAR
    * include/ruby/ruby.h (RB_UNUSED_VAR): move code from
      UNINITIALIZED_VAR() in vm_core.h.
    * vm_core.h (UNINITIALIZED_VAR): use RB_UNUSED_VAR().
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39861 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- vm_core.h
+++ vm_core.h
@@ -115,13 +115,7 @@
 #define UNLIKELY(x) (x)
 #endif /* __GNUC__ >= 3 */
 
-#if (defined(__clang__) && (__clang_major__ == 4 && __clang_minor__ == 2))
-#define UNINITIALIZED_VAR(x) x __attribute__((unused))
-#elif defined(__GNUC__) && __GNUC__ >= 3
-#define UNINITIALIZED_VAR(x) x = x
-#else
-#define UNINITIALIZED_VAR(x) x
-#endif
+#define UNINITIALIZED_VAR(x) RB_UNUSED_VAR(x)
 
 typedef unsigned long rb_num_t;
 
