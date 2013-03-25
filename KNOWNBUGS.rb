6f8e5cc821c2c118ff2997fbcaf3c269ae454a8c
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-03-25 05:39:32 +0000

    * test/ruby/test_method.rb (test_unlinked_method_entry_in_method_object_bug):   move from KNOWNBUGS.rb.  [Bug #8100]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39925 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- KNOWNBUGS.rb
+++ KNOWNBUGS.rb
@@ -3,14 +3,3 @@
 # So all tests will cause failure.
 #
 
-assert_normal_exit %q{
-  require 'timeout'
-  timeout(2) do
-    loop do
-      def x
-        "hello" * 1000
-      end
-      method(:x).call
-    end
-  end
-}, '[ruby-core:53640] [Bug #8100]'
