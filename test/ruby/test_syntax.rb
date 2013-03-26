b6a6fa0af47f9e1647b17aedc2e4269498b0f086
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-03-26 02:59:09 +0000

    test_syntax.rb: envutil
    * test/ruby/test_syntax.rb (test_syntax): require envutil for
      assert_valid_syntax.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39934 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_syntax.rb
+++ test/ruby/test_syntax.rb
@@ -3,7 +3,7 @@ require_relative 'envutil'
 
 class TestSyntax < Test::Unit::TestCase
   def test_syntax
-    assert_separately %w[--disable-gem], __FILE__, __LINE__, <<-'eom'
+    assert_separately %W[--disable-gem -r#{__dir__}/envutil], __FILE__, __LINE__, <<-'eom'
     assert_nothing_raised(Exception) do
       for script in Dir[File.expand_path("../../../{lib,sample,ext,test}/**/*.rb", __FILE__)].sort
         assert_valid_syntax(IO::read(script), script)
