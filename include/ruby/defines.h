6537dc309962c7fefcb8d698bbebe4e4f78d0096
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-03-27 21:56:20 +0000

    * include/ruby/defines.h: Fix a compilation error on NetBSD,   "type of formal parameter 1 is incomplete" for the rb_thread_wait_for   invocation in rb_file_flock, by including header files as   AC_INCLUDES_DEFAULT of autoconf.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39972 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/defines.h
+++ include/ruby/defines.h
@@ -22,6 +22,41 @@ extern "C" {
 #include RUBY_EXTCONF_H
 #endif
 
+/* AC_INCLUDES_DEFAULT */
+#include <stdio.h>
+#ifdef HAVE_SYS_TYPES_H
+# include <sys/types.h>
+#endif
+#ifdef HAVE_SYS_STAT_H
+# include <sys/stat.h>
+#endif
+#ifdef STDC_HEADERS
+# include <stdlib.h>
+# include <stddef.h>
+#else
+# ifdef HAVE_STDLIB_H
+#  include <stdlib.h>
+# endif
+#endif
+#ifdef HAVE_STRING_H
+# if !defined STDC_HEADERS && defined HAVE_MEMORY_H
+#  include <memory.h>
+# endif
+# include <string.h>
+#endif
+#ifdef HAVE_STRINGS_H
+# include <strings.h>
+#endif
+#ifdef HAVE_INTTYPES_H
+# include <inttypes.h>
+#endif
+#ifdef HAVE_STDINT_H
+# include <stdint.h>
+#endif
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+
 #define RUBY
 
 # include <stddef.h>
