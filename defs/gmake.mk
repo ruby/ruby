6b7478c4bd7cc844079f2b02fd350975fb7a0bba
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-03-27 03:42:19 +0000

    gmake.mk: order test-all and test-knownbug
    * defs/gmake.mk (test-all, test-ruby): depends on test-knownbug if
      check is given or with test-knownbug.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39964 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- defs/gmake.mk
+++ defs/gmake.mk
@@ -12,6 +12,9 @@ endif
 ifneq ($(filter check%,$(MAKECMDGOALS)) $(filter test-all,$(TEST_TARGETS)),)
 yes-test-all yes-test-ruby: $(filter-out %test-all %test-ruby check%,$(TEST_TARGETS))
 endif
+ifneq ($(filter check%,$(MAKECMDGOALS))$(if $(filter test-all,$(MAKECMDGOALS)),$(filter test-knownbug,$(MAKECMDGOALS))),)
+yes-test-all yes-test-ruby: yes-test-knownbug
+endif
 
 $(TEST_TARGETS): $(TEST_DEPENDS)
 
