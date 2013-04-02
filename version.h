525c60b4cd6381f033e4a25e1df40c843ca0edd8
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-04-02 19:56:46 +0000

    merge revision(s) 40048:
    	fix typo: s/@ca_cert/@cacert/
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@40074 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2013-03-27"
-#define RUBY_PATCHLEVEL 100
+#define RUBY_RELEASE_DATE "2013-04-03"
+#define RUBY_PATCHLEVEL 101
 
 #define RUBY_RELEASE_YEAR 2013
-#define RUBY_RELEASE_MONTH 3
-#define RUBY_RELEASE_DAY 27
+#define RUBY_RELEASE_MONTH 4
+#define RUBY_RELEASE_DAY 3
 
 #include "ruby/version.h"
 
