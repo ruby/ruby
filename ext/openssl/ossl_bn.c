106595827ad33295d2f5bd1c1255c87293215a54
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-04-04 02:35:07 +0000

    * ext/openssl/ossl_bn.c (ossl_bn_to_i): Use bn2hex to speed up.   In general, binary to/from decimal needs extra cost.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@40094 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/openssl/ossl_bn.c
+++ ext/openssl/ossl_bn.c
@@ -226,10 +226,10 @@ ossl_bn_to_i(VALUE self)
 
     GetBN(self, bn);
 
-    if (!(txt = BN_bn2dec(bn))) {
+    if (!(txt = BN_bn2hex(bn))) {
 	ossl_raise(eBNError, NULL);
     }
-    num = rb_cstr_to_inum(txt, 10, Qtrue);
+    num = rb_cstr_to_inum(txt, 16, Qtrue);
     OPENSSL_free(txt);
 
     return num;
