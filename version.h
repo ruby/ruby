116843c6f00e3a5dca5437b6b9364d4466fdb542
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-04-11 02:19:39 +0000

    merge revision(s) 39475: [Backport #7868]
    	* ext/date/date_core.c: [ruby-core:52303]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@40227 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 411
+#define RUBY_PATCHLEVEL 412
 
-#define RUBY_RELEASE_DATE "2013-04-09"
+#define RUBY_RELEASE_DATE "2013-04-11"
 #define RUBY_RELEASE_YEAR 2013
 #define RUBY_RELEASE_MONTH 4
-#define RUBY_RELEASE_DAY 9
+#define RUBY_RELEASE_DAY 11
 
 #include "ruby/version.h"
 
