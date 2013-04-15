36eb3366780a42736905a66c44fb3f21484591a8
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-04-15 16:27:33 +0000

    revert r40311(merge revision(s) 40117,40118). It breaks rubyci tests.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@40313 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/test_curses.rb
+++ test/test_curses.rb
@@ -1,9 +1,7 @@
 require 'test/unit'
-require_relative 'ruby/envutil'
 
 begin
   require 'curses'
-  require 'pty'
 rescue LoadError
 end
 
@@ -12,49 +10,3 @@ class TestCurses < Test::Unit::TestCase
     assert_instance_of(String, Curses::VERSION)
   end
 end if defined? Curses
-
-class TestCurses
-  def run_curses(src, input = nil, timeout: 1)
-    PTY.spawn(EnvUtil.rubybin, "-e", <<-"src") {|r, w, pid|
-require 'timeout'
-require 'curses'
-include Curses
-init_screen
-begin
-  result = Timeout.timeout(#{timeout}) do
-    #{src}
-  end
-rescue Exception => e
-ensure
-  close_screen
-  puts "", [Marshal.dump([result, e])].pack('m').delete("\n")
-end
-src
-      if input
-        w.print(input)
-        w.flush
-      end
-      res = r.read
-      return unless res
-      res, error = Marshal.load(res[/(.*)\Z/, 1].unpack('m')[0])
-      raise error if error
-      return res
-    }
-  end
-
-  def test_getch
-    assert_equal("a", run_curses("getch", "a"))
-  end
-  def test_getch_cbreak
-    assert_equal("a", run_curses("cbreak; getch", "a"))
-  end
-  def test_getch_nocbreak
-    assert_raise(Timeout::Error) {run_curses("nocbreak; getch", "a")}
-  end
-  def test_getch_crmode
-    assert_equal("a", run_curses("crmode; getch", "a"))
-  end
-  def test_getch_nocrmode
-    assert_raise(Timeout::Error) {run_curses("nocrmode; getch", "a")}
-  end
-end if defined? TestCurses and defined? PTY
