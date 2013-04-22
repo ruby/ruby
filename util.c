f544ca4e4e817dd722d96f09e15b0170a5b392cd
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-04-22 02:59:09 +0000

    * util.c (ruby_hdtoa): revert r29729.   If you want ruby to behave as before on x86, specify to use SSE like   -msse2 -mfpmath=sse for gcc.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@40404 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- util.c
+++ util.c
@@ -3878,13 +3878,10 @@ ruby_hdtoa(double d, const char *xdigs, int ndigits, int *decpt, int *sign,
 	/* Round to the desired number of digits. */
 	if (SIGFIGS > ndigits && ndigits > 0) {
 		float redux = 1.0f;
-		volatile double d;
 		int offset = 4 * ndigits + DBL_MAX_EXP - 4 - DBL_MANT_DIG;
 		dexp_set(u, offset);
-		d = u.d;
-		d += redux;
-		d -= redux;
-		u.d = d;
+		u.d += redux;
+		u.d -= redux;
 		*decpt += dexp_get(u) - offset;
 	}
 
