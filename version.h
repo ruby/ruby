4dfec53d1936289736cd7cf8c2f79a22430977a9
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-05-01 15:14:14 +0000

    * 2013-05-02
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@40543 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2013-05-01"
+#define RUBY_RELEASE_DATE "2013-05-02"
 #define RUBY_PATCHLEVEL 177
 
 #define RUBY_RELEASE_YEAR 2013
 #define RUBY_RELEASE_MONTH 5
-#define RUBY_RELEASE_DAY 1
+#define RUBY_RELEASE_DAY 2
 
 #include "ruby/version.h"
 
