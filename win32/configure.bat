93f83417928d0b12020949cd02deb96340e70448
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-05-01 03:03:58 +0000

    configure.bat: remove stale message
    * win32/configure.bat (help): remove stale message.  win95 support had
      been thrown away long ago.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@40533 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- win32/configure.bat
+++ win32/configure.bat
@@ -179,7 +179,6 @@ goto :loop
   echo   --with-ext="a,b,..."    use extensions a, b, ...
   echo   --without-ext="a,b,..." ignore extensions a, b, ...
   echo   --disable-install-doc   do not install rdoc indexes during install
-  echo   --disable-win95         disable win95 support
   echo   --with-ntver=0xXXXX     target NT version (shouldn't use with old SDK)
   del *.tmp
   del ~tmp~.mak
