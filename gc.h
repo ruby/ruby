b1dd4c92ce95bc0deaf7ab34524f239953434ecb
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-05-10 15:49:56 +0000

    * gc.h (SET_MACHINE_STACK_END): Add !defined(_ILP32) to a defining   condition to avoid compilation error on x32.   https://sites.google.com/site/x32abi/
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@40636 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- gc.h
+++ gc.h
@@ -2,7 +2,7 @@
 #ifndef RUBY_GC_H
 #define RUBY_GC_H 1
 
-#if defined(__x86_64__) && defined(__GNUC__) && !defined(__native_client__)
+#if defined(__x86_64__) && !defined(_ILP32) && defined(__GNUC__) && !defined(__native_client__)
 #define SET_MACHINE_STACK_END(p) __asm__ __volatile__ ("movq\t%%rsp, %0" : "=r" (*(p)))
 #elif defined(__i386) && defined(__GNUC__) && !defined(__native_client__)
 #define SET_MACHINE_STACK_END(p) __asm__ __volatile__ ("movl\t%%esp, %0" : "=r" (*(p)))
