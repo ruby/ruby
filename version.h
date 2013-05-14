19f0852363d26f44307e40d0c447f7527b4c36d8
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-05-14 11:13:40 +0000

    merge revision(s) 40728:
    	* ext/dl/lib/dl/func.rb (DL::Function#call): check tainted when
    	  $SAFE > 0.
    
    	* ext/fiddle/function.c (function_call): check tainted when $SAFE > 0.
    
    	* test/fiddle/test_func.rb (module Fiddle): add test for above.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@40729 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2013-05-12"
-#define RUBY_PATCHLEVEL 193
+#define RUBY_RELEASE_DATE "2013-05-14"
+#define RUBY_PATCHLEVEL 194
 
 #define RUBY_RELEASE_YEAR 2013
 #define RUBY_RELEASE_MONTH 5
-#define RUBY_RELEASE_DAY 12
+#define RUBY_RELEASE_DAY 14
 
 #include "ruby/version.h"
 
