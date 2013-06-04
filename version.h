28b7866304e66f3f77b5515adc508bb8cb7283d0
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-06-04 15:05:58 +0000

    merge revision(s) 40920: [Backport #8450]
    	* string.c (rb_str_quote_unprintable): check if argument is a string.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@41067 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2013-06-04"
-#define RUBY_PATCHLEVEL 204
+#define RUBY_RELEASE_DATE "2013-06-05"
+#define RUBY_PATCHLEVEL 205
 
 #define RUBY_RELEASE_YEAR 2013
 #define RUBY_RELEASE_MONTH 6
-#define RUBY_RELEASE_DAY 4
+#define RUBY_RELEASE_DAY 5
 
 #include "ruby/version.h"
 
