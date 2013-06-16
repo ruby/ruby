a70a7ea2fe84f932728bd796606824bef6263a31
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-06-16 08:24:09 +0000

    fix typo of r41331
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@41334 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/config_files.rb
+++ tool/config_files.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 
 ConfigFiles = "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=%s;hb=HEAD"
 def ConfigFiles.download(name, dir = nil)
-  data = URI.read(self % name)
+  data = URI(self % name).read
   file = dir ? File.join(dir, name) : name
   open(file, "wb", 0755) {|f| f.write(data)}
 end
