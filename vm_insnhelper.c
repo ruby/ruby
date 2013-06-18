65dc35a58245f9afa529803450318c8067bca786
charliesome <charliesome@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-06-18 14:01:32 +0000

    * vm_insnhelper.c (vm_call_method): ensure methods of type   VM_METHOD_TYPE_ATTR_SET are called with 1 argument
    * test/ruby/test_module.rb (class TestModule): add test
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@41389 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- vm_insnhelper.c
+++ vm_insnhelper.c
@@ -1773,7 +1773,7 @@ vm_call_method(rb_thread_t *th, rb_control_frame_t *cfp, rb_call_info_t *ci)
 		CI_SET_FASTPATH(ci, vm_call_cfunc, enable_fastpath);
 		return vm_call_cfunc(th, cfp, ci);
 	      case VM_METHOD_TYPE_ATTRSET:{
-		rb_check_arity(ci->argc, 0, 1);
+		rb_check_arity(ci->argc, 1, 1);
 		ci->aux.index = 0;
 		CI_SET_FASTPATH(ci, vm_call_attrset, enable_fastpath && !(ci->flag & VM_CALL_ARGS_SPLAT));
 		return vm_call_attrset(th, cfp, ci);
