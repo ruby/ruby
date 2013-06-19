55ae2402f40c428307a1ac1e8a9f2800a4a2f1bf
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-06-19 21:26:13 +0000

    * tool/rdocbench.rb: add summary.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@41457 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/rdocbench.rb
+++ tool/rdocbench.rb
@@ -10,13 +10,19 @@ Dir.mktmpdir('rdocbench-'){|d|
   args << '--op' << dir
 
   GC::Profiler.enable
-  Benchmark.bm{|x|
-    x.report('rdoc'){
-      r = RDoc::RDoc.new
-      r.document args
-      GC::Profiler.report
-      pp GC.stat
-      puts "GC Total Time:#{GC::Profiler.total_time}"
-    }
+  tms = Benchmark.measure{|x|
+    r = RDoc::RDoc.new
+    r.document args
   }
+  GC::Profiler.report
+  pp GC.stat
+  puts
+  puts Benchmark::CAPTION
+  puts tms
+  puts "GC total time (sec): #{GC::Profiler.total_time}"
+  puts
+  puts "Summary (ruby): #{RUBY_DESCRIPTION})"
+  puts "Summary (real): #{tms.real} sec"
+  puts "Summary (gctm): #{GC::Profiler.total_time} sec"
+  puts "Summary (gc#) : #{GC.count}"
 }
