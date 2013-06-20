9970a90c88d30c78ae0ff220a6c3ddcc446cdde1
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-06-20 13:41:00 +0000

    merge revision(s) 41468:
    	test_fiber.rb: longer timeout
    
    	* test/ruby/test_fiber.rb (test_many_fibers): make timeout longer.
      10sec is not sufficient.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@41499 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2013-06-19"
-#define RUBY_PATCHLEVEL 234
+#define RUBY_RELEASE_DATE "2013-06-20"
+#define RUBY_PATCHLEVEL 235
 
 #define RUBY_RELEASE_YEAR 2013
 #define RUBY_RELEASE_MONTH 6
-#define RUBY_RELEASE_DAY 19
+#define RUBY_RELEASE_DAY 20
 
 #include "ruby/version.h"
 
