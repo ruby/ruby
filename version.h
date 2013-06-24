8b2c390b4a3e057f0ef414643938fd1526afbee6
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-06-24 15:11:21 +0000

    merge revision(s) 41590,41591:
    	* win32/README.win32: grammar typo by @blankenshipz [Fix GH-334]
    	  https://github.com/ruby/ruby/pull/334
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@41604 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2013-06-23"
-#define RUBY_PATCHLEVEL 238
+#define RUBY_RELEASE_DATE "2013-06-25"
+#define RUBY_PATCHLEVEL 239
 
 #define RUBY_RELEASE_YEAR 2013
 #define RUBY_RELEASE_MONTH 6
-#define RUBY_RELEASE_DAY 23
+#define RUBY_RELEASE_DAY 25
 
 #include "ruby/version.h"
 
