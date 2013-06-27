2669b84d407ab431e965145c827db66c91158f89
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-06-27 11:13:08 +0000

    merge revision(s) 41671:
    * ext/openssl/lib/openssl/ssl-inernal.rb (verify_certificate_identity):
      fix hostname verification. Patched by nahi.
    
    * test/openssl/test_ssl.rb (test_verify_certificate_identity): test for
      above.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@41673 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 447
+#define RUBY_PATCHLEVEL 448
 
-#define RUBY_RELEASE_DATE "2013-06-26"
+#define RUBY_RELEASE_DATE "2013-06-27"
 #define RUBY_RELEASE_YEAR 2013
 #define RUBY_RELEASE_MONTH 6
-#define RUBY_RELEASE_DAY 26
+#define RUBY_RELEASE_DAY 27
 
 #include "ruby/version.h"
 
