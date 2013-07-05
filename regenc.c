ed37bfcc9d4306e47f4c73e0eeca202cb1e00d9d
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-07-05 00:54:11 +0000

    broken utf-8
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@41786 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- regenc.c
+++ regenc.c
@@ -55,10 +55,12 @@ extern int
 onigenc_mbclen_approximate(const OnigUChar* p,const OnigUChar* e, struct OnigEncodingTypeST* enc)
 {
   int ret = ONIGENC_PRECISE_MBC_ENC_LEN(enc,p,e);
-  if (ONIGENC_MBCLEN_CHARFOUND_P(ret))
-    return ONIGENC_MBCLEN_CHARFOUND_LEN(ret);
-  else if (ONIGENC_MBCLEN_NEEDMORE_P(ret))
+  if (ONIGENC_MBCLEN_NEEDMORE_P(ret))
     return (int)(e-p)+ONIGENC_MBCLEN_NEEDMORE_LEN(ret);
+  else if (ONIGENC_MBCLEN_INVALID_P(ret))
+    return (int)(e-p)+ONIGENC_MBCLEN_INVALID_LEN(ret);
+  else if (ONIGENC_MBCLEN_CHARFOUND_P(ret))
+    return ONIGENC_MBCLEN_CHARFOUND_LEN(ret);
   return 1;
 }
 
