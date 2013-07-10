843f3a656476d2620c2b228eae58f2812832e485
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-07-10 15:36:06 +0000

    On FreeBSD, threads don't die on daemon(3)
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@41903 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_process.rb
+++ test/ruby/test_process.rb
@@ -1492,7 +1492,19 @@ class TestProcess < Test::Unit::TestCase
         assert_equal(2, data.size, bug4920)
         assert_not_include(data.map(&:to_i), pid)
       end
-    else
+    elsif /freebsd/ =~ RUBY_PLATFORM
+      def test_daemon_no_threads
+        data = Timeout.timeout(3) do
+          IO.popen("-") do |f|
+            break f.readlines.map(&:chomp) if f
+            th = Thread.start {sleep 3}
+            Process.daemon(true, true)
+            puts Thread.list.size, th.status.inspect
+          end
+        end
+        assert_equal(["2", "\"run\""], data)
+      end
+    else # darwin
       def test_daemon_no_threads
         data = Timeout.timeout(3) do
           IO.popen("-") do |f|
