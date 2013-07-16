3569c6c3a611a5e0ed8b84e1dad27f0a31568d80
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-07-16 09:50:11 +0000

    array.c: fix typo
    * array.c (rb_ary_count): [DOC] fix typo.  Array#count uses ==, not
      ===.  a question at asakusa.rb ML.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@42003 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- array.c
+++ array.c
@@ -4113,7 +4113,7 @@ rb_ary_compact(VALUE ary)
  *  Returns the number of elements.
  *
  *  If an argument is given, counts the number of elements which equal +obj+
- *  using <code>===</code>.
+ *  using <code>==</code>.
  *
  *  If a block is given, counts the number of elements for which the block
  *  returns a true value.
