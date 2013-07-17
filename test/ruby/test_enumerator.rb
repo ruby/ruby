9ddff4c8c782e62d896af8281e81c9c4639570fd
Avdi Grimm <avdi@avdi.org>
2013-07-17 12:43:44 -0400

    Clarify test for enumerator size as callable object.

--- test/ruby/test_enumerator.rb
+++ test/ruby/test_enumerator.rb
@@ -381,8 +381,7 @@ class TestEnumerator < Test::Unit::TestCase
     a = []
     e = a.each_with_object(a)
     a << e
-    assert_equal("#<Enumerator: [#<Enumerator: ...>]:each_with_object([#<Enumerator: ...>])>",
-                e.inspect)
+    assert_equal("#<Enumerator: [#<Enumerator: ...>]:each_with_object([#<Enumerator: ...>])>", e.inspect)
   end
 
   def test_inspect_verbose
@@ -438,8 +437,8 @@ class TestEnumerator < Test::Unit::TestCase
     assert_equal nil, Enumerator.new{}.size
     assert_equal 42, Enumerator.new(->{42}){}.size
     obj = Object.new
-    def obj.forty_two; 42; end
-    assert_equal 42, Enumerator.new(obj.method(:forty_two)){}.size
+    def obj.call; 42; end
+    assert_equal 42, Enumerator.new(obj){}.size
     assert_equal 42, Enumerator.new(42){}.size
     assert_equal 1 << 70, Enumerator.new(1 << 70){}.size
     assert_equal Float::INFINITY, Enumerator.new(Float::INFINITY){}.size
