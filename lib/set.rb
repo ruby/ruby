b426f8ebb5510af095852dfd4fa9c9f41ea99358
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-07-31 05:58:14 +0000

    * lib/set.rb: [DOC] Fix example result.  Hash is now ordered.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@42264 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/set.rb
+++ lib/set.rb
@@ -49,7 +49,7 @@
 #   s2 = [1, 2].to_set                    # -> #<Set: {1, 2}>
 #   s1 == s2                              # -> true
 #   s1.add("foo")                         # -> #<Set: {1, 2, "foo"}>
-#   s1.merge([2, 6])                      # -> #<Set: {6, 1, 2, "foo"}>
+#   s1.merge([2, 6])                      # -> #<Set: {1, 2, "foo", 6}>
 #   s1.subset? s2                         # -> false
 #   s2.subset? s1                         # -> true
 #
