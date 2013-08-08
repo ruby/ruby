e0932e3ad38a0740e65478bd85897db734980083
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-08-08 10:58:03 +0000

    * string.c (rb_str_format_m): use RARRAY_RAWPTR() instead of   RARRAY_PTR() because there is no new reference.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@42443 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- string.c
+++ string.c
@@ -1309,7 +1309,7 @@ rb_str_format_m(VALUE str, VALUE arg)
     volatile VALUE tmp = rb_check_array_type(arg);
 
     if (!NIL_P(tmp)) {
-	return rb_str_format(RARRAY_LENINT(tmp), RARRAY_PTR(tmp), str);
+	return rb_str_format(RARRAY_LENINT(tmp), RARRAY_RAWPTR(tmp), str);
     }
     return rb_str_format(1, &arg, str);
 }
