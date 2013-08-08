eb369d430adda4b5e2835165270592042343a9f9
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-08-08 07:09:48 +0000

    merge revision(s) 42439:
    	* test/coverage/test_coverage.rb (TestCoverage#test_big_code): use `1'
    	  instead of `p' to get rid of a side effect.
    	  Kernel#p without any argument seems to do nothing, but flushes stdout.
    	  and, if stdout is redirected to file, fsync() will be called on
    	  Windows.  so, when running test-all on Windows with redirection, such
    	  as CI environment, this test took a lot of time.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@42440 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 458
+#define RUBY_PATCHLEVEL 459
 
-#define RUBY_RELEASE_DATE "2013-08-04"
+#define RUBY_RELEASE_DATE "2013-08-08"
 #define RUBY_RELEASE_YEAR 2013
 #define RUBY_RELEASE_MONTH 8
-#define RUBY_RELEASE_DAY 4
+#define RUBY_RELEASE_DAY 8
 
 #include "ruby/version.h"
 
