37cdb7c147fc8189b21f309b022cbf3ecdc4c62b
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-08-19 09:32:32 +0000

    merge revision(s) 42282: [Backport #8785]
    	* parse.y: fix build error with bison-3.0.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@42621 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 466
+#define RUBY_PATCHLEVEL 467
 
-#define RUBY_RELEASE_DATE "2013-08-16"
+#define RUBY_RELEASE_DATE "2013-08-19"
 #define RUBY_RELEASE_YEAR 2013
 #define RUBY_RELEASE_MONTH 8
-#define RUBY_RELEASE_DAY 16
+#define RUBY_RELEASE_DAY 19
 
 #include "ruby/version.h"
 
