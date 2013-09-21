2e648080254736ab5eb954c9c3bc58bc54bb98fd
Gourav Tiwari <gouravtiwari21@gmail.com>
2013-09-21 12:09:32 +0530

    removed duplicate code

--- benchmark/bm_app_tak.rb
+++ benchmark/bm_app_tak.rb
@@ -1,13 +1,1 @@
-
-def tak x, y, z
-  unless y < x
-    z
-  else
-    tak( tak(x-1, y, z),
-         tak(y-1, z, x),
-         tak(z-1, x, y))
-  end
-end
-
-tak(18, 9, 0)
-
+require_relative 'other-lang/tak'
