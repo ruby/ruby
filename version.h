fe17a46edf43cac07432c1b5a32dd1a9fbe62f7e
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-09-26 16:00:20 +0000

    merge revision(s) 42994: [Backport #8913]
    	* lib/gserver.rb: [DOC] correct gserver.rb license [Bug #8913]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@43060 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2013-09-15"
-#define RUBY_PATCHLEVEL 317
+#define RUBY_RELEASE_DATE "2013-09-27"
+#define RUBY_PATCHLEVEL 318
 
 #define RUBY_RELEASE_YEAR 2013
 #define RUBY_RELEASE_MONTH 9
-#define RUBY_RELEASE_DAY 15
+#define RUBY_RELEASE_DAY 27
 
 #include "ruby/version.h"
 
