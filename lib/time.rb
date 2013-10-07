a8178c69bee18250aa022bb270390279487a8618
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-10-07 21:00:12 +0000

    * lib/time.rb (Time.strptime): Time.strptime('0', '%s') returns local   time Time object as Ruby 2.0 and before.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@43191 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/time.rb
+++ lib/time.rb
@@ -393,8 +393,11 @@ class Time
       d = Date._strptime(date, format)
       raise ArgumentError, "invalid strptime format - `#{format}'" unless d
       if seconds = d[:seconds]
-        offset = d[:offset] || 0
-        Time.at(seconds).localtime(offset)
+        if offset = d[:offset]
+          Time.at(seconds).localtime(offset)
+        else
+          Time.at(seconds)
+        end
       else
         year = d[:year]
         year = yield(year) if year && block_given?
