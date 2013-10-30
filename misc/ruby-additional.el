6ca6820a56176fe6dde99608acf55b7eef9c0681
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-10-30 10:11:20 +0000

    misc/ruby-additional.el: Remove functions implemented in Emacs 24.3.
    * misc/ruby-additional.el (ruby-brace-to-do-end)
      (ruby-do-end-to-brace, ruby-toggle-block): Remove functions that
      are already in the latest released version of Emacs (24.3).
      [Bug #7565]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@43475 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- misc/ruby-additional.el
+++ misc/ruby-additional.el
@@ -19,7 +19,6 @@
 (eval-after-load 'ruby-mode
   '(progn
      (define-key ruby-mode-map "\C-c\C-e" 'ruby-insert-end)
-     (define-key ruby-mode-map "\C-c{" 'ruby-toggle-block)
 
      (defun ruby-insert-end ()
        (interactive)
@@ -31,47 +30,6 @@
              (insert " "))
          (ruby-indent-line t)
          (end-of-line)))
-
-     (defun ruby-brace-to-do-end ()
-       (when (looking-at "{")
-         (let ((orig (point)) (end (progn (ruby-forward-sexp) (point))))
-           (when (eq (preceding-char) ?\})
-             (delete-char -1)
-             (if (eq (char-syntax (preceding-char)) ?w)
-                 (insert " "))
-             (insert "end")
-             (if (eq (char-syntax (following-char)) ?w)
-                 (insert " "))
-             (goto-char orig)
-             (delete-char 1)
-             (if (eq (char-syntax (preceding-char)) ?w)
-                 (insert " "))
-             (insert "do")
-             (when (looking-at "\\sw\\||")
-               (insert " ")
-               (backward-char))
-             t))))
-
-     (defun ruby-do-end-to-brace ()
-       (when (and (or (bolp)
-                      (not (memq (char-syntax (preceding-char)) '(?w ?_))))
-                  (looking-at "\\<do\\(\\s \\|$\\)"))
-         (let ((orig (point)) (end (progn (ruby-forward-sexp) (point))))
-           (backward-char 3)
-           (when (looking-at ruby-block-end-re)
-             (delete-char 3)
-             (insert "}")
-             (goto-char orig)
-             (delete-char 2)
-             (insert "{")
-             (if (looking-at "\\s +|")
-                 (delete-char (- (match-end 0) (match-beginning 0) 1)))
-             t))))
-
-     (defun ruby-toggle-block ()
-       (interactive)
-       (or (ruby-brace-to-do-end)
-           (ruby-do-end-to-brace)))
 
      (defconst ruby-default-encoding-map
        '((us-ascii       . nil)       ;; Do not put coding: us-ascii
