b1529a30e08040b717adef8ac1fa8be1c060e7e1
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-11-21 13:34:38 +0000

    * gc.c: RGENGC_CHECK_MODE should be 0.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@43766 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- gc.c
+++ gc.c
@@ -176,7 +176,7 @@ static ruby_gc_params_t gc_params = {
  * 3: show all references
  */
 #ifndef RGENGC_CHECK_MODE
-#define RGENGC_CHECK_MODE  3
+#define RGENGC_CHECK_MODE  0
 #endif
 
 /* RGENGC_PROFILE
