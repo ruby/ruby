c38b4a9b3d4b99cba59a9953c53f1f736e40a60b
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-11-27 00:32:37 +0000

    test_bug_reporter.rb: remove core
    * test/-ext-/bug_reporter/test_bug_reporter.rb (test_bug_reporter_add):
      remove expected core file to get rid of false error in chkbuild.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@43867 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/-ext-/bug_reporter/test_bug_reporter.rb
+++ test/-ext-/bug_reporter/test_bug_reporter.rb
@@ -1,9 +1,17 @@
 require 'test/unit'
+require 'tmpdir'
 require_relative "../../ruby/envutil"
 
 class TestBugReporter < Test::Unit::TestCase
   def test_bug_reporter_add
     expected_stderr = /Sample bug reporter: 12345/
-    assert_in_out_err(["--disable-gems", "-r-test-/bug_reporter/bug_reporter", "-e", "register_sample_bug_reporter(12345); Process.kill :SEGV, $$"], "", [], expected_stderr, nil)
+    tmpdir = Dir.mktmpdir
+    assert_in_out_err(["--disable-gems", "-r-test-/bug_reporter/bug_reporter",
+                       "-C", tmpdir],
+                      "register_sample_bug_reporter(12345); Process.kill :SEGV, $$",
+                      [],
+                      expected_stderr, nil)
+  ensure
+    FileUtils.rm_rf(tmpdir) if tmpdir
   end
 end
