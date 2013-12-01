c4487ee06b3fb399ee7ad0122ee3f741d56f61f8
zzak <zzak@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-12-01 02:58:50 +0000

    * ext/openssl/lib/openssl/buffering.rb: Fix warning in copyright
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@43940 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/openssl/lib/openssl/buffering.rb
+++ ext/openssl/lib/openssl/buffering.rb
@@ -1,18 +1,19 @@
-<<EOL
-= $RCSfile$ -- Buffering mix-in module.
-
-= Info
-  'OpenSSL for Ruby 2' project
-  Copyright (C) 2001 GOTOU YUUZOU <gotoyuzo@notwork.org>
-  All rights reserved.
-
-= Licence
-  This program is licenced under the same licence as Ruby.
-  (See the file 'LICENCE'.)
-
-= Version
-  $Id$
-EOL
+#
+#--
+#= $RCSfile$ -- Buffering mix-in module.
+#
+#= Info
+#  'OpenSSL for Ruby 2' project
+#  Copyright (C) 2001 GOTOU YUUZOU <gotoyuzo@notwork.org>
+#  All rights reserved.
+#
+#= Licence
+#  This program is licenced under the same licence as Ruby.
+#  (See the file 'LICENCE'.)
+#
+#= Version
+#  $Id$
+#++
 
 ##
 # OpenSSL IO buffering mix-in module.
