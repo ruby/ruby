16e4a501a3e5efbd7fed7096cda0cf816f39c9a9
Vipul A M <vipulnsward@gmail.com>
2013-12-03 18:38:20 +0530

    [DOC] fix some typos in webrick and xmlrpc lib.

--- lib/webrick/compat.rb
+++ lib/webrick/compat.rb
@@ -9,7 +9,7 @@
 # $IPR: compat.rb,v 1.6 2002/10/01 17:16:32 gotoyuzo Exp $
 
 ##
-# System call error module used by webrick for cross platform compatability.
+# System call error module used by webrick for cross platform compatibility.
 #
 # EPROTO:: protocol error
 # ECONNRESET:: remote host reset the connection request
