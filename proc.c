43c3f447a4d442e0b135220013c89f3742645b37
marcandre <marcandre@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-12-25 21:27:48 +0000

    * proc.c: Having optional keyword arguments makes maximum arity +1, not unlimited [#8072]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@44432 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- proc.c
+++ proc.c
@@ -877,8 +877,9 @@ proc_arity(VALUE self)
 static inline int
 rb_iseq_min_max_arity(const rb_iseq_t *iseq, int *max)
 {
-    *max = (iseq->arg_rest == -1 && iseq->arg_keyword == -1) ?
-        iseq->argc + iseq->arg_post_len + iseq->arg_opts - (iseq->arg_opts > 0)
+    *max = iseq->arg_rest == -1 ?
+	iseq->argc + iseq->arg_post_len + iseq->arg_opts -
+	(iseq->arg_opts > 0) + (iseq->arg_keyword != -1)
       : UNLIMITED_ARGUMENTS;
     return iseq->argc + iseq->arg_post_len;
 }
