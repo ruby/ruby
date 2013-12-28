4bf73205d4c22b33bd64c9aa47d2bf8e35cee6ec
DV Suresh <e@dvsuresh.me>
2013-12-28 02:02:34 -0600

    Fix a few typos

--- ext/fiddle/lib/fiddle/import.rb
+++ ext/fiddle/lib/fiddle/import.rb
@@ -63,7 +63,7 @@ module Fiddle
     extend Importer
 
     # Creates an array of handlers for the given +libs+, can be an instance of
-    # Fiddle::Handle, Fiddle::Importer, or will create a new istance of
+    # Fiddle::Handle, Fiddle::Importer, or will create a new instance of
     # Fiddle::Handle using Fiddle.dlopen
     #
     # Raises a DLError if the library cannot be loaded.
