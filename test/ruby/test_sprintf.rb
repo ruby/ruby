c2e807d05d29649fff8a9964c6e86ce883fe9825
Lucas Allan Amorim <lucas.allan@gmail.com>
2013-12-28 14:02:52 -0800

    Added tests for sprintf with a hash as parameter.

--- test/ruby/test_sprintf.rb
+++ test/ruby/test_sprintf.rb
@@ -147,6 +147,11 @@ class TestSprintf < Test::Unit::TestCase
       sprintf("%b", -2147483649), '[ruby-dev:32365]')
     assert_equal(" Inf", sprintf("% e", inf), '[ruby-dev:34002]')
   end
+  
+  def test_hash
+    options = {:capture=>/\d+/}
+    assert_equal("with options {:capture=>/\\d+/}", sprintf("with options %p" % options))
+  end
 
   def test_invalid
     # Star precision before star width:
