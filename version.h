e1536a74243b4c6b006ea51d93ee42b28f234955
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-12-30 21:11:18 +0000

    fix ruby/test_hash.rb [ruby-core:59154] [Bug #9223]
    the behavior is under 2.2.0
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@44466 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.1"
-#define RUBY_RELEASE_DATE "2013-12-27"
-#define RUBY_PATCHLEVEL 1
+#define RUBY_RELEASE_DATE "2013-12-31"
+#define RUBY_PATCHLEVEL 2
 
 #define RUBY_RELEASE_YEAR 2013
 #define RUBY_RELEASE_MONTH 12
-#define RUBY_RELEASE_DAY 27
+#define RUBY_RELEASE_DAY 31
 
 #include "ruby/version.h"
 
