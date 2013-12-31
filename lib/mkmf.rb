2bca263569cba745ffee0856487f993a8a5b5624
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2013-12-31 06:44:58 +0000

    mkmf.rb: expand RUBY_SO_NAME
    * lib/mkmf.rb (RbConfig): expand RUBY_SO_NAME for extensions
      backward compatibility.  [ruby-core:59426] [Bug #9329]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@44469 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -2509,6 +2509,8 @@ MESSAGE
   $configure_args["--topdir"] ||= $curdir
   $ruby = arg_config("--ruby", File.join(RbConfig::CONFIG["bindir"], CONFIG["ruby_install_name"]))
 
+  RbConfig.expand(CONFIG["RUBY_SO_NAME"])
+
   # :startdoc:
 
   split = Shellwords.method(:shellwords).to_proc
