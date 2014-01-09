cc366241ce89ee2397b9c203b5798b9f197a293d
marcandre <marcandre@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-01-09 17:34:27 +0000

    * gc.c: Fix and remove outdated rdoc for object_id
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@44546 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- gc.c
+++ gc.c
@@ -2337,13 +2337,8 @@ id2ref(VALUE obj, VALUE objid)
  *
  *  Returns an integer identifier for +obj+.
  *
- *  The same number will be returned on all calls to +id+ for a given object,
+ *  The same number will be returned on all calls to +object_id+ for a given object,
  *  and no two active objects will share an id.
- *
- *  Object#object_id is a different concept from the +:name+ notation, which
- *  returns the symbol id of +name+.
- *
- *  Replaces the deprecated Object#id.
  */
 
 /*
