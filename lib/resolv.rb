e86a90420d06f613cf597f266abb5429aa2d6b43
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-01-18 14:01:43 +0000

    * lib/resolv.rb (Resolv::DNS::Resource::TXT#data): Return concatenated   string.   Patch by Ryan Brunner.  [ruby-core:58220] [Bug #9093]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@44642 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/resolv.rb
+++ lib/resolv.rb
@@ -1963,10 +1963,10 @@ class Resolv
         attr_reader :strings
 
         ##
-        # Returns the first string from +strings+.
+        # Returns the concatenated string from +strings+.
 
         def data
-          @strings[0]
+          @strings.join("")
         end
 
         def encode_rdata(msg) # :nodoc:
