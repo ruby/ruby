7d1ca1a5b4edcf78b61474c00abb71f3bc861d20
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-01-27 07:47:11 +0000

    merge revision(s) 44402: [Backport #9301]
    	* ext/openssl/ossl_ssl.c (Init_ossl_ssl): Declare a constant
    	  `OP_MSIE_SSLV2_RSA_PADDING` only if the macro is defined.  The
    	  `SSL_OP_MSIE_SSLV2_RSA_PADDING` has been removed from latest
    	  snapshot of OpenSSL 1.0.1.  [Fixes GH-488]
    
    
    git-svn-id: svn+ssh://svn.ruby-lang.org/ruby/branches/ruby_2_1@44710 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.1"
-#define RUBY_RELEASE_DATE "2014-01-23"
-#define RUBY_PATCHLEVEL 8
+#define RUBY_RELEASE_DATE "2014-01-27"
+#define RUBY_PATCHLEVEL 9
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 1
-#define RUBY_RELEASE_DAY 23
+#define RUBY_RELEASE_DAY 27
 
 #include "ruby/version.h"
 
