672deabc5a43f8fb0ea6e524b099eec976072e06
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-01-29 03:35:43 +0000

    merge revision(s) 39388: [Backport #9368]
    	* lib/rexml/document.rb (REXML::Document.entity_expansion_text_limit):
    	  fix a typo in comment in r39384.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@44735 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 489
+#define RUBY_PATCHLEVEL 490
 
-#define RUBY_RELEASE_DATE "2014-01-09"
+#define RUBY_RELEASE_DATE "2014-01-29"
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 1
-#define RUBY_RELEASE_DAY 9
+#define RUBY_RELEASE_DAY 29
 
 #include "ruby/version.h"
 
