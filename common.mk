ruby/ruby

commit 4a429c11e1319b0e13368851454f109217ab5de5

enumerator.c: allow Bignum for with_index
* enumerator.c (enumerator_with_index_i): allow Bignum as offset, to
  get rid of conversion exception and integer overflow.
  [ruby-dev:47131] [Bug #8010]


git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@39594 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

enumerator.c
@@ -460,11 +460,9 @@ enumerator_each(int argc, VALUE *argv, VALUE obj)
 static VALUE
 enumerator_with_index_i(VALUE val, VALUE m, int argc, VALUE *argv)
 {
-    VALUE idx;
     VALUE *memo = (VALUE *)m;
-
-    idx = INT2FIX(*memo);
-    ++*memo;
+    VALUE idx = *memo;
+    *memo = rb_int_succ(idx);
 
     if (argc <= 1)
 	return rb_yield_values(2, val, idx);
@@ -494,7 +492,6 @@ enumerator_with_index(int argc, VALUE *argv, VALUE obj)
 
     rb_scan_args(argc, argv, "01", &memo);
     RETURN_SIZED_ENUMERATOR(obj, argc, argv, enumerator_size);
-    memo = NIL_P(memo) ? 0 : (VALUE)NUM2LONG(memo);
     return enumerator_block_call(obj, enumerator_with_index_i, (VALUE)&memo);
 }
 

test/ruby/test_enumerator.rb
@@ -104,6 +104,14 @@ class TestEnumerator < Test::Unit::TestCase
     assert_equal([[1,5],[2,6],[3,7]], @obj.to_enum(:foo, 1, 2, 3).with_index(5).to_a)
   end
 
+  def test_with_index_large_offset
+    bug8010 = '[ruby-dev:47131] [Bug #8010]'
+    s = 1 << (8*1.size-2)
+    assert_equal([[1,s],[2,s+1],[3,s+2]], @obj.to_enum(:foo, 1, 2, 3).with_index(s).to_a, bug8010)
+    s <<= 1
+    assert_equal([[1,s],[2,s+1],[3,s+2]], @obj.to_enum(:foo, 1, 2, 3).with_index(s).to_a, bug8010)
+  end
+
   def test_with_object
     obj = [0, 1]
     ret = (1..10).each.with_object(obj) {|i, memo|

internal.h
@@ -169,6 +169,8 @@ VALUE num_interval_step_size(VALUE from, VALUE to, VALUE step, int excl);
 int ruby_float_step(VALUE from, VALUE to, VALUE step, int excl);
 double ruby_float_mod(double x, double y);
 int rb_num_negative_p(VALUE);
+VALUE rb_int_succ(VALUE num);
+VALUE rb_int_pred(VALUE num);
 
 /* object.c */
 VALUE rb_obj_equal(VALUE obj1, VALUE obj2);

numeric.c
@@ -2383,8 +2383,8 @@ fix_succ(VALUE num)
  *     (-1).next   #=> 0
  */
 
-static VALUE
-int_succ(VALUE num)
+VALUE
+rb_int_succ(VALUE num)
 {
     if (FIXNUM_P(num)) {
 	long i = FIX2LONG(num) + 1;
@@ -2396,6 +2396,8 @@ int_succ(VALUE num)
     return rb_funcall(num, '+', 1, INT2FIX(1));
 }
 
+#define int_succ rb_int_succ
+
 /*
  *  call-seq:
  *     int.pred  ->  integer
@@ -2406,8 +2408,8 @@ int_succ(VALUE num)
  *     (-1).pred   #=> -2
  */
 
-static VALUE
-int_pred(VALUE num)
+VALUE
+rb_int_pred(VALUE num)
 {
     if (FIXNUM_P(num)) {
 	long i = FIX2LONG(num) - 1;
@@ -2418,6 +2420,8 @@ int_pred(VALUE num)
     }
     return rb_funcall(num, '-', 1, INT2FIX(1));
 }
+
+#define int_pred rb_int_pred
 
 VALUE
 rb_enc_uint_chr(unsigned int code, rb_encoding *enc)

commit 0678e0250181948c998d96219ac03eb471b53edc

merge revision(s) 39722,43929: [Backport #9178]
	* enumerator.c (enumerator_with_index): try to convert given offset to
	  integer. fix bug introduced in r39594.

	* enumerator.c (enumerator_with_index): should not store local variable
	  address to memoise the arguments.  it is invalidated after the return.
	  [ruby-core:58692] [Bug #9178]


git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@44745 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

common.mk
@@ -619,7 +619,7 @@ encoding.$(OBJEXT): {$(VPATH)}encoding.c $(RUBY_H_INCLUDES) \
   {$(VPATH)}internal.h
 enum.$(OBJEXT): {$(VPATH)}enum.c $(RUBY_H_INCLUDES) {$(VPATH)}node.h \
   {$(VPATH)}util.h $(ID_H_INCLUDES)
-enumerator.$(OBJEXT): {$(VPATH)}enumerator.c $(RUBY_H_INCLUDES)
+enumerator.$(OBJEXT): {$(VPATH)}enumerator.c $(RUBY_H_INCLUDES) {$(VPATH)}node.h
 error.$(OBJEXT): {$(VPATH)}error.c {$(VPATH)}known_errors.inc \
   $(RUBY_H_INCLUDES) $(VM_CORE_H_INCLUDES) $(ENCODING_H_INCLUDES) \
   {$(VPATH)}debug.h \

enumerator.c
@@ -13,6 +13,7 @@
 ************************************************/
 
 #include "ruby/ruby.h"
+#include "node.h"
 
 /*
  * Document-class: Enumerator
@@ -368,9 +369,9 @@ enumerator_each(VALUE obj)
 static VALUE
 enumerator_with_index_i(VALUE val, VALUE m, int argc, VALUE *argv)
 {
-    VALUE *memo = (VALUE *)m;
-    VALUE idx = *memo;
-    *memo = rb_int_succ(idx);
+    NODE *memo = (NODE *)m;
+    VALUE idx = memo->u1.value;
+    memo->u1.value = rb_int_succ(idx);
 
     if (argc <= 1)
 	return rb_yield_values(2, val, idx);
@@ -401,7 +402,7 @@ enumerator_with_index(int argc, VALUE *argv, VALUE obj)
 	memo = INT2FIX(0);
     else
 	memo = rb_to_int(memo);
-    return enumerator_block_call(obj, enumerator_with_index_i, (VALUE)&memo);
+    return enumerator_block_call(obj, enumerator_with_index_i, (VALUE)NEW_MEMO(memo, 0, 0));
 }
 
 /*

node.h
@@ -447,6 +447,7 @@ typedef struct RNode {
 #define NEW_ATTRASGN(r,m,a) NEW_NODE(NODE_ATTRASGN,r,m,a)
 #define NEW_PRELUDE(p,b) NEW_NODE(NODE_PRELUDE,p,b,0)
 #define NEW_OPTBLOCK(a) NEW_NODE(NODE_OPTBLOCK,a,0,0)
+#define NEW_MEMO(a,b,c) NEW_NODE(NODE_MEMO,a,b,c)
 
 #if defined __GNUC__ && __GNUC__ >= 4
 #pragma GCC visibility push(default)

version.h
@@ -1,5 +1,5 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 499
+#define RUBY_PATCHLEVEL 500
 
 #define RUBY_RELEASE_DATE "2014-01-29"
 #define RUBY_RELEASE_YEAR 2014

test/ruby/test_enumerator.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require_relative "envutil"
 
 class TestEnumerator < Test::Unit::TestCase
   def setup
@@ -103,6 +104,28 @@ class TestEnumerator < Test::Unit::TestCase
     assert_equal([[1,s],[2,s+1],[3,s+2]], @obj.to_enum(:foo, 1, 2, 3).with_index(s).to_a, bug8010)
     s <<= 1
     assert_equal([[1,s],[2,s+1],[3,s+2]], @obj.to_enum(:foo, 1, 2, 3).with_index(s).to_a, bug8010)
+  end
+
+  def test_with_index_nonnum_offset
+    bug8010 = '[ruby-dev:47131] [Bug #8010]'
+    s = Object.new
+    def s.to_int; 1 end
+    assert_equal([[1,1],[2,2],[3,3]], @obj.to_enum(:foo, 1, 2, 3).with_index(s).to_a, bug8010)
+  end
+
+  def test_with_index_string_offset
+    bug8010 = '[ruby-dev:47131] [Bug #8010]'
+    assert_raise(TypeError, bug8010){ @obj.to_enum(:foo, 1, 2, 3).with_index('1').to_a }
+  end
+
+  def test_with_index_dangling_memo
+    bug9178 = '[ruby-core:58692] [Bug #9178]'
+    assert_in_out_err([], <<-"end;", ["Enumerator", "[false, [1]]"], [], bug9178)
+    bug = "#{bug9178}"
+    e = [1].to_enum(:chunk).with_index {|c,i| i == 5}
+    puts e.class
+    p e.to_a[0]
+    end;
   end
 
   def test_with_object