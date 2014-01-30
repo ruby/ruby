2e83f537e28f1c79533a80d0c8411058d84579dc
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-01-30 17:51:17 +0000

    vm_eval.c: compare method type
    * vm_eval.c (vm_call0_body): compare method type with
      VM_METHOD_TYPE_ZSUPER.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@44770 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- vm_eval.c
+++ vm_eval.c
@@ -198,7 +198,7 @@ vm_call0_body(rb_thread_t* th, rb_call_info_t *ci, const VALUE *argv)
 	    ci->defined_class = RCLASS_SUPER(ci->defined_class);
 
 	    if (!ci->defined_class || !(ci->me = rb_method_entry(ci->defined_class, ci->mid, &ci->defined_class))) {
-		int ex = VM_METHOD_TYPE_ZSUPER ? NOEX_SUPER : 0;
+		int ex = type == VM_METHOD_TYPE_ZSUPER ? NOEX_SUPER : 0;
 		ret = method_missing(ci->recv, ci->mid, ci->argc, argv, ex);
 		goto success;
 	    }
