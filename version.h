fb4cce605eddec2e6f180eb8229ab42dcea0328c
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-01-31 12:58:39 +0000

    fixed merge mistake at r44766.
    git-svn-id: svn+ssh://svn.ruby-lang.org/ruby/branches/ruby_1_9_3@44779 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 510
+#define RUBY_PATCHLEVEL 511
 
-#define RUBY_RELEASE_DATE "2014-01-30"
+#define RUBY_RELEASE_DATE "2014-01-31"
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 1
-#define RUBY_RELEASE_DAY 30
+#define RUBY_RELEASE_DAY 31
 
 #include "ruby/version.h"
 
