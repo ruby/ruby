b91118ccbcbeb231a815fffa72c0bbd2cdc53026
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-05 02:38:51 +0000

    merge revision(s) 37905: [Backport #8757]
    	* configure.in (opt-dir): don't use non portable flag -E of sed.
    
    
    git-svn-id: svn+ssh://svn.ruby-lang.org/ruby/branches/ruby_1_9_3@44820 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 511
+#define RUBY_PATCHLEVEL 512
 
-#define RUBY_RELEASE_DATE "2014-01-31"
+#define RUBY_RELEASE_DATE "2014-02-05"
 #define RUBY_RELEASE_YEAR 2014
-#define RUBY_RELEASE_MONTH 1
-#define RUBY_RELEASE_DAY 31
+#define RUBY_RELEASE_MONTH 2
+#define RUBY_RELEASE_DAY 5
 
 #include "ruby/version.h"
 
