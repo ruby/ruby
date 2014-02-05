252adbfb32325b04ade5a42a05b74311a81c13f3
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-05 14:03:31 +0000

    merge revision(s) r42456: [Backport #8101]
    	* time.c (get_timeval, get_new_timeval): use rb_obj_class()
    	  instead of CLASS_OF() because CLASS_OF() may return
    	  a singleton class.
    
    
    git-svn-id: svn+ssh://svn.ruby-lang.org/ruby/branches/ruby_2_0_0@44844 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2014-02-02"
-#define RUBY_PATCHLEVEL 394
+#define RUBY_RELEASE_DATE "2014-02-05"
+#define RUBY_PATCHLEVEL 395
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 2
+#define RUBY_RELEASE_DAY 5
 
 #include "ruby/version.h"
 
