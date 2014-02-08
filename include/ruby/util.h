cd476cd86be581cca4ca56ad1277228ea1dc24ee
glass <glass@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-08 12:50:28 +0000

    * configure.in: check qsort_r(3) and whether it is GNU version.   BSD version has different prototype.
    * util.h: use qsort_r() as ruby_qsort() if it is GNU version.
    
    * util.c: define ruby_qsort() if needed.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@44889 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/util.h
+++ include/ruby/util.h
@@ -55,8 +55,12 @@ unsigned long ruby_scan_oct(const char *, size_t, size_t *);
 #define scan_hex(s,l,e) ((int)ruby_scan_hex((s),(l),(e)))
 unsigned long ruby_scan_hex(const char *, size_t, size_t *);
 
+#ifdef HAVE_GNU_QSORT_R
+# define ruby_qsort qsort_r
+#else
 void ruby_qsort(void *, const size_t, const size_t,
 		int (*)(const void *, const void *, void *), void *);
+#endif
 
 void ruby_setenv(const char *, const char *);
 void ruby_unsetenv(const char *);
