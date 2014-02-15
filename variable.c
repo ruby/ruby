89e43aa9329121996f5a4cd212b7e0405017f704
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-15 15:57:46 +0000

    merge revision(s) r44463,r44464: [Backport #9210]
    	* variable.c: adding extra example in docs.
    	  patched by Steve Klabnik. [Bug #9210]
    
    	* variable.c: [DOC] adding extra example in docs.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@44984 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- variable.c
+++ variable.c
@@ -2500,8 +2500,9 @@ cvar_list(void *data)
  *     class Two < One
  *       @@var2 = 2
  *     end
- *     One.class_variables   #=> [:@@var1]
- *     Two.class_variables   #=> [:@@var2, :@@var1]
+ *     One.class_variables          #=> [:@@var1]
+ *     Two.class_variables          #=> [:@@var2, :@@var1]
+ *     Two.class_variables(false)   #=> [:@@var2]
  */
 
 VALUE
