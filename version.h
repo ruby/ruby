fe4916ca477c1782d975bec6379ae4232b6c2f88
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-15 06:53:01 +0000

    merge revision(s) r44931: [Backport #9452]
    	* vm_insnhelper.c (vm_call_method): should check ci->me->flag of
    	  a refining method in case the method is private.
    	  [ruby-core:60111] [Bug #9452]
    
    	* vm_method.c (make_method_entry_refined): set me->flag of a refined
    	  method entry to NOEX_PUBLIC in case the original method is private
    	  and it is refined as a public method.  The original flag is stored
    	  in me->def->body.orig_me, so it's OK to make a refined method
    	  entry public.  [ruby-core:60111] [Bug #9452]
    
    	* test/ruby/test_refinement.rb: related tests.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@44967 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2014-02-11"
-#define RUBY_PATCHLEVEL 402
+#define RUBY_RELEASE_DATE "2014-02-15"
+#define RUBY_PATCHLEVEL 403
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 11
+#define RUBY_RELEASE_DAY 15
 
 #include "ruby/version.h"
 
