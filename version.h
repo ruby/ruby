84fe2b2f1db84d029a0d68a26ae9b9aa59281244
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-16 00:13:51 +0000

    merge revision(s) 44474,44538,44539,44890,44896: [Backport #9495]
    	* configure.in: use SSE2 instructions for drop unexpected
    	  precisions.  [ruby-core:54738] [Bug #8358]
    
    	* configure.in: use SSE2 instructions to drop unexpected precisions on
    	  other than mingw.  [ruby-core:59472] [Bug #8358]
    
    	* configure.in: -mstackrealign is necessary for -msse2 working.
    	  [ruby-core:54716] [Bug #8349]
    
    	* configure.in: -mstackrealign is necessary for -msse2 working.
    	  [ruby-core:54716] [Bug #8349]
    
    	* configure.in: add qouting brackets and append wildcard for the
    	  rest after target_cpu, to properly detect platform for SSE2
    	  instructions.  [ruby-core:60576] [Bug #8358]
    
    	* configure.in: Fix compilation error.
    	  https://bugs.ruby-lang.org/issues/8358#note-16
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@44993 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.1"
-#define RUBY_RELEASE_DATE "2014-02-10"
-#define RUBY_PATCHLEVEL 30
+#define RUBY_RELEASE_DATE "2014-02-16"
+#define RUBY_PATCHLEVEL 31
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 10
+#define RUBY_RELEASE_DAY 16
 
 #include "ruby/version.h"
 
