b258a1547513192278ef6069178766855a947248
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-16 21:28:55 +0000

    merge revision(s) 44476: [Backport #9317]
    	* configure.in: reset LDFLAGS and DLDFLAGS for opt-dir again after
    	  LIBPATHFLAG and RPATHFLAG are set.  [ruby-dev:47868] [Bug #9317]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@45017 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.1"
-#define RUBY_RELEASE_DATE "2014-02-16"
-#define RUBY_PATCHLEVEL 31
+#define RUBY_RELEASE_DATE "2014-02-17"
+#define RUBY_PATCHLEVEL 32
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 16
+#define RUBY_RELEASE_DAY 17
 
 #include "ruby/version.h"
 
