efe5d8db6e7934e7a6e4cec3f8b134b622a63b0c
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-17 09:09:32 +0000

    merge revision(s) 43148,43149,43152: [Backport #8433]
    	* thread.c (terminate_atfork_i): fix locking mutexes not unlocked in
    	  forks when not tracked in thread.  [ruby-core:55102] [Bug #8433]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@45026 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 534
+#define RUBY_PATCHLEVEL 535
 
-#define RUBY_RELEASE_DATE "2014-02-14"
+#define RUBY_RELEASE_DATE "2014-02-17"
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 14
+#define RUBY_RELEASE_DAY 17
 
 #include "ruby/version.h"
 
