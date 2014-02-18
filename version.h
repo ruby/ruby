85186dab0add187a7f02aec0abead7fd4e1c6e71
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-18 13:54:32 +0000

    * ruby_atomic.h: fixed merge mistake of r44946.  reported by ngoto at   [ruby-dev:47980] [Backport #9530]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@45036 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 535
+#define RUBY_PATCHLEVEL 536
 
-#define RUBY_RELEASE_DATE "2014-02-17"
+#define RUBY_RELEASE_DATE "2014-02-18"
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 17
+#define RUBY_RELEASE_DAY 18
 
 #include "ruby/version.h"
 
