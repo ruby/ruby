401dc07f45899c68489bf84d05b321cf85cbb8b0
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-21 11:55:57 +0000

    merge revision(s) r45003: [Backport #9481]
    	ext/psych/yaml: suppress warnings
    
    	* ext/psych/yaml/emitter.c (WRITE_BREAK, PUT_BREAK): suppress
      unused-value warnings.
    
    	* ext/psych/yaml/parser.c, ext/psych/yaml/scanner.c: suppress
      unused-but-set-variable warnings.
    
    	* ext/psych/yaml/yaml_private.h (STACK_EMPTY): ditto.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@45077 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/psych/yaml/scanner.c
+++ ext/psych/yaml/scanner.c
@@ -1193,11 +1193,9 @@ yaml_parser_increase_flow_level(yaml_parser_t *parser)
 static int
 yaml_parser_decrease_flow_level(yaml_parser_t *parser)
 {
-    yaml_simple_key_t dummy_key;    /* Used to eliminate a compiler warning. */
-
     if (parser->flow_level) {
         parser->flow_level --;
-        dummy_key = POP(parser, parser->simple_keys);
+        (void)POP(parser, parser->simple_keys);
     }
 
     return 1;
