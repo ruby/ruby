4423b56753be681b4ddd4541c48044ce289d122e
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-21 05:52:00 +0000

    merge revision(s) 44720: [Backport #9455]
    	* compile.c (iseq_build_from_ary_body): Use :blockptr instead of :block
    	  as hash key when loading serialized instruction sequences from arrays.
    	  [Bug #9455] [ruby-core:60146]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@45069 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.1"
-#define RUBY_RELEASE_DATE "2014-02-20"
-#define RUBY_PATCHLEVEL 39
+#define RUBY_RELEASE_DATE "2014-02-21"
+#define RUBY_PATCHLEVEL 40
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 20
+#define RUBY_RELEASE_DAY 21
 
 #include "ruby/version.h"
 
