cf3896bf771859581ac2b1ffc90df06502985cfa
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-22 05:05:22 +0000

    merge revision(s) 44931: [Backport #9452]
    	* vm_insnhelper.c (vm_call_method): should check ci->me->flag of
    	  a refining method in case the method is private.
    	  [ruby-core:60111] [Bug #9452]
    
    	* vm_method.c (make_method_entry_refined): set me->flag of a refined
    	  method entry to NOEX_PUBLIC in case the original method is private
    	  and it is refined as a public method.  The original flag is stored
    	  in me->def->body.orig_me, so it's OK to make a refined method
    	  entry public.  [ruby-core:60111] [Bug #9452]
    
    	* test/ruby/test_refinement.rb: related tests.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@45107 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- vm_insnhelper.c
+++ vm_insnhelper.c
@@ -1829,7 +1829,7 @@ vm_call_method(rb_thread_t *th, rb_control_frame_t *cfp, rb_call_info_t *ci)
 		    ci->me = me;
 		    ci->defined_class = defined_class;
 		    if (me->def->type != VM_METHOD_TYPE_REFINED) {
-			goto normal_method_dispatch;
+			goto start_method_dispatch;
 		    }
 		}
 
@@ -1838,11 +1838,8 @@ vm_call_method(rb_thread_t *th, rb_control_frame_t *cfp, rb_call_info_t *ci)
 		    ci->me = ci->me->def->body.orig_me;
 		    if (UNDEFINED_METHOD_ENTRY_P(ci->me)) {
 			ci->me = 0;
-			goto start_method_dispatch;
 		    }
-		    else {
-			goto normal_method_dispatch;
-		    }
+		    goto start_method_dispatch;
 		}
 		else {
 		    klass = ci->me->klass;
