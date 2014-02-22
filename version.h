9e11c028e3ff12da9e44f1ece7d100b84834e183
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-22 00:50:33 +0000

    merge revision(s) 45076: [Backport #9535]
    	* class.c (rb_mod_init_copy): do nothing if copying self.
    	  [ruby-dev:47989] [Bug #9535]
    
    	* hash.c (rb_hash_initialize_copy): ditto.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@45091 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 537
+#define RUBY_PATCHLEVEL 538
 
-#define RUBY_RELEASE_DATE "2014-02-19"
+#define RUBY_RELEASE_DATE "2014-02-22"
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 19
+#define RUBY_RELEASE_DAY 22
 
 #include "ruby/version.h"
 
