61899ed62244d393f2203146ecc3c3952528f482
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-24 03:33:47 +0000

    merge revision(s) 44585: [Backport #9268]
    	* lib/net/smtp.rb: [DOC] Remove dead link to RAA by Giorgos Tsiftsis
    	  Fixes the following bugs: [Bug #9152] [Bug #9268] [Bug #9394]
    
    	* lib/open-uri.rb: ditto
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@45154 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 542
+#define RUBY_PATCHLEVEL 543
 
-#define RUBY_RELEASE_DATE "2014-02-22"
+#define RUBY_RELEASE_DATE "2014-02-24"
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 22
+#define RUBY_RELEASE_DAY 24
 
 #include "ruby/version.h"
 
