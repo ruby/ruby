cd18c82b695c68b5549831634c1e7302ee23f42c
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-02-24 11:48:41 +0000

    merge revision(s) r45101:
    	Fix a filename.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@45166 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2014-02-22"
-#define RUBY_PATCHLEVEL 450
+#define RUBY_RELEASE_DATE "2014-02-24"
+#define RUBY_PATCHLEVEL 451
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 22
+#define RUBY_RELEASE_DAY 24
 
 #include "ruby/version.h"
 
