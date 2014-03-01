3b12b75e87412762ad4bb2a51b5e4a81d2982979
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-03-01 08:25:29 +0000

    merge revision(s) 44433: [Backport #9299]
    	* proc.c: Having any mandatory keyword argument increases min arity
    	  [#9299]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@45226 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.2"
-#define RUBY_RELEASE_DATE "2014-02-24"
-#define RUBY_PATCHLEVEL 77
+#define RUBY_RELEASE_DATE "2014-03-01"
+#define RUBY_PATCHLEVEL 78
 
 #define RUBY_RELEASE_YEAR 2014
-#define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 24
+#define RUBY_RELEASE_MONTH 3
+#define RUBY_RELEASE_DAY 1
 
 #include "ruby/version.h"
 
