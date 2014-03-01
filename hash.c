4b6a0ae306f995d7bec668cf8e046c01defc641b
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-03-01 09:02:36 +0000

    merge revision(s) 44370: [Backport #9576]
    	hash.c: extra states
    
    	* hash.c (rb_hash_reject): use words "extra states".
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@45227 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- hash.c
+++ hash.c
@@ -1178,7 +1178,7 @@ rb_hash_reject(VALUE hash)
 #endif
 	}
     }
-#if HASH_REJECT_COPY_MISC_ATTRIBUTES
+#if HASH_REJECT_COPY_EXTRA_STATES
     result = rb_hash_dup_empty(hash);
 #else
     result = rb_hash_new();
