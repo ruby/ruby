516881a1198442d8a2e6319e725b125663f959b6
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-03-15 16:00:32 +0000

    merge revision(s) r45342:
    	* ext/.document: remove refinement from documentable directories.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@45343 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2014-03-03"
-#define RUBY_PATCHLEVEL 457
+#define RUBY_RELEASE_DATE "2014-03-16"
+#define RUBY_PATCHLEVEL 458
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 3
-#define RUBY_RELEASE_DAY 3
+#define RUBY_RELEASE_DAY 16
 
 #include "ruby/version.h"
 
