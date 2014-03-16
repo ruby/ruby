83a649687038d1b39fff8503be22f52b729cacb9
gogotanaka <yakuruto771@live.jp>
2014-03-16 19:55:58 +0900

    Add cofactor method for Matrix class

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -59,6 +59,7 @@ end
 # * #find_index
 # * #minor(*param)
 # * #first_minor(row, column)
+# * #cofactor(row, column)
 #
 # Properties of a matrix:
 # * #diagonal?
@@ -544,6 +545,7 @@ class Matrix
     nil
   end
   alias_method :find_index, :index
+
   #
   # Returns a section of the matrix.  The parameters are either:
   # *  start_row, nrows, start_col, ncols; OR
@@ -616,6 +618,20 @@ class Matrix
     end
 
     new_matrix arrays, column_count - 1
+  end
+
+  #
+  # Returns a (i,j) cofactor which is obtained by multiplying the first minor by (-1)**(i+j).
+  #
+  #   Matrix.diagonal(9, 5, -3, 4).cofactor(1, 1)
+  #     => -108
+  #
+  def cofactor(row, column)
+    raise TypeError, "cofactor of empty matrix is not defined" if empty?
+    Matrix.Raise ErrDimensionMismatch unless square?
+
+    det_of_minor = first_minor(row, column).determinant
+    det_of_minor * (-1) ** (row + column)
   end
 
   #--
