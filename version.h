e19b240b5761c294420e12958512bea9435060f4
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-03-21 15:34:57 +0000

    merge revision(s) r45311: [Backport #9599]
    	r45298 fix trivial bug
    
    	$initial_size must be a size, not a status
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@45379 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2014-03-20"
-#define RUBY_PATCHLEVEL 461
+#define RUBY_RELEASE_DATE "2014-03-22"
+#define RUBY_PATCHLEVEL 462
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 3
-#define RUBY_RELEASE_DAY 20
+#define RUBY_RELEASE_DAY 22
 
 #include "ruby/version.h"
 
