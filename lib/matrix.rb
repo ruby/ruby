ea4ab6ccbb0ad50b9a1404fb7f7b536f16f09c47
gogotanaka <yakuruto771@live.jp>
2014-03-23 16:35:10 +0900

    revert "improve exception for matrix#inverse"

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -911,7 +911,6 @@ class Matrix
   #         0 -1
   #
   def inverse
-    raise RuntimeError, "inverse of empty matrix is not defined" if empty?
     Matrix.Raise ErrDimensionMismatch unless square?
     self.class.I(row_count).send(:inverse_from, self)
   end
