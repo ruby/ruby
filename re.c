6c27d383ddd1e416926533d16618b5886c9b49a4
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-03-27 02:58:34 +0000

    re.c: indent
    * re.c (rb_reg_search): adjust indent.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@45444 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- re.c
+++ re.c
@@ -1467,7 +1467,7 @@ rb_reg_search0(VALUE re, VALUE str, long pos, int reverse, int set_backref_str)
 long
 rb_reg_search(VALUE re, VALUE str, long pos, int reverse)
 {
-	return rb_reg_search0(re, str, pos, reverse, 1);
+    return rb_reg_search0(re, str, pos, reverse, 1);
 }
 
 VALUE
