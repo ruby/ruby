0b57f67531a1dc722524a5316657ffb9c783b108
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-04-01 08:29:36 +0000

    config_files.rb: show failure
    * tool/config_files.rb (ConfigFiles.download): show failed URI.
      [ruby-core:61792] [Bug #9690]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@45494 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/config_files.rb
+++ tool/config_files.rb
@@ -1,8 +1,11 @@
 require 'open-uri'
 
-ConfigFiles = "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=%s;hb=HEAD"
+ConfigFiles = "http://git0.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=%s;hb=HEAD"
 def ConfigFiles.download(name, dir = nil)
-  data = URI(self % name).read
+  uri = URI(self % name)
+  data = uri.read
   file = dir ? File.join(dir, name) : name
   open(file, "wb", 0755) {|f| f.write(data)}
+rescue => e
+  raise "failed to download #{name}\n#{e.message}: #{uri}"
 end
