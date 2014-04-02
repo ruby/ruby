67b2d561d3aa92ac01903d015b50e78430b50950
AndyObtiva <andy.am@gmail.com>
2014-04-01 21:38:28 -0400

    Switched to using class << base as per suggestion by marcandre in Pull Request https://github.com/ruby/ruby/pull/581

--- lib/super_module.rb
+++ lib/super_module.rb
@@ -81,50 +81,47 @@ module SuperModule
     :singleton_method_added
   ]
   def self.included(base)
-    base.class_eval do
-      class << self
-
-        def include(base, &block)
-          method_missing('include', base, &block)
-        end
-
-        def __super_module_class_method_calls
-          @__super_module_class_method_calls ||= []
-        end
-
-        def __super_module_class_methods
-          @__super_module_class_methods ||= []
-        end
-
-        def singleton_method_added(method_name)
-          __super_module_class_methods << [method_name, method(method_name)] unless EXCLUDED_SINGLETON_METHODS.include?(method_name)
-          super
-        end
-
-        def method_missing(method_name, *args, &block)
-          __super_module_class_method_calls << [method_name, args, block]
-        end
-
-        def __invoke_super_module_class_method_calls(base)
-          __super_module_class_method_calls.each do |method_name, args, block|
-            base.class_eval do
-              send(method_name, *args, &block)
-            end
+    class << base
+      def include(base, &block)
+        method_missing('include', base, &block)
+      end
+      
+      def __super_module_class_method_calls
+        @__super_module_class_method_calls ||= []
+      end
+      
+      def __super_module_class_methods
+        @__super_module_class_methods ||= []
+      end
+      
+      def singleton_method_added(method_name)
+        __super_module_class_methods << [method_name, method(method_name)] unless EXCLUDED_SINGLETON_METHODS.include?(method_name)
+        super
+      end
+      
+      def method_missing(method_name, *args, &block)
+        __super_module_class_method_calls << [method_name, args, block]
+      end
+      
+      def __invoke_super_module_class_method_calls(base)
+        __super_module_class_method_calls.each do |method_name, args, block|
+          base.class_eval do
+            send(method_name, *args, &block)
           end
         end
-
-        def __define_super_module_class_methods(base)
-          __super_module_class_methods.each do |method_name, method|
-            base.class_eval do
-              self.class.send(:define_method, method_name, &method)
-            end
+      end
+      
+      def __define_super_module_class_methods(base)
+        __super_module_class_methods.each do |method_name, method|
+          base.class_eval do
+            self.class.send(:define_method, method_name, &method)
           end
         end
-
-        def included(base)
-          __invoke_super_module_class_method_calls(base)
-          __define_super_module_class_methods(base)
-        end
+      end
+      
+      def included(base)
+        __invoke_super_module_class_method_calls(base)
+        __define_super_module_class_methods(base)
       end
     end
   end
