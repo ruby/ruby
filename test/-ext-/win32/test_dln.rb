cf0a0c5a6632a654119e93c343d6512b562783d6
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-04-10 07:39:44 +0000

    explicitly specify ::File
    There's Bug::File in test/-ext-/file/test_stat.rb.
    It prevent constan search with `File`.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@45551 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/-ext-/win32/test_dln.rb
+++ test/-ext-/win32/test_dln.rb
@@ -14,18 +14,18 @@ module Bug
       def test_nonascii_load
         bug9699 = '[ruby-core:61845] [Bug #9699]'
         so = "-test-/win32/dln/empty." + RbConfig::CONFIG["DLEXT"]
-        so = $:.find {|d| d = File.join(d, so); break d if File.exist?(d)}
+        so = $:.find {|d| d = ::File.join(d, so); break d if ::File.exist?(d)}
         assert_not_nil(so)
-        Dir.mkdir(dir = File.join(testdir = Dir.mktmpdir("test"), "\u{30c6 30b9 30c8}"))
-        File.copy_stream(so, File.join(dir, File.basename(so)))
-        assert_separately(['-', bug9699, testdir, File.basename(so)], <<-'end;')
+        Dir.mkdir(dir = ::File.join(testdir = Dir.mktmpdir("test"), "\u{30c6 30b9 30c8}"))
+        ::File.copy_stream(so, ::File.join(dir, ::File.basename(so)))
+        assert_separately(['-', bug9699, testdir, ::File.basename(so)], <<-'end;')
           bug, dir, so = *ARGV
           assert_nothing_raised(LoadError, bug) do
-            require File.join(dir, "\u{30c6 30b9 30c8}", so)
+            require ::File.join(dir, "\u{30c6 30b9 30c8}", so)
           end
         end;
       ensure
-        File.unlink(File.join(dir, File.basename(so))) rescue nil
+        ::File.unlink(::File.join(dir, ::File.basename(so))) rescue nil
         Dir.rmdir(dir) rescue nil
         Dir.rmdir(testdir) rescue nil
       end
