0ee6aff847ecccdcfda911b54ad149a1f2543ad2
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-04-18 07:55:04 +0000

    string.c: keep code range
    * string.c (str_buf_cat): keep code range if possible.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@45621 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- string.c
+++ string.c
@@ -2021,7 +2021,7 @@ str_buf_cat(VALUE str, const char *ptr, long len)
     if (ptr >= RSTRING_PTR(str) && ptr <= RSTRING_END(str)) {
         off = ptr - RSTRING_PTR(str);
     }
-    rb_str_modify(str);
+    str_modify_keep_cr(str);
     if (len == 0) return 0;
     if (STR_EMBED_P(str)) {
 	capa = RSTRING_EMBED_LEN_MAX;
