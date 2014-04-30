d46cd5cfd6524cdc4a38d7dca9f65a9db0b5b15d
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-04-30 14:53:19 +0000

    missed at r45748.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@45762 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/envutil.rb
+++ test/ruby/envutil.rb
@@ -375,6 +375,29 @@ eom
         assert(1.0/f == -Float::INFINITY, "#{f} is not -0.0")
       end
 
+      def assert_raise_with_message(exception, expected, msg = nil, &block)
+        case expected
+        when String
+          assert = :assert_equal
+        when Regexp
+          assert = :assert_match
+        else
+          raise TypeError, "Expected #{expected.inspect} to be a kind of String or Regexp, not #{expected.class}"
+        end
+
+        ex = assert_raise(exception, *msg) {yield}
+        msg = message(msg, "") {"Expected Exception(#{exception}) was raised, but the message doesn't match"}
+
+        if assert == :assert_equal
+          assert_equal(expected, ex.message, msg)
+        else
+          msg = message(msg) { "Expected #{mu_pp expected} to match #{mu_pp ex.message}" }
+          assert expected =~ ex.message, msg
+          block.binding.eval("proc{|_|$~=_}").call($~)
+        end
+        ex
+      end
+
       def assert_file
         AssertFile
       end
