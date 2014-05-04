1b814b5a742758f561f1f8c216d83fca4f6ce505
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-05-04 15:17:10 +0000

    merge revision(s) r45393: [Backport #9668]
    	* gc.c: Fix up default GC params by @csfrancis [fix GH-556]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@45810 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.2"
-#define RUBY_RELEASE_DATE "2014-05-02"
-#define RUBY_PATCHLEVEL 83
+#define RUBY_RELEASE_DATE "2014-05-05"
+#define RUBY_PATCHLEVEL 84
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 5
-#define RUBY_RELEASE_DAY 2
+#define RUBY_RELEASE_DAY 5
 
 #include "ruby/version.h"
 
