de94db5d61dddfada92dd07a2c1885d79c74e751
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-05-05 16:16:48 +0000

    merge revision(s) r45760: [Backport #9796]
    	* gc.c (gc_after_sweep): suppress unnecessary expanding heap.
    	  Tomb heap pages are freed pages here, so expanding heap is
    	  not required.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@45833 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.2"
-#define RUBY_RELEASE_DATE "2014-05-05"
-#define RUBY_PATCHLEVEL 92
+#define RUBY_RELEASE_DATE "2014-05-06"
+#define RUBY_PATCHLEVEL 93
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 5
-#define RUBY_RELEASE_DAY 5
+#define RUBY_RELEASE_DAY 6
 
 #include "ruby/version.h"
 
