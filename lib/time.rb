1c3f862f589ceea1bcc24fb4fe177997a274d71c
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-05-06 08:27:47 +0000

    [DOC] Use EST for previous documentation fix.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@45838 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/time.rb
+++ lib/time.rb
@@ -297,12 +297,12 @@ class Time
     # values (1 or 0) are assumed if broken or missing.  For example:
     #
     #     # Suppose it is "Thu Nov 29 14:33:20 2001" now and
-    #     # your time zone is EST5EDT (Eastern Standard Time in US):
+    #     # your time zone is EST which is GMT-5.
     #     now = Time.parse("Thu Nov 29 14:33:20 2001")
     #     Time.parse("16:30", now)     #=> 2001-11-29 16:30:00 -0500
-    #     Time.parse("7/23", now)      #=> 2001-07-23 00:00:00 -0400
-    #     Time.parse("Aug 31", now)    #=> 2001-08-31 00:00:00 -0400
-    #     Time.parse("Aug 2000", now)  #=> 2000-08-01 00:00:00 -0400
+    #     Time.parse("7/23", now)      #=> 2001-07-23 00:00:00 -0500
+    #     Time.parse("Aug 31", now)    #=> 2001-08-31 00:00:00 -0500
+    #     Time.parse("Aug 2000", now)  #=> 2000-08-01 00:00:00 -0500
     #
     # Since there are numerous conflicts among locally defined time zone
     # abbreviations all over the world, this method is not intended to
