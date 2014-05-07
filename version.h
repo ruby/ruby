93e234056c4b9dcfa3c5573cc7778d4764e19cd0
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-05-07 16:59:18 +0000

    merge revision(s) 45595: [Backport #9743]
    	* ext/openssl/ossl_pkey.c (ossl_pkey_verify): as EVP_VerifyFinal()
    	  finalizes only a copy of the digest context, the context must be
    	  cleaned up after initialization by EVP_MD_CTX_cleanup() or a
    	  memory leak will occur.  [ruby-core:62038] [Bug #9743]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@45868 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2014-04-30"
-#define RUBY_PATCHLEVEL 477
+#define RUBY_RELEASE_DATE "2014-05-08"
+#define RUBY_PATCHLEVEL 478
 
 #define RUBY_RELEASE_YEAR 2014
-#define RUBY_RELEASE_MONTH 4
-#define RUBY_RELEASE_DAY 30
+#define RUBY_RELEASE_MONTH 5
+#define RUBY_RELEASE_DAY 8
 
 #include "ruby/version.h"
 
