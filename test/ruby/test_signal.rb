4bcc27c98a8c87c77cf0e51dfebf7a828c48974c
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-05-10 14:25:23 +0000

    merge revision(s) r45891,r45893,r45895:
    	test_beginendblock.rb, test_signal.rb: run with default handler
    
    	* test/ruby/test_beginendblock.rb (test_propagate_signaled): run
      with default handler.
    
    	* test/ruby/test_signal.rb (test_hup_me): ditto.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@45905 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_signal.rb
+++ test/ruby/test_signal.rb
@@ -255,9 +255,12 @@ EOS
     # that signal will be deliverd synchronously.
     # This ugly workaround was introduced to don't break
     # compatibility against silly example codes.
+    assert_separately([], <<-RUBY)
+    trap(:HUP, "DEFAULT")
     assert_raise(SignalException) {
       Process.kill('HUP', Process.pid)
     }
+    RUBY
     bug8137 = '[ruby-dev:47182] [Bug #8137]'
     assert_nothing_raised(bug8137) {
       Timeout.timeout(1) {
