75edd5f7a30cf82a365e7270cbb20fee5f158274
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-05-15 15:28:16 +0000

    merge revision(s) r45374: [Backport #8405]
    	* lib/csv.rb: Fixed a broken regular expression that was causing
    	  CSV to miss escaping some special meaning characters when used
    	  in parsing.
    	  Reported by David Unric
    	  [ruby-core:54986] [Bug #8405]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@45956 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/csv.rb
+++ lib/csv.rb
@@ -1507,8 +1507,7 @@ class CSV
     # if we can transcode the needed characters
     #
     @re_esc   =   "\\".encode(@encoding) rescue ""
-    @re_chars =   /#{%"[-][\\.^$?*+{}()|# \r\n\t\f\v]".encode(@encoding)}/
-    # @re_chars =   /#{%"[-][\\.^$?*+{}()|# \r\n\t\f\v]".encode(@encoding, fallback: proc{""})}/
+    @re_chars =   /#{%"[-\\]\\[\\.^$?*+{}()|# \r\n\t\f\v]".encode(@encoding)}/
 
     init_separators(options)
     init_parsers(options)
