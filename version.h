d41b88bbd6d9ddcad69898aa166c82c966298e4c
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-05-18 15:36:58 +0000

    merge revision(s) r45405,r45408: [Backport #9669] [Backport #9740]
    	* parse.y (lex_state_e, parser_params, f_arglist, parser_yylex):
    	  separate EXPR_LABELARG from EXPR_BEG and let newline significant,
    	  so that required keyword argument can place at the end of
    	  argument list without parentheses.  [ruby-core:61658] [Bug #9669]
    
    	* parse.y (parser_yylex): only a newline after label should be
    	  significant.  [ruby-core:61658] [Bug #9669]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@46005 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.2"
-#define RUBY_RELEASE_DATE "2014-05-16"
-#define RUBY_PATCHLEVEL 102
+#define RUBY_RELEASE_DATE "2014-05-19"
+#define RUBY_PATCHLEVEL 103
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 5
-#define RUBY_RELEASE_DAY 16
+#define RUBY_RELEASE_DAY 19
 
 #include "ruby/version.h"
 
