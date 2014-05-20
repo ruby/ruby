3f1f9e9a0aaed4ecbbfd750ad70f879de01ed166
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-05-20 06:39:08 +0000

    show progress of rdoc parsing to prevent timeout on chkbuild
    http://c64b.rubyci.org/~chkbuild/ruby-trunk/log/20140520T030303Z.fail.html.gz
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@46027 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/rdoc/stats/normal.rb
+++ lib/rdoc/stats/normal.rb
@@ -5,15 +5,13 @@
 class RDoc::Stats::Normal < RDoc::Stats::Quiet
 
   def begin_adding # :nodoc:
-    puts "Parsing sources..." if $stdout.tty?
+    puts "Parsing sources..."
   end
 
   ##
   # Prints a file with a progress bar
 
   def print_file files_so_far, filename
-    return unless $stdout.tty?
-
     progress_bar = sprintf("%3d%% [%2d/%2d]  ",
                            100 * files_so_far / @num_files,
                            files_so_far,
@@ -30,18 +28,21 @@ class RDoc::Stats::Normal < RDoc::Stats::Quiet
       filename[0..2] = "..."
     end
 
-    # Pad the line with whitespaces so that leftover output from the
-    # previous line doesn't show up.
     line = "#{progress_bar}#{filename}"
-    padding = terminal_width - line.size
-    line << (" " * padding) if padding > 0
-
-    $stdout.print("#{line}\r")
+    if $stdout.tty?
+      # Pad the line with whitespaces so that leftover output from the
+      # previous line doesn't show up.
+      padding = terminal_width - line.size
+      line << (" " * padding) if padding > 0
+      $stdout.print("#{line}\r")
+    else
+      $stdout.puts(line)
+    end
     $stdout.flush
   end
 
   def done_adding # :nodoc:
-    puts if $stdout.tty?
+    puts
   end
 
 end
