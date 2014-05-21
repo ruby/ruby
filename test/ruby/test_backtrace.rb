abf2980b7a34d73ee93b9f5ee2a3d528df3b851f
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-05-21 06:11:22 +0000

    test_backtrace.rb: r18554 tests
    * test/ruby/test_backtrace.rb (test_core_backtrace_{alias,undef}):
      add tests for r18554.  [ruby-dev:35820] [Bug #416]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@46036 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_backtrace.rb
+++ test/ruby/test_backtrace.rb
@@ -214,4 +214,24 @@ class TestBacktrace < Test::Unit::TestCase
       q << true
     end
   end
+
+  def test_core_backtrace_alias
+    obj = BasicObject.new
+    e = assert_raise(NameError) do
+      class << obj
+        alias foo bar
+      end
+    end
+    assert_not_match(/\Acore#/, e.backtrace_locations[0].base_label)
+  end
+
+  def test_core_backtrace_undef
+    obj = BasicObject.new
+    e = assert_raise(NameError) do
+      class << obj
+        undef foo
+      end
+    end
+    assert_not_match(/\Acore#/, e.backtrace_locations[0].base_label)
+  end
 end
