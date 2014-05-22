4c9ec80ece69b0295f88dcc4262960bc0c298b2a
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-05-22 10:42:06 +0000

    I suspect such early wakeup is because of automatic time correction
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@46043 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_sleep.rb
+++ test/ruby/test_sleep.rb
@@ -4,17 +4,10 @@ require 'etc'
 class TestSleep < Test::Unit::TestCase
   def test_sleep_5sec
     GC.disable
-    start = Time.now
+    start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
     sleep 5
-    slept = Time.now-start
-    bottom =
-      case RUBY_PLATFORM
-      when /linux/
-        4.98 if (Etc.uname[:release].split('.').map(&:to_i)<=>[2,6,18]) <= 0
-      when /mswin|mingw/
-        4.98
-      end
-    bottom ||= 5.0
+    slept = Process.clock_gettime(Process::CLOCK_MONOTONIC) - start
+    bottom = 5.0
     assert_operator(slept, :>=, bottom)
     assert_operator(slept, :<=, 6.0, "[ruby-core:18015]: longer than expected")
   ensure
