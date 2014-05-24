bdf98fc6274b6322d2cdd91d43d8b690e4786d84
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-05-24 06:01:51 +0000

    * test/lib/minitest: Remove comments not appropriate now.
    * test/minitest: Ditto.
    
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@46070 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/lib/minitest/pride.rb
+++ test/lib/minitest/pride.rb
@@ -1,9 +1,4 @@
 # encoding: utf-8
-######################################################################
-# This file is imported from the minitest project.
-# DO NOT make modifications in this repo. They _will_ be reverted!
-# File a patch instead and assign it to Ryan Davis.
-######################################################################
 
 require "minitest/unit"
 
