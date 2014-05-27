f29cf5c1b0a08f3ff89409db4968e34408cccf16
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-05-27 02:48:46 +0000

    merge revision(s) 45534: [Backport #9709]
    	* string.c (str_buf_cat): should round up the capacity by 4KiB,
    	  but not number of rooms.  [ruby-core:61886] [Bug #9709]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@46154 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- string.c
+++ string.c
@@ -1925,7 +1925,7 @@ str_buf_cat(VALUE str, const char *ptr, long len)
     if (capa <= total) {
 	while (total > capa) {
 	    if (capa + 1 >= LONG_MAX / 2) {
-		capa = (total + 4095) / 4096;
+		capa = (total + 4095) / 4096 * 4096;
 		break;
 	    }
 	    capa = (capa + 1) * 2;
