1c9a3e6eb8cca1e61c1f1d4bb4152321abf9ccb5
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-05-27 15:19:53 +0000

    merge revision(s) r45520: [Backport #9706]
    	* ext/date/date_core.c (d_lite_cmp): should compare with #<.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@46185 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.2"
-#define RUBY_RELEASE_DATE "2014-05-23"
-#define RUBY_PATCHLEVEL 106
+#define RUBY_RELEASE_DATE "2014-05-28"
+#define RUBY_PATCHLEVEL 107
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 5
-#define RUBY_RELEASE_DAY 23
+#define RUBY_RELEASE_DAY 28
 
 #include "ruby/version.h"
 
