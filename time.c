282462b206b0d6643f272e1e4a4b45f148688b18
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-06-01 17:17:44 +0000

    merge revision(s) r45364,r45395,r45396,r45403,r45406: [Backport #9652]
    	* time.c (time_mload): freeze and preserve marshal-loaded time zone
    
    	* test/ruby/test_time.rb: add test for GC on loaded object
    	  [Bug #9652]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@46304 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- time.c
+++ time.c
@@ -4805,7 +4805,9 @@ end_submicro: ;
 	time_fixoff(time);
     }
     if (!NIL_P(zone)) {
+	zone = rb_str_new_frozen(zone);
 	tobj->vtm.zone = RSTRING_PTR(zone);
+	rb_ivar_set(time, id_zone, zone);
     }
 
     return time;
