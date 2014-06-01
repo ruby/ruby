2b1813e4bc06cfd34d25fad1a22e6bfc6e4bb4a0
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-06-01 17:17:44 +0000

    merge revision(s) r45364,r45395,r45396,r45403,r45406: [Backport #9652]
    	* time.c (time_mload): freeze and preserve marshal-loaded time zone
    
    	* test/ruby/test_time.rb: add test for GC on loaded object
    	  [Bug #9652]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@46304 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_time.rb
+++ test/ruby/test_time.rb
@@ -311,6 +311,21 @@ class TestTime < Test::Unit::TestCase
     end
   end
 
+  def test_marshal_zone_gc
+    assert_separately(%w(--disable-gems), <<-'end;', timeout: 30)
+      ENV["TZ"] = "JST-9"
+      s = Marshal.dump(Time.now)
+      t = Marshal.load(s)
+      n = 0
+      done = 100000
+      while t.zone.dup == "JST" && n < done
+        n += 1
+      end
+      assert_equal done, n, "Bug #9652"
+      assert_equal "JST", t.zone, "Bug #9652"
+    end;
+  end
+
   def test_marshal_to_s
     t1 = Time.new(2011,11,8, 0,42,25, 9*3600)
     t2 = Time.at(Marshal.load(Marshal.dump(t1)))
