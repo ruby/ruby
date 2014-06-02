8416142f7dfadf9d7885aff0e7c9ac97f028480a
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-06-02 15:48:24 +0000

    merge revision(s) r45302: [Backport #9616]
    	* class.c (rb_class_subclass_add): use xmalloc
    
    	* class.c (rb_module_add_to_subclasses_list): ditto
    
    	* class.c (rb_class_remove_from_super_subclasses): use xfree
    
    	* class.c (rb_class_remove_from_module_subclasses): ditto
    	  [Bug #9616]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@46319 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.2"
-#define RUBY_RELEASE_DATE "2014-06-02"
-#define RUBY_PATCHLEVEL 122
+#define RUBY_RELEASE_DATE "2014-06-03"
+#define RUBY_PATCHLEVEL 123
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 6
-#define RUBY_RELEASE_DAY 2
+#define RUBY_RELEASE_DAY 3
 
 #include "ruby/version.h"
 
