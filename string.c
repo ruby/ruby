d07a013cf92c866f08433db017a2ffe0615daadd
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-06-12 04:28:16 +0000

    string.c: shrink before freezing
    * string.c (rb_str_freeze): shrink the buffer before freezing, as
      pointed out by Eric Wong at [ruby-core:63119].
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@46411 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- string.c
+++ string.c
@@ -1905,6 +1905,8 @@ rb_str_substr(VALUE str, long beg, long len)
 VALUE
 rb_str_freeze(VALUE str)
 {
+    if (OBJ_FROZEN(str)) return str;
+    rb_str_resize(str, RSTRING_LEN(str));
     return rb_obj_freeze(str);
 }
 
