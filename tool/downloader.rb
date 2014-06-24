204a83f5f3a3167815a1439e7e6652a7eec934f2
hsbt <hsbt@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-06-24 01:26:21 +0000

    * tool/downloader.rb: make Downloader class to general download utility.   It can be used without config.guess and configu.sub. * tool/get-config_files: ditto. * tool/make-snapshot: ditto.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@46535 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/downloader.rb
+++ tool/downloader.rb
@@ -1,11 +1,11 @@
 require 'open-uri'
 
-Downloader = "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=%s;hb=HEAD"
-def Downloader.download(name, dir = nil)
-  uri = URI(self % name)
-  data = uri.read
-  file = dir ? File.join(dir, name) : name
-  open(file, "wb", 0755) {|f| f.write(data)}
-rescue => e
-  raise "failed to download #{name}\n#{e.message}: #{uri}"
+class Downloader
+  def self.download(url, name, dir = nil)
+    data = URI(url).read
+    file = dir ? File.join(dir, name) : name
+    open(file, "wb", 0755) {|f| f.write(data)}
+  rescue => e
+    raise "failed to download #{name}\n#{e.message}: #{uri}"
+  end
 end
