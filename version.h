2fcf1bb6736ae0c34239abc43d01826924988de7
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-06-27 07:57:24 +0000

    merge revision(s) 46194: [Backport #9835]
    	* signal.c (signal_exec): ignore immediate cmd for SIG_IGN
    
    	* signal.c (trap_handler): set cmd to true for SIG_IGN
    
    	* signal.c (trap): handle nil and true values for oldcmd
    	  [Bug #9835]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@46575 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2014-06-23"
-#define RUBY_PATCHLEVEL 499
+#define RUBY_RELEASE_DATE "2014-06-27"
+#define RUBY_PATCHLEVEL 500
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 6
-#define RUBY_RELEASE_DAY 23
+#define RUBY_RELEASE_DAY 27
 
 #include "ruby/version.h"
 
