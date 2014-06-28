bf18eb2a02243e213afb71674f0ac0b1191540df
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-06-28 05:51:57 +0000

    merge revision(s) r45845,r45846,r45847: [Backport #9486]
    	* parse.y (local_tbl_gen): remove local variables duplicated with
    	  arguments.
    
    	* parse.y (new_bv_gen): no duplicated names, if already added in
    	  shadowing_lvar().
    	  [ruby-core:60501] [Bug #9486]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@46596 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.2"
-#define RUBY_RELEASE_DATE "2014-06-24"
-#define RUBY_PATCHLEVEL 142
+#define RUBY_RELEASE_DATE "2014-06-28"
+#define RUBY_PATCHLEVEL 143
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 6
-#define RUBY_RELEASE_DAY 24
+#define RUBY_RELEASE_DAY 28
 
 #include "ruby/version.h"
 
