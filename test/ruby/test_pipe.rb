428a637f0f7feffd33fed9a984844c6b0d167758
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-06-30 18:12:05 +0000

    merge revision(s) r46360,r46372: [Backport #8625]
    	* io.c (io_setstrbuf, io_read): should not shorten the given buffer until
    	  read succeeds.  [ruby-core:55951] [Bug #8625]
    
    	* io.c (read_all): truncate the buffer before appending read data,
    	  instead of truncating before reading.
    	  [ruby-core:55951] [Bug #8625]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@46629 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_pipe.rb
+++ test/ruby/test_pipe.rb
@@ -13,4 +13,17 @@ class TestPipe < Test::Unit::TestCase
       r.close
     end
   end
+  class WithConversion < self
+    def open_file(content)
+      r, w = IO.pipe
+      w << content
+      w.close
+      r.set_encoding("us-ascii:utf-8")
+      begin
+        yield r
+      ensure
+        r.close
+      end
+    end
+  end
 end
