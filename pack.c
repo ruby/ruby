5acdbee3eb1037b97ac520469e9d1d933b24d8cc
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-07-13 13:59:09 +0000

    merge revision(s) r46778: [Backport #10019]
    	* pack.c (encodes): fix buffer overrun by tail_lf.  Thanks to
    	  Mamoru Tasaka and Tomas Hoger.  [ruby-core:63604] [Bug #10019]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@46806 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- pack.c
+++ pack.c
@@ -946,7 +946,8 @@ static const char b64_table[] =
 static void
 encodes(VALUE str, const char *s, long len, int type, int tail_lf)
 {
-    char buff[4096];
+    enum {buff_size = 4096, encoded_unit = 4};
+    char buff[buff_size + 1];	/* +1 for tail_lf */
     long i = 0;
     const char *trans = type == 'u' ? uu_table : b64_table;
     char padding;
@@ -959,7 +960,7 @@ encodes(VALUE str, const char *s, long len, int type, int tail_lf)
 	padding = '=';
     }
     while (len >= 3) {
-        while (len >= 3 && sizeof(buff)-i >= 4) {
+        while (len >= 3 && buff_size-i >= encoded_unit) {
             buff[i++] = trans[077 & (*s >> 2)];
             buff[i++] = trans[077 & (((*s << 4) & 060) | ((s[1] >> 4) & 017))];
             buff[i++] = trans[077 & (((s[1] << 2) & 074) | ((s[2] >> 6) & 03))];
@@ -967,7 +968,7 @@ encodes(VALUE str, const char *s, long len, int type, int tail_lf)
             s += 3;
             len -= 3;
         }
-        if (sizeof(buff)-i < 4) {
+        if (buff_size-i < encoded_unit) {
             rb_str_buf_cat(str, buff, i);
             i = 0;
         }
@@ -987,6 +988,7 @@ encodes(VALUE str, const char *s, long len, int type, int tail_lf)
     }
     if (tail_lf) buff[i++] = '\n';
     rb_str_buf_cat(str, buff, i);
+    if ((size_t)i > sizeof(buff)) rb_bug("encodes() buffer overrun");
 }
 
 static const char hex_table[] = "0123456789ABCDEF";
