86b38466dfdf224323df707611c8ae1d199bee8a
hsbt <hsbt@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-07-22 19:06:12 +0000

    * lib/drb/extserv.rb: remove duplicate code with sample direcotry.   contributed from @vipulnsward. [fix GH-679]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@46901 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/drb/extserv.rb
+++ lib/drb/extserv.rb
@@ -41,33 +41,3 @@ module DRb
     end
   end
 end
-
-# :stopdoc:
-
-if __FILE__ == $0
-  class Foo
-    include DRbUndumped
-
-    def initialize(str)
-      @str = str
-    end
-
-    def hello(it)
-      "#{it}: #{self}"
-    end
-
-    def to_s
-      @str
-    end
-  end
-
-  cmd = ARGV.shift
-  case cmd
-  when 'itest1', 'itest2'
-    front = Foo.new(cmd)
-    manager = DRb::DRbServer.new(nil, front)
-    es = DRb::ExtServ.new(ARGV.shift, ARGV.shift, manager)
-    es.server.thread.join
-  end
-end
-
