8fded5ba3e464277eb5bce0717a168ae53f3d0f6
hsbt <hsbt@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-07-23 19:45:09 +0000

    * lib/benchmark.rb: split executable code into sample directory. * sample/benchmark.rb: ditto.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@46922 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/benchmark.rb
+++ lib/benchmark.rb
@@ -546,23 +546,3 @@ module Benchmark
   # The default format string used to display times.  See also Benchmark::Tms#format.
   FORMAT = Benchmark::Tms::FORMAT
 end
-
-if __FILE__ == $0
-  include Benchmark
-
-  n = ARGV[0].to_i.nonzero? || 50000
-  puts %Q([#{n} times iterations of `a = "1"'])
-  benchmark(CAPTION, 7, FORMAT) do |x|
-    x.report("for:")   {for _ in 1..n; _ = "1"; end} # Benchmark.measure
-    x.report("times:") {n.times do   ; _ = "1"; end}
-    x.report("upto:")  {1.upto(n) do ; _ = "1"; end}
-  end
-
-  benchmark do
-    [
-      measure{for _ in 1..n; _ = "1"; end},  # Benchmark.measure
-      measure{n.times do   ; _ = "1"; end},
-      measure{1.upto(n) do ; _ = "1"; end}
-    ]
-  end
-end
