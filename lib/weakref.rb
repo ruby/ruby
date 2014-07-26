25926315eaf1293cea0c57edd055dab3f1a17a57
hsbt <hsbt@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-07-26 17:12:11 +0000

    * lib/weakref.rb: split executable code into sample directory. * sample/weakref.rb: ditto.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@46967 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/weakref.rb
+++ lib/weakref.rb
@@ -104,14 +104,3 @@ class WeakRef < Delegator
     @@__map.key?(self) or defined?(@delegate_sd_obj)
   end
 end
-
-if __FILE__ == $0
-#  require 'thread'
-  foo = Object.new
-  p foo.to_s                    # original's class
-  foo = WeakRef.new(foo)
-  p foo.to_s                    # should be same class
-  ObjectSpace.garbage_collect
-  ObjectSpace.garbage_collect
-  p foo.to_s                    # should raise exception (recycled)
-end
