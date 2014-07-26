22e09d1e22901e45bf65b652a54983af8ad69637
zzak <zzak@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-07-26 20:13:03 +0000

    * lib/irb.rb: [DOC] PROMPT_I cannot be nil, patch by @hgillane [ci skip]   Fixes documenting-ruby/ruby#37   https://github.com/documenting-ruby/ruby/pull/37
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@46970 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/irb.rb
+++ lib/irb.rb
@@ -139,7 +139,7 @@ STDOUT.sync = true
 #
 #     IRB.conf[:PROMPT][:MY_PROMPT] = { # name of prompt mode
 #       :AUTO_INDENT => true,           # enables auto-indent mode
-#       :PROMPT_I => nil,		# normal prompt
+#       :PROMPT_I =>  ">> ",		# simple prompt
 #       :PROMPT_S => nil,		# prompt for continuated strings
 #       :PROMPT_C => nil,		# prompt for continuated statement
 #       :RETURN => "    ==>%s\n"	# format to return value
