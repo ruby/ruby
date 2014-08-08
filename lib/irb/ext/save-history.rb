8f30e944bee937a3d004ca37e26488efb918021d
Laurent Arnoud <laurent@spkdev.net>
2014-08-08 17:01:54 +0200

    lib/irb/ext/save-history.rb: fix ident

--- lib/irb/ext/save-history.rb
+++ lib/irb/ext/save-history.rb
@@ -18,7 +18,7 @@ module IRB
   class Context
     def init_save_history# :nodoc:
       unless (class<<@io;self;end).include?(HistorySavingAbility)
-	@io.extend(HistorySavingAbility)
+        @io.extend(HistorySavingAbility)
       end
     end
 
@@ -39,9 +39,9 @@ module IRB
     def save_history=(val)
       IRB.conf[:SAVE_HISTORY] = val
       if val
-	main_context = IRB.conf[:MAIN_CONTEXT]
-	main_context = self unless main_context
-	main_context.init_save_history
+        main_context = IRB.conf[:MAIN_CONTEXT]
+        main_context = self unless main_context
+        main_context.init_save_history
       end
     end
 
@@ -83,37 +83,37 @@ module IRB
 
     def load_history
       if history_file = IRB.conf[:HISTORY_FILE]
-	history_file = File.expand_path(history_file)
+        history_file = File.expand_path(history_file)
       end
       history_file = IRB.rc_file("_history") unless history_file
       if File.exist?(history_file)
-	open(history_file) do |f|
-	  f.each {|l| HISTORY << l.chomp}
-	end
+        open(history_file) do |f|
+          f.each {|l| HISTORY << l.chomp}
+        end
       end
     end
 
     def save_history
       if num = IRB.conf[:SAVE_HISTORY] and (num = num.to_i) > 0
-	if history_file = IRB.conf[:HISTORY_FILE]
-	  history_file = File.expand_path(history_file)
-	end
-	history_file = IRB.rc_file("_history") unless history_file
+        if history_file = IRB.conf[:HISTORY_FILE]
+          history_file = File.expand_path(history_file)
+        end
+        history_file = IRB.rc_file("_history") unless history_file
 
-	# Change the permission of a file that already exists[BUG #7694]
-	begin
-	  if File.stat(history_file).mode & 066 != 0
-	    File.chmod(0600, history_file)
-	  end
-	rescue Errno::ENOENT
-	rescue
-	  raise
-	end
+        # Change the permission of a file that already exists[BUG #7694]
+        begin
+          if File.stat(history_file).mode & 066 != 0
+            File.chmod(0600, history_file)
+          end
+        rescue Errno::ENOENT
+        rescue
+          raise
+        end
 
-	open(history_file, 'w', 0600 ) do |f|
-	  hist = HISTORY.to_a
-	  f.puts(hist[-num..-1] || hist)
-	end
+        open(history_file, 'w', 0600 ) do |f|
+          hist = HISTORY.to_a
+          f.puts(hist[-num..-1] || hist)
+        end
       end
     end
   end
