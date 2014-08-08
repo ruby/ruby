13c2abad81b4ef7af70bc3c09ac8e85773a185b7
Laurent Arnoud <laurent@spkdev.net>
2014-08-08 16:52:32 +0200

    lib/irb/cmd/nop.rb: fix ident

--- lib/irb/cmd/nop.rb
+++ lib/irb/cmd/nop.rb
@@ -16,22 +16,22 @@ module IRB
       @RCS_ID='-$Id$-'
 
       def self.execute(conf, *opts)
-	command = new(conf)
-	command.execute(*opts)
+        command = new(conf)
+        command.execute(*opts)
       end
 
       def initialize(conf)
-	@irb_context = conf
+        @irb_context = conf
       end
 
       attr_reader :irb_context
 
       def irb
-	@irb_context.irb
+        @irb_context.irb
       end
 
       def execute(*opts)
-	#nop
+        #nop
       end
     end
   end
