59ca946543058a68ae94405061c0c1be6275a84a
hsbt <hsbt@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-08-21 04:23:56 +0000

    * lib/rinda/ring.rb: split executable code into sample directory. * sample/rinda-ring.rb: ditto.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@47236 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/rinda/ring.rb
+++ lib/rinda/ring.rb
@@ -478,27 +478,3 @@ module Rinda
   end
 
 end
-
-if __FILE__ == $0
-  DRb.start_service
-  case ARGV.shift
-  when 's'
-    require 'rinda/tuplespace'
-    ts = Rinda::TupleSpace.new
-    Rinda::RingServer.new(ts)
-    $stdin.gets
-  when 'w'
-    finger = Rinda::RingFinger.new(nil)
-    finger.lookup_ring do |ts2|
-      p ts2
-      ts2.write([:hello, :world])
-    end
-  when 'r'
-    finger = Rinda::RingFinger.new(nil)
-    finger.lookup_ring do |ts2|
-      p ts2
-      p ts2.take([nil, nil])
-    end
-  end
-end
-
