738e647a61ac3bd8b11b069f0a25f190f0037fdd
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-08-30 21:06:58 +0900

    imported assets files

--- test/lib/simplecov-html.rb
+++ test/lib/simplecov-html.rb
@@ -6,7 +6,7 @@ require 'time'
 
 class SimpleCov::Formatter::HTMLFormatter
   def format(result)
-    Dir[File.join(File.dirname(__FILE__), '../public/*')].each do |path|
+    Dir[File.join(File.dirname(__FILE__), 'simplecov-html/public/*')].each do |path|
       FileUtils.cp_r(path, asset_output_path)
     end
 
@@ -24,7 +24,7 @@ class SimpleCov::Formatter::HTMLFormatter
 
   # Returns the an erb instance for the template of given name
   def template(name)
-    ERB.new(File.read(File.join(File.dirname(__FILE__), './simplecov-html/views/', "#{name}.erb")))
+    ERB.new(File.read(File.join(File.dirname(__FILE__), 'simplecov-html/views/', "#{name}.erb")))
   end
 
   def output_path
