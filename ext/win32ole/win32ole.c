1cb08e96046b30deebd8cd26a603a89d994a54bc
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-08-30 16:09:10 +0000

    merge revision(s) r47153: [Backport #10127]
    	* ext/win32ole/win32ole.c (ole_create_dcom): use the converted
    	  result if the argument can be converted to a string, to get rid
    	  of invalid access. Thanks to nobu. [ruby-dev:48467] [Bug #10127]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@47325 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/win32ole/win32ole.c
+++ ext/win32ole/win32ole.c
@@ -361,7 +361,7 @@ static VALUE typelib_file_from_typelib(VALUE ole);
 static VALUE typelib_file(VALUE ole);
 static void ole_const_load(ITypeLib *pTypeLib, VALUE klass, VALUE self);
 static HRESULT clsid_from_remote(VALUE host, VALUE com, CLSID *pclsid);
-static VALUE ole_create_dcom(int argc, VALUE *argv, VALUE self);
+static VALUE ole_create_dcom(VALUE self, VALUE ole, VALUE host, VALUE others);
 static VALUE ole_bind_obj(VALUE moniker, int argc, VALUE *argv, VALUE self);
 static VALUE fole_s_connect(int argc, VALUE *argv, VALUE self);
 static VALUE fole_s_const_load(int argc, VALUE *argv, VALUE self);
@@ -2636,9 +2636,8 @@ clsid_from_remote(VALUE host, VALUE com, CLSID *pclsid)
 }
 
 static VALUE
-ole_create_dcom(int argc, VALUE *argv, VALUE self)
+ole_create_dcom(VALUE self, VALUE ole, VALUE host, VALUE others)
 {
-    VALUE ole, host, others;
     HRESULT hr;
     CLSID   clsid;
     OLECHAR *pbuf;
@@ -2656,7 +2655,6 @@ ole_create_dcom(int argc, VALUE *argv, VALUE self)
             GetProcAddress(gole32, "CoCreateInstanceEx");
     if (!gCoCreateInstanceEx)
         rb_raise(rb_eRuntimeError, "CoCreateInstanceEx is not supported in this environment");
-    rb_scan_args(argc, argv, "2*", &ole, &host, &others);
 
     pbuf  = ole_vstr2wc(ole);
     hr = CLSIDFromProgID(pbuf, &clsid);
@@ -3270,7 +3268,7 @@ fole_initialize(int argc, VALUE *argv, VALUE self)
             rb_raise(rb_eSecurityError, "Insecure Object Creation - %s",
                      StringValuePtr(svr_name));
         }
-        return ole_create_dcom(argc, argv, self);
+        return ole_create_dcom(self, svr_name, host, others);
     }
 
     /* get CLSID from OLE server name */
