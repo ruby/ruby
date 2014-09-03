9e95cee04401f01917fa0e0adcb82a9ddf7ef36e
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-09-03 15:21:34 +0000

    merge revision(s) r47362: [Backport #9984]
    	* test/openssl/test_pkey_rsa.rb (OpenSSL#test_sign_verify_memory_leak):
    	  added timeout into testcase for low performance environment.
    	  [Bug #9984][ruby-core:63367]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@47375 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.2"
-#define RUBY_RELEASE_DATE "2014-09-02"
-#define RUBY_PATCHLEVEL 218
+#define RUBY_RELEASE_DATE "2014-09-04"
+#define RUBY_PATCHLEVEL 219
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 9
-#define RUBY_RELEASE_DAY 2
+#define RUBY_RELEASE_DAY 4
 
 #include "ruby/version.h"
 
