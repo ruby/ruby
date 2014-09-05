bf09511754d63e7018066962107014a905a1f9b5
hsbt <hsbt@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-09-05 01:41:28 +0000

    * test/rdoc/test_rdoc_rdoc.rb (TestRDocRDoc#test_parse_file_encoding):   typofix.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@47392 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/rdoc/test_rdoc_rdoc.rb
+++ test/rdoc/test_rdoc_rdoc.rb
@@ -261,7 +261,7 @@ class TestRDocRDoc < RDoc::TestCase
       assert_equal Encoding::ISO_8859_1, top_level.absolute_name.encoding
       io
     end
-    tf.close! if tf.respond_to? :close?
+    tf.close! if tf.respond_to? :close!
   end
 
   def test_parse_file_forbidden
