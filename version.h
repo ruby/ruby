7fdb955c36db70a17fbd46ad1f9c5f3d156682b2
zzak <zzak@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-09-06 00:12:24 +0000

    Backport r47425
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@47428 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2014-09-05"
-#define RUBY_PATCHLEVEL 555
+#define RUBY_RELEASE_DATE "2014-09-06"
+#define RUBY_PATCHLEVEL 556
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 9
-#define RUBY_RELEASE_DAY 5
+#define RUBY_RELEASE_DAY 6
 
 #include "ruby/version.h"
 
