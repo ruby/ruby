b8eec6b6915e87f341bbb7166eda72694f47f12d
normal <normal@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-09-11 08:09:07 +0000

    lib/benchmark.rb: remove CLOCK_MONOTONIC_RAW support
    In addition to being unaffected by _offset_ correction,
    CLOCK_MONOTONIC_RAW is also unaffected by _frequency_ correction,
    making it unsuitable for measuring real time on systems where the
    clock is always running too fast or slow.
    
    CLOCK_MONOTONIC (without _RAW) is the correct clock, as it is unaffected
    by _offset_ correction (due to human error or battery replacement), but
    still takes _frequency_ correction into account for clocks which
    consistently run too fast or slow.
    
    Thanks to Vít Ondruch for reporting the issue on ARM [Bug #10202]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@47526 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/benchmark.rb
+++ lib/benchmark.rb
@@ -272,9 +272,6 @@ module Benchmark
 
   # :stopdoc:
   case
-  when (defined?(Process::CLOCK_MONOTONIC_RAW) and
-      (Process.clock_gettime(Process::CLOCK_MONOTONIC_RAW) rescue false))
-    BENCHMARK_CLOCK = Process::CLOCK_MONOTONIC_RAW
   when defined?(Process::CLOCK_MONOTONIC)
     BENCHMARK_CLOCK = Process::CLOCK_MONOTONIC
   else
