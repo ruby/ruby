0016d87a3fbd5999609d160e84a9544b5944ddcc
Laurent Arnoud <laurent@spkdev.net>
2014-09-13 21:56:53 +0200

    fiddle/test_import.rb: added test for long long
    for fix https://github.com/ruby/ruby/pull/714

--- test/fiddle/test_import.rb
+++ test/fiddle/test_import.rb
@@ -63,6 +63,7 @@ module Fiddle
       assert_equal(SIZEOF_VOIDP, LIBC.sizeof("FILE*"))
       assert_equal(LIBC::MyStruct.size(), LIBC.sizeof(LIBC::MyStruct))
       assert_equal(LIBC::MyStruct.size(), LIBC.sizeof(LIBC::MyStruct.malloc()))
+      assert_equal(SIZEOF_LONG_LONG, LIBC.sizeof("long long"))
     end
 
     def test_unsigned_result()
