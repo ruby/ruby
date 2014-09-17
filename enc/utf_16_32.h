106075eac89ede7bcd71616ba78f1344892140a4
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-09-17 14:53:08 +0000

    Re-apply r43023 which is accidentally reverted by r47598
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@47615 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- enc/utf_16_32.h
+++ enc/utf_16_32.h
@@ -1,5 +1,5 @@
 #include "regenc.h"
 /* dummy for unsupported, statefull encoding */
-#define ENC_DUMMY_UNICODE(name) ENC_DUMMY(name)
+#define ENC_DUMMY_UNICODE(name) ENC_REPLICATE(name, name "BE")
 ENC_DUMMY_UNICODE("UTF-16");
 ENC_DUMMY_UNICODE("UTF-32");
