9a0a3f766ac6c624c8e03acde48243a477968dc8
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-09-18 16:55:05 +0000

    * version.h (RUBY_VERSION): bump RUBY_VERSION to 2.1.3.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@47629 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
-#define RUBY_VERSION "2.1.2"
-#define RUBY_RELEASE_DATE "2014-09-15"
-#define RUBY_PATCHLEVEL 241
+#define RUBY_VERSION "2.1.3"
+#define RUBY_RELEASE_DATE "2014-09-19"
+#define RUBY_PATCHLEVEL 242
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 9
-#define RUBY_RELEASE_DAY 15
+#define RUBY_RELEASE_DAY 19
 
 #include "ruby/version.h"
 
