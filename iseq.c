39fd4a87e8d745cdb76fd3573a9c59e82e7a7ece
normal <normal@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-09-21 03:50:23 +0000

    iseq.c (rb_iseq_defined_string): trim redundant semi-colon
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@47666 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- iseq.c
+++ iseq.c
@@ -2063,7 +2063,7 @@ rb_iseq_defined_string(enum defined_type type)
     }
     str = defs[type-1];
     if (!str) {
-	str = rb_str_new_cstr(estr);;
+	str = rb_str_new_cstr(estr);
 	OBJ_FREEZE(str);
 	defs[type-1] = str;
 	rb_gc_register_mark_object(str);
