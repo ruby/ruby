77ce45dbe435124df3a6f496caa74be8c3a47bf7
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-09-23 17:07:16 +0000

    merge revision(s) r47111,r47212,r47451,r47452,r47680: [Backport #10245]
    	* test/minitest/test_minitest_unit.rb: removed obsoleted condition
    	  for Ruby 1.8.
    
    	* test/ruby/test_time_tz.rb: ditto.
    
    	* test/ruby/test_time_tz.rb: Fix test error with tzdata-2014g.
    	  [ruby-core:65058] [Bug #10245] Reported by Vit Ondruch.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@47700 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/minitest/test_minitest_unit.rb
+++ test/minitest/test_minitest_unit.rb
@@ -197,11 +197,7 @@ class TestMiniTestUnit < MetaMetaMetaTestCase
   end
 
   def util_expand_bt bt
-    if RUBY_VERSION >= '1.9.0' then
-      bt.map { |f| (f =~ /^\./) ? File.expand_path(f) : f }
-    else
-      bt
-    end
+    bt.map { |f| (f =~ /^\./) ? File.expand_path(f) : f }
   end
 end
 
