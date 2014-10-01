0fa956d7ca6968d9be23ac4577e673dce302c25d
Vais Salikhov <vsalikhov@gmail.com>
2014-10-01 21:30:51 +0700

    Corrections for PR comments: reset the flag after test and extract repetitions

--- test/webrick/test_do_not_reverse_lookup.rb
+++ test/webrick/test_do_not_reverse_lookup.rb
@@ -9,6 +9,22 @@ class TestDoNotReverseLookup < Test::Unit::TestCase
     end
   end
 
+  @@original_do_not_reverse_lookup_value = Socket.do_not_reverse_lookup
+
+  def teardown
+    Socket.do_not_reverse_lookup = @@original_do_not_reverse_lookup_value
+  end
+
+  def do_not_reverse_lookup?(config)
+    result = nil
+    TestWEBrick.start_server(DNRL, config) do |server, addr, port, log|
+      TCPSocket.open(addr, port) do |sock|
+        result = {'true' => true, 'false' => false}[sock.gets]
+      end
+    end
+    result
+  end
+
   # +--------------------------------------------------------------------------+
   # |        Expected interaction between Socket.do_not_reverse_lookup         |
   # |            and WEBrick::Config::General[:DoNotReverseLookup]             |
@@ -24,61 +40,31 @@ class TestDoNotReverseLookup < Test::Unit::TestCase
 
   def test_socket_dnrl_true_server_dnrl_true
     Socket.do_not_reverse_lookup = true
-    config = {:DoNotReverseLookup => true}
-    TestWEBrick.start_server(DNRL, config) do |server, addr, port, log|
-      TCPSocket.open(addr, port) do |sock|
-        assert_equal('true', sock.gets, log.call)
-      end
-    end
+    assert_equal(true, do_not_reverse_lookup?(:DoNotReverseLookup => true))
   end
 
   def test_socket_dnrl_true_server_dnrl_false
     Socket.do_not_reverse_lookup = true
-    config = {:DoNotReverseLookup => false}
-    TestWEBrick.start_server(DNRL, config) do |server, addr, port, log|
-      TCPSocket.open(addr, port) do |sock|
-        assert_equal('false', sock.gets, log.call)
-      end
-    end
+    assert_equal(false, do_not_reverse_lookup?(:DoNotReverseLookup => false))
   end
 
   def test_socket_dnrl_true_server_dnrl_nil
     Socket.do_not_reverse_lookup = true
-    config = {:DoNotReverseLookup => nil}
-    TestWEBrick.start_server(DNRL, config) do |server, addr, port, log|
-      TCPSocket.open(addr, port) do |sock|
-        assert_equal('true', sock.gets, log.call)
-      end
-    end
+    assert_equal(true, do_not_reverse_lookup?(:DoNotReverseLookup => nil))
   end
 
   def test_socket_dnrl_false_server_dnrl_true
     Socket.do_not_reverse_lookup = false
-    config = {:DoNotReverseLookup => true}
-    TestWEBrick.start_server(DNRL, config) do |server, addr, port, log|
-      TCPSocket.open(addr, port) do |sock|
-        assert_equal('true', sock.gets, log.call)
-      end
-    end
+    assert_equal(true, do_not_reverse_lookup?(:DoNotReverseLookup => true))
   end
 
   def test_socket_dnrl_false_server_dnrl_false
     Socket.do_not_reverse_lookup = false
-    config = {:DoNotReverseLookup => false}
-    TestWEBrick.start_server(DNRL, config) do |server, addr, port, log|
-      TCPSocket.open(addr, port) do |sock|
-        assert_equal('false', sock.gets, log.call)
-      end
-    end
+    assert_equal(false, do_not_reverse_lookup?(:DoNotReverseLookup => false))
   end
 
   def test_socket_dnrl_false_server_dnrl_nil
     Socket.do_not_reverse_lookup = false
-    config = {:DoNotReverseLookup => nil}
-    TestWEBrick.start_server(DNRL, config) do |server, addr, port, log|
-      TCPSocket.open(addr, port) do |sock|
-        assert_equal('false', sock.gets, log.call)
-      end
-    end
+    assert_equal(false, do_not_reverse_lookup?(:DoNotReverseLookup => nil))
   end
 end
