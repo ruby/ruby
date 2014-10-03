66b6179ec16788c7fb451d40071f30933c02752e
hsbt <hsbt@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-10-03 09:52:21 +0000

    * test/ruby/test_io.rb (TestIO#test_advise): added workaround of fadvise(2)   with tmpfs and old linux kernel. [ruby-core:65355][Bug #10313]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@47771 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_io.rb
+++ test/ruby/test_io.rb
@@ -2486,7 +2486,15 @@ End
         [[0,0], [0, 20], [400, 2]].each do |offset, len|
           open(tf.path) do |t|
             ret = assert_nothing_raised(lambda { os_and_fs(tf.path) }) {
-              t.advise(adv, offset, len)
+              begin
+                t.advise(adv, offset, len)
+              rescue Errno::EINVAL
+                if /linux/ =~ RUBY_PLATFORM && (Etc.uname[:release].split('.').map(&:to_i) <=> [3,6]) < 0
+                  next # [ruby-core:65355] tmpfs is not supported
+                else
+                  retry
+                end
+              end
             }
             assert_nil(ret)
             assert_raise(ArgumentError, "superfluous arguments") do
