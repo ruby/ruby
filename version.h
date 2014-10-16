4110e5fa6213995e6c8561527e4c658e406405e9
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-10-16 07:23:46 +0000

    merge revision(s) 46783: [Backport #6716]
    	* lib/fileutils.rb: handle ENOENT error with symlink targeted to
    	  non-exists file. [ruby-dev:45933] [Bug #6716]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@47974 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2014-10-15"
-#define RUBY_PATCHLEVEL 585
+#define RUBY_RELEASE_DATE "2014-10-16"
+#define RUBY_PATCHLEVEL 586
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 10
-#define RUBY_RELEASE_DAY 15
+#define RUBY_RELEASE_DAY 16
 
 #include "ruby/version.h"
 
