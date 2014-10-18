3ec2fe7a21f3f4a69d6d7cd6dd6739f5d6d8daeb
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-10-18 18:21:39 +0000

    merge revision(s) r47973: [Backport #10392]
    	* parse.y (parser_here_document): do not append already appended
    	  and disposed code fragment.  [ruby-dev:48647] [Bug #10392]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@48003 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.4"
-#define RUBY_RELEASE_DATE "2014-10-16"
-#define RUBY_PATCHLEVEL 259
+#define RUBY_RELEASE_DATE "2014-10-19"
+#define RUBY_PATCHLEVEL 260
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 10
-#define RUBY_RELEASE_DAY 16
+#define RUBY_RELEASE_DAY 19
 
 #include "ruby/version.h"
 
