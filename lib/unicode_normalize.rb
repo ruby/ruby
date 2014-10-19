b95ec57da2bfba783b299c593c5654e932c29992
duerst <duerst@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-10-19 01:08:13 +0000

    lib/unicode_normalize.rb: Changing require statement, adjusting copyright.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@48012 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/unicode_normalize.rb
+++ lib/unicode_normalize.rb
@@ -1,10 +1,8 @@
 ﻿# coding: utf-8
 
-# Copyright 2010-2013 Ayumu Nojima (野島 歩) and Martin J. Dürst (duerst@it.aoyama.ac.jp)
-# available under the same licence as Ruby itself
-# (see http://www.ruby-lang.org/en/LICENSE.txt)
+# Copyright Ayumu Nojima (野島 歩) and Martin J. Dürst (duerst@it.aoyama.ac.jp)
 
-require File.dirname(__FILE__) + '/normalize'
+require 'unicode_normalize/normalize.rb'
 
 class String
   def normalize(form = :nfc)
