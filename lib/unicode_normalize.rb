e4f71908d62d7e55a55888ad00dce25eb66a1dea
duerst <duerst@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-10-19 01:04:10 +0000

    lib/unicode_normalize.rb: Importing from https://github.com/duerst/eprun/blob/master/lib/string_normalize.rb. (removing trailing whitespace, fixing EOLs and adding EOL property)
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@48011 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ lib/unicode_normalize.rb
@@ -0,0 +1,22 @@
+﻿# coding: utf-8
+
+# Copyright 2010-2013 Ayumu Nojima (野島 歩) and Martin J. Dürst (duerst@it.aoyama.ac.jp)
+# available under the same licence as Ruby itself
+# (see http://www.ruby-lang.org/en/LICENSE.txt)
+
+require File.dirname(__FILE__) + '/normalize'
+
+class String
+  def normalize(form = :nfc)
+    Normalize.normalize(self, form)
+  end
+
+  def normalize!(form = :nfc)
+    replace(self.normalize(form))
+  end
+
+  def normalized?(form = :nfc)
+    Normalize.normalized?(self, form)
+  end
+end
+
