a7a045f1f049cf4252a0b855c738dbff6968375a
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-10-25 18:29:00 +0000

    merge revision(s) r48102: [Backport #10413]
    	* class.c (unknown_keyword_error): delete expected keywords
    	  directly from raw table, so that the given block is not called.
    	  [ruby-core:65837] [Bug #10413]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@48137 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.4"
-#define RUBY_RELEASE_DATE "2014-10-22"
-#define RUBY_PATCHLEVEL 262
+#define RUBY_RELEASE_DATE "2014-10-26"
+#define RUBY_PATCHLEVEL 263
 
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 10
-#define RUBY_RELEASE_DAY 22
+#define RUBY_RELEASE_DAY 26
 
 #include "ruby/version.h"
 
