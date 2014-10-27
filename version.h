8dab31ed2281946e58aac67a2764d2bb4f1310d5
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-10-27 11:24:04 +0000

    merge revision(s) 48161:
    	* lib/rexml/entity.rb: keep the entity size within the limitation.
    	  reported by Willis Vandevanter <will@silentrobots.com> and
    	  patched by nahi.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_1_9_3@48164 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "1.9.3"
-#define RUBY_PATCHLEVEL 549
+#define RUBY_PATCHLEVEL 550
 
-#define RUBY_RELEASE_DATE "2014-10-24"
+#define RUBY_RELEASE_DATE "2014-10-27"
 #define RUBY_RELEASE_YEAR 2014
 #define RUBY_RELEASE_MONTH 10
-#define RUBY_RELEASE_DAY 24
+#define RUBY_RELEASE_DAY 27
 
 #include "ruby/version.h"
 
