167dd87d360b5f275a025b4464c19f20c5204b85
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-11-03 00:33:44 +0000

    bump URI::VERSION to 0.10.0
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@48243 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/uri.rb
+++ lib/uri.rb
@@ -95,7 +95,7 @@
 
 module URI
   # :stopdoc:
-  VERSION_CODE = '000911'.freeze
+  VERSION_CODE = '001000'.freeze
   VERSION = VERSION_CODE.scan(/../).collect{|n| n.to_i}.join('.').freeze
   # :startdoc:
 
