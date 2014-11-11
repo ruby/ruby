7b4383a2a953597d3b549d494be7fb0cf7701fcc
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-11-11 06:22:34 +0000

    remove debug code
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@48371 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/rubygems/test_gem_commands_contents_command.rb
+++ test/rubygems/test_gem_commands_contents_command.rb
@@ -205,7 +205,7 @@ lib/foo.rb
     expected = [
       [RbConfig::CONFIG['bindir'], 'default_command'],
       [RbConfig::CONFIG['rubylibdir'], 'default/gem.rb'],
-      [RbConfig::CONFIG['archdir'].sub(/x86_64/,'armv7l'), 'default_gem.so']
+      [RbConfig::CONFIG['archdir'], 'default_gem.so']
     ].sort.map{|a|File.join a}.join "\n"
 
     assert_equal expected, @ui.output.chomp
