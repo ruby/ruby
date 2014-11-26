18a7dade10a06a66ba9415ee8e2cc1f0dc98d20a
tarui <tarui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-11-26 13:31:23 +0000

    * test/ruby/test_autoload.rb (class TestAutoload): change load target 	  from Continuation to Date. Becouse Continuation is no longer quiet.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@48592 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_autoload.rb
+++ test/ruby/test_autoload.rb
@@ -4,10 +4,10 @@ require 'thread'
 
 class TestAutoload < Test::Unit::TestCase
   def test_autoload_so
-    # Continuation is always available, unless excluded intentionally.
+    # Date is always available, unless excluded intentionally.
     assert_in_out_err([], <<-INPUT, [], [])
-    autoload :Continuation, "continuation"
-    begin Continuation; rescue LoadError; end
+    autoload :Date, "date"
+    begin Date; rescue LoadError; end
     INPUT
   end
 
