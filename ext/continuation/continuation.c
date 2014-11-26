262245b0c1a3d5d823fd36c2b688121649fd9e18
tarui <tarui@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-11-26 11:50:55 +0000

    * ext/continuation/continuation.c (Init_continuation): obsolete callcc. 	  first step of [Feature #10548].
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@48588 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/continuation/continuation.c
+++ ext/continuation/continuation.c
@@ -1,8 +1,11 @@
 
+#include "ruby/ruby.h"
+
 void ruby_Init_Continuation_body(void);
 
 void
 Init_continuation(void)
 {
+    rb_warn("callcc is obsolete; use Fiber instead");
     ruby_Init_Continuation_body();
 }
