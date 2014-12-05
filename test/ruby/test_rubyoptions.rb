8a7690a5eda55aa218211490b990f982eaea4930
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-12-05 11:37:10 +0000

    test_rubyoptions.rb: non-codepage names glob
    * test/ruby/test_rubyoptions.rb (test_command_line_glob_noncodepage):
      more assertion for fallback replacement on non-codepage names.
      [ruby-dev:48752] [Bug #10555]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@48717 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_rubyoptions.rb
+++ test/ruby/test_rubyoptions.rb
@@ -724,6 +724,8 @@ class TestRubyOptions < Test::Unit::TestCase
       with_tmpchdir do |dir|
         Ougai.each {|f| open(f, "w") {}}
         assert_in_out_err(["-Eutf-8", "-e", "puts ARGV", "*"], "", Ougai, encoding: "utf-8")
+        ougai = Ougai.map {|f| f.encode("locale", replace: "?")}
+        assert_in_out_err(["-e", "puts ARGV", "*.txt"], "", ougai)
       end
     end
   end
