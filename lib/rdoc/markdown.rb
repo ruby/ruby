4b0ebe8fd099fc14ab2dc5bdcda76975888e585a
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-12-07 01:22:51 +0000

    * remove trailing spaces.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@48733 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/rdoc/markdown.rb
+++ lib/rdoc/markdown.rb
@@ -15289,7 +15289,7 @@ class RDoc::Markdown
         self.pos = _save
         break
       end
-      @result = begin; 
+      @result = begin;
                ref = [:inline, @note_order.length]
                @footnotes[ref] = paragraph a
 
