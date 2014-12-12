c3fdb1c3bd7ccf213a9bff384c85b11487093a9e
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-12-12 23:38:44 +0000

    ossl_x509attr.c: typed data
    * ext/openssl/ossl_x509attr.c (ossl_x509attr_type): use typed data.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@48809 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/openssl/ossl_x509attr.c
+++ ext/openssl/ossl_x509attr.c
@@ -14,10 +14,10 @@
     if (!(attr)) { \
 	ossl_raise(rb_eRuntimeError, "ATTR wasn't initialized!"); \
     } \
-    (obj) = Data_Wrap_Struct((klass), 0, X509_ATTRIBUTE_free, (attr)); \
+    (obj) = TypedData_Wrap_Struct((klass), &ossl_x509attr_type, (attr)); \
 } while (0)
 #define GetX509Attr(obj, attr) do { \
-    Data_Get_Struct((obj), X509_ATTRIBUTE, (attr)); \
+    TypedData_Get_Struct((obj), X509_ATTRIBUTE, &ossl_x509attr_type, (attr)); \
     if (!(attr)) { \
 	ossl_raise(rb_eRuntimeError, "ATTR wasn't initialized!"); \
     } \
@@ -32,6 +32,20 @@
  */
 VALUE cX509Attr;
 VALUE eX509AttrError;
+
+static void
+ossl_x509attr_free(void *ptr)
+{
+    X509_ATTRIBUTE_free(ptr);
+}
+
+static const rb_data_type_t ossl_x509attr_type = {
+    "OpenSSL/X509/ATTRIBUTE",
+    {
+	0, ossl_x509attr_free,
+    },
+    0, 0, RUBY_TYPED_FREE_IMMEDIATELY,
+};
 
 /*
  * Public
