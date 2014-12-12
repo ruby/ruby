8326a747f991be50b3cf0ab9eab8c091f904e14c
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-12-12 19:59:22 +0000

    erb: suppress warnings
    * bin/erb (ERB::Main#run): get rid of shadowing outer local
      variables.  [ruby-core:65772] [Feature #10395]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@48788 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/erb/test_erb_command.rb
+++ test/erb/test_erb_command.rb
@@ -3,7 +3,8 @@ require 'test/unit'
 
 class TestErbCommand < Test::Unit::TestCase
   def test_var
-    assert_in_out_err([File.expand_path("../../../bin/erb", __FILE__),
+    assert_in_out_err(["-w",
+                       File.expand_path("../../../bin/erb", __FILE__),
                        "var=hoge"],
                       "<%=var%>", ["hoge"])
   end
