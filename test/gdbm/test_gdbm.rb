da7640b0075444e5668ecea8b8f3ecd63b2a8bf9
Joe Yates <joe.g.yates@gmail.com>
2014-12-13 09:01:50 +0100

    test gdbm each_key called without a block
    * The aim of this test is to aid other implementations in being
      conformant with MRI.
    
    * The existing test calls GDBM#each_key with a block,
    * this commit tests that, when called without a block, the method
      returns an Enumerator, and does not assume there is a block,
    * the 'gdbm' gem attempts to have identical behaviour to this
      implementation, but assumes there is a block.

--- test/gdbm/test_gdbm.rb
+++ test/gdbm/test_gdbm.rb
@@ -390,6 +390,10 @@ if defined? GDBM
       assert_equal(@gdbm, ret)
     end
 
+    def test_each_key_without_block
+      assert_kind_of Enumerator, @gdbm.each_key
+    end
+
     def test_keys
       assert_equal([], @gdbm.keys)
 
