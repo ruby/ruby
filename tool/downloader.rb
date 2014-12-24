7979a5ad085508a759216c7831dcff0e5d76fe40
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-12-24 03:06:59 +0000

    url is not required here
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@48960 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/downloader.rb
+++ tool/downloader.rb
@@ -8,7 +8,7 @@ class Downloader
   end
 
   class RubyGems < self
-    def self.download(url, name, dir = nil, ims = true, options = {})
+    def self.download(name, dir = nil, ims = true, options = {})
       options[:ssl_ca_cert] = Dir.glob(File.expand_path("../lib/rubygems/ssl_certs/*.pem", File.dirname(__FILE__)))
       super("https://rubygems.org/downloads/#{name}", name, dir, ims, options)
     end
