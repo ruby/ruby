86693b30c53ea750d0f1e1a7210476ec9c9f8374
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-12-25 00:21:00 +0000

    * test/resolv/test_dns.rb (TestResolvDNS#test_query_ipv4_address):   set timeout for recvfrom because if client thread is crashed, it   waits infinity.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@48981 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/resolv/test_dns.rb
+++ test/resolv/test_dns.rb
@@ -53,7 +53,9 @@ class TestResolvDNS < Test::Unit::TestCase
           }
         }
         server_thread = Thread.new {
-          msg, (_, client_port, _, client_address) = u.recvfrom(4096)
+          timeout(5) do
+            msg, (_, client_port, _, client_address) = u.recvfrom(4096)
+          end
           id, word2, qdcount, ancount, nscount, arcount = msg.unpack("nnnnnn")
           qr =     (word2 & 0x8000) >> 15
           opcode = (word2 & 0x7800) >> 11
