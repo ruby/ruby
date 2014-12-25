f0f4d12faf18e904c02df19efa2e8ddacf13dd56
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2014-12-25 15:37:03 +0000

    remove old empty directories.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@49013 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_extlibs.rb
+++ test/ruby/test_extlibs.rb
@@ -15,10 +15,14 @@ class TestExtLibs < Test::Unit::TestCase
   check_existance "bigdecimal"
   check_existance "continuation"
   check_existance "coverage"
-  #check_existance "curses" # depend on libcurses
   check_existance "date"
   #check_existance "dbm" # depend on libdbm
   check_existance "digest"
+  check_existance "digest/bubblebabble"
+  check_existance "digest/md5"
+  check_existance "digest/rmd160"
+  check_existance "digest/sha1"
+  check_existance "digest/sha2"
   check_existance "etc"
   check_existance "fcntl"
   check_existance "fiber"
@@ -28,7 +32,8 @@ class TestExtLibs < Test::Unit::TestCase
   check_existance "io/nonblock"
   check_existance "io/wait"
   check_existance "json"
-  check_existance "mathn"
+  check_existance "mathn/complex"
+  check_existance "mathn/rational"
   check_existance "nkf"
   check_existance "objspace"
   check_existance "openssl", "this may be false positive, but should assert because rubygems requires this"
@@ -36,7 +41,7 @@ class TestExtLibs < Test::Unit::TestCase
   check_existance "psych"
   check_existance "pty" unless windows?
   check_existance "racc/cparse"
-  check_existance "rbconfig"
+  check_existance "rbconfig/sizeof"
   #check_existance "readline" # depend on libreadline
   check_existance "ripper"
   check_existance "sdbm"
@@ -45,7 +50,8 @@ class TestExtLibs < Test::Unit::TestCase
   check_existance "strscan"
   check_existance "syslog" unless windows?
   check_existance "thread"
-  #check_existance "tk"  # depend on Tcl/Tk
+  #check_existance "tk" # depend on Tcl/Tk
+  #check_existance "tk/tkutil" # depend on Tcl/Tk
   check_existance "Win32API" if windows?
   check_existance "win32ole" if windows?
   check_existance "zlib", "this may be false positive, but should assert because rubygems requires this"
