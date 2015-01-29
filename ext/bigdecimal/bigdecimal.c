78400919bad04fcba0b9dee75b6217933c958fbc
mrkn <mrkn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-01-29 14:34:43 +0000

    * ext/bigdecimal/bigdecimal.c (rb_rational_num): add fallback function   for rubies lower than 2.2.0.
    * ext/bigdecimal/bigdecimal.c (rb_rational_den): ditto.
    
    * ext/bigdecimal/extconf.rb: check the existences of struct RRational,
      rb_rational_num, and rb_rational_den.
    
    * ext/bigdecimal/bigdecimal.bundle: bump version.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@49440 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/bigdecimal/bigdecimal.c
+++ ext/bigdecimal/bigdecimal.c
@@ -109,6 +109,30 @@ static ID id_eq;
 # define RB_OBJ_STRING(obj) StringValueCStr(obj)
 #endif
 
+#ifndef HAVE_RB_RATIONAL_NUM
+static inline VALUE
+rb_rational_num(VALUE rat)
+{
+#ifdef HAVE_TYPE_STRUCT_RRATIONAL
+    return RRATIONAL(rat)->num;
+#else
+    return rb_funcall(rat, rb_intern("numerator"));
+#endif
+}
+#endif
+
+#ifndef HAVE_RB_RATIONAL_DEN
+static inline VALUE
+rb_rational_den(VALUE rat)
+{
+#ifdef HAVE_TYPE_STRUCT_RRATIONAL
+    return RRATIONAL(rat)->den;
+#else
+    return rb_funcall(rat, rb_intern("denominator"));
+#endif
+}
+#endif
+
 /*
  * ================== Ruby Interface part ==========================
  */
