6a3397749a6a9784862e7e72c715c9d4862789db
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-02-17 15:18:40 +0000

    merge revision(s) r49223: [Backport #10732]
    	* lib/rdoc/text.rb (expand_tabs): get rid of infinite loop with
    	  CR.  should check if substitution occurred too.
    	  [ruby-dev:48813] [Bug #10732]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@49629 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/rdoc/text.rb
+++ lib/rdoc/text.rb
@@ -68,11 +68,11 @@ module RDoc::Text
     expanded = []
 
     text.each_line do |line|
-      line.gsub!(/^((?:.{8})*?)([^\t\r\n]{0,7})\t/) do
+      nil while line.gsub!(/(?:\G|\r)((?:.{8})*?)([^\t\r\n]{0,7})\t/) do
         r = "#{$1}#{$2}#{' ' * (8 - $2.size)}"
         r.force_encoding text.encoding if Object.const_defined? :Encoding
         r
-      end until line !~ /\t/
+      end
 
       expanded << line
     end
