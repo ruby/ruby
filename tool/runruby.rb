7722b3b67f764afd9c796c99b53dbfd54a36dcf2
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-02-25 09:17:28 +0000

    runruby.rb: follow r49740
    * tool/runruby.rb: follow the change of RUBY_VERSION check in
      rbconfig by r49740.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@49747 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/runruby.rb
+++ tool/runruby.rb
@@ -47,7 +47,7 @@ abs_archdir = File.expand_path(archdir)
 $:.unshift(abs_archdir)
 
 config = File.read(conffile = File.join(abs_archdir, 'rbconfig.rb'))
-config.sub!(/^(\s*)RUBY_VERSION\s*==.*(\sor\s*)$/, '\1true\2')
+config.sub!(/^(\s*)RUBY_VERSION\s.*(\sor\s*)$/, '\1true\2')
 config = Module.new {module_eval(config, conffile)}::RbConfig::CONFIG
 
 ruby = File.join(archdir, config["RUBY_INSTALL_NAME"]+config['EXEEXT'])
