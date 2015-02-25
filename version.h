b27d06d30664c80600f832dd1f6cb653efa8ec1f
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-02-25 05:18:39 +0000

    merge revision(s) 48747: [Backport #10582]
    	* string.c: [DOC] Add missing documentation around String#chomp.
    	  Patchby @stderr [ci skip][fix GH-780]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_0_0@49733 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.0.0"
-#define RUBY_RELEASE_DATE "2015-02-21"
-#define RUBY_PATCHLEVEL 635
+#define RUBY_RELEASE_DATE "2015-02-25"
+#define RUBY_PATCHLEVEL 636
 
 #define RUBY_RELEASE_YEAR 2015
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 21
+#define RUBY_RELEASE_DAY 25
 
 #include "ruby/version.h"
 
