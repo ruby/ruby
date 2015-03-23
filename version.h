5727caf689097592fedf0a33420e1b8136c50c1a
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-03-23 20:11:27 +0000

    merge revision(s) r47129,r48844: [Backport #10120]
    	configure.in: remove SSE options
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@50067 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.5"
-#define RUBY_RELEASE_DATE "2015-03-22"
-#define RUBY_PATCHLEVEL 320
+#define RUBY_RELEASE_DATE "2015-03-24"
+#define RUBY_PATCHLEVEL 321
 
 #define RUBY_RELEASE_YEAR 2015
 #define RUBY_RELEASE_MONTH 3
-#define RUBY_RELEASE_DAY 22
+#define RUBY_RELEASE_DAY 24
 
 #include "ruby/version.h"
 
