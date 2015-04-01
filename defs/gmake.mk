5e977e7f7debce76d60f1b1ca8890c825ee07253
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-04-01 08:14:35 +0000

    gmake.mk: CHECK_TARGETS
    * defs/gmake.mk (CHECK_TARGETS): goals depending on test and
      test-all.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@50147 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- defs/gmake.mk
+++ defs/gmake.mk
@@ -5,8 +5,9 @@ TEST_TARGETS := $(filter check test check% test% btest%,$(MAKECMDGOALS))
 TEST_TARGETS += $(subst check,test-all,$(patsubst check-%,test-%,$(TEST_TARGETS)))
 TEST_TARGETS := $(patsubst test-%,yes-test-%,$(patsubst btest-%,yes-btest-%,$(TEST_TARGETS)))
 TEST_DEPENDS := $(if $(TEST_TARGETS),$(filter all main exts,$(MAKECMDGOALS)))
-TEST_DEPENDS += $(TEST_DEPENDS) $(if $(filter check%,$(MAKECMDGOALS)),main)
+TEST_DEPENDS += $(if $(filter check%,$(MAKECMDGOALS)),main)
 TEST_DEPENDS += $(if $(filter all,$(INSTALLDOC)),docs)
+CHECK_TARGETS := love check%
 
 ifneq ($(filter -O0 -Od,$(optflags)),)
 override XCFLAGS := $(filter-out -D_FORTIFY_SOURCE=%,$(XCFLAGS))
@@ -39,15 +40,15 @@ $(foreach arch,$(filter -arch=%,$(subst -arch ,-arch=,$(ARCH_FLAG))),\
 	$(eval $(call archcmd,$(patsubst -arch=%,%,$(value arch)),$(patsubst -arch=%,-arch %,$(value arch)))))
 endif
 
-ifneq ($(filter love check% test,$(MAKECMDGOALS)),)
+ifneq ($(filter $(CHECK_TARGETS) test,$(MAKECMDGOALS)),)
 yes-test-knownbug: $(TEST_DEPENDS) yes-btest-ruby
 yes-btest-ruby: $(TEST_DEPENDS) yes-test-sample
 yes-test-sample: $(TEST_DEPENDS)
 endif
-ifneq ($(filter love check%,$(MAKECMDGOALS)) $(filter test-all,$(TEST_TARGETS)),)
+ifneq ($(filter $(CHECK_TARGETS),$(MAKECMDGOALS)) $(filter test-all,$(TEST_TARGETS)),)
 yes-test-all yes-test-ruby: $(filter-out %test-all %test-ruby check%,$(TEST_TARGETS))
 endif
-ifneq ($(filter love check%,$(MAKECMDGOALS))$(if $(filter test-all,$(MAKECMDGOALS)),$(filter test-knownbug,$(MAKECMDGOALS))),)
+ifneq ($(filter $(CHECK_TARGETS),$(MAKECMDGOALS))$(if $(filter test-all,$(MAKECMDGOALS)),$(filter test-knownbug,$(MAKECMDGOALS))),)
 yes-test-all yes-test-ruby: yes-test-knownbug
 endif
 
