8c0b2a286080609613b6b007e030ff7c7adaa23c
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-04-04 02:30:26 +0000

    string.c: check before modify
    * string.c (rb_str_setbyte): check the argument first not to
      discard shared string and code range unnecessarily until
      actually changing the contents.  pointed out by headius.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@50162 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- string.c
+++ string.c
@@ -4567,13 +4567,14 @@ rb_str_setbyte(VALUE str, VALUE index, VALUE value)
 {
     long pos = NUM2LONG(index);
     int byte = NUM2INT(value);
+    long len = RSTRING_LEN(str);
 
-    rb_str_modify(str);
-
-    if (pos < -RSTRING_LEN(str) || RSTRING_LEN(str) <= pos)
+    if (pos < -len || len <= pos)
         rb_raise(rb_eIndexError, "index %ld out of string", pos);
     if (pos < 0)
-        pos += RSTRING_LEN(str);
+        pos += len;
+
+    rb_str_modify(str);
 
     RSTRING_PTR(str)[pos] = byte;
 
