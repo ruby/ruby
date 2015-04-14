116ec560bde5402a5fa6a508376c4e988c583b64
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-04-14 07:02:40 +0000

    * version.h (RUBY_VERSION): bump RUBY_VERSION to 2.1.7.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@50312 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
-#define RUBY_VERSION "2.1.6"
-#define RUBY_RELEASE_DATE "2015-04-13"
-#define RUBY_PATCHLEVEL 336
+#define RUBY_VERSION "2.1.7"
+#define RUBY_RELEASE_DATE "2015-04-14"
+#define RUBY_PATCHLEVEL 337
 
 #define RUBY_RELEASE_YEAR 2015
 #define RUBY_RELEASE_MONTH 4
-#define RUBY_RELEASE_DAY 13
+#define RUBY_RELEASE_DAY 14
 
 #include "ruby/version.h"
 
