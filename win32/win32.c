698ec88c6f7d8526087dfdd5746756719b69b4f2
NARUSE, Yui <naruse@ruby-lang.org>
2015-04-25 08:05:15 +0900

    workaround for MSVC14

--- win32/win32.c
+++ win32/win32.c
@@ -2321,6 +2321,21 @@ typedef struct {
 #endif
 
 /* License: Ruby's */
+#if RUBY_MSVCRT_VERSION >= 140
+typedef struct {
+    CRITICAL_SECTION           lock;
+    intptr_t                   osfhnd;          // underlying OS file HANDLE
+    __int64                    startpos;        // File position that matches buffer start
+    unsigned char              osfile;          // Attributes of file (e.g., open in text mode?)
+    char      textmode;
+    char _pipe_lookahead;
+
+    uint8_t unicode          : 1; // Was the file opened as unicode?
+    uint8_t utf8translations : 1; // Buffer contains translations other than CRLF
+    uint8_t dbcsBufferUsed   : 1; // Is the dbcsBuffer in use?
+    char    dbcsBuffer;           // Buffer for the lead byte of DBCS when converting from DBCS to Unicode
+} ioinfo;
+#else
 typedef struct	{
     intptr_t osfhnd;	/* underlying OS file HANDLE */
     char osfile;	/* attributes of file (e.g., open in text mode?) */
@@ -2332,16 +2347,21 @@ typedef struct	{
     char pipech2[2];
 #endif
 }	ioinfo;
+#endif
 
 #if !defined _CRTIMP || defined __MINGW32__
 #undef _CRTIMP
 #define _CRTIMP __declspec(dllimport)
 #endif
 
+#if RUBY_MSVCRT_VERSION >= 140
+static ioinfo ** __pioinfo = NULL;
+#else
 EXTERN_C _CRTIMP ioinfo * __pioinfo[];
+#endif
 static inline ioinfo* _pioinfo(int);
 
-#define IOINFO_L2E			5
+#define IOINFO_L2E	(RUBY_MSVCRT_VERSION >= 140 ? 6 : 5)
 #define IOINFO_ARRAY_ELTS	(1 << IOINFO_L2E)
 #define _osfhnd(i)  (_pioinfo(i)->osfhnd)
 #define _osfile(i)  (_pioinfo(i)->osfile)
@@ -2356,6 +2376,12 @@ static size_t pioinfo_extra = 0;	/* workaround for VC++8 SP1 */
 static void
 set_pioinfo_extra(void)
 {
+#if RUBY_MSVCRT_VERSION >= 140
+    HMODULE mod = GetModuleHandle("ucrtbase.dll");
+    FARPROC addr = GetProcAddress(mod, "_isatty");
+    intptr_t delta = 0xc07b0; /* __pioinfo - _isatty() */
+    __pioinfo = (ioinfo**)((intptr_t)addr + delta);
+#else
     int fd;
 
     fd = _open("NUL", O_RDONLY);
@@ -2370,6 +2396,7 @@ set_pioinfo_extra(void)
 	/* not found, maybe something wrong... */
 	pioinfo_extra = 0;
     }
+#endif
 }
 #else
 #define pioinfo_extra 0
