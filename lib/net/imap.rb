f954f0d8404059b2be73d382e3166ecc1076a3c0
shugo <shugo@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-05-08 06:46:18 +0000

    * lib/net/imap.rb (body_ext_mpart): should work even if body-fld-dsp   is omitted.  [ruby-core:69093] [Bug #11128]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@50444 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/net/imap.rb
+++ lib/net/imap.rb
@@ -2639,7 +2639,13 @@ module Net
           return param
         end
         disposition = body_fld_dsp
-        match(T_SPACE)
+
+        token = lookahead
+        if token.symbol == T_SPACE
+          shift_token
+        else
+          return param, disposition
+        end
         language = body_fld_lang
 
         token = lookahead
