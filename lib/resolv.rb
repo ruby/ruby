0cddf1b4331877019f65bf3ba572f7f8d0371115
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-05-13 05:33:45 +0000

    merge revision(s) 49095: [Backport #11144]
    	* lib/resolv.rb (Resolv::DNS::Label::Str#==): Check class equality.
    	  (Resolv::DNS::Name#initialize): Normalize labels as
    	  Resolv::DNS::Label::Str objects.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@50482 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/resolv.rb
+++ lib/resolv.rb
@@ -1187,7 +1187,7 @@ class Resolv
         end
 
         def ==(other)
-          return @downcase == other.downcase
+          return self.class == other.class && @downcase == other.downcase
         end
 
         def eql?(other)
@@ -1223,6 +1223,14 @@ class Resolv
       end
 
       def initialize(labels, absolute=true) # :nodoc:
+        labels = labels.map {|label|
+          case label
+          when String then Label::Str.new(label)
+          when Label::Str then label
+          else
+            raise ArgumentError, "unexpected label: #{label.inspect}"
+          end
+        }
         @labels = labels
         @absolute = absolute
       end
--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.7"
-#define RUBY_RELEASE_DATE "2015-05-12"
-#define RUBY_PATCHLEVEL 343
+#define RUBY_RELEASE_DATE "2015-05-13"
+#define RUBY_PATCHLEVEL 344
 
 #define RUBY_RELEASE_YEAR 2015
 #define RUBY_RELEASE_MONTH 5
-#define RUBY_RELEASE_DAY 12
+#define RUBY_RELEASE_DAY 13
 
 #include "ruby/version.h"
 
