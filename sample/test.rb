75a865406f8b65827a414059b609f8077d9070ed
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-05-17 00:50:02 +0000

    reset colors
    * bootstraptest/runner.rb, sample/test.rb, test/lib/test/unit.rb:
      reset for each colors.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@50516 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- sample/test.rb
+++ sample/test.rb
@@ -34,8 +34,8 @@ class Progress
     if @color
       # dircolors-like style
       colors = (colors = ENV['TEST_COLORS']) ? Hash[colors.scan(/(\w+)=([^:]*)/)] : {}
-      @passed = "\e[#{colors["pass"] || "32"}m"
-      @failed = "\e[#{colors["fail"] || "31"}m"
+      @passed = "\e[;#{colors["pass"] || "32"}m"
+      @failed = "\e[;#{colors["fail"] || "31"}m"
       @reset = "\e[m"
     else
       @passed = @failed = @reset = ""
