534c2b03b30d60d2490a5550c7c43b2272b108d1
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-05-20 15:51:27 +0000

    merge revision(s) 50372,50373: [Backport #11086]
    	* vm_eval.c (rb_obj_instance_eval, rb_obj_instance_exec): allow
    	  symbols to just instance_eval/exec, except for definition of
    	  singletons.  [ruby-core:68961] [Bug #11086]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_2@50561 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.2.3"
-#define RUBY_RELEASE_DATE "2015-05-20"
-#define RUBY_PATCHLEVEL 110
+#define RUBY_RELEASE_DATE "2015-05-21"
+#define RUBY_PATCHLEVEL 111
 
 #define RUBY_RELEASE_YEAR 2015
 #define RUBY_RELEASE_MONTH 5
-#define RUBY_RELEASE_DAY 20
+#define RUBY_RELEASE_DAY 21
 
 #include "ruby/version.h"
 
