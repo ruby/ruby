39a65664265525fcd7ca23c5613f832b0889cd01
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-05-21 04:31:19 +0000

    merge revision(s) 50206: [Backport #10724]
    	* vm.c (vm_exec): check other events when RETURN is thrown.
    	  [Bug #10724]
    
    	* test/ruby/test_settracefunc.rb: add a test.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@50576 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_settracefunc.rb
+++ test/ruby/test_settracefunc.rb
@@ -1348,4 +1348,24 @@ class TestSetTraceFunc < Test::Unit::TestCase
       }
     end
   end
+
+  class Bug10724
+    def initialize
+      loop{return}
+    end
+  end
+
+  def test_throwing_return_with_finish_frame
+    target_th = Thread.current
+    evs = []
+
+    TracePoint.new(:call, :return){|tp|
+      return if Thread.current != target_th
+      evs << tp.event
+    }.enable{
+      a = Bug10724.new
+    }
+
+    assert_equal([:call, :return], evs)
+  end
 end
