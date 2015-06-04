91a6ba4c2193263976d42f33e600afcf3822c510
hsbt <hsbt@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-06-04 02:53:11 +0000

    * ext/win32ole/win32ole.c: fix a typo. Patch by @davydovanton   [fix GH-923] * include/ruby/st.h: ditto. * include/ruby/util.h: ditto.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@50765 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/util.h
+++ include/ruby/util.h
@@ -48,7 +48,7 @@ extern "C" {
 RUBY_SYMBOL_EXPORT_BEGIN
 
 #define DECIMAL_SIZE_OF_BITS(n) (((n) * 3010 + 9998) / 9999)
-/* an approximation of ceil(n * log10(2)), upto 65536 at least */
+/* an approximation of ceil(n * log10(2)), up to 65536 at least */
 
 #define scan_oct(s,l,e) ((int)ruby_scan_oct((s),(l),(e)))
 unsigned long ruby_scan_oct(const char *, size_t, size_t *);
