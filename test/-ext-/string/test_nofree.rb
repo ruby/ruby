f9803212e30476bab48accf118e76614a6d9cf71
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-06-12 08:04:39 +0000

    test_nofree.rb: fix commit miss
    * test/-ext-/string/test_nofree.rb (test_no_memory_leak): remove
      limit and make the interation longer.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@50843 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/-ext-/string/test_nofree.rb
+++ test/-ext-/string/test_nofree.rb
@@ -3,9 +3,10 @@ require 'test/unit'
 class Test_StringNoFree < Test::Unit::TestCase
   def test_no_memory_leak
     bug10942 = '[ruby-core:68436] [Bug #10942] no leak on nofree string'
-    code = '100_000.times {Bug::String.nofree << "a" * 100}'
+    code = '.times {Bug::String.nofree << "a" * 100}'
     assert_no_memory_leak(%w(-r-test-/string/string),
-                          code, code,
-                          bug10942, rss: true, limit: 1.1)
+                          "100_000#{code}",
+                          "1_000_000#{code}",
+                          bug10942, rss: true)
   end
 end
