d5a78c89e528a3b07da3c6414d91d2c5ab2acaf2
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-06-12 08:02:49 +0000

    test_nofree.rb: rehearsal
    * test/-ext-/string/test_nofree.rb (test_no_memory_leak): add a
      rehearsal.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@50842 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/-ext-/string/test_nofree.rb
+++ test/-ext-/string/test_nofree.rb
@@ -3,8 +3,9 @@ require 'test/unit'
 class Test_StringNoFree < Test::Unit::TestCase
   def test_no_memory_leak
     bug10942 = '[ruby-core:68436] [Bug #10942] no leak on nofree string'
-    assert_no_memory_leak(%w(-r-test-/string/string), '',
-                          '1000000.times {Bug::String.nofree << "a" * 100}',
-                          bug10942, rss: true)
+    code = '100_000.times {Bug::String.nofree << "a" * 100}'
+    assert_no_memory_leak(%w(-r-test-/string/string),
+                          code, code,
+                          bug10942, rss: true, limit: 1.1)
   end
 end
