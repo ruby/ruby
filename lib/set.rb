9f2e41b7e0be219e1db219dbc31f4a0a8c892809
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-06-15 05:37:38 +0000

    * lib/set.rb: Make Set#each and SortedSet#each generate a sized   enumerator. [GH-931] by kachick (Kenichi Kamiya)
    * test/test_set.rb: Import tests from Set into SortedSet. [GH-931]
      by kachick (Kenichi Kamiya)
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@50908 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/set.rb
+++ lib/set.rb
@@ -284,7 +284,7 @@ class Set
   # the element as parameter.  Returns an enumerator if no block is
   # given.
   def each(&block)
-    block or return enum_for(__method__)
+    block or return enum_for(__method__) { size }
     @hash.each_key(&block)
     self
   end
@@ -663,7 +663,7 @@ class SortedSet < Set
           end
 
           def each(&block)
-            block or return enum_for(__method__)
+            block or return enum_for(__method__) { size }
             to_a.each(&block)
             self
           end
