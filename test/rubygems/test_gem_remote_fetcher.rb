6398515adfc86813686605019a3e22d49cd95517
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-06-19 06:04:00 +0000

    test_gem_remote_fetcher.rb: get rid of errors
    * test/rubygems/test_gem_remote_fetcher.rb (start_ssl_server):
      temporary measure for "dh key too small" error of OpenSSL
      1.0.2c+.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@50972 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/rubygems/test_gem_remote_fetcher.rb
+++ test/rubygems/test_gem_remote_fetcher.rb
@@ -898,7 +898,7 @@ gems:
       server.mount_proc("/insecure_redirect") { |req, res|
         res.set_redirect(WEBrick::HTTPStatus::MovedPermanently, req.query['to'])
       }
-      server.ssl_context.tmp_dh_callback = proc { OpenSSL::PKey::DH.new 128 }
+      server.ssl_context.tmp_dh_callback = proc {|_, _, k| OpenSSL::PKey::DH.new(k) }
       t = Thread.new do
         begin
           server.start
