1a9d51fbc186508f78f791916c706f057f255897
ktsj <ktsj@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-06-20 08:32:54 +0000

    * object.c (rb_obj_taint): [DOC] $SAFE=3 is obsolete.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@50979 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- object.c
+++ object.c
@@ -953,9 +953,6 @@ rb_obj_tainted(VALUE obj)
  *
  *  You should only untaint a tainted object if your code has inspected it and
  *  determined that it is safe. To do so use #untaint.
- *
- *  In $SAFE level 3, all newly created objects are tainted and you can't untaint
- *  objects.
  */
 
 VALUE
