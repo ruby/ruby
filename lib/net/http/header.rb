0d4f70c6478c2cf58f42feb03591b0f75d0266bc
shishir127 <shishir127@gmail.com>
2015-06-29 10:23:18 +0530

    Skipping the header instead of assigning a blank string

--- lib/net/http/header.rb
+++ lib/net/http/header.rb
@@ -14,11 +14,8 @@ module Net::HTTPHeader
     return unless initheader
     initheader.each do |key, value|
       warn "net/http: warning: duplicated HTTP header: #{key}" if key?(key) and $VERBOSE
-      if value.nil? 
-        @header[key.downcase] = ""
-      else  
-        @header[key.downcase] = [value.strip]
-      end
+      next if value.nil?
+      @header[key.downcase] = [value.strip]
     end
   end
 
