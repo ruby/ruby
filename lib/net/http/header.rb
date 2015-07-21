4bc93da638e5b1fc604cfb052a3420aaa9b6d222
Teodor Szente <teodor98sz@gmail.com>
2015-07-22 00:44:17 +0300

    fix if condition

--- lib/net/http/header.rb
+++ lib/net/http/header.rb
@@ -15,7 +15,7 @@ module Net::HTTPHeader
     initheader.each do |key, value|
       warn "net/http: warning: duplicated HTTP header: #{key}" if key?(key) and $VERBOSE
       if value.nil? and $VERBOSE
-        warn "net/http: warning: nil HTTP header: #{key}" if key?(key) and $VERBOSE
+        warn "net/http: warning: nil HTTP header: #{key}" if $VERBOSE
       else
         @header[key.downcase] = [value.strip]
       end
