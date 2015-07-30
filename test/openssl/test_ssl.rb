6eac95821eb299b0b37d87e12a1f2af31730de5d
Aaron Patterson <aaron.patterson@gmail.com>
2015-07-30 14:52:13 -0700

    print out the available methods

--- test/openssl/test_ssl.rb
+++ test/openssl/test_ssl.rb
@@ -380,7 +380,7 @@ class OpenSSL::TestSSL < OpenSSL::SSLTestCase
     ctx = OpenSSL::SSL::SSLContext.new
     ctx.ciphers = "ADH"
     p ctx.ciphers
-    p self
+    p OpenSSL::SSL::SSLContext::METHODS
     puts
     puts
 
