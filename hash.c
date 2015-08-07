cdb58a19cc02e59ad632860794f924383f24da61
zzak <zzak@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-08-07 18:42:01 +0000

    * hash.c: [DOC] Improve description of symbol key syntax   Patch by Raphael Das Gupta in documenting-ruby/ruby#51:   https://github.com/documenting-ruby/ruby/pull/51
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@51506 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- hash.c
+++ hash.c
@@ -3909,7 +3909,7 @@ env_update(VALUE env, VALUE hash)
  *
  *    grades = { "Jane Doe" => 10, "Jim Doe" => 6 }
  *
- *  Hashes allow an alternate syntax form when your keys are always symbols.
+ *  Hashes allow an alternate syntax for keys that are symbols.
  *  Instead of
  *
  *    options = { :font_size => 10, :font_family => "Arial" }
