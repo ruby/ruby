bc0642a500b51bd355ded8827d0fd9174aea8a3b
Zachary Scott <e@zzak.io>
2015-08-07 14:25:32 -0400

    Refactor tmpdir to use Tmpdir module instead of monkey patch
    Although this changes the implementation, but shouldn't affect users that
    depend on Dir.mktmpdir and Dir.tmpdir interface.

--- lib/tmpdir.rb
+++ lib/tmpdir.rb
@@ -1,7 +1,7 @@
 #
 # tmpdir - retrieve temporary directory path
 #
-# $Id$
+# $Id: tmpdir.rb 48905 2014-12-20 08:58:07Z akr $
 #
 
 require 'fileutils'
@@ -10,14 +10,15 @@ begin
 rescue LoadError # rescue LoadError for miniruby
 end
 
-class Dir
+# Library for retrieving and creating temporary directories.
+module Tmpdir
 
   @@systmpdir ||= defined?(Etc.systmpdir) ? Etc.systmpdir : '/tmp'
 
   ##
   # Returns the operating system's temporary file path.
 
-  def self.tmpdir
+  def tmpdir
     if $SAFE > 0
       @@systmpdir
     else
@@ -81,7 +82,7 @@ class Dir
   #    FileUtils.remove_entry dir
   #  end
   #
-  def Dir.mktmpdir(prefix_suffix=nil, *rest)
+  def mktmpdir(prefix_suffix=nil, *rest)
     path = Tmpname.create(prefix_suffix || "d", *rest) {|n| mkdir(n, 0700)}
     if block_given?
       begin
@@ -97,43 +98,47 @@ class Dir
       path
     end
   end
+end
 
-  module Tmpname # :nodoc:
-    module_function
+module Tmpname # :nodoc:
+  module_function
 
-    def tmpdir
-      Dir.tmpdir
-    end
-
-    def make_tmpname((prefix, suffix), n)
-      prefix = (String.try_convert(prefix) or
-                raise ArgumentError, "unexpected prefix: #{prefix.inspect}")
-      suffix &&= (String.try_convert(suffix) or
-                  raise ArgumentError, "unexpected suffix: #{suffix.inspect}")
-      t = Time.now.strftime("%Y%m%d")
-      path = "#{prefix}#{t}-#{$$}-#{rand(0x100000000).to_s(36)}"
-      path << "-#{n}" if n
-      path << suffix if suffix
-      path
-    end
-
-    def create(basename, tmpdir=nil, max_try: nil, **opts)
-      if $SAFE > 0 and tmpdir.tainted?
-        tmpdir = '/tmp'
-      else
-        tmpdir ||= tmpdir()
-      end
-      n = nil
-      begin
-        path = File.join(tmpdir, make_tmpname(basename, n))
-        yield(path, n, opts)
-      rescue Errno::EEXIST
-        n ||= 0
-        n += 1
-        retry if !max_try or n < max_try
-        raise "cannot generate temporary name using `#{basename}' under `#{tmpdir}'"
-      end
-      path
-    end
+  def tmpdir
+    Dir.tmpdir
   end
+
+  def make_tmpname((prefix, suffix), n)
+    prefix = (String.try_convert(prefix) or
+              raise ArgumentError, "unexpected prefix: #{prefix.inspect}")
+    suffix &&= (String.try_convert(suffix) or
+                raise ArgumentError, "unexpected suffix: #{suffix.inspect}")
+    t = Time.now.strftime("%Y%m%d")
+    path = "#{prefix}#{t}-#{$$}-#{rand(0x100000000).to_s(36)}"
+    path << "-#{n}" if n
+    path << suffix if suffix
+    path
+  end
+
+  def create(basename, tmpdir=nil, max_try: nil, **opts)
+    if $SAFE > 0 and tmpdir.tainted?
+      tmpdir = '/tmp'
+    else
+      tmpdir ||= tmpdir()
+    end
+    n = nil
+    begin
+      path = File.join(tmpdir, make_tmpname(basename, n))
+      yield(path, n, opts)
+    rescue Errno::EEXIST
+      n ||= 0
+      n += 1
+      retry if !max_try or n < max_try
+      raise "cannot generate temporary name using `#{basename}' under `#{tmpdir}'"
+    end
+    path
+  end
+end
+
+class Dir
+  extend Tmpdir
 end
