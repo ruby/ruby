d3a6478dbb6bf9046917738b785f59c8b38a1641
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-08-17 08:05:45 +0000

    merge revision(s) 51531: [Backport #11368]
    	* test/openssl/test_ssl.rb: Fix LocalJumpErrors being raised
    	  in OpenSSL tests. [ruby-core:70020][Bug #11368]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@51604 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/openssl/test_ssl.rb
+++ test/openssl/test_ssl.rb
@@ -830,7 +830,7 @@ end
     ssl = ctx ? OpenSSL::SSL::SSLSocket.new(sock, ctx) : OpenSSL::SSL::SSLSocket.new(sock)
     ssl.sync_close = true
     ssl.connect
-    yield ssl
+    yield ssl if block_given?
   ensure
     ssl.close
   end
