5ce4bfc9d227cf99cfc5390601344c2818c0e081
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-08-26 01:52:04 +0000

    re.c: fix wchar match at EOS
    * re.c (rb_memsearch_wchar, rb_memsearch_qchar): test matching
      till the end of string.  [ruby-core:70592] [Bug #11488]
    * test/ruby/test_m17n.rb (test_include?, tet_index): add tests by
      Tom Stuart.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@51685 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- re.c
+++ re.c
@@ -227,7 +227,7 @@ rb_memsearch_wchar(const unsigned char *xs, long m, const unsigned char *ys, lon
     const unsigned char *x = xs, x0 = *xs, *y = ys;
     enum {char_size = 2};
 
-    for (n -= m; n > 0; n -= char_size, y += char_size) {
+    for (n -= m; n >= 0; n -= char_size, y += char_size) {
 	if (x0 == *y && memcmp(x+1, y+1, m-1) == 0)
 	    return y - ys;
     }
@@ -240,7 +240,7 @@ rb_memsearch_qchar(const unsigned char *xs, long m, const unsigned char *ys, lon
     const unsigned char *x = xs, x0 = *xs, *y = ys;
     enum {char_size = 4};
 
-    for (n -= m; n > 0; n -= char_size, y += char_size) {
+    for (n -= m; n >= 0; n -= char_size, y += char_size) {
 	if (x0 == *y && memcmp(x+1, y+1, m-1) == 0)
 	    return y - ys;
     }
