801e1fe46d83c856844ba18ae4751478c59af0d1
tenderlove <tenderlove@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-08-29 17:45:14 +0000

    * ext/openssl/ossl_ssl.c (static const struct): Only add SSLv3 support   if the SSL library supports it.  Thanks Kurt Roeckx <kurt@roeckx.be>   [Bug #11376]
    * ext/openssl/extconf.rb: check for SSLv3 support in the SSL
      implementation.
    
    * test/openssl/test_ssl.rb (class OpenSSL): Skip tests that need SSLv3
      if there is no support.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@51722 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/openssl/ossl_ssl.c
+++ ext/openssl/ossl_ssl.c
@@ -109,9 +109,12 @@ static const struct {
     OSSL_SSL_METHOD_ENTRY(SSLv2_server),
     OSSL_SSL_METHOD_ENTRY(SSLv2_client),
 #endif
+#if defined(HAVE_SSLV3_METHOD) && defined(HAVE_SSLV3_SERVER_METHOD) && \
+        defined(HAVE_SSLV3_CLIENT_METHOD)
     OSSL_SSL_METHOD_ENTRY(SSLv3),
     OSSL_SSL_METHOD_ENTRY(SSLv3_server),
     OSSL_SSL_METHOD_ENTRY(SSLv3_client),
+#endif
     OSSL_SSL_METHOD_ENTRY(SSLv23),
     OSSL_SSL_METHOD_ENTRY(SSLv23_server),
     OSSL_SSL_METHOD_ENTRY(SSLv23_client),
