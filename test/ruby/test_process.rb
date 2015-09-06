9df8d3c830a515540ece0281eb08ce933f8ef0db
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-09-06 06:00:12 +0000

    test_process.rb: suppress warnings
    * test/ruby/test_process.rb (test_exec_close_reserved_fd):
      suppress "cannot close fd before spawn" warnings.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@51775 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_process.rb
+++ test/ruby/test_process.rb
@@ -2094,6 +2094,7 @@ EOS
       (3..6).each do |i|
         ret = run_in_child(<<-INPUT)
           begin
+            $VERBOSE = nil
             Process.exec('#{cmd}', 'dummy', #{i} => :close)
           rescue SystemCallError
           end
