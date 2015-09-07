c36a11dc5e506e012358e28d1323024dc7059199
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-09-07 15:15:02 +0000

    * 2015-09-08
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@51788 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.3.0"
-#define RUBY_RELEASE_DATE "2015-09-07"
+#define RUBY_RELEASE_DATE "2015-09-08"
 #define RUBY_PATCHLEVEL -1
 
 #define RUBY_RELEASE_YEAR 2015
 #define RUBY_RELEASE_MONTH 9
-#define RUBY_RELEASE_DAY 7
+#define RUBY_RELEASE_DAY 8
 
 #include "ruby/version.h"
 
