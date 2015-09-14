aa926a80d1e74654aedafc83de76b940476e40f9
shishir127 <shishir127@gmail.com>
2015-09-14 09:42:56 +0530

    trailing whitespaces

--- test/net/http/test_httpheader.rb
+++ test/net/http/test_httpheader.rb
@@ -142,13 +142,13 @@ class HTTPHeaderTest < Test::Unit::TestCase
     end
   end
 
-  def test_each_capitalized_with_symbol   
-    @c[:my_header] = ['a', 'b']    
-    @c.each_capitalized do |k,v|   
-      assert_equal "My_header", k    
-      assert_equal 'a, b', v   
-    end    
-  end    
+  def test_each_capitalized_with_symbol
+    @c[:my_header] = ['a', 'b']
+    @c.each_capitalized do |k,v|
+      assert_equal "My_header", k
+      assert_equal 'a, b', v
+    end
+  end  
 
   def test_key?
     @c['My-Header'] = 'test'
