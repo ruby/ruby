85701849eb533d055e0e39d23b2fffab42f05846
leriksen <leif.eriksen.au@gmail.com>
2015-09-23 21:27:49 +1000

    [DOC] clarify docs - take_while and drop_while pass elements to their blocks, not arrays [skip ci]

--- array.c
+++ array.c
@@ -5423,7 +5423,7 @@ rb_ary_take(VALUE obj, VALUE n)
 
 /*
  *  call-seq:
- *     ary.take_while { |arr| block }  -> new_ary
+ *     ary.take_while { |obj| block }  -> new_ary
  *     ary.take_while                  -> Enumerator
  *
  *  Passes elements to the block until the block returns +nil+ or +false+, then
@@ -5482,7 +5482,7 @@ rb_ary_drop(VALUE ary, VALUE n)
 
 /*
  *  call-seq:
- *     ary.drop_while { |arr| block }   -> new_ary
+ *     ary.drop_while { |obj| block }   -> new_ary
  *     ary.drop_while                  -> Enumerator
  *
  *  Drops elements up to, but not including, the first element for which the
