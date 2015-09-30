804d95f721aac0c2b68447ab8e3020a2b7225ac4
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-09-30 06:47:18 +0000

    vm_args.c: optimize symbol block passing
    * vm_args.c (vm_caller_setup_arg_block): bypass Symbol#to_proc
      call to optimize symbol block passing.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@51989 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- vm_args.c
+++ vm_args.c
@@ -776,7 +776,12 @@ vm_caller_setup_arg_block(const rb_thread_t *th, rb_control_frame_t *reg_cfp,
 
 	proc = *(--reg_cfp->sp);
 
-	if (proc != Qnil) {
+	if (SYMBOL_P(proc) && rb_method_basic_definition_p(rb_cSymbol, idTo_proc)) {
+	    calling->blockptr = RUBY_VM_GET_BLOCK_PTR_IN_CFP(reg_cfp);
+	    calling->blockptr->iseq = (rb_iseq_t *)IFUNC_NEW(rb_sym_proc_call, SYM2ID(proc), 0);
+	    calling->blockptr->proc = 0;
+	}
+	else if (!NIL_P(proc)) {
 	    if (!rb_obj_is_proc(proc)) {
 		VALUE b;
 		b = rb_check_convert_type(proc, T_DATA, "Proc", "to_proc");
