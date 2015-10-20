8744d5164d4434d73ded703063363a3c90b337b7
marcandre <marcandre@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-10-20 03:18:14 +0000

    * lib/prime.rb: Optimize Integer#prime?   Patch by Nick Slocum [Bug #10354]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@52200 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/prime.rb
+++ lib/prime.rb
@@ -31,7 +31,14 @@ class Integer
 
   # Returns true if +self+ is a prime number, else returns false.
   def prime?
-    Prime.prime?(self)
+    return self >= 2 if self <= 3
+    return false if self % 2 == 0 or self % 3 == 0
+    (5..(self**0.5).floor).step(6).each do |i|
+      if self % i == 0 || self % (i + 2) == 0
+        return false
+      end
+    end
+    true
   end
 
   # Iterates the given block over all prime numbers.
