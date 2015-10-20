96003bc9ac12f0ac7e622486bc3c3b4ae3608655
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-10-20 07:15:16 +0000

    missing.h: fix condition
    * include/ruby/missing.h (bytesequence4_or_float): define by
      configured result.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@52203 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/missing.h
+++ include/ruby/missing.h
@@ -136,7 +136,7 @@ RUBY_EXTERN double lgamma_r(double, int *);
 RUBY_EXTERN double cbrt(double);
 #endif
 
-#if !defined(INFINITY) || !defined(NAN)
+#if !defined(HAVE_INFINITY) || !defined(HAVE_NAN)
 union bytesequence4_or_float {
   unsigned char bytesequence[4];
   float float_value;
