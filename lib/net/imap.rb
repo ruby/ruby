e0ea5e903bcd5cf84c0aab5e81ab81995e6af753
shugo <shugo@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-10-23 06:29:08 +0000

    * lib/net/imap.rb: remove an empty comment line and -*-.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@52240 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/net/imap.rb
+++ lib/net/imap.rb
@@ -1,5 +1,4 @@
-#
-# -*- frozen_string_literal: true -*-
+# frozen_string_literal: true
 #
 # = net/imap.rb
 #
--- test/net/imap/test_imap.rb
+++ test/net/imap/test_imap.rb
@@ -1,5 +1,4 @@
-#
-# -*- frozen_string_literal: true -*-
+# frozen_string_literal: true
 
 require "net/imap"
 require "test/unit"
