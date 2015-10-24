e0ed0012cb5752ff39eebd245aabba5d95bce986
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-10-24 06:42:22 +0000

    relax error message format
    * bootstraptest/test_method.rb: relax error message format.
    * test/ruby/test_arity.rb (err_mess): ditto.
      [Feature #9025], [ruby-core:71178] [Bug #11617]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@52265 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- bootstraptest/test_method.rb
+++ bootstraptest/test_method.rb
@@ -3,7 +3,7 @@ assert_equal '1',       'def m() 1 end; m()'
 assert_equal '1',       'def m(a) a end; m(1)'
 assert_equal '[1, 2]',  'def m(a,b) [a, b] end; m(1,2)'
 assert_equal '[1, 2, 3]', 'def m(a,b,c) [a, b, c] end; m(1,2,3)'
-assert_equal 'wrong number of arguments (1 for 0)', %q{
+assert_match /\Awrong number of arguments \(.*\b1\b.* 0\)\z/, %q{
   def m; end
   begin
     m(1)
@@ -12,7 +12,7 @@ assert_equal 'wrong number of arguments (1 for 0)', %q{
   end
 }
 
-assert_equal 'wrong number of arguments (0 for 1)', %q{
+assert_match /\Awrong number of arguments \(.*\b0\b.* 1\)\z/, %q{
   def m a; end
   begin
     m
