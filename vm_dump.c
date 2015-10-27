17e5dff210df9aa7a9203ffdb3b78e6b9cfc5a46
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-10-27 07:13:39 +0000

    * vm_dump.c (rb_print_backtrace): our addr2line doesn't work on sparc.   http://rubyci.s3.amazonaws.com/unstable11s/ruby-trunk/log/20151027T043311Z.log.html.gz
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@52296 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- vm_dump.c
+++ vm_dump.c
@@ -686,7 +686,7 @@ rb_print_backtrace(void)
 #define MAX_NATIVE_TRACE 1024
     static void *trace[MAX_NATIVE_TRACE];
     int n = (int)backtrace(trace, MAX_NATIVE_TRACE);
-#if defined(USE_ELF) && defined(HAVE_DLADDR)
+#if defined(USE_ELF) && defined(HAVE_DLADDR) && !defined(__sparc)
     rb_dump_backtrace_with_lines(n, trace);
 #else
     char **syms = backtrace_symbols(trace, n);
