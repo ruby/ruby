d8cca44e3febd8a378d6bd51d631ccf35506b5ad
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-10-28 02:52:25 +0000

    suppress warnings
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@52315 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_process.rb
+++ test/ruby/test_process.rb
@@ -1364,7 +1364,7 @@ class TestProcess < Test::Unit::TestCase
     return unless Signal.list.include?("QUIT")
 
     with_tmpchdir do
-      cur, max = Process.getrlimit(:CORE)
+      _, max = Process.getrlimit(:CORE)
       s = assert_in_out_err([], "Process.kill(:SIGQUIT, $$);sleep 30", //, //, :rlimit_core=>[0,max])
       assert_equal([false, true, false, nil],
                    [s.exited?, s.signaled?, s.stopped?, s.success?],
