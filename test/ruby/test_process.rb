8ced6af83ffa9c9bce220adf2ccca899e41b9bc3
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-10-30 10:18:29 +0000

    explicitly overwrite signal handling
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@52391 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_process.rb
+++ test/ruby/test_process.rb
@@ -1364,7 +1364,7 @@ class TestProcess < Test::Unit::TestCase
     return unless Signal.list.include?("QUIT")
 
     with_tmpchdir do
-      s = assert_in_out_err([], "Process.kill(:SIGQUIT, $$);sleep 30", //, //, rlimit_core: 0)
+      s = assert_in_out_err([], "Signal.trap(:QUIT,'DEFAULT'); Process.kill(:SIGQUIT, $$);sleep 30", //, //, rlimit_core: 0)
       assert_equal([false, true, false, nil],
                    [s.exited?, s.signaled?, s.stopped?, s.success?],
                    "[s.exited?, s.signaled?, s.stopped?, s.success?]")
