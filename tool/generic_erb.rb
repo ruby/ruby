e71223338716ec7e1e16bfc97ed035eee516af15
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-10-31 00:02:19 +0000

    ignore dump terminal
    * tool/generic_erb.rb, tool/ifchange: no colorization if tput
      returned nothing or dump terminal.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@52395 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/generic_erb.rb
+++ tool/generic_erb.rb
@@ -24,7 +24,7 @@ end
 unchanged = "unchanged"
 updated = "updated"
 if color or (color == nil && STDOUT.tty?)
-  if (/\A(?:\e\[.*m|)\z/ =~ IO.popen("tput smso", "r", err: IO::NULL, &:read) rescue nil)
+  if (/\A\e\[.*m\z/ =~ IO.popen("tput smso", "r", err: IO::NULL, &:read) rescue nil)
     beg = "\e["
     colors = (colors = ENV['TEST_COLORS']) ? Hash[colors.scan(/(\w+)=([^:\n]*)/)] : {}
     reset = "#{beg}m"
