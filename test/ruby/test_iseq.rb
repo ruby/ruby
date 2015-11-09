17c8b5ecbac085fab04bb34305bcfe5c577c0c3e
shugo <shugo@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-11-09 08:32:37 +0000

    * compile.c (iseq_compile_each): Dynamic string literals should be   frozen.   [ruby-core:57574] [Feature #8976]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@52501 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_iseq.rb
+++ test/ruby/test_iseq.rb
@@ -175,7 +175,7 @@ class TestISeq < Test::Unit::TestCase
     s1, s2, s3, s4 = compile(code, line, {frozen_string_literal: true}).eval
     assert_predicate(s1, :frozen?)
     assert_predicate(s2, :frozen?)
-    assert_not_predicate(s3, :frozen?)
+    assert_predicate(s3, :frozen?)
     assert_predicate(s4, :frozen?)
     assert_same(s1, s2)
   end
