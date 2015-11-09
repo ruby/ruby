2074e17d9a0aad60dfb24b57e240918befa0c778
akr <akr@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-11-09 15:57:07 +0000

    add a test.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@52509 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_refinement.rb
+++ test/ruby/test_refinement.rb
@@ -1494,6 +1494,35 @@ class TestRefinement < Test::Unit::TestCase
     INPUT
   end
 
+  def test_reopen_refinement_module
+    assert_separately([], <<-"end;")
+      $VERBOSE = nil
+      class C
+      end
+
+      module R
+        refine C do
+          def m
+            :foo
+          end
+        end
+      end
+
+      using R
+      assert_equal(:foo, C.new.m)
+
+      module R
+        refine C do
+          def m
+            :bar
+          end
+        end
+      end
+
+      assert_equal(:bar, C.new.m)
+    end;
+  end
+
   private
 
   def eval_using(mod, s)
