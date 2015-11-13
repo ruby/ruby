2069c9e031fc968d6d3d0fe30a9316851e4d91d8
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-11-13 04:05:43 +0000

    prelude.rb: deprecate Thread.exclusive
    * prelude.rb (Thread.exclusive): warn as deprecated.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@52554 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- prelude.rb
+++ prelude.rb
@@ -8,6 +8,7 @@ class Thread
   # value of the block. A thread executing inside the exclusive section will
   # only block other threads which also use the Thread.exclusive mechanism.
   def self.exclusive
+    warn "Thread.exclusive is deprecated, use Mutex", caller
     MUTEX_FOR_THREAD_EXCLUSIVE.synchronize{
       yield
     }
