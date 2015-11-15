3e895a046cda712297e2d4d3b3e977bf65851784
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-11-15 18:40:58 +0000

    merge revision(s) 50985: [Backport #11693]
    	runruby.rb: don't close other fds
    
    	* tool/runruby.rb: rubyspec now requires other FDs not to be
      closed since 7b6ce1fee.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_2@52580 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.2.4"
-#define RUBY_RELEASE_DATE "2015-11-08"
-#define RUBY_PATCHLEVEL 186
+#define RUBY_RELEASE_DATE "2015-11-16"
+#define RUBY_PATCHLEVEL 187
 
 #define RUBY_RELEASE_YEAR 2015
 #define RUBY_RELEASE_MONTH 11
-#define RUBY_RELEASE_DAY 8
+#define RUBY_RELEASE_DAY 16
 
 #include "ruby/version.h"
 
