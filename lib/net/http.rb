041afd1cfb337d1d056defa118281728185beb97
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-11-30 12:17:15 +0000

    merge revision(s) 52682: [Backport #11401]
    	* lib/net/http.rb: set hostname before call ossl_ssl_set_session.
    	  [Bug #11401][ruby-core:70152][fix GH-964] Patch by @mkarnebeek
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@52799 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/net/http.rb
+++ lib/net/http.rb
@@ -914,12 +914,12 @@ module Net   #:nodoc:
             @socket.write(buf)
             HTTPResponse.read_new(@socket).value
           end
+          # Server Name Indication (SNI) RFC 3546
+          s.hostname = @address if s.respond_to? :hostname=
           if @ssl_session and
              Time.now < @ssl_session.time + @ssl_session.timeout
             s.session = @ssl_session if @ssl_session
           end
-          # Server Name Indication (SNI) RFC 3546
-          s.hostname = @address if s.respond_to? :hostname=
           Timeout.timeout(@open_timeout, Net::OpenTimeout) { s.connect }
           if @ssl_context.verify_mode != OpenSSL::SSL::VERIFY_NONE
             s.post_connection_check(@address)
