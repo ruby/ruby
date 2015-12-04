cae3905e89ebfbfffa181bf94c3ed4550ef87619
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-12-04 07:48:22 +0000

    string.c: should not taint fstring
    * string.c (rb_obj_as_string): fstring should not be infected.
      re-apply r52872 and fix a typo.
      TODO: other frozen strings also may not be.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@52882 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- KNOWNBUGS.rb
+++ KNOWNBUGS.rb
@@ -3,12 +3,3 @@
 # So all tests will cause failure.
 #
 
-assert_equal 'false', %q{
-  x = Object.new.taint
-  class << x
-    def to_s; "foo".freeze; end
-  end
-  x.taint
-  [x].join("")
-  eval '"foo".freeze.tainted?'
-}
