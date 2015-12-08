81154fcbd1da8b2f0a5b509c9d67df82753f12c2
Jose Añasco <joseanasco1@gmail.com>
2015-12-08 14:39:24 -0500

    add test case for empty strings in lib/abbrev

--- test/test_abbrev.rb
+++ test/test_abbrev.rb
@@ -51,4 +51,10 @@ class TestAbbrev < Test::Unit::TestCase
         "de"      => "de",
       }, words.abbrev('d'))
   end
+
+  def test_abbrev_empty
+    words = ["d", "", " "]
+
+    assert_equal({"d" => "d"}, words.abbrev)
+  end
 end
