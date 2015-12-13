fdfb8804c0d2665e8b82f211fe327b887f2d0b01
knu <knu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-12-13 02:43:56 +0000

    * lib/shellwords.rb: Turn on frozen-string-literal after fixing   shellsplit.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@53066 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/shellwords.rb
+++ lib/shellwords.rb
@@ -1,4 +1,4 @@
-# frozen-string-literal: false
+# frozen-string-literal: true
 ##
 # == Manipulates strings like the UNIX Bourne shell
 #
@@ -70,14 +70,14 @@ module Shellwords
   #   argv #=> ["here", "are", "two words"]
   def shellsplit(line)
     words = []
-    field = ''
+    field = String.new
     line.scan(/\G\s*(?>([^\s\\\'\"]+)|'([^\']*)'|"((?:[^\"\\]|\\.)*)"|(\\.?)|(\S))(\s|\z)?/m) do
       |word, sq, dq, esc, garbage, sep|
       raise ArgumentError, "Unmatched double quote: #{line.inspect}" if garbage
       field << (word || sq || (dq || esc).gsub(/\\(.)/, '\\1'))
       if sep
         words << field
-        field = ''
+        field = String.new
       end
     end
     words
