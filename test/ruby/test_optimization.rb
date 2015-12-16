703a24a9478e83925e4275d126bb54848a72076b
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-12-16 07:11:41 +0000

    merge revision(s) 53039: [Backport #11804]
    	* insns.def (opt_case_dispatch): avoid converting Infinity
    
    	* test/ruby/test_optimization.rb (test_opt_case_dispatch_inf): new
    	  [ruby-dev:49423] [Bug #11804]'
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@53147 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_optimization.rb
+++ test/ruby/test_optimization.rb
@@ -242,4 +242,15 @@ class TestRubyOptimization < Test::Unit::TestCase
       end;
     end
   end
+
+  def test_opt_case_dispatch_inf
+    inf = 1.0/0.0
+    result = case inf
+             when 1 then 1
+             when 0 then 0
+             else
+               inf.to_i rescue nil
+             end
+    assert_nil result, '[ruby-dev:49423] [Bug #11804]'
+  end
 end
