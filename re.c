2fd5719e62a247ab57b7d45a42909a11759de2b0
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-12-17 00:46:07 +0000

    re.c: names encoding
    * re.c (reg_names_iter): should consider encoding of regexp.
      [ruby-core:72185] [Bug #11825]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@53167 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- re.c
+++ re.c
@@ -757,7 +757,7 @@ reg_names_iter(const OnigUChar *name, const OnigUChar *name_end,
           int back_num, int *back_refs, OnigRegex regex, void *arg)
 {
     VALUE ary = (VALUE)arg;
-    rb_ary_push(ary, rb_str_new((const char *)name, name_end-name));
+    rb_ary_push(ary, rb_enc_str_new((const char *)name, name_end-name, regex->enc));
     return 0;
 }
 
