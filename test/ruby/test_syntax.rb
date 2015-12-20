b9623dbdb805998cb975adacb62df362ac567014
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2015-12-20 02:49:46 +0000

    test_syntax.rb: assertions for other heredocs
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@53215 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_syntax.rb
+++ test/ruby/test_syntax.rb
@@ -345,7 +345,11 @@ WARN
   def test_heredoc_after_label
     bug11849 = '[ruby-core:72396] [Bug #11849]'
     assert_valid_syntax("{label:<<DOC\n""DOC\n""}", bug11849)
+    assert_valid_syntax("{label:<<-DOC\n""DOC\n""}", bug11849)
+    assert_valid_syntax("{label:<<~DOC\n""DOC\n""}", bug11849)
     assert_valid_syntax("{label: <<DOC\n""DOC\n""}", bug11849)
+    assert_valid_syntax("{label: <<-DOC\n""DOC\n""}", bug11849)
+    assert_valid_syntax("{label: <<~DOC\n""DOC\n""}", bug11849)
   end
 
   def test_duplicated_arg
