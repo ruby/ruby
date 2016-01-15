fd7b490fc3dc33c22b7c173617363953fc8453c8
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-01-15 13:15:39 +0000

    disable unaligned word access
    * include/ruby/defines.h, st.c: disable unaligned word access with
      gcc 6 or later.  [Bug #11831]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@53546 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/defines.h
+++ include/ruby/defines.h
@@ -260,6 +260,10 @@ void rb_ia64_flushrs(void);
     RUBY_ALIAS_FUNCTION_TYPE(VALUE, prot, name, args)
 #endif
 
+#if !defined(UNALIGNED_WORD_ACCESS) && defined(__GNUC__) && __GNUC__ >= 6
+# define UNALIGNED_WORD_ACCESS 0
+#endif
+
 #ifndef UNALIGNED_WORD_ACCESS
 # if defined(__i386) || defined(__i386__) || defined(_M_IX86) || \
      defined(__x86_64) || defined(__x86_64__) || defined(_M_AMD64) || \
