5df321fb1dfdfc032d882f7e160279512c8078b8
ngoto <ngoto@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-01-15 11:25:29 +0000

    * regint.h (PLATFORM_UNALIGNED_WORD_ACCESS): The value of   UNALIGNED_WORD_ACCESS should be used to determine whether   unaligned word access is allowed or not. After this commit,   ./configure CPPFLAGS="-DUNALIGNED_WORD_ACCESS=0" disables   unaligned word access even on platforms that support the feature.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@53543 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- regint.h
+++ regint.h
@@ -48,10 +48,18 @@
 #endif
 #endif
 
+#ifndef UNALIGNED_WORD_ACCESS
 #if defined(__i386) || defined(__i386__) || defined(_M_IX86) || \
     defined(__x86_64) || defined(__x86_64__) || defined(_M_AMD64) || \
     defined(__powerpc64__) || \
     defined(__mc68020__)
+#define UNALIGNED_WORD_ACCESS 1
+#else
+#define UNALIGNED_WORD_ACCESS 0
+#endif
+#endif
+
+#if UNALIGNED_WORD_ACCESS
 #define PLATFORM_UNALIGNED_WORD_ACCESS
 #endif
 
