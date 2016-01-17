ddd40c73b622fb1854a6b4fee39c41d154328fbf
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-01-17 05:43:04 +0000

    Add a test case for ENV#reject_bang,delete_if
    * test/ruby/test_env.rb: [Fix GH-1201]
      * Extract test code for ENV#delete_if from ENV#reject_bang
      * Add a test case for ENV#reject_bang,delete_if
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@53559 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_env.rb
+++ test/ruby/test_env.rb
@@ -188,6 +188,10 @@ class TestEnv < Test::Unit::TestCase
     ENV.each_pair {|k, v| h2[k] = v }
     assert_equal(h1, h2)
 
+    assert_nil(ENV.reject! {|k, v| IGNORE_CASE ? k.upcase == "TEST" : k == "test" })
+  end
+
+  def test_delete_if
     h1 = {}
     ENV.each_pair {|k, v| h1[k] = v }
     ENV["test"] = "foo"
@@ -195,6 +199,8 @@ class TestEnv < Test::Unit::TestCase
     h2 = {}
     ENV.each_pair {|k, v| h2[k] = v }
     assert_equal(h1, h2)
+
+    assert_equal(ENV, ENV.delete_if {|k, v| IGNORE_CASE ? k.upcase == "TEST" : k == "test" })
   end
 
   def test_select_bang
