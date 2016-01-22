df424c68e791e473b230009113cc913543b05def
K0mA <mctj1218@gmail.com>
2016-01-22 19:01:52 +0900

    Add test for String#next and String#succ.

--- test/ruby/test_string.rb
+++ test/ruby/test_string.rb
@@ -995,6 +995,9 @@ class TestString < Test::Unit::TestCase
     assert_equal(S("AAAAA000"), S("ZZZZ999").next)
 
     assert_equal(S("*+"), S("**").next)
+
+    assert_equal(S("!"), S(" ").next)
+    assert_equal(S(""), S("").next)
   end
 
   def test_next!
@@ -1031,6 +1034,12 @@ class TestString < Test::Unit::TestCase
     a = S("**")
     assert_equal(S("*+"), a.next!)
     assert_equal(S("*+"), a)
+
+    a = S(" ")
+    assert_equal(S("!"), a.next!)
+
+    a = S("")
+    assert_equal(S(""), a.next!)
   end
 
   def test_oct
@@ -1541,6 +1550,9 @@ class TestString < Test::Unit::TestCase
     assert_equal("2000aaa", "1999zzz".succ)
     assert_equal("AAAA0000", "ZZZ9999".succ)
     assert_equal("**+", "***".succ)
+
+    assert_equal("!", " ".succ)
+    assert_equal("", "".succ)
   end
 
   def test_succ!
@@ -1581,6 +1593,12 @@ class TestString < Test::Unit::TestCase
     a = S("No.9")
     assert_equal(S("No.10"), a.succ!)
     assert_equal(S("No.10"), a)
+
+    a = S(" ")
+    assert_equal(S("!"), a.succ!)
+
+    a = S("")
+    assert_equal(S(""), a.succ!)
 
     assert_equal("aaaaaaaaaaaa", "zzzzzzzzzzz".succ!)
     assert_equal("aaaaaaaaaaaaaaaaaaaaaaaa", "zzzzzzzzzzzzzzzzzzzzzzz".succ!)
