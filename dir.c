9145a64be0f80a8f3c6fa1dd7b8c72a13eaa33ff
Nobuyoshi Nakada <nobu@ruby-lang.org>
2016-01-23 23:44:39 +0900

    dir.c: use ATTR_DIR_ENTRYCOUNT
    * dir.c (rb_dir_s_empty_p): optimize by getattrlist() and
      ATTR_DIR_ENTRYCOUNT on HFS.

--- dir.c
+++ dir.c
@@ -2655,6 +2655,25 @@ rb_dir_s_empty_p(VALUE obj, VALUE dirname)
     dirname = rb_str_dup_frozen(dirname);
     path = RSTRING_PTR(dirname);
 
+#if defined HAVE_GETATTRLIST && defined ATTR_DIR_ENTRYCOUNT
+    {
+	u_int32_t attrbuf[SIZEUP32(fsobj_tag_t)];
+	struct attrlist al = {ATTR_BIT_MAP_COUNT, 0, ATTR_CMN_OBJTAG,};
+	if (getattrlist(path, &al, attrbuf, sizeof(attrbuf), 0) != 0)
+	    rb_sys_fail_path(orig);
+	if (*(const fsobj_tag_t *)(attrbuf+1) == VT_HFS) {
+	    al.commonattr = 0;
+	    al.dirattr = ATTR_DIR_ENTRYCOUNT;
+	    if (getattrlist(path, &al, attrbuf, sizeof(attrbuf), 0) == 0) {
+		if (attrbuf[0] >= 2 * sizeof(u_int32_t))
+		    return attrbuf[1] ? Qfalse : Qtrue;
+		if (false_on_notdir) return Qfalse;
+	    }
+	    rb_sys_fail_path(orig);
+	}
+    }
+#endif
+
     dir = opendir(path);
     if (!dir) {
 	int e = errno;
