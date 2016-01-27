b7b5692aea3e1db3dfefb2e05fbc564235f09a63
hsbt <hsbt@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-01-27 12:04:47 +0000

    * test/-ext-/string/test_capacity.rb: Added missing library.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@53674 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/-ext-/string/test_capacity.rb
+++ test/-ext-/string/test_capacity.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require 'test/unit'
 require '-test-/string'
+require 'rbconfig/sizeof'
 
 class Test_StringCapacity < Test::Unit::TestCase
   def test_capacity_embeded
