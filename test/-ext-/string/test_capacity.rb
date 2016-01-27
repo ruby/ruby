d14f0cfd168e3ce760effd5484b1d4e76f41e65e
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-01-27 11:37:02 +0000

    Add tests about String's internal capacity
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@53671 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ test/-ext-/string/test_capacity.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require 'test/unit'
+require '-test-/string'
+
+class Test_StringCapacity < Test::Unit::TestCase
+  def test_capacity_embeded
+    size = RbConfig::SIZEOF['void*'] * 3 - 1
+    assert_equal size, Bug::String.capacity('foo')
+  end
+
+  def test_capacity_shared
+    assert_equal 0, Bug::String.capacity(:abcdefghijklmnopqrstuvwxyz.to_s)
+  end
+
+  def test_capacity_normal
+    assert_equal 128, Bug::String.capacity('1'*128)
+  end
+end
