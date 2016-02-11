8e30c7b48e38bb4548ea710e56d4a283d40e2a25
Tadashi Saito <tad.a.digger@gmail.com>
2016-02-11 19:08:24 +0900

    optimize Rational#fdiv. 1.5~2.8x faster

--- rational.c
+++ rational.c
@@ -33,7 +33,7 @@
 VALUE rb_cRational;
 
 static ID id_abs, id_cmp, id_eqeq_p, id_expt, id_fdiv,
-    id_idiv, id_integer_p, id_negate, id_to_f,
+    id_idiv, id_integer_p, id_negate,
     id_to_i, id_truncate, id_i_num, id_i_den;
 
 #define f_boolcast(x) ((x) ? Qtrue : Qfalse)
@@ -131,13 +131,6 @@ f_to_i(VALUE x)
     if (RB_TYPE_P(x, T_STRING))
 	return rb_str_to_inum(x, 10, 0);
     return rb_funcall(x, id_to_i, 0);
-}
-inline static VALUE
-f_to_f(VALUE x)
-{
-    if (RB_TYPE_P(x, T_STRING))
-	return DBL2NUM(rb_str_to_dbl(x, 0));
-    return rb_funcall(x, id_to_f, 0);
 }
 
 inline static VALUE
@@ -944,9 +937,17 @@ nurat_div(VALUE self, VALUE other)
 static VALUE
 nurat_fdiv(VALUE self, VALUE other)
 {
+    VALUE div;
     if (f_zero_p(other))
-	return f_div(self, f_to_f(other));
-    return f_to_f(f_div(self, other));
+	return DBL2NUM(RFLOAT_VALUE(nurat_to_f(self)) / 0.0);
+    if (FIXNUM_P(other) && FIX2LONG(other) == 1)
+	return nurat_to_f(self);
+    div = nurat_div(self, other);
+    if (RB_TYPE_P(div, T_RATIONAL))
+	return nurat_to_f(div);
+    if (RB_TYPE_P(div, T_FLOAT))
+	return div;
+    return rb_funcall(div, rb_intern("to_f"), 0);
 }
 
 inline static VALUE
@@ -2536,7 +2537,6 @@ Init_Rational(void)
     id_idiv = rb_intern("div");
     id_integer_p = rb_intern("integer?");
     id_negate = rb_intern("-@");
-    id_to_f = rb_intern("to_f");
     id_to_i = rb_intern("to_i");
     id_truncate = rb_intern("truncate");
     id_i_num = rb_intern("@numerator");
