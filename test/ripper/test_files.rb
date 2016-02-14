145ee4b44605266f61eb190a3a6e41cfea9cff73
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-02-14 16:38:32 +0000

    merge revision(s) 53411,53425: [Backport #11932]
    	* parse.y (regexp): set_yylval_num sets u1, should use nd_tag
    	  instead of nd_state.  [ruby-core:72638] [Bug #11932]
    
    	* parse.y (set_yylval_num): should be used as nd_state, set to u3.
    	  [ruby-core:72638] [Bug #11932]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_2@53824 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ripper/test_files.rb
+++ test/ripper/test_files.rb
@@ -6,14 +6,14 @@ class TestRipper::Generic < Test::Unit::TestCase
     srcdir = File.expand_path("../../..", __FILE__)
     assert_separately(%W[--disable-gem -rripper - #{srcdir}],
                       __FILE__, __LINE__, <<-'eom', timeout: Float::INFINITY)
-      TEST_RATIO = 0.05 # testing all files needs too long time...
+      TEST_RATIO = (ENV["TEST_RIPPER_RATIO"].nil? ? 0.05 : ENV["TEST_RIPPER_RATIO"].to_f) # testing all files needs too long time...
       class Parser < Ripper
         PARSER_EVENTS.each {|n| eval "def on_#{n}(*args) r = [:#{n}, *args]; r.inspect; Object.new end" }
         SCANNER_EVENTS.each {|n| eval "def on_#{n}(*args) r = [:#{n}, *args]; r.inspect; Object.new end" }
       end
       dir = ARGV.shift
       for script in Dir["#{dir}/{lib,sample,ext,test}/**/*.rb"].sort
-        next if TEST_RATIO < rand
+        next if TEST_RATIO and TEST_RATIO < rand
         assert_nothing_raised("ripper failed to parse: #{script.inspect}") {
           Parser.new(File.read(script), script).parse
         }
