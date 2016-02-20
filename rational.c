4974b59e7dbaabedca554e99e610276705c30d5c
Tadashi Saito <tad.a.digger@gmail.com>
2016-02-20 17:46:44 +0900

    refactoring: remove needless marcos

--- rational.c
+++ rational.c
@@ -2023,9 +2023,6 @@ float_decode(VALUE self)
 }
 #endif
 
-#define id_lshift rb_intern("<<")
-#define f_lshift(x,n) rb_funcall((x), id_lshift, 1, (n))
-
 /*
  * call-seq:
  *    flt.to_r  ->  rational
