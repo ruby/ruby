782b9a89380c80ba4eb2276b4974e25a3b329921
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-02-23 23:25:27 +0000

    merge revision(s) 53202: [Backport #11843]
    	* enc/windows_1250.c: Should not use C++ style comments (C99 feature).
    	  [Bug #11843]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_2@53909 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.2.5"
-#define RUBY_RELEASE_DATE "2016-02-23"
-#define RUBY_PATCHLEVEL 240
+#define RUBY_RELEASE_DATE "2016-02-24"
+#define RUBY_PATCHLEVEL 241
 
 #define RUBY_RELEASE_YEAR 2016
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 23
+#define RUBY_RELEASE_DAY 24
 
 #include "ruby/version.h"
 
