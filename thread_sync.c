f708fda780db119f3240a1109ec6f21fe93ad9bb
Anton Davydov <antondavydov.o@gmail.com>
2016-03-17 00:26:27 +0300

    Update documentation for Queue class description [skip ci]

--- thread_sync.c
+++ thread_sync.c
@@ -632,28 +632,34 @@ queue_do_close(VALUE self, int is_szq)
 /*
  *  Document-class: Queue
  *
- *  This class provides a way to synchronize communication between threads.
+ *  The Queue class implements multi-producer, multi-consumer queues.
+ *  It is especially useful in threaded programming when information
+ *  must be exchanged safely between multiple threads. The Queue class
+ *  implements all the required locking semantics.
+ *
+ *  The class implements FIFO type of queue. In a FIFO queue, the first
+ *  tasks added are the first retrieved.
  *
  *  Example:
  *
- *	require 'thread'
- *    	queue = Queue.new
+ *    require 'thread'
+ *    queue = Queue.new
  *
- *	producer = Thread.new do
- *	  5.times do |i|
- *	     sleep rand(i) # simulate expense
- *	     queue << i
- *	     puts "#{i} produced"
- *	  end
- *	end
+ *    producer = Thread.new do
+ *      5.times do |i|
+ *        sleep rand(i) # simulate expense
+ *        queue << i
+ *        puts "#{i} produced"
+ *      end
+ *    end
  *
- *	consumer = Thread.new do
- *	  5.times do |i|
- *	     value = queue.pop
- *	     sleep rand(i/2) # simulate expense
- *	     puts "consumed #{value}"
- *	  end
- *	end
+ *    consumer = Thread.new do
+ *      5.times do |i|
+ *        value = queue.pop
+ *        sleep rand(i/2) # simulate expense
+ *        puts "consumed #{value}"
+ *      end
+ *    end
  *
  */
 
