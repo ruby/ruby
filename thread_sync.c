fe9817618d0d3b382e440e6fbd08540b53354986
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-03-17 08:20:29 +0000

    thread_sync.c: Update rdoc for Queue [skip ci]
    * thread_sync.c: [DOC] Update documentation for Queue class
      description.  [Fix GH-1292]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@54146 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- thread_sync.c
+++ thread_sync.c
@@ -632,7 +632,13 @@ queue_do_close(VALUE self, int is_szq)
 /*
  *  Document-class: Queue
  *
- *  This class provides a way to synchronize communication between threads.
+ *  The Queue class implements multi-producer, multi-consumer queues.
+ *  It is especially useful in threaded programming when information
+ *  must be exchanged safely between multiple threads. The Queue class
+ *  implements all the required locking semantics.
+ *
+ *  The class implements FIFO type of queue. In a FIFO queue, the first
+ *  tasks added are the first retrieved.
  *
  *  Example:
  *
