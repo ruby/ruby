d3c464d9056c633a94c72402d78f5d0de22608b2
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-03-28 14:18:47 +0000

    merge revision(s) 53455: [Backport #11962]
    	* ruby.h: undef HAVE_BUILTIN___BUILTIN_CHOOSE_EXPR_CONSTANT_P
    	  and HAVE_BUILTIN___BUILTIN_TYPES_COMPATIBLE_P on C++.
    	  [ruby-core:72736] [Bug #11962]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_2@54315 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.2.5"
-#define RUBY_RELEASE_DATE "2016-03-24"
-#define RUBY_PATCHLEVEL 257
+#define RUBY_RELEASE_DATE "2016-03-28"
+#define RUBY_PATCHLEVEL 258
 
 #define RUBY_RELEASE_YEAR 2016
 #define RUBY_RELEASE_MONTH 3
-#define RUBY_RELEASE_DAY 24
+#define RUBY_RELEASE_DAY 28
 
 #include "ruby/version.h"
 
