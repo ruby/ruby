3409e318f3079c714912c748033e81776b197acf
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-03-29 06:05:18 +0000

    merge revision(s) 54022: [Backport #11893]
    	* ext/tk/lib/tkextlib/tcllib/tablelist_tile.rb: fix method name typo.
    	  [ruby-core:72513] [Bug #11893] The patch provided by Akira Matsuda.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_3@54368 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/tk/lib/tkextlib/tcllib/tablelist_tile.rb
+++ ext/tk/lib/tkextlib/tcllib/tablelist_tile.rb
@@ -16,7 +16,7 @@ unless defined? Tk::Tcllib::Tablelist_usingTile
   Tk::Tcllib::Tablelist_usingTile = true
 end
 
-requrie 'tkextlib/tcllib/tablelist_core'
+require 'tkextlib/tcllib/tablelist_core'
 
 module Tk
   module Tcllib
