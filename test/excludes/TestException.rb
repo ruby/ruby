0ebf3849e4dee70b65dec91997400a235411dc15
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-04-11 08:27:19 +0000

    TestException.rb: suppress warning
    * test/excludes/TestException.rb: suppress "ambiguous first
      argument" warning.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@54541 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/excludes/TestException.rb
+++ test/excludes/TestException.rb
@@ -5,5 +5,4 @@ sections including external libraries, it is very neary impossible to
 recover from such situation.
 ]
 
-exclude /test_machine_stackoverflow/, reason
-exclude :test_machine_stackoverflow_by_define_method, reason
+exclude %r[test_machine_stackoverflow], reason
