54c011a92fcc93dbf3bd5f4cb587196510fb3d1f
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-04-22 08:49:52 +0000

    merge revision(s) 54307: [Backport #12193]
    	* lib/rubygems/test_case.rb: Fix test on Windows for inconsistent temp path.
    	  https://github.com/rubygems/rubygems/pull/1554
    	  [Bug #12193][ruby-core:74431]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_1@54689 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.1.10"
-#define RUBY_RELEASE_DATE "2016-04-01"
-#define RUBY_PATCHLEVEL 492
+#define RUBY_RELEASE_DATE "2016-04-22"
+#define RUBY_PATCHLEVEL 493
 
 #define RUBY_RELEASE_YEAR 2016
 #define RUBY_RELEASE_MONTH 4
-#define RUBY_RELEASE_DAY 1
+#define RUBY_RELEASE_DAY 22
 
 #include "ruby/version.h"
 
