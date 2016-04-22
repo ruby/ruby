ab3376b99289c50d7dee06f4dcb86e6a0e608b55
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-04-22 16:10:08 +0000

    merge revision(s) 54681: [Backport #12302]
    	* eval_jump.c (exec_end_procs_chain): restore previous error info
    	  for each end procs.  [ruby-core:75038] [Bug #12302]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_3@54718 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- eval_jump.c
+++ eval_jump.c
@@ -94,10 +94,11 @@ rb_mark_end_proc(void)
 }
 
 static void
-exec_end_procs_chain(struct end_proc_data *volatile *procs)
+exec_end_procs_chain(struct end_proc_data *volatile *procs, VALUE *errp)
 {
     struct end_proc_data volatile endproc;
     struct end_proc_data *link;
+    VALUE errinfo = *errp;
 
     while ((link = *procs) != 0) {
 	*procs = link->next;
@@ -105,6 +106,7 @@ exec_end_procs_chain(struct end_proc_data *volatile *procs)
 	xfree(link);
 	rb_set_safe_level_force(endproc.safe);
 	(*endproc.func) (endproc.data);
+	*errp = errinfo;
     }
 }
 
@@ -119,8 +121,8 @@ rb_exec_end_proc(void)
     TH_PUSH_TAG(th);
     if ((status = EXEC_TAG()) == 0) {
       again:
-	exec_end_procs_chain(&ephemeral_end_procs);
-	exec_end_procs_chain(&end_procs);
+	exec_end_procs_chain(&ephemeral_end_procs, &th->errinfo);
+	exec_end_procs_chain(&end_procs, &th->errinfo);
     }
     else {
 	VAR_INITIALIZED(th);
