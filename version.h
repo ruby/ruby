7f36210ab24273c3831f41704852a11a47c6648e
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-04-25 17:53:49 +0000

    merge revision(s) 54758:
    	sizes.c.tmpl: extra semicolon
    
    	* template/sizes.c.tmpl (DEFINE): remove extra semicolon.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_3@54764 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.3.0"
-#define RUBY_RELEASE_DATE "2016-04-25"
-#define RUBY_PATCHLEVEL 108
+#define RUBY_RELEASE_DATE "2016-04-26"
+#define RUBY_PATCHLEVEL 109
 
 #define RUBY_RELEASE_YEAR 2016
 #define RUBY_RELEASE_MONTH 4
-#define RUBY_RELEASE_DAY 25
+#define RUBY_RELEASE_DAY 26
 
 #include "ruby/version.h"
 
