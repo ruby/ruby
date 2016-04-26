ba1bd67dbecb5922a28ef61df98e1a09a234dc1b
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-04-26 04:17:39 +0000

    merge revision(s) 54681: [Backport #12302]
    	* eval_jump.c (exec_end_procs_chain): restore previous error info
    	  for each end procs.  [ruby-core:75038] [Bug #12302]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_2@54769 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.2.5"
-#define RUBY_RELEASE_DATE "2016-04-22"
-#define RUBY_PATCHLEVEL 314
+#define RUBY_RELEASE_DATE "2016-04-26"
+#define RUBY_PATCHLEVEL 315
 
 #define RUBY_RELEASE_YEAR 2016
 #define RUBY_RELEASE_MONTH 4
-#define RUBY_RELEASE_DAY 22
+#define RUBY_RELEASE_DAY 26
 
 #include "ruby/version.h"
 
