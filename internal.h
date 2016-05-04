0e46a3d01619c0ab21b46787a2bf3930a2344e41
Tadashi Saito <tad.a.digger@gmail.com>
2016-05-04 13:36:32 +0900

    export int_cmp() as rb_int_cmp()

--- internal.h
+++ internal.h
@@ -1168,6 +1168,7 @@ VALUE rb_int_ge(VALUE x, VALUE y);
 enum ruby_num_rounding_mode rb_num_get_rounding_option(VALUE opts);
 VALUE rb_int_pow(VALUE x, VALUE y);
 VALUE rb_float_pow(VALUE x, VALUE y);
+VALUE rb_int_cmp(VALUE x, VALUE y);
 
 #if USE_FLONUM
 #define RUBY_BIT_ROTL(v, n) (((v) << (n)) | ((v) >> ((sizeof(v) * 8) - n)))
