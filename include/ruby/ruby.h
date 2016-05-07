a5e1a2f8b431f1925a8fa415cd49b13012edd775
cremno <cremno@mail.ru>
2016-05-07 16:09:14 +0200

    drop Atari ST support

--- include/ruby/ruby.h
+++ include/ruby/ruby.h
@@ -58,11 +58,9 @@ RUBY_SYMBOL_EXPORT_BEGIN
 
 /* Make alloca work the best possible way.  */
 #ifdef __GNUC__
-# ifndef atarist
-#  ifndef alloca
-#   define alloca __builtin_alloca
-#  endif
-# endif	/* atarist */
+# ifndef alloca
+#  define alloca __builtin_alloca
+# endif
 #else
 # ifdef HAVE_ALLOCA_H
 #  include <alloca.h>
