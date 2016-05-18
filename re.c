a6c38cb4d35d8d94b408e3b690675d2bb91f237c
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-05-18 15:17:31 +0000

    * re.c (reg_names_iter): specify capacify
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@55065 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- re.c
+++ re.c
@@ -780,8 +780,9 @@ reg_names_iter(const OnigUChar *name, const OnigUChar *name_end,
 static VALUE
 rb_reg_names(VALUE re)
 {
-    VALUE ary = rb_ary_new();
+    VALUE ary;
     rb_reg_check(re);
+    ary = rb_ary_new_capa(onig_number_of_names(RREGEXP_PTR(re)));
     onig_foreach_name(RREGEXP_PTR(re), reg_names_iter, (void*)ary);
     return ary;
 }
