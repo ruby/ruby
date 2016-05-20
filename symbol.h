8a775968586a1d3f1a3520ffe0b49989d59d3ae4
ngoto <ngoto@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-05-20 13:33:10 +0000

    Use HAVE_BUILTIN___BUILTIN_CONSTANT_P
    * symbol.h (rb_id2sym): Use HAVE_BUILTIN___BUILTIN_CONSTANT_P
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@55084 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- symbol.h
+++ symbol.h
@@ -17,10 +17,10 @@
 #define DYNAMIC_ID_P(id) (!(id&ID_STATIC_SYM)&&id>tLAST_OP_ID)
 #define STATIC_ID2SYM(id)  (((VALUE)(id)<<RUBY_SPECIAL_SHIFT)|SYMBOL_FLAG)
 
-#ifdef __GNUC__
+#ifdef HAVE_BUILTIN___BUILTIN_CONSTANT_P
 #define rb_id2sym(id) \
-    __extension__(__builtin_constant_p(id) && !DYNAMIC_ID_P(id) ? \
-		  STATIC_ID2SYM(id) : rb_id2sym(id))
+    RB_GNUC_EXTENSION_BLOCK(__builtin_constant_p(id) && !DYNAMIC_ID_P(id) ? \
+			    STATIC_ID2SYM(id) : rb_id2sym(id))
 #endif
 
 struct RSymbol {
