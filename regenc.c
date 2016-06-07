61b3be7dbbd3090df355e4a23e5d73caec0bdc82
duerst <duerst@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-06-07 03:55:37 +0000

    * regenc.c (onigenc_not_support_case_map): Move to end of file;   (onigenc_single_byte_ascii_only_case_map): Add new function.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@55302 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- regenc.c
+++ regenc.c
@@ -576,38 +576,6 @@ onigenc_not_support_get_ctype_code_range(OnigCtype ctype ARG_UNUSED,
   return ONIG_NO_SUPPORT_CONFIG;
 }
 
-#ifdef ONIG_CASE_MAPPING
-extern int
-onigenc_not_support_case_map (OnigCaseFoldType* flagP, const OnigUChar** pp, const OnigUChar* end,
-			      OnigUChar* to, OnigUChar* to_end, const struct OnigEncodingTypeST* enc)
-{
-  OnigCodePoint code;
-  OnigUChar *to_start = to;
-  OnigCaseFoldType flags = *flagP;
-  int codepoint_length;
-
-  to_end -= 4; /* longest possible length of a single character */
-
-  while (*pp<end && to<=to_end) {
-    codepoint_length = ONIGENC_PRECISE_MBC_ENC_LEN(enc, *pp, end);
-    if (codepoint_length < 0)
-      return codepoint_length; /* encoding invalid */
-    code = ONIGENC_MBC_TO_CODE(enc, *pp, end);
-    *pp += codepoint_length;
-
-    if (code>='a' && code<='z' && (flags&ONIGENC_CASE_UPCASE))
-      flags |= ONIGENC_CASE_MODIFIED,  code += 'A'-'a';
-    else if (code>='A' && code<='Z' && (flags&(ONIGENC_CASE_DOWNCASE|ONIGENC_CASE_FOLD)))
-      flags |= ONIGENC_CASE_MODIFIED,  code += 'a'-'A';
-    to += ONIGENC_CODE_TO_MBC(enc, code, to);
-    if (flags & ONIGENC_CASE_TITLECASE)  /* switch from titlecase to lowercase for capitalize */
-      flags ^= (ONIGENC_CASE_UPCASE|ONIGENC_CASE_DOWNCASE|ONIGENC_CASE_TITLECASE);
-  }
-  *flagP = flags;
-  return (int)(to-to_start);
-}
-#endif   /* ONIG_CASE_MAPPING */
-
 extern int
 onigenc_is_mbc_newline_0x0a(const UChar* p, const UChar* end, OnigEncoding enc ARG_UNUSED)
 {
@@ -989,3 +957,59 @@ onigenc_property_list_init(int (*f)(void))
   THREAD_ATOMIC_END;
   return r;
 }
+
+#ifdef ONIG_CASE_MAPPING
+extern int
+onigenc_not_support_case_map (OnigCaseFoldType* flagP, const OnigUChar** pp, const OnigUChar* end,
+			      OnigUChar* to, OnigUChar* to_end, const struct OnigEncodingTypeST* enc)
+{
+  OnigCodePoint code;
+  OnigUChar *to_start = to;
+  OnigCaseFoldType flags = *flagP;
+  int codepoint_length;
+
+  to_end -= 4; /* longest possible length of a single character */
+
+  while (*pp<end && to<=to_end) {
+    codepoint_length = ONIGENC_PRECISE_MBC_ENC_LEN(enc, *pp, end);
+    if (codepoint_length < 0)
+      return codepoint_length; /* encoding invalid */
+    code = ONIGENC_MBC_TO_CODE(enc, *pp, end);
+    *pp += codepoint_length;
+
+    if (code>='a' && code<='z' && (flags&ONIGENC_CASE_UPCASE))
+      flags |= ONIGENC_CASE_MODIFIED,  code += 'A'-'a';
+    else if (code>='A' && code<='Z' && (flags&(ONIGENC_CASE_DOWNCASE|ONIGENC_CASE_FOLD)))
+      flags |= ONIGENC_CASE_MODIFIED,  code += 'a'-'A';
+    to += ONIGENC_CODE_TO_MBC(enc, code, to);
+    if (flags & ONIGENC_CASE_TITLECASE)  /* switch from titlecase to lowercase for capitalize */
+      flags ^= (ONIGENC_CASE_UPCASE|ONIGENC_CASE_DOWNCASE|ONIGENC_CASE_TITLECASE);
+  }
+  *flagP = flags;
+  return (int)(to-to_start);
+}
+
+extern int
+onigenc_single_byte_ascii_only_case_map (OnigCaseFoldType* flagP, const OnigUChar** pp,
+					 const OnigUChar* end, OnigUChar* to, OnigUChar* to_end,
+					 const struct OnigEncodingTypeST* enc)
+{
+  OnigCodePoint code;
+  OnigUChar *to_start = to;
+  OnigCaseFoldType flags = *flagP;
+
+  while (*pp<end && to<to_end) {
+    code = *(*pp)++;
+
+    if (code>='a' && code<='z' && (flags&ONIGENC_CASE_UPCASE))
+      flags |= ONIGENC_CASE_MODIFIED,  code += 'A'-'a';
+    else if (code>='A' && code<='Z' && (flags&(ONIGENC_CASE_DOWNCASE|ONIGENC_CASE_FOLD)))
+      flags |= ONIGENC_CASE_MODIFIED,  code += 'a'-'A';
+    *to++ = code;
+    if (flags & ONIGENC_CASE_TITLECASE)  /* switch from titlecase to lowercase for capitalize */
+      flags ^= (ONIGENC_CASE_UPCASE|ONIGENC_CASE_DOWNCASE|ONIGENC_CASE_TITLECASE);
+  }
+  *flagP = flags;
+  return (int)(to-to_start);
+}
+#endif   /* ONIG_CASE_MAPPING */
