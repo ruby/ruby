b5960a63b1769e114c4d674140aa5f18a33eef27
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-06-08 01:09:23 +0000

    ext/date/zonetab.list: gperf timezone names
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@55320 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- common.mk
+++ common.mk
@@ -788,7 +788,9 @@ EXT_SRCS = $(srcdir)/ext/ripper/ripper.c \
 
 srcs-ext: $(EXT_SRCS)
 
-srcs-extra: $(srcdir)/ext/json/parser/parser.c
+srcs-extra: $(srcdir)/ext/json/parser/parser.c \
+	    $(srcdir)/ext/date/zonetab.c \
+	    $(empty)
 
 LIB_SRCS = $(srcdir)/lib/unicode_normalize/tables.rb
 
@@ -885,6 +887,11 @@ $(srcdir)/ext/ripper/ripper.c: $(srcdir)/parse.y id.h
 		RUBY="$(BASERUBY)" PATH_SEPARATOR="$(PATH_SEPARATOR)"
 
 $(srcdir)/ext/json/parser/parser.c: $(srcdir)/ext/json/parser/parser.rl
+	$(ECHO) generating $@
+	$(Q) $(CHDIR) $(@D) && $(exec) $(MAKE) -f prereq.mk $(MFLAGS) \
+		Q=$(Q) ECHO=$(ECHO) top_srcdir=../../.. srcdir=. VPATH=../../.. BASERUBY="$(BASERUBY)"
+
+$(srcdir)/ext/date/zonetab.c: $(srcdir)/ext/date/zonetab.list
 	$(ECHO) generating $@
 	$(Q) $(CHDIR) $(@D) && $(exec) $(MAKE) -f prereq.mk $(MFLAGS) \
 		Q=$(Q) ECHO=$(ECHO) top_srcdir=../../.. srcdir=. VPATH=../../.. BASERUBY="$(BASERUBY)"
