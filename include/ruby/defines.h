8a8d3e2ed433a7a305253ea2bb6698510b6e1ed6
ngoto <ngoto@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-06-10 16:47:05 +0000

    * include/ruby/defines.h (GCC_VERSION_SINCE): Fix logic error by   adding parentheses. Fix failures of TestMkmf::TestConvertible   with GCC 3.4.3 on Solaris 10. [Bug #12479] [ruby-dev:49660]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@55371 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/defines.h
+++ include/ruby/defines.h
@@ -63,8 +63,8 @@ extern "C" {
   (defined(__GNUC__) && !defined(__INTEL_COMPILER) && !defined(__clang__) && \
    ((__GNUC__ > (major)) ||  \
     ((__GNUC__ == (major) && \
-      (__GNUC_MINOR__ > (minor)) || \
-      (__GNUC_MINOR__ == (minor) && __GNUC_PATCHLEVEL__ >= (patchlevel))))))
+      ((__GNUC_MINOR__ > (minor)) ||					\
+       (__GNUC_MINOR__ == (minor) && __GNUC_PATCHLEVEL__ >= (patchlevel)))))))
 #endif
 
 /* likely */
