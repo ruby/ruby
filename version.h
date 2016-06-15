13f618f1ed08ebe182c3bf2e4948fd3be2a2be3d
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-06-15 15:40:10 +0000

    merge revision(s) 55005: [Backport #12382]
    	* vm_insnhelper.c (vm_get_ev_const): warn deprecated constant even
    	  in the class context.  [ruby-core:75505] [Bug #12382]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_3@55425 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.3.2"
-#define RUBY_RELEASE_DATE "2016-06-14"
-#define RUBY_PATCHLEVEL 130
+#define RUBY_RELEASE_DATE "2016-06-16"
+#define RUBY_PATCHLEVEL 131
 
 #define RUBY_RELEASE_YEAR 2016
 #define RUBY_RELEASE_MONTH 6
-#define RUBY_RELEASE_DAY 14
+#define RUBY_RELEASE_DAY 16
 
 #include "ruby/version.h"
 
