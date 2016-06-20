40a9064cbf08ec80156b444f9e94151ab9b0bcb1
Nobuyoshi Nakada <nobu@ruby-lang.org>
2016-06-20 12:53:06 +0900

    Export rb_unexpected_type for fiddle

--- internal.h
+++ internal.h
@@ -955,12 +955,6 @@ VALUE rb_name_err_new(VALUE mesg, VALUE recv, VALUE method);
     rb_name_err_raise_str(rb_fstring_cstr(mesg), (recv), (name))
 NORETURN(void ruby_only_for_internal_use(const char *));
 #define ONLY_FOR_INTERNAL_USE(func) ruby_only_for_internal_use(func)
-NORETURN(void rb_unexpected_type(VALUE,int,const char*));
-#undef Check_Type
-#define Check_Type(v, t) \
-    (RB_TYPE_P((VALUE)(v), (t)) ? (void)0 : rb_unexpected_type((VALUE)(v), (t), NULL))
-#define Check_Type_Of(v, t) \
-    (RB_TYPE_P((VALUE)(v), (t)) ? (void)0 : rb_unexpected_type((VALUE)(v), (t), #v))
 
 /* eval.c */
 VALUE rb_refinement_module_get_refined_class(VALUE module);
@@ -1544,6 +1538,12 @@ VALUE rb_str2big_gmp(VALUE arg, int base, int badcheck);
 
 /* error.c (export) */
 int rb_bug_reporter_add(void (*func)(FILE *, void *), void *data);
+NORETURN(void rb_unexpected_type(VALUE,int,const char*));
+#undef Check_Type
+#define Check_Type(v, t) \
+    (RB_TYPE_P((VALUE)(v), (t)) ? (void)0 : rb_unexpected_type((VALUE)(v), (t), NULL))
+#define Check_Type_Of(v, t) \
+    (RB_TYPE_P((VALUE)(v), (t)) ? (void)0 : rb_unexpected_type((VALUE)(v), (t), #v))
 
 /* file.c (export) */
 #ifdef HAVE_READLINK
