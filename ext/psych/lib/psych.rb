ae3cb8bc4ed566327d77d97f5130b22febf7cffd
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-06-24 09:06:09 +0000

    * remove trailing spaces.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@55498 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/psych/lib/psych.rb
+++ ext/psych/lib/psych.rb
@@ -469,7 +469,7 @@ module Psych
   ###
   # Load the document contained in +filename+.  Returns the yaml contained in
   # +filename+ as a Ruby object, or if the file is empty, it returns
-  # the specified default return value, which defaults to an empty Hash 
+  # the specified default return value, which defaults to an empty Hash
   def self.load_file filename, fallback = false
     File.open(filename, 'r:bom|utf-8') { |f|
       self.load f, filename, FALLBACK.new(fallback)
