79b1758b65563815abb5d626a21b597e39fcfea4
duerst <duerst@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-07-06 09:34:23 +0000

    * enc/iso_8859_16.c, test/ruby/enc/test_case_comprehensive.rb:   Implement non-ASCII case conversion for ISO-8859-16, by Satoshi Kayama.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@55590 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- enc/iso_8859_16.c
+++ enc/iso_8859_16.c
@@ -222,6 +222,62 @@ get_case_fold_codes_by_str(OnigCaseFoldType flag,
 	     flag, p, end, items);
 }
 
+#ifdef ONIG_CASE_MAPPING
+static int
+case_map (OnigCaseFoldType* flagP, const OnigUChar** pp,
+					 const OnigUChar* end, OnigUChar* to, OnigUChar* to_end,
+					 const struct OnigEncodingTypeST* enc)
+{
+  OnigCodePoint code;
+  OnigUChar *to_start = to;
+  OnigCaseFoldType flags = *flagP;
+
+  while (*pp<end && to<to_end) {
+    code = *(*pp)++;
+    if (code==SHARP_s) {
+      if (flags&ONIGENC_CASE_UPCASE) {
+        flags |= ONIGENC_CASE_MODIFIED;
+        *to++ = 'S';
+        code = (flags&ONIGENC_CASE_TITLECASE) ? 's' : 'S';
+      }
+      else if (flags&ONIGENC_CASE_FOLD) {
+        flags |= ONIGENC_CASE_MODIFIED;
+        *to++ = 's';
+        code = 's';
+      }
+    }
+    //else if (code==0xAA || code==0xBA) ;
+    else if ((EncISO_8859_16_CtypeTable[code] & BIT_CTYPE_UPPER)
+	     && (flags & (ONIGENC_CASE_DOWNCASE|ONIGENC_CASE_FOLD))) {
+        flags |= ONIGENC_CASE_MODIFIED;
+        if(code==0xA1 || code==0xBC) code++;
+        else if(code==0xA3 || code==0xAA || code==0xAF) code += 0x10;
+        else if(code==0xA6 || code==0xAC) code += 0x02;
+        else if(code==0xB2) code += 0x07;
+        else if(code==0xB4) code += 0x04;
+        else if(code==0xBE) code += 0x41;
+        else code += 0x20;
+    }
+    else if ((EncISO_8859_16_CtypeTable[code]&BIT_CTYPE_LOWER)
+	     && (flags&ONIGENC_CASE_UPCASE)) {
+        flags |= ONIGENC_CASE_MODIFIED;
+        if(code==0xA2 || code==0xBD) code--;
+        else if(code==0xB3 || code==0xBA || code==0xBF) code -= 0x10;
+        else if(code==0xA8 || code==0xAE) code -= 0x02;
+        else if(code==0xB9) code -= 0x07;
+        else if(code==0xB8) code -= 0x04;
+        else if(code==0xFF) code -= 0x41;
+        else code -= 0x20;
+    }
+    *to++ = code;
+  if (flags&ONIGENC_CASE_TITLECASE)  /* switch from titlecase to lowercase for capitalize */
+    flags ^= (ONIGENC_CASE_UPCASE|ONIGENC_CASE_DOWNCASE|ONIGENC_CASE_TITLECASE);
+  }
+  *flagP = flags;
+  return (int)(to-to_start);
+}
+#endif   /* ONIG_CASE_MAPPING */
+
 OnigEncodingDefine(iso_8859_16, ISO_8859_16) = {
   onigenc_single_byte_mbc_enc_len,
   "ISO-8859-16",  /* name */
@@ -242,7 +298,7 @@ OnigEncodingDefine(iso_8859_16, ISO_8859_16) = {
   0,
   ONIGENC_FLAG_NONE,
 #ifdef ONIG_CASE_MAPPING
-  onigenc_single_byte_ascii_only_case_map,
+  case_map,
 #endif   /* ONIG_CASE_MAPPING */
 };
 ENC_ALIAS("ISO8859-16", "ISO-8859-16")
--- test/ruby/enc/test_case_comprehensive.rb
+++ test/ruby/enc/test_case_comprehensive.rb
@@ -248,7 +248,7 @@ class TestComprehensiveCaseFold
   generate_ascii_only_case_mapping_tests 'ISO-8859-13'
   generate_ascii_only_case_mapping_tests 'ISO-8859-14'
   generate_ascii_only_case_mapping_tests 'ISO-8859-15'
-  generate_ascii_only_case_mapping_tests 'ISO-8859-16'
+  generate_case_mapping_tests 'ISO-8859-16'
   generate_ascii_only_case_mapping_tests 'KOI8-R'
   generate_ascii_only_case_mapping_tests 'KOI8-U'
   generate_ascii_only_case_mapping_tests 'Big5'
