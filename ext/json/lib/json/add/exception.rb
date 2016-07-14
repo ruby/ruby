bf7cc652ae0fc0c3d9683c3a762669c2d28737a4
hsbt <hsbt@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-07-14 03:48:54 +0000

    * ext/json/**/*.rb: merge original files from upstream repository.   It only fixes styles of frozen string literal.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@55671 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/json/lib/json/add/exception.rb
+++ ext/json/lib/json/add/exception.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: false
+#frozen_string_literal: false
 unless defined?(::JSON::JSON_LOADED) and ::JSON::JSON_LOADED
   require 'json'
 end
--- ext/json/lib/json/common.rb
+++ ext/json/lib/json/common.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: false
+#frozen_string_literal: false
 require 'json/version'
 require 'json/generic_object'
 
