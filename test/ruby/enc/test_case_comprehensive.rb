c8a1d8b33bc612e6c58e3fbeb3226375bc4f4184
duerst <duerst@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-07-26 06:30:39 +0000

    * enc/windows_1251.c, test/ruby/enc/test_case_comprehensive.rb:   Implement non-ASCII case conversion for Windows-1251, by Shunsuke Sato.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@55749 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/enc/test_case_comprehensive.rb
+++ test/ruby/enc/test_case_comprehensive.rb
@@ -287,7 +287,7 @@ TestComprehensiveCaseFold.data_files_available? and  class TestComprehensiveCase
   generate_ascii_only_case_mapping_tests 'Shift_JIS'
   generate_ascii_only_case_mapping_tests 'Windows-31J'
   generate_ascii_only_case_mapping_tests 'Windows-1250'
-  generate_ascii_only_case_mapping_tests 'Windows-1251'
+  generate_case_mapping_tests 'Windows-1251'
   generate_case_mapping_tests 'Windows-1252'
   generate_ascii_only_case_mapping_tests 'Windows-1253'
   generate_ascii_only_case_mapping_tests 'Windows-1254'
