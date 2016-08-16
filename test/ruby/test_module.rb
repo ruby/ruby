caec808f74a7545cc2b6930dbfa629071315f065
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-08-16 05:56:26 +0000

    merge revision(s) 55705: [Backport #12345]
    	* variable.c (rb_local_constants_i): exclude private constants
    	  when excluding inherited constants too.  [Bug #12345]
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_2@55930 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_module.rb
+++ test/ruby/test_module.rb
@@ -1360,6 +1360,8 @@ class TestModule < Test::Unit::TestCase
 
   def test_constants_with_private_constant
     assert_not_include(::TestModule.constants, :PrivateClass)
+    assert_not_include(::TestModule.constants(true), :PrivateClass)
+    assert_not_include(::TestModule.constants(false), :PrivateClass)
   end
 
   def test_toplevel_private_constant
