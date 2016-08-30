14df4b4108d5de3f5212007e408b152652e2692d
Étienne Barrié <etienne.barrie@gmail.com>
2016-08-30 13:29:07 -0400

    delegate.rb: don't look for methods on Kernel
    Instead, look for instance methods of Kernel.
    Otherwise, instance methods of Module (which are methods of Kernel
    itself) are mistakenly believed to exist, and it fails when calling
    Kernel.instance_method().

--- lib/delegate.rb
+++ lib/delegate.rb
@@ -81,7 +81,7 @@ class Delegator < BasicObject
 
     if r && target.respond_to?(m)
       target.__send__(m, *args, &block)
-    elsif ::Kernel.respond_to?(m, true)
+    elsif ::Kernel.method_defined?(m) || ::Kernel.private_method_defined?(m)
       ::Kernel.instance_method(m).bind(self).(*args, &block)
     else
       super(m, *args, &block)
