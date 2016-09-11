c63c41c7e33fbd7445ec6956627abf0d8c1f7f94
Anton Davydov <antondavydov.o@gmail.com>
2016-09-11 18:47:38 +0300

    Increase test coverage for URI::MailTo to 100%

--- test/uri/test_mailto.rb
+++ test/uri/test_mailto.rb
@@ -97,6 +97,11 @@ class TestMailTo < Test::Unit::TestCase
     ok[-1] << {:to => 'listman@example.com', :headers => [['subject', 'subscribe']]}
     ok[-1] << {:to => 'listman@example.com', :headers => [['subject', 'subscribe']]}
 
+    # mailto:listman@example.com?subject=subscribe
+    ok << ["mailto:listman@example.com?subject=subscribe"]
+    ok[-1] << {:to => 'listman@example.com', :headers => { 'subject' => 'subscribe' }}
+    ok[-1] << {:to => 'listman@example.com', :headers => 'subject=subscribe' }
+
     ok_all = ok.flatten.join("\0")
 
     # mailto:joe@example.com?cc=bob@example.com?body=hello   ; WRONG!
@@ -127,6 +132,52 @@ class TestMailTo < Test::Unit::TestCase
     end
 
     assert_equal(ok_all, ok.flatten.join("\0"))
+  end
+
+  def test_initializer
+    assert_raise(InvalidComponentError) do
+      URI::MailTo.new('mailto', 'sdmitry:bla', 'localhost', '2000', nil,
+                      'joe@example.com', nil, nil, 'subject=Ruby')
+    end
+  end
+
+  def test_check_to
+    u = URI::MailTo.build(['joe@example.com', 'subject=Ruby'])
+
+    assert_raise(InvalidComponentError) do
+      u.to = '#1@mail.com'
+    end
+
+    assert_raise(InvalidComponentError) do
+      u.to = '@invalid.email'
+    end
+  end
+
+  def test_to_s
+    u = URI::MailTo.build([nil, 'subject=Ruby'])
+
+    u.send(:set_to, nil)
+    assert_equal('mailto:?subject=Ruby', u.to_s)
+
+    u.fragment = 'test'
+    assert_equal('mailto:?subject=Ruby#test', u.to_s)
+  end
+
+  def test_to_mailtext
+    results = []
+    results << ["To: ruby-list@ruby-lang.org\nSubject: subscribe\n\n\n"]
+    results[-1] << { to: 'ruby-list@ruby-lang.org', headers: { 'subject' => 'subscribe' } }
+
+    results << ["To: ruby-list@ruby-lang.org\n\nBody\n"]
+    results[-1] << { to: 'ruby-list@ruby-lang.org', headers: { 'body' => 'Body' } }
+
+    results << ["To: ruby-list@ruby-lang.org, cc@ruby-lang.org\n\n\n"]
+    results[-1] << { to: 'ruby-list@ruby-lang.org', headers: { 'to' => 'cc@ruby-lang.org' } }
+
+    results.each do |expected, params|
+      u = URI::MailTo.build(params)
+      assert_equal(expected, u.to_mailtext)
+    end
   end
 
   def test_select
