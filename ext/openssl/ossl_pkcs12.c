dce01373fd866e8ac77ed3d3b1b148044d2259af
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-09-26 14:41:15 +0000

    merge revision(s) 55057:
    	* ext/openssl/ossl_pkcs12.c (ossl_pkcs12_initialize): pop errors
    	  leaked by PKCS12_parse(). This is a bug in OpenSSL, which exists
    	  in the versions before the version 1.0.0t, 1.0.1p, 1.0.2d.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_3@56258 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/openssl/ossl_pkcs12.c
+++ ext/openssl/ossl_pkcs12.c
@@ -165,8 +165,12 @@ ossl_pkcs12_initialize(int argc, VALUE *argv, VALUE self)
     BIO_free(in);
 
     pkey = cert = ca = Qnil;
+    /* OpenSSL's bug; PKCS12_parse() puts errors even if it succeeds.
+     * Fixed in OpenSSL 1.0.0t, 1.0.1p, 1.0.2d */
+    ERR_set_mark();
     if(!PKCS12_parse(pkcs, passphrase, &key, &x509, &x509s))
 	ossl_raise(ePKCS12Error, "PKCS12_parse");
+    ERR_pop_to_mark();
     pkey = rb_protect((VALUE(*)_((VALUE)))ossl_pkey_new, (VALUE)key,
 		      &st); /* NO DUP */
     if(st) goto err;
