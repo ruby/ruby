67351ac6f503dfafb1aba0b6e4e3928f12c36b96
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-10-06 17:48:47 +0000

    * lib/rubygems/ssl_certs/GlobalSignRootCA.pem: add for RugyGems.org.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_3@56369 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.3.2"
-#define RUBY_RELEASE_DATE "2016-10-04"
-#define RUBY_PATCHLEVEL 193
+#define RUBY_RELEASE_DATE "2016-10-07"
+#define RUBY_PATCHLEVEL 194
 
 #define RUBY_RELEASE_YEAR 2016
 #define RUBY_RELEASE_MONTH 10
-#define RUBY_RELEASE_DAY 4
+#define RUBY_RELEASE_DAY 7
 
 #include "ruby/version.h"
 
