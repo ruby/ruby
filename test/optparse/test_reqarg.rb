99ad512486bc0a216d297e16f5346762230015e4
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-10-14 08:20:26 +0000

    optparse.rb: hyphenize
    * lib/optparse.rb (make_switch, parse_in_order): unify underscores
      to hyphens.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@56419 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/optparse/test_reqarg.rb
+++ test/optparse/test_reqarg.rb
@@ -2,6 +2,12 @@
 require_relative 'test_optparse'
 
 module TestOptionParser::ReqArg
+  def setup
+    super
+    @opt.def_option "--with_underscore=VAL" do |x| @flag = x end
+    @opt.def_option "--with-hyphen=VAL" do |x| @flag = x end
+  end
+
   class Def1 < TestOptionParser
     include ReqArg
     def setup
@@ -62,6 +68,17 @@ module TestOptionParser::ReqArg
     assert_equal("", @flag)
     assert_equal(%w"", no_error {@opt.parse!(%w"--opt=foo")})
     assert_equal("foo", @flag)
+  end
+
+  def test_hyphenize
+    assert_equal(%w"", no_error {@opt.parse!(%w"--with_underscore foo1")})
+    assert_equal("foo1", @flag)
+    assert_equal(%w"", no_error {@opt.parse!(%w"--with-underscore foo2")})
+    assert_equal("foo2", @flag)
+    assert_equal(%w"", no_error {@opt.parse!(%w"--with-hyphen foo3")})
+    assert_equal("foo3", @flag)
+    assert_equal(%w"", no_error {@opt.parse!(%w"--with_hyphen foo4")})
+    assert_equal("foo4", @flag)
   end
 
   class TestOptionParser::WithPattern < TestOptionParser
