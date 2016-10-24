7cc1cd3d1ed1d482ea2ec247fa6a4ad0f044418a
ksss <co000ri@gmail.com>
2016-10-24 11:42:31 +0900

    Module#define_method: Add UnboundMethod to expected classes

--- proc.c
+++ proc.c
@@ -2068,7 +2068,7 @@ rb_mod_define_method(int argc, VALUE *argv, VALUE mod)
 	}
 	else {
 	    rb_raise(rb_eTypeError,
-		     "wrong argument type %s (expected Proc/Method)",
+		     "wrong argument type %s (expected Proc/Method/UnboundMethod)",
 		     rb_obj_classname(body));
 	}
     }
