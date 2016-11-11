a6f510959e409cb32e8da6a0b6417ac6325302eb
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-11-11 15:26:06 +0000

    merge revision(s) 56684: [Backport #12909]
    	test_process.rb: fix pgroup test
    
    	* test/ruby/test_process.rb (TestProcess#test_execopts_pgroup):
      use dynamically assigned pid for the process group, instead of a
      magic number 2.  [ruby-core:78051] [Bug #12909]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_3@56714 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.3.2"
-#define RUBY_RELEASE_DATE "2016-11-05"
-#define RUBY_PATCHLEVEL 205
+#define RUBY_RELEASE_DATE "2016-11-12"
+#define RUBY_PATCHLEVEL 206
 
 #define RUBY_RELEASE_YEAR 2016
 #define RUBY_RELEASE_MONTH 11
-#define RUBY_RELEASE_DAY 5
+#define RUBY_RELEASE_DAY 12
 
 #include "ruby/version.h"
 
