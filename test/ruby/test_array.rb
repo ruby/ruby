33b3645611cf7bee83d8d5ac9d4373b33f7fff85
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2016-12-27 11:03:50 +0000

    merge revision(s) 57119: [Backport #13052]
    	array.c: check array length every time after yielding
    
    	Since the Array may be modified during rb_yield(), the length before
    	invoking the block can't be trusted. Fix possible out-of-bounds read in
    	Array#combination and Array#repeated_combination.
    
    	It may better to make a defensive copy of the Array, but for now let's
    	follow what Array#permutation does.  [ruby-core:78738] [Bug #13052]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_2@57220 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_array.rb
+++ test/ruby/test_array.rb
@@ -2329,11 +2329,18 @@ class TestArray < Test::Unit::TestCase
 
   def test_combination_clear
     bug9939 = '[ruby-core:63149] [Bug #9939]'
-    assert_separately([], <<-'end;')
-      100_000.times {Array.new(1000)}
+    assert_nothing_raised(bug9939) {
       a = [*0..100]
       a.combination(3) {|*,x| a.clear}
-    end;
+    }
+
+    bug13052 = '[ruby-core:78738] [Bug #13052] Array#combination segfaults if the Array is modified during iteration'
+    assert_nothing_raised(bug13052) {
+      a = [*0..100]
+      a.combination(1) { a.clear }
+      a = [*0..100]
+      a.repeated_combination(1) { a.clear }
+    }
   end
 
   def test_product2
