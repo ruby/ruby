13dffd8a2a1350c3293ac9e180e3840b9f522e79
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-01-18 03:38:52 +0000

    class.c: non-keyword hash class
    * class.c (rb_extract_keywords): keep the class of non-keyword
      elements hash as the original.  [ruby-core:77813] [Bug #12884]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@57360 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- class.c
+++ class.c
@@ -1841,6 +1841,9 @@ rb_extract_keywords(VALUE *orighash)
     }
     st_foreach(rb_hash_tbl_raw(hash), separate_symbol, (st_data_t)&parthash);
     *orighash = parthash[1];
+    if (parthash[1] && RBASIC_CLASS(hash) != rb_cHash) {
+	RBASIC_SET_CLASS(parthash[1], RBASIC_CLASS(hash));
+    }
     return parthash[0];
 }
 
