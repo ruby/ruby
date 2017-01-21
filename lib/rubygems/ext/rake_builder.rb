25441f72f3e9f782a02bd34e47adf764fd2c60bd
Masataka Kuwabara <kuwabara@pocke.me>
2017-01-21 23:39:50 +0900

    Fix for frozen_string_literal

--- lib/rubygems/ext/rake_builder.rb
+++ lib/rubygems/ext/rake_builder.rb
@@ -9,7 +9,7 @@ class Gem::Ext::RakeBuilder < Gem::Ext::Builder
 
   def self.build(extension, directory, dest_path, results, args=[], lib_dir=nil)
     if File.basename(extension) =~ /mkrf_conf/i then
-      cmd = "#{Gem.ruby} #{File.basename extension}"
+      cmd = "#{Gem.ruby} #{File.basename extension}".dup
       cmd << " #{args.join " "}" unless args.empty?
       run cmd, results
     end
