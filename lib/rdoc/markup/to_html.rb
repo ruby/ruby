84012757a7db4b24622b1499df2c2ae4b8cbf2ad
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-02-06 02:35:48 +0000

    rdoc: suppress warnings in eval
    * lib/rdoc/markup/to_html.rb (RDoc::Markup::ToHtml): the argument
      text may contain warnings, which are useless to check if
      parseable.  merge rdoc/rdoc#440.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@57548 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/rdoc/markup/to_html.rb
+++ lib/rdoc/markup/to_html.rb
@@ -383,9 +383,12 @@ class RDoc::Markup::ToHtml < RDoc::Markup::Formatter
   # Returns true if text is valid ruby syntax
 
   def parseable? text
+    verbose, $VERBOSE = $VERBOSE, nil
     eval("BEGIN {return true}\n#{text}")
   rescue SyntaxError
     false
+  ensure
+    $VERBOSE = verbose
   end
 
   ##
