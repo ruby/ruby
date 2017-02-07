24a575800189c766a1d8371da03691d57de1f81b
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-02-07 16:42:32 +0000

    merge revision(s) 56892: [Backport #12910]
    	test_fileutils.rb: fix deadlock
    
    	* test/fileutils/test_fileutils.rb (assert_output_lines): ensure
      that write pipe is closed, not the reader thread to deadlock
      when an exception raised in the given block.
      [ruby-core:78053] [Bug #12910]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_3@57566 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/fileutils/test_fileutils.rb
+++ test/fileutils/test_fileutils.rb
@@ -18,8 +18,11 @@ class TestFileUtils < Test::Unit::TestCase
       fu.instance_variable_set(:@fileutils_output, write)
       th = Thread.new { read.read }
       th2 = Thread.new {
-        yield
-        write.close
+        begin
+          yield
+        ensure
+          write.close
+        end
       }
       th_value, _ = assert_join_threads([th, th2])
       lines = th_value.lines.map {|l| l.chomp }
@@ -215,6 +218,16 @@ class TestFileUtils < Test::Unit::TestCase
   #
   # Test Cases
   #
+
+  def test_assert_output_lines
+    assert_raise(MiniTest::Assertion) {
+      Timeout.timeout(0.1) {
+        assert_output_lines([]) {
+          raise "ok"
+        }
+      }
+    }
+  end
 
   def test_pwd
     check_singleton :pwd
