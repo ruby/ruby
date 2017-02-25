15ef28a991f66f65bb29751cf2bef3bcb51066fc
normal <normal@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-02-25 02:31:03 +0000

    NEWS: document String#-@ change
    * test/ruby/test_string.rb (test_uplus_minus): test deduplication
      [ruby-core:79747] [Feature #13077]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@57710 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_string.rb
+++ test/ruby/test_string.rb
@@ -2537,6 +2537,8 @@ CODE
 
     assert_not_equal(str.object_id, (+str).object_id)
     assert_equal(str.object_id, (-str).object_id)
+    bar = %w(b a r).join('')
+    assert_same(str, -bar, "uminus deduplicates [Feature #13077]")
   end
 
   def test_ord
