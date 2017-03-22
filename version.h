4e1f1ec1a869e8b417b7f638ac4b62945b422b61
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-03-22 13:46:12 +0000

    merge revision(s) 57024: [Backport #13015]
    	vm.c: check type of hash to merge
    
    	* vm.c (core_hash_merge): check the type of the target hash to
    	  merge.  [ruby-core:78536] [Bug #13015]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_3@58056 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.3.3"
-#define RUBY_RELEASE_DATE "2017-03-20"
-#define RUBY_PATCHLEVEL 255
+#define RUBY_RELEASE_DATE "2017-03-22"
+#define RUBY_PATCHLEVEL 256
 
 #define RUBY_RELEASE_YEAR 2017
 #define RUBY_RELEASE_MONTH 3
-#define RUBY_RELEASE_DAY 20
+#define RUBY_RELEASE_DAY 22
 
 #include "ruby/version.h"
 
