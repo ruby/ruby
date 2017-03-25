fd69ec05c0726e8e22714e5d351db34964c4e174
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-03-25 17:37:23 +0000

    merge revision(s) 57522: [Backport #13189]
    	doc: restore class documentation for Struct
    
    	* struct.c: restore class documentation for Struct
    	  that disappeared with r46663.
    
    	Due to r46663, the class documentation for Struct disappeared.
    	(The revision inserted the definition of `InitVM_Struct` between
    	the rdoc and the definition of `Init_Struct`.)
    
    	The docs are rendered for 2.1: <https://docs.ruby-lang.org/en/2.1.0/Struct.html>,
    	but not for later versions, see: <https://docs.ruby-lang.org/en/2.2.0/Struct.html>
    	(Same for `ri` pages).
    
    	[ruby-core:79416] [Bug #13189]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_2@58113 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- struct.c
+++ struct.c
@@ -1041,6 +1041,8 @@ rb_struct_size(VALUE s)
 }
 
 /*
+ *  Document-class: Struct
+ *
  *  A Struct is a convenient way to bundle a number of attributes together,
  *  using accessor methods, without having to write an explicit class.
  *
