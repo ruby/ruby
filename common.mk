0e22e24de5b839607f24120e86c704a956b0d5fb
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-03-26 00:50:59 +0000

    common.mk: test-rubyspec
    * common.mk (yes-test-rubyspec): make jobserver ready.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@58135 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- common.mk
+++ common.mk
@@ -710,6 +710,7 @@ test-rubyspec-precheck: $(arch)-fake.rb
 
 test-rubyspec: $(TEST_RUNNABLE)-test-rubyspec
 yes-test-rubyspec: test-rubyspec-precheck
+	$(gnumake_recursive)$(Q) \
 	$(RUNRUBY) -r./$(arch)-fake $(srcdir)/spec/mspec/bin/mspec run -B $(srcdir)/spec/default.mspec $(MSPECOPT)
 no-test-rubyspec: test-rubyspec-precheck
 
