07e4aa67e1e46b895b567d1427bf369771253d1a
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-03-26 01:21:57 +0000

    gmake.mk: multi mspec
    * defs/gmake.mk (MSPECOPT): enable multi exec if parallel make.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@58136 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- defs/gmake.mk
+++ defs/gmake.mk
@@ -2,6 +2,7 @@
 gnumake = yes
 override gnumake_recursive := $(if $(findstring n,$(firstword $(MFLAGS))),,+)
 override mflags := $(filter-out -j%,$(MFLAGS))
+MSPECOPT += $(if $(filter -j%,$(MFLAGS)),-j)
 
 CHECK_TARGETS := great exam love check test check% test% btest%
 # expand test targets, and those dependents
