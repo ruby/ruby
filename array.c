d9df4b5084bf1fd2704de5a4c3424ab8bcdc0b98
pascbjumper2 <stowers.joshua@live.com>
2017-03-30 22:24:58 -0500

    Add alias "prepend" to Array#unshift

--- array.c
+++ array.c
@@ -6170,6 +6170,7 @@ Init_Array(void)
     rb_define_method(rb_cArray, "pop", rb_ary_pop_m, -1);
     rb_define_method(rb_cArray, "shift", rb_ary_shift_m, -1);
     rb_define_method(rb_cArray, "unshift", rb_ary_unshift_m, -1);
+    rb_define_alias(rb_cArray,  "prepend", "unshift");
     rb_define_method(rb_cArray, "insert", rb_ary_insert, -1);
     rb_define_method(rb_cArray, "each", rb_ary_each, 0);
     rb_define_method(rb_cArray, "each_index", rb_ary_each_index, 0);
