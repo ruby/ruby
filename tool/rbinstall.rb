e326045afc62ca73b762a5a1f1128a1b5bc64c60
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-04-16 14:54:59 +0000

    rbinstall.rb: ignore skipped exts
    * tool/rbinstall.rb (default-gems): skip gemspec if corresponding
      Makefile does not exist.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@58371 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/rbinstall.rb
+++ tool/rbinstall.rb
@@ -699,9 +699,14 @@ end
 install?(:ext, :arch, :gem, :'default-gems', :'default-gems-arch') do
   install_default_gem('ext', srcdir) do |path|
     # assume that gemspec and extconf.rb are placed in the same directory
-    IO.foreach(File.dirname(path[(srcdir.size+1)..-1]) + "/Makefile") do |l|
-      break true if /^TARGET\s*=/ =~ l
+    success = false
+    begin
+      IO.foreach(File.dirname(path[(srcdir.size+1)..-1]) + "/Makefile") do |l|
+        break success = true if /^TARGET\s*=/ =~ l
+      end
+    rescue Errno::ENOENT
     end
+    success
   end
 end
 
