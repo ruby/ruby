afac56df6fe9b6088322f1a6ac15edbdcbe9442e
Bernhard M. Wiedemann <bwiedemann@suse.de>
2017-04-30 22:53:55 +0200

    rdoc: do not store current timestamps in gz headers
    to enable reproducible builds of rdoc
    
    Normally, 0 would be the preferred value to indicate "no date"
    but that value is handled differently in Zlib::GzipWriter
    to put in the current time

--- lib/rdoc/generator/json_index.rb
+++ lib/rdoc/generator/json_index.rb
@@ -175,7 +175,7 @@ class RDoc::Generator::JsonIndex
     debug_msg "Writing gzipped search index to %s" % outfile
 
     Zlib::GzipWriter.open(outfile) do |gz|
-      gz.mtime = File.mtime(search_index_file)
+      gz.mtime = 1 # make output reproducible
       gz.orig_name = search_index_file.basename.to_s
       gz.write search_index
       gz.close
@@ -193,7 +193,7 @@ class RDoc::Generator::JsonIndex
         debug_msg "Writing gzipped file to %s" % outfile
 
         Zlib::GzipWriter.open(outfile) do |gz|
-          gz.mtime = File.mtime(dest)
+          gz.mtime = 1 # make output reproducible
           gz.orig_name = dest.basename.to_s
           gz.write data
           gz.close
