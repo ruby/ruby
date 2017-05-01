ff2422d77c0b2a58afb4fcfd1c5b0bc8d6e34811
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-05-01 15:17:32 +0000

    thread.c: suppress warnings [ci skip]
    * thread.c (fill_thread_id_string): cast to void pointer to
      suppress warnings when pthread_t is not pointer type.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@58532 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- thread.c
+++ thread.c
@@ -269,7 +269,7 @@ rb_thread_s_debug_set(VALUE self, VALUE val)
 #endif
 
 #ifndef fill_thread_id_str
-# define fill_thread_id_string(thid, buf) (thid)
+# define fill_thread_id_string(thid, buf) (void *)(thid)
 # define fill_thread_id_str(th) (void)0
 # define thread_id_str(th) ((void *)(th)->thread_id)
 # define PRI_THREAD_ID "p"
