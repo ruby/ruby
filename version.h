8ef9767608128b5f9c14adfa0a877bd25be6b11f
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-05-09 15:06:00 +0000

    * 2017-05-10
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_4@58633 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.4.2"
-#define RUBY_RELEASE_DATE "2017-05-09"
+#define RUBY_RELEASE_DATE "2017-05-10"
 #define RUBY_PATCHLEVEL 127
 
 #define RUBY_RELEASE_YEAR 2017
 #define RUBY_RELEASE_MONTH 5
-#define RUBY_RELEASE_DAY 9
+#define RUBY_RELEASE_DAY 10
 
 #include "ruby/version.h"
 
