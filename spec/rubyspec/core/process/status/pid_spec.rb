877d1dd1e7e880652d3f329eb0b4b694319d099e
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-05-12 08:11:35 +0000

    Skip on Windows because it always fails
    On Windows, when invoking ruby via runruby.rb, the pid of invoker and of invokee
    are diffrent.  Therefore, this spec always fails.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@58676 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/rubyspec/core/process/status/pid_spec.rb
+++ spec/rubyspec/core/process/status/pid_spec.rb
@@ -1,13 +1,15 @@
 require File.expand_path('../../../../spec_helper', __FILE__)
 
-describe "Process::Status#pid" do
+platform_is_not :windows do
+  describe "Process::Status#pid" do
 
-  before :each do
-    @pid = ruby_exe("print $$").to_i
+    before :each do
+      @pid = ruby_exe("print $$").to_i
+    end
+
+    it "returns the pid of the process" do
+      $?.pid.should == @pid
+    end
+
   end
-
-  it "returns the pid of the process" do
-    $?.pid.should == @pid
-  end
-
 end
