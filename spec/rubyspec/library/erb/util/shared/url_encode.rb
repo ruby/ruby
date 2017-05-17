d06a7cfeddc376fdf6150e5bc409c6e507ede068
k0kubun <k0kubun@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-05-17 14:11:01 +0000

    spec/rubyspec: Add `ruby_version_is` guard
    for future backport to ruby/spec repository.
    
    See r58772 r58773 r58774.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@58775 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/rubyspec/library/erb/util/shared/url_encode.rb
+++ spec/rubyspec/library/erb/util/shared/url_encode.rb
@@ -1,9 +1,21 @@
 describe :erb_util_url_encode, shared: true do
   it "encode characters" do
     #input  = (0x20..0x7E).to_a.collect{|ch| ch.chr}.join
-    input    = " !\"\#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-    expected = "%20%21%22%23%24%25%26%27%28%29%2A%2B%2C-.%2F0123456789%3A%3B%3C%3D%3E%3F%40ABCDEFGHIJKLMNOPQRSTUVWXYZ%5B%5C%5D%5E_%60abcdefghijklmnopqrstuvwxyz%7B%7C%7D~"
+    input    = " !\"\#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}"
+    expected = "%20%21%22%23%24%25%26%27%28%29%2A%2B%2C-.%2F0123456789%3A%3B%3C%3D%3E%3F%40ABCDEFGHIJKLMNOPQRSTUVWXYZ%5B%5C%5D%5E_%60abcdefghijklmnopqrstuvwxyz%7B%7C%7D"
     ERB::Util.__send__(@method, input).should == expected
+  end
+
+  ruby_version_is ""..."2.5" do
+    it "escapes tilde" do
+      ERB::Util.__send__(@method, "~").should == "%7E"
+    end
+  end
+
+  ruby_version_is "2.5" do
+    it "does not escape tilde" do
+      ERB::Util.__send__(@method, "~").should == "~"
+    end
   end
 
   it "encode unicode string" do
