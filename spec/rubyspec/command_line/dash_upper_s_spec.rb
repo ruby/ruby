8ffc4094a42feb49f6dc740af2657fc32ca72353
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-05-17 14:43:22 +0000

    modify r58771.
    * spec/rubyspec/command_line/dash_upper_s_spec.rb: enable tests on vboxsf
      (VirtualBox shared directory) and change tests to match /success$/ to
      ignore warnings. This technique is suggested by @unak.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@58776 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/rubyspec/command_line/dash_upper_s_spec.rb
+++ spec/rubyspec/command_line/dash_upper_s_spec.rb
@@ -8,18 +8,16 @@ describe 'The -S command line option' do
   platform_is_not :windows do
     # On VirtualBox shared directory (vboxsf) all files are world writable
     # and MRI shows warning when including world writable path in ENV['PATH'].
-    # This warning fails the following tests.
-    unless FileTest.world_writable?(fixture(__FILE__, "bin"))
+    # This is why we are using /success$/ matching in the following cases.
 
-      it "runs launcher found in PATH, but only code after the first /\#!.*ruby.*/-ish line in target file" do
-        result = ruby_exe(nil, options: '-S hybrid_launcher.sh', env: { 'PATH' => @path }, args: '2>&1')
-        result.should == "success\n"
-      end
+    it "runs launcher found in PATH, but only code after the first /\#!.*ruby.*/-ish line in target file" do
+      result = ruby_exe(nil, options: '-S hybrid_launcher.sh', env: { 'PATH' => @path }, args: '2>&1')
+      result.should =~ /success$/
+    end
 
-      it "runs launcher found in PATH" do
-        result = ruby_exe(nil, options: '-S launcher.rb', env: { 'PATH' => @path }, args: '2>&1')
-        result.should == "success\n"
-      end
+    it "runs launcher found in PATH" do
+      result = ruby_exe(nil, options: '-S launcher.rb', env: { 'PATH' => @path }, args: '2>&1')
+      result.should =~ /success$/
     end
   end
 end
