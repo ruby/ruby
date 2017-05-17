95d3671e94515fa83de975604fd1a03fa79f081b
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-05-17 09:31:34 +0000

    skip some tests on vboxsf.
    * spec/rubyspec/command_line/dash_upper_s_spec.rb:
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@58771 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/rubyspec/command_line/dash_upper_s_spec.rb
+++ spec/rubyspec/command_line/dash_upper_s_spec.rb
@@ -6,14 +6,20 @@ describe 'The -S command line option' do
   end
 
   platform_is_not :windows do
-    it "runs launcher found in PATH, but only code after the first /\#!.*ruby.*/-ish line in target file" do
-      result = ruby_exe(nil, options: '-S hybrid_launcher.sh', env: { 'PATH' => @path }, args: '2>&1')
-      result.should == "success\n"
-    end
+    # On VirtualBox shared directory (vboxsf) all files are world writable
+    # and MRI shows warning when including world writable path in ENV['PATH'].
+    # This warning fails the following tests.
+    unless FileTest.world_writable?(fixture(__FILE__, "bin"))
 
-    it "runs launcher found in PATH" do
-      result = ruby_exe(nil, options: '-S launcher.rb', env: { 'PATH' => @path }, args: '2>&1')
-      result.should == "success\n"
+      it "runs launcher found in PATH, but only code after the first /\#!.*ruby.*/-ish line in target file" do
+        result = ruby_exe(nil, options: '-S hybrid_launcher.sh', env: { 'PATH' => @path }, args: '2>&1')
+        result.should == "success\n"
+      end
+
+      it "runs launcher found in PATH" do
+        result = ruby_exe(nil, options: '-S launcher.rb', env: { 'PATH' => @path }, args: '2>&1')
+        result.should == "success\n"
+      end
     end
   end
 end
