d48e2b77324225ab2098a7d9be7e8003efe3dfb1
Yusuke Endoh <mame@ruby-lang.org>
2017-05-19 02:02:07 +0900

    Refactoring run-gcov.rb

--- tool/run-gcov.rb
+++ tool/run-gcov.rb
@@ -2,15 +2,7 @@
 require "pathname"
 require "open3"
 
-BASE_DIR = Pathname(__dir__).dirname
-
-# Hack to allow gcov to find parse.c
-unless File.symlink?("ext/ripper/parse.c")
-  File.symlink("../../parse.c", "ext/ripper/parse.c")
-end
-
-Pathname.glob(BASE_DIR + "**/*.gcda").sort.each do |gcda|
-  gcda = gcda.relative_path_from(BASE_DIR)
+Pathname.glob("**/*.gcda").sort.each do |gcda|
   if gcda.fnmatch("ext/*")
     cwd, gcda = gcda.split.map {|s| s.to_s }
     objdir = "."
@@ -18,10 +10,43 @@ Pathname.glob(BASE_DIR + "**/*.gcda").sort.each do |gcda|
     cwd, objdir, gcda = ".", gcda.dirname.to_s, gcda.to_s
   end
   puts "$ gcov -lpbc -o #{ objdir } #{ gcda }"
-  out, _status = Open3.capture2e("gcov", "-lpbc", "-o", objdir, gcda, chdir: cwd)
+  out, err, _status = Open3.capture3("gcov", "-lpbc", "-o", objdir, gcda, chdir: cwd)
   puts out
+  puts err
 
-  if out !~ /\A(File .*\nLines executed:.*\n(Branches executed:.*\nTaken at least once:.*\n|No branches\n)?(Calls executed:.*\n|No calls\n)?Creating .*\n\n)+\z/
+  # a black list of source files that contains wrong #line directives
+  if err !~ %r(
+    \A(
+      Cannot\ open\ source\ file\ (
+         defs/keywords
+        |zonetab\.list
+        |enc/jis/props\.kwd
+        |parser\.c
+        |parser\.rl
+      )\n
+    )*\z
+  )x
+    raise "Unexpected gcov output"
+  end
+
+  if out !~ %r(
+    \A(
+      File\ .*\nLines\ executed:.*\n
+      (
+        Branches\ executed:.*\n
+        Taken\ at\ least\ once:.*\n
+      |
+        No\ branches\n
+      )?
+      (
+        Calls\ executed:.*\n
+      |
+        No\ calls\n
+      )?
+      Creating\ .*\n
+      \n
+    )+\z
+  )x
     raise "Unexpected gcov output"
   end
 end
