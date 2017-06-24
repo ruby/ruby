3c45a7899e239e7ece3c778d9f71e3be85fdfbed
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-06-24 03:35:29 +0000

    Delegate to `eql?` [Fix GH-1564]
    * lib/delegate.rb (eql?): Delegate to `eql?` of the inner object.
      based on the patch by giginet <giginet.net@gmail.com>.
      [ruby-core:76950] [Bug #12684]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@59167 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/delegate.rb
+++ lib/delegate.rb
@@ -40,7 +40,7 @@ class Delegator < BasicObject
   kernel = ::Kernel.dup
   kernel.class_eval do
     alias __raise__ raise
-    [:to_s, :inspect, :=~, :!~, :===, :<=>, :eql?, :hash].each do |m|
+    [:to_s, :inspect, :=~, :!~, :===, :<=>, :hash].each do |m|
       undef_method m
     end
     private_instance_methods.each do |m|
@@ -143,6 +143,14 @@ class Delegator < BasicObject
   def !=(obj)
     return false if obj.equal?(self)
     __getobj__ != obj
+  end
+
+  #
+  # Returns true if two objects are considered of equal value.
+  #
+  def eql?(obj)
+    return true if obj.equal?(self)
+    obj.eql?(__getobj__)
   end
 
   #
