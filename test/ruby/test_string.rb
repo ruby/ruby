2e596f6891090eaae14aaf039b3f8d817e430e37
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-07-18 10:29:19 +0000

    descriptive asssertions
    * test/ruby/test_string.rb (test_uplus_minus): prefer more
      descriptive asssertions than assert_equal.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@59360 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_string.rb
+++ test/ruby/test_string.rb
@@ -2735,20 +2735,20 @@ CODE
 
   def test_uplus_minus
     str = "foo"
-    assert_equal(false, str.frozen?)
-    assert_equal(false, (+str).frozen?)
-    assert_equal(true,  (-str).frozen?)
+    assert_not_predicate(str, :frozen?)
+    assert_not_predicate(+str, :frozen?)
+    assert_predicate(-str, :frozen?)
 
-    assert_equal(str.object_id, (+str).object_id)
-    assert_not_equal(str.object_id, (-str).object_id)
+    assert_same(str, +str)
+    assert_not_send(str, -str)
 
     str = "bar".freeze
-    assert_equal(true,  str.frozen?)
-    assert_equal(false, (+str).frozen?)
-    assert_equal(true,  (-str).frozen?)
+    assert_predicate(str, :frozen?)
+    assert_not_predicate(+str, :frozen?)
+    assert_predicate(-str, :frozen?)
 
-    assert_not_equal(str.object_id, (+str).object_id)
-    assert_equal(str.object_id, (-str).object_id)
+    assert_not_same(str, +str)
+    assert_same(str, -str)
 
     return unless @cls == String
     bar = %w(b a r).join('')
