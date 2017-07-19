1729052fe8eb16680fa0a005313e45fde8c2fe36
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2017-07-19 16:47:28 +0900

    Removed bundler's examples on ruby source tree.

--- spec/bundler/bundler/psyched_yaml_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-require "spec_helper"
-require "bundler/psyched_yaml"
-
-describe "Bundler::YamlLibrarySyntaxError" do
-  it "is raised on YAML parse errors" do
-    expect { YAML.parse "{foo" }.to raise_error(Bundler::YamlLibrarySyntaxError)
-  end
-end
