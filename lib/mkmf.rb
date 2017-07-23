b1b4772f0b84bedde34ca5344e2dafafbeebff72
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-07-23 07:46:05 +0000

    merge revision(s) 57446: [Backport #13413]
    	mkmf.rb: fix script installation
    
    	* lib/mkmf.rb (MakeMakefile): fix condition to install script
    	  files.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_4@59402 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -2306,7 +2306,7 @@ CLEANOBJS     = *.#{$OBJEXT} #{config_string('cleanobjs') {|t| t.gsub(/\$\*/, "$
     mfile.puts(conf)
     mfile.print "
 all:    #{$extout ? "install" : target ? "$(DLLIB)" : "Makefile"}
-static: #{$extmk && !$static ? "all" : "$(STATIC_LIB)#{!$extmk ? " install-rb" : ""}"}
+static: #{$extmk && !$static ? "all" : "$(STATIC_LIB)#{$extout ? " install-rb" : ""}"}
 .PHONY: all install static install-so install-rb
 .PHONY: clean clean-so clean-static clean-rb
 " #"
