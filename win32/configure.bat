3bec82c728234b0fce537826fa71c25a85353122
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-08-03 03:25:46 +0000

    Mention about `--with-opt-dir`
    * win32/configure.bat (--help): mention about `--with-opt-dir`.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@59477 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- win32/configure.bat
+++ win32/configure.bat
@@ -222,6 +222,7 @@ goto :loop ;
   echo   --with-static-linked-ext link external modules statically
   echo   --with-ext="a,b,..."    use extensions a, b, ...
   echo   --without-ext="a,b,..." ignore extensions a, b, ...
+  echo   --with-out-dir=DIR-LIST add optional headers and libraries directories separated by $PATH_SEPARATOR
   echo   --disable-install-doc   do not install rdoc indexes during install
   echo   --with-ntver=0xXXXX     target NT version (shouldn't use with old SDK)
   del *.tmp
