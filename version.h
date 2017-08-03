16acdaefd3531265effeb90ceac0a7923d8285ae
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-08-03 13:29:12 +0000

    merge revision(s) 58503: [Backport #13523]
    	node.h: sign-extend
    
    	* node.h (nd_line): should sign-extend.  shifting `VALUE` extends
    	  with zero bits if `sizeof(VALUE)` equals to `sizeof(int)`.  the
    	  zero bits are truncated if `sizeof(VALUE)` is bigger enough.
    	  [ruby-core:80920] [Bug #13523]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_4@59488 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.4.2"
-#define RUBY_RELEASE_DATE "2017-07-23"
-#define RUBY_PATCHLEVEL 161
+#define RUBY_RELEASE_DATE "2017-08-03"
+#define RUBY_PATCHLEVEL 162
 
 #define RUBY_RELEASE_YEAR 2017
-#define RUBY_RELEASE_MONTH 7
-#define RUBY_RELEASE_DAY 23
+#define RUBY_RELEASE_MONTH 8
+#define RUBY_RELEASE_DAY 3
 
 #include "ruby/version.h"
 
