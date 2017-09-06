e413909516eaa5416a33d9dd8011d2310a74f5bf
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-09-06 03:18:19 +0000

    th is not defined
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@59755 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- vm.c
+++ vm.c
@@ -2380,7 +2380,7 @@ rb_execution_context_mark(const rb_execution_context_t *ec)
 	while (cfp != limit_cfp) {
 #if VM_CHECK_MODE > 0
 	    const VALUE *ep = cfp->ep;
-	    VM_ASSERT(!!VM_ENV_FLAGS(ep, VM_ENV_FLAG_ESCAPED) == vm_ep_in_heap_p_(th, ep));
+	    VM_ASSERT(!!VM_ENV_FLAGS(ep, VM_ENV_FLAG_ESCAPED) == vm_ep_in_heap_p_(GET_THREAD(), ep));
 #endif
 	    rb_gc_mark(cfp->self);
 	    rb_gc_mark((VALUE)cfp->iseq);
