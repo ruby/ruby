05bc6eb40a552355ab862e3db9171d5335deb751
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-09-08 14:25:52 +0000

    merge revision(s) 59584: [Backport #13850]
    	REXML: Fix a bug that unexpected methods can be called as a XPath function
    
    	[HackerOne:249295]
    
    	Reported by Andrea Jegher. Thanks!!!
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_4@59782 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.4.2"
-#define RUBY_RELEASE_DATE "2017-08-05"
-#define RUBY_PATCHLEVEL 181
+#define RUBY_RELEASE_DATE "2017-09-08"
+#define RUBY_PATCHLEVEL 182
 
 #define RUBY_RELEASE_YEAR 2017
-#define RUBY_RELEASE_MONTH 8
-#define RUBY_RELEASE_DAY 5
+#define RUBY_RELEASE_MONTH 9
+#define RUBY_RELEASE_DAY 8
 
 #include "ruby/version.h"
 
