b0326bce01af3c6c29a29672875b6618964088bc
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-09-23 07:09:05 +0000

    dup String#partition return value
    * string.c (rb_str_partition): return duplicated receiver, when no
      splits.  [ruby-core:82911] [Bug#13925]
    
    Author:    Seiei Miyagi <hanachin@gmail.com>
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@60000 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- string.c
+++ string.c
@@ -9124,7 +9124,7 @@ rb_str_partition(VALUE str, VALUE sep)
 	pos = rb_reg_search(sep, str, 0, 0);
 	if (pos < 0) {
 	  failed:
-	    return rb_ary_new3(3, str, str_new_empty(str), str_new_empty(str));
+	    return rb_ary_new3(3, rb_str_dup(str), str_new_empty(str), str_new_empty(str));
 	}
 	sep = rb_str_subpat(str, sep, INT2FIX(0));
 	if (pos == 0 && RSTRING_LEN(sep) == 0) goto failed;
