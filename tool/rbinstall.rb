d19171d75121506da57b8c035238f566be2dc130
Yusuke Yagyu <gyugyu@pepabo.com>
2017-10-05 12:11:04 +0900

    Remove README* from lib's no_install
    to fix `bundle gem` command, because the command is invalid due to lack
    of README.md.tt

--- tool/rbinstall.rb
+++ tool/rbinstall.rb
@@ -530,7 +530,7 @@ end
 
 install?(:local, :comm, :lib) do
   prepare "library scripts", rubylibdir
-  noinst = %w[README* *.txt *.rdoc *.gemspec]
+  noinst = %w[*.txt *.rdoc *.gemspec]
   install_recursive(File.join(srcdir, "lib"), rubylibdir, :no_install => noinst, :mode => $data_mode)
 end
 
