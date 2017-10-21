961c0701d91d31645bf2eb458f8dbbef7eb01dd6
Takashi Kokubun <takashikkbn@gmail.com>
2017-10-22 00:14:52 +0900

    driver.rb: add option to specify target with rbenv

--- benchmark/driver.rb
+++ benchmark/driver.rb
@@ -381,6 +381,11 @@ if __FILE__ == $0
          opt[:execs] << path
        }
     }
+    o.on('--rbenv [VERSIONS]', 'Specify benchmark targets with rbenv version (vX.X.X;vX.X.X;...)'){|v|
+      v.split(/;/).each{|version|
+        opt[:execs] << "#{version}::#{`RBENV_VERSION='#{version}' rbenv which ruby`.rstrip}"
+      }
+    }
     o.on('-d', '--directory [DIRECTORY]', "Benchmark suites directory"){|d|
       opt[:dir] = d
     }
