ef82d9487327333cb85d0dd087eb33a511ca23d5
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-10-25 23:35:46 +0000

    test-bundled-gems-fetch
    * tool/fetch-bundled_gems.rb: get rid of tool/git-refresh which
      requries Bourne shell.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@60431 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/fetch-bundled_gems.rb
+++ tool/fetch-bundled_gems.rb
@@ -1,17 +1,27 @@
-require 'fileutils'
+#!ruby -an
+BEGIN {
+  require 'fileutils'
 
-File.readlines("#{ARGV[0]}/gems/bundled_gems").each do |gem|
-  n, v, u = gem.split
+  dir = ARGV.shift
+  ARGF.eof?
+  FileUtils.mkdir_p(dir)
+  Dir.chdir(dir)
+}
 
+n, v, u = $F
+case n
+when "minitest"
+  v = "master"
+when "test-unit"
+else
   v = "v" + v
-
-  case n
-  when "minitest"
-    v = "master"
-  when "test-unit"
-    v = v[1..-1]
-  end
-
-  FileUtils.mkdir_p "#{ARGV[0]}/gems/src"
-  `#{ARGV[0]}/tool/git-refresh -C #{ARGV[0]}/gems/src --branch #{v} #{u} #{n}`
 end
+
+if File.directory?(n)
+  puts "updating #{n} ..."
+  system(*%W"git fetch", chdir: n) or abort
+else
+  puts "retrieving #{n} ..."
+  system(*%W"git clone #{u} #{n}") or abort
+end
+system(*%W"git checkout #{v}", chdir: n) or abort
