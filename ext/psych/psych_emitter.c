19a015545ed8836212c63ea351eb24da842b85f7
hsbt <hsbt@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-10-31 07:24:29 +0000

    null byte at Psych::Emitter.
      Check null byte. Patched by tommy (Masahiro Tomita).
      [Bug #13993][ruby-dev:50285]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@60588 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/psych/psych_emitter.c
+++ ext/psych/psych_emitter.c
@@ -272,8 +272,8 @@ static VALUE scalar(
 
     yaml_scalar_event_initialize(
 	    &event,
-	    (yaml_char_t *)(NIL_P(anchor) ? NULL : StringValuePtr(anchor)),
-	    (yaml_char_t *)(NIL_P(tag) ? NULL : StringValuePtr(tag)),
+	    (yaml_char_t *)(NIL_P(anchor) ? NULL : StringValueCStr(anchor)),
+	    (yaml_char_t *)(NIL_P(tag) ? NULL : StringValueCStr(tag)),
 	    (yaml_char_t*)StringValuePtr(value),
 	    (int)RSTRING_LEN(value),
 	    plain ? 1 : 0,
@@ -319,8 +319,8 @@ static VALUE start_sequence(
 
     yaml_sequence_start_event_initialize(
 	    &event,
-	    (yaml_char_t *)(NIL_P(anchor) ? NULL : StringValuePtr(anchor)),
-	    (yaml_char_t *)(NIL_P(tag) ? NULL : StringValuePtr(tag)),
+	    (yaml_char_t *)(NIL_P(anchor) ? NULL : StringValueCStr(anchor)),
+	    (yaml_char_t *)(NIL_P(tag) ? NULL : StringValueCStr(tag)),
 	    implicit ? 1 : 0,
 	    (yaml_sequence_style_t)NUM2INT(style)
 	    );
@@ -383,8 +383,8 @@ static VALUE start_mapping(
 
     yaml_mapping_start_event_initialize(
 	    &event,
-	    (yaml_char_t *)(NIL_P(anchor) ? NULL : StringValuePtr(anchor)),
-	    (yaml_char_t *)(NIL_P(tag) ? NULL : StringValuePtr(tag)),
+	    (yaml_char_t *)(NIL_P(anchor) ? NULL : StringValueCStr(anchor)),
+	    (yaml_char_t *)(NIL_P(tag) ? NULL : StringValueCStr(tag)),
 	    implicit ? 1 : 0,
 	    (yaml_mapping_style_t)NUM2INT(style)
 	    );
@@ -432,7 +432,7 @@ static VALUE alias(VALUE self, VALUE anchor)
 
     yaml_alias_event_initialize(
 	    &event,
-	    (yaml_char_t *)(NIL_P(anchor) ? NULL : StringValuePtr(anchor))
+	    (yaml_char_t *)(NIL_P(anchor) ? NULL : StringValueCStr(anchor))
 	    );
 
     emit(emitter, &event);
