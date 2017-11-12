7a693278c2897dbcb0bf425dba88cd4555fd6c81
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-11-12 04:45:51 +0000

    load.c: cwd encoding
    * load.c (rb_get_expanded_load_path): save cwd cache in OS path
      encoding, to get rid of unnecessary conversion and infinite
      loading when it needs encoding conversion.
      [ruby-dev:50221] [Bug #13863]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@60743 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_rubyoptions.rb
+++ test/ruby/test_rubyoptions.rb
@@ -966,4 +966,16 @@ class TestRubyOptions < Test::Unit::TestCase
       end
     end
   end
+
+  def test_cwd_encoding
+    with_tmpchdir do
+      testdir = "\u30c6\u30b9\u30c8"
+      Dir.mkdir(testdir)
+      Dir.chdir(testdir) do
+        File.write("a.rb", "require './b'")
+        File.write("b.rb", "puts 'ok'")
+        assert_ruby_status([{"RUBYLIB"=>"."}, *%w[-E cp932:utf-8 a.rb]])
+      end
+    end
+  end
 end
