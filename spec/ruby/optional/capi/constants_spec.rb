63c6d4e042e3ea30efdd2d2139dd96d5ba4bc27a
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-11-29 10:38:36 +0000

    spec/ruby/optional/capi/constants_spec.rb: Data is deprecated now
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@60934 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/optional/capi/constants_spec.rb
+++ spec/ruby/optional/capi/constants_spec.rb
@@ -25,8 +25,10 @@ describe "C-API constant" do
     @s.rb_mComparable.should == Comparable
   end
 
-  specify "rb_cData references the Data class" do
-    @s.rb_cData.should == Data
+  ruby_version_is ""..."2.5" do
+    specify "rb_cData references the Data class" do
+      @s.rb_cData.should == Data
+    end
   end
 
   specify "rb_mEnumerable references the Enumerable module" do
