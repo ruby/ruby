f759d9a2b315e39ed8bed7029e874cdfa913411f
eregon <eregon@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-12-12 22:37:40 +0000

    Use EnvUtil.with_default_external in tests needing it
    * Reverts part of r54522.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@61193 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_numeric.rb
+++ test/ruby/test_numeric.rb
@@ -22,10 +22,12 @@ class TestNumeric < Test::Unit::TestCase
     assert_raise_with_message(TypeError, /:\u{3042}/) {1&:"\u{3042}"}
     assert_raise_with_message(TypeError, /:\u{3042}/) {1|:"\u{3042}"}
     assert_raise_with_message(TypeError, /:\u{3042}/) {1^:"\u{3042}"}
-    assert_raise_with_message(TypeError, /:"\\u3042"/) {1+:"\u{3042}"}
-    assert_raise_with_message(TypeError, /:"\\u3042"/) {1&:"\u{3042}"}
-    assert_raise_with_message(TypeError, /:"\\u3042"/) {1|:"\u{3042}"}
-    assert_raise_with_message(TypeError, /:"\\u3042"/) {1^:"\u{3042}"}
+    EnvUtil.with_default_external(Encoding::US_ASCII) do
+      assert_raise_with_message(TypeError, /:"\\u3042"/) {1+:"\u{3042}"}
+      assert_raise_with_message(TypeError, /:"\\u3042"/) {1&:"\u{3042}"}
+      assert_raise_with_message(TypeError, /:"\\u3042"/) {1|:"\u{3042}"}
+      assert_raise_with_message(TypeError, /:"\\u3042"/) {1^:"\u{3042}"}
+    end
 
     bug10711 = '[ruby-core:67405] [Bug #10711]'
     exp = "1.2 can't be coerced into Integer"
