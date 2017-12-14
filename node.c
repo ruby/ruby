e85051ee6caa225216032fb38e7b55070d2de948
mame <mame@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-12-14 06:02:26 +0000

    node.[ch], parse.y, compile.c: rename nd_column to nd_first_column
    nd_set_column -> nd_first_set_column
    nd_lineno -> nd_first_lineno
    nd_set_lineno -> nd_first_set_lineno
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@61224 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- node.c
+++ node.c
@@ -24,7 +24,7 @@
 #define A_LIT(lit) AR(rb_inspect(lit))
 #define A_NODE_HEADER(node, term) \
     rb_str_catf(buf, "@ %s (line: %d, code_range: (%d,%d)-(%d,%d))"term, \
-	ruby_node_name(nd_type(node)), nd_line(node), nd_lineno(node), nd_column(node), nd_last_lineno(node), nd_last_column(node))
+	ruby_node_name(nd_type(node)), nd_line(node), nd_first_lineno(node), nd_first_column(node), nd_last_lineno(node), nd_last_column(node))
 #define A_FIELD_HEADER(len, name, term) \
     rb_str_catf(buf, "+- %.*s:"term, (len), (name))
 #define D_FIELD_HEADER(len, name, term) (A_INDENT, A_FIELD_HEADER(len, name, term))
