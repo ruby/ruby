8dbc8caa0dfa366af6818b04e3ee4aebfe09478d
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-12-14 18:49:20 +0000

    * version.h: Bump version to 2.3.7
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_3@61264 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
-#define RUBY_VERSION "2.3.6"
-#define RUBY_RELEASE_DATE "2017-12-14"
-#define RUBY_PATCHLEVEL 384
+#define RUBY_VERSION "2.3.7"
+#define RUBY_RELEASE_DATE "2017-12-15"
+#define RUBY_PATCHLEVEL 385
 
 #define RUBY_RELEASE_YEAR 2017
 #define RUBY_RELEASE_MONTH 12
-#define RUBY_RELEASE_DAY 14
+#define RUBY_RELEASE_DAY 15
 
 #include "ruby/version.h"
 
