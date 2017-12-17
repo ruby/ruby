356b738864480e3f942d882d01a4b76b7b590766
marcandre <marcandre@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-12-17 18:19:41 +0000

    Integer#{any|all|no}_bits: Fix coercion. Add specs [#12753]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@61305 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- numeric.c
+++ numeric.c
@@ -3175,6 +3175,7 @@ int_even_p(VALUE num)
 static VALUE
 int_allbits_p(VALUE num, VALUE mask)
 {
+    mask = rb_to_int(mask);
     return rb_int_equal(rb_int_and(num, mask), mask);
 }
 
@@ -3188,6 +3189,7 @@ int_allbits_p(VALUE num, VALUE mask)
 static VALUE
 int_anybits_p(VALUE num, VALUE mask)
 {
+    mask = rb_to_int(mask);
     return num_zero_p(rb_int_and(num, mask)) ? Qfalse : Qtrue;
 }
 
@@ -3201,6 +3203,7 @@ int_anybits_p(VALUE num, VALUE mask)
 static VALUE
 int_nobits_p(VALUE num, VALUE mask)
 {
+    mask = rb_to_int(mask);
     return num_zero_p(rb_int_and(num, mask));
 }
 
---
+++ spec/ruby/core/integer/anybits_spec.rb
@@ -0,0 +1,36 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+describe "Integer#anybits?" do
+  it "returns true iff all the bits of the argument are set in the receiver" do
+    42.anybits?(42).should == true
+    0b1010_1010.anybits?(0b1000_0010).should == true
+    0b1010_1010.anybits?(0b1000_0001).should == true
+    0b1000_0010.anybits?(0b0010_1100).should == false
+    different_bignum = (2 * bignum_value) & (~bignum_value)
+    (0b1010_1010 | different_bignum).anybits?(0b1000_0010 | bignum_value).should == true
+    (0b1010_1010 | different_bignum).anybits?(0b0010_1100 | bignum_value).should == true
+    (0b1000_0010 | different_bignum).anybits?(0b0010_1100 | bignum_value).should == false
+  end
+
+  it "handles negative values using two's complement notation" do
+    (~42).anybits?(42).should == false
+    (-42).anybits?(-42).should == true
+    (~0b100).anybits?(~0b1).should == true
+    (~(0b100 | bignum_value)).anybits?(~(0b1 | bignum_value)).should == true
+  end
+
+  it "coerces the rhs using to_int" do
+    obj = mock("the int 0b10")
+    obj.should_receive(:to_int).and_return(0b10)
+    0b110.anybits?(obj).should == true
+  end
+
+  it "raises a TypeError when given a non-Integer" do
+    lambda {
+      (obj = mock('10')).should_receive(:coerce).any_number_of_times.and_return([42,10])
+      13.anybits?(obj)
+    }.should raise_error(TypeError)
+    lambda { 13.anybits?("10")    }.should raise_error(TypeError)
+    lambda { 13.anybits?(:symbol) }.should raise_error(TypeError)
+  end
+end
