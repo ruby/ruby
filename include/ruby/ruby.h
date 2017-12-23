98bddfefc1773dc9a0f3771eb65224c26ee66281
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-12-23 16:17:57 +0000

    undef HAVE_BUILTIN___BUILTIN_CHOOSE_EXPR_CONSTANT_P on GCC 4.8
    GCC 4.8 with optimization causes error if it compiles following code.
    [Bug #14221]
    
    ```c
    __builtin_choose_expr(__builtin_constant_p(b),0,1)
    ```
    
    https://github.com/ruby/ruby/pull/1778
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@61429 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/ruby.h
+++ include/ruby/ruby.h
@@ -26,14 +26,16 @@ extern "C" {
 #include RUBY_EXTCONF_H
 #endif
 
+#include "defines.h"
+
 #if defined(__cplusplus)
 /* __builtin_choose_expr and __builtin_types_compatible aren't available
  * on C++.  See https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html */
 # undef HAVE_BUILTIN___BUILTIN_CHOOSE_EXPR_CONSTANT_P
 # undef HAVE_BUILTIN___BUILTIN_TYPES_COMPATIBLE_P
+#elif GCC_VERSION_BEFORE(4,8,6) /* Bug #14221 */
+# undef HAVE_BUILTIN___BUILTIN_CHOOSE_EXPR_CONSTANT_P
 #endif
-
-#include "defines.h"
 
 #ifndef ASSUME
 # ifdef UNREACHABLE
