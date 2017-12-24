db22ab157503ac1417a3d63aaaf6bbe159decea7
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2017-12-24 02:29:19 +0000

    merge revision(s) 59444,59445: [Backport #13776]
    	adjust indent [ci skip]
    
    	* vm_insnhelper.c (vm_call_method_each_type): adjust indent of a
    	  block in switch.
    
    	visibility of inherited method
    
    	* vm_insnhelper.c (vm_call_method_each_type): honor the original
    	  visibility of inherited methods when a refinement is defined but
    	  not activated.  [ruby-core:82209] [Bug #13776]
    
    	Author:    Mon_Ouie (Mon ouie) <mon.ouie@gmail.com>
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_4@61436 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.4.4"
-#define RUBY_RELEASE_DATE "2017-12-22"
-#define RUBY_PATCHLEVEL 220
+#define RUBY_RELEASE_DATE "2017-12-24"
+#define RUBY_PATCHLEVEL 221
 
 #define RUBY_RELEASE_YEAR 2017
 #define RUBY_RELEASE_MONTH 12
-#define RUBY_RELEASE_DAY 22
+#define RUBY_RELEASE_DAY 24
 
 #include "ruby/version.h"
 
