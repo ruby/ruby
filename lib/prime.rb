3be8923477a2cd35c5b385ee47c3beab5561a0f4
Mycroft1891 <mycroft1891@gmail.com>
2018-01-04 01:12:51 +0100

    fix error

--- lib/prime.rb
+++ lib/prime.rb
@@ -32,13 +32,13 @@ class Integer
 
   # Returns true if +self+ is a prime number, else returns false.
   def prime?
-    return false if n == 1
-    return true  if n == 2 || n == 3
-    return false if n % 2 == 0 || n % 3 == 0
+    return false if self == 1
+    return true  if self == 2 || self == 3
+    return false if self % 2 == 0 || self % 3 == 0
     i = 5
     w = 2
-    while i * i <= n
-        return false if n % i == 0
+    while i * i <= self
+        return false if self % i == 0
         i += w
         w = 6 - w
     end
