52156436002aee7d0b5077732debb9c119e97c40
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-01-11 19:41:49 +0000

    fix for IPv6 env
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@61778 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/resolv/test_mdns.rb
+++ test/resolv/test_mdns.rb
@@ -12,15 +12,16 @@ class TestResolvMDNS < Test::Unit::TestCase
         yield typeclass.new("::1")
       end
     end
+    addrs = mdns.__send__(:use_ipv6?) ? ["127.0.0.1", "::1"] : ["127.0.0.1"]
     [
       ["example.com", []],
-      ["foo.local", ["127.0.0.1"]],
+      ["foo.local", addrs],
     ].each do |name, expect|
       results = []
       mdns.each_address(name) do |result|
         results << result.to_s
       end
-      assert_equal expect, results, "GH-1484"
+      assert_equal expect, results.sort, "GH-1484"
     end
   end
 end
