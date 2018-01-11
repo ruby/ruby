54e6b47a9d9875dbe0e88d759833d4ed6fe6d846
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-01-11 18:26:49 +0000

    Mock fetching data from real DNS [Feature #14340]
    This test just tests MDNS#each_address method.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@61774 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/resolv/test_mdns.rb
+++ test/resolv/test_mdns.rb
@@ -3,20 +3,24 @@ require 'test/unit'
 require 'resolv'
 
 class TestResolvMDNS < Test::Unit::TestCase
-  def setup
-  end
-
   def test_mdns_each_address
-    begin
-      mdns = Resolv::MDNS.new
-      mdns.each_resource '_http._tcp.local', Resolv::DNS::Resource::IN::PTR do |r|
-        srv = mdns.getresource r.name, Resolv::DNS::Resource::IN::SRV
-        mdns.each_address(srv.target) do |result|
-          assert_not_nil(result)
-        end
+    mdns = Resolv::MDNS.new
+    def mdns.each_resource(name, typeclass)
+      if typeclass == Resolv::DNS::Resource::IN::A
+        yield typeclass.new("127.0.0.1")
+      else
+        yield typeclass.new("::1")
       end
-    rescue Errno::EADDRNOTAVAIL
-      # Handle Raspberry Pi environment.
+    end
+    [
+      ["example.com", []],
+      ["foo.local", ["127.0.0.1"]],
+    ].each do |name, expect|
+      results = []
+      mdns.each_address(name) do |result|
+        results << result.to_s
+      end
+      assert_equal expect, results, "GH-1484"
     end
   end
 end
