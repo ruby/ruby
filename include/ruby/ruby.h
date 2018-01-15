02a52588e84694f6e0eb28f9ba495b5e4d845874
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-01-15 05:51:46 +0000

    also blacklist gcc 4.9 for  __builtin_alloca_with_align
    Reports show that gcc (Raspbian 4.9.2-10) 4.9.2 fails here.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@61839 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/ruby.h
+++ include/ruby/ruby.h
@@ -1595,11 +1595,11 @@ rb_num2char_inline(VALUE x)
 #define ZALLOC(type) RB_ZALLOC(type)
 #define REALLOC_N(var,type,n) RB_REALLOC_N(var,type,n)
 
-#if GCC_VERSION_BEFORE(4,8,6)
-/* GCC 4.8.5 reportedly has this feature and is broken.
+#if GCC_VERSION_BEFORE(4,9,5)
+/* GCC 4.9.2 reportedly has this feature and is broken.
  * The function is not officially documented below.
  * Seems we should not use it.
- * https://gcc.gnu.org/onlinedocs/gcc-4.8.5/gcc/Other-Builtins.html#Other-Builtins */
+ * https://gcc.gnu.org/onlinedocs/gcc-4.9.4/gcc/Other-Builtins.html#Other-Builtins */
 # undef HAVE_BUILTIN___BUILTIN_ALLOCA_WITH_ALIGN
 #endif
 
