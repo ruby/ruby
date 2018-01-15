3bdd9d85ae05e997e5fc4adf24de4a640e29eb7b
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-01-15 12:25:24 +0000

    test_console_attr.rb: fix test
    * test/-ext-/win32/test_console_attr.rb (reverse_video): fix test
      when reverse video is set.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@61853 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/-ext-/win32/test_console_attr.rb
+++ test/-ext-/win32/test_console_attr.rb
@@ -5,6 +5,17 @@ if /mswin|mingw/ =~ RUBY_PLATFORM and STDOUT.tty?
   require 'test/unit'
 
   class Test_Win32Console < Test::Unit::TestCase
+    REVERSE_VIDEO = Bug::Win32::REVERSE_VIDEO
+
+    def reverse_video(fore, back = 0x0)
+      info = STDOUT.console_info
+      if (info.attr & REVERSE_VIDEO) == 0
+        (fore << 4) | back
+      else
+        (back << 4) | fore | REVERSE_VIDEO
+      end
+    end
+
     def reset
       STDOUT.console_attribute(7)
     end
@@ -20,7 +31,7 @@ if /mswin|mingw/ =~ RUBY_PLATFORM and STDOUT.tty?
     def test_reverse
       print "\e[7m"
       info = STDOUT.console_info
-      assert_equal(0x70, info.attr);
+      assert_equal(reverse_video(0x7), info.attr);
     end
 
     def test_bold
@@ -32,13 +43,13 @@ if /mswin|mingw/ =~ RUBY_PLATFORM and STDOUT.tty?
     def test_bold_reverse
       print "\e[1;7m"
       info = STDOUT.console_info
-      assert_equal(0xf0, info.attr);
+      assert_equal(reverse_video(0xf), info.attr);
     end
 
     def test_reverse_bold
       print "\e[7;1m"
       info = STDOUT.console_info
-      assert_equal(0xf0, info.attr);
+      assert_equal(reverse_video(0xf), info.attr);
     end
   end
 end
