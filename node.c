deaa466126a8d14f6a18b1ee3854821cf78bc448
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-01-16 09:21:07 +0000

    nested flexible array member is a GCCism
    This is NG. The ISO C section 6.7.2.1 explicitly states that
    structs having flexible array members "shall not be a member
    of a structure or an element of an array."
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@61875 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- node.c
+++ node.c
@@ -1042,7 +1042,7 @@ rb_node_init(NODE *n, enum node_type type, VALUE a0, VALUE a1, VALUE a2)
 
 typedef struct node_buffer_elem_struct {
     struct node_buffer_elem_struct *next;
-    NODE buf[FLEX_ARY_LEN];
+    NODE buf[1];
 } node_buffer_elem_t;
 
 struct node_buffer_struct {
