5ece070475a3e33d27b08035824225dd7053cc24
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-01-17 18:02:47 +0000

    extend timeout
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@61892 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_proc.rb
+++ test/ruby/test_proc.rb
@@ -1342,7 +1342,7 @@ class TestProc < Test::Unit::TestCase
   end
 
   def test_local_variable_set_wb
-    assert_ruby_status([], <<-'end;', '[Bug #13605]', timeout: 15)
+    assert_ruby_status([], <<-'end;', '[Bug #13605]', timeout: 20)
       b = binding
       n = 20_000
 
