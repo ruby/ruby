f668be2d34313acccd8738cb09b8d15f04747656
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-01-25 01:48:41 +0000

    optparse.rb: literal newline
    * lib/optparse.rb (OptionParser#summarize): use literal newline to
      join option summaries as IO#puts does, not the special gloval
      variable $/.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@62035 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/optparse.rb
+++ lib/optparse.rb
@@ -1237,7 +1237,8 @@ XXX
   # +indent+:: Indentation, defaults to @summary_indent.
   #
   def summarize(to = [], width = @summary_width, max = width - 1, indent = @summary_indent, &blk)
-    blk ||= proc {|l| to << (l.index($/, -1) ? l : l + $/)}
+    nl = "\n"
+    blk ||= proc {|l| to << (l.index(nl, -1) ? l : l + nl)}
     visit(:summarize, {}, {}, width, max, indent, &blk)
     to
   end
