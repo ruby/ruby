7b9a3e3e49e0dd6575288ebc06f7432ba73944cd
k0kubun <k0kubun@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-01-27 02:14:20 +0000

    insns2vm.rb: make #generate_parser private
    which is only used by #router.
    
    To make it private, we need to stop defining them on top-level.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@62065 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/insns2vm.rb
+++ tool/insns2vm.rb
@@ -6,7 +6,7 @@
 require_relative 'ruby_vm/scripts/insns2vm'
 
 if $0 == __FILE__
-  router(ARGV).each do |(path, generator)|
+  RubyVM::Insns2VM.router(ARGV).each do |(path, generator)|
     str = generator.generate path
     path.open 'wb:utf-8' do |fp|
       fp.write str
