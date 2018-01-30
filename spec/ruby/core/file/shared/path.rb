00e5621d9d9e2e09d21b510960b32faa7d268047
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-01-30 02:21:59 +0000

    skip EINVAL on unsupported platform
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@62102 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/core/file/shared/path.rb
+++ spec/ruby/core/file/shared/path.rb
@@ -69,7 +69,7 @@ describe :file_path, shared: true do
             File.open(@dir, File::RDWR | File::TMPFILE) do |f|
               -> { f.send(@method) }.should raise_error(IOError)
             end
-          rescue Errno::EOPNOTSUPP
+          rescue Errno::EOPNOTSUPP, Errno::EINVAL
             # EOPNOTSUPP: no support from the filesystem
             1.should == 1
           end
