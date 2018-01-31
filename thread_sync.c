4cc2435aa77f3e651fb7b9c5270672dfb5364576
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-01-31 13:52:40 +0000

    merge revision(s) 56285: [Backport #14354] 	* thread_sync.c (Init_thread_sync): Remove confusing doc comments, 	  which are picked up by rdoc unexpectedly, from Queue and 	  SizedQueue.  [Fix GH-1450]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_3@62143 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- thread_sync.c
+++ thread_sync.c
@@ -1258,11 +1258,11 @@ Init_thread_sync(void)
     rb_define_method(rb_cQueue, "length", rb_queue_length, 0);
     rb_define_method(rb_cQueue, "num_waiting", rb_queue_num_waiting, 0);
 
-    rb_define_alias(rb_cQueue, "enq", "push");    /* Alias for #push. */
-    rb_define_alias(rb_cQueue, "<<", "push");     /* Alias for #push. */
-    rb_define_alias(rb_cQueue, "deq", "pop");     /* Alias for #pop. */
-    rb_define_alias(rb_cQueue, "shift", "pop");   /* Alias for #pop. */
-    rb_define_alias(rb_cQueue, "size", "length"); /* Alias for #length. */
+    rb_define_alias(rb_cQueue, "enq", "push");
+    rb_define_alias(rb_cQueue, "<<", "push");
+    rb_define_alias(rb_cQueue, "deq", "pop");
+    rb_define_alias(rb_cQueue, "shift", "pop");
+    rb_define_alias(rb_cQueue, "size", "length");
 
     rb_cSizedQueue = rb_struct_define_without_accessor_under(
 	rb_cThread,
@@ -1278,10 +1278,10 @@ Init_thread_sync(void)
     rb_define_method(rb_cSizedQueue, "clear", rb_szqueue_clear, 0);
     rb_define_method(rb_cSizedQueue, "num_waiting", rb_szqueue_num_waiting, 0);
 
-    rb_define_alias(rb_cSizedQueue, "enq", "push");  /* Alias for #push. */
-    rb_define_alias(rb_cSizedQueue, "<<", "push");   /* Alias for #push. */
-    rb_define_alias(rb_cSizedQueue, "deq", "pop");   /* Alias for #pop. */
-    rb_define_alias(rb_cSizedQueue, "shift", "pop"); /* Alias for #pop. */
+    rb_define_alias(rb_cSizedQueue, "enq", "push");
+    rb_define_alias(rb_cSizedQueue, "<<", "push");
+    rb_define_alias(rb_cSizedQueue, "deq", "pop");
+    rb_define_alias(rb_cSizedQueue, "shift", "pop");
 
     /* CVar */
     rb_cConditionVariable = rb_struct_define_without_accessor_under(
