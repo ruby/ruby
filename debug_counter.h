fe6906fc5f2409c469869f6e583a551eaa992d04
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-02-09 03:15:21 +0000

    debug_counter.h: include __FILE__ to recurse
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@62329 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- debug_counter.h
+++ debug_counter.h
@@ -77,7 +77,7 @@ RB_DEBUG_COUNTER(load_path_is_not_realpath)
 
 enum rb_debug_counter_type {
 #define RB_DEBUG_COUNTER(name) RB_DEBUG_COUNTER_##name,
-#include "debug_counter.h"
+#include __FILE__
     RB_DEBUG_COUNTER_MAX
 #undef RB_DEBUG_COUNTER
 };
