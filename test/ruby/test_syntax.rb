d191339b9eea8ae1e3500f75ec539e5d4ea03b00
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-02-18 11:53:14 +0000

    Revert r62426
    it cause test failure
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_5@62470 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_syntax.rb
+++ test/ruby/test_syntax.rb
@@ -1128,15 +1128,6 @@ eom
     assert_equal(:begin, result)
   end
 
-  def test_return_in_loop
-    obj = Object.new
-    def obj.test
-      x = nil
-      return until x unless x
-    end
-    assert_nil obj.test
-  end
-
   private
 
   def not_label(x) @result = x; @not_label ||= nil end
