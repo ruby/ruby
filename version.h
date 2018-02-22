526cc900922d5a316818bcacbf52b088f561a147
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-02-22 11:01:13 +0000

    merge revision(s) 62333: [Backport #14459]
    	compile.c: popped hash
    
    	* compile.c (compile_array): skip creating new hash if preceeding
    	  elements are popped all.  [ruby-core:85486] [Bug #14459]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_5@62527 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.5.0"
-#define RUBY_RELEASE_DATE "2018-02-21"
-#define RUBY_PATCHLEVEL 29
+#define RUBY_RELEASE_DATE "2018-02-22"
+#define RUBY_PATCHLEVEL 30
 
 #define RUBY_RELEASE_YEAR 2018
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 21
+#define RUBY_RELEASE_DAY 22
 
 #include "ruby/version.h"
 
