86613d13d5f576f2a84413f04297ed3f4e2c25e1
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-02-22 22:04:35 +0000

    merge revision(s) 61862: [Backport #14368]
    	parse.y (new_regexp): Fix SEGV of `/#{"\u3042"}#{'?'}/` in non UTF-8
    
    	Mixing other encoding string literals in one Regexp caused SEGV.
    	This bug was found by CoverityScan.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_5@62539 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_mixed_unicode_escapes.rb
+++ test/ruby/test_mixed_unicode_escapes.rb
@@ -13,14 +13,18 @@ class TestMixedUnicodeEscape < Test::Unit::TestCase
     # 8-bit character escapes are okay.
     assert_equal("B\xFF", "\u0042\xFF")
 
-    # sjis mb chars mixed with Unicode shound not work
+    # sjis mb chars mixed with Unicode should not work
     assert_raise(SyntaxError) { eval %q("é\u1234")}
     assert_raise(SyntaxError) { eval %q("\u{1234}é")}
+
+    # also should not work for Regexp
+    assert_raise(SyntaxError) { eval %q(/#{"\u1234"}#{"é"}/)}
+    assert_raise(RegexpError) { eval %q(/\u{1234}#{nil}é/)}
+    assert_raise(RegexpError) { eval %q(/é#{nil}\u1234/)}
 
     # String interpolation turns into an expression and we get
     # a different kind of error, but we still can't mix these
     assert_raise(Encoding::CompatibilityError) { eval %q("\u{1234}#{nil}é")}
     assert_raise(Encoding::CompatibilityError) { eval %q("é#{nil}\u1234")}
-
   end
 end
