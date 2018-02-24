dee70aa5e63f43d115c74ef14fd96c48111bd31a
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-02-24 02:58:41 +0000

    Judge ERB version not RUBY_VERSION but ERB.version
    On cross compilation, ruby command uses fake RUBY_VERSION.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@62560 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- enc/make_encmake.rb
+++ enc/make_encmake.rb
@@ -121,7 +121,7 @@ ENCS, ENC_DEPS = target_encodings
 ATRANS, TRANS = target_transcoders
 
 if File.exist?(depend = File.join($srcdir, "depend"))
-  if RUBY_VERSION >= '2.6'
+  if ERB.version[/\d+\.\d+/] >= '2.2'
     erb = ERB.new(File.read(depend), trim_mode: '%')
   else
     erb = ERB.new(File.read(depend), nil, '%')
@@ -139,7 +139,7 @@ open(ARGV[0], 'wb') {|f|
 }
 if MODULE_TYPE == :static
   filename = "encinit.c.erb"
-  if RUBY_VERSION >= '2.6'
+  if ERB.version[/\d+\.\d+/] >= '2.2'
     erb = ERB.new(File.read(File.join($srcdir, filename)), trim_mode: '%-')
   else
     erb = ERB.new(File.read(File.join($srcdir, filename)), nil, '%-')
