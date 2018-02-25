b1a8c64483b5ba5e4a391aa68234e7bde6355034
eregon <eregon@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-02-25 13:52:07 +0000

    Add a new #filter alias for #select
    * In Enumerable, Enumerator::Lazy, Array, Hash and Set
      [Feature #13784] [ruby-core:82285]
    * Share specs for the various #select#select! methods and
      reuse them for #filter/#filter!.
    * Add corresponding filter tests for select tests.
    * Update NEWS.
    
    [Fix GH-1824]
    
    From: Alexander Patrick <adp90@case.edu>
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@62575 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/library/set/sortedset/filter_spec.rb
+++ spec/ruby/library/set/sortedset/filter_spec.rb
@@ -2,6 +2,6 @@ require File.expand_path('../../../../spec_helper', __FILE__)
 require File.expand_path('../shared/select', __FILE__)
 require 'set'
 
-describe "SortedSet#select!" do
-  it_behaves_like :sorted_set_select_bang, :select!
+describe "SortedSet#filter!" do
+  it_behaves_like :sorted_set_select_bang, :filter!
 end
