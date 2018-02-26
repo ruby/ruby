9264270b12a326b1655b48f43a824e66c5f59050
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-02-26 08:01:05 +0000

    r62071 is backported into Ruby 2.5 [Bug #14407]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@62584 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/language/defined_spec.rb
+++ spec/ruby/language/defined_spec.rb
@@ -756,13 +756,13 @@ describe "The defined? keyword for a scoped constant" do
     defined?(DefinedSpecs::String).should be_nil
   end
 
-  ruby_version_is ""..."2.6" do
+  ruby_version_is ""..."2.5" do
     it "returns 'constant' when a constant is defined on top-level but not on the class" do
       defined?(DefinedSpecs::Basic::String).should == 'constant'
     end
   end
 
-  ruby_version_is "2.6" do
+  ruby_version_is "2.5" do
     it "returns nil when a constant is defined on top-level but not on the class" do
       defined?(DefinedSpecs::Basic::String).should be_nil
     end
