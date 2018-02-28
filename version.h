954f4e009ec842e856581a9e302a9122a40cb178
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-02-28 09:49:29 +0000

    merge revision(s) 62506,62508: [Backport #14438]
    	test_time_tz.rb: past Japanese DST tzdata fix
    
    	* test/ruby/test_time_tz.rb (TestTimeTZ#test_asia_tokyo): follow
    	  the tzdata 2018 fix of Japanese DST transitions (1948-1951).
    	  [ruby-core:85373] [Bug #14438]
    
    	test_time_tz.rb: past Japanese DST tzdata fix
    
    	* test/ruby/test_time_tz.rb (TestTimeTZ#test_asia_tokyo): fix
    	  the expected data at the end of DST.  [Bug #14438]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_5@62609 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.5.0"
-#define RUBY_RELEASE_DATE "2018-02-26"
-#define RUBY_PATCHLEVEL 36
+#define RUBY_RELEASE_DATE "2018-02-28"
+#define RUBY_PATCHLEVEL 37
 
 #define RUBY_RELEASE_YEAR 2018
 #define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 26
+#define RUBY_RELEASE_DAY 28
 
 #include "ruby/version.h"
 
