0e60fdc51154aef5d4c6e2530a03f628c827ede5
k0kubun <k0kubun@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-03-13 13:03:12 +0000

    test/ripper/test_lexer.rb: add test for r62743
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@62744 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ripper/test_lexer.rb
+++ test/ripper/test_lexer.rb
@@ -90,4 +90,9 @@ class TestRipper::Lexer < Test::Unit::TestCase
     ]
     assert_equal expect, Ripper.lex(src).map {|e| e[1]}
   end
+
+  def test_slice
+    assert_equal "string\#{nil}\n",
+      Ripper.slice(%(<<HERE\nstring\#{nil}\nHERE), "heredoc_beg .*? nl $(.*?) heredoc_end", 1)
+  end
 end
