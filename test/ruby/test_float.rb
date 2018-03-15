1b4771a9e8413f4a770342c64cb03aeb558d998e
mrkn <mrkn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-03-15 07:19:45 +0000

    Add `exception:` keyword in Kernel#Float()
    Support `exception:` keyword argument in `Kernel#Float()`.
    If `exception:` is `false`, `Kernel#Float()` returns `nil` if the given
    value cannot be interpreted as a float value.
    The default value of `exception:` is `true`.
    This is part of [Feature #12732].
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@62758 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_float.rb
+++ test/ruby/test_float.rb
@@ -783,6 +783,7 @@ class TestFloat < Test::Unit::TestCase
     assert_equal(Float::INFINITY, Float('0xf.fp1000000000000000'))
     assert_equal(1, suppress_warning {Float("1e10_00")}.infinite?)
     assert_raise(TypeError) { Float(nil) }
+    assert_raise(TypeError) { Float(:test) }
     o = Object.new
     def o.to_f; inf = Float::INFINITY; inf/inf; end
     assert_predicate(Float(o), :nan?)
@@ -791,6 +792,43 @@ class TestFloat < Test::Unit::TestCase
   def test_invalid_str
     bug4310 = '[ruby-core:34820]'
     assert_raise(ArgumentError, bug4310) {under_gc_stress {Float('a'*10000)}}
+  end
+
+  def test_Float_with_exception_keyword
+    assert_raise(ArgumentError) {
+      Float(".", exception: true)
+    }
+    assert_nothing_raised(ArgumentError) {
+      assert_equal(nil, Float(".", exception: false))
+    }
+    assert_raise(RangeError) {
+      Float(1i, exception: true)
+    }
+    assert_nothing_raised(RangeError) {
+      assert_equal(nil, Float(1i, exception: false))
+    }
+    assert_raise(TypeError) {
+      Float(nil, exception: true)
+    }
+    assert_nothing_raised(TypeError) {
+      assert_equal(nil, Float(nil, exception: false))
+    }
+    assert_nothing_raised(TypeError) {
+      assert_equal(nil, Float(:test, exception: false))
+    }
+    assert_nothing_raised(TypeError) {
+      assert_equal(nil, Float(Object.new, exception: false))
+    }
+    assert_nothing_raised(TypeError) {
+      o = Object.new
+      def o.to_f; 3.14; end
+      assert_equal(3.14, Float(o, exception: false))
+    }
+    assert_nothing_raised(RuntimeError) {
+      o = Object.new
+      def o.to_f; raise; end
+      assert_equal(nil, Float(o, exception: false))
+    }
   end
 
   def test_num2dbl
