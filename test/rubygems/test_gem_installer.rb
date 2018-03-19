332938df510b510589561b41249e1eb34954c259
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-03-19 08:15:16 +0000

    merge revision(s) 61501,61758: [Backport #14481]
    	fix concurrent test.
    
    	* test/rubygems/test_require.rb (test_concurrent_require):
    	  Synchronizations should be in ensure clause. Sometimes
    	  `require` fails (not sure why) and latch is not released.
    	  Such case introduces unlimited awaiting.
    	  This patch soleve this problem.
    
    
    	skip some tests so that no failure occurs in root privilege
    
    	Some tests had failed on `sudo make test-all`, mainly because root can
    	access any files regardless of permission.  This change adds `skip`
    	guards into such tests.
    
    	Note that almost all tests in which `skip` guards is added, already have
    	"windows" guard.  This is because there is no support to avoid read
    	access by owner on Windows.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_5@62834 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/rubygems/test_gem_installer.rb
+++ test/rubygems/test_gem_installer.rb
@@ -437,6 +437,8 @@ gem 'other', version
 
     if win_platform?
       skip('test_generate_bin_script_no_perms skipped on MS Windows')
+    elsif Process.uid == 0
+      skip('test_generate_bin_script_no_perms skipped in root privilege')
     else
       FileUtils.chmod 0000, util_inst_bindir
 
@@ -529,6 +531,8 @@ gem 'other', version
 
     if win_platform?
       skip('test_generate_bin_symlink_no_perms skipped on MS Windows')
+    elsif Process.uid == 0
+      skip('test_user_install_disabled_read_only test skipped in root privilege')
     else
       FileUtils.chmod 0000, util_inst_bindir
 
--- test/rubygems/test_gem_specification.rb
+++ test/rubygems/test_gem_specification.rb
@@ -1461,6 +1461,7 @@ dependencies: []
 
   def test_build_extensions_extensions_dir_unwritable
     skip 'chmod not supported' if Gem.win_platform?
+    skip 'skipped in root privilege' if Process.uid == 0
 
     ext_spec
 
@@ -1486,7 +1487,7 @@ dependencies: []
     @ext.build_extensions
     refute_path_exists @ext.extension_dir
   ensure
-    unless ($DEBUG or win_platform?) then
+    unless ($DEBUG or win_platform? or Process.uid == 0) then
       FileUtils.chmod 0755, File.join(@ext.base_dir, 'extensions')
       FileUtils.chmod 0755, @ext.base_dir
     end
