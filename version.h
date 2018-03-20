c28d3d02c20a88d3d2fff10aadaf062ad8d56cc8
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-03-20 02:08:28 +0000

    * 2018-03-20
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_5@62853 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.5.0"
-#define RUBY_RELEASE_DATE "2018-03-19"
+#define RUBY_RELEASE_DATE "2018-03-20"
 #define RUBY_PATCHLEVEL 43
 
 #define RUBY_RELEASE_YEAR 2018
 #define RUBY_RELEASE_MONTH 3
-#define RUBY_RELEASE_DAY 19
+#define RUBY_RELEASE_DAY 20
 
 #include "ruby/version.h"
 
