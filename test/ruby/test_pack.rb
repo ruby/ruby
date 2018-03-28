70ad5986df7972d8f7cc5240990f2c348ae30842
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-03-28 11:44:55 +0000

    merge revision(s) 62992:
    	pack.c: fix underflow
    
    	* pack.c (pack_unpack_internal): get rid of underflow.
    	  https://hackerone.com/reports/298246
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_4@63001 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_pack.rb
+++ test/ruby/test_pack.rb
@@ -548,6 +548,9 @@ class TestPack < Test::Unit::TestCase
     assert_equal([1, 2], "\x01\x00\x00\x02".unpack("C@3C"))
     assert_equal([nil], "\x00".unpack("@1C")) # is it OK?
     assert_raise(ArgumentError) { "\x00".unpack("@2C") }
+
+    pos = (1 << [nil].pack("p").bytesize * 8) - 100 # -100
+    assert_raise(RangeError) {"0123456789".unpack("@#{pos}C10")}
   end
 
   def test_pack_unpack_percent
