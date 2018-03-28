d92ce90c5410582debe012ecb552a9a5c06083aa
hsbt <hsbt@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-03-28 10:03:03 +0000

    Ignore file separator from tmpfile/tmpdir name.
    From: SHIBATA Hiroshi <hsbt@ruby-lang.org>
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@62990 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/tmpdir.rb
+++ lib/tmpdir.rb
@@ -116,8 +116,10 @@ class Dir
       prefix, suffix = basename
       prefix = (String.try_convert(prefix) or
                 raise ArgumentError, "unexpected prefix: #{prefix.inspect}")
+      prefix = prefix.delete("#{File::SEPARATOR}#{File::ALT_SEPARATOR}")
       suffix &&= (String.try_convert(suffix) or
                   raise ArgumentError, "unexpected suffix: #{suffix.inspect}")
+      suffix &&= suffix.delete("#{File::SEPARATOR}#{File::ALT_SEPARATOR}")
       begin
         t = Time.now.strftime("%Y%m%d")
         path = "#{prefix}#{t}-#{$$}-#{rand(0x100000000).to_s(36)}"\
