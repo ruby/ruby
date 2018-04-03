89a5b1550debade2877a3968d03e7745b58e2857
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-04-03 10:50:47 +0000

    Reproducible test for [Feature#14370] @ [Bug #14660]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@63075 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- KNOWNBUGS.rb
+++ KNOWNBUGS.rb
@@ -5,3 +5,25 @@
 # This test file includes tests which point out known bugs.
 # So all tests will cause failure.
 #
+assert_normal_exit("#{<<~"begin;"}\n#{<<~'end;#1'}", timeout: 5)
+begin;
+  str = "#{<<~"begin;"}\n#{<<~'end;'}"
+  begin;
+    class P
+      def p; end
+      def q; end
+      E = ""
+      N = "#{E}"
+      attr_reader :i
+      undef p
+      undef q
+      remove_const :E
+      remove_const :N
+    end
+  end;
+  iseq = RubyVM::InstructionSequence.compile(str)
+  100.times {|i|
+    bin = iseq.to_binary
+    RubyVM::InstructionSequence.load_from_binary(bin).eval
+  }
+end;#1
