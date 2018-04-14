d6dc676d07427e4c1459e9c7509329bb54cbdc7c
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-04-14 12:49:30 +0000

    irb/{context,workspace}.rb: use local_variable_set
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@63149 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ test/irb/test_context.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: false
+require 'test/unit'
+require 'tempfile'
+require 'irb'
+require 'rubygems' if defined?(Gem)
+
+module TestIRB
+  class TestContext < Test::Unit::TestCase
+    class TestInputMethod < ::IRB::InputMethod
+      attr_reader :line, :line_no
+
+      def initialize(list = [])
+        super("test")
+        @line_no = 0
+        @line = list
+      end
+
+      def gets
+        @list[@line_no.tap {@line_no += 1}]
+      end
+
+      def eof?
+        @line_no >= @list.size
+      end
+    end
+
+    def setup
+      IRB.init_config(nil)
+      IRB.conf[:USE_READLINE] = false
+      IRB.conf[:VERBOSE] = false
+      workspace = IRB::WorkSpace.new(Object.new)
+      @context = IRB::Context.new(nil, workspace, TestInputMethod.new)
+    end
+
+    def test_last_value
+      assert_nil(@context.last_value)
+      assert_nil(@context.evaluate('_', 1))
+      obj = Object.new
+      @context.set_last_value(obj)
+      assert_same(obj, @context.last_value)
+      assert_same(obj, @context.evaluate('_', 1))
+    end
+  end
+end
