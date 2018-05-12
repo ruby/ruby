6fc7c6cdc7e59e385e20b303d694d191ccc95f89
rhe <rhe@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-05-12 07:33:53 +0000

    openssl: merge changes in v2.1.1
    Commits in upstream repository since v2.1.0 can be found at:
    
    	https://github.com/ruby/openssl/compare/v2.1.0...v2.1.1
    
    ----------------------------------------------------------------
    Kazuki Yamaguchi (7):
          test/utils: disable Thread's report_on_exception in start_server
          cipher: validate iterations argument for Cipher#pkcs5_keyivgen
          extconf.rb: fix build with LibreSSL 2.7.0
          test/test_pkey_rsa: fix test failure with OpenSSL 1.1.1
          test/test_ssl_session: set client protocol version explicitly
          Ruby/OpenSSL 2.0.8
          Ruby/OpenSSL 2.1.1
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@63406 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/openssl/extconf.rb
+++ ext/openssl/extconf.rb
@@ -125,8 +125,11 @@ OpenSSL.check_func_or_macro("SSL_get_server_tmp_key", "openssl/ssl.h")
 have_func("SSL_is_server")
 
 # added in 1.1.0
+if !have_struct_member("SSL", "ctx", "openssl/ssl.h") ||
+    try_static_assert("LIBRESSL_VERSION_NUMBER >= 0x2070000fL", "openssl/opensslv.h")
+  $defs.push("-DHAVE_OPAQUE_OPENSSL")
+end
 have_func("CRYPTO_lock") || $defs.push("-DHAVE_OPENSSL_110_THREADING_API")
-have_struct_member("SSL", "ctx", "openssl/ssl.h") || $defs.push("-DHAVE_OPAQUE_OPENSSL")
 have_func("BN_GENCB_new")
 have_func("BN_GENCB_free")
 have_func("BN_GENCB_get_arg")
