ebdd0846e296e542e90ca96913852a2bac00fd53
k0kubun <k0kubun@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-05-27 05:47:43 +0000

    jit_support.rb: cahce JIT support check
    * Before
    make test-all -C .ruby-svn TESTS="../test/ruby/test_jit.rb"  22.40s user 5.38s system 91% cpu 30.196 total
    
    * After
    make test-all -C .ruby-svn TESTS="../test/ruby/test_jit.rb"  12.91s user 3.33s system 91% cpu 17.648 total
    
    Also, this makes it easier to check if JIT is actually tested, by
    showing warning on stderr.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@63504 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/lib/jit_support.rb
+++ test/lib/jit_support.rb
@@ -6,18 +6,7 @@ module JITSupport
     'clang',
   ]
 
-  module_function
-  def eval_with_jit(env = nil, script, verbose: 0, min_calls: 5, save_temps: false, timeout: JIT_TIMEOUT)
-    args = ['--disable-gems', '--jit-wait', "--jit-verbose=#{verbose}", "--jit-min-calls=#{min_calls}"]
-    args << '--jit-save-temps' if save_temps
-    args << '-e' << script
-    args.unshift(env) if env
-    EnvUtil.invoke_ruby(args,
-      '', true, true, timeout: timeout,
-    )
-  end
-
-  def supported?
+  def self.check_support
     # Experimental. If you want to ensure JIT is working with this test, please set this for now.
     if ENV.key?('RUBY_FORCE_TEST_JIT')
       return true
@@ -31,6 +20,26 @@ module JITSupport
       false
     else
       err.match?(JIT_SUCCESS_PREFIX)
+    end
+  end
+
+  module_function
+  def eval_with_jit(env = nil, script, verbose: 0, min_calls: 5, save_temps: false, timeout: JIT_TIMEOUT)
+    args = ['--disable-gems', '--jit-wait', "--jit-verbose=#{verbose}", "--jit-min-calls=#{min_calls}"]
+    args << '--jit-save-temps' if save_temps
+    args << '-e' << script
+    args.unshift(env) if env
+    EnvUtil.invoke_ruby(args,
+      '', true, true, timeout: timeout,
+    )
+  end
+
+  def supported?
+    return @supported if defined?(@supported)
+    @supported = JITSupport.check_support.tap do |supported|
+      unless supported
+        warn "JIT tests are skiped since JIT seems not working. Set RUBY_FORCE_TEST_JIT=1 to let it fail.", uplevel: 1
+      end
     end
   end
 
