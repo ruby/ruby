62dc851e5170aa5d5f2775a3def313d2b5d6c947
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-06-22 11:57:06 +0000

    remove debug print
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@63723 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/net/http/test_http.rb
+++ test/net/http/test_http.rb
@@ -542,7 +542,7 @@ module TestNetHTTP_version_1_1_methods
 
       th = Thread.new do
         err = !windows? ? Net::WriteTimeout : Net::ReadTimeout
-        assert_raise(err, "experimental:sysctl: #{`sysctl net.core.wmem_default`rescue nil} #{`sysctl net.core.wmem_max`rescue nil}") { conn.post('/', "a"*5_000) }
+        assert_raise(err) { conn.post('/', "a"*5_000_000) }
       end
       assert th.join(10)
     }
