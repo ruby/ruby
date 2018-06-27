fa45654a8514a561be2d210d6939f8a00ec4a162
normal <normal@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-06-27 03:16:59 +0000

    Revert "spec: skip Process wait specs on MJIT"
    This reverts r63731 (commit 359dd59db2512d801bb983f98bede4fc598f139a).
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@63760 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/mspec/lib/mspec/guards/feature.rb
+++ spec/mspec/lib/mspec/guards/feature.rb
@@ -39,9 +39,3 @@ end
 def with_feature(*features, &block)
   FeatureGuard.new(*features).run_if(:with_feature, &block)
 end
-
-MSpecEnv.class_eval do
-  def without_feature(*features, &block)
-    FeatureGuard.new(*features).run_unless(:without_feature, &block)
-  end
-end
