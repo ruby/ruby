a8e369f9907a34afed247af73dbb3f1e2e976a0c
kazu <kazu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-07-01 01:38:18 +0000

    Fix a typo [ci skip]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@63814 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- array.c
+++ array.c
@@ -5784,7 +5784,7 @@ rb_ary_drop_while(VALUE ary)
 /*
  *  call-seq:
  *     ary.any? [{ |obj| block }]   -> true or false
- *     any.any?(pattern)            -> true or false
+ *     ary.any?(pattern)            -> true or false
  *
  *  See also Enumerable#any?
  */
