d0bfcdf892948d1380a3707a28b9f2f61f9593b5
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-07-05 11:53:18 +0000

    Add missing file
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@63858 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ ext/-test-/time/leap_second.c
@@ -0,0 +1,14 @@
+#include "ruby.h"
+
+static VALUE
+bug_time_s_reset_leap_second_info(VALUE klass)
+{
+    reset_leap_second_info();
+    return Qnil;
+}
+
+void
+Init_time_leap_second(VALUE klass)
+{
+    rb_define_singleton_method(klass, "reset_leap_second_info", bug_time_s_reset_leap_second_info, 0);
+}
