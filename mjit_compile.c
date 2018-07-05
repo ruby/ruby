ruby/ruby

commit 6b534134a78e3e43c344682c3585e1abab015216

give up insn attr handles_frame
I introduced this mechanism in r62051 to speed things up. Later it
was reported that the change causes problems.  I searched for
workarounds but nothing seemed appropriate.  I hereby officially
give it up.  The idea to move ADD_PC around was a mistake.

Fixes [Bug #14809] and [Bug #14834].

Signed-off-by: Urabe, Shyouhei <shyouhei@ruby-lang.org>


git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@63763 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

insns.def
@@ -43,8 +43,6 @@
        * sp_inc: Used to dynamically calculate sp increase in
          `insn_stack_increase`.
 
-       * handles_frame: If it is true, VM moves pc before insn body.
-
    - Attributes can access operands, but not stack (push/pop) variables.
 
    - An instruction's body is a pure C block, copied verbatimly into
@@ -366,6 +364,7 @@ concatstrings
 // attr rb_snum_t sp_inc = 1 - num;
 {
     val = rb_str_concat_literals(num, STACK_ADDR_FROM_TOP(num));
+    POPN(num);
 }
 
 /* push the result of to_s. */
@@ -401,6 +400,7 @@ toregexp
     const VALUE ary = rb_ary_tmp_new_from_values(0, cnt, STACK_ADDR_FROM_TOP(cnt));
     val = rb_reg_new_ary(ary, (int)opt);
     rb_ary_clear(ary);
+    POPN(cnt);
 }
 
 /* intern str to Symbol and push it. */
@@ -422,6 +422,7 @@ newarray
 // attr rb_snum_t sp_inc = 1 - num;
 {
     val = rb_ary_new4(num, STACK_ADDR_FROM_TOP(num));
+    POPN(num);
 }
 
 /* dup array */
@@ -450,7 +451,7 @@ expandarray
 (...)
 // attr rb_snum_t sp_inc = num - 1 + (flag & 1 ? 1 : 0);
 {
-    vm_expandarray(GET_SP(), ary, num, (int)flag);
+    vm_expandarray(GET_CFP(), ary, num, (int)flag);
 }
 
 /* concat two arrays */
@@ -487,6 +488,7 @@ newhash
 
     if (num) {
         rb_hash_bulk_insert(num, STACK_ADDR_FROM_TOP(num), val);
+        POPN(num);
     }
 }
 
@@ -536,6 +538,7 @@ dupn
     void *dst = GET_SP();
     void *src = STACK_ADDR_FROM_TOP(n);
 
+    INC_SP(n); /* alloca */
     MEMCPY(dst, src, VALUE, n);
 }
 
@@ -598,7 +601,7 @@ setn
 (VALUE val)
 // attr rb_snum_t sp_inc = 0;
 {
-    TOPN(n) = val;
+    TOPN(n - 1) = val;
 }
 
 /* empty current stack */
@@ -609,7 +612,7 @@ adjuststack
 (...)
 // attr rb_snum_t sp_inc = -(rb_snum_t)n;
 {
-    /* none */
+    POPN(n);
 }
 
 /**********************************************************/
@@ -687,7 +690,6 @@ defineclass
 (ID id, ISEQ class_iseq, rb_num_t flags)
 (VALUE cbase, VALUE super)
 (VALUE val)
-// attr bool handles_frame = true;
 {
     VALUE klass = vm_find_or_create_class_by_id(id, flags, cbase, super);
 
@@ -714,7 +716,6 @@ send
 (CALL_INFO ci, CALL_CACHE cc, ISEQ blockiseq)
 (...)
 (VALUE val)
-// attr bool handles_frame = true;
 // attr rb_snum_t sp_inc = - (int)(ci->orig_argc + ((ci->flag & VM_CALL_ARGS_BLOCKARG) ? 1 : 0));
 {
     struct rb_calling_info calling;
@@ -750,6 +751,7 @@ opt_newarray_max
 // attr rb_snum_t sp_inc = 1 - num;
 {
     val = vm_opt_newarray_max(num, STACK_ADDR_FROM_TOP(num));
+    POPN(num);
 }
 
 DEFINE_INSN
@@ -760,6 +762,7 @@ opt_newarray_min
 // attr rb_snum_t sp_inc = 1 - num;
 {
     val = vm_opt_newarray_min(num, STACK_ADDR_FROM_TOP(num));
+    POPN(num);
 }
 
 /* Invoke method without block */
@@ -768,7 +771,6 @@ opt_send_without_block
 (CALL_INFO ci, CALL_CACHE cc)
 (...)
 (VALUE val)
-// attr bool handles_frame = true;
 // attr rb_snum_t sp_inc = -ci->orig_argc;
 {
     struct rb_calling_info calling;
@@ -783,7 +785,6 @@ invokesuper
 (CALL_INFO ci, CALL_CACHE cc, ISEQ blockiseq)
 (...)
 (VALUE val)
-// attr bool handles_frame = true;
 // attr rb_snum_t sp_inc = - (int)(ci->orig_argc + ((ci->flag & VM_CALL_ARGS_BLOCKARG) ? 1 : 0));
 {
     struct rb_calling_info calling;
@@ -801,7 +802,6 @@ invokeblock
 (CALL_INFO ci)
 (...)
 (VALUE val)
-// attr bool handles_frame = true;
 // attr rb_snum_t sp_inc = 1 - ci->orig_argc;
 {
     struct rb_calling_info calling;
@@ -828,7 +828,6 @@ leave
 ()
 (VALUE val)
 (VALUE val)
-// attr bool handles_frame = true;
 {
     if (OPT_CHECKED_RUN) {
 	const VALUE *const bp = vm_base_ptr(reg_cfp);
@@ -990,6 +989,11 @@ opt_plus
     val = vm_opt_plus(recv, obj);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        PUSH(obj);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1004,6 +1008,11 @@ opt_minus
     val = vm_opt_minus(recv, obj);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        PUSH(obj);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1018,6 +1027,11 @@ opt_mult
     val = vm_opt_mult(recv, obj);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        PUSH(obj);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1032,6 +1046,11 @@ opt_div
     val = vm_opt_div(recv, obj);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        PUSH(obj);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1046,6 +1065,11 @@ opt_mod
     val = vm_opt_mod(recv, obj);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        PUSH(obj);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1060,6 +1084,11 @@ opt_eq
     val = opt_eq_func(recv, obj, ci, cc);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        PUSH(obj);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1075,7 +1104,9 @@ opt_neq
 
     if (val == Qundef) {
 #ifndef MJIT_HEADER
-	ADD_PC(2); /* !!! */
+        PUSH(recv);
+        PUSH(obj);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
@@ -1091,6 +1122,11 @@ opt_lt
     val = vm_opt_lt(recv, obj);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        PUSH(obj);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1105,6 +1141,11 @@ opt_le
     val = vm_opt_le(recv, obj);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        PUSH(obj);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1119,6 +1160,11 @@ opt_gt
     val = vm_opt_gt(recv, obj);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        PUSH(obj);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1133,6 +1179,11 @@ opt_ge
     val = vm_opt_ge(recv, obj);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        PUSH(obj);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1147,6 +1198,11 @@ opt_ltlt
     val = vm_opt_ltlt(recv, obj);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        PUSH(obj);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1161,6 +1217,11 @@ opt_aref
     val = vm_opt_aref(recv, obj);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        PUSH(obj);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1175,6 +1236,12 @@ opt_aset
     val = vm_opt_aset(recv, obj, set);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        PUSH(obj);
+        PUSH(set);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1193,9 +1260,10 @@ opt_aset_with
     }
     else {
 #ifndef MJIT_HEADER
-	TOPN(0) = rb_str_resurrect(key);
-	PUSH(val);
-	ADD_PC(1); /* !!! */
+        PUSH(recv);
+        PUSH(rb_str_resurrect(key));
+        PUSH(val);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
@@ -1212,8 +1280,9 @@ opt_aref_with
 
     if (val == Qundef) {
 #ifndef MJIT_HEADER
-	PUSH(rb_str_resurrect(key));
-	ADD_PC(1); /* !!! */
+        PUSH(recv);
+        PUSH(rb_str_resurrect(key));
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
@@ -1229,6 +1298,10 @@ opt_length
     val = vm_opt_length(recv, BOP_LENGTH);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1243,6 +1316,10 @@ opt_size
     val = vm_opt_length(recv, BOP_SIZE);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1257,6 +1334,10 @@ opt_empty_p
     val = vm_opt_empty_p(recv);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1271,6 +1352,10 @@ opt_succ
     val = vm_opt_succ(recv);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1285,6 +1370,10 @@ opt_not
     val = vm_opt_not(ci, cc, recv);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(recv);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1309,6 +1398,11 @@ opt_regexpmatch2
     val = vm_opt_regexpmatch2(obj2, obj1);
 
     if (val == Qundef) {
+#ifndef MJIT_HEADER
+        PUSH(obj2);
+        PUSH(obj1);
+        ADD_PC(-WIDTH_OF_opt_send_without_block);
+#endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
     }
 }
@@ -1319,7 +1413,6 @@ opt_call_c_function
 (rb_insn_func_t funcptr)
 ()
 ()
-// attr bool handles_frame = true;
 {
     reg_cfp = (funcptr)(ec, reg_cfp);
 

tool/ruby_vm/views/_mjit_compile_insn_body.erb
@@ -77,31 +77,6 @@
         }
         fprintf(f, "            goto cancel;\n");
 %   else
-%     if insn.handles_frame?
-%       # If insn.handles_frame? is true, cfp->sp might be changed inside insns (like vm_caller_setup_arg_block)
-%       # and thus we need to use cfp->sp, even when local_stack_p is TRUE. When insn.handles_frame? is true,
-%       # cfp->sp should be available too because _mjit_compile_pc_and_sp.erb sets it.
         fprintf(f, <%= to_cstr.call(line) %>);
-%     else
-%       # If local_stack_p is TRUE and insn.handles_frame? is false, stack values are only available in local variables
-%       # for stack. So we need to replace those macros if local_stack_p is TRUE here.
-%       case line
-%       when /\bGET_SP\(\)/
-%         # reg_cfp->sp
-        fprintf(f, <%= to_cstr.call(line.sub(/\bGET_SP\(\)/, '%s')) %>, (status->local_stack_p ? "(stack + stack_size)" : "GET_SP()"));
-%       when /\bSTACK_ADDR_FROM_TOP\((?<num>[^)]+)\)/
-%         # #define STACK_ADDR_FROM_TOP(n) (GET_SP()-(n))
-%         num = Regexp.last_match[:num]
-        fprintf(f, <%= to_cstr.call(line.sub(/\bSTACK_ADDR_FROM_TOP\(([^)]+)\)/, '%s')) %>,
-                (status->local_stack_p ? "stack + (stack_size - (<%= num %>))" : "STACK_ADDR_FROM_TOP(<%= num %>)"));
-%       when /\bTOPN\((?<num>[^)]+)\)/
-%         # #define TOPN(n) (*(GET_SP()-(n)-1))
-%         num = Regexp.last_match[:num]
-        fprintf(f, <%= to_cstr.call(line.sub(/\bTOPN\(([^)]+)\)/, '%s')) %>,
-              (status->local_stack_p ? "*(stack + (stack_size - (<%= num %>) - 1))" : "TOPN(<%= num %>)"));
-%       else
-        fprintf(f, <%= to_cstr.call(line) %>);
-%       end
-%     end
 %   end
 % end

tool/ruby_vm/views/_mjit_compile_pc_and_sp.erb
@@ -8,33 +8,19 @@
 % # JIT: Move pc so that catch table lookup condition is met. If the ISeq might not catch an exception,
 % # the pc motion is optimized away and thus pc should be set properly before `goto cancel`.
         if (body->catch_except_p) {
-% if insn.handles_frame?
             fprintf(f, "    reg_cfp->pc = original_body_iseq + %d;\n", next_pos); /* ADD_PC(INSN_ATTR(width)); */
-% else
-            fprintf(f, "    reg_cfp->pc = original_body_iseq + %d;\n", pos);
-% end
         }
 %
 % # JIT: move sp to use or preserve stack variables
         if (status->local_stack_p) {
-% # sp motion is optimized away for `handles_frame? #=> false` case.
-% # Thus sp should be set properly before `goto cancel`.
-% if insn.handles_frame?
 %           # JIT-only behavior (pushing JIT's local variables to VM's stack):
-            {
                 rb_snum_t i, push_size;
                 push_size = -<%= insn.call_attribute('sp_inc') %> + <%= insn.rets.size %> - <%= insn.pops.size %>;
                 fprintf(f, "    reg_cfp->sp = (VALUE *)reg_cfp->bp + %ld + 1;\n", push_size); /* POPN(INSN_ATTR(popn)); */
                 for (i = 0; i < push_size; i++) { /* TODO: use memcpy? */
                     fprintf(f, "    *(reg_cfp->sp + %ld) = stack[%ld];\n", i - push_size, (rb_snum_t)b->stack_size - push_size + i);
                 }
-            }
-% end
         }
         else {
-% if insn.handles_frame?
             fprintf(f, "    reg_cfp->sp = (VALUE *)reg_cfp->bp + %d;\n", b->stack_size + 1 - <%= insn.pops.size %>); /* POPN(INSN_ATTR(popn)); */
-% else
-            fprintf(f, "    reg_cfp->sp = (VALUE *)reg_cfp->bp + %d;\n", b->stack_size + 1);
-% end
         }

gc.c
@@ -1805,10 +1805,7 @@ rb_objspace_set_event_hook(const rb_event_flag_t event)
 static void
 gc_event_hook_body(rb_execution_context_t *ec, rb_objspace_t *objspace, const rb_event_flag_t event, VALUE data)
 {
-    /* increment PC because source line is calculated with PC-1 */
-    ec->cfp->pc++;
     EXEC_EVENT_HOOK(ec, event, ec->cfp->self, 0, 0, 0, data);
-    ec->cfp->pc--;
 }
 
 #define gc_event_hook_available_p(objspace) ((objspace)->flags.has_hook)
@@ -9955,4 +9952,3 @@ ruby_xrealloc2(void *ptr, size_t n, size_t new_size)
 #endif
     return ruby_xrealloc2_body(ptr, n, new_size);
 }
-

test/ruby/test_settracefunc.rb
@@ -1901,4 +1901,17 @@ class TestSetTraceFunc < Test::Unit::TestCase
     assert_equal ["c-call", base_line + 35],   events[9] # Thread#set_trace_func
     assert_equal nil,                          events[10]
   end
+
+  def test_lineno_in_optimized_insn
+    actual, _, _ = EnvUtil.invoke_ruby [], <<-EOF.gsub(/^.*?: */, ""), true
+      1: class String
+      2:   def -@
+      3:     puts caller_locations(1, 1)[0].lineno
+      4:   end
+      5: end
+      6:
+      7: -""
+    EOF
+    assert_equal "7\n", actual, '[Bug #14809]'
+  end
 end

tool/ruby_vm/models/bare_instructions.rb
@@ -101,10 +101,6 @@ class RubyVM::BareInstructions
     }.join
   end
 
-  def handles_frame?
-    /\b(false|0)\b/ !~ @attrs['handles_frame'].expr.expr
-  end
-
   def inspect
     sprintf "#<%s %s@%s:%d>", self.class.name, @name, @loc[0], @loc[1]
   end
@@ -129,7 +125,6 @@ class RubyVM::BareInstructions
     generate_attribute 'rb_num_t', 'retn', rets.size
     generate_attribute 'rb_num_t', 'width', width
     generate_attribute 'rb_snum_t', 'sp_inc', rets.size - pops.size
-    generate_attribute 'bool', 'handles_frame', false
   end
 
   def typesplit a

tool/ruby_vm/views/_insn_entry.erb
@@ -29,28 +29,17 @@ INSN_ENTRY(<%= insn.name %>)
     <%= pop[:name] %> = <%= insn.cast_from_VALUE pop, "TOPN(#{i})"%>;
 % end
     DEBUG_ENTER_INSN(INSN_ATTR(name));
-% if insn.handles_frame?
     ADD_PC(INSN_ATTR(width));
     POPN(INSN_ATTR(popn));
-% end
     COLLECT_USAGE_INSN(INSN_ATTR(bin));
 % insn.opes.each_with_index do |ope, i|
     COLLECT_USAGE_OPERAND(INSN_ATTR(bin), <%= i %>, <%= ope[:name] %>);
 % end
 <%= render_c_expr insn.expr -%>
     CHECK_VM_STACK_OVERFLOW_FOR_INSN(VM_REG_CFP, INSN_ATTR(retn));
-% if insn.handles_frame?
-%   insn.rets.reverse_each do |ret|
+%   insn.rets.each do |ret|
     PUSH(<%= insn.cast_to_VALUE ret %>);
 %   end
-% else
-    ADJ_SP(INSN_ATTR(sp_inc));
-%   insn.rets.reverse_each.with_index do |ret, i|
-    TOPN(<%= i %>) = <%= insn.cast_to_VALUE ret %>;
-%   end
-    ADD_PC(INSN_ATTR(width));
-    PREFETCH(GET_PC());
-% end
     END_INSN(<%= insn.name %>);
 #   undef INSN_ATTR
 #   undef NAME_OF_CURRENT_INSN

commit 7a0a58544452e6e01cf2ec87f83e7080d229dad5

revert r62655 for r63763
r63655 was tightly coupled to handle_frames and some assumptions seems
to have been broken by r63763.

To partially resolve Bug#14892, this reverts the optimization for now. I
want to make MJIT CI happy first and then I'll probably retry r63655 by
partially reverting r63763 for sp changes.

The skipped test is not fixed yet.

git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@63863 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

insns.def
@@ -989,9 +989,9 @@ opt_plus
     val = vm_opt_plus(recv, obj);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1008,9 +1008,9 @@ opt_minus
     val = vm_opt_minus(recv, obj);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1027,9 +1027,9 @@ opt_mult
     val = vm_opt_mult(recv, obj);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1046,9 +1046,9 @@ opt_div
     val = vm_opt_div(recv, obj);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1065,9 +1065,9 @@ opt_mod
     val = vm_opt_mod(recv, obj);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1084,9 +1084,9 @@ opt_eq
     val = opt_eq_func(recv, obj, ci, cc);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1103,9 +1103,9 @@ opt_neq
     val = vm_opt_neq(ci, cc, ci_eq, cc_eq, recv, obj);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1122,9 +1122,9 @@ opt_lt
     val = vm_opt_lt(recv, obj);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1141,9 +1141,9 @@ opt_le
     val = vm_opt_le(recv, obj);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1160,9 +1160,9 @@ opt_gt
     val = vm_opt_gt(recv, obj);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1179,9 +1179,9 @@ opt_ge
     val = vm_opt_ge(recv, obj);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1198,9 +1198,9 @@ opt_ltlt
     val = vm_opt_ltlt(recv, obj);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1217,9 +1217,9 @@ opt_aref
     val = vm_opt_aref(recv, obj);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1236,10 +1236,10 @@ opt_aset
     val = vm_opt_aset(recv, obj, set);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(obj);
         PUSH(set);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1259,10 +1259,10 @@ opt_aset_with
 	val = tmp;
     }
     else {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(rb_str_resurrect(key));
         PUSH(val);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1279,9 +1279,9 @@ opt_aref_with
     val = vm_opt_aref_with(recv, key);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
         PUSH(rb_str_resurrect(key));
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1298,8 +1298,8 @@ opt_length
     val = vm_opt_length(recv, BOP_LENGTH);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1316,8 +1316,8 @@ opt_size
     val = vm_opt_length(recv, BOP_SIZE);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1334,8 +1334,8 @@ opt_empty_p
     val = vm_opt_empty_p(recv);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1352,8 +1352,8 @@ opt_succ
     val = vm_opt_succ(recv);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1370,8 +1370,8 @@ opt_not
     val = vm_opt_not(ci, cc, recv);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(recv);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);
@@ -1398,9 +1398,9 @@ opt_regexpmatch2
     val = vm_opt_regexpmatch2(obj2, obj1);
 
     if (val == Qundef) {
-#ifndef MJIT_HEADER
         PUSH(obj2);
         PUSH(obj1);
+#ifndef MJIT_HEADER
         ADD_PC(-WIDTH_OF_opt_send_without_block);
 #endif
 	DISPATCH_ORIGINAL_INSN(opt_send_without_block);

mjit_compile.c
@@ -24,9 +24,6 @@
 struct compile_status {
     int success; /* has TRUE if compilation has had no issue */
     int *stack_size_for_pos; /* stack_size_for_pos[pos] has stack size for the position (otherwise -1) */
-    /* If TRUE, JIT-ed code will use local variables to store pushed values instead of
-       using VM's stack and moving stack pointer. */
-    int local_stack_p;
 };
 
 /* Storage to keep data which is consistent in each conditional branch.
@@ -175,13 +172,7 @@ compile_insns(FILE *f, const struct rb_iseq_constant_body *body, unsigned int st
 static void
 compile_cancel_handler(FILE *f, const struct rb_iseq_constant_body *body, struct compile_status *status)
 {
-    unsigned int i;
     fprintf(f, "\ncancel:\n");
-    if (status->local_stack_p) {
-        for (i = 0; i < body->stack_max; i++) {
-            fprintf(f, "    *((VALUE *)reg_cfp->bp + %d) = stack[%d];\n", i + 1, i);
-        }
-    }
     fprintf(f, "    return Qundef;\n");
 }
 
@@ -191,7 +182,6 @@ mjit_compile(FILE *f, const struct rb_iseq_constant_body *body, const char *func
 {
     struct compile_status status;
     status.success = TRUE;
-    status.local_stack_p = !body->catch_except_p;
     status.stack_size_for_pos = ALLOC_N(int, body->iseq_size);
     memset(status.stack_size_for_pos, NOT_COMPILED_STACK_SIZE, sizeof(int) * body->iseq_size);
 
@@ -205,12 +195,7 @@ mjit_compile(FILE *f, const struct rb_iseq_constant_body *body, const char *func
     fprintf(f, "__declspec(dllexport)\n");
 #endif
     fprintf(f, "VALUE\n%s(rb_execution_context_t *ec, rb_control_frame_t *reg_cfp)\n{\n", funcname);
-    if (status.local_stack_p) {
-        fprintf(f, "    VALUE stack[%d];\n", body->stack_max);
-    }
-    else {
-        fprintf(f, "    VALUE *stack = reg_cfp->sp;\n");
-    }
+    fprintf(f, "    VALUE *stack = reg_cfp->sp;\n");
     fprintf(f, "    static const VALUE *const original_body_iseq = (VALUE *)0x%"PRIxVALUE";\n",
             (VALUE)body->iseq_encoded);
 

tool/ruby_vm/views/_mjit_compile_insn.erb
@@ -20,11 +20,6 @@
         MAYBE_UNUSED(<%= ope.fetch(:decl) %>) = (<%= ope.fetch(:type) %>)operands[<%= i %>];
 % end
 %
-% # JIT: Declare stack_size to be used in some macro of _mjit_compile_insn_body.erb
-        if (status->local_stack_p) {
-            fprintf(f, "    MAYBE_UNUSED(unsigned int) stack_size = %u;\n", b->stack_size);
-        }
-%
 % # JIT: Declare variables for operands, popped values and return values
 % insn.declarations.each do |decl|
         fprintf(f, "    <%= decl %>;\n");

tool/ruby_vm/views/_mjit_compile_insn_body.erb
@@ -69,9 +69,6 @@
 %     end
 %   when /\A\s+DISPATCH_ORIGINAL_INSN\([^)]+\);\s+\z/
 %     # For `opt_xxx`'s fallbacks.
-        if (status->local_stack_p) {
-            fprintf(f, "            reg_cfp->sp = (VALUE *)reg_cfp->bp + %d;\n", b->stack_size + 1);
-        }
         if (!body->catch_except_p) {
             fprintf(f, "            reg_cfp->pc = original_body_iseq + %d;\n", pos);
         }

tool/ruby_vm/views/_mjit_compile_pc_and_sp.erb
@@ -12,15 +12,4 @@
         }
 %
 % # JIT: move sp to use or preserve stack variables
-        if (status->local_stack_p) {
-%           # JIT-only behavior (pushing JIT's local variables to VM's stack):
-                rb_snum_t i, push_size;
-                push_size = -<%= insn.call_attribute('sp_inc') %> + <%= insn.rets.size %> - <%= insn.pops.size %>;
-                fprintf(f, "    reg_cfp->sp = (VALUE *)reg_cfp->bp + %ld + 1;\n", push_size); /* POPN(INSN_ATTR(popn)); */
-                for (i = 0; i < push_size; i++) { /* TODO: use memcpy? */
-                    fprintf(f, "    *(reg_cfp->sp + %ld) = stack[%ld];\n", i - push_size, (rb_snum_t)b->stack_size - push_size + i);
-                }
-        }
-        else {
-            fprintf(f, "    reg_cfp->sp = (VALUE *)reg_cfp->bp + %d;\n", b->stack_size + 1 - <%= insn.pops.size %>); /* POPN(INSN_ATTR(popn)); */
-        }
+        fprintf(f, "    reg_cfp->sp = (VALUE *)reg_cfp->bp + %d;\n", b->stack_size + 1 - <%= insn.pops.size %>); /* POPN(INSN_ATTR(popn)); */

tool/ruby_vm/views/_mjit_compile_send.erb
@@ -24,10 +24,6 @@
             int param_size = iseq->body->param.size; /* TODO: check calling->argc for argument_arity_error */
 
             fprintf(f, "{\n");
-% # JIT: Declare stack_size to be used in some macro of _mjit_compile_insn_body.erb
-            if (status->local_stack_p) {
-                fprintf(f, "    MAYBE_UNUSED(unsigned int) stack_size = %u;\n", b->stack_size);
-            }
 
 % # JIT: Invalidate call cache if it requires vm_search_method. This allows to inline some of following things.
 <%= render 'mjit_compile_send_guard' -%>

tool/ruby_vm/views/mjit_compile.inc.erb
@@ -31,13 +31,13 @@
 % #   reg_cfp: the second argument of _mjitXXX
 % #   GET_CFP(): refers to `reg_cfp`
 % #   GET_EP(): refers to `reg_cfp->ep`
-% #   GET_SP(): refers to `reg_cfp->sp`, or `(stack + stack_size)` if local_stack_p
+% #   GET_SP(): refers to `reg_cfp->sp`
 % #   GET_SELF(): refers to `reg_cfp->self`
 % #   GET_LEP(): refers to `VM_EP_LEP(reg_cfp->ep)`
 % #   EXEC_EC_CFP(): refers to `val = vm_exec(ec, TRUE)` with frame setup
 % #   CALL_METHOD(): using `GET_CFP()` and `EXEC_EC_CFP()`
-% #   TOPN(): refers to `reg_cfp->sp`, or `*(stack + (stack_size - num - 1))` if local_stack_p
-% #   STACK_ADDR_FROM_TOP(): refers to `reg_cfp->sp`, or `stack + (stack_size - num)` if local_stack_p
+% #   TOPN(): refers to `reg_cfp->sp`
+% #   STACK_ADDR_FROM_TOP(): refers to `reg_cfp->sp`
 % #   DISPATCH_ORIGINAL_INSN(): expanded in _mjit_compile_insn.erb
 % #   THROW_EXCEPTION(): specially defined for JIT
 % #   RESTORE_REGS(): specially defined for `leave`

test/ruby/test_jit.rb
@@ -675,6 +675,24 @@ class TestJIT < Test::Unit::TestCase
     end;
   end
 
+  def test_stack_pointer_with_assignment
+    assert_eval_with_jit("#{<<~"begin;"}\n#{<<~"end;"}", stdout: "nil\nnil\n", success_count: 1)
+    begin;
+      2.times do
+        a, b = nil
+        p a
+      end
+    end;
+  end
+
+  def test_stack_pointer_with_regexpmatch
+    skip
+    assert_eval_with_jit("#{<<~"begin;"}\n#{<<~"end;"}", stdout: "nil\nnil\n", success_count: 1)
+    begin;
+      1000.times { break if /a/ =~ "ab" && !$~[0] }
+    end;
+  end
+
   private
 
   # The shortest way to test one proc