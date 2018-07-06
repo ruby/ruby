bb86c102e53d3df23208db524c879857064be620
kazu <kazu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-07-06 01:53:57 +0000

    Fixup r63864
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@63865 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/-test-/time/leap_second.c
+++ ext/-test-/time/leap_second.c
@@ -4,7 +4,7 @@ void ruby_reset_leap_second_info(void);
 static VALUE
 bug_time_s_reset_leap_second_info(VALUE klass)
 {
-    reset_leap_second_info();
+    ruby_reset_leap_second_info();
     return Qnil;
 }
 
