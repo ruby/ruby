133cca81e429d7252ae2749cacbb6312f5c31103
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-07-22 10:47:33 +0000

    fix sum on infinity
    * array.c (rb_ary_sum): consider non-finite floats.
      [ruby-core:88024] [Bug #14926]
    
    * enum.c (sum_iter): ditto.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64014 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- array.c
+++ array.c
@@ -5973,6 +5973,20 @@ rb_ary_sum(int argc, VALUE *argv, VALUE ary)
             else
                 goto not_float;
 
+            if (isnan(f)) continue;
+            if (isnan(x)) {
+                f = x;
+                continue;
+            }
+            if (isinf(x)) {
+                if (isinf(f) && signbit(x) != signbit(f))
+                    f = NAN;
+                else
+                    f = x;
+                continue;
+            }
+            if (isinf(f)) continue;
+
             t = f + x;
             if (fabs(f) >= fabs(x))
                 c += ((f - t) + x);
