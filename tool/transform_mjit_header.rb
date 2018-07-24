63f51f528312c1c534bdc8418520299781145e6a
k0kubun <k0kubun@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-07-24 14:43:40 +0000

    transform_mjit_header.rb: fix performance regression
    on r64031. Compiling vm_search_method_slowpath is very slow.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64034 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- tool/transform_mjit_header.rb
+++ tool/transform_mjit_header.rb
@@ -32,7 +32,7 @@ module MJITHeader
   ]
 
   IGNORED_FUNCTIONS = [
-    'vm_search_method_slowpath', # This increases the time to compile when inlined. So we use it as external function.
+    'rb_vm_search_method_slowpath', # This increases the time to compile when inlined. So we use it as external function.
     'rb_equal_opt', # Not used from VM and not compilable
   ]
 
