08c74b7593c1b338b92bdfcfdb43543d24bb1746
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-07-30 14:00:13 +0000

    merge revision(s) 63112: [Backport #14818]
    	Remove outdated example from Numeric documentation
    
    	Since 2.5, Numeric instances can be cloned and duplicated.
    
    	[Fix GH-1850]
    
    	From: Miguel Landaeta <miguel@miguel.cc>
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_4@64127 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- numeric.c
+++ numeric.c
@@ -5179,10 +5179,11 @@ int_truncate(int argc, VALUE* argv, VALUE num)
  *   puts 1.object_id == a.object_id   #=> true
  *
  * There can only ever be one instance of the integer +1+, for example. Ruby ensures this
- * by preventing instantiation and duplication.
+ * by preventing instantiation. If duplication is attempted, the same instance is returned.
  *
- *   Integer.new(1)   #=> NoMethodError: undefined method `new' for Integer:Class
- *   1.dup            #=> TypeError: can't dup Integer
+ *   Integer.new(1)                   #=> NoMethodError: undefined method `new' for Integer:Class
+ *   1.dup                            #=> 1
+ *   1.object_id == 1.dup.object_id   #=> true
  *
  * For this reason, Numeric should be used when defining other numeric classes.
  *
