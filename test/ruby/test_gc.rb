8b310ae75dc6b189e25909c6bf96a6f575382088
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-07-31 14:21:59 +0000

    merge revision(s) 60011:
    	test_gc.rb: relax criterion
    
    	* test/ruby/test_gc.rb (TestGc#test_expand_heap): relax the
    	  criterion and compare by epsilon.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_4@64149 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_gc.rb
+++ test/ruby/test_gc.rb
@@ -290,7 +290,7 @@ class TestGc < Test::Unit::TestCase
     base_length = GC.stat[:heap_eden_pages]
     (base_length * 500).times{ 'a' }
     GC.start
-    assert_in_delta base_length, (v = GC.stat[:heap_eden_pages]), 2,
+    assert_in_epsilon base_length, (v = GC.stat[:heap_eden_pages]), 1/8r,
            "invalid heap expanding (base_length: #{base_length}, GC.stat[:heap_eden_pages]: #{v})"
 
     a = []
