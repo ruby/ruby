510cd06c78a7a2e24b30468d54d675577083c5ad
k0kubun <k0kubun@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-08-09 09:58:07 +0000

    mjit.c: add :wait option to RubyVM::MJIT.pause
    and wait until JIT queue is flushed when wait option is not passed or
    `wait: true` is passed.
    
    vm.c: ditto
    
    test/ruby/test_rubyvm_mjit.rb: added test for pause/resume
    
    test/lib/jit_support.rb: allow retrying MJIT on JITSupport level
    test/ruby/test_jit.rb: ditto
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64250 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/lib/jit_support.rb
+++ test/lib/jit_support.rb
@@ -14,7 +14,7 @@ module JITSupport
 
     # Very pessimistic check. With this check, we can't ensure JIT is working.
     begin
-      _, err = JITSupport.eval_with_jit('proc {}.call', verbose: 1, min_calls: 1, timeout: 10)
+      _, err = JITSupport.eval_with_jit_without_retry('proc {}.call', verbose: 1, min_calls: 1, timeout: 10)
     rescue Timeout::Error
       $stderr.puts "TestJIT: #jit_supported? check timed out"
       false
@@ -28,11 +28,25 @@ module JITSupport
   end
 
   module_function
-  def eval_with_jit(env = nil, script, verbose: 0, min_calls: 5, save_temps: false, max_cache: 1000, timeout: JIT_TIMEOUT)
+  # Run Ruby script with --jit-wait (Synchronous JIT compilation).
+  # Returns [stdout, stderr]
+  def eval_with_jit(env = nil, script, **opts)
+    stdout, stderr = nil, nil
+    # retry 3 times while cc1 error happens.
+    3.times do |i|
+      stdout, stderr, status = eval_with_jit_without_retry(env, script, **opts)
+      assert_equal(true, status.success?, "Failed to run script with JIT:\n#{code_block(script)}\nstdout:\n#{code_block(stdout)}\nstderr:\n#{code_block(stderr)}")
+      break unless retried_stderr?(stderr)
+    end
+    [stdout, stderr]
+  end
+
+  def eval_with_jit_without_retry(env = nil, script, verbose: 0, min_calls: 5, save_temps: false, max_cache: 1000, wait: true, timeout: JIT_TIMEOUT)
     args = [
-      '--disable-gems', '--jit-wait', "--jit-verbose=#{verbose}",
+      '--disable-gems', "--jit-verbose=#{verbose}",
       "--jit-min-calls=#{min_calls}", "--jit-max-cache=#{max_cache}",
     ]
+    args << '--jit-wait' if wait
     args << '--jit-save-temps' if save_temps
     args << '-e' << script
     base_env = { 'MJIT_SEARCH_BUILD_DIR' => 'true' } # workaround to skip requiring `make install` for `make test-all`
@@ -57,5 +71,15 @@ module JITSupport
     else
       stderr
     end
+  end
+
+  def code_block(code)
+    "```\n#{code}\n```\n\n"
+  end
+
+  # We're retrying cc1 not found error on gcc, which should be solved in the future but ignored for now.
+  def retried_stderr?(stderr)
+    RbConfig::CONFIG['CC'].start_with?('gcc') &&
+      stderr.include?("error trying to exec 'cc1': execvp: No such file or directory")
   end
 end
