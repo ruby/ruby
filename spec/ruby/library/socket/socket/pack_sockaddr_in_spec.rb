17e2e75a3b757b6ddb93c9badefb8e31643a5dac
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-08-10 08:31:17 +0000

    Remove failing spec files
    Re-commit after specs are fixed.
    http://rubyci.s3.amazonaws.com/freebsd11zfs/ruby-trunk/log/20180810T063001Z.log.html.gz
    http://rubyci.s3.amazonaws.com/unstable10x/ruby-trunk/log/20180809T191808Z.log.html.gz
    http://rubyci.s3.amazonaws.com/unstable11x/ruby-trunk/log/20180809T192406Z.log.html.gz
    http://rubyci.s3.amazonaws.com/unstable10s/ruby-trunk/log/20180809T151911Z.log.html.gz
    http://rubyci.s3.amazonaws.com/unstable11s/ruby-trunk/log/20180809T192507Z.log.html.gz
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64269 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/library/socket/socket/pack_sockaddr_in_spec.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../spec_helper'
-require_relative '../fixtures/classes'
-require_relative '../shared/pack_sockaddr'
-
-describe "Socket#pack_sockaddr_in" do
-  it_behaves_like :socket_pack_sockaddr_in, :pack_sockaddr_in
-end
