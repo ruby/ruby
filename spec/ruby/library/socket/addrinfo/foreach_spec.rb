df9521fd043df1fb862e46f9b1af83223f16eb2d
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-08-10 08:31:17 +0000

    Remove failing spec files
    Re-commit after specs are fixed.
    http://rubyci.s3.amazonaws.com/freebsd11zfs/ruby-trunk/log/20180810T063001Z.log.html.gz
    http://rubyci.s3.amazonaws.com/unstable10x/ruby-trunk/log/20180809T191808Z.log.html.gz
    http://rubyci.s3.amazonaws.com/unstable11x/ruby-trunk/log/20180809T192406Z.log.html.gz
    http://rubyci.s3.amazonaws.com/unstable10s/ruby-trunk/log/20180809T151911Z.log.html.gz
    http://rubyci.s3.amazonaws.com/unstable11s/ruby-trunk/log/20180809T192507Z.log.html.gz
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64269 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/library/socket/addrinfo/foreach_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../spec_helper'
-
-describe 'Addrinfo.foreach' do
-  it 'yields Addrinfo instances to the supplied block' do
-    Addrinfo.foreach('localhost', 80) do |addr|
-      addr.should be_an_instance_of(Addrinfo)
-    end
-  end
-end
