08563f2e14853e43f0efd9e005fd793a2a58e445
eregon <eregon@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-08-18 19:35:54 +0000

    Use 127.0.0.1 instead of localhost in Addrinfo specs
    * Solaries doesn't support Addrinfo.getaddrinfo('localhost', 80),
      but supports Addrinfo.getaddrinfo('127.0.0.1', 80).
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64456 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/library/socket/addrinfo/foreach_spec.rb
+++ spec/ruby/library/socket/addrinfo/foreach_spec.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper'
 
 describe 'Addrinfo.foreach' do
   it 'yields Addrinfo instances to the supplied block' do
-    Addrinfo.foreach('localhost', 80) do |addr|
+    Addrinfo.foreach('127.0.0.1', 80) do |addr|
       addr.should be_an_instance_of(Addrinfo)
     end
   end
