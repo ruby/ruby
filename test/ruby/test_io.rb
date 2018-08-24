4f93867aee50d540562be275cfb8cd5004ab89d7
normal <normal@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-08-24 20:28:08 +0000

    test/ruby/test_io.rb (test_recycled_fd_close): use IO#read to avoid ppoll call
    IO#sysread calls rb_wait_for_single_fd for compatibility, and
    perhaps something is amiss with that (unrelated to timer-thread
    elimination)
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64529 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_io.rb
+++ test/ruby/test_io.rb
@@ -3779,7 +3779,7 @@ __END__
       noex = Thread.new do # everything right and never see exceptions :)
         until sig_rd.wait_readable(0)
           IO.pipe do |r, w|
-            th = Thread.new { r.sysread(1) }
+            th = Thread.new { r.read(1) }
             w.write(dot)
             assert_same th, th.join(15), '"good" reader timeout'
             assert_equal(dot, th.value)
