1bbf778962743171693a9a279914bf9dae431405
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-08-27 13:59:09 +0000

    merge revision(s) 63994: [Backport #14920]
    	reduce tzset
    
    	* time.c (rb_localtime_r): call tzset() only after TZ environment
    	  variable is changed.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_4@64560 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- hash.c
+++ hash.c
@@ -3136,6 +3136,9 @@ env_str_new2(const char *ptr)
 
 static int env_path_tainted(const char *);
 
+static const char TZ_ENV[] = "TZ";
+extern int ruby_tz_update;
+
 static rb_encoding *
 env_encoding_for(const char *name, const char *ptr)
 {
@@ -3216,6 +3219,9 @@ env_delete(VALUE obj, VALUE name)
 	if (ENVMATCH(nam, PATH_ENV)) {
 	    RB_GC_GUARD(name);
 	    path_tainted = 0;
+	}
+	else if (ENVMATCH(nam, TZ_ENV)) {
+	    ruby_tz_update = 0;
 	}
 	return value;
     }
@@ -3575,6 +3581,9 @@ env_aset(VALUE obj, VALUE nm, VALUE val)
 	else {
 	    path_tainted_p(value);
 	}
+    }
+    else if (ENVMATCH(name, TZ_ENV)) {
+	ruby_tz_update = 0;
     }
     return val;
 }
