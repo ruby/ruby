82f463753fc57f3f6326e18467f9bdab90d0659e
kazu <kazu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-09-01 06:59:36 +0000

    Re-try to add workaround for warnings
    ```
    .../ext/psych/lib/psych/versions.rb:4: warning: already initialized constant Psych::VERSION
    .../.ext/common/psych/versions.rb:4: warning: previous definition of VERSION was here
    ```
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64609 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/psych/lib/psych/versions.rb
+++ ext/psych/lib/psych/versions.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Psych
   # The version is Psych you're using
-  VERSION = '3.1.0.pre1'
+  VERSION = '3.1.0.pre1' unless defined?(::Psych::VERSION)
 
   if RUBY_ENGINE == 'jruby'
     DEFAULT_SNAKEYAML_VERSION = '1.21'.freeze
