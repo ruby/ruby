ad10b43f7f3a56dd77708416a6cf5b2f16d07325
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-09-06 09:24:18 +0000

    re-patched r50599 because of test failure on TravisCI. [Backport #14060]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_4@64649 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.4.5"
-#define RUBY_RELEASE_DATE "2018-08-27"
-#define RUBY_PATCHLEVEL 322
+#define RUBY_RELEASE_DATE "2018-09-06"
+#define RUBY_PATCHLEVEL 323
 
 #define RUBY_RELEASE_YEAR 2018
-#define RUBY_RELEASE_MONTH 8
-#define RUBY_RELEASE_DAY 27
+#define RUBY_RELEASE_MONTH 9
+#define RUBY_RELEASE_DAY 6
 
 #include "ruby/version.h"
 
