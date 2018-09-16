24b079b1c8705296231f2a95beba67b204532f27
marcandre <marcandre@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-09-16 02:49:44 +0000

    [DOC] Improve String#strip documentation.
    Patch by Josh Goldberg. [Fix GH-1933] [ci skip]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64757 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- string.c
+++ string.c
@@ -9015,6 +9015,7 @@ rb_str_strip_bang(VALUE str)
  *
  *     "    hello    ".strip   #=> "hello"
  *     "\tgoodbye\r\n".strip   #=> "goodbye"
+ *     "hello".strip           #=> "hello"
  *     "\x00\t\n\v\f\r ".strip #=> ""
  */
 
