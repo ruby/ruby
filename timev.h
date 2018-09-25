0903356172dda9a90657a158cae1cd83b1910684
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-09-25 13:42:02 +0000

    Moved `struct vtm` stuff from internal.h to timev.h
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64833 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- timev.h
+++ timev.h
@@ -39,4 +39,12 @@ typedef unsigned LONG_LONG unsigned_time_t;
 # error cannot find integer type which size is same as time_t.
 #endif
 
+/* strftime.c */
+#ifdef RUBY_ENCODING_H
+VALUE rb_strftime_timespec(const char *format, size_t format_len, rb_encoding *enc,
+			   const struct vtm *vtm, struct timespec *ts, int gmt);
+VALUE rb_strftime(const char *format, size_t format_len, rb_encoding *enc,
+		  const struct vtm *vtm, VALUE timev, int gmt);
+#endif
+
 #endif
