ef69efef1da9a36d0953fdb0651faf881531781a
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-09-29 17:49:33 +0000

    no-op if it is T_STRING
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64887 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- re.c
+++ re.c
@@ -3084,6 +3084,9 @@ reg_operand(VALUE s, int check)
     if (SYMBOL_P(s)) {
 	return rb_sym2str(s);
     }
+    else if (RB_TYPE_P(s, T_STRING)) {
+        return s;
+    }
     else {
         return check ? rb_str_to_str(s) : rb_check_string_type(s);
     }
