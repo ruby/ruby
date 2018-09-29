7bcc535a05dbffcf057f3a4fba27bbcdee940dfa
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-09-29 17:49:03 +0000

    Remove unnecessary use of function pointer
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64884 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- re.c
+++ re.c
@@ -3085,7 +3085,7 @@ reg_operand(VALUE s, int check)
 	return rb_sym2str(s);
     }
     else {
-	return (check ? rb_str_to_str : rb_check_string_type)(s);
+	return check ? rb_str_to_str(s) : rb_check_string_type(s);
     }
 }
 
