49e96f9ed5d06a529670eec1234df00d0392c421
k0kubun <k0kubun@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-10-07 06:08:54 +0000

    add_spec.rb: skip unstable spec on mingw
    This sometimes crashes mspec worker like
    https://ci.appveyor.com/project/ruby/ruby/build/9806/job/37tx2atojy96227m
    
    and it doesn't generate helpful output and it seems hard to fix it soon.
    As AppVeyor is too unstable by too many factors, let me skip this
    at least for a short term. until AppVeyor gets stable.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64933 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/core/threadgroup/add_spec.rb
+++ spec/ruby/core/threadgroup/add_spec.rb
@@ -13,24 +13,26 @@ describe "ThreadGroup#add" do
     @thread.join
   end
 
-  it "adds the given thread to a group and returns self" do
-    @thread.group.should_not == nil
+  platform_is_not :mingw do
+    it "adds the given thread to a group and returns self" do
+      @thread.group.should_not == nil
 
-    tg = ThreadGroup.new
-    tg.add(@thread).should == tg
-    @thread.group.should == tg
-    tg.list.include?(@thread).should == true
-  end
+      tg = ThreadGroup.new
+      tg.add(@thread).should == tg
+      @thread.group.should == tg
+      tg.list.include?(@thread).should == true
+    end
 
-  it "removes itself from any other threadgroup" do
-    tg1 = ThreadGroup.new
-    tg2 = ThreadGroup.new
+    it "removes itself from any other threadgroup" do
+      tg1 = ThreadGroup.new
+      tg2 = ThreadGroup.new
 
-    tg1.add(@thread)
-    @thread.group.should == tg1
-    tg2.add(@thread)
-    @thread.group.should == tg2
-    tg2.list.include?(@thread).should == true
-    tg1.list.include?(@thread).should == false
+      tg1.add(@thread)
+      @thread.group.should == tg1
+      tg2.add(@thread)
+      @thread.group.should == tg2
+      tg2.list.include?(@thread).should == true
+      tg1.list.include?(@thread).should == false
+    end
   end
 end
