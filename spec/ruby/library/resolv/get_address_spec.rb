db201ecc94e785590df99f847ff87e47c2a957e1
Christophe Maximin <christophe.maximin@gmail.com>
2018-10-08 22:17:30 +0100

    Add Resolv.current_resolver

--- spec/ruby/library/resolv/get_address_spec.rb
+++ spec/ruby/library/resolv/get_address_spec.rb
@@ -17,3 +17,26 @@ describe "Resolv#getaddress" do
     }.should raise_error(Resolv::ResolvError)
   end
 end
+
+describe "Resolv.getaddress" do
+  it "calls DefaultResolver#getaddress" do
+    Resolv::DefaultResolver.should_receive(:getaddress).with("localhost")
+    Resolv.getaddress("localhost")
+  end
+
+  ruby_version_is "2.6" do
+    context "with a custom resolver" do
+      after do
+        Resolv.current_resolver = nil
+      end
+
+      it "calls #getaddress on the custom resolver" do
+        resolver = Resolv.new([])
+        resolver.should_receive(:getaddress).with("localhost")
+
+        Resolv.current_resolver = resolver
+        Resolv.getaddress("localhost")
+      end
+    end
+  end
+end
