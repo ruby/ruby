e94ede5a85f98c353f3e9f6a6d78d870237a98ad
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-10-09 06:18:28 +0000

    use `should include`.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64976 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/command_line/dash_v_spec.rb
+++ spec/ruby/command_line/dash_v_spec.rb
@@ -6,7 +6,7 @@ describe "The -v command line option" do
 
   describe "when used alone" do
     it "prints version and ends" do
-      ruby_exe(nil, args: '-v').include?(RUBY_DESCRIPTION).should == true
+      ruby_exe(nil, args: '-v').should include(RUBY_DESCRIPTION)
     end
   end
 end
