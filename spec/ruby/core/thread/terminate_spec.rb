c3d5e0261eb215f2bbe3430a73e074c9dc5faab0
k0kubun <k0kubun@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-10-10 11:35:58 +0000

    terminate_spec.rb: exclude unstable spec for MinGW
    This spec seems to randomly kill mspec worker.
    https://ci.appveyor.com/project/ruby/ruby/builds/19390874/job/wv1bsm8skd4e1pxl
    
    To make CI stable, let me exclude this for MinGW until we get some logs
    that help debugging or resolve that.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@64991 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/core/thread/terminate_spec.rb
+++ spec/ruby/core/thread/terminate_spec.rb
@@ -3,5 +3,7 @@ require_relative 'fixtures/classes'
 require_relative 'shared/exit'
 
 describe "Thread#terminate" do
-  it_behaves_like :thread_exit, :terminate
+  platform_is_not :mingw do
+    it_behaves_like :thread_exit, :terminate
+  end
 end
