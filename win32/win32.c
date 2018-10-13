e296f6cf63f41516ea8c7407d5c883fef2743322
k0kubun <k0kubun@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-10-13 01:32:20 +0000

    win32/win32.c: fix typo in comment [ci skip]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@65051 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- win32/win32.c
+++ win32/win32.c
@@ -1297,7 +1297,7 @@ rb_w32_start_process(const char *abspath, char *const *argv, int out_fd)
 {
     /* NOTE: This function is used by MJIT worker, so it can be used parallelly with
        Ruby's main thread. So functions touching things shared with main thread can't
-       be sued, like `ALLOCV` that may trigger GC or `FindFreeChildSlot` that finds
+       be used, like `ALLOCV` that may trigger GC or `FindFreeChildSlot` that finds
        a slot from shared memory without atomic locks. */
     struct ChildRecord child;
     char *cmd;
