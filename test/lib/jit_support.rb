2baf760cc2a0b878752e18602ca6d8420600c2fb
k0kubun <k0kubun@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-10-19 14:03:37 +0000

    test/lib/jit_support.rb: fire test_jit everywhere
    That was a workaround to make rubyci green for a short term after the
    MJIT merge. As we've done many portability fixes for MJIT, let's try
    running test_jit on all rubyci platforms.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@65175 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/lib/jit_support.rb
+++ test/lib/jit_support.rb
@@ -6,27 +6,6 @@ module JITSupport
     'clang',
   ]
 
-  def self.check_support
-    # Experimental. If you want to ensure JIT is working with this test, please set this for now.
-    if ENV.key?('RUBY_FORCE_TEST_JIT')
-      return true
-    end
-
-    # Very pessimistic check. With this check, we can't ensure JIT is working.
-    begin
-      _, err = JITSupport.eval_with_jit_without_retry('proc {}.call', verbose: 1, min_calls: 1, timeout: 10)
-    rescue Timeout::Error
-      $stderr.puts "TestJIT: #jit_supported? check timed out"
-      false
-    else
-      err.match?(JIT_SUCCESS_PREFIX).tap do |success|
-        unless success
-          $stderr.puts "TestJIT.check_support stderr:\n```\n#{err}\n```\n"
-        end
-      end
-    end
-  end
-
   module_function
   # Run Ruby script with --jit-wait (Synchronous JIT compilation).
   # Returns [stdout, stderr]
@@ -54,15 +33,6 @@ module JITSupport
     EnvUtil.invoke_ruby(args,
       '', true, true, timeout: timeout,
     )
-  end
-
-  def supported?
-    return @supported if defined?(@supported)
-    @supported = JITSupport.check_support.tap do |supported|
-      unless supported
-        warn "JIT tests are skipped since JIT seems not working. Set RUBY_FORCE_TEST_JIT=1 to let it fail.", uplevel: 1
-      end
-    end
   end
 
   def remove_mjit_logs(stderr)
