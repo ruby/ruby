4757c7eead9ef8aa999fb2a8b7f68dc14701914f
kazu <kazu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-10-20 13:14:02 +0000

    Run background threads while testing drb
    Do not start background thread on load test/drb/drbtest.rb,
    and stop threads on each test.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@65247 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/drb/test_drbssl.rb
+++ test/drb/test_drbssl.rb
@@ -15,37 +15,35 @@ class DRbSSLService < DRbService
   %w(ut_drb_drbssl.rb ut_array_drbssl.rb).each do |nm|
     add_service_command(nm)
   end
-  config = Hash.new
 
-  config[:SSLVerifyMode] = OpenSSL::SSL::VERIFY_PEER
-  config[:SSLVerifyCallback] = lambda{ |ok,x509_store|
-    true
-  }
-  begin
-    data = open("sample.key"){|io| io.read }
-    config[:SSLPrivateKey] = OpenSSL::PKey::RSA.new(data)
-    data = open("sample.crt"){|io| io.read }
-    config[:SSLCertificate] = OpenSSL::X509::Certificate.new(data)
-  rescue
-    # $stderr.puts "Switching to use self-signed certificate"
-    config[:SSLCertName] =
-      [ ["C","JP"], ["O","Foo.DRuby.Org"], ["CN", "Sample"] ]
+  def start
+    config = Hash.new
+
+    config[:SSLVerifyMode] = OpenSSL::SSL::VERIFY_PEER
+    config[:SSLVerifyCallback] = lambda{ |ok,x509_store|
+      true
+    }
+    begin
+      data = open("sample.key"){|io| io.read }
+      config[:SSLPrivateKey] = OpenSSL::PKey::RSA.new(data)
+      data = open("sample.crt"){|io| io.read }
+      config[:SSLCertificate] = OpenSSL::X509::Certificate.new(data)
+    rescue
+      # $stderr.puts "Switching to use self-signed certificate"
+      config[:SSLCertName] =
+        [ ["C","JP"], ["O","Foo.DRuby.Org"], ["CN", "Sample"] ]
+    end
+
+    @server = DRb::DRbServer.new('drbssl://:0', manager, config)
   end
-
-  uri = ARGV.shift if $0 == __FILE__
-  @server = DRb::DRbServer.new(uri || 'drbssl://:0', self.manager, config)
 end
 
 class TestDRbSSLCore < Test::Unit::TestCase
   include DRbCore
   def setup
+    @drb_service = DRbSSLService.new
+    super
     setup_service 'ut_drb_drbssl.rb'
-    super
-  end
-
-  def teardown
-    super
-    DRbService.finish
   end
 
   def test_02_unknown
@@ -61,13 +59,9 @@ end
 class TestDRbSSLAry < Test::Unit::TestCase
   include DRbAry
   def setup
+    @drb_service = DRbSSLService.new
+    super
     setup_service 'ut_array_drbssl.rb'
-    super
-  end
-
-  def teardown
-    super
-    DRbService.finish
   end
 end
 
