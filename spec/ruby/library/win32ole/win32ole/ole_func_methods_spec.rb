f0e567e2a0043fcc183d7b4667e7737797443254
k0kubun <k0kubun@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-10-27 13:57:23 +0000

    revert r65389 and r64993
    and move retry logic to internal method
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@65391 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/library/win32ole/win32ole/ole_func_methods_spec.rb
+++ spec/ruby/library/win32ole/win32ole/ole_func_methods_spec.rb
@@ -5,20 +5,7 @@ platform_is :windows do
 
   describe "WIN32OLE#ole_func_methods" do
     before :each do
-      # This part is unstable, so retrying 3 times.
-      tries = 0
-      begin
-        @ie = WIN32OLESpecs.new_ole('InternetExplorer.Application')
-      rescue WIN32OLERuntimeError => e
-        # WIN32OLERuntimeError: failed to create WIN32OLE object from `InternetExplorer.Application'
-        #     HRESULT error code:0x800704a6
-        #       A system shutdown has already been scheduled.
-        if tries < 3
-          tries += 1
-          $stderr.puts "WIN32OLE#ole_func_methods retry (#{tries}): #{e.class}: #{e.message}"
-          retry
-        end
-      end
+      @ie = WIN32OLESpecs.new_ole('InternetExplorer.Application')
     end
 
     after :each do
