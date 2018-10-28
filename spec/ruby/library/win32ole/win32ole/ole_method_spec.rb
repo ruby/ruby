d7d83fa0cf5202bc6aca917a82575f0e42cd4432
suke <suke@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-10-28 03:48:17 +0000

    use Scripting.Dictionary instead of InternetExplorer.Application
    * spec/ruby/library/win32ole/win32ole/ole_method_spec.rb: recuire in
      platform_is block.
    * spec/ruby/library/win32ole/win32ole/ole_func_methods_spec.rb: ditto.
    * spec/ruby/library/win32ole/win32ole/shared/ole_method.rb: use
      Scripting.Dictionary. InternetExplorer.Application is not available on
      some environments. Thanks to MSP-Greg (Greg L).
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@65405 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/library/win32ole/win32ole/ole_method_spec.rb
+++ spec/ruby/library/win32ole/win32ole/ole_method_spec.rb
@@ -1,8 +1,6 @@
-require_relative '../fixtures/classes'
-require_relative 'shared/ole_method'
-
 platform_is :windows do
-  require 'win32ole'
+  require_relative '../fixtures/classes'
+  require_relative 'shared/ole_method'
 
   describe "WIN32OLE#ole_method" do
     it_behaves_like :win32ole_ole_method, :ole_method
