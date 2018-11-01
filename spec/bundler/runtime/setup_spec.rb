64758c7c8419e2023d576767f3ca5d61323448d0
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2018-11-01 22:05:42 +0900

    Fixed typo.

--- spec/bundler/runtime/setup_spec.rb
+++ spec/bundler/runtime/setup_spec.rb
@@ -877,7 +877,7 @@ end
       FileUtils.ln_s(bundler_dir, File.join(gems_dir, "bundler-#{Bundler::VERSION}"))
 
       gemspec_file = ruby_core? ? "#{bundler_dir}/lib/bundler.gemspec" : "#{bundler_dir}/bundler.gemspec"
-      gemspec = File.read(gemmspec_file).
+      gemspec = File.read(gemspec_file).
                 sub("Bundler::VERSION", %("#{Bundler::VERSION}"))
       gemspec = gemspec.lines.reject {|line| line =~ %r{lib/bundler/version} }.join
 
