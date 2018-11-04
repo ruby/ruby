980840237593d8037a0453af8fdba9bf51a3abe0
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-11-04 02:02:28 +0000

    Refine Timezone class in test-spec
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@65525 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/core/time/new_spec.rb
+++ spec/ruby/core/time/new_spec.rb
@@ -117,8 +117,7 @@ end
 ruby_version_is "2.6" do
   describe "Time.new with a timezone argument" do
     it "returns a Time correspoinding to UTC time returned by local_to_utc" do
-      zone = mock('timezone')
-      zone.should_receive(:local_to_utc).and_return(Time::TM.new(2000, 1, 1, 6, 30, 0))
+      zone = TimeSpecs::Timezone.new("Asia/Colombo", "MMT", (5*3600+30*60))
       t = Time.new(2000, 1, 1, 12, 0, 0, zone)
       t.to_a[0, 6].should == [0, 0, 12, 1, 1, 2000]
       t.utc_offset.should == 19800
