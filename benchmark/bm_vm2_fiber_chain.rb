b58001d7b2b1496991e87e17447bf11e03a15dec
samuel <samuel@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-11-20 10:09:53 +0000

    Fix broken benchmark
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@65843 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- benchmark/bm_vm2_fiber_chain.rb
+++ benchmark/bm_vm2_fiber_chain.rb
@@ -10,25 +10,25 @@ def make_link(previous)
   end
 end
 
-def make_chain
-  chain = Fiber.new do
-    while true
-      Fiber.yield(message)
-    end
-  end
-
-  (fibers - 1).times do
+def make_chain(length, &block)
+  chain = Fiber.new(&block)
+  
+  (length - 1).times do
     chain = make_link(chain)
   end
 
   return chain
 end
 
-def run_benchmark(fibers, repeats, message = :hello)
+def run_benchmark(length, repeats, message = :hello)
   chain = nil
 
   time = Benchmark.realtime do
-    chain = make_chain
+    chain = make_chain(length) do
+      while true
+        Fiber.yield(message)
+      end
+    end
   end
 
   puts "Creating #{fibers} fibers took #{time}..."
