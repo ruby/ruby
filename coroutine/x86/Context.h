627490f4968977cda43318d27169a532d8ff8b7e
samuel <samuel@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-11-20 10:13:59 +0000

    Initial effort to support 32-bit Linux.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@65847 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ coroutine/x86/Context.h
@@ -0,0 +1,61 @@
+//
+//  File file is part of the "Coroutine" project and released under the MIT License.
+//
+//  Created by Samuel Williams on 3/11/2018.
+//  Copyright, 2018, by Samuel Williams. All rights reserved.
+//
+
+#pragma once
+
+#include <assert.h>
+#include <string.h>
+
+#if __cplusplus
+extern "C" {
+#endif
+
+#define COROUTINE void __attribute__((fastcall))
+
+const size_t COROUTINE_REGISTERS = 4;
+
+// The fiber context (stack pointer).
+typedef struct
+{
+	void **stack_pointer;
+} coroutine_context;
+
+// The initialization function.
+typedef void(* coroutine_start)(coroutine_context *from, coroutine_context *self) __attribute__((fastcall));
+
+void coroutine_initialize(
+	coroutine_context *context,
+	coroutine_start start,
+	void *stack_pointer,
+	size_t stack_size
+) {
+	/* Force 16-byte alignment */
+	context->stack_pointer = (void**)((uintptr_t)stack_pointer & ~0xF);
+
+	if (!start) {
+		assert(!context->stack_pointer);
+		/* We are main coroutine for this thread */
+		return;
+	}
+
+	*--context->stack_pointer = NULL;
+	*--context->stack_pointer = (void*)start;
+
+	context->stack_pointer -= COROUTINE_REGISTERS;
+	memset(context->stack_pointer, 0, sizeof(void*) * COROUTINE_REGISTERS);
+}
+
+coroutine_context * coroutine_transfer(coroutine_context * current, coroutine_context * target) __attribute__((fastcall));
+
+inline void coroutine_destroy(coroutine_context * context)
+{
+	context->stack_pointer = NULL;
+}
+
+#if __cplusplus
+}
+#endif
