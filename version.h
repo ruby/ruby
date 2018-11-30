cf18fc32728e7f1415cb9625e77435dac61cc9aa
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-11-30 23:58:17 +0000

    * 2018-12-01
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66108 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -3,8 +3,8 @@
 #define RUBY_PATCHLEVEL -1
 
 #define RUBY_RELEASE_YEAR 2018
-#define RUBY_RELEASE_MONTH 11
-#define RUBY_RELEASE_DAY 30
+#define RUBY_RELEASE_MONTH 12
+#define RUBY_RELEASE_DAY 1
 
 #include "ruby/version.h"
 
