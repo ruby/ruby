842d21e4007be04e1fea3a4a9649bd487734410f
cremno <cremno@mail.ru>
2018-11-30 20:22:37 +0100

    assume <time.h> exists

--- include/ruby/missing.h
+++ include/ruby/missing.h
@@ -29,9 +29,7 @@ extern "C" {
 #endif
 
 #if !defined(HAVE_STRUCT_TIMEVAL) || !defined(HAVE_STRUCT_TIMESPEC)
-#if defined(HAVE_TIME_H)
-# include <time.h>
-#endif
+#include <time.h>
 #if defined(HAVE_SYS_TIME_H)
 # include <sys/time.h>
 #endif
