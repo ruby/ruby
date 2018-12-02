c9512eb571d31832802923110030d46992bbfc8d
duerst <duerst@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-12-02 01:06:28 +0000

    reformat code [ci skip]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66122 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- regparse.c
+++ regparse.c
@@ -5760,19 +5760,11 @@ quantify_property_node(Node **np, ScanEnv* env, const char* propname, char repet
   r = create_property_node(np, env, propname);
   if (r != 0) return r;
   switch (repetitions) {
-    case '?':
-      upper = 1;
-      break;
-    case '+':
-      lower = 1;
-      break;
-    case '*':
-      break;
-    case '2':
-      lower = upper = 2;
-      break;
-    default:
-      return ONIGERR_PARSER_BUG;
+    case '?':  upper = 1;          break;
+    case '+':  lower = 1;          break;
+    case '*':                      break;
+    case '2':  lower = upper = 2;  break;
+    default :  return ONIGERR_PARSER_BUG;
   }
   return quantify_node(np, lower, upper);
 }
