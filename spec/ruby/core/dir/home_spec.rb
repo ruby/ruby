5e8e1fcf87601d2f91a5d58b2a92c7b6089adddf
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-12-04 00:40:49 +0000

    Use getent and cut on Solaris to get home directory
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66177 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/core/dir/home_spec.rb
+++ spec/ruby/core/dir/home_spec.rb
@@ -14,9 +14,15 @@ describe "Dir.home" do
     Dir.home.should == home_directory
   end
 
-  platform_is_not :windows do
+  platform_is :solaris do
     it "returns the named user's home directory, from the user database, as a string if called with an argument" do
-      Dir.home(ENV['USER']).should == `/bin/echo ~#{ENV['USER']}`.chomp
+      Dir.home(ENV['USER']).should == `getent passwd ~#{ENV['USER']}|cut -d: -f6`.chomp
+    end
+  end
+
+  platform_is_not :windows, :solaris do
+    it "returns the named user's home directory, from the user database, as a string if called with an argument" do
+      Dir.home(ENV['USER']).should == `echo ~#{ENV['USER']}`.chomp
     end
   end
 
