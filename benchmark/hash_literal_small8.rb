0f2a257fead701ca0e33a96421dc126fe7b53f50
tenderlove <tenderlove@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-12-06 18:20:11 +0000

    Add benchmark for hash small literals
    Co-Authored-By: Krzysztof Rybka <krzysztof.rybka@gmail.com>
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66256 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

---
+++ benchmark/hash_literal_small8.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+1_000_000.times.map { { "foo" => "bar", "bar" => "baz", "baz" => "lol", "lol" => "lgtm", "lgtm" => "nope", "nope" => "ok", "ok" => "again", "again" => "wait" } }
