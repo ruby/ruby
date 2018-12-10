7405bbda27a60f2c06af10f2ab49e5b15b043274
naruse <naruse@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-12-10 13:58:36 +0000

    Define HAVE_STMT_AND_DECL_IN_EXPR and use it [Bug #15293]
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66311 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/ruby.h
+++ include/ruby/ruby.h
@@ -1818,7 +1818,7 @@ VALUE rb_check_symbol(volatile VALUE *namep);
     do RUBY_CONST_ID_CACHE((var) =, (str)) while (0)
 #define CONST_ID_CACHE(result, str) RUBY_CONST_ID_CACHE(result, str)
 #define CONST_ID(var, str) RUBY_CONST_ID(var, str)
-#ifdef HAVE_BUILTIN___BUILTIN_CONSTANT_P
+#if defined(HAVE_BUILTIN___BUILTIN_CONSTANT_P) && defined(HAVE_STMT_AND_DECL_IN_EXPR)
 /* __builtin_constant_p and statement expression is available
  * since gcc-2.7.2.3 at least. */
 #define rb_intern(str) \
