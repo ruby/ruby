623c6d972a5365f469d4ceb0fc3326faf35ba7b6
hsbt <hsbt@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-12-18 02:01:39 +0000

    Bump version to psych 3.1.0.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66430 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- ext/psych/lib/psych/versions.rb
+++ ext/psych/lib/psych/versions.rb
@@ -1,7 +1,8 @@
+
 # frozen_string_literal: true
 module Psych
   # The version of Psych you are using
-  VERSION = '3.1.0.pre2' unless defined?(::Psych::VERSION)
+  VERSION = '3.1.0' unless defined?(::Psych::VERSION)
 
   if RUBY_ENGINE == 'jruby'
     DEFAULT_SNAKEYAML_VERSION = '1.23'.freeze
