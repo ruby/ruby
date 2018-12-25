83515c6c32ce02f6360107e4be1c558c8586b425
ko1 <ko1@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-12-25 07:21:45 +0000

    fix a test by restoring value checking.
    * test/ruby/test_literal.rb (test_hash_literal_frozen): restore value
      checking code which is removed accidentaly at r66466.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66548 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_literal.rb
+++ test/ruby/test_literal.rb
@@ -291,7 +291,8 @@ class TestRubyLiteral < Test::Unit::TestCase
       end
 
       ObjectSpace.each_object(Hash) do |a|
-        if a.class == Hash and !a.default_proc and a.size == 3
+        if a.class == Hash and !a.default_proc and a.size == 3 &&
+           a[0] == 1 && a[1] == 4 && a[2] == 17
           # should not be found.
           raise
         end
