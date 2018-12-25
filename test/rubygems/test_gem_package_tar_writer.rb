8cfbaf943efc0a533d0e6e6848951aecc262a9ff
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-12-25 13:33:51 +0000

    Restore SOURCE_DATE_EPOCH
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66554 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/rubygems/test_gem_package_tar_writer.rb
+++ test/rubygems/test_gem_package_tar_writer.rb
@@ -11,9 +11,12 @@ class TestGemPackageTarWriter < Gem::Package::TarTestCase
     @data = 'abcde12345'
     @io = TempIO.new
     @tar_writer = Gem::Package::TarWriter.new @io
+    @epoch = ENV["SOURCE_DATE_EPOCH"]
+    ENV["SOURCE_DATE_EPOCH"] = nil
   end
 
   def teardown
+    ENV["SOURCE_DATE_EPOCH"] = @epoch
     @tar_writer.close unless @tar_writer.closed?
     @io.close!
 
