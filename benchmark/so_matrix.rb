d1b14e8bb5c14f007e8fdcda7f25ffe438a18a51
shyouhei <shyouhei@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-12-27 06:12:09 +0000

    delete emacs mode lines [ci skip]
    These settings are now covered by .dir-locals.el.
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66584 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- benchmark/so_matrix.rb
+++ benchmark/so_matrix.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
-# -*- mode: ruby -*-
+# -*- Ruby -*-
 # $Id: matrix-ruby.code,v 1.4 2004/11/13 07:42:14 bfulgham Exp $
 # http://www.bagley.org/~doug/shootout/
 
--- benchmark/so_nested_loop.rb
+++ benchmark/so_nested_loop.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
-# -*- mode: ruby -*-
+# -*- Ruby -*-
 # $Id: nestedloop-ruby.code,v 1.4 2004/11/13 07:42:22 bfulgham Exp $
 # http://www.bagley.org/~doug/shootout/
 # from Avi Bryant
