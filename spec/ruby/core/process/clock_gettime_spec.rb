c20353a628b185c02261fbc4b5d5a981187a37d1
eregon <eregon@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-12-29 10:54:37 +0000

    Reenable spec on Linux to find out which clocks fail on RHEL
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66636 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/core/process/clock_gettime_spec.rb
+++ spec/ruby/core/process/clock_gettime_spec.rb
@@ -4,7 +4,7 @@ describe "Process.clock_gettime" do
   # This does not succeed with some platforms:
   # RHEL: https://rubyci.org/logs/rubyci.s3.amazonaws.com/rhel_zlinux/ruby-trunk/log/20181229T063303Z.fail.html.gz
   # Solaris: https://rubyci.org/logs/rubyci.s3.amazonaws.com/unstable11x/ruby-trunk/log/20181229T002409Z.fail.html.gz
-  platform_is_not :windows, :linux, :solaris do
+  platform_is_not :windows, :solaris do
     Process.constants.select { |c| c.to_s.start_with?('CLOCK_') }.each do |c|
       it "can be called with Process::#{c}" do
         value = Process.const_get(c)
