695cc19be963e35dabd85ea824a02f698bc260c0
k0kubun <k0kubun@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-12-30 07:36:27 +0000

    accept_nonblock_spec.rb: wait before calling #accept_nonblock
    fixes r66593 in a better way like r66646.
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66652 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/library/socket/tcpserver/accept_nonblock_spec.rb
+++ spec/ruby/library/socket/tcpserver/accept_nonblock_spec.rb
@@ -63,7 +63,7 @@ describe 'TCPServer#accept_nonblock' do
       end
     end
 
-    platform_is_not :windows, :solaris do # spurious
+    platform_is_not :windows do # spurious
       describe 'with a connected client' do
         before do
           @client = TCPSocket.new(ip_address, @server.connect_address.ip_port)
@@ -75,6 +75,7 @@ describe 'TCPServer#accept_nonblock' do
         end
 
         it 'returns a TCPSocket' do
+          IO.select([@server])
           @socket = @server.accept_nonblock
           @socket.should be_an_instance_of(TCPSocket)
         end
