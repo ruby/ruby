99d363184765ef6b11e739299a805c366171fd26
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-01-01 01:51:01 +0000

    * expand tabs.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66675 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- compile.c
+++ compile.c
@@ -7462,10 +7462,10 @@ iseq_compile_each0(rb_iseq_t *iseq, LINK_ANCHOR *const ret, const NODE *node, in
 	DECL_ANCHOR(args);
 	unsigned int flag = 0;
 	ID mid = node->nd_mid;
-	LABEL *else_label = 0;
-	LABEL *end_label = 0;
+        LABEL *else_label = 0;
+        LABEL *end_label = 0;
 	VALUE argc;
-	VALUE branches = 0;
+        VALUE branches = 0;
 
 	/* optimization shortcut
 	 *   obj["literal"] = value -> opt_aset_with(obj, "literal", value)
@@ -7508,7 +7508,7 @@ iseq_compile_each0(rb_iseq_t *iseq, LINK_ANCHOR *const ret, const NODE *node, in
             else_label = NEW_LABEL(line);
             end_label = NEW_LABEL(line);
             DECL_BRANCH_BASE(branches, nd_first_lineno(node), nd_first_column(node), nd_last_lineno(node), nd_last_column(node), "&.");
-	    ADD_INSNL(recv, line, branchnil, else_label);
+            ADD_INSNL(recv, line, branchnil, else_label);
             ADD_TRACE_BRANCH_COVERAGE(recv, nd_first_lineno(node), nd_first_column(node), nd_last_lineno(node), nd_last_column(node), "then", branches);
 	}
 	if (!popped) {
@@ -7541,10 +7541,10 @@ iseq_compile_each0(rb_iseq_t *iseq, LINK_ANCHOR *const ret, const NODE *node, in
 	    ADD_SEQ(ret, args);
 	}
 	ADD_SEND_WITH_FLAG(ret, line, mid, argc, INT2FIX(flag));
-	if (else_label && end_label) {
-	    ADD_INSNL(ret, line, jump, end_label);
+        if (else_label && end_label) {
+            ADD_INSNL(ret, line, jump, end_label);
             ADD_LABEL(ret, else_label);
-	    ADD_TRACE_BRANCH_COVERAGE(ret, nd_first_lineno(node), nd_first_column(node), nd_last_lineno(node), nd_last_column(node), "else", branches);
+            ADD_TRACE_BRANCH_COVERAGE(ret, nd_first_lineno(node), nd_first_column(node), nd_last_lineno(node), nd_last_column(node), "else", branches);
             ADD_LABEL(ret, end_label);
         }
 	ADD_INSN(ret, line, pop);
