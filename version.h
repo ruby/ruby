06c9877346fd88279ab664ea56e8b970667cdaa2
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-01-10 15:02:22 +0000

    * 2019-01-11
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_5@66785 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.5.4"
-#define RUBY_RELEASE_DATE "2019-01-10"
+#define RUBY_RELEASE_DATE "2019-01-11"
 #define RUBY_PATCHLEVEL 128
 
 #define RUBY_RELEASE_YEAR 2019
 #define RUBY_RELEASE_MONTH 1
-#define RUBY_RELEASE_DAY 10
+#define RUBY_RELEASE_DAY 11
 
 #include "ruby/version.h"
 
