b30f3a805f469bb40dfd83b8b9c01bec4c875bfd
k0kubun <k0kubun@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-01-21 01:11:57 +0000

    test_io.rb: skip test on MJIT to prevent random failure
    like this:
    http://ci.rvm.jp/results/trunk-mjit-wait@silicon-docker/1636642
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66890 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_io.rb
+++ test/ruby/test_io.rb
@@ -1500,6 +1500,7 @@ class TestIO < Test::Unit::TestCase
   end if have_nonblock?
 
   def test_read_nonblock_no_exceptions
+    skip '[ruby-core:90895] MJIT worker may leave fd open in a forked child' if RubyVM::MJIT.enabled? # TODO: consider acquiring GVL from MJIT worker.
     with_pipe {|r, w|
       assert_equal :wait_readable, r.read_nonblock(4096, exception: false)
       w.puts "HI!"
