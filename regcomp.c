780c2b9853219b56a18c2114ffb43801780b346a
K.Takata <kentkt@csc.jp>
2019-01-29 18:59:19 +0900

    Remove old code for BMH search
    Remove the code for Boyer-Moore-Horspool search.
    Now we are using Sunday's quick search.
    
    https://github.com/k-takata/Onigmo/commit/3d9072419a1578b742a422412d004fd8a54209fd

--- regcomp.c
+++ regcomp.c
@@ -4213,89 +4213,6 @@ restart:
   return r;
 }
 
-#ifndef USE_SUNDAY_QUICK_SEARCH
-/* set skip map for Boyer-Moore search */
-static int
-set_bm_skip(UChar* s, UChar* end, regex_t* reg,
-            UChar skip[], int** int_skip, int ignore_case)
-{
-  OnigDistance i, len;
-  int clen, flen, n, j, k;
-  UChar *p, buf[ONIGENC_MBC_CASE_FOLD_MAXLEN];
-  OnigCaseFoldCodeItem items[ONIGENC_GET_CASE_FOLD_CODES_MAX_NUM];
-  OnigEncoding enc = reg->enc;
-
-  len = end - s;
-  if (len < ONIG_CHAR_TABLE_SIZE) {
-    for (i = 0; i < ONIG_CHAR_TABLE_SIZE; i++) skip[i] = (UChar )len;
-
-    n = 0;
-    for (i = 0; i < len - 1; i += clen) {
-      p = s + i;
-      if (ignore_case)
-        n = ONIGENC_GET_CASE_FOLD_CODES_BY_STR(enc, reg->case_fold_flag,
-                                               p, end, items);
-      clen = enclen(enc, p, end);
-      if (p + clen > end)
-        clen = (int )(end - p);
-
-      for (j = 0; j < n; j++) {
-        if ((items[j].code_len != 1) || (items[j].byte_len != clen))
-          return 1;  /* different length isn't supported. */
-        flen = ONIGENC_CODE_TO_MBC(enc, items[j].code[0], buf[j]);
-        if (flen != clen)
-          return 1;  /* different length isn't supported. */
-      }
-      for (j = 0; j < clen; j++) {
-        skip[s[i + j]] = (UChar )(len - 1 - i - j);
-        for (k = 0; k < n; k++) {
-          skip[buf[k][j]] = (UChar )(len - 1 - i - j);
-        }
-      }
-    }
-  }
-  else {
-# if OPT_EXACT_MAXLEN < ONIG_CHAR_TABLE_SIZE
-    /* This should not happen. */
-    return ONIGERR_TYPE_BUG;
-# else
-    if (IS_NULL(*int_skip)) {
-      *int_skip = (int* )xmalloc(sizeof(int) * ONIG_CHAR_TABLE_SIZE);
-      if (IS_NULL(*int_skip)) return ONIGERR_MEMORY;
-    }
-    for (i = 0; i < ONIG_CHAR_TABLE_SIZE; i++) (*int_skip)[i] = (int )len;
-
-    n = 0;
-    for (i = 0; i < len - 1; i += clen) {
-      p = s + i;
-      if (ignore_case)
-        n = ONIGENC_GET_CASE_FOLD_CODES_BY_STR(enc, reg->case_fold_flag,
-                                               p, end, items);
-      clen = enclen(enc, p, end);
-      if (p + clen > end)
-        clen = (int )(end - p);
-
-      for (j = 0; j < n; j++) {
-        if ((items[j].code_len != 1) || (items[j].byte_len != clen))
-          return 1;  /* different length isn't supported. */
-        flen = ONIGENC_CODE_TO_MBC(enc, items[j].code[0], buf[j]);
-        if (flen != clen)
-          return 1;  /* different length isn't supported. */
-      }
-      for (j = 0; j < clen; j++) {
-        (*int_skip)[s[i + j]] = (int )(len - 1 - i - j);
-        for (k = 0; k < n; k++) {
-          (*int_skip)[buf[k][j]] = (int )(len - 1 - i - j);
-        }
-      }
-    }
-# endif
-  }
-  return 0;
-}
-
-#else /* USE_SUNDAY_QUICK_SEARCH */
-
 /* set skip map for Sunday's quick search */
 static int
 set_bm_skip(UChar* s, UChar* end, regex_t* reg,
@@ -4397,7 +4314,6 @@ endcheck:
   }
   return (int)len;
 }
-#endif /* USE_SUNDAY_QUICK_SEARCH */
 
 typedef struct {
   OnigDistance min;  /* min byte length */
