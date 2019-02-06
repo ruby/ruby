4c0e21add7c87b70df27fbff81d8f192a467556d
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-02-06 00:11:04 +0000

    common.mk: allow brace expansion for benchmark targets
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@67014 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- common.mk
+++ common.mk
@@ -1157,7 +1157,7 @@ bisect-ruby: PHONY
 COMPARE_RUBY = $(BASERUBY)
 BENCH_RUBY = $(MINIRUBY)
 ITEM =
-ARGS = $$(find $(srcdir)/benchmark -maxdepth 1 -name '*$(ITEM)*.yml' -o -name '*$(ITEM)*.rb' | sort)
+ARGS = $$(ls $(srcdir)/benchmark/*$(ITEM)*.{yml,rb} 2>/dev/null)
 OPTS =
 
 # You can pass several options through OPTS environment variable.
