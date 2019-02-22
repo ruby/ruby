b9357e4d1b6f318d2ffa7323f14f01272ad336ee
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-02-22 06:07:30 +0000

    File#birthtime depends on the kernel version on Linux
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@67117 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- spec/ruby/core/file/birthtime_spec.rb
+++ spec/ruby/core/file/birthtime_spec.rb
@@ -50,9 +50,11 @@ describe "File#birthtime" do
     end
   end
 
-  platform_is :linux, :openbsd do
+  platform_is :openbsd do
     it "raises an NotImplementedError" do
       lambda { @file.birthtime }.should raise_error(NotImplementedError)
     end
   end
+
+  # TODO: depends on Linux kernel version
 end
