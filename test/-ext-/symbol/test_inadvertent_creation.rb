1598b5e7b343b6c05e6cab47ac3878276aea6d37
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-03-06 11:49:53 +0000

    Symbol creation is not a problem now unless pinned down
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@67187 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/-ext-/symbol/test_inadvertent_creation.rb
+++ test/-ext-/symbol/test_inadvertent_creation.rb
@@ -20,21 +20,13 @@ module Test_Symbol
 
     def assert_not_interned_error(obj, meth, name, msg = nil, &block)
       e = assert_raise(NameError, msg) {obj.__send__(meth, name, &block)}
-      if Symbol === name
-        assert_not_pinneddown(name, msg)
-      else
-        assert_not_interned(name, msg)
-      end
+      assert_not_pinneddown(name, msg)
       e
     end
 
     def assert_not_interned_false(obj, meth, name, msg = nil)
       assert_not_send([obj, meth, name], msg)
-      if Symbol === name
-        assert_not_pinneddown(name, msg)
-      else
-        assert_not_interned(name, msg)
-      end
+      assert_not_pinneddown(name, msg)
     end
 
     Feature5072 = '[ruby-core:38367]'
