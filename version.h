80266bfb39984cd6f1b9c5fed790a738fcd54d16
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-03-06 11:32:02 +0000

    merge revision(s) 67183: [Backport #15607]
    	Fix PTY.open on OpenBSD [Bug #15607]
    
    	From: Jeremy Evans <code@jeremyevans.net>
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_5@67186 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.5.4"
-#define RUBY_RELEASE_DATE "2019-02-11"
-#define RUBY_PATCHLEVEL 140
+#define RUBY_RELEASE_DATE "2019-03-06"
+#define RUBY_PATCHLEVEL 141
 
 #define RUBY_RELEASE_YEAR 2019
-#define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 11
+#define RUBY_RELEASE_MONTH 3
+#define RUBY_RELEASE_DAY 6
 
 #include "ruby/version.h"
 
