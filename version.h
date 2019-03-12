a648423e271f7bb24bee730101b3b6d4051cd599
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-03-12 12:59:25 +0000

    merge revision(s) 64636,64637: [Backport #15659]
    	AIX doesn't set OptionLength for boolean options
    
    	https://rubyci.org/logs/rubyci.s3.amazonaws.com/aix71_ppc/ruby-trunk/log/20180904T103302Z.fail.html.gz
    	https://www.ibm.com/support/knowledgecenter/ja/ssw_aix_72/com.ibm.aix.commtrf2/getsockopt.htm
    
    	fix typo
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_5@67228 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.5.4"
-#define RUBY_RELEASE_DATE "2019-03-11"
-#define RUBY_PATCHLEVEL 144
+#define RUBY_RELEASE_DATE "2019-03-12"
+#define RUBY_PATCHLEVEL 145
 
 #define RUBY_RELEASE_YEAR 2019
 #define RUBY_RELEASE_MONTH 3
-#define RUBY_RELEASE_DAY 11
+#define RUBY_RELEASE_DAY 12
 
 #include "ruby/version.h"
 
