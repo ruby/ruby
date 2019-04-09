b0fab6e4139eee0e998e64152db4f93fc17fc71c
Jeremy Evans <code@jeremyevans.net>
2019-04-08 19:45:54 -0700

    Set symbol export for rb_hash_stlike_foreach
    This fixes MJIT after rb_hash_stlike_foreach used vm_args.c.

--- internal.h
+++ internal.h
@@ -1661,7 +1661,9 @@ VALUE rb_hash_set_pair(VALUE hash, VALUE pair);
 
 int rb_hash_stlike_lookup(VALUE hash, st_data_t key, st_data_t *pval);
 int rb_hash_stlike_delete(VALUE hash, st_data_t *pkey, st_data_t *pval);
+RUBY_SYMBOL_EXPORT_BEGIN
 int rb_hash_stlike_foreach(VALUE hash, st_foreach_callback_func *func, st_data_t arg);
+RUBY_SYMBOL_EXPORT_END
 int rb_hash_stlike_foreach_with_replace(VALUE hash, st_foreach_check_callback_func *func, st_update_callback_func *replace, st_data_t arg);
 int rb_hash_stlike_update(VALUE hash, st_data_t key, st_update_callback_func func, st_data_t arg);
 
