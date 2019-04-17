e85b60f84b399f68cdc7ef787dd4be7a3774c5c5
svn <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-04-17 06:16:39 +0000

    * expand tabs.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@67585 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- gc.c
+++ gc.c
@@ -4981,7 +4981,7 @@ gc_mark_children(rb_objspace_t *objspace, VALUE obj)
       case T_CLASS:
       case T_MODULE:
 	mark_m_tbl(objspace, RCLASS_M_TBL(obj));
-	gc_mark(objspace, RCLASS_SUPER((VALUE)obj));
+        gc_mark(objspace, RCLASS_SUPER((VALUE)obj));
 	if (!RCLASS_EXT(obj)) break;
         mark_tbl_no_pin(objspace, RCLASS_IV_TBL(obj));
 	mark_const_tbl(objspace, RCLASS_CONST_TBL(obj));
@@ -4991,7 +4991,7 @@ gc_mark_children(rb_objspace_t *objspace, VALUE obj)
 	if (FL_TEST(obj, RICLASS_IS_ORIGIN)) {
 	    mark_m_tbl(objspace, RCLASS_M_TBL(obj));
 	}
-	gc_mark(objspace, RCLASS_SUPER((VALUE)obj));
+        gc_mark(objspace, RCLASS_SUPER((VALUE)obj));
 	if (!RCLASS_EXT(obj)) break;
 	mark_m_tbl(objspace, RCLASS_CALLABLE_M_TBL(obj));
 	break;
