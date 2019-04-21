f5eba895d08c5bcee0430a85a33c55408c015fda
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-04-21 06:08:00 +0000

    Remove unused variable and check if successed
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@67680 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/irb/test_raise_no_backtrace_exception.rb
+++ test/irb/test_raise_no_backtrace_exception.rb
@@ -4,7 +4,7 @@ require 'test/unit'
 module TestIRB
   class TestRaiseNoBacktraceException < Test::Unit::TestCase
     def test_raise_exception
-      status = assert_in_out_err(%w[-rirb -W1 -e IRB.start(__FILE__) -- -f --], <<-IRB, /Exception: foo/, [])
+      assert_in_out_err(%w[-rirb -W1 -e IRB.start(__FILE__) -- -f --], <<-IRB, /Exception: foo/, [], success: true)
       e = Exception.new("foo")
       def e.backtrace; nil; end
       raise e
