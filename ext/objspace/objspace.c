e50e60b96a90db8dd92a39f89bd1315d63e1515d
git <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-04-23 12:34:10 +0900

    * expand tabs.

--- ext/objspace/objspace.c
+++ ext/objspace/objspace.c
@@ -188,7 +188,7 @@ type2sym(enum ruby_value_type i)
 	CASE_TYPE(T_IMEMO);
 	CASE_TYPE(T_NODE);
 	CASE_TYPE(T_ICLASS);
-	CASE_TYPE(T_MOVED);
+        CASE_TYPE(T_MOVED);
 	CASE_TYPE(T_ZOMBIE);
 #undef CASE_TYPE
       default: rb_bug("type2sym: unknown type (%d)", i);
