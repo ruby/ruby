c06821161ae9dcfebeabd30ec7ab598ee70c89ce
git <svn@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-05-01 00:03:58 +0900

    * 2019-05-01

--- version.h
+++ version.h
@@ -5,8 +5,8 @@
 #define RUBY_PATCHLEVEL -1
 
 #define RUBY_RELEASE_YEAR 2019
-#define RUBY_RELEASE_MONTH 4
-#define RUBY_RELEASE_DAY 30
+#define RUBY_RELEASE_MONTH 5
+#define RUBY_RELEASE_DAY 1
 
 #include "ruby/version.h"
 
