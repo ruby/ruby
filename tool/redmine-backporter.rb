173d25e810e58b682ecc2ff67d8a02abd16ecdb9
NAKAMURA Usaku <usa@ruby-lang.org>
2019-04-30 22:24:45 +0900

    Use redirect keyword arguments instead of ">"

--- tool/redmine-backporter.rb
+++ tool/redmine-backporter.rb
@@ -436,7 +436,7 @@ eom
       next
     end
 
-    if system("svn info #{RUBY_REPO_PATH&.shellescape} > #{IO::NULL} 2>&1") # SVN
+    if system("svn info #{RUBY_REPO_PATH&.shellescape}", %i(out err) => IO::NULL) # SVN
       if log = find_svn_log("##@issue]") && /revision="(?<rev>\d+)/ =~ log
         rev = "r#{rev}"
       end
