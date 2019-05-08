a95ca6d5e9c1c7371f7324159840ac1e8c013125
Takashi Kokubun <takashikkbn@gmail.com>
2019-05-08 17:30:11 +0900

    Trim MJIT output from TestHideSkip
    to prevent failure like
    https://app.wercker.com/ruby/ruby/runs/mjit-test1/5cd28aa6ab79a30008ee819b?step=5cd28b2403f44600070db083
    with --jit-verbose=1.

--- test/testunit/test_hideskip.rb
+++ test/testunit/test_hideskip.rb
@@ -5,8 +5,12 @@ class TestHideSkip < Test::Unit::TestCase
   def test_hideskip
     assert_not_match(/^ *1\) Skipped/, hideskip)
     assert_match(/^ *1\) Skipped/, hideskip("--show-skip"))
-    assert_match(/assertions\/s.\n+1 tests, 0 assertions, 0 failures, 0 errors, 1 skips/, hideskip("--hide-skip"))
+    output = hideskip("--hide-skip")
+    output.gsub!(/Successful MJIT finish\n/, '') if RubyVM::MJIT.enabled?
+    assert_match(/assertions\/s.\n+1 tests, 0 assertions, 0 failures, 0 errors, 1 skips/, output)
   end
+
+  private
 
   def hideskip(*args)
     IO.popen([*@options[:ruby], "#{File.dirname(__FILE__)}/test4test_hideskip.rb",
