49fd414b82ac1d34db41faf2821515ae22eca02b
Nobuyoshi Nakada <nobu@ruby-lang.org>
2019-05-16 14:58:57 +0900

    Fix fd leak
    merged https://github.com/rubygems/rubygems/pull/2765

--- test/rubygems/test_gem_specification.rb
+++ test/rubygems/test_gem_specification.rb
@@ -2898,7 +2898,7 @@ duplicate dependency on c (>= 1.2.3, development), (~> 1.2) use:
     util_setup_validate
 
     FileUtils.mkdir_p File.join(@tempdir, 'bin')
-    File.open File.join(@tempdir, 'bin', 'exec'), 'w'
+    File.write File.join(@tempdir, 'bin', 'exec'), ''
     FileUtils.mkdir_p File.join(@tempdir, 'exec')
 
     use_ui @ui do
