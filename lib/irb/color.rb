c5cbabf8a8aa42b7dcf943c64274489286174108
NAKAMURA Usaku <usa@ruby-lang.org>
2019-05-23 15:33:40 +0900

    Use colorized IRB on Windows without checking `TERM`

--- lib/irb/color.rb
+++ lib/irb/color.rb
@@ -43,7 +43,7 @@ module IRB # :nodoc:
 
     class << self
       def colorable?
-        $stdout.tty? && ENV.key?('TERM') && ENV['TERM'] != 'dumb'
+        $stdout.tty? && (/mswin|mingw/ =~ RUBY_PLATFORM || (ENV.key?('TERM') && ENV['TERM'] != 'dumb'))
       end
 
       def inspect_colorable?(obj)
