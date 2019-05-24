a28a40f69c91ae269f3f815dc872f5866a03d67d
David Rodríguez <deivid.rodriguez@riseup.net>
2019-05-24 17:46:26 +0200

    [bundler/bundler] Clarify spec description
    https://github.com/bundler/bundler/commit/b2abde04aa

--- spec/bundler/commands/exec_spec.rb
+++ spec/bundler/commands/exec_spec.rb
@@ -842,7 +842,7 @@ __FILE__: #{path.to_s.inspect}
   context "nested bundle exec" do
     let(:system_gems_to_install) { super() << :bundler }
 
-    context "with shared gems disabled" do
+    context "when bundle in a local path" do
       before do
         gemfile <<-G
           source "#{file_uri_for(gem_repo1)}"
@@ -851,7 +851,7 @@ __FILE__: #{path.to_s.inspect}
         bundle :install, :system_bundler => true, :path => "vendor/bundler"
       end
 
-      it "overrides disable_shared_gems so bundler can be found", :ruby_repo do
+      it "correctly shells out", :ruby_repo do
         system_gems :bundler
         file = bundled_app("file_that_bundle_execs.rb")
         create_file(file, <<-RB)
