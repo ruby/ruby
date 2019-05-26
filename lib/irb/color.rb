30671d97d159a4bd41607cde638657f024746539
aycabta <aycabta@gmail.com>
2019-05-27 02:55:37 +0900

    Support :@@cvar and : on colorize

--- lib/irb/color.rb
+++ lib/irb/color.rb
@@ -140,7 +140,7 @@ module IRB # :nodoc:
         case token
         when :on_symbeg
           @stack << true
-        when :on_ident, :on_op, :on_const, :on_ivar, :on_kw
+        when :on_ident, :on_op, :on_const, :on_ivar, :on_cvar, :on_gvar, :on_kw
           if @stack.last # Pop only when it's Symbol
             @stack.pop
             return prev_state
