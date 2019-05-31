8b39df854d7dea1c15949c6b047bcb0f0a12c287
Nobuyoshi Nakada <nobu@ruby-lang.org>
2019-05-31 16:31:22 +0900

    Let irb use an empty file as irbrc
    to get rid of side-effect by existing .irbrc file.

--- spec/ruby/core/binding/irb_spec.rb
+++ spec/ruby/core/binding/irb_spec.rb
@@ -4,8 +4,9 @@ ruby_version_is "2.5" do
   describe "Binding#irb" do
     it "creates an IRB session with the binding in scope" do
       irb_fixture = fixture __FILE__, "irb.rb"
+      irbrc_fixture = fixture __FILE__, "irbrc"
 
-      out = IO.popen([*ruby_exe, irb_fixture], "r+") do |pipe|
+      out = IO.popen([{"IRBRC"=>irbrc_fixture}, *ruby_exe, irb_fixture], "r+") do |pipe|
         pipe.puts "a ** 2"
         pipe.puts "exit"
         pipe.readlines.map(&:chomp)
