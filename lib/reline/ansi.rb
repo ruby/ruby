552c42f51f75eeeaa5a46e2df28fd5dab77261da
Nobuyoshi Nakada <nobu@ruby-lang.org>
2019-06-01 00:34:38 +0900

    Seprate raw keystroke config for each platforms

--- lib/reline/ansi.rb
+++ lib/reline/ansi.rb
@@ -1,4 +1,16 @@
 class Reline::ANSI
+  RAW_KEYSTROKE_CONFIG = {
+    key_mapping: {
+      [27, 91, 65] => :ed_prev_history,     # ↑
+      [27, 91, 66] => :ed_next_history,     # ↓
+      [27, 91, 67] => :ed_next_char,        # →
+      [27, 91, 68] => :ed_prev_char,        # ←
+      [27, 91, 51, 126] => :key_delete,     # Del
+      [27, 91, 49, 126] => :ed_move_to_beg, # Home
+      [27, 91, 52, 126] => :ed_move_to_end, # End
+    }.each_key(&:freeze).freeze,
+  }.freeze
+
   @@input = STDIN
   def self.input=(val)
     @@input = val
