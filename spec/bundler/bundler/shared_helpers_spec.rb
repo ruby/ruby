370428164c2fb1d724c869b6de14123e6a97cc4f
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-06-09 11:00:21 +0900

    Fixed wrong BUNDLE_BIN_PATH for ruby core.

--- spec/bundler/bundler/shared_helpers_spec.rb
+++ spec/bundler/bundler/shared_helpers_spec.rb
@@ -401,7 +401,7 @@ RSpec.describe Bundler::SharedHelpers do
 
       it "sets BUNDLE_BIN_PATH to the bundle executable file" do
         subject.set_bundle_environment
-        bundle_exe = ruby_core? ? "../../../../exe/bundle" : "../../../exe/bundle"
+        bundle_exe = ruby_core? ? "../../../../../bin/bundle" : "../../../exe/bundle"
         expect(ENV["BUNDLE_BIN_PATH"]).to eq(File.expand_path(bundle_exe, __FILE__))
       end
     end
