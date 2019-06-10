626ae5a9b6531412f598b2f37a15821628857707
Benoit Daloze <eregontp@gmail.com>
2019-06-10 12:47:08 +0200

    Resolv specs should pass on Windows now

--- spec/ruby/library/resolv/get_name_spec.rb
+++ spec/ruby/library/resolv/get_name_spec.rb
@@ -2,13 +2,11 @@ require_relative '../../spec_helper'
 require 'resolv'
 
 describe "Resolv#getname" do
-  platform_is_not :windows do
-    it "resolves 127.0.0.1" do
-      hosts = Resolv::Hosts.new(fixture(__FILE__ , "hosts"))
-      res = Resolv.new([hosts])
+  it "resolves 127.0.0.1" do
+    hosts = Resolv::Hosts.new(fixture(__FILE__ , "hosts"))
+    res = Resolv.new([hosts])
 
-      res.getname("127.0.0.1").should == "localhost"
-    end
+    res.getname("127.0.0.1").should == "localhost"
   end
 
   it "raises ResolvError when there is no result" do
