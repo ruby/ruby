aa32465ab36a56655d214313b828422563e109af
Nobuyoshi Nakada <nobu@ruby-lang.org>
2019-06-14 15:31:03 +0900

    irb.rb: [DOC] the default prompt includes :PROMPT_N [ci skip]

--- lib/irb.rb
+++ lib/irb.rb
@@ -170,6 +170,7 @@ require "irb/version"
 #
 #     IRB.conf[:PROMPT_MODE][:DEFAULT] = {
 #       :PROMPT_I => "%N(%m):%03n:%i> ",
+#       :PROMPT_N => "%N(%m):%03n:%i> ",
 #       :PROMPT_S => "%N(%m):%03n:%i%l ",
 #       :PROMPT_C => "%N(%m):%03n:%i* ",
 #       :RETURN => "%s\n" # used to printf
