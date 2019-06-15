d118c84b0b9110462e479487ffaf175a75e5718e
Jeremy Evans <code@jeremyevans.net>
2019-06-14 17:50:31 -0700

    Fix IO#scanf on pipes on Windows
    IO.seek on a pipe on Windows raises Errno::EINVAL instead of
    Errno::ESPIPE.
    
    Fixes Ruby Bug #15199

--- lib/scanf.rb
+++ lib/scanf.rb
@@ -660,7 +660,7 @@ class IO
 
     begin
       seek(start_position + matched_so_far, IO::SEEK_SET)
-    rescue Errno::ESPIPE
+    rescue Errno::ESPIPE, Errno::EINVAL
     end
 
     soak_up_spaces if fstr.last_spec && fstr.space
