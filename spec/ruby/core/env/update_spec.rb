51062a091a238faab4c397bdb43195e430064787
Kenichi Kamiya <kachick1@gmail.com>
2019-06-21 12:28:28 +0900

    Alias ENV.merge! as ENV.update

--- spec/ruby/core/env/update_spec.rb
+++ spec/ruby/core/env/update_spec.rb
@@ -1,25 +1,6 @@
 require_relative '../../spec_helper'
+require_relative 'shared/update'
 
 describe "ENV.update" do
-
-  it "adds the parameter hash to ENV" do
-    ENV["foo"].should == nil
-    ENV.update "foo" => "bar"
-    ENV["foo"].should == "bar"
-    ENV.delete "foo"
-  end
-
-  it "yields key, the old value and the new value when replacing entries" do
-    ENV.update "foo" => "bar"
-    ENV["foo"].should == "bar"
-    ENV.update("foo" => "boo") do |key, old, new|
-      key.should == "foo"
-      old.should == "bar"
-      new.should == "boo"
-      "rab"
-    end
-    ENV["foo"].should == "rab"
-    ENV.delete "foo"
-  end
-
+  it_behaves_like :env_update, :update
 end
