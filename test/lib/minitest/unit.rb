4d650bc25737a4d2c0dcc9d4c845688cb5dc85ce
Nobuyoshi Nakada <nobu@ruby-lang.org>
2019-06-22 09:29:33 +0900

    Fix over-expansion
    * test/lib/minitest/unit.rb (MiniTest::Assertions#mu_pp_for_diff):
      do not expand escaped backslash followed by 'n'.

--- test/lib/minitest/unit.rb
+++ test/lib/minitest/unit.rb
@@ -179,7 +179,7 @@ module MiniTest
     # uses mu_pp to do the first pass and then cleans it up.
 
     def mu_pp_for_diff obj
-      mu_pp(obj).gsub(/\\n/, "\n").gsub(/:0x[a-fA-F0-9]{4,}/m, ':0xXXXXXX')
+      mu_pp(obj).gsub(/(?<!\\)(?:\\\\)*\K\\n/, "\n").gsub(/:0x[a-fA-F0-9]{4,}/m, ':0xXXXXXX')
     end
 
     def _assertions= n # :nodoc:
