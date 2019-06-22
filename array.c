7447d3fa961919a2cd58847efcb6ea03608e28c3
Luke Gruber <luke.gru@gmail.com>
2019-06-22 18:37:08 -0400

    array.c: always check frozenness in Array#unshift. Fixes [Bug #15952]
    Closes: https://github.com/ruby/ruby/pull/2251

--- array.c
+++ array.c
@@ -1373,6 +1373,8 @@ ary_ensure_room_for_unshift(VALUE ary, int argc)
 	rb_raise(rb_eIndexError, "index %ld too big", new_len);
     }
 
+    rb_ary_modify(ary);
+
     if (ARY_SHARED_P(ary)) {
 	VALUE shared = ARY_SHARED(ary);
 	capa = RARRAY_LEN(shared);
@@ -1383,7 +1385,6 @@ ary_ensure_room_for_unshift(VALUE ary, int argc)
 	}
     }
 
-    rb_ary_modify(ary);
     capa = ARY_CAPA(ary);
     if (capa - (capa >> 6) <= new_len) {
 	ary_double_capa(ary, new_len);
