6c6bf9ffcbfeb8be9d9c342e7604b74ec819e88a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-06-24 23:54:19 +1200

    Add `ucontext` coroutine implementation for generic fallback.

--- coroutine/win64/Context.h
+++ coroutine/win64/Context.h
@@ -10,10 +10,6 @@
 #include <assert.h>
 #include <string.h>
 
-#if __cplusplus
-extern "C" {
-#endif
-
 #define COROUTINE __declspec(noreturn) void
 
 enum {
@@ -21,29 +17,30 @@ enum {
     COROUTINE_XMM_REGISTERS = 1+10*2,
 };
 
-typedef struct
+struct coroutine_context
 {
     void **stack_pointer;
-} coroutine_context;
+};
 
-typedef void(* coroutine_start)(coroutine_context *from, coroutine_context *self);
+typedef void(* coroutine_start)(struct coroutine_context *from, struct coroutine_context *self);
 
 void coroutine_trampoline();
 
-static inline void coroutine_initialize(
-    coroutine_context *context,
-    coroutine_start start,
-    void *stack_pointer,
-    size_t stack_size
-) {
-    /* Force 16-byte alignment */
-    context->stack_pointer = (void**)((uintptr_t)stack_pointer & ~0xF);
+static inline void coroutine_initialize_main(struct coroutine_context * context) {
+    context->stack_pointer = NULL;
+}
 
-    if (!start) {
-        assert(!context->stack_pointer);
-        /* We are main coroutine for this thread */
-        return;
-    }
+static inline void coroutine_initialize(
+    struct coroutine_context *context,
+    coroutine_start start,
+    void *stack,
+    size_t size
+) {
+    assert(start && stack && size >= 1024);
+
+    // Stack grows down. Force 16-byte alignment.
+    char * top = (char*)stack + size;
+    context->stack_pointer = (void**)((uintptr_t)top & ~0xF);
 
     /* Win64 ABI requires space for arguments */
     context->stack_pointer -= 4;
@@ -55,20 +52,16 @@ static inline void coroutine_initialize(
 
     /* Windows Thread Information Block */
     /* *--context->stack_pointer = 0; */ /* gs:[0x00] is not used */
-    *--context->stack_pointer = (void*)stack_pointer; /* gs:[0x08] */
-    *--context->stack_pointer = (void*)((char *)stack_pointer - stack_size);  /* gs:[0x10] */
+    *--context->stack_pointer = (void*)top; /* gs:[0x08] */
+    *--context->stack_pointer = (void*)stack;  /* gs:[0x10] */
 
     context->stack_pointer -= COROUTINE_REGISTERS;
     memset(context->stack_pointer, 0, sizeof(void*) * COROUTINE_REGISTERS);
     memset(context->stack_pointer - COROUTINE_XMM_REGISTERS, 0, sizeof(void*) * COROUTINE_XMM_REGISTERS);
 }
 
-coroutine_context * coroutine_transfer(coroutine_context * current, coroutine_context * target);
+struct coroutine_context * coroutine_transfer(struct coroutine_context * current, struct coroutine_context * target);
 
-static inline void coroutine_destroy(coroutine_context * context)
+static inline void coroutine_destroy(struct coroutine_context * context)
 {
 }
-
-#if __cplusplus
-}
-#endif
