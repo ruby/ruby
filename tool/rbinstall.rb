ruby/ruby

commit f550da512cefbe0283106ca839c4836a98efaf3a

Removed old gemspecs of default gems for the Ruby committers.
  [Bug #12764][ruby-core:77284]

git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@66844 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

tool/rbinstall.rb
@@ -766,6 +766,11 @@ def install_default_gem(dir, srcdir)
     load_gemspec(src)
   }
   gems.compact.sort_by(&:name).each do |gemspec|
+    old_gemspecs = Dir[File.join(default_spec_dir, "#{gemspec.name}-*.gemspec")]
+    if old_gemspecs.size > 0
+      old_gemspecs.each {|spec| FileUtils.rm spec }
+    end
+
     full_name = "#{gemspec.name}-#{gemspec.version}"
 
     puts "#{INDENT}#{gemspec.name} #{gemspec.version}"

commit 51361272f9efca3eb0e1acb620ac62b576ddbb04

Respect --dest-dir when removing old default gems
Before this, tool/rbinstall would try to remove a gem file that
it may not have access too.

Fixes issue introduced in f550da512cefbe0283106ca839c4836a98efaf3a.

tool/rbinstall.rb
@@ -827,7 +827,7 @@ def install_default_gem(dir, srcdir)
     spec
   }
   gems.compact.sort_by(&:name).each do |gemspec|
-    old_gemspecs = Dir[File.join(default_spec_dir, "#{gemspec.name}-*.gemspec")]
+    old_gemspecs = Dir[File.join(with_destdir(default_spec_dir), "#{gemspec.name}-*.gemspec")]
     if old_gemspecs.size > 0
       old_gemspecs.each {|spec| FileUtils.rm spec }
     end