31541d738924e714e9b580b33eb44608ea248486
David Rodríguez <deivid.rodriguez@riseup.net>
2019-07-11 11:33:03 +0200

    [bundler/bundler] Drop rubygems 2.5.0 and 2.5.1 support
    https://github.com/bundler/bundler/commit/8ecd722b9f

--- lib/bundler/source/git.rb
+++ lib/bundler/source/git.rb
@@ -314,12 +314,10 @@ module Bundler
       # no-op, since we validate when re-serializing the gemspec
       def validate_spec(_spec); end
 
-      if Bundler.rubygems.stubs_provide_full_functionality?
-        def load_gemspec(file)
-          stub = Gem::StubSpecification.gemspec_stub(file, install_path.parent, install_path.parent)
-          stub.full_gem_path = Pathname.new(file).dirname.expand_path(root).to_s.untaint
-          StubSpecification.from_stub(stub)
-        end
+      def load_gemspec(file)
+        stub = Gem::StubSpecification.gemspec_stub(file, install_path.parent, install_path.parent)
+        stub.full_gem_path = Pathname.new(file).dirname.expand_path(root).to_s.untaint
+        StubSpecification.from_stub(stub)
       end
 
       def git_scope
