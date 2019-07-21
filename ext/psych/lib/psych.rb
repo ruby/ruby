13616a29cddcaa5b8a48695cb0475d7253e9dbb7
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-07-21 12:21:27 +0900

    [ruby/psych] Drop to support fat gem support.
      ref. https://github.com/ruby/bigdecimal/pull/149
    
    https://github.com/ruby/psych/commit/25ae263252

--- ext/psych/lib/psych.rb
+++ ext/psych/lib/psych.rb
@@ -10,11 +10,7 @@ when 'jruby'
     org.jruby.ext.psych.PsychLibrary.new.load(JRuby.runtime, false)
   end
 else
-  begin
-    require "#{RUBY_VERSION[/\d+\.\d+/]}/psych.so"
-  rescue LoadError
-    require 'psych.so'
-  end
+  require 'psych.so'
 end
 require 'psych/nodes'
 require 'psych/streaming'
