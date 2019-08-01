b8e351a1b9a16ce27f53d15051a1d1f83911b8cb
Yusuke Endoh <mame@ruby-lang.org>
2019-08-01 21:09:59 +0900

    ext/-test-/bug-14834/bug-14384.c: fallback for MAYBE_UNUSED
    __unused__ is unavailable on Sun C.
    https://rubyci.org/logs/rubyci.s3.amazonaws.com/solaris11s-sunc/ruby-master/log/20190801T112505Z.fail.html.gz

--- ext/-test-/bug-14834/bug-14384.c
+++ ext/-test-/bug-14834/bug-14384.c
@@ -1,6 +1,10 @@
 #include <ruby/ruby.h>
 #include <ruby/debug.h>
 
+#ifndef MAYBE_UNUSED
+# define MAYBE_UNUSED(x) x
+#endif
+
 static NOINLINE(VALUE f(VALUE));
 static NOINLINE(void g(VALUE, void*));
 extern NOINLINE(void Init_bug_14384(void));
