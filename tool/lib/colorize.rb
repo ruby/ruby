1267d2d196f189dd5c562549454bbb1ed6cdbcf4
Nobuyoshi Nakada <nobu@ruby-lang.org>
2019-08-03 14:56:07 +0900

    Added more attributes [ci skip]

--- tool/lib/colorize.rb
+++ tool/lib/colorize.rb
@@ -26,6 +26,7 @@ class Colorize
     "pass"=>"32", "fail"=>"31;1", "skip"=>"33;1",
     "black"=>"30", "red"=>"31", "green"=>"32", "yellow"=>"33",
     "blue"=>"34", "magenta"=>"35", "cyan"=>"36", "white"=>"37",
+    "bold"=>"1", "underline"=>"4", "reverse"=>"7",
   }
 
   def decorate(str, name)
