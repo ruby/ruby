a5e8ae58be8ad7bd9b97e5860f58d01a7a25339e
Nobuyoshi Nakada <nobu@ruby-lang.org>
2019-08-08 22:17:22 +0900

    Reduce unnecessary EXEC_TAG in require

--- test/ruby/test_require.rb
+++ test/ruby/test_require.rb
@@ -260,8 +260,8 @@ class TestRequire < Test::Unit::TestCase
       e = assert_raise_with_message(SyntaxError, /unexpected/) {
         yield req
       }
-      assert_not_nil(bt = e.backtrace)
-      assert_not_empty(bt.find_all {|b| b.start_with? __FILE__})
+      assert_not_nil(bt = e.backtrace, "no backtrace")
+      assert_not_empty(bt.find_all {|b| b.start_with? __FILE__}, proc {bt.inspect})
     end
   end
 
