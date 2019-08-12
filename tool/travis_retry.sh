b1ef14176b3716e3900672a9563b94aa3abedb2d
Takashi Kokubun <takashikkbn@gmail.com>
2019-08-12 14:35:49 +0900

    We did not have tool/ before checkout
    anyway we don't need authorization here.
    Also retry does not seem to work in the original version, so let's
    extend this with retries as a separate github action later.

--- tool/travis_retry.sh
+++ tool/travis_retry.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -eu
 # The modified version of `travis_retry` to support custom backoffs, which is used by .travis.yml.
 # https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/bash/travis_retry.bash
-#
-# Now this is also used by .github/workflow/*.yml.
 
 for sleep in 0 ${WAITS:- 1 25 100}; do
   sleep "$sleep"
