b64911f4e262bef582557f6d11dc5cb35dae669c
aycabta <aycabta@gmail.com>
2019-08-14 19:06:27 +0900

    Parser was replaced

--- test/rdoc/test_rdoc_markup_parser.rb
+++ test/rdoc/test_rdoc_markup_parser.rb
@@ -691,7 +691,6 @@ B. l2
 
   def test_parse_trailing_cr
     expected = [ @RM::Paragraph.new('Text') ]
-    # FIXME hangs the parser:
     assert_equal expected, @RMP.parse("Text\r").parts
   end
 
