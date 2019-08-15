dbc2fddb9f0253307a7cc9c06410c0b2161e5831
David Rodríguez <deivid.rodriguez@riseup.net>
2019-08-15 17:58:34 +0200

    [bundler/bundler] Remove unnecessary rubygems require
    https://github.com/bundler/bundler/commit/039604ff95

--- spec/bundler/support/helpers.rb
+++ spec/bundler/support/helpers.rb
@@ -205,7 +205,7 @@ module Spec
         args = args.gsub(/(?=")/, "\\")
         args = %("#{args}")
       end
-      gem = ENV["BUNDLE_GEM"] || "#{Gem.ruby} -rrubygems -S gem --backtrace"
+      gem = ENV["BUNDLE_GEM"] || "#{Gem.ruby} -S gem --backtrace"
       sys_exec("#{gem} #{command} #{args}")
     end
     bang :gem_command
