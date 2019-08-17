097038d78026c18e94923e5c64127de064c92786
David Rodríguez <deivid.rodriguez@riseup.net>
2019-08-17 20:23:48 +0200

    A title that long doesn't get set sometimes
    No idea why, but the test doesn't need that the title is that long.

--- spec/bundler/commands/exec_spec.rb
+++ spec/bundler/commands/exec_spec.rb
@@ -57,13 +57,13 @@ RSpec.describe "bundle exec" do
 
   it "respects custom process title when loading through ruby", :github_action_linux do
     script_that_changes_its_own_title_and_checks_if_picked_up_by_ps_unix_utility = <<~'RUBY'
-      Process.setproctitle("1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16")
+      Process.setproctitle("1-2-3-4-5-6-7-8-9-10-11-12-13-14-15")
       puts `ps -ocommand= -p#{$$}`
     RUBY
     create_file "Gemfile"
     create_file "a.rb", script_that_changes_its_own_title_and_checks_if_picked_up_by_ps_unix_utility
     bundle "exec ruby a.rb"
-    expect(out).to eq("1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16")
+    expect(out).to eq("1-2-3-4-5-6-7-8-9-10-11-12-13-14-15")
   end
 
   it "accepts --verbose" do
