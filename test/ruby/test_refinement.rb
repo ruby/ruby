470dae33ea1a958bb00739b083c1c582a8f16e10
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-08-26 15:40:44 +0000

    merge revision(s) 5e018214e7435030727a97ac49db038d96438e74: [Backport #15720]
    	Fix SystemStackError when calling a method in an unused refinement
    
    	Fixes [Bug #15720]
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_5@67765 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- test/ruby/test_refinement.rb
+++ test/ruby/test_refinement.rb
@@ -2055,6 +2055,38 @@ class TestRefinement < Test::Unit::TestCase
     INPUT
   end
 
+  def test_call_method_in_unused_refinement
+    bug15720 = '[ruby-core:91916] [Bug #15720]'
+    assert_in_out_err([], <<-INPUT, ["ok"], [], bug15720)
+      module M1
+        refine Kernel do
+          def foo
+            'foo called!'
+          end
+        end
+      end
+
+      module M2
+        refine Kernel do
+          def bar
+            'bar called!'
+          end
+        end
+      end
+
+      using M1
+
+      foo
+
+      begin
+        bar
+      rescue NameError
+      end
+
+      puts "ok"
+    INPUT
+  end
+
   def test_super_from_refined_module
     a = EnvUtil.labeled_module("A") do
       def foo;"[A#{super}]";end
