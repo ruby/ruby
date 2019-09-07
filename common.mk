9c0626fa909b57e43a476aa78171e140ae09d31f
Takashi Kokubun <takashikkbn@gmail.com>
2019-09-07 09:14:32 +0900

    Upgrade benchmark-driver to v0.15.4
    Fixing a bug on Windows introduced in v0.15.0

--- common.mk
+++ common.mk
@@ -47,7 +47,7 @@ GEM_PATH =
 GEM_VENDOR =
 
 BENCHMARK_DRIVER_GIT_URL = https://github.com/benchmark-driver/benchmark-driver
-BENCHMARK_DRIVER_GIT_REF = v0.15.3
+BENCHMARK_DRIVER_GIT_REF = v0.15.4
 SIMPLECOV_GIT_URL = https://github.com/colszowka/simplecov.git
 SIMPLECOV_GIT_REF = v0.17.0
 SIMPLECOV_HTML_GIT_URL = https://github.com/colszowka/simplecov-html.git
