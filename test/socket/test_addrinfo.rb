8c09de38c5f2dab324bcb30ab4a8db9bd55357b2
Masaki Matsushita <glass.saga@gmail.com>
2019-09-09 18:44:42 +0900

    Fix expected ip_port

--- test/socket/test_addrinfo.rb
+++ test/socket/test_addrinfo.rb
@@ -691,7 +691,7 @@ class TestSocketAddrinfo < Test::Unit::TestCase
     def test_addrinfo_timeout
       ai = Addrinfo.getaddrinfo("localhost", nil, Socket::PF_INET, Socket::SOCK_STREAM, timeout: 1).fetch(0)
       assert_equal(6, ai.protocol)
-      assert_equal(80, ai.ip_port)
+      assert_equal(0, ai.ip_port)
     end
   end
 end
