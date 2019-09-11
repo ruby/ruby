31339ef4f2af9c7dad2a81eaa9470ee4574181a0
George Claghorn <george@basecamp.com>
2019-09-11 15:47:20 -0400

    Honor Syslog::Logger#level overrides

--- ext/syslog/lib/syslog/logger.rb
+++ ext/syslog/lib/syslog/logger.rb
@@ -112,7 +112,7 @@ class Syslog::Logger
       end
 
       def #{meth}?
-        @level <= #{level}
+        level <= #{level}
       end
     EOM
   end
@@ -202,7 +202,7 @@ class Syslog::Logger
 
   def add severity, message = nil, progname = nil, &block
     severity ||= ::Logger::UNKNOWN
-    @level <= severity and
+    level <= severity and
       @@syslog.log( (LEVEL_MAP[severity] | @facility), '%s', formatter.call(severity, Time.now, progname, (message || block.call)) )
     true
   end
