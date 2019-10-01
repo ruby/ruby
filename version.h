88387876af112eb0bdefe7408bca6aaaacf52d6f
usa <usa@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2019-10-01 11:04:48 +0000

    merge revision(s) a0a2640b398cffd351f87d3f6243103add66575b
    Fix for wrong fnmatch patttern
    
    * dir.c (file_s_fnmatch): ensure that pattern does not contain a
      NUL character.  https://hackerone.com/reports/449617
    
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_4@67817 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
-#define RUBY_VERSION "2.4.7"
-#define RUBY_RELEASE_DATE "2019-08-28"
-#define RUBY_PATCHLEVEL 357
+#define RUBY_VERSION "2.4.8"
+#define RUBY_RELEASE_DATE "2019-10-01"
+#define RUBY_PATCHLEVEL 358
 
 #define RUBY_RELEASE_YEAR 2019
-#define RUBY_RELEASE_MONTH 8
-#define RUBY_RELEASE_DAY 28
+#define RUBY_RELEASE_MONTH 10
+#define RUBY_RELEASE_DAY 1
 
 #include "ruby/version.h"
 
