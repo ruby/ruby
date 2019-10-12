f4055647115f2003ce614dd16cf68d2933f811fe
Nobuyoshi Nakada <nobu@ruby-lang.org>
2019-10-12 18:47:06 +0900

    Suppress deprecation warnings of MD5 from Xcode 11.1

--- ext/digest/md5/md5cc.h
+++ ext/digest/md5/md5cc.h
@@ -1,6 +1,13 @@
 #define COMMON_DIGEST_FOR_OPENSSL 1
 #include <CommonCrypto/CommonDigest.h>
 
+#ifdef __clang__
+# pragma clang diagnostic ignored "-Wdeprecated-declarations"
+/* Suppress deprecation warnings of MD5 from Xcode 11.1 */
+/* Although we know MD5 is deprecated too, provide just for backward
+ * compatibility, as well as Apple does. */
+#endif
+
 #define MD5_BLOCK_LENGTH	CC_MD5_BLOCK_BYTES
 
 static DEFINE_UPDATE_FUNC_FOR_UINT(MD5)
