95c420c4a65ca2e7f3edf27134ad33691959296c
Sutou Kouhei <kou@cozmixng.org>
2019-10-14 12:40:50 +0900

    Import StringScanner 1.0.3 (#2553)

--- ext/strscan/extconf.rb
+++ ext/strscan/extconf.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 require 'mkmf'
-$INCFLAGS << " -I$(top_srcdir)"
+$INCFLAGS << " -I$(top_srcdir)" if $extmk
+have_func("onig_region_memsize", "ruby.h")
 create_makefile 'strscan'
