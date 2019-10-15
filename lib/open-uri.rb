eaf8632f5c7a7ed2d0234c7f307d42cbc4ddc994
expeehaa <expeehaa@outlook.com>
2019-10-15 21:13:38 +0200

    Add option to use http basic authentication on redirects in open-uri.

--- lib/open-uri.rb
+++ lib/open-uri.rb
@@ -125,6 +125,7 @@ module OpenURI
     :ftp_active_mode => false,
     :redirect => true,
     :encoding => nil,
+    :http_basic_authentication_follow_redirect => false,
   }
 
   def OpenURI.check_options(options) # :nodoc:
@@ -247,7 +248,7 @@ module OpenURI
         unless OpenURI.redirectable?(uri, redirect)
           raise "redirection forbidden: #{uri} -> #{redirect}"
         end
-        if options.include? :http_basic_authentication
+        if options.include?(:http_basic_authentication) && !options.fetch(:http_basic_authentication_follow_redirect, false)
           # send authentication only for the URI directly specified.
           options = options.dup
           options.delete :http_basic_authentication
