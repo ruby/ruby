c15856d76dd46030c805523b05808d32ead15162
Takashi Kokubun <takashikkbn@gmail.com>
2019-10-15 23:14:55 -0700

    Do not test --jit-debug on -DVM_CHECK_MODE

--- test/ruby/test_jit_debug.rb
+++ test/ruby/test_jit_debug.rb
@@ -1,5 +1,8 @@
 require_relative 'test_jit'
 
+return unless defined?(TestJIT)
+return if RbConfig::CONFIG['CPPFLAGS'].include?('-DVM_CHECK_MODE')
+
 class TestJITDebug < TestJIT
   def setup
     # let `#eval_with_jit` use --jit-debug
