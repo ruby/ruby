c4b627e2daf76e2f8922c62c8e0a6c2a10c8b435
Jeremy Evans <code@jeremyevans.net>
2019-10-18 09:53:59 -0700

    Only taint on Ruby <2.7
    Ruby 2.7 deprecates taint and it no longer has an effect.

--- lib/reline.rb
+++ lib/reline.rb
@@ -166,7 +166,7 @@ module Reline
       inner_readline(prompt, add_hist, true, &confirm_multiline_termination)
 
       whole_buffer = line_editor.whole_buffer.dup
-      whole_buffer.taint
+      whole_buffer.taint if RUBY_VERSION < '2.7'
       if add_hist and whole_buffer and whole_buffer.chomp.size > 0
         Reline::HISTORY << whole_buffer
       end
@@ -179,7 +179,7 @@ module Reline
       inner_readline(prompt, add_hist, false)
 
       line = line_editor.line.dup
-      line.taint
+      line.taint if RUBY_VERSION < '2.7'
       if add_hist and line and line.chomp.size > 0
         Reline::HISTORY << line.chomp
       end
