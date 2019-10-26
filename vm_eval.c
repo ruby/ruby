5aacb304551aa29f86e4c59bef5136bf9d20726a
卜部昌平 <shyouhei@ruby-lang.org>
2019-10-26 15:33:16 +0900

    refactor assign variables
    For readability.  Requested by ko1.
    
    See: https://github.com/ruby/ruby/commit/356e203a3acd4d3d20ba12f956fd22e17b6363e9#r35661401

--- vm_eval.c
+++ vm_eval.c
@@ -45,14 +45,11 @@ static VALUE vm_call0_body(rb_execution_context_t* ec, struct rb_calling_info *c
 MJIT_FUNC_EXPORTED VALUE
 rb_vm_call0(rb_execution_context_t *ec, VALUE recv, ID id, int argc, const VALUE *argv, const rb_callable_method_entry_t *me, int kw_splat)
 {
-    return vm_call0_body(
-        ec,
-        &(struct rb_calling_info) {
-            Qundef, recv, argc, kw_splat, },
-        &(struct rb_call_data) {
-            { 0, 0, me, me->def, vm_call_general, { 0, }, },
-            { id, (kw_splat ? VM_CALL_KW_SPLAT : 0), argc, }, },
-        argv);
+    struct rb_calling_info calling = { Qundef, recv, argc, kw_splat, };
+    struct rb_call_info ci = { id, (kw_splat ? VM_CALL_KW_SPLAT : 0), argc, };
+    struct rb_call_cache cc = { 0, 0, me, me->def, vm_call_general, { 0, }, };
+    struct rb_call_data cd = { cc, ci, };
+    return vm_call0_body(ec, &calling, &cd, argv);
 }
 
 static VALUE
