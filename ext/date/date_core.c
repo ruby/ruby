3895e548bd2d75cdf47cd247e84d6071247dd3b3
Jeremy Evans <code@jeremyevans.net>
2019-10-26 18:20:46 -0700

    [ruby/date] Revert "Simplify #inspect"
    This reverts commit af01edd7d8575f544f647dbe8cde5b6ae535d459.
    
    Revert requested by Yui Naruse.
    
    https://github.com/ruby/date/commit/875d563557

--- ext/date/date_core.c
+++ ext/date/date_core.c
@@ -6574,8 +6574,11 @@ static VALUE
 mk_inspect(union DateData *x, VALUE klass, VALUE to_s)
 {
     return rb_enc_sprintf(rb_usascii_encoding(),
-			  "#<%"PRIsVALUE": %"PRIsVALUE">",
-			  klass, to_s);
+			  "#<%"PRIsVALUE": %"PRIsVALUE" "
+			  "((%+"PRIsVALUE"j,%ds,%+"PRIsVALUE"n),%+ds,%.0fj)>",
+			  klass, to_s,
+			  m_real_jd(x), m_df(x), m_sf(x),
+			  m_of(x), m_sg(x));
 }
 
 /*
