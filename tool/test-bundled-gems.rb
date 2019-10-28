48f982aba7ab5b23af168d3c108e71e3a27c0648
Nobuyoshi Nakada <nobu@ruby-lang.org>
2019-10-28 19:14:38 +0900

    test-bundled-gems.rb: fixed for out-of-place build

--- tool/test-bundled-gems.rb
+++ tool/test-bundled-gems.rb
@@ -3,12 +3,13 @@ require 'rbconfig'
 allowed_failures = ENV['TEST_BUNDLED_GEMS_ALLOW_FAILURES'] || ''
 allowed_failures = allowed_failures.split(',').reject(&:empty?)
 
+gem_dir = File.expand_path('../../gems', __FILE__)
 exit_code = 0
-File.foreach('gems/bundled_gems') do |line|
+File.foreach("#{gem_dir}/bundled_gems") do |line|
   gem = line.split.first
   puts "\nTesting the #{gem} gem"
 
-  gem_src_dir = File.expand_path("../../gems/src/#{gem}", __FILE__ )
+  gem_src_dir = File.expand_path("#{gem_dir}/src/#{gem}", __FILE__)
   test_command = "#{ARGV.join(' ')} -C #{gem_src_dir} -Ilib ../../../.bundle/bin/rake"
   puts test_command
   system test_command
