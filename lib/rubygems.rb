ffd36550f70b140c8f5e6c23ccef024a71d09c1f
David Rodríguez <deivid.rodriguez@riseup.net>
2019-10-28 18:08:07 +0100

    [rubygems/rubygems] Remove unnecessary spec manipulation
    https://github.com/rubygems/rubygems/commit/c7c00c280f

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -1121,7 +1121,6 @@ An Array (#{env.inspect}) was passed in from #{caller[3]}
       ensure
         Gem::DefaultUserInteraction.ui.close
       end
-      @gemdeps.requested_specs.map(&:to_spec).sort_by(&:name)
     end
 
   rescue => e
