03fdf02321d4ac3123fb6908e7be53e232a92b67
Nobuyoshi Nakada <nobu@ruby-lang.org>
2019-11-09 23:00:32 +0900

    Removed trial and errors

--- spec/ruby/security/cve_2018_6914_spec.rb
+++ spec/ruby/security/cve_2018_6914_spec.rb
@@ -9,27 +9,6 @@ describe "CVE-2018-6914 is resisted by" do
     @dir = tmp("CVE-2018-6914")
     Dir.mkdir(@dir, 0700)
     ENV['TMPDIR'] = @dir
-
-    # Make sure that ENV["TMPDIR"] is used by Dir.tmpdir
-    # https://github.com/ruby/ruby/runs/294462511#step:10:134
-    10.times do
-      break if Dir.tmpdir == File.expand_path(@dir)
-      sleep 0.1
-    end
-
-    @debug_print = ->(actual) {
-      PP.pp({
-        actual: actual,
-        absolute: File.absolute_path(actual),
-        dir: @dir,
-        pwd: Dir.pwd,
-        tmpdir: @tmpdir,
-        Dir_tmpdir: Dir.tmpdir,
-        TMPDIR: ENV['TMPDIR'],
-        stat: File.stat(@dir),
-      }, STDERR)
-    }
-
     @dir << '/'
 
     @tempfile = nil
@@ -44,14 +23,12 @@ describe "CVE-2018-6914 is resisted by" do
   it "Tempfile.open by deleting separators" do
     @tempfile = Tempfile.open(['../', 'foo'])
     actual = @tempfile.path
-    @debug_print.call(actual)
     File.absolute_path(actual).should.start_with?(@dir)
   end
 
   it "Tempfile.new by deleting separators" do
     @tempfile = Tempfile.new('../foo')
     actual = @tempfile.path
-    @debug_print.call(actual)
     File.absolute_path(actual).should.start_with?(@dir)
   end
 
@@ -59,7 +36,6 @@ describe "CVE-2018-6914 is resisted by" do
     actual = Tempfile.create('../foo') do |t|
       t.path
     end
-    @debug_print.call(actual)
     File.absolute_path(actual).should.start_with?(@dir)
   end
 
@@ -67,7 +43,6 @@ describe "CVE-2018-6914 is resisted by" do
     actual = Dir.mktmpdir('../foo') do |path|
       path
     end
-    @debug_print.call(actual)
     File.absolute_path(actual).should.start_with?(@dir)
   end
 
@@ -75,7 +50,6 @@ describe "CVE-2018-6914 is resisted by" do
     actual = Dir.mktmpdir(['../', 'foo']) do |path|
       path
     end
-    @debug_print.call(actual)
     File.absolute_path(actual).should.start_with?(@dir)
   end
 end
