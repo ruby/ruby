a8f9e25ca8cc160683711baed00008461db2ad16
Takashi Kokubun <takashikkbn@gmail.com>
2019-11-13 09:37:25 -0800

    Suspend many fibers test on JIT for now
    https://github.com/ruby/ruby/runs/301411717
    
    No C backtrace information and this is hard to fix immediately.
    As CI doesn't provide helpful information, this should be debugged
    locally or at least have more logs there.

--- test/ruby/test_fiber.rb
+++ test/ruby/test_fiber.rb
@@ -34,6 +34,7 @@ class TestFiber < Test::Unit::TestCase
   end
 
   def test_many_fibers
+    skip 'This is unstable on GitHub Actions --jit-wait. TODO: debug it' if RubyVM::MJIT.enabled?
     max = 10_000
     assert_equal(max, max.times{
       Fiber.new{}
