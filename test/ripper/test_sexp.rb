61131edba7c885a6d12c4d6f0e502fb40381f184
Nobuyoshi Nakada <nobu@ruby-lang.org>
2019-11-20 00:55:14 +0900

    Revert "[ripper] Quoted label without expression must be a local variable"
    This reverts commit 0b8c73aa65add5c57b46b0cfdf4e661508802172, which
    seems breaking RVALUE consistency check.

--- test/ripper/test_sexp.rb
+++ test/ripper/test_sexp.rb
@@ -435,9 +435,6 @@ eot
 
     [__LINE__, %q{ case 0; in a?:; end }] =>
     nil,
-
-    [__LINE__, %q{ case 0; in "A":; end }] =>
-    nil,
   }
   pattern_matching_data.each do |(i, src), expected|
     define_method(:"test_pattern_matching_#{i}") do
