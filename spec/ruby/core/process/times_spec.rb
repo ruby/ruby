ruby/ruby

commit 1243255c3a36433041012b6107a5ac48658a0895

Update to ruby/spec@4eec3dc

spec/ruby/core/process/times_spec.rb
@@ -7,21 +7,19 @@ describe "Process.times" do
 
   it "returns current cpu times" do
     t = Process.times
+    user = t.utime
 
-    # Do busy work for a wall-clock interval.
-    start = Time.now
-    1 until (Time.now - start) > 0.5
+    1 until Process.times.utime > user
+    Process.times.utime.should > user
+  end
 
-    # Ensure times is larger. NOTE that there is no
-    # guarantee of an upper bound since anything may be
-    # happening at the OS level, so we ONLY check that at
-    # least an interval has elapsed. Also, we are assuming
-    # there is a correlation between wall clock time and
-    # process time. In practice, there is an observed
-    # discrepancy often 10% or greater. In other words,
-    # this is a very fuzzy test.
-    t2 = Process.times
-    diff = (t2.utime + t2.stime) - (t.utime + t.stime)
-    diff.should > 0
+  ruby_version_is "2.5" do
+    platform_is_not :windows do
+      it "uses getrusage when available to improve precision beyond milliseconds" do
+        times = 100.times.map { Process.times }
+        times.count { |t| ((t.utime * 1e6).to_i % 1000) > 0 }.should > 0
+        times.count { |t| ((t.stime * 1e6).to_i % 1000) > 0 }.should > 0
+      end
+    end
   end
 end

commit 5b1f7f26b4ff3b05e1c184ae87154a323b1be915

Skip if getrusage is not supported
1243255c3a36433041012b6107a5ac48658a0895 broke ci.rvm.jp tests like
http://ci.rvm.jp/results/trunk_clang_39@silicon-docker/2445098.

253232c028a5565dbeecc05fab5e81b35ab58bcc works only if getrusage is supported.

spec/ruby/core/process/times_spec.rb
@@ -16,6 +16,11 @@ describe "Process.times" do
   ruby_version_is "2.5" do
     platform_is_not :windows do
       it "uses getrusage when available to improve precision beyond milliseconds" do
+        times = 100.times.map { Process.clock_gettime(:GETRUSAGE_BASED_CLOCK_PROCESS_CPUTIME_ID) }
+        if times.count { |t| ((t * 1e6).to_i % 1000) > 0 } == 0
+          skip "getrusage is not supported on this environment"
+        end
+
         times = 100.times.map { Process.times }
         times.count { |t| ((t.utime * 1e6).to_i % 1000) > 0 }.should > 0
         times.count { |t| ((t.stime * 1e6).to_i % 1000) > 0 }.should > 0