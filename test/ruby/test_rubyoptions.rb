6bc8b4d8ea6cb8865225c0afcf1abd9e4b725376
Takashi Kokubun <takashikkbn@gmail.com>
2019-12-01 10:51:04 -0800

    Skip --jit-debug= test on mswin
    it fails like
    https://ci.appveyor.com/project/ruby/ruby/builds/29235837/job/v0apdjj4qx8afars

--- test/ruby/test_rubyoptions.rb
+++ test/ruby/test_rubyoptions.rb
@@ -1044,7 +1044,8 @@ class TestRubyOptions < Test::Unit::TestCase
   end
 
   def test_jit_debug
-    if JITSupport.supported?
+    # mswin uses prebuilt precompiled header. Thus it does not show a pch compilation log to check "-O0 -O1".
+    if JITSupport.supported? && !RUBY_PLATFORM.match?(/mswin/)
       env = { 'MJIT_SEARCH_BUILD_DIR' => 'true' }
       assert_in_out_err([env, "--jit-debug=-O0 -O1", "--jit-verbose=2", "" ], "", [], /-O0 -O1/)
     end
