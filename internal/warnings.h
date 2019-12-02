6581db2187a1d5b6316fd1c942dccc6b6a3b9ca8
卜部昌平 <shyouhei@ruby-lang.org>
2019-12-02 17:02:24 +0900

    internal/warnings.h rework
    Not a big rewrite.  Just to make those macros readable.

--- internal/warnings.h
+++ internal/warnings.h
@@ -9,42 +9,41 @@
  *             modify this file, provided that  the conditions mentioned in the
  *             file COPYING are met.  Consult the file for details.
  */
+#include "internal/compilers.h" /* for MSC_VERSION_SINCE */
 
-#if defined(_MSC_VER)
+#if MSC_VERSION_SINCE(1200)
+# /* Not sure exactly when but it seems VC++ 6.0 is a version with it.*/
 # define COMPILER_WARNING_PUSH          __pragma(warning(push))
 # define COMPILER_WARNING_POP           __pragma(warning(pop))
-# define COMPILER_WARNING_ERROR(flag)   __pragma(warning(error: flag)))
-# define COMPILER_WARNING_IGNORED(flag) __pragma(warning(suppress: flag)))
+# define COMPILER_WARNING_ERROR(flag)   __pragma(warning(error: flag))
+# define COMPILER_WARNING_IGNORED(flag) __pragma(warning(disable: flag))
 
-#elif defined(__clang__) /* clang 2.6 already had this feature */
-# define COMPILER_WARNING_PUSH          _Pragma("clang diagnostic push")
-# define COMPILER_WARNING_POP           _Pragma("clang diagnostic pop")
-# define COMPILER_WARNING_SPECIFIER(kind, msg) \
-    clang diagnostic kind # msg
-# define COMPILER_WARNING_ERROR(flag) \
-    COMPILER_WARNING_PRAGMA(COMPILER_WARNING_SPECIFIER(error, flag))
-# define COMPILER_WARNING_IGNORED(flag) \
-    COMPILER_WARNING_PRAGMA(COMPILER_WARNING_SPECIFIER(ignored, flag))
+#elif defined(__clang__)
+# /* Not sure exactly when but it seems LLVM 2.6.0 is a version with it. */
+# define COMPILER_WARNING_PRAGMA0(x)    _Pragma(# x)
+# define COMPILER_WARNING_PRAGMA1(x)    COMPILER_WARNING_PRAGMA0(clang diagnostic x)
+# define COMPILER_WARNING_PRAGMA2(x, y) COMPILER_WARNING_PRAGMA1(x # y)
+# define COMPILER_WARNING_PUSH          COMPILER_WARNING_PRAGMA1(push)
+# define COMPILER_WARNING_POP           COMPILER_WARNING_PRAGMA1(pop)
+# define COMPILER_WARNING_ERROR(flag)   COMPILER_WARNING_PRAGMA2(error, flag)
+# define COMPILER_WARNING_IGNORED(flag) COMPILER_WARNING_PRAGMA2(ignored, flag)
 
 #elif GCC_VERSION_SINCE(4, 6, 0)
-/* https://gcc.gnu.org/onlinedocs/gcc-4.6.4/gcc/Diagnostic-Pragmas.html */
-# define COMPILER_WARNING_PUSH          _Pragma("GCC diagnostic push")
-# define COMPILER_WARNING_POP           _Pragma("GCC diagnostic pop")
-# define COMPILER_WARNING_SPECIFIER(kind, msg) \
-    GCC diagnostic kind # msg
-# define COMPILER_WARNING_ERROR(flag) \
-    COMPILER_WARNING_PRAGMA(COMPILER_WARNING_SPECIFIER(error, flag))
-# define COMPILER_WARNING_IGNORED(flag) \
-    COMPILER_WARNING_PRAGMA(COMPILER_WARNING_SPECIFIER(ignored, flag))
+# /* https://gcc.gnu.org/onlinedocs/gcc-4.6.0/gcc/Diagnostic-Pragmas.html */
+# define COMPILER_WARNING_PRAGMA0(x)    _Pragma(# x)
+# define COMPILER_WARNING_PRAGMA1(x)    COMPILER_WARNING_PRAGMA0(GCC diagnostic x)
+# define COMPILER_WARNING_PRAGMA2(x, y) COMPILER_WARNING_PRAGMA1(x # y)
+# define COMPILER_WARNING_PUSH          COMPILER_WARNING_PRAGMA1(push)
+# define COMPILER_WARNING_POP           COMPILER_WARNING_PRAGMA1(pop)
+# define COMPILER_WARNING_ERROR(flag)   COMPILER_WARNING_PRAGMA2(error, flag)
+# define COMPILER_WARNING_IGNORED(flag) COMPILER_WARNING_PRAGMA2(ignored, flag)
 
-#else /* other compilers to follow? */
-# define COMPILER_WARNING_PUSH          /* nop */
-# define COMPILER_WARNING_POP           /* nop */
-# define COMPILER_WARNING_ERROR(flag)   /* nop */
-# define COMPILER_WARNING_IGNORED(flag) /* nop */
-#endif
+#else
+# /* :FIXME: improve here, for instace icc seems to have something? */
+# define COMPILER_WARNING_PUSH          /* void */
+# define COMPILER_WARNING_POP           /* void */
+# define COMPILER_WARNING_ERROR(flag)   /* void */
+# define COMPILER_WARNING_IGNORED(flag) /* void */
 
-#define COMPILER_WARNING_PRAGMA(str) COMPILER_WARNING_PRAGMA_(str)
-#define COMPILER_WARNING_PRAGMA_(str) _Pragma(#str)
-
+#endif /* _MSC_VER */
 #endif /* INTERNAL_WARNINGS_H */
