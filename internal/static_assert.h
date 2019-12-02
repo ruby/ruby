68c0dc8d363675881d60b9fde15645d9ee14fafc
卜部昌平 <shyouhei@ruby-lang.org>
2019-12-02 17:06:40 +0900

    internal/static_assert.h rework
    ISO/IEC 9899:2011 section 7.2 states that <assert.h> must define
    static_assert.  Use it when available.

--- internal/static_assert.h
+++ internal/static_assert.h
@@ -9,13 +9,20 @@
  *             modify this file, provided that  the conditions mentioned in the
  *             file COPYING are met.  Consult the file for details.
  */
+#include <assert.h>             /* for static_assert */
+#include "compilers.h"          /* for __has_extension */
 
-#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 201112L)
-# define STATIC_ASSERT(name, expr) _Static_assert(expr, #name ": " #expr)
-#elif GCC_VERSION_SINCE(4, 6, 0) || __has_extension(c_static_assert)
-# define STATIC_ASSERT(name, expr) RB_GNUC_EXTENSION _Static_assert(expr, #name ": " #expr)
+#if defined(static_assert)
+/* Take assert.h definition */
+# define STATIC_ASSERT(name, expr) static_assert(expr, # name ": " # expr)
+
+#elif __has_extension(c_static_assert) || GCC_VERSION_SINCE(4, 6, 0)
+# define STATIC_ASSERT(name, expr) \
+    __extension__ _Static_assert(expr, # name ": " # expr)
+
 #else
-# define STATIC_ASSERT(name, expr) typedef int static_assert_##name##_check[1 - 2*!(expr)]
-#endif
+# define STATIC_ASSERT(name, expr) \
+    typedef int static_assert_ ## name ## _check[1 - 2 * !(expr)]
 
+#endif /* static_assert */
 #endif /* INTERNAL_STATIC_ASSERT_H */
