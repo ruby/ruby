7f2cd2ae6fea76ff3a2b95b69e6e2f749e8a249f
KOSAKI Motohiro <kosaki.motohiro@gmail.com>
2019-12-03 10:50:16 +0000

    fix typo

--- time.c
+++ time.c
@@ -3268,7 +3268,7 @@ find_time_t(struct tm *tptr, int utc_p, time_t *tp)
                 status = 0;
             }
             if (guess <= guess_lo || guess_hi <= guess) {
-                /* Precious guess is invalid. try binary search. */
+                /* Previous guess is invalid. try binary search. */
 #ifdef DEBUG_GUESSRANGE
                 if (guess <= guess_lo) fprintf(stderr, "too small guess: %ld <= %ld\n", guess, guess_lo);
                 if (guess_hi <= guess) fprintf(stderr, "too big guess: %ld <= %ld\n", guess_hi, guess);
