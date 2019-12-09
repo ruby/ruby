4b36832ba64a8879a4b0702d6d5079ddd9d3078e
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-12-09 19:11:18 +0900

    Merge the upstream changes for test-unit on ruby/logger.
      This commits are based with:
        https://github.com/ruby/logger/commit/f067f7d1aa743b467d633ec6d1790bd93ed9e25b
        https://github.com/ruby/logger/commit/86058f420d8f6909500cccceb24f58bef0597b4d
        https://github.com/ruby/logger/commit/02db6e8ed85d1f4ba974f08fee292bad400d9fc2

--- lib/logger/logger.gemspec
+++ lib/logger/logger.gemspec
@@ -24,5 +24,6 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", ">= 0"
   spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "minitest", "~> 5.0"
+  spec.add_development_dependency "test-unit"
+  spec.add_development_dependency "rdoc"
 end
--- test/logger/helper.rb
+++ test/logger/helper.rb
@@ -3,3 +3,6 @@ $LOAD_PATH.unshift File.join(ROOT_DIR, 'lib') # to use logger in this repo inste
 $LOAD_PATH.unshift File.join(ROOT_DIR, 'test', 'lib') # to use custom test-unit in this repo
 require 'logger'
 require 'test/unit'
+require 'core_assertions'
+
+Test::Unit::TestCase.include Test::Unit::CoreAssertions
