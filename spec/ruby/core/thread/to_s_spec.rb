8a80bfcfd4d510a20a62e21d8d2f4119cb823d4f
Jean byroot Boussier <jean.boussier+github@shopify.com>
2019-12-10 11:49:39 +0100

    Make Thread#to_s consistent with Method and Proc to_s

--- spec/ruby/core/thread/to_s_spec.rb
+++ spec/ruby/core/thread/to_s_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../../spec_helper'
 require_relative 'shared/to_s'
 
-describe "Thread#inspect" do
-  it_behaves_like :thread_to_s, :inspect
+describe "Thread#to_s" do
+  it_behaves_like :thread_to_s, :to_s
 end
