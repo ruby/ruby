b4b22b9278007b106fe40c0191f8dcf5e7e8c0f2
Benoit Daloze <eregontp@gmail.com>
2019-12-14 12:16:58 +0100

    Clarify in the documentation that RubyVM::AbstractSyntaxTree is not stable API
    * See [Feature #14844].

--- ast.rb
+++ ast.rb
@@ -9,6 +9,16 @@ class RubyVM
   # This class is MRI specific as it exposes implementation details
   # of the MRI abstract syntax tree.
   #
+  # This class is experimental and its API is not stable, therefore it might
+  # change without notice. As examples, the order of children nodes is not
+  # guaranteed, the number of children nodes might change, there is no way to
+  # access children nodes by name, etc.
+  #
+  # If you are looking for a stable API or an API working under multiple Ruby
+  # implementations, consider using the +parser+ gem or Ripper. If you would
+  # like to make RubyVM::AbstractSyntaxTree stable, please join the discussion
+  # at https://bugs.ruby-lang.org/issues/14844.
+  #
   module AbstractSyntaxTree
 
     #  call-seq:
