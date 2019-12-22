7c33ee5ddfafe737add2a7f4c4975108f9196dbd
Benoit Daloze <eregontp@gmail.com>
2019-12-22 11:39:22 +0100

    Move version guard outside to make it clear the method was added in Ruby 2.5

--- spec/ruby/core/thread/to_s_spec.rb
+++ spec/ruby/core/thread/to_s_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../../spec_helper'
 require_relative 'shared/to_s'
 
-describe "Thread#to_s" do
-  ruby_version_is "2.5" do
+ruby_version_is "2.5" do
+  describe "Thread#to_s" do
     it_behaves_like :thread_to_s, :to_s
   end
 end
