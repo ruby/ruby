54083fd41bf876b42e1a97acdc7574c8edbb87ed
Marcus Stollsteimer <sto.mar@web.de>
2019-12-24 13:01:47 +0100

    [DOC] Fix grammar in Process module docs

--- process.c
+++ process.c
@@ -418,13 +418,13 @@ parent_redirect_close(int fd)
  *
  * The module contains several groups of functionality for handling OS processes:
  *
- * * Low-level property introspection and management of current process, like
+ * * Low-level property introspection and management of the current process, like
  *   Process.argv0, Process.pid;
  * * Low-level introspection of other processes, like Process.getpgid, Process.getpriority;
- * * Management of the current process: Process.abort, Process.exit, Process.daemon etc.
- *   (for convenience, most of those are also available as a global functions,
+ * * Management of the current process: Process.abort, Process.exit, Process.daemon, etc.
+ *   (for convenience, most of those are also available as global functions
  *   and module functions of Kernel);
- * * Creation and management of child processes: Process.fork, Process.spawn and
+ * * Creation and management of child processes: Process.fork, Process.spawn, and
  *   related methods;
  * * Management of low-level system clock: Process.times and Process.clock_gettime,
  *   which could be important for proper benchmarking and other elapsed
