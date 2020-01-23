109183c2c0902a0af735b2660737e1724307b264
Nobuyoshi Nakada <nobu@ruby-lang.org>
2020-01-23 22:03:13 +0900

    Guarded the examples for deprecated "taint"

--- spec/ruby/library/stringio/reopen_spec.rb
+++ spec/ruby/library/stringio/reopen_spec.rb
@@ -23,13 +23,15 @@ describe "StringIO#reopen when passed [Object, Integer]" do
     @io.string.should == "reopened, another time"
   end
 
-  # NOTE: WEIRD!
-  it "does not taint self when the passed Object was tainted" do
-    @io.reopen("reopened".taint, IO::RDONLY)
-    @io.tainted?.should be_false
+  ruby_version_is ""..."2.8" do
+    # NOTE: WEIRD!
+    it "does not taint self when the passed Object was tainted" do
+      @io.reopen("reopened".taint, IO::RDONLY)
+      @io.tainted?.should be_false
 
-    @io.reopen("reopened".taint, IO::WRONLY)
-    @io.tainted?.should be_false
+      @io.reopen("reopened".taint, IO::WRONLY)
+      @io.tainted?.should be_false
+    end
   end
 
   it "tries to convert the passed Object to a String using #to_str" do
@@ -90,13 +92,15 @@ describe "StringIO#reopen when passed [Object, Object]" do
     str.should == ""
   end
 
-  # NOTE: WEIRD!
-  it "does not taint self when the passed Object was tainted" do
-    @io.reopen("reopened".taint, "r")
-    @io.tainted?.should be_false
+  ruby_version_is ""..."2.8" do
+    # NOTE: WEIRD!
+    it "does not taint self when the passed Object was tainted" do
+      @io.reopen("reopened".taint, "r")
+      @io.tainted?.should be_false
 
-    @io.reopen("reopened".taint, "w")
-    @io.tainted?.should be_false
+      @io.reopen("reopened".taint, "w")
+      @io.tainted?.should be_false
+    end
   end
 
   it "tries to convert the passed Object to a String using #to_str" do
@@ -160,10 +164,12 @@ describe "StringIO#reopen when passed [String]" do
     @io.string.should == "reopened"
   end
 
-  # NOTE: WEIRD!
-  it "does not taint self when the passed Object was tainted" do
-    @io.reopen("reopened".taint)
-    @io.tainted?.should be_false
+  ruby_version_is ""..."2.8" do
+    # NOTE: WEIRD!
+    it "does not taint self when the passed Object was tainted" do
+      @io.reopen("reopened".taint)
+      @io.tainted?.should be_false
+    end
   end
 
   it "resets self's position to 0" do
