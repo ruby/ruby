9b55a9649fb1973180559a067cfee0d6f234fef8
Nobuyoshi Nakada <nobu@ruby-lang.org>
2020-01-27 10:10:58 +0900

    Removed useless "spec"s
    It is not specific to particular methods that keyword option
    arguments are coerced to `Hash`es using `to_hash` method.

--- spec/ruby/core/array/shuffle_spec.rb
+++ spec/ruby/core/array/shuffle_spec.rb
@@ -25,15 +25,6 @@ describe "Array#shuffle" do
     ArraySpecs::MyArray[1, 2, 3].shuffle.should be_an_instance_of(Array)
   end
 
-  ruby_version_is ""..."2.8" do
-    # keyword argument since 2.8
-    it "attempts coercion via #to_hash" do
-      obj = mock('hash')
-      obj.should_receive(:to_hash).once.and_return({})
-      [2, 3].shuffle(obj)
-    end
-  end
-
   it "calls #rand on the Object passed by the :random key in the arguments Hash" do
     obj = mock("array_shuffle_random")
     obj.should_receive(:rand).at_least(1).times.and_return(0.5)
