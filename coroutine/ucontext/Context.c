3f524bbd3ec190ba1018a24227af083e94ab2004
卜部昌平 <shyouhei@ruby-lang.org>
2020-01-31 15:53:03 +0900

    function pointers are not void*
    The same as 8427fca49bd85205f5a8766292dd893f003c0e48.

--- coroutine/ucontext/Context.c
+++ coroutine/ucontext/Context.c
@@ -15,7 +15,7 @@
 
 void coroutine_trampoline(void * _start, void * _context)
 {
-    coroutine_start start = _start;
+    coroutine_start start = (coroutine_start)_start;
     struct coroutine_context * context = _context;
 
     start(context->from, context);
