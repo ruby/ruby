8c67080381166f97a1153f092f40df699d020a53
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-02-06 22:07:39 +0900

    Revert "[rubygems/rubygems] Fix require issue with file extension priority"
    This reverts commit d767da428c28b7b9fec56b383bb32f6f76c6ad26.
    
      It fails with spec/ruby/core/kernel/require_spec.rb:5

--- lib/rubygems/core_ext/kernel_require.rb
+++ lib/rubygems/core_ext/kernel_require.rb
@@ -43,18 +43,18 @@ module Kernel
     # https://github.com/rubygems/rubygems/pull/1868
     resolved_path = begin
       rp = nil
-      Gem.suffixes.each do |s|
-        $LOAD_PATH[0...Gem.load_path_insert_index || -1].each do |lp|
-          safe_lp = lp.dup.tap(&Gem::UNTAINT)
-          begin
-            if File.symlink? safe_lp # for backward compatibility
-              next
-            end
-          rescue SecurityError
-            RUBYGEMS_ACTIVATION_MONITOR.exit
-            raise
+      $LOAD_PATH[0...Gem.load_path_insert_index || -1].each do |lp|
+        safe_lp = lp.dup.tap(&Gem::UNTAINT)
+        begin
+          if File.symlink? safe_lp # for backward compatibility
+            next
           end
+        rescue SecurityError
+          RUBYGEMS_ACTIVATION_MONITOR.exit
+          raise
+        end
 
+        Gem.suffixes.each do |s|
           full_path = File.expand_path(File.join(safe_lp, "#{path}#{s}"))
           if File.file?(full_path)
             rp = full_path
@@ -67,8 +67,12 @@ module Kernel
     end
 
     if resolved_path
-      RUBYGEMS_ACTIVATION_MONITOR.exit
-      return gem_original_require(resolved_path)
+      begin
+        RUBYGEMS_ACTIVATION_MONITOR.exit
+        return gem_original_require(resolved_path)
+      rescue LoadError
+        RUBYGEMS_ACTIVATION_MONITOR.enter
+      end
     end
 
     if spec = Gem.find_unresolved_default_spec(path)
