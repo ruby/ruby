53a8b1486b0e020858aa04f004aa22f4396a1bd0
Koichi Sasada <ko1@atdot.net>
2020-02-13 04:20:22 +0900

    skip SEGV test if RUBY_ON_BUG is specified

--- test/-ext-/bug_reporter/test_bug_reporter.rb
+++ test/-ext-/bug_reporter/test_bug_reporter.rb
@@ -4,6 +4,8 @@ require 'tmpdir'
 
 class TestBugReporter < Test::Unit::TestCase
   def test_bug_reporter_add
+    skip if ENV['RUBY_ON_BUG']
+
     description = RUBY_DESCRIPTION
     description = description.sub(/\+JIT /, '') if RubyVM::MJIT.enabled?
     expected_stderr = [
