155ab9caa6bbdeae2377bd9c18f11fc83d938217
Nobuyoshi Nakada <nobu@ruby-lang.org>
2020-02-12 11:47:08 +0900

    Removed unnecessary conversion
    Paths in `rb_execarg_t` should have been converted already.

--- process.c
+++ process.c
@@ -2736,8 +2736,6 @@ rb_execarg_parent_start1(VALUE execarg_obj)
             int fd2;
             if (NIL_P(fd2v)) {
                 struct open_struct open_data;
-                FilePathValue(vpath);
-		vpath = rb_str_encode_ospath(vpath);
               again:
                 open_data.fname = vpath;
                 open_data.oflags = flags;
