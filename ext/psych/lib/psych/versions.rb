6e7732051d9377d224bec696fb236bf382136edf
David Rodríguez <deivid.rodriguez@riseup.net>
2020-02-14 00:26:37 +0100

    [ruby/psych] Remove unnecessary version guard from versions.rb
    Removing it triggers the following warnings when running `bundle` under
    jruby from the root of the `psych` repo prints the following warnings:
    
    ```
    /path/to/jruby-9.2.9.0/lib/ruby/gems/shared/gems/psych-3.1.0-java/lib/psych/versions.rb:7: warning: already initialized constant VERSION
    /path/to//jruby-9.2.9.0/lib/ruby/gems/shared/gems/psych-3.1.0-java/lib/psych/versions.rb:10: warning: already initialized constant DEFAULT_SNAKEYAML_VERSION
    ```
    
    This is because bundler loads the versions file relatively from the
    local gemspec, and then internally loads the psych gem, causing the
    redefinition warnings.
    
    Instead, we modify the $LOAD_PATH so that when working locally on the
    `psych` repo, the local version of `psych` gets used.
    
    https://github.com/ruby/psych/commit/a3fc8191a7

--- ext/psych/lib/psych/versions.rb
+++ ext/psych/lib/psych/versions.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 module Psych
   # The version of Psych you are using
-  VERSION = '3.1.0' unless defined?(::Psych::VERSION)
+  VERSION = '3.1.0'
 
   if RUBY_ENGINE == 'jruby'
     DEFAULT_SNAKEYAML_VERSION = '1.23'.freeze
