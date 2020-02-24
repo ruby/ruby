f8401732dea7b22c849cefc722d7d7f41f2ddd1b
Nobuyoshi Nakada <nobu@ruby-lang.org>
2020-02-24 12:40:34 +0900

    Try with and without "v" prefix for numeric tag

--- tool/fetch-bundled_gems.rb
+++ tool/fetch-bundled_gems.rb
@@ -9,25 +9,16 @@ BEGIN {
 }
 
 n, v, u = $F
-case n
-when "test-unit"
-else
-  v = "v" + v
-end
 
 if File.directory?(n)
   puts "updating #{n} ..."
-  if v == "master"
-    system(*%W"git pull", chdir: n) or abort
-  else
-    system(*%W"git fetch", chdir: n) or abort
-  end
+  system("git", (v == "master" ? "pull" : "fetch"), chdir: n) or abort
 else
   puts "retrieving #{n} ..."
   system(*%W"git clone #{u} #{n}") or abort
 end
-unless system(*%W"git checkout #{v}", chdir: n)
-  unless v.sub!(/\Av/, '') and system(*%W"git checkout #{v}", chdir: n)
+unless system(*%W"git checkout #{v.sub(/\A(?=\d)/, 'v')}", chdir: n)
+  unless /\A\d/ =~ v and system(*%W"git checkout #{v}", chdir: n)
     abort
   end
 end
