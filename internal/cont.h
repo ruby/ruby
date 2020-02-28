ae737a590e8bea33590a41f7e7e36d0ff688613e
Takashi Kokubun <takashikkbn@gmail.com>
2020-02-28 01:21:06 -0800

    Make sure methods used in root_fiber aren't unloaded

--- internal/cont.h
+++ internal/cont.h
@@ -12,10 +12,12 @@
 #include "ruby/ruby.h"          /* for VALUE */
 
 struct rb_thread_struct;        /* in vm_core.h */
+struct rb_fiber_struct;         /* in cont.c */
 
 /* cont.c */
 VALUE rb_obj_is_fiber(VALUE);
 void rb_fiber_reset_root_local_storage(struct rb_thread_struct *);
 void ruby_register_rollback_func_for_ensure(VALUE (*ensure_func)(VALUE), VALUE (*rollback_func)(VALUE));
+void rb_fiber_init_mjit_cont(struct rb_fiber_struct *fiber);
 
 #endif /* INTERNAL_CONT_H */
