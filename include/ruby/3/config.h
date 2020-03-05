63cfc399c21cac8e4041f88475620f8980ea73a3
卜部昌平 <shyouhei@ruby-lang.org>
2020-03-05 22:49:28 +0900

    include/ruby/3/config.h rework
    This one is trivial.  Cosmetic updates only.

--- include/ruby/3/config.h
+++ include/ruby/3/config.h
@@ -26,63 +26,73 @@
 # include RUBY_EXTCONF_H
 #endif
 
-#include "ruby/backward/2/gcc_version_since.h"
+#include "ruby/3/compiler_since.h"
 
 #if defined(__cplusplus)
-/* __builtin_choose_expr and __builtin_types_compatible aren't available
- * on C++.  See https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html */
+#/* __builtin_choose_expr and __builtin_types_compatible aren't available
+# * on C++.  See https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html */
 # undef HAVE_BUILTIN___BUILTIN_CHOOSE_EXPR_CONSTANT_P
 # undef HAVE_BUILTIN___BUILTIN_TYPES_COMPATIBLE_P
-#elif GCC_VERSION_BEFORE(4,8,6) /* Bug #14221 */
+
+# undef  HAVE_PROTOTYPES
+# define HAVE_PROTOTYPES 1
+
+# undef  HAVE_STDARG_PROTOTYPES
+# define HAVE_STDARG_PROTOTYPES 1
+#endif
+
+#if RUBY3_COMPILER_BEFORE(GCC, 4, 9, 0)
+# /* See https://bugs.ruby-lang.org/issues/14221 */
 # undef HAVE_BUILTIN___BUILTIN_CHOOSE_EXPR_CONSTANT_P
 #endif
 
-#ifdef __cplusplus
-# ifndef  HAVE_PROTOTYPES
-#  define HAVE_PROTOTYPES 1
-# endif
-# ifndef  HAVE_STDARG_PROTOTYPES
-#  define HAVE_STDARG_PROTOTYPES 1
-# endif
-#endif
-
-#if GCC_VERSION_BEFORE(4,9,5)
-/* GCC 4.9.2 reportedly has this feature and is broken.
- * The function is not officially documented below.
- * Seems we should not use it.
- * https://gcc.gnu.org/onlinedocs/gcc-4.9.4/gcc/Other-Builtins.html#Other-Builtins */
+#if RUBY3_COMPILER_BEFORE(GCC, 5, 0, 0)
+# /* GCC 4.9.2 reportedly has this feature  and is broken.  The function is not
+#  * officially documented below.  Seems we should not use it.
+#  * https://gcc.gnu.org/onlinedocs/gcc-4.9.4/gcc/Other-Builtins.html */
 # undef HAVE_BUILTIN___BUILTIN_ALLOCA_WITH_ALIGN
 #endif
 
-#define STRINGIZE(expr) STRINGIZE0(expr)
 #ifndef STRINGIZE0
-#define STRINGIZE0(expr) #expr
+# define STRINGIZE(expr) STRINGIZE0(expr)
+# define STRINGIZE0(expr) #expr
 #endif
 
 #ifdef AC_APPLE_UNIVERSAL_BUILD
-#undef WORDS_BIGENDIAN
-#ifdef __BIG_ENDIAN__
-#define WORDS_BIGENDIAN
-#endif
+# undef WORDS_BIGENDIAN
+# ifdef __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN
+# endif
 #endif
 
 #ifndef DLEXT_MAXLEN
-#define DLEXT_MAXLEN 4
+# define DLEXT_MAXLEN 4
 #endif
 
 #ifndef RUBY_PLATFORM
-#define RUBY_PLATFORM "unknown-unknown"
+# define RUBY_PLATFORM "unknown-unknown"
 #endif
 
-#ifndef UNALIGNED_WORD_ACCESS
-# if defined(__i386) || defined(__i386__) || defined(_M_IX86) || \
-     defined(__x86_64) || defined(__x86_64__) || defined(_M_AMD64) || \
-     defined(__powerpc64__) || \
-     defined(__mc68020__)
-#   define UNALIGNED_WORD_ACCESS 1
-# else
-#   define UNALIGNED_WORD_ACCESS 0
-# endif
+#ifdef UNALIGNED_WORD_ACCESS
+# /* Take that. */
+#elif defined(__i386)
+# define UNALIGNED_WORD_ACCESS 1
+#elif defined(__i386__)
+# define UNALIGNED_WORD_ACCESS 1
+#elif defined(_M_IX86)
+# define UNALIGNED_WORD_ACCESS 1
+#elif defined(__x86_64)
+# define UNALIGNED_WORD_ACCESS 1
+#elif defined(__x86_64__)
+# define UNALIGNED_WORD_ACCESS 1
+#elif defined(_M_AMD64)
+# define UNALIGNED_WORD_ACCESS 1
+#elif defined(__powerpc64__)
+# define UNALIGNED_WORD_ACCESS 1
+#elif defined(__mc68020__)
+# define UNALIGNED_WORD_ACCESS 1
+#else
+# define UNALIGNED_WORD_ACCESS 0
 #endif
 
 #endif /* RUBY3_CONFIG_H */
