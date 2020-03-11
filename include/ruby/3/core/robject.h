b9bed92490c610a59d16cdfcddb3ed83319f44cb
卜部昌平 <shyouhei@ruby-lang.org>
2020-03-11 17:43:50 +0900

    include/ruby/3/core/robject.h rework
    Turned macros into inline functions.  Note that ROBJECT_IV_INDEX_TBL is
    broken.  RCLASS_IV_INDEX_TBL does not exist.

--- include/ruby/3/core/robject.h
+++ include/ruby/3/core/robject.h
@@ -20,22 +20,30 @@
  */
 #ifndef  RUBY3_ROBJECT_H
 #define  RUBY3_ROBJECT_H
-#include "ruby/3/core/rbasic.h"
-#include "ruby/3/dllexport.h"
+#include "ruby/3/config.h"
+
+#ifdef HAVE_STDINT_H
+# include <stdint.h>
+#endif
+
+#include "ruby/3/attr/artificial.h"
+#include "ruby/3/attr/pure.h"
+#include "ruby/3/cast.h"
 #include "ruby/3/fl_type.h"
 #include "ruby/3/value.h"
-#include "ruby/backward/2/r_cast.h"
+#include "ruby/3/value_type.h"
 
-RUBY3_SYMBOL_EXPORT_BEGIN()
-
+#define ROBJECT(obj)          RUBY3_CAST((struct RObject *)(obj))
 #define ROBJECT_EMBED_LEN_MAX ROBJECT_EMBED_LEN_MAX
-#define ROBJECT_EMBED ROBJECT_EMBED
-enum ruby_robject_flags {
-    ROBJECT_EMBED_LEN_MAX = RVALUE_EMBED_LEN_MAX,
-    ROBJECT_EMBED = RUBY_FL_USER1,
+#define ROBJECT_EMBED         ROBJECT_EMBED
+/** @cond INTERNAL_MACRO */
+#define ROBJECT_NUMIV         ROBJECT_NUMIV
+#define ROBJECT_IVPTR         ROBJECT_IVPTR
+/** @endcond */
 
-    ROBJECT_ENUM_END
-};
+enum ruby_robject_flags { ROBJECT_EMBED = RUBY_FL_USER1 };
+
+enum { ROBJECT_EMBED_LEN_MAX = RUBY3_EMBED_LEN_MAX_OF(VALUE) };
 
 struct RObject {
     struct RBasic basic;
@@ -48,20 +56,42 @@ struct RObject {
         VALUE ary[ROBJECT_EMBED_LEN_MAX];
     } as;
 };
-#define ROBJECT_NUMIV(o) \
-    ((RBASIC(o)->flags & ROBJECT_EMBED) ? \
-     ROBJECT_EMBED_LEN_MAX : \
-     ROBJECT(o)->as.heap.numiv)
-#define ROBJECT_IVPTR(o) \
-    ((RBASIC(o)->flags & ROBJECT_EMBED) ? \
-     ROBJECT(o)->as.ary : \
-     ROBJECT(o)->as.heap.ivptr)
+
+RUBY3_ATTR_PURE_ON_NDEBUG()
+RUBY3_ATTR_ARTIFICIAL()
+static inline uint32_t
+ROBJECT_NUMIV(VALUE obj)
+{
+    RUBY3_ASSERT_TYPE(obj, RUBY_T_OBJECT);
+
+    if (RB_FL_ANY_RAW(obj, ROBJECT_EMBED)) {
+        return ROBJECT_EMBED_LEN_MAX;
+    }
+    else {
+        return ROBJECT(obj)->as.heap.numiv;
+    }
+}
+
+RUBY3_ATTR_PURE_ON_NDEBUG()
+RUBY3_ATTR_ARTIFICIAL()
+static inline VALUE *
+ROBJECT_IVPTR(VALUE obj)
+{
+    RUBY3_ASSERT_TYPE(obj, RUBY_T_OBJECT);
+
+    struct RObject *const ptr = ROBJECT(obj);
+
+    if (RB_FL_ANY_RAW(obj, ROBJECT_EMBED)) {
+        return ptr->as.ary;
+    }
+    else {
+        return ptr->as.heap.ivptr;
+    }
+}
+
 #define ROBJECT_IV_INDEX_TBL(o) \
     ((RBASIC(o)->flags & ROBJECT_EMBED) ? \
      RCLASS_IV_INDEX_TBL(rb_obj_class(o)) : \
      ROBJECT(o)->as.heap.iv_index_tbl)
-#define ROBJECT(obj) (R_CAST(RObject)(obj))
-
-RUBY3_SYMBOL_EXPORT_END()
 
 #endif /* RUBY3_ROBJECT_H */
