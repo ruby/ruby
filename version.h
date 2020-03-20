9ed9246f5a0945db89fb48d4dd88e9e80f8d3e2c
nagachika <nagachika@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2020-03-20 02:45:26 +0000

    merge revision(s) 67322,67323:
    	Insert --jit-verbose=2 logs for mswin debugging
    
    	Explain why cl.exe needs --jit-verbose=2 [ci skip]
    
    	I forgot to explain it in the previous commit
    
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/ruby_2_6@67847 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #define RUBY_VERSION "2.6.6"
-#define RUBY_RELEASE_DATE "2020-03-19"
-#define RUBY_PATCHLEVEL 134
+#define RUBY_RELEASE_DATE "2020-03-20"
+#define RUBY_PATCHLEVEL 135
 
 #define RUBY_RELEASE_YEAR 2020
 #define RUBY_RELEASE_MONTH 3
-#define RUBY_RELEASE_DAY 19
+#define RUBY_RELEASE_DAY 20
 
 #include "ruby/version.h"
 
