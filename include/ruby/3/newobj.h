b6500c35464d84519922f2b3296ffb90789305d0
卜部昌平 <shyouhei@ruby-lang.org>
2020-03-24 14:44:51 +0900

    include/ruby/3/newobj.h rework
    Added assertions and RUBY3_CAST where necessary.

--- include/ruby/3/newobj.h
+++ include/ruby/3/newobj.h
@@ -20,13 +20,24 @@
  */
 #ifndef  RUBY3_NEWOBJ_H
 #define  RUBY3_NEWOBJ_H
+#include "ruby/3/cast.h"
 #include "ruby/3/core/rbasic.h"
 #include "ruby/3/dllexport.h"
 #include "ruby/3/fl_type.h"
+#include "ruby/3/special_consts.h"
 #include "ruby/3/value.h"
+#include "ruby/assert.h"
+
+#define RB_NEWOBJ(obj,type) type *(obj) = RUBY3_CAST((type *)rb_newobj())
+#define RB_NEWOBJ_OF(obj,type,klass,flags) type *(obj) = RUBY3_CAST((type *)rb_newobj_of(klass, flags))
+
+#define NEWOBJ     RB_NEWOBJ
+#define NEWOBJ_OF  RB_NEWOBJ_OF /* core has special NEWOBJ_OF() in internal.h */
+#define OBJSETUP   rb_obj_setup /* use NEWOBJ_OF instead of NEWOBJ()+OBJSETUP() */
+#define CLONESETUP rb_clone_setup
+#define DUPSETUP   rb_dup_setup
 
 RUBY3_SYMBOL_EXPORT_BEGIN()
-
 VALUE rb_newobj(void);
 VALUE rb_newobj_of(VALUE, VALUE);
 VALUE rb_obj_setup(VALUE obj, VALUE klass, VALUE type);
@@ -34,19 +45,17 @@ VALUE rb_obj_class(VALUE);
 VALUE rb_singleton_class_clone(VALUE);
 void rb_singleton_class_attached(VALUE,VALUE);
 void rb_copy_generic_ivar(VALUE,VALUE);
-#define RB_NEWOBJ(obj,type) type *(obj) = (type*)rb_newobj()
-#define RB_NEWOBJ_OF(obj,type,klass,flags) type *(obj) = (type*)rb_newobj_of(klass, flags)
-#define NEWOBJ(obj,type) RB_NEWOBJ(obj,type)
-#define NEWOBJ_OF(obj,type,klass,flags) RB_NEWOBJ_OF(obj,type,klass,flags) /* core has special NEWOBJ_OF() in internal.h */
-#define OBJSETUP(obj,c,t) rb_obj_setup(obj, c, t) /* use NEWOBJ_OF instead of NEWOBJ()+OBJSETUP() */
-#define CLONESETUP(clone,obj) rb_clone_setup(clone,obj)
-#define DUPSETUP(dup,obj) rb_dup_setup(dup,obj)
+RUBY3_SYMBOL_EXPORT_END()
 
 static inline void
 rb_clone_setup(VALUE clone, VALUE obj)
 {
+    RUBY3_ASSERT_OR_ASSUME(! RB_SPECIAL_CONST_P(obj));
+    RUBY3_ASSERT_OR_ASSUME(! RB_SPECIAL_CONST_P(clone));
+
+    const VALUE flags = RUBY_FL_PROMOTED0 | RUBY_FL_PROMOTED1 | RUBY_FL_FINALIZE;
     rb_obj_setup(clone, rb_singleton_class_clone(obj),
-                 RBASIC(obj)->flags & ~(FL_PROMOTED0|FL_PROMOTED1|FL_FINALIZE));
+                 RB_FL_TEST_RAW(obj, ~flags));
     rb_singleton_class_attached(RBASIC_CLASS(clone), clone);
     if (RB_FL_TEST(obj, RUBY_FL_EXIVAR)) rb_copy_generic_ivar(clone, obj);
 }
@@ -54,10 +63,11 @@ rb_clone_setup(VALUE clone, VALUE obj)
 static inline void
 rb_dup_setup(VALUE dup, VALUE obj)
 {
+    RUBY3_ASSERT_OR_ASSUME(! RB_SPECIAL_CONST_P(obj));
+    RUBY3_ASSERT_OR_ASSUME(! RB_SPECIAL_CONST_P(dup));
+
     rb_obj_setup(dup, rb_obj_class(obj), RB_FL_TEST_RAW(obj, RUBY_FL_DUPPED));
     if (RB_FL_TEST(obj, RUBY_FL_EXIVAR)) rb_copy_generic_ivar(dup, obj);
 }
-
-RUBY3_SYMBOL_EXPORT_END()
 
 #endif /* RUBY3_NEWOBJ_H */
