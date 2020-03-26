6ecf07ab3538bfb74d298121ac0a1b2079b1d3fa
卜部昌平 <shyouhei@ruby-lang.org>
2020-03-26 16:43:15 +0900

    include/ruby/3/symbol.h rework
    Refactored a part of macros into an inline function.

--- include/ruby/3/symbol.h
+++ include/ruby/3/symbol.h
@@ -20,18 +20,38 @@
  */
 #ifndef  RUBY3_SYMBOL_H
 #define  RUBY3_SYMBOL_H
+#include "ruby/3/config.h"
+
+#ifdef HAVE_STDDEF_H
+# include <stddef.h>
+#endif
+
+#ifdef HAVE_STRING_H
+# include <string.h>
+#endif
+
+#include "ruby/3/attr/nonnull.h"
+#include "ruby/3/attr/pure.h"
+#include "ruby/3/attr/noalias.h"
+#include "ruby/3/cast.h"
 #include "ruby/3/dllexport.h"
+#include "ruby/3/has/builtin.h"
 #include "ruby/3/value.h"
 
-RUBY3_SYMBOL_EXPORT_BEGIN()
+#define RB_ID2SYM      rb_id2sym
+#define RB_SYM2ID      rb_sym2id
+#define ID2SYM         RB_ID2SYM
+#define SYM2ID         RB_SYM2ID
+#define CONST_ID_CACHE RUBY_CONST_ID_CACHE
+#define CONST_ID       RUBY_CONST_ID
 
+/** @cond INTERNAL_MACRO */
+#define rb_intern_const rb_intern_const
+/** @endcond */
+
+RUBY3_SYMBOL_EXPORT_BEGIN()
 ID rb_sym2id(VALUE);
 VALUE rb_id2sym(ID);
-#define RB_ID2SYM(x) (rb_id2sym(x))
-#define RB_SYM2ID(x) (rb_sym2id(x))
-#define ID2SYM(x) RB_ID2SYM(x)
-#define SYM2ID(x) RB_SYM2ID(x)
-
 ID rb_intern(const char*);
 ID rb_intern2(const char*, long);
 ID rb_intern_str(VALUE str);
@@ -42,34 +62,53 @@ VALUE rb_id2str(ID);
 VALUE rb_sym2str(VALUE);
 VALUE rb_to_symbol(VALUE name);
 VALUE rb_check_symbol(volatile VALUE *namep);
+RUBY3_SYMBOL_EXPORT_END()
 
+RUBY3_ATTR_PURE()
+RUBY3_ATTR_NONNULL(())
+static inline ID
+rb_intern_const(const char *str)
+{
+    size_t len = strlen(str);
+    return rb_intern2(str, RUBY3_CAST((long)len));
+}
+
+RUBY3_ATTR_NOALIAS()
+RUBY3_ATTR_NONNULL(())
+static inline ID
+ruby3_intern_const(ID *ptr, const char *str)
+{
+    while (! *ptr) {
+        *ptr = rb_intern_const(str);
+    }
+
+    return *ptr;
+}
+
+/* Does anyone use it?  Preserved for backward compat. */
 #define RUBY_CONST_ID_CACHE(result, str)                \
     {                                                   \
         static ID rb_intern_id_cache;                   \
-        if (!rb_intern_id_cache)                        \
-            rb_intern_id_cache = rb_intern2((str), (long)strlen(str)); \
+        ruby3_intern_const(&rb_intern_id_cache, (str)); \
         result rb_intern_id_cache;                      \
     }
 #define RUBY_CONST_ID(var, str) \
-    do RUBY_CONST_ID_CACHE((var) =, (str)) while (0)
-#define CONST_ID_CACHE(result, str) RUBY_CONST_ID_CACHE(result, str)
-#define CONST_ID(var, str) RUBY_CONST_ID(var, str)
-#if defined(HAVE_BUILTIN___BUILTIN_CONSTANT_P) && defined(HAVE_STMT_AND_DECL_IN_EXPR)
+    do { \
+        static ID ruby3_id; \
+        (var) = ruby3_intern_const(&ruby3_id, (str)); \
+    } while (0)
+
+#if RUBY3_HAS_BUILTIN(__builtin_constant_p) && defined(HAVE_STMT_AND_DECL_IN_EXPR)
 /* __builtin_constant_p and statement expression is available
  * since gcc-2.7.2.3 at least. */
 #define rb_intern(str) \
     (__builtin_constant_p(str) ? \
-        __extension__ (RUBY_CONST_ID_CACHE((ID), (str))) : \
-        rb_intern(str))
-#define rb_intern_const(str) \
-    (__builtin_constant_p(str) ? \
-     __extension__ (rb_intern2((str), (long)strlen(str))) : \
+     __extension__ ({ \
+         static ID ruby3_id; \
+         ruby3_intern_const(&ruby3_id, (str)); \
+     }) : \
      (rb_intern)(str))
-
-#else
-#define rb_intern_const(str) rb_intern2((str), (long)strlen(str))
 #endif
 
-RUBY3_SYMBOL_EXPORT_END()
 
 #endif /* RUBY3_SYMBOL_H */
