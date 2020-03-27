1039c32de6ac27400b2cc9eca2b0cd382ddcfbdd
David Rodríguez <deivid.rodriguez@riseup.net>
2020-03-27 18:31:51 +0100

    [rubygems/rubygems] Simplify nested require exceptions
    https://github.com/rubygems/rubygems/commit/7fbef2f0e3

--- test/rubygems/test_kernel.rb
+++ test/rubygems/test_kernel.rb
@@ -91,6 +91,25 @@ class TestKernel < Gem::TestCase
     refute $:.any? { |p| %r{a-1/bin} =~ p }
   end
 
+  def test_gem_failing_inside_require_doesnt_cause_double_exceptions
+    File.write("activate.rb", "gem('a', '= 999')\n")
+
+    require "open3"
+
+    output, _ = Open3.capture2e(
+      { "GEM_HOME" => Gem.paths.home },
+      Gem.ruby,
+      "-I",
+      File.expand_path("../../lib", __dir__),
+      "-r",
+      "./activate.rb"
+    )
+
+    load_errors = output.split("\n").select { |line| line.include?("Could not find")}
+
+    assert_equal 1, load_errors.size
+  end
+
   def test_gem_bundler
     quick_gem 'bundler', '1'
     quick_gem 'bundler', '2.a'
