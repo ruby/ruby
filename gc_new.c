cdb38b4657180aeec34b72af02cacea5a1c9150b
Jacob Matthews <jamatthews@users.noreply.github.com>
2020-04-19 14:38:49 +1200

    new heap struct

--- gc_new.c
+++ gc_new.c
@@ -6,8 +6,22 @@
 
 **********************************************************************/
 
+struct new_heap {
+  int total_blocks;
+};
+
+static struct new_heap global_new_heap;
+
+static struct new_heap*
+new_heap_get(void)
+{
+    struct new_heap* heap = &global_new_heap;
+    //new_heap_verify(heap);
+    return heap;
+}
+
 void
 Init_NewHeap(void)
 {
-
+    struct new_heap* heap = new_heap_get();
 }
