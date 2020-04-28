3261cd85fa9d65cd7a9c0e77823df5f2cb80f64d
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-04-28 22:29:48 +0900

    Move rb_big_isqrt defination to internal/bignum.h

--- internal/bignum.h
+++ internal/bignum.h
@@ -116,6 +116,7 @@ VALUE rb_big_uminus(VALUE x);
 VALUE rb_big_hash(VALUE);
 VALUE rb_big_odd_p(VALUE);
 VALUE rb_big_even_p(VALUE);
+VALUE rb_big_isqrt(VALUE);
 size_t rb_big_size(VALUE);
 VALUE rb_integer_float_cmp(VALUE x, VALUE y);
 VALUE rb_integer_float_eq(VALUE x, VALUE y);
