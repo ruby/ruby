94a4708dbdfac7a339b868981ab30d7e704eb5b3
David Rodríguez <deivid.rodriguez@riseup.net>
2020-05-02 12:37:57 +0200

    [rubygems/rubygems] Prefer `tr` to `gsub` when replacing path separators
    This is not detected by the `Performance/StringReplacement` cop, I guess
    because of using constants. But still seems like a good change.
    
    Co-authored-by: MSP-Greg <MSP-Greg@users.noreply.github.com>
    
    https://github.com/rubygems/rubygems/commit/f862103133

--- lib/rubygems/installer.rb
+++ lib/rubygems/installer.rb
@@ -709,7 +709,7 @@ class Gem::Installer
     return if self.class.path_warning
 
     user_bin_dir = @bin_dir || Gem.bindir(gem_home)
-    user_bin_dir = user_bin_dir.gsub(File::SEPARATOR, File::ALT_SEPARATOR) if
+    user_bin_dir = user_bin_dir.tr(File::SEPARATOR, File::ALT_SEPARATOR) if
       File::ALT_SEPARATOR
 
     path = ENV['PATH']
