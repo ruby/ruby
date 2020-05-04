0041812323e05131c77b9d9936cca627eba3c3d1
BurdetteLamar <burdettelamar@yahoo.com>
2020-05-04 14:35:50 -0500

    More on JSON

--- ext/json/lib/json.rb
+++ ext/json/lib/json.rb
@@ -12,22 +12,17 @@ require 'json/common'
 # - Boolean:  +true+, +false+.
 # - Null: +null+.
 # - \Array: an ordered list of values, enclosed by square brackets:
-#
 #     ["foo", 1, 1.0, 2.0e2, true, false, null]
 #
 # - \Object: a collection of name/value pairs, enclosed by curly braces;
 #   each name is double-quoted text;
 #   the values may be any \JSON values:
-#
 #     {"a": "foo", "b": 1, "c": 1.0, "d": 2.0e2, "e": true, "f": false, "g": null}
-#
 #
 # A \JSON array or object may contain nested arrays, objects, and scalars
 # to any depth:
 #   {"foo": {"bar": 1, "baz": 2}, "bat": [0, 1, 2]}
 #   [{"foo": 0, "bar": 1}, ["baz", 2]]
-#
-# To read more about \JSON visit: http://json.org
 #
 # == Using \Module \JSON
 #
@@ -56,118 +51,118 @@ require 'json/common'
 # ==== Parsing \JSON Arrays
 #
 # When +source+ is a \JSON array, JSON.parse by default returns a Ruby \Array:
-#   source = '["foo", 1, 1.0, 2.0e2, true, false, null]'
-#   a = JSON.parse(source)
-#   a # => ["foo", 1, 1.0, 200.0, true, false, nil]
-#   a.class # => Array
+#   json = '["foo", 1, 1.0, 2.0e2, true, false, null]'
+#   ruby = JSON.parse(json)
+#   ruby # => ["foo", 1, 1.0, 200.0, true, false, nil]
+#   ruby.class # => Array
 #
 # The \JSON array may contain nested arrays, objects, and scalars
 # to any depth:
-#   source = '[{"foo": 0, "bar": 1}, ["baz", 2]]'
-#   JSON.parse(source) # => [{"foo"=>0, "bar"=>1}, ["baz", 2]]
+#   json = '[{"foo": 0, "bar": 1}, ["baz", 2]]'
+#   JSON.parse(json) # => [{"foo"=>0, "bar"=>1}, ["baz", 2]]
 #
 # ==== Parsing \JSON \Objects
 #
-# When +source+ is a \JSON object, JSON.parse by default returns a Ruby \Hash:
-#   source = '{"a": "foo", "b": 1, "c": 1.0, "d": 2.0e2, "e": true, "f": false, "g": null}'
-#   h = JSON.parse(source)
-#   h # => {"a"=>"foo", "b"=>1, "c"=>1.0, "d"=>200.0, "e"=>true, "f"=>false, "g"=>nil}
-#   h.class # => Hash
+# When the source is a \JSON object, JSON.parse by default returns a Ruby \Hash:
+#   json = '{"a": "foo", "b": 1, "c": 1.0, "d": 2.0e2, "e": true, "f": false, "g": null}'
+#   ruby = JSON.parse(json)
+#   ruby # => {"a"=>"foo", "b"=>1, "c"=>1.0, "d"=>200.0, "e"=>true, "f"=>false, "g"=>nil}
+#   ruby.class # => Hash
 #
 # The \JSON object may contain nested arrays, objects, and scalars
 # to any depth:
-#   source = '{"foo": {"bar": 1, "baz": 2}, "bat": [0, 1, 2]}'
-#   JSON.parse(source) # => {"foo"=>{"bar"=>1, "baz"=>2}, "bat"=>[0, 1, 2]}
+#   json = '{"foo": {"bar": 1, "baz": 2}, "bat": [0, 1, 2]}'
+#   JSON.parse(json) # => {"foo"=>{"bar"=>1, "baz"=>2}, "bat"=>[0, 1, 2]}
 #
 # ==== Parsing \JSON Scalars
 #
-# When +source+ is a \JSON scalar (not an array or object),
+# When the source is a \JSON scalar (not an array or object),
 # JSON.parse returns a Ruby scalar.
 #
 # \String:
-#   s = JSON.parse('"foo"')
-#   s # => "foo"
-#   s.class # => String
+#   ruby = JSON.parse('"foo"')
+#   ruby # => "foo"
+#   ruby.class # => String
 # \Integer:
-#   n = JSON.parse('1')
-#   n # => 1
-#   n.class # => Integer
+#   ruby = JSON.parse('1')
+#   ruby # => 1
+#   ruby.class # => Integer
 # \Float:
-#   f = JSON.parse('1.0')
-#   f # => 1.0
-#   f.class # => Float
-#   f = JSON.parse('2.0e2')
-#   f # => 200
-#   f.class # => Float
+#   ruby = JSON.parse('1.0')
+#   ruby # => 1.0
+#   ruby.class # => Float
+#   ruby = JSON.parse('2.0e2')
+#   ruby # => 200
+#   ruby.class # => Float
 # Boolean:
-#   b = JSON.parse('true')
-#   b # => true
-#   b.class # => TrueClass
-#   b = JSON.parse('false')
-#   b # => false
-#   b.class # => FalseClass
+#   ruby = JSON.parse('true')
+#   ruby # => true
+#   ruby.class # => TrueClass
+#   ruby = JSON.parse('false')
+#   ruby # => false
+#   ruby.class # => FalseClass
 # Null:
-#   n = JSON.parse('null')
-#   n # => nil
-#   n.class # => NilClass
+#   ruby = JSON.parse('null')
+#   ruby # => nil
+#   ruby.class # => NilClass
 #
 # === Generating \JSON
 #
 # To generate a Ruby \String containing \JSON data,
-# use method JSON.generate(source, opts), where
+# use method <tt>JSON.generate(source, opts)</tt>, where
 # - +source+ is a Ruby object.
 # - +opts+ is a \Hash object containing options data.
 #
 # ==== Generating \JSON from Arrays
 #
-# When +source+ is a Ruby \Array, JSON.generate returns
+# When the source is a Ruby \Array, JSON.generate returns
 # a \String containing a \JSON array:
-#   source = [0, 's', :foo]
-#   json = JSON.generate(source)
+#   ruby = [0, 's', :foo]
+#   json = JSON.generate(ruby)
 #   json # => "[0,\"s\",\"foo\"]"
 #
 # The Ruby \Array array may contain nested arrays, hashes, and scalars
 # to any depth:
-#   source = [0, [1, 2], {foo: 3, bar: 4}]
-#   json = JSON.generate(source)
+#   ruby = [0, [1, 2], {foo: 3, bar: 4}]
+#   json = JSON.generate(ruby)
 #   json # => "[0,[1,2],{\"foo\":3,\"bar\":4}]"
 #
 # ==== Generating \JSON from Hashes
 #
-# When +source+ is a Ruby \Hash, JSON.generate returns
+# When the source is a Ruby \Hash, JSON.generate returns
 # a \String containing a \JSON object:
-#   source = {foo: 0, bar: 's', baz: :bat}
-#   json = JSON.generate(source)
+#   ruby = {foo: 0, bar: 's', baz: :bat}
+#   json = JSON.generate(ruby)
 #   json # => "{\"foo\":0,\"bar\":\"s\",\"baz\":\"bat\"}"
 #
 # The Ruby \Hash array may contain nested arrays, hashes, and scalars
 # to any depth:
-#   source = {foo: [0, 1], bar: {baz: 2, bat: 3}, bam: :bad}
-#   json = JSON.generate(source)
+#   ruby = {foo: [0, 1], bar: {baz: 2, bat: 3}, bam: :bad}
+#   json = JSON.generate(ruby)
 #   json # => "{\"foo\":[0,1],\"bar\":{\"baz\":2,\"bat\":3},\"bam\":\"bad\"}"
 #
 # ==== Generating \JSON from Other Objects
 #
-# When +source+ is neither an \Array nor a \Hash,
-# the generated \JSON data depends on the class of +source+.
+# When the source is neither an \Array nor a \Hash,
+# the generated \JSON data depends on the class of the source.
 #
-# When +source+ is a Ruby \Integer or \Float, JSON.generate returns
+# When the source is a Ruby \Integer or \Float, JSON.generate returns
 # a \String containing a \JSON number:
 #   JSON.generate(Integer(0)) # => "0""
 #   JSON.generate(Float(1.0)) # => "1.0"
 #
-# When +source+ is a Ruby \String, JSON.generate returns
+# When the source is a Ruby \String, JSON.generate returns
 # a \String containing a \JSON string (with double-quotes):
 #   JSON.generate('A string') # => "\"A string\""
 #
-# When +source+ is +true+, +false+ or +nil+, JSON.generate returns
+# When the source is +true+, +false+ or +nil+, JSON.generate returns
 # a \String containing the corresponding \JSON token:
 #   JSON.generate(true) # => "true"
 #   JSON.generate(false) # => "false"
 #   JSON.generate(nil) # => "null"
 #
-# When +source+ is none of the above, JSON.generate returns
-# a \String containing a string representation of +source+:
+# When the source is none of the above, JSON.generate returns
+# a \String containing a \JSON string representation of the source:
 #   JSON.generate(:foo) # => "\"foo\""
 #   JSON.generate(Complex(0, 0)) # => "\"0+0i\""
 #   JSON.generate(Dir.new('.')) # => "\"#<Dir:0x0000000006bb30b8>\""
@@ -176,30 +171,29 @@ require 'json/common'
 #
 # When you "round trip" a non-\String object from Ruby to \JSON and back,
 # you have a new \String, instead of the object you began with:
-#
-#   range = Range.new(0, 2)
-#   json = JSON.generate(range)
+#   ruby0 = Range.new(0, 2)
+#   json = JSON.generate(ruby0)
 #   json # => "\"0..2\""
-#   object = JSON.parse(json)
-#   object # => "0..2"
-#   object.class # => String
+#   ruby1 = JSON.parse(json)
+#   ruby1 # => "0..2"
+#   ruby1.class # => String
 #
-# You can use \JSON _additions_ to restore the original object.
+# You can use \JSON _additions_ to preserve the original object.
 # The addition is an extension of a ruby class, so that:
 # - \JSON.generate stores more information in the \JSON string.
-# - \JSON.parse, called with option +create_additions,
+# - \JSON.parse, called with option +create_additions+,
 #   uses that information to create a proper Ruby object.
 #
 # This example shows a \Range being generated into \JSON
 # and parsed back into Ruby, both without and with
 # the addition for \Range:
-#   range = Range.new(0, 2)
+#   ruby = Range.new(0, 2)
 #   # This passage does not use the addition for Range.
-#   json0 = JSON.generate(range)
+#   json0 = JSON.generate(ruby)
 #   ruby0 = JSON.parse(json0)
 #   # This passage uses the addition for Range.
 #   require 'json/add/range'
-#   json1 = JSON.generate(range)
+#   json1 = JSON.generate(ruby)
 #   ruby1 = JSON.parse(json1, create_additions: true)
 #   # Make a nice display.
 #   display = <<EOT
@@ -221,11 +215,13 @@ require 'json/common'
 #     With addition:     0..2 (Range)
 #
 # The \JSON module includes additions for certain classes.
-# You can also craft custom additions.  See {Custom \JSON Additions}[#module-label-Custom+JSON+Additions]
+# You can also craft custom additions.
+# See {Custom \JSON Additions}[#module-JSON-label-Custom+JSON+Additions].
 #
 # === Built-in Additions
 #
-# The \JSON module includes additions for these classes:
+# The \JSON module includes additions for certain classes.
+# To use an addition, +require+ its source:
 # - BigDecimal: <tt>require 'json/add/bigdecimal'</tt>
 # - Complex: <tt>require 'json/add/complex'</tt>
 # - Date: <tt>require 'json/add/date'</tt>
@@ -245,100 +241,99 @@ require 'json/common'
 #
 # \BigDecimal:
 #   require 'json/add/bigdecimal'
-#   value = BigDecimal(0) # 0.0
-#   json = JSON.generate(value) # {"json_class":"BigDecimal","b":"27:0.0"}
-#   ruby = JSON.parse(json, create_additions: true) # 0.0
+#   ruby0 = BigDecimal(0) # 0.0
+#   json = JSON.generate(ruby0) # {"json_class":"BigDecimal","b":"27:0.0"}
+#   ruby1 = JSON.parse(json, create_additions: true) # 0.0
+#   ruby1.class # => BigDecimal
 #
 # \Complex:
 #   require 'json/add/complex'
-#   value = Complex(1+0i) # 1+0i
-#   json = JSON.generate(value) # {"json_class":"Complex","r":1,"i":0}
-#   ruby = JSON.parse(json, create_additions: true) # 1+0i
-#   ruby.class # Complex
+#   ruby0 = Complex(1+0i) # 1+0i
+#   json = JSON.generate(ruby0) # {"json_class":"Complex","r":1,"i":0}
+#   ruby1 = JSON.parse(json, create_additions: true) # 1+0i
+#   ruby1.class # Complex
 #
 # \Date:
 #   require 'json/add/date'
-#   value = Date.today # 2020-05-02
-#   json = JSON.generate(value) # {"json_class":"Date","y":2020,"m":5,"d":2,"sg":2299161.0}
-#   ruby = JSON.parse(json, create_additions: true) # 2020-05-02
-#   ruby.class # Date
+#   ruby0 = Date.today # 2020-05-02
+#   json = JSON.generate(ruby0) # {"json_class":"Date","y":2020,"m":5,"d":2,"sg":2299161.0}
+#   ruby1 = JSON.parse(json, create_additions: true) # 2020-05-02
+#   ruby1.class # Date
 #
 # \DateTime:
 #   require 'json/add/date_time'
-#   value = DateTime.now # 2020-05-02T10:38:13-05:00
-#   json = JSON.generate(value) # {"json_class":"DateTime","y":2020,"m":5,"d":2,"H":10,"M":38,"S":13,"of":"-5/24","sg":2299161.0}
-#   ruby = JSON.parse(json, create_additions: true) # 2020-05-02T10:38:13-05:00
-#   ruby.class # DateTime
+#   ruby0 = DateTime.now # 2020-05-02T10:38:13-05:00
+#   json = JSON.generate(ruby0) # {"json_class":"DateTime","y":2020,"m":5,"d":2,"H":10,"M":38,"S":13,"of":"-5/24","sg":2299161.0}
+#   ruby1 = JSON.parse(json, create_additions: true) # 2020-05-02T10:38:13-05:00
+#   ruby1.class # DateTime
 #
 # \Exception (and its subclasses including \RuntimeError):
 #   require 'json/add/exception'
-#   value = Exception.new('A message') # A message
-#   json = JSON.generate(value) # {"json_class":"Exception","m":"A message","b":null}
-#   ruby = JSON.parse(json, create_additions: true) # A message
-#   ruby.class # Exception
-#   value = RuntimeError.new('Another message') # Another message
-#   json = JSON.generate(value) # {"json_class":"RuntimeError","m":"Another message","b":null}
-#   ruby = JSON.parse(json, create_additions: true) # Another message
-#   ruby.class # RuntimeError
+#   ruby0 = Exception.new('A message') # A message
+#   json = JSON.generate(ruby0) # {"json_class":"Exception","m":"A message","b":null}
+#   ruby1 = JSON.parse(json, create_additions: true) # A message
+#   ruby1.class # Exception
+#   ruby0 = RuntimeError.new('Another message') # Another message
+#   json = JSON.generate(ruby0) # {"json_class":"RuntimeError","m":"Another message","b":null}
+#   ruby1 = JSON.parse(json, create_additions: true) # Another message
+#   ruby1.class # RuntimeError
 #
 # \OpenStruct:
 #   require 'json/add/ostruct'
-#   value = OpenStruct.new(name: 'Matz', language: 'Ruby') # #<OpenStruct name="Matz", language="Ruby">
-#   json = JSON.generate(value) # {"json_class":"OpenStruct","t":{"name":"Matz","language":"Ruby"}}
-#   ruby = JSON.parse(json, create_additions: true) # #<OpenStruct name="Matz", language="Ruby">
-#   ruby.class # OpenStruct
+#   ruby0 = OpenStruct.new(name: 'Matz', language: 'Ruby') # #<OpenStruct name="Matz", language="Ruby">
+#   json = JSON.generate(ruby0) # {"json_class":"OpenStruct","t":{"name":"Matz","language":"Ruby"}}
+#   ruby1 = JSON.parse(json, create_additions: true) # #<OpenStruct name="Matz", language="Ruby">
+#   ruby1.class # OpenStruct
 #
 # \Range:
 #   require 'json/add/range'
-#   value = Range.new(0, 2) # 0..2
-#   json = JSON.generate(value) # {"json_class":"Range","a":[0,2,false]}
-#   ruby = JSON.parse(json, create_additions: true) # 0..2
-#   ruby.class # Range
+#   ruby0 = Range.new(0, 2) # 0..2
+#   json = JSON.generate(ruby0) # {"json_class":"Range","a":[0,2,false]}
+#   ruby1 = JSON.parse(json, create_additions: true) # 0..2
+#   ruby1.class # Range
 #
 # \Rational:
 #   require 'json/add/rational'
-#   value = Rational(1, 3) # 1/3
-#   json = JSON.generate(value) # {"json_class":"Rational","n":1,"d":3}
-#   ruby = JSON.parse(json, create_additions: true) # 1/3
-#   ruby.class # Rational
+#   ruby0 = Rational(1, 3) # 1/3
+#   json = JSON.generate(ruby0) # {"json_class":"Rational","n":1,"d":3}
+#   ruby1 = JSON.parse(json, create_additions: true) # 1/3
+#   ruby1.class # Rational
 #
 # \Regexp:
 #   require 'json/add/regexp'
-#   value = Regexp.new('foo') # (?-mix:foo)
-#   json = JSON.generate(value) # {"json_class":"Regexp","o":0,"s":"foo"}
-#   ruby = JSON.parse(json, create_additions: true) # (?-mix:foo)
-#   ruby.class # Regexp
+#   ruby0 = Regexp.new('foo') # (?-mix:foo)
+#   json = JSON.generate(ruby0) # {"json_class":"Regexp","o":0,"s":"foo"}
+#   ruby1 = JSON.parse(json, create_additions: true) # (?-mix:foo)
+#   ruby1.class # Regexp
 #
 # \Set:
 #   require 'json/add/set'
-#   value = Set.new([0, 1, 2]) # #<Set: {0, 1, 2}>
-#   json = JSON.generate(value) # {"json_class":"Set","a":[0,1,2]}
-#   ruby = JSON.parse(json, create_additions: true) # #<Set: {0, 1, 2}>
-#   ruby.class # Set
-#
-# ---
+#   ruby0 = Set.new([0, 1, 2]) # #<Set: {0, 1, 2}>
+#   json = JSON.generate(ruby0) # {"json_class":"Set","a":[0,1,2]}
+#   ruby1 = JSON.parse(json, create_additions: true) # #<Set: {0, 1, 2}>
+#   ruby1.class # Set
 #
 # \Struct:
 #   require 'json/add/struct'
 #   Customer = Struct.new(:name, :address) # Customer
-#   value = Customer.new("Dave", "123 Main") # #<struct Customer name="Dave", address="123 Main">
-#   json = JSON.generate(value) # {"json_class":"Customer","v":["Dave","123 Main"]}
-#   ruby = JSON.parse(json, create_additions: true) # #<struct Customer name="Dave", address="123 Main">
-#   ruby.class # Customer
-#
+#   ruby0 = Customer.new("Dave", "123 Main") # #<struct Customer name="Dave", address="123 Main">
+#   json = JSON.generate(ruby0) # {"json_class":"Customer","v":["Dave","123 Main"]}
+#   ruby1 = JSON.parse(json, create_additions: true) # #<struct Customer name="Dave", address="123 Main">
+#   ruby1.class # Customer
+  #
 # \Symbol:
 #   require 'json/add/symbol'
-#   value = :foo # foo
-#   json = JSON.generate(value) # {"json_class":"Symbol","s":"foo"}
-#   ruby = JSON.parse(json, create_additions: true) # foo
-#   ruby.class # Symbol
+#   ruby0 = :foo # foo
+#   json = JSON.generate(ruby0) # {"json_class":"Symbol","s":"foo"}
+#   ruby1 = JSON.parse(json, create_additions: true) # foo
+#   ruby1.class # Symbol
 #
 # \Time:
 #   require 'json/add/time'
-#   value = Time.now # 2020-05-02 11:28:26 -0500
-#   json = JSON.generate(value) # {"json_class":"Time","s":1588436906,"n":840560000}
-#   ruby = JSON.parse(json, create_additions: true) # 2020-05-02 11:28:26 -0500
-#   ruby.class # Time
+#   ruby0 = Time.now # 2020-05-02 11:28:26 -0500
+#   json = JSON.generate(ruby0) # {"json_class":"Time","s":1588436906,"n":840560000}
+#   ruby1 = JSON.parse(json, create_additions: true) # 2020-05-02 11:28:26 -0500
+#   ruby1.class # Time
 #
 #
 # === Custom \JSON Additions
@@ -348,7 +343,6 @@ require 'json/common'
 # either for Ruby built-in classes or for user-defined classes.
 #
 # Here's a user-defined class +Foo+:
-#
 #   class Foo
 #     attr_accessor :bar, :baz
 #     def initialize(bar, baz)
@@ -358,7 +352,6 @@ require 'json/common'
 #   end
 #
 # Here's the \JSON addition for it:
-#
 #   # Extend class Foo with JSON addition.
 #   class Foo
 #     # Serialize Foo object with its class name and arguments
