91912ca559b7675f312110a5db38e40c2ebb604e
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-05-11 22:00:20 +0900

    Use relative path in ruby core repository

--- spec/bundler/quality_spec.rb
+++ spec/bundler/quality_spec.rb
@@ -233,7 +233,7 @@ RSpec.describe "The library itself" do
     ]
     files_to_require = lib_tracked_files.grep(/\.rb$/) - exclusions
     files_to_require.reject! {|f| f.start_with?("lib/bundler/vendor") }
-    files_to_require.map! {|f| File.expand_path("../#{f}", __dir__) }
+    files_to_require.map! {|f| File.expand_path(root.join("#{f}"), __dir__) }
     sys_exec!("ruby -w") do |input, _, _|
       files_to_require.each do |f|
         input.puts "require '#{f}'"
