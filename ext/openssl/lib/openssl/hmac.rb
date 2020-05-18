b91f62f3840582bb3e4fbece15654e1d852c829b
Kazuki Yamaguchi <k@rhe.jp>
2020-05-18 16:15:07 +0900

    [ruby/openssl] hmac: migrate from the low-level HMAC API to the EVP API
    Use the EVP API instead of the low-level HMAC API. Use of the HMAC API
    has been discouraged and is being marked as deprecated starting from
    OpenSSL 3.0.0.
    
    The two singleton methods OpenSSL::HMAC, HMAC.digest and HMAC.hexdigest
    are now in lib/openssl/hmac.rb.
    
    https://github.com/ruby/openssl/commit/0317e2fc02

--- ext/openssl/lib/openssl/hmac.rb
+++ ext/openssl/lib/openssl/hmac.rb
@@ -9,5 +9,45 @@ module OpenSSL
 
       OpenSSL.fixed_length_secure_compare(self.digest, other.digest)
     end
+
+    class << self
+      # :call-seq:
+      #    HMAC.digest(digest, key, data) -> aString
+      #
+      # Returns the authentication code as a binary string. The _digest_ parameter
+      # specifies the digest algorithm to use. This may be a String representing
+      # the algorithm name or an instance of OpenSSL::Digest.
+      #
+      # === Example
+      #  key = 'key'
+      #  data = 'The quick brown fox jumps over the lazy dog'
+      #
+      #  hmac = OpenSSL::HMAC.digest('SHA1', key, data)
+      #  #=> "\xDE|\x9B\x85\xB8\xB7\x8A\xA6\xBC\x8Az6\xF7\n\x90p\x1C\x9D\xB4\xD9"
+      def digest(digest, key, data)
+        hmac = new(key, digest)
+        hmac << data
+        hmac.digest
+      end
+
+      # :call-seq:
+      #    HMAC.hexdigest(digest, key, data) -> aString
+      #
+      # Returns the authentication code as a hex-encoded string. The _digest_
+      # parameter specifies the digest algorithm to use. This may be a String
+      # representing the algorithm name or an instance of OpenSSL::Digest.
+      #
+      # === Example
+      #  key = 'key'
+      #  data = 'The quick brown fox jumps over the lazy dog'
+      #
+      #  hmac = OpenSSL::HMAC.hexdigest('SHA1', key, data)
+      #  #=> "de7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9"
+      def hexdigest(digest, key, data)
+        hmac = new(key, digest)
+        hmac << data
+        hmac.hexdigest
+      end
+    end
   end
 end
