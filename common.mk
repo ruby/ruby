047471c52960e32146025fab064487d25f92a141
Nobuyoshi Nakada <nobu@ruby-lang.org>
2020-06-04 10:26:40 +0900

    No GITPULLOPTIONS by default
    To honor the environment variable, keep GITPULLOPTIONS unset by
    default, and appended the option to VCSUP.

--- common.mk
+++ common.mk
@@ -38,7 +38,7 @@ RUBYLIB       = $(PATH_SEPARATOR)
 RUBYOPT       = -
 RUN_OPTS      = --disable-gems
 
-GITPULLOPTIONS = --rebase
+# GITPULLOPTIONS = --no-tags
 
 INCFLAGS = -I. -I$(arch_hdrdir) -I$(hdrdir) -I$(srcdir) -I$(UNICODE_HDR_DIR)
 
