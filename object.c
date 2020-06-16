60212cd8eef55b1b3c4482dfd0c65d2151cb2d30
卜部昌平 <shyouhei@ruby-lang.org>
2020-06-16 10:52:30 +0900

    rb_mod_const_defined: do not goto into a branch
    I'm not necessarily against every goto in general, but jumping into a
    branch is definitely a bad idea.  Better refactor.

--- object.c
+++ object.c
@@ -2676,8 +2676,7 @@ rb_mod_const_defined(int argc, VALUE *argv, VALUE mod)
     pend = path + RSTRING_LEN(name);
 
     if (p >= pend || !*p) {
-      wrong_name:
-	rb_name_err_raise(wrong_constant_name, mod, name);
+        goto wrong_name;
     }
 
     if (p + 2 < pend && p[0] == ':' && p[1] == ':') {
@@ -2750,6 +2749,9 @@ rb_mod_const_defined(int argc, VALUE *argv, VALUE mod)
     }
 
     return Qtrue;
+
+  wrong_name:
+    rb_name_err_raise(wrong_constant_name, mod, name);
 }
 
 /*
