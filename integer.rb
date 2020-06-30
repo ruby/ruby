eefc2d8a3c62fa7d8cf1f1674fcb33addc8962be
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2020-06-30 17:13:37 +0900

    Fix a typo [ci skip]

--- integer.rb
+++ integer.rb
@@ -80,7 +80,7 @@ class Integer
   # call-seq:
   #    int.zero? -> true or false
   #
-  # Returns +true+ if +num+ has a zero value.
+  # Returns +true+ if +int+ has a zero value.
   def zero?
     Primitive.attr! 'inline'
     Primitive.cexpr! 'int_zero_p(self)'
