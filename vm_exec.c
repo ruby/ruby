98288c10143032433ed4191dfc0213582b432ea1
卜部昌平 <shyouhei@ruby-lang.org>
2020-07-06 15:39:26 +0900

    RUBY_CONST_ASSERT: use STATIC_ASSERT instead
    Static assertions shall be done using STATIC_ASSERT these days.

--- vm_exec.c
+++ vm_exec.c
@@ -62,17 +62,6 @@ static void vm_insns_counter_count_insn(int insn) {}
 #endif
 /* #define DECL_SC_REG(r, reg) VALUE reg_##r */
 
-#if VM_DEBUG_STACKOVERFLOW
-NORETURN(static void vm_stack_overflow_for_insn(void));
-static void
-vm_stack_overflow_for_insn(void)
-{
-    rb_bug("CHECK_VM_STACK_OVERFLOW_FOR_INSN: should not overflow here. "
-	   "Please contact ruby-core/dev with your (a part of) script. "
-	   "This check will be removed soon.");
-}
-#endif
-
 #if !OPT_CALL_THREADED_CODE
 static VALUE
 vm_exec_core(rb_execution_context_t *ec, VALUE initial)
