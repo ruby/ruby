601613fdf3907f3842a9d465926dca7c37d56f72
nagachika <nagachika@ruby-lang.org>
2020-07-09 21:03:58 +0900

    merge revision(s) 87ad5ea729735ff6b8f1ac0bb3aad2aedd6f716e,9745e90197f129708986803e8913fb151862478a:
    	Skip jit_test on some new RubyCI envs for now
    
    	Propagate JIT skip to all tests

--- test/lib/jit_support.rb
+++ test/lib/jit_support.rb
@@ -8,6 +8,12 @@ module JITSupport
     %r[\A/opt/intel/.*/bin/intel64/icc\b],
     %r[\A/opt/developerstudio\d+\.\d+/bin/cc\z],
   ]
+  # freebsd12: cc1 internal failure https://rubyci.org/logs/rubyci.s3.amazonaws.com/freebsd12/ruby-master/log/20200306T103003Z.fail.html.gz
+  # rhel8: one or more PCH files were found, but they were invalid https://rubyci.org/logs/rubyci.s3.amazonaws.com/rhel8/ruby-master/log/20200306T153003Z.fail.html.gz
+  PENDING_RUBYCI_NICKNAMES = %w[
+    freebsd12
+    rhel8
+  ]
 
   module_function
   # Run Ruby script with --jit-wait (Synchronous JIT compilation).
@@ -47,7 +53,7 @@ module JITSupport
     return @supported if defined?(@supported)
     @supported = UNSUPPORTED_COMPILERS.all? do |regexp|
       !regexp.match?(RbConfig::CONFIG['MJIT_CC'])
-    end && RbConfig::CONFIG["MJIT_SUPPORT"] != 'no'
+    end && RbConfig::CONFIG["MJIT_SUPPORT"] != 'no' && !PENDING_RUBYCI_NICKNAMES.include?(ENV['RUBYCI_NICKNAME'])
   end
 
   def remove_mjit_logs(stderr)
