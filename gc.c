ad15fd03e915272bdf4b3a8403722b397e3afcf8
nagachika <nagachika@ruby-lang.org>
2020-07-19 11:32:59 +0900

    merge revision(s) 26c179d7e7e7ae0eb21050659c3e8778358230ab: [Backport #17026]
    	Check argument to ObjectSpace._id2ref
    
    	Ensure that the argument is an Integer or implicitly convert to,
    	before dereferencing as a Bignum.  Addressed a regression in
    	b99833baec2.
    
    	Reported by u75615 at https://hackerone.com/reports/898614

--- gc.c
+++ gc.c
@@ -3648,6 +3648,7 @@ id2ref(VALUE objid)
     VALUE orig;
     void *p0;
 
+    objid = rb_to_int(objid);
     if (FIXNUM_P(objid) || rb_big_size(objid) <= SIZEOF_VOIDP) {
         ptr = NUM2PTR(objid);
         if (ptr == Qtrue) return Qtrue;
