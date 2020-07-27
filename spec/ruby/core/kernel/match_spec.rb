980bf94f022116308fb7f95f697a85dc24f5884a
卜部昌平 <shyouhei@ruby-lang.org>
2020-07-27 14:54:46 +0900

    Kernel#=~: delete
    Has been deprecated since ebff9dc10e6e72239c23e50acc7d3cbfdc659e7a.

--- spec/ruby/core/kernel/match_spec.rb
+++ spec/ruby/core/kernel/match_spec.rb
@@ -1,22 +1,24 @@
 require_relative '../../spec_helper'
 
-describe "Kernel#=~" do
-  it "returns nil matching any object" do
-    o = Object.new
+ruby_version_is ''...'3.2' do
+  describe "Kernel#=~" do
+    it "returns nil matching any object" do
+      o = Object.new
 
-    suppress_warning do
-      (o =~ /Object/).should   be_nil
-      (o =~ 'Object').should   be_nil
-      (o =~ Object).should     be_nil
-      (o =~ Object.new).should be_nil
-      (o =~ nil).should        be_nil
-      (o =~ true).should       be_nil
+      suppress_warning do
+        (o =~ /Object/).should   be_nil
+        (o =~ 'Object').should   be_nil
+        (o =~ Object).should     be_nil
+        (o =~ Object.new).should be_nil
+        (o =~ nil).should        be_nil
+        (o =~ true).should       be_nil
+      end
     end
-  end
 
-  it "is deprecated" do
-    -> do
-      Object.new =~ /regexp/
-    end.should complain(/deprecated Object#=~ is called on Object/, verbose: true)
+    it "is deprecated" do
+      -> do
+        Object.new =~ /regexp/
+      end.should complain(/deprecated Object#=~ is called on Object/, verbose: true)
+    end
   end
 end
