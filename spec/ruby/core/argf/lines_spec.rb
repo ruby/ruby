c47136d97b3b74511aa9f57d7aefa53cf4bc53aa
卜部昌平 <shyouhei@ruby-lang.org>
2020-07-27 11:15:50 +0900

    delete deprecated IO-like methods
    This commit deletes
    {IO,ARGF,StringIO,Zib::GZipReader}#{bytes,chars,lines,codepoints}, which
    have been deprecated since c47c095b9740e7c19d6fdca29ab661c1089221d4.
    
    Note that String also has those methods.  They are neither depreacted
    nor deleted because they are not aliases of counterpart each_something.

--- spec/ruby/core/argf/lines_spec.rb
+++ spec/ruby/core/argf/lines_spec.rb
@@ -1,6 +1,8 @@
 require_relative '../../spec_helper'
 require_relative 'shared/each_line'
 
-describe "ARGF.lines" do
-  it_behaves_like :argf_each_line, :lines
+ruby_version_is ''...'2.8' do
+  describe "ARGF.lines" do
+    it_behaves_like :argf_each_line, :lines
+  end
 end
