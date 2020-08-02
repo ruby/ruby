2c169e15884d163e0a3b939f9d6a30b55c3e3b4f
Austin Pray <austin@austinpray.com>
2020-08-01 22:14:43 -0500

    [ruby/rubygems] More tests
    https://github.com/ruby/rubygems/commit/210fa87f65

--- spec/bundler/commands/open_spec.rb
+++ spec/bundler/commands/open_spec.rb
@@ -49,7 +49,7 @@ RSpec.describe "bundle open" do
 
     it "suggests alternatives for similar-sounding gems" do
       bundle "open Rails", env: { "EDITOR" => "echo editor", "VISUAL" => "", "BUNDLER_EDITOR" => "" }, raise_on_error: false
-      expect(err).to match(/did you mean rails\?/i)
+      expect(err).to match(/did you mean 'rails'\?/i)
     end
 
     it "opens the gem with short words" do
