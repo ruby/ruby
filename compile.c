89a581c160cab9c4a903fe4b1b30080962e2ecbf
wanabe <s.wanabe@gmail.com>
2020-08-07 07:56:33 +0900

    Show hidden object and TS_BUILTIN for halfbaked insn data

--- compile.c
+++ compile.c
@@ -8965,7 +8965,11 @@ insn_data_to_s_detail(INSN *iobj)
 	      case TS_VALUE:	/* VALUE */
 		{
 		    VALUE v = OPERAND_AT(iobj, j);
-		    rb_str_concat(str, opobj_inspect(v));
+                    if (!CLASS_OF(v))
+                        rb_str_cat2(str, "<hidden>");
+                    else {
+                        rb_str_concat(str, opobj_inspect(v));
+                    }
 		    break;
 		}
 	      case TS_ID:	/* ID */
@@ -9001,7 +9005,7 @@ insn_data_to_s_detail(INSN *iobj)
 		}
 		break;
               case TS_BUILTIN:
-                rb_bug("unsupported: TS_BUILTIN");
+                rb_str_cat2(str, "<TS_BUILTIN>");
                 break;
 	      default:{
 		rb_raise(rb_eSyntaxError, "unknown operand type: %c", type);
