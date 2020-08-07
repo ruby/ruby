015617a96a00c57d28c1a315251f8cb108f27e73
wanabe <s.wanabe@gmail.com>
2020-08-08 08:38:46 +0900

    Adjust sp for NODE_RESBODY

--- compile.c
+++ compile.c
@@ -6859,6 +6859,9 @@ compile_resbody(rb_iseq_t *iseq, LINK_ANCHOR *const ret, const NODE *const node,
 	    ADD_INSN(ret, line, nop);
 	}
 	ADD_INSN(ret, line, leave);
+        if (!popped) {
+            ADD_INSN(ret, line, pop);
+        }
 	ADD_LABEL(ret, label_miss);
 	resq = resq->nd_head;
     }
