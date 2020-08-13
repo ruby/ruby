5c5fd09a5fba9934273dc410a99d0f07b539c37e
卜部昌平 <shyouhei@ruby-lang.org>
2020-08-13 15:08:29 +0900

    rb_fs_setter: to remove in 3.2
    Has been deprecated since 4d1f86a1ffceb3323b4e2fac82dfc40c5a382e9e.

--- string.c
+++ string.c
@@ -10263,7 +10263,7 @@ rb_fs_setter(VALUE val, ID id, VALUE *var)
 		 rb_id2str(id));
     }
     if (!NIL_P(val)) {
-        rb_warn_deprecated("`$;'", NULL);
+        rb_warn_deprecated_to_remove("`$;'", "3.2");
     }
     *var = val;
 }
