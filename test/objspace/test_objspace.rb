7226f76255ff0fc209c973c6560f2331a676232b
Aaron Patterson <tenderlove@ruby-lang.org>
2020-08-18 19:56:42 -0700

    Reset object allocation trace information
    Compaction makes collisions in the weak map more likely.  For now, clear
    the tracing information before the test run so we get accurate results

--- test/objspace/test_objspace.rb
+++ test/objspace/test_objspace.rb
@@ -165,6 +165,7 @@ class TestObjSpace < Test::Unit::TestCase
   end
 
   def test_trace_object_allocations
+    ObjectSpace.trace_object_allocations_clear # clear object_table to get rid of erroneous detection for c0
     Class.name
     o0 = Object.new
     ObjectSpace.trace_object_allocations{
