44d25c65287558e899188eb9e0fbd7d76e6f74ce
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-08-21 20:40:26 +0900

    Fixup 3292f4d965845d543733559fd1736b8c42fadab3

--- test/erb/test_erb_command.rb
+++ test/erb/test_erb_command.rb
@@ -20,7 +20,7 @@ class TestErbCommand < Test::Unit::TestCase
   def test_deprecated_option
     warnings = [
       "warning: -S option of erb command is deprecated. Please do not use this.",
-      /\n.+\/bin\/erb:\d+: warning: Passing safe_level with the 2nd argument of ERB\.new is deprecated\. Do not use it, and specify other arguments as keyword arguments\.\n/,
+      /\n.+\/libexec\/erb:\d+: warning: Passing safe_level with the 2nd argument of ERB\.new is deprecated\. Do not use it, and specify other arguments as keyword arguments\.\n/,
     ]
     assert_in_out_err(["-w",
                        File.expand_path("../../../libexec/erb", __FILE__),
