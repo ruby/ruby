8f40a62647cd7b961faac6b810053b87502d3994
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2020-09-08 16:45:35 -0400

    Progress on x86 assembler. Encode a few simple instructions.

--- common.mk
+++ common.mk
@@ -151,7 +151,6 @@ COMMONOBJS    = array.$(OBJEXT) \
 		vm_sync.$(OBJEXT) \
 		vm_trace.$(OBJEXT) \
 		ujit_asm.$(OBJEXT) \
-		ujit_asm_tests.$(OBJEXT) \
 		$(COROUTINE_OBJ) \
 		$(DTRACE_OBJ) \
 		$(BUILTIN_ENCOBJS) \
--- test_asm.sh
+++ test_asm.sh
@@ -1,6 +1,10 @@
 # NOTE: I did not know what would be the sensible way to compile
 # and run these tests from the Ruby makefile
 
-clang -std=c99 -Wall ujit_asm.c ujit_asm_tests.c -o asm_test
+clear
+
+clang -std=gnu99 -Wall ujit_asm.c ujit_asm_tests.c -o asm_test
 
 ./asm_test
+
+rm asm_test
