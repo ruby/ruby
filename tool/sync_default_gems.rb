867204c4a558f318be9d39da47298d5ccc85ff17
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-09-10 20:42:53 +0900

    Promote syslog to default gems

--- tool/sync_default_gems.rb
+++ tool/sync_default_gems.rb
@@ -71,6 +71,7 @@ REPOSITORIES = {
   abbrev: "ruby/abbrev",
   shellwords: "ruby/shellwords",
   base64: "ruby/base64",
+  syslog: "ruby/syslog",
 }
 
 def sync_default_gems(gem)
@@ -283,6 +284,14 @@ def sync_default_gems(gem)
     cp_r("#{upstream}/test/nkf", "test")
     cp_r("#{upstream}/nkf.gemspec", "ext/nkf")
     `git checkout ext/nkf/depend`
+  when "syslog"
+    rm_rf(%w[ext/syslog test/syslog test/test_syslog.rb])
+    cp_r("#{upstream}/ext/syslog", "ext")
+    cp_r("#{upstream}/lib", "ext/syslog")
+    cp_r("#{upstream}/test/syslog", "test")
+    cp_r("#{upstream}/test/test_syslog.rb", "test")
+    cp_r("#{upstream}/syslog.gemspec", "ext/syslog")
+    `git checkout ext/syslog/depend`
   else
     sync_lib gem
   end
