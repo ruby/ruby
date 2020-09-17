b7c2fcfe97257ac2431817eba027ad6346e74860
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-09-17 22:44:51 +0900

    remove unsued int_round_zero_p definiation

--- numeric.c
+++ numeric.c
@@ -176,7 +176,6 @@ static VALUE fix_lshift(long, unsigned long);
 static VALUE fix_rshift(long, unsigned long);
 static VALUE int_pow(long x, unsigned long y);
 static VALUE int_even_p(VALUE x);
-static int int_round_zero_p(VALUE num, int ndigits);
 static VALUE rb_int_floor(VALUE num, int ndigits);
 static VALUE rb_int_ceil(VALUE num, int ndigits);
 static VALUE flo_to_i(VALUE num);
