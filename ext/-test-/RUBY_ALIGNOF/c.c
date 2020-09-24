dae0b90c325ce90596d6668b66ce2be534fa9182
卜部昌平 <shyouhei@ruby-lang.org>
2020-09-24 12:16:31 +0900

    ext/-test-/RUBY_ALIGNOF: skip C++ when no compiler
    C++ compilers are optional.  Skip C++ tests when they are absent.

--- ext/-test-/RUBY_ALIGNOF/c.c
+++ ext/-test-/RUBY_ALIGNOF/c.c
@@ -7,3 +7,9 @@ struct T {
 };
 
 RBIMPL_STATIC_ASSERT(RUBY_ALIGNOF, RUBY_ALIGNOF(double) == offsetof(struct T, t));
+
+void
+Init_RUBY_ALIGNOF()
+{
+    // Windows linker mandates this symbol to exist.
+}
--- ext/-test-/RUBY_ALIGNOF/cpp.cpp
+++ ext/-test-/RUBY_ALIGNOF/cpp.cpp
@@ -7,9 +7,3 @@ struct T {
 };
 
 RBIMPL_STATIC_ASSERT(RUBY_ALIGNOF, RUBY_ALIGNOF(double) == offsetof(T, t));
-
-extern "C" void
-Init_RUBY_ALIGNOF()
-{
-    // Windows linker mandates this symbol to exist.
-}
