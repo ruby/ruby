f2d1808e7364aa909101c5208086ef026d5367a2
Masaki Matsushita <glass.saga@gmail.com>
2020-09-25 15:39:26 +0900

    Add comments for resolv_timeout

--- ext/socket/tcpsocket.c
+++ ext/socket/tcpsocket.c
@@ -12,11 +12,13 @@
 
 /*
  * call-seq:
- *    TCPSocket.new(remote_host, remote_port, local_host=nil, local_port=nil)
+ *    TCPSocket.new(remote_host, remote_port, local_host=nil, local_port=nil, resolv_timeout: nil)
  *
  * Opens a TCP connection to +remote_host+ on +remote_port+.  If +local_host+
  * and +local_port+ are specified, then those parameters are used on the local
  * end to establish the connection.
+ *
+ * [:resolv_timeout] specify the name resolution timeout in seconds.
  */
 static VALUE
 tcp_init(int argc, VALUE *argv, VALUE sock)
