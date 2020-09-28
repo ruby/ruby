e8c914c2502f6049ebc5bd93e8db0c62dd14400e
Alan Wu <XrXr@users.noreply.github.com>
2020-09-28 07:03:28 -0400

    Implement the --disable-ujit command line option

--- ujit_compile.h
+++ ujit_compile.h
@@ -3,12 +3,15 @@
 
 #include "stddef.h"
 #include "stdint.h"
+#include "stdbool.h"
 
 #ifndef rb_iseq_t
 typedef struct rb_iseq_struct rb_iseq_t;
 #define rb_iseq_t rb_iseq_t
 #endif
 
+void rb_ujit_init(void);
+bool rb_ujit_enabled_p(void);
 uint8_t* ujit_compile_insn(rb_iseq_t *iseq, unsigned int insn_idx, unsigned int* next_ujit_idx);
 
 #endif
