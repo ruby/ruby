828c34e58b63d64558ec0f2d1d7ae401c5e6b21f
nagachika <nagachika@ruby-lang.org>
2020-09-29 22:46:14 +0900

    merge revision(s) d23d2f3f6fbb5d787b0dd80675c489a692be23e2:
    	[ruby/webrick] Make it more strict to interpret some headers
    
    	Some regexps were too tolerant.
    
    	https://github.com/ruby/webrick/commit/8946bb38b4

--- lib/webrick/httprequest.rb
+++ lib/webrick/httprequest.rb
@@ -226,9 +226,9 @@ module WEBrick
         raise HTTPStatus::BadRequest, "bad URI `#{@unparsed_uri}'."
       end
 
-      if /close/io =~ self["connection"]
+      if /\Aclose\z/io =~ self["connection"]
         @keep_alive = false
-      elsif /keep-alive/io =~ self["connection"]
+      elsif /\Akeep-alive\z/io =~ self["connection"]
         @keep_alive = true
       elsif @http_version < "1.1"
         @keep_alive = false
@@ -503,7 +503,7 @@ module WEBrick
       return unless socket
       if tc = self['transfer-encoding']
         case tc
-        when /chunked/io then read_chunked(socket, block)
+        when /\Achunked\z/io then read_chunked(socket, block)
         else raise HTTPStatus::NotImplemented, "Transfer-Encoding: #{tc}."
         end
       elsif self['content-length'] || @remaining_size
