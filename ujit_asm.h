657a6b8b2c90763cc2b88af39d301a69d5c5c382
Alan Wu <XrXr@users.noreply.github.com>
2020-10-01 15:17:24 -0400

    Comment out jnl to fix Linux build again

--- ujit_asm.h
+++ ujit_asm.h
@@ -297,7 +297,7 @@ void jnc(codeblock_t* cb, size_t label_idx);
 void jne(codeblock_t* cb, size_t label_idx);
 void jng(codeblock_t* cb, size_t label_idx);
 void jnge(codeblock_t* cb, size_t label_idx);
-void jnl(codeblock_t* cb, size_t label_idx);
+// void jnl(codeblock_t* cb, size_t label_idx); // this conflicts with jnl(3)
 void jnle(codeblock_t* cb, size_t label_idx);
 void jno(codeblock_t* cb, size_t label_idx);
 void jnp(codeblock_t* cb, size_t label_idx);
