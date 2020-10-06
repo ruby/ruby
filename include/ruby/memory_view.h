7d594399f666f35eff1c7be4ec944605dc9aa49c
卜部昌平 <shyouhei@ruby-lang.org>
2020-10-06 12:00:16 +0900

    memory_view.h: use bool
    Because `bool` is already used in the header there is no reason to
    hesitate.

--- include/ruby/memory_view.h
+++ include/ruby/memory_view.h
@@ -49,7 +49,7 @@ typedef struct {
     ssize_t len;
 
     /* 1 for readonly memory, 0 for writable memory. */
-    int readonly;
+    bool readonly;
 
     /* A string to describe the format of an element, or NULL for unsigned byte.
      * The format string is a sequence the following pack-template specifiers:
@@ -122,10 +122,10 @@ bool rb_memory_view_register(VALUE klass, const rb_memory_view_entry_t *entry);
     rb_memory_view_is_row_major_contiguous(view) \
     || rb_memory_view_is_column_major_contiguous(view))
 
-int rb_memory_view_is_row_major_contiguous(const rb_memory_view_t *view);
-int rb_memory_view_is_column_major_contiguous(const rb_memory_view_t *view);
-void rb_memory_view_fill_contiguous_strides(const ssize_t ndim, const ssize_t item_size, const ssize_t *const shape, const int row_major_p, ssize_t *const strides);
-int rb_memory_view_init_as_byte_array(rb_memory_view_t *view, VALUE obj, void *data, const ssize_t len, const int readonly);
+bool rb_memory_view_is_row_major_contiguous(const rb_memory_view_t *view);
+bool rb_memory_view_is_column_major_contiguous(const rb_memory_view_t *view);
+void rb_memory_view_fill_contiguous_strides(const ssize_t ndim, const ssize_t item_size, const ssize_t *const shape, const bool row_major_p, ssize_t *const strides);
+int rb_memory_view_init_as_byte_array(rb_memory_view_t *view, VALUE obj, void *data, const ssize_t len, const bool readonly);
 ssize_t rb_memory_view_parse_item_format(const char *format,
                                          rb_memory_view_item_component_t **members,
                                          ssize_t *n_members, const char **err);
