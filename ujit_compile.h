f34f69111a93575290aab0ae13d2722fa36e2dd4
Alan Wu <XrXr@users.noreply.github.com>
2020-10-13 11:15:22 -0400

    MicroJIT: compile after ten calls

--- ujit_compile.h
+++ ujit_compile.h
@@ -10,8 +10,16 @@ typedef struct rb_iseq_struct rb_iseq_t;
 #define rb_iseq_t rb_iseq_t
 #endif
 
+extern bool rb_ujit_enabled;
+
+static inline
+bool rb_ujit_enabled_p(void)
+{
+    return rb_ujit_enabled;
+}
+
 void rb_ujit_init(void);
-bool rb_ujit_enabled_p(void);
-uint8_t* ujit_compile_insn(rb_iseq_t *iseq, unsigned int insn_idx, unsigned int* next_ujit_idx);
+uint8_t *ujit_compile_insn(const rb_iseq_t *iseq, unsigned int insn_idx, unsigned int *next_ujit_idx);
+void rb_ujit_compile_iseq(const rb_iseq_t *iseq);
 
 #endif
