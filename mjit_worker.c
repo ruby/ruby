9a6720a15d1e0c7c2423e99efecc3447484abb8f
Aaron Patterson <tenderlove@ruby-lang.org>
2020-10-22 09:49:26 -0700

    Revert "Add assertions when inline caches are copied to MJIT"
    This reverts commit 6cb6d5abc36ede9d5158c2cd90734134838e6bfb.
    This reverts commit 1484b786aee8d411a9e2278ac6d6e44aedbf6662.
    
    I think we don't need these assertions anymore.  I believe the problem
    is solved by abf678a4397c6c00a1bb686043e377d372e695a4

--- mjit_worker.c
+++ mjit_worker.c
@@ -1261,17 +1261,6 @@ mjit_capture_cc_entries(const struct rb_iseq_constant_body *compiled_iseq, const
     // Capture cc to cc_enties
     for (unsigned int i = 0; i < captured_iseq->ci_size; i++) {
         cc_entries[i] = captured_iseq->call_data[i].cc;
-
-        // Adding assertions to debug GC problem.
-        // FIXME: remove these when we find it
-        const struct rb_callcache *cc = cc_entries[i];
-
-        if (cc && vm_cc_markable(cc)) {
-            assert(BUILTIN_TYPE((VALUE)cc) != T_MOVED);
-            assert(BUILTIN_TYPE((VALUE)vm_cc_cme(cc)) != T_MOVED);
-            assert(!rb_objspace_garbage_object_p((VALUE)cc));
-            assert(!rb_objspace_garbage_object_p((VALUE)vm_cc_cme(cc)));
-        }
     }
 
     return cc_entries_index;
