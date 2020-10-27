7cc20324d2554929b14c3e1f53733a37d10b21d7
Alan Wu <XrXr@users.noreply.github.com>
2020-10-27 18:49:17 -0400

    MicroJIT: generate less code for CFUNCs
    Added UJIT_CHECK_MODE. Set to 1 to double check method dispatch in
    generated code.
    
    It's surprising to me that we need to watch both cc and cme. There might
    be opportunities to simplify there.

--- ujit_compile.h
+++ ujit_compile.h
@@ -4,6 +4,7 @@
 #include "stddef.h"
 #include "stdint.h"
 #include "stdbool.h"
+#include "method.h"
 
 #ifndef rb_iseq_t
 typedef struct rb_iseq_struct rb_iseq_t;
@@ -22,6 +23,7 @@ bool rb_ujit_enabled_p(void)
 
 #define UJIT_CALL_THRESHOLD (10u)
 
+void rb_ujit_method_lookup_change(VALUE cme_or_cc);
 void rb_ujit_init(void);
 uint8_t *ujit_compile_insn(const rb_iseq_t *iseq, unsigned int insn_idx, unsigned int *next_ujit_idx);
 void rb_ujit_compile_iseq(const rb_iseq_t *iseq);
