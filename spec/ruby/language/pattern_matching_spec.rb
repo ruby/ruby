7282f3113ddb117f5d4ea13a60c223a3911ea224
Kazuki Tsujimoto <kazuki@callcc.net>
2020-11-01 18:19:05 +0900

    Suppress "One-line pattern matching is experimental" warning

--- spec/ruby/language/pattern_matching_spec.rb
+++ spec/ruby/language/pattern_matching_spec.rb
@@ -11,10 +11,15 @@ ruby_version_is "2.7" do
 
     ruby_version_is "3.0" do
       it "can be standalone assoc operator that deconstructs value" do
-        eval(<<-RUBY).should == [0, 1]
-          [0, 1] => [a, b]
-          [a, b]
-        RUBY
+        $VERBOSE, verbose = nil, $VERBOSE
+        begin
+          eval(<<-RUBY).should == [0, 1]
+            [0, 1] => [a, b]
+            [a, b]
+          RUBY
+        ensure
+          $VERBOSE = verbose
+        end
       end
     end
 
