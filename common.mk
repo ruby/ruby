aa3ed36a0580eeaaa657f3f1d4d6331dc66aa5f6
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-11-02 16:48:20 +0900

    Added tool/lib into test-spec loadpath

--- common.mk
+++ common.mk
@@ -838,7 +838,7 @@ test-spec-precheck: main
 test-spec: $(TEST_RUNNABLE)-test-spec
 yes-test-spec: test-spec-precheck
 	$(gnumake_recursive)$(Q) \
-	$(RUNRUBY) -r./$(arch)-fake $(srcdir)/spec/mspec/bin/mspec run -B $(srcdir)/spec/default.mspec $(MSPECOPT) $(SPECOPTS)
+	$(RUNRUBY) -r./$(arch)-fake $(srcdir)/spec/mspec/bin/mspec run -B $(srcdir)/spec/default.mspec -I$(srcdir)/tool/lib $(MSPECOPT) $(SPECOPTS)
 no-test-spec:
 
 RUNNABLE = $(LIBRUBY_RELATIVE:no=un)-runnable
