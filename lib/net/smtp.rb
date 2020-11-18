dc3a65bd99a5d086906bdf299fced99be4d862ac
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-11-18 15:43:46 +0900

    [ruby/net-smtp] Bump version to 0.2.1
    https://github.com/ruby/net-smtp/commit/6e5c09dcc4

--- lib/net/smtp.rb
+++ lib/net/smtp.rb
@@ -168,7 +168,7 @@ module Net
   #                     user: 'Your Account', secret: 'Your Password', authtype: :cram_md5)
   #
   class SMTP < Protocol
-    VERSION = "0.2.0"
+    VERSION = "0.2.1"
 
     Revision = %q$Revision$.split[1]
 
