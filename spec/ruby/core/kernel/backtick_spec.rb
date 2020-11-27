ddb1736bfc0cc65351bc52c150b85baf45871870
Benoit Daloze <eregontp@gmail.com>
2020-11-27 15:47:17 +0100

    Update to ruby/spec@c4170a3

--- spec/ruby/core/kernel/backtick_spec.rb
+++ spec/ruby/core/kernel/backtick_spec.rb
@@ -37,7 +37,7 @@ describe "Kernel#`" do
 
   platform_is_not :windows do
     it "handles invalid UTF-8 bytes in command" do
-      `echo "testing\xC2 a non UTF-8 string"`.should == "testing\xC2 a non UTF-8 string\n"
+      `echo "testing\xC2 a non UTF-8 string"`.b.should == "testing\xC2 a non UTF-8 string\n".b
     end
 
     it "sets $? to the exit status of the executed sub-process" do
