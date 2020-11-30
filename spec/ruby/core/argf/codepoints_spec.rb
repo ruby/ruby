b5ef0fc41ab196521a01d0726bc96579b9382257
Nobuyoshi Nakada <nobu@ruby-lang.org>
2020-11-30 23:36:10 +0900

    Removed deprecated IO and ARGV methods

--- spec/ruby/core/argf/codepoints_spec.rb
+++ spec/ruby/core/argf/codepoints_spec.rb
@@ -1,6 +1,8 @@
-require_relative '../../spec_helper'
-require_relative 'shared/each_codepoint'
+ruby_version_is ""..."3.0" do
+  require_relative '../../spec_helper'
+  require_relative 'shared/each_codepoint'
 
-describe "ARGF.codepoints" do
-  it_behaves_like :argf_each_codepoint, :codepoints
+  describe "ARGF.codepoints" do
+    it_behaves_like :argf_each_codepoint, :codepoints
+  end
 end
--- spec/ruby/core/io/chars_spec.rb
+++ spec/ruby/core/io/chars_spec.rb
@@ -1,12 +1,14 @@
 # -*- encoding: utf-8 -*-
-require_relative '../../spec_helper'
-require_relative 'fixtures/classes'
-require_relative 'shared/chars'
+ruby_version_is ""..."3.0" do
+  require_relative '../../spec_helper'
+  require_relative 'fixtures/classes'
+  require_relative 'shared/chars'
 
-describe "IO#chars" do
-  it_behaves_like :io_chars, :chars
-end
+  describe "IO#chars" do
+    it_behaves_like :io_chars, :chars
+  end
 
-describe "IO#chars" do
-  it_behaves_like :io_chars_empty, :chars
+  describe "IO#chars" do
+    it_behaves_like :io_chars_empty, :chars
+  end
 end
