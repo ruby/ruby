b06ffce4aef002dc47c3c5968181230e7ab8d7cc
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-12-04 19:34:06 +0900

    SortedSet was removed at a3db08d7b6ff119223f77e3df00b4f6deac971e2

--- spec/ruby/library/set/sortedset/collect_spec.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../../../spec_helper'
-require 'set'
-require_relative 'shared/collect'
-
-describe "SortedSet#collect!" do
-  it_behaves_like :sorted_set_collect_bang, :collect!
-end
