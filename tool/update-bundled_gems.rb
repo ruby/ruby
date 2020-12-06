e4e2a33560ca1ec4543fd593b344a29d452f251f
Nobuyoshi Nakada <nobu@ruby-lang.org>
2020-12-06 15:59:46 +0900

    update-bundled_gems: support recent format, keep the tag to test

--- tool/update-bundled_gems.rb
+++ tool/update-bundled_gems.rb
@@ -9,5 +9,6 @@ unless /^[^#]/ !~ (gem = $F[0])
   gem = src.fetch_spec(gem)
   uri = gem.metadata["source_code_uri"] || gem.homepage
   uri = uri.sub(%r[\Ahttps://github\.com/[^/]+/[^/]+\K/tree/.*], "").chomp(".git")
-  $_ = [gem.name, gem.version, uri].join(" ")
+  $F[3][$F[1]] = gem.version.to_s if $F[3] && $F[3].include?($F[1])
+  $_ = [gem.name, gem.version, uri, *$F[3..-1]].join(" ")
 end
