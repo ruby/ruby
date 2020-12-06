20d53dad47064b28721d81ad0a98a25dcb0731c6
Nobuyoshi Nakada <nobu@ruby-lang.org>
2020-12-06 23:37:21 +0900

    Update the version guard to 3.0

--- spec/ruby/core/hash/index_spec.rb
+++ spec/ruby/core/hash/index_spec.rb
@@ -2,7 +2,7 @@ require_relative '../../spec_helper'
 require_relative 'fixtures/classes'
 require_relative 'shared/index'
 
-ruby_version_is ''...'2.8' do
+ruby_version_is ''...'3.0' do
   describe "Hash#index" do
     it_behaves_like :hash_index, :index
   end
