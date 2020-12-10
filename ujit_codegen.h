f7717b1d463548414e607498e84815ac641ce3dc
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2020-12-10 00:06:10 -0500

    Start refactoring JIT engine

--- ujit_codegen.h
+++ ujit_codegen.h
@@ -3,7 +3,7 @@
 
 #include "stddef.h"
 
-uint8_t * ujit_compile_insn(const rb_iseq_t *iseq, unsigned int insn_idx, unsigned int *next_ujit_idx);
+uint8_t * ujit_compile_block(const rb_iseq_t *iseq, unsigned int insn_idx);
 
 void ujit_init_codegen(void);
 
--- ujit_core.c
+++ ujit_core.c
@@ -1,6 +1,36 @@
+#include "internal.h"
 #include "ujit_asm.h"
 #include "ujit_iface.h"
 #include "ujit_core.h"
+
+
+
+
+// Table of block versions indexed by (iseq, index) tuples
+st_table * version_tbl;
+
+/*
+struct st_hash_type {
+    int (*compare)(st_data_t, st_data_t); // st_compare_func*
+    st_index_t (*hash)(st_data_t);        // st_hash_func*
+};
+
+static const struct st_hash_type st_hashtype_num = {
+    st_numcmp,
+    st_numhash,
+};
+
+strcasehash(st_data_t arg)
+{
+    register const char *string = (const char *)arg;
+    ...
+}
+
+*/
+
+
+
+
 
 // Get the current instruction opcode from the context object
 int
@@ -23,7 +53,7 @@ Get an operand for the adjusted stack pointer address
 x86opnd_t
 ctx_sp_opnd(ctx_t* ctx, int32_t offset_bytes)
 {
-    int32_t offset = (ctx->stack_diff) * 8 + offset_bytes;
+    int32_t offset = (ctx->stack_size) * 8 + offset_bytes;
     return mem_opnd(64, REG_SP, offset);
 }
 
@@ -34,10 +64,10 @@ Return a pointer to the new stack top
 x86opnd_t
 ctx_stack_push(ctx_t* ctx, size_t n)
 {
-    ctx->stack_diff += n;
+    ctx->stack_size += n;
 
     // SP points just above the topmost value
-    int32_t offset = (ctx->stack_diff - 1) * 8;
+    int32_t offset = (ctx->stack_size - 1) * 8;
     return mem_opnd(64, REG_SP, offset);
 }
 
@@ -49,10 +79,10 @@ x86opnd_t
 ctx_stack_pop(ctx_t* ctx, size_t n)
 {
     // SP points just above the topmost value
-    int32_t offset = (ctx->stack_diff - 1) * 8;
+    int32_t offset = (ctx->stack_size - 1) * 8;
     x86opnd_t top = mem_opnd(64, REG_SP, offset);
 
-    ctx->stack_diff -= n;
+    ctx->stack_size -= n;
 
     return top;
 }
@@ -61,7 +91,7 @@ x86opnd_t
 ctx_stack_opnd(ctx_t* ctx, int32_t idx)
 {
     // SP points just above the topmost value
-    int32_t offset = (ctx->stack_diff - 1 - idx) * 8;
+    int32_t offset = (ctx->stack_size - 1 - idx) * 8;
     x86opnd_t opnd = mem_opnd(64, REG_SP, offset);
 
     return opnd;
