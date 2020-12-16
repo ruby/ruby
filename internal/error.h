24a4a50d402e8f07ff06d81a425cfe71670b77e3
Jeremy Evans <code@jeremyevans.net>
2020-12-16 08:15:13 -0800

    Switch rb_category_warn{,ing} to accept an rb_warning_category_t
    Since we decided to only allowing specific warning categories,
    there is no reason to have an API that accepts a general string,
    as it is more error-prone.  Switch to only allowing the specific
    warning categories.
    
    As rb_category_warn{,ing} are public API, this requires making
    rb_warning_category_t public API as well.

--- internal/error.h
+++ internal/error.h
@@ -40,13 +40,6 @@
 #endif
 
 /* error.c */
-typedef enum {
-    RB_WARN_CATEGORY_NONE,
-    RB_WARN_CATEGORY_DEPRECATED,
-    RB_WARN_CATEGORY_EXPERIMENTAL,
-    RB_WARN_CATEGORY_ALL_BITS = 0x6, /* no RB_WARN_CATEGORY_NONE bit */
-} rb_warning_category_t;
-
 extern long rb_backtrace_length_limit;
 extern VALUE rb_eEAGAIN;
 extern VALUE rb_eEWOULDBLOCK;
