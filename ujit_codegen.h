3c1a2a301e6333aaa2d70af7a345adddb7fbf9c3
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2020-12-16 17:07:18 -0500

    WIP branch generation code

--- ujit_codegen.h
+++ ujit_codegen.h
@@ -3,6 +3,9 @@
 
 #include "stddef.h"
 
+// Code generation function signature
+typedef bool (*codegen_fn)(codeblock_t* cb, codeblock_t* ocb, ctx_t* ctx);
+
 uint8_t *ujit_compile_block(const rb_iseq_t *iseq, uint32_t insn_idx, bool gen_entry);
 
 void ujit_init_codegen(void);
--- ujit_core.h
+++ ujit_core.h
@@ -20,17 +20,6 @@
 // Maximum number of versions per block
 #define MAX_VERSIONS 5
 
-// Tuple of (iseq, idx) used to idenfity basic blocks
-typedef struct BlockId
-{
-    // Instruction sequence
-    const rb_iseq_t *iseq;
-
-    // Instruction index
-    const uint32_t idx;
-
-} blockid_t;
-
 // Code generation context
 typedef struct ctx_struct
 {
@@ -39,6 +28,8 @@ typedef struct ctx_struct
     // Some of the information here is only needed during
     // code generation, eg: current pc
 
+    // FIXME: we probably don't need this? we just need to
+    // know which initial bytecode we're replacing
     // The start of the generated code
     uint8_t *code_ptr;
 
@@ -62,7 +53,42 @@ typedef struct ctx_struct
 
 } ctx_t;
 
-uint8_t* get_block_version(const rb_iseq_t *iseq, uint32_t idx);
+// Tuple of (iseq, idx) used to idenfity basic blocks
+typedef struct BlockId
+{
+    // Instruction sequence
+    const rb_iseq_t *iseq;
+
+    // Instruction index
+    const uint32_t idx;
+
+} blockid_t;
+
+/// Branch code shape enumeration
+enum uint8_t
+{
+    NEXT0,  // Target 0 is next
+    NEXT1,  // Target 1 is next
+    DEFAULT // Neither target is next
+};
+
+// Branch code generation function signature
+typedef void (*branchgen_fn)(codeblock_t* cb, uint8_t* target0, uint8_t* target1, uint8_t shape);
+
+// Store info about an outgoing branch in a code segment
+typedef struct BranchEntry
+{
+    // Positions where the generated code starts and ends
+    uint32_t start_pos;
+    uint32_t end_pos;
+
+    // Branch target blocks
+    blockid_t targets[2];
+
+    // Branch code generation function
+    branchgen_fn gen_fn;
+
+} branch_t;
 
 // Context object methods
 int ctx_get_opcode(ctx_t *ctx);
@@ -71,6 +97,10 @@ x86opnd_t ctx_sp_opnd(ctx_t* ctx, int32_t offset_bytes);
 x86opnd_t ctx_stack_push(ctx_t* ctx, size_t n);
 x86opnd_t ctx_stack_pop(ctx_t* ctx, size_t n);
 x86opnd_t ctx_stack_opnd(ctx_t* ctx, int32_t idx);
+
+uint8_t* get_block_version(blockid_t block);
+
+void gen_branch(codeblock_t* cb, codeblock_t* ocb, blockid_t target0, blockid_t target1, branchgen_fn gen_fn);
 
 void ujit_init_core(void);
 
