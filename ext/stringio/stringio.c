3d3194412918012ddab185751b84ebb64fe6e5d5
Kenta Murata <mrkn@mrkn.jp>
2020-12-17 10:05:07 +0900

    [stringio] Make stringio Ractor safe
    https://github.com/ruby/stringio/commit/ee3fec7512
    https://github.com/ruby/stringio/commit/18dcd045ef
    https://github.com/ruby/stringio/commit/18dcd045ef

--- ext/stringio/stringio.c
+++ ext/stringio/stringio.c
@@ -1750,6 +1750,11 @@ void
 Init_stringio(void)
 {
 #undef rb_intern
+
+#ifdef HAVE_RB_EXT_RACTOR_SAFE
+  rb_ext_ractor_safe(true);
+#endif
+
     VALUE StringIO = rb_define_class("StringIO", rb_cData);
 
     rb_define_const(StringIO, "VERSION", rb_str_new_cstr(STRINGIO_VERSION));
