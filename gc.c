74ab2c3b463da4d98803fe694acad831938a97e5
Koichi Sasada <ko1@atdot.net>
2020-12-18 17:59:26 +0900

    finalizing should be checked before VM lock

--- gc.c
+++ gc.c
@@ -3724,10 +3724,11 @@ finalize_deferred(rb_objspace_t *objspace)
 static void
 gc_finalize_deferred(void *dmy)
 {
+    rb_objspace_t *objspace = dmy;
+    if (ATOMIC_EXCHANGE(finalizing, 1)) return;
+
     RB_VM_LOCK_ENTER();
     {
-        rb_objspace_t *objspace = dmy;
-        if (ATOMIC_EXCHANGE(finalizing, 1)) return;
         finalize_deferred(objspace);
         ATOMIC_SET(finalizing, 0);
     }
