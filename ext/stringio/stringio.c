c71934f8a50746c9cd01a86375b178d5c7823259
Kenta Murata <mrkn@mrkn.jp>
2020-12-18 09:24:37 +0900

    [stringio] Version 3.0.0
    https://github.com/ruby/stringio/commit/831be01071

--- ext/stringio/stringio.c
+++ ext/stringio/stringio.c
@@ -11,7 +11,7 @@
 
 **********************************************************************/
 
-#define STRINGIO_VERSION "0.1.4"
+#define STRINGIO_VERSION "3.0.0"
 
 #include "ruby.h"
 #include "ruby/io.h"
