75598a1d189aabea2a2cd9966f7d4735ce252c7b
Nobuyoshi Nakada <nobu@ruby-lang.org>
2020-12-20 18:48:15 +0900

    [rubygems/rubygems] Exclude gemspec file itself from gem
    The processed YML data is included as metadata, the source gemspec
    file is unused and just confusing.
    
    https://github.com/rubygems/rubygems/commit/f444478eac

--- spec/bundler/commands/newgem_spec.rb
+++ spec/bundler/commands/newgem_spec.rb
@@ -588,6 +588,14 @@ RSpec.describe "bundle gem" do
       expect(bundled_app("#{gem_name}/lib/#{require_path}.rb").read).to match(/class Error < StandardError; end$/)
     end
 
+    it "does not include the gemspec file in files" do
+      bundle "gem #{gem_name}"
+
+      bundler_gemspec = Bundler::GemHelper.new(gemspec_dir).gemspec
+
+      expect(bundler_gemspec.files).not_to include("#{gem_name}.gemspec")
+    end
+
     it "runs rake without problems" do
       bundle "gem #{gem_name}"
 
