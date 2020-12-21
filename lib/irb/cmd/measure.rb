0b14abe8e7610e81e6bac7a4696cdcc24ca164c0
aycabta <aycabta@gmail.com>
2020-12-22 05:08:12 +0900

    [ruby/irb] Suppress "shadowing outer" warning
    https://github.com/ruby/irb/commit/27b149c599

--- lib/irb/cmd/measure.rb
+++ lib/irb/cmd/measure.rb
@@ -14,8 +14,8 @@ module IRB
           IRB.conf[:MEASURE] = nil
           IRB.unset_measure_callback(arg)
         when :list
-          IRB.conf[:MEASURE_CALLBACKS].each do |type_name, _, arg|
-            puts "- #{type_name}" + (arg ? "(#{arg.inspect})" : '')
+          IRB.conf[:MEASURE_CALLBACKS].each do |type_name, _, arg_val|
+            puts "- #{type_name}" + (arg_val ? "(#{arg_val.inspect})" : '')
           end
         when :on
           IRB.conf[:MEASURE] = true
