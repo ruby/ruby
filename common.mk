3adf84a0790aa2edb9e6c121e1e1e223cc7c0bbd
Nobuyoshi Nakada <nobu@ruby-lang.org>
2021-01-05 11:30:53 +0900

    Commented out the sh-specific code, in cmd.exe

--- common.mk
+++ common.mk
@@ -737,7 +737,8 @@ clean-spec: PHONY
 
 check: main test test-tool test-all test-spec
 	$(ECHO) check succeeded
-	-$(Q) if [ x"$(GIT)" != x ] && $(CHDIR) "$(srcdir)" && \
+	-$(Q) : : "run only on sh"; \
+	if [ x"$(GIT)" != x ] && $(CHDIR) "$(srcdir)" && \
 	    b=`$(GIT) symbolic-ref --short HEAD 2>&1` && \
 	    u=`$(GIT) branch --list --format='%(upstream:short)' $$b`; then \
 	  set -x; $(GIT) --no-pager log --format=oneline -G "^ *# *include" $$u..HEAD --; \
