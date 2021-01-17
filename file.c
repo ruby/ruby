e2e84c9359f36087d8ce9eb36931a4c1a0894da4
Nobuyoshi Nakada <nobu@ruby-lang.org>
2021-01-17 12:44:39 +0900

    New optional argument to File.dirname

--- file.c
+++ file.c
@@ -4683,9 +4683,11 @@ rb_file_s_basename(int argc, VALUE *argv, VALUE _)
     return basename;
 }
 
+static VALUE rb_file_dirname_n(VALUE fname, int level);
+
 /*
  *  call-seq:
- *     File.dirname(file_name)  ->  dir_name
+ *     File.dirname(file_name, level = 1)  ->  dir_name
  *
  *  Returns all components of the filename given in <i>file_name</i>
  *  except the last one (after first stripping trailing separators).
@@ -4697,13 +4699,24 @@ rb_file_s_basename(int argc, VALUE *argv, VALUE _)
  */
 
 static VALUE
-rb_file_s_dirname(VALUE klass, VALUE fname)
+rb_file_s_dirname(int argc, VALUE *argv, VALUE klass)
 {
-    return rb_file_dirname(fname);
+    int level = 1;
+    if ((argc = rb_check_arity(argc, 1, 2)) > 1) {
+        if ((level = NUM2INT(argv[1])) < 0)
+            rb_raise(rb_eArgError, "negative level: %d", level);
+    }
+    return rb_file_dirname_n(argv[0], level);
 }
 
 VALUE
 rb_file_dirname(VALUE fname)
+{
+    return rb_file_dirname_n(fname, 1);
+}
+
+static VALUE
+rb_file_dirname_n(VALUE fname, int level)
 {
     const char *name, *root, *p, *end;
     VALUE dirname;
@@ -4721,7 +4734,7 @@ rb_file_dirname(VALUE fname)
     if (root > name + 1)
 	name = root - 1;
 #endif
-    p = strrdirsep(root, end, enc);
+    for (p = end; level-- > 0 && (p = strrdirsep(root, p, enc)) != 0;) /**/;
     if (!p) {
 	p = root;
     }
@@ -6545,7 +6558,7 @@ Init_File(void)
     rb_define_singleton_method(rb_cFile, "realpath", rb_file_s_realpath, -1);
     rb_define_singleton_method(rb_cFile, "realdirpath", rb_file_s_realdirpath, -1);
     rb_define_singleton_method(rb_cFile, "basename", rb_file_s_basename, -1);
-    rb_define_singleton_method(rb_cFile, "dirname", rb_file_s_dirname, 1);
+    rb_define_singleton_method(rb_cFile, "dirname", rb_file_s_dirname, -1);
     rb_define_singleton_method(rb_cFile, "extname", rb_file_s_extname, 1);
     rb_define_singleton_method(rb_cFile, "path", rb_file_s_path, 1);
 
