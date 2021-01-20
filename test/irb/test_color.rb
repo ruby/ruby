a55a3fc7479f24c42b93af96b4d16ea09c494ff7
Takashi Kokubun <takashikkbn@gmail.com>
2021-01-20 01:00:32 -0800

    [ruby/irb] Split test files for IRB::Color and IRB::ColorPrinter
    https://github.com/ruby/irb/commit/d95e8daab3

--- test/irb/test_color.rb
+++ test/irb/test_color.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: false
 require 'test/unit'
 require 'irb/color'
-require 'irb/color_printer'
 require 'rubygems'
 require 'stringio'
 
@@ -166,23 +165,6 @@ module TestIRB
       end
     end
 
-    IRBTestColorPrinter = Struct.new(:a)
-
-    def test_color_printer
-      unless ripper_lexer_scan_supported?
-        skip 'Ripper::Lexer#scan is supported in Ruby 2.7+'
-      end
-      {
-        1 => "#{BLUE}#{BOLD}1#{CLEAR}\n",
-        IRBTestColorPrinter.new('test') => "#{GREEN}#<struct TestIRB::TestColor::IRBTestColorPrinter#{CLEAR} a#{GREEN}=#{CLEAR}#{RED}#{BOLD}\"#{CLEAR}#{RED}test#{CLEAR}#{RED}#{BOLD}\"#{CLEAR}#{GREEN}>#{CLEAR}\n",
-        Ripper::Lexer.new('1').scan => "[#{GREEN}#<Ripper::Lexer::Elem:#{CLEAR} on_int@1:0 END token: #{RED}#{BOLD}\"#{CLEAR}#{RED}1#{CLEAR}#{RED}#{BOLD}\"#{CLEAR}#{GREEN}>#{CLEAR}]\n",
-        Class.new{define_method(:pretty_print){|q| q.text("[__FILE__, __LINE__, __ENCODING__]")}}.new => "[#{CYAN}#{BOLD}__FILE__#{CLEAR}, #{CYAN}#{BOLD}__LINE__#{CLEAR}, #{CYAN}#{BOLD}__ENCODING__#{CLEAR}]\n",
-      }.each do |object, result|
-        actual = with_term { IRB::ColorPrinter.pp(object, '') }
-        assert_equal(result, actual, "Case: IRB::ColorPrinter.pp(#{object.inspect}, '')")
-      end
-    end
-
     def test_inspect_colorable
       {
         1 => true,
@@ -212,10 +194,6 @@ module TestIRB
 
     # `complete: true` is the same as `complete: false` in Ruby 2.6-
     def complete_option_supported?
-      Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.7.0')
-    end
-
-    def ripper_lexer_scan_supported?
       Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.7.0')
     end
 
