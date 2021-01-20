718890aa04579e7b4a96517e7d44b99ee31edfb4
Takashi Kokubun <takashikkbn@gmail.com>
2021-01-19 22:29:08 -0800

    Pend erb command tests for Ruby 2.5 for now

--- test/erb/test_erb_command.rb
+++ test/erb/test_erb_command.rb
@@ -4,6 +4,7 @@ require 'test/unit'
 
 class TestErbCommand < Test::Unit::TestCase
   def test_var
+    pend 'this test has not supported Ruby 2.5' if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.6.0')
     assert_in_out_err(["-w",
                        File.expand_path("../../../libexec/erb", __FILE__),
                        "var=hoge"],
@@ -11,6 +12,7 @@ class TestErbCommand < Test::Unit::TestCase
   end
 
   def test_template_file_encoding
+    pend 'this test has not supported Ruby 2.5' if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.6.0')
     assert_in_out_err(["-w",
                        File.expand_path("../../../libexec/erb", __FILE__)],
                       "<%=''.encoding.to_s%>", ["UTF-8"])
@@ -18,6 +20,7 @@ class TestErbCommand < Test::Unit::TestCase
 
   # These interfaces will be removed at Ruby 2.7.
   def test_deprecated_option
+    pend 'this test has not supported Ruby 2.5' if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.6.0')
     warnings = [
       "warning: -S option of erb command is deprecated. Please do not use this.",
       /\n.+\/libexec\/erb:\d+: warning: Passing safe_level with the 2nd argument of ERB\.new is deprecated\. Do not use it, and specify other arguments as keyword arguments\.\n/,
