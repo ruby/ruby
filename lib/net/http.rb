37258b64c759c0add4d97f79aeeda8ae4ffc3677
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2021-01-20 20:16:22 -0500

    [ruby/net-http] Replace "iff" with "if and only if"
    iff means if and only if, but readers without that knowledge might
    assume this to be a spelling mistake. To me, this seems like
    exclusionary language that is unnecessary. Simply using "if and only if"
    instead should suffice.
    
    https://github.com/ruby/net-http/commit/ffb87cad32

--- lib/net/http.rb
+++ lib/net/http.rb
@@ -524,7 +524,7 @@ module Net   #:nodoc:
     #
     #   { "cmd" => "search", "q" => "ruby", "max" => "50" }
     #
-    # This method also does Basic Authentication iff +url+.user exists.
+    # This method also does Basic Authentication if and only if +url+.user exists.
     # But userinfo for authentication is deprecated (RFC3986).
     # So this feature will be removed.
     #
