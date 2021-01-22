c303085e09afb964e36887c0295150227b543881
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2021-01-22 15:24:04 -0500

    Minor changes to disasm output. Fix GCC warning.

--- ujit.rb
+++ ujit.rb
@@ -1,5 +1,9 @@
 module UJIT
   def self.disasm(iseq)
+    if iseq.is_a? Method
+        iseq = RubyVM::InstructionSequence.of(iseq)
+    end
+
     blocks = UJIT.blocks_for(iseq)
     return if blocks.empty?
 
@@ -10,13 +14,14 @@ module UJIT
     str << iseq.disasm
     str << "\n"
 
+    # Sort the blocks by increasing addresses
     blocks.sort_by(&:address).reverse.each do |block|
-      str << "== ISEQ RANGE: #{block.iseq_start_index} -> #{block.iseq_end_index} ".ljust(80, "=")
+      str << "== ISEQ RANGE: [#{block.iseq_start_index},#{block.iseq_end_index}[ ".ljust(80, "=")
       str << "\n"
 
       cs.disasm(block.code, 0).each do |i|
         str << sprintf(
-          "\t0x%<address>x:\t%<instruction>s\t%<details>s\n",
+          "\t%<address>04X:\t%<instruction>s\t%<details>s\n",
           address: i.address,
           instruction: i.mnemonic,
           details: i.op_str
