20a877a7e17ef87ae153ad1a3a72b2c139901ef4
Jun Aruga <jaruga@redhat.com>
2021-01-23 17:10:00 +0100

    Skip the ssl tests in case of raising the additional Errno:: classes.
    Considering it as offline.

--- test/rubygems/test_bundled_ca.rb
+++ test/rubygems/test_bundled_ca.rb
@@ -32,7 +32,7 @@ class TestBundledCA < Gem::TestCase
     http.verify_mode = OpenSSL::SSL::VERIFY_PEER
     http.cert_store = bundled_certificate_store
     http.get('/')
-  rescue Errno::ENOENT, Errno::ETIMEDOUT, SocketError
+  rescue Errno::ECONNREFUSED, Errno::ENETUNREACH, Errno::ENOENT, Errno::ETIMEDOUT, SocketError
     skip "#{host} seems offline, I can't tell whether ssl would work."
   rescue OpenSSL::SSL::SSLError => e
     # Only fail for certificate verification errors
