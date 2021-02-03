8b0dbca2f41228ed0c7adb2ae592aea722a440c4
卜部昌平 <shyouhei@ruby-lang.org>
2021-02-03 16:39:38 +0900

    include/ruby/internal/core/rarray.h: add doxygen
    Must not be a bad idea to improve documents. [ci skip]

--- internal/array.h
+++ internal/array.h
@@ -36,10 +36,6 @@ static inline bool ARY_PTR_USING_P(VALUE ary);
 static inline void RARY_TRANSIENT_SET(VALUE ary);
 static inline void RARY_TRANSIENT_UNSET(VALUE ary);
 
-RUBY_SYMBOL_EXPORT_BEGIN
-/* array.c (export) */
-RUBY_SYMBOL_EXPORT_END
-
 MJIT_SYMBOL_EXPORT_BEGIN
 VALUE rb_ary_tmp_new_from_values(VALUE, long, const VALUE *);
 VALUE rb_check_to_array(VALUE ary);
