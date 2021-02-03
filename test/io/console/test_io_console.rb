8960d6b5bdf8cffe01aa21bec17d8b889398acae
Charles Oliver Nutter <headius@headius.com>
2021-02-03 14:51:02 -0600

    [ruby/io-console] Add console.rb to search for paths
    https://github.com/ruby/io-console/commit/74567376c4

--- test/io/console/test_io_console.rb
+++ test/io/console/test_io_console.rb
@@ -7,7 +7,7 @@ rescue LoadError
 end
 
 class TestIO_Console < Test::Unit::TestCase
-  PATHS = $LOADED_FEATURES.grep(%r"/io/console(?:\.#{RbConfig::CONFIG['DLEXT']}|/\w+\.rb)\z") {$`}
+  PATHS = $LOADED_FEATURES.grep(%r"/io/console(?:\.#{RbConfig::CONFIG['DLEXT']}|\.rb|/\w+\.rb)\z") {$`}
   PATHS.uniq!
 
   # FreeBSD seems to hang on TTOU when running parallel tests
