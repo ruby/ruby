df4f8aa248ed959938e0071ec75fb6a615eed4e8
Nobuyoshi Nakada <nobu@ruby-lang.org>
2021-02-09 17:38:52 +0900

    [ruby/io-console] Fixed regression against 3.0

--- ext/io/console/extconf.rb
+++ ext/io/console/extconf.rb
@@ -24,7 +24,9 @@ when true
   # rb_funcallv: 2.1.0
   # RARRAY_CONST_PTR: 2.1.0
   # rb_sym2str: 2.2.0
-  if have_func("rb_fiber_scheduler_make_timeout")
+  if have_macro("HAVE_RUBY_FIBER_SCHEDULER_H")
+    $defs << "-D""HAVE_RB_IO_WAIT=1"
+  elsif have_func("rb_scheduler_timeout") # 3.0
     have_func("rb_io_wait")
   end
   $defs << "-D""ENABLE_IO_GETPASS=1"
