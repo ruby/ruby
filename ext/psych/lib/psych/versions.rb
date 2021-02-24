b7250026dd03de457cf7d36b6918e540ca2ca1c8
Aaron Patterson <tenderlove@ruby-lang.org>
2021-02-24 09:29:49 -0800

    [ruby/psych] bump version
    https://github.com/ruby/psych/commit/091cd46b1f

--- ext/psych/lib/psych/versions.rb
+++ ext/psych/lib/psych/versions.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 module Psych
   # The version of Psych you are using
-  VERSION = '3.3.0'
+  VERSION = '3.3.1'
 
   if RUBY_ENGINE == 'jruby'
     DEFAULT_SNAKEYAML_VERSION = '1.28'.freeze
