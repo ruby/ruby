f7c342f8759c9914e1216da29bd8d7b1b8c8c91b
git <svn-admin@ruby-lang.org>
2021-03-02 09:23:55 +0900

    * 2021-03-02 [ci skip]

--- version.h
+++ version.h
@@ -15,8 +15,8 @@
 #define RUBY_PATCHLEVEL -1
 
 #define RUBY_RELEASE_YEAR 2021
-#define RUBY_RELEASE_MONTH 2
-#define RUBY_RELEASE_DAY 28
+#define RUBY_RELEASE_MONTH 3
+#define RUBY_RELEASE_DAY 2
 
 #include "ruby/version.h"
 
