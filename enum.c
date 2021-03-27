0b0814c912a144ee487cf6e81f141b601f2f5aa8
Kenichi Kamiya <kachick1@gmail.com>
2021-03-27 10:01:12 +0900

    FIx cosmetic damage enum.c
    Co-authored-by: Nobuyoshi Nakada <nobu@ruby-lang.org>

--- enum.c
+++ enum.c
@@ -1072,7 +1072,8 @@ enum_tally(int argc, VALUE *argv, VALUE obj)
     if (rb_check_arity(argc, 0, 1)) {
         hash = rb_convert_type(argv[0], T_HASH, "Hash", "to_hash");
         rb_check_frozen(hash);
-    } else {
+    }
+    else {
         hash = rb_hash_new();
     }
 
