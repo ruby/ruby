66abeb0370feca203d1f179be39be158ba60aa6c
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2021-04-06 18:37:09 +0900

    [ruby/un] Add colorize command
    https://github.com/ruby/un/commit/4d080eeec0

--- lib/un.rb
+++ lib/un.rb
@@ -23,6 +23,7 @@
 #   ruby -run -e wait_writable -- [OPTION] FILE
 #   ruby -run -e mkmf -- [OPTION] EXTNAME [OPTION]
 #   ruby -run -e httpd -- [OPTION] [DocumentRoot]
+#   ruby -run -e colorize -- FILE
 #   ruby -run -e help [COMMAND]
 
 require "fileutils"
@@ -370,6 +371,25 @@ def httpd
       Signal.trap(sig, shut)
     end
     s.start
+  end
+end
+
+##
+# Colorize ruby code.
+#
+#   ruby -run -e colorize -- FILE
+#
+
+def colorize
+  begin
+    require "irb/color"
+  rescue LoadError
+    raise "colorize requires irb 1.1.0 or later"
+  end
+  setup do |argv, |
+    argv.each do |file|
+      puts IRB::Color.colorize_code File.read(file)
+    end
   end
 end
 
