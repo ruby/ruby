fe72cff487283dbaadb9757e74f00291d772cb6f
Burdette Lamar <BurdetteLamar@Yahoo.com>
2021-04-06 13:55:21 -0500

    [ruby/optparse] More on tutorial (#9)
    * More on tutorial: clearer example output
    
    https://github.com/ruby/optparse/commit/84dfd92d2a

--- doc/ruby/mixed_names.rb
+++
@@ -1,9 +0,0 @@
-require 'optparse'
-parser = OptionParser.new
-parser.on('-x', '--xxx') do |option|
-  p "--xxx #{option}"
-end
-parser.on('-y', '--y1%') do |option|
-  p "--y1% #{option}"
-end
-parser.parse!
