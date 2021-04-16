6268fdc662c75c61091b2ab0b476ec15df023328
Yusuke Endoh <mame@ruby-lang.org>
2021-04-16 11:04:23 +0900

    spec/ruby/core/hash/transform_keys_spec.rb: Fix the failure of ruby_3_0
    https://github.com/ruby/spec/pull/833
    https://github.com/ruby/spec/commit/8290e5ad8952b14ee9a5069651d9864c66681112

--- spec/ruby/core/hash/transform_keys_spec.rb
+++ spec/ruby/core/hash/transform_keys_spec.rb
@@ -84,7 +84,7 @@ describe "Hash#transform_keys!" do
     end
   end
 
-  ruby_version_is "2.5.1"..."3.1" do
+  ruby_version_is "2.5.1"..."3.0.2" do # https://bugs.ruby-lang.org/issues/17735
     it "returns the processed keys if we broke from the block" do
       @hash.transform_keys! do |v|
         break if v == :c
@@ -94,7 +94,7 @@ describe "Hash#transform_keys!" do
     end
   end
 
-  ruby_version_is "3.1" do
+  ruby_version_is "3.0.2" do
     it "returns the processed keys and non evaluated keys if we broke from the block" do
       @hash.transform_keys! do |v|
         break if v == :c
