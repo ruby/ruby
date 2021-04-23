52f84419e04e3d3e7628f56530ceaa6c9e5b1b16
Alan Wu <XrXr@users.noreply.github.com>
2021-04-22 22:11:44 -0400

    Make gen_opt_aref() delegate to gen_opt_send_without_block() (#9)
    Basically mirroring the interpreter for the general case.

--- yjit_codegen.c
+++ yjit_codegen.c
@@ -1125,6 +1125,8 @@ gen_opt_gt(jitstate_t* jit, ctx_t* ctx)
     return gen_fixnum_cmp(jit, ctx, cmovg);
 }
 
+static codegen_status_t gen_opt_send_without_block(jitstate_t *jit, ctx_t *ctx);
+
 static codegen_status_t
 gen_opt_aref(jitstate_t *jit, ctx_t *ctx)
 {
@@ -1266,8 +1268,10 @@ gen_opt_aref(jitstate_t *jit, ctx_t *ctx)
         jit_jump_to_next_insn(jit, ctx);
         return YJIT_END_BLOCK;
     }
-
-    return YJIT_CANT_COMPILE;
+    else {
+        // General case. Call the [] method.
+        return gen_opt_send_without_block(jit, ctx);
+    }
 }
 
 static codegen_status_t
