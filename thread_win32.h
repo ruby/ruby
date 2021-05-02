22de815fbf8bc4536b66ad44b3b84facde41fa92
xtkoba <69125751+xtkoba@users.noreply.github.com>
2021-05-02 13:48:19 +0900

    Get rid of typedef redefinition
    Redefinition of typedef is a C11 feature.

--- thread_win32.h
+++ thread_win32.h
@@ -21,10 +21,10 @@
 WINBASEAPI BOOL WINAPI
 TryEnterCriticalSection(IN OUT LPCRITICAL_SECTION lpCriticalSection);
 
-typedef struct rb_thread_cond_struct {
+struct rb_thread_cond_struct {
     struct cond_event_entry *next;
     struct cond_event_entry *prev;
-} rb_nativethread_cond_t;
+};
 
 typedef struct native_thread_data_struct {
     HANDLE interrupt_event;
