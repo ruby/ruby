508d419393950ccb652c5940a22607b13c651c89
卜部昌平 <shyouhei@ruby-lang.org>
2021-05-10 10:02:12 +0900

    test_cdhash: refactor change class
    It is now strange to test Complex in a class named Rational_Test.

--- test/ruby/test_rational.rb
+++ test/ruby/test_rational.rb
@@ -830,21 +830,6 @@ class Rational_Test < Test::Unit::TestCase
     assert_raise(ZeroDivisionError) {Rational("1/0")}
   end
 
-  def test_cdhash
-    assert_separately([], <<-RUBY)
-      n = case 1 when 2r then false else true end
-      assert_equal(n, true, '[ruby-core:103759] [Bug #17854]')
-    RUBY
-    assert_separately([], <<-RUBY)
-      n = case 3/2r when 1.5r then true else false end
-      assert_equal(n, true, '[ruby-core:103759] [Bug #17854]')
-    RUBY
-    assert_separately([], <<-RUBY)
-      n = case 1i when 1i then true else false end
-      assert_equal(n, true, '[ruby-core:103759] [Bug #17854]')
-    RUBY
-  end
-
   def test_Rational_with_invalid_exception
     assert_raise(ArgumentError) {
       Rational("1/1", exception: 1)
