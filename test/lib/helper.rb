bcaa6aeceadd34eb6a0de1d55bf17ecb153a7916
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2021-05-10 18:38:01 +0900

    [ruby/psych] Removed needless platform detection
    https://github.com/ruby/psych/commit/57d704fd63

--- test/lib/helper.rb
+++ test/lib/helper.rb
@@ -2,13 +2,3 @@ require "test/unit"
 require_relative "core_assertions"
 
 Test::Unit::TestCase.include Test::Unit::CoreAssertions
-
-module Test
-  module Unit
-    class TestCase
-      def windows? platform = RUBY_PLATFORM
-        /mswin|mingw/ =~ platform
-      end
-    end
-  end
-end
