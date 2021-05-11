fae5863c04466b1a7c777491d44f8b8d8e38bad2
Takashi Kokubun <takashikkbn@gmail.com>
2021-05-11 00:27:12 -0700

    Revert "Revert "Skip tests not working in mswin GitHub Actions""
    This reverts commit e1f8ad7862c9c4be52dc6e1031a004621eb07e6e.

--- tool/test/webrick/test_filehandler.rb
+++ tool/test/webrick/test_filehandler.rb
@@ -247,6 +247,7 @@ class WEBrick::TestFileHandler < Test::Unit::TestCase
 
   def test_short_filename
     return if File.executable?(__FILE__) # skip on strange file system
+    return if /mswin/ =~ RUBY_PLATFORM && ENV.key?('GITHUB_ACTIONS') # not working from the beginning
 
     config = {
       :CGIInterpreter => TestWEBrick::RubyBin,
