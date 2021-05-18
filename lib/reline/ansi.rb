46c813969be642e61763379e03c9698b91ab1a96
aycabta <aycabta@gmail.com>
2021-05-18 21:46:16 +0900

    [ruby/reline] Add terminfo support
    https://github.com/ruby/reline/commit/74a7ffaa2f

--- lib/reline/ansi.rb
+++ lib/reline/ansi.rb
@@ -1,7 +1,12 @@
 require 'io/console'
 require 'timeout'
+require_relative 'terminfo'
 
 class Reline::ANSI
+  if Reline::Terminfo.enabled?
+    Reline::Terminfo.setupterm(0, 2)
+  end
+
   def self.encoding
     Encoding.default_external
   end
@@ -11,6 +16,51 @@ class Reline::ANSI
   end
 
   def self.set_default_key_bindings(config)
+    if Reline::Terminfo.enabled?
+      set_default_key_bindings_terminfo(config)
+    else
+      set_default_key_bindings_comprehensive_list(config)
+    end
+    {
+      # extended entries of terminfo
+      [27, 91, 49, 59, 53, 67] => :em_next_word, # Ctrl+→, extended entry
+      [27, 91, 49, 59, 53, 68] => :ed_prev_word, # Ctrl+←, extended entry
+    }.each_pair do |key, func|
+      config.add_default_key_binding_by_keymap(:emacs, key, func)
+      config.add_default_key_binding_by_keymap(:vi_insert, key, func)
+      config.add_default_key_binding_by_keymap(:vi_command, key, func)
+    end
+    {
+      # default bindings
+      [27, 32] => :em_set_mark,             # M-<space>
+      [24, 24] => :em_exchange_mark,        # C-x C-x TODO also add Windows
+    }.each_pair do |key, func|
+      config.add_default_key_binding_by_keymap(:emacs, key, func)
+    end
+  end
+
+  def self.set_default_key_bindings_terminfo(config)
+    {
+      Reline::Terminfo.tigetstr('khome').to_s.bytes => :ed_move_to_beg,
+      Reline::Terminfo.tigetstr('kend').to_s.bytes => :ed_move_to_end,
+      Reline::Terminfo.tigetstr('kcuu1').to_s.bytes => :ed_prev_history,
+      Reline::Terminfo.tigetstr('kcud1').to_s.bytes => :ed_next_history,
+      Reline::Terminfo.tigetstr('kcuf1').to_s.bytes => :ed_next_char,
+      Reline::Terminfo.tigetstr('kcub1').to_s.bytes => :ed_prev_char,
+      # Escape sequences that omit the move distance and are set to defaults
+      # value 1 may be sometimes sent by pressing the arrow-key.
+      Reline::Terminfo.tigetstr('cuu').to_s.sub(/%p1%d/, '').bytes => :ed_prev_history,
+      Reline::Terminfo.tigetstr('cud').to_s.sub(/%p1%d/, '').bytes => :ed_next_history,
+      Reline::Terminfo.tigetstr('cuf').to_s.sub(/%p1%d/, '').bytes => :ed_next_char,
+      Reline::Terminfo.tigetstr('cub').to_s.sub(/%p1%d/, '').bytes => :ed_prev_char,
+    }.each_pair do |key, func|
+      config.add_default_key_binding_by_keymap(:emacs, key, func)
+      config.add_default_key_binding_by_keymap(:vi_insert, key, func)
+      config.add_default_key_binding_by_keymap(:vi_command, key, func)
+    end
+  end
+
+  def self.set_default_key_bindings_comprehensive_list(config)
     {
       # Console (80x25)
       [27, 91, 49, 126] => :ed_move_to_beg, # Home
@@ -41,14 +91,10 @@ class Reline::ANSI
       # Arrow keys are the same of KDE
 
       # iTerm2
-      [27, 27, 91, 67] => :em_next_word,    # Option+→
-      [27, 27, 91, 68] => :ed_prev_word,    # Option+←
+      [27, 27, 91, 67] => :em_next_word,    # Option+→, extended entry
+      [27, 27, 91, 68] => :ed_prev_word,    # Option+←, extended entry
       [195, 166] => :em_next_word,          # Option+f
       [195, 162] => :ed_prev_word,          # Option+b
-
-      # others
-      [27, 91, 49, 59, 53, 67] => :em_next_word, # Ctrl+→
-      [27, 91, 49, 59, 53, 68] => :ed_prev_word, # Ctrl+←
 
       [27, 79, 65] => :ed_prev_history,     # ↑
       [27, 79, 66] => :ed_next_history,     # ↓
@@ -58,14 +104,6 @@ class Reline::ANSI
       config.add_default_key_binding_by_keymap(:emacs, key, func)
       config.add_default_key_binding_by_keymap(:vi_insert, key, func)
       config.add_default_key_binding_by_keymap(:vi_command, key, func)
-    end
-
-    {
-      # others
-      [27, 32] => :em_set_mark,             # M-<space>
-      [24, 24] => :em_exchange_mark,        # C-x C-x TODO also add Windows
-    }.each_pair do |key, func|
-      config.add_default_key_binding_by_keymap(:emacs, key, func)
     end
   end
 
