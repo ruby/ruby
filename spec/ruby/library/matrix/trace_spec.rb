835a4956081e43ae21a78667f2b87f275467b70e
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2021-05-26 20:36:23 +0900

    Guard ruby/spec with spec/mspec/tool/wrap_with_guard.rb

--- spec/ruby/library/matrix/trace_spec.rb
+++ spec/ruby/library/matrix/trace_spec.rb
@@ -1,7 +1,10 @@
 require_relative '../../spec_helper'
-require_relative 'shared/trace'
-require 'matrix'
 
-describe "Matrix#trace" do
-  it_behaves_like :trace, :trace
+ruby_version_is ""..."3.1" do
+  require_relative 'shared/trace'
+  require 'matrix'
+
+  describe "Matrix#trace" do
+    it_behaves_like :trace, :trace
+  end
 end
