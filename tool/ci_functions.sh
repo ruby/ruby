cda3b34899de988d09dd03797563da7fbd083771
Jun Aruga <jaruga@redhat.com>
2021-05-31 16:21:10 +0200

    tool/ci_functions.sh: Fix typos and improve the comment. [ci skip]

--- tool/ci_functions.sh
+++ tool/ci_functions.sh
@@ -7,7 +7,7 @@
 # See `ruby tool/test/runner.rb --help` `-n` option.
 function ci_to_excluded_test_opts {
   local tests_str="${1}"
-  # Use the backward matching `!/name$/`, as the perfect matching does not work.
+  # Use the backward matching `!/name$/`, as the perfect matching doesn't work.
   # https://bugs.ruby-lang.org/issues/16936
   ruby <<EOF
     opts = "${tests_str}".split.map { |test| "-n \!/#{test}\$$/" }
@@ -16,7 +16,7 @@ EOF
   return 0
 }
 
-# Create options with patterns `-n /name1/ -n /name2/ ..` to exclude the test
+# Create options with patterns `-n name1 -n name2 ..` to include the test
 # method names by the method names `name1 name2 ..`.
 # See `ruby tool/test/runner.rb --help` `-n` option.
 function ci_to_included_test_opts {
