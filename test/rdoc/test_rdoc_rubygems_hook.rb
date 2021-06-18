6aaa1c4d09249baae93d5bb7fba585be420c4fee
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2021-06-18 14:21:20 +0900

    [ruby/rdoc] Use pend instead of skip
    https://github.com/ruby/rdoc/commit/8460a36d84

--- test/rdoc/test_rdoc_rubygems_hook.rb
+++ test/rdoc/test_rdoc_rubygems_hook.rb
@@ -32,7 +32,7 @@ class TestRDocRubygemsHook < Test::Unit::TestCase
     begin
       RDoc::RubygemsHook.load_rdoc
     rescue Gem::DocumentError => e
-      skip e.message
+      pend e.message
     end
     @old_ui = Gem::DefaultUserInteraction.ui
     Gem::DefaultUserInteraction.ui = Gem::SilentUI.new
@@ -220,8 +220,8 @@ class TestRDocRubygemsHook < Test::Unit::TestCase
   end
 
   def test_remove_unwritable
-    skip 'chmod not supported' if Gem.win_platform?
-    skip "assumes that euid is not root" if Process.euid == 0
+    pend 'chmod not supported' if Gem.win_platform?
+    pend "assumes that euid is not root" if Process.euid == 0
 
     FileUtils.mkdir_p @a.base_dir
     FileUtils.chmod 0, @a.base_dir
@@ -250,8 +250,8 @@ class TestRDocRubygemsHook < Test::Unit::TestCase
   end
 
   def test_setup_unwritable
-    skip 'chmod not supported' if Gem.win_platform?
-    skip "assumes that euid is not root" if Process.euid == 0
+    pend 'chmod not supported' if Gem.win_platform?
+    pend "assumes that euid is not root" if Process.euid == 0
 
     FileUtils.mkdir_p @a.doc_dir
     FileUtils.chmod 0, @a.doc_dir
