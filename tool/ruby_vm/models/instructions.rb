cc9de5ac8eb13c69a47aa099c6c012e07f7573a1
Aaron Patterson <tenderlove@ruby-lang.org>
2021-06-21 15:16:13 -0700

    Remove some MicroJIT vestiges
    Just happened to run across this, so lets fix them

--- tool/ruby_vm/models/instructions.rb
+++ tool/ruby_vm/models/instructions.rb
@@ -13,12 +13,12 @@
 require_relative 'bare_instructions'
 require_relative 'operands_unifications'
 require_relative 'instructions_unifications'
-require_relative 'micro_jit'
+require_relative 'yjit'
 
 RubyVM::Instructions = RubyVM::BareInstructions.to_a + \
                        RubyVM::OperandsUnifications.to_a + \
                        RubyVM::InstructionsUnifications.to_a + \
-                       RubyVM::MicroJIT::ExampleInstructions.to_a
+                       RubyVM::YJIT::ExampleInstructions.to_a
 
 
 require_relative 'trace_instructions'
