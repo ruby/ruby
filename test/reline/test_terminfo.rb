8fc98295cb22cf179ba6805872e545921d080745
aycabta <aycabta@gmail.com>
2021-06-25 18:04:27 +0900

    [ruby/reline] Add Terminfo tests
    https://github.com/ruby/reline/commit/17721e477e

---
+++ test/reline/test_terminfo.rb
@@ -0,0 +1,24 @@
+require_relative 'helper'
+require "reline"
+
+class Reline::Terminfo::Test < Reline::TestCase
+  def setup
+    Reline::Terminfo.setupterm(0, 2)
+  end
+
+  def test_tigetstr
+    assert Reline::Terminfo.tigetstr('khome')
+  end
+
+  def test_tiparm
+    assert Reline::Terminfo.tigetstr('khome').tiparm
+  end
+
+  def test_tigetstr_with_param
+    assert Reline::Terminfo.tigetstr('cuu').include?('%p1%d')
+  end
+
+  def test_tiparm_with_param
+    assert Reline::Terminfo.tigetstr('cuu').tiparm(4649).include?('4649')
+  end
+end if Reline::Terminfo.enabled?
