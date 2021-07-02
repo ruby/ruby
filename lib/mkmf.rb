1ac228378cc0447220d229c1e1a7448e7b862b78
Nobuyoshi Nakada <nobu@ruby-lang.org>
2021-07-02 20:07:23 +0900

    Use $ignore_error defined in mkmf.rb

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -2497,7 +2497,7 @@ site-install-rb: install-rb
       mfile.print "$(ECHO) linking static-library $(@#{rsep})\n\t$(Q) "
       mfile.print "$(AR) #{config_string('ARFLAGS') || 'cru '}$@ $(OBJS)"
       config_string('RANLIB') do |ranlib|
-        mfile.print "\n\t-$(Q)#{ranlib} $(@) 2> /dev/null || true"
+        mfile.print "\n\t-$(Q)#{ranlib} $(@)#{$ignore_error}"
       end
     end
     mfile.print "\n\n"
