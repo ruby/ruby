b0ae4fdcfbe0004908adcc44f0ae0e3a3762917b
Kevin Deisz <kevin.deisz@gmail.com>
2021-07-06 14:45:36 -0400

    Implement opt_mult
    Basically the same thing as opt_mod, but for multiplying.

--- bootstraptest/test_yjit.rb
+++ bootstraptest/test_yjit.rb
@@ -26,6 +26,16 @@ assert_equal '2', %q{
   mod(7, 5)
 }
 
+# Test for opt_mult
+assert_equal '12', %q{
+  def mult(a, b)
+    a * b
+  end
+
+  mult(6, 2)
+  mult(6, 2)
+}
+
 # BOP redefined methods work when JIT compiled
 assert_equal 'false', %q{
   def less_than x
