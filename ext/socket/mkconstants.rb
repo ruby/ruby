86ad0ce93cc236dfb317540646fe666928738dba
David Carlier <devnexen@gmail.com>
2021-07-07 21:40:15 +0100

    openbsd sockets add SO_RTABLE constant

--- ext/socket/mkconstants.rb
+++ ext/socket/mkconstants.rb
@@ -662,6 +662,7 @@ SO_BUSY_POLL    nil     Set the threshold in microseconds for low latency pollin
 SO_MAX_PACING_RATE      nil     Cap the rate computed by transport layer. [bytes per second] (Linux 3.13)
 SO_BPF_EXTENSIONS       nil     Query supported BPF extensions (Linux 3.14)
 SO_SETFIB       nil     Set the associated routing table for the socket (FreeBSD)
+SO_RTABLE               nil     Set the routing table for this socket (OpenBSD)
 
 SOPRI_INTERACTIVE	nil	Interactive socket priority
 SOPRI_NORMAL	nil	Normal socket priority
