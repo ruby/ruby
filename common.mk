41f405c486a01c1a16c3f102c11d41c8fbbafe60
Aaron Patterson <tenderlove@ruby-lang.org>
2021-07-15 14:30:43 -0700

    Remove the scraper
    Now that we're using the jit function entry point, we don't need the
    scraper.  Thank you for your service, scraper. ❤️

--- common.mk
+++ common.mk
@@ -1111,8 +1111,6 @@ incs: $(INSNS) {$(VPATH)}node_name.inc {$(VPATH)}known_errors.inc \
 
 insns: $(INSNS)
 
-yjit_hooks.inc: vm.$(OBJEXT)
-
 id.h: $(tooldir)/generic_erb.rb $(srcdir)/template/id.h.tmpl $(srcdir)/defs/id.def
 	$(ECHO) generating $@
 	$(Q) $(BASERUBY) $(tooldir)/generic_erb.rb --output=$@ \
@@ -17106,7 +17104,6 @@ yjit_compile.$(OBJEXT): {$(VPATH)}vm_sync.h
 yjit_compile.$(OBJEXT): {$(VPATH)}yjit.h
 yjit_compile.$(OBJEXT): {$(VPATH)}yjit_asm.h
 yjit_compile.$(OBJEXT): {$(VPATH)}yjit_compile.c
-yjit_compile.$(OBJEXT): {$(VPATH)}yjit_hooks.inc
 yjit_compile.$(OBJEXT): {$(VPATH)}yjit_utils.h
 yjit_core.$(OBJEXT): $(CCAN_DIR)/check_type/check_type.h
 yjit_core.$(OBJEXT): $(CCAN_DIR)/container_of/container_of.h
@@ -17503,7 +17500,6 @@ yjit_iface.$(OBJEXT): {$(VPATH)}yjit.rbinc
 yjit_iface.$(OBJEXT): {$(VPATH)}yjit_asm.h
 yjit_iface.$(OBJEXT): {$(VPATH)}yjit_codegen.h
 yjit_iface.$(OBJEXT): {$(VPATH)}yjit_core.h
-yjit_iface.$(OBJEXT): {$(VPATH)}yjit_hooks.inc
 yjit_iface.$(OBJEXT): {$(VPATH)}yjit_iface.c
 yjit_iface.$(OBJEXT): {$(VPATH)}yjit_iface.h
 yjit_utils.$(OBJEXT): {$(VPATH)}yjit_asm.h
