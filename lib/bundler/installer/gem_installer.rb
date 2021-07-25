095b5bbcc168f324588d42d4ef233b446ed9f860
David Rodríguez <deivid.rodriguez@riseup.net>
2021-07-25 16:10:55 +0200

    [rubygems/rubygems] Lazily load shellwords
    https://github.com/rubygems/rubygems/commit/ef6388656a

--- lib/bundler/installer/gem_installer.rb
+++ lib/bundler/installer/gem_installer.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "shellwords"
-
 module Bundler
   class GemInstaller
     attr_reader :spec, :standalone, :worker, :force, :installer
@@ -47,6 +45,7 @@ module Bundler
     def spec_settings
       # Fetch the build settings, if there are any
       if settings = Bundler.settings["build.#{spec.name}"]
+        require "shellwords"
         Shellwords.shellsplit(settings)
       end
     end
