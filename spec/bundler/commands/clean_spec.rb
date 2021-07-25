9e7249da4e954088b5a3701a034e4ecd003cef43
David Rodríguez <deivid.rodriguez@riseup.net>
2021-07-25 16:22:28 +0200

    [rubygems/rubygems] This spec can pass now on ruby 3
    TSort was released as a library so we can install it, and also other
    gems that are loaded by the spec. Also, Ruby on Windows apparently loads
    fiddle 1.0.6, so we need to also install that to make that not fail.
    
    https://github.com/rubygems/rubygems/commit/2b8dcab99e

--- spec/bundler/commands/clean_spec.rb
+++ spec/bundler/commands/clean_spec.rb
@@ -626,7 +626,7 @@ RSpec.describe "bundle clean" do
   end
 
   it "when using --force, it doesn't remove default gem binaries" do
-    skip "does not work on ruby 3.0 because it changes the path to look for default gems, tsort is a default gem there, and we can't install it either like we do with fiddle because it doesn't yet exist" unless RUBY_VERSION < "3.0.0"
+    skip "does not work on old rubies because the realworld gems that need to be installed don't support them" if RUBY_VERSION < "2.7.0"
 
     skip "does not work on rubygems versions where `--install_dir` doesn't respect --default" unless Gem::Installer.for_spec(loaded_gemspec, :install_dir => "/foo").default_spec_file == "/foo/specifications/default/bundler-#{Bundler::VERSION}.gemspec" # Since rubygems 3.2.0.rc.2
 
@@ -638,7 +638,7 @@ RSpec.describe "bundle clean" do
       s.executables = "irb"
     end
 
-    realworld_system_gems "fiddle --version 1.0.0"
+    realworld_system_gems "fiddle --version 1.0.6", "tsort --version 0.1.0", "pathname --version 0.1.0", "set --version 1.0.1"
 
     install_gemfile <<-G
       source "#{file_uri_for(gem_repo2)}"
