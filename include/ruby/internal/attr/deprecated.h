1cdc75104de1caa048d6eace3422b36dd69e6d70
卜部昌平 <shyouhei@ruby-lang.org>
2021-08-12 09:40:38 +0900

    RBIMPL_ATTR_DEPRECATED: enable for GCC 10.3+
    They fixed the bug.

--- include/ruby/internal/attr/deprecated.h
+++ include/ruby/internal/attr/deprecated.h
@@ -35,7 +35,7 @@
 #elif RBIMPL_HAS_EXTENSION(attribute_deprecated_with_message)
 # define RBIMPL_ATTR_DEPRECATED(msg) __attribute__((__deprecated__ msg))
 
-#elif defined(__cplusplus) && RBIMPL_COMPILER_SINCE(GCC, 10, 1, 0) /* && RBIMPL_COMPILER_BEFORE(GCC, 10, X, Y) */
+#elif defined(__cplusplus) && RBIMPL_COMPILER_SINCE(GCC, 10, 1, 0) && RBIMPL_COMPILER_BEFORE(GCC, 10, 3, 0)
 # /* https://gcc.gnu.org/bugzilla/show_bug.cgi?id=95302 */
 # define RBIMPL_ATTR_DEPRECATED(msg) /* disable until they fix this bug */
 
