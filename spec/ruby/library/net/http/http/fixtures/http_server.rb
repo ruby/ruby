73085c8d8ee5456b6c157042b78d812f3d1ab054
Benoit Daloze <eregontp@gmail.com>
2021-08-13 18:09:14 +0200

    Update to ruby/spec@330c641

--- spec/ruby/library/net/http/http/fixtures/http_server.rb
+++ spec/ruby/library/net/http/http/fixtures/http_server.rb
@@ -1,5 +1,4 @@
-require 'webrick'
-require 'webrick/httpservlet/abstract'
+require 'socket'
 
 module NetHTTPSpecs
   class NullWriter
@@ -9,102 +8,117 @@ module NetHTTPSpecs
     def printf(*args) end
   end
 
-  class SpecServlet < WEBrick::HTTPServlet::AbstractServlet
-    def handle(req, res)
-      reply(req, res)
+  class SmallHTTPServer
+    def initialize(bind_address)
+      @server = TCPServer.new(bind_address, 0)
+      @running = Mutex.new
+      @thread = Thread.new {
+        Thread.current.abort_on_exception = true
+        listen
+      }
     end
 
-    %w{ do_GET do_HEAD do_POST do_PUT do_PROPPATCH do_LOCK do_UNLOCK
-        do_OPTIONS do_PROPFIND do_DELETE do_MOVE do_COPY
-        do_MKCOL do_TRACE }.each do |method|
-      alias_method method.to_sym, :handle
+    def port
+      @server.addr[1]
     end
-  end
 
-  class RequestServlet < SpecServlet
-    def reply(req, res)
-      res.content_type = "text/plain"
-      res.body = "Request type: #{req.request_method}"
-    end
-  end
+    def listen
+      loop do
+        begin
+          client = @server.accept
+        rescue IOError => e
+          if @running.locked? # close
+            break
+          else
+            raise e
+          end
+        end
 
-  class RequestBodyServlet < SpecServlet
-    def reply(req, res)
-      res.content_type = "text/plain"
-      res.body = req.body
-    end
-  end
-
-  class RequestHeaderServlet < SpecServlet
-    def reply(req, res)
-      res.content_type = "text/plain"
-      res.body = req.header.inspect
-    end
-  end
-
-  class RequestBasicAuthServlet < SpecServlet
-    def reply(req, res)
-      res.content_type = "text/plain"
-
-      WEBrick::HTTPAuth.basic_auth(req, res, "realm") do |user, pass|
-        res.body = "username: #{user}\npassword: #{pass}"
-        true
+        handle_client(client)
       end
+    end
+
+    def handle_client(client)
+      begin
+        until client.closed?
+          request = client.gets("\r\n\r\n")
+          break unless request
+          handle_request(client, request)
+        end
+      ensure
+        client.close
+      end
+    end
+
+    def parse_request(request)
+      request, *headers = request.chomp.lines.map { |line| line.chomp }
+      request_method, request_uri, _http_version = request.split
+      headers = headers.map { |line| line.split(': ', 2) }.to_h
+      [request_method, request_uri, headers]
+    end
+
+    def handle_request(client, request)
+      request_method, request_uri, headers = parse_request(request)
+
+      if headers.include? 'Content-Length'
+        request_body_size = Integer(headers['Content-Length'])
+        request_body = client.read(request_body_size)
+      end
+
+      case request_uri
+      when '/'
+        raise request_method unless request_method == 'GET'
+        reply(client, "This is the index page.", request_method)
+      when '/request'
+        reply(client, "Request type: #{request_method}", request_method)
+      when '/request/body'
+        reply(client, request_body, request_method)
+      when '/request/header'
+        reply(client, headers.inspect, request_method)
+      when '/request/basic_auth'
+        reply(client, "username: \npassword: ", request_method)
+      else
+        raise request_uri
+      end
+    end
+
+    def reply(client, body, request_method)
+      client.print "HTTP/1.1 200 OK\r\n"
+      if request_method == 'HEAD'
+        client.close
+      else
+        client.print "Content-Type: text/plain\r\n"
+        client.print "Content-Length: #{body.bytesize}\r\n"
+        client.print "\r\n"
+        client.print body
+      end
+    end
+
+    def close
+      @running.lock
+      @server.close
+      @thread.join
     end
   end
 
   @server = nil
-  @server_thread = nil
 
   class << self
     def port
       raise "server not started" unless @server
-      @server.config[:Port]
+      @server.port
     end
 
     def start_server
       bind_address = platform_is(:windows) ? "localhost" : "127.0.0.1"
-      server_config = {
-        BindAddress: bind_address,
-        Port: 0,
-        Logger: WEBrick::Log.new(NullWriter.new),
-        AccessLog: [],
-        ServerType: Thread
-      }
-
-      @server = WEBrick::HTTPServer.new(server_config)
-
-      @server.mount_proc('/') do |req, res|
-        res.content_type = "text/plain"
-        res.body = "This is the index page."
-      end
-      @server.mount('/request', RequestServlet)
-      @server.mount("/request/body", RequestBodyServlet)
-      @server.mount("/request/header", RequestHeaderServlet)
-      @server.mount("/request/basic_auth", RequestBasicAuthServlet)
-
-      @server_thread = @server.start
+      @server = SmallHTTPServer.new(bind_address)
     end
 
     def stop_server
       if @server
-        begin
-          @server.shutdown
-        rescue Errno::EPIPE, Errno::EBADF
-          # Because WEBrick is not thread-safe and only catches IOError
-
-          # EBADF can happen because WEBrick @server_thread concurrently closes the shutdown pipe
-          # once @status = :Shutdown, while the current thread does write_nonblock("\0").
-          # On MRI this EBADF is replaced by IOError due to the GIL around both #close and #write_nonblock.
-        end
+        @server.close
         @server = nil
       end
-      if @server_thread
-        @server_thread.join
-        @server_thread = nil
-      end
-      timeout = WEBrick::Utils::TimeoutHandler
-      timeout.terminate if timeout.respond_to?(:terminate)
     end
   end
 end
