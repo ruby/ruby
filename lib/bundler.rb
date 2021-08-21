d0da3a2a7f48fb912d7617f19d18f32cb5d7eb56
David Rodríguez <deivid.rodriguez@riseup.net>
2021-08-21 16:33:00 +0200

    [rubygems/rubygems] Remove more unused code
    https://github.com/rubygems/rubygems/commit/986e889a79

--- lib/bundler.rb
+++ lib/bundler.rb
@@ -662,16 +662,11 @@ EOF
     end
 
     def configure_gem_path(env = ENV)
-      blank_home = env["GEM_HOME"].nil? || env["GEM_HOME"].empty?
-      if !use_system_gems?
+      unless use_system_gems?
         # this needs to be empty string to cause
         # PathSupport.split_gem_path to only load up the
         # Bundler --path setting as the GEM_PATH.
         env["GEM_PATH"] = ""
-      elsif blank_home
-        possibles = [Bundler.rubygems.gem_dir, Bundler.rubygems.gem_path]
-        paths = possibles.flatten.compact.uniq.reject(&:empty?)
-        env["GEM_PATH"] = paths.join(File::PATH_SEPARATOR)
       end
     end
 
