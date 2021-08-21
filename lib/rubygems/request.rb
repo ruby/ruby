8e49b24078e963ab21b4b61cd0e51bf4eb2ee3fc
Daniel Niknam <mhmd.niknam@gmail.com>
2021-08-22 01:37:32 +1000

    [rubygems/rubygems] Using `Gem::PrintableUri` in `Gem::Request` class
    The `@uri` variable could be a source URI with a credential. Using `Gem::PrintableUri` to make sure we are redacting sensitive information from it when logging on verbose mode.
    
    https://github.com/rubygems/rubygems/commit/f566787211

--- lib/rubygems/request.rb
+++ lib/rubygems/request.rb
@@ -184,6 +184,7 @@ class Gem::Request
 
   def perform_request(request) # :nodoc:
     connection = connection_for @uri
+    uri = Gem::PrintableUri.parse_uri(@uri)
 
     retried = false
     bad_response = false
@@ -191,7 +192,7 @@ class Gem::Request
     begin
       @requests[connection.object_id] += 1
 
-      verbose "#{request.method} #{@uri}"
+      verbose "#{request.method} #{uri}"
 
       file_name = File.basename(@uri.path)
       # perform download progress reporter only for gems
