4a6b5f4622b5f33936fb1af7a8277335b7e15bbe
Yusuke Endoh <mame@ruby-lang.org>
2021-08-24 18:07:50 +0900

    Revert "tool/test-bundled-gems.rb: Use the bundled RBS code to test TypeProf"
    This reverts commit 22deda43cb98aa3cee48d0bebbff7c4db1d7652a.
    
    It was incomplete. Sorry!

--- common.mk
+++ common.mk
@@ -1372,7 +1372,7 @@ no-test-bundled-gems-prepare: no-test-bundled-gems-precheck
 yes-test-bundled-gems-prepare: yes-test-bundled-gems-precheck
 	$(ACTIONS_GROUP)
 	$(XRUBY) -C "$(srcdir)" bin/gem install --no-document \
-		--install-dir .bundle --conservative "bundler" "minitest:~> 5" "test-unit" "rake" "hoe" "yard" "pry" "packnga" "rexml" "json-schema"
+		--install-dir .bundle --conservative "bundler" "minitest:~> 5" "test-unit" "rake" "hoe" "yard" "pry" "packnga" "rexml" "json-schema" "rbs"
 	$(ACTIONS_ENDGROUP)
 
 PREPARE_BUNDLED_GEMS = test-bundled-gems-prepare
