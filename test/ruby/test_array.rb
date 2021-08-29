ruby/ruby

commit 4f2425549a870d8c42ff26812aa53ab93bba5bc8

rb_ary_slice_bang: do not goto into a branch
I'm not necessarily against every goto in general, but jumping into a
branch is definitely a bad idea.  Better refactor.

array.c
@@ -4407,6 +4407,37 @@ rb_ary_delete_at_m(VALUE ary, VALUE pos)
     return rb_ary_delete_at(ary, NUM2LONG(pos));
 }
 
+static VALUE
+ary_slice_bang_by_rb_ary_splice(VALUE ary, long pos, long len)
+{
+    const long orig_len = RARRAY_LEN(ary);
+
+    if (len < 0) {
+        return Qnil;
+    }
+    else if (pos < -orig_len) {
+        return Qnil;
+    }
+    else if (pos < 0) {
+        pos += orig_len;
+    }
+    else if (orig_len < pos) {
+        return Qnil;
+    }
+    else if (orig_len < pos + len) {
+        len = orig_len - pos;
+    }
+    if (len == 0) {
+        return rb_ary_new2(0);
+    }
+    else {
+        VALUE arg2 = rb_ary_new4(len, RARRAY_CONST_PTR_TRANSIENT(ary)+pos);
+        RBASIC_SET_CLASS(arg2, rb_obj_class(ary));
+        rb_ary_splice(ary, pos, len, 0, 0);
+        return arg2;
+    }
+}
+
 /*
  *  call-seq:
  *     ary.slice!(index)         -> obj or nil
@@ -4431,39 +4462,24 @@ rb_ary_delete_at_m(VALUE ary, VALUE pos)
 static VALUE
 rb_ary_slice_bang(int argc, VALUE *argv, VALUE ary)
 {
-    VALUE arg1, arg2;
-    long pos, len, orig_len;
+    VALUE arg1;
+    long pos, len;
 
     rb_ary_modify_check(ary);
+    rb_check_arity(argc, 1, 2);
+    arg1 = argv[0];
+
     if (argc == 2) {
 	pos = NUM2LONG(argv[0]);
 	len = NUM2LONG(argv[1]);
-      delete_pos_len:
-	if (len < 0) return Qnil;
-	orig_len = RARRAY_LEN(ary);
-	if (pos < 0) {
-	    pos += orig_len;
-	    if (pos < 0) return Qnil;
-	}
-	else if (orig_len < pos) return Qnil;
-	if (orig_len < pos + len) {
-	    len = orig_len - pos;
-	}
-	if (len == 0) return rb_ary_new2(0);
-        arg2 = rb_ary_new4(len, RARRAY_CONST_PTR_TRANSIENT(ary)+pos);
-	RBASIC_SET_CLASS(arg2, rb_obj_class(ary));
-	rb_ary_splice(ary, pos, len, 0, 0);
-	return arg2;
+        return ary_slice_bang_by_rb_ary_splice(ary, pos, len);
     }
-
-    rb_check_arity(argc, 1, 2);
-    arg1 = argv[0];
 
     if (!FIXNUM_P(arg1)) {
 	switch (rb_range_beg_len(arg1, &pos, &len, RARRAY_LEN(ary), 0)) {
 	  case Qtrue:
 	    /* valid range */
-	    goto delete_pos_len;
+            return ary_slice_bang_by_rb_ary_splice(ary, pos, len);
 	  case Qnil:
 	    /* invalid range */
 	    return Qnil;

commit 8899fa0b3d41fd27dd1a2c6f75106cb78ff27236

merge revision(s) d43279edacd09edf3a43e02d62f5be475e7c3bcb,5dc36ddcd00fc556c04c15ce9770c5a84d7d43dc,523bf31564f160f899f8cf9f73540d6a6f687f17: [Backport #18138]
	Fix length calculation for Array#slice!

	Commit 4f24255 introduced a bug which allows a length to be passed to
	rb_ary_new4 which is too large, resulting in invalid memory access.

	For example:

	    (1..1000).to_a.slice!(-2, 1000)
	---
	 array.c | 2 +-
	 1 file changed, 1 insertion(+), 1 deletion(-)

	Add out of range tests for Array#slice!

	---
	 test/ruby/test_array.rb | 13 +++++++++++++
	 1 file changed, 13 insertions(+)

	Add negative position tests [Bug #18138]

	---
	 test/ruby/test_array.rb | 4 ++++
	 1 file changed, 4 insertions(+)

array.c
@@ -4072,7 +4072,7 @@ ary_slice_bang_by_rb_ary_splice(VALUE ary, long pos, long len)
     else if (orig_len < pos) {
         return Qnil;
     }
-    else if (orig_len < pos + len) {
+    if (orig_len < pos + len) {
         len = orig_len - pos;
     }
     if (len == 0) {

version.h
@@ -12,11 +12,11 @@
 # define RUBY_VERSION_MINOR RUBY_API_VERSION_MINOR
 #define RUBY_VERSION_TEENY 3
 #define RUBY_RELEASE_DATE RUBY_RELEASE_YEAR_STR"-"RUBY_RELEASE_MONTH_STR"-"RUBY_RELEASE_DAY_STR
-#define RUBY_PATCHLEVEL 124
+#define RUBY_PATCHLEVEL 125
 
 #define RUBY_RELEASE_YEAR 2021
 #define RUBY_RELEASE_MONTH 8
-#define RUBY_RELEASE_DAY 19
+#define RUBY_RELEASE_DAY 29
 
 #include "ruby/version.h"
 

test/ruby/test_array.rb
@@ -1563,6 +1563,8 @@ class TestArray < Test::Unit::TestCase
 
     assert_nil(a.slice(10, -3))
     assert_equal @cls[], a.slice(10..7)
+
+    assert_equal([100], a.slice(-1, 1_000_000_000))
   end
 
   def test_slice!
@@ -1609,6 +1611,21 @@ class TestArray < Test::Unit::TestCase
 
     assert_raise(ArgumentError) { @cls[1].slice! }
     assert_raise(ArgumentError) { @cls[1].slice!(0, 0, 0) }
+  end
+
+  def test_slice_out_of_range!
+    a = @cls[*(1..100).to_a]
+
+    assert_nil(a.clone.slice!(-101..-1))
+    assert_nil(a.clone.slice!(-101..))
+
+    # assert_raise_with_message(RangeError, "((-101..-1).%(2)) out of range") { a.clone.slice!((-101..-1)%2) }
+    # assert_raise_with_message(RangeError, "((-101..).%(2)) out of range") { a.clone.slice!((-101..)%2) }
+
+    assert_nil(a.clone.slice!(10, -3))
+    assert_equal @cls[], a.clone.slice!(10..7)
+
+    assert_equal([100], a.clone.slice!(-1, 1_000_000_000))
   end
 
   def test_sort