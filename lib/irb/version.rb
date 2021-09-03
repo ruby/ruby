b7e2f7759de208b6d7b6670095c4079727164cce
aycabta <aycabta@gmail.com>
2021-09-04 02:18:40 +0900

    [ruby/irb] Version 1.3.8.pre.7
    https://github.com/ruby/irb/commit/02c4a8d3b6

--- lib/irb/version.rb
+++ lib/irb/version.rb
@@ -11,7 +11,7 @@
 #
 
 module IRB # :nodoc:
-  VERSION = "1.3.8.pre.6"
+  VERSION = "1.3.8.pre.7"
   @RELEASE_VERSION = VERSION
-  @LAST_UPDATE_DATE = "2021-09-02"
+  @LAST_UPDATE_DATE = "2021-09-04"
 end
