4b58d698b14752f4dfd405637df384c758cae396
Alan Wu <XrXr@users.noreply.github.com>
2021-09-08 12:01:39 -0400

    Count interpreter instructions when -DYJIT_STATS=1
    The interpreter instruction count was enabled based on RUBY_DEBUG as
    opposed to YJIT_STATS. In builds with YJIT_STATS=1 but RUBY_DEBUG=0,
    the count was not available.
    
    Move YJIT_STATS in yjit.h where declarations are expoed to code outside
    of YJIT. Also reduce the changes made to the interpreter for calling
    into YJIT's instruction counting function.

--- vm_insnhelper.h
+++ vm_insnhelper.h
@@ -25,9 +25,9 @@ MJIT_SYMBOL_EXPORT_END
 #define COLLECT_USAGE_OPERAND(insn, n, op) vm_collect_usage_operand((insn), (n), ((VALUE)(op)))
 
 #define COLLECT_USAGE_REGISTER(reg, s)     vm_collect_usage_register((reg), (s))
-#elif RUBY_DEBUG
+#elif YJIT_STATS
 /* for --yjit-stats */
-#define COLLECT_USAGE_INSN(insn)           vm_yjit_collect_usage_insn(insn)
+#define COLLECT_USAGE_INSN(insn)           rb_yjit_collect_vm_usage_insn(insn)
 #define COLLECT_USAGE_OPERAND(insn, n, op)	/* none */
 #define COLLECT_USAGE_REGISTER(reg, s)		/* none */
 #else
