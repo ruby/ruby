44214e8ad9e8ba1adf704c8231b4ecced77d3b7a
John Hawthorn <john@hawthorn.email>
2021-09-08 18:47:09 -0700

    Implement getspecial

--- test/ruby/test_yjit.rb
+++ test/ruby/test_yjit.rb
@@ -205,6 +205,15 @@ class TestYJIT < Test::Unit::TestCase
     RUBY
   end
 
+  def test_getspecial_backref
+    assert_compiles("'foo' =~ /(o)./; $&", insns: %i[getspecial], result: "oo")
+    assert_compiles("'foo' =~ /(o)./; $`", insns: %i[getspecial], result: "f")
+    assert_compiles("'foo' =~ /(o)./; $'", insns: %i[getspecial], result: "")
+    assert_compiles("'foo' =~ /(o)./; $+", insns: %i[getspecial], result: "o")
+    assert_compiles("'foo' =~ /(o)./; $1", insns: %i[getspecial], result: "o")
+    assert_compiles("'foo' =~ /(o)./; $2", insns: %i[getspecial], result: nil)
+  end
+
   def test_compile_opt_getinlinecache
     assert_compiles(<<~RUBY, insns: %i[opt_getinlinecache], result: 123, min_calls: 2)
       def get_foo
