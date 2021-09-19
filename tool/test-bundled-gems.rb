c114b8e4a7b82999dcb627c74111f9e0f68ade1b
Soutaro Matsumoto <matsumoto@soutaro.com>
2021-09-20 03:13:05 +0900

    Bundle RBS 1.7.0

--- tool/test-bundled-gems.rb
+++ tool/test-bundled-gems.rb
@@ -27,7 +27,7 @@ File.foreach("#{gem_dir}/bundled_gems") do |line|
 
   if gem == "rbs"
     racc = File.realpath("../../libexec/racc", __FILE__)
-    pid = Process.spawn("#{ruby} -C #{gem_dir}/src/#{gem} -Ilib #{racc} -v -o lib/rbs/parser.rb lib/rbs/parser.y")
+    pid = Process.spawn("#{ruby} -C #{gem_dir}/src/#{gem} -Ilib #{racc} compile")
     Process.waitpid(pid)
     test_command << " stdlib_test validate"
 
