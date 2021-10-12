8d80447f5b0616a508d40b2f46595756b404fa0a
xtkoba <69125751+xtkoba@users.noreply.github.com>
2021-10-12 10:56:05 -0700

    Fix segmentation fault where DW_AT_ranges can refer to the wrong section
    Fixes [Bug #18081]

--- addr2line.c
+++ addr2line.c
@@ -844,6 +844,7 @@ typedef struct {
     const char *pend;
     const char *q0;
     const char *q;
+    uint16_t version;
     int format; // 4 or 8
     uint8_t address_size;
     int level;
@@ -1444,7 +1445,10 @@ ranges_include(DebugInfoReader *reader, ranges_t *ptr, uint64_t addr)
         const char *p;
         uint64_t base = ptr->low_pc_set ? ptr->low_pc : reader->current_low_pc;
         bool base_valid = true;
-        if (reader->obj->debug_rnglists.ptr) {
+        if (reader->version >= 5) {
+            if (!reader->obj->debug_rnglists.ptr) {
+                return false;
+            }
             p = reader->obj->debug_rnglists.ptr + ptr->ranges;
             for (;;) {
                 uint8_t rle = read_uint8(&p);
@@ -1485,6 +1489,10 @@ ranges_include(DebugInfoReader *reader, ranges_t *ptr, uint64_t addr)
                     return from;
                 }
             }
+            return false;
+        }
+        /* assert(reader->version < 5); */
+        if (!reader->obj->debug_ranges.ptr) {
             return false;
         }
         p = reader->obj->debug_ranges.ptr + ptr->ranges;
@@ -1544,7 +1552,6 @@ static int
 di_read_cu(DebugInfoReader *reader)
 {
     uint64_t unit_length;
-    uint16_t version;
     uint64_t debug_abbrev_offset;
     reader->format = 4;
     reader->current_cu = reader->p;
@@ -1554,11 +1561,11 @@ di_read_cu(DebugInfoReader *reader)
         reader->format = 8;
     }
     reader->cu_end = reader->p + unit_length;
-    version = read_uint16(&reader->p);
-    if (version > 5) {
+    reader->version = read_uint16(&reader->p);
+    if (reader->version > 5) {
         return -1;
     }
-    else if (version == 5) {
+    else if (reader->version == 5) {
         /* unit_type = */ read_uint8(&reader->p);
         reader->address_size = read_uint8(&reader->p);
         debug_abbrev_offset = read_uint(reader);
