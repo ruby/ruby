3f0150f2fd408421c4625976d0c88df27b6753d3
Nobuyoshi Nakada <nobu@ruby-lang.org>
2021-10-13 22:19:25 +0900

    [rubygems/rubygems] Keep loaded features
    Now `$LOADED_FEATURES` list is being maintained by `setup` and
    `teardown` and, only libaries under the temporary directory will
    be removed.  As `save_loaded_features` removes the rest libraries
    other than this test directory, ordinary libraries loaded from
    files under rubygems also removed, and often causes constant
    redefinition warnings.
    
    https://github.com/rubygems/rubygems/commit/9e1f92aafd

--- test/rubygems/helper.rb
+++ test/rubygems/helper.rb
@@ -826,13 +826,7 @@ class Gem::TestCase < Test::Unit::TestCase
   end
 
   def save_loaded_features
-    old_loaded_features = $LOADED_FEATURES.dup
     yield
-  ensure
-    prefix = File.dirname(__FILE__) + "/"
-    new_features = ($LOADED_FEATURES - old_loaded_features)
-    old_loaded_features.concat(new_features.select {|f| f.rindex(prefix, 0) })
-    $LOADED_FEATURES.replace old_loaded_features
   end
 
   def new_default_spec(name, version, deps = nil, *files)
