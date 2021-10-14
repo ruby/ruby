13772caee21345f688a65ca1fdef27c34f055b80
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2021-10-14 13:23:45 +0900

    Move pure ruby files under the ext/gemname/lib directory.

--- tool/sync_default_gems.rb
+++ tool/sync_default_gems.rb
@@ -318,8 +318,9 @@ def sync_default_gems(gem)
   when "digest"
     rm_rf(%w[ext/digest test/digest])
     cp_r("#{upstream}/ext/digest", "ext")
-    mkdir_p("ext/digest/lib")
+    mkdir_p("ext/digest/lib/digest")
     cp_r("#{upstream}/lib/digest.rb", "ext/digest/lib/")
+    cp_r("#{upstream}/lib/digest/sha2.rb", "ext/digest/lib/digest")
     cp_r("#{upstream}/test/digest", "test")
     cp_r("#{upstream}/digest.gemspec", "ext/digest")
     `git checkout ext/digest/depend ext/digest/*/depend`
