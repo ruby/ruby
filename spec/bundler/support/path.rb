b5949ad62a1bd2f3caee79ad2b4dbbb4d37d1092
David Rodriguez <deivid.rodriguez@riseup.net>
2021-10-18 18:53:09 +0200

    [rubygems/rubygems] Simplify check for ruby-core setup
    https://github.com/rubygems/rubygems/commit/77bc6f1ecc

--- spec/bundler/support/path.rb
+++ spec/bundler/support/path.rb
@@ -258,14 +258,7 @@ module Spec
     end
 
     def ruby_core?
-      # avoid to warnings
-      @ruby_core ||= nil
-
-      if @ruby_core.nil?
-        @ruby_core = true & ENV["GEM_COMMAND"]
-      else
-        @ruby_core
-      end
+      !ENV["GEM_COMMAND"].nil?
     end
 
     def git_root
