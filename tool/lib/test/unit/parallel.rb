8a49c7e4812a3f6d027dac1a574f47f125886935
Koichi Sasada <ko1@atdot.net>
2021-10-26 11:55:42 +0900

    Revert "introduce check code for mysterious EBADF"
    This reverts commit 7864efa105921eb3900c843126f2e0db02b9c6ae.

--- tool/lib/test/unit/parallel.rb
+++ tool/lib/test/unit/parallel.rb
@@ -85,21 +85,8 @@ module Test
         Test::Unit::Runner.output = orig_stdout
         $stdin = orig_stdin if orig_stdin
         $stdout = orig_stdout if orig_stdout
-
-        # To figure out which suite raises EBADF error.
-        begin
-          o.close if o && !o.closed?
-        rescue Exception => e
-          STDERR.puts "#{e} at #{suite.name} (o)"
-          raise
-        end
-
-        begin
-          i.close if i && !i.closed?
-        rescue Exception => e
-          STDERR.puts "#{e} at #{suite.name} (i)"
-          raise
-        end
+        o.close if o && !o.closed?
+        i.close if i && !i.closed?
       end
 
       def run(args = []) # :nodoc:
