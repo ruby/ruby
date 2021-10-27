020d77f7887e61c3f5d046fa8108f3c6294e677e
Jeremy Evans <code@jeremyevans.net>
2021-10-27 15:36:00 -0700

    Add {Method,UnboundMethod}#{public?,private?,protected?}
    These methods allow for checking whether the method has that
    visibility.
    
    Implements [Feature #11689]

--- test/ruby/test_method.rb
+++ test/ruby/test_method.rb
@@ -1181,6 +1181,25 @@ class TestMethod < Test::Unit::TestCase
     assert_nil(super_method)
   end
 
+  def test_method_visibility_predicates
+    v = Visibility.new
+    assert_equal(true, v.method(:mv1).public?)
+    assert_equal(true, v.method(:mv2).private?)
+    assert_equal(true, v.method(:mv3).protected?)
+    assert_equal(false, v.method(:mv2).public?)
+    assert_equal(false, v.method(:mv3).private?)
+    assert_equal(false, v.method(:mv1).protected?)
+  end
+
+  def test_unbound_method_visibility_predicates
+    assert_equal(true, Visibility.instance_method(:mv1).public?)
+    assert_equal(true, Visibility.instance_method(:mv2).private?)
+    assert_equal(true, Visibility.instance_method(:mv3).protected?)
+    assert_equal(false, Visibility.instance_method(:mv2).public?)
+    assert_equal(false, Visibility.instance_method(:mv3).private?)
+    assert_equal(false, Visibility.instance_method(:mv1).protected?)
+  end
+
   def rest_parameter(*rest)
     rest
   end
