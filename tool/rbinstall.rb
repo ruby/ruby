a2770377b4ae105189e5f333ef4a6f9608195832
Nobuyoshi Nakada <nobu@ruby-lang.org>
2021-10-30 00:40:21 +0900

    Get rid of exponential backtracks found by CodeQL
    Since these regexps are used at build/installation, they are not
    vulnerabilities.

--- tool/rbinstall.rb
+++ tool/rbinstall.rb
@@ -504,7 +504,7 @@ $script_installer = Class.new(installer) do
   if trans = CONFIG["program_transform_name"]
     exp = []
     trans.gsub!(/\$\$/, '$')
-    trans.scan(%r[\G[\s;]*(/(?:\\.|[^/])*/)?([sy])(\\?\W)((?:(?!\3)(?:\\.|.))*)\3((?:(?!\3)(?:\\.|.))*)\3([gi]*)]) do
+    trans.scan(%r[\G[\s;]*(/(?:\\.|[^/])*+/)?([sy])(\\?\W)((?:(?!\3)(?:\\.|.))*+)\3((?:(?!\3)(?:\\.|.))*+)\3([gi]*)]) do
       |addr, cmd, sep, pat, rep, opt|
       addr &&= Regexp.new(addr[/\A\/(.*)\/\z/, 1])
       case cmd
