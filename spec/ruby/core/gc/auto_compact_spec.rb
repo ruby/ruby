800dad62970989c0afeb26f1780c53683335a8d9
Benoit Daloze <eregontp@gmail.com>
2021-10-29 21:54:17 +0200

    Skip GC.auto_compact= spec for platforms not supporting it
    * See https://github.com/ruby/spec/pull/891

--- spec/ruby/core/gc/auto_compact_spec.rb
+++ spec/ruby/core/gc/auto_compact_spec.rb
@@ -2,18 +2,19 @@ require_relative '../../spec_helper'
 
 ruby_version_is "3.0" do
   describe "GC.auto_compact" do
-    before :each do
-      @default = GC.auto_compact
-    end
-
-    after :each do
-      GC.auto_compact = @default
-    end
-
     it "can set and get a boolean value" do
       original = GC.auto_compact
-      GC.auto_compact = !original
-      GC.auto_compact.should == !original
+      begin
+        GC.auto_compact = !original
+      rescue NotImplementedError # platform does not support autocompact
+        skip
+      end
+
+      begin
+        GC.auto_compact.should == !original
+      ensure
+        GC.auto_compact = original
+      end
     end
   end
 end
