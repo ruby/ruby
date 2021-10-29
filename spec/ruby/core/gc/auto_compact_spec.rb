a954f273a89f517de188bdd77e73a80a9c9dad2c
Benoit Daloze <eregontp@gmail.com>
2021-10-29 21:59:35 +0200

    Cleanup GC.auto_compact spec
    * Make the supported check more obvious.

--- spec/ruby/core/gc/auto_compact_spec.rb
+++ spec/ruby/core/gc/auto_compact_spec.rb
@@ -3,14 +3,15 @@ require_relative '../../spec_helper'
 ruby_version_is "3.0" do
   describe "GC.auto_compact" do
     it "can set and get a boolean value" do
-      original = GC.auto_compact
       begin
-        GC.auto_compact = !original
+        GC.auto_compact = GC.auto_compact
       rescue NotImplementedError # platform does not support autocompact
         skip
       end
 
+      original = GC.auto_compact
       begin
+        GC.auto_compact = !original
         GC.auto_compact.should == !original
       ensure
         GC.auto_compact = original
