666a9185953e84e861688ed5ea6f2ac71c4ace32
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2021-11-06 07:22:35 +0900

    [rubygems/rubygems] GNU Make needs DESTDIR on Windows environment like msys or mingw
    https://github.com/rubygems/rubygems/commit/7fd987d30d

--- lib/rubygems/ext/builder.rb
+++ lib/rubygems/ext/builder.rb
@@ -24,14 +24,14 @@ class Gem::Ext::Builder
 
     # try to find make program from Ruby configure arguments first
     RbConfig::CONFIG['configure_args'] =~ /with-make-prog\=(\w+)/
-    make_program = ENV['MAKE'] || ENV['make'] || $1
-    unless make_program
-      make_program = (/mswin/ =~ RUBY_PLATFORM) ? 'nmake' : 'make'
+    make_program_name = ENV['MAKE'] || ENV['make'] || $1
+    unless make_program_name
+      make_program_name = (/mswin/ =~ RUBY_PLATFORM) ? 'nmake' : 'make'
     end
-    make_program = Shellwords.split(make_program)
+    make_program = Shellwords.split(make_program_name)
 
     # The installation of the bundled gems is failed when DESTDIR is empty in mswin platform.
-    destdir = (ENV['DESTDIR'] && ENV['DESTDIR'] != "") ? 'DESTDIR=%s' % ENV['DESTDIR'] : ''
+    destdir = (/\bnmake/i !~ make_program_name || ENV['DESTDIR'] && ENV['DESTDIR'] != "") ? 'DESTDIR=%s' % ENV['DESTDIR'] : ''
 
     ['clean', '', 'install'].each do |target|
       # Pass DESTDIR via command line to override what's in MAKEFLAGS
