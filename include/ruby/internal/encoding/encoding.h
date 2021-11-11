a4a6e5cd2e69049c9e9296e597a429f849fbe749
卜部昌平 <shyouhei@ruby-lang.org>
2021-11-11 11:18:44 +0900

    ENCODING_MASK: fix doxygen link [ci skip]

--- include/ruby/internal/encoding/encoding.h
+++ include/ruby/internal/encoding/encoding.h
@@ -65,7 +65,7 @@ enum ruby_encoding_consts {
 
 #define ENCODING_INLINE_MAX RUBY_ENCODING_INLINE_MAX /**< @old{RUBY_ENCODING_INLINE_MAX} */
 #define ENCODING_SHIFT RUBY_ENCODING_SHIFT           /**< @old{RUBY_ENCODING_SHIFT} */
-#define ENCODING_MASK RUBY_ENCODING_MASK             /**< @old{RUBY_ENCODING_SHIFT} */
+#define ENCODING_MASK RUBY_ENCODING_MASK             /**< @old{RUBY_ENCODING_MASK} */
 
 /**
  * Destructively assigns the passed encoding  to the passed object.  The object
