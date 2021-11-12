489c8cebf575741d62effd0d212f1319beff3c40
Yusuke Endoh <mame@ruby-lang.org>
2021-11-12 12:15:25 +0900

    [ruby/date] Add length limit option for methods that parses date strings
    `Date.parse` now raises an ArgumentError when a given date string is
    longer than 128. You can configure the limit by giving `limit` keyword
    arguments like `Date.parse(str, limit: 1000)`. If you pass `limit: nil`,
    the limit is disabled.
    
    Not only `Date.parse` but also the following methods are changed.
    
    * Date._parse
    * Date.parse
    * DateTime.parse
    * Date._iso8601
    * Date.iso8601
    * DateTime.iso8601
    * Date._rfc3339
    * Date.rfc3339
    * DateTime.rfc3339
    * Date._xmlschema
    * Date.xmlschema
    * DateTime.xmlschema
    * Date._rfc2822
    * Date.rfc2822
    * DateTime.rfc2822
    * Date._rfc822
    * Date.rfc822
    * DateTime.rfc822
    * Date._jisx0301
    * Date.jisx0301
    * DateTime.jisx0301
    
    https://github.com/ruby/date/commit/3959accef8

--- test/date/test_date_parse.rb
+++ test/date/test_date_parse.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require 'test/unit'
 require 'date'
+require 'timeout'
 
 class TestDateParse < Test::Unit::TestCase
 
@@ -1228,4 +1229,32 @@ class TestDateParse < Test::Unit::TestCase
     assert_equal(s0, s)
   end
 
+  def test_length_limit
+    assert_raise(ArgumentError) { Date._parse("1" * 1000) }
+    assert_raise(ArgumentError) { Date._iso8601("1" * 1000) }
+    assert_raise(ArgumentError) { Date._rfc3339("1" * 1000) }
+    assert_raise(ArgumentError) { Date._xmlschema("1" * 1000) }
+    assert_raise(ArgumentError) { Date._rfc2822("1" * 1000) }
+    assert_raise(ArgumentError) { Date._rfc822("1" * 1000) }
+    assert_raise(ArgumentError) { Date._jisx0301("1" * 1000) }
+
+    assert_raise(ArgumentError) { Date.parse("1" * 1000) }
+    assert_raise(ArgumentError) { Date.iso8601("1" * 1000) }
+    assert_raise(ArgumentError) { Date.rfc3339("1" * 1000) }
+    assert_raise(ArgumentError) { Date.xmlschema("1" * 1000) }
+    assert_raise(ArgumentError) { Date.rfc2822("1" * 1000) }
+    assert_raise(ArgumentError) { Date.rfc822("1" * 1000) }
+    assert_raise(ArgumentError) { Date.jisx0301("1" * 1000) }
+
+    assert_raise(ArgumentError) { DateTime.parse("1" * 1000) }
+    assert_raise(ArgumentError) { DateTime.iso8601("1" * 1000) }
+    assert_raise(ArgumentError) { DateTime.rfc3339("1" * 1000) }
+    assert_raise(ArgumentError) { DateTime.xmlschema("1" * 1000) }
+    assert_raise(ArgumentError) { DateTime.rfc2822("1" * 1000) }
+    assert_raise(ArgumentError) { DateTime.rfc822("1" * 1000) }
+    assert_raise(ArgumentError) { DateTime.jisx0301("1" * 1000) }
+
+    assert_raise(ArgumentError) { Date._parse("Jan " + "9" * 1000000) }
+    assert_raise(Timeout::Error) { Timeout.timeout(1) { Date._parse("Jan " + "9" * 1000000, limit: nil) } }
+  end
 end
