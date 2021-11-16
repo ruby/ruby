9a76072873a0698038a87961b2fed31ac1485c64
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2021-11-16 19:51:06 +0900

    Added LICENSE entry of vendored molinillo to LEGAL

--- tool/sync_default_gems.rb
+++ tool/sync_default_gems.rb
@@ -105,6 +105,8 @@ def sync_default_gems(gem)
     cp_r(Dir.glob("#{upstream}/bundler/tool/bundler/standard_gems*"), "tool/bundler")
     rm_rf(%w[spec/bundler/support/artifice/vcr_cassettes])
     rm_rf("lib/bundler/vendor/thor/LICENSE.md")
+    rm_rf("lib/rubygems/resolver/molinillo/LICENSE")
+    rm_rf("lib/bundler/vendor/molinillo/LICENSE")
   when "rdoc"
     rm_rf(%w[lib/rdoc lib/rdoc.rb test/rdoc libexec/rdoc libexec/ri])
     cp_r(Dir.glob("#{upstream}/lib/rdoc*"), "lib")
