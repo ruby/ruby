d0a7312dfb0962978aa2fb11b1d73eda62dfd163
BurdetteLamar <BurdetteLamar@Yahoo.com>
2021-11-17 16:41:05 -0600

    Enhanced RDoc for Integer

--- numeric.c
+++ numeric.c
@@ -4777,12 +4777,20 @@ int_xor(VALUE x, VALUE y)
 }
 
 /*
- * Document-method: Integer#<<
- * call-seq:
- *   int << count  ->  integer
+ *  call-seq:
+ *    int << count -> integer
  *
- * Returns +int+ shifted left +count+ positions, or right if +count+
- * is negative.
+ *  Returns +self+ with bits shifted +count+ positions to the left,
+ *  or to the right if +count+ is negative:
+ *
+ *    n = 0b11110000
+ *    "%08b" % (n << 1)  # => "111100000"
+ *    "%08b" % (n << 3)  # => "11110000000"
+ *    "%08b" % (n << -1) # => "01111000"
+ *    "%08b" % (n << -3) # => "00011110"
+ *
+ *  Related: Integer#>>.
+ *
  */
 
 static VALUE
@@ -4821,6 +4829,23 @@ rb_int_lshift(VALUE x, VALUE y)
     }
     return Qnil;
 }
+
+/*
+ *  call-seq:
+ *    int >> count -> integer
+ *
+ *  Returns +self+ with bits shifted +count+ positions to the right,
+ *  or to the left if +count+ is negative:
+ *
+ *    n = 0b11110000
+ *    "%08b" % (n >> 1)  # => "01111000"
+ *    "%08b" % (n >> 3)  # => "00011110"
+ *    "%08b" % (n >> -1) # => "111100000"
+ *    "%08b" % (n >> -3) # => "11110000000"
+ *
+ *  Related: Integer#<<.
+ *
+ */
 
 /*
  * Document-method: Integer#>>
@@ -5031,12 +5056,20 @@ int_aref(int const argc, VALUE * const argv, VALUE const num)
 }
 
 /*
- *  Document-method: Integer#to_f
  *  call-seq:
- *     int.to_f  ->  float
+ *    to_f -> float
  *
- *  Converts +int+ to a Float.  If +int+ doesn't fit in a Float,
- *  the result is infinity.
+ *  Converts +self+ to a Float:
+ *
+ *    1.to_f  # => 1.0
+ *    -1.to_f # => -1.0
+ *
+ *  If the value of +self+ does not fit in a \Float,
+ *  the result is infinity:
+ *
+ *    (10**400).to_f  # => Infinity
+ *    (-10**400).to_f # => -Infinity
+ *
  */
 
 static VALUE
