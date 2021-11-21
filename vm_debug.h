cbfd5249d4475451731c4b1a54034ddd61d480e9
Nobuyoshi Nakada <nobu@ruby-lang.org>
2021-11-21 11:58:15 +0900

    Stop including node.h from vm_debug.h only for `NODE`

--- vm_debug.h
+++ vm_debug.h
@@ -12,7 +12,6 @@
 **********************************************************************/
 
 #include "ruby/ruby.h"
-#include "node.h"
 
 RUBY_SYMBOL_EXPORT_BEGIN
 
@@ -21,9 +20,11 @@ RUBY_SYMBOL_EXPORT_BEGIN
 #define dpi(i)   ruby_debug_print_id(-1, 0, "", (i))
 #define dpn(n)   ruby_debug_print_node(-1, 0, "", (n))
 
+struct RNode;
+
 VALUE ruby_debug_print_value(int level, int debug_level, const char *header, VALUE v);
 ID    ruby_debug_print_id(int level, int debug_level, const char *header, ID id);
-NODE *ruby_debug_print_node(int level, int debug_level, const char *header, const NODE *node);
+struct RNode *ruby_debug_print_node(int level, int debug_level, const char *header, const struct RNode *node);
 int   ruby_debug_print_indent(int level, int debug_level, int indent_level);
 void  ruby_debug_gc_check_func(void);
 void ruby_set_debug_option(const char *str);
