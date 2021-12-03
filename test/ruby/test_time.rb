e4c7c5468e01125cf10b06e7db1006b64bf1cd7e
Nobuyoshi Nakada <nobu@ruby-lang.org>
2021-12-03 18:35:35 +0900

    Add tests of string argument to Time.at

--- test/ruby/test_time.rb
+++ test/ruby/test_time.rb
@@ -37,6 +37,8 @@ class TestTime < Test::Unit::TestCase
   end
 
   def test_new
+    assert_equal(Time.new(2000,1,1,0,0,0), Time.new(2000))
+    assert_equal(Time.new(2000,2,1,0,0,0), Time.new("2000", "Feb"))
     assert_equal(Time.utc(2000,2,10), Time.new(2000,2,10, 11,0,0, 3600*11))
     assert_equal(Time.utc(2000,2,10), Time.new(2000,2,9, 13,0,0, -3600*11))
     assert_equal(Time.utc(2000,2,29,23,0,0), Time.new(2000, 3, 1, 0, 0, 0, 3600))
