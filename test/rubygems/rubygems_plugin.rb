1e5597532ac72ae324550c7cf58f94017f0c8072
David Rodríguez <deivid.rodriguez@riseup.net>
2021-12-04 12:49:00 +0100

    [rubygems/rubygems] Cleaup unnecessary code
    https://github.com/rubygems/rubygems/commit/2e05dadbc5

--- test/rubygems/rubygems_plugin.rb
+++ test/rubygems/rubygems_plugin.rb
@@ -1,15 +1,6 @@
 # frozen_string_literal: true
 require "rubygems/command_manager"
 
-##
-# This is an example of exactly what NOT to do.
-#
-# DO NOT include code like this in your rubygems_plugin.rb
-
-module Gem::Commands
-  remove_const(:InterruptCommand) if defined?(InterruptCommand)
-end
-
 class Gem::Commands::InterruptCommand < Gem::Command
   def initialize
     super("interrupt", "Raises an Interrupt Exception", {})
