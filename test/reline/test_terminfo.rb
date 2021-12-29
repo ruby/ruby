239e71c0af79e0c4b91cc21e7dff44a6c282d7b1
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2021-12-29 19:29:30 +0900

    [ruby/reline] Use omit instead of skip
    https://github.com/ruby/reline/commit/a538de421f

--- test/reline/test_terminfo.rb
+++ test/reline/test_terminfo.rb
@@ -9,24 +9,24 @@ class Reline::Terminfo::Test < Reline::TestCase
   def test_tigetstr
     assert Reline::Terminfo.tigetstr('khome')
   rescue Reline::Terminfo::TerminfoError => e
-    skip e.message
+    omit e.message
   end
 
   def test_tiparm
     assert Reline::Terminfo.tigetstr('khome').tiparm
   rescue Reline::Terminfo::TerminfoError => e
-    skip e.message
+    omit e.message
   end
 
   def test_tigetstr_with_param
     assert Reline::Terminfo.tigetstr('cuu').include?('%p1%d')
   rescue Reline::Terminfo::TerminfoError => e
-    skip e.message
+    omit e.message
   end
 
   def test_tiparm_with_param
     assert Reline::Terminfo.tigetstr('cuu').tiparm(4649).include?('4649')
   rescue Reline::Terminfo::TerminfoError => e
-    skip e.message
+    omit e.message
   end
 end if Reline::Terminfo.enabled?
