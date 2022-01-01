7deee741ab2aeb74304f2e09be7783c0600d1afe
aycabta <aycabta@gmail.com>
2022-01-01 23:12:45 +0900

    [ruby/reline] Remove unnecessary qualifiers on definition of comments
    https://github.com/ruby/reline/commit/abc1e4ee88

--- lib/reline/terminfo.rb
+++ lib/reline/terminfo.rb
@@ -75,11 +75,11 @@ module Reline::Terminfo
     @tiparm = Fiddle::Function.new(curses_dl['tparm'], [Fiddle::TYPE_VOIDP, Fiddle::TYPE_VARIADIC], Fiddle::TYPE_VOIDP)
   end
   begin
-    #extern 'char *tigetflag(const char *str, ...)'
+    #extern 'char *tigetflag(char *str, ...)'
     @tigetflag = Fiddle::Function.new(curses_dl['tigetflag'], [Fiddle::TYPE_VOIDP], Fiddle::TYPE_INT)
   rescue Fiddle::DLError
     # OpenBSD lacks tigetflag
-    #extern 'char *tgetflag(const char *str, ...)'
+    #extern 'char *tgetflag(char *str, ...)'
     @tigetflag = Fiddle::Function.new(curses_dl['tgetflag'], [Fiddle::TYPE_VOIDP], Fiddle::TYPE_INT)
   end
   # TODO: add int tigetnum(char *capname)
