ddb9d60f7df1a7316092c795f97ca2fcae19e2c3
Jean Boussier <jean.boussier@gmail.com>
2022-01-11 16:14:20 +0100

    [ruby/io-wait] Make the gem a noop on Rubies older than 2.6
    Ref: https://github.com/mikel/mail/pull/1439
    
    Some gems depend on io-wait, but still support older rubies,
    so they have to chose between droping support or not listing io-wait.
    
    But io-wait could act a a noop on older rubies.
    
    https://github.com/ruby/io-wait/commit/75fcb74c32

--- ext/io/wait/extconf.rb
+++ ext/io/wait/extconf.rb
@@ -1,20 +1,24 @@
 # frozen_string_literal: false
 require 'mkmf'
-target = "io/wait"
 
-have_func("rb_io_wait")
-unless macro_defined?("DOSISH", "#include <ruby.h>")
-  have_header(ioctl_h = "sys/ioctl.h") or ioctl_h = nil
-  fionread = %w[sys/ioctl.h sys/filio.h sys/socket.h].find do |h|
-    have_macro("FIONREAD", [h, ioctl_h].compact)
-  end
-  if fionread
-    $defs << "-DFIONREAD_HEADER=\"<#{fionread}>\""
-    create_makefile(target)
-  end
+if RUBY_VERSION < "2.6"
+  File.write("Makefile", dummy_makefile($srcdir).join(""))
 else
-  if have_func("rb_w32_ioctlsocket", "ruby.h")
-    have_func("rb_w32_is_socket", "ruby.h")
-    create_makefile(target)
+  target = "io/wait"
+  have_func("rb_io_wait")
+  unless macro_defined?("DOSISH", "#include <ruby.h>")
+    have_header(ioctl_h = "sys/ioctl.h") or ioctl_h = nil
+    fionread = %w[sys/ioctl.h sys/filio.h sys/socket.h].find do |h|
+      have_macro("FIONREAD", [h, ioctl_h].compact)
+    end
+    if fionread
+      $defs << "-DFIONREAD_HEADER=\"<#{fionread}>\""
+      create_makefile(target)
+    end
+  else
+    if have_func("rb_w32_ioctlsocket", "ruby.h")
+      have_func("rb_w32_is_socket", "ruby.h")
+      create_makefile(target)
+    end
   end
 end
