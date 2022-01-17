f4ee60543acf05742360c8d1f029d88672ff1eef
aycabta <aycabta@gmail.com>
2022-01-18 06:10:06 +0900

    [ruby/reline] The AltGr behaves exactly the same as Ctrl+Alt
    On European keyboards.
    
    https://github.com/ruby/reline/commit/75fe9759a4

--- lib/reline/windows.rb
+++ lib/reline/windows.rb
@@ -249,7 +249,7 @@ class Reline::Windows
     # no char, only control keys
     return if key.char_code == 0 and key.control_keys.any?
 
-    @@output_buf.push("\e".ord) if key.control_keys.include?(:ALT)
+    @@output_buf.push("\e".ord) if key.control_keys.include?(:ALT) and !key.control_keys.include?(:CTRL)
 
     @@output_buf.concat(key.char.bytes)
   end
