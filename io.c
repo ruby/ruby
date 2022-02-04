8b04a749b69f448c9baae0abbe13ef22c208bd3e
BurdetteLamar <BurdetteLamar@Yahoo.com>
2022-02-04 16:13:25 -0600

    Enhanced RDoc for io.c

--- io.c
+++ io.c
@@ -11605,15 +11605,17 @@ io_s_write(int argc, VALUE *argv, VALUE klass, int binary)
  *  passes +data+ through standard input, writes its output to $stdout,
  *  and returns the length of the given +data+:
  *
- *    IO.write('| cat t.txt', 'abd') # => 3
+ *    IO.write('| cat t.txt -', 'abc') # => 3
  *
- *  Output (the third line is blank):
+ *  The output has five lines from <tt>t.txt</tt> (third line empty)
+ *  and one more line, +abc+, from $stdin):
  *
  *    First line
  *    Second line
  *
  *    Third line
  *    Fourth line
+ *    abc
  *
  *  With argument +path+ given, writes the given +data+ to the file
  *  at that path:
