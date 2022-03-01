128972189284f4338722e8a910d0b4f6e7a02b31
Peter Zhu <peter@peterzhu.ca>
2022-03-01 13:38:48 -0500

    Wrap ruby_abi_version in `extern "C"` for C++
    Make ruby_abi_version have C linkage so that the symbol can be found
    in the shared object.

--- include/ruby/internal/abi.h
+++ include/ruby/internal/abi.h
@@ -32,11 +32,19 @@
 
 #ifdef RUBY_DLN_CHECK_ABI
 
+# ifdef __cplusplus
+extern "C" {
+# endif
+
 RUBY_FUNC_EXPORTED unsigned long long __attribute__((weak))
 ruby_abi_version(void)
 {
     return RUBY_ABI_VERSION;
 }
+
+# ifdef __cplusplus
+}
+# endif
 
 #endif
 
