c757c4e054397142ffa1b3e8110e5f799cd7075f
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-03-04 15:56:03 +0900

    sitelibdir makes no sense in ruby itself

--- spec/ruby/language/predefined_spec.rb
+++ spec/ruby/language/predefined_spec.rb
@@ -854,6 +854,8 @@ describe "Execution variable $:" do
   end
 
   it "default $LOAD_PATH entries until sitelibdir included have @gem_prelude_index set" do
+    skip "no sense in ruby itself" if MSpecScript.instance_variable_defined?(:@testing_ruby)
+
     $:.should.include?(RbConfig::CONFIG['sitelibdir'])
     idx = $:.index(RbConfig::CONFIG['sitelibdir'])
 
