bfc697f1e26f1406c45ec7309ca0d4c0b5ecedd6
Yusuke Endoh <mame@ruby-lang.org>
2022-03-08 17:40:02 +0900

    test/io/console/test_io_console.rb: parens needed

--- test/io/console/test_io_console.rb
+++ test/io/console/test_io_console.rb
@@ -390,7 +390,7 @@ defined?(PTY) and defined?(IO.console) and TestIO_Console.class_eval do
       # TestIO_Console#test_intr [/usr/home/chkbuild/chkbuild/tmp/build/20220304T163001Z/ruby/test/io/console/test_io_console.rb:387]:
       # <"25"> expected but was
       # <"-e:12:in `p': \e[1mexecution expired (\e[1;4mTimeout::Error\e[m\e[1m)\e[m">.
-      if cc = ctrl["dsusp"] && /freebsd/ !~ RUBY_PLATFORM
+      if (cc = ctrl["dsusp"]) && /freebsd/ !~ RUBY_PLATFORM
         assert_ctrl("#{cc.ord}", cc, r, w)
         assert_ctrl("#{cc.ord}", cc, r, w)
         assert_ctrl("#{cc.ord}", cc, r, w)
