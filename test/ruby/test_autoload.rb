93e22ada0b04d333dc643f7f050df1d7a9004737
Jeremy Evans <code@jeremyevans.net>
2022-03-11 21:43:47 -0800

    Fix Module#const_source_location for autoload constants with direct requires
    If an autoload exists for a constant, but the path for the autoload
    was required, const_source_location would return [false, 0] instead
    of the actual file and line. This fixes it by setting the appropriate
    file and line in rb_const_set, and saving the file and line in
    const_tbl_update before they get reset by current_autoload_data.
    
    Fixes [Bug #18624]

--- test/ruby/test_autoload.rb
+++ test/ruby/test_autoload.rb
@@ -443,6 +443,23 @@ p Foo::Bar
     end
   end
 
+  def test_source_location_after_require
+    bug = "Bug18624"
+    Dir.mktmpdir('autoload') do |tmpdir|
+      path = "#{tmpdir}/test-#{bug}.rb"
+      File.write(path, "C::#{bug} = __FILE__\n")
+      assert_separately(%W[-I #{tmpdir}], "#{<<-"begin;"}\n#{<<-"end;"}")
+      begin;
+        class C; end
+        C.autoload(:Bug18624, #{path.dump})
+        require #{path.dump}
+        assert_equal [#{path.dump}, 1], C.const_source_location(#{bug.dump})
+        assert_equal #{path.dump}, C.const_get(#{bug.dump})
+        assert_equal [#{path.dump}, 1], C.const_source_location(#{bug.dump})
+      end;
+    end
+  end
+
   def test_no_memory_leak
     assert_no_memory_leak([], '', "#{<<~"begin;"}\n#{<<~'end;'}", 'many autoloads', timeout: 60)
     begin;
