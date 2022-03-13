1b571d0abf6070673320b11a30769bbe74d12e39
Benoit Daloze <eregontp@gmail.com>
2022-03-13 13:18:56 +0100

    Fix guards for unicode versions specs

--- spec/ruby/library/rbconfig/unicode_emoji_version_spec.rb
+++ spec/ruby/library/rbconfig/unicode_emoji_version_spec.rb
@@ -20,11 +20,11 @@ describe "RbConfig::CONFIG['UNICODE_EMOJI_VERSION']" do
     end
   end
 
-#  ruby_version_is "3.1" do
-#    it "is 13.1 for Ruby 3.1" do
-#      RbConfig::CONFIG['UNICODE_EMOJI_VERSION'].should == "13.1"
-#    end
-#  end
+ ruby_version_is "3.1"..."3.2" do
+   it "is 13.1 for Ruby 3.1" do
+     RbConfig::CONFIG['UNICODE_EMOJI_VERSION'].should == "13.1"
+   end
+ end
 
   ruby_version_is "3.2" do
     it "is 14.0 for Ruby 3.2" do
