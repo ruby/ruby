bb0a22a8c05994396aa316c242ff8816d8d0a259
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-04-06 22:57:01 +0900

    Obey spec file locations to rubygems

--- tool/gem-unpack.rb
+++ tool/gem-unpack.rb
@@ -5,13 +5,14 @@ require 'rubygems/package'
 # This library is used by "make extract-gems" to
 # unpack bundled gem files.
 
-def Gem.unpack(file, dir = nil)
+def Gem.unpack(file, dir = nil, spec_dir = nil)
   pkg = Gem::Package.new(file)
   spec = pkg.spec
   target = spec.full_name
   target = File.join(dir, target) if dir
   pkg.extract_files target
-  spec_file = File.join(target, "#{spec.name}-#{spec.version}.gemspec")
+  FileUtils.mkdir_p(spec_dir ||= target)
+  spec_file = File.join(spec_dir, "#{spec.name}-#{spec.version}.gemspec")
   open(spec_file, 'wb') do |f|
     f.print spec.to_ruby
   end
