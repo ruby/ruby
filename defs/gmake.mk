c4daf8e445925695c34bab8bf5135dcd1e8575a3
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-04-06 22:57:01 +0900

    Obey spec file locations to rubygems

--- defs/gmake.mk
+++ defs/gmake.mk
@@ -295,7 +295,7 @@ extract-gems: | $(patsubst %,.bundle/gems/%,$(bundled-gems))
 	$(ECHO) Extracting bundle gem $*...
 	$(Q) $(BASERUBY) -C "$(srcdir)" \
 	    -Itool -rgem-unpack \
-	    -e 'Gem.unpack("gems/$(@F).gem", ".bundle/gems")'
+	    -e 'Gem.unpack("gems/$(@F).gem", ".bundle/gems", ".bundle/specifications")'
 	$(RMALL) "$(srcdir)/$(@:.gem=)/".git*
 
 $(srcdir)/.bundle/gems:
