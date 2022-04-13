0c68c54ef3ea4dedc93e237c352a5922ed5a8ddb
Burdette Lamar <BurdetteLamar@Yahoo.com>
2022-04-13 13:38:13 -0500

    Update string.c
    Co-authored-by: Peter Zhu <peter@peterzhu.ca>

--- string.c
+++ string.c
@@ -11756,7 +11756,7 @@ sym_start_with(int argc, VALUE *argv, VALUE sym)
 
 /*
  *  call-seq:
- *    end_with?(*string_or_regexp)-> true or false
+ *    end_with?(*string_or_regexp) -> true or false
  *
  *
  *  Equivalent to <tt>self.to_s.end_with?</tt>; see String#end_with?.
