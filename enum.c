9e8841e592c40e65bbad410a490c05f07a87052e
Colin Hart <colin.hart@stitchfix.com>
2022-04-25 15:32:29 -0400

    Simplify example code for Enumerable#each_with_object

--- enum.c
+++ enum.c
@@ -3098,8 +3098,10 @@ each_with_object_i(RB_BLOCK_CALL_FUNC_ARGLIST(i, memo))
  *  Calls the block once for each element, passing both the element
  *  and the given object:
  *
- *    (1..4).each_with_object([]) {|i, a| a.push(i**2) } # => [1, 4, 9, 16]
- *    h.each_with_object({}) {|element, h| k, v = *element; h[v] = k }
+ *    (1..4).each_with_object([]) {|i, a| a.push(i**2) }
+ *    # => [1, 4, 9, 16]
+ *
+ *    {foo: 0, bar: 1, baz: 2}.each_with_object({}) {|(k, v), h| h[v] = k }
  *    # => {0=>:foo, 1=>:bar, 2=>:baz}
  *
  *  With no block given, returns an Enumerator.
