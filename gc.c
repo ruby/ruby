ffcf94cfdea4aaabe5e31875948f14831d0aa159
Kunshan Wang <wks1986@gmail.com>
2022-04-29 01:04:03 +0800

    Fix stack scanning.
    Since we now stop the world in our own way, we need to handle stack
    scanning, too.  In `block_for_gc`, we need to update the "stack top"
    field in the rb_execution_context_t of the current thread so that the
    conservative stack scanner won't miss any local variables held in local
    variables in C functions.

--- gc.c
+++ gc.c
@@ -14510,6 +14510,13 @@ rb_mmtk_resume_mutators(MMTk_VMWorkerThread tls)
 static void
 rb_mmtk_block_for_gc_internal(void *unused)
 {
+    // Dump callee-saved registers and set the stack end for conservative stack scanning.
+    // See also: mark_current_machine_context
+    rb_jmp_buf j;
+    rb_setjmp(j); // Hope __builtin_setjmp includes all callee-saved registers.  GCC's docs doesn't seem to guarantee that.
+    rb_execution_context_t *ec = GET_EC();
+    SET_STACK_END; // Hope the SP includes the jmp buf `j`.  There's no guarantee on the C language level.
+
     // Increment the stopped ractor count
     rb_mmtk_global.stopped_ractors++;
     if (rb_mmtk_global.stopped_ractors == 1) {
