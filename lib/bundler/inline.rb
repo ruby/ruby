07fa9e3a8f4246498ff143407aa5e8f8f49bcc9f
David Rodríguez <deivid.rodriguez@riseup.net>
2022-05-13 14:03:24 +0200

    [rubygems/rubygems] Remove unnecessary verification
    `DSL#to_definition` already verifies this.
    
    https://github.com/rubygems/rubygems/commit/c596f0af83

--- lib/bundler/inline.rb
+++ lib/bundler/inline.rb
@@ -46,7 +46,6 @@ def gemfile(install = false, options = {}, &gemfile)
     Bundler::Plugin.gemfile_install(&gemfile) if Bundler.feature_flag.plugins?
     builder = Bundler::Dsl.new
     builder.instance_eval(&gemfile)
-    builder.check_primary_source_safety
 
     Bundler.settings.temporary(deployment: false, frozen: false) do
       definition = builder.to_definition(nil, true)
