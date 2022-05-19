83d5b34c6eb4741cd268a67e8011782fd4878159
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2022-05-19 16:17:09 -0400

    Fix issue with load, gen_dup

--- yjit/src/codegen.rs
+++ yjit/src/codegen.rs
@@ -3,6 +3,7 @@
 
 use crate::asm::x86_64::*;
 use crate::asm::*;
+use crate::backend::ir::*;
 use crate::core::*;
 use crate::cruby::*;
 use crate::invariants::*;
@@ -876,6 +877,10 @@ fn gen_pop(
     KeepCompiling
 }
 
+
+
+
+/*
 fn gen_dup(
     _jit: &mut JITState,
     ctx: &mut Context,
@@ -891,17 +896,10 @@ fn gen_dup(
 
     KeepCompiling
 }
-
-
-
-
-
-
-
-use crate::backend::ir::*;
+*/
 
 #[allow(dead_code)]
-fn gen_dup_ir(
+fn gen_dup(
     _jit: &mut JITState,
     ctx: &mut Context,
     cb: &mut CodeBlock,
