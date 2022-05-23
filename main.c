c183f44381319dfa07e8127413aaed5d380015ad
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-05-23 18:56:41 +0900

    [WASM] System initialization has no stack rewinding code

--- main.c
+++ main.c
@@ -33,6 +33,19 @@
 static int
 rb_main(int argc, char **argv)
 {
+    RUBY_INIT_STACK;
+    ruby_init();
+    return ruby_run_node(ruby_options(argc, argv));
+}
+
+#if defined(__wasm__) && !defined(__EMSCRIPTEN__)
+int rb_wasm_rt_start(int (main)(int argc, char **argv), int argc, char **argv);
+#define rb_main(argc, argv) rb_wasm_rt_start(rb_main, argc, argv)
+#endif
+
+int
+main(int argc, char **argv)
+{
 #ifdef RUBY_DEBUG_ENV
     ruby_set_debug_option(getenv("RUBY_DEBUG"));
 #endif
@@ -41,20 +54,5 @@ rb_main(int argc, char **argv)
 #endif
 
     ruby_sysinit(&argc, &argv);
-    {
-	RUBY_INIT_STACK;
-	ruby_init();
-	return ruby_run_node(ruby_options(argc, argv));
-    }
-}
-
-int
-main(int argc, char **argv)
-{
-#if defined(__wasm__) && !defined(__EMSCRIPTEN__)
-    int rb_wasm_rt_start(int (main)(int argc, char **argv), int argc, char **argv);
-    return rb_wasm_rt_start(rb_main, argc, argv);
-#else
     return rb_main(argc, argv);
-#endif
 }
