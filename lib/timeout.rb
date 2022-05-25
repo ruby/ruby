fde4519af8762fccc8488d1972d984a1c44691f4
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-05-25 17:55:14 +0900

    [ruby/timeout] Bump version to 0.3.0
    https://github.com/ruby/timeout/commit/f69f954a94

--- lib/timeout.rb
+++ lib/timeout.rb
@@ -23,7 +23,7 @@
 # Copyright:: (C) 2000  Information-technology Promotion Agency, Japan
 
 module Timeout
-  VERSION = "0.2.0"
+  VERSION = "0.3.0"
 
   # Raised by Timeout.timeout when the block times out.
   class Error < RuntimeError
