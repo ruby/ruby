ccfbcc730230fddeab6d7f8820c9d232117a07fd
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-06-08 11:55:21 +0900

    [DOC] RDoc now accepts other than magic numbers at `rb_attr`

--- error.c
+++ error.c
@@ -2990,7 +2990,7 @@ Init_Exception(void)
 
     rb_eLoadError   = rb_define_class("LoadError", rb_eScriptError);
     /* the path failed to load */
-    rb_attr(rb_eLoadError, rb_intern_const("path"), 1, 0, Qfalse);
+    rb_attr(rb_eLoadError, rb_intern_const("path"), TRUE, FALSE, FALSE);
 
     rb_eNotImpError = rb_define_class("NotImplementedError", rb_eScriptError);
 
