fb4e666bff4b6bb676fc498311df194c646ba83f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-06-13 15:33:54 +1200

    Handle case where write result is zero.

--- io.c
+++ io.c
@@ -1620,6 +1620,12 @@ io_binwrite_string(VALUE arg)
         // Write as much as possible:
         ssize_t result = io_binwrite_string_internal(p->fptr, ptr, remaining);
 
+        // If only the internal buffer is written, result will be zero [bytes of given data written]. This means we
+        // should try again.
+        if (result == 0) {
+            errno = EWOULDBLOCK;
+        }
+
         if (result > 0) {
             if ((size_t)result == remaining) break;
             ptr += result;
