026100c4c6163d6b893dc20b62ac81c0368c62a6
Jemma Issroff <jemmaissroff@gmail.com>
2022-06-15 10:18:17 -0400

    Adding more clone tests, and adding dup tests

--- test/ruby/test_dup.rb
+++ test/ruby/test_dup.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: false
 require 'test/unit'
 
-class TestClone < Test::Unit::TestCase
+class TestDup < Test::Unit::TestCase
   module M001; end
   module M002; end
   module M003; include M002; end
   module M002; include M001; end
   module M003; include M002; end
 
-  def test_clone
+  def test_dup
     foo = Object.new
     def foo.test
       "test"
     end
-    bar = foo.clone
+    bar = foo.dup
     def bar.test2
       "test2"
     end
 
     assert_equal("test2", bar.test2)
-    assert_equal("test", bar.test)
+    assert_raise(NoMethodError) { bar.test }
     assert_equal("test", foo.test)
 
     assert_raise(NoMethodError) {foo.test2}
@@ -27,50 +27,50 @@ class TestClone < Test::Unit::TestCase
     assert_equal([M003, M002, M001], M003.ancestors)
   end
 
-  def test_frozen_properties_retained_on_clone
+  def test_frozen_properties_not_retained_on_dup
     obj = Object.new.freeze
-    cloned_obj = obj.clone
+    duped_obj = obj.dup
 
     assert_predicate(obj, :frozen?)
-    assert_predicate(cloned_obj, :frozen?)
+    refute_predicate(duped_obj, :frozen?)
   end
 
-  def test_ivar_retained_on_clone
+  def test_ivar_retained_on_dup
     obj = Object.new
     obj.instance_variable_set(:@a, 1)
-    cloned_obj = obj.clone
+    duped_obj = obj.dup
 
     assert_equal(obj.instance_variable_get(:@a), 1)
-    assert_equal(cloned_obj.instance_variable_get(:@a), 1)
+    assert_equal(duped_obj.instance_variable_get(:@a), 1)
   end
 
-  def test_ivars_retained_on_extended_obj_clone
+  def test_ivars_retained_on_extended_obj_dup
     ivars = { :@a => 1, :@b => 2, :@c => 3, :@d => 4 }
     obj = Object.new
     ivars.each do |ivar_name, val|
       obj.instance_variable_set(ivar_name, val)
     end
 
-    cloned_obj = obj.clone
+    duped_obj = obj.dup
 
     ivars.each do |ivar_name, val|
       assert_equal(obj.instance_variable_get(ivar_name), val)
-      assert_equal(cloned_obj.instance_variable_get(ivar_name), val)
+      assert_equal(duped_obj.instance_variable_get(ivar_name), val)
     end
   end
 
-  def test_frozen_properties_and_ivars_retained_on_clone_with_ivar
+  def test_frozen_properties_not_retained_on_dup_with_ivar
     obj = Object.new
     obj.instance_variable_set(:@a, 1)
     obj.freeze
 
-    cloned_obj = obj.clone
+    duped_obj = obj.dup
 
     assert_predicate(obj, :frozen?)
     assert_equal(obj.instance_variable_get(:@a), 1)
 
-    assert_predicate(cloned_obj, :frozen?)
-    assert_equal(cloned_obj.instance_variable_get(:@a), 1)
+    refute_predicate(duped_obj, :frozen?)
+    assert_equal(duped_obj.instance_variable_get(:@a), 1)
   end
 
   def test_user_flags
@@ -80,7 +80,7 @@ class TestClone < Test::Unit::TestCase
         undef initialize_copy
         def initialize_copy(*); end
       end
-      x = [1, 2, 3].clone
+      x = [1, 2, 3].dup
       assert_equal [], x, '[Bug #14847]'
     EOS
 
@@ -90,7 +90,7 @@ class TestClone < Test::Unit::TestCase
         undef initialize_copy
         def initialize_copy(*); end
       end
-      x = [1,2,3,4,5,6,7][1..-2].clone
+      x = [1,2,3,4,5,6,7][1..-2].dup
       x.push(1,1,1,1,1)
       assert_equal [1, 1, 1, 1, 1], x, '[Bug #14847]'
     EOS
@@ -103,7 +103,7 @@ class TestClone < Test::Unit::TestCase
       end
       h = {}
       h.default_proc = proc { raise }
-      h = h.clone
+      h = h.dup
       assert_equal nil, h[:not_exist], '[Bug #14847]'
     EOS
   end
