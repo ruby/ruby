78425d7e74887b57ee15e6b8933bd3878db6a888
David Rodríguez <deivid.rodriguez@riseup.net>
2022-06-15 20:54:29 +0200

    [rubygems/rubygems] Unset ENV set by official Ruby docker images
    So that it does not interfere with our specs.
    
    https://github.com/rubygems/rubygems/commit/1029714e21

--- spec/bundler/spec_helper.rb
+++ spec/bundler/spec_helper.rb
@@ -74,6 +74,8 @@ RSpec.configure do |config|
     ENV["BUNDLER_SPEC_RUN"] = "true"
     ENV["BUNDLER_NO_OLD_RUBYGEMS_WARNING"] = "true"
     ENV["BUNDLE_USER_CONFIG"] = ENV["BUNDLE_USER_CACHE"] = ENV["BUNDLE_USER_PLUGIN"] = nil
+    ENV["BUNDLE_APP_CONFIG"] = nil
+    ENV["BUNDLE_SILENCE_ROOT_WARNING"] = nil
     ENV["RUBYGEMS_GEMDEPS"] = nil
     ENV["XDG_CONFIG_HOME"] = nil
     ENV["GEMRC"] = nil
