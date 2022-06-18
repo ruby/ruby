ruby/ruby

commit 71c746379d5872e250d90ae45c585760afaf9516

Make alias for aliased original method
Chaining aliased methods increases searching cost linearly.

vm_method.c
@@ -1932,11 +1932,17 @@ rb_alias(VALUE klass, ID alias_name, ID original_name)
 	}
     }
 
-    if (orig_me->def->type == VM_METHOD_TYPE_ZSUPER) {
+    switch (orig_me->def->type) {
+      case VM_METHOD_TYPE_ZSUPER:
 	klass = RCLASS_SUPER(klass);
 	original_name = orig_me->def->original_id;
 	visi = METHOD_ENTRY_VISI(orig_me);
 	goto again;
+      case VM_METHOD_TYPE_ALIAS:
+        orig_me = orig_me->def->body.alias.original_me;
+        VM_ASSERT(orig_me->def->type != VM_METHOD_TYPE_ALIAS);
+        break;
+      default: break;
     }
 
     if (visi == METHOD_VISI_UNDEF) visi = METHOD_ENTRY_VISI(orig_me);

commit 9d11ddcf6ace6676cea7cac836b4302240e3efb4

merge revision(s) 0c6e24d102e894a7211a596e6aa95828b1cf4406: [Backport #18600]
	Fix visibility of alias of zsuper methods

	This was broken by 71c746379d5872e250d90ae45c585760afaf9516.

	Fixes [Bug #18600]
	---
	 test/ruby/test_alias.rb | 10 ++++++++++
	 vm_method.c             |  1 +
	 2 files changed, 11 insertions(+)

version.h
@@ -11,10 +11,10 @@
 # define RUBY_VERSION_MINOR RUBY_API_VERSION_MINOR
 #define RUBY_VERSION_TEENY 3
 #define RUBY_RELEASE_DATE RUBY_RELEASE_YEAR_STR"-"RUBY_RELEASE_MONTH_STR"-"RUBY_RELEASE_DAY_STR
-#define RUBY_PATCHLEVEL 31
+#define RUBY_PATCHLEVEL 32
 
 #define RUBY_RELEASE_YEAR 2022
-#define RUBY_RELEASE_MONTH 5
+#define RUBY_RELEASE_MONTH 6
 #define RUBY_RELEASE_DAY 18
 
 #include "ruby/version.h"

vm_method.c
@@ -2133,6 +2133,7 @@ rb_alias(VALUE klass, ID alias_name, ID original_name)
 	visi = METHOD_ENTRY_VISI(orig_me);
 	goto again;
       case VM_METHOD_TYPE_ALIAS:
+        visi = METHOD_ENTRY_VISI(orig_me);
         orig_me = orig_me->def->body.alias.original_me;
         VM_ASSERT(orig_me->def->type != VM_METHOD_TYPE_ALIAS);
         break;

test/ruby/test_alias.rb
@@ -265,6 +265,16 @@ class TestAlias < Test::Unit::TestCase
     end;
   end
 
+  class C2
+    public :system
+    alias_method :bar, :system
+    alias_method :system, :bar
+  end
+
+  def test_zsuper_alias_visibility
+    assert(C2.new.respond_to?(:system))
+  end
+
   def test_alias_memory_leak
     assert_no_memory_leak([], "#{<<~"begin;"}", "#{<<~'end;'}", rss: true)
     begin;