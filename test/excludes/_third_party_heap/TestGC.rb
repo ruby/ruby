8e537c60e9c34739c70daa5d1a93d4d19fe5d1d2
Chris Seaton <chris@chrisseaton.com>
2022-07-14 04:25:41 +0100

    There should be a single GC excludes file (#3)

--- test/excludes/_third_party_heap/TestGC.rb
+++
@@ -1,4 +0,0 @@
-exclude :gc_mark_begin, "no gc implemented yet"
-exclude :gc_mark_end, "no gc implemented yet"
-exclude :gc_sweep_begin, "no gc implemented yet"
-exclude :gc_sweep_end, "no gc implemented yet"
