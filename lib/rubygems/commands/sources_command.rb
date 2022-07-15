8e451d1083cb07ad974365f99b4f01742e4759c8
Takuya Noguchi <takninnovationresearch@gmail.com>
2022-07-15 01:49:57 +0000

    [rubygems/rubygems] Fix wrong information about default RubyGems source
    Also fix regression from rubygems/rubygems PR 3056
    
    https://my.diffend.io/gems/rubygems-update/1.3.5/1.3.6/page/4#d2h-514986
    https://my.diffend.io/gems/rubygems-update/2.0.0/2.0.2/page/2#d2h-514986
    
    Signed-off-by: Takuya Noguchi <takninnovationresearch@gmail.com>
    
    https://github.com/rubygems/rubygems/commit/06b1e9ebc5

--- lib/rubygems/commands/sources_command.rb
+++ lib/rubygems/commands/sources_command.rb
@@ -138,8 +138,8 @@ do not recognize you should remove them.
 RubyGems has been configured to serve gems via the following URLs through
 its history:
 
-* http://gems.rubyforge.org (RubyGems 1.3.6 and earlier)
-* https://rubygems.org/       (RubyGems 1.3.7 through 1.8.25)
+* http://gems.rubyforge.org (RubyGems 1.3.5 and earlier)
+* http://rubygems.org       (RubyGems 1.3.6 through 1.8.30, and 2.0.0)
 * https://rubygems.org      (RubyGems 2.0.1 and newer)
 
 Since all of these sources point to the same set of gems you only need one
