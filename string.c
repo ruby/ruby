2d1cf658eeafbd989e0b61bae08a8d0b1b49e2bd
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-07-26 18:33:21 +0900

    Adjust indent [ci skip]

--- string.c
+++ string.c
@@ -10784,16 +10784,16 @@ rb_str_b(VALUE str)
     // If we know the receiver's code range then we know the result's code range.
     int cr = ENC_CODERANGE(str);
     switch (cr) {
-        case ENC_CODERANGE_7BIT:
-            ENC_CODERANGE_SET(str2, ENC_CODERANGE_7BIT);
-            break;
-        case ENC_CODERANGE_BROKEN:
-        case ENC_CODERANGE_VALID:
-            ENC_CODERANGE_SET(str2, ENC_CODERANGE_VALID);
-            break;
-        default:
-            ENC_CODERANGE_CLEAR(str2);
-            break;
+      case ENC_CODERANGE_7BIT:
+        ENC_CODERANGE_SET(str2, ENC_CODERANGE_7BIT);
+        break;
+      case ENC_CODERANGE_BROKEN:
+      case ENC_CODERANGE_VALID:
+        ENC_CODERANGE_SET(str2, ENC_CODERANGE_VALID);
+        break;
+      default:
+        ENC_CODERANGE_CLEAR(str2);
+        break;
     }
 
     return str2;
