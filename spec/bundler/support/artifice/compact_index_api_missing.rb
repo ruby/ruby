a9509068db84d6a86a718b5ae931b8d65fc15272
David Rodríguez <deivid.rodriguez@riseup.net>
2022-08-08 23:48:02 +0200

    [rubygems/rubygems] Make `compact_index_api_missing` server more strict
    The compact index should not request any marshaled gemspecs whatsoever.
    
    https://github.com/rubygems/rubygems/commit/6dbd44d0c0

--- spec/bundler/support/artifice/compact_index_api_missing.rb
+++ spec/bundler/support/artifice/compact_index_api_missing.rb
@@ -6,12 +6,7 @@ Artifice.deactivate
 
 class CompactIndexApiMissing < CompactIndexAPI
   get "/fetch/actual/gem/:id" do
-    warn params[:id]
-    if params[:id] == "rack-1.0.gemspec.rz"
-      halt 404
-    else
-      File.binread("#{gem_repo2}/quick/Marshal.4.8/#{params[:id]}")
-    end
+    halt 404
   end
 end
 
