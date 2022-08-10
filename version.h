3569d1309501a5e8ae4656d90d521c7eaa3330c6
git <svn-admin@ruby-lang.org>
2022-08-10 14:13:45 +0900

    * 2022-08-10 [ci skip]

--- version.h
+++ version.h
@@ -15,7 +15,7 @@
 
 #define RUBY_RELEASE_YEAR 2022
 #define RUBY_RELEASE_MONTH 8
-#define RUBY_RELEASE_DAY 9
+#define RUBY_RELEASE_DAY 10
 
 #include "ruby/version.h"
 #include "ruby/internal/abi.h"
