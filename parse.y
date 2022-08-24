ruby/ruby

commit 688169fd83b24564b653c03977c168cea50ccd35

2000-03-23
git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@648 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

parse.y
@@ -27,7 +27,7 @@
 #define ID_GLOBAL   0x03
 #define ID_ATTRSET  0x04
 #define ID_CONST    0x05
-#define ID_SHARED   0x06
+#define ID_CLASS    0x06
 
 #define is_notop_id(id) ((id)>LAST_TOKEN)
 #define is_local_id(id) (is_notop_id(id)&&((id)&ID_SCOPE_MASK)==ID_LOCAL)
@@ -35,7 +35,7 @@
 #define is_instance_id(id) (is_notop_id(id)&&((id)&ID_SCOPE_MASK)==ID_INSTANCE)
 #define is_attrset_id(id) (is_notop_id(id)&&((id)&ID_SCOPE_MASK)==ID_ATTRSET)
 #define is_const_id(id) (is_notop_id(id)&&((id)&ID_SCOPE_MASK)==ID_CONST)
-#define is_shared_id(id) (is_notop_id(id)&&((id)&ID_SCOPE_MASK)==ID_SHARED)
+#define is_class_id(id) (is_notop_id(id)&&((id)&ID_SCOPE_MASK)==ID_CLASS)
 
 NODE *ruby_eval_tree_begin = 0;
 NODE *ruby_eval_tree = 0;
@@ -166,17 +166,17 @@ static void top_local_setup();
 	k__LINE__
 	k__FILE__
 
-%token <id>   tIDENTIFIER tFID tGVAR tIVAR tCONSTANT tSHVAR
+%token <id>   tIDENTIFIER tFID tGVAR tIVAR tCONSTANT tCVAR
 %token <val>  tINTEGER tFLOAT tSTRING tXSTRING tREGEXP
 %token <node> tDSTRING tDXSTRING tDREGEXP tNTH_REF tBACK_REF
 
 %type <node> singleton string
 %type <val>  literal numeric
 %type <node> compstmt stmts stmt expr arg primary command_call method_call
-%type <node> if_tail opt_else case_body cases rescue ensure
+%type <node> if_tail opt_else case_body cases rescue exc_list exc_var ensure
 %type <node> opt_call_args call_args ret_args args when_args
 %type <node> aref_args opt_block_arg block_arg stmt_rhs
-%type <node> mrhs opt_list superclass generic_call block_call var_ref
+%type <node> mrhs superclass generic_call block_call var_ref
 %type <node> f_arglist f_args f_optarg f_opt f_block_arg opt_f_block_arg
 %type <node> array assoc_list assocs assoc undef_list backref
 %type <node> block_var opt_block_var brace_block do_block lhs none
@@ -918,9 +918,6 @@ opt_block_arg	: ',' block_arg
 		    }
 		| none
 
-opt_list	: args
-		| none
-
 args 		: arg
 		    {
 			value_expr($1);
@@ -1428,12 +1425,25 @@ when_args	: args
 cases		: opt_else
 		| case_body
 
-rescue		: kRESCUE opt_list do
+exc_list	: args
+		| none
+
+exc_var		: kIN lhs
+		    {
+			$$ = $2;
+		    }
+		| none
+
+rescue		: kRESCUE exc_list exc_var do
 		  compstmt
 		  rescue
 		    {
-			$$ = NEW_RESBODY($2, $4, $5);
-		        fixpos($$, $2?$2:$4);
+		        if ($3 && $5) {
+		            $3 = node_assign($3, NEW_GVAR(rb_intern("$!")));
+			    $5 = block_append($3, $5);
+			}
+			$$ = NEW_RESBODY($2, $5, $6);
+		        fixpos($$, $2?$2:$5);
 		    }
 		| none
 
@@ -1496,7 +1506,7 @@ variable	: tIDENTIFIER
 		| tIVAR
 		| tGVAR
 		| tCONSTANT
-		| tSHVAR
+		| tCVAR
 		| kNIL {$$ = kNIL;}
 		| kSELF {$$ = kSELF;}
 		| kTRUE {$$ = kTRUE;}
@@ -3386,7 +3396,7 @@ yylex()
 	  case '@':
 	    lex_state = EXPR_END;
 	    if (tok()[1] == '@')
-		result = tSHVAR;
+		result = tCVAR;
 	    else
 		result = tIVAR;
 	    break;
@@ -3838,10 +3848,10 @@ gettable(id)
 	return NEW_IVAR(id);
     }
     else if (is_const_id(id)) {
-	return NEW_CVAR(id);
+	return NEW_CONST(id);
     }
-    else if (is_shared_id(id)) {
-	return NEW_SHVAR(id);
+    else if (is_class_id(id)) {
+	return NEW_CVAR(id);
     }
     rb_bug("invalid id for gettable");
     return 0;
@@ -3899,12 +3909,12 @@ assignable(id, val)
 	    yyerror("dynamic constant assignment");
 	lhs = NEW_CDECL(id, val);
     }
-    else if (is_shared_id(id)) {
+    else if (is_class_id(id)) {
 	if (cur_mid || in_single) {
-	    lhs = NEW_SHASGN(id, val);
+	    lhs = NEW_CVASGN(id, val);
 	}
 	else {
-	    lhs = NEW_SHDECL(id, val);
+	    lhs = NEW_CVDECL(id, val);
 	}
     }
     else {
@@ -3994,8 +4004,8 @@ node_assign(lhs, rhs)
       case NODE_MASGN:
       case NODE_CASGN:
       case NODE_CDECL:
-      case NODE_SHASGN:
-      case NODE_SHDECL:
+      case NODE_CVASGN:
+      case NODE_CVDECL:
 	lhs->nd_value = rhs;
 	break;
 
@@ -4099,11 +4109,12 @@ void_expr(node)
       case NODE_DVAR:
       case NODE_GVAR:
       case NODE_IVAR:
+      case NODE_CVAR:
       case NODE_NTH_REF:
       case NODE_BACK_REF:
 	useless = "a variable";
 	break;
-      case NODE_CVAR:
+      case NODE_CONST:
       case NODE_CREF:
 	useless = "a constant";
 	break;
@@ -4607,7 +4618,7 @@ rb_intern(name)
 	break;
       case '@':
 	if (name[1] == '@')
-	    id |= ID_SHARED;
+	    id |= ID_CLASS;
 	else
 	    id |= ID_INSTANCE;
 	break;
@@ -4701,10 +4712,10 @@ rb_is_const_id(id)
 }
 
 int
-rb_is_shared_id(id)
+rb_is_class_id(id)
     ID id;
 {
-    if (is_shared_id(id)) return Qtrue;
+    if (is_class_id(id)) return Qtrue;
     return Qfalse;
 }
 

ToDo
@@ -5,7 +5,8 @@ Language Spec.
 - rescue modifier; a rescue b => begin a rescue; b end
 - %w(a\ b\ c abc) => ["a b c", "abc"]
 - objectify symbols
-- class variable (prefix @@) - still need work for singletons
+- class variable (prefix @@)
+- rescue RuntimeError in err ??
 * operator !! for rescue. ???
 * objectify characters
 * ../... outside condition invokes operator method too.
@@ -21,6 +22,8 @@ Language Spec.
 * I18N (or M17N) script/string/regexp
 * Fixnum 0 as false ????
 * discourage use of symbol variable (e.g. $/, etc.) in manual
+* discourage use of Perlish features by giving warnings.
+* `exception' method to be alternative for `$!'. ??
 
 Hacking Interpreter
 
@@ -31,7 +34,7 @@ Hacking Interpreter
 - remove end_proc registered out of require only
 - all object made freezable
 * non-blocking open (e.g. for named pipe) for thread
-* avoid blocking with gethostbyname/gethostbyaddr
+* avoid blocking with gethostbyname/gethostbyaddr (use fork ???)
 * objectify interpreters
 * remove rb_eval() recursions
 * syntax tree -> bytecode ???
@@ -40,7 +43,7 @@ Hacking Interpreter
 
 Standard Libraries
 
-- hash[key] = nil may not remove entry; hashes may have nil as the value.
+- hash[key] = nil does not remove entry; hashes may have nil as the value.
 - hash.fetch(key) raises exception if key is not found.
 - Array#{first,last,at}
 - Dir.glob(pat){|f|...}
@@ -53,13 +56,13 @@ Standard Libraries
 - debugger for thread programming
 - SyntaxError, NameError, LoadError and NotImplementError are subclasses of
   ScriptError<Exception, not StandardError.
-- String's bang methods return string always
 - Thread::start gives arguments, not a thread object to the block
 - regexp: (?>..), \G
 - Struct::new([name,]member,...)
 - IO#reopen accepts path as well
 - Kernel#scan
-- call initialize for builtin class too (not yet: Regexp, IO, etc)
+- call initialize for builtin class too (not yet: Regexp, etc)
+- performance tune for String's non-bang methods.
 * String#scanf(?)
 * Object#fmt(?)
 * Integer#{bin,oct,hex,heX}
@@ -69,7 +72,6 @@ Standard Libraries
 * Stream or Port, abstract superclass of IO ?
 * String#{pred,prev}, String#downto
 * optional stepsize argument for succ()
-* performance tune for String's non-bang methods.
 * Ruby module -- Ruby::Version, Ruby::Interpreter
 
 Extension Libraries

eval.c
@@ -420,6 +420,8 @@ rb_method_boundp(klass, id, ex)
     return Qfalse;
 }
 
+static ID init, eqq, each, aref, aset, match, missing, added, singleton_added;
+
 void
 rb_attr(klass, id, read, write, ex)
     VALUE klass;
@@ -454,15 +456,16 @@ rb_attr(klass, id, read, write, ex)
     attriv = rb_intern(buf);
     if (read) {
 	rb_add_method(klass, id, NEW_IVAR(attriv), noex);
+	rb_funcall(klass, added, 1, ID2SYM(id));
     }
     sprintf(buf, "%s=", name);
     id = rb_intern(buf);
     if (write) {
 	rb_add_method(klass, id, NEW_ATTRSET(attriv), noex);
+	rb_funcall(klass, added, 1, ID2SYM(id));
     }
 }
 
-static ID init, eqq, each, aref, aset, match, missing;
 VALUE ruby_errinfo = Qnil;
 extern NODE *ruby_eval_tree_begin;
 extern NODE *ruby_eval_tree;
@@ -1294,7 +1297,7 @@ superclass(self, node)
 	  case NODE_COLON2:
 	    rb_raise(rb_eTypeError, "undefined superclass `%s'",
 		     rb_id2name(node->nd_mid));
-	  case NODE_CVAR:
+	  case NODE_CONST:
 	    rb_raise(rb_eTypeError, "undefined superclass `%s'",
 		     rb_id2name(node->nd_vid));
 	  default:
@@ -1310,7 +1313,7 @@ superclass(self, node)
     return val;
 }
 
-#define ruby_cbase (((NODE*)ruby_frame->cbase)->nd_clss)
+#define ruby_cbase (RNODE(ruby_frame->cbase)->nd_clss)
 
 static VALUE
 ev_const_defined(cref, id)
@@ -1375,7 +1378,7 @@ ev_const_set(cref, id, val)
 static VALUE
 rb_mod_nesting()
 {
-    NODE *cbase = (NODE*)ruby_frame->cbase;
+    NODE *cbase = RNODE(ruby_frame->cbase);
     VALUE ary = rb_ary_new();
 
     while (cbase && cbase->nd_clss != rb_cObject) {
@@ -1388,7 +1391,7 @@ rb_mod_nesting()
 static VALUE
 rb_mod_s_constants()
 {
-    NODE *cbase = (NODE*)ruby_frame->cbase;
+    NODE *cbase = RNODE(ruby_frame->cbase);
     VALUE ary = rb_ary_new();
 
     while (cbase && cbase->nd_clss != rb_cObject) {
@@ -1682,8 +1685,8 @@ is_defined(self, node, buf)
 	}
 	break;
 
-      case NODE_CVAR:
-	if (ev_const_defined((NODE*)ruby_frame->cbase, node->nd_vid)) {
+      case NODE_CONST:
+	if (ev_const_defined(RNODE(ruby_frame->cbase), node->nd_vid)) {
 	    return "constant";
 	}
 	break;
@@ -2494,11 +2497,11 @@ rb_eval(self, node)
 	break;
 
       case NODE_CASGN:
-	if (NIL_P(ruby_class)) {
+	if (NIL_P(ruby_cbase)) {
 	    rb_raise(rb_eTypeError, "no class/module to define constant");
 	}
 	result = rb_eval(self, node->nd_value);
-	ev_const_set((NODE*)ruby_frame->cbase, node->nd_vid, result);
+	ev_const_set(RNODE(ruby_frame->cbase), node->nd_vid, result);
 	break;
 
       case NODE_CDECL:
@@ -2509,20 +2512,20 @@ rb_eval(self, node)
 	rb_const_set(ruby_class, node->nd_vid, result);
 	break;
 
-      case NODE_SHASGN:
-	if (NIL_P(ruby_class)) {
-	    rb_raise(rb_eTypeError, "no class/module to define shared variable");
+      case NODE_CVASGN:
+	if (NIL_P(ruby_cbase)) {
+	    rb_raise(rb_eTypeError, "no class/module to define class variable");
 	}
 	result = rb_eval(self, node->nd_value);
-	rb_shared_variable_set(ruby_cbase, node->nd_vid, result);
+	rb_cvar_set(ruby_cbase, node->nd_vid, result);
 	break;
 
-      case NODE_SHDECL:
-	if (NIL_P(ruby_class)) {
-	    rb_raise(rb_eTypeError, "no class/module to define shared variable");
+      case NODE_CVDECL:
+	if (NIL_P(ruby_cbase)) {
+	    rb_raise(rb_eTypeError, "no class/module to define class variable");
 	}
 	result = rb_eval(self, node->nd_value);
-	rb_shared_variable_declare(ruby_class, node->nd_vid, result);
+	rb_cvar_declare(ruby_cbase, node->nd_vid, result);
 	break;
 
       case NODE_LVAR:
@@ -2544,12 +2547,12 @@ rb_eval(self, node)
 	result = rb_ivar_get(self, node->nd_vid);
 	break;
 
-      case NODE_CVAR:
-	result = ev_const_get((NODE*)ruby_frame->cbase, node->nd_vid);
+      case NODE_CONST:
+	result = ev_const_get(RNODE(ruby_frame->cbase), node->nd_vid);
 	break;
 
-      case NODE_SHVAR:
-	result = rb_shared_variable_get(ruby_cbase, node->nd_vid);
+      case NODE_CVAR:
+	result = rb_cvar_get(ruby_cbase, node->nd_vid);
 	break;
 
       case NODE_BLOCK_ARG:
@@ -2760,17 +2763,14 @@ rb_eval(self, node)
 	    if (scope_vmode == SCOPE_MODFUNC) {
 		rb_add_method(rb_singleton_class(ruby_class),
 			      node->nd_mid, node->nd_defn, NOEX_PUBLIC);
-		rb_funcall(ruby_class, rb_intern("singleton_method_added"),
-			   1, INT2FIX(node->nd_mid));
+		rb_funcall(ruby_class, singleton_added, 1, ID2SYM(node->nd_mid));
 	    }
 	    if (FL_TEST(ruby_class, FL_SINGLETON)) {
 		rb_funcall(rb_iv_get(ruby_class, "__attached__"),
-			   rb_intern("singleton_method_added"),
-			   1, INT2FIX(node->nd_mid));
+			   singleton_added, 1, ID2SYM(node->nd_mid));
 	    }
 	    else {
-		rb_funcall(ruby_class, rb_intern("method_added"),
-			   1, INT2FIX(node->nd_mid));
+		rb_funcall(ruby_class, added, 1, ID2SYM(node->nd_mid));
 	    }
 	    result = Qnil;
 	}
@@ -2805,8 +2805,7 @@ rb_eval(self, node)
 	    rb_clear_cache_by_id(node->nd_mid);
 	    rb_add_method(klass, node->nd_mid, node->nd_defn, 
 			  NOEX_PUBLIC|(body?body->nd_noex&NOEX_UNDEF:0));
-	    rb_funcall(recv, rb_intern("singleton_method_added"),
-		       1, INT2FIX(node->nd_mid));
+	    rb_funcall(recv, singleton_added, 1, ID2SYM(node->nd_mid));
 	    result = Qnil;
 	}
 	break;
@@ -2824,8 +2823,7 @@ rb_eval(self, node)
 	    rb_raise(rb_eTypeError, "no class to make alias");
 	}
 	rb_alias(ruby_class, node->nd_new, node->nd_old);
-	rb_funcall(ruby_class, rb_intern("method_added"),
-		   1, INT2FIX(node->nd_mid));
+	rb_funcall(ruby_class, added, 1, ID2SYM(node->nd_mid));
 	result = Qnil;
 	break;
 
@@ -3507,16 +3505,16 @@ assign(self, lhs, val, check)
 	break;
 
       case NODE_CASGN:
-	ev_const_set((NODE*)ruby_frame->cbase, lhs->nd_vid, val);
+	ev_const_set(RNODE(ruby_frame->cbase), lhs->nd_vid, val);
 	break;
 
       case NODE_CDECL:
 	rb_const_set(ruby_class, lhs->nd_vid, val);
 	break;
 
-      case NODE_SHDECL:
-      case NODE_SHASGN:
-	rb_shared_variable_set(ruby_cbase, lhs->nd_vid, val);
+      case NODE_CVDECL:
+      case NODE_CVASGN:
+	rb_cvar_set(ruby_cbase, lhs->nd_vid, val);
 	break;
 
       case NODE_MASGN:
@@ -4397,10 +4395,9 @@ rb_f_caller(argc, argv)
 void
 rb_backtrace()
 {
-    int i, lev;
+    int i;
     VALUE ary;
 
-    lev = INT2FIX(0);
     ary = backtrace(-1);
     for (i=0; i<RARRAY(ary)->len; i++) {
 	printf("\tfrom %s\n", RSTRING(RARRAY(ary)->ptr[i])->ptr);
@@ -4410,9 +4407,6 @@ rb_backtrace()
 static VALUE
 make_backtrace()
 {
-    VALUE lev;
-
-    lev = INT2FIX(0);
     return backtrace(-1);
 }
 
@@ -4598,7 +4592,6 @@ exec_under(func, under, args)
     VALUE val;			/* OK */
     int state;
     int mode;
-    VALUE cbase = ruby_frame->cbase;
 
     PUSH_CLASS();
     ruby_class = under;
@@ -4607,7 +4600,10 @@ exec_under(func, under, args)
     ruby_frame->last_class = _frame.prev->last_class;
     ruby_frame->argc = _frame.prev->argc;
     ruby_frame->argv = _frame.prev->argv;
-    ruby_frame->cbase = (VALUE)rb_node_newnode(NODE_CREF,under,0,cbase);
+    if (ruby_cbase != under) {
+	ruby_frame->cbase = (VALUE)rb_node_newnode(NODE_CREF,under,0,ruby_frame->cbase);
+    }
+
     mode = scope_vmode;
     SCOPE_SET(SCOPE_PUBLIC);
     PUSH_TAG(PROT_NONE);
@@ -5129,6 +5125,7 @@ rb_mod_modfunc(argc, argv, module)
 	}
 	rb_clear_cache_by_id(id);
 	rb_add_method(rb_singleton_class(module), id, body->nd_body, NOEX_PUBLIC);
+	rb_funcall(module, singleton_added, 1, ID2SYM(id));
     }
     return module;
 }
@@ -5398,6 +5395,8 @@ Init_eval()
     aset = rb_intern("[]=");
     match = rb_intern("=~");
     missing = rb_intern("method_missing");
+    added = rb_intern("method_added");
+    singleton_added = rb_intern("singleton_method_added");
 
     rb_global_variable((VALUE*)&top_scope);
     rb_global_variable((VALUE*)&ruby_eval_tree_begin);

hash.c
@@ -411,10 +411,11 @@ rb_hash_delete(hash, key)
     VALUE val;
 
     rb_hash_modify(hash);
-    if (RHASH(hash)->iter_lev > 0 &&
-	st_delete_safe(RHASH(hash)->tbl, &key, &val, Qundef)) {
-	FL_SET(hash, HASH_DELETED);
-	return val;
+    if (RHASH(hash)->iter_lev > 0) {
+	if (st_delete_safe(RHASH(hash)->tbl, &key, &val, Qundef)) {
+	    FL_SET(hash, HASH_DELETED);
+	    return val;
+	}
     }
     else if (st_delete(RHASH(hash)->tbl, &key, &val))
 	return val;

io.c
@@ -942,6 +942,7 @@ void
 rb_io_fptr_finalize(fptr)
     OpenFile *fptr;
 {
+    if (!fptr) return;
     rb_io_fptr_close(fptr);
     if (fptr->path) {
 	free(fptr->path);
@@ -1282,41 +1283,41 @@ rb_fdopen(fd, mode)
 }
 
 static VALUE
-rb_file_open_internal(klass, fname, mode)
-    VALUE klass;
+rb_file_open_internal(io, fname, mode)
+    VALUE io;
     const char *fname, *mode;
 {
     OpenFile *fptr;
-    NEWOBJ(port, struct RFile);
-    OBJSETUP(port, klass, T_FILE);
-    MakeOpenFile(port, fptr);
+
+    MakeOpenFile(io, fptr);
 
     fptr->mode = rb_io_mode_flags(mode);
     fptr->f = rb_fopen(fname, mode);
     fptr->path = strdup(fname);
 
-    return (VALUE)port;
+    return io;
 }
 
 VALUE
 rb_file_open(fname, mode)
     const char *fname, *mode;
 {
-    return rb_file_open_internal(rb_cFile, fname, mode);
+    NEWOBJ(io, struct RFile);
+    OBJSETUP(io, rb_cFile, T_FILE);
+    return rb_file_open_internal((VALUE)io, fname, mode);
 }
 
-VALUE
-rb_file_sysopen_internal(klass, fname, flags, mode)
-    VALUE klass;
+static VALUE
+rb_file_sysopen_internal(io, fname, flags, mode)
+    VALUE io;
     char *fname;
     int flags, mode;
 {
     OpenFile *fptr;
     int fd;
     char *m;
-    NEWOBJ(port, struct RFile);
-    OBJSETUP(port, klass, T_FILE);
-    MakeOpenFile(port, fptr);
+
+    MakeOpenFile(io, fptr);
 
     fd = rb_open(fname, flags, mode);
     m = rb_io_flags_mode(flags);
@@ -1324,7 +1325,7 @@ rb_file_sysopen_internal(klass, fname, flags, mode)
     fptr->f = rb_fdopen(fd, m);
     fptr->path = strdup(fname);
 
-    return (VALUE)port;
+    return io;
 }
 
 VALUE
@@ -1332,7 +1333,9 @@ rb_file_sysopen(fname, flags, mode)
     const char *fname;
     int flags, mode;
 {
-    return rb_file_sysopen_internal(rb_cFile, fname, flags, mode);
+    NEWOBJ(io, struct RFile);
+    OBJSETUP(io, rb_cFile, T_FILE);
+    return rb_file_sysopen_internal((VALUE)io, fname, mode);
 }
 
 #if defined (NT) || defined(DJGPP) || defined(__CYGWIN__) || defined(__human68k__)
@@ -1567,10 +1570,10 @@ rb_io_s_popen(argc, argv, self)
 }
 
 static VALUE
-rb_file_s_open(argc, argv, klass)
+rb_file_initialize(argc, argv, io)
     int argc;
     VALUE *argv;
-    VALUE klass;
+    VALUE io;
 {
     VALUE fname, vmode, file, perm;
     char *path, *mode;
@@ -1583,7 +1586,7 @@ rb_file_s_open(argc, argv, klass)
 	int flags = NUM2INT(vmode);
 	int fmode = NIL_P(perm) ? 0666 : NUM2INT(perm);
 
-	file = rb_file_sysopen_internal(klass, path, flags, fmode);
+	file = rb_file_sysopen_internal(io, path, flags, fmode);
     }
     else {
 	if (!NIL_P(vmode)) {
@@ -1592,7 +1595,45 @@ rb_file_s_open(argc, argv, klass)
 	else {
 	    mode = "r";
 	}
-	file = rb_file_open_internal(klass, RSTRING(fname)->ptr, mode);
+	file = rb_file_open_internal(io, RSTRING(fname)->ptr, mode);
+    }
+
+    if (rb_iterator_p()) {
+	return rb_ensure(rb_yield, file, rb_io_close, file);
+    }
+
+    return file;
+}
+
+static VALUE
+rb_file_s_open(argc, argv, klass)
+    int argc;
+    VALUE *argv;
+    VALUE klass;
+{
+    VALUE fname, vmode, file, perm;
+    char *path, *mode;
+
+    NEWOBJ(io, struct RFile);
+    OBJSETUP(io, klass, T_FILE);
+    rb_scan_args(argc, argv, "12", &fname, &vmode, &perm);
+    Check_SafeStr(fname);
+    path = RSTRING(fname)->ptr;
+
+    if (FIXNUM_P(vmode)) {
+	int flags = NUM2INT(vmode);
+	int fmode = NIL_P(perm) ? 0666 : NUM2INT(perm);
+
+	file = rb_file_sysopen_internal((VALUE)io, path, flags, fmode);
+    }
+    else {
+	if (!NIL_P(vmode)) {
+	    mode = STR2CSTR(vmode);
+	}
+	else {
+	    mode = "r";
+	}
+	file = rb_file_open_internal((VALUE)io, RSTRING(fname)->ptr, mode);
     }
 
     if (rb_iterator_p()) {
@@ -2188,14 +2229,40 @@ rb_io_s_new(argc, argv, klass)
     VALUE *argv;
     VALUE klass;
 {
+    OpenFile *fp;
+    NEWOBJ(io, struct RFile);
+    OBJSETUP(io, klass, T_FILE);
+    
+    io->fptr = 0;
+    rb_obj_call_init((VALUE)io, argc, argv);
+
+    return (VALUE)io;
+}
+
+static VALUE
+rb_io_initialize(argc, argv, io)
+    int argc;
+    VALUE *argv;
+    VALUE io;
+{
     VALUE fnum, mode;
+    OpenFile *fp;
     char *m = "r";
 
+    if (RFILE(io)->fptr) {
+	rb_io_fptr_finalize(RFILE(io)->fptr);
+	free(RFILE(io)->fptr);
+	RFILE(io)->fptr = 0;
+    }
     if (rb_scan_args(argc, argv, "11", &fnum, &mode) == 2) {
 	Check_SafeStr(mode);
 	m = RSTRING(mode)->ptr;
     }
-    return prep_stdio(rb_fdopen(NUM2INT(fnum), m), rb_io_mode_flags(m), klass);
+    MakeOpenFile(io, fp);
+    fp->f = rb_fdopen(NUM2INT(fnum), m);
+    fp->mode = rb_io_mode_flags(m);
+
+    return io;
 }
 
 static int binmode = 0;
@@ -3174,6 +3241,7 @@ Init_IO()
     rb_include_module(rb_cIO, rb_mEnumerable);
 
     rb_define_singleton_method(rb_cIO, "new", rb_io_s_new, -1);
+    rb_define_method(rb_cIO, "initialize", rb_io_initialize, -1);
     rb_define_singleton_method(rb_cIO, "popen", rb_io_s_popen, -1);
     rb_define_singleton_method(rb_cIO, "foreach", rb_io_s_foreach, -1);
     rb_define_singleton_method(rb_cIO, "readlines", rb_io_s_readlines, -1);
@@ -3318,8 +3386,8 @@ Init_IO()
 
     Init_File();
 
-    rb_define_singleton_method(rb_cFile, "new",  rb_file_s_open, -1);
     rb_define_singleton_method(rb_cFile, "open",  rb_file_s_open, -1);
+    rb_define_method(rb_cFile, "initialize",  rb_file_initialize, -1);
 
     rb_file_const("RDONLY", INT2FIX(O_RDONLY));
     rb_file_const("WRONLY", INT2FIX(O_WRONLY));

lib/date2.rb
@@ -1,5 +1,5 @@
-# date2.rb: Written by Tadayoshi Funaba 1998, 1999
-# $Id: date2.rb,v 1.17 1999/09/15 05:34:07 tadf Exp $
+# date2.rb: Written by Tadayoshi Funaba 1998-2000
+# $Id: date2.rb,v 1.18 2000/03/20 16:23:32 tadf Exp $
 
 class Date
 
@@ -110,6 +110,10 @@ class Date
     def jd_to_mjd(jd) jd - 2400000.5 end
     def tjd_to_jd(tjd) tjd + 2440000.5 end
     def jd_to_tjd(jd) jd - 2440000.5 end
+    def tjd2_to_jd(cycle, tjd) tjd_to_jd(cycle * 10000 + tjd) end
+    def jd_to_tjd2(jd) clfloor(jd_to_tjd(jd), 10000) end
+    def ld_to_jd(ld) ld + 2299160 end
+    def jd_to_ld(jd) jd - 2299160 end
 
     def julian_leap? (y) y % 4 == 0 end
     def gregorian_leap? (y) y % 4 == 0 and y % 100 != 0 or y % 400 == 0 end
@@ -213,15 +217,18 @@ class Date
   def rjd() @rjd end
   def rmjd() Date.jd_to_mjd(@rjd) end
   def rtjd() Date.jd_to_tjd(@rjd) end
+  def rtjd2() Date.jd_to_tjd2(@rjd) end
 
-  once :rmjd, :rtjd
+  once :rmjd, :rtjd, :rtjd2
 
   def jd() Date.rjd_to_jd(@rjd)[0] end
   def fr1() Date.rjd_to_jd(@rjd)[1] end
   def mjd() Date.jd_to_mjd(jd) end
   def tjd() Date.jd_to_tjd(jd) end
+  def tjd2() Date.jd_to_tjd2(jd) end
+  def ld() Date.jd_to_ld(jd) end
 
-  once :jd, :fr1, :mjd, :tjd
+  once :jd, :fr1, :mjd, :tjd, :tjd2, :ld
 
   def civil() Date.jd_to_civil(jd, @sg) end
   def ordinal() Date.jd_to_ordinal(jd, @sg) end

lib/getopts.rb
@@ -1,141 +1,127 @@
 #
-#		getopts.rb - 
-#			$Release Version: $
-#			$Revision$
-#			$Date$
-#			by Yasuo OHBA(SHL Japan Inc. Technology Dept.)
+#               getopts.rb - 
+#                       $Release Version: $
+#                       $Revision$
+#                       $Date$
+#                       by Yasuo OHBA(SHL Japan Inc. Technology Dept.)
 #
 # --
 # this is obsolete; use getoptlong
 #
+# 2000-03-21
+# modified by Minero Aoki <aamine@dp.u-netsurf.ne.jp>
 #
 
 $RCS_ID=%q$Header$
 
-def isSingle(lopt)
-  if lopt.index(":")
-    if lopt.split(":")[0].length == 1
-      return true
-    end
-  end
-  return nil
-end
 
-def getOptionName(lopt)
-  return lopt.split(":")[0]
-end
+def getopts( single_opts, *options )
+  single_opts_exp = (single_opts && !single_opts.empty?) ?
+                        /[#{single_opts}]/ : nil
+  single_colon_exp = nil
+  single_colon = nil
+  opt = arg = val = nil
+  boolopts = {}
+  valopts = {}
+  argv = ARGV
+  newargv = []
 
-def getDefaultOption(lopt)
-  od = lopt.split(":")[1]
-  if od
-    return od
-  end
-  return nil
-end
-
-def setOption(name, value)
-  eval("$OPT_" + name + " = " + 'value')
-end
-
-def setDefaultOption(lopt)
-  d = getDefaultOption(lopt)
-  if d
-    setOption(getOptionName(lopt), d)
+  #
+  # set default
+  #
+  if single_opts then
+    single_opts.each_byte do |byte|
+      boolopts[ byte.chr ] = false
   end
 end
+  unless options.empty? then
+    single_colon = ''
 
-def setNewArgv(newargv)
-  ARGV.clear
-  for na in newargv
-    ARGV << na
-  end
-end
-
-
-def getopts(single_opts, *options)
-  if options
-    single_colon = ""
-    long_opts = []
-    sc = 0
-    for o in options
-      setDefaultOption(o)
-      if isSingle(o)
-	single_colon[sc, 0] = getOptionName(o)
-	sc += 1
+    options.each do |opt|
+      m = /\A([^:]+):(.*)\z/.match( opt )
+      if m then
+        valopts[ m[1] ] = m[2].empty? ? 0 : m[2]
       else
-	long_opts.push(o)
-      end
+        boolopts[ opt ] = false
+end
+  end
+    valopts.each do |opt, dflt|
+      if opt.size == 1 then
+        single_colon << opt
+end
+  end
+
+    if single_colon.empty? then
+      single_colon = single_colon_exp = nil
+      else
+      single_colon_exp = /[#{single_colon}]/
     end
   end
   
-  opts = {}
-  count = 0
-  newargv = []
-  while ARGV.length != 0
-    compare = nil
-    case ARGV[0]
-    when /^--?$/
-      ARGV.shift
-      newargv += ARGV
+  #
+  # scan
+  #
+  c = 0
+  arg = argv.shift
+  while arg do
+    case arg
+    when /\A--?\z/                      # xinit -- -bpp 24
+      newargv.concat argv
       break
-    when /^--.*/
-      compare = ARGV[0][2, (ARGV[0].length - 2)]
-      if long_opts != ""
-	for lo in long_opts
-	  if lo.index(":") && getOptionName(lo) == compare
-	    if ARGV.length <= 1
-	      return nil
-	    end
-	    setOption(compare, ARGV[1])
-	    opts[compare] = true
-	    ARGV.shift
-	    count += 1
-	    break
-	  elsif lo == compare
-	    setOption(compare, true)
-	    opts[compare] = true
-	    count += 1
-	    break
-	  end
-	end
+
+    when /\A--(.*)/
+      opt = $1
+      if valopts.key? opt  then         # imclean --src +trash
+        return nil if argv.empty?
+        valopts[ opt ] = argv.shift
+      elsif boolopts.key? opt then      # ruby --verbose
+        boolopts[ opt ] = true
+      else
+              return nil
+            end
+      c += 1
+
+    when /\A-(.+)/
+      arg = $1
+      0.upto( arg.size - 1 ) do |idx|
+        opt = arg[idx, 1]
+        if single_opts and single_opts_exp === opt then
+          boolopts[ opt ] = true        # ruby -h
+          c += 1
+
+        elsif single_colon and single_colon_exp === opt then
+          val = arg[ (idx+1)..-1 ]
+          if val.empty? then            # ruby -e 'p $:'
+            return nil if argv.empty?
+            valopts[ opt ] = argv.shift
+          else                          # cc -ohello ...
+            valopts[ opt ] = val
       end
-      if compare.length <= 1
-	return nil
+          c += 1
+
+          break
+          else
+          return nil
+          end
+        end
+
+    else                                # ruby test.rb
+      newargv.push arg
       end
-    when /^-.*/
-      for idx in 1..(ARGV[0].length - 1)
-	compare = ARGV[0][idx, 1]
-	if single_opts && compare =~ "[" + single_opts + "]"
-	  setOption(compare, true)
-	  opts[compare] = true
-	  count += 1
-	elsif single_colon != "" && compare =~ "[" + single_colon + "]"
-	  if ARGV[0][idx..-1].length > 1
-	    setOption(compare, ARGV[0][(idx + 1)..-1])
-	    opts[compare] = true
-	    count += 1
-	  elsif ARGV.length <= 1
-	    return nil
-	  else
-	    setOption(compare, ARGV[1])
-	    opts[compare] = true
-	    ARGV.shift
-	    count += 1
-	  end
-	  break
-	end
-      end
-    else
-      compare = ARGV[0]
-      opts[compare] = true
-      newargv << ARGV[0]
+
+    arg = argv.shift
     end
     
-    ARGV.shift
-    if !opts.has_key?(compare)
-      return nil
+  #
+  # set
+  #
+  boolopts.each do |opt, val|
+    eval "$OPT_#{opt} = val"
     end
+  valopts.each do |opt, val|
+    eval "$OPT_#{opt} = #{val == 0 ? 'nil' : 'val'}"
   end
-  setNewArgv(newargv)
-  return count
+  argv.replace newargv
+
+  c
 end

commit 4c9c1fe272a24cf3d11dc298e036f578eaf60ce4

Fix the position of rescue clause without exc_list
If the rescue clause has only exc_var and not exc_list, use the
exc_var position instead of the rescue body position.

This issue appears to have been introduced in
688169fd83b24564b653c03977c168cea50ccd35 when "opt_list" was split
into "exc_list exc_var".

Fixes [Bug #18974]

parse.y
@@ -4633,7 +4633,16 @@ opt_rescue	: k_rescue exc_list exc_var then
 			$$ = NEW_RESBODY($2,
 					 $3 ? block_append(p, node_assign(p, $3, NEW_ERRINFO(&@3), NO_LEX_CTXT, &@3), $5) : $5,
 					 $6, &@$);
-			fixpos($$, $2?$2:$5);
+
+                        if ($2) {
+                            fixpos($$, $2);
+                        }
+                        else if ($3) {
+                            fixpos($$, $3);
+                        }
+                        else {
+                            fixpos($$, $5);
+                        }
 		    /*% %*/
 		    /*% ripper: rescue!(escape_Qundef($2), escape_Qundef($3), escape_Qundef($5), escape_Qundef($6)) %*/
 		    }