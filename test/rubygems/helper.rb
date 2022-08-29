4bc782ed87d05d251d1656ca08e176f8bb56a448
Yusuke Endoh <mame@ruby-lang.org>
2022-08-29 20:23:51 +0900

    Revert "Chect what remains in TMPDIR"
    This reverts commit 7bdb999d0f28c7bb9d7a35ca775e405674527e5f.
    
    I think I confirmed the mechanism. GCC (invoked by MJIT) creates a
    temporary file in TMPDIR, which prevents rm_rf from removing the
    directory.

--- test/rubygems/helper.rb
+++ test/rubygems/helper.rb
@@ -466,18 +466,13 @@ class Gem::TestCase < Test::Unit::TestCase
     Dir.chdir @current_dir
 
     # Prevent a race condition on removing TMPDIR being written by MJIT
-    #if defined?(RubyVM::MJIT.enabled?) && RubyVM::MJIT.enabled?
-    #  RubyVM::MJIT.pause(wait: false)
-    #end
-    begin
-      FileUtils.rm_rf @tempdir
-    ensure
-      # mame: Temporal code for debugging. Let me confirm what remains in the directory
-      pp Dir.glob(File.join(@tempdir, "**", "{.*,*}")) if $!
+    if defined?(RubyVM::MJIT.enabled?) && RubyVM::MJIT.enabled?
+      RubyVM::MJIT.pause(wait: false)
     end
-    #if defined?(RubyVM::MJIT.enabled?) && RubyVM::MJIT.enabled?
-    #  RubyVM::MJIT.resume
-    #end
+    FileUtils.rm_rf @tempdir
+    if defined?(RubyVM::MJIT.enabled?) && RubyVM::MJIT.enabled?
+      RubyVM::MJIT.resume
+    end
 
     ENV.replace(@orig_env)
 
