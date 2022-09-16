c8d94d2797f798e2666a057bb1940e1ffe41b717
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-09-16 14:46:21 +0900

    Now test-bundler nees fake.rb

--- common.mk
+++ common.mk
@@ -1461,7 +1461,7 @@ no-test-syntax-suggest:
 
 test-bundler-precheck: $(TEST_RUNNABLE)-test-bundler-precheck
 no-test-bundler-precheck:
-yes-test-bundler-precheck: main
+yes-test-bundler-precheck: main $(arch)-fake.rb
 
 no-test-bundler-prepare: no-test-bundler-precheck
 yes-test-bundler-prepare: yes-test-bundler-precheck
