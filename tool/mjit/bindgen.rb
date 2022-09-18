fe3fc346c362e81dabe6e437e742433d02dcce22
Takashi Kokubun <takashikkbn@gmail.com>
2022-09-18 23:25:20 +0900

    Automatically setup bundler of bindgen.rb
    to easily use it with `tool/mjit/bindgen.rb BUILDDIR` instead of using
    `make mjit-bindgen`.

--- tool/mjit/bindgen.rb
+++ tool/mjit/bindgen.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('./Gemfile', __dir__)
+require 'bundler/setup'
+
 require 'etc'
 require 'fiddle/import'
 require 'set'
