3e93291571221f81326237c954e22588d2fbc98d
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-09-27 10:10:29 +0900

    [Feature #18639] Follow up spec tests

--- spec/ruby/library/rbconfig/unicode_version_spec.rb
+++ spec/ruby/library/rbconfig/unicode_version_spec.rb
@@ -16,8 +16,8 @@ describe "RbConfig::CONFIG['UNICODE_VERSION']" do
 
   # Caution: ruby_version_is means is_or_later
   ruby_version_is "3.2" do
-    it "is 14.0.0" do
-      RbConfig::CONFIG['UNICODE_VERSION'].should == "14.0.0"
+    it "is 15.0.0" do
+      RbConfig::CONFIG['UNICODE_VERSION'].should == "15.0.0"
     end
   end
 end
