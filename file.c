8218cb73ba0b4c07d977fbf6e9fd02e1928288b7
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-10-03 22:59:22 +0900

    [Bug #19034] No runtime check for `utimensat` if unavailable

--- file.c
+++ file.c
@@ -2873,7 +2873,9 @@ utime_failed(struct apply_arg *aa)
 
 #if defined(HAVE_UTIMES)
 
-# if defined(__APPLE__) && \
+# if !defined(HAVE_UTIMENSAT)
+/* utimensat() is not found, runtime check is not needed */
+# elif defined(__APPLE__) && \
     (!defined(MAC_OS_X_VERSION_13_0) || (MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_13_0))
 
 #   if defined(__has_attribute) && __has_attribute(availability)
