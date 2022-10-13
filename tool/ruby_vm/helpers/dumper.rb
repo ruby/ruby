751ffb276f658518c6fe06461a9d3d1c136c7d5d
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-10-13 12:10:10 +0900

    FreeBSD make uses the target under srcdir [ci skip]

--- tool/ruby_vm/helpers/dumper.rb
+++ tool/ruby_vm/helpers/dumper.rb
@@ -28,7 +28,7 @@ class RubyVM::Dumper
     path  = Pathname.new(__FILE__)
     path  = (path.relative_path_from(Pathname.pwd) rescue path).dirname
     path += '../views'
-    path += Pathname.pwd.join(spec).expand_path.to_s.sub("#{@base}/", '')
+    path += Pathname.pwd.join(File.basename(spec)).expand_path.to_s.sub("#{@base}/", '')
     src   = path.expand_path.read mode: 'rt:utf-8:utf-8'
   rescue Errno::ENOENT
     raise "don't know how to generate #{path}"
