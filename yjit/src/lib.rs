12c30f215cfadb8feb678c659941d4cfe5310785
Jimmy Miller <jimmy.miller@shopify.com>
2022-10-18 16:03:55 -0400

    Fixes remaining issues and turns back on dead code and unused (#6584)

--- yjit/src/lib.rs
+++ yjit/src/lib.rs
@@ -3,9 +3,6 @@
 #![allow(clippy::too_many_arguments)] // :shrug:
 #![allow(clippy::identity_op)] // Sometimes we do it for style
 
-// Temporary while switching to the new backend
-#![allow(dead_code)]
-#![allow(unused)]
 
 mod asm;
 mod backend;
