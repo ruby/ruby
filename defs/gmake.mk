4bb62ee57285da427d73ba30e7d76f0f377c9936
nagachika <nagachika@ruby-lang.org>
2022-10-20 21:33:39 +0900

    merge revision(s) 6a8f1a9e5cd1c9c2b3c6925d8d3fa76a29dabf73:
    	Copy from bundled gem source for test
    
    	---
    	 common.mk          | 13 +++++++----
    	 defs/gmake.mk      |  4 ++++
    	 tool/gem-unpack.rb | 63 +++++++++++++++++++++++++++++++++++-------------------
    	 3 files changed, 54 insertions(+), 26 deletions(-)

--- defs/gmake.mk
+++ defs/gmake.mk
@@ -289,6 +289,9 @@ gems/%.gem:
 	    -e 'File.unlink(*old) and' \
 	    -e 'FileUtils.rm_rf(old.map{'"|n|"'n.chomp(".gem")})'
 
+ifeq (,)
+extract-gems: extract-gems-sequential
+else
 extract-gems: | $(patsubst %,.bundle/gems/%,$(bundled-gems))
 
 .bundle/gems/%: gems/%.gem | .bundle/gems
@@ -299,6 +302,7 @@ extract-gems: | $(patsubst %,.bundle/gems/%,$(bundled-gems))
 
 $(srcdir)/.bundle/gems:
 	$(MAKEDIRS) $@
+endif
 
 ifneq ($(filter update-bundled_gems refresh-gems,$(MAKECMDGOALS)),)
 update-gems: update-bundled_gems
