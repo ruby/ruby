6d835901575d58e7db404665801a1c455ee982a8
Peter Zhu <peter@peterzhu.ca>
2022-11-04 15:36:30 -0400

    Don't report changed ENV caused by Bundler
    Bundler's backups changes environment variables starting with
    BUNDLER_ORIG_. This causes a lot of noise in tests as the leakchecker
    reports them as changed.

--- tool/lib/leakchecker.rb
+++ tool/lib/leakchecker.rb
@@ -234,6 +234,9 @@ class LeakChecker
     new_env = find_env
     return false if old_env == new_env
     (old_env.keys | new_env.keys).sort.each {|k|
+      # Don't report changed environment variables caused by Bundler's backups
+      next if k.start_with?(Bundler::EnvironmentPreserver::BUNDLER_PREFIX)
+
       if old_env.has_key?(k)
         if new_env.has_key?(k)
           if old_env[k] != new_env[k]
