570dee15a6939f1a3c8cf301202a1c508c2c2a33
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-11-04 15:17:49 +0900

    sync_default_gems.rb: accept log input from other than STDIN

--- tool/sync_default_gems.rb
+++ tool/sync_default_gems.rb
@@ -418,8 +418,8 @@ IGNORE_FILE_PATTERN =
   |rakelib\/.*
   )\z/mx
 
-def message_filter(repo, sha)
-  log = STDIN.read
+def message_filter(repo, sha, input: ARGF)
+  log = input.read
   log.delete!("\r")
   url = "https://github.com/#{repo}"
   subject, log = log.split(/\n(?:[\s\t]*(?:\n|\z))/, 2)
@@ -659,8 +659,10 @@ when "list"
   end
 when "--message-filter"
   ARGV.shift
-  abort unless ARGV.size == 2
-  message_filter(*ARGV)
+  if ARGV.size < 2
+    abort "usage: #{$0} --message-filter repository commit-hash [input...]"
+  end
+  message_filter(*ARGV.shift(2))
   exit
 when "rdoc-ref"
   ARGV.shift
