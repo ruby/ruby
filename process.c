2c7449c4526e41e98f204ee0edd8e35634b6344b
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-11-19 11:23:51 +0900

    Remove trailing spaces

--- process.c
+++ process.c
@@ -3146,7 +3146,7 @@ NORETURN(static VALUE f_exec(int c, const VALUE *a, VALUE _));
  *
  *  * no meta characters,
  *  * not starting with shell reserved word or special built-in,
- *  
+ *
  *  Ruby invokes the command directly without shell.
  *
  *  You can force shell invocation by adding ";" to the string (because ";" is
