77f963004f9982c70adde487587cf747503be314
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-11-22 13:58:56 +0900

    Avoid unnecessary use of global variables

--- test/ruby/test_pack.rb
+++ test/ruby/test_pack.rb
@@ -4,22 +4,22 @@ require 'test/unit'
 
 class TestPack < Test::Unit::TestCase
   def test_pack
-    $format = "c2x5CCxsdils_l_a6";
+    format = "c2x5CCxsdils_l_a6";
     # Need the expression in here to force ary[5] to be numeric.  This avoids
     # test2 failing because ary2 goes str->numeric->str and ary does not.
     ary = [1,-100,127,128,32767,987.654321098 / 100.0,12345,123456,-32767,-123456,"abcdef"]
-    $x = ary.pack($format)
-    ary2 = $x.unpack($format)
+    x = ary.pack(format)
+    ary2 = x.unpack(format)
 
     assert_equal(ary.length, ary2.length)
     assert_equal(ary.join(':'), ary2.join(':'))
-    assert_match(/def/, $x)
+    assert_match(/def/, x)
 
-    $x = [-1073741825]
-    assert_equal($x, $x.pack("q").unpack("q"))
+    x = [-1073741825]
+    assert_equal(x, x.pack("q").unpack("q"))
 
-    $x = [-1]
-    assert_equal($x, $x.pack("l").unpack("l"))
+    x = [-1]
+    assert_equal(x, x.pack("l").unpack("l"))
   end
 
   def test_pack_n
