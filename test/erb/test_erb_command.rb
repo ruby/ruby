1c0c1c8098bfb989ff5288b2e0a43b1bd0be2781
Takashi Kokubun <takashikkbn@gmail.com>
2022-11-24 14:47:11 -0800

    [ruby/erb] Drop deprecated -S option from erb command
    We attempted to remove things like this and reverted all of them, but I
    think nobody relies on this unlike the positional arguments of
    `ERB#initialize`.
    
    https://github.com/ruby/erb/commit/92fde7e403

--- test/erb/test_erb_command.rb
+++ test/erb/test_erb_command.rb
@@ -17,17 +17,4 @@ class TestErbCommand < Test::Unit::TestCase
                        File.expand_path("../../libexec/erb", __dir__)],
                       "<%=''.encoding.to_s%>", ["UTF-8"])
   end
-
-  # These interfaces will be removed at Ruby 2.7.
-  def test_deprecated_option
-    pend if RUBY_ENGINE == 'truffleruby'
-    warnings = [
-      "warning: -S option of erb command is deprecated. Please do not use this.",
-      /\n.+\/libexec\/erb:\d+: warning: Passing safe_level with the 2nd argument of ERB\.new is deprecated\. Do not use it, and specify other arguments as keyword arguments\.\n/,
-    ]
-    assert_in_out_err(["-I#{File.expand_path('../../lib', __dir__)}",
-                       File.expand_path("../../libexec/erb", __dir__),
-                       "-S", "0"],
-                      "hoge", ["hoge"], warnings)
-  end
 end
