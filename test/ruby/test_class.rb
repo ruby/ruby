ruby/ruby

commit e7b1ff984fde859a7778dec564731eb79392406f

RCLASS uses FLUSER bits 0 through 3

internal/class.h
@@ -122,10 +122,10 @@ typedef struct rb_classext_struct rb_classext_t;
 #define RCLASS_SUPERCLASS_DEPTH(c) (RCLASS_EXT(c)->superclass_depth)
 #define RCLASS_SUPERCLASSES(c) (RCLASS_EXT(c)->superclasses)
 
-#define RICLASS_IS_ORIGIN FL_USER5
-#define RCLASS_CLONED     FL_USER6
-#define RCLASS_SUPERCLASSES_INCLUDE_SELF FL_USER7
-#define RICLASS_ORIGIN_SHARED_MTBL FL_USER8
+#define RICLASS_IS_ORIGIN FL_USER0
+#define RCLASS_CLONED     FL_USER1
+#define RCLASS_SUPERCLASSES_INCLUDE_SELF FL_USER2
+#define RICLASS_ORIGIN_SHARED_MTBL FL_USER3
 
 /* class.c */
 void rb_class_subclass_add(VALUE super, VALUE klass);

commit 597ab71ac1ae0d213b83fd3671f929286f212969

Fix Object#freeze also freezing part of the class
[Bug #19164]

The bug was introduced in e7b1ff984fde859a7778dec564731eb79392406f

test/ruby/test_class.rb
@@ -809,4 +809,13 @@ PREP
 3_000_000.times(&code)
 CODE
   end
+
+  def test_instance_freeze_dont_freeze_the_class_bug_19164
+    klass = Class.new
+    klass.prepend(Module.new)
+
+    klass.new.freeze
+    klass.define_method(:bar) {}
+    assert_equal klass, klass.remove_method(:bar), '[Bug #19164]'
+  end
 end