06cd77c1b53bac833d985fdd1c12baf43c1ab965
Lars Kanis <lars@greiz-reinsdorf.de>
2022-12-18 21:05:54 +0100

    Windows: Prefer USERPROFILE over HOMEPATH
    HOMEPATH is set to "\WINDOWS\system32" when running per "runas" session.
    This directory is not writable by ordinary users, leading to errors with many ruby tools.
    Also config files in the home directory are not recognized.
    
    Still keeping HOME at first which is not used by native Windows,
    but by ruby specs and by MSYS2 environment.

--- spec/ruby/core/dir/home_spec.rb
+++ spec/ruby/core/dir/home_spec.rb
@@ -21,6 +21,23 @@ describe "Dir.home" do
     end
   end
 
+  platform_is :windows do
+    it "retrieves the directory from HOME, USERPROFILE, HOMEDRIVE/HOMEPATH and the WinAPI in that order" do
+      old_dirs = [ENV.delete('HOME'), ENV.delete('USERPROFILE'), ENV.delete('HOMEDRIVE'), ENV.delete('HOMEPATH')]
+
+      Dir.home.should == old_dirs[1].gsub("\\", "/")
+      ENV['HOMEDRIVE'] = "C:"
+      ENV['HOMEPATH'] = "\\rubyspec\\home1"
+      Dir.home.should == "C:/rubyspec/home1"
+      ENV['USERPROFILE'] = "C:\\rubyspec\\home2"
+      Dir.home.should == "C:/rubyspec/home2"
+      ENV['HOME'] = "C:\\rubyspec\\home3"
+      Dir.home.should == "C:/rubyspec/home3"
+    ensure
+      ENV['HOME'], ENV['USERPROFILE'], ENV['HOMEDRIVE'], ENV['HOMEPATH'] = *old_dirs
+    end
+  end
+
   describe "when called with the current user name" do
     platform_is :solaris do
       it "returns the named user's home directory from the user database" do
