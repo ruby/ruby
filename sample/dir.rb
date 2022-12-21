e4f6edd3734d3ccc9429ef8322d17a52bde90241
Thomas R. Koll <tomk32@tomk32.de>
2022-12-21 08:42:47 +0100

    Refactor dir.rb sample
    The original (1998) sample with a for-loop and use of case/when isn't what we'd write nowadays

--- sample/dir.rb
+++ sample/dir.rb
@@ -1,12 +1,7 @@
 # directory access
 # list all files but .*/*~/*.o
-dirp = Dir.open(".")
-for f in dirp
-  case f
-  when /\A\./, /~\z/, /\.o\z/
-    # do not print
-  else
-    print f, "\n"
+Dir.open(".").each do |file|
+  unless file.match(/\A\./, /~\z/, /\.o\z/)
+    puts file
   end
 end
-dirp.close
