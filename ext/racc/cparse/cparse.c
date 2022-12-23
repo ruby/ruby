ba342088009619f5df84116a0567e93bf6c9181b
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-12-23 09:11:59 +0900

    Bump up Racc parser version

--- ext/racc/cparse/cparse.c
+++ ext/racc/cparse/cparse.c
@@ -22,7 +22,7 @@
                         Important Constants
 ----------------------------------------------------------------------- */
 
-#define RACC_VERSION "1.4.15"
+#define RACC_VERSION "1.6.1"
 
 #define DEFAULT_TOKEN -1
 #define ERROR_TOKEN    1
