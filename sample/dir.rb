7a5816366cf93f505158fc68ce35af3a5e37a064
Nobuyoshi Nakada <nobu@ruby-lang.org>
2022-12-26 21:37:40 +0900

    [DOC] Update sample/dir.rb [ci skip]
    Fix ArgumentError.

--- sample/dir.rb
+++ sample/dir.rb
@@ -1,7 +1,7 @@
 # directory access
 # list all files but .*/*~/*.o
 Dir.foreach(".") do |file|
-  unless file.match(/\A\./, /~\z/, /\.o\z/)
+  unless file.start_with?('.') or file.end_with?('~', '.o')
     puts file
   end
 end
