e112be82af4070269fb05953a56243aeee905fde
Takashi Kokubun <takashikkbn@gmail.com>
2023-01-06 09:44:31 -0800

    YJIT: Call it "YJIT entry point"
    Co-authored-by: Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>

--- yjit/src/codegen.rs
+++ yjit/src/codegen.rs
@@ -586,7 +586,7 @@ pub fn gen_entry_prologue(cb: &mut CodeBlock, iseq: IseqPtr, insn_idx: u32) -> O
 
     let mut asm = Assembler::new();
     if get_option_ref!(dump_disasm).is_some() {
-        asm.comment(&format!("YJIT entry: {}", iseq_get_location(iseq, 0)));
+        asm.comment(&format!("YJIT entry point: {}", iseq_get_location(iseq, 0)));
     } else {
         asm.comment("YJIT entry");
     }
