75b97d8af84b7b2e83b21e5f8bf719120ef76685
Kunshan Wang <wks1986@gmail.com>
2023-01-09 19:06:28 +0800

    Add doc comments to methods of GC::MMTk

--- gc.c
+++ gc.c
@@ -14990,27 +14990,64 @@ gc_using_rvargc_p(VALUE mod)
 }
 
 #if USE_MMTK
-VALUE
+/*
+ *  call-seq:
+ *      GC::MMTk.plan_name -> String
+ *
+ *  Returns the name of the current MMTk plan.
+ */
+static VALUE
 rb_mmtk_plan_name(VALUE _)
 {
     const char* plan_name = mmtk_plan_name();
     return rb_str_new(plan_name, strlen(plan_name));
 }
 
-VALUE
+/*
+ *  call-seq:
+ *      GC::MMTk.enabled? -> true or false
+ *
+ *  Returns true if using MMTk as garbage collector, false otherwise.
+ *
+ *  Note: If the Ruby interpreter is not compiled with MMTk support, the
+ *  <code>GC::MMTk</code> module will not exist in the first place.
+ *  You can check if the module exists by
+ *
+ *    defined? GC::MMTk
+ */
+static VALUE
 rb_mmtk_enabled(VALUE _)
 {
     return RBOOL(rb_mmtk_enabled_p());
 }
 
-VALUE
+/*
+ *  call-seq:
+ *      GC::MMTk.harness_begin
+ *
+ *  A hook to be called before a benchmark begins.
+ *
+ *  MMTk will do necessary preparations (such as triggering a full-heap GC)
+ *  and start collecting statistic data, such as the number of GC triggered,
+ *  time spent in GC, time spent in mutator, etc.
+ */
+static VALUE
 rb_mmtk_harness_begin(VALUE _)
 {
     mmtk_harness_begin((MMTk_VMMutatorThread)GET_THREAD());
     return Qnil;
 }
 
-VALUE
+/*
+ *  call-seq:
+ *      GC::MMTk.harness_end
+ *
+ *  A hook to be called after a benchmark ends.
+ *
+ *  When this method is called, MMTk will stop collecting statistic data and
+ *  print out the data already collected.
+ */
+static VALUE
 rb_mmtk_harness_end(VALUE _)
 {
     mmtk_harness_end((MMTk_VMMutatorThread)GET_THREAD());
