bac31b87dc6f442b5029bda6f3a6bee51707068d
Nobuyoshi Nakada <nobu@ruby-lang.org>
2023-01-10 21:19:58 +0900

    mkconfig: Map `includedir` only for system ruby
    Only when installing to the system path on macOS, prepend '$(SDKROOT)'
    and remap `includedir`.
    Fix https://github.com/rbenv/ruby-build/discussions/2123

--- test/mkmf/test_config.rb
+++ test/mkmf/test_config.rb
@@ -7,9 +7,9 @@ require 'mkmf'
 class TestMkmfConfig < Test::Unit::TestCase
   def test_dir_config
     bug8074 = '[Bug #8074]'
-    lib = RbConfig.expand(RbConfig::MAKEFILE_CONFIG["libdir"], "exec_prefix"=>"")
+    lib = RbConfig.expand(RbConfig::MAKEFILE_CONFIG["libdir"], "exec_prefix"=>"/test/foo")
     assert_separately %w[-rmkmf - -- --with-foo-dir=/test/foo], %{
-      assert_equal(%w[/test/foo/include /test/foo#{lib}], dir_config("foo"), #{bug8074.dump})
+      assert_equal(%w[/test/foo/include #{lib}], dir_config("foo"), #{bug8074.dump})
     }
   end
 end
