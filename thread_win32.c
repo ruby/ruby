704dd25812f34228a28f37c37757a675ad078a2b
Jean Boussier <byroot@ruby-lang.org>
2023-02-06 09:29:39 +0100

    TestThreadInstrumentation: emit the EXIT event sooner
    ```
      1) Failure:
    TestThreadInstrumentation#test_thread_instrumentation [/tmp/ruby/src/trunk-repeat20-asserts/test/-ext-/thread/test_instrumentation_api.rb:33]:
    Call counters[4]: [3, 4, 4, 4, 0].
    Expected 0 to be > 0.
    ```
    
    We fire the EXIT hook after the call to `thread_sched_to_dead` which
    mean another thread might be running before the `EXIT` hook have been
    executed.

--- thread_win32.c
+++ thread_win32.c
@@ -131,6 +131,8 @@ thread_sched_to_running(struct rb_thread_sched *sched, rb_thread_t *th)
     if (GVL_DEBUG) fprintf(stderr, "gvl acquire (%p): acquire\n", th);
 }
 
+#define thread_sched_to_dead thread_sched_to_waiting
+
 static void
 thread_sched_to_waiting(struct rb_thread_sched *sched)
 {
