4339aade1c702406ae453270f7e995d4aeb714b9
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-02-07 16:20:05 +0900

    Merge URI-0.12.0

--- test/uri/test_parser.rb
+++ test/uri/test_parser.rb
@@ -7,6 +7,11 @@ class URI::TestParser < Test::Unit::TestCase
     uri.class.component.collect {|c| uri.send(c)}
   end
 
+  def test_inspect
+    assert_match(/URI::RFC2396_Parser/, URI::Parser.new.inspect)
+    assert_match(/URI::RFC3986_Parser/, URI::RFC3986_Parser.new.inspect)
+  end
+
   def test_compare
     url = 'http://a/b/c/d;p?q'
     u0 = URI.parse(url)
@@ -45,6 +50,15 @@ class URI::TestParser < Test::Unit::TestCase
     assert_raise(URI::InvalidURIError) { URI.parse('https://www.example.com/search?q=%XX') }
   end
 
+  def test_parse_auth
+    str = "http://al%40ice:p%40s%25sword@example.com/dir%2Fname/subdir?foo=bar%40example.com"
+    uri = URI.parse(str)
+    assert_equal "al%40ice", uri.user
+    assert_equal "p%40s%25sword", uri.password
+    assert_equal "al@ice", uri.decoded_user
+    assert_equal "p@s%sword", uri.decoded_password
+  end
+
   def test_raise_bad_uri_for_integer
     assert_raise(URI::InvalidURIError) do
       URI.parse(1)
@@ -57,5 +71,12 @@ class URI::TestParser < Test::Unit::TestCase
     assert_equal("\xe3\x83\x90", p1.unescape('%e3%83%90'))
     assert_equal("\u3042", p1.unescape('%e3%81%82'.force_encoding(Encoding::US_ASCII)))
     assert_equal("\xe3\x83\x90\xe3\x83\x90", p1.unescape("\xe3\x83\x90%e3%83%90"))
+  end
+
+  def test_split
+    assert_equal(["http", nil, "example.com", nil, nil, "", nil, nil, nil], URI.split("http://example.com"))
+    assert_equal(["http", nil, "[0::0]", nil, nil, "", nil, nil, nil], URI.split("http://[0::0]"))
+    assert_equal([nil, nil, "example.com", nil, nil, "", nil, nil, nil], URI.split("//example.com"))
+    assert_equal([nil, nil, "[0::0]", nil, nil, "", nil, nil, nil], URI.split("//[0::0]"))
   end
 end
