18e840ac6084715e1ccd4c84153d213583279691
Sutou Kouhei <kou@clear-code.com>
2023-02-08 09:33:09 +0900

    [ruby/strscan] Bump version
    https://github.com/ruby/strscan/commit/681cde0f27

--- ext/strscan/strscan.c
+++ ext/strscan/strscan.c
@@ -22,7 +22,7 @@ extern size_t onig_region_memsize(const struct re_registers *regs);
 
 #include <stdbool.h>
 
-#define STRSCAN_VERSION "3.0.6"
+#define STRSCAN_VERSION "3.0.7"
 
 /* =======================================================================
                          Data Type Definitions
