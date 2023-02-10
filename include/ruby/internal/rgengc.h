a532eca38d27559402884764867e7444fad7dd79
Jean Boussier <byroot@ruby-lang.org>
2023-02-10 14:32:46 +0100

    Implement Write Barrier for RMatch objects
    They only have two references.

--- include/ruby/internal/rgengc.h
+++ include/ruby/internal/rgengc.h
@@ -133,6 +133,17 @@
  * @private
  *
  * This  is   a  compile-time   flag  to   enable/disable  write   barrier  for
+ * struct ::RMatch.  It has to be set at the time ruby itself compiles.  Makes
+ * no sense for 3rd parties.
+ */
+#ifndef RGENGC_WB_PROTECTED_MATCH
+# define RGENGC_WB_PROTECTED_MATCH 1
+#endif
+
+/**
+ * @private
+ *
+ * This  is   a  compile-time   flag  to   enable/disable  write   barrier  for
  * struct ::RClass.  It has to be set  at the time ruby itself compiles.  Makes
  * no sense for 3rd parties.
  */
