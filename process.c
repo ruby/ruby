a43ca15dd4a411c7feeb18240eef1db02e673edc
zverok <zverok.offline@gmail.com>
2023-02-15 21:28:06 +0200

    [DOC] Restore Process module doc lost due to RDoc update

--- process.c
+++ process.c
@@ -494,24 +494,6 @@ parent_redirect_close(int fd)
 #define parent_redirect_close(fd) close_unless_reserved(fd)
 #endif
 
-/*
- * Document-module: Process
- *
- * The module contains several groups of functionality for handling OS processes:
- *
- * * Low-level property introspection and management of the current process, like
- *   Process.argv0, Process.pid;
- * * Low-level introspection of other processes, like Process.getpgid, Process.getpriority;
- * * Management of the current process: Process.abort, Process.exit, Process.daemon, etc.
- *   (for convenience, most of those are also available as global functions
- *   and module functions of Kernel);
- * * Creation and management of child processes: Process.fork, Process.spawn, and
- *   related methods;
- * * Management of low-level system clock: Process.times and Process.clock_gettime,
- *   which could be important for proper benchmarking and other elapsed
- *   time measurement tasks.
- */
-
 static VALUE
 get_pid(void)
 {
@@ -8774,8 +8756,21 @@ static VALUE rb_mProcID_Syscall;
 
 
 /*
- *  The Process module is a collection of methods used to
- *  manipulate processes.
+ * Document-module: Process
+ *
+ *   The module contains several groups of functionality for handling OS processes:
+ *
+ *   * Low-level property introspection and management of the current process, like
+ *     Process.argv0, Process.pid;
+ *   * Low-level introspection of other processes, like Process.getpgid, Process.getpriority;
+ *   * Management of the current process: Process.abort, Process.exit, Process.daemon, etc.
+ *     (for convenience, most of those are also available as global functions
+ *     and module functions of Kernel);
+ *   * Creation and management of child processes: Process.fork, Process.spawn, and
+ *     related methods;
+ *   * Management of low-level system clock: Process.times and Process.clock_gettime,
+ *     which could be important for proper benchmarking and other elapsed
+ *     time measurement tasks.
  */
 
 void
