1ddda4c0d7d2b83658ab149e36e088961ca0b2dc
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-02-22 09:44:53 +0900

    [ruby/readline-ext] Import EnvUtils and related test libraries
    https://github.com/ruby/readline-ext/commit/f034697a48

--- test/readline/helper.rb
+++ test/readline/helper.rb
@@ -1,4 +1,12 @@
 begin
+  # for standalone test suite on ruby/logger
+  require 'test/unit'
+  require_relative '../lib/core_assertions'
+  Test::Unit::TestCase.include Test::Unit::CoreAssertions
+rescue LoadError
+end
+
+begin
   require "readline.so"
   ReadlineSo = Readline
 rescue LoadError
