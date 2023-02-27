283c71eeffc02da6928d7a14f42a0d38d00f592d
Takashi Kokubun <takashikkbn@gmail.com>
2023-02-27 12:53:03 -0800

    Skip a spec failing on master
    https://github.com/ruby/ruby/actions/runs/4286361460/jobs/7466545010
    http://ci.rvm.jp/results/trunk_gcc9@ruby-sp2-docker/4462424
    http://ci.rvm.jp/results/trunk_clang_13@ruby-sp2-docker/4462422

--- spec/ruby/core/io/read_spec.rb
+++ spec/ruby/core/io/read_spec.rb
@@ -67,12 +67,14 @@ describe "IO.read" do
   end
 
   platform_is_not :windows do
-    it "uses an :open_args option" do
-      string = IO.read(@fname, nil, 0, open_args: ["r", nil, {encoding: Encoding::US_ASCII}])
-      string.encoding.should == Encoding::US_ASCII
+    ruby_version_is ""..."3.3" do
+      it "uses an :open_args option" do
+        string = IO.read(@fname, nil, 0, open_args: ["r", nil, {encoding: Encoding::US_ASCII}])
+        string.encoding.should == Encoding::US_ASCII
 
-      string = IO.read(@fname, nil, 0, open_args: ["r", nil, {}])
-      string.encoding.should == Encoding::UTF_8
+        string = IO.read(@fname, nil, 0, open_args: ["r", nil, {}])
+        string.encoding.should == Encoding::UTF_8
+      end
     end
   end
 
