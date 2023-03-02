6207a3f588ae0d0f7eb6303f7cccbe19f37ba6d1
Koichi Sasada <ko1@atdot.net>
2023-03-02 15:30:27 +0900

    Enable flaky ractor test
    I hope a4421bd73c286253311c2cdf8c78ed258f8cff44 will solve the issue...

--- bootstraptest/test_ractor.rb
+++ bootstraptest/test_ractor.rb
@@ -284,8 +284,7 @@ assert_equal 30.times.map { 'ok' }.to_s, %q{
     test i
   }
 } unless ENV['RUN_OPTS'] =~ /--mjit-call-threshold=5/ || # This always fails with --mjit-wait --mjit-call-threshold=5
-  (ENV.key?('TRAVIS') && ENV['TRAVIS_CPU_ARCH'] == 'arm64') || # https://bugs.ruby-lang.org/issues/17878
-  true # too flaky everywhere http://ci.rvm.jp/results/trunk@ruby-sp1/4321096
+  (ENV.key?('TRAVIS') && ENV['TRAVIS_CPU_ARCH'] == 'arm64') # https://bugs.ruby-lang.org/issues/17878
 
 # Exception for empty select
 assert_match /specify at least one ractor/, %q{
