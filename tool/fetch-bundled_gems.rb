26c86bea1a019944a37a78a1459ef9bd6f00efd4
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-03-08 15:45:58 +0900

    Check bundled_gems version and gemspec version strictly

--- tool/fetch-bundled_gems.rb
+++ tool/fetch-bundled_gems.rb
@@ -23,6 +23,15 @@ end
 if r
   puts "fetching #{r} ..."
   system("git", "fetch", "origin", r, chdir: n) or abort
+
+  unless File.exist? "#{n}/#{n}.gemspec"
+    require_relative "lib/bundled_gem"
+    BundledGem.dummy_gemspec("#{n}/#{n}.gemspec")
+  end
+
+  # Check bundled_gems version and gemspec version same as BundledGem.build
+  spec = Gem::Specification.load("#{n}/#{n}.gemspec")
+  abort "Unexpected version #{spec.version}" unless spec.version == Gem::Version.new(v)
 end
 c = r || "v#{v}"
 checkout = %w"git -c advice.detachedHead=false checkout"
