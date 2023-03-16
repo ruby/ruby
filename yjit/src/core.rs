e1b6553c67ce01ea29280db895d566ee9b93e4dd
Takashi Kokubun <takashikkbn@gmail.com>
2023-03-16 11:07:03 -0700

    Move iseq_pc_to_insn_idx to cruby.rs

--- yjit/src/core.rs
+++ yjit/src/core.rs
@@ -1803,12 +1803,6 @@ fn gen_block_series_body(
     Some(first_block)
 }
 
-// Given an ISEQ pointer, convert PC to insn_idx
-fn iseq_pc_to_insn_idx(iseq: IseqPtr, pc: *mut VALUE) -> Option<u16> {
-    let pc_zero = unsafe { rb_iseq_pc_at_idx(iseq, 0) };
-    unsafe { pc.offset_from(pc_zero) }.try_into().ok()
-}
-
 /// Generate a block version that is an entry point inserted into an iseq
 /// NOTE: this function assumes that the VM lock has been taken
 pub fn gen_entry_point(iseq: IseqPtr, ec: EcPtr) -> Option<CodePtr> {
--- yjit/src/cruby.rs
+++ yjit/src/cruby.rs
@@ -244,6 +244,12 @@ pub struct VALUE(pub usize);
 /// Pointer to an ISEQ
 pub type IseqPtr = *const rb_iseq_t;
 
+// Given an ISEQ pointer, convert PC to insn_idx
+pub fn iseq_pc_to_insn_idx(iseq: IseqPtr, pc: *mut VALUE) -> Option<u16> {
+    let pc_zero = unsafe { rb_iseq_pc_at_idx(iseq, 0) };
+    unsafe { pc.offset_from(pc_zero) }.try_into().ok()
+}
+
 /// Opaque execution-context type from vm_core.h
 #[repr(C)]
 pub struct rb_execution_context_struct {
