ebebc90ec297c945cdf94c90f8db85dd7ddbcb7b
David Rodríguez <deivid.rodriguez@riseup.net>
2023-03-16 20:11:18 +0100

    Refactor incomplete specs handling
    Recent bugs fixed made me realize we were relying on state too much
    here. We only need to keep incomplete specs to be able to expire them
    and retry resolution without them locked. If we use a separate class, we
    can do that more transparently and handle them just like we handle
    "missing specs".

--- lib/bundler.rb
+++ lib/bundler.rb
@@ -62,6 +62,7 @@ module Bundler
   autoload :GemHelpers,             File.expand_path("bundler/gem_helpers", __dir__)
   autoload :GemVersionPromoter,     File.expand_path("bundler/gem_version_promoter", __dir__)
   autoload :Graph,                  File.expand_path("bundler/graph", __dir__)
+  autoload :IncompleteSpecification, File.expand_path("bundler/incomplete_specification", __dir__)
   autoload :Index,                  File.expand_path("bundler/index", __dir__)
   autoload :Injector,               File.expand_path("bundler/injector", __dir__)
   autoload :Installer,              File.expand_path("bundler/installer", __dir__)
