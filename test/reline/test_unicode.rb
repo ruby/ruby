60ca800d4fc410ea9d49ef24dfb80577d4183f15
tomoya ishida <tomoyapenguin@gmail.com>
2023-03-26 00:01:30 +0900

    [ruby/reline] Fix split_by_width to retain color sequences (https://github.com/ruby/reline/pull/490)
    * Fix split_by_width to retain color sequences
    
    * Add OSC escape sequence testcase of Reline::Unicode.split_by_width

--- test/reline/test_unicode.rb
+++ test/reline/test_unicode.rb
@@ -18,6 +18,15 @@ class Reline::Unicode::Test < Reline::TestCase
     assert_equal 2, Reline::Unicode.calculate_width('√', true)
   end
 
+  def test_split_by_width
+    assert_equal [['abc', nil, 'de'], 2], Reline::Unicode.split_by_width('abcde', 3)
+    assert_equal [['abc', nil, 'def', nil, ''], 3], Reline::Unicode.split_by_width('abcdef', 3)
+    assert_equal [['ab', nil, 'あd', nil, 'ef'], 3], Reline::Unicode.split_by_width('abあdef', 3)
+    assert_equal [["ab\1zero\2c", nil, 'def', nil, ''], 3], Reline::Unicode.split_by_width("ab\1zero\2cdef", 3)
+    assert_equal [["\e[31mabc", nil, "\e[31md\e[42mef", nil, "\e[31m\e[42mg"], 3], Reline::Unicode.split_by_width("\e[31mabcd\e[42mefg", 3)
+    assert_equal [["ab\e]0;1\ac", nil, "\e]0;1\ad"], 2], Reline::Unicode.split_by_width("ab\e]0;1\acd", 3)
+  end
+
   def test_take_range
     assert_equal 'cdef', Reline::Unicode.take_range('abcdefghi', 2, 4)
     assert_equal 'いう', Reline::Unicode.take_range('あいうえお', 2, 4)
