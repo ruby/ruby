f8c775cb413724a45bb5641de94f4f9210044116
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-03-28 20:36:36 +0900

    Use URI-0.12.1 for Ruby 3.2 (#7603)
    * Merge URI-0.12.1
    
    * Use URI-0.12.1
    
    * Update core_assertions for using assert_linear_performance

--- lib/bundler/vendor/uri/lib/uri/version.rb
+++ lib/bundler/vendor/uri/lib/uri/version.rb
@@ -1,6 +1,6 @@
 module Bundler::URI
   # :stopdoc:
-  VERSION_CODE = '001200'.freeze
+  VERSION_CODE = '001201'.freeze
   VERSION = VERSION_CODE.scan(/../).collect{|n| n.to_i}.join('.').freeze
   # :startdoc:
 end
