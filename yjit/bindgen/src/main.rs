cbf04e0aad8ae37838bcecb71468dc2759cf4022
Takashi Kokubun <takashikkbn@gmail.com>
2023-04-03 11:20:37 -0700

    YJIT: Add codegen for Array#<<

--- yjit/bindgen/src/main.rs
+++ yjit/bindgen/src/main.rs
@@ -136,6 +136,7 @@ fn main() {
         .allowlist_function("rb_ary_resurrect")
         .allowlist_function("rb_ary_clear")
         .allowlist_function("rb_ary_dup")
+        .allowlist_function("rb_ary_push")
         .allowlist_function("rb_ary_unshift_m")
         .allowlist_function("rb_yjit_rb_ary_subseq_length")
 
--- yjit/src/cruby_bindings.inc.rs
+++ yjit/src/cruby_bindings.inc.rs
@@ -1094,6 +1094,7 @@ extern "C" {
     pub fn rb_ary_store(ary: VALUE, key: ::std::os::raw::c_long, val: VALUE);
     pub fn rb_ary_dup(ary: VALUE) -> VALUE;
     pub fn rb_ary_resurrect(ary: VALUE) -> VALUE;
+    pub fn rb_ary_push(ary: VALUE, elem: VALUE) -> VALUE;
     pub fn rb_ary_clear(ary: VALUE) -> VALUE;
     pub fn rb_hash_new() -> VALUE;
     pub fn rb_hash_aref(hash: VALUE, key: VALUE) -> VALUE;
