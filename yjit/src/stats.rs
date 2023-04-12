ec02c15acf5c46b62109f807a533b94939e55eb2
Jimmy Miller <jimmy.miller@shopify.com>
2023-04-12 15:46:17 -0400

    YJIT: Add support for rest with option and splat args

--- yjit/src/stats.rs
+++ yjit/src/stats.rs
@@ -263,7 +263,7 @@ make_counters! {
     send_iseq_has_rest_and_captured,
     send_iseq_has_rest_and_send,
     send_iseq_has_rest_and_kw_supplied,
-    send_iseq_has_rest_and_optional,
+    send_iseq_has_rest_opt_and_block,
     send_iseq_has_rest_and_splat_not_equal,
     send_is_a_class_mismatch,
     send_instance_of_class_mismatch,
