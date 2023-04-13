6a5c355e4e7deb76363f474041d00465be3b317b
Jean Boussier <byroot@ruby-lang.org>
2023-04-13 16:48:59 +0200

    Add RB_WARN_CATEGORY_DEFAULT_BITS
    Followup: ac123f167a364c3d7a43eca78d564e41f6dbb91e
    
    RB_WARN_CATEGORY_ALL_BITS is exposed in a public header, so it
    makes sense for it to be updated to contain all valid bits.
    
    Instead we introduce RB_WARN_CATEGORY_DEFAULT_BITS to list the
    categories that are enabled by default.

--- include/ruby/internal/error.h
+++ include/ruby/internal/error.h
@@ -53,9 +53,15 @@ typedef enum {
     /** Warning is for performance issues (not enabled by -w). */
     RB_WARN_CATEGORY_PERFORMANCE,
 
+    RB_WARN_CATEGORY_DEFAULT_BITS = (
+        (1U << RB_WARN_CATEGORY_DEPRECATED) |
+        (1U << RB_WARN_CATEGORY_EXPERIMENTAL) |
+        0),
+
     RB_WARN_CATEGORY_ALL_BITS = (
         (1U << RB_WARN_CATEGORY_DEPRECATED) |
         (1U << RB_WARN_CATEGORY_EXPERIMENTAL) |
+        (1U << RB_WARN_CATEGORY_PERFORMANCE) |
         0)
 } rb_warning_category_t;
 
