de2967d18f3537ed5d1ae8eea88e1e589c347fa0
Nobuyoshi Nakada <nobu@ruby-lang.org>
2023-04-29 02:09:55 +0900

    core_assertions.rb: Support old rubies
    Some symbol argument might not be accepted by Process.clock_gettime.

--- tool/lib/core_assertions.rb
+++ tool/lib/core_assertions.rb
@@ -731,8 +731,7 @@ eom
         CLOCK_MONOTONIC
       ].find do |clk|
         if Process.const_defined?(clk)
-          clk = clk.to_sym
-          begin
+          [clk.to_sym, Process.const_get(clk)].find do |clk|
             Process.clock_gettime(clk)
           rescue
             # Constants may be defined but not implemented, e.g., mingw.
