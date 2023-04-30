160f83ba553aa3ff9345cf9cd320bab5996e211f
Nobuyoshi Nakada <nobu@ruby-lang.org>
2023-04-30 22:53:24 +0900

    Relax timeout limit for FreeBSD again [ci skip]

--- test/ruby/test_process.rb
+++ test/ruby/test_process.rb
@@ -1543,7 +1543,7 @@ class TestProcess < Test::Unit::TestCase
       end
       t1 = Time.now
       diff = t1 - t0
-      sec = RUBY_PLATFORM =~ /freebsd/ ? sec * 2 : sec
+      sec *= 3 if RUBY_PLATFORM =~ /freebsd/
       assert_operator(diff, :<, sec,
                   ->{"#{bug11340}: #{diff} seconds to interrupt Process.wait"})
       f.puts
