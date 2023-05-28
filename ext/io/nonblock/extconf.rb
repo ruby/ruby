14abb799c93c8e5cb7ce36d829fb805f80397d4c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-05-28 17:40:33 +0900

    [ruby/io-nonblock] Remove usage of IO internals. (https://github.com/ruby/io-nonblock/pull/11)
    https://github.com/ruby/io-nonblock/commit/caa2b94d19

--- ext/io/nonblock/extconf.rb
+++ ext/io/nonblock/extconf.rb
@@ -2,6 +2,8 @@
 require 'mkmf'
 target = "io/nonblock"
 
+have_func("rb_io_descriptor")
+
 hdr = %w"fcntl.h"
 if have_macro("O_NONBLOCK", hdr) and
     (have_macro("F_GETFL", hdr) or have_macro("F_SETFL", hdr))
