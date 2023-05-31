74ad1cfeca5579d5a6f56f50356f052b0239a18a
yui-knk <spiketeika@gmail.com>
2023-05-31 21:02:04 +0900

    No need to define "BISON" on extconf.rb
    "BISON" is defined in "ext/ripper/depend".

--- ext/ripper/extconf.rb
+++ ext/ripper/extconf.rb
@@ -5,13 +5,6 @@ require 'mkmf'
 require 'rbconfig'
 
 def main
-  yacc = ENV["YACC"] || "bison"
-
-  # unless find_executable(yacc)
-  #   unless File.exist?('ripper.c') or File.exist?("#{$srcdir}/ripper.c")
-  #     raise 'missing bison; abort'
-  #   end
-  # end
   $objs = %w(ripper.o)
   $distcleanfiles.concat %w(ripper.y ripper.c eventids1.c eventids2table.c)
   $cleanfiles.concat %w(ripper.E ripper.output y.output .eventids2-check)
@@ -19,9 +12,7 @@ def main
   $defs << '-DRIPPER_DEBUG' if $debug
   $VPATH << '$(topdir)' << '$(top_srcdir)'
   $INCFLAGS << ' -I$(topdir) -I$(top_srcdir)'
-  create_makefile 'ripper' do |conf|
-    conf << "BISON = #{yacc}"
-  end
+  create_makefile 'ripper'
 end
 
 main
