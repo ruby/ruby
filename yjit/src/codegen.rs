1b3e6e966d1bdec370a85832c12b18a8436f2532
Alan Wu <XrXr@users.noreply.github.com>
2023-06-05 18:46:00 -0400

    YJIT: Avoid identity-based known-class guards for IO objects
    `IO#reopen` is very special in that it is able to change the class and
    singleton class of IO instances. In its presence, it is not correct to
    assume that IO instances has a stable class/singleton class and guard
    by comparing identity.

--- yjit/src/codegen.rs
+++ yjit/src/codegen.rs
@@ -3871,6 +3871,7 @@ fn jit_guard_known_klass(
     } else if unsafe {
         FL_TEST(known_klass, VALUE(RUBY_FL_SINGLETON as usize)) != VALUE(0)
             && sample_instance == rb_class_attached_object(known_klass)
+            && !rb_obj_is_kind_of(sample_instance, rb_cIO).test()
     } {
         // Singleton classes are attached to one specific object, so we can
         // avoid one memory access (and potentially the is_heap check) by
@@ -3882,6 +3883,8 @@ fn jit_guard_known_klass(
         // that its singleton class is empty, so we can't avoid the memory
         // access. As an example, `Object.new.singleton_class` is an object in
         // this situation.
+        // Also, guarding by identity is incorrect for IO objects because
+        // IO#reopen can be used to change the class and singleton class of IO objects!
         asm.comment("guard known object with singleton class");
         asm.cmp(obj_opnd, sample_instance.into());
         jit_chain_guard(JCC_JNE, jit, asm, ocb, max_chain_depth, counter);
