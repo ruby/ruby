c2f4b414804a4ccd3edeec87e6998beb60c002bb
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-06-15 07:02:03 +0900

    Merge rubygems/rubygems HEAD
      Pick from https://github.com/rubygems/rubygems/commit/7a7b234721c375874b7e22b1c5b14925b943f04e

--- tool/bundler/test_gems.rb
+++ tool/bundler/test_gems.rb
@@ -7,6 +7,8 @@ gem "webrick", "1.7.0"
 gem "rack-test", "~> 1.1"
 gem "compact_index", "~> 0.13.0"
 gem "sinatra", "~> 2.0"
+# for Ruby 2.6. bundler/spec/install/gemfile/sources_spec.rb is failed with sinatra-2.0.8.1 and tilt-2.2.0.
+gem "tilt", "~> 2.0.11"
 gem "rake", "13.0.1"
 gem "builder", "~> 3.2"
 gem "rb_sys"
