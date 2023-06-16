9e58105954bc86188bce84dcd8348dd22da156f4
Nobuyoshi Nakada <nobu@ruby-lang.org>
2023-06-16 17:31:47 +0900

    Suppress redefined macro warnings

--- parser_st.c
+++ parser_st.c
@@ -114,6 +114,7 @@
 #define NOT_RUBY 1
 #undef RUBY
 
+#undef MEMCPY
 #define MEMCPY(tab,p1,p2,type,n) (tab->functions->nonempty_memcpy((p1), (p2), sizeof(type), (n)))
 
 #include <stdio.h>
@@ -151,45 +152,83 @@
 #define ST_CHECK ST2_CHECK
 #define ST_REPLACE ST2_REPLACE
 
+#undef st_numcmp
 #define st_numcmp rb_parser_st_numcmp
+#undef st_numhash
 #define st_numhash rb_parser_st_numhash
+#undef st_free_table
 #define st_free_table rb_parser_st_free_table
 #define rb_st_hash_start rb_parser_st_hash_start
+#undef st_delete
 #define st_delete rb_parser_st_delete
+#undef st_foreach
 #define st_foreach rb_parser_st_foreach
+#undef st_init_numtable
 #define st_init_numtable rb_parser_st_init_numtable
+#undef st_init_table_with_size
 #define st_init_table_with_size rb_parser_st_init_table_with_size
+#undef st_insert
 #define st_insert rb_parser_st_insert
+#undef st_lookup
 #define st_lookup rb_parser_st_lookup
 
+#undef st_table_size
 #define st_table_size rb_parser_st_table_size
+#undef st_clear
 #define st_clear rb_parser_st_clear
+#undef st_init_strtable
 #define st_init_strtable rb_parser_st_init_strtable
+#undef st_init_table
 #define st_init_table rb_parser_st_init_table
+#undef st_init_strcasetable
 #define st_init_strcasetable rb_parser_st_init_strcasetable
+#undef st_init_strtable_with_size
 #define st_init_strtable_with_size rb_parser_st_init_strtable_with_size
+#undef st_init_numtable_with_size
 #define st_init_numtable_with_size rb_parser_st_init_numtable_with_size
+#undef st_init_strcasetable_with_size
 #define st_init_strcasetable_with_size rb_parser_st_init_strcasetable_with_size
+#undef st_memsize
 #define st_memsize rb_parser_st_memsize
+#undef st_get_key
 #define st_get_key rb_parser_st_get_key
+#undef st_add_direct
 #define st_add_direct rb_parser_st_add_direct
+#undef st_insert2
 #define st_insert2 rb_parser_st_insert2
+#undef st_copy
 #define st_copy rb_parser_st_copy
+#undef st_delete_safe
 #define st_delete_safe rb_parser_st_delete_safe
+#undef st_shift
 #define st_shift rb_parser_st_shift
+#undef st_cleanup_safe
 #define st_cleanup_safe rb_parser_st_cleanup_safe
+#undef st_update
 #define st_update rb_parser_st_update
+#undef st_foreach_with_replace
 #define st_foreach_with_replace rb_parser_st_foreach_with_replace
+#undef st_foreach_check
 #define st_foreach_check rb_parser_st_foreach_check
+#undef st_keys
 #define st_keys rb_parser_st_keys
+#undef st_keys_check
 #define st_keys_check rb_parser_st_keys_check
+#undef st_values
 #define st_values rb_parser_st_values
+#undef st_values_check
 #define st_values_check rb_parser_st_values_check
+#undef st_hash
 #define st_hash rb_parser_st_hash
+#undef st_hash_uint32
 #define st_hash_uint32 rb_parser_st_hash_uint32
+#undef st_hash_uint
 #define st_hash_uint rb_parser_st_hash_uint
+#undef st_hash_end
 #define st_hash_end rb_parser_st_hash_end
+#undef st_locale_insensitive_strcasecmp
 #define st_locale_insensitive_strcasecmp rb_parser_st_locale_insensitive_strcasecmp
+#undef st_locale_insensitive_strncasecmp
 #define st_locale_insensitive_strncasecmp rb_parser_st_locale_insensitive_strncasecmp
 
 /* The type of hashes.  */
