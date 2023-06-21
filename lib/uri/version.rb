dd73fe077cae077808e820f4765a12b1f4660521
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-06-21 13:20:54 +0900

    Merge URI-0.12.2

--- lib/uri/version.rb
+++ lib/uri/version.rb
@@ -1,6 +1,6 @@
 module URI
   # :stopdoc:
-  VERSION_CODE = '001201'.freeze
+  VERSION_CODE = '001202'.freeze
   VERSION = VERSION_CODE.scan(/../).collect{|n| n.to_i}.join('.').freeze
   # :startdoc:
 end
