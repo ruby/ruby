65c217933639b9f4f40d028440b09b046b0cf115
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-06-22 13:15:07 +0900

    [rubygems/rubygems] Molinillo::DependencyGraph is initialized in Molinillo::Resolver#resolve
    https://github.com/rubygems/rubygems/commit/1c39e24c95

--- lib/rubygems/resolver.rb
+++ lib/rubygems/resolver.rb
@@ -187,8 +187,7 @@ class Gem::Resolver
   # Proceed with resolution! Returns an array of ActivationRequest objects.
 
   def resolve
-    locking_dg = Molinillo::DependencyGraph.new
-    Molinillo::Resolver.new(self, self).resolve(@needed.map {|d| DependencyRequest.new d, nil }, locking_dg).tsort.map(&:payload).compact
+    Molinillo::Resolver.new(self, self).resolve(@needed.map {|d| DependencyRequest.new d, nil }).tsort.map(&:payload).compact
   rescue Molinillo::VersionConflict => e
     conflict = e.conflicts.values.first
     raise Gem::DependencyResolutionError, Conflict.new(conflict.requirement_trees.first.first, conflict.existing, conflict.requirement)
