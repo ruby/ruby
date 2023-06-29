616926b55e306a0704254a7ddfd6e9834d06c7f2
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-06-29 22:25:17 +0900

    CVE-2023-36617 for Ruby 3.0 (#7997)
    * Merge URI-0.10.3
    
    * Merge URI-0.10.0.3 for Bundler

--- lib/uri/version.rb
+++ lib/uri/version.rb
@@ -1,6 +1,6 @@
 module URI
   # :stopdoc:
-  VERSION_CODE = '001002'.freeze
+  VERSION_CODE = '001003'.freeze
   VERSION = VERSION_CODE.scan(/../).collect{|n| n.to_i}.join('.').freeze
   # :startdoc:
 end
