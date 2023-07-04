03d8ebcf2b237733b4df4dc0e96b21bbae53bb94
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-07-04 17:23:00 +0900

    spec/mspec/tool/wrap_with_guard.rb 'ruby_version_is ...3.3' spec/ruby/library/getoptlong/**/*_spec.rb

--- spec/ruby/library/getoptlong/each_option_spec.rb
+++ spec/ruby/library/getoptlong/each_option_spec.rb
@@ -1,7 +1,10 @@
 require_relative '../../spec_helper'
-require 'getoptlong'
-require_relative 'shared/each'
 
-describe "GetoptLong#each_option" do
-  it_behaves_like :getoptlong_each, :each_option
+ruby_version_is ""..."3.3" do
+  require 'getoptlong'
+  require_relative 'shared/each'
+
+  describe "GetoptLong#each_option" do
+    it_behaves_like :getoptlong_each, :each_option
+  end
 end
