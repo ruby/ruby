b60bbc9e3beb63afb27135753bd06dd71093bf0f
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-07-06 09:20:50 +0900

    spec/mspec/tool/wrap_with_guard.rb 'ruby_version_is ...3.3' spec/ruby/library/csv/**/*_spec.rb

--- spec/ruby/library/csv/basicwriter/close_on_terminate_spec.rb
+++ spec/ruby/library/csv/basicwriter/close_on_terminate_spec.rb
@@ -1,6 +1,9 @@
 require_relative '../../../spec_helper'
-require 'csv'
 
-describe "CSV::BasicWriter#close_on_terminate" do
-  it "needs to be reviewed for spec completeness"
+ruby_version_is ""..."3.3" do
+  require 'csv'
+
+  describe "CSV::BasicWriter#close_on_terminate" do
+    it "needs to be reviewed for spec completeness"
+  end
 end
--- spec/ruby/library/csv/streambuf/add_buf_spec.rb
+++ spec/ruby/library/csv/streambuf/add_buf_spec.rb
@@ -1,6 +1,9 @@
 require_relative '../../../spec_helper'
-require 'csv'
 
-describe "CSV::StreamBuf#add_buf" do
-  it "needs to be reviewed for spec completeness"
+ruby_version_is ""..."3.3" do
+  require 'csv'
+
+  describe "CSV::StreamBuf#add_buf" do
+    it "needs to be reviewed for spec completeness"
+  end
 end
