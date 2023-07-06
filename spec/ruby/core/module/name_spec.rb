99527f197165628b592711240787a3d981e3ef87
Benoit Daloze <eregontp@gmail.com>
2023-07-06 13:32:23 +0200

    Specs for Module#set_temporary_name should be in their own file

--- spec/ruby/core/module/name_spec.rb
+++ spec/ruby/core/module/name_spec.rb
@@ -6,55 +6,6 @@ describe "Module#name" do
     Module.new.name.should be_nil
   end
 
-  ruby_version_is "3.3" do
-    it "can assign a temporary name" do
-      m = Module.new
-      m.name.should be_nil
-
-      m.set_temporary_name("fake_name")
-      m.name.should == "fake_name"
-
-      m.set_temporary_name(nil)
-      m.name.should be_nil
-    end
-
-    it "can't assign empty string as name" do
-      m = Module.new
-      -> { m.set_temporary_name("") }.should raise_error(ArgumentError, "empty class/module name")
-    end
-
-    it "can't assign a constant name as a temporary name" do
-      m = Module.new
-      -> { m.set_temporary_name("Object") }.should raise_error(ArgumentError, "name must not be valid constant name")
-    end
-
-    it "can't assign name to permanent module" do
-      -> { Object.set_temporary_name("fake_name") }.should raise_error(RuntimeError, "can't change permanent name")
-    end
-
-    it "can assign a temporary name to a nested module" do
-      m = Module.new
-      module m::N; end
-      m::N.name.should =~ /\A#<Module:0x\h+>::N\z/
-
-      m::N.set_temporary_name("fake_name")
-      m::N.name.should == "fake_name"
-
-      m::N.set_temporary_name(nil)
-      m::N.name.should be_nil
-    end
-
-    it "can update the name when assigned to a constant" do
-      m = Module.new
-      m::N = Module.new
-      m::N.name.should =~ /\A#<Module:0x\h+>::N\z/
-      m::N.set_temporary_name(nil)
-
-      m::M = m::N
-      m::M.name.should =~ /\A#<Module:0x\h+>::M\z/m
-    end
-  end
-
   it "is not nil when assigned to a constant in an anonymous module" do
     m = Module.new
     m::N = Module.new
