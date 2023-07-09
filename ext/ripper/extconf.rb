7f0e64510635aab8c6cf6b057e46fc736ca574fd
yui-knk <spiketeika@gmail.com>
2023-07-09 11:04:40 +0900

    Include ripper.h into `$distcleanfiles`

--- ext/ripper/extconf.rb
+++ ext/ripper/extconf.rb
@@ -6,7 +6,7 @@ require 'rbconfig'
 
 def main
   $objs = %w(eventids1.o eventids2.o ripper.o ripper_init.o)
-  $distcleanfiles.concat %w(ripper.y ripper.c eventids1.c eventids1.h eventids2table.c ripper_init.c)
+  $distcleanfiles.concat %w(ripper.y ripper.c ripper.h eventids1.c eventids1.h eventids2table.c ripper_init.c)
   $cleanfiles.concat %w(ripper.E ripper.output y.output .eventids2-check)
   $defs << '-DRIPPER'
   $defs << '-DRIPPER_DEBUG' if $debug
