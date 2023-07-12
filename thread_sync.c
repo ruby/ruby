5c82bd8421924c05b54f4c0519cb2612721bc540
Shane Becker <veganstraightedge@gmail.com>
2023-07-12 16:57:03 -0700

    [DOC] Add parenthetical to explain what FIFO abbrev means

--- thread_sync.c
+++ thread_sync.c
@@ -846,8 +846,8 @@ queue_closed_result(VALUE self, struct rb_queue *q)
  *  information must be exchanged safely between multiple threads. The
  *  Thread::Queue class implements all the required locking semantics.
  *
- *  The class implements FIFO type of queue. In a FIFO queue, the first
- *  tasks added are the first retrieved.
+ *  The class implements FIFO (first in, first out) type of queue.
+ *  In a FIFO queue, the first tasks added are the first retrieved.
  *
  *  Example:
  *
