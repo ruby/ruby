174bc2257065785efbbefca797a50f0847bb6062
Stan Lo <stan001212@gmail.com>
2023-07-14 16:45:04 +0100

    [ruby/irb] Fix history-saving feature (https://github.com/ruby/irb/pull/642)
    * Define RelineInputMethod::HISTORY
    
    The HistorySavingAbility module doesn't do anything if the input method
    class doesn't define HISTORY.
    
    - https://github.com/ruby/irb/blob/3ac96be660bf052902fb4e532c7a46cf294b71eb/lib/irb/history.rb#L10
    - https://github.com/ruby/irb/blob/3ac96be660bf052902fb4e532c7a46cf294b71eb/lib/irb/history.rb#L34
    
    This patch defines RelineInputMethod::HISTORY to avoid this.
    
    * Improve history-saving's ability check
    
    Instead of checking the existence of `input_method_class::HISTORY`, we should
    make every input method class declare if it supports history saving or not.
    
    Since the default value is `false`, it shouldn't break any custom input method
    that inherits from `IRB::InputMethod`.
    
    https://github.com/ruby/irb/commit/aec7a5b3f5

--- lib/irb/input-method.rb
+++ lib/irb/input-method.rb
@@ -47,6 +47,10 @@ module IRB
       false
     end
 
+    def support_history_saving?
+      false
+    end
+
     # For debug message
     def inspect
       'Abstract InputMethod'
@@ -230,6 +234,10 @@ module IRB
         true
       end
 
+      def support_history_saving?
+        true
+      end
+
       # Returns the current line number for #io.
       #
       # #line counts the number of times #gets is called.
@@ -256,6 +264,7 @@ module IRB
   end
 
   class RelineInputMethod < InputMethod
+    HISTORY = Reline::HISTORY
     # Creates a new input method object using Reline
     def initialize
       IRB.__send__(:set_encoding, Reline.encoding_system_needs.name, override: false)
@@ -457,6 +466,10 @@ module IRB
       inputrc_path = File.expand_path(config.inputrc_path)
       str += " and #{inputrc_path}" if File.exist?(inputrc_path)
       str
+    end
+
+    def support_history_saving?
+      true
     end
   end
 
