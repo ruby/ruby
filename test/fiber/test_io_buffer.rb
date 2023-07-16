5ad2390b551f4a28ea3d4db78c21863489fdd681
nagachika <nagachika@ruby-lang.org>
2023-07-16 16:44:22 +0900

    merge revision(s) 466aa8010fb49f9ec6c78ea1de4e8ca0965f4fdf: [Backport #19546]
    	Fix incorrect usage of `rb_fiber_scheduler_io_(p)(read|write)`.
    	 (#7593)
    
    	---
    	 io_buffer.c                  |  8 ++++----
    	 scheduler.c                  | 48 ++++++++++++++++++++++----------------------
    	 test/fiber/test_io_buffer.rb | 33 ++++++++++++++++++++++++++++++
    	 3 files changed, 61 insertions(+), 28 deletions(-)

--- test/fiber/test_io_buffer.rb
+++ test/fiber/test_io_buffer.rb
@@ -122,4 +122,37 @@ class TestFiberIOBuffer < Test::Unit::TestCase
     i&.close
     o&.close
   end
+
+  def test_io_buffer_read_write
+    omit "UNIXSocket is not defined!" unless defined?(UNIXSocket)
+
+    i, o = UNIXSocket.pair
+    source_buffer = IO::Buffer.for("Hello World!")
+    destination_buffer = IO::Buffer.new(source_buffer.size)
+
+    # Test non-scheduler code path:
+    source_buffer.write(o, source_buffer.size)
+    destination_buffer.read(i, source_buffer.size)
+    assert_equal source_buffer, destination_buffer
+
+    # Test scheduler code path:
+    destination_buffer.clear
+
+    thread = Thread.new do
+      scheduler = IOBufferScheduler.new
+      Fiber.set_scheduler scheduler
+
+      Fiber.schedule do
+        source_buffer.write(o, source_buffer.size)
+        destination_buffer.read(i, source_buffer.size)
+      end
+    end
+
+    thread.join
+
+    assert_equal source_buffer, destination_buffer
+  ensure
+    i&.close
+    o&.close
+  end
 end
