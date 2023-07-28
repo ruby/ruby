nobu/turkdevops-ruby

Warn default gems which will be gemified in future

gem_prelude.rb
@@ -4,6 +4,8 @@ rescue LoadError => e
   raise unless e.path == 'rubygems'
 
   warn "`RubyGems' were not loaded."
+else
+  require 'default_bundled_gems'
 end if defined?(Gem)
 
 begin

ruby/ruby

gem_prelude.rb
@@ -4,6 +4,8 @@ rescue LoadError => e
   raise unless e.path == 'rubygems'
 
   warn "`RubyGems' were not loaded."
+else
+  require 'bundled_gems'
 end if defined?(Gem)
 
 begin