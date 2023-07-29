437fd9e9021df06108584098260c9eab8e637368
Benoit Daloze <eregontp@gmail.com>
2023-07-29 16:49:54 +0200

    [ruby/yarp] Move efficient file reading using demand paging to librubyparser
    * So it can be reused by the Fiddle backend, etc and not just the C extension.
    * Add YP_STRING_MAPPED to use a consistent interface for yp_string_t.
      That way yp_string_free() can be used like for other string types.
    * Fix handling of empty file for !HAVE_MMAP && !_WIN32
    
    https://github.com/ruby/yarp/commit/e40bc35801

--- yarp/util/yp_string.c
+++ yarp/util/yp_string.c
@@ -1,5 +1,15 @@
 #include "yarp/util/yp_string.h"
 
+// The following headers are necessary to read files using demand paging.
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#endif
+
 // Initialize a shared string that is based on initial input.
 void
 yp_string_shared_init(yp_string_t *string, const char *start, const char *end) {
@@ -30,6 +40,17 @@ yp_string_constant_init(yp_string_t *string, const char *source, size_t length) 
     *string = (yp_string_t) {
         .type = YP_STRING_CONSTANT,
         .as.constant = {
+            .source = source,
+            .length = length
+        }
+    };
+}
+
+static void
+yp_string_mapped_init_internal(yp_string_t *string, char *source, size_t length) {
+    *string = (yp_string_t) {
+        .type = YP_STRING_MAPPED,
+        .as.mapped = {
             .source = source,
             .length = length
         }
@@ -84,5 +105,113 @@ YP_EXPORTED_FUNCTION void
 yp_string_free(yp_string_t *string) {
     if (string->type == YP_STRING_OWNED) {
         free(string->as.owned.source);
+    } else if (string->type == YP_STRING_MAPPED && string->as.mapped.length) {
+        void *memory = (void *) string->as.mapped.source;
+        #if defined(_WIN32)
+        UnmapViewOfFile(memory);
+        #elif defined(HAVE_MMAP)
+        munmap(memory, string->as.mapped.length);
+        #else
+        free(memory);
+        #endif
     }
+}
+
+bool
+yp_string_mapped_init(yp_string_t *string, const char *filepath) {
+#ifdef _WIN32
+    // Open the file for reading.
+    HANDLE file = CreateFile(filepath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (file == INVALID_HANDLE_VALUE) {
+        perror("CreateFile failed");
+        return false;
+    }
+
+    // Get the file size.
+    DWORD file_size = GetFileSize(file, NULL);
+    if (file_size == INVALID_FILE_SIZE) {
+        CloseHandle(file);
+        perror("GetFileSize failed");
+        return false;
+    }
+
+    // If the file is empty, then we don't need to do anything else, we'll set
+    // the source to a constant empty string and return.
+    if (file_size == 0) {
+        CloseHandle(file);
+        yp_string_mapped_init_internal(string, "", 0);
+        return true;
+    }
+
+    // Create a mapping of the file.
+    HANDLE mapping = CreateFileMapping(file, NULL, PAGE_READONLY, 0, 0, NULL);
+    if (mapping == NULL) {
+        CloseHandle(file);
+        perror("CreateFileMapping failed");
+        return false;
+    }
+
+    // Map the file into memory.
+    char *source = (char *) MapViewOfFile(mapping, FILE_MAP_READ, 0, 0, 0);
+    CloseHandle(mapping);
+    CloseHandle(file);
+
+    if (source == NULL) {
+        perror("MapViewOfFile failed");
+        return false;
+    }
+
+    yp_string_mapped_init_internal(string, source, (size_t) file_size);
+    return true;
+#else
+    // Open the file for reading
+    int fd = open(filepath, O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        return false;
+    }
+
+    // Stat the file to get the file size
+    struct stat sb;
+    if (fstat(fd, &sb) == -1) {
+        close(fd);
+        perror("fstat");
+        return false;
+    }
+
+    // mmap the file descriptor to virtually get the contents
+    size_t size = (size_t) sb.st_size;
+    char *source = NULL;
+
+    if (size == 0) {
+        close(fd);
+        yp_string_mapped_init_internal(string, "", 0);
+        return true;
+    }
+
+#ifdef HAVE_MMAP
+    source = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (source == MAP_FAILED) {
+        perror("Map failed");
+        return false;
+    }
+#else
+    source = malloc(size);
+    if (source == NULL) {
+        return false;
+    }
+
+    ssize_t read_size = read(fd, (void *) source, size);
+    if (read_size < 0 || (size_t)read_size != size) {
+        perror("Read size is incorrect");
+        free((void *) source);
+        return false;
+    }
+#endif
+
+    close(fd);
+    yp_string_mapped_init_internal(string, source, size);
+    return true;
+#endif
 }
