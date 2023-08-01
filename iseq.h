26933d1c88224ad48608ad4ff0f50cdd08125455
Koichi Sasada <ko1@atdot.net>
2023-08-01 17:25:20 +0900

    support `rescue` event for TracePoint
    fix [Feature #19572]

--- iseq.h
+++ iseq.h
@@ -83,9 +83,10 @@ ISEQ_ORIGINAL_ISEQ_ALLOC(const rb_iseq_t *iseq, long size)
                            RUBY_EVENT_CALL  | \
                            RUBY_EVENT_RETURN| \
                            RUBY_EVENT_C_CALL| \
-                           RUBY_EVENT_C_RETURN| \
-                           RUBY_EVENT_B_CALL| \
-                           RUBY_EVENT_B_RETURN| \
+                           RUBY_EVENT_C_RETURN | \
+                           RUBY_EVENT_B_CALL   | \
+                           RUBY_EVENT_B_RETURN | \
+                           RUBY_EVENT_RESCUE   | \
                            RUBY_EVENT_COVERAGE_LINE| \
                            RUBY_EVENT_COVERAGE_BRANCH)
 
