ruby/ruby

commit dbbc3583ba432c279f07b1fa0afb0a8a9ba50c91

Preserve already set options in `moreswitches`

ruby.c
@@ -887,6 +887,14 @@ moreswitches(const char *s, ruby_cmdline_options_t *opt, int envopt)
     VALUE argstr, argary;
     void *ptr;
 
+    VALUE src_enc_name = opt->src.enc.name;
+    VALUE ext_enc_name = opt->ext.enc.name;
+    VALUE int_enc_name = opt->intern.enc.name;
+    ruby_features_t feat = opt->features;
+    ruby_features_t warn = opt->warn;
+
+    opt->src.enc.name = opt->ext.enc.name = opt->intern.enc.name = 0;
+
     while (ISSPACE(*s)) s++;
     if (!*s) return;
     argstr = rb_str_tmp_new((len = strlen(s)) + (envopt!=0));
@@ -921,6 +929,18 @@ moreswitches(const char *s, ruby_cmdline_options_t *opt, int envopt)
             --argv;
         }
     }
+
+    if (src_enc_name) {
+        opt->src.enc.name = src_enc_name;
+    }
+    if (ext_enc_name) {
+        opt->ext.enc.name = ext_enc_name;
+    }
+    if (int_enc_name) {
+        opt->intern.enc.name = int_enc_name;
+    }
+    FEATURE_SET_RESTORE(opt->features, feat);
+    FEATURE_SET_RESTORE(opt->warn, warn);
 
     ruby_xfree(ptr);
     /* get rid of GC */
@@ -2003,22 +2023,7 @@ process_options(int argc, char **argv, ruby_cmdline_options_t *opt)
     argv += i;
 
     if (FEATURE_SET_P(opt->features, rubyopt) && (s = getenv("RUBYOPT"))) {
-        VALUE src_enc_name = opt->src.enc.name;
-        VALUE ext_enc_name = opt->ext.enc.name;
-        VALUE int_enc_name = opt->intern.enc.name;
-        ruby_features_t feat = opt->features;
-        ruby_features_t warn = opt->warn;
-
-        opt->src.enc.name = opt->ext.enc.name = opt->intern.enc.name = 0;
         moreswitches(s, opt, 1);
-        if (src_enc_name)
-            opt->src.enc.name = src_enc_name;
-        if (ext_enc_name)
-            opt->ext.enc.name = ext_enc_name;
-        if (int_enc_name)
-            opt->intern.enc.name = int_enc_name;
-        FEATURE_SET_RESTORE(opt->features, feat);
-        FEATURE_SET_RESTORE(opt->warn, warn);
     }
 
     if (opt->src.enc.name)

commit 3c41a04b6cedbace1406d46a20a673f59b1502de

Fix encoding switches when RUBYOPT is empty or only spaces
* Follow-up of dbbc3583ba432c279f07b1fa0afb0a8a9ba50c91 which broke this.

ruby.c
@@ -895,10 +895,11 @@ moreswitches(const char *s, ruby_cmdline_options_t *opt, int envopt)
     ruby_features_t warn = opt->warn;
     int backtrace_length_limit = opt->backtrace_length_limit;
 
-    opt->src.enc.name = opt->ext.enc.name = opt->intern.enc.name = 0;
-
     while (ISSPACE(*s)) s++;
     if (!*s) return;
+
+    opt->src.enc.name = opt->ext.enc.name = opt->intern.enc.name = 0;
+
     argstr = rb_str_tmp_new((len = strlen(s)) + (envopt!=0));
     argary = rb_str_tmp_new(0);
 

spec/ruby/command_line/dash_upper_u_spec.rb
@@ -6,6 +6,13 @@ describe "ruby -U" do
               options: '-U').should == 'UTF-8'
   end
 
+  it "sets Encoding.default_internal to UTF-8 when RUBYOPT is empty or only spaces" do
+    ruby_exe('p Encoding.default_internal',
+             options: '-U', env: { 'RUBYOPT' => '' }).should == "#<Encoding:UTF-8>\n"
+    ruby_exe('p Encoding.default_internal',
+             options: '-U', env: { 'RUBYOPT' => '  ' }).should == "#<Encoding:UTF-8>\n"
+  end
+
   it "does nothing different if specified multiple times" do
      ruby_exe('print Encoding.default_internal.name',
               options: '-U -U').should == 'UTF-8'