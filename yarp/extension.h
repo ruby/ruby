e0eab271d105216472dc6a561294dfb3a20e26b4
Kevin Newton <kddnewton@gmail.com>
2023-08-07 13:11:59 -0400

    [ruby/yarp] Less const_get
    Right now whenever you go to create a Ruby object from a C struct
    we dynamically look up the constants. This is not great for
    performance. Now instead we template out a constant for each VALUE
    that holds the classes, and then directly reference it.
    
    https://github.com/ruby/yarp/commit/f4756cda77

--- yarp/extension.h
+++ yarp/extension.h
@@ -11,6 +11,7 @@ VALUE yp_source_new(yp_parser_t *parser);
 VALUE yp_token_new(yp_parser_t *parser, yp_token_t *token, rb_encoding *encoding, VALUE source);
 VALUE yp_ast_new(yp_parser_t *parser, yp_node_t *node, rb_encoding *encoding);
 
+void Init_yarp_api_node(void);
 void Init_yarp_pack(void);
 YP_EXPORTED_FUNCTION void Init_yarp(void);
 
