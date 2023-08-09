9d08cf3ac771132ed38d2d5fd4d7627463e25024
yui-knk <spiketeika@gmail.com>
2023-08-09 11:55:19 +0900

    Resync Lrama v0.5.3
    Previous sync included unrelated files and missed template file update.

--- tool/lrama/lib/lrama/irb.rb
+++
@@ -1,8 +0,0 @@
-require "lrama/irb/load_grammar"
-
-# You need to explicitly require this file to add irb commands
-# See: "lib/irb/extend-command.rb" in ruby/irb
-module Lrama
-  module IRB
-  end
-end
