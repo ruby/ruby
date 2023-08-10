fe240b672b709e800f2b4fb996f04f2005329dab
David Rodríguez <deivid.rodriguez@riseup.net>
2023-08-10 21:37:15 +0200

    [rubygems/rubygems] Show better error when PAT can't authenticate to a private server
    Before:
    
    ```
    Fetching gem metadata from https://rubygems.org/........
    Fetching source index from https://rubygems.pkg.github.com/my-org/
    
    Bad username or password for https://x-access-token@rubygems.pkg.github.com/my-org/.
    Please double-check your credentials and correct them.
    ```
    
    After:
    
    ```
    Fetching gem metadata from https://rubygems.org/........
    Fetching source index from https://rubygems.pkg.github.com/my-org/
    
    Access token could not be authenticated for https://x-access-token@rubygems.pkg.github.com/my-org/.
    Make sure it's valid and has the necessary scopes configured.
    ```
    
    https://github.com/rubygems/rubygems/commit/2ae69c964a

--- spec/bundler/bundler/fetcher/index_spec.rb
+++ spec/bundler/bundler/fetcher/index_spec.rb
@@ -63,26 +63,9 @@ RSpec.describe Bundler::Fetcher::Index do
     context "when a 403 response occurs" do
       let(:error_message) { "403" }
 
-      before do
-        allow(remote_uri).to receive(:userinfo).and_return(userinfo)
-      end
-
-      context "and there was userinfo" do
-        let(:userinfo) { double(:userinfo) }
-
-        it "should raise a Bundler::Fetcher::BadAuthenticationError" do
-          expect { subject.specs(gem_names) }.to raise_error(Bundler::Fetcher::BadAuthenticationError,
-            %r{Bad username or password for http://remote-uri.org})
-        end
-      end
-
-      context "and there was no userinfo" do
-        let(:userinfo) { nil }
-
-        it "should raise a Bundler::Fetcher::AuthenticationRequiredError" do
-          expect { subject.specs(gem_names) }.to raise_error(Bundler::Fetcher::AuthenticationRequiredError,
-            %r{Authentication is required for http://remote-uri.org})
-        end
+      it "should raise a Bundler::Fetcher::AuthenticationForbiddenError" do
+        expect { subject.specs(gem_names) }.to raise_error(Bundler::Fetcher::AuthenticationForbiddenError,
+          %r{Access token could not be authenticated for http://remote-uri.org})
       end
     end
 
