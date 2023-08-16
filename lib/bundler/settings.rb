7cb6cbee953b8cf020e4e337bdbadd426460c71f
Josh Nichols <josh.nichols@gusto.com>
2023-08-16 15:05:39 -0400

    [rubygems/rubygems] String#start_with? is faster than regex with beginning boundaries
    https://github.com/rubygems/rubygems/commit/d7cde68034

--- lib/bundler/settings.rb
+++ lib/bundler/settings.rb
@@ -161,7 +161,7 @@ module Bundler
     def local_overrides
       repos = {}
       all.each do |k|
-        repos[$'] = self[k] if k =~ /^local\./
+        repos[$'] = self[k] if k.start_with?("local.")
       end
       repos
     end
