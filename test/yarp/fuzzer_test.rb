440cdceffb7efef0aa93ad519b056a25d610f52d
Mike Dalessio <mike.dalessio@gmail.com>
2023-08-29 16:36:18 -0400

    [ruby/yarp] test: new test file for capturing interesting fuzzer snippets
    Note that we call `YARP.dump` for these fuzzer tests to better match
    the fuzz.parse harness, which also serializes.
    
    https://github.com/ruby/yarp/commit/032ad047e9

---
+++ test/yarp/fuzzer_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+
+# These tests are simply to exercise snippets found by the fuzzer that caused invalid memory access.
+class FuzzerTest < Test::Unit::TestCase
+  class << self
+    def snippet(name, source)
+      test "fuzzer #{name}" do
+        YARP.dump(source)
+      end
+    end
+  end
+end
