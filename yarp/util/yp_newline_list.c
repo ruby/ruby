d99db1199d4318e97a26687571b10f7629604018
Kevin Newton <kddnewton@gmail.com>
2023-09-01 12:47:37 -0400

    [ruby/yarp] Update src/util/yp_newline_list.c style
    https://github.com/ruby/yarp/commit/bd7fc2c946

--- yarp/util/yp_newline_list.c
+++ yarp/util/yp_newline_list.c
@@ -25,7 +25,7 @@ yp_newline_list_init(yp_newline_list_t *list, const uint8_t *start, size_t capac
 bool
 yp_newline_list_append(yp_newline_list_t *list, const uint8_t *cursor) {
     if (list->size == list->capacity) {
-        size_t * original_offsets = list->offsets;
+        size_t *original_offsets = list->offsets;
 
         list->capacity = (list->capacity * 3) / 2;
         list->offsets = (size_t *) calloc(list->capacity, sizeof(size_t));
