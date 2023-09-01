e8ef5b1281b9dce518902b3451096164c30bca5c
Kevin Newton <kddnewton@gmail.com>
2023-09-01 14:51:50 -0400

    [ruby/yarp] Provide a better inspect
    https://github.com/ruby/yarp/commit/ef14ae66e4

--- lib/yarp.rb
+++ lib/yarp.rb
@@ -141,6 +141,10 @@ module YARP
     def deconstruct_keys(keys)
       { type: type, location: location }
     end
+
+    def inspect
+      "#<YARP::Comment @type=#{@type.inspect} @location=#{@location.inspect}>"
+    end
   end
 
   # This represents an error that was encountered during parsing.
@@ -155,6 +159,10 @@ module YARP
     def deconstruct_keys(keys)
       { message: message, location: location }
     end
+
+    def inspect
+      "#<YARP::ParseError @message=#{@message.inspect} @location=#{@location.inspect}>"
+    end
   end
 
   # This represents a warning that was encountered during parsing.
@@ -168,6 +176,10 @@ module YARP
 
     def deconstruct_keys(keys)
       { message: message, location: location }
+    end
+
+    def inspect
+      "#<YARP::ParseWarning @message=#{@message.inspect} @location=#{@location.inspect}>"
     end
   end
 
@@ -323,13 +335,77 @@ module YARP
         q.nest(2) do
           deconstructed = deconstruct_keys([])
           deconstructed.delete(:location)
-
           q.breakable("")
           q.seplist(deconstructed, lambda { q.comma_breakable }, :each_value) { |value| q.pp(value) }
         end
         q.breakable("")
         q.text(")")
       end
+    end
+  end
+
+  # This object is responsible for generating the output for the inspect method
+  # implementations of child nodes.
+  class NodeInspector
+    attr_reader :prefix, :output
+
+    def initialize(prefix = "")
+      @prefix = prefix
+      @output = +""
+    end
+
+    # Appends a line to the output with the current prefix.
+    def <<(line)
+      output << "#{prefix}#{line}"
+    end
+
+    # This generates a string that is used as the header of the inspect output
+    # for any given node.
+    def header(node)
+      output = +"@ #{node.class.name.split("::").last} ("
+      output << "location: (#{node.location.start_offset}...#{node.location.end_offset})"
+      output << ", newline: true" if node.newline?
+      output << ")\n"
+      output
+    end
+
+    # Generates a string that represents a list of nodes. It handles properly
+    # using the box drawing characters to make the output look nice.
+    def list(prefix, nodes)
+      output = +"(length: #{nodes.length})\n"
+      last_index = nodes.length - 1
+
+      nodes.each_with_index do |node, index|
+        pointer, preadd = (index == last_index) ? ["└── ", "    "] : ["├── ", "│   "]
+        node_prefix = "#{prefix}#{preadd}"
+        output << node.inspect(NodeInspector.new(node_prefix)).sub(node_prefix, "#{prefix}#{pointer}")
+      end
+
+      output
+    end
+
+    # Generates a string that represents a location field on a node.
+    def location(value)
+      if value
+        "(#{value.start_offset}...#{value.end_offset}) = #{value.slice.inspect}"
+      else
+        "∅"
+      end
+    end
+
+    # Generates a string that represents a child node.
+    def child_node(node, append)
+      node.inspect(child_inspector(append)).delete_prefix(prefix)
+    end
+
+    # Returns a new inspector that can be used to inspect a child node.
+    def child_inspector(append)
+      NodeInspector.new("#{prefix}#{append}")
+    end
+
+    # Returns the output as a string.
+    def to_str
+      output
     end
   end
 
