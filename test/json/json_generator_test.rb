1851824b1c7b816e41b567b5310bb18ab6f9ff7a
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-09-13 15:30:21 +0900

    [flori/json] alias_method is private on Ruby 2.3 and 2.4
    https://github.com/flori/json/commit/573ef94bc5

--- test/json/json_generator_test.rb
+++ test/json/json_generator_test.rb
@@ -395,7 +395,7 @@ EOT
     def test_string_ext_included_calls_super
       included = false
 
-      Module.alias_method(:included_orig, :included)
+      Module.send(:alias_method, [:included_orig, :included])
       Module.remove_method(:included)
       Module.define_method(:included) do |base|
         included_orig(base)
@@ -410,7 +410,7 @@ EOT
     ensure
       if Module.private_method_defined?(:included_orig)
         Module.remove_method(:included) if Module.method_defined?(:included)
-        Module.alias_method(:included, :included_orig)
+        Module.send(:alias_method, [:included, :included_orig])
         Module.remove_method(:included_orig)
       end
     end
