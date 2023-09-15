35960ce65e0300d897033f2ff15191bd60a2c53d
Vinicius Stock <vinicius.stock@shopify.com>
2023-09-15 15:42:24 -0400

    [ruby/yarp] Add node event dispatcher
    This commit changes the node template to create a dispatcher class,
    which can be used to walk an AST an emit events to all registered
    listeners
    
    https://github.com/ruby/yarp/commit/03a45f85e6
    
    Co-authored-by: Kevin Newton <kddnewton@users.noreply.github.com>

---
+++ test/yarp/dispatcher_test.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+
+module YARP
+  class DispatcherTest < TestCase
+    def setup
+      @listener = Class.new do
+        attr_reader :events_received
+
+        def initialize
+          @events_received = []
+        end
+
+        def call_node_enter(node)
+          @events_received << :call_node_enter
+        end
+
+        def call_node_leave(node)
+          @events_received << :call_node_leave
+        end
+      end.new
+    end
+
+    def test_dispatching_events
+      dispatcher = Dispatcher.new
+      dispatcher.register(@listener, :call_node_enter, :call_node_leave)
+
+      root = YARP.parse(<<~RUBY).value
+        def foo
+          something(1, 2, 3)
+        end
+      RUBY
+
+      dispatcher.dispatch(root)
+
+      assert_equal([:call_node_enter, :call_node_leave], @listener.events_received)
+    end
+  end
+end
