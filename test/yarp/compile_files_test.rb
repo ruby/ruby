f6489f5943fb9e04f191090f64562f07c1e53851
Jemma Issroff <jemmaissroff@gmail.com>
2023-09-21 15:17:17 -0400

    wip

--- test/yarp/compile_files_test.rb
+++ test/yarp/compile_files_test.rb
@@ -14,14 +14,33 @@ module YARP
     end
 
     # Fixtures that currently lead to crashes when run
+    # first ones are assertion falures
     crashes = %w(
+      whitequark/args_args_assocs.txt
+      whitequark/args_block_pass.txt
+      method_calls.txt
+      unparser/corpus/literal/since/31.txt
+      whitequark/args_args_star.txt
+      seattlerb/mlhs_mid_splat.txt
+      constants.txt
+      whitequark/args_assocs_legacy.txt
+      whitequark/args_star.txt
+      whitequark/anonymous_blockarg.txt
+
       break.txt
+      seattlerb/masgn_command_call.txt
+      whitequark/args_assocs.txt
+      seattlerb/call_block_arg_named.txt
+      seattlerb/mlhs_back_anonsplat.txt
       endless_range_in_conditional.txt
+      seattlerb/masgn_var_star_var.txt
       if.txt
       keywords.txt
       next.txt
+      variables.txt
       procs.txt
       seattlerb/assoc__bare.txt
+      seattlerb/mlhs_mid_anonsplat.txt
       seattlerb/block_break.txt
       seattlerb/block_next.txt
       seattlerb/masgn_star.txt
@@ -45,32 +64,25 @@ module YARP
 
     # Fixtures that currently lead to errors when run
     failures = %w(
-      alias.txt
       arrays.txt
       classes.txt
-      integer_operations.txt
       methods.txt
       modules.txt
       not.txt
-      regex.txt
       seattlerb/bug191.txt
       seattlerb/difficult1_line_numbers2.txt
       seattlerb/difficult2_.txt
       seattlerb/dsym_esc_to_sym.txt
-      seattlerb/dsym_to_sym.txt
       seattlerb/flip2_env_lvar.txt
       seattlerb/magic_encoding_comment.txt
       seattlerb/return_call_assocs.txt
       ternary_operator.txt
       undef.txt
-      unparser/corpus/literal/alias.txt
       unparser/corpus/literal/class.txt
       unparser/corpus/literal/flipflop.txt
-      unparser/corpus/literal/if.txt
       unparser/corpus/literal/module.txt
       unparser/corpus/semantic/and.txt
       unparser/corpus/semantic/dstr.txt
-      whitequark/alias_gvar.txt
       whitequark/array_assocs.txt
       whitequark/array_splat.txt
       whitequark/bug_regex_verification.txt
@@ -79,12 +91,8 @@ module YARP
       whitequark/if.txt
       whitequark/if_else.txt
       whitequark/if_while_after_class__since_32.txt
-      whitequark/keyword_argument_omission.txt
-      whitequark/lvar_injecting_match.txt
-      whitequark/non_lvar_injecting_match.txt
       whitequark/parser_bug_830.txt
       whitequark/send_attr_asgn.txt
-      whitequark/send_binary_op.txt
       whitequark/unless.txt
       whitequark/unless_else.txt
     )
