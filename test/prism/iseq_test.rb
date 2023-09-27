3d0a46796b780417096b65de5edb590d3b1537f1
Kevin Newton <kddnewton@gmail.com>
2023-09-27 12:39:53 -0400

    Rename YARP symbols to prism

--- test/prism/iseq_test.rb
+++ test/prism/iseq_test.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-module YARP
+module Prism
   class ISeqTest < Test::Unit::TestCase
     def test_empty_program
-      test_yarp_eval("")
+      test_prism_eval("")
     end
 
     ############################################################################
@@ -11,47 +11,47 @@ module YARP
     ############################################################################
 
     def test_FalseNode
-      test_yarp_eval("false")
+      test_prism_eval("false")
     end
 
     def test_FloatNode
-      test_yarp_eval("1.2")
-      test_yarp_eval("1.2e3")
-      test_yarp_eval("+1.2e+3")
-      test_yarp_eval("-1.2e-3")
+      test_prism_eval("1.2")
+      test_prism_eval("1.2e3")
+      test_prism_eval("+1.2e+3")
+      test_prism_eval("-1.2e-3")
     end
 
     def test_ImaginaryNode
-      test_yarp_eval("1i")
-      test_yarp_eval("+1.0i")
-      test_yarp_eval("1ri")
+      test_prism_eval("1i")
+      test_prism_eval("+1.0i")
+      test_prism_eval("1ri")
     end
 
     def test_IntegerNode
-      test_yarp_eval("1")
-      test_yarp_eval("+1")
-      test_yarp_eval("-1")
-      test_yarp_eval("0x10")
-      test_yarp_eval("0b10")
-      test_yarp_eval("0o10")
-      test_yarp_eval("010")
+      test_prism_eval("1")
+      test_prism_eval("+1")
+      test_prism_eval("-1")
+      test_prism_eval("0x10")
+      test_prism_eval("0b10")
+      test_prism_eval("0o10")
+      test_prism_eval("010")
     end
 
     def test_NilNode
-      test_yarp_eval("nil")
+      test_prism_eval("nil")
     end
 
     def test_RationalNode
-      test_yarp_eval("1.2r")
-      test_yarp_eval("+1.2r")
+      test_prism_eval("1.2r")
+      test_prism_eval("+1.2r")
     end
 
     def test_SelfNode
-      test_yarp_eval("self")
+      test_prism_eval("self")
     end
 
     def test_TrueNode
-      test_yarp_eval("true")
+      test_prism_eval("true")
     end
 
     ############################################################################
@@ -59,27 +59,27 @@ module YARP
     ############################################################################
 
     def test_ClassVariableReadNode
-      test_yarp_eval("class YARP::ISeqTest; @@yct = 1; @@yct; end")
+      test_prism_eval("class Prism::ISeqTest; @@pit = 1; @@pit; end")
     end
 
     def test_ConstantPathNode
-      test_yarp_eval("YARP::ISeqTest")
+      test_prism_eval("Prism::ISeqTest")
     end
 
     def test_ConstantReadNode
-      test_yarp_eval("YARP")
+      test_prism_eval("Prism")
     end
 
     def test_GlobalVariableReadNode
-      test_yarp_eval("$yct = 1; $yct")
+      test_prism_eval("$pit = 1; $pit")
     end
 
     def test_InstanceVariableReadNode
-      test_yarp_eval("class YARP::ISeqTest; @yct = 1; @yct; end")
+      test_prism_eval("class Prism::ISeqTest; @pit = 1; @pit; end")
     end
 
     def test_LocalVariableReadNode
-      test_yarp_eval("yct = 1; yct")
+      test_prism_eval("pit = 1; pit")
     end
 
     ############################################################################
@@ -87,49 +87,49 @@ module YARP
     ############################################################################
 
     def test_ClassVariableTargetNode
-      test_yarp_eval("class YARP::ISeqTest; @@yct, @@yct1 = 1; end")
+      test_prism_eval("class Prism::ISeqTest; @@pit, @@pit1 = 1; end")
     end
 
     def test_ClassVariableWriteNode
-      test_yarp_eval("class YARP::ISeqTest; @@yct = 1; end")
+      test_prism_eval("class Prism::ISeqTest; @@pit = 1; end")
     end
 
     def test_ClassVariableAndWriteNode
-      test_yarp_eval("class YARP::ISeqTest; @@yct = 0; @@yct &&= 1; end")
+      test_prism_eval("class Prism::ISeqTest; @@pit = 0; @@pit &&= 1; end")
     end
 
     def test_ClassVariableOrWriteNode
-      test_yarp_eval("class YARP::ISeqTest; @@yct = 1; @@yct ||= 0; end")
-      test_yarp_eval("class YARP::ISeqTest; @@yct = nil; @@yct ||= 1; end")
+      test_prism_eval("class Prism::ISeqTest; @@pit = 1; @@pit ||= 0; end")
+      test_prism_eval("class Prism::ISeqTest; @@pit = nil; @@pit ||= 1; end")
     end
 
     def test_ClassVariableOperatorWriteNode
-      test_yarp_eval("class YARP::ISeqTest; @@yct = 0; @@yct += 1; end")
+      test_prism_eval("class Prism::ISeqTest; @@pit = 0; @@pit += 1; end")
     end
 
     def test_ConstantTargetNode
-      # We don't call test_yarp_eval directly in this case becuase we
+      # We don't call test_prism_eval directly in this case becuase we
       # don't want to assign the constant mutliple times if we run
       # with `--repeat-count`
       # Instead, we eval manually here, and remove the constant to
       constant_names = ["YCT", "YCT2"]
       source = "#{constant_names.join(",")} = 1"
-      yarp_eval = RubyVM::InstructionSequence.compile_yarp(source).eval
-      assert_equal yarp_eval, 1
+      prism_eval = RubyVM::InstructionSequence.compile_prism(source).eval
+      assert_equal prism_eval, 1
       constant_names.map { |name|
         Object.send(:remove_const, name)
       }
     end
 
     def test_ConstantWriteNode
-      # We don't call test_yarp_eval directly in this case becuase we
+      # We don't call test_prism_eval directly in this case becuase we
       # don't want to assign the constant mutliple times if we run
       # with `--repeat-count`
       # Instead, we eval manually here, and remove the constant to
       constant_name = "YCT"
       source = "#{constant_name} = 1"
-      yarp_eval = RubyVM::InstructionSequence.compile_yarp(source).eval
-      assert_equal yarp_eval, 1
+      prism_eval = RubyVM::InstructionSequence.compile_prism(source).eval
+      assert_equal prism_eval, 1
       Object.send(:remove_const, constant_name)
     end
 
@@ -141,11 +141,11 @@ module YARP
 
       constant_names = ["MyBar", "MyFoo::Bar", "MyFoo::Bar::Baz"]
       source = "#{constant_names.join(",")} = Object"
-      iseq = RubyVM::InstructionSequence.compile_yarp(source)
+      iseq = RubyVM::InstructionSequence.compile_prism(source)
       $VERBOSE = nil
-      yarp_eval = iseq.eval
+      prism_eval = iseq.eval
       $VERBOSE = verbose
-      assert_equal yarp_eval, Object
+      assert_equal prism_eval, Object
 
     ensure
       ## Teardown temp constants
@@ -156,67 +156,67 @@ module YARP
     end
 
     def test_ConstantPathWriteNode
-      # test_yarp_eval("YARP::YCT = 1")
+      # test_prism_eval("Prism::YCT = 1")
     end
 
     def test_GlobalVariableTargetNode
-      test_yarp_eval("$yct, $yct1 = 1")
+      test_prism_eval("$pit, $pit1 = 1")
     end
 
     def test_GlobalVariableWriteNode
-      test_yarp_eval("$yct = 1")
+      test_prism_eval("$pit = 1")
     end
 
     def test_GlobalVariableAndWriteNode
-      test_yarp_eval("$yct = 0; $yct &&= 1")
+      test_prism_eval("$pit = 0; $pit &&= 1")
     end
 
     def test_GlobalVariableOrWriteNode
-      test_yarp_eval("$yct ||= 1")
+      test_prism_eval("$pit ||= 1")
     end
 
     def test_GlobalVariableOperatorWriteNode
-      test_yarp_eval("$yct = 0; $yct += 1")
+      test_prism_eval("$pit = 0; $pit += 1")
     end
 
     def test_InstanceVariableTargetNode
-      test_yarp_eval("class YARP::ISeqTest; @yct, @yct1 = 1; end")
+      test_prism_eval("class Prism::ISeqTest; @pit, @pit1 = 1; end")
     end
 
     def test_InstanceVariableWriteNode
-      test_yarp_eval("class YARP::ISeqTest; @yct = 1; end")
+      test_prism_eval("class Prism::ISeqTest; @pit = 1; end")
     end
 
     def test_InstanceVariableAndWriteNode
-      test_yarp_eval("@yct = 0; @yct &&= 1")
+      test_prism_eval("@pit = 0; @pit &&= 1")
     end
 
     def test_InstanceVariableOrWriteNode
-      test_yarp_eval("@yct ||= 1")
+      test_prism_eval("@pit ||= 1")
     end
 
     def test_InstanceVariableOperatorWriteNode
-      test_yarp_eval("@yct = 0; @yct += 1")
+      test_prism_eval("@pit = 0; @pit += 1")
     end
 
     def test_LocalVariableTargetNode
-      test_yarp_eval("yct, yct1 = 1")
+      test_prism_eval("pit, pit1 = 1")
     end
 
     def test_LocalVariableWriteNode
-      test_yarp_eval("yct = 1")
+      test_prism_eval("pit = 1")
     end
 
     def test_LocalVariableAndWriteNode
-      test_yarp_eval("yct = 0; yct &&= 1")
+      test_prism_eval("pit = 0; pit &&= 1")
     end
 
     def test_LocalVariableOrWriteNode
-      test_yarp_eval("yct ||= 1")
+      test_prism_eval("pit ||= 1")
     end
 
     def test_LocalVariableOperatorWriteNode
-      test_yarp_eval("yct = 0; yct += 1")
+      test_prism_eval("pit = 0; pit += 1")
     end
 
     ############################################################################
@@ -224,60 +224,60 @@ module YARP
     ############################################################################
 
     def test_EmbeddedVariableNode
-      # test_yarp_eval('class YARP::ISeqTest; @yct = 1; "#@yct"; end')
-      # test_yarp_eval('class YARP::ISeqTest; @@yct = 1; "#@@yct"; end')
-      test_yarp_eval('$yct = 1; "#$yct"')
+      # test_prism_eval('class Prism::ISeqTest; @pit = 1; "#@pit"; end')
+      # test_prism_eval('class Prism::ISeqTest; @@pit = 1; "#@@pit"; end')
+      test_prism_eval('$pit = 1; "#$pit"')
     end
 
     def test_InterpolatedRegularExpressionNode
-      test_yarp_eval('$yct = 1; /1 #$yct 1/')
-      test_yarp_eval('/1 #{1 + 2} 1/')
-      test_yarp_eval('/1 #{"2"} #{1 + 2} 1/')
+      test_prism_eval('$pit = 1; /1 #$pit 1/')
+      test_prism_eval('/1 #{1 + 2} 1/')
+      test_prism_eval('/1 #{"2"} #{1 + 2} 1/')
     end
 
     def test_InterpolatedStringNode
-      test_yarp_eval('$yct = 1; "1 #$yct 1"')
-      test_yarp_eval('"1 #{1 + 2} 1"')
+      test_prism_eval('$pit = 1; "1 #$pit 1"')
+      test_prism_eval('"1 #{1 + 2} 1"')
     end
 
     def test_InterpolatedSymbolNode
-      test_yarp_eval('$yct = 1; :"1 #$yct 1"')
-      test_yarp_eval(':"1 #{1 + 2} 1"')
+      test_prism_eval('$pit = 1; :"1 #$pit 1"')
+      test_prism_eval(':"1 #{1 + 2} 1"')
     end
 
     def test_InterpolatedXStringNode
-      test_yarp_eval('`echo #{1}`')
-      test_yarp_eval('`printf #{"100"}`')
+      test_prism_eval('`echo #{1}`')
+      test_prism_eval('`printf #{"100"}`')
     end
 
     def test_RegularExpressionNode
-      test_yarp_eval('/yct/')
-      test_yarp_eval('/yct/i')
-      test_yarp_eval('/yct/x')
-      test_yarp_eval('/yct/m')
-      test_yarp_eval('/yct/im')
-      test_yarp_eval('/yct/mx')
-      test_yarp_eval('/yct/xi')
-      test_yarp_eval('/yct/ixm')
+      test_prism_eval('/pit/')
+      test_prism_eval('/pit/i')
+      test_prism_eval('/pit/x')
+      test_prism_eval('/pit/m')
+      test_prism_eval('/pit/im')
+      test_prism_eval('/pit/mx')
+      test_prism_eval('/pit/xi')
+      test_prism_eval('/pit/ixm')
     end
 
     def test_StringConcatNode
-      # test_yarp_eval('"YARP" "::" "ISeqTest"')
+      # test_prism_eval('"Prism" "::" "ISeqTest"')
     end
 
     def test_StringNode
-      test_yarp_eval('"yct"')
+      test_prism_eval('"pit"')
     end
 
     def test_SymbolNode
-      test_yarp_eval(":yct")
+      test_prism_eval(":pit")
     end
 
     def test_XStringNode
-      # test_yarp_eval(<<~RUBY)
-      #   class YARP::ISeqTest
+      # test_prism_eval(<<~RUBY)
+      #   class Prism::ISeqTest
       #     def self.`(command) = command * 2
-      #     `yct`
+      #     `pit`
       #   end
       # RUBY
     end
@@ -287,13 +287,13 @@ module YARP
     ############################################################################
 
     def test_AndNode
-      test_yarp_eval("true && 1")
-      test_yarp_eval("false && 1")
+      test_prism_eval("true && 1")
+      test_prism_eval("false && 1")
     end
 
     def test_OrNode
-      test_yarp_eval("true || 1")
-      test_yarp_eval("false || 1")
+      test_prism_eval("true || 1")
+      test_prism_eval("false || 1")
     end
 
     ############################################################################
@@ -301,7 +301,7 @@ module YARP
     ############################################################################
 
     def test_BlockArgumentNode
-      test_yarp_eval("1.then(&:to_s)")
+      test_prism_eval("1.then(&:to_s)")
     end
 
     ############################################################################
@@ -309,20 +309,20 @@ module YARP
     ############################################################################
 
     def test_ParenthesesNode
-      test_yarp_eval("()")
-      test_yarp_eval("(1)")
+      test_prism_eval("()")
+      test_prism_eval("(1)")
     end
 
     private
 
     def compare_eval(source)
       ruby_eval = RubyVM::InstructionSequence.compile(source).eval
-      yarp_eval = RubyVM::InstructionSequence.compile_yarp(source).eval
+      prism_eval = RubyVM::InstructionSequence.compile_prism(source).eval
 
-      assert_equal ruby_eval, yarp_eval
+      assert_equal ruby_eval, prism_eval
     end
 
-    def test_yarp_eval(source)
+    def test_prism_eval(source)
       compare_eval(source)
 
       begin
