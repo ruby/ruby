cd0d010c8eb8ab14ce57a630aa87ff7caff70c1d
yui-knk <spiketeika@gmail.com>
2023-09-29 21:15:55 +0900

    Remove not used fields from POSTEXE

--- rubyparser.h
+++ rubyparser.h
@@ -956,9 +956,7 @@ typedef struct RNode_DEFINED {
 typedef struct RNode_POSTEXE {
     NODE node;
 
-    VALUE not_used;
     struct RNode *nd_body;
-    VALUE not_used2;
 } rb_node_postexe_t;
 
 typedef struct RNode_DSYM {
