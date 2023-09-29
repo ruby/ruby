01c765677566f227854d737efcf2c06d139ddbac
Nobuyoshi Nakada <nobu@ruby-lang.org>
2023-09-29 17:43:14 +0900

    Fix to replace only `-e` that is a path name in `assert_crash_report`

--- test/ruby/test_rubyoptions.rb
+++ test/ruby/test_rubyoptions.rb
@@ -861,7 +861,7 @@ class TestRubyOptions < Test::Unit::TestCase
         FileUtils.mkpath(File.join(dir, File.dirname(cmd)))
         File.write(File.join(dir, cmd), SEGVTest::KILL_SELF+"\n")
         c = Regexp.quote(cmd)
-        list = list.map {|re| Regexp.new(re.source.gsub(/-e/) {c}, re.options)}
+        list = list.map {|re| Regexp.new(re.source.gsub(/^\s*(\(\?:)?\K-e(?=:)/) {c}, re.options)}
       else
         cmd = ['-e', SEGVTest::KILL_SELF]
       end
