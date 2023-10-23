062d6050b05dba8c0cc915e83a05a418a1c8ab1d
dearblue <dearblue@users.osdn.me>
2023-10-23 23:49:19 +0900

    [rubygems/rubygems] Ignore non-tar format `.gem` files during search
    Previously, `rake install` or `rake update` would fail if there was a non-tar format `.gem` file in the current working directory.
    
    https://github.com/rubygems/rubygems/commit/f562788f1d

--- lib/rubygems/source/local.rb
+++ lib/rubygems/source/local.rb
@@ -40,10 +40,11 @@ class Gem::Source::Local < Gem::Source
 
       Dir["*.gem"].each do |file|
         pkg = Gem::Package.new(file)
+        spec = pkg.spec
       rescue SystemCallError, Gem::Package::FormatError
         # ignore
       else
-        tup = pkg.spec.name_tuple
+        tup = spec.name_tuple
         @specs[tup] = [File.expand_path(file), pkg]
 
         case type
