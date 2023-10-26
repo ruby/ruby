dbc58f5cdc504da2ff1bc96485d4d514a515486d
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-10-26 12:03:38 +0900

    Fix test failure with __runner_options__ renaming

--- tool/test/testunit/test_timeout.rb
+++ tool/test/testunit/test_timeout.rb
@@ -3,7 +3,7 @@ require 'test/unit'
 
 class TestTiemout < Test::Unit::TestCase
   def test_timeout
-    cmd = [*@options[:ruby], "#{File.dirname(__FILE__)}/test4test_timeout.rb"]
+    cmd = [*@__runner_options__[:ruby], "#{File.dirname(__FILE__)}/test4test_timeout.rb"]
     result = IO.popen(cmd, err: [:child, :out], &:read)
     assert_not_match(/^T{10}$/, result)
   end
