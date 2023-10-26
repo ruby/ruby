634e0ac140d890904c59eab8bdec09b80c78b1a4
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-10-26 12:03:38 +0900

    Fix test failure with __runner_options__ renaming

--- tool/test/testunit/test_load_failure.rb
+++ tool/test/testunit/test_load_failure.rb
@@ -13,7 +13,7 @@ class TestLoadFailure < Test::Unit::TestCase
   private
 
   def load_failure(*args)
-    IO.popen([*@options[:ruby], "#{__dir__}/../runner.rb",
+    IO.popen([*@__runner_options__[:ruby], "#{__dir__}/../runner.rb",
               "#{__dir__}/test4test_load_failure.rb",
               "--verbose", *args], err: [:child, :out]) {|f|
       assert_include(f.read, "test4test_load_failure.rb")
