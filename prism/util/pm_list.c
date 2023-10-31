dc10f5a500a8c95db254be1b48e348ff31002287
Kevin Newton <kddnewton@gmail.com>
2023-10-30 23:28:54 -0400

    [ruby/prism] Documentation for more C functions
    https://github.com/ruby/prism/commit/88336e7d9f

--- prism/util/pm_list.c
+++ prism/util/pm_list.c
@@ -1,18 +1,24 @@
 #include "prism/util/pm_list.h"
 
-// Returns true if the given list is empty.
+/**
+ * Returns true if the given list is empty.
+ */
 PRISM_EXPORTED_FUNCTION bool
 pm_list_empty_p(pm_list_t *list) {
     return list->head == NULL;
 }
 
-// Returns the size of the list.
+/**
+ * Returns the size of the list.
+ */
 PRISM_EXPORTED_FUNCTION size_t
 pm_list_size(pm_list_t *list) {
     return list->size;
 }
 
-// Append a node to the given list.
+/**
+ * Append a node to the given list.
+ */
 void
 pm_list_append(pm_list_t *list, pm_list_node_t *node) {
     if (list->head == NULL) {
@@ -25,7 +31,9 @@ pm_list_append(pm_list_t *list, pm_list_node_t *node) {
     list->size++;
 }
 
-// Deallocate the internal state of the given list.
+/**
+ * Deallocate the internal state of the given list.
+ */
 PRISM_EXPORTED_FUNCTION void
 pm_list_free(pm_list_t *list) {
     pm_list_node_t *node = list->head;
--- prism/util/pm_state_stack.h
+++ prism/util/pm_state_stack.h
@@ -12,11 +12,6 @@
 typedef uint32_t pm_state_stack_t;
 
 /**
- * Initializes the state stack to an empty stack.
- */
-#define PM_STATE_STACK_EMPTY ((pm_state_stack_t) 0)
-
-/**
  * Pushes a value onto the stack.
  *
  * @param stack The stack to push the value onto.
