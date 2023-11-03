c44c982c9332a2671d65d48273629fc265ddf530
Kevin Newton <kddnewton@gmail.com>
2023-11-03 11:00:14 -0400

    [ruby/prism] Bump to version 0.17.0
    https://github.com/ruby/prism/commit/61a306cd5c

--- lib/prism/prism.gemspec
+++ lib/prism/prism.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |spec|
   spec.name = "prism"
-  spec.version = "0.16.0"
+  spec.version = "0.17.0"
   spec.authors = ["Shopify"]
   spec.email = ["ruby@shopify.com"]
 
--- prism/version.h
+++ prism/version.h
@@ -14,7 +14,7 @@
 /**
  * The minor version of the Prism library as an int.
  */
-#define PRISM_VERSION_MINOR 16
+#define PRISM_VERSION_MINOR 17
 
 /**
  * The patch version of the Prism library as an int.
@@ -24,6 +24,6 @@
 /**
  * The version of the Prism library as a constant string.
  */
-#define PRISM_VERSION "0.16.0"
+#define PRISM_VERSION "0.17.0"
 
 #endif
