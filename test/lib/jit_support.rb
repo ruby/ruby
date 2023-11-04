9f95b6eb5d3458cc79668afd24b0b10c161d7d84
Takashi Kokubun <takashikkbn@gmail.com>
2023-11-03 22:18:15 -0700

    Skip a test that is flaky with RJIT
    It's crashing inside the bug reporter after a crash, so not sure why
    it's crashing. It's not really useful for maintaining RJIT to flag this
    test failure, so let's just ignore it until we figure out why it fails.
    
    https://github.com/ruby/ruby/actions/runs/6752729246/job/18358439166

--- test/lib/jit_support.rb
+++ test/lib/jit_support.rb
@@ -9,6 +9,10 @@ module JITSupport
     @yjit_supported = ![nil, 'no'].include?(RbConfig::CONFIG['YJIT_SUPPORT'])
   end
 
+  def yjit_enabled?
+    defined?(RubyVM::YJIT.enabled?) && RubyVM::YJIT.enabled?
+  end
+
   def yjit_force_enabled?
     "#{RbConfig::CONFIG['CFLAGS']} #{RbConfig::CONFIG['CPPFLAGS']}".match?(/(\A|\s)-D ?YJIT_FORCE_ENABLE\b/)
   end
@@ -17,6 +21,10 @@ module JITSupport
     return @rjit_supported if defined?(@rjit_supported)
     # nil in mswin
     @rjit_supported = ![nil, 'no'].include?(RbConfig::CONFIG['RJIT_SUPPORT'])
+  end
+
+  def rjit_enabled?
+    defined?(RubyVM::RJIT) && RubyVM::RJIT.enabled?
   end
 
   def rjit_force_enabled?
