0c55886fc2aad1688dbbb116803f36789f4cde9c
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-11-07 11:53:33 +0900

    [ruby/tempfile] Bump up 0.2.0
    https://github.com/ruby/tempfile/commit/d6ddf7881e

--- lib/tempfile.rb
+++ lib/tempfile.rb
@@ -88,7 +88,7 @@ require 'tmpdir'
 # mutex.
 class Tempfile < DelegateClass(File)
 
-  VERSION = "0.1.3"
+  VERSION = "0.2.0"
 
   # Creates a file in the underlying file system;
   # returns a new \Tempfile object based on that file.
