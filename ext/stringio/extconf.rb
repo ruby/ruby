1c3088117a6b22b6aa07fb8c9c28028851786c1b
Benoit Daloze <eregontp@gmail.com>
2023-11-24 13:14:56 +0100

    [ruby/stringio] Do not compile the C extension on TruffleRuby
    * Before this it was compiled but not used, because TruffleRuby has
      a stringio.rb in stdlib and .rb has precedence over .so.
      In fact that extension never worked on TruffleRuby,
      because rb_io_extract_modeenc() has never been defined on TruffleRuby.
    * So this just skip compiling the extension since compilation of it now fails:
      https://github.com/ruby/openssl/issues/699
    
    https://github.com/ruby/stringio/commit/d791b63df6

--- ext/stringio/extconf.rb
+++ ext/stringio/extconf.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: false
 require 'mkmf'
-create_makefile('stringio')
+if RUBY_ENGINE == 'ruby'
+  create_makefile('stringio')
+else
+  File.write('Makefile', dummy_makefile("").join)
+end
