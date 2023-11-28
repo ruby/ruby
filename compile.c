b8516d6d0174a10579817f4bcf5a94c8ef03dd7a
Jeremy Evans <code@jeremyevans.net>
2023-11-28 12:14:45 -0800

    Add pushtoarray VM instruction
    This instruction is similar to concattoarray, but it takes the
    number of arguments to push to the array, removes that number
    of arguments from the stack, and adds them to the array now at
    the top of the stack.
    
    This allows `f(*a, 1)` to allocate only a single array on the
    caller side (which can be reused on the callee side in the case of
    `def f(*a)`). Prior to this commit, `f(*a, 1)` would generate
    3 arrays:
    
    * a dupped by splatarray true
    * 1 wrapped in array by newarray
    * a dupped again by concatarray
    
    Instructions Before for `a = []; f(*a, 1)`:
    
    ```
    0000 newarray                               0                         (   1)[Li]
    0002 setlocal_WC_0                          a@0
    0004 putself
    0005 getlocal_WC_0                          a@0
    0007 splatarray                             true
    0009 putobject_INT2FIX_1_
    0010 newarray                               1
    0012 concatarray
    0013 opt_send_without_block                 <calldata!mid:f, argc:1, ARGS_SPLAT|FCALL>
    0015 leave
    ```
    
    Instructions After for `a = []; f(*a, 1)`:
    
    ```
    0000 newarray                               0                         (   1)[Li]
    0002 setlocal_WC_0                          a@0
    0004 putself
    0005 getlocal_WC_0                          a@0
    0007 splatarray                             true
    0009 putobject_INT2FIX_1_
    0010 pushtoarray                            1
    0012 opt_send_without_block                 <calldata!mid:f, argc:1, ARGS_SPLAT|ARGS_SPLAT_MUT|FCALL>
    0014 leave
    ```
    
    With these changes, method calls to Ruby methods should
    implicitly allocate at most one array.
    
    Ignore typeprof bundled gem failure due to unrecognized instruction.

--- compile.c
+++ compile.c
@@ -6182,11 +6182,13 @@ setup_args_core(rb_iseq_t *iseq, LINK_ANCHOR *const args, const NODE *argn,
       case NODE_ARGSCAT: {
         if (flag_ptr) *flag_ptr |= VM_CALL_ARGS_SPLAT | VM_CALL_ARGS_SPLAT_MUT;
         int argc = setup_args_core(iseq, args, RNODE_ARGSCAT(argn)->nd_head, 1, NULL, NULL);
+        bool args_pushed = false;
 
         if (nd_type_p(RNODE_ARGSCAT(argn)->nd_body, NODE_LIST)) {
             int rest_len = compile_args(iseq, args, RNODE_ARGSCAT(argn)->nd_body, &kwnode);
             if (kwnode) rest_len--;
-            ADD_INSN1(args, argn, newarray, INT2FIX(rest_len));
+            ADD_INSN1(args, argn, pushtoarray, INT2FIX(rest_len));
+            args_pushed = true;
         }
         else {
             RUBY_ASSERT(!check_keyword(RNODE_ARGSCAT(argn)->nd_body));
@@ -6197,7 +6199,7 @@ setup_args_core(rb_iseq_t *iseq, LINK_ANCHOR *const args, const NODE *argn,
             ADD_INSN1(args, argn, splatarray, Qtrue);
             argc += 1;
         }
-        else {
+        else if (!args_pushed) {
             ADD_INSN(args, argn, concattoarray);
         }
 
@@ -6220,8 +6222,7 @@ setup_args_core(rb_iseq_t *iseq, LINK_ANCHOR *const args, const NODE *argn,
             int rest_len = compile_args(iseq, args, RNODE_ARGSPUSH(argn)->nd_body, &kwnode);
             if (kwnode) rest_len--;
             ADD_INSN1(args, argn, newarray, INT2FIX(rest_len));
-            ADD_INSN1(args, argn, newarray, INT2FIX(1));
-            ADD_INSN(args, argn, concatarray);
+            ADD_INSN1(args, argn, pushtoarray, INT2FIX(1));
         }
         else {
             if (keyword_node_p(RNODE_ARGSPUSH(argn)->nd_body)) {
@@ -6229,8 +6230,7 @@ setup_args_core(rb_iseq_t *iseq, LINK_ANCHOR *const args, const NODE *argn,
             }
             else {
                 NO_CHECK(COMPILE(args, "args (cat: splat)", RNODE_ARGSPUSH(argn)->nd_body));
-                ADD_INSN1(args, argn, newarray, INT2FIX(1));
-                ADD_INSN(args, argn, concatarray);
+                ADD_INSN1(args, argn, pushtoarray, INT2FIX(1));
             }
         }
 
