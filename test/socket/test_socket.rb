722cb9a56d091a10fd6ff9eddfcc7773f06a393e
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-11-30 20:17:27 +0900

    Skip test_resolurion_error_error_code with FreeBSD environment
      https://rubyci.s3.amazonaws.com/freebsd12/ruby-master/log/20231130T103002Z.fail.html.gz

--- test/socket/test_socket.rb
+++ test/socket/test_socket.rb
@@ -771,6 +771,9 @@ class TestSocket < Test::Unit::TestCase
   end
 
   def test_resolurion_error_error_code
+    # https://rubyci.s3.amazonaws.com/freebsd12/ruby-master/log/20231130T103002Z.fail.html.gz
+    omit if /freebsd/ =~ RUBY_PLATFORM
+
     begin
       Socket.getaddrinfo("www.kame.net", 80, "AF_UNIX")
     rescue => e
