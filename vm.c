72e3d4821800a368275ea4f856f1f4f380d6dd1d
HParker <HParker@github.com>
2023-12-01 13:25:41 -0800

    allow enabling Prism via flag or env var
    Enable Prism using either --prism
    
        ruby --prism test.rb
    
    or via env var
    
        RUBY_PRISM=1 ruby test.rb

--- vm.c
+++ vm.c
@@ -4188,6 +4188,13 @@ rb_ruby_verbose_ptr(void)
     return &cr->verbose;
 }
 
+bool *
+rb_ruby_prism_ptr(void)
+{
+    rb_ractor_t *cr = GET_RACTOR();
+    return &cr->prism;
+}
+
 VALUE *
 rb_ruby_debug_ptr(void)
 {
