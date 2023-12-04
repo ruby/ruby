7d371ca25d8e8f6524894be4413484cc9b04cb72
eileencodes <eileencodes@gmail.com>
2023-12-04 15:40:43 -0500

    [PRISM] Handle percent literals for `defined?`
    Tests all the possible percent literal with `defined?`. Implements the
    missing `PM_X_STRING_NODE` for the `%x` literal.
    
    Code:
    
    ```ruby
    defined?(%x[1,2,3])
    ```
    
    ```
    "********* Ruby *************"
    == disasm: #<ISeq:<compiled>@<compiled>:0 (0,0)-(0,19)>
    0000 putobject                              "expression"
    0002 leave
    
    "********* PRISM *************"
    == disasm: #<ISeq:<compiled>@<compiled>:0 (0,0)-(0,19)>
    0000 putobject                              "expression"
    0002 leave
    ```

--- prism_compile.c
+++ prism_compile.c
@@ -1486,6 +1486,7 @@ pm_compile_defined_expr0(rb_iseq_t *iseq, const pm_node_t *node, LINK_ANCHOR *co
       case PM_REGULAR_EXPRESSION_NODE:
       case PM_STRING_NODE:
       case PM_SYMBOL_NODE:
+      case PM_X_STRING_NODE:
         dtype = DEFINED_EXPR;
         break;
       case PM_LOCAL_VARIABLE_READ_NODE:
