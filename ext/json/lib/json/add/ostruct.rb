c8faaf4c7edf67a19786cb9ba53805e5b813b918
Burdette Lamar <BurdetteLamar@Yahoo.com>
2023-12-04 18:59:48 -0600

    [flori/json] [DOC] RDoc for additions (https://github.com/flori/json/pull/557)
    * RDoc for additions
    
    * Update lib/json/add/time.rb
    
    Co-authored-by: Hiroshi SHIBATA <hsbt@ruby-lang.org>
    
    ---------
    
    https://github.com/flori/json/commit/3f2efd60f7
    
    Co-authored-by: Hiroshi SHIBATA <hsbt@ruby-lang.org>

--- ext/json/lib/json/add/ostruct.rb
+++ ext/json/lib/json/add/ostruct.rb
@@ -6,14 +6,27 @@ require 'ostruct'
 
 class OpenStruct
 
-  # Deserializes JSON string by constructing new Struct object with values
-  # <tt>t</tt> serialized by <tt>to_json</tt>.
+  # See #as_json.
   def self.json_create(object)
     new(object['t'] || object[:t])
   end
 
-  # Returns a hash, that will be turned into a JSON object and represent this
-  # object.
+  # Methods <tt>OpenStruct#as_json</tt> and +OpenStruct.json_create+ may be used
+  # to serialize and deserialize a \OpenStruct object;
+  # see Marshal[rdoc-ref:Marshal].
+  #
+  # \Method <tt>OpenStruct#as_json</tt> serializes +self+,
+  # returning a 2-element hash representing +self+:
+  #
+  #   require 'json/add/ostruct'
+  #   x = OpenStruct.new('name' => 'Rowdy', :age => nil).as_json
+  #   # => {"json_class"=>"OpenStruct", "t"=>{:name=>'Rowdy', :age=>nil}}
+  #
+  # \Method +JSON.create+ deserializes such a hash, returning a \OpenStruct object:
+  #
+  #   OpenStruct.json_create(x)
+  #   # => #<OpenStruct name='Rowdy', age=nil>
+  #
   def as_json(*)
     klass = self.class.name
     klass.to_s.empty? and raise JSON::JSONError, "Only named structs are supported!"
@@ -23,8 +36,15 @@ class OpenStruct
     }
   end
 
-  # Stores class name (OpenStruct) with this struct's values <tt>t</tt> as a
-  # JSON string.
+  # Returns a JSON string representing +self+:
+  #
+  #   require 'json/add/ostruct'
+  #   puts OpenStruct.new('name' => 'Rowdy', :age => nil).to_json
+  #
+  # Output:
+  #
+  #   {"json_class":"OpenStruct","t":{'name':'Rowdy',"age":null}}
+  #
   def to_json(*args)
     as_json.to_json(*args)
   end
