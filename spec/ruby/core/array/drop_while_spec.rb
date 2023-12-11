c0f3bccba3957761499108dba5a3c0e88bc6009b
HoneyryderChuck <cardoso_tiago@hotmail.com>
2023-12-11 12:48:57 +0000

    adding `Array#drop_while!`
    the counterpart to `Enumerable#drop_while` which mutates the array on
    which it is called.

--- spec/ruby/core/array/drop_while_spec.rb
+++ spec/ruby/core/array/drop_while_spec.rb
@@ -22,3 +22,27 @@ describe "Array#drop_while" do
     ArraySpecs::MyArray[1, 2, 3, 4, 5].drop_while { |n| n < 4 }.should be_an_instance_of(Array)
   end
 end
+
+describe "Array#drop_while!" do
+  it "returns nil if no changes were made in the array" do
+    [1, 2, 3].drop_while! { false }.should be_nil
+  end
+
+  it "removes elements from the start of the array while the block evaluates to true" do
+    a = [1, 2, 3, 4]
+    a.should equal(a.drop_while! { |n| n < 4 })
+    a.should == [4]
+  end
+
+  it "removes elements from the start of the array until the block returns nil" do
+    a = [1, 2, 3, nil, 5]
+    a.should equal(a.drop_while! { |n| n })
+    a.should == [nil, 5]
+  end
+
+  it "removes elements from the start of the array until the block returns false" do
+    a = [1, 2, 3, false, 5]
+    a.should equal(a.drop_while! { |n| n })
+    a.should == [false, 5]
+  end
+end
