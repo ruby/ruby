96e4f42b3d13f37afce1a8c584a56f6d7e98ec23
Takashi Kokubun <takashikkbn@gmail.com>
2023-12-19 00:06:21 -0800

    RJIT: Correct the macro that defines rb_vm_insns_count

--- vm_exec.c
+++ vm_exec.c
@@ -11,7 +11,7 @@
 
 #include <math.h>
 
-#if USE_YJIT
+#if USE_YJIT || USE_RJIT
 // The number of instructions executed on vm_exec_core. --yjit-stats uses this.
 uint64_t rb_vm_insns_count = 0;
 #endif
