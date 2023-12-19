8a3c95778b5c36ed22ac1665bf0765f99470c84d
Peter Zhu <peter@peterzhu.ca>
2023-12-18 20:55:59 -0500

    Fix ary_make_partial_step for compaction
    ary could change embeddedness due to compaction, so we should only get
    the pointer after allocations.
    
    The included test was crashing with:
    
        TestArray#test_slice_gc_compact_stress
        ruby/lib/pp.rb:192: [BUG] Segmentation fault at 0x0000000000000038

--- test/ruby/test_array.rb
+++ test/ruby/test_array.rb
@@ -1704,6 +1704,10 @@ class TestArray < Test::Unit::TestCase
 
   def test_slice_gc_compact_stress
     EnvUtil.under_gc_compact_stress { assert_equal([1, 2, 3, 4, 5], (0..10).to_a[1, 5]) }
+    EnvUtil.under_gc_compact_stress do
+      a = [0, 1, 2, 3, 4, 5]
+      assert_equal([2, 1, 0], a.slice((2..).step(-1)))
+    end
   end
 
   def test_slice!
