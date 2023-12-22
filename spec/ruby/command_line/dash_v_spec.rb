2fe9e08578689adec0be8a36d0ae49541551d357
Koichi Sasada <ko1@atdot.net>
2023-12-23 07:44:36 +0900

    MN: skip RUBY_DESCRIPTION spec on MN enabled

--- spec/ruby/command_line/dash_v_spec.rb
+++ spec/ruby/command_line/dash_v_spec.rb
@@ -8,6 +8,7 @@ describe "The -v command line option" do
     it "prints version and ends" do
       ruby_exe(nil, args: '-v').should include(RUBY_DESCRIPTION)
     end unless (defined?(RubyVM::YJIT) && RubyVM::YJIT.enabled?) ||
-               (defined?(RubyVM::RJIT) && RubyVM::RJIT.enabled?)
+               (defined?(RubyVM::RJIT) && RubyVM::RJIT.enabled?) ||
+               (ENV['RUBY_MN_THREADS'] == '1')
   end
 end
