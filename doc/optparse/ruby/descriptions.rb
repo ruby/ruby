e233730846fd2634208c3c568c49b76bf3b29a0b
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-12-25 11:22:12 +0900

    Partly reverted c903cddf55af1505a0779e1a131f2fe47b838260
      These are intentional words

--- doc/optparse/ruby/descriptions.rb
+++ doc/optparse/ruby/descriptions.rb
@@ -5,7 +5,7 @@ Lorem ipsum dolor sit amet, consectetuer
 adipiscing elit. Aenean commodo ligula eget.
 Aenean massa. Cum sociis natoque penatibus
 et magnis dis parturient montes, nascetur
-ridiculous mus. Donec quam felis, ultricies
+ridiculus mus. Donec quam felis, ultricies
 nec, pellentesque eu, pretium quis, sem.
 EOT
 descriptions = description.split($/)
