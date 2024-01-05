242c9538789c13222ee240f6a7459703d4e6ddd4
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-01-05 17:37:56 +0900

    Bump up net-http to 0.3.0.1

--- lib/net/http.rb
+++ lib/net/http.rb
@@ -397,7 +397,7 @@ module Net   #:nodoc:
   class HTTP < Protocol
 
     # :stopdoc:
-    VERSION = "0.3.0"
+    VERSION = "0.3.0.1"
     Revision = %q$Revision$.split[1]
     HTTPVersion = '1.1'
     begin
