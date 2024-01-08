7b37fe1ae7c3b164677d4ebb1aaa93f63c61c2e2
Aaron Patterson <tenderlove@ruby-lang.org>
2024-01-08 13:32:35 -0800

    Always freeze strings that are in the instructions
    Any objects that the instructions reference should be frozen.
    
    Co-Authored-By: Matt Valentine-House <matt@eightbitraptor.com>

--- prism_compile.c
+++ prism_compile.c
@@ -756,7 +756,7 @@ pm_interpolated_node_compile(pm_node_list_t *parts, rb_iseq_t *iseq, NODE dummy_
                         ADD_INSN1(ret, &dummy_line_node, putobject, rb_str_freeze(current_string));
                     }
                     else {
-                        ADD_INSN1(ret, &dummy_line_node, putstring, current_string);
+                        ADD_INSN1(ret, &dummy_line_node, putstring, rb_str_freeze(current_string));
                     }
                     current_string = Qnil;
                     number_of_items_pushed++;
@@ -775,7 +775,7 @@ pm_interpolated_node_compile(pm_node_list_t *parts, rb_iseq_t *iseq, NODE dummy_
                 ADD_INSN1(ret, &dummy_line_node, putobject, rb_str_freeze(current_string));
             }
             else {
-                ADD_INSN1(ret, &dummy_line_node, putstring, current_string);
+                ADD_INSN1(ret, &dummy_line_node, putstring, rb_str_freeze(current_string));
             }
             current_string = Qnil;
             number_of_items_pushed++;
