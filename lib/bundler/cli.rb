a1d5c6555efbec37a85a816c0617071ff4eccb97
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-01-10 17:03:30 +0900

    [rubygems/rubygems] bin/rubocop -A --only Layout/ExtraSpacing
    https://github.com/rubygems/rubygems/commit/39be5cd236

--- lib/bundler/cli.rb
+++ lib/bundler/cli.rb
@@ -620,7 +620,7 @@ module Bundler
     method_option "major", type: :boolean, banner:       "If updating, prefer updating to next major version (default)"
     method_option "pre", type: :boolean, banner: "If updating, always choose the highest allowed version, regardless of prerelease status"
     method_option "strict", type: :boolean, banner: "If updating, do not allow any gem to be updated past latest --patch | --minor | --major"
-    method_option "conservative", type: :boolean, banner:       "If updating, use bundle install conservative update behavior and do not allow shared dependencies to be updated"
+    method_option "conservative", type: :boolean, banner: "If updating, use bundle install conservative update behavior and do not allow shared dependencies to be updated"
     method_option "bundler", type: :string, lazy_default: "> 0.a", banner: "Update the locked version of bundler"
     def lock
       require_relative "cli/lock"
