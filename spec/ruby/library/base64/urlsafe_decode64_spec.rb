afb30eb2654253c938fbf47dbb6e24c2a3b74625
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-01-11 11:34:25 +0900

    spec/mspec/tool/wrap_with_guard.rb 'ruby_version_is ""..."3.4"' spec/ruby/library/base64/**/*_spec.rb

--- spec/ruby/library/base64/urlsafe_decode64_spec.rb
+++ spec/ruby/library/base64/urlsafe_decode64_spec.rb
@@ -1,19 +1,22 @@
 require_relative '../../spec_helper'
 
-require 'base64'
+ruby_version_is ""..."3.4" do
 
-describe "Base64#urlsafe_decode64" do
-  it "uses '_' instead of '/'" do
-    decoded = Base64.urlsafe_decode64("V2hlcmUgYW0gST8gV2hvIGFtIEk_IEFtIEk_IEk_")
-    decoded.should == 'Where am I? Who am I? Am I? I?'
-  end
+  require 'base64'
 
-  it "uses '-' instead of '+'" do
-    decoded = Base64.urlsafe_decode64('IkJlaW5nIGRpc2ludGVncmF0ZWQgbWFrZXMgbWUgdmUtcnkgYW4tZ3J5ISIgPGh1ZmYsIGh1ZmY-')
-    decoded.should == '"Being disintegrated makes me ve-ry an-gry!" <huff, huff>'
-  end
+  describe "Base64#urlsafe_decode64" do
+    it "uses '_' instead of '/'" do
+      decoded = Base64.urlsafe_decode64("V2hlcmUgYW0gST8gV2hvIGFtIEk_IEFtIEk_IEk_")
+      decoded.should == 'Where am I? Who am I? Am I? I?'
+    end
 
-  it "does not require padding" do
-    Base64.urlsafe_decode64("MQ").should == "1"
+    it "uses '-' instead of '+'" do
+      decoded = Base64.urlsafe_decode64('IkJlaW5nIGRpc2ludGVncmF0ZWQgbWFrZXMgbWUgdmUtcnkgYW4tZ3J5ISIgPGh1ZmYsIGh1ZmY-')
+      decoded.should == '"Being disintegrated makes me ve-ry an-gry!" <huff, huff>'
+    end
+
+    it "does not require padding" do
+      Base64.urlsafe_decode64("MQ").should == "1"
+    end
   end
 end
