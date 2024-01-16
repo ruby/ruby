4b6936aa047158c98a9ac8861d51e5e09229f8c0
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-01-16 16:30:32 +0900

    Unbundled samples for getoptlong

--- sample/getoptlong/abbrev.rb
+++
@@ -1,9 +0,0 @@
-require 'getoptlong'
-
-options = GetoptLong.new(
-  ['--xxx', GetoptLong::NO_ARGUMENT],
-  ['--xyz', GetoptLong::NO_ARGUMENT]
-)
-options.each do |option, argument|
-  p [option, argument]
-end
