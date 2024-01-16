d0a7c33f05626bdec6f085ff4084aaaf25214ec3
eileencodes <eileencodes@gmail.com>
2024-01-16 15:43:24 -0500

    [Prism] Implement defined? for PM_REDO_NODE
    Ruby code:
    
    ```ruby
    defined?(redo)
    ```
    
    Instructions:
    
    ```
    "********* Ruby *************"
    == disasm: #<ISeq:<compiled>@<compiled>:1 (1,0)-(59,14)>
    0000 putobject                              "expression"              (  59)[Li]
    0002 leave
    "********* PRISM *************"
    == disasm: #<ISeq:<compiled>@<compiled>:58 (58,0)-(58,14)>
    0000 putobject                              "expression"              (  58)[Li]
    0002 leave
    ```
    
    Related: ruby/prism#2188

--- prism_compile.c
+++ prism_compile.c
@@ -2478,6 +2478,7 @@ pm_compile_defined_expr0(rb_iseq_t *iseq, const pm_node_t *node, LINK_ANCHOR *co
       case PM_NEXT_NODE:
       case PM_OR_NODE:
       case PM_RANGE_NODE:
+      case PM_REDO_NODE:
       case PM_REGULAR_EXPRESSION_NODE:
       case PM_SOURCE_ENCODING_NODE:
       case PM_SOURCE_FILE_NODE:
