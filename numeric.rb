d07f9f0b2b32c6ccf0ba03d9b160af2a839714be
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-01-17 17:04:14 +0900

    Make `Integer#times` redefined-method-free

--- numeric.rb
+++ numeric.rb
@@ -229,13 +229,13 @@ class Integer
   #
   # With no block given, returns an Enumerator.
   def times
-    unless block_given?
-      return to_enum(:times) { self < 0 ? 0 : self }
+    unless defined?(yield)
+      return Primitive.cexpr! 'SIZED_ENUMERATOR(self, 0, 0, int_dotimes_size)'
     end
     i = 0
-    while i < self
+    while Primitive.cexpr!('int_lt(i, self)')
       yield i
-      i = i.succ
+      i = Primitive.cexpr!('rb_int_succ(i)')
     end
     self
   end
