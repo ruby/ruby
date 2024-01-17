4328f190eaae5fc7e15e9889a0d9e7b2b8fa56ab
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-01-17 13:15:37 +0900

    spec/mspec/tool/wrap_with_guard.rb 'ruby_version_is ""..."3.4"' spec/ruby/library/bigdecimal/**/*_spec.rb

--- spec/ruby/library/bigdecimal/inspect_spec.rb
+++ spec/ruby/library/bigdecimal/inspect_spec.rb
@@ -1,30 +1,33 @@
 require_relative '../../spec_helper'
-require 'bigdecimal'
 
-describe "BigDecimal#inspect" do
+ruby_version_is ""..."3.4" do
+  require 'bigdecimal'
 
-  before :each do
-    @bigdec = BigDecimal("1234.5678")
-  end
+  describe "BigDecimal#inspect" do
 
-  it "returns String" do
-    @bigdec.inspect.kind_of?(String).should == true
-  end
+    before :each do
+      @bigdec = BigDecimal("1234.5678")
+    end
 
-  it "looks like this" do
-    @bigdec.inspect.should == "0.12345678e4"
-  end
+    it "returns String" do
+      @bigdec.inspect.kind_of?(String).should == true
+    end
 
-  it "does not add an exponent for zero values" do
-    BigDecimal("0").inspect.should == "0.0"
-    BigDecimal("+0").inspect.should == "0.0"
-    BigDecimal("-0").inspect.should == "-0.0"
-  end
+    it "looks like this" do
+      @bigdec.inspect.should == "0.12345678e4"
+    end
 
-  it "properly cases non-finite values" do
-    BigDecimal("NaN").inspect.should == "NaN"
-    BigDecimal("Infinity").inspect.should == "Infinity"
-    BigDecimal("+Infinity").inspect.should == "Infinity"
-    BigDecimal("-Infinity").inspect.should == "-Infinity"
+    it "does not add an exponent for zero values" do
+      BigDecimal("0").inspect.should == "0.0"
+      BigDecimal("+0").inspect.should == "0.0"
+      BigDecimal("-0").inspect.should == "-0.0"
+    end
+
+    it "properly cases non-finite values" do
+      BigDecimal("NaN").inspect.should == "NaN"
+      BigDecimal("Infinity").inspect.should == "Infinity"
+      BigDecimal("+Infinity").inspect.should == "Infinity"
+      BigDecimal("-Infinity").inspect.should == "-Infinity"
+    end
   end
 end
--- spec/ruby/library/bigdecimal/modulo_spec.rb
+++ spec/ruby/library/bigdecimal/modulo_spec.rb
@@ -1,12 +1,15 @@
 require_relative '../../spec_helper'
-require_relative 'shared/modulo'
 
-describe "BigDecimal#%" do
-  it_behaves_like :bigdecimal_modulo, :%
-  it_behaves_like :bigdecimal_modulo_zerodivisionerror, :%
-end
+ruby_version_is ""..."3.4" do
+  require_relative 'shared/modulo'
 
-describe "BigDecimal#modulo" do
-  it_behaves_like :bigdecimal_modulo, :modulo
-  it_behaves_like :bigdecimal_modulo_zerodivisionerror, :modulo
+  describe "BigDecimal#%" do
+    it_behaves_like :bigdecimal_modulo, :%
+    it_behaves_like :bigdecimal_modulo_zerodivisionerror, :%
+  end
+
+  describe "BigDecimal#modulo" do
+    it_behaves_like :bigdecimal_modulo, :modulo
+    it_behaves_like :bigdecimal_modulo_zerodivisionerror, :modulo
+  end
 end
