6bd997ecfded267d968915076606f3ae0888957a
David Rodríguez <deivid.rodriguez@riseup.net>
2024-01-17 12:43:35 +0100

    [rubygems/rubygems] Remove no longer necessary code
    https://github.com/rubygems/rubygems/commit/b7d2de2ba8

--- lib/rubygems/commands/rdoc_command.rb
+++ lib/rubygems/commands/rdoc_command.rb
@@ -84,14 +84,7 @@ Use --overwrite to force rebuilding of documentation.
         FileUtils.rm_rf File.join(spec.doc_dir, "rdoc")
       end
 
-      begin
-        doc.generate
-      rescue Errno::ENOENT => e
-        match = e.message.include?(" - ")
-        alert_error "Unable to document #{spec.full_name}, " \
-                    " #{match.post_match} is missing, skipping"
-        terminate_interaction 1 if specs.length == 1
-      end
+      doc.generate
     end
   end
 end
