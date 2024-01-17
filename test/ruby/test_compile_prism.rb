1c33dfcd54ab1928d2087215cde27f4d3512288f
eileencodes <eileencodes@gmail.com>
2024-01-17 10:09:18 -0500

    [Prism] Implement defined? for PM_CONSTANT_PATH_OPERATOR_WRITE_NODE
    Ruby code:
    
    ```ruby
    defined?(Prism::CPOWN += 1)
    ```
    
    Instructions:
    
    ```
    "********* Ruby *************"
    == disasm: #<ISeq:<compiled>@<compiled>:1 (1,0)-(59,58)>
    0000 putobject                              "assignment"              (  59)[Li]
    0002 leave
    "********* PRISM *************"
    == disasm: #<ISeq:<compiled>@<compiled>:58 (58,0)-(58,58)>
    0000 putobject                              "assignment"              (  58)[Li]
    0002 leave
    ```
    
    Related: ruby/prism#2188

--- test/ruby/test_compile_prism.rb
+++ test/ruby/test_compile_prism.rb
@@ -242,6 +242,7 @@ module Prism
       assert_prism_eval("defined?(PrismTestSubclass.test_call_operator_write_node += 1)")
       assert_prism_eval("defined?(PrismTestSubclass.test_call_or_write_node ||= 1)")
       assert_prism_eval("defined?(Prism::CPAWN &&= 1)")
+      assert_prism_eval("defined?(Prism::CPOWN += 1)")
     end
 
     def test_GlobalVariableReadNode
