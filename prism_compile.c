f8ef77af3bbd2690c81cffccdf40afe953c7e6a7
eileencodes <eileencodes@gmail.com>
2024-01-17 10:22:01 -0500

    [Prism] Implement defined? for PM_INDEX_AND_WRITE_NODE
    Ruby code:
    
    ```ruby
    defined?([0][0] &&= 1)
    ```
    
    Instructions:
    
    ```
    "********* Ruby *************"
    == disasm: #<ISeq:<compiled>@<compiled>:1 (1,0)-(59,58)>
    0000 putobject                              "assignment"              (  59)[Li]
    0002 leave
    "********* PRISM *************"
    == disasm: #<ISeq:<compiled>@<compiled>:58 (58,0)-(58,58)>
    0000 putobject                              "assignment"              (  58)[Li]
    0002 leave
    ```
    
    Related: ruby/prism#2188

--- prism_compile.c
+++ prism_compile.c
@@ -2710,6 +2710,8 @@ pm_compile_defined_expr0(rb_iseq_t *iseq, const pm_node_t *node, LINK_ANCHOR *co
       case PM_CLASS_VARIABLE_AND_WRITE_NODE:
       case PM_CLASS_VARIABLE_OR_WRITE_NODE:
 
+      case PM_INDEX_AND_WRITE_NODE:
+
       case PM_INSTANCE_VARIABLE_WRITE_NODE:
       case PM_INSTANCE_VARIABLE_OPERATOR_WRITE_NODE:
       case PM_INSTANCE_VARIABLE_AND_WRITE_NODE:
