264b7363aa7e47a7868b91038a0578970bc19c1f
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-01-18 18:09:42 +0900

    Preserve spaces in messages [ci skip]

--- tool/missing-baseruby.bat
+++ tool/missing-baseruby.bat
@@ -1,6 +1,15 @@
 : "
-@echo off
+@echo off || (
+  :warn
+    echo>&2.%~1
+  goto :eof
+  :abort
+    exit /b 1
+)
 : "
-echo>&2 executable host ruby is required.  use --with-baseruby option.
-echo>&2 Note that BASERUBY must be Ruby 2.7.0 or later.
-exit 1
+: ; call:warn() { echo "$1" >&2; }
+: ; call:abort () { exit 1; }
+
+call:warn "executable host ruby is required.  use --with-baseruby option."
+call:warn "Note that BASERUBY must be Ruby 2.7.0 or later."
+call:abort
