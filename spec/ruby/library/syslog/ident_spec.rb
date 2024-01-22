c1ba155c4ed37a5eba35b7b55f61a7e98b2ad9bd
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-01-22 15:25:21 +0900

    spec/mspec/tool/wrap_with_guard.rb 'ruby_version_is ""..."3.4"' spec/ruby/library/syslog/**/*.rb

--- spec/ruby/library/syslog/ident_spec.rb
+++ spec/ruby/library/syslog/ident_spec.rb
@@ -1,34 +1,37 @@
 require_relative '../../spec_helper'
 
-platform_is_not :windows do
-  require 'syslog'
+ruby_version_is ""..."3.4" do
 
-  describe "Syslog.ident" do
-    platform_is_not :windows do
+  platform_is_not :windows do
+    require 'syslog'
 
-      before :each do
-        Syslog.opened?.should be_false
-      end
+    describe "Syslog.ident" do
+      platform_is_not :windows do
 
-      after :each do
-        Syslog.opened?.should be_false
-      end
+        before :each do
+          Syslog.opened?.should be_false
+        end
 
-      it "returns the logging identity" do
-        Syslog.open("rubyspec")
-        Syslog.ident.should == "rubyspec"
-        Syslog.close
-      end
+        after :each do
+          Syslog.opened?.should be_false
+        end
 
-      it "returns nil if the log is closed" do
-        Syslog.should_not.opened?
-        Syslog.ident.should == nil
-      end
+        it "returns the logging identity" do
+          Syslog.open("rubyspec")
+          Syslog.ident.should == "rubyspec"
+          Syslog.close
+        end
 
-      it "defaults to $0" do
-        Syslog.open
-        Syslog.ident.should == $0
-        Syslog.close
+        it "returns nil if the log is closed" do
+          Syslog.should_not.opened?
+          Syslog.ident.should == nil
+        end
+
+        it "defaults to $0" do
+          Syslog.open
+          Syslog.ident.should == $0
+          Syslog.close
+        end
       end
     end
   end
