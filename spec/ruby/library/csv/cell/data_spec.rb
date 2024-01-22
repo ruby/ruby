f2dce4e6e07abf632357ac105f4ed201e4ee9781
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-01-22 17:23:33 +0900

    spec/mspec/tool/wrap_with_guard.rb 'ruby_version_is ""..."3.4"' spec/ruby/library/csv/**/*.rb

--- spec/ruby/library/csv/cell/data_spec.rb
+++ spec/ruby/library/csv/cell/data_spec.rb
@@ -1,6 +1,9 @@
 require_relative '../../../spec_helper'
-require 'csv'
 
-describe "CSV::Cell#data" do
-  it "needs to be reviewed for spec completeness"
+ruby_version_is ""..."3.4" do
+  require 'csv'
+
+  describe "CSV::Cell#data" do
+    it "needs to be reviewed for spec completeness"
+  end
 end
