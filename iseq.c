6bcbb9a02baa8585c2d65e29c0a65d791b077611
Kevin Newton <kddnewton@gmail.com>
2024-01-22 09:39:59 -0500

    Make prism respect dump_without_opt

--- iseq.c
+++ iseq.c
@@ -1399,7 +1399,7 @@ iseqw_s_compile(int argc, VALUE *argv, VALUE self)
 }
 
 static void
-iseqw_s_compile_prism_compile(pm_parser_t *parser, VALUE opt, rb_iseq_t *iseq, VALUE file, VALUE path, int first_lineno)
+iseqw_s_compile_prism_compile(pm_parser_t *parser, VALUE optimize, rb_iseq_t *iseq, VALUE file, VALUE path, int first_lineno)
 {
     pm_node_t *node = pm_parse(parser);
 
@@ -1422,7 +1422,7 @@ iseqw_s_compile_prism_compile(pm_parser_t *parser, VALUE opt, rb_iseq_t *iseq, V
         pm_code_location(&code_location, &parser->newline_list, &node->location);
 
         rb_compile_option_t option;
-        make_compile_option(&option, opt);
+        make_compile_option(&option, optimize);
         prepare_iseq_build(iseq, rb_fstring_lit("<compiled>"), file, path, first_lineno, &code_location, -1, NULL, 0, ISEQ_TYPE_TOP, Qnil, &option);
 
         pm_scope_node_t scope_node;
@@ -1533,7 +1533,7 @@ iseqw_s_compile_file_prism(int argc, VALUE *argv, VALUE self)
 }
 
 rb_iseq_t *
-rb_iseq_new_main_prism(pm_string_t *input, pm_options_t *options, VALUE path)
+rb_iseq_new_main_prism(pm_string_t *input, pm_options_t *options, VALUE path, VALUE optimize)
 {
     pm_parser_t parser;
     pm_parser_init(&parser, pm_string_source(input), pm_string_length(input), options);
@@ -1543,7 +1543,7 @@ rb_iseq_new_main_prism(pm_string_t *input, pm_options_t *options, VALUE path)
     pm_options_line_set(options, start_line);
 
     rb_iseq_t *iseq = iseq_alloc();
-    iseqw_s_compile_prism_compile(&parser, Qnil, iseq, path, path, start_line);
+    iseqw_s_compile_prism_compile(&parser, optimize, iseq, path, path, start_line);
 
     pm_parser_free(&parser);
     return iseq;
