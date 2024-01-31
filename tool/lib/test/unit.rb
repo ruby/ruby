a31e0c9f7f5faff6e8ac52c4e3d120b923f52f71
Naoto Ono <onoto1998@gmail.com>
2024-01-31 22:21:32 +0900

    fix

--- tool/lib/test/unit.rb
+++ tool/lib/test/unit.rb
@@ -855,7 +855,10 @@ module Test
         end
         # (((@record ||= {})[suite] ||= {})[method]) = [assertions, time, error]
         if writer = @options[:launchable_test_reports]
-          location = suite&.instance_method(method).source_location
+          location = nil
+          if suite.method_defined?(:instance_method)
+            location = suite.instance_method(method).source_location
+          end
           if location && path = location.first
             # Launchable JSON schema is defined at
             # https://github.com/search?q=repo%3Alaunchableinc%2Fcli+https%3A%2F%2Flaunchableinc.com%2Fschema%2FRecordTestInput&type=code.
