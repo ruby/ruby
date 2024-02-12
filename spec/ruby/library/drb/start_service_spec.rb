d15301d4827cb0e14dedf6456ee64ab0a36622f9
Benoit Daloze <eregontp@gmail.com>
2024-02-12 11:05:51 +0100

    Exclude a problematic spec when run in CRuby via make test-spec until fixed

--- spec/ruby/library/drb/start_service_spec.rb
+++ spec/ruby/library/drb/start_service_spec.rb
@@ -1,28 +1,33 @@
 require_relative '../../spec_helper'
-require_relative 'fixtures/test_server'
-require 'drb'
 
-describe "DRb.start_service" do
-  before :each do
-    @server = DRb.start_service("druby://localhost:0", TestServer.new)
-  end
+# This does not work yet when run in CRuby via make test-spec:
+# Gem::MissingSpecError: Could not find 'ruby2_keywords' (>= 0) among 28 total gem(s)
+guard_not -> { MSpecScript.instance_variable_defined?(:@testing_ruby) } do
+  require_relative 'fixtures/test_server'
+  require 'drb'
 
-  after :each do
-    DRb.stop_service if @server
-  end
+  describe "DRb.start_service" do
+    before :each do
+      @server = DRb.start_service("druby://localhost:0", TestServer.new)
+    end
 
-  it "runs a basic remote call" do
-    DRb.current_server.should == @server
-    obj = DRbObject.new(nil, @server.uri)
-    obj.add(1,2,3).should == 6
-  end
+    after :each do
+      DRb.stop_service if @server
+    end
 
-  it "runs a basic remote call passing a block" do
-    DRb.current_server.should == @server
-    obj = DRbObject.new(nil, @server.uri)
-    obj.add_yield(2) do |i|
-      i.should == 2
-      i+1
-    end.should == 4
+    it "runs a basic remote call" do
+      DRb.current_server.should == @server
+      obj = DRbObject.new(nil, @server.uri)
+      obj.add(1,2,3).should == 6
+    end
+
+    it "runs a basic remote call passing a block" do
+      DRb.current_server.should == @server
+      obj = DRbObject.new(nil, @server.uri)
+      obj.add_yield(2) do |i|
+        i.should == 2
+        i+1
+      end.should == 4
+    end
   end
 end
