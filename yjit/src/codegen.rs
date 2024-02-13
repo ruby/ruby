35964a6256359b8990c083404cd269ba870fdb0e
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2024-02-13 10:52:35 -0500

    Update yjit/src/codegen.rs

--- yjit/src/codegen.rs
+++ yjit/src/codegen.rs
@@ -5329,7 +5329,6 @@ fn jit_rb_str_byteslice(
     let ret_opnd = asm.ccall(rb_str_byte_substr as *const u8, vec![recv, beg, len]);
     asm.stack_pop(3);
 
-    // Can either return a FIXNUM or nil
     let out_opnd = asm.stack_push(Type::TString);
     asm.mov(out_opnd, ret_opnd);
 
