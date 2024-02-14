be62a871f8118046f98f0a2faf3381bb6f606e79
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-02-14 11:37:56 +0900

    Reapply spec/mspec/tool/wrap_with_guard.rb 'ruby_version_is ""..."3.4"' spec/ruby/library/getoptlong/**/*.rb

--- spec/ruby/library/getoptlong/get_spec.rb
+++ spec/ruby/library/getoptlong/get_spec.rb
@@ -1,7 +1,10 @@
 require_relative '../../spec_helper'
-require 'getoptlong'
-require_relative 'shared/get'
 
-describe "GetoptLong#get" do
-  it_behaves_like :getoptlong_get, :get
+ruby_version_is ""..."3.4" do
+  require 'getoptlong'
+  require_relative 'shared/get'
+
+  describe "GetoptLong#get" do
+    it_behaves_like :getoptlong_get, :get
+  end
 end
--- spec/ruby/library/getoptlong/shared/each.rb
+++ spec/ruby/library/getoptlong/shared/each.rb
@@ -1,18 +1,20 @@
-describe :getoptlong_each, shared: true do
-  before :each do
-    @opts = GetoptLong.new(
-      [ '--size', '-s',             GetoptLong::REQUIRED_ARGUMENT ],
-      [ '--verbose', '-v',          GetoptLong::NO_ARGUMENT ],
-      [ '--query', '-q',            GetoptLong::NO_ARGUMENT ],
-      [ '--check', '--valid', '-c', GetoptLong::NO_ARGUMENT ]
-    )
-  end
+ruby_version_is ""..."3.4" do
+  describe :getoptlong_each, shared: true do
+    before :each do
+      @opts = GetoptLong.new(
+        [ '--size', '-s',             GetoptLong::REQUIRED_ARGUMENT ],
+        [ '--verbose', '-v',          GetoptLong::NO_ARGUMENT ],
+        [ '--query', '-q',            GetoptLong::NO_ARGUMENT ],
+        [ '--check', '--valid', '-c', GetoptLong::NO_ARGUMENT ]
+      )
+    end
 
-  it "passes each argument/value pair to the block" do
-    argv [ "--size", "10k", "-v", "-q", "a.txt", "b.txt" ] do
-      pairs = []
-      @opts.send(@method) { |arg, val| pairs << [ arg, val ] }
-      pairs.should == [ [ "--size", "10k" ], [ "--verbose", "" ], [ "--query", ""] ]
+    it "passes each argument/value pair to the block" do
+      argv [ "--size", "10k", "-v", "-q", "a.txt", "b.txt" ] do
+        pairs = []
+        @opts.send(@method) { |arg, val| pairs << [ arg, val ] }
+        pairs.should == [ [ "--size", "10k" ], [ "--verbose", "" ], [ "--query", ""] ]
+      end
     end
   end
 end
