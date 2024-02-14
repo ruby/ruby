1fc7c79dea7360c1b6cf24dcf4c2eee02c13f6d1
Kevin Newton <kddnewton@gmail.com>
2024-02-13 21:40:47 -0500

    [PRISM] Exclude additional tests from rebase

--- test/.excludes-prism/TestERBCoreWOStrScan.rb
+++ test/.excludes-prism/TestERBCoreWOStrScan.rb
@@ -1,1 +1,4 @@
 exclude(:test_def_method_with_fname, "unknown")
+exclude(:test_def_method_without_filename, "unknown")
+exclude(:test_keep_lineno, "unknown")
+exclude(:test_multi_line_comment_lineno, "unknown")
