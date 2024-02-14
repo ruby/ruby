997197b3f333f9397cf1ab3e4890ded54218d5ce
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-02-14 11:34:09 +0900

    Reapply spec/mspec/tool/wrap_with_guard.rb 'ruby_version_is ""..."3.4"' spec/ruby/library/csv/**/*.rb

--- spec/ruby/library/csv/liberal_parsing_spec.rb
+++ spec/ruby/library/csv/liberal_parsing_spec.rb
@@ -1,19 +1,22 @@
 require_relative '../../spec_helper'
-require 'csv'
 
-describe "CSV#liberal_parsing?" do
-  it "returns true if illegal input is handled" do
-    csv = CSV.new("", liberal_parsing: true)
-    csv.should.liberal_parsing?
-  end
+ruby_version_is ""..."3.4" do
+  require 'csv'
 
-  it "returns false if illegal input is not handled" do
-    csv = CSV.new("", liberal_parsing: false)
-    csv.should_not.liberal_parsing?
-  end
+  describe "CSV#liberal_parsing?" do
+    it "returns true if illegal input is handled" do
+      csv = CSV.new("", liberal_parsing: true)
+      csv.should.liberal_parsing?
+    end
 
-  it "returns false by default" do
-    csv = CSV.new("")
-    csv.should_not.liberal_parsing?
+    it "returns false if illegal input is not handled" do
+      csv = CSV.new("", liberal_parsing: false)
+      csv.should_not.liberal_parsing?
+    end
+
+    it "returns false by default" do
+      csv = CSV.new("")
+      csv.should_not.liberal_parsing?
+    end
   end
 end
