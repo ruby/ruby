9e27e969cfe454320eab91ea55de5bf480c4076e
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-02-14 11:35:20 +0900

    Reapply spec/mspec/tool/wrap_with_guard.rb 'ruby_version_is ""..."3.4"' spec/ruby/library/syslog/**/*.rb

--- spec/ruby/library/syslog/emerg_spec.rb
+++ spec/ruby/library/syslog/emerg_spec.rb
@@ -1,16 +1,19 @@
 require_relative '../../spec_helper'
 
-platform_is_not :windows do
-  require_relative 'shared/log'
-  require 'syslog'
+ruby_version_is ""..."3.4" do
 
-  describe "Syslog.emerg" do
-    # Some way needs do be found to prevent this spec
-    # from causing output on all open terminals. If this
-    # is not possible, this spec may need a special guard
-    # that only runs when requested.
-    quarantine! do
-      it_behaves_like :syslog_log, :emerg
+  platform_is_not :windows do
+    require_relative 'shared/log'
+    require 'syslog'
+
+    describe "Syslog.emerg" do
+      # Some way needs do be found to prevent this spec
+      # from causing output on all open terminals. If this
+      # is not possible, this spec may need a special guard
+      # that only runs when requested.
+      quarantine! do
+        it_behaves_like :syslog_log, :emerg
+      end
     end
   end
 end
