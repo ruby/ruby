cee0d0eed15962835eb7dce25e5ce8bc1ca923e5
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-02-14 11:37:07 +0900

    Reapply spec/mspec/tool/wrap_with_guard.rb 'ruby_version_is ""..."3.4"' spec/ruby/library/observer/**/*.rb

--- spec/ruby/library/observer/add_observer_spec.rb
+++ spec/ruby/library/observer/add_observer_spec.rb
@@ -1,23 +1,26 @@
 require_relative '../../spec_helper'
-require_relative 'fixtures/classes'
 
-describe "Observer#add_observer" do
+ruby_version_is ""..."3.4" do
+  require_relative 'fixtures/classes'
 
-  before :each do
-    @observable = ObservableSpecs.new
-    @observer = ObserverCallbackSpecs.new
+  describe "Observer#add_observer" do
+
+    before :each do
+      @observable = ObservableSpecs.new
+      @observer = ObserverCallbackSpecs.new
+    end
+
+    it "adds the observer" do
+      @observer.value.should == nil
+      @observable.changed
+      @observable.notify_observers("test")
+      @observer.value.should == nil
+
+      @observable.add_observer(@observer)
+      @observable.changed
+      @observable.notify_observers("test2")
+      @observer.value.should == "test2"
+    end
+
   end
-
-  it "adds the observer" do
-    @observer.value.should == nil
-    @observable.changed
-    @observable.notify_observers("test")
-    @observer.value.should == nil
-
-    @observable.add_observer(@observer)
-    @observable.changed
-    @observable.notify_observers("test2")
-    @observer.value.should == "test2"
-  end
-
 end
--- spec/ruby/library/observer/delete_observer_spec.rb
+++ spec/ruby/library/observer/delete_observer_spec.rb
@@ -1,19 +1,22 @@
 require_relative '../../spec_helper'
-require_relative 'fixtures/classes'
 
-describe "Observer#delete_observer" do
-  before :each do
-    @observable = ObservableSpecs.new
-    @observer = ObserverCallbackSpecs.new
+ruby_version_is ""..."3.4" do
+  require_relative 'fixtures/classes'
+
+  describe "Observer#delete_observer" do
+    before :each do
+      @observable = ObservableSpecs.new
+      @observer = ObserverCallbackSpecs.new
+    end
+
+    it "deletes the observer" do
+      @observable.add_observer(@observer)
+      @observable.delete_observer(@observer)
+
+      @observable.changed
+      @observable.notify_observers("test")
+      @observer.value.should == nil
+    end
+
   end
-
-  it "deletes the observer" do
-    @observable.add_observer(@observer)
-    @observable.delete_observer(@observer)
-
-    @observable.changed
-    @observable.notify_observers("test")
-    @observer.value.should == nil
-  end
-
 end
