961872192cde3b4a1e39b3ea11794f5c032dc0d5
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-02-15 16:39:14 +0900

    Rename and restructured net/ftp and net/http examples

--- spec/ruby/library/net/http/http/request_get_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../../../../spec_helper'
-require 'net/http'
-require_relative 'fixtures/http_server'
-require_relative 'shared/request_get'
-
-describe "Net::HTTP#request_get" do
-  it_behaves_like :net_http_request_get, :get2
-end
