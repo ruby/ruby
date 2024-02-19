cb9e0a45372d56a434fb5af4f8573815d55af34b
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-02-19 13:55:06 +0900

    Don't exclude `test_gem_package_task.rb` explicitly.
    3d4f5e7671271335429e33605366f792bc84a2a2 suppressed warnings.

--- common.mk
+++ common.mk
@@ -943,7 +943,7 @@ test-all: $(TEST_RUNNABLE)-test-all
 yes-test-all: $(PRECHECK_TEST_ALL)
 	$(ACTIONS_GROUP)
 	$(gnumake_recursive)$(Q)$(exec) $(RUNRUBY) "$(TESTSDIR)/runner.rb" --ruby="$(RUNRUBY)" \
-	$(TEST_EXCLUDES) $(TESTOPTS) $(TESTS) --exclude='rubygems/test_gem_package_task\.rb'
+	$(TEST_EXCLUDES) $(TESTOPTS) $(TESTS)
 	$(ACTIONS_ENDGROUP)
 TESTS_BUILD = mkmf
 no-test-all: PHONY
