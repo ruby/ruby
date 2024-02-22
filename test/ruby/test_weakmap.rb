16c196e71e6880fc4b29ceb96a2fb965f1e229d7
Takashi Kokubun <takashikkbn@gmail.com>
2024-02-22 13:46:10 -0800

    Skip under_gc_compact_stress on s390x

--- test/ruby/test_weakmap.rb
+++ test/ruby/test_weakmap.rb
@@ -238,6 +238,7 @@ class TestWeakMap < Test::Unit::TestCase
   end
 
   def test_gc_compact_stress
+    omit "compaction doesn't work well on s390x" if RUBY_PLATFORM =~ /s390x/ # https://github.com/ruby/ruby/pull/5077
     EnvUtil.under_gc_compact_stress { ObjectSpace::WeakMap.new }
   end
 
