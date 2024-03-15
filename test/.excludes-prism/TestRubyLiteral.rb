a83703af79dca708736a00d45917f19a3dd698bb
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-03-16 00:09:29 +0900

    Skip warnings for duplicate keys

--- test/.excludes-prism/TestRubyLiteral.rb
+++ test/.excludes-prism/TestRubyLiteral.rb
@@ -1,3 +1,4 @@
 exclude(:test_dregexp, "unknown")
 exclude(:test_frozen_string_in_array_literal, "incorrect frozen value")
+exclude(:test_hash_duplicated_key, "parser implementation dependent")
 exclude(:test_string, "https://github.com/ruby/prism/issues/2331")
