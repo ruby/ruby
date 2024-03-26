8cfa8e87b2705fb356bbbb9ef719b5c5a54f9862
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-03-26 20:30:29 +0900

    [ruby/irb] Fix a typo (https://github.com/ruby/irb/pull/912)
    https://github.com/ruby/irb/commit/2057248e40

--- lib/irb/cmd/nop.rb
+++ lib/irb/cmd/nop.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 # This file is just a placeholder for backward-compatibility.
-# Please require 'irb' and inheirt your command from `IRB::Command::Base` instead.
+# Please require 'irb' and inherit your command from `IRB::Command::Base` instead.
