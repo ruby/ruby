5e0abde8385f2f095fb7548bd8f94c5e45b1289c
Kevin Newton <kddnewton@gmail.com>
2024-03-26 14:56:17 -0400

    [PRISM] Enable passing frozen string in array test

--- test/.excludes-prism/TestRubyLiteral.rb
+++ test/.excludes-prism/TestRubyLiteral.rb
@@ -1,4 +1,3 @@
 exclude(:test_dregexp, "unknown")
-exclude(:test_frozen_string_in_array_literal, "incorrect frozen value")
 exclude(:test_hash_duplicated_key, "parser implementation dependent")
 exclude(:test_string, "https://github.com/ruby/prism/issues/2331")
