817eecf685cf2408ff468ba9c3b814e3c6389ce7
Kevin Newton <kddnewton@gmail.com>
2024-03-28 17:35:54 -0400

    [PRISM] Enable passing regexp test

--- test/.excludes-prism/TestRegexp.rb
+++ test/.excludes-prism/TestRegexp.rb
@@ -1,6 +1,5 @@
 exclude(:test_unicode_age_14_0, "unknown")
 exclude(:test_invalid_fragment, "unknown")
-exclude(:test_assign_named_capture_to_reserved_word, "unknown")
 exclude(:test_unicode_age_15_0, "unknown")
 exclude(:test_unescape, "unknown")
 exclude(:test_invalid_escape_error, "unknown")
