70645a5acdaa6028a897b828ceacd57efb9cf257
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-04-01 16:03:57 +0900

    Write gzipped data as binary
    Be careful when writing binary data on Windows.
    
    ```
    $ ruby -e 's = Gem::Util.gzip("\x04\x08[\x05".b); p s.index("\n"); puts IO::Buffer.for(s).hexdump'
    6
    0x00000000  1f 8b 08 00 6c 3d 0a 66 00 03 63 e1 88 66 05 00 ....l=.f..c..f..
    0x00000010  e3 69 10 89 04 00 00 00                         .i......
    ```

--- test/rubygems/test_gem_source.rb
+++ test/rubygems/test_gem_source.rb
@@ -54,8 +54,9 @@ class TestGemSource < Gem::TestCase
   end
 
   def test_dependency_resolver_set_file_uri
-    FileUtils.cp File.expand_path(File.join("fixtures", "prerelease_specs.4.8.gz"), __dir__), @tempdir
-    FileUtils.cp File.expand_path(File.join("fixtures", "specs.4.8.gz"), __dir__), @tempdir
+    empty_gzip = Gem::Util.gzip("\x04\x08[\x05".b)
+    File.binwrite(File.join(@tempdir, "prerelease_specs.4.8.gz"), empty_gzip)
+    File.binwrite(File.join(@tempdir, "specs.4.8.gz"), empty_gzip)
 
     source = Gem::Source.new "file://#{@tempdir}/"
 
