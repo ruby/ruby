85e5a99e3689861723af8c46c85db1a3273fcd60
Kevin Newton <kddnewton@gmail.com>
2024-04-02 10:28:40 -0400

    [PRISM] Document more reasons for failures

--- test/.excludes-prism/TestCall.rb
+++ test/.excludes-prism/TestCall.rb
@@ -1,3 +1,3 @@
-exclude(:test_call_op_asgn_keywords, "unknown")
-exclude(:test_call_op_asgn_keywords_mutable, "unknown")
-exclude(:test_kwsplat_block_order_op_asgn, "unknown")
+exclude(:test_call_op_asgn_keywords, "https://github.com/ruby/prism/issues/2438")
+exclude(:test_call_op_asgn_keywords_mutable, "https://github.com/ruby/prism/issues/2438")
+exclude(:test_kwsplat_block_order_op_asgn, "https://github.com/ruby/prism/issues/2438")
--- test/.excludes-prism/TestRubyLiteral.rb
+++ test/.excludes-prism/TestRubyLiteral.rb
@@ -1,6 +1,6 @@
 exclude(:test_debug_frozen_string_in_array_literal, "unknown")
 exclude(:test_debug_frozen_string, "unknown")
-exclude(:test_dregexp, "unknown")
+exclude(:test_dregexp, "https://github.com/ruby/prism/issues/2664")
 exclude(:test_hash_value_omission, "unknown")
 exclude(:test_integer, "unknown")
 exclude(:test_string, "https://github.com/ruby/prism/issues/2331")
