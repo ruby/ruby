7e28d3c1e376a617210152fb2fe79397f135d2df
Kevin Newton <kddnewton@gmail.com>
2024-04-03 15:06:20 -0400

    [PRISM] Enable passing tests for invalid block exits

--- test/.excludes-prism/TestISeq.rb
+++ test/.excludes-prism/TestISeq.rb
@@ -4,4 +4,3 @@ exclude(:test_syntax_error_message, "Assertion checks against specific error for
 exclude(:test_to_binary_class_tracepoint, "https://github.com/ruby/prism/issues/2662")
 exclude(:test_to_binary_end_tracepoint, "https://github.com/ruby/prism/issues/2663")
 exclude(:test_trace_points, "https://github.com/ruby/prism/issues/2660")
-exclude(:test_unreachable_syntax_error, "https://github.com/ruby/prism/issues/1604")
