f4ee3990f6560c4abe4aa796c106d726fd9746cc
Kevin Newton <kddnewton@gmail.com>
2024-04-03 15:06:20 -0400

    [PRISM] Enable passing tests for invalid block exits

--- test/.excludes-prism/TestClass.rb
+++
@@ -1,3 +0,0 @@
-exclude(:test_invalid_break_from_class_definition, "https://github.com/ruby/prism/issues/1604")
-exclude(:test_invalid_next_from_class_definition, "https://github.com/ruby/prism/issues/1604")
-exclude(:test_invalid_return_from_class_definition, "https://github.com/ruby/prism/issues/1604")
