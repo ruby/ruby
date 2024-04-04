3efc1c36f28b06973504dd2d02ed18ca443c98b2
Kevin Newton <kddnewton@gmail.com>
2024-04-04 14:31:19 -0400

    [PRISM] Add CLASS and END tracepoint events

--- test/.excludes-prism/TestISeq.rb
+++ test/.excludes-prism/TestISeq.rb
@@ -1,6 +1,4 @@
 exclude(:test_each_child, "https://github.com/ruby/prism/issues/2660")
 exclude(:test_frozen_string_literal_compile_option, "https://github.com/ruby/prism/issues/2661")
 exclude(:test_syntax_error_message, "Assertion checks against specific error format")
-exclude(:test_to_binary_class_tracepoint, "https://github.com/ruby/prism/issues/2662")
-exclude(:test_to_binary_end_tracepoint, "https://github.com/ruby/prism/issues/2663")
 exclude(:test_trace_points, "https://github.com/ruby/prism/issues/2660")
