f2ac26d914d349f9fe8fc61ad2db875f03a07fa6
Kevin Newton <kddnewton@gmail.com>
2024-04-05 17:28:45 -0400

    [ruby/prism] Bump to v0.25.0
    https://github.com/ruby/prism/commit/4da514456f

--- prism/version.h
+++ prism/version.h
@@ -14,7 +14,7 @@
 /**
  * The minor version of the Prism library as an int.
  */
-#define PRISM_VERSION_MINOR 24
+#define PRISM_VERSION_MINOR 25
 
 /**
  * The patch version of the Prism library as an int.
@@ -24,6 +24,6 @@
 /**
  * The version of the Prism library as a constant string.
  */
-#define PRISM_VERSION "0.24.0"
+#define PRISM_VERSION "0.25.0"
 
 #endif
