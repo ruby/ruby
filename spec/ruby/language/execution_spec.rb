9183101aa7399a56a93045cc85220707bb413218
Jean Boussier <byroot@ruby-lang.org>
2024-04-11 12:24:05 +0200

    prism_compile.c: X_STRING should be frozen
    The backtick method recieves a frozen string unless it is interpolated.
    
    Otherwise the string held in the ISeq could be mutated by a custom
    backtick method.

--- spec/ruby/language/execution_spec.rb
+++ spec/ruby/language/execution_spec.rb
@@ -5,11 +5,89 @@ describe "``" do
     ip = 'world'
     `echo disc #{ip}`.should == "disc world\n"
   end
+
+  it "can be redefined and receive a frozen string as argument" do
+    called = false
+    runner = Object.new
+
+    runner.singleton_class.define_method(:`) do |str|
+      called = true
+
+      str.should == "test command"
+      str.frozen?.should == true
+    end
+
+    runner.instance_exec do
+      `test command`
+    end
+
+    called.should == true
+  end
+
+  it "the argument isn't frozen if it contains interpolation" do
+    called = false
+    runner = Object.new
+
+    runner.singleton_class.define_method(:`) do |str|
+      called = true
+
+      str.should == "test command"
+      str.frozen?.should == false
+      str << "mutated"
+    end
+
+    2.times do
+      runner.instance_exec do
+        `test #{:command}`
+      end
+    end
+
+    called.should == true
+  end
 end
 
 describe "%x" do
   it "is the same as ``" do
     ip = 'world'
     %x(echo disc #{ip}).should == "disc world\n"
+  end
+
+  it "can be redefined and receive a frozen string as argument" do
+    called = false
+    runner = Object.new
+
+    runner.singleton_class.define_method(:`) do |str|
+      called = true
+
+      str.should == "test command"
+      str.frozen?.should == true
+    end
+
+    runner.instance_exec do
+      %x{test command}
+    end
+
+    called.should == true
+  end
+
+  it "the argument isn't frozen if it contains interpolation" do
+    called = false
+    runner = Object.new
+
+    runner.singleton_class.define_method(:`) do |str|
+      called = true
+
+      str.should == "test command"
+      str.frozen?.should == false
+      str << "mutated"
+    end
+
+    2.times do
+      runner.instance_exec do
+        %x{test #{:command}}
+      end
+    end
+
+    called.should == true
   end
 end
