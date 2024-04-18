a51139230bfbd509b300fafc48e9a195b4d07c77
Kevin Newton <kddnewton@gmail.com>
2024-04-18 14:36:42 -0400

    [ruby/prism] Bump to v0.26.0
    https://github.com/ruby/prism/commit/eadb09ef36

--- lib/prism/prism.gemspec
+++ lib/prism/prism.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |spec|
   spec.name = "prism"
-  spec.version = "0.25.0"
+  spec.version = "0.26.0"
   spec.authors = ["Shopify"]
   spec.email = ["ruby@shopify.com"]
 
--- prism/extension.h
+++ prism/extension.h
@@ -1,7 +1,7 @@
 #ifndef PRISM_EXT_NODE_H
 #define PRISM_EXT_NODE_H
 
-#define EXPECTED_PRISM_VERSION "0.25.0"
+#define EXPECTED_PRISM_VERSION "0.26.0"
 
 #include <ruby.h>
 #include <ruby/encoding.h>
