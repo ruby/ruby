9cb9edc8ea2a31e9ca5efe5cb4b2c3b1d4843755
Eugene Kenny <elkenny@gmail.com>
2024-04-24 12:10:48 +0100

    [Bug #20450] Remove rubyarchdir from bootsnap paths

--- tool/test_for_warn_bundled_gems/test.sh
+++ tool/test_for_warn_bundled_gems/test.sh
@@ -32,6 +32,10 @@ echo "* Show warning with bootsnap"
 ruby test_warn_bootsnap.rb
 echo
 
+echo "* Show warning with bootsnap for gem with native extension"
+ruby test_warn_bootsnap_rubyarchdir_gem.rb
+echo
+
 echo "* Show warning with zeitwerk"
 ruby test_warn_zeitwerk.rb
 echo
