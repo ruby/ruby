881c450135167a8ebee7ca17e4febf9bf4c70029
Kevin Newton <kddnewton@gmail.com>
2024-04-25 11:14:03 -0400

    [PRISM] Enable branch coverage for while/until loops

--- test/.excludes-prism/TestCoverage.rb
+++ test/.excludes-prism/TestCoverage.rb
@@ -1,6 +1,5 @@
 exclude(:test_clear_with_branches, "unknown")
 exclude(:test_eval, "unknown")
-exclude(:test_branch_coverage_for_while_statement, "branch coverage in while")
 exclude(:test_branch_coverage_for_if_statement, "branch coverage in if")
 exclude(:test_tag_break_with_branch_coverage, "branch coverage in &.")
 exclude(:test_coverage_ensure_if_return, "branch coverage in if")
