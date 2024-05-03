912fd6d6084d0809794f9cbf04c620ca69f2eb6e
Kevin Newton <kddnewton@gmail.com>
2024-05-03 12:19:45 -0400

    [PRISM] Enable passing tests for index expressions

--- test/.excludes-prism/TestCall.rb
+++
@@ -1,3 +0,0 @@
-exclude(:test_call_op_asgn_keywords, "https://github.com/ruby/prism/issues/2438")
-exclude(:test_call_op_asgn_keywords_mutable, "https://github.com/ruby/prism/issues/2438")
-exclude(:test_kwsplat_block_order_op_asgn, "https://github.com/ruby/prism/issues/2438")
