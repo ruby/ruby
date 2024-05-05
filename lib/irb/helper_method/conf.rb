e973f9cbb38668c61084481b35ae8c1d63be74b6
Stan Lo <stan001212@gmail.com>
2024-05-05 18:44:49 +0800

    [ruby/irb] Clarify that the context is IRB context (https://github.com/ruby/irb/pull/950)
    https://github.com/ruby/irb/commit/8cde57f55a

--- lib/irb/helper_method/conf.rb
+++ lib/irb/helper_method/conf.rb
@@ -1,7 +1,7 @@
 module IRB
   module HelperMethod
     class Conf < Base
-      description "Returns the current context."
+      description "Returns the current IRB context."
 
       def execute
         IRB.CurrentContext
