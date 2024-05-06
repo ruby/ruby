5e6bb0857aac9ee16d47b5a7a547fc5ba329f088
Kevin Newton <kddnewton@gmail.com>
2024-05-06 11:05:49 -0400

    [PRISM] Enable passing forwarding parameter tests

--- test/.excludes-prism/TestSyntax.rb
+++ test/.excludes-prism/TestSyntax.rb
@@ -1,7 +1,4 @@
 exclude(:test__END___cr, "unknown")
-exclude(:test_anonymous_block_forwarding, "unknown")
-exclude(:test_anonymous_keyword_rest_forwarding, "unknown")
-exclude(:test_anonymous_rest_forwarding, "unknown")
 exclude(:test_argument_forwarding_with_super, "unknown")
 exclude(:test_argument_forwarding, "unknown")
 exclude(:test_dedented_heredoc_concatenation, "unknown")
