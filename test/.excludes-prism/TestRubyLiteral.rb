494986f7d0a20b8da5828b2d7ac86150cfd3a6cb
Kevin Newton <kddnewton@gmail.com>
2024-05-08 09:19:19 -0400

    [PRISM] Enable more passing tests

--- test/.excludes-prism/TestRubyLiteral.rb
+++ test/.excludes-prism/TestRubyLiteral.rb
@@ -1,3 +1,2 @@
 exclude(:test_dregexp, "https://github.com/ruby/prism/issues/2664")
-exclude(:test_hash_value_omission, "unknown")
 exclude(:test_string, "https://github.com/ruby/prism/issues/2331")
