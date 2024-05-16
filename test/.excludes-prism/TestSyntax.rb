2dc77b247622e2fbe415d99159e5232b8821e1a3
Kevin Newton <kddnewton@gmail.com>
2024-05-16 09:24:38 -0400

    [PRISM] Enable TestSyntax#test__END___cr

--- test/.excludes-prism/TestSyntax.rb
+++ test/.excludes-prism/TestSyntax.rb
@@ -1,6 +1,4 @@
-exclude(:test__END___cr, "error message format")
 exclude(:test_dedented_heredoc_continued_line, "heredoc line continuation dedent calculation")
-exclude(:test_duplicated_when, "error message format")
 exclude(:test_error_message_encoding, "syntax error message encoding")
 exclude(:test_it, "https://github.com/ruby/prism/issues/2323")
 exclude(:test_numbered_parameter, "should raise syntax error for numbered parameters in inner blocks")
@@ -8,5 +6,6 @@ exclude(:test_syntax_error_at_newline, "error message format")
 exclude(:test_unterminated_heredoc_cr, "quoted \r heredoc terminators should not match \r\n")
 exclude(:test_warn_balanced, "missing warning for ** being interpreted as a binary operator")
 
+exclude(:test_duplicated_when, "https://bugs.ruby-lang.org/issues/20401")
 exclude(:test_optional_self_reference, "https://bugs.ruby-lang.org/issues/20478")
 exclude(:test_keyword_self_reference, "https://bugs.ruby-lang.org/issues/20478")
