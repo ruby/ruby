4ab926d0ff6b42b9b7619066f03d7992ae8143aa
Kevin Newton <kddnewton@gmail.com>
2024-05-21 13:11:11 -0400

    [PRISM] Enable TestSyntax#test_warn_balanced

--- test/.excludes-prism/TestSyntax.rb
+++ test/.excludes-prism/TestSyntax.rb
@@ -2,7 +2,6 @@ exclude(:test_dedented_heredoc_continued_line, "heredoc line continuation dedent
 exclude(:test_it, "https://github.com/ruby/prism/issues/2323")
 exclude(:test_numbered_parameter, "should raise syntax error for numbered parameters in inner blocks")
 exclude(:test_unterminated_heredoc_cr, "quoted \r heredoc terminators should not match \r\n")
-exclude(:test_warn_balanced, "missing warning for ** being interpreted as a binary operator")
 
 exclude(:test_duplicated_when, "https://bugs.ruby-lang.org/issues/20401")
 exclude(:test_optional_self_reference, "https://bugs.ruby-lang.org/issues/20478")
