bcbfc675a41579e72eb56e4fc50ddf203592868a
Kevin Newton <kddnewton@gmail.com>
2024-05-21 11:47:19 -0400

    [PRISM] Enable TestSyntax#test_numbered_parameter

--- test/.excludes-prism/TestSyntax.rb
+++ test/.excludes-prism/TestSyntax.rb
@@ -1,6 +1,5 @@
 exclude(:test_dedented_heredoc_continued_line, "heredoc line continuation dedent calculation")
 exclude(:test_it, "https://github.com/ruby/prism/issues/2323")
-exclude(:test_numbered_parameter, "should raise syntax error for numbered parameters in inner blocks")
 exclude(:test_unterminated_heredoc_cr, "quoted \r heredoc terminators should not match \r\n")
 exclude(:test_warn_balanced, "missing warning for ** being interpreted as a binary operator")
 
