2f786c1a80f689552f8fe2f69d21ba4c0d321d8f
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-05-24 12:05:24 +0900

    LibreSSL 3.5 and later provide and need to use PEM_write_bio_PrivateKey_traditional()
    Co-authored-by: "Jeremy Evans" <code@jeremyevans.net>

--- ext/openssl/ossl_pkey.c
+++ ext/openssl/ossl_pkey.c
@@ -710,7 +710,7 @@ ossl_pkey_export_traditional(int argc, VALUE *argv, VALUE self, int to_der)
 	}
     }
     else {
-#if OPENSSL_VERSION_NUMBER >= 0x10100000 && !defined(LIBRESSL_VERSION_NUMBER)
+#if OPENSSL_VERSION_NUMBER >= 0x10100000
 	if (!PEM_write_bio_PrivateKey_traditional(bio, pkey, enc, NULL, 0,
 						  ossl_pem_passwd_cb,
 						  (void *)pass)) {
