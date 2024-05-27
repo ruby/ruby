bc47ca5546a1287fbb052329075293c200d830b0
Yusuke Endoh <mame@ruby-lang.org>
2024-05-27 18:16:28 +0900

    Add a debug print for a random failure
    ```
      1) Error:
    TestRubyLiteral#test_float:
    ArgumentError: SyntaxError#path changed: "(eval at /home/chkbuild/chkbuild/tmp/build/20240527T050036Z/ruby/test/ruby/test_literal.rb:642)"->"(eval at /home/chkbuild/chkbuild/tmp/build/20240527T050036Z/ruby/test/ruby/test_literal.rb:642)"
    ```
    https://rubyci.s3.amazonaws.com/s390x/ruby-master/log/20240527T050036Z.fail.html.gz

--- test/ruby/test_literal.rb
+++ test/ruby/test_literal.rb
@@ -640,6 +640,10 @@ class TestRubyLiteral < Test::Unit::TestCase
           end
           begin
             r2 = eval(s)
+          rescue ArgumentError
+            # Debug log for a random failure: ArgumentError: SyntaxError#path changed
+            $stderr.puts "TestRubyLiteral#test_float failed: %p" % s
+            raise
           rescue SyntaxError => e
             r2 = :err
           rescue NameError
