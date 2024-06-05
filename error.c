0eef4beeab418c22046a756ef74c835d94a5f40e
BurdetteLamar <burdettelamar@yahoo.com>
2024-06-05 16:57:44 +0100

    Doc for module Errno

--- error.c
+++ error.c
@@ -2741,45 +2741,37 @@ syntax_error_with_path(VALUE exc, VALUE path, VALUE *mesg, rb_encoding *enc)
 
 /*
  *  Document-module: Errno
- *
- *  :markup: markdown
 
  *  When an operating system encounters an error,
  *  it typically reports the error as an integer error code:
  *
- *  ```
- *  $ ls nosuch.txt
- *  ls: cannot access 'nosuch.txt': No such file or directory
- *  $ echo $? # Code for last error.
- *  2
- *  ```
+ *    $ ls nosuch.txt
+ *    ls: cannot access 'nosuch.txt': No such file or directory
+ *    $ echo $? # Code for last error.
+ *    2
  *
  *  When the Ruby interpreter interacts with the operating system
- *  and receives such an error code (e.g., `2`),
- *  it maps the code to a particular Ruby exception class (e.g., `Errno::ENOENT`):
+ *  and receives such an error code (e.g., +2+),
+ *  it maps the code to a particular Ruby exception class (e.g., +Errno::ENOENT+):
  *
- *  ```
- *  File.open('nosuch.txt')
- *  # => No such file or directory @ rb_sysopen - nosuch.txt (Errno::ENOENT)
- *  ```
+ *    File.open('nosuch.txt')
+ *    # => No such file or directory @ rb_sysopen - nosuch.txt (Errno::ENOENT)
  *
  *  Each such class is:
  *
- *  - A nested class in this module, `Errno`.
+ *  - A nested class in this module, +Errno+.
  *  - A subclass of class SystemCallError.
  *  - Associated with an error code.
  *
- *  ```
- *  Errno::ENOENT.superclass # => SystemCallError
- *  Errno::ENOENT::Errno     # => 2
- *  ```
+ *  Thus:
+ *
+ *    Errno::ENOENT.superclass # => SystemCallError
+ *    Errno::ENOENT::Errno     # => 2
  *
  *  The names of nested classes are returned by method `Errno.constants`:
  *
- *  ```
- *  Errno.constants.size         # => 158
- *  Errno.constants.sort.take(5) # => [:E2BIG, :EACCES, :EADDRINUSE, :EADDRNOTAVAIL, :EADV]
- *  ```
+ *    Errno.constants.size         # => 158
+ *    Errno.constants.sort.take(5) # => [:E2BIG, :EACCES, :EADDRINUSE, :EADDRNOTAVAIL, :EADV]
  *
  *  As seen above, the error code associated with each class
  *  is available as the value of a constant;
@@ -2787,10 +2779,8 @@ syntax_error_with_path(VALUE exc, VALUE path, VALUE *mesg, rb_encoding *enc)
  *  If the class is not needed for the particular operating system,
  *  the value is zero:
  *
- *  ```
- *  Errno::ENOENT::Errno      # => 2
- *  Errno::ENOTCAPABLE::Errno # => 0
- *  ```
+ *    Errno::ENOENT::Errno      # => 2
+ *    Errno::ENOTCAPABLE::Errno # => 0
  *
  */
 
