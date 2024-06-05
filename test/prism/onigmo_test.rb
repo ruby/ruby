d9740f33e176d4036c4869d9c2ef88cc62b2ed5f
Kevin Newton <kddnewton@gmail.com>
2024-06-05 11:25:26 -0400

    [ruby/prism] Add Onigmo errors for invalid groups
    https://github.com/ruby/prism/commit/27e7685dd4

--- test/prism/onigmo_test.rb
+++ test/prism/onigmo_test.rb
@@ -30,6 +30,25 @@ module Prism
       assert_error("(?<>)", "group name is empty")
     end
 
+    def test_ONIGERR_END_PATTERN_WITH_UNMATCHED_PARENTHESIS
+      assert_error("(", "end pattern with unmatched parenthesis")
+      assert_error("(|", "end pattern with unmatched parenthesis")
+      assert_error("(?<", "end pattern with unmatched parenthesis")
+    end
+
+    def test_ONIGERR_END_PATTERN_IN_GROUP
+      assert_error("(?", "end pattern in group")
+      assert_error("(?#", "end pattern in group")
+    end
+
+    def test_ONIGERR_UNDEFINED_GROUP_OPTION
+      assert_error("(?P", "undefined group option")
+    end
+
+    def test_ONIGERR_UNMATCHED_CLOSE_PARENTHESIS
+      assert_error(")", "unmatched close parenthesis")
+    end
+
     private
 
     def assert_error(source, message)
@@ -43,73 +62,3 @@ module Prism
     end
   end
 end
-
-__END__
-
-case ONIGERR_UNMATCHED_RANGE_SPECIFIER_IN_CHAR_CLASS:
-  p = "unmatched range specifier in char-class"; break;
-case ONIGERR_UNMATCHED_CLOSE_PARENTHESIS:
-  p = "unmatched close parenthesis"; break;
-case ONIGERR_END_PATTERN_WITH_UNMATCHED_PARENTHESIS:
-  p = "end pattern with unmatched parenthesis"; break;
-case ONIGERR_END_PATTERN_IN_GROUP:
-  p = "end pattern in group"; break;
-case ONIGERR_UNDEFINED_GROUP_OPTION:
-  p = "undefined group option"; break;
-case ONIGERR_INVALID_POSIX_BRACKET_TYPE:
-  p = "invalid POSIX bracket type"; break;
-case ONIGERR_INVALID_LOOK_BEHIND_PATTERN:
-  p = "invalid pattern in look-behind"; break;
-case ONIGERR_INVALID_REPEAT_RANGE_PATTERN:
-  p = "invalid repeat range {lower,upper}"; break;
-case ONIGERR_INVALID_CONDITION_PATTERN:
-  p = "invalid conditional pattern"; break;
-case ONIGERR_TOO_BIG_NUMBER:
-  p = "too big number"; break;
-case ONIGERR_TOO_BIG_NUMBER_FOR_REPEAT_RANGE:
-  p = "too big number for repeat range"; break;
-case ONIGERR_UPPER_SMALLER_THAN_LOWER_IN_REPEAT_RANGE:
-  p = "upper is smaller than lower in repeat range"; break;
-case ONIGERR_EMPTY_RANGE_IN_CHAR_CLASS:
-  p = "empty range in char class"; break;
-case ONIGERR_TOO_MANY_MULTI_BYTE_RANGES:
-  p = "too many multibyte code ranges are specified"; break;
-case ONIGERR_TOO_SHORT_MULTI_BYTE_STRING:
-  p = "too short multibyte code string"; break;
-case ONIGERR_INVALID_BACKREF:
-  p = "invalid backref number/name"; break;
-  case ONIGERR_NUMBERED_BACKREF_OR_CALL_NOT_ALLOWED:
-    p = "numbered backref/call is not allowed. (use name)"; break;
-  case ONIGERR_TOO_SHORT_DIGITS:
-    p = "too short digits"; break;
-  case ONIGERR_TOO_LONG_WIDE_CHAR_VALUE:
-    p = "too long wide-char value"; break;
-  case ONIGERR_EMPTY_GROUP_NAME:
-    p = "group name is empty"; break;
-  case ONIGERR_INVALID_GROUP_NAME:
-    p = "invalid group name <%n>"; break;
-  case ONIGERR_INVALID_CHAR_IN_GROUP_NAME:
-#ifdef USE_NAMED_GROUP
-    p = "invalid char in group name <%n>"; break;
-#else
-    p = "invalid char in group number <%n>"; break;
-#endif
-case ONIGERR_UNDEFINED_NAME_REFERENCE:
-  p = "undefined name <%n> reference"; break;
-case ONIGERR_UNDEFINED_GROUP_REFERENCE:
-  p = "undefined group <%n> reference"; break;
-case ONIGERR_MULTIPLEX_DEFINED_NAME:
-  p = "multiplex defined name <%n>"; break;
-case ONIGERR_MULTIPLEX_DEFINITION_NAME_CALL:
-  p = "multiplex definition name <%n> call"; break;
-case ONIGERR_NEVER_ENDING_RECURSION:
-  p = "never ending recursion"; break;
-#ifdef USE_CAPTURE_HISTORY
-case ONIGERR_INVALID_CODE_POINT_VALUE:
-  p = "invalid code point value"; break;
-case ONIGERR_TOO_BIG_WIDE_CHAR_VALUE:
-  p = "too big wide-char value"; break;
-case ONIGERR_NOT_SUPPORTED_ENCODING_COMBINATION:
-  p = "not supported encoding combination"; break;
-case ONIGERR_INVALID_COMBINATION_OF_OPTIONS:
-  p = "invalid combination of options"; break;    
