d51883e420e0dea3f43db0c3a2127d4a062f80bf
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-06-07 14:19:48 +0900

    Enclose in ruby_version_is blocks

--- spec/ruby/optional/capi/data_spec.rb
+++ spec/ruby/optional/capi/data_spec.rb
@@ -1,55 +1,53 @@
 require_relative 'spec_helper'
-ruby_version_is "3.4" do
-  return
-end
+ruby_version_is ""..."3.4" do
+  load_extension("data")
 
-load_extension("data")
-
-describe "CApiAllocSpecs (a class with an alloc func defined)" do
-  it "calls the alloc func" do
-    @s = CApiAllocSpecs.new
-    @s.wrapped_data.should == 42 # not defined in initialize
-  end
-end
-
-describe "CApiWrappedStruct" do
-  before :each do
-    @s = CApiWrappedStructSpecs.new
-  end
-
-  it "wraps with Data_Wrap_Struct and Data_Get_Struct returns data" do
-    a = @s.wrap_struct(1024)
-    @s.get_struct(a).should == 1024
-  end
-
-  describe "RDATA()" do
-    it "returns the struct data" do
-      a = @s.wrap_struct(1024)
-      @s.get_struct_rdata(a).should == 1024
-    end
-
-    it "allows changing the wrapped struct" do
-      a = @s.wrap_struct(1024)
-      @s.change_struct(a, 100)
-      @s.get_struct(a).should == 100
-    end
-
-    it "raises a TypeError if the object does not wrap a struct" do
-      -> { @s.get_struct(Object.new) }.should raise_error(TypeError)
+  describe "CApiAllocSpecs (a class with an alloc func defined)" do
+    it "calls the alloc func" do
+      @s = CApiAllocSpecs.new
+      @s.wrapped_data.should == 42 # not defined in initialize
     end
   end
 
-  describe "rb_check_type" do
-    it "does not raise an exception when checking data objects" do
-      a = @s.wrap_struct(1024)
-      @s.rb_check_type(a, a).should == true
+  describe "CApiWrappedStruct" do
+    before :each do
+      @s = CApiWrappedStructSpecs.new
     end
-  end
 
-  describe "DATA_PTR" do
-    it "returns the struct data" do
+    it "wraps with Data_Wrap_Struct and Data_Get_Struct returns data" do
       a = @s.wrap_struct(1024)
-      @s.get_struct_data_ptr(a).should == 1024
+      @s.get_struct(a).should == 1024
+    end
+
+    describe "RDATA()" do
+      it "returns the struct data" do
+        a = @s.wrap_struct(1024)
+        @s.get_struct_rdata(a).should == 1024
+      end
+
+      it "allows changing the wrapped struct" do
+        a = @s.wrap_struct(1024)
+        @s.change_struct(a, 100)
+        @s.get_struct(a).should == 100
+      end
+
+      it "raises a TypeError if the object does not wrap a struct" do
+        -> { @s.get_struct(Object.new) }.should raise_error(TypeError)
+      end
+    end
+
+    describe "rb_check_type" do
+      it "does not raise an exception when checking data objects" do
+        a = @s.wrap_struct(1024)
+        @s.rb_check_type(a, a).should == true
+      end
+    end
+
+    describe "DATA_PTR" do
+      it "returns the struct data" do
+        a = @s.wrap_struct(1024)
+        @s.get_struct_data_ptr(a).should == 1024
+      end
     end
   end
 end
--- spec/ruby/optional/capi/rbasic_spec.rb
+++ spec/ruby/optional/capi/rbasic_spec.rb
@@ -14,37 +14,35 @@ describe "RBasic support for regular objects" do
   it_should_behave_like :rbasic
 end
 
-ruby_version_is "3.4" do
-  return
-end
+ruby_version_is ""..."3.4" do
+  describe "RBasic support for RData" do
+    before :all do
+      @specs = CApiRBasicRDataSpecs.new
+      @wrapping = CApiWrappedStructSpecs.new
+      @data = -> { [@wrapping.wrap_struct(1024), @wrapping.wrap_struct(1025)] }
+    end
+    it_should_behave_like :rbasic
 
-describe "RBasic support for RData" do
-  before :all do
-    @specs = CApiRBasicRDataSpecs.new
-    @wrapping = CApiWrappedStructSpecs.new
-    @data = -> { [@wrapping.wrap_struct(1024), @wrapping.wrap_struct(1025)] }
-  end
-  it_should_behave_like :rbasic
+    it "supports user flags" do
+      obj, _ = @data.call
+      initial = @specs.get_flags(obj)
+      @specs.set_flags(obj, 1 << 14 | 1 << 16 | initial).should == 1 << 14 | 1 << 16 | initial
+      @specs.get_flags(obj).should == 1 << 14 | 1 << 16 | initial
+      @specs.set_flags(obj, initial).should == initial
+    end
 
-  it "supports user flags" do
-    obj, _ = @data.call
-    initial = @specs.get_flags(obj)
-    @specs.set_flags(obj, 1 << 14 | 1 << 16 | initial).should == 1 << 14 | 1 << 16 | initial
-    @specs.get_flags(obj).should == 1 << 14 | 1 << 16 | initial
-    @specs.set_flags(obj, initial).should == initial
-  end
+    it "supports copying the flags from one object over to the other" do
+      obj1, obj2 = @data.call
+      initial = @specs.get_flags(obj1)
+      @specs.get_flags(obj2).should == initial
+      @specs.set_flags(obj1, 1 << 14 | 1 << 16 | initial)
+      @specs.get_flags(obj1).should == 1 << 14 | 1 << 16 | initial
 
-  it "supports copying the flags from one object over to the other" do
-    obj1, obj2 = @data.call
-    initial = @specs.get_flags(obj1)
-    @specs.get_flags(obj2).should == initial
-    @specs.set_flags(obj1, 1 << 14 | 1 << 16 | initial)
-    @specs.get_flags(obj1).should == 1 << 14 | 1 << 16 | initial
-
-    @specs.copy_flags(obj2, obj1)
-    @specs.get_flags(obj2).should == 1 << 14 | 1 << 16 | initial
-    @specs.set_flags(obj1, initial)
-    @specs.copy_flags(obj2, obj1)
-    @specs.get_flags(obj2).should == initial
+      @specs.copy_flags(obj2, obj1)
+      @specs.get_flags(obj2).should == 1 << 14 | 1 << 16 | initial
+      @specs.set_flags(obj1, initial)
+      @specs.copy_flags(obj2, obj1)
+      @specs.get_flags(obj2).should == initial
+    end
   end
 end
