a71c34a3f980ea7d6cd36e70f64999a65d6331ad
Jean Boussier <jean.boussier@gmail.com>
2024-06-10 15:12:54 +0200

    compile.c: use putspecialobject for RubyVM::FrozenCore
    [Bug #20569]
    
    `putobject RubyVM::FrozenCore`, is not serializable, we
    have to use `putspecialobject VM_SPECIAL_OBJECT_VMCORE`.

--- compile.c
+++ compile.c
@@ -9750,7 +9750,12 @@ iseq_compile_each0(rb_iseq_t *iseq, LINK_ANCHOR *const ret, const NODE *const no
       case NODE_LIT:{
         debugp_param("lit", node->nd_lit);
         if (!popped) {
-            ADD_INSN1(ret, node, putobject, node->nd_lit);
+            if (UNLIKELY(node->nd_lit == rb_mRubyVMFrozenCore)) {
+                ADD_INSN1(ret, node, putspecialobject, INT2FIX(VM_SPECIAL_OBJECT_VMCORE)); // [Bug #20569]
+            }
+            else {
+                ADD_INSN1(ret, node, putobject, node->nd_lit);
+            }
             RB_OBJ_WRITTEN(iseq, Qundef, node->nd_lit);
         }
         break;
