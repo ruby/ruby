696d9c69478c2e3cacbf832ae53660c67f2fce70
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2024-06-11 10:23:18 -0400

    Try cache size 512 for debugging

--- yjit/src/core.rs
+++ yjit/src/core.rs
@@ -842,7 +842,7 @@ enum CtxOp {
 }
 
 // Number of entries in the context cache
-const CTX_CACHE_SIZE: usize = 1024;
+const CTX_CACHE_SIZE: usize = 512;
 
 // Cache of the last contexts encoded
 // Empirically this saves a few percent of memory
