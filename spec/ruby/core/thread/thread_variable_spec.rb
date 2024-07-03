8dc0d2904a7d859b463a3f44ac73e5d4540a1cc1
Jeremy Evans <code@jeremyevans.net>
2024-07-03 12:17:12 -0700

    Update exception message in string_for_symbol
    This is a static function only called in two places (rb_to_id and
    rb_to_symbol), and in both places, both symbols and strings are
    allowed.  This makes the error message consistent with rb_check_id
    and rb_check_symbol.
    
    Fixes [Bug #20607]

--- spec/ruby/core/thread/thread_variable_spec.rb
+++ spec/ruby/core/thread/thread_variable_spec.rb
@@ -43,18 +43,18 @@ describe "Thread#thread_variable?" do
 
   it "raises a TypeError if the key is neither Symbol nor String when thread variables are already set" do
     @t.thread_variable_set(:a, 49)
-    -> { @t.thread_variable?(123) }.should raise_error(TypeError, "123 is not a symbol")
+    -> { @t.thread_variable?(123) }.should raise_error(TypeError, /123 is not a symbol/)
   end
 
   ruby_version_is '3.4' do
     it "raises a TypeError if the key is neither Symbol nor String when no thread variables are set" do
-      -> { @t.thread_variable?(123) }.should raise_error(TypeError, "123 is not a symbol")
+      -> { @t.thread_variable?(123) }.should raise_error(TypeError, /123 is not a symbol/)
     end
 
     it "raises a TypeError if the key is neither Symbol nor String without calling #to_sym" do
       key = mock('key')
       key.should_not_receive(:to_sym)
-      -> { @t.thread_variable?(key) }.should raise_error(TypeError, "#{key.inspect} is not a symbol")
+      -> { @t.thread_variable?(key) }.should raise_error(TypeError, /#{Regexp.escape(key.inspect)} is not a symbol/)
     end
   end
 end
