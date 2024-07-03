9fd6f55228ff3b629db6f17615a6daa7c0092b9d
Jeremy Evans <code@jeremyevans.net>
2024-07-03 12:17:12 -0700

    Update exception message in string_for_symbol
    This is a static function only called in two places (rb_to_id and
    rb_to_symbol), and in both places, both symbols and strings are
    allowed.  This makes the error message consistent with rb_check_id
    and rb_check_symbol.
    
    Fixes [Bug #20607]

--- spec/ruby/core/thread/thread_variable_set_spec.rb
+++ spec/ruby/core/thread/thread_variable_set_spec.rb
@@ -51,12 +51,12 @@ describe "Thread#thread_variable_set" do
   end
 
   it "raises a TypeError if the key is neither Symbol nor String, nor responds to #to_str" do
-    -> { @t.thread_variable_set(123, 1) }.should raise_error(TypeError, '123 is not a symbol')
+    -> { @t.thread_variable_set(123, 1) }.should raise_error(TypeError, /123 is not a symbol/)
   end
 
   it "does not try to convert the key with #to_sym" do
     key = mock('key')
     key.should_not_receive(:to_sym)
-    -> { @t.thread_variable_set(key, 42) }.should raise_error(TypeError, "#{key.inspect} is not a symbol")
+    -> { @t.thread_variable_set(key, 42) }.should raise_error(TypeError, /#{Regexp.quote(key.inspect)} is not a symbol/)
   end
 end
