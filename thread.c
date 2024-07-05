5feae29af41e48a69c2567065ccf5c0064cb757d
Jeremy Evans <code@jeremyevans.net>
2024-07-05 08:15:47 -0700

    Raise a TypeError for Thread#thread_variable{?,_get} for non-symbol
    Previously, a TypeError was not raised if there were no thread
    variables, because the conversion to symbol was done after that
    check.  Convert to symbol before checking for whether thread
    variables are set to make the behavior consistent.
    
    Fixes [Bug #20606]

--- thread.c
+++ thread.c
@@ -3756,12 +3756,13 @@ static VALUE
 rb_thread_variable_get(VALUE thread, VALUE key)
 {
     VALUE locals;
+    VALUE symbol = rb_to_symbol(key);
 
     if (LIKELY(!THREAD_LOCAL_STORAGE_INITIALISED_P(thread))) {
         return Qnil;
     }
     locals = rb_thread_local_storage(thread);
-    return rb_hash_aref(locals, rb_to_symbol(key));
+    return rb_hash_aref(locals, symbol);
 }
 
 /*
@@ -3912,13 +3913,14 @@ static VALUE
 rb_thread_variable_p(VALUE thread, VALUE key)
 {
     VALUE locals;
+    VALUE symbol = rb_to_symbol(key);
 
     if (LIKELY(!THREAD_LOCAL_STORAGE_INITIALISED_P(thread))) {
         return Qfalse;
     }
     locals = rb_thread_local_storage(thread);
 
-    return RBOOL(rb_hash_lookup(locals, rb_to_symbol(key)) != Qnil);
+    return RBOOL(rb_hash_lookup(locals, symbol) != Qnil);
 }
 
 /*
