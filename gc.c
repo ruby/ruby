91935f8d89b1e7621d054ec216ade090290ee746
BurdetteLamar <burdettelamar@yahoo.com>
2024-07-18 14:31:47 +0100

    Attempt to revert change to gc.c

--- gc.c
+++ gc.c
@@ -1781,37 +1781,26 @@ rb_memory_id(VALUE obj)
  *  Document-method: object_id
  *
  *  call-seq:
- *    __id__ -> integer
- *    object_id -> integer
+ *     obj.__id__       -> integer
+ *     obj.object_id    -> integer
  *
- *  Returns the integer identifier for +self+.
- *  The same identifier is returned for all calls for a specific object;
- *  no two active objects have the same identifier.
+ *  Returns an integer identifier for +obj+.
  *
- *  Though their behaviors are identical:
+ *  The same number will be returned on all calls to +object_id+ for a given
+ *  object, and no two active objects will share an id.
  *
- *  - BasicObject implements +__id__+;
- *  - Kernel (which is included in Object) implements +object_id+.
+ *  Note: that some objects of builtin classes are reused for optimization.
+ *  This is the case for immediate values and frozen string literals.
  *
- *  Note: some instances of builtin classes are reused for optimization;
- *  this is the case for immediate values and for frozen string literals;
- *  immediate values include:
+ *  BasicObject implements +__id__+, Kernel implements +object_id+.
  *
- *  - +nil+, +true+, +false+.
- *  - Any frozen String object.
- *  - Any Symbol object.
- *  - Some Integer objects.
- *  - Some Float objects.
+ *  Immediate values are not passed by reference but are passed by value:
+ *  +nil+, +true+, +false+, Fixnums, Symbols, and some Floats.
  *
- *  Examples:
- *
- *    nil.__id__ == nil.__id__                   # => true
- *    'foo'.__id__ == 'foo'.__id__               # => false # Not frozen.
- *    'foo'.freeze.__id__ == 'foo'.freeze.__id__ # => true  # Frozen.
- *    :foo.__id__ == :foo.__id__                 # => true
- *    1.__id__ == 1.__id__                       # => true
- *    3.14.__id__ == 3.14.__id__                 # => true
- *
+ *      Object.new.object_id  == Object.new.object_id  # => false
+ *      (21 * 2).object_id    == (21 * 2).object_id    # => true
+ *      "hello".object_id     == "hello".object_id     # => false
+ *      "hi".freeze.object_id == "hi".freeze.object_id # => true
  */
 
 VALUE
