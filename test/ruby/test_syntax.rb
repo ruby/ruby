c544f2672680d3c2992543bd91cdff6e7ce5ec51
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-07-21 13:00:38 +0900

    Fix a typo in setup of block-after-blockcall tests
    Unparenthesize the argument and make `command_call` when calling with
    `do`-block.

--- test/ruby/test_syntax.rb
+++ test/ruby/test_syntax.rb
@@ -208,7 +208,7 @@ class TestSyntax < Test::Unit::TestCase
     blocks = [['do end', 'do'], ['{}', 'brace']],
     *|
     [%w'. dot', %w':: colon'].product(methods, blocks) do |(c, n1), (m, n2), (b, n3)|
-      m = m.tr_s('()', ' ').strip if n2 == 'do'
+      m = m.tr_s('()', ' ').strip if n3 == 'do'
       name = "test_#{n3}_block_after_blockcall_#{n1}_#{n2}_arg"
       code = "#{blockcall}#{c}#{m} #{b}"
       define_method(name) {assert_valid_syntax(code, bug6115)}
