58facddc413a27f8da3542ce37e863b81e545562
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-08-08 12:08:12 +0900

    Revert "[ruby/uri] Warn compatibility methods in RFC3986_PARSER"
    This reverts commit c3becc3ba6c584fbeabd5182e304e61529235fe6.

--- lib/uri/rfc3986_parser.rb
+++ lib/uri/rfc3986_parser.rb
@@ -147,19 +147,16 @@ module URI
 
     # Compatibility for RFC2396 parser
     def make_regexp(schemes = nil) # :nodoc:
-      warn "URI::RFC3986_PARSER.make_regexp is obsoleted. Use URI::RFC2396_PARSER.make_regexp explicitly.", uplevel: 1 if $VERBOSE
       RFC2396_PARSER.make_regexp(schemes)
     end
 
     # Compatibility for RFC2396 parser
     def escape(str, unsafe = nil) # :nodoc:
-      warn "URI::RFC3986_PARSER.escape is obsoleted. Use URI::RFC2396_PARSER.escape explicitly.", uplevel: 1 if $VERBOSE
       unsafe ? RFC2396_PARSER.escape(str, unsafe) : RFC2396_PARSER.escape(str)
     end
 
     # Compatibility for RFC2396 parser
     def unescape(str, escaped = nil) # :nodoc:
-      warn "URI::RFC3986_PARSER.unescape is obsoleted. Use URI::RFC2396_PARSER.unescape explicitly.", uplevel: 1 if $VERBOSE
       escaped ? RFC2396_PARSER.unescape(str, escaped) : RFC2396_PARSER.unescape(str)
     end
 
