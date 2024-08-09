2be7143583d56f8b91f4e1dcb2e62ed731cc15d0
Naoto Ono <onoto1998@gmail.com>
2024-08-09 11:16:50 +0900

    Increase timeout in `test_darwin_invalid_call` method

--- test/ruby/test_vm_dump.rb
+++ test/ruby/test_vm_dump.rb
@@ -4,14 +4,14 @@ require 'test/unit'
 return unless /darwin/ =~ RUBY_PLATFORM
 
 class TestVMDump < Test::Unit::TestCase
-  def assert_darwin_vm_dump_works(args)
+  def assert_darwin_vm_dump_works(args, timeout=nil)
     pend "macOS 15 beta is not working with this assertion" if /darwin/ =~ RUBY_PLATFORM && /15/ =~ `sw_vers -productVersion`
 
-    assert_in_out_err(args, "", [], /^\[IMPORTANT\]/, timeout: 60)
+    assert_in_out_err(args, "", [], /^\[IMPORTANT\]/, timeout: timeout || 60)
   end
 
   def test_darwin_invalid_call
-    assert_darwin_vm_dump_works(['-r-test-/fatal', '-eBug.invalid_call(1)'])
+    assert_darwin_vm_dump_works(['-r-test-/fatal', '-eBug.invalid_call(1)'], 180)
   end
 
   def test_darwin_segv_in_syscall
