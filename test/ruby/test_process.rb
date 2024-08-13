6d962fd59c45b0caeb50dc03031cc7414ddba5d8
Naoto Ono <onoto1998@gmail.com>
2024-08-13 15:59:49 +0900

    fix

--- test/ruby/test_process.rb
+++ test/ruby/test_process.rb
@@ -1725,24 +1725,11 @@ class TestProcess < Test::Unit::TestCase
   end
 
   def test_wait_and_sigchild
-    Timeout.timeout(180) do
-      signal_received = []
-      IO.pipe do |sig_r, sig_w|
-        Signal.trap(:CHLD) do
-          signal_received << true
-          sig_w.write('?')
-        end
-        pid = nil
-        IO.pipe do |r, w|
-          pid = fork { r.read(1); exit }
-          Thread.start {
-            Thread.current.report_on_exception = false
-            raise
-          }
-          w.puts
-        end
-        Process.wait pid
-        assert_send [sig_r, :wait_readable, 5], 'self-pipe not readable'
+    signal_received = []
+    IO.pipe do |sig_r, sig_w|
+      Signal.trap(:CHLD) do
+        signal_received << true
+        sig_w.write('?')
       end
       pid = nil
       th = nil
@@ -1878,14 +1865,12 @@ class TestProcess < Test::Unit::TestCase
 
   if Process.respond_to?(:daemon)
     def test_daemon_default
-      Timeout.timeout(180) do
-        data = IO.popen("-", "r+") do |f|
-          break f.read if f
-          Process.daemon
-          puts "ng"
-        end
-        assert_equal("", data)
+      data = IO.popen("-", "r+") do |f|
+        break f.read if f
+        Process.daemon
+        puts "ng"
       end
+      assert_equal("", data)
     end
 
     def test_daemon_noclose
