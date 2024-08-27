2157dcb568de0e387c6a203a731caea7923f856e
eileencodes <eileencodes@gmail.com>
2024-08-27 16:40:48 -0400

    [ruby/prism] Add `contains_splat` flag
    If we have a splat, add a flag for it named `contains_splat`.
    
    https://github.com/ruby/prism/commit/5be97a75c8

--- prism/prism.c
+++ prism/prism.c
@@ -2063,6 +2063,10 @@ pm_arguments_node_arguments_append(pm_arguments_node_t *node, pm_node_t *argumen
 
     node->base.location.end = argument->location.end;
     pm_node_list_append(&node->arguments, argument);
+
+    if (PM_NODE_TYPE_P(argument, PM_SPLAT_NODE)) {
+        pm_node_flag_set((pm_node_t *) node, PM_ARGUMENTS_NODE_FLAGS_CONTAINS_SPLAT);
+    }
 }
 
 /**
