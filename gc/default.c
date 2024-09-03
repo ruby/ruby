ruby/ruby

commit 53eaa67305611e29e45722036cd49b528ac32abf

Unpoision the object in rb_gc_impl_garbage_object_p

gc/default.c
@@ -1653,13 +1653,15 @@ rb_gc_impl_garbage_object_p(void *objspace_ptr, VALUE ptr)
 {
     rb_objspace_t *objspace = objspace_ptr;
 
-    switch (BUILTIN_TYPE(ptr)) {
-      case T_NONE:
-      case T_MOVED:
-      case T_ZOMBIE:
-        return true;
-      default:
-        break;
+    asan_unpoisoning_object(ptr) {
+        switch (BUILTIN_TYPE(ptr)) {
+        case T_NONE:
+        case T_MOVED:
+        case T_ZOMBIE:
+            return true;
+        default:
+            break;
+        }
     }
 
     return is_lazy_sweeping(objspace) && GET_HEAP_PAGE(ptr)->flags.before_sweep &&

commit e7fbdf8187ab62bfbe8b2660eda01de94341412e

Fix indentation broken in 53eaa67 [ci skip]

gc/default.c
@@ -1655,11 +1655,11 @@ rb_gc_impl_garbage_object_p(void *objspace_ptr, VALUE ptr)
 
     asan_unpoisoning_object(ptr) {
         switch (BUILTIN_TYPE(ptr)) {
-        case T_NONE:
-        case T_MOVED:
-        case T_ZOMBIE:
+          case T_NONE:
+          case T_MOVED:
+          case T_ZOMBIE:
             return true;
-        default:
+          default:
             break;
         }
     }