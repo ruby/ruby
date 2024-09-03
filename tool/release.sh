6986536488ba23c033c043ef8765438f22df7bf5
Takashi Kokubun <takashikkbn@gmail.com>
2024-09-02 23:31:58 -0700

    Experiment with removing --profile ruby for now

--- tool/release.sh
+++ tool/release.sh
@@ -5,11 +5,6 @@
 #   tool/release.sh 3.0.0-rc1
 
 EXTS='.tar.gz .tar.xz .zip'
-if [[ -n $AWS_ACCESS_KEY_ID ]]; then
-  AWS_CLI_OPTS=""
-else
-  AWS_CLI_OPTS="--profile ruby"
-fi
 
 ver=$1
 if [[ $ver =~ ^([1-9]\.[0-9])\.([0-9]|[1-9][0-9]|0-(preview[1-9]|rc[1-9]))$ ]]; then
@@ -23,5 +18,5 @@ short=${BASH_REMATCH[1]}
 echo $ver
 echo $short
 for ext in $EXTS; do
-  aws $AWS_CLI_OPTS s3 cp s3://ftp.r-l.o/pub/tmp/ruby-$ver-draft$ext s3://ftp.r-l.o/pub/ruby/$short/ruby-$ver$ext
+  aws s3 cp s3://ftp.r-l.o/pub/tmp/ruby-$ver-draft$ext s3://ftp.r-l.o/pub/ruby/$short/ruby-$ver$ext
 done
