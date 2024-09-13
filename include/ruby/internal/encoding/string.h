b5bebf9df4119c5ccabc84dbbc19b53dcb12f275
NAITOH Jun <naitoh@gmail.com>
2024-09-14 06:36:11 +0900

    fix rb_memsearch() document
    ## Why?
    The explanation of x and y is reversed.
    
    https://github.com/ruby/ruby/blob/ddbd64400199fd408d23c85f9fb0d7f742ecf9e1/re.c#L251-L256
    ```
    long
    rb_memsearch(const void *x0, long m, const void *y0, long n, rb_encoding *enc)
    {
        const unsigned char *x = x0, *y = y0;
    
        if (m > n) return -1;
    ```

--- include/ruby/internal/encoding/string.h
+++ include/ruby/internal/encoding/string.h
@@ -307,13 +307,13 @@ RBIMPL_ATTR_NONNULL(())
 /**
  * Looks for the passed string in the passed buffer.
  *
- * @param[in]  x          Buffer that potentially includes `y`.
+ * @param[in]  x          Query string.
  * @param[in]  m          Number of bytes of `x`.
- * @param[in]  y          Query string.
+ * @param[in]  y          Buffer that potentially includes `x`.
  * @param[in]  n          Number of bytes of `y`.
  * @param[in]  enc        Encoding of both `x` and `y`.
  * @retval     -1         Not found.
- * @retval     otherwise  Found index in `x`.
+ * @retval     otherwise  Found index in `y`.
  * @note       This API can match at a non-character-boundary.
  */
 long rb_memsearch(const void *x, long m, const void *y, long n, rb_encoding *enc);
