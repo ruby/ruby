1ecac5d4ed620247bdb74fa2c20e1665fae75b1a
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-09-17 16:16:45 +0900

    TestProcess#test_daemon_noclose is working fine with macOS 15.1 beta3 and Xcode 16 RC

--- test/ruby/test_process.rb
+++ test/ruby/test_process.rb
@@ -1874,8 +1874,6 @@ class TestProcess < Test::Unit::TestCase
     end
 
     def test_daemon_noclose
-      pend "macOS 15 beta is not working with this test" if macos?(15)
-
       data = IO.popen("-", "r+") do |f|
         break f.read if f
         Process.daemon(false, true)
