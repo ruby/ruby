ff6eac7e27c27846c4d331a3dc097c488e0984ac
Misaki Shioi <shioi.mm@gmail.com>
2024-09-20 22:14:50 +0900

    Introduction of Happy Eyeballs Version 2 (RFC8305) in TCPSocket.new
    This is an implementation of Happy Eyeballs version 2 (RFC 8305) in `TCPSocket.new`.
    See https://github.com/ruby/ruby/pull/11653
    
    1. Background
    Prior to this implementation, I implemented Happy Eyeballs Version 2 (HEv2) for `Socket.tcp` in https://github.com/ruby/ruby/pull/9374.
    HEv2 is an algorithm defined in [RFC 8305](https://datatracker.ietf.org/doc/html/rfc8305), aimed at improving network connectivity.
    For more details on the specific cases that HEv2 helps, please refer to https://bugs.ruby-lang.org/issues/20108.
    
    2. Proposal & Outcome
    This proposal implements the same HEv2 algorithm in `TCPSocket.new`.
    Since `TCPSocket.new` is used more widely than `Socket.tcp`, this change is expected to broaden the impact of HEv2's benefits.
    Like `Socket.tcp`, I have also added `fast_fallback` keyword argument to `TCPSocket.new`.
    This option is set to true by default, enabling the HEv2 functionality.
    However, users can explicitly set it to false to disable HEv2 and use the previous behavior of `TCPSocket.new`.
    
    It should be noted that HEv2 is enabled only in environments where pthreads are available.
    This specification follows the approach taken in https://bugs.ruby-lang.org/issues/19965 , where name resolution can be interrupted.
    (In environments where pthreads are not available, the `fast_fallback` option is ignored.)
    
    3. Performance
    Below is the benchmark of 100 requests to `www.ruby-lang.org` with the fast_fallback option set to true and false, respectively.
    While there is a slight performance degradation when HEv2 is enabled, the degradation is smaller compared to that seen in `Socket.tcp`.
    
    ```
    ~/s/build ❯❯❯ ../install/bin/ruby ../ruby/test.rb
    Rehearsal --------------------------------------------------------
    fast_fallback: true    0.017588   0.097045   0.114633 (  1.460664)
    fast_fallback: false   0.014033   0.078984   0.093017 (  1.413951)
    ----------------------------------------------- total: 0.207650sec
    
                               user     system      total        real
    fast_fallback: true    0.020891   0.124054   0.144945 (  1.473816)
    fast_fallback: false   0.018392   0.110852   0.129244 (  1.466014)
    ```

--- ext/socket/raddrinfo.c
+++ ext/socket/raddrinfo.c
@@ -469,7 +469,7 @@ cancel_getaddrinfo(void *ptr)
     rb_nativethread_lock_unlock(&arg->lock);
 }
 
-static int
+int
 do_pthread_create(pthread_t *th, void *(*start_routine) (void *), void *arg)
 {
     int limit = 3, ret;
@@ -822,7 +822,7 @@ str_is_number(const char *p)
     ((ptr)[0] == name[0] && \
      rb_strlen_lit(name) == (len) && memcmp(ptr, name, len) == 0)
 
-static char*
+char*
 host_str(VALUE host, char *hbuf, size_t hbuflen, int *flags_ptr)
 {
     if (NIL_P(host)) {
@@ -861,7 +861,7 @@ host_str(VALUE host, char *hbuf, size_t hbuflen, int *flags_ptr)
     }
 }
 
-static char*
+char*
 port_str(VALUE port, char *pbuf, size_t pbuflen, int *flags_ptr)
 {
     if (NIL_P(port)) {
@@ -3022,6 +3022,109 @@ rsock_io_socket_addrinfo(VALUE io, struct sockaddr *addr, socklen_t len)
     }
 
     UNREACHABLE_RETURN(Qnil);
+}
+
+void
+free_fast_fallback_getaddrinfo_shared(struct fast_fallback_getaddrinfo_shared **shared)
+{
+    free((*shared)->node);
+    (*shared)->node = NULL;
+    free((*shared)->service);
+    (*shared)->service = NULL;
+    close((*shared)->notify);
+    close((*shared)->wait);
+    rb_nativethread_lock_destroy((*shared)->lock);
+    free(*shared);
+    *shared = NULL;
+}
+
+void
+free_fast_fallback_getaddrinfo_entry(struct fast_fallback_getaddrinfo_entry **entry)
+{
+    if ((*entry)->ai) {
+        freeaddrinfo((*entry)->ai);
+        (*entry)->ai = NULL;
+    }
+    free(*entry);
+    *entry = NULL;
+}
+
+void *
+do_fast_fallback_getaddrinfo(void *ptr)
+{
+    int debug = false;
+    struct fast_fallback_getaddrinfo_entry *entry = (struct fast_fallback_getaddrinfo_entry *)ptr;
+    struct fast_fallback_getaddrinfo_shared *shared = entry->shared;
+    int err = 0, need_free = 0, shared_need_free = 0;
+
+    err = numeric_getaddrinfo(shared->node, shared->service, &entry->hints, &entry->ai);
+
+    if (debug) printf("do_fast_fallback_getaddrinfo %d starts to getaddrinfo \n", entry->family);
+
+    if (err != 0) {
+        err = getaddrinfo(shared->node, shared->service, &entry->hints, &entry->ai);
+       #ifdef __linux__
+       /* On Linux (mainly Ubuntu 13.04) /etc/nsswitch.conf has mdns4 and
+        * it cause getaddrinfo to return EAI_SYSTEM/ENOENT. [ruby-list:49420]
+        */
+       if (err == EAI_SYSTEM && errno == ENOENT)
+           err = EAI_NONAME;
+       #endif
+    }
+    if (debug) printf("do_fast_fallback_getaddrinfo %d finished to getaddrinfo\n", entry->family);
+
+    /* for testing HEv2 */
+    if (entry->test_sleep_ms > 0) {
+        struct timespec sleep_ts;
+        sleep_ts.tv_sec = entry->test_sleep_ms / 1000;
+        sleep_ts.tv_nsec = (entry->test_sleep_ms % 1000) * 1000000L;
+        if (sleep_ts.tv_nsec >= 1000000000L) {
+            sleep_ts.tv_sec += sleep_ts.tv_nsec / 1000000000L;
+            sleep_ts.tv_nsec = sleep_ts.tv_nsec % 1000000000L;
+        }
+        if (debug) printf("do_fast_fallback_getaddrinfo %d starts to nanosleep %ld:%ld\n", entry->family, sleep_ts.tv_sec, sleep_ts.tv_nsec);
+        nanosleep(&sleep_ts, NULL);
+        if (debug) printf("do_fast_fallback_getaddrinfo %d finished to nanosleep %ld:%ld\n", entry->family, sleep_ts.tv_sec, sleep_ts.tv_nsec);
+    }
+    if (entry->test_ecode > 0) {
+        err = entry->test_ecode;
+        if (entry->ai) {
+            freeaddrinfo(entry->ai);
+            entry->ai = NULL;
+        }
+        if (debug) printf("do_fast_fallback_getaddrinfo %d fail\n", entry->family);
+    }
+
+    rb_nativethread_lock_lock(shared->lock);
+    {
+        entry->err = err;
+        if (*shared->cancelled) {
+            if (entry->ai) {
+                freeaddrinfo(entry->ai);
+                entry->ai = NULL;
+            }
+        } else {
+            if (debug) printf("do_fast_fallback_getaddrinfo %d starts to write\n", entry->family);
+            if (entry->family == AF_INET6) {
+                write(shared->notify, IPV6_HOSTNAME_RESOLVED, strlen(IPV6_HOSTNAME_RESOLVED));
+            } else if (entry->family == AF_INET) {
+                write(shared->notify, IPV4_HOSTNAME_RESOLVED, strlen(IPV4_HOSTNAME_RESOLVED));
+            }
+            if (debug) printf("do_fast_fallback_getaddrinfo %d finished to write\n", entry->family);
+        }
+        if (--(entry->refcount) == 0) need_free = 1;
+        if (--(shared->refcount) == 0) shared_need_free = 1;
+    }
+    rb_nativethread_lock_unlock(shared->lock);
+
+    if (need_free && entry) {
+        free_fast_fallback_getaddrinfo_entry(&entry);
+    }
+    if (shared_need_free && shared) {
+        free_fast_fallback_getaddrinfo_shared(&shared);
+    }
+
+    return 0;
 }
 
 /*
