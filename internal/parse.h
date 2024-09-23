462523e405234ca1d01943f5326b2d379dff622f
Peter Zhu <peter@peterzhu.ca>
2024-09-23 13:55:37 -0400

    Fix memory leak in Ripper for indented heredocs
    The allocated parser string is never freed, which causes a memory leak.
    
    The following code leaks memory:
    
        Ripper.sexp_raw(DATA.read)
    
        __END__
        <<~EOF
          a
            #{1}
          a
        EOF

--- internal/parse.h
+++ internal/parse.h
@@ -70,6 +70,7 @@ rb_encoding *rb_ruby_parser_encoding(rb_parser_t *p);
 int rb_ruby_parser_end_seen_p(rb_parser_t *p);
 int rb_ruby_parser_set_yydebug(rb_parser_t *p, int flag);
 rb_parser_string_t *rb_str_to_parser_string(rb_parser_t *p, VALUE str);
+void rb_parser_string_free(rb_parser_t *p, rb_parser_string_t *str);
 
 int rb_parser_dvar_defined_ref(struct parser_params*, ID, ID**);
 ID rb_parser_internal_id(struct parser_params*);
