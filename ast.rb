b873787a42e4ec23dab3dbc5dded95c4804472d2
Benoit Daloze <eregontp@gmail.com>
2024-09-27 16:45:36 +0200

    Update recommendations regarding parsing Ruby code now that Prism is stable

--- ast.rb
+++ ast.rb
@@ -13,9 +13,8 @@
 # access children nodes by name, etc.
 #
 # If you are looking for a stable API or an API working under multiple Ruby
-# implementations, consider using the _parser_ gem or Ripper. If you would
-# like to make RubyVM::AbstractSyntaxTree stable, please join the discussion
-# at https://bugs.ruby-lang.org/issues/14844.
+# implementations, consider using the _prism_ gem, which is the official
+# Ruby API to parse Ruby code.
 #
 module RubyVM::AbstractSyntaxTree
 
