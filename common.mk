d07b74360efe958d6e6f71c454abcca1c2b56460
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-10-08 15:36:58 +0900

    `rb_fix_mul_fix` needs internal/bits.h for `MUL_OVERFLOW_FIXNUM_P`

--- common.mk
+++ common.mk
@@ -15743,6 +15743,7 @@ rjit_c.$(OBJEXT): $(hdrdir)/ruby/ruby.h
 rjit_c.$(OBJEXT): $(srcdir)/rjit_c.rb
 rjit_c.$(OBJEXT): $(top_srcdir)/internal/array.h
 rjit_c.$(OBJEXT): $(top_srcdir)/internal/basic_operators.h
+rjit_c.$(OBJEXT): $(top_srcdir)/internal/bits.h
 rjit_c.$(OBJEXT): $(top_srcdir)/internal/class.h
 rjit_c.$(OBJEXT): $(top_srcdir)/internal/compile.h
 rjit_c.$(OBJEXT): $(top_srcdir)/internal/compilers.h
