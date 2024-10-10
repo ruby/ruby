146ff6617326893130e5f29935cdaeae0836cfb5
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-10-10 11:28:27 +0900

    [ruby/digest] Cast via `uintptr_t` function pointer between object pointer
    https://github.com/ruby/digest/commit/661d4643ef

--- ext/digest/digest.h
+++ ext/digest/digest.h
@@ -80,7 +80,8 @@ rb_digest_make_metadata(const rb_digest_metadata_t *meta)
     typedef VALUE (*wrapper_func_type)(const rb_digest_metadata_t *meta);
     static wrapper_func_type wrapper;
     if (!wrapper) {
-        wrapper = (wrapper_func_type)rb_ext_resolve_symbol("digest.so", "rb_digest_wrap_metadata");
+        wrapper = (wrapper_func_type)(uintptr_t)
+            rb_ext_resolve_symbol("digest.so", "rb_digest_wrap_metadata");
         if (!wrapper) rb_raise(rb_eLoadError, "rb_digest_wrap_metadata not found");
     }
     return wrapper(meta);
