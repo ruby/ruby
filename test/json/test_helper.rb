3c1b0f21aa6dd7b7cfde67497c5b50717cdf14d2
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-10-16 12:51:38 +0900

    [ruby/json] Fixed load path for ext version
    https://github.com/ruby/json/commit/c17823688e

--- test/json/test_helper.rb
+++ test/json/test_helper.rb
@@ -1,12 +1,12 @@
 case ENV['JSON']
 when 'pure'
-  $:.unshift File.join(__dir__, '../lib')
+  $:.unshift File.join(__dir__, '../../lib')
   require 'json/pure'
 when 'ext'
-  $:.unshift File.join(__dir__, '../ext'), File.join(__dir__, '../lib')
+  $:.unshift File.join(__dir__, '../../ext'), File.join(__dir__, '../../lib')
   require 'json/ext'
 else
-  $:.unshift File.join(__dir__, '../ext'), File.join(__dir__, '../lib')
+  $:.unshift File.join(__dir__, '../../ext'), File.join(__dir__, '../../lib')
   require 'json'
 end
 
