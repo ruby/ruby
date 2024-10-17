e4330536d2f4a9b96e52a60b9d698625ab26a547
Peter Zhu <peter@peterzhu.ca>
2024-10-17 13:39:48 -0400

    [ruby/json] Fix State#max_nesting=
    Returning state->max_nesting is not valid because it's not a Ruby object.
    
    https://github.com/ruby/json/commit/6679ceb

--- ext/json/generator/generator.c
+++ ext/json/generator/generator.c
@@ -1164,7 +1164,8 @@ static VALUE cState_max_nesting_set(VALUE self, VALUE depth)
 {
     GET_STATE(self);
     Check_Type(depth, T_FIXNUM);
-    return state->max_nesting = FIX2LONG(depth);
+    state->max_nesting = FIX2LONG(depth);
+    return Qnil;
 }
 
 /*
