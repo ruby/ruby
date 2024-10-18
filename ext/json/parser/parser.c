c79ad00c8ab73b7ab78a05f7b103357902b802b7
Jean Boussier <jean.boussier@gmail.com>
2024-10-18 12:30:58 +0200

    [ruby/json] Get rid of the remaining tabs.
    https://github.com/ruby/json/commit/1a9af430d2

--- ext/json/parser/parser.c
+++ ext/json/parser/parser.c
@@ -1834,78 +1834,78 @@ static VALUE cParser_initialize(int argc, VALUE *argv, VALUE self)
     }
 
     if (!NIL_P(opts)) {
-	VALUE tmp = ID2SYM(i_max_nesting);
-	if (option_given_p(opts, tmp)) {
-	    VALUE max_nesting = rb_hash_aref(opts, tmp);
-	    if (RTEST(max_nesting)) {
-		Check_Type(max_nesting, T_FIXNUM);
-		json->max_nesting = FIX2INT(max_nesting);
-	    } else {
-		json->max_nesting = 0;
-	    }
-	} else {
-	    json->max_nesting = 100;
-	}
-	tmp = ID2SYM(i_allow_nan);
-	if (option_given_p(opts, tmp)) {
-	    json->allow_nan = RTEST(rb_hash_aref(opts, tmp)) ? 1 : 0;
-	} else {
-	    json->allow_nan = 0;
-	}
-	tmp = ID2SYM(i_symbolize_names);
-	if (option_given_p(opts, tmp)) {
-	    json->symbolize_names = RTEST(rb_hash_aref(opts, tmp)) ? 1 : 0;
-	} else {
-	    json->symbolize_names = 0;
-	}
-	tmp = ID2SYM(i_freeze);
-	if (option_given_p(opts, tmp)) {
-	    json->freeze = RTEST(rb_hash_aref(opts, tmp)) ? 1 : 0;
-	} else {
-	    json->freeze = 0;
-	}
-	tmp = ID2SYM(i_create_additions);
-	if (option_given_p(opts, tmp)) {
-	    json->create_additions = RTEST(rb_hash_aref(opts, tmp));
-	} else {
-	    json->create_additions = 0;
-	}
-	if (json->symbolize_names && json->create_additions) {
-	    rb_raise(rb_eArgError,
-		     "options :symbolize_names and :create_additions cannot be "
-		     " used in conjunction");
-	}
-	tmp = ID2SYM(i_create_id);
-	if (option_given_p(opts, tmp)) {
-	    json->create_id = rb_hash_aref(opts, tmp);
-	} else {
-	    json->create_id = rb_funcall(mJSON, i_create_id, 0);
-	}
-	tmp = ID2SYM(i_object_class);
-	if (option_given_p(opts, tmp)) {
-	    json->object_class = rb_hash_aref(opts, tmp);
-	} else {
-	    json->object_class = Qnil;
-	}
-	tmp = ID2SYM(i_array_class);
-	if (option_given_p(opts, tmp)) {
-	    json->array_class = rb_hash_aref(opts, tmp);
-	} else {
-	    json->array_class = Qnil;
-	}
-	tmp = ID2SYM(i_decimal_class);
-	if (option_given_p(opts, tmp)) {
-	    json->decimal_class = rb_hash_aref(opts, tmp);
-	} else {
-	    json->decimal_class = Qnil;
-	}
-	tmp = ID2SYM(i_match_string);
-	if (option_given_p(opts, tmp)) {
-	    VALUE match_string = rb_hash_aref(opts, tmp);
-	    json->match_string = RTEST(match_string) ? match_string : Qnil;
-	} else {
-	    json->match_string = Qnil;
-	}
+    VALUE tmp = ID2SYM(i_max_nesting);
+    if (option_given_p(opts, tmp)) {
+        VALUE max_nesting = rb_hash_aref(opts, tmp);
+        if (RTEST(max_nesting)) {
+            Check_Type(max_nesting, T_FIXNUM);
+            json->max_nesting = FIX2INT(max_nesting);
+        } else {
+            json->max_nesting = 0;
+        }
+    } else {
+        json->max_nesting = 100;
+    }
+    tmp = ID2SYM(i_allow_nan);
+    if (option_given_p(opts, tmp)) {
+        json->allow_nan = RTEST(rb_hash_aref(opts, tmp)) ? 1 : 0;
+    } else {
+        json->allow_nan = 0;
+    }
+    tmp = ID2SYM(i_symbolize_names);
+    if (option_given_p(opts, tmp)) {
+        json->symbolize_names = RTEST(rb_hash_aref(opts, tmp)) ? 1 : 0;
+    } else {
+        json->symbolize_names = 0;
+    }
+    tmp = ID2SYM(i_freeze);
+    if (option_given_p(opts, tmp)) {
+        json->freeze = RTEST(rb_hash_aref(opts, tmp)) ? 1 : 0;
+    } else {
+        json->freeze = 0;
+    }
+    tmp = ID2SYM(i_create_additions);
+    if (option_given_p(opts, tmp)) {
+        json->create_additions = RTEST(rb_hash_aref(opts, tmp));
+    } else {
+        json->create_additions = 0;
+    }
+    if (json->symbolize_names && json->create_additions) {
+        rb_raise(rb_eArgError,
+            "options :symbolize_names and :create_additions cannot be "
+            " used in conjunction");
+    }
+    tmp = ID2SYM(i_create_id);
+    if (option_given_p(opts, tmp)) {
+        json->create_id = rb_hash_aref(opts, tmp);
+    } else {
+        json->create_id = rb_funcall(mJSON, i_create_id, 0);
+    }
+    tmp = ID2SYM(i_object_class);
+    if (option_given_p(opts, tmp)) {
+        json->object_class = rb_hash_aref(opts, tmp);
+    } else {
+        json->object_class = Qnil;
+    }
+    tmp = ID2SYM(i_array_class);
+    if (option_given_p(opts, tmp)) {
+        json->array_class = rb_hash_aref(opts, tmp);
+    } else {
+        json->array_class = Qnil;
+    }
+    tmp = ID2SYM(i_decimal_class);
+    if (option_given_p(opts, tmp)) {
+        json->decimal_class = rb_hash_aref(opts, tmp);
+    } else {
+        json->decimal_class = Qnil;
+    }
+    tmp = ID2SYM(i_match_string);
+    if (option_given_p(opts, tmp)) {
+        VALUE match_string = rb_hash_aref(opts, tmp);
+        json->match_string = RTEST(match_string) ? match_string : Qnil;
+    } else {
+        json->match_string = Qnil;
+    }
     } else {
         json->max_nesting = 100;
         json->allow_nan = 0;
