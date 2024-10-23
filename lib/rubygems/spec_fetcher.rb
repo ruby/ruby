5549dd2c12c0285acd487626756fbf1cb3e5d236
Ellen Marie Dash <me@duckie.co>
2024-10-23 18:32:17 -0400

    [rubygems/rubygems] [SpecFetcher] Change < to <= like it should be.
    https://github.com/rubygems/rubygems/commit/3d5135e69b

--- lib/rubygems/spec_fetcher.rb
+++ lib/rubygems/spec_fetcher.rb
@@ -172,9 +172,9 @@ class Gem::SpecFetcher
   def suggest_gems_from_name(gem_name, type = :latest, num_results = 5)
     gem_name = gem_name.downcase.tr("_-", "")
 
-    # All results for 3-character (minus hyphens/underscores) gem names
-    # get rejected, so we just return an empty array immediately instead.
-    return [] if gem_name.length < 3
+    # All results for 3-character-or-shorter (minus hyphens/underscores) gem
+    # names get rejected, so we just return an empty array immediately instead.
+    return [] if gem_name.length <= 3
 
     max   = gem_name.size / 2
     names = available_specs(type).first.values.flatten(1)
