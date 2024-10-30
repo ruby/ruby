a1df2d08ee7cfee311ff38b19881523029e0fa22
Jean Boussier <jean.boussier@gmail.com>
2024-10-30 11:21:59 +0100

    [ruby/json] Setup ruby_memcheck
    Hoping it might find the leak reported in https://github.com/ruby/json/issues/460
    
    https://github.com/ruby/json/commit/08635312e5

--- test/json/test_helper.rb
+++ test/json/test_helper.rb
@@ -1,12 +1,15 @@
 case ENV['JSON']
 when 'pure'
   $LOAD_PATH.unshift(File.expand_path('../../../lib', __FILE__))
+  $stderr.puts("Testing JSON::Pure")
   require 'json/pure'
 when 'ext'
+  $stderr.puts("Testing JSON::Ext")
   $LOAD_PATH.unshift(File.expand_path('../../../ext', __FILE__), File.expand_path('../../../lib', __FILE__))
   require 'json/ext'
 else
   $LOAD_PATH.unshift(File.expand_path('../../../ext', __FILE__), File.expand_path('../../../lib', __FILE__))
+  $stderr.puts("Testing JSON")
   require 'json'
 end
 
