6bafd5b597020184e7f5fc689faa3b45379f0bcf
Takashi Kokubun <takashikkbn@gmail.com>
2024-10-31 23:14:31 -0700

    Refactor a check of BUILTIN_ATTR_C_TRACE

--- vm_backtrace.c
+++ vm_backtrace.c
@@ -701,7 +701,7 @@ rb_ec_partial_backtrace_object(const rb_execution_context_t *ec, long start_fram
                         loc = &bt->backtrace[bt->backtrace_size++];
                         RB_OBJ_WRITE(btobj, &loc->cme, rb_vm_frame_method_entry(cfp));
                         // Ruby methods with `Primitive.attr! :c_trace` should behave like C methods
-                        if ((cfp->iseq->body->builtin_attrs & BUILTIN_ATTR_C_TRACE) != 0) {
+                        if (rb_iseq_attr_p(cfp->iseq, BUILTIN_ATTR_C_TRACE)) {
                             loc->iseq = NULL;
                             loc->pc = NULL;
                             cfunc_counter++;
