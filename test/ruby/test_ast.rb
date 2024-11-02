0b5e18f647f0535db01e8b0384e8b3cf87c2663a
ydah <t.yudai92@gmail.com>
2024-11-03 00:26:25 +0900

    Implement DOT2 NODE locations

--- test/ruby/test_ast.rb
+++ test/ruby/test_ast.rb
@@ -1376,6 +1376,20 @@ dummy
       assert_locations(node.children[-1].locations, [[1, 0, 1, 17], [1, 0, 1, 4], [1, 14, 1, 17]])
     end
 
+    def test_dot2_locations
+      node = ast_parse("1..2")
+      assert_locations(node.children[-1].locations, [[1, 0, 1, 4], [1, 1, 1, 3]])
+
+      node = ast_parse("foo(1..2)")
+      assert_locations(node.children[-1].children[-1].children[0].locations, [[1, 4, 1, 8], [1, 5, 1, 7]])
+
+      node = ast_parse("foo(1..2, 3)")
+      assert_locations(node.children[-1].children[-1].children[0].locations, [[1, 4, 1, 8], [1, 5, 1, 7]])
+
+      node = ast_parse("foo(..2)")
+      assert_locations(node.children[-1].children[-1].children[0].locations, [[1, 4, 1, 7], [1, 4, 1, 6]])
+    end
+
     def test_next_locations
       node = ast_parse("loop { next 1 }")
       assert_locations(node.children[-1].children[-1].children[-1].locations, [[1, 7, 1, 13], [1, 7, 1, 11]])
