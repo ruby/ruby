fa2517451ec265d5b273e864bc750a1b9ba2957f
ydah <t.yudai92@gmail.com>
2024-11-02 17:21:51 +0900

    Implement LAMBDA NODE locations
    The following Location information has been added This is the information required for parse.y to be a universal parser:
    
    ```
    ❯ ruby --parser=prism --dump=parsetree -e "-> (a, b) do foo end"
    @ ProgramNode (location: (1,0)-(1,20))
    +-- locals: []
    +-- statements:
        @ StatementsNode (location: (1,0)-(1,20))
        +-- body: (length: 1)
            +-- @ LambdaNode (location: (1,0)-(1,20))
                +-- locals: [:a, :b]
                +-- operator_loc: (1,0)-(1,2) = "->"
                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
                +-- opening_loc: (1,10)-(1,12) = "do"
                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
                +-- closing_loc: (1,17)-(1,20) = "end"
                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    : (snip)
    ```

--- ast.c
+++ ast.c
@@ -812,6 +812,12 @@ node_locations(VALUE ast_value, const NODE *node)
                                     location_new(nd_code_loc(node)),
                                     location_new(&RNODE_EVSTR(node)->opening_loc),
                                     location_new(&RNODE_EVSTR(node)->closing_loc));
+      case NODE_LAMBDA:
+        return rb_ary_new_from_args(4,
+                                    location_new(nd_code_loc(node)),
+                                    location_new(&RNODE_LAMBDA(node)->operator_loc),
+                                    location_new(&RNODE_LAMBDA(node)->opening_loc),
+                                    location_new(&RNODE_LAMBDA(node)->closing_loc));
       case NODE_IF:
         return rb_ary_new_from_args(4,
                                     location_new(nd_code_loc(node)),
