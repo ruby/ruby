9c1746f38cc3427ebecca856e7237ded1ad134ab
ydah <t.yudai92@gmail.com>
2024-11-04 23:32:00 +0900

    Implement EVSTR NODE locations
    The following Location information has been added This is the information required for parse.y to be a universal parser:
    
    ```
    ❯ ruby --parser=prism --dump=parsetree -e '"#{foo}"'
    @ ProgramNode (location: (1,0)-(1,8))
    +-- locals: []
    +-- statements:
        @ StatementsNode (location: (1,0)-(1,8))
        +-- body: (length: 1)
            +-- @ InterpolatedStringNode (location: (1,0)-(1,8))
                +-- InterpolatedStringNodeFlags: nil
                +-- opening_loc: (1,0)-(1,1) = "\""
                +-- parts: (length: 1)
                |   +-- @ EmbeddedStatementsNode (location: (1,1)-(1,7))
                |       +-- opening_loc: (1,1)-(1,3) = "\#{"
                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
                |       +-- statements:
                |       |   @ StatementsNode (location: (1,3)-(1,6))
                |       |   +-- body: (length: 1)
                |       |       +-- @ CallNode (location: (1,3)-(1,6))
                |       |           +-- CallNodeFlags: variable_call, ignore_visibility
                |       |           +-- receiver: nil
                |       |           +-- call_operator_loc: nil
                |       |           +-- name: :foo
                |       |           +-- message_loc: (1,3)-(1,6) = "foo"
                |       |           +-- opening_loc: nil
                |       |           +-- arguments: nil
                |       |           +-- closing_loc: nil
                |       |           +-- block: nil
                |       +-- closing_loc: (1,6)-(1,7) = "}"
                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
                +-- closing_loc: (1,7)-(1,8) = "\""
    ```

--- test/ruby/test_ast.rb
+++ test/ruby/test_ast.rb
@@ -1376,6 +1376,14 @@ dummy
       assert_locations(node.children[-1].locations, [[1, 0, 1, 17], [1, 0, 1, 4], [1, 14, 1, 17]])
     end
 
+    def test_evstr_locations
+      node = ast_parse('"#{foo}"')
+      assert_locations(node.children[-1].children[1].locations, [[1, 0, 1, 8], [1, 1, 1, 3], [1, 6, 1, 7]])
+
+      node = ast_parse('"#$1"')
+      assert_locations(node.children[-1].children[1].locations, [[1, 0, 1, 5], [1, 1, 1, 2], nil])
+    end
+
     def test_next_locations
       node = ast_parse("loop { next 1 }")
       assert_locations(node.children[-1].children[-1].children[-1].locations, [[1, 7, 1, 13], [1, 7, 1, 11]])
