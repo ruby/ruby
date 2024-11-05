847ae79f52e02b9690c4e5a2b6f1a1807769e127
Jean Boussier <jean.boussier@gmail.com>
2024-11-05 17:30:42 +0100

    [ruby/json] Appease ruby/ruby CI
    https://github.com/ruby/json/commit/bc2c970ce4

--- test/json/test_helper.rb
+++ test/json/test_helper.rb
@@ -3,21 +3,23 @@ $LOAD_PATH.unshift(File.expand_path('../../../ext', __FILE__), File.expand_path(
 require 'json'
 require 'test/unit'
 
-if GC.respond_to?(:verify_compaction_references)
-  # This method was added in Ruby 3.0.0. Calling it this way asks the GC to
-  # move objects around, helping to find object movement bugs.
-  begin
-    GC.verify_compaction_references(expand_heap: true, toward: :empty)
-  rescue NotImplementedError, ArgumentError
-    # Some platforms don't support compaction
+if ENV["JSON_COMPACT"]
+  if GC.respond_to?(:verify_compaction_references)
+    # This method was added in Ruby 3.0.0. Calling it this way asks the GC to
+    # move objects around, helping to find object movement bugs.
+    begin
+      GC.verify_compaction_references(expand_heap: true, toward: :empty)
+    rescue NotImplementedError, ArgumentError
+      # Some platforms don't support compaction
+    end
   end
-end
 
-if GC.respond_to?(:auto_compact=)
-  begin
-    GC.auto_compact = true
-  rescue NotImplementedError
-    # Some platforms don't support compaction
+  if GC.respond_to?(:auto_compact=)
+    begin
+      GC.auto_compact = true
+    rescue NotImplementedError
+      # Some platforms don't support compaction
+    end
   end
 end
 
