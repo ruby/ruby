74077a99bfdb62c66cbabd9a111c24fe9c2fa66c
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-11-08 10:34:54 +0900

    [ruby/ostruct] Bump up v0.6.1
    https://github.com/ruby/ostruct/commit/b38680089f

--- lib/ostruct.rb
+++ lib/ostruct.rb
@@ -107,7 +107,7 @@
 # For all these reasons, consider not using OpenStruct at all.
 #
 class OpenStruct
-  VERSION = "0.6.0"
+  VERSION = "0.6.1"
 
   HAS_PERFORMANCE_WARNINGS = begin
     Warning[:performance]
