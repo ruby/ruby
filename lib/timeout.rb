f6b62d001a7ee868a745054d2e6146167570e682
Cosmic Oppai <cosmicoppai@protonmail.com>
2024-11-19 01:56:21 +0530

    [ruby/timeout] removed the non numeric check
    https://github.com/ruby/timeout/commit/7d2af46a00
    
    Co-authored-by: Jeremy Evans <code@jeremyevans.net>

--- lib/timeout.rb
+++ lib/timeout.rb
@@ -165,8 +165,8 @@ module Timeout
   # Timeout</tt> into your classes so they have a #timeout method, as well as
   # a module method, so you can call it directly as Timeout.timeout().
   def timeout(sec, klass = nil, message = nil, &block)   #:yield: +sec+
-    raise ArgumentError, "Timeout sec must be a non-negative number" if sec && !(sec.is_a?(Numeric) && sec >= 0)
     return yield(sec) if sec == nil or sec.zero?
+    raise ArgumentError, "Timeout sec must be a non-negative number" if 0 > sec
 
     message ||= "execution expired"
 
