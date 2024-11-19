00c90fdac1bc51644507bc4cb35f4fd851599677
David Rodríguez <deivid.rodriguez@riseup.net>
2024-11-19 16:25:16 +0100

    [rubygems/rubygems] Filter out gems to unlock inside `converge_specs`
    https://github.com/rubygems/rubygems/commit/d0f789970f

--- lib/bundler/definition.rb
+++ lib/bundler/definition.rb
@@ -957,7 +957,7 @@ module Bundler
     def converge_locked_specs
       converged = converge_specs(@locked_specs)
 
-      resolve = SpecSet.new(converged.reject {|s| @gems_to_unlock.include?(s.name) })
+      resolve = SpecSet.new(converged)
 
       diff = nil
 
@@ -1017,7 +1017,7 @@ module Bundler
         end
       end
 
-      filter_specs(converged, deps)
+      filter_specs(converged, deps).reject {|s| @gems_to_unlock.include?(s.name) }
     end
 
     def metadata_dependencies
