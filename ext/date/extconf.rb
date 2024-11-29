b910de641bed7cbcac2760261d747c17db0ebbab
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-11-29 11:08:44 +0900

    [ruby/date] Suppress compound-token-split-by-macro warnings
    It was used intentionally.
    
    https://github.com/ruby/date/commit/291b40f939

--- ext/date/extconf.rb
+++ ext/date/extconf.rb
@@ -3,6 +3,7 @@ require 'mkmf'
 
 config_string("strict_warnflags") {|w| $warnflags += " #{w}"}
 
+append_cflags("-Wno-compound-token-split-by-macro") if RUBY_VERSION < "2.7."
 have_func("rb_category_warn")
 with_werror("", {:werror => true}) do |opt, |
   have_var("timezone", "time.h", opt)
