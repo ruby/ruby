d588a1c880c5aadb3a802358df73b0c6e1fcf1a1
BurdetteLamar <burdettelamar@yahoo.com>
2024-12-01 16:56:47 -0600

    Correct order of listed methods

--- numeric.c
+++ numeric.c
@@ -3592,6 +3592,7 @@ rb_num2ull(VALUE val)
  * - #>>: Returns the value of +self+ after a rightward bit-shift.
  * - #[]: Returns a slice of bits from +self+.
  * - #^: Returns the bitwise EXCLUSIVE OR of +self+ and the given value.
+ * - #|: Returns the bitwise OR of +self+ and the given value.
  * - #ceil: Returns the smallest number greater than or equal to +self+.
  * - #chr: Returns a 1-character string containing the character
  *   represented by the value of +self+.
@@ -3611,7 +3612,6 @@ rb_num2ull(VALUE val)
  * - #to_s (aliased as #inspect): Returns a string containing the place-value
  *   representation of +self+ in the given radix.
  * - #truncate: Returns +self+ truncated to the given precision.
- * - #|: Returns the bitwise OR of +self+ and the given value.
  *
  * === Other
  *
