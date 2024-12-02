ff52431ee5a9bdefa8240e69ea4220591babad4b
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-12-02 16:44:51 +0900

    [ruby/securerandom] Bump up 0.4.0
    https://github.com/ruby/securerandom/commit/53f97f3151

--- lib/securerandom.rb
+++ lib/securerandom.rb
@@ -41,7 +41,7 @@ require 'random/formatter'
 module SecureRandom
 
   # The version
-  VERSION = "0.3.2"
+  VERSION = "0.4.0"
 
   class << self
     # Returns a random binary string containing +size+ bytes.
