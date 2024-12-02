d5c2fb464595af7b0515d85977dbb24e840192ea
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-12-02 10:20:53 +0900

    Removed trailing spaces

--- ext/strscan/lib/strscan/strscan.rb
+++ ext/strscan/lib/strscan/strscan.rb
@@ -3,13 +3,13 @@
 class StringScanner
   # call-seq:
   #   scan_integer(base: 10)
-  # 
+  #
   # If `base` isn't provided or is `10`, then it is equivalent to calling `#scan` with a `[+-]?\d+` pattern,
   # and returns an Integer or nil.
   #
   # If `base` is `16`, then it is equivalent to calling `#scan` with a `[+-]?(0x)?[0-9a-fA-F]+` pattern,
   # and returns an Integer or nil.
-  # 
+  #
   # The scanned string must be encoded with an ASCII compatible encoding, otherwise
   # Encoding::CompatibilityError will be raised.
   def scan_integer(base: 10)
