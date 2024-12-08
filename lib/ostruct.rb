5d49714b45c8e1145e4f43c5aa162fd6f06bed46
Stan Lo <stan.lo@shopify.com>
2024-12-08 12:07:32 +0000

    Reorganize top-level doc files for better doc navigation

--- lib/ostruct.rb
+++ lib/ostruct.rb
@@ -329,7 +329,7 @@ class OpenStruct
   # Finds and returns the object in nested objects
   # that is specified by +name+ and +identifiers+.
   # The nested objects may be instances of various classes.
-  # See {Dig Methods}[rdoc-ref:dig_methods.rdoc].
+  # See {Dig Methods}[rdoc-ref:ruby/dig_methods.rdoc].
   #
   # Examples:
   #   require "ostruct"
