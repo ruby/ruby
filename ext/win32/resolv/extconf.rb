af9a904f387d2b6b7ab2ade446b408ecd364aa53
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-12-10 15:50:55 +0900

    [ruby/resolv] Build the extension library only when CRuby (Fix https://github.com/ruby/resolv/pull/69)
    On JRuby no C compiler is available and this call results in a runtime
    error.
    
    https://github.com/ruby/resolv/commit/33105bc504

--- ext/win32/resolv/extconf.rb
+++ ext/win32/resolv/extconf.rb
@@ -1,5 +1,5 @@
 require 'mkmf'
-if have_library('iphlpapi', 'GetNetworkParams')
+if RUBY_ENGINE == "ruby" and have_library('iphlpapi', 'GetNetworkParams')
   create_makefile('win32/resolv')
 else
   File.write('Makefile', "all clean install:\n\t@echo Done: $(@)\n")
