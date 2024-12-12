faac664b3768cdb3932118b7863ab556d57d67dd
Kouhei Yanagita <yanagi@shakenbu.org>
2024-12-13 08:19:26 +0900

    [DOC] Fix markup and refine pharasing
    Co-authored-by: Stan Lo <stan001212@gmail.com>

--- object.c
+++ object.c
@@ -604,9 +604,9 @@ rb_obj_dup_setup(VALUE obj, VALUE dup)
  *  When using #dup, any modules that the object has been extended with will not
  *  be copied.
  *
- *  If you need #clone and #dup to behave differently, you can use
- *  #+initialize_clone+ and #+initialize_dup+.
- *  By default, both methods call #+initialize_copy+.
+ *  If you need #clone and #dup to behave differently, you can override
+ *  +#initialize_clone+ and +#initialize_dup+ respectively.
+ *  By default, both methods invoke +#initialize_copy+.
  *
  *	class Klass
  *	  attr_accessor :str
