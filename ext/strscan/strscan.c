78ca87f8a8c79f0af1b7c6a0d819faacd75ec76e
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-12-12 16:14:25 +0900

    Lock released version of strscan-3.1.1

--- ext/strscan/strscan.c
+++ ext/strscan/strscan.c
@@ -22,7 +22,7 @@ extern size_t onig_region_memsize(const struct re_registers *regs);
 
 #include <stdbool.h>
 
-#define STRSCAN_VERSION "3.1.1.dev"
+#define STRSCAN_VERSION "3.1.1"
 
 /* =======================================================================
                          Data Type Definitions
