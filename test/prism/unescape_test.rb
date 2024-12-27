4cf4cad4abca43c9396ec8f736f0021b3c678f4d
Kevin Newton <kddnewton@gmail.com>
2024-12-26 20:26:47 -0500

    [ruby/prism] Turn off unescape tests for Ruby >= 3.4.0
    https://github.com/ruby/prism/commit/f982769314

--- test/prism/unescape_test.rb
+++ test/prism/unescape_test.rb
@@ -2,7 +2,9 @@
 
 require_relative "test_helper"
 
-return if RUBY_VERSION < "3.1.0" || Prism::BACKEND == :FFI
+return if Prism::BACKEND == :FFI
+return if RUBY_VERSION < "3.1.0"
+return if RUBY_VERSION >= "3.4.0"
 
 module Prism
   class UnescapeTest < TestCase
