2273d5b20de1b5be44f35f4052115babbe13055d
Burdette Lamar <BurdetteLamar@Yahoo.com>
2025-01-04 21:18:39 -0600

    Update hash.rb
    Co-authored-by: Jeremy Evans <code@jeremyevans.net>

--- hash.rb
+++ hash.rb
@@ -7,8 +7,11 @@ class Hash
   # Initializes the values of Hash#default and Hash#default_proc,
   # which determine the value to be returned by method Hash#[] when the entry does not exist.
   #
-  # With no block given, initializes Hash#default to the given +default_value+,
-  # and Hash#default_proc to +nil+:
+  # By default, a hash has +nil+ values for both +default+ and +default_proc+:
+  #
+  #   h = Hash.new        # => {}
+  #   h.default           # => nil
+  #   h.default_proc      # => nil
   #
   #   h = Hash.new        # => {}
   #   h.default           # => nil
