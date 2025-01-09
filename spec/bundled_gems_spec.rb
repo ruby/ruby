fcecef7752d9d6c760c8ca7f6a7ce12c82988360
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-01-09 14:25:48 +0900

    Added logger dependency for Bundler's example

--- spec/bundled_gems_spec.rb
+++ spec/bundled_gems_spec.rb
@@ -29,7 +29,7 @@ RSpec.configure do |config|
   config.around(:each) do |example|
     FileUtils.cp_r Spec::Path.pristine_system_gem_path, Spec::Path.system_gem_path
     FileUtils.mkdir_p Spec::Path.base_system_gem_path.join("gems")
-    %w[sinatra rack tilt rack-protection rack-session rack-test mustermann base64 compact_index].each do |gem|
+    %w[sinatra rack tilt rack-protection rack-session rack-test mustermann base64 logger compact_index].each do |gem|
       path = Dir[File.expand_path("../.bundle/gems/#{gem}-*", __dir__)].map(&:to_s).first
       FileUtils.cp_r path, Spec::Path.base_system_gem_path.join("gems")
     end
