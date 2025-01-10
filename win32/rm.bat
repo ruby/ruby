3845488220e185d0c6e53e697c885d80759bf492
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-01-10 11:55:09 +0900

    Win32: Fix rm.bat removing non existent file

--- win32/rm.bat
+++ win32/rm.bat
@@ -8,11 +8,12 @@ if "%1" == "--debug" (shift & set PROMPT=$E[34m+$E[m$S & echo on & goto :optloop
 :begin
 if "%1" == "" goto :end
 set p=%1
+shift
 set p=%p:/=\%
-if exist "%p%" del /q "%p%" > nul
+if not exist "%p%" goto :begin
+del /q "%p%" > nul && goto :begin
 if "%recursive%" == "1" for /D %%I in (%p%) do (
     rd /s /q %%I
 )
-shift
 goto :begin
 :end
