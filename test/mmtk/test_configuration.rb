e2f0769ca2993a08a849dbd8ede099a3cc327aef
Peter Zhu <peter@peterzhu.ca>
2025-01-14 11:35:53 -0500

    [ruby/mmtk] Also test invalid values for MMTK_HEAP_MIN and MMTK_HEAP_MAX
    https://github.com/ruby/mmtk/commit/90f1aa8d76

--- test/mmtk/test_configuration.rb
+++ test/mmtk/test_configuration.rb
@@ -2,15 +2,17 @@
 require_relative "helper"
 module MMTk
   class TestConfiguration < TestCase
-    def test_invalid_MMTK_THREADS
-      exit_code = assert_in_out_err(
-        [{ "MMTK_THREADS" => "foobar" }, "--"],
-        "",
-        [],
-        ["[FATAL] Invalid MMTK_THREADS foobar"]
-      )
+    %w(MMTK_THREADS MMTK_HEAP_MIN MMTK_HEAP_MAX).each do |var|
+      define_method(:"test_invalid_#{var}") do
+        exit_code = assert_in_out_err(
+          [{ var => "foobar" }, "--"],
+          "",
+          [],
+          ["[FATAL] Invalid #{var} foobar"]
+        )
 
-      assert_equal(1, exit_code.exitstatus)
+        assert_equal(1, exit_code.exitstatus)
+      end
     end
   end
 end
