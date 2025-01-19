94e12ffa39e46f5b1f80485e9b2e06a59cd69dac
Earlopain <14981592+Earlopain@users.noreply.github.com>
2025-01-19 15:54:07 +0100

    [ruby/prism] Fix parser translator multiline interpolated symbols
    In https://github.com/ruby/prism/commit/26370079291a420c6b2b7be5cdbd5c609da62f21 I added tests but didn't modify them correctly
    
    https://github.com/ruby/prism/commit/de021e74de

--- lib/prism/translation/parser/compiler.rb
+++ lib/prism/translation/parser/compiler.rb
@@ -1168,7 +1168,7 @@ module Prism
         def visit_interpolated_symbol_node(node)
           builder.symbol_compose(
             token(node.opening_loc),
-            visit_all(node.parts),
+            string_nodes_from_interpolation(node, node.opening),
             token(node.closing_loc)
           )
         end
