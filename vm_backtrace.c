ac28ea71c051e4971941353f6558d27427682bad
Daisuke Aritomo <osyoyu@osyoyu.com>
2025-01-24 00:31:17 +0900

    Declare rb_profile_frames() family as async-signal-safe
    Since 64fef3b870a8ed8147654531aef4c065d8a730c6, it should be safe to
    call rb_profile_frames() and rb_profile_thread_frames() in a signal
    handler.

--- vm_backtrace.c
+++ vm_backtrace.c
@@ -1772,6 +1772,7 @@ thread_profile_frames(rb_execution_context_t *ec, int start, int limit, VALUE *b
     return i;
 }
 
+/* async-signal-safe */
 int
 rb_profile_frames(int start, int limit, VALUE *buff, int *lines)
 {
@@ -1786,6 +1787,7 @@ rb_profile_frames(int start, int limit, VALUE *buff, int *lines)
     return thread_profile_frames(ec, start, limit, buff, lines);
 }
 
+/* async-signal-safe */
 int
 rb_profile_thread_frames(VALUE thread, int start, int limit, VALUE *buff, int *lines)
 {
