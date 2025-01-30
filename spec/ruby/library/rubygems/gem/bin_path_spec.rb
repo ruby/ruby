895aa44b81013513e1e504a683ee6eaa6f03bb46
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-01-30 12:52:51 +0900

    bin_path_spec.rb relied to available Ruby environment with after `make install`
    But we stub-out GEM_HOME variable for test-bundled-gems and others on ruby/ruby. It means
    the installation path mismatched with GEM_HOME variable always. We can't test this example
    collectly.
    
    ```
    1)
    Gem.bin_path finds executables of default gems, which are the only files shipped for default gems FAILED
    Expected File.exist? "/Users/hsbt/Documents/github.com/ruby/ruby/.bundle/gems/bundler-2.7.0.dev/exe/bundle"
    to be truthy but was false
    ```

--- spec/ruby/library/rubygems/gem/bin_path_spec.rb
+++ spec/ruby/library/rubygems/gem/bin_path_spec.rb
@@ -22,6 +22,7 @@ describe "Gem.bin_path" do
       end
 
       skip "Could not find the default gemspecs" unless Dir.exist?(default_specifications_dir)
+      skip "default_specifications_dir mismatch with GEM_HOME" unless default_specifications_dir.include?(ENV['GEM_HOME'])
 
       Gem::Specification.each_spec([default_specifications_dir]) do |spec|
         spec.executables.each do |exe|
