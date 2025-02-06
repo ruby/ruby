2a3dc9f32fd841a42b7c41ef15f5fd39dad2e288
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2025-02-06 10:28:28 -0500

    Add stats.rs placeholder

---
+++ zjit/src/stats.rs
@@ -0,0 +1,4 @@
+
+// Maxime would like to rebuild an improved stats system
+// Individual stats should be tagged as always available, or only available in stats mode
+// We could also tag which stats are fallback or exit counters, etc. Maybe even tag units?
