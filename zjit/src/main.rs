dca2b1ddaf85e8751b76f74b044d0de279541c51
Max Bernstein <max.bernstein@shopify.com>
2025-02-06 10:27:58 -0500

    Sketch HIR

--- zjit/src/main.rs
+++ zjit/src/main.rs
@@ -1,3 +1,24 @@
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+pub struct InsnId(usize);
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+pub struct BlockId(usize);
+
+enum Insn {
+    Param { idx: usize },
+}
+
+#[derive(Debug)]
+struct Block {
+    params: Vec<InsnId>,
+    insns: Vec<InsnId>,
+}
+
+#[derive(Debug)]
+struct Function {
+    name: String,
+    entry_block: BlockId,
+}
+
 fn main() {
     println!("zjit");
 }
