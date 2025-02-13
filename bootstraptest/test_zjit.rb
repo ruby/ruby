22eec6592805be64603aa624b014f8d87746d462
Takashi Kokubun <takashikkbn@gmail.com>
2025-02-13 10:23:51 -0800

    Recommend the same task as what CI uses

--- bootstraptest/test_zjit.rb
+++ bootstraptest/test_zjit.rb
@@ -1,5 +1,5 @@
 # Tests of Ruby methods that ZJIT can currently compile.
-# make btest-bruby BTESTS=bootstraptest/test_zjit.rb RUN_OPTS="--zjit"
+# make btest BTESTS=bootstraptest/test_zjit.rb RUN_OPTS="--zjit"
 
 assert_equal 'nil', %q{
   def test = nil
