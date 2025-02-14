4bafaef0a55fdc45a7b8275a82ceb0f3e3419d00
Takashi Kokubun <takashikkbn@gmail.com>
2025-02-13 17:47:24 -0800

    merge revision(s) 7df5d65eac86940619f87da7e70bc0911097ae2f: [Backport #20981]
    	[Bug #20981] Bring back `rb_undefine_finalizer`

--- gc.c
+++ gc.c
@@ -1619,6 +1619,12 @@ os_each_obj(int argc, VALUE *argv, VALUE os)
 static VALUE
 undefine_final(VALUE os, VALUE obj)
 {
+    return rb_undefine_finalizer(obj);
+}
+
+VALUE
+rb_undefine_finalizer(VALUE obj)
+{
     rb_check_frozen(obj);
 
     rb_gc_impl_undefine_finalizer(rb_gc_get_objspace(), obj);
