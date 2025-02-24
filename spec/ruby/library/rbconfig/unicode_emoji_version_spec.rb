e63c516046b6dbf2f684454b68013b4eea12e94a
Mari Imaizumi <mariimaizumi5@gmail.com>
2025-02-24 12:01:14 +0900

    [Feature #19908] Update Unicode headers to 15.1.0

--- spec/ruby/library/rbconfig/unicode_emoji_version_spec.rb
+++ spec/ruby/library/rbconfig/unicode_emoji_version_spec.rb
@@ -14,10 +14,16 @@ describe "RbConfig::CONFIG['UNICODE_EMOJI_VERSION']" do
     end
   end
 
-  # Caution: ruby_version_is means is_or_later
-  ruby_version_is "3.2" do
+  ruby_version_is "3.2"..."3.4" do
     it "is 15.0" do
       RbConfig::CONFIG['UNICODE_EMOJI_VERSION'].should == "15.0"
+    end
+  end
+
+  # Caution: ruby_version_is means is_or_later
+  ruby_version_is "3.5" do
+    it "is 15.1" do
+      RbConfig::CONFIG['UNICODE_EMOJI_VERSION'].should == "15.1"
     end
   end
 end
