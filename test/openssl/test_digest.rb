c0f3dcf7958b07e7d2cf008770594bd9c656fb51
Kazuki Yamaguchi <k@rhe.jp>
2025-02-26 23:25:35 +0900

    [ruby/openssl] digest: correct assert_equal usage in test_sha3
    assert_equal accepts an expected value as the first argument.
    
    https://github.com/ruby/openssl/commit/8faa1d89de

--- test/openssl/test_digest.rb
+++ test/openssl/test_digest.rb
@@ -103,10 +103,10 @@ class OpenSSL::TestDigest < OpenSSL::TestCase
     s256 = 'a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a'
     s384 = '0c63a75b845e4f7d01107d852e4c2485c51a50aaaa94fc61995e71bbee983a2ac3713831264adb47fb6bd1e058d5f004'
     s512 = 'a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26'
-    assert_equal(OpenSSL::Digest.hexdigest('SHA3-224', ""), s224)
-    assert_equal(OpenSSL::Digest.hexdigest('SHA3-256', ""), s256)
-    assert_equal(OpenSSL::Digest.hexdigest('SHA3-384', ""), s384)
-    assert_equal(OpenSSL::Digest.hexdigest('SHA3-512', ""), s512)
+    assert_equal(s224, OpenSSL::Digest.hexdigest('SHA3-224', ""))
+    assert_equal(s256, OpenSSL::Digest.hexdigest('SHA3-256', ""))
+    assert_equal(s384, OpenSSL::Digest.hexdigest('SHA3-384', ""))
+    assert_equal(s512, OpenSSL::Digest.hexdigest('SHA3-512', ""))
   end
 
   def test_digest_by_oid_and_name_sha2
