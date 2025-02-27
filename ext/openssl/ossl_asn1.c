f179885d3c454c6a98c23b2a977480657bb0f676
Kazuki Yamaguchi <k@rhe.jp>
2025-02-28 03:10:35 +0900

    [ruby/openssl] asn1: use ASN1_TIME_to_tm() to decode UTCTime and GeneralizedTime
    The current logic relies on sscanf() and error checks are almost
    entirely missing. It also assumes that ASN1_STRING contents are NUL
    terminated, which is undocumented and not guaranteed for all valid
    ASN1_TIME objects.
    
    Switch to using ASN1_TIME_to_tm() added in OpenSSL 1.1.1. It is also
    supported by LibreSSL and AWS-LC.
    
    In the long term, we may want to replace ASN1_TIME_to_tm() with a
    hand-rolled decoder, since the function is intended for a specific
    use-case. It is too permissive for strict DER, yet still does not
    support all valid DER inputs and silently drops information such as
    fractional seconds. However, it handles everything that the current
    sscanf() code could handle.
    
    https://github.com/ruby/openssl/commit/73484f6794

--- ext/openssl/ossl_asn1.c
+++ ext/openssl/ossl_asn1.c
@@ -55,57 +55,35 @@ static ID id_each;
 /*
  * DATE conversion
  */
+static VALUE
+time_utc_new(VALUE args)
+{
+    return rb_funcallv(rb_cTime, rb_intern("utc"), 6, (VALUE *)args);
+}
+
+static VALUE
+time_utc_new_rescue(VALUE args, VALUE exc)
+{
+    rb_raise(eASN1Error, "invalid time");
+}
+
 VALUE
 asn1time_to_time(const ASN1_TIME *time)
 {
     struct tm tm;
-    VALUE argv[6];
-    int count;
+    if (!ASN1_TIME_to_tm(time, &tm))
+        ossl_raise(eASN1Error, "ASN1_TIME_to_tm");
 
-    memset(&tm, 0, sizeof(struct tm));
-
-    switch (time->type) {
-      case V_ASN1_UTCTIME:
-        count = sscanf((const char *)time->data, "%2d%2d%2d%2d%2d%2dZ",
-                       &tm.tm_year, &tm.tm_mon, &tm.tm_mday, &tm.tm_hour, &tm.tm_min,
-                       &tm.tm_sec);
-
-        if (count == 5) {
-            tm.tm_sec = 0;
-        } else if (count != 6) {
-            ossl_raise(rb_eTypeError, "bad UTCTIME format: \"%s\"",
-                       time->data);
-        }
-        if (tm.tm_year < 50) {
-            tm.tm_year += 2000;
-        } else {
-            tm.tm_year += 1900;
-        }
-        break;
-      case V_ASN1_GENERALIZEDTIME:
-        count = sscanf((const char *)time->data, "%4d%2d%2d%2d%2d%2dZ",
-                       &tm.tm_year, &tm.tm_mon, &tm.tm_mday, &tm.tm_hour, &tm.tm_min,
-                       &tm.tm_sec);
-        if (count == 5) {
-            tm.tm_sec = 0;
-        }
-        else if (count != 6) {
-            ossl_raise(rb_eTypeError, "bad GENERALIZEDTIME format: \"%s\"",
-                       time->data);
-        }
-        break;
-      default:
-        rb_warning("unknown time format");
-        return Qnil;
-    }
-    argv[0] = INT2NUM(tm.tm_year);
-    argv[1] = INT2NUM(tm.tm_mon);
-    argv[2] = INT2NUM(tm.tm_mday);
-    argv[3] = INT2NUM(tm.tm_hour);
-    argv[4] = INT2NUM(tm.tm_min);
-    argv[5] = INT2NUM(tm.tm_sec);
-
-    return rb_funcall2(rb_cTime, rb_intern("utc"), 6, argv);
+    VALUE args[] = {
+        INT2NUM(tm.tm_year + 1900),
+        INT2NUM(tm.tm_mon + 1),
+        INT2NUM(tm.tm_mday),
+        INT2NUM(tm.tm_hour),
+        INT2NUM(tm.tm_min),
+        INT2NUM(tm.tm_sec),
+    };
+    return rb_rescue2(time_utc_new, (VALUE)args, time_utc_new_rescue, Qnil,
+                      rb_eArgError, 0);
 }
 
 static VALUE
