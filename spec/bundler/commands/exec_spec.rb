3294d3d4571bd50a86e701a46ffd70505afdad87
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-02-28 08:53:39 +0900

    [rubygems/rubygems] Skip default gems executable example with Windows platform
    https://github.com/rubygems/rubygems/commit/b4a8bda811

--- spec/bundler/commands/exec_spec.rb
+++ spec/bundler/commands/exec_spec.rb
@@ -197,6 +197,7 @@ RSpec.describe "bundle exec" do
 
     context "when not specified in Gemfile" do
       before do
+        skip "syntax_suggest executable is not provided on Windows" if Gem.win_platform?
         skip "syntax_suggest isn't a default gem" if default_ss_version.empty?
         skip "syntax_suggest executable is broken" if default_ss_version <= "2.0.1"
 
