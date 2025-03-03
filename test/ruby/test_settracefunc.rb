937b31f470e2b2489b79ea2e2f287527c0941289
Naoto Ono <onoto1998@gmail.com>
2025-03-03 10:57:52 +0900

    fix

--- test/ruby/test_settracefunc.rb
+++ test/ruby/test_settracefunc.rb
@@ -844,8 +844,8 @@ CODE
     ary = []
     args = nil
     trace = TracePoint.trace(:call){|tp|
-      $stderr.puts "e: #{tp.event}, f: #{tp.path}, l: #{tp.lineno}, i: #{tp.method_id}, d: #{tp.defined_class}"
       next if !target_thread?
+      $stderr.puts "e: #{tp.event}, f: #{tp.path}, l: #{tp.lineno}, i: #{tp.method_id}, d: #{tp.defined_class}"
       ary << tp.method_id
     }
     foo
