d2115562b92e70bea71cfaca175f59587c6a77da
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2025-03-07 10:59:46 -0500

    Add two more small btests

--- bootstraptest/test_zjit.rb
+++ bootstraptest/test_zjit.rb
@@ -20,3 +20,14 @@ assert_equal '[6, 3]', %q{
   def test(a, b) = a + b
   [test(2, 4), test(1, 2)]
 }
+
+# Test argument ordering
+assert_equal '2', %q{
+  def test(a, b) = a - b
+  test(6, 4)
+}
+
+assert_equal '6', %q{
+  def test(a, b, c) = a + b + c
+  test(1, 2, 3)
+}
