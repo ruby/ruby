fff5baf200572762a60c624bc7bea866992c2f30
nagachika <nagachika@ruby-lang.org>
2025-03-08 18:19:14 +0900

    merge revision(s) 46b544c54955348ef1ea9692b837b061f59f91cd, d3abee739f4feb91bb9aaae33877d70c8c576db0: [Backport #21095]
    	Prefer `uname -n` over `hostname`. (#12647)
    
    	Add fallback for `hostname` if `uname` isn't available. (#12655)

--- spec/ruby/library/socket/socket/gethostname_spec.rb
+++ spec/ruby/library/socket/socket/gethostname_spec.rb
@@ -2,7 +2,15 @@ require_relative '../spec_helper'
 require_relative '../fixtures/classes'
 
 describe "Socket.gethostname" do
+  def system_hostname
+    # Most platforms implement this POSIX standard:
+    `uname -n`.strip
+  rescue
+    # Only really required for Windows without MSYS/MinGW/Cygwin etc:
+    `hostname`.strip
+  end
+
   it "returns the host name" do
-    Socket.gethostname.should == `hostname`.strip
+    Socket.gethostname.should == system_hostname
   end
 end
