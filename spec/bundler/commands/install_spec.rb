afda30774c8165428ca886945889c8747c66e599
David Rodríguez <deivid.rodriguez@riseup.net>
2025-03-12 19:26:06 +0100

    [rubygems/rubygems] Reword log message about current platform being missing
    It reads better this way I think.
    
    https://github.com/rubygems/rubygems/commit/ce9743290d

--- spec/bundler/commands/install_spec.rb
+++ spec/bundler/commands/install_spec.rb
@@ -1368,7 +1368,7 @@ RSpec.describe "bundle install with gem sources" do
     it "adds the current platform to the lockfile" do
       bundle "install --verbose"
 
-      expect(out).to include("re-resolving dependencies because your lockfile does not include the current platform")
+      expect(out).to include("re-resolving dependencies because your lockfile is missing the current platform")
       expect(out).not_to include("you are adding a new platform to your lockfile")
 
       expect(lockfile).to eq <<~L
