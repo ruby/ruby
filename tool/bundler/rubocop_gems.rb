33aaa069a4e7b405e6d7ec5fcbf04a487e36e345
Kevin Newton <kddnewton@gmail.com>
2025-03-18 10:51:45 -0400

    [ruby/prism] Update truffleruby version
    https://github.com/ruby/prism/commit/2afe89f8ce

--- tool/bundler/rubocop_gems.rb
+++ tool/bundler/rubocop_gems.rb
@@ -2,7 +2,11 @@
 
 source "https://rubygems.org"
 
+<<<<<<< HEAD:tool/bundler/rubocop_gems.rb
 gem "rubocop", ">= 1.52.1", "< 2"
+=======
+ruby "~> 3.3.5", engine: "truffleruby", engine_version: "~> 24.2.0"
+>>>>>>> 2afe89f8ce (Update truffleruby version):gemfiles/truffleruby/Gemfile
 
 gem "minitest"
 gem "irb"
