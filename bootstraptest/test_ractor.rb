ruby/ruby

commit 0350290262ea0fbc4e1807901797ee8a6970c2b9

Ractor: Fix moving embedded objects
[Bug #20271]
[Bug #20267]
[Bug #20255]

`rb_obj_alloc(RBASIC_CLASS(obj))` will always allocate from the basic
40B pool, so if `obj` is larger than `40B`, we'll create a corrupted
object when we later copy the shape_id.

Instead we can use the same logic than ractor copy, which is
to use `rb_obj_clone`, and later ask the GC to free the original
object.

We then must turn it into a `T_OBJECT`, because otherwise
just changing its class to `RactorMoved` leaves a lot of
ways to keep using the object, e.g.:

```
a = [1, 2, 3]
Ractor.new{}.send(a, move: true)
[].concat(a) # Should raise, but wasn't.
```

If it turns out that `rb_obj_clone` isn't performant enough
for some uses, we can always have carefully crafted specialized
paths for the types that would benefit from it.

ractor.c
@@ -12,6 +12,7 @@
 #include "internal/error.h"
 #include "internal/gc.h"
 #include "internal/hash.h"
+#include "internal/object.h"
 #include "internal/ractor.h"
 #include "internal/rational.h"
 #include "internal/struct.h"
@@ -3546,37 +3547,6 @@ rb_obj_traverse_replace(VALUE obj,
     }
 }
 
-struct RVALUE {
-    VALUE flags;
-    VALUE klass;
-    VALUE v1;
-    VALUE v2;
-    VALUE v3;
-};
-
-static const VALUE fl_users = FL_USER1  | FL_USER2  | FL_USER3  |
-                              FL_USER4  | FL_USER5  | FL_USER6  | FL_USER7  |
-                              FL_USER8  | FL_USER9  | FL_USER10 | FL_USER11 |
-                              FL_USER12 | FL_USER13 | FL_USER14 | FL_USER15 |
-                              FL_USER16 | FL_USER17 | FL_USER18 | FL_USER19;
-
-static void
-ractor_moved_bang(VALUE obj)
-{
-    // invalidate src object
-    struct RVALUE *rv = (void *)obj;
-
-    rv->klass = rb_cRactorMovedObject;
-    rv->v1 = 0;
-    rv->v2 = 0;
-    rv->v3 = 0;
-    rv->flags = rv->flags & ~fl_users;
-
-    if (BUILTIN_TYPE(obj) == T_OBJECT) ROBJECT_SET_SHAPE_ID(obj, ROOT_SHAPE_ID);
-
-    // TODO: record moved location
-}
-
 static enum obj_traverse_iterator_result
 move_enter(VALUE obj, struct obj_traverse_replace_data *data)
 {
@@ -3585,39 +3555,16 @@ move_enter(VALUE obj, struct obj_traverse_replace_data *data)
         return traverse_skip;
     }
     else {
-        VALUE moved = rb_obj_alloc(RBASIC_CLASS(obj));
-        rb_shape_set_shape(moved, rb_shape_get_shape(obj));
-        data->replacement = moved;
+        data->replacement = rb_obj_clone(obj);
         return traverse_cont;
     }
 }
 
-void rb_replace_generic_ivar(VALUE clone, VALUE obj); // variable.c
-
 static enum obj_traverse_iterator_result
 move_leave(VALUE obj, struct obj_traverse_replace_data *data)
 {
-    VALUE v = data->replacement;
-    struct RVALUE *dst = (struct RVALUE *)v;
-    struct RVALUE *src = (struct RVALUE *)obj;
-
-    dst->flags = (dst->flags & ~fl_users) | (src->flags & fl_users);
-
-    dst->v1 = src->v1;
-    dst->v2 = src->v2;
-    dst->v3 = src->v3;
-
-    if (UNLIKELY(FL_TEST_RAW(obj, FL_EXIVAR))) {
-        rb_replace_generic_ivar(v, obj);
-    }
-
-    if (OBJ_FROZEN(obj)) {
-        OBJ_FREEZE(v);
-    }
-
-    // TODO: generic_ivar
-
-    ractor_moved_bang(obj);
+    rb_gc_ractor_moved(data->replacement, obj);
+    RBASIC_SET_CLASS_RAW(obj, rb_cRactorMovedObject);
     return traverse_cont;
 }
 

bootstraptest/test_ractor.rb
@@ -1987,3 +1987,127 @@ assert_equal 'ok', %q{
   GC.start
   :ok.itself
 }
+
+# moved objects being corrupted if embeded (String)
+assert_equal 'ok', %q{
+  ractor = Ractor.new { Ractor.receive }
+  obj = "foobarbazfoobarbazfoobarbazfoobarbaz"
+  ractor.send(obj.dup, move: true)
+  roundtripped_obj = ractor.take
+  roundtripped_obj == obj ? :ok : roundtripped_obj
+}
+
+# moved objects being corrupted if embeded (Array)
+assert_equal 'ok', %q{
+  ractor = Ractor.new { Ractor.receive }
+  obj = Array.new(10, 42)
+  ractor.send(obj.dup, move: true)
+  roundtripped_obj = ractor.take
+  roundtripped_obj == obj ? :ok : roundtripped_obj
+}
+
+# moved objects being corrupted if embeded (Hash)
+assert_equal 'ok', %q{
+  ractor = Ractor.new { Ractor.receive }
+  obj = { foo: 1, bar: 2 }
+  ractor.send(obj.dup, move: true)
+  roundtripped_obj = ractor.take
+  roundtripped_obj == obj ? :ok : roundtripped_obj
+}
+
+# moved objects being corrupted if embeded (MatchData)
+assert_equal 'ok', %q{
+  ractor = Ractor.new { Ractor.receive }
+  obj = "foo".match(/o/)
+  ractor.send(obj.dup, move: true)
+  roundtripped_obj = ractor.take
+  roundtripped_obj == obj ? :ok : roundtripped_obj
+}
+
+# moved objects being corrupted if embeded (Struct)
+assert_equal 'ok', %q{
+  ractor = Ractor.new { Ractor.receive }
+  obj = Struct.new(:a, :b, :c, :d, :e, :f).new(1, 2, 3, 4, 5, 6)
+  ractor.send(obj.dup, move: true)
+  roundtripped_obj = ractor.take
+  roundtripped_obj == obj ? :ok : roundtripped_obj
+}
+
+# moved objects being corrupted if embeded (Object)
+assert_equal 'ok', %q{
+  ractor = Ractor.new { Ractor.receive }
+  class SomeObject
+    attr_reader :a, :b, :c, :d, :e, :f
+    def initialize
+      @a = @b = @c = @d = @e = @f = 1
+    end
+
+    def ==(o)
+      @a == o.a &&
+      @b == o.b &&
+      @c == o.c &&
+      @d == o.d &&
+      @e == o.e &&
+      @f == o.f
+    end
+  end
+
+  SomeObject.new # initial non-embeded
+
+  obj = SomeObject.new
+  ractor.send(obj.dup, move: true)
+  roundtripped_obj = ractor.take
+  roundtripped_obj == obj ? :ok : roundtripped_obj
+}
+
+# moved arrays can't be used
+assert_equal 'ok', %q{
+  ractor = Ractor.new { Ractor.receive }
+  obj = [1]
+  ractor.send(obj, move: true)
+  begin
+    [].concat(obj)
+  rescue TypeError
+    :ok
+  else
+    :fail
+  end
+}
+
+# moved strings can't be used
+assert_equal 'ok', %q{
+  ractor = Ractor.new { Ractor.receive }
+  obj = "hello"
+  ractor.send(obj, move: true)
+  begin
+    "".replace(obj)
+  rescue TypeError
+    :ok
+  else
+    :fail
+  end
+}
+
+# moved hashes can't be used
+assert_equal 'ok', %q{
+  ractor = Ractor.new { Ractor.receive }
+  obj = { a: 1 }
+  ractor.send(obj, move: true)
+  begin
+    {}.merge(obj)
+  rescue TypeError
+    :ok
+  else
+    :fail
+  end
+}
+
+# moved objects keep their object_id
+assert_equal 'ok', %q{
+  ractor = Ractor.new { Ractor.receive }
+  obj = Object.new
+  id = obj.object_id
+  ractor.send(obj, move: true)
+  roundtripped_obj = ractor.take
+  roundtripped_obj.object_id == id ? :ok : :fail
+}

common.mk
@@ -13531,6 +13531,7 @@ ractor.$(OBJEXT): $(top_srcdir)/internal/gc.h
 ractor.$(OBJEXT): $(top_srcdir)/internal/hash.h
 ractor.$(OBJEXT): $(top_srcdir)/internal/imemo.h
 ractor.$(OBJEXT): $(top_srcdir)/internal/numeric.h
+ractor.$(OBJEXT): $(top_srcdir)/internal/object.h
 ractor.$(OBJEXT): $(top_srcdir)/internal/ractor.h
 ractor.$(OBJEXT): $(top_srcdir)/internal/rational.h
 ractor.$(OBJEXT): $(top_srcdir)/internal/sanitizers.h

gc.c
@@ -665,6 +665,7 @@ typedef struct gc_function_map {
     // Object ID
     VALUE (*object_id)(void *objspace_ptr, VALUE obj);
     VALUE (*object_id_to_ref)(void *objspace_ptr, VALUE object_id);
+    void (*object_id_move)(void *objspace_ptr, VALUE dest, VALUE src);
     // Forking
     void (*before_fork)(void *objspace_ptr);
     void (*after_fork)(void *objspace_ptr, rb_pid_t pid);
@@ -842,6 +843,7 @@ ruby_modular_gc_init(void)
     // Object ID
     load_modular_gc_func(object_id);
     load_modular_gc_func(object_id_to_ref);
+    load_modular_gc_func(object_id_move);
     // Forking
     load_modular_gc_func(before_fork);
     load_modular_gc_func(after_fork);
@@ -925,6 +927,7 @@ ruby_modular_gc_init(void)
 // Object ID
 # define rb_gc_impl_object_id rb_gc_functions.object_id
 # define rb_gc_impl_object_id_to_ref rb_gc_functions.object_id_to_ref
+# define rb_gc_impl_object_id_move rb_gc_functions.object_id_move
 // Forking
 # define rb_gc_impl_before_fork rb_gc_functions.before_fork
 # define rb_gc_impl_after_fork rb_gc_functions.after_fork
@@ -966,7 +969,6 @@ rb_objspace_alloc(void)
 
     void *objspace = rb_gc_impl_objspace_alloc();
     ruby_current_vm_ptr->gc.objspace = objspace;
-
     rb_gc_impl_objspace_init(objspace);
     rb_gc_impl_stress_set(objspace, initial_stress);
 
@@ -2658,6 +2660,19 @@ rb_gc_mark_roots(void *objspace, const char **categoryp)
 }
 
 #define TYPED_DATA_REFS_OFFSET_LIST(d) (size_t *)(uintptr_t)RTYPEDDATA(d)->type->function.dmark
+
+void
+rb_gc_ractor_moved(VALUE dest, VALUE src)
+{
+    void *objspace = rb_gc_get_objspace();
+    if (UNLIKELY(FL_TEST_RAW(src, FL_SEEN_OBJ_ID))) {
+        rb_gc_impl_object_id_move(objspace, dest, src);
+    }
+
+    rb_gc_obj_free(objspace, src);
+    MEMZERO((void *)src, char, rb_gc_obj_slot_size(src));
+    RBASIC(src)->flags = T_OBJECT | FL_FREEZE; // Avoid mutations using bind_call, etc.
+}
 
 void
 rb_gc_mark_children(void *objspace, VALUE obj)

gc/default/default.c
@@ -1565,25 +1565,6 @@ rb_gc_impl_garbage_object_p(void *objspace_ptr, VALUE ptr)
 }
 
 VALUE
-rb_gc_impl_object_id_to_ref(void *objspace_ptr, VALUE object_id)
-{
-    rb_objspace_t *objspace = objspace_ptr;
-
-    VALUE obj;
-    if (st_lookup(objspace->id_to_obj_tbl, object_id, &obj) &&
-            !rb_gc_impl_garbage_object_p(objspace, obj)) {
-        return obj;
-    }
-
-    if (rb_funcall(object_id, rb_intern(">="), 1, ULL2NUM(objspace->next_object_id))) {
-        rb_raise(rb_eRangeError, "%+"PRIsVALUE" is not id value", rb_funcall(object_id, rb_intern("to_s"), 1, INT2FIX(10)));
-    }
-    else {
-        rb_raise(rb_eRangeError, "%+"PRIsVALUE" is recycled object", rb_funcall(object_id, rb_intern("to_s"), 1, INT2FIX(10)));
-    }
-}
-
-VALUE
 rb_gc_impl_object_id(void *objspace_ptr, VALUE obj)
 {
     VALUE id;
@@ -1612,6 +1593,46 @@ rb_gc_impl_object_id(void *objspace_ptr, VALUE obj)
     rb_gc_vm_unlock(lev);
 
     return id;
+}
+
+VALUE
+rb_gc_impl_object_id_to_ref(void *objspace_ptr, VALUE object_id)
+{
+    rb_objspace_t *objspace = objspace_ptr;
+
+    VALUE obj;
+    if (st_lookup(objspace->id_to_obj_tbl, object_id, &obj) &&
+            !rb_gc_impl_garbage_object_p(objspace, obj)) {
+        return obj;
+    }
+
+    if (rb_funcall(object_id, rb_intern(">="), 1, ULL2NUM(objspace->next_object_id))) {
+        rb_raise(rb_eRangeError, "%+"PRIsVALUE" is not id value", rb_funcall(object_id, rb_intern("to_s"), 1, INT2FIX(10)));
+    }
+    else {
+        rb_raise(rb_eRangeError, "%+"PRIsVALUE" is recycled object", rb_funcall(object_id, rb_intern("to_s"), 1, INT2FIX(10)));
+    }
+}
+
+void
+rb_gc_impl_object_id_move(void *objspace_ptr, VALUE dest, VALUE src)
+{
+   /* If the source object's object_id has been seen, we need to update
+    * the object to object id mapping. */
+   st_data_t id = 0;
+   rb_objspace_t *objspace = objspace_ptr;
+
+   unsigned int lev = rb_gc_vm_lock();
+   st_data_t key = (st_data_t)src;
+   if (!st_delete(objspace->obj_to_id_tbl, &key, &id)) {
+       rb_bug("gc_move: object ID seen, but not in mapping table: %s", rb_obj_info(src));
+   }
+   FL_UNSET_RAW(src, FL_SEEN_OBJ_ID);
+
+   st_insert(objspace->obj_to_id_tbl, (st_data_t)dest, id);
+   st_insert(objspace->id_to_obj_tbl, id, (st_data_t)dest);
+   FL_SET_RAW(dest, FL_SEEN_OBJ_ID);
+   rb_gc_vm_unlock(lev);
 }
 
 static void free_stack_chunks(mark_stack_t *);

gc/gc_impl.h
@@ -103,6 +103,7 @@ GC_IMPL_FN void rb_gc_impl_shutdown_call_finalizer(void *objspace_ptr);
 // Object ID
 GC_IMPL_FN VALUE rb_gc_impl_object_id(void *objspace_ptr, VALUE obj);
 GC_IMPL_FN VALUE rb_gc_impl_object_id_to_ref(void *objspace_ptr, VALUE object_id);
+GC_IMPL_FN void rb_gc_impl_object_id_move(void *objspace_ptr, VALUE dest, VALUE src);
 // Forking
 GC_IMPL_FN void rb_gc_impl_before_fork(void *objspace_ptr);
 GC_IMPL_FN void rb_gc_impl_after_fork(void *objspace_ptr, rb_pid_t pid);

gc/mmtk/mmtk.c
@@ -1108,16 +1108,21 @@ objspace_obj_id_init(struct objspace *objspace)
 VALUE
 rb_gc_impl_object_id(void *objspace_ptr, VALUE obj)
 {
+    VALUE id;
     struct objspace *objspace = objspace_ptr;
 
     unsigned int lev = rb_gc_vm_lock();
-
-    VALUE id;
-    if (st_lookup(objspace->obj_to_id_tbl, (st_data_t)obj, &id)) {
-        RUBY_ASSERT(FL_TEST(obj, FL_SEEN_OBJ_ID));
+    if (FL_TEST(obj, FL_SEEN_OBJ_ID)) {
+        st_data_t val;
+        if (st_lookup(objspace->obj_to_id_tbl, (st_data_t)obj, &val)) {
+            id = (VALUE)val;
+        }
+        else {
+            rb_bug("rb_gc_impl_object_id: FL_SEEN_OBJ_ID flag set but not found in table");
+        }
     }
     else {
-        RUBY_ASSERT(!FL_TEST(obj, FL_SEEN_OBJ_ID));
+        RUBY_ASSERT(!st_lookup(objspace->obj_to_id_tbl, (st_data_t)obj, NULL));
 
         id = ULL2NUM(objspace->next_object_id);
         objspace->next_object_id += OBJ_ID_INCREMENT;
@@ -1126,7 +1131,6 @@ rb_gc_impl_object_id(void *objspace_ptr, VALUE obj)
         st_insert(objspace->id_to_obj_tbl, (st_data_t)id, (st_data_t)obj);
         FL_SET(obj, FL_SEEN_OBJ_ID);
     }
-
     rb_gc_vm_unlock(lev);
 
     return id;
@@ -1149,6 +1153,27 @@ rb_gc_impl_object_id_to_ref(void *objspace_ptr, VALUE object_id)
     else {
         rb_raise(rb_eRangeError, "%+"PRIsVALUE" is recycled object", rb_funcall(object_id, rb_intern("to_s"), 1, INT2FIX(10)));
     }
+}
+
+void
+rb_gc_impl_object_id_move(void *objspace_ptr, VALUE dest, VALUE src)
+{
+   /* If the source object's object_id has been seen, we need to update
+    * the object to object id mapping. */
+   st_data_t id = 0;
+   struct objspace *objspace = objspace_ptr;
+
+   unsigned int lev = rb_gc_vm_lock();
+   st_data_t key = (st_data_t)src;
+   if (!st_delete(objspace->obj_to_id_tbl, &key, &id)) {
+       rb_bug("gc_move: object ID seen, but not in mapping table: %s", rb_obj_info(src));
+   }
+   FL_UNSET_RAW(src, FL_SEEN_OBJ_ID);
+
+   st_insert(objspace->obj_to_id_tbl, (st_data_t)dest, id);
+   st_insert(objspace->id_to_obj_tbl, id, (st_data_t)dest);
+   FL_SET_RAW(dest, FL_SEEN_OBJ_ID);
+   rb_gc_vm_unlock(lev);
 }
 
 // Forking

internal/gc.h
@@ -183,6 +183,7 @@ struct rb_gc_object_metadata_entry {
 /* gc.c */
 RUBY_ATTR_MALLOC void *ruby_mimmalloc(size_t size);
 RUBY_ATTR_MALLOC void *ruby_mimcalloc(size_t num, size_t size);
+void rb_gc_ractor_moved(VALUE dest, VALUE src);
 void ruby_mimfree(void *ptr);
 void rb_gc_prepare_heap(void);
 void rb_objspace_set_event_hook(const rb_event_flag_t event);

commit 9f7467feac014cd2d00e3a277936e37bda1227b9

Fix moving composite object parts
Some bugs were introduced in commit 0350290262ea0fbc4e1807901797ee8a6970c2b9
that had to do with moving composite object parts. This fixes those bugs
and adds tests for those cases.

ractor.c
@@ -3353,25 +3353,21 @@ obj_traverse_replace_i(VALUE obj, struct obj_traverse_replace_data *data)
         st_insert(obj_traverse_replace_rec(data), (st_data_t)obj, replacement);
     }
 
-    if (!data->move) {
-        obj = replacement;
-    }
-
 #define CHECK_AND_REPLACE(v) do { \
     VALUE _val = (v); \
     if (obj_traverse_replace_i(_val, data)) { return 1; } \
-    else if (data->replacement != _val)     { RB_OBJ_WRITE(obj, &v, data->replacement); } \
+    else if (data->replacement != _val)     { RB_OBJ_WRITE(replacement, &v, data->replacement); } \
 } while (0)
 
-    if (UNLIKELY(FL_TEST_RAW(obj, FL_EXIVAR))) {
+    if (UNLIKELY(FL_TEST_RAW(replacement, FL_EXIVAR))) {
         struct gen_ivtbl *ivtbl;
-        rb_ivar_generic_ivtbl_lookup(obj, &ivtbl);
+        rb_ivar_generic_ivtbl_lookup(replacement, &ivtbl);
 
-        if (UNLIKELY(rb_shape_obj_too_complex(obj))) {
+        if (UNLIKELY(rb_shape_obj_too_complex(replacement))) {
             struct obj_traverse_replace_callback_data d = {
                 .stop = false,
                 .data = data,
-                .src = obj,
+                .src = replacement,
             };
             rb_st_foreach_with_replace(
                 ivtbl->as.complex.table,
@@ -3400,19 +3396,19 @@ obj_traverse_replace_i(VALUE obj, struct obj_traverse_replace_data *data)
       case T_MATCH:
         break;
       case T_STRING:
-        rb_str_make_independent(obj);
+        rb_str_make_independent(replacement);
         break;
 
       case T_OBJECT:
         {
-            if (rb_shape_obj_too_complex(obj)) {
+            if (rb_shape_obj_too_complex(replacement)) {
                 struct obj_traverse_replace_callback_data d = {
                     .stop = false,
                     .data = data,
-                    .src = obj,
+                    .src = replacement,
                 };
                 rb_st_foreach_with_replace(
-                    ROBJECT_IV_HASH(obj),
+                    ROBJECT_IV_HASH(replacement),
                     obj_iv_hash_traverse_replace_foreach_i,
                     obj_iv_hash_traverse_replace_i,
                     (st_data_t)&d
@@ -3420,8 +3416,8 @@ obj_traverse_replace_i(VALUE obj, struct obj_traverse_replace_data *data)
                 if (d.stop) return 1;
             }
             else {
-                uint32_t len = ROBJECT_IV_COUNT(obj);
-                VALUE *ptr = ROBJECT_IVPTR(obj);
+                uint32_t len = ROBJECT_IV_COUNT(replacement);
+                VALUE *ptr = ROBJECT_IVPTR(replacement);
 
                 for (uint32_t i = 0; i < len; i++) {
                     CHECK_AND_REPLACE(ptr[i]);
@@ -3432,19 +3428,19 @@ obj_traverse_replace_i(VALUE obj, struct obj_traverse_replace_data *data)
 
       case T_ARRAY:
         {
-            rb_ary_cancel_sharing(obj);
+            rb_ary_cancel_sharing(replacement);
 
-            for (int i = 0; i < RARRAY_LENINT(obj); i++) {
-                VALUE e = rb_ary_entry(obj, i);
+            for (int i = 0; i < RARRAY_LENINT(replacement); i++) {
+                VALUE e = rb_ary_entry(replacement, i);
 
                 if (obj_traverse_replace_i(e, data)) {
                     return 1;
                 }
                 else if (e != data->replacement) {
-                    RARRAY_ASET(obj, i, data->replacement);
+                    RARRAY_ASET(replacement, i, data->replacement);
                 }
             }
-            RB_GC_GUARD(obj);
+            RB_GC_GUARD(replacement);
         }
         break;
       case T_HASH:
@@ -3452,9 +3448,9 @@ obj_traverse_replace_i(VALUE obj, struct obj_traverse_replace_data *data)
             struct obj_traverse_replace_callback_data d = {
                 .stop = false,
                 .data = data,
-                .src = obj,
+                .src = replacement,
             };
-            rb_hash_stlike_foreach_with_replace(obj,
+            rb_hash_stlike_foreach_with_replace(replacement,
                                                 obj_hash_traverse_replace_foreach_i,
                                                 obj_hash_traverse_replace_i,
                                                 (VALUE)&d);
@@ -3466,15 +3462,15 @@ obj_traverse_replace_i(VALUE obj, struct obj_traverse_replace_data *data)
                 return 1;
             }
             else if (ifnone != data->replacement) {
-                RHASH_SET_IFNONE(obj, data->replacement);
+                RHASH_SET_IFNONE(replacement, data->replacement);
             }
         }
         break;
 
       case T_STRUCT:
         {
-            long len = RSTRUCT_LEN(obj);
-            const VALUE *ptr = RSTRUCT_CONST_PTR(obj);
+            long len = RSTRUCT_LEN(replacement);
+            const VALUE *ptr = RSTRUCT_CONST_PTR(replacement);
 
             for (long i=0; i<len; i++) {
                 CHECK_AND_REPLACE(ptr[i]);
@@ -3483,16 +3479,16 @@ obj_traverse_replace_i(VALUE obj, struct obj_traverse_replace_data *data)
         break;
 
       case T_RATIONAL:
-        CHECK_AND_REPLACE(RRATIONAL(obj)->num);
-        CHECK_AND_REPLACE(RRATIONAL(obj)->den);
+        CHECK_AND_REPLACE(RRATIONAL(replacement)->num);
+        CHECK_AND_REPLACE(RRATIONAL(replacement)->den);
         break;
       case T_COMPLEX:
-        CHECK_AND_REPLACE(RCOMPLEX(obj)->real);
-        CHECK_AND_REPLACE(RCOMPLEX(obj)->imag);
+        CHECK_AND_REPLACE(RCOMPLEX(replacement)->real);
+        CHECK_AND_REPLACE(RCOMPLEX(replacement)->imag);
         break;
 
       case T_DATA:
-        if (!data->move && obj_refer_only_shareables_p(obj)) {
+        if (!data->move && obj_refer_only_shareables_p(replacement)) {
             break;
         }
         else {

bootstraptest/test_ractor.rb
@@ -2101,3 +2101,76 @@ assert_equal 'ok', %q{
     :fail
   end
 }
+
+# moved composite types move their non-shareable parts properly
+assert_equal 'ok', %q{
+  k, v = String.new("key"), String.new("value")
+  h = { k => v }
+  h.instance_variable_set("@b", String.new("b"))
+  a = [k,v]
+  o_singleton = Object.new
+  def o_singleton.a
+    @a
+  end
+  o_singleton.instance_variable_set("@a", String.new("a"))
+  class MyObject
+    attr_reader :a
+    def initialize(a)
+      @a = a
+    end
+  end
+  struct_class = Struct.new(:a)
+  struct = struct_class.new(String.new('a'))
+  o = MyObject.new(String.new('a'))
+  r = Ractor.new do
+    loop do
+      obj = Ractor.receive
+      val = case obj
+      when Hash
+        obj['key'] == 'value' && obj.instance_variable_get("@b") == 'b'
+      when Array
+        obj[0] == 'key'
+      when Struct
+        obj.a == 'a'
+      when Object
+        obj.a == 'a'
+      end
+      Ractor.yield val
+    end
+  end
+
+  objs = [h, a, o_singleton, o, struct]
+  objs.each_with_index do |obj, i|
+    klass = obj.class
+    parts_moved = {}
+    case obj
+    when Hash
+      parts_moved[klass] = [obj['key'], obj.instance_variable_get("@b")]
+    when Array
+      parts_moved[klass] = obj.dup # the contents
+    when Struct, Object
+      parts_moved[klass] = [obj.a]
+    end
+    r.send(obj, move: true)
+    val = r.take
+    if val != true
+      raise "bad val in ractor for obj at i:#{i}"
+    end
+    begin
+      p obj
+    rescue
+    else
+      raise "should be moved"
+    end
+    parts_moved.each do |klass, parts|
+      parts.each_with_index do |part, j|
+        case part
+        when Ractor::MovedObject
+        else
+          raise "part for class #{klass} at i:#{j} should be moved"
+        end
+      end
+    end
+  end
+  'ok'
+}