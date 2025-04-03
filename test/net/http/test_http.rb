0d25bc2b81651e4054654f212166547a547dd6e7
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-04-03 19:30:24 +0900

    Use EnvUtil.apply_timeout_scale

--- test/net/http/test_http.rb
+++ test/net/http/test_http.rb
@@ -589,9 +589,9 @@ module TestNetHTTP_version_1_1_methods
       port = server.addr[1]
 
       conn = Net::HTTP.new('localhost', port)
-      conn.write_timeout = 0.01
-      conn.read_timeout = 0.01 if windows?
-      conn.open_timeout = 1
+      conn.write_timeout = EnvUtil.apply_timeout_scale(0.01)
+      conn.read_timeout = EnvUtil.apply_timeout_scale(0.01) if windows?
+      conn.open_timeout = EnvUtil.apply_timeout_scale(1)
 
       req = Net::HTTP::Post.new('/')
       data = "a"*50_000_000
