fdfa92d847e88b553a133bfed5532c46b0f863ce
Fabio Sangiovanni <f@sangiovanni.dev>
2025-04-04 17:05:51 +0200

    Drop redundant invocation of `rb_class_real` in `make_singleton_class`
    Since:
      - `orig_class` is prevented to be a singleton class by client code
        (see `singleton_class_of`), and
      - `orig_class` cannot be an iclass,
    
    we can safely avoid the call `rb_class_real(orig_class)`.

--- class.c
+++ class.c
@@ -808,7 +808,7 @@ make_singleton_class(VALUE obj)
     rb_singleton_class_attached(klass, obj);
     rb_yjit_invalidate_no_singleton_class(orig_class);
 
-    SET_METACLASS_OF(klass, METACLASS_OF(rb_class_real(orig_class)));
+    SET_METACLASS_OF(klass, METACLASS_OF(orig_class));
     return klass;
 }
 
