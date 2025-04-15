ccc7493308ae327fb6c089e88e0cd4436c4dc513
Mari Imaizumi <mariimaizumi5@gmail.com>
2025-04-16 00:31:10 +0900

    U+036F is now alnum
    0363..036F    ; Alphabetic # Mn  [13] COMBINING LATIN SMALL LETTER A..COMBINING LATIN SMALL LETTER X

--- spec/ruby/language/regexp/character_classes_spec.rb
+++ spec/ruby/language/regexp/character_classes_spec.rb
@@ -113,7 +113,7 @@ describe "Regexp with character classes" do
   end
 
   it "doesn't matches Unicode marks with [[:alnum:]]" do
-    "\u{36F}".match(/[[:alnum:]]/).should be_nil
+    "\u{3099}".match(/[[:alnum:]]/).should be_nil
   end
 
   it "doesn't match Unicode control characters with [[:alnum:]]" do
@@ -133,7 +133,7 @@ describe "Regexp with character classes" do
   end
 
   it "doesn't matches Unicode marks with [[:alpha:]]" do
-    "\u{36F}".match(/[[:alpha:]]/).should be_nil
+    "\u{3099}".match(/[[:alpha:]]/).should be_nil
   end
 
   it "doesn't match Unicode control characters with [[:alpha:]]" do
