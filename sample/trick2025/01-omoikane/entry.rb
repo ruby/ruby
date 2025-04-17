25efd09c71fc53acf1c5ce7f994b1bb78396b9f9
Yusuke Endoh <mame@ruby-lang.org>
2025-04-17 17:10:16 +0900

    sample/trick2025/: adds the top-five entries of TRICK 2025
    See https://github.com/tric/trick2025 for TRICK 2025.

---
+++ sample/trick2025/01-omoikane/entry.rb
@@ -0,0 +1,32 @@
+            a=+Math::PI/13
+          #Z---z';#za-mRUBY
+         #A-ZaA-Mn--\[+>+>++
+         '"N-Z(\++\[->++++@"
+        b=\[->+>     +>+>\[h_
+        p%{}             eact
+       zoraq  ;%{   GF.  rin);
+       %{eb} r   A R   p *""\]
+      <<<{{{             }<\]<b
+     ]<l(%w|    }   ;    a;a=%Y/
+    evar{|c)}    <][    #pgny\W{f
+    chaa,b)]>++[     ->+>>>>>[40v
+   .tr(= ' ;eval(%w{r=u=b= y =0;%{
+  (ct;c ) ; ] <<->--<<< < < ] >>[>,
+ exi}; a * = A RGV.siz e > 0 ? -1:1;
+ z=[] ; A R G F .ea c h _ l i n e{|i
+|i.eac h _ g r aph e m e _ c l u ster
+{|j|i f ( k = j.o r d ) < 3 3 ; r+=k<
+32?k==9? 8 - r%8 : k = = 1 0 | |k==13
+?[u+=1,-r][ 1]: 0 : 1 ; e lse;z+=[[u,
+r,j]];b+=r;y+=u;r+=1;end;}};if(s=z.si
+ze)>0;b/=s;y/=s;m,n=z[0];i=Math::tan(
+a/2);j=Math::sin(a);z.map!{|d|p=d[1]-
+b;q=d[0]-y;p-=(i*q).round;m=[m,q+=(j*
+ p).round].min;n=[n,p-=(i*q).round].
+  min;[q,p,d[2]]};r=n;u=m;z.sort.eac
+   h{|d|p,b=d;r=(u<p)?n:r;print"\n"
+    *(p-u),"\40"*(b-r),d[2];u=p;r=
+     b+1};print"\n";end}*"");%(]>
+       "tyvuts(}}.--.>--.>+.<++'
+         )b\40"gena.(c)2025<<<
+           #)#ehol""+a*.^_^
