06a7b3c14453b35ff530b3a06c134193dc25207e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-04-18 01:19:05 +0900

    Only test on known good platforms. (#13123)

--- ext/-test-/stack/stack.c
+++ ext/-test-/stack/stack.c
@@ -17,8 +17,19 @@ stack_alloca_overflow(VALUE self)
     return Qnil;
 }
 
+static VALUE
+asan_p(VALUE klass)
+{
+#if defined(__SANITIZE_ADDRESS__) || __has_feature(address_sanitizer)
+    return Qtrue;
+#else
+    return Qfalse;
+#endif
+}
+
 void
 Init_stack(VALUE klass)
 {
     rb_define_singleton_method(rb_cThread, "alloca_overflow", stack_alloca_overflow, 0);
+    rb_define_singleton_method(rb_cThread, "asan?", asan_p, 0);
 }
