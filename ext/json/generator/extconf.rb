960a2754c20200896b15d343b151522d21cd4c19
Scott Myron <samyron@gmail.com>
2025-04-28 07:57:10 -0500

    [ruby/json] Introduce ARM Neon and SSE2 SIMD. (https://github.com/ruby/json/pull/743)
    See the pull request for the long development history: https://github.com/ruby/json/pull/743
    
    ```
    == Encoding activitypub.json (52595 bytes)
    ruby 3.4.2 (2025-02-15 revision https://github.com/ruby/json/commit/d2930f8e7a) +YJIT +PRISM [arm64-darwin24]
    Warming up --------------------------------------
                   after     2.913k i/100ms
    Calculating -------------------------------------
                   after     29.377k (± 2.0%) i/s   (34.04 μs/i) -    148.563k in   5.059169s
    
    Comparison:
                  before:    23314.1 i/s
                   after:    29377.3 i/s - 1.26x  faster
    
    == Encoding citm_catalog.json (500298 bytes)
    ruby 3.4.2 (2025-02-15 revision https://github.com/ruby/json/commit/d2930f8e7a) +YJIT +PRISM [arm64-darwin24]
    Warming up --------------------------------------
                   after   152.000 i/100ms
    Calculating -------------------------------------
                   after      1.569k (± 0.8%) i/s  (637.49 μs/i) -      7.904k in   5.039001s
    
    Comparison:
                  before:     1485.6 i/s
                   after:     1568.7 i/s - 1.06x  faster
    
    == Encoding twitter.json (466906 bytes)
    ruby 3.4.2 (2025-02-15 revision https://github.com/ruby/json/commit/d2930f8e7a) +YJIT +PRISM [arm64-darwin24]
    Warming up --------------------------------------
                   after   309.000 i/100ms
    Calculating -------------------------------------
                   after      3.115k (± 3.1%) i/s  (321.01 μs/i) -     15.759k in   5.063776s
    
    Comparison:
                  before:     2508.3 i/s
                   after:     3115.2 i/s - 1.24x  faster
    ```
    
    https://github.com/ruby/json/commit/49003523da

--- ext/json/generator/extconf.rb
+++ ext/json/generator/extconf.rb
@@ -6,5 +6,36 @@ if RUBY_ENGINE == 'truffleruby'
 else
   append_cflags("-std=c99")
   $defs << "-DJSON_GENERATOR"
+
+  if enable_config('generator-use-simd', default=!ENV["JSON_DISABLE_SIMD"])
+    if RbConfig::CONFIG['host_cpu'] =~ /^(arm.*|aarch64.*)/
+      # Try to compile a small program using NEON instructions
+      if have_header('arm_neon.h')
+        have_type('uint8x16_t', headers=['arm_neon.h']) && try_compile(<<~'SRC')
+          #include <arm_neon.h>
+          int main() {
+              uint8x16_t test = vdupq_n_u8(32);
+              return 0;
+          }
+        SRC
+          $defs.push("-DENABLE_SIMD")
+      end
+    end
+
+    if have_header('x86intrin.h') && have_type('__m128i', headers=['x86intrin.h']) && try_compile(<<~'SRC', opt='-msse2')
+      #include <x86intrin.h>
+      int main() {
+          __m128i test = _mm_set1_epi8(32);
+          return 0;
+      }
+      SRC
+        $defs.push("-DENABLE_SIMD")
+    end
+
+    have_header('cpuid.h')
+  end
+
+  create_header
+
   create_makefile 'json/ext/generator'
 end
