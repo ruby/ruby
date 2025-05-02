0f066269150375982712bd4a918438db75256ba6
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-05-02 10:11:09 +0900

    Bump up strscan version to 3.1.5.dev

--- ext/strscan/strscan.c
+++ ext/strscan/strscan.c
@@ -22,7 +22,7 @@ extern size_t onig_region_memsize(const struct re_registers *regs);
 
 #include <stdbool.h>
 
-#define STRSCAN_VERSION "3.1.4.dev"
+#define STRSCAN_VERSION "3.1.5.dev"
 
 /* =======================================================================
                          Data Type Definitions
