8872d3e10bc36a07dff72d0baf461992cca9b699
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-05-10 15:40:24 +0900

    [Feature #21205] Fix up birthtime in ruby/spec

--- spec/ruby/core/file/birthtime_spec.rb
+++ spec/ruby/core/file/birthtime_spec.rb
@@ -1,15 +1,15 @@
 require_relative '../../spec_helper'
 
-describe "File.birthtime" do
-  before :each do
-    @file = __FILE__
-  end
+platform_is :windows, :darwin, :freebsd, :netbsd, :linux do
+  describe "File.birthtime" do
+    before :each do
+      @file = __FILE__
+    end
 
-  after :each do
-    @file = nil
-  end
+    after :each do
+      @file = nil
+    end
 
-  platform_is :windows, :darwin, :freebsd, :netbsd do
     it "returns the birth time for the named file as a Time object" do
       File.birthtime(@file)
       File.birthtime(@file).should be_kind_of(Time)
@@ -24,35 +24,19 @@ describe "File.birthtime" do
     end
   end
 
-  platform_is :openbsd do
-    it "raises an NotImplementedError" do
-      -> { File.birthtime(@file) }.should raise_error(NotImplementedError)
+  describe "File#birthtime" do
+    before :each do
+      @file = File.open(__FILE__)
     end
-  end
 
-  # TODO: depends on Linux kernel version
-end
+    after :each do
+      @file.close
+      @file = nil
+    end
 
-describe "File#birthtime" do
-  before :each do
-    @file = File.open(__FILE__)
-  end
-
-  after :each do
-    @file.close
-    @file = nil
-  end
-
-  platform_is :windows, :darwin, :freebsd, :netbsd do
     it "returns the birth time for self" do
       @file.birthtime
       @file.birthtime.should be_kind_of(Time)
-    end
-  end
-
-  platform_is :openbsd do
-    it "raises an NotImplementedError" do
-      -> { @file.birthtime }.should raise_error(NotImplementedError)
     end
   end
 
--- spec/ruby/core/file/stat/birthtime_spec.rb
+++ spec/ruby/core/file/stat/birthtime_spec.rb
@@ -1,27 +1,22 @@
 require_relative '../../../spec_helper'
 
-describe "File::Stat#birthtime" do
-  before :each do
-    @file = tmp('i_exist')
-    touch(@file) { |f| f.write "rubinius" }
-  end
+platform_is(:windows, :darwin, :freebsd, :netbsd,
+            *ruby_version_is("3.5") { :linux },
+           ) do
+  describe "File::Stat#birthtime" do
+    before :each do
+      @file = tmp('i_exist')
+      touch(@file) { |f| f.write "rubinius" }
+    end
 
-  after :each do
-    rm_r @file
-  end
+    after :each do
+      rm_r @file
+    end
 
-  platform_is :windows, :darwin, :freebsd, :netbsd do
     it "returns the birthtime of a File::Stat object" do
       st = File.stat(@file)
       st.birthtime.should be_kind_of(Time)
       st.birthtime.should <= Time.now
-    end
-  end
-
-  platform_is :linux, :openbsd do
-    it "raises an NotImplementedError" do
-      st = File.stat(@file)
-      -> { st.birthtime }.should raise_error(NotImplementedError)
     end
   end
 end
