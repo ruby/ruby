c6528548d04cb918f424885111a2d6ae9516518d
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-05-12 20:26:49 +0900

    [ruby/erb] Use cgi/escape instead of deprecated cgi/util

--- lib/erb.rb
+++ lib/erb.rb
@@ -12,11 +12,7 @@
 #
 # You can redistribute it and/or modify it under the same terms as Ruby.
 
-begin
-  require 'cgi/escape'
-rescue LoadError
-  require 'cgi/util'
-end
+require 'cgi/escape'
 require 'erb/version'
 require 'erb/compiler'
 require 'erb/def_method'
