6e8be3a63438e2686a9c1dccc4c469aa7dbf0b08
David Rodríguez <deivid.rodriguez@riseup.net>
2025-05-13 12:54:35 +0200

    [rubygems/rubygems] Test JRuby 10
    Necessary changes to get tests passing are:
    
    * Rewrite one "out of memory" error spec to not define a subclass inside
      a RSpec context block. Due to some [JRuby issue], that's failing in
      JRuby 10, so I rewrote the test so that the Bundler process really
      goes OOM and that class definition is not necessary.
    * JRuby 10, even if Ruby 3.4-compatible, has not yet adapted backtraces
      to include receivers, so our tests need an special case for JRuby when
      detecting a test method call inside backtraces.
    * Warbler test is upgraded to use JRuby 10. Getting it to pass needs [a
      PR] to warbler, so our test is temporarily pointing to that PR.
    
    [JRuby issue]: https://github.com/jruby/jruby/issues/8838
    [a PR]: https://github.com/jruby/warbler/pull/557
    
    https://github.com/rubygems/rubygems/commit/edec85d4c3

--- spec/bundler/bundler/friendly_errors_spec.rb
+++ spec/bundler/bundler/friendly_errors_spec.rb
@@ -130,17 +130,13 @@ RSpec.describe Bundler, "friendly errors" do
       # Does nothing
     end
 
-    context "Java::JavaLang::OutOfMemoryError" do
-      module Java
-        module JavaLang
-          class OutOfMemoryError < StandardError; end
-        end
-      end
-
+    context "Java::JavaLang::OutOfMemoryError", :jruby_only do
       it "Bundler.ui receive error" do
-        error = Java::JavaLang::OutOfMemoryError.new
-        expect(Bundler.ui).to receive(:error).with(/JVM has run out of memory/)
-        Bundler::FriendlyErrors.log_error(error)
+        install_gemfile <<-G, raise_on_error: false, env: { "JRUBY_OPTS" => "-J-Xmx32M" }, artifice: nil
+          source "https://gem.repo1"
+        G
+
+        expect(err).to include("JVM has run out of memory")
       end
     end
 
