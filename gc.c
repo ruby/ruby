ruby/ruby

commit bb180b87b43c45e17ff49735a26d7a188d5c8396

[Bug #21331] Prohibit modification during stlike loop

hash.c
@@ -880,7 +880,7 @@ ar_general_foreach(VALUE hash, st_foreach_check_callback_func *func, st_update_c
                 return 0;
               case ST_REPLACE:
                 if (replace) {
-                    retval = (*replace)(&key, &val, arg, TRUE);
+                    (*replace)(&key, &val, arg, TRUE);
 
                     // TODO: pair should be same as pair before.
                     pair = RHASH_AR_TABLE_REF(hash, i);
@@ -1404,26 +1404,84 @@ hash_foreach_ensure(VALUE hash)
     return 0;
 }
 
-int
-rb_hash_stlike_foreach(VALUE hash, st_foreach_callback_func *func, st_data_t arg)
+struct hash_stlike_foreach_arg {
+    VALUE hash;
+    st_foreach_callback_func *func;
+    VALUE arg;
+};
+
+static VALUE
+hash_stlike_foreach_call(VALUE args)
 {
+    struct hash_stlike_foreach_arg *argp = (void *)args;
+    VALUE hash = argp->hash;
+    st_foreach_callback_func *func = argp->func;
+    VALUE arg = argp->arg;
+    int ret;
+
     if (RHASH_AR_TABLE_P(hash)) {
-        return ar_foreach(hash, func, arg);
+        ret = ar_foreach(hash, func, arg);
     }
     else {
-        return st_foreach(RHASH_ST_TABLE(hash), func, arg);
+        ret = st_foreach(RHASH_ST_TABLE(hash), func, arg);
     }
+    return (VALUE)ret;
 }
 
 int
-rb_hash_stlike_foreach_with_replace(VALUE hash, st_foreach_check_callback_func *func, st_update_callback_func *replace, st_data_t arg)
+rb_hash_stlike_foreach(VALUE hash, st_foreach_callback_func *func, st_data_t arg)
 {
+    struct hash_stlike_foreach_arg args = {
+        .hash = hash,
+        .func = func,
+        .arg = arg,
+    };
+    hash_iter_lev_inc(hash);
+    VALUE ret = rb_ensure(hash_stlike_foreach_call, (VALUE)&args,
+                          hash_foreach_ensure, hash);
+    return (int)ret;
+}
+
+struct hash_stlike_foreach_with_replace_arg {
+    VALUE hash;
+    st_foreach_check_callback_func *func;
+    st_update_callback_func *replace;
+    VALUE arg;
+};
+
+static VALUE
+hash_stlike_foreach_with_replace_call(VALUE args)
+{
+    struct hash_stlike_foreach_with_replace_arg *argp = (void *)args;
+    VALUE hash = argp->hash;
+    st_foreach_check_callback_func *func = argp->func;
+    st_update_callback_func *replace = argp->replace;
+    VALUE arg = argp->arg;
+    int ret;
+
     if (RHASH_AR_TABLE_P(hash)) {
-        return ar_foreach_with_replace(hash, func, replace, arg);
+        ret = ar_foreach_with_replace(hash, func, replace, arg);
     }
     else {
-        return st_foreach_with_replace(RHASH_ST_TABLE(hash), func, replace, arg);
+        ret = st_foreach_with_replace(RHASH_ST_TABLE(hash), func, replace, arg);
     }
+    return (VALUE)ret;
+}
+
+int
+rb_hash_stlike_foreach_with_replace(VALUE hash, st_foreach_check_callback_func *func,
+                                    st_update_callback_func *replace, st_data_t arg)
+{
+    struct hash_stlike_foreach_with_replace_arg args = {
+        .hash = hash,
+        .func = func,
+        .replace = replace,
+        .arg = arg,
+    };
+    hash_iter_lev_inc(hash);
+    VALUE ret = rb_ensure(hash_stlike_foreach_with_replace_call, (VALUE)&args,
+                          hash_foreach_ensure, hash);
+    return (int)ret;
 }
 
 static VALUE

test/ruby/test_hash.rb
@@ -1853,6 +1853,14 @@ class TestHash < Test::Unit::TestCase
       end
     end
     assert_equal(@cls[a: 2, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10], x)
+
+    x = (1..1337).to_h {|k| [k, k]}
+    assert_raise_with_message(RuntimeError, /rehash during iteration/) do
+      x.transform_values! {|v|
+        x.rehash if v == 1337
+        v * 2
+      }
+    end
   end
 
   def hrec h, n, &b

commit 08d9e8ab20758194b8b701ba5e246ecaeb5edfb2

[Bug #21336] Stop using `rb_ensure` during GC for hash iteration
bb180b87b43c45e17ff49735a26d7a188d5c8396 introduced some `rb_ensure`
calls in `rb_hash_stlike_foreach{,with_replace}`. This caused
"Cannot malloc during GC" on Wasm because:
1. `rb_ensure` calls `xmalloc` on Wasm to allocate jump buffers
2. `rb_hash_stlike_foreach{,with_replace}` is called during GC to mark
   hash values

All `rb_hash_stlike_foreach{,with_replace}` calls during GC are known
not to raise exceptions, so we can safely use them without `rb_ensure`.
This commit adds `_no_ensure` variants of these functions and uses
them in the GC code to fix the Wasm issue.

Co-authored-by: Koichi Sasada <ko1@atdot.net>

gc.c
@@ -2675,10 +2675,10 @@ static void
 mark_hash(VALUE hash)
 {
     if (rb_hash_compare_by_id_p(hash)) {
-        rb_hash_stlike_foreach(hash, pin_key_mark_value, 0);
+        rb_hash_stlike_foreach_no_ensure(hash, pin_key_mark_value, 0);
     }
     else {
-        rb_hash_stlike_foreach(hash, mark_keyvalue, 0);
+        rb_hash_stlike_foreach_no_ensure(hash, mark_keyvalue, 0);
     }
 
     gc_mark_internal(RHASH(hash)->ifnone);
@@ -3536,7 +3536,7 @@ rb_gc_update_tbl_refs(st_table *ptr)
 static void
 gc_ref_update_hash(void *objspace, VALUE v)
 {
-    rb_hash_stlike_foreach_with_replace(v, hash_foreach_replace, hash_replace_ref, (st_data_t)objspace);
+    rb_hash_stlike_foreach_with_replace_no_ensure(v, hash_foreach_replace, hash_replace_ref, (st_data_t)objspace);
 }
 
 static void

hash.c
@@ -1442,6 +1442,18 @@ rb_hash_stlike_foreach(VALUE hash, st_foreach_callback_func *func, st_data_t arg
     return (int)ret;
 }
 
+int
+rb_hash_stlike_foreach_no_ensure(VALUE hash, st_foreach_callback_func *func, st_data_t arg)
+{
+    struct hash_stlike_foreach_arg args = {
+        .hash = hash,
+        .func = func,
+        .arg = arg,
+    };
+
+    return (int)hash_stlike_foreach_call((VALUE)&args);
+}
+
 struct hash_stlike_foreach_with_replace_arg {
     VALUE hash;
     st_foreach_check_callback_func *func;
@@ -1483,6 +1495,21 @@ rb_hash_stlike_foreach_with_replace(VALUE hash, st_foreach_check_callback_func *
                           hash_foreach_ensure, hash);
     return (int)ret;
 }
+
+int
+rb_hash_stlike_foreach_with_replace_no_ensure(VALUE hash, st_foreach_check_callback_func *func,
+                                              st_update_callback_func *replace, st_data_t arg)
+{
+    struct hash_stlike_foreach_with_replace_arg args = {
+        .hash = hash,
+        .func = func,
+        .replace = replace,
+        .arg = arg,
+    };
+
+    return (int)hash_stlike_foreach_with_replace_call((VALUE)&args);
+}
+
 
 static VALUE
 hash_foreach_call(VALUE arg)

internal/hash.h
@@ -86,6 +86,8 @@ int rb_hash_add_new_element(VALUE hash, VALUE key, VALUE val);
 VALUE rb_hash_set_pair(VALUE hash, VALUE pair);
 int rb_hash_stlike_delete(VALUE hash, st_data_t *pkey, st_data_t *pval);
 int rb_hash_stlike_foreach_with_replace(VALUE hash, st_foreach_check_callback_func *func, st_update_callback_func *replace, st_data_t arg);
+int rb_hash_stlike_foreach_no_ensure(VALUE hash, st_foreach_callback_func *func, st_data_t arg);
+int rb_hash_stlike_foreach_with_replace_no_ensure(VALUE hash, st_foreach_check_callback_func *func, st_update_callback_func *replace, st_data_t arg);
 int rb_hash_stlike_update(VALUE hash, st_data_t key, st_update_callback_func *func, st_data_t arg);
 bool rb_hash_default_unredefined(VALUE hash);
 VALUE rb_ident_hash_new_with_size(st_index_t size);