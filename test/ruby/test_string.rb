e7b9e3674100c6989b3da587fde7684cc33290db
Matheus Richard <matheusrichardt@gmail.com>
2025-05-16 16:33:11 -0300

    Add `String#ensure_suffix`
    Returns a new string with the given suffix appended if not present, or a
    duplicate of self if it already ends with suffix.
    
        "Hell".ensure_suffix("o!")   # => "Hello!"
        "Hello!".ensure_suffix("o!") # => "Hello!"

--- test/ruby/test_string.rb
+++ test/ruby/test_string.rb
@@ -3737,6 +3737,18 @@ CODE
     Warning[:deprecated] = deprecated
   end
 
+  def test_string_ensure_suffix
+    s = S("foobar")
+
+    assert_equal("foobarbaz", s.ensure_suffix("baz"))
+    assert_equal("foobar", s.ensure_suffix("bar"))
+    assert_equal("foobarrb", s.ensure_suffix("rb"))
+    assert_equal(false, s.ensure_suffix("bar").equal?(s))
+    assert_equal("foobarBAR", s.ensure_suffix("BAR"))
+    assert_equal("foobar", s.ensure_suffix(""))
+    assert_equal(false, s.ensure_suffix("").equal?(s))
+  end
+
   private
 
   def assert_bytesplice_result(expected, s, *args)
