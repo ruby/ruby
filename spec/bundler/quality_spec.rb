21a14ea2308d765e2860b6c4a33697c8c20a73ed
Matthew Hively <matthewhively@viz.com>
2025-05-22 12:26:31 -0700

    [rubygems/rubygems] Moved the REMEMBERING OPTIONS section to be after CONFIGURATION KEYS
    Since the remembering options are discouraged, the preferred method should be explained first.
    
    Slight tweak to wording
    
    Fix documentation spec test as per suggested patch
    
    https://github.com/rubygems/rubygems/commit/9f082ccf31

--- spec/bundler/quality_spec.rb
+++ spec/bundler/quality_spec.rb
@@ -165,7 +165,8 @@ RSpec.describe "The library itself" do
         line.scan(/Bundler\.settings\[:#{key_pattern}\]/).flatten.each {|s| all_settings[s] << "referenced at `#{filename}:#{number.succ}`" }
       end
     end
-    documented_settings = File.read("lib/bundler/man/bundle-config.1.ronn")[/LIST OF AVAILABLE KEYS.*/m].scan(/^\* `#{key_pattern}`/).flatten
+    settings_section = File.read("lib/bundler/man/bundle-config.1.ronn").split(/^## /).find {|section| section.start_with?("LIST OF AVAILABLE KEYS") }
+    documented_settings = settings_section.scan(/^\* `#{key_pattern}`/).flatten
 
     documented_settings.each do |s|
       all_settings.delete(s)
