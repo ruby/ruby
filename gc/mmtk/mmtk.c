38ecaca15544644e68eed8a0f5a86c8567ce9269
Peter Zhu <peter@peterzhu.ca>
2025-05-29 10:46:22 -0400

    [ruby/mmtk] Remove dependance on internal/object.h
    https://github.com/ruby/mmtk/commit/fdc13963f0

--- gc/mmtk/mmtk.c
+++ gc/mmtk/mmtk.c
@@ -4,7 +4,6 @@
 #include "ruby/assert.h"
 #include "ruby/atomic.h"
 #include "ruby/debug.h"
-#include "internal/object.h"
 
 #include "gc/gc.h"
 #include "gc/gc_impl.h"
@@ -1021,7 +1020,7 @@ rb_gc_impl_shutdown_call_finalizer(void *objspace_ptr)
 
         if (rb_gc_shutdown_call_finalizer_p(obj)) {
             rb_gc_obj_free(objspace_ptr, obj);
-            RBASIC_RESET_FLAGS(obj);
+            RBASIC(obj)->flags = 0;
         }
     }
     mmtk_free_raw_vec_of_obj_ref(registered_candidates);
