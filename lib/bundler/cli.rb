6efa02b7ee39d46a91b65fc0107eb2d72f8f8c02
Schneems <richard.schneeman+foo@gmail.com>
2025-05-29 11:11:48 -0500

    [rubygems/rubygems] Introduce `bundle list --format=json`
    The `bundle list` command is a convenient way for human to know what gems and versions are available. By introducing a `--format=json` option, we can provide the same information to machines in a stable format that is robust to UI additions or modifications. It indirectly supports  `Gemfile.lock` modifications by discouraging external tools from attempting to parse that format.
    
    This addition allows for the scripting of installation tools, such as buildpacks, that wish to branch logic based on gem versions. For example:
    
    ```ruby
    require "json"
    
    command = "bundle list --format=json"
    output = `#{command}`
    raise "Command `#{command}` errored: #{output}" unless $?.success?
    
    railties = JSON.parse(output).find {|gem| gem["name"] == railties }
    if railties && Gem::Version.new(railties["version"]) >= Gem::Version.new("7")
      puts "Using Rails greater than 7!"
    end
    ```
    
    The top level is an object with a single key, "gems", this structure allows us to add other information in the future (should we desire) without having to change the json schema.
    
    https://github.com/rubygems/rubygems/commit/9e081b0689

--- lib/bundler/cli.rb
+++ lib/bundler/cli.rb
@@ -299,6 +299,7 @@ module Bundler
     method_option "name-only", type: :boolean, banner: "print only the gem names"
     method_option "only-group", type: :array, default: [], banner: "print gems from a given set of groups"
     method_option "without-group", type: :array, default: [], banner: "print all gems except from a given set of groups"
+    method_option "format", type: :string, banner: "format output ('json' is the only supported format)"
     method_option "paths", type: :boolean, banner: "print the path to each gem in the bundle"
     def list
       require_relative "cli/list"
