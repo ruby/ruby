c6c51569cb29a70aa086523d7508f88e645c72ff
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-06-03 15:59:25 +0900

    [ruby/io-console] Alias value or join to take in old Ruby
    https://github.com/ruby/io-console/commit/7106d05219

--- test/io/console/test_ractor.rb
+++ test/io/console/test_ractor.rb
@@ -8,6 +8,10 @@ class TestIOConsoleInRactor < Test::Unit::TestCase
     path = $".find {|path| path.end_with?(ext)}
     assert_in_out_err(%W[-r#{path}], "#{<<~"begin;"}\n#{<<~'end;'}", ["true"], [])
     begin;
+      class Ractor
+        alias value take
+      end unless Ractor.method_defined? :value # compat with Ruby 3.4 and olders
+
       $VERBOSE = nil
       r = Ractor.new do
         $stdout.console_mode
@@ -23,6 +27,10 @@ class TestIOConsoleInRactor < Test::Unit::TestCase
 
     assert_in_out_err(%W[-r#{path}], "#{<<~"begin;"}\n#{<<~'end;'}", ["true"], [])
     begin;
+      class Ractor
+        alias value take
+      end unless Ractor.method_defined? :value # compat with Ruby 3.4 and olders
+
       console = IO.console
       $VERBOSE = nil
       r = Ractor.new do
