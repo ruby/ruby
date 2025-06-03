6609ba05141a8e95e4c0b7db2579d85b1e35d074
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-06-03 16:00:58 +0900

    [ruby/io-wait] Alias value or join to take in old Ruby
    https://github.com/ruby/io-wait/commit/cf84aea70d

--- test/io/wait/test_ractor.rb
+++ test/io/wait/test_ractor.rb
@@ -7,6 +7,10 @@ class TestIOWaitInRactor < Test::Unit::TestCase
     ext = "/io/wait.#{RbConfig::CONFIG['DLEXT']}"
     path = $".find {|path| path.end_with?(ext)}
     assert_in_out_err(%W[-r#{path}], <<-"end;", ["true"], [])
+      class Ractor
+        alias value take
+      end unless Ractor.method_defined? :value # compat with Ruby 3.4 and olders
+
       $VERBOSE = nil
       r = Ractor.new do
         $stdout.equal?($stdout.wait_writable)
