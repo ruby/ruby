7ddc5e6187fdf531fe5e92f01bb4624c06bddda2
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-06-04 13:49:16 +0900

    Suppress overflow warning at `ALLOC_N` in `iseq_set_local_table`
    `xmalloc2` checks the overflow of arguments multiplication.

--- compile.c
+++ compile.c
@@ -2184,7 +2184,10 @@ iseq_set_local_table(rb_iseq_t *iseq, const rb_ast_id_table_t *tbl, const NODE *
     }
 
     if (size > 0) {
-        ID *ids = (ID *)ALLOC_N(ID, size);
+#if SIZEOF_INT >= SIZEOF_SIZE_T
+        ASSUME(size < SIZE_MAX / sizeof(ID)); /* checked in xmalloc2_size */
+#endif
+        ID *ids = ALLOC_N(ID, size);
         MEMCPY(ids, tbl->ids + offset, ID, size);
         ISEQ_BODY(iseq)->local_table = ids;
     }
