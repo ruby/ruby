a84f734343e4f564b6645f56b0c0c91c07235172
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-06-04 12:47:09 +0900

    Added warning for CGI.parse

--- lib/cgi/util.rb
+++ lib/cgi/util.rb
@@ -3,4 +3,5 @@
 require "cgi/escape"
 warn <<-WARNING, uplevel: Gem::BUNDLED_GEMS.uplevel if $VERBOSE
 CGI::Util is removed from Ruby 3.5. Please use cgi/escape instead for CGI.escape and CGI.unescape features.
+If you are using CGI.parse, please install and use the cgi gem instead.
 WARNING
