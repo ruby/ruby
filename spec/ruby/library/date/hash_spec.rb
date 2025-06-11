bfd14d7c3308a039a962d2c68a55b8f24364745d
Dmitry Dygalo <dmitry.dygalo@workato.com>
2025-06-11 15:17:09 +0200

    fix: Date#hash for large years
    Signed-off-by: Dmitry Dygalo <dmitry.dygalo@workato.com>

--- spec/ruby/library/date/hash_spec.rb
+++ spec/ruby/library/date/hash_spec.rb
@@ -4,5 +4,6 @@ require 'date'
 describe "Date#hash" do
   it "returns the same value for equal dates" do
     Date.civil(2004, 7, 12).hash.should == Date.civil(2004, 7, 12).hash
+    Date.civil(3171505571716611468830131104691, 2, 19).hash.should == Date.civil(3171505571716611468830131104691, 2, 19).hash
   end
 end
