d5c8fd2043f4425c3fe2a87558dfbd80ebff9911
nagachika <nagachika@ruby-lang.org>
2025-06-14 11:39:08 +0900

    merge revision(s) f6cbf499bc98b851034fffb49fcbb59d495f6f7b: [Backport #21354]
    	Fix Symbol#to_proc (rb_sym_to_proc) to be ractor safe
    
    	In non-main ractors, don't use `sym_proc_cache`. It is not thread-safe
    	to add to this array without a lock and also it leaks procs from one
    	ractor to another. Instead, we create a new proc each time. If this
    	results in poor performance we can come up with a solution later.
    
    	Fixes [Bug #21354]

--- bootstraptest/test_ractor.rb
+++ bootstraptest/test_ractor.rb
@@ -1773,3 +1773,15 @@ assert_equal 'ok', %q{
 }
 
 end # if !ENV['GITHUB_WORKFLOW']
+
+# Using Symbol#to_proc inside ractors
+# [Bug #21354]
+assert_equal 'ok', %q{
+  :inspect.to_proc
+  Ractor.new do
+    # It should not use this cached proc, it should create a new one. If it used
+    # the cached proc, we would get a ractor_confirm_belonging error here.
+    :inspect.to_proc
+  end.take
+  'ok'
+}
