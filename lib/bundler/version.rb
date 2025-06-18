18618810a20bcacc98054b54c478fe423c059a2c
David Rodríguez <deivid.rodriguez@riseup.net>
2025-06-18 20:37:01 +0200

    [rubygems/rubygems] Use  Gem::Version` methods instead of string splitting
    https://github.com/rubygems/rubygems/commit/75fed35264

--- lib/bundler/version.rb
+++ lib/bundler/version.rb
@@ -4,7 +4,7 @@ module Bundler
   VERSION = (ENV["BUNDLER_4_MODE"] == "true" ? "4.0.0" : "2.7.0.dev").freeze
 
   def self.bundler_major_version
-    @bundler_major_version ||= VERSION.split(".").first.to_i
+    @bundler_major_version ||= gem_version.segments.first
   end
 
   def self.gem_version
