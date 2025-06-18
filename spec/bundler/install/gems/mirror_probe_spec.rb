7c115b8633cd32def0f6b4bd4e33b4b2db864b62
David Rodríguez <deivid.rodriguez@riseup.net>
2025-06-18 11:22:54 +0200

    [rubygems/rubygems] Fix flaky test failures in mirror probing specs
    The mirror probing spec file was moved to our regular suite, which runs
    in parallel, recently. These specs rely on starting and stopping actual
    servers in localhost, but this does not play nice with parallelization,
    because a spec may kill the webserver another spec has created.
    
    This commit moves mirror probing specs to not need to start servers in
    localhost and do something more similar to what the other specs do.
    
    https://github.com/rubygems/rubygems/commit/ca9a19706f

--- spec/bundler/install/gems/mirror_probe_spec.rb
+++ spec/bundler/install/gems/mirror_probe_spec.rb
@@ -1,33 +1,23 @@
 # frozen_string_literal: true
 
 RSpec.describe "fetching dependencies with a not available mirror" do
-  let(:host) { "127.0.0.1" }
-
   before do
-    require_rack_test
-    setup_server
-    setup_mirror
-  end
+    build_repo2
 
-  after do
-    Artifice.deactivate
-    @server_thread.kill
-    @server_thread.join
+    gemfile <<-G
+      source "https://gem.repo2"
+      gem 'weakling'
+    G
   end
 
   context "with a specific fallback timeout" do
     before do
-      global_config("BUNDLE_MIRROR__HTTP://127__0__0__1:#{@server_port}/__FALLBACK_TIMEOUT/" => "true",
-                    "BUNDLE_MIRROR__HTTP://127__0__0__1:#{@server_port}/" => @mirror_uri)
+      global_config("BUNDLE_MIRROR__HTTPS://GEM__REPO2/__FALLBACK_TIMEOUT/" => "true",
+                    "BUNDLE_MIRROR__HTTPS://GEM__REPO2/" => "https://gem.mirror")
     end
 
     it "install a gem using the original uri when the mirror is not responding" do
-      gemfile <<-G
-        source "#{@server_uri}"
-        gem 'weakling'
-      G
-
-      bundle :install, artifice: nil
+      bundle :install, env: { "BUNDLER_SPEC_FAKE_RESOLVE" => "gem.mirror" }, verbose: true
 
       expect(out).to include("Installing weakling")
       expect(out).to include("Bundle complete")
@@ -38,16 +28,11 @@ RSpec.describe "fetching dependencies with a not available mirror" do
   context "with a global fallback timeout" do
     before do
       global_config("BUNDLE_MIRROR__ALL__FALLBACK_TIMEOUT/" => "1",
-                    "BUNDLE_MIRROR__ALL" => @mirror_uri)
+                    "BUNDLE_MIRROR__ALL" => "https://gem.mirror")
     end
 
     it "install a gem using the original uri when the mirror is not responding" do
-      gemfile <<-G
-        source "#{@server_uri}"
-        gem 'weakling'
-      G
-
-      bundle :install, artifice: nil
+      bundle :install, env: { "BUNDLER_SPEC_FAKE_RESOLVE" => "gem.mirror" }
 
       expect(out).to include("Installing weakling")
       expect(out).to include("Bundle complete")
@@ -57,73 +42,27 @@ RSpec.describe "fetching dependencies with a not available mirror" do
 
   context "with a specific mirror without a fallback timeout" do
     before do
-      global_config("BUNDLE_MIRROR__HTTP://127__0__0__1:#{@server_port}/" => @mirror_uri)
+      global_config("BUNDLE_MIRROR__HTTPS://GEM__REPO2/" => "https://gem.mirror")
     end
 
     it "fails to install the gem with a timeout error when the mirror is not responding" do
-      gemfile <<-G
-        source "#{@server_uri}"
-        gem 'weakling'
-      G
+      bundle :install, artifice: "compact_index_mirror_down", raise_on_error: false
 
-      bundle :install, artifice: nil, raise_on_error: false
-
-      expect(out).to include("Fetching source index from #{@mirror_uri}")
-
-      err_lines = err.split("\n")
-      expect(err_lines).to include(%r{\ARetrying fetcher due to error \(2/4\): Bundler::HTTPError Could not fetch specs from #{@mirror_uri}/ due to underlying error <})
-      expect(err_lines).to include(%r{\ARetrying fetcher due to error \(3/4\): Bundler::HTTPError Could not fetch specs from #{@mirror_uri}/ due to underlying error <})
-      expect(err_lines).to include(%r{\ARetrying fetcher due to error \(4/4\): Bundler::HTTPError Could not fetch specs from #{@mirror_uri}/ due to underlying error <})
-      expect(err_lines).to include(%r{\ACould not fetch specs from #{@mirror_uri}/ due to underlying error <})
+      expect(out).to be_empty
+      expect(err).to eq("Could not reach host gem.mirror. Check your network connection and try again.")
     end
   end
 
   context "with a global mirror without a fallback timeout" do
     before do
-      global_config("BUNDLE_MIRROR__ALL" => @mirror_uri)
+      global_config("BUNDLE_MIRROR__ALL" => "https://gem.mirror")
     end
 
     it "fails to install the gem with a timeout error when the mirror is not responding" do
-      gemfile <<-G
-        source "#{@server_uri}"
-        gem 'weakling'
-      G
+      bundle :install, artifice: "compact_index_mirror_down", raise_on_error: false
 
-      bundle :install, artifice: nil, raise_on_error: false
-
-      expect(out).to include("Fetching source index from #{@mirror_uri}")
-
-      err_lines = err.split("\n")
-      expect(err_lines).to include(%r{\ARetrying fetcher due to error \(2/4\): Bundler::HTTPError Could not fetch specs from #{@mirror_uri}/ due to underlying error <})
-      expect(err_lines).to include(%r{\ARetrying fetcher due to error \(3/4\): Bundler::HTTPError Could not fetch specs from #{@mirror_uri}/ due to underlying error <})
-      expect(err_lines).to include(%r{\ARetrying fetcher due to error \(4/4\): Bundler::HTTPError Could not fetch specs from #{@mirror_uri}/ due to underlying error <})
-      expect(err_lines).to include(%r{\ACould not fetch specs from #{@mirror_uri}/ due to underlying error <})
+      expect(out).to be_empty
+      expect(err).to eq("Could not reach host gem.mirror. Check your network connection and try again.")
     end
-  end
-
-  def setup_server
-    @server_port = find_unused_port
-    @server_uri = "http://#{host}:#{@server_port}"
-
-    require_relative "../../support/artifice/compact_index"
-    require_relative "../../support/silent_logger"
-
-    require "rackup/server"
-
-    @server_thread = Thread.new do
-      Rackup::Server.start(app: CompactIndexAPI,
-                           Host: host,
-                           Port: @server_port,
-                           server: "webrick",
-                           AccessLog: [],
-                           Logger: Spec::SilentLogger.new)
-    end.run
-
-    wait_for_server(host, @server_port)
-  end
-
-  def setup_mirror
-    @mirror_port = find_unused_port
-    @mirror_uri = "http://#{host}:#{@mirror_port}"
   end
 end
