41242560b621d076e0695d3eea77b803c1d03146
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-06-29 14:07:10 +0900

    * adjust indents. [ci skip]

--- vm_insnhelper.c
+++ vm_insnhelper.c
@@ -976,7 +976,8 @@ rb_vm_rewrite_cref(rb_cref_t *cref, VALUE old_klass, VALUE new_klass)
     #define ADD_NEW_CREF(new_cref) \
         if (new_cref_tail) { \
             RB_OBJ_WRITE(new_cref_tail, &new_cref_tail->next, new_cref); \
-        } else { \
+        } \
+        else { \
             new_cref_head = new_cref; \
         } \
         new_cref_tail = new_cref;
