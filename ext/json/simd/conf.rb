a9e2a818bd8dd8787d3e211c0384725125b9111c
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-06-30 21:38:56 +0900

    [ruby/json] Refactor simd/conf.rb - balance
    Align code for arm and x86_64 in parallel.
    
    https://github.com/ruby/json/commit/2211e30a59

--- ext/json/simd/conf.rb
+++ ext/json/simd/conf.rb
@@ -1,4 +1,5 @@
-if RbConfig::CONFIG['host_cpu'] =~ /^(arm.*|aarch64.*)/
+case RbConfig::CONFIG['host_cpu']
+when /^(arm|aarch64)/
   # Try to compile a small program using NEON instructions
   if have_header('arm_neon.h')
     have_type('uint8x16_t', headers=['arm_neon.h']) && try_compile(<<~'SRC')
@@ -8,20 +9,20 @@ if RbConfig::CONFIG['host_cpu'] =~ /^(arm.*|aarch64.*)/
           if (argc > 100000) printf("%p", &test);
           return 0;
       }
-    SRC
-      $defs.push("-DJSON_ENABLE_SIMD")
-  end
-end
-
-if have_header('x86intrin.h') && have_type('__m128i', headers=['x86intrin.h']) && try_compile(<<~'SRC')
-  #include <x86intrin.h>
-  int main(int argc, char **argv) {
-      __m128i test = _mm_set1_epi8(32);
-      if (argc > 100000) printf("%p", &test);
-      return 0;
-  }
-  SRC
+      SRC
     $defs.push("-DJSON_ENABLE_SIMD")
+  end
+when /^(x86_64|x64)/
+  if have_header('x86intrin.h') && have_type('__m128i', headers=['x86intrin.h']) && try_compile(<<~'SRC')
+      #include <x86intrin.h>
+      int main(int argc, char **argv) {
+          __m128i test = _mm_set1_epi8(32);
+          if (argc > 100000) printf("%p", &test);
+          return 0;
+      }
+      SRC
+    $defs.push("-DJSON_ENABLE_SIMD")
+  end
 end
 
 have_header('cpuid.h')
