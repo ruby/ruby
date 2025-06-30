60eb1d5d233a753aaa2d60b305caf1909192b55a
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-06-30 21:37:20 +0900

    [ruby/json] Refactor simd/conf.rb - compiler warnings
    Suppress warnings for old style function definition and unused
    variable.
    
    https://github.com/ruby/json/commit/58dc0aa938

--- ext/json/simd/conf.rb
+++ ext/json/simd/conf.rb
@@ -3,8 +3,9 @@ if RbConfig::CONFIG['host_cpu'] =~ /^(arm.*|aarch64.*)/
   if have_header('arm_neon.h')
     have_type('uint8x16_t', headers=['arm_neon.h']) && try_compile(<<~'SRC')
       #include <arm_neon.h>
-      int main() {
+      int main(int argc, char **argv) {
           uint8x16_t test = vdupq_n_u8(32);
+          if (argc > 100000) printf("%p", &test);
           return 0;
       }
     SRC
@@ -14,8 +15,9 @@ end
 
 if have_header('x86intrin.h') && have_type('__m128i', headers=['x86intrin.h']) && try_compile(<<~'SRC')
   #include <x86intrin.h>
-  int main() {
+  int main(int argc, char **argv) {
       __m128i test = _mm_set1_epi8(32);
+      if (argc > 100000) printf("%p", &test);
       return 0;
   }
   SRC
