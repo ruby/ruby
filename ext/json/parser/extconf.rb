9d080765cc3c6266521863ffe5882ba8d8322271
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-07-01 17:38:25 +0900

    [ruby/json] Run `have_func` with the header providing the declarations
    https://github.com/ruby/json/commit/95fb084027

--- ext/json/parser/extconf.rb
+++ ext/json/parser/extconf.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'mkmf'
 
-have_func("rb_enc_interned_str", "ruby.h") # RUBY_VERSION >= 3.0
+have_func("rb_enc_interned_str", "ruby/encoding.h") # RUBY_VERSION >= 3.0
 have_func("rb_hash_new_capa", "ruby.h") # RUBY_VERSION >= 3.2
 have_func("rb_hash_bulk_insert", "ruby.h") # Missing on TruffleRuby
 have_func("strnlen", "string.h") # Missing on Solaris 10
