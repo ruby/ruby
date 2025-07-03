84a5ccb486f7e3cc3d289a0706174b9fdfdbfb4e
Takashi Kokubun <takashikkbn@gmail.com>
2025-07-03 10:14:56 -0700

    ZJIT: Panic on BOP redefinition only when needed

--- zjit/src/invariants.rs
+++ zjit/src/invariants.rs
@@ -1,6 +1,6 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-use crate::{cruby::{ruby_basic_operators, IseqPtr, RedefinitionFlag}, state::ZJITState, state::zjit_enabled_p};
+use crate::{cruby::{ruby_basic_operators, IseqPtr, RedefinitionFlag}, state::{zjit_enabled_p, ZJITState}, virtualmem::CodePtr};
 
 /// Used to track all of the various block references that contain assumptions
 /// about the state of the virtual machine.
@@ -11,19 +11,28 @@ pub struct Invariants {
 
     /// Set of ISEQs whose JIT code assumes that it doesn't escape EP
     no_ep_escape_iseqs: HashSet<IseqPtr>,
+
+    /// Map from a class and its associated basic operator to a set of patch points
+    bop_patch_points: HashMap<(RedefinitionFlag, ruby_basic_operators), HashSet<CodePtr>>,
 }
 
 /// Called when a basic operator is redefined. Note that all the blocks assuming
 /// the stability of different operators are invalidated together and we don't
 /// do fine-grained tracking.
 #[unsafe(no_mangle)]
-pub extern "C" fn rb_zjit_bop_redefined(_klass: RedefinitionFlag, _bop: ruby_basic_operators) {
+pub extern "C" fn rb_zjit_bop_redefined(klass: RedefinitionFlag, bop: ruby_basic_operators) {
     // If ZJIT isn't enabled, do nothing
     if !zjit_enabled_p() {
         return;
     }
 
-    unimplemented!("Invalidation on BOP redefinition is not implemented yet");
+    let invariants = ZJITState::get_invariants();
+    if let Some(code_ptrs) = invariants.bop_patch_points.get(&(klass, bop)) {
+        // Invalidate all patch points for this BOP
+        for &ptr in code_ptrs {
+            unimplemented!("Invalidation on BOP redefinition is not implemented yet: {ptr:?}");
+        }
+    }
 }
 
 /// Invalidate blocks for a given ISEQ that assumes environment pointer is
@@ -56,4 +65,10 @@ pub fn track_no_ep_escape_assumption(iseq: IseqPtr) {
 /// Returns true if a given ISEQ has previously escaped environment pointer.
 pub fn iseq_escapes_ep(iseq: IseqPtr) -> bool {
     ZJITState::get_invariants().ep_escape_iseqs.contains(&iseq)
+}
+
+/// Track a patch point for a basic operator in a given class.
+pub fn track_bop_assumption(klass: RedefinitionFlag, bop: ruby_basic_operators, code_ptr: CodePtr) {
+    let invariants = ZJITState::get_invariants();
+    invariants.bop_patch_points.entry((klass, bop)).or_default().insert(code_ptr);
 }
