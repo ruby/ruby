d4621b42f2dea9ec34097027c9b66144e85e0d11
Kazuki Yamaguchi <k@rhe.jp>
2025-07-11 01:14:43 +0900

    [ruby/openssl] test/openssl/test_ossl.rb: fix style issues
    Use OpenSSL::TestCase instead of OpenSSL::SSLTestCase.
    
    Prefer assert_true and assert_false over the bare assert and refute.
    OpenSSL.fixed_length_secure_compare and OpenSSL.secure_compare will
    only return true or false, and it should be checked.
    
    https://github.com/ruby/openssl/commit/3d9938ed40

--- test/openssl/test_ossl.rb
+++ test/openssl/test_ossl.rb
@@ -3,42 +3,42 @@ require_relative "utils"
 
 if defined?(OpenSSL)
 
-class OpenSSL::OSSL < OpenSSL::SSLTestCase
+class OpenSSL::TestOSSL < OpenSSL::TestCase
   def test_fixed_length_secure_compare
     assert_raise(ArgumentError) { OpenSSL.fixed_length_secure_compare("aaa", "a") }
     assert_raise(ArgumentError) { OpenSSL.fixed_length_secure_compare("aaa", "aa") }
 
-    assert OpenSSL.fixed_length_secure_compare("aaa", "aaa")
-    assert OpenSSL.fixed_length_secure_compare(
+    assert_true(OpenSSL.fixed_length_secure_compare("aaa", "aaa"))
+    assert_true(OpenSSL.fixed_length_secure_compare(
       OpenSSL::Digest.digest('SHA256', "aaa"), OpenSSL::Digest::SHA256.digest("aaa")
-    )
+    ))
 
     assert_raise(ArgumentError) { OpenSSL.fixed_length_secure_compare("aaa", "aaaa") }
-    refute OpenSSL.fixed_length_secure_compare("aaa", "baa")
-    refute OpenSSL.fixed_length_secure_compare("aaa", "aba")
-    refute OpenSSL.fixed_length_secure_compare("aaa", "aab")
+    assert_false(OpenSSL.fixed_length_secure_compare("aaa", "baa"))
+    assert_false(OpenSSL.fixed_length_secure_compare("aaa", "aba"))
+    assert_false(OpenSSL.fixed_length_secure_compare("aaa", "aab"))
     assert_raise(ArgumentError) { OpenSSL.fixed_length_secure_compare("aaa", "aaab") }
     assert_raise(ArgumentError) { OpenSSL.fixed_length_secure_compare("aaa", "b") }
     assert_raise(ArgumentError) { OpenSSL.fixed_length_secure_compare("aaa", "bb") }
-    refute OpenSSL.fixed_length_secure_compare("aaa", "bbb")
+    assert_false(OpenSSL.fixed_length_secure_compare("aaa", "bbb"))
     assert_raise(ArgumentError) { OpenSSL.fixed_length_secure_compare("aaa", "bbbb") }
   end
 
   def test_secure_compare
-    refute OpenSSL.secure_compare("aaa", "a")
-    refute OpenSSL.secure_compare("aaa", "aa")
+    assert_false(OpenSSL.secure_compare("aaa", "a"))
+    assert_false(OpenSSL.secure_compare("aaa", "aa"))
 
-    assert OpenSSL.secure_compare("aaa", "aaa")
+    assert_true(OpenSSL.secure_compare("aaa", "aaa"))
 
-    refute OpenSSL.secure_compare("aaa", "aaaa")
-    refute OpenSSL.secure_compare("aaa", "baa")
-    refute OpenSSL.secure_compare("aaa", "aba")
-    refute OpenSSL.secure_compare("aaa", "aab")
-    refute OpenSSL.secure_compare("aaa", "aaab")
-    refute OpenSSL.secure_compare("aaa", "b")
-    refute OpenSSL.secure_compare("aaa", "bb")
-    refute OpenSSL.secure_compare("aaa", "bbb")
-    refute OpenSSL.secure_compare("aaa", "bbbb")
+    assert_false(OpenSSL.secure_compare("aaa", "aaaa"))
+    assert_false(OpenSSL.secure_compare("aaa", "baa"))
+    assert_false(OpenSSL.secure_compare("aaa", "aba"))
+    assert_false(OpenSSL.secure_compare("aaa", "aab"))
+    assert_false(OpenSSL.secure_compare("aaa", "aaab"))
+    assert_false(OpenSSL.secure_compare("aaa", "b"))
+    assert_false(OpenSSL.secure_compare("aaa", "bb"))
+    assert_false(OpenSSL.secure_compare("aaa", "bbb"))
+    assert_false(OpenSSL.secure_compare("aaa", "bbbb"))
   end
 
   def test_memcmp_timing
