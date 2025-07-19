a9a9b1504af4b8c44469b188124931834fb3582f
André <andre.soares@altxtech.net>
2025-07-19 11:59:29 -0300

    Make autogen more readable

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
+# Clear PWD to force commands to recompute working directory
 PWD=
-case "$0" in
-*/*) srcdir=`dirname $0`;;
-*) srcdir="";;
-esac
 
-symlink='--install --symlink'
-case " $* " in
-    *" -i "*|*" --install "*)
-        # reset to copy missing standard auxiliary files, instead of symlinks
-        symlink=
-        ;;
-esac
+# Figure out the source directory for this script
+# configure.ac should be in the same place
+srcdir() {
+  case "$0" in
+    */* ) dirname "$0" ;;
+    *   ) echo ""    ;;
+  esac
+}
 
-exec ${AUTORECONF:-autoreconf} ${symlink} "$@" ${srcdir:+"$srcdir"}
+# If install only is explicitly requested, disbale symlink flags
+symlink_flags() {
+  case " $* " in
+    *" -i "* | *" --install"* ) echo "" ;;
+    *                         ) echo "--install --symlink" ;;
+  esac
+}
+
+exec ${AUTORECONF:-autoreconf} \
+     $(symlink_flags "$@") \
+     "$@" \
+     $(srcdir)
