d488935910d8902e96ea5c49537506f0b2aba492
Jean Boussier <jean.boussier@gmail.com>
2025-07-28 11:18:54 +0200

    Get rid of ID_JUNK
    It has been aliased as ID_INTERNAL for a long time and that alias
    is much more descriptive.

--- symbol.h
+++ symbol.h
@@ -39,7 +39,7 @@ struct RSymbol {
 #define is_attrset_id(id) ((id)==idASET||id_type(id)==ID_ATTRSET)
 #define is_const_id(id) (id_type(id)==ID_CONST)
 #define is_class_id(id) (id_type(id)==ID_CLASS)
-#define is_junk_id(id) (id_type(id)==ID_JUNK)
+#define is_internal_id(id) (id_type(id)==ID_INTERNAL)
 
 static inline int
 id_type(ID id)
@@ -91,7 +91,7 @@ sym_type(VALUE sym)
 #define is_attrset_sym(sym) (sym_type(sym)==ID_ATTRSET)
 #define is_const_sym(sym) (sym_type(sym)==ID_CONST)
 #define is_class_sym(sym) (sym_type(sym)==ID_CLASS)
-#define is_junk_sym(sym) (sym_type(sym)==ID_JUNK)
+#define is_internal_sym(sym) (sym_type(sym)==ID_INTERNAL)
 
 #ifndef RIPPER
 RUBY_FUNC_EXPORTED
