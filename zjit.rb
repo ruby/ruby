3d58c8a5ce71d7a5307850ac2283ccb1d9147b3e
Takashi Kokubun <takashikkbn@gmail.com>
2025-07-29 09:19:20 -0700

    Build a string buffer

--- zjit.rb
+++ zjit.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
+# This module allows for introspection of \ZJIT, CRuby's just-in-time compiler.
+# Everything in the module is highly implementation specific and the API might
+# be less stable compared to the standard library.
+#
+# This module may not exist if \ZJIT does not support the particular platform
+# for which CRuby is built.
 module RubyVM::ZJIT
   # Avoid calling a Ruby method here to avoid interfering with compilation tests
   if Primitive.rb_zjit_stats_enabled_p
@@ -20,11 +28,22 @@ class << RubyVM::ZJIT
 
   # Get the summary of ZJIT statistics as a String
   def stats_string
-    # Lazily require StringIO to avoid breaking miniruby
-    require 'stringio'
-    strio = StringIO.new
-    print_stats(out: strio)
-    strio.string
+    buf = +''
+    stats = self.stats
+
+    [
+      :total_insns_count,
+      :vm_insns_count,
+      :zjit_insns_count,
+      :ratio_in_zjit,
+    ].each do |key|
+      value = stats[key]
+      if key == :ratio_in_zjit
+        value = '%0.1f%%' % value
+      end
+      buf << "#{'%-18s' % "#{key}:"} #{value}\n"
+    end
+    buf
   end
 
   # Assert that any future ZJIT compilation will return a function pointer
@@ -36,19 +55,7 @@ class << RubyVM::ZJIT
   private
 
   # Print ZJIT stats
-  def print_stats(out: $stderr)
-    stats = self.stats
-    [
-      :total_insns_count,
-      :vm_insns_count,
-      :zjit_insns_count,
-      :ratio_in_zjit,
-    ].each do |key|
-      value = stats[key]
-      if key == :ratio_in_zjit
-        value = '%0.1f%%' % value
-      end
-      out.puts "#{'%-18s' % "#{key}:"} #{value}"
-    end
+  def print_stats
+    $stderr.write stats_string
   end
 end
