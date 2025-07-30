e980143bc744d68efff2f945bd9ada3d0de00255
Takashi Kokubun <takashikkbn@gmail.com>
2025-07-30 10:10:12 -0700

    Attach an issue

--- jit_hook.rb
+++ jit_hook.rb
@@ -3,7 +3,7 @@ class Module
   # This method is removed in jit_undef.rb.
   private def with_jit(&block) # :nodoc:
     # ZJIT currently doesn't compile Array#each properly, so it's disabled for now.
-    if defined?(RubyVM::ZJIT) && Primitive.rb_zjit_option_enabled_p && false # TODO: remove `&& false`
+    if defined?(RubyVM::ZJIT) && Primitive.rb_zjit_option_enabled_p && false # TODO: remove `&& false` (Shopify/ruby#667)
       # We don't support lazily enabling ZJIT yet, so we can call the block right away.
       block.call
     elsif defined?(RubyVM::YJIT)
