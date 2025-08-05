052b38a5d9ca4d0ab2ad872bf896fe256a0186f5
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-08-05 18:45:25 +0900

    [rubygems/rubygems] Deprecate Gem::Specification#datadir and will remove it at RG 4.1
    https://github.com/rubygems/rubygems/commit/e99cdab171

--- test/rubygems/test_config.rb
+++ test/rubygems/test_config.rb
@@ -5,13 +5,6 @@ require "rubygems"
 require "shellwords"
 
 class TestGemConfig < Gem::TestCase
-  def test_datadir
-    util_make_gems
-    spec = Gem::Specification.find_by_name("a")
-    spec.activate
-    assert_equal "#{spec.full_gem_path}/data/a", spec.datadir
-  end
-
   def test_good_rake_path_is_escaped
     path = Gem::TestCase.class_variable_get(:@@good_rake)
     ruby, rake = path.shellsplit
