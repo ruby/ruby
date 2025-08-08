8d5f00c5371ff71f33ef57b1419fd8d4b1aa9074
David Rodríguez <deivid.rodriguez@riseup.net>
2025-08-08 12:42:16 +0200

    [rubygems/rubygems] Fix Bundler printing more flags than actually passed in verbose mode
    This reverts commit https://github.com/rubygems/rubygems/commit/bea87eab0b17 and adds a
    regression spec for it.
    
    https://github.com/rubygems/rubygems/commit/ac98107864

--- lib/bundler/cli.rb
+++ lib/bundler/cli.rb
@@ -714,7 +714,12 @@ module Bundler
       command_name = cmd.name
       return if PARSEABLE_COMMANDS.include?(command_name)
       command = ["bundle", command_name] + args
-      command << Thor::Options.to_switches(options.sort_by(&:first)).strip
+      options_to_print = options.dup
+      options_to_print.delete_if do |k, v|
+        next unless o = cmd.options[k]
+        o.default == v
+      end
+      command << Thor::Options.to_switches(options_to_print.sort_by(&:first)).strip
       command.reject!(&:empty?)
       Bundler.ui.info "Running `#{command * " "}` with bundler #{Bundler.verbose_version}"
     end
