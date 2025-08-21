c37668406930f5fa5df5045df3310d3bf0cbe117
John Hawthorn <john@hawthorn.email>
2025-08-20 17:47:17 -0700

    Only set cached flag when unset
    The same CME may be reused in multiple caches, so once the CACHED flag
    is set, other Ractors may be checking for it being invalidated and we
    should avoid writing to the field again.

--- method.h
+++ method.h
@@ -73,9 +73,15 @@ typedef struct rb_callable_method_entry_struct { /* same fields with rb_method_e
 #define METHOD_ENTRY_COMPLEMENTED(me)        ((me)->flags & IMEMO_FL_USER3)
 #define METHOD_ENTRY_COMPLEMENTED_SET(me)    ((me)->flags |= IMEMO_FL_USER3)
 #define METHOD_ENTRY_CACHED(me)              ((me)->flags & IMEMO_FL_USER4)
-#define METHOD_ENTRY_CACHED_SET(me)          ((me)->flags |= IMEMO_FL_USER4)
 #define METHOD_ENTRY_INVALIDATED(me)         ((me)->flags & IMEMO_FL_USER5)
 #define METHOD_ENTRY_INVALIDATED_SET(me)     ((me)->flags |= IMEMO_FL_USER5)
+
+static inline void
+METHOD_ENTRY_CACHED_SET(rb_callable_method_entry_t *me) {
+    if (!METHOD_ENTRY_CACHED(me)) {
+        me->flags |= IMEMO_FL_USER4;
+    }
+}
 
 static inline void
 METHOD_ENTRY_VISI_SET(rb_method_entry_t *me, rb_method_visibility_t visi)
