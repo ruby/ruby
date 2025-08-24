a099bd74a8862382a464bfd2f8ccfe75b26845b5
BurdetteLamar <burdettelamar@yahoo.com>
2025-08-24 09:57:06 -0500

    [DOC] Tweaks for String#inspect

--- string.c
+++ string.c
@@ -7313,12 +7313,7 @@ rb_str_escape(VALUE str)
  *  call-seq:
  *    inspect -> string
  *
- *  Returns a printable version of +self+, enclosed in double-quotes,
- *  and with special characters escaped:
- *
- *    s = "foo\tbar\tbaz\n"
- *    s.inspect
- *    # => "\"foo\\tbar\\tbaz\\n\""
+ *  :include: doc/string/inspect.rdoc
  *
  */
 
