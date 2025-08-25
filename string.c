1e2ae2a1f5b66b33ff221e05eda3b33f0ed1e72b
BurdetteLamar <burdettelamar@yahoo.com>
2025-08-25 10:30:25 -0500

    [DOC] Tweaks for String#ljust

--- string.c
+++ string.c
@@ -11043,11 +11043,9 @@ rb_str_justify(int argc, VALUE *argv, VALUE str, char jflag)
 
 /*
  *  call-seq:
- *    ljust(size, pad_string = ' ') -> new_string
+ *    ljust(width, pad_string = ' ') -> new_string
  *
  *  :include: doc/string/ljust.rdoc
- *
- *  Related: String#rjust, String#center.
  *
  */
 
