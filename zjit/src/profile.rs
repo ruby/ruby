ea765697182324c18b58dd2a3e47e1ed58c749da
Max Bernstein <ruby@bernsteinbear.com>
2025-08-26 23:49:00 -0400

    ZJIT: Profile and specialize Array#empty?

--- zjit/src/profile.rs
+++ zjit/src/profile.rs
@@ -66,6 +66,7 @@ fn profile_insn(bare_opcode: ruby_vminsn_type, ec: EcPtr) {
         YARVINSN_opt_ge    => profile_operands(profiler, profile, 2),
         YARVINSN_opt_and   => profile_operands(profiler, profile, 2),
         YARVINSN_opt_or    => profile_operands(profiler, profile, 2),
+        YARVINSN_opt_empty_p  => profile_operands(profiler, profile, 1),
         YARVINSN_opt_send_without_block => {
             let cd: *const rb_call_data = profiler.insn_opnd(0).as_ptr();
             let argc = unsafe { vm_ci_argc((*cd).ci) };
