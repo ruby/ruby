50f531e9503703be07f4f04b3e3adff8dff757c1
Jeremy Evans <code@jeremyevans.net>
2025-08-29 17:45:31 -0700

    Initialize class dup/clone before calling initialize_dup/initialize_clone
    Previously, you could override the class initialize_dup/initialize_clone
    method and the class hierarchy would not be set correctly inside the
    method before calling super.
    
    This removes Class#initialize_copy, and instead makes Object#dup/clone
    call the underlying C function (rb_mod_init_copy) before calling the
    appropriate initialize_dup/initialize_clone method.
    
    This results in the following fixes:
    
    * The appropriate initialize_dup method is called (dup on a class
      will respect superclass initialize_dup).
    
    * Inside class initialize_dup/initialize_clone/initialize_copy,
      class ancestor hierarchy is correct.
    
    * Calling singleton_class inside initialize_dup no longer raises
      a TypeError later in dup.
    
    * Calling singleton_class.ancestors inside initialize_dup no
      longer results in missing ancestors.
    
    Fixes [Bug #21538]

--- test/ruby/test_class.rb
+++ test/ruby/test_class.rb
@@ -259,6 +259,48 @@ class TestClass < Test::Unit::TestCase
     assert_raise(TypeError) { BasicObject.dup }
   end
 
+  def test_class_hierarchy_inside_initialize_dup_bug_21538
+    initialize_dup_called = false
+    ancestors = sc_ancestors = nil
+    b = Class.new
+    b.define_singleton_method(:initialize_dup) do |x|
+      ancestors = self.ancestors
+      sc_ancestors = singleton_class.ancestors
+      super(x)
+    end
+
+    a = Class.new(b)
+
+    c = a.dup
+
+    expected_ancestors = [c, b, *Object.ancestors]
+    expected_sc_ancestors = [c.singleton_class, b.singleton_class, *Object.singleton_class.ancestors]
+    assert_equal expected_ancestors, ancestors
+    assert_equal expected_sc_ancestors, sc_ancestors
+    assert_equal expected_ancestors, c.ancestors
+    assert_equal expected_sc_ancestors, c.singleton_class.ancestors
+  end
+
+  def test_class_hierarchy_inside_initialize_clone_bug_21538
+    initialize_dup_called = false
+    ancestors = sc_ancestors = nil
+    a = Class.new
+    a.define_singleton_method(:initialize_clone) do |x|
+      ancestors = self.ancestors
+      sc_ancestors = singleton_class.ancestors
+      super(x)
+    end
+
+    c = a.clone
+
+    expected_ancestors = [c,  *Object.ancestors]
+    expected_sc_ancestors = [c.singleton_class, *Object.singleton_class.ancestors]
+    assert_equal expected_ancestors, ancestors
+    assert_equal expected_sc_ancestors, sc_ancestors
+    assert_equal expected_ancestors, c.ancestors
+    assert_equal expected_sc_ancestors, c.singleton_class.ancestors
+  end
+
   def test_singleton_class
     assert_raise(TypeError) { 1.extend(Module.new) }
     assert_raise(TypeError) { 1.0.extend(Module.new) }
