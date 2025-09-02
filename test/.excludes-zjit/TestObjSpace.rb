142f2b0bfee62108aafbb75619fafff2a5b56962
Stan Lo <stan.lo@shopify.com>
2025-09-02 21:25:40 +0100

    ZJIT: Remove unneeded test exclusions

--- test/.excludes-zjit/TestObjSpace.rb
+++
@@ -1,2 +0,0 @@
-exclude(:test_dump_to_io, 'Test fails with ZJIT')
-exclude(:test_dump_to_default, 'Test fails with ZJIT')
--- test/.excludes-zjit/TestParse.rb
+++
@@ -1,2 +0,0 @@
-exclude(:test_flip_flop, 'Test fails with ZJIT')
-exclude(:test_rescue_in_command_assignment, 'Test fails with ZJIT')
