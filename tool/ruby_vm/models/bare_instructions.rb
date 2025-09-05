799d57bd0178168c977cc0c2b45ff3a5f01d1297
Takashi Kokubun <takashikkbn@gmail.com>
2025-09-05 09:14:56 -0700

    insns.def: Drop unused leafness_of_check_ints
    It was used to let MJIT override the leafness of the instruction when it
    decides to remove check_ints for it. Now that MJIT is gone, nobody needs
    to "override" the leafness using this.

--- tool/ruby_vm/models/bare_instructions.rb
+++ tool/ruby_vm/models/bare_instructions.rb
@@ -111,10 +111,6 @@ class RubyVM::BareInstructions
     @attrs.fetch('leaf').expr.expr == 'true;'
   end
 
-  def leaf_without_check_ints?
-    @attrs.fetch('leaf').expr.expr == 'leafness_of_check_ints;'
-  end
-
   def handle_canary stmt
     # Stack canary is basically a good thing that we want to add, however:
     #
