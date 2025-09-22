e12fd218f2059d271ad87cbe558020e98bdee03d
Stan Lo <stan.lo@shopify.com>
2025-09-22 19:18:34 +0100

    YJIT: Pass iseq pointer to get/set classvariable functions
    Since we already have the iseq pointer, we can actually save one
    memory read by passing it directly.
    
    We need to wrap the iseq in a VALUE so it can be marked correctly
    by GC.

--- yjit/src/codegen.rs
+++ yjit/src/codegen.rs
@@ -10207,7 +10207,7 @@ fn gen_getclassvariable(
     let val_opnd = asm.ccall(
         rb_vm_getclassvariable as *const u8,
         vec![
-            Opnd::mem(64, CFP, RUBY_OFFSET_CFP_ISEQ),
+            VALUE(jit.iseq as usize).into(),
             CFP,
             Opnd::UImm(jit.get_arg(0).as_u64()),
             Opnd::UImm(jit.get_arg(1).as_u64()),
@@ -10231,7 +10231,7 @@ fn gen_setclassvariable(
     asm.ccall(
         rb_vm_setclassvariable as *const u8,
         vec![
-            Opnd::mem(64, CFP, RUBY_OFFSET_CFP_ISEQ),
+            VALUE(jit.iseq as usize).into(),
             CFP,
             Opnd::UImm(jit.get_arg(0).as_u64()),
             val,
