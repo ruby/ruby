667979f14f4c975b8295a0291d613523c80e56a3
Aiden Fox Ivey <aiden.foxivey@shopify.com>
2025-10-01 14:00:00 -0400

    ZJIT: Add extra diagnostic info to dump_locations

--- zjit.rb
+++ zjit.rb
@@ -129,6 +129,8 @@ class << RubyVM::ZJIT
     File.open(filename, "wb") do |file|
       Marshal.dump(RubyVM::ZJIT.exit_locations, file)
     end
+
+    File.size(filename)
   end
 
   # Check if `--zjit-stats` is used
@@ -275,9 +277,9 @@ class << RubyVM::ZJIT
   def dump_locations # :nodoc:
     return unless trace_exit_locations_enabled?
 
-    filename = "zjit_exit_locations.dump"
-    dump_exit_locations(filename)
+    filename = "zjit_exits_#{Time.now.to_i}.dump"
+    n_bytes = dump_exit_locations(filename)
 
-    $stderr.puts("ZJIT exit locations dumped to `#{filename}`.")
+    $stderr.puts("#{n_bytes} bytes written to #{filename}.")
   end
 end
