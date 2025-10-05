f0e8950cbb9649766eaf425ba0fda4d933889a4c
Peter Zhu <peter@peterzhu.ca>
2025-10-05 11:15:09 -0400

    Also add LSAN_OPTIONS=handle_segv=0 in assert_segv
    Just like ASAN, when running with LSAN, we also want to set handle_segv=0
    in assert_segv to make sure that the tests pass.

--- test/ruby/test_rubyoptions.rb
+++ test/ruby/test_rubyoptions.rb
@@ -858,7 +858,7 @@ class TestRubyOptions < Test::Unit::TestCase
     env['RUBY_CRASH_REPORT'] ||= nil # default to not passing down parent setting
     # ASAN registers a segv handler which prints out "AddressSanitizer: DEADLYSIGNAL" when
     # catching sigsegv; we don't expect that output, so suppress it.
-    env.update({'ASAN_OPTIONS' => 'handle_segv=0'})
+    env.update({'ASAN_OPTIONS' => 'handle_segv=0', 'LSAN_OPTIONS' => 'handle_segv=0'})
     args.unshift(env)
 
     test_stdin = ""
