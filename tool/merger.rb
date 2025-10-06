a7eb7e7c6fe02f7127ff527fff18a0914a7aa888
Takashi Kokubun <takashikkbn@gmail.com>
2025-10-06 10:53:24 -0700

    tool/merger.rb: Fetch diff from GitHub instead of cgit
    Our cgit server has been shut down.
    
    tool/merger.rb: Fetch a diff in the patch format
    
    It expects "Subject:", so it needs to be a patch file.

--- tool/merger.rb
+++ tool/merger.rb
@@ -260,7 +260,7 @@ else
     end
 
     # Merge revision from Git patch
-    git_uri = "https://git.ruby-lang.org/ruby.git/patch/?id=#{git_rev}"
+    git_uri = "https://github.com/ruby/ruby/commit/#{git_rev}.patch"
     resp = Net::HTTP.get_response(URI(git_uri))
     if resp.code != '200'
       abort "'#{git_uri}' returned status '#{resp.code}':\n#{resp.body}"
