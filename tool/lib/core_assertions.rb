68e03213271f44ae63ad4a4e1ebe3ed59d589a9c
Peter Zhu <peter@peterzhu.ca>
2025-10-06 21:22:52 -0400

    Always load -test-/asan in tests
    -test-/asan should always be available.

--- tool/lib/core_assertions.rb
+++ tool/lib/core_assertions.rb
@@ -74,10 +74,7 @@ module Test
     module CoreAssertions
       require_relative 'envutil'
       require 'pp'
-      begin
-        require '-test-/asan'
-      rescue LoadError
-      end
+      require '-test-/asan'
 
       nil.pretty_inspect
 
@@ -162,7 +159,7 @@ module Test
         pend 'assert_no_memory_leak may consider MJIT memory usage as leak' if defined?(RubyVM::MJIT) && RubyVM::MJIT.enabled?
         # ASAN has the same problem - its shadow memory greatly increases memory usage
         # (plus asan has better ways to detect memory leaks than this assertion)
-        pend 'assert_no_memory_leak may consider ASAN memory usage as leak' if defined?(Test::ASAN) && Test::ASAN.enabled?
+        pend 'assert_no_memory_leak may consider ASAN memory usage as leak' if Test::ASAN.enabled?
 
         require_relative 'memory_status'
         raise Test::Unit::PendedError, "unsupported platform" unless defined?(Memory::Status)
