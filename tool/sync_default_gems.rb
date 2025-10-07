40d1603e549b12808ed7f94064014658b91660e0
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-10-08 00:03:48 +0900

    [ruby/io-console] Skip emptied commits
    https://github.com/ruby/io-console/commit/431c3f3369

--- tool/sync_default_gems.rb
+++ tool/sync_default_gems.rb
@@ -626,11 +626,14 @@ module SyncDefaultGems
       return picked || nil      # Fail unless cherry-picked
     end
 
+    if porcelain_status().empty?
+      system(*%w"git cherry-pick --skip")
+      return true
+    end
+
     # Commit cherry-picked commit
     if picked
       system(*%w"git commit --amend --no-edit")
-    elsif porcelain_status().empty?
-      system(*%w"git cherry-pick --skip")
     else
       system(*%w"git cherry-pick --continue --no-edit")
     end or return nil
