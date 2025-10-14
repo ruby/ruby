65647e8c84c053111ed255a9ab1fdfe724f55f69
Max Bernstein <ruby@bernsteinbear.com>
2025-10-14 17:32:56 -0400

    ZJIT: Inline String#getbyte

--- zjit/bindgen/src/main.rs
+++ zjit/bindgen/src/main.rs
@@ -73,6 +73,7 @@ fn main() {
         .allowlist_function("rb_utf8_str_new")
         .allowlist_function("rb_str_buf_append")
         .allowlist_function("rb_str_dup")
+        .allowlist_function("rb_str_getbyte")
         .allowlist_type("ruby_preserved_encindex")
         .allowlist_function("rb_class2name")
 
