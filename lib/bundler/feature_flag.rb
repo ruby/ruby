fdc37df3d3bd8cb2c4b2fdbafa9e3a6d93d51ab4
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-10-15 13:09:58 +0900

    [rubygems/rubygems] Removed deprecated settings methods
    https://github.com/rubygems/rubygems/commit/89bcdfc941

--- lib/bundler/feature_flag.rb
+++ lib/bundler/feature_flag.rb
@@ -2,29 +2,6 @@
 
 module Bundler
   class FeatureFlag
-    def self.settings_flag(flag, &default)
-      unless Bundler::Settings::BOOL_KEYS.include?(flag.to_s)
-        raise "Cannot use `#{flag}` as a settings feature flag since it isn't a bool key"
-      end
-
-      settings_method("#{flag}?", flag, &default)
-    end
-    private_class_method :settings_flag
-
-    def self.settings_option(key, &default)
-      settings_method(key, key, &default)
-    end
-    private_class_method :settings_option
-
-    def self.settings_method(name, key, &default)
-      define_method(name) do
-        value = Bundler.settings[key]
-        value = instance_eval(&default) if value.nil?
-        value
-      end
-    end
-    private_class_method :settings_method
-
     (1..10).each {|v| define_method("bundler_#{v}_mode?") { @major_version >= v } }
 
     def removed_major?(target_major_version)
