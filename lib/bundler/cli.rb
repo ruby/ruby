8104c833ef47429ded66edf328fc7f9b57874cb4
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-10-15 12:23:40 +0900

    [rubygems/rubygems] Fixed wrong option message
    https://github.com/rubygems/rubygems/commit/15be905c44

--- lib/bundler/cli.rb
+++ lib/bundler/cli.rb
@@ -438,7 +438,7 @@ module Bundler
     map aliases_for("cache")
 
     desc "exec [OPTIONS]", "Run the command in context of the bundle"
-    method_option :keep_file_descriptors, type: :boolean, default: true, banner: "Passes all file descriptors to the new processes. Default is true, and setting it to false is deprecated"
+    method_option :keep_file_descriptors, type: :boolean, default: true, banner: "Passes all file descriptors to the new processes. Default is true, and setting it to false is not permitted (removed)."
     method_option :gemfile, type: :string, required: false, banner: "Use the specified gemfile instead of Gemfile"
     long_desc <<-D
       Exec runs a command, providing it access to the gems in the bundle. While using
