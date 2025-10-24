c3c254439f06073a9c7e167f89e1c1e97629d947
Max Bernstein <ruby@bernsteinbear.com>
2025-10-24 15:54:15 -0700

    ZJIT: Annotate Symbol#to_s and Symbol#name as returning StringExact

--- zjit/src/cruby_methods.rs
+++ zjit/src/cruby_methods.rs
@@ -225,6 +225,8 @@ pub fn init() -> Annotations {
     annotate_builtin!(rb_mKernel, "Float", types::Float);
     annotate_builtin!(rb_mKernel, "Integer", types::Integer);
     annotate_builtin!(rb_mKernel, "class", types::Class, leaf);
+    annotate_builtin!(rb_cSymbol, "name", types::StringExact);
+    annotate_builtin!(rb_cSymbol, "to_s", types::StringExact);
 
     Annotations {
         cfuncs: std::mem::take(cfuncs),
