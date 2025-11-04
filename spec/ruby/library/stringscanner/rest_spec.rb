7a0d730ee320e8b7a46d8fd4719a1ec709fd958c
Takashi Kokubun <takashikkbn@gmail.com>
2025-11-04 13:39:51 -0800

    Resurrect tests for StringScanner#rest?
    that has not been obsolete.
    
    Partially reverting https://github.com/ruby/ruby/pull/15049.

--- spec/ruby/library/stringscanner/rest_spec.rb
+++ spec/ruby/library/stringscanner/rest_spec.rb
@@ -25,3 +25,24 @@ describe "StringScanner#rest" do
   it_behaves_like :extract_range_matched, :rest
 
 end
+
+describe "StringScanner#rest?" do
+  before :each do
+    @s = StringScanner.new("This is a test")
+  end
+
+  it "returns true if there is more data in the string" do
+    @s.rest?.should be_true
+    @s.scan(/This/)
+    @s.rest?.should be_true
+  end
+
+  it "returns false if there is no more data in the string" do
+    @s.terminate
+    @s.rest?.should be_false
+  end
+
+  it "is the opposite of eos?" do
+    @s.rest?.should_not == @s.eos?
+  end
+end
