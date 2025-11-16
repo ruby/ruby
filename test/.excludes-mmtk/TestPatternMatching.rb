8d0105824f1cf722c1319496ed903b6a18efdafe
Kevin Newton <kddnewton@gmail.com>
2025-11-16 16:57:42 -0500

    Handle deeply nested capture variables syntax err
    When there are nested capture variables inside of a pattern match
    that has an alternation pattern, it is a syntax error. Currently it
    only adds a syntax error when it is at the top level of the pattern.

---
+++ test/.excludes-mmtk/TestPatternMatching.rb
@@ -0,0 +1,1 @@
+exclude(:test_alternative_pattern_nested, "Changes here for syntax errors")
---
+++ test/.excludes-parsey/TestPatternMatching.rb
@@ -0,0 +1,1 @@
+exclude(:test_alternative_pattern_nested, "Deeply nested captures variables are missing a syntax error")
