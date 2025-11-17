e582d199e3a9f73d2d9009646dbfe6fc5d165ca6
Peter Zhu <peter@peterzhu.ca>
2025-11-17 17:05:36 -0500

    Fix EnvUtil.current_parser for modular GC
    The regexp used in EnvUtil.current_parser did not allow square brackets
    for feature names. Modular GC uses square brackets for the GC name (such
    as +GC[mmtk]).

--- tool/lib/envutil.rb
+++ tool/lib/envutil.rb
@@ -279,7 +279,7 @@ module EnvUtil
   module_function :invoke_ruby
 
   def current_parser
-    features = RUBY_DESCRIPTION[%r{\)\K [-+*/%._0-9a-zA-Z ]*(?=\[[-+*/%._0-9a-zA-Z]+\]\z)}]
+    features = RUBY_DESCRIPTION[%r{\)\K [-+*/%._0-9a-zA-Z\[\] ]*(?=\[[-+*/%._0-9a-zA-Z]+\]\z)}]
     features&.split&.include?("+PRISM") ? "prism" : "parse.y"
   end
   module_function :current_parser
