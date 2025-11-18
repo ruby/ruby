2c42f7c60115d78436e599cf4f56aaf05bc7ac79
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-11-18 19:34:28 +0900

    [ruby/resolv] Move minimal registry access to the extension
    Now win32/registry depends on fiddle, and its conversion is complex
    and too generic for the purpose of resolv.
    
    https://github.com/ruby/resolv/commit/bd24870d2d

--- ext/win32/lib/win32/resolv.rb
+++ ext/win32/lib/win32/resolv.rb
@@ -4,8 +4,23 @@
 
 =end
 
+require 'win32/resolv.so'
+
 module Win32
   module Resolv
+    # Error at Win32 API
+    class Error < StandardError
+      # +code+ Win32 Error code
+      # +message+ Formatted message for +code+
+      def initialize(code, message)
+        super(message)
+        @code = code
+      end
+
+      # Win32 error code
+      attr_reader :code
+    end
+
     def self.get_hosts_path
       path = get_hosts_dir
       path = File.expand_path('hosts', path)
@@ -29,121 +44,62 @@ module Win32
       end
       [ search, nameserver ]
     end
-  end
-end
-
-begin
-  require 'win32/resolv.so'
-rescue LoadError
-end
-
-module Win32
-#====================================================================
-# Windows NT
-#====================================================================
-  module Resolv
-    begin
-      require 'win32/registry'
-      module SZ
-        refine Registry do
-          # ad hoc workaround for broken registry
-          def read_s(key)
-            type, str = read(key)
-            unless type == Registry::REG_SZ
-              warn "Broken registry, #{name}\\#{key} was #{Registry.type2name(type)}, ignored"
-              return String.new
-            end
-            str
-          end
-        end
-      end
-      using SZ
-    rescue LoadError, Gem::LoadError
-      require "open3"
-    end
-
-    TCPIP_NT = 'SYSTEM\CurrentControlSet\Services\Tcpip\Parameters'
 
     class << self
       private
       def get_hosts_dir
-        get_item_property(TCPIP_NT, 'DataBasePath', expand: true)
+        tcpip_params do |params|
+          params.value('DataBasePath')
+        end
       end
 
       def get_info
         search = nil
         nameserver = get_dns_server_list
 
-        slist = get_item_property(TCPIP_NT, 'SearchList')
-        search = slist.split(/,\s*/) unless slist.empty?
+        tcpip_params do |params|
+          slist = params.value('SearchList')
+          search = slist.split(/,\s*/) if slist and !slist.empty?
 
-        if add_search = search.nil?
-          search = []
-          nvdom = get_item_property(TCPIP_NT, 'NV Domain')
+          if add_search = search.nil?
+            search = []
+            nvdom = params.value('NV Domain')
 
-          unless nvdom.empty?
-            search = [ nvdom ]
-            udmnd = get_item_property(TCPIP_NT, 'UseDomainNameDevolution', dword: true)
-            if udmnd != 0
-              if /^\w+\./ =~ nvdom
-                devo = $'
-              end
-            end
-          end
-        end
-
-        ifs = if defined?(Win32::Registry)
-                Registry::HKEY_LOCAL_MACHINE.open(TCPIP_NT + '\Interfaces') do |reg|
-                  reg.keys
-                rescue Registry::Error
-                  []
+            if nvdom and !nvdom.empty?
+              search = [ nvdom ]
+              udmnd = params.value('UseDomainNameDevolution')
+              if udmnd&.nonzero?
+                if /^\w+\./ =~ nvdom
+                  devo = $'
                 end
-              else
-                cmd = "Get-ChildItem 'HKLM:\\#{TCPIP_NT}\\Interfaces' | ForEach-Object { $_.PSChildName }"
-                output, _ = Open3.capture2('powershell', '-Command', cmd)
-                output.split(/\n+/)
-              end
-
-        ifs.each do |iface|
-          next unless ns = %w[NameServer DhcpNameServer].find do |key|
-            ns = get_item_property(TCPIP_NT + '\Interfaces' + "\\#{iface}", key)
-            break ns.split(/[,\s]\s*/) unless ns.empty?
-          end
-
-          next if (nameserver & ns).empty?
-
-          if add_search
-            [ 'Domain', 'DhcpDomain' ].each do |key|
-              dom = get_item_property(TCPIP_NT + '\Interfaces' + "\\#{iface}", key)
-              unless dom.empty?
-                search.concat(dom.split(/,\s*/))
-                break
               end
             end
           end
+
+          params.open('Interfaces') do |reg|
+            reg.each_key do |iface|
+              next unless ns = %w[NameServer DhcpNameServer].find do |key|
+                ns = iface.value(key)
+                break ns.split(/[,\s]\s*/) if ns and !ns.empty?
+              end
+
+              next if (nameserver & ns).empty?
+
+              if add_search
+                [ 'Domain', 'DhcpDomain' ].each do |key|
+                  dom = iface.value(key)
+                  if dom and !dom.empty?
+                    search.concat(dom.split(/,\s*/))
+                    break
+                  end
+                end
+              end
+            end
+          end
+
+          search << devo if add_search and devo
         end
-        search << devo if add_search and devo
         [ search.uniq, nameserver.uniq ]
-      end
-
-      def get_item_property(path, name, expand: false, dword: false)
-        if defined?(Win32::Registry)
-          begin
-            Registry::HKEY_LOCAL_MACHINE.open(path) do |reg|
-              if dword
-                reg.read_i(name)
-              else
-                expand ? reg.read_s_expand(name) : reg.read_s(name)
-              end
-            end
-          rescue Registry::Error
-            dword ? 0 : ""
-          end
-        else
-          cmd = "Get-ItemProperty -Path 'HKLM:\\#{path}' -Name '#{name}' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty '#{name}'"
-          output, _ = Open3.capture2('powershell', '-Command', cmd)
-          dword ? output.strip.to_i : output.strip
-        end
       end
     end
   end
--- ext/win32/resolv/extconf.rb
+++ ext/win32/resolv/extconf.rb
@@ -1,5 +1,6 @@
 require 'mkmf'
 if RUBY_ENGINE == "ruby" and have_library('iphlpapi', 'GetNetworkParams', ['windows.h', 'iphlpapi.h'])
+  have_library('advapi32', 'RegGetValueW', ['windows.h'])
   create_makefile('win32/resolv')
 else
   File.write('Makefile', "all clean install:\n\t@echo Done: $(@)\n")
