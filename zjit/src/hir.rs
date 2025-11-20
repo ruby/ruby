937c7959ac669e1272f11ff0a673fef955583ca4
Alan Wu <XrXr@users.noreply.github.com>
2025-11-20 17:25:53 -0500

    ZJIT: For JIT-to-JIT send, avoid loading uninitialized local through EP
    JIT-to-JIT sends don't blit locals to nil in the callee's
    EP memory region because HIR is aware of this initial state and
    memory ops are only done when necessary. Previously, we
    read from this initialized memory by emitting `GetLocal` in e.g. BBs
    that are immediate successor to an entrypoint.
    
    The entry points sets up the frame state properly and we also reload
    locals if necessary after an operation that potentially makes the
    environment escape. So, listen to the frame state when it's supposed to
    be up-to-date (`!local_inval`).

--- zjit/src/hir.rs
+++ zjit/src/hir.rs
@@ -5122,7 +5122,12 @@ pub fn iseq_to_hir(iseq: *const rb_iseq_t) -> Result<Function, ParseError> {
                 }
                 YARVINSN_getlocal_WC_0 => {
                     let ep_offset = get_arg(pc, 0).as_u32();
-                    if ep_escaped || has_blockiseq { // TODO: figure out how to drop has_blockiseq here
+                    if !local_inval {
+                        // The FrameState is the source of truth for locals until invalidated.
+                        // In case of JIT-to-JIT send locals might never end up in EP memory.
+                        let val = state.getlocal(ep_offset);
+                        state.stack_push(val);
+                    } else if ep_escaped || has_blockiseq { // TODO: figure out how to drop has_blockiseq here
                         // Read the local using EP
                         let val = fun.push_insn(block, Insn::GetLocal { ep_offset, level: 0, use_sp: false, rest_param: false });
                         state.setlocal(ep_offset, val); // remember the result to spill on side-exits
