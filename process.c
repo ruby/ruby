46c9a0c9880657e66945934d6ee257adb2f3cf9d
Peter Zhu <peter@peterzhu.ca>
2025-11-23 12:47:33 -0500

    Replace vfork with fork for ASAN
    Older versions of ASAN do not support vfork. See https://github.com/google/sanitizers/issues/925

--- process.c
+++ process.c
@@ -4011,7 +4011,10 @@ retry_fork_async_signal_safe(struct rb_process_status *status, int *ep,
     while (1) {
         prefork();
         disable_child_handler_before_fork(&old);
-#ifdef HAVE_WORKING_VFORK
+
+    // Older versions of ASAN does not work with vfork
+    // See https://github.com/google/sanitizers/issues/925
+#if defined(HAVE_WORKING_VFORK) && !defined(RUBY_ASAN_ENABLED)
         if (!has_privilege())
             pid = vfork();
         else
