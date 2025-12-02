a3dc71eb5fe8ae7c411067a58ec91201fec240ee
Daisuke Aritomo <osyoyu@osyoyu.com>
2025-12-03 03:41:21 +0900

    Suppress bundled gem warning on `binding.irb'
    This patch silences the "this won't work in the next version of Ruby"
    warning displayed when irb is autoloaded via `binding.irb`.
    
        main.rb:1: warning: irb used to be loaded from the standard library, but is not part of the default gems since Ruby 4.0.0.
        You can add irb to your Gemfile or gemspec to fix this error.
        /.../irb.rb:9: warning: reline used to be loaded from the standard library, but is not part of the default gems since Ruby 4.0.0.
        You can add reline to your Gemfile or gemspec to fix this error.
    
        From: main.rb @ line 1 :
    
         => 1: binding.irb
    
        /.../input-method.rb:284: warning: rdoc used to be loaded from the standard library, but is not part of the default gems since Ruby 4.0.0.
        You can add rdoc to your Gemfile or gemspec to fix this error.
    
    This warning is incorrect and misleading: users should not need to
    include irb (and its dependencies) to their Gemfiles to use
    `binding.irb`, even in future versions of Ruby. It is agreed that the
    runtime takes care of that.

--- lib/bundled_gems.rb
+++ lib/bundled_gems.rb
@@ -122,6 +122,10 @@ module Gem::BUNDLED_GEMS # :nodoc:
       false
     end
 
+    if suppress_list = Thread.current[:__bundled_gems_warning_suppression]
+      return if suppress_list.include?(name) || suppress_list.include?(feature)
+    end
+
     return if specs.include?(name)
 
     # Don't warn if a hyphenated gem provides this feature
