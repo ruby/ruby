7ecd369a87d65879f98d95d726772238c1dabc16
Alan Wu <XrXr@users.noreply.github.com>
2025-12-03 14:31:48 -0500

    ZJIT: Account for when YJIT is on by default in test_zjit_enable

--- test/ruby/test_zjit.rb
+++ test/ruby/test_zjit.rb
@@ -60,7 +60,9 @@ class TestZJIT < Test::Unit::TestCase
   end
 
   def test_zjit_enable
-    assert_separately([], <<~'RUBY')
+    # --disable-all is important in case the build/environment has YJIT enabled by
+    # default through e.g. -DYJIT_FORCE_ENABLE. Can't enable ZJIT when YJIT is on.
+    assert_separately(["--disable-all"], <<~'RUBY')
       refute_predicate RubyVM::ZJIT, :enabled?
       refute_predicate RubyVM::ZJIT, :stats_enabled?
       refute_includes RUBY_DESCRIPTION, "+ZJIT"
