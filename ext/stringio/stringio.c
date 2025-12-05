734dab5ec885107b9d1da81d5d071927e59fddee
BurdetteLamar <burdettelamar@yahoo.com>
2025-12-05 16:21:55 +0000

    [ruby/stringio] [DOC] Link to on-page section, not class File doc
    https://github.com/ruby/stringio/commit/dc93aa51d2

--- ext/stringio/stringio.c
+++ ext/stringio/stringio.c
@@ -774,7 +774,7 @@ strio_set_lineno(VALUE self, VALUE lineno)
  *   binmode -> self
  *
  * Sets the data mode in +self+ to binary mode;
- * see {Data Mode}[rdoc-ref:File@Data+Mode].
+ * see {Data Mode}[rdoc-ref:StringIO@Data+Mode].
  *
  */
 static VALUE
