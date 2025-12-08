a82aa08fe0112aefd35e28dc5ca3f9ea9238ae17
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-12-08 12:04:11 +0900

    Make `ruby_reset_leap_second_info` internal
    It is exported only for the extension library to test, but the method
    is no longer used since 29e31e72fb5a14194a78ec974c4ba56c33ad8d45.

--- internal/time.h
+++ internal/time.h
@@ -27,7 +27,6 @@ struct timeval rb_time_timeval(VALUE);
 
 RUBY_SYMBOL_EXPORT_BEGIN
 /* time.c (export) */
-void ruby_reset_leap_second_info(void);
 RUBY_SYMBOL_EXPORT_END
 
 void ruby_reset_timezone(const char *);
