bd752290e743e3465286b3c656c8e31869f1c4fc
Koichi Sasada <ko1@atdot.net>
2025-12-09 00:50:32 +0900

    [ruby/timeout] Revert "Exclude constantly-failing test on x86_64-darwin"
    This reverts commit https://github.com/ruby/timeout/commit/45816b1b2602.
    
    https://github.com/ruby/timeout/commit/b54f91e9dd

--- test/test_timeout.rb
+++ test/test_timeout.rb
@@ -302,6 +302,5 @@ class TestTimeout < Test::Unit::TestCase
 
       assert_equal :ok, r
     end;
-  end if defined?(::Ractor) && RUBY_VERSION >= '4.0' && !RUBY_PLATFORM.include?('x86_64-darwin')
-  # Exclude on x86_64-darwin as it failed 4 times out of 4 tries in the CI of ruby/ruby-dev-builder
+  end if defined?(::Ractor) && RUBY_VERSION >= '4.0'
 end
