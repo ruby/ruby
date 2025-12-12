b12a03a182031f2a054bc46bfe6f27e90fc2185a
Luke Gruber <luke.gruber@shopify.com>
2025-12-12 13:33:23 -0500

    Timeout.timeout works in ractors now

--- test/.excludes-ractor/TestThreadConditionVariable.rb
+++ test/.excludes-ractor/TestThreadConditionVariable.rb
@@ -1,4 +1,2 @@
 exclude(:test_condvar_fork, "fork")
-exclude(:test_condvar_wait_and_broadcast, "Timeout.timeout")
-exclude(:test_condvar_wait_deadlock_2, "Timeout.timeout")
 exclude(:test_condvar_timed_wait, "FIXME: unpredictable")
--- test/.excludes-ractor/TestThreadQueue.rb
+++ test/.excludes-ractor/TestThreadQueue.rb
@@ -1,2 +1,1 @@
 exclude(:test_fork_while_queue_waiting, "fork")
-exclude(:test_queue_thread_raise, "Timeout.timeout")
