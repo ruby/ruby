1794cfe12fe61dedebadead542927f9fef4104eb
Yusuke Endoh <mame@ruby-lang.org>
2025-12-12 14:44:09 +0900

    Binding#local_variable_defined? raises a NameError for numbered params.
    [Bug #21776]

--- test/ruby/test_proc.rb
+++ test/ruby/test_proc.rb
@@ -1659,35 +1659,35 @@ class TestProc < Test::Unit::TestCase
       assert_equal([], binding.local_variables)
       assert_raise(NameError) { binding.local_variable_get(:_9) }
       assert_raise(NameError) { binding.local_variable_set(:_9, 1) }
-      assert_equal(false, binding.local_variable_defined?(:_9))
+      assert_raise(NameError) { binding.local_variable_defined?(:_9) }
       "bar".tap do
         assert_equal([], binding.local_variables)
         assert_raise(NameError) { binding.local_variable_get(:_9) }
         assert_raise(NameError) { binding.local_variable_set(:_9, 1) }
-        assert_equal(false, binding.local_variable_defined?(:_9))
+        assert_raise(NameError) { binding.local_variable_defined?(:_9) }
       end
       assert_equal([], binding.local_variables)
       assert_raise(NameError) { binding.local_variable_get(:_9) }
       assert_raise(NameError) { binding.local_variable_set(:_9, 1) }
-      assert_equal(false, binding.local_variable_defined?(:_9))
+      assert_raise(NameError) { binding.local_variable_defined?(:_9) }
     end
 
     "foo".tap do
       assert_equal([], binding.local_variables)
       assert_raise(NameError) { binding.local_variable_get(:_9) }
       assert_raise(NameError) { binding.local_variable_set(:_9, 1) }
-      assert_equal(false, binding.local_variable_defined?(:_9))
+      assert_raise(NameError) { binding.local_variable_defined?(:_9) }
       "bar".tap do
         _9 and flunk
         assert_equal([], binding.local_variables)
         assert_raise(NameError) { binding.local_variable_get(:_9) }
         assert_raise(NameError) { binding.local_variable_set(:_9, 1) }
-        assert_equal(false, binding.local_variable_defined?(:_9))
+        assert_raise(NameError) { binding.local_variable_defined?(:_9) }
       end
       assert_equal([], binding.local_variables)
       assert_raise(NameError) { binding.local_variable_get(:_9) }
       assert_raise(NameError) { binding.local_variable_set(:_9, 1) }
-      assert_equal(false, binding.local_variable_defined?(:_9))
+      assert_raise(NameError) { binding.local_variable_defined?(:_9) }
     end
   end
 
