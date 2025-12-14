d63cd7c114907a5c0e484c1fe22d60f3bba574eb
BurdetteLamar <burdettelamar@yahoo.com>
2025-12-14 22:28:41 +0000

    [DOC] Fix link in MakeMakefile

--- lib/mkmf.rb
+++ lib/mkmf.rb
@@ -40,7 +40,7 @@ class Array # :nodoc:
 end
 
 ##
-# mkmf.rb is used by Ruby C extensions to generate a Makefile which will
+# \Module \MakeMakefile is used by Ruby C extensions to generate a Makefile which will
 # correctly compile and link the C extension to Ruby and a third-party
 # library.
 module MakeMakefile
