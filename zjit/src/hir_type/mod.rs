f9220a7e6e2f025c3b43e15b42dfe2d168c94f83
Benoit Daloze <eregontp@gmail.com>
2025-12-16 16:23:29 +0100

    Revert "ZJIT: Do not call rb_gc_writebarrier() with an immediate value in gen_write_barrier()"
    * This reverts commit 623559faa3dd0927b4034a752226a30ae8821604.
    * There is an issue with the jump in LIR, see https://github.com/ruby/ruby/pull/15542.

--- zjit/src/hir_type/mod.rs
+++ zjit/src/hir_type/mod.rs
@@ -517,10 +517,6 @@ impl Type {
         self.is_subtype(types::Immediate)
     }
 
-    pub fn is_heap_object(&self) -> bool {
-        self.is_subtype(types::HeapBasicObject)
-    }
-
     pub fn print(self, ptr_map: &PtrPrintMap) -> TypePrinter<'_> {
         TypePrinter { inner: self, ptr_map }
     }
