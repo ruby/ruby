cfc5d351a383ee79d54c1a755cfc00d9ff0d4fda
Benoit Daloze <eregontp@gmail.com>
2025-12-16 16:31:29 +0100

    ZJIT: Use rb_zjit_writebarrier_check_immediate() instead of rb_gc_writebarrier() in gen_write_barrier()
    * To avoid calling rb_gc_writebarrier() with an immediate value in gen_write_barrier(),
      and avoid the LIR jump issue.

--- zjit/src/cruby_bindings.inc.rs
+++ zjit/src/cruby_bindings.inc.rs
@@ -2092,6 +2092,7 @@ unsafe extern "C" {
     pub fn rb_zjit_class_get_alloc_func(klass: VALUE) -> rb_alloc_func_t;
     pub fn rb_zjit_class_has_default_allocator(klass: VALUE) -> bool;
     pub fn rb_vm_get_untagged_block_handler(reg_cfp: *mut rb_control_frame_t) -> VALUE;
+    pub fn rb_zjit_writebarrier_check_immediate(recv: VALUE, val: VALUE);
     pub fn rb_iseq_encoded_size(iseq: *const rb_iseq_t) -> ::std::os::raw::c_uint;
     pub fn rb_iseq_pc_at_idx(iseq: *const rb_iseq_t, insn_idx: u32) -> *mut VALUE;
     pub fn rb_iseq_opcode_at_pc(iseq: *const rb_iseq_t, pc: *const VALUE) -> ::std::os::raw::c_int;
