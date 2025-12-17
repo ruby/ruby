d6612177f2558ff71987d3978d23feec6b2298cf
Mike Dalessio <mike@37signals.com>
2025-12-17 17:28:47 -0500

    Ensure rubyspec-capiext won't be built by `make` or `make install`
    rubyspec-capiext is only needed for running specs, not for building/installing Ruby.
    Building it as part of `exts` breaks `--with-ext=` builds because some spec extensions
    (e.g., digest_spec.c) depend on extension headers that aren't installed.
    
    [Bug #21792]

--- defs/gmake.mk
+++ defs/gmake.mk
@@ -529,11 +529,9 @@ RUBYSPEC_CAPIEXT_SO := $(patsubst %.c,$(RUBYSPEC_CAPIEXT)/%.$(DLEXT),$(notdir $(
 rubyspec-capiext: $(RUBYSPEC_CAPIEXT_SO)
 	@ $(NULLCMD)
 
-ifeq ($(ENABLE_SHARED),yes)
-exts: rubyspec-capiext
-endif
+yes-test-spec: $(if $(filter yes,$(ENABLE_SHARED)),rubyspec-capiext)
 
-spec/%/ spec/%_spec.rb: programs exts PHONY
+spec/%/ spec/%_spec.rb: programs exts $(if $(filter yes,$(ENABLE_SHARED)),rubyspec-capiext) PHONY
 	+$(RUNRUBY) -r./$(arch)-fake $(srcdir)/spec/mspec/bin/mspec-run -B $(srcdir)/spec/default.mspec $(SPECOPTS) $(patsubst %,$(srcdir)/%,$@)
 
 ruby.pc: $(filter-out ruby.pc,$(ruby_pc))
