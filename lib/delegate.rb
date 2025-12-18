2fcad967aab5aae921b2288c8080096d79ddb8dd
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-12-18 14:16:49 +0900

    Revert "Allow use of DelegateClass in ractor"
    This reverts commit 6e0f2b31f0f4a2a942f3c1daad1bb64852fe6815.

--- lib/delegate.rb
+++ lib/delegate.rb
@@ -344,15 +344,17 @@ class SimpleDelegator < Delegator
   end
 end
 
-def Delegator.delegating_code(mid) # :nodoc:
-  line = __LINE__+2
-  src = <<~RUBY
-  ruby2_keywords def #{mid}(*args, &block)
+def Delegator.delegating_block(mid) # :nodoc:
+  prok = lambda do |*args, &block|
     target = self.__getobj__
-    target.__send__(:'#{mid}', *args, &block)
+    target.__send__(mid, *args, &block)
   end
-  RUBY
-  [src, __FILE__, line]
+  prok.ruby2_keywords
+  if defined?(Ractor.shareable_proc)
+    Ractor.shareable_proc(&prok)
+  else
+    prok
+  end
 end
 
 #
@@ -431,7 +433,7 @@ def DelegateClass(superclass, &block)
     class_eval(source.join(";"), __FILE__, __LINE__)
 
     special.each do |method|
-      module_eval(*Delegator.delegating_code(method))
+      define_method(method, Delegator.delegating_block(method))
     end
 
     protected(*protected_instance_methods)
