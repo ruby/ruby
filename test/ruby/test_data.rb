527ebd6538a28a4db330a086b15ba577440b4bd5
Shannon Skipper <shannonskipper@gmail.com>
2026-01-01 18:15:11 -0800

    Drop memberless Data/Struct#inspect trailing space
    Anonymous memberless Structs and Data were returning `#<struct >` and
    `#<data >` with a trailing space. Now they return `#<struct>` and
    `#<data>` to match attrless class behavior and look a bit more compact.

--- test/ruby/test_data.rb
+++ test/ruby/test_data.rb
@@ -259,7 +259,7 @@ class TestData < Test::Unit::TestCase
     assert_equal(klass.new, test)
     assert_not_equal(Data.define.new, test)
 
-    assert_equal('#<data >', test.inspect)
+    assert_equal('#<data>', test.inspect)
     assert_equal([], test.members)
     assert_equal({}, test.to_h)
   end
