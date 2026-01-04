2bf954305323857418b917f4a987c300245ee0eb
Benoit Daloze <eregontp@gmail.com>
2026-01-04 16:04:02 +0100

    Remove assertion which does not hold
    * https://github.com/ruby/ruby/actions/runs/20694508956/job/59407571754
      1)
      UNIXSocket.pair emulates unnamed sockets with a temporary file with a path FAILED
      Expected "C:\\a\\_temp\\102424668889-2384.($)".match? /\\AppData\\Local\\Temp\\\d+-\d+\.\(\$\)\z/
      to be truthy but was false

--- spec/ruby/library/socket/unixsocket/shared/pair.rb
+++ spec/ruby/library/socket/unixsocket/shared/pair.rb
@@ -31,7 +31,6 @@ describe :unixsocket_pair, shared: true do
 
   platform_is :windows do
     it "emulates unnamed sockets with a temporary file with a path" do
-      @s1.path.should.match?(/\\AppData\\Local\\Temp\\\d+-\d+\.\(\$\)\z/)
       @s1.addr.should == ["AF_UNIX", @s1.path]
       @s2.peeraddr.should == ["AF_UNIX", @s1.path]
     end
