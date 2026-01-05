7e81bf5c0c8f43602e6d901f4253dca2f3d71745
Luke Gruber <luke.gruber@shopify.com>
2026-01-05 12:18:47 -0500

    Fix sleep spurious wakeup from sigchld (#15802)
    When sleeping with `sleep`, currently the main thread can get woken up from sigchld
    from any thread (subprocess exited). The timer thread wakes up the main thread when this
    happens, as it checks for signals. The main thread then executes the ruby sigchld handler
    if one is registered and is supposed to go back to sleep immediately. This is not ideal but
    it's the way it's worked for a while. In commit 8d8159e7d8 I added writes to `th->status`
    before and after `wait_running_turn` in `thread_sched_to_waiting_until_wakeup`, which is
    called from `sleep`. This is usually the right way to set the thread's status, but `sleep`
    is an exception because the writes to `th->status` are done in `sleep_forever`. There's a
    loop that checks `th->status` in `sleep_forever`. When the main thread got woken up from
    sigchld it saw the changed `th->status` and continued to run the main thread instead of
    going back to sleep.
    
    The following script shows the error. It was returning instead of sleeping forever.
    
    ```ruby
    t = Thread.new do
      sleep 0.3
      `echo hello`  # Spawns subprocess
      puts "Subprocess exited"
    end
    
    puts "Main thread sleeping..."
    result = sleep  # Should block forever
    puts "sleep returned: #{result.inspect}"
    ```
    
    Fixes [Bug #21812]

--- test/ruby/test_sleep.rb
+++ test/ruby/test_sleep.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: false
 require 'test/unit'
 require 'etc'
+require 'timeout'
 
 class TestSleep < Test::Unit::TestCase
   def test_sleep_5sec
@@ -11,6 +12,23 @@ class TestSleep < Test::Unit::TestCase
       bottom = 5.0
       assert_operator(slept, :>=, bottom)
       assert_operator(slept, :<=, 6.0, "[ruby-core:18015]: longer than expected")
+    end
+  end
+
+  def test_sleep_forever_not_woken_by_sigchld
+    begin
+      t = Thread.new do
+        sleep 0.5
+        `echo hello`
+      end
+
+      assert_raise Timeout::Error do
+        Timeout.timeout 2 do
+          sleep # Should block forever
+        end
+      end
+    ensure
+      t.join
     end
   end
 end
