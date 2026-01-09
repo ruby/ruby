a93ff4880da5c4e56f7ec2066a9c3f7748892231
Jörmungandrk <github@zerodaysec.org>
2026-01-09 10:22:01 +0700

    Fix integer overflow checks in enumerator

--- enumerator.c
+++ enumerator.c
@@ -18,6 +18,7 @@
 #include <float.h>
 #endif
 
+#include <limits.h>
 #include "id.h"
 #include "internal.h"
 #include "internal/class.h"
@@ -3971,7 +3972,7 @@ arith_seq_first(int argc, VALUE *argv, VALUE self)
             ary = rb_ary_new_capa((n < len) ? n : len);
             while (n > 0 && i < end) {
                 rb_ary_push(ary, LONG2FIX(i));
-                if (i + unit < i) break;
+                if (i > LONG_MAX - unit) break;
                 i += unit;
                 --n;
             }
@@ -3984,7 +3985,7 @@ arith_seq_first(int argc, VALUE *argv, VALUE self)
             ary = rb_ary_new_capa((n < len) ? n : len);
             while (n > 0 && i > end) {
                 rb_ary_push(ary, LONG2FIX(i));
-                if (i + unit > i) break;
+                if (i < LONG_MIN - unit) break;
                 i += unit;
                 --n;
             }
