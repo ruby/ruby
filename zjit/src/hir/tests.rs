5cdc0ccee011607a2e30aa40ed29f8df4a950ed7
nozomemein <b8yukifsukeo999n@gmail.com>
2026-01-15 07:18:31 +0900

    ZJIT: Remove redundant unboxing

--- zjit/src/hir/tests.rs
+++ zjit/src/hir/tests.rs
@@ -3565,12 +3565,10 @@ pub mod hir_build_tests {
           v21:ArrayExact = GuardType v13, ArrayExact
           v22:CInt64 = ArrayLength v21
           v23:CInt64[2] = GuardBitEquals v22, CInt64(2)
-          v24:Fixnum[1] = Const Value(1)
-          v25:CInt64[1] = UnboxFixnum v24
-          v26:BasicObject = ArrayAref v21, v25
-          v27:Fixnum[0] = Const Value(0)
-          v28:CInt64[0] = UnboxFixnum v27
-          v29:BasicObject = ArrayAref v21, v28
+          v24:CInt64[1] = Const CInt64(1)
+          v25:BasicObject = ArrayAref v21, v24
+          v26:CInt64[0] = Const CInt64(0)
+          v27:BasicObject = ArrayAref v21, v26
           PatchPoint NoEPEscape(test)
           CheckInterrupts
           Return v13
