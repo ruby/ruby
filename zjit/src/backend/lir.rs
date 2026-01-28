3b1dd30c3190c0c409407d64d65fb73559333249
Aaron Patterson <tenderlove@ruby-lang.org>
2026-01-28 14:21:34 -0800

    always add padding to the last block

--- zjit/src/backend/lir.rs
+++ zjit/src/backend/lir.rs
@@ -1745,7 +1745,10 @@ impl Assembler
         // Emit instructions with labels, expanding branch parameters
         let mut insns = Vec::with_capacity(ASSEMBLER_INSNS_CAPACITY);
 
-        for block in self.sorted_blocks() {
+        let blocks = self.sorted_blocks();
+        let num_blocks = blocks.len();
+
+        for (block_id, block) in blocks.iter().enumerate() {
             // Entry blocks shouldn't ever be preceded by something that can
             // stomp on this block.
             if !block.is_entry {
@@ -1757,11 +1760,9 @@ impl Assembler
                 self.expand_branch_insn(insn, &mut insns);
             }
 
-            // If the last instruction of the block is a ret, add a pad patch point after it
-            if let Some(last_insn) = block.insns.last() {
-                if matches!(last_insn, Insn::CRet(_)) {
-                    insns.push(Insn::PadPatchPoint);
-                }
+            // Make sure we don't stomp on the next function
+            if block_id == num_blocks - 1 {
+                insns.push(Insn::PadPatchPoint);
             }
         }
         insns
