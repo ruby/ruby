11e4f99a4ef3096c59f7b221865fbbe11b4cb1ed
Earlopain <14981592+Earlopain@users.noreply.github.com>
2026-02-16 09:03:43 +0100

    [ruby/prism] Only compare &nil syntax on rubies that support it
    https://github.com/ruby/prism/commit/8aad2480ab

--- test/prism/ruby/parameters_signature_test.rb
+++ test/prism/ruby/parameters_signature_test.rb
@@ -51,7 +51,8 @@ module Prism
     end
 
     def test_noblock
-      assert_parameters([[:noblock]], "&nil")
+      # FIXME: `compare: RUBY_VERSION >= "4.1"` once builds are available
+      assert_parameters([[:noblock]], "&nil", compare: false)
     end
 
     def test_keyrest_anonymous
@@ -78,10 +79,11 @@ module Prism
 
     private
 
-    def assert_parameters(expected, source)
+    def assert_parameters(expected, source, compare: true)
       # Compare against our expectation.
       assert_equal(expected, signature(source))
 
+      return unless compare
       # Compare against Ruby's expectation.
       object = Object.new
       eval("def object.m(#{source}); end")
