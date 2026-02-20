dc1f1511df4ce019842776bdfa14ab0539f07820
Kasumi Hanazuki <kasumi@rollingapple.net>
2026-02-20 10:56:50 +0900

    IO::Buffer#locked: Release lock even when the block raises (#16180)
    IO::Buffer#locked: Release lock even when the block raises/breaks
    
    Previously, `IO::Buffer#locked` leaks the lock when the block raises
    an exception, or breaks.
    
    Fixes: [Bug #21882]

--- io_buffer.c
+++ io_buffer.c
@@ -1424,6 +1424,17 @@ rb_io_buffer_try_unlock(VALUE self)
     return 0;
 }
 
+static VALUE
+rb_io_buffer_locked_ensure(VALUE self)
+{
+    struct rb_io_buffer *buffer = NULL;
+    TypedData_Get_Struct(self, struct rb_io_buffer, &rb_io_buffer_type, buffer);
+
+    buffer->flags &= ~RB_IO_BUFFER_LOCKED;
+
+    return Qnil;
+}
+
 /*
  *  call-seq: locked { ... }
  *
@@ -1466,11 +1477,7 @@ rb_io_buffer_locked(VALUE self)
 
     buffer->flags |= RB_IO_BUFFER_LOCKED;
 
-    VALUE result = rb_yield(self);
-
-    buffer->flags &= ~RB_IO_BUFFER_LOCKED;
-
-    return result;
+    return rb_ensure(rb_yield, self, rb_io_buffer_locked_ensure, self);
 }
 
 /*
