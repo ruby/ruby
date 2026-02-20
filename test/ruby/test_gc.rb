678b53c03d3f0f2e5288be3db024f24a2740734d
Matt Valentine-House <matt@eightbitraptor.com>
2026-02-20 20:28:40 +0000

    Relax test_thrashing_for_young_objects to fix CI flake
    This test has been failing intermittently on multiple CI platforms
    (Debian 13, RHEL 9/10, Ubuntu aarch64) with heap_allocated_pages
    growing from 100 to 130, or major_gc_count incrementing by 1.
    
    Failing run: https://rubyci.s3.amazonaws.com/ubuntu/ruby-master/log/20260220T001003Z.fail.html.gz
    
    The test uses assert_separately, so test ordering is not a factor.
    In failing runs sweep_slots (27,386) lands within ~1,200 of
    min_free_slots (28,652) after warmup.
    
    I've been unable to reliably replicate this.
    
    However, when looking at the failures messages all failing runs
    total_freed_pages is 0: pages are not being freed and reallocated in a
    cycle. This is distinct from [Bug #18929] where pages cycled between
    allocation and the tomb heap.
    
    Since total_freed_pages == 0 already guards against the thrashing
    from [Bug #18929], relax heap_allocated_pages to assert_in_epsilon
    (50% tolerance) and drop major_gc_count which follows from the same
    marginal threshold.

--- test/ruby/test_gc.rb
+++ test/ruby/test_gc.rb
@@ -659,10 +659,8 @@ class TestGc < Test::Unit::TestCase
       debug_msg = "before_stats: #{before_stats}\nbefore_stat_heap: #{before_stat_heap}\nafter_stats: #{after_stats}\nafter_stat_heap: #{after_stat_heap}"
 
       # Should not be thrashing in page creation
-      assert_equal before_stats[:heap_allocated_pages], after_stats[:heap_allocated_pages], debug_msg
+      assert_in_epsilon before_stats[:heap_allocated_pages], after_stats[:heap_allocated_pages], 0.5, debug_msg
       assert_equal 0, after_stats[:total_freed_pages], debug_msg
-      # Only young objects, so should not trigger major GC
-      assert_equal before_stats[:major_gc_count], after_stats[:major_gc_count], debug_msg
     RUBY
   end
 
