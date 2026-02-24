331ee9dd930addb07ffe3b9c9c11d886dab50e8f
Matt Valentine-House <matt@eightbitraptor.com>
2026-02-24 23:36:30 +0000

    Update MMTk for new pool layout
    BASE_SLOT_SIZE is now 32 on 64-bit (16 on 32-bit), matching the
    smallest slot in heap_sizes[]. RVALUE_SIZE stays at 64/32 as the
    minimum slot for a standard RVALUE.
    
    Remove stale test exclusion for test_dump_includes_slot_size
    (explicitly marked for removal when BASE_SLOT_SIZE changed).
    
    Update pool count comment from 5 to 6.

--- gc/mmtk/mmtk.c
+++ gc/mmtk/mmtk.c
@@ -650,12 +650,12 @@ void
 rb_gc_impl_init(void)
 {
     VALUE gc_constants = rb_hash_new();
-    rb_hash_aset(gc_constants, ID2SYM(rb_intern("BASE_SLOT_SIZE")), SIZET2NUM(SIZEOF_VALUE >= 8 ? 64 : 32));
+    rb_hash_aset(gc_constants, ID2SYM(rb_intern("BASE_SLOT_SIZE")), SIZET2NUM(SIZEOF_VALUE >= 8 ? 32 : 16));
     rb_hash_aset(gc_constants, ID2SYM(rb_intern("RVALUE_SIZE")), SIZET2NUM(SIZEOF_VALUE >= 8 ? 64 : 32));
     rb_hash_aset(gc_constants, ID2SYM(rb_intern("RBASIC_SIZE")), SIZET2NUM(sizeof(struct RBasic)));
     rb_hash_aset(gc_constants, ID2SYM(rb_intern("RVALUE_OVERHEAD")), INT2NUM(0));
     rb_hash_aset(gc_constants, ID2SYM(rb_intern("RVARGC_MAX_ALLOCATE_SIZE")), LONG2FIX(MMTK_MAX_OBJ_SIZE));
-    // Pretend we have 5 size pools
+    // Pretend we have 6 size pools
     rb_hash_aset(gc_constants, ID2SYM(rb_intern("SIZE_POOL_COUNT")), LONG2FIX(MMTK_HEAP_COUNT));
     // TODO: correctly set RVALUE_OLD_AGE when we have generational GC support
     rb_hash_aset(gc_constants, ID2SYM(rb_intern("RVALUE_OLD_AGE")), INT2FIX(0));
