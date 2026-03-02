009afd1f56844269cbb55475ab4b9c152fe69d29
Alan Wu <XrXr@users.noreply.github.com>
2026-03-02 12:10:19 -0500

    put rb_gc_guard after the last usage.
    it's intended to be used to extend the liverange of a VALUE

--- string.c
+++ string.c
@@ -2621,10 +2621,11 @@ static VALUE
 rb_str_format_m(VALUE str, VALUE arg)
 {
     VALUE tmp = rb_check_array_type(arg);
-    RB_GC_GUARD(tmp);
 
     if (!NIL_P(tmp)) {
-        return rb_str_format(RARRAY_LENINT(tmp), RARRAY_CONST_PTR(tmp), str);
+        VALUE result = rb_str_format(RARRAY_LENINT(tmp), RARRAY_CONST_PTR(tmp), str);
+        RB_GC_GUARD(tmp);
+        return result;
     }
     return rb_str_format(1, &arg, str);
 }
