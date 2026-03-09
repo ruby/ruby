5b7647228a7bb0654d692a3f1557bb8c07fd4e4a
BurdetteLamar <burdettelamar@yahoo.com>
2026-03-10 00:08:03 +0100

    [DOC] Doc for Pathname class

--- lib/pathname.rb
+++ lib/pathname.rb
@@ -1,14 +1,5 @@
 # frozen_string_literal: true
 #
-# = pathname.rb
-#
-# Object-Oriented Pathname Class
-#
-# Author:: Tanaka Akira <akr@m17n.org>
-# Documentation:: Author and Gavin Sinclair
-#
-# For documentation, see class Pathname.
-#
 
 class Pathname    # * Find *
   #
