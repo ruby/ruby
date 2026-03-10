62b90b506066dc65f3ed4aeadcb96785913d0fd7
BurdetteLamar <burdettelamar@yahoo.com>
2026-03-10 20:29:19 +0100

    Restore author

--- lib/pathname.rb
+++ lib/pathname.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 #
-
+# = pathname.rb
+#
+# Object-Oriented Pathname Class
+#
+# Author:: Tanaka Akira <akr@m17n.org>
+# Documentation:: Author and Gavin Sinclair
+#
+# For documentation, see class Pathname.
+#
 class Pathname    # * Find *
   #
   # Iterates over the directory tree in a depth first manner, yielding a
