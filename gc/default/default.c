48835019f3b58d9905b739b1ff29099768fd7590
Matt Valentine-House <matt@eightbitraptor.com>
2026-03-12 18:10:56 +0000

    wip

--- gc/default/default.c
+++ gc/default/default.c
@@ -3883,9 +3883,8 @@ gc_sweep_finish_heap(rb_objspace_t *objspace, rb_heap_t *heap)
                     heap_allocatable_slots_expand(objspace, heap, swept_slots, heap->total_slots);
                 }
             }
-            else if (objspace->heap_pages.allocatable_slots < (min_free_slots - swept_slots)) {
-                fprintf(stderr, "NOFREE major forced by heap %d (slot_size=%zu): swept=%zu min_free=%zu total=%zu alloc_slots=%zu\n",
-                        (int)(heap - heaps), heap->slot_size, swept_slots, min_free_slots, total_slots, objspace->heap_pages.allocatable_slots);
+            else if (swept_slots < min_free_slots / 2 &&
+                     objspace->heap_pages.allocatable_slots < (min_free_slots / 2 - swept_slots)) {
                 gc_needs_major_flags |= GPR_FLAG_MAJOR_BY_NOFREE;
                 heap->force_major_gc_count++;
             }
