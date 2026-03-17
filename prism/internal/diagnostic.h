3d8cc80bdafc5c115f403611463d94aee30739f4
Kevin Newton <kddnewton@gmail.com>
2026-03-17 11:00:14 -0400

    [ruby/prism] Split up diagnostic headers into public and internal
    https://github.com/ruby/prism/commit/af683324ea

---
+++ prism/internal/diagnostic.h
@@ -0,0 +1,38 @@
+/**
+ * @file internal/diagnostic.h
+ *
+ * A list of diagnostics generated during parsing.
+ */
+#ifndef PRISM_INTERNAL_DIAGNOSTIC_H
+#define PRISM_INTERNAL_DIAGNOSTIC_H
+
+#include "prism/diagnostic.h"
+#include "prism/arena.h"
+#include "prism/util/pm_list.h"
+
+/**
+ * Append a diagnostic to the given list of diagnostics that is using shared
+ * memory for its message.
+ *
+ * @param arena The arena to allocate from.
+ * @param list The list to append to.
+ * @param start The source offset of the start of the diagnostic.
+ * @param length The length of the diagnostic.
+ * @param diag_id The diagnostic ID.
+ */
+void pm_diagnostic_list_append(pm_arena_t *arena, pm_list_t *list, uint32_t start, uint32_t length, pm_diagnostic_id_t diag_id);
+
+/**
+ * Append a diagnostic to the given list of diagnostics that is using a format
+ * string for its message.
+ *
+ * @param arena The arena to allocate from.
+ * @param list The list to append to.
+ * @param start The source offset of the start of the diagnostic.
+ * @param length The length of the diagnostic.
+ * @param diag_id The diagnostic ID.
+ * @param ... The arguments to the format string for the message.
+ */
+void pm_diagnostic_list_append_format(pm_arena_t *arena, pm_list_t *list, uint32_t start, uint32_t length, pm_diagnostic_id_t diag_id, ...);
+
+#endif
