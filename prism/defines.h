c133e0471de110ac57ea49a158e1246c7ca0fa06
Kevin Newton <kddnewton@gmail.com>
2026-03-17 06:48:50 -0400

    [ruby/prism] More correctly detect SIMD on MSVC
    https://github.com/ruby/prism/commit/5fe0448219

--- prism/defines.h
+++ prism/defines.h
@@ -280,9 +280,9 @@
  * Platform detection for SIMD / fast-path implementations. At most one of
  * these macros is defined, selecting the best available vectorization strategy.
  */
-#if (defined(__aarch64__) && defined(__ARM_NEON)) || defined(_M_ARM64)
+#if (defined(__aarch64__) && defined(__ARM_NEON)) || (defined(_MSC_VER) && defined(_M_ARM64))
     #define PRISM_HAS_NEON
-#elif (defined(__x86_64__) && defined(__SSSE3__)) || defined(_M_X64)
+#elif (defined(__x86_64__) && defined(__SSSE3__)) || (defined(_MSC_VER) && defined(_M_X64))
     #define PRISM_HAS_SSSE3
 #elif defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
     #define PRISM_HAS_SWAR
