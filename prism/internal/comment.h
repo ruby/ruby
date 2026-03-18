9f1e5e5ad38b64c5371f89bc6a04eca293f83d53
Kevin Newton <kddnewton@gmail.com>
2026-03-18 10:44:19 -0400

    [ruby/prism] Move comment into its own section
    https://github.com/ruby/prism/commit/1ddff36b54

---
+++ prism/internal/comment.h
@@ -0,0 +1,35 @@
+/**
+ * @file internal/comment.h
+ */
+#ifndef PRISM_INTERNAL_COMMENT_H
+#define PRISM_INTERNAL_COMMENT_H
+
+#include "prism/comment.h"
+
+/**
+ * A comment found while parsing.
+ */
+struct pm_comment_t {
+    /** The embedded base node. */
+    pm_list_node_t node;
+
+    /** The location of the comment in the source. */
+    pm_location_t location;
+
+    /** The type of the comment. */
+    pm_comment_type_t type;
+};
+
+/**
+ * A struct used as an opaque pointer for the client to iterate through the
+ * comments found while parsing.
+ */
+struct pm_comments_iter_t {
+    /** The number of comments in the list. */
+    size_t size;
+
+    /** The current node in the list. */
+    const pm_list_node_t *current;
+};
+
+#endif
