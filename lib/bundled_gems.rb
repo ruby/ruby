3bd7988d36b2e76c3a7398c946280b5d519069a0
Mau Magnaguagno <maumagnaguagno@gmail.com>
2026-03-23 21:41:48 -0300

    Refactor bundled gem warning suppressor
    Related to #16369

--- lib/bundled_gems.rb
+++ lib/bundled_gems.rb
@@ -137,8 +137,8 @@ module Gem::BUNDLED_GEMS # :nodoc:
       # Don't warn if the feature is found outside the standard library
       # (e.g., benchmark-ips's lib dir is on $LOAD_PATH but not in specs)
       resolved = $LOAD_PATH.resolve_feature_path(feature) rescue nil
-      if resolved
-        return unless resolved[1].start_with?(LIBDIR) || resolved[1].start_with?(ARCHDIR)
+      if resolved and not resolved[1].start_with?(LIBDIR, ARCHDIR)
+        return
       end
     end
 
