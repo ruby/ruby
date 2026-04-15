c879bf97d0f7be17e256f04172d41f2e4e92a7aa
BurdetteLamar <burdettelamar@yahoo.com>
2026-04-15 19:55:29 +0100

    Respond to review

--- set.c
+++ set.c
@@ -477,13 +477,20 @@ set_initialize_with_block(RB_BLOCK_CALL_FUNC_ARGLIST(i, set))
 
 /*
  * call-seq:
- *   Set.new(object) -> new_set
- *   Set.new(object) {|element| ... } -> new_set
+ *   Set.new(object = nil) -> new_set
+ *   Set.new(object = nil) {|element| ... } -> new_set
  *
- * Returns a new set based on the given +object+,
- * which must be an Enumerable.
+ * Returns a new \Set object based on the given +object+,
+ * which must be an Enumerable or +nil+.
  *
- * With no block given, populates the new set with the elements of +object+:
+ * With no argument given,
+ * returns a new empty \Set object:
+ *
+ *   Set.new                          # => Set[]
+ *   Set.new { fail 'Cannot happen' } # => Set[]
+ *
+ * With no block given and +object+ given as an Enumerable,
+ * populates the new set with the elements of +object+:
  *
  *   Set.new(%w[ a b c ])     # => Set["a", "b", "c"]
  *   Set.new({foo: 0, bar: 1})     # => Set[[:foo, 0], [:bar, 1]]
@@ -493,11 +500,12 @@ set_initialize_with_block(RB_BLOCK_CALL_FUNC_ARGLIST(i, set))
  *   Set.new(File.new('doc/NEWS/NEWS-4.0.0.md')).take(3)
  *   # => ["# NEWS for Ruby 4.0.0\n", "\n", "This document is a list of user-visible feature changes\n"]
  *
- * With a block given, calls the block with each element of +object+;
+ * With a block given and +object+ given as an Enumerable,
+ * calls the block with each element of +object+;
  * adds the block's return value to the new set:
  *
- *   Set.new(4..10) {|i| i * 2 }
-     # => Set[8, 10, 12, 14, 16, 18, 20]
+ *   Set.new(4..10) {|i| i * 2 }      # => Set[8, 10, 12, 14, 16, 18, 20]
+ *   Set.new { fail 'Cannot happen' } # => Set[]  # Block not called.
  *
  */
 static VALUE
