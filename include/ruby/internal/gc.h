64d939b16f31357d6659c4a65a10c2f281034212
Jean Boussier <jean.boussier@gmail.com>
2026-04-28 09:04:00 +0900

    Get rid of RGENGC_WB_PROTECTED_COMPLEX
    It has been the default for a very long time and there is no good reason to
    disable it.

--- include/ruby/internal/gc.h
+++ include/ruby/internal/gc.h
@@ -439,17 +439,6 @@ RBIMPL_SYMBOL_EXPORT_END()
  * @private
  *
  * This  is   a  compile-time   flag  to   enable/disable  write   barrier  for
- * struct ::RComplex.   It has  to be  set at  the time  ruby itself  compiles.
- * Makes no sense for 3rd parties.
- */
-#ifndef RGENGC_WB_PROTECTED_COMPLEX
-# define RGENGC_WB_PROTECTED_COMPLEX 1
-#endif
-
-/**
- * @private
- *
- * This  is   a  compile-time   flag  to   enable/disable  write   barrier  for
  * struct ::RRational.  It  has to  be set  at the  time ruby  itself compiles.
  * Makes no sense for 3rd parties.
  */
