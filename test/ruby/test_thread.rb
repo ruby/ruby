50c5b9a19f4c2851d1ae766b6728ca1ab52d6701
Nobuyoshi Nakada <nobu@ruby-lang.org>
2026-05-19 09:03:38 +0900

    Use `type` instead of `cat` on Windows

--- test/ruby/test_thread.rb
+++ test/ruby/test_thread.rb
@@ -1672,7 +1672,8 @@ q.pop
 
       class ProcessWrapper
         def initialize
-          @stdin, @stdout, @stderr, @wait_thread = Open3.popen3("cat") # hangs until we close our stdin side
+          cat = /mswin/.match?(RUBY_PLATFORM) ? "cat" : "type"
+          @stdin, @stdout, @stderr, @wait_thread = Open3.popen3(cat) # hangs until we close our stdin side
           ObjectSpace.define_finalizer(self, self.class.make_finalizer(@stdin, @stdout, @stderr, @wait_thread))
         end
 
