8c854d2cfb14fdc78c9e82c2f6101bcb6ad9851d
John Hawthorn <john@hawthorn.email>
2026-05-26 10:01:13 -0700

    Bump RUBY_ABI_VERSION

--- include/ruby/internal/abi.h
+++ include/ruby/internal/abi.h
@@ -24,7 +24,7 @@
  * In released versions of Ruby, this number is not defined since teeny
  * versions of Ruby should guarantee ABI compatibility.
  */
-#define RUBY_ABI_VERSION 1
+#define RUBY_ABI_VERSION 2
 
 /* Windows does not support weak symbols so ruby_abi_version will not exist
  * in the shared library. */
