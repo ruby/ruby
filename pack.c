e9444aa0dadf59777d07509e23006e741ff66838
Nobuyoshi Nakada <nobu@ruby-lang.org>
2026-06-10 09:26:25 +0900

    [Feature #22185] Add alignment directives to pack templates

--- pack.c
+++ pack.c
@@ -72,8 +72,12 @@ is_bigendian(void)
 
 #ifdef NATINT_PACK
 # define NATINT_LEN(type,len) (natint?(int)sizeof(type):(int)(len))
+# define NATINT_ALIGN(type,len) (natint?(int)RUBY_ALIGNOF(type):(int)(len))
+# define USING_NATINT(expr) expr
 #else
 # define NATINT_LEN(type,len) ((int)sizeof(type))
+# define NATINT_ALIGN(type,len) ((int)RUBY_ALIGNOF(type))
+# define USING_NATINT(expr) /* void */
 #endif
 
 typedef union {
@@ -218,9 +222,7 @@ pack_modifiers(const char *p, const char *pend, char type, int *natint, int *exp
           case '_':
           case '!':
             if (strchr(natstr, type)) {
-#ifdef NATINT_PACK
-                *natint = 1;
-#endif
+                USING_NATINT(*natint = 1);
                 p++;
             }
             else {
@@ -245,6 +247,91 @@ pack_modifiers(const char *p, const char *pend, char type, int *natint, int *exp
     return p;
 }
 
+#ifndef NATINT_PACK
+# define pack_alignof(t, n) pack_alignof(t)
+#endif
+static size_t
+pack_alignof(char type, int natint)
+{
+    switch (type) {
+      case 'c': case 'C':
+        return RUBY_ALIGNOF(char);
+      case 's': case 'S':
+        return NATINT_ALIGN(short, 2);
+      case 'i': case 'I':
+        return RUBY_ALIGNOF(int);
+      case 'l': case 'L':
+        return NATINT_ALIGN(long, 4);
+      case 'q': case 'Q':
+        return RUBY_ALIGNOF(int64_t);
+      case 'j':
+        return RUBY_ALIGNOF(intptr_t);
+      case 'J':
+        return RUBY_ALIGNOF(uintptr_t);
+      case 'n': case 'v':
+        return RUBY_ALIGNOF(uint16_t);
+      case 'N': case 'V':
+        return RUBY_ALIGNOF(uint32_t);
+      case 'f': case 'F': case 'e': case 'g':
+        return RUBY_ALIGNOF(float);
+      case 'd': case 'D': case 'E': case 'G':
+        return RUBY_ALIGNOF(double);
+      case 'p': case 'P':
+        return RUBY_ALIGNOF(char *);
+      default:
+        return 0;
+    }
+}
+
+static long
+pack_align_pad(long pos, long base, size_t alignment)
+{
+    long offset, mod;
+
+    if (alignment <= 1) return 0;
+    if (alignment > LONG_MAX) rb_raise(rb_eRangeError, "alignment too big");
+    offset = pos - base;
+    mod = offset % (long)alignment;
+    if (mod < 0) mod += alignment;
+    return mod ? (long)alignment - mod : 0;
+}
+
+static char *
+pack_alignment(const char *p, const char *pend, VALUE fmt, size_t *alignment)
+{
+    char type;
+    int explicit_endian = 0;
+    USING_NATINT(int natint = 0);
+
+    if (p >= pend) {
+        rb_raise(rb_eArgError, "missing alignment");
+    }
+    type = *p++;
+    p = pack_modifiers(p, pend, type, &natint, &explicit_endian);
+    if (explicit_endian) {
+        rb_raise(rb_eArgError, "endian modifier is not allowed for alignment");
+    }
+    *alignment = pack_alignof(type, natint);
+    if (!*alignment) {
+        unknown_directive("alignment", type, fmt);
+    }
+    return (char *)p;
+}
+
+static char *
+pack_alignment_size(const char *p, const char *pend, VALUE fmt, size_t *alignment)
+{
+    if (p < pend && ISDIGIT(*p)) {
+        errno = 0;
+        *alignment = STRTOUL(p, (char**)&p, 10);
+        if (*alignment <= 0 || errno) {
+            rb_raise(rb_eRangeError, "invalid alignment");
+        }
+        return (char *)p;
+    }
+    return pack_alignment(p, pend, fmt, alignment);
+}
+
 static VALUE
 pack_pack(rb_execution_context_t *ec, VALUE ary, VALUE fmt, VALUE buffer)
 {
@@ -254,6 +341,7 @@ pack_pack(rb_execution_context_t *ec, VALUE ary, VALUE fmt, VALUE buffer)
     const char *ptr;
     int enc_info = 1;		/* 0 - BINARY, 1 - US-ASCII, 2 - UTF-8 */
     int integer_size, bigendian_p;
+    long align_base;
 
     StringValue(fmt);
     rb_must_asciicompat(fmt);
@@ -271,6 +359,7 @@ pack_pack(rb_execution_context_t *ec, VALUE ary, VALUE fmt, VALUE buffer)
     }
 
     idx = 0;
+    align_base = RSTRING_LEN(res);
 
 #define TOO_FEW (rb_raise(rb_eArgError, toofew), 0)
 #define MORE_ITEM (idx < RARRAY_LEN(ary))
@@ -281,15 +370,25 @@ pack_pack(rb_execution_context_t *ec, VALUE ary, VALUE fmt, VALUE buffer)
 
     while (p < pend) {
         int explicit_endian = 0;
+        size_t align = 0;
         if (RSTRING_END(fmt) != pend) {
             rb_raise(rb_eRuntimeError, "format string modified");
         }
         const char type = *p++; /* get data type */
-#ifdef NATINT_PACK
-        int natint = 0; 	/* native integer */
-#endif
+        USING_NATINT(int natint = 0);	/* native integer */
 
         if (skip_blank(p, type)) continue;
+
+        /* Directives that do not take modifiers. */
+        if ((type == 'x' || type == '@') && p < pend && *p == '!') {
+            p++;
+            p = pack_alignment_size(p, pend, fmt, &align);
+            len = pack_align_pad(RSTRING_LEN(res), type == '@' ? 0 : align_base, align);
+            rb_str_modify_expand(res, len);
+            str_expand_fill(res, '\0', len);
+            continue;
+        }
+
         p = pack_modifiers(p, pend, type, &natint, &explicit_endian);
 
         if (*p == '*') {	/* set data length */
@@ -1005,6 +1104,7 @@ pack_unpack_internal(VALUE str, VALUE fmt, VALUE ofs, enum unpack_mode mode)
     long len;
     AVOID_CC_BUG long tmp_len;
     int signed_p, integer_size, bigendian_p;
+    long align_base;
 #define UNPACK_PUSH(item) do {\
         VALUE item_val = (item);\
         if ((mode) == UNPACK_BLOCK) {\
@@ -1031,6 +1131,7 @@ pack_unpack_internal(VALUE str, VALUE fmt, VALUE ofs, enum unpack_mode mode)
     s = RSTRING_PTR(str);
     send = s + len;
     s += offset;
+    align_base = offset;
 
     p = RSTRING_PTR(fmt);
     pend = p + RSTRING_LEN(fmt);
@@ -1041,12 +1142,24 @@ pack_unpack_internal(VALUE str, VALUE fmt, VALUE ofs, enum unpack_mode mode)
     while (p < pend) {
         int explicit_endian = 0;
         const char type = *p++;
-#ifdef NATINT_PACK
-        int natint = 0;		/* native integer */
-#endif
+        size_t align = 0;
         int star = 0;
+        USING_NATINT(int natint = 0);	/* native integer */
 
         if (skip_blank(p, type)) continue;
+
+        /* Directives that do not take modifiers. */
+        if ((type == 'x' || type == '@') && p < pend && *p == '!') {
+            p++;
+            p = pack_alignment_size(p, pend, fmt, &align);
+            len = pack_align_pad(s - RSTRING_PTR(str), type == '@' ? 0 : align_base, align);
+            if (len > send - s) {
+                rb_raise(rb_eArgError, type == '@' ? "@ outside of string" : "x outside of string");
+            }
+            s += len;
+            continue;
+        }
+
         p = pack_modifiers(p, pend, type, &natint, &explicit_endian);
 
         if (p >= pend)
