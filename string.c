2f96976f1dca77c576b6b630e07c4906ed59ed2d
Peter Zhu <peter@peterzhu.ca>
2026-06-14 14:22:46 +0900

    [DOC] Fix documentation for String#gsub!
    String#gsub! returns self if any substitutions were performed, not characters removed.

--- string.c
+++ string.c
@@ -6505,7 +6505,7 @@ str_gsub(int argc, VALUE *argv, VALUE str, int bang)
  *  Like String#gsub, except that:
  *
  *  - Performs substitutions in +self+ (not in a copy of +self+).
- *  - Returns +self+ if any characters are removed, +nil+ otherwise.
+ *  - Returns +self+ if any substitutions were performed, +nil+ otherwise.
  *
  *  Related: see {Modifying}[rdoc-ref:String@Modifying].
  */
