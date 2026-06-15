452ffa37ea97c4a022bd4112d20270446c050e2f
YO4 <ysno@ac.auone-net.jp>
2026-06-15 22:27:39 +0900

    win32: rb_vm_t.ractor does not have a member "rb_vm_t"
    I wasn't sure whether adding an assertion for `vm->ractor.sync.barrier_waiting` for win32 was correct, so I've simply worked around the compilation error for now.

--- vm_sync.c
+++ vm_sync.c
@@ -34,7 +34,9 @@ RUBY_ASSERT_vm_locking_with_barrier(void)
 
         if (vm->ractor.cnt > 1) {
             /* Written to only when holding both ractor.sync and ractor.sched lock */
+#ifdef RUBY_THREAD_PTHREAD_H
             VM_ASSERT(vm->ractor.sched.barrier_waiting);
+#endif
         }
     }
 }
@@ -100,7 +102,9 @@ vm_lock_enter(rb_ractor_t *cr, rb_vm_t *vm, bool locked, bool no_barrier, unsign
 
             do {
                 VM_ASSERT(vm_need_barrier_waiting(vm));
+#ifdef RUBY_THREAD_PTHREAD_H
                 RUBY_DEBUG_LOG("barrier serial:%u", vm->ractor.sched.barrier_serial);
+#endif
                 rb_ractor_sched_barrier_join(vm, cr);
             } while (vm_need_barrier_waiting(vm));
         }
@@ -272,7 +276,9 @@ rb_vm_barrier(void)
             return;
         }
         else {
+#ifdef RUBY_THREAD_PTHREAD_H
             VM_ASSERT(!vm->ractor.sched.barrier_waiting);
+#endif
             rb_ractor_sched_barrier_start(vm, cr);
         }
     }
