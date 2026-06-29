f2e09df297c8839388eb9d8083f4c5c2b12c746a
Burdette Lamar <BurdetteLamar@Yahoo.com>
2026-06-29 01:44:48 -0500

    [DOC] Update Set#each documentation

--- set.c
+++ set.c
@@ -1471,12 +1471,18 @@ set_each_i(st_data_t key, st_data_t dummy)
 
 /*
  *  call-seq:
- *    each { |o| ... } -> self
+ *    each {|element| ... } -> self
  *    each -> enumerator
  *
- *  Calls the given block once for each element in the set, passing
- *  the element as parameter.  Returns an enumerator if no block is
- *  given.
+ *  With a block given, calls the block once for each element in the set,
+ *  passing the element as a parameter;
+ *  returns +self+:
+ *
+ *    sum = 0
+ *    Set[1, 2, 3].each {|i| sum += i }
+ *    sum => 6
+ *
+ *  With no block given, returns an Enumerator.
  */
 static VALUE
 set_i_each(VALUE set)
