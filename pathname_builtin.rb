48ea06e6d595d8a5f16026353b4bc1603a9562da
BurdetteLamar <burdettelamar@yahoo.com>
2026-07-11 12:00:33 -0500

    Remove trailing space

--- pathname_builtin.rb
+++ pathname_builtin.rb
@@ -2101,7 +2101,7 @@ class Pathname    # * FileTest *
   # Pathname('README.md').socket? # => false
   # Pathname('nosuch').socket?    # => false
   # ```
-  # 
+  #
   # Returns `false` on Windows.
   #
   def socket?() FileTest.socket?(@path) end
