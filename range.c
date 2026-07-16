5878ffaef9ecd12930f236581ef1e14b58d3c1c6
Ajay Krishnan <ajay@krishnan.ca>
2026-07-16 06:11:14 -0700

    [DOC] Fix Range#step return value example

--- range.c
+++ range.c
@@ -422,7 +422,7 @@ range_step_size(VALUE range, VALUE args, VALUE eobj)
  *  Iterates over the elements of range in steps of +s+. The iteration is performed
  *  by <tt>+</tt> operator:
  *
- *    (0..6).step(2) { puts _1 } #=> 1..5
+ *    (0..6).step(2) { puts _1 } #=> 0..6
  *    # Prints: 0, 2, 4, 6
  *
  *    # Iterate between two dates in step of 1 day (24 hours)
