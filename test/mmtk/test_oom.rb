3ca77abb61c13d10350167e0f3a1919f91ce105d
Peter Zhu <peter@peterzhu.ca>
2026-07-29 10:24:07 -0400

    Drop MMTK_HEAP_MAX in test_oom
    Sync ruby/mmtk@11ea3a437f788fb085a61d1c7d931a9527dc13ba.

--- test/mmtk/test_oom.rb
+++ test/mmtk/test_oom.rb
@@ -5,7 +5,7 @@ require_relative "helper"
 module MMTk
   class TestOOM < TestCase
     def test_oom
-      assert_in_out_err([{ "MMTK_HEAP_MAX" => "64MiB" }], <<~RUBY, [], /failed to allocate memory/)
+      assert_in_out_err([{ "MMTK_HEAP_MAX" => "4MiB" }], <<~RUBY, [], /failed to allocate memory/)
         10_000_000.times.map do
           Object.new
         end
