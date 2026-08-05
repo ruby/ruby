f98ed95288a4c301aabf00144cc4d8de837b9be7
XrXr <XrXr@users.noreply.github.com>
2026-08-05 12:23:32 -0400

    Revert recent PRISM changes to fix irb bundled_gems CI
    This reverts commit 0017dea39538b75f54a64364e3878f8421378812,
    5e36630fc012a305ea23aacd1c14cc8f9ad194f1, and
    f3ec056c9f14e8e65a1e4617252f96c72b71d149.
    
    We are trying to bump to latest IRB, which has "Accomodate new prism
    tokens", but that runs into another CI failure. While we figure out a
    path forward at <https://github.com/ruby/ruby/pull/18207>, let's get
    back to a green CI.

--- lib/prism/lex_compat.rb
+++ lib/prism/lex_compat.rb
@@ -78,8 +78,6 @@ module Prism
       BANG_EQUAL: :on_op,
       BANG_TILDE: :on_op,
       BRACE_LEFT: :on_lbrace,
-      BRACE_LEFT_ARGUMENT: :on_lbrace,
-      BRACE_LEFT_HASH: :on_lbrace,
       BRACE_RIGHT: :on_rbrace,
       BRACKET_LEFT: :on_lbracket,
       BRACKET_LEFT_ARRAY: :on_lbracket,
@@ -193,7 +191,6 @@ module Prism
       NEWLINE: :on_nl,
       NUMBERED_REFERENCE: :on_backref,
       PARENTHESIS_LEFT: :on_lparen,
-      PARENTHESIS_LEFT_GROUPING: :on_lparen,
       PARENTHESIS_LEFT_PARENTHESES: :on_lparen,
       PARENTHESIS_RIGHT: :on_rparen,
       PERCENT: :on_op,
@@ -236,7 +233,6 @@ module Prism
       USTAR: :on_op,
       USTAR_STAR: :on_op,
       WORDS_SEP: :on_words_sep,
-      XSTRING_BEGIN: :on_backtick,
       "__END__": :on___end__
     }.freeze
 
