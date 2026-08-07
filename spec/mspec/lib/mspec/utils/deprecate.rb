9b80bb9719a742483dc03e0e406a3f320795af5a
Benoit Daloze <eregontp@gmail.com>
2026-08-07 23:17:16 +0200

    Update to ruby/mspec@b79c65e

--- spec/mspec/lib/mspec/utils/deprecate.rb
+++ spec/mspec/lib/mspec/utils/deprecate.rb
@@ -1,6 +1,10 @@
 module MSpec
+  FATAL_DEPRECATION = ENV['MSPEC_FATAL_DEPRECATION']
+
   def self.deprecate(what, replacement)
     user_caller = caller.find { |line| !line.include?('lib/mspec') }
-    $stderr.puts "\n#{what} is deprecated, use #{replacement} instead.\nfrom #{user_caller}"
+    message = "\n#{what} is deprecated, use #{replacement} instead.\nfrom #{user_caller}"
+    $stderr.puts message
+    raise SpecExpectationNotMetError, message if FATAL_DEPRECATION
   end
 end
