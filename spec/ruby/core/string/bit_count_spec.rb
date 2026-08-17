49d85c6690b12e2781171fdbabed74bcba9cae70
HASUMI Hitoshi <hasumikin@gmail.com>
2026-08-17 16:41:01 +0900

    [Feature #22279] Add region arguments to String bit operations
    This patch extends the following String methods:
    
    * String#bit_set(offset, length, lsb_first: true) -> self
    * String#bit_set(range, lsb_first: true) -> self
    * String#bit_clear(offset, length, lsb_first: true) -> self
    * String#bit_clear(range, lsb_first: true) -> self
    * String#bit_flip(offset, length, lsb_first: true) -> self
    * String#bit_flip(range, lsb_first: true) -> self
    * String#bit_count(offset, length, lsb_first: true) -> Integer
    * String#bit_count(range, lsb_first: true) -> Integer
    
    Link: [Feature #22279]

--- spec/ruby/core/string/bit_count_spec.rb
+++ spec/ruby/core/string/bit_count_spec.rb
@@ -10,9 +10,43 @@ ruby_version_is "4.1" do
       "\xAA\xF0".bit_count.should == 8
     end
 
-    it "raises an ArgumentError when given an argument" do
+    it "counts the set bits in a region given as offset and length" do
+      data = "\xFF\x00\xF0"
+      data.bit_count(0, 8).should == 8
+      data.bit_count(8, 8).should == 0
+      data.bit_count(4, 8).should == 4
+    end
+
+    it "counts the set bits in a region given as a Range" do
+      data = "\xFF\x00\xF0"
+      data.bit_count(0..7).should == 8
+      data.bit_count(8...16).should == 0
+      data.bit_count(16..).should == 4
+    end
+
+    it "clamps a region that extends past the end of the string" do
+      data = "\xFF\x00\xF0"
+      data.bit_count(16, 100).should == 4
+      data.bit_count(100, 8).should == 0
+    end
+
+    it "interprets a non-byte-aligned region according to lsb_first" do
+      "\xF0".bit_count(0, 4).should == 0
+      "\xF0".bit_count(0, 4, lsb_first: false).should == 4
+    end
+
+    it "accepts lsb_first for a whole-string count but does not use it" do
+      "\xFF".bit_count(lsb_first: false).should == 8
+    end
+
+    it "raises an ArgumentError for a lone offset without a length" do
       -> { "\x00".bit_count(0) }.should.raise(ArgumentError)
-      -> { "\x00".bit_count(lsb_first: false) }.should.raise(ArgumentError)
+    end
+
+    it "raises for an invalid region" do
+      -> { "\x00".bit_count(-1, 4) }.should.raise(IndexError)
+      -> { "\x00".bit_count(0, -1) }.should.raise(ArgumentError)
+      -> { "\x00".bit_count(0, 4, lsb_first: nil) }.should.raise(ArgumentError)
     end
   end
 end
