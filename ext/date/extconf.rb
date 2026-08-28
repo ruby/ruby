7af1270680ddfa42b101ec802167df933f32d2b5
Nobuyoshi Nakada <nobu@ruby-lang.org>
2026-08-28 19:31:33 +0900

    [ruby/date] Suppress warnings by clang 21
    https://bugs.ruby-lang.org/issues/21629
    
    https://github.com/ruby/date/commit/214024627a

--- ext/date/extconf.rb
+++ ext/date/extconf.rb
@@ -3,6 +3,9 @@ require 'mkmf'
 
 config_string("strict_warnflags") {|w| $warnflags += " #{w}"}
 
+unless try_compile("", "", werror: true)
+  append_cflags("-Wno-default-const-init-field-unsafe")
+end
 append_cflags("-Wno-compound-token-split-by-macro") if RUBY_VERSION < "2.7."
 have_func("rb_category_warn")
 with_werror("", {:werror => true}) do |opt, |
