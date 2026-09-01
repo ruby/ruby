56fd81b5148120925df87b23701150950ec3ed8a
mugitti9 <mugikurash@gmail.com>
2026-09-01 10:08:58 +0900

    [Bug #22269] Make coverage_enabled: false effective for compile/compile_file (#18533)
    Use it in Coverage.line_stub so compiling the stub does not overwrite already-collected coverage data.
    
    Fixes: [Bug #22269]

--- ext/coverage/lib/coverage.rb
+++ ext/coverage/lib/coverage.rb
@@ -3,7 +3,7 @@ require "coverage.so"
 module Coverage
   def self.line_stub(file)
     lines = File.foreach(file).map { nil }
-    iseqs = [RubyVM::InstructionSequence.compile_file(file)]
+    iseqs = [RubyVM::InstructionSequence.compile_file(file, coverage_enabled: false)]
     until iseqs.empty?
       iseq = iseqs.pop
       iseq.trace_points.each {|n, type| lines[n - 1] = 0 if type == :line }
