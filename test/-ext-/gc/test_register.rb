28bfa22c2c83ce4aa1f688799c3429555eeff363
Matt Valentine-House <matt@eightbitraptor.com>
2026-09-09 22:01:31 +0100

    Run Ractor-creating register tests in subprocesses
    This is a workaround for [Bug #22263].

--- test/-ext-/gc/test_register.rb
+++ test/-ext-/gc/test_register.rb
@@ -12,35 +12,43 @@ class Test_GCRegisterAddress < Test::Unit::TestCase
   end
 
   def test_registered_value_survives_owner_ractor_join
-    r = Ractor.new { Bug::GC.register_static("registered in child".dup) }
-    assert_equal(true, r.value)
+    # assert_separately: keep the test-all process Ractor-free (Bug #22263)
+    assert_separately([], <<~RUBY)
+      Warning[:experimental] = false
+      require '-test-/gc/register'
 
-    2.times { GC.start(full_mark: true) }
-    assert_equal("registered in child", Bug::GC.static_slot_value)
-  ensure
-    Bug::GC.unregister_static
+      r = Ractor.new { Bug::GC.register_static("registered in child".dup) }
+      assert_equal(true, r.value)
+
+      2.times { GC.start(full_mark: true) }
+      assert_equal("registered in child", Bug::GC.static_slot_value)
+    RUBY
   end
 
   def test_unregister_address_from_another_ractor
-    alive = 5.times.count do
-      ref = Thread.new {
-        v = "main owns this".dup
-        Bug::GC.register_static(v)
-        WeakRef.new(v)
-      }.value
-      assert_predicate(ref, :weakref_alive?)
+    assert_separately([], <<~RUBY)
+      Warning[:experimental] = false
+      require 'weakref'
+      require '-test-/gc/register'
 
-      assert_equal(true, Ractor.new { Bug::GC.unregister_static; true }.value)
+      alive = 5.times.count do
+        ref = Thread.new {
+          v = "main owns this".dup
+          Bug::GC.register_static(v)
+          WeakRef.new(v)
+        }.value
+        assert_predicate(ref, :weakref_alive?)
 
-      3.times do
-        GC.start(full_mark: true)
-        break unless ref.weakref_alive?
+        assert_equal(true, Ractor.new { Bug::GC.unregister_static; true }.value)
+
+        3.times do
+          GC.start(full_mark: true)
+          break unless ref.weakref_alive?
+        end
+        ref.weakref_alive?
       end
-      ref.weakref_alive?
-    end
-    assert_operator(alive, :<, 5, "value stayed alive after unregister in every trial")
-  ensure
-    Bug::GC.unregister_static
+      assert_operator(alive, :<, 5, "value stayed alive after unregister in every trial")
+    RUBY
   end
 
   def test_registered_value_survives_fork
@@ -91,25 +99,27 @@ class Test_GCRegisterAddress < Test::Unit::TestCase
 
   def test_verify_internal_consistency_with_ractor_stored_values
     omit "needs GC.verify_internal_consistency" unless GC.respond_to?(:verify_internal_consistency)
-    Bug::GC.register_static(0)
+    assert_separately([], <<~RUBY)
+      Warning[:experimental] = false
+      require '-test-/gc/register'
 
-    port = Ractor::Port.new
-    r = Ractor.new(port) do |port|
-      Bug::GC.assign_static(Ractor.make_shareable("shareable".dup))
-      port.send(:stored)
-      Ractor.receive
-    end
-    port.receive
-    GC.verify_internal_consistency
+      Bug::GC.register_static(0)
 
-    Bug::GC.assign_static("main owns this".dup)
-    GC.verify_internal_consistency
+      port = Ractor::Port.new
+      r = Ractor.new(port) do |port|
+        Bug::GC.assign_static(Ractor.make_shareable("shareable".dup))
+        port.send(:stored)
+        Ractor.receive
+      end
+      port.receive
+      GC.verify_internal_consistency
 
-    r.send(:done)
-    r.value
-  ensure
-    Bug::GC.assign_static(0)
-    Bug::GC.unregister_static
+      Bug::GC.assign_static("main owns this".dup)
+      GC.verify_internal_consistency
+
+      r.send(:done)
+      r.value
+    RUBY
   end
 
   def test_verify_internal_consistency_reports_foreign_unshareable
