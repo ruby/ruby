a473ea27e68ee0bc7d879ad536911126e47e2f6e
Takashi Kokubun <takashikkbn@gmail.com>
2026-09-11 16:33:17 -0700

    ZJIT: Set cfp->sp before exposing a stub frame to the GC

--- zjit/src/codegen.rs
+++ zjit/src/codegen.rs
@@ -3833,12 +3833,14 @@ c_callable! {
             // gen_push_frame() doesn't set PC or ISEQ, so we need to set them before exit.
             // function_stub_hit_body() may allocate and call gc_validate_pc(), so we always set PC and ISEQ.
             // Clear jit_return so the interpreter reads cfp->pc and cfp->iseq directly.
+            // cfp->sp is set to the base pointer, so it needs to be fixed before exit.
             let pc = unsafe { rb_iseq_pc_at_idx(iseq, entry_insn_idx) };
             unsafe { rb_set_cfp_pc(cfp, pc) };
             unsafe { (*cfp)._iseq = iseq };
             unsafe { (*cfp).jit_return = std::ptr::null_mut() };
             let ec_cfp = unsafe { ec.byte_add(RUBY_OFFSET_EC_CFP as usize) as *mut CfpPtr };
             unsafe { *ec_cfp = cfp };
+            unsafe { rb_set_cfp_sp(cfp, sp) };
         }
 
         with_vm_lock(src_loc!(), || {
