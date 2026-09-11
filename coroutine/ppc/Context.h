3261e701186a5e5ff5d701b9e241c7a1c1a22e8b
Samuel Williams <samuel.williams@shopify.com>
2026-09-12 11:26:05 +1200

    ppc/ppc64 coroutines: preserve nonvolatile CR fields. (#18773)

--- coroutine/ppc/Context.h
+++ coroutine/ppc/Context.h
@@ -14,7 +14,7 @@
 enum {
   COROUTINE_REGISTERS =
   20  /* 19 general purpose registers (r13-r31) and 1 return address */
-  + 4  /* space for fiber_entry() to store the link register */
+  + 4  /* caller linkage area, including the condition register save slot */
 };
 
 struct coroutine_context
