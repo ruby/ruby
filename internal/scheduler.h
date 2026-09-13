dba050191c3d34167c31c31ebe42ee41f2a1574d
Samuel Williams <samuel.williams@shopify.com>
2026-09-13 12:22:17 +1200

    Simplify internal scheduler header
    Assisted-By: devx/a8c5bae5-6150-4d42-8280-94eeb4b1962a

--- internal/scheduler.h
+++ internal/scheduler.h
@@ -1,15 +1,7 @@
-#ifndef INTERNAL_SCHEDULER_H                            /*-*-C-*-vi:se ft=cpp:*/
-#define INTERNAL_SCHEDULER_H
-/**
- * @author     Ruby developers <ruby-core@ruby-lang.org>
- * @copyright  This file is a part of the Ruby language project.
- * @brief      Internal interfaces for the Fiber scheduler.
- */
+#pragma once
 
 #include "ruby/ruby.h"
 
 VALUE rb_fiber_scheduler_interrupt_target_new(VALUE fiber, VALUE exception);
 VALUE rb_fiber_scheduler_interrupt_target_exception(VALUE target);
 void rb_fiber_scheduler_interrupt_target_invalidate(VALUE target);
-
-#endif /* INTERNAL_SCHEDULER_H */
