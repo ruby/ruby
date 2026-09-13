b3601aeb96b3341d0c4c01d94a74c80c174e12bb
Jeremy Evans <code@jeremyevans.net>
2026-09-13 08:56:23 -0700

    Emit deprecation warning for aliasing method in prepended module
    Aliasing a method in a prepended module can result in a super
    call going into a descendant instead of an ancestor. Removal plan:
    
    4.1: Deprecation warning
    4.2: Warning even in non-verbose mode
    4.3: Removal (target method lookup starts at origin class)
    
    Fixes [Bug #22273]

--- test/ruby/test_module.rb
+++ test/ruby/test_module.rb
@@ -1587,6 +1587,30 @@ class TestModule < Test::Unit::TestCase
     INPUT
   end
 
+  def test_alias_prepended_module_warning
+    assert_in_out_err([], <<-INPUT, [], /aliasing C#foo defined in a prepended module M is deprecated/)
+      Warning[:deprecated] = true
+      module M
+        def foo = :foo
+      end
+      class C
+        prepend M
+        alias bar foo
+      end
+    INPUT
+
+    assert_in_out_err([], <<-INPUT, [], /aliasing C#foo defined in a prepended module M is deprecated/)
+      Warning[:deprecated] = true
+      module M
+        def foo = :foo
+      end
+      class C
+        prepend M
+        alias_method :bar, :foo
+      end
+    INPUT
+  end
+
   def test_mod_constants
     m = Module.new
     m.const_set(:Foo, :foo)
--- vm_method.c
+++ vm_method.c
@@ -2944,6 +2944,28 @@ rb_alias(VALUE klass, ID alias_name, ID original_name)
 
     if (visi == METHOD_VISI_UNDEF) visi = METHOD_ENTRY_VISI(orig_me);
 
+    if (!NIL_P(ruby_verbose) && rb_warning_category_enabled_p(RB_WARN_CATEGORY_DEPRECATED)) {
+        VALUE owner_class = orig_me->defined_class ? orig_me->defined_class : defined_class;
+        VALUE origin = RCLASS_ORIGIN(target_klass);
+        bool in_prepended_module = false;
+
+        if (origin != target_klass) {
+            for (VALUE p = RCLASS_SUPER(target_klass); !in_prepended_module && p && p != origin; p = RCLASS_SUPER(p)) {
+                if (p == owner_class) {
+                    in_prepended_module = true;
+                }
+            }
+        }
+
+        if (in_prepended_module) {
+            rb_category_warn(RB_WARN_CATEGORY_DEPRECATED,
+                "aliasing %"PRIsVALUE"#%"PRIsVALUE" defined in a prepended module %"PRIsVALUE" is "
+                "deprecated and will be removed in Ruby 4.3",
+                rb_class_path(target_klass), QUOTE_ID(original_name),
+                rb_class_path(orig_me->owner));
+        }
+    }
+
     if (orig_me->defined_class == 0) {
         const rb_method_entry_t *alias_me =
             rb_method_entry_make(target_klass, alias_name, target_klass, visi,
