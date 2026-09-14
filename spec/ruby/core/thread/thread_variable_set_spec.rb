e52991e79c5dd091b2a0e27dae471e4e3dce66f4
Samuel Williams <samuel.williams@shopify.com>
2026-09-14 13:48:17 +1200

    Guard thread variable deletion spec
    Assisted-By: devx/a8c5bae5-6150-4d42-8280-94eeb4b1962a

--- spec/ruby/core/thread/thread_variable_set_spec.rb
+++ spec/ruby/core/thread/thread_variable_set_spec.rb
@@ -43,7 +43,10 @@ describe "Thread#thread_variable_set" do
     @t.thread_variable_set(:a, 52)
     @t.thread_variable_set(:a, nil).should == nil
     @t.thread_variable?(:a).should == false
-    @t.thread_variables.should_not.include?(:a)
+
+    ruby_version_is "4.1" do
+      @t.thread_variables.should_not.include?(:a)
+    end
   end
 
   it "raises a FrozenError if the thread is frozen" do
