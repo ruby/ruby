7c9dff4fb20ead0267b29f47b888d19d1bb3b704
Max Bernstein <ruby@bernsteinbear.com>
2026-09-16 12:35:29 -0400

    ZJIT: Skip Kernel#dup for immediates
        VALUE
        rb_obj_dup(VALUE obj)
        {
            VALUE dup;
    
            if (special_object_p(obj)) {
                return obj;
            }
    
            // ...
        }
    
        static inline int
        special_object_p(VALUE obj)
        {
            if (SPECIAL_CONST_P(obj)) return TRUE;
            switch (BUILTIN_TYPE(obj)) {
              case T_BIGNUM:
              case T_FLOAT:
              case T_SYMBOL:
              case T_RATIONAL:
              case T_COMPLEX:
                /* not a comprehensive list */
                return TRUE;
              default:
                return FALSE;
            }
        }

--- zjit/src/hir/opt_tests.rs
+++ zjit/src/hir/opt_tests.rs
@@ -18457,9 +18457,8 @@ mod hir_opt_tests {
         bb3(v9:BasicObject, v10:BasicObject):
           PatchPoint MethodRedefined(NilClass@0x1008, dup@0x1010, cme:0x1018)
           v23:NilClass = GuardType v10, NilClass recompile
-          v24:BasicObject = CCallWithFrame v23, :Kernel#dup@0x1040
           CheckInterrupts
-          Return v24
+          Return v23
         ");
     }
 
@@ -18485,9 +18484,8 @@ mod hir_opt_tests {
         bb3(v9:BasicObject, v10:BasicObject):
           PatchPoint MethodRedefined(TrueClass@0x1008, dup@0x1010, cme:0x1018)
           v23:TrueClass = GuardType v10, TrueClass recompile
-          v24:BasicObject = CCallWithFrame v23, :Kernel#dup@0x1040
           CheckInterrupts
-          Return v24
+          Return v23
         ");
     }
 
@@ -18513,9 +18511,8 @@ mod hir_opt_tests {
         bb3(v9:BasicObject, v10:BasicObject):
           PatchPoint MethodRedefined(FalseClass@0x1008, dup@0x1010, cme:0x1018)
           v23:FalseClass = GuardType v10, FalseClass recompile
-          v24:BasicObject = CCallWithFrame v23, :Kernel#dup@0x1040
           CheckInterrupts
-          Return v24
+          Return v23
         ");
     }
 
@@ -18541,9 +18538,8 @@ mod hir_opt_tests {
         bb3(v9:BasicObject, v10:BasicObject):
           PatchPoint MethodRedefined(Symbol@0x1008, dup@0x1010, cme:0x1018)
           v23:StaticSymbol = GuardType v10, StaticSymbol recompile
-          v24:BasicObject = CCallWithFrame v23, :Kernel#dup@0x1040
           CheckInterrupts
-          Return v24
+          Return v23
         ");
     }
 
