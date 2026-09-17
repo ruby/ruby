544f62b72b4f8ee512d1a2f9010ec59c9b850279
Jean Boussier <byroot@ruby-lang.org>
2026-09-17 09:07:55 +0200

    Implement RubyVM::YJIT.max_compile_time_ns
    [Feature #22236]
    
    Allows to give a time budget to YJIT. When YJIT goes over the budget
    it pauses compilation until the budget is raised.
    
    It is best effort, YJIT may go over, or have no choice but to compile
    in some cases, but the goal is to be able to limit the CPU time overhead
    of YJIT during warmup, so that it's spread over a longer period of
    time and doesn't impact service latency as much.

--- yjit.h
+++ yjit.h
@@ -28,6 +28,8 @@ extern unsigned int rb_yjit_call_threshold;
 extern unsigned int rb_yjit_cold_threshold;
 extern uint64_t rb_yjit_live_iseq_count;
 extern uint64_t rb_yjit_iseq_alloc_count;
+extern uint64_t rb_yjit_max_compile_time_ns;
+extern uint64_t rb_yjit_total_compile_time_ns;
 extern bool rb_yjit_enabled_p;
 extern bool rb_yjit_compiling_p;
 void rb_yjit_incr_counter(const char *counter_name);
