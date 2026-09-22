0d412b37d3cc411605bdef10abfa7980086d0f08
John Hawthorn <john@hawthorn.email>
2026-09-22 09:58:58 -0700

    Prevent uninitialized classes from being allocated

--- object.c
+++ object.c
@@ -2199,15 +2199,16 @@ rb_mod_initialize_clone(int argc, VALUE* argv, VALUE clone)
  *  want to treat it like a regular class.
  */
 
+/* :nodoc: */
 static VALUE
-rb_class_initialize(int argc, VALUE *argv, VALUE klass)
+rb_class_s_new(int argc, VALUE *argv, VALUE self)
 {
     VALUE super;
+    VALUE klass;
 
-    // an uninitialized class is still somebody's, and this writes its superclass
-    rb_class_modify_check(klass);
-    if (RCLASS_SUPER(klass) != 0 || klass == rb_cBasicObject) {
-        rb_raise(rb_eTypeError, "already initialized class");
+    // #<Class:Class> is an ancestor of every metaclass, so this also receives Foo.singleton_class.new
+    if (self != rb_cClass) {
+        rb_raise(rb_eTypeError, "can't create instance of singleton class");
     }
     if (rb_check_arity(argc, 0, 1) == 0) {
         super = rb_cObject;
@@ -2219,6 +2220,7 @@ rb_class_initialize(int argc, VALUE *argv, VALUE klass)
             rb_raise(rb_eTypeError, "can't inherit uninitialized class");
         }
     }
+    klass = rb_class_s_alloc(self);
     rb_class_set_super(klass, super);
     RCLASS_SET_MAX_IV_COUNT(klass, RCLASS_MAX_IV_COUNT(super));
     RCLASS_SET_ALLOCATOR(klass, RCLASS_ALLOCATOR(super));
@@ -2227,6 +2229,23 @@ rb_class_initialize(int argc, VALUE *argv, VALUE klass)
     rb_mod_initialize_exec(klass);
 
     return klass;
+}
+
+/* :nodoc: */
+static VALUE
+rb_class_dup(VALUE self)
+{
+    VALUE dup = rb_class_s_alloc(rb_obj_class(self));
+    return rb_obj_dup_setup(self, dup);
+}
+
+/* :nodoc: */
+static VALUE
+rb_class_clone(int argc, VALUE *argv, VALUE self)
+{
+    VALUE kwfreeze = rb_get_freeze_opt(argc, argv);
+    VALUE clone = rb_class_s_alloc(rb_obj_class(self));
+    return rb_obj_clone_setup(self, clone, kwfreeze);
 }
 
 /*! \private */
@@ -4686,14 +4705,15 @@ InitVM_Object(void)
     rb_define_method(rb_cModule, "deprecate_constant", rb_mod_deprecate_constant, -1); /* in variable.c */
     rb_define_method(rb_cModule, "singleton_class?", rb_mod_singleton_p, 0);
 
-    rb_define_method(rb_singleton_class(rb_cClass), "allocate", rb_class_alloc, 0);
     rb_define_method(rb_cClass, "allocate", rb_class_alloc, 0);
     rb_define_method(rb_cClass, "new", rb_class_new_instance_pass_kw, -1);
-    rb_define_method(rb_cClass, "initialize", rb_class_initialize, -1);
     rb_define_method(rb_cClass, "superclass", rb_class_superclass, 0);
     rb_define_method(rb_cClass, "subclasses", rb_class_subclasses, 0); /* in class.c */
     rb_define_method(rb_cClass, "attached_object", rb_class_attached_object, 0); /* in class.c */
-    rb_define_alloc_func(rb_cClass, rb_class_s_alloc);
+    rb_define_singleton_method(rb_cClass, "new", rb_class_s_new, -1);
+    rb_define_method(rb_cClass, "dup", rb_class_dup, 0);
+    rb_define_method(rb_cClass, "clone", rb_class_clone, -1);
+    rb_undef_alloc_func(rb_cClass);
     rb_undef_method(rb_cClass, "extend_object");
     rb_undef_method(rb_cClass, "append_features");
     rb_undef_method(rb_cClass, "prepend_features");
--- test/ruby/test_class.rb
+++ test/ruby/test_class.rb
@@ -317,14 +317,18 @@ class TestClass < Test::Unit::TestCase
   end
 
   def test_uninitialized
-    assert_raise(TypeError) { Class.allocate.new }
-    assert_raise(TypeError) { Class.allocate.superclass }
-    bug6863 = '[ruby-core:47148]'
-    assert_raise(TypeError, bug6863) { Class.new(Class.allocate) }
+    assert_raise(NoMethodError) { Class.allocate }
 
     allocator = Class.instance_method(:allocate)
+    assert_raise(TypeError) { allocator.bind_call(Class) }
     assert_nothing_raised { allocator.bind(Rational).call }
     assert_nothing_raised { allocator.bind_call(Rational) }
+  end
+
+  def test_singleton_class_new
+    assert_raise_with_message(TypeError, /singleton class/) { Integer.singleton_class.new }
+    assert_raise_with_message(TypeError, /singleton class/) { Class.new.singleton_class.new }
+    assert_raise_with_message(TypeError, /singleton class/) { Class.singleton_class.new }
   end
 
   def test_nonascii_name
