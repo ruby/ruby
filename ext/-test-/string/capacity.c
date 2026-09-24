d9bff46ae1d89b86daba09f3eed9ed666b0d8162
Randy Stauner <randy.stauner@shopify.com>
2026-09-24 16:26:10 -0700

    [Bug #22383] Fix inverted `STR_SHARED` check in `rb_str_tmp_frozen_release`
    45a2c95d0f7184c9cd64ddd26699af31bea8675d mistakenly rewrote
    
        FL_TEST_RAW(orig, STR_SHARED) && !FL_TEST_RAW(orig, STR_TMPLOCK|RUBY_FL_FREEZE)
    
    as
    
        FL_TEST_RAW(orig, STR_SHARED | STR_TMPLOCK) == STR_TMPLOCK
    
    requiring orig to not be shared, the opposite of the original
    condition.  orig always shares the buffer with tmp here, so the buffer
    was never given back and the string stayed shared until its next
    modification copied it.  The code then read aux.shared from strings
    that are not shared, where the union holds aux.capa.
    
    This should be backported to 4.0

--- ext/-test-/string/capacity.c
+++ ext/-test-/string/capacity.c
@@ -11,8 +11,17 @@ bug_str_capacity(VALUE klass, VALUE str)
     return LONG2FIX(rb_str_capacity(str));
 }
 
+static VALUE
+bug_str_tmp_frozen_acquire_release(VALUE klass, VALUE str)
+{
+    VALUE tmp = rb_str_tmp_frozen_acquire(str);
+    rb_str_tmp_frozen_release(str, tmp);
+    return str;
+}
+
 void
 Init_string_capacity(VALUE klass)
 {
     rb_define_singleton_method(klass, "capacity", bug_str_capacity, 1);
+    rb_define_singleton_method(klass, "tmp_frozen_acquire_release", bug_str_tmp_frozen_acquire_release, 1);
 }
